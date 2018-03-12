4bf2312952e83a01d0fc9d8345552439a916206e
Thuy.Copeland <thuycopeland@gmail.com>
2018-03-12 14:34:32 -0500

    Add woo purple theme color to nav bar. Stub in Order List Cell

---
+++ WooCommerce/Classes/Helpers/ThemeColors.swift
@@ -0,0 +1,6 @@
+import UIKit
+import DynamicColor
+
+class ThemeColors: NSObject {
+    static let wooCommercePurple = UIColor(hexString: "#96588A")
+}
---
+++ WooCommerce/Classes/ViewRelated/Orders/OrderListCell.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class OrderListCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var statusLabel: UILabel!
+}

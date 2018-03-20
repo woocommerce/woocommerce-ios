33e050bd0c7ac5312b7d30c75c1e6745e7936571
Thuy.Copeland <thuycopeland@gmail.com>
2018-03-20 16:16:57 -0400

    Remove DynamicColor pod / remove dependencies

--- WooCommerce/Classes/Helpers/ThemeColors.swift
+++ WooCommerce/Classes/Helpers/ThemeColors.swift
@@ -1,6 +1,5 @@
 import UIKit
-import DynamicColor
 
-class ThemeColors: NSObject {
-    static let wooCommercePurple = UIColor(hexString: "#96588A")
+class ThemeColors {
+    static let wooCommercePurple = UIColor(red: 0x96/255.0, green: 0x58/255.0, blue: 0x8A/255.0, alpha: 0xFF/255.0)
 }

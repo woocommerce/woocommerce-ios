67016107c2895e6295af7167cf8e41a8c1f9dae2
Thuy.Copeland <thuycopeland@gmail.com>
2018-03-28 12:18:27 -0500

    Tweak hard coded data

--- WooCommerce/Classes/ViewRelated/Orders/OrdersViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/OrdersViewController.swift
@@ -14,7 +14,7 @@ class OrdersViewController: UIViewController {
         //FIXME: this is a hack so you can see a row working on the orders list table.
         let billingAddress = Address(firstName: "Thuy", lastName: "Copeland", company: "", address1: "500 Hollywood Blvd", address2: "", city: "Las Vegas", state: "NE", postcode: "90210", country: "US")
         let shippingAddress = Address(firstName: "Thuy", lastName: "Copeland", company: "", address1: "500 Hollywood Blvd", address2: "", city: "Las Vegas", state: "NE", postcode: "90210", country: "US")
-        let customer = Customer(identifier: "1", firstName: "Thuy", lastName: "Copeland", email: nil, phone: nil, billingAddress: nil, shippingAddress: nil)
+        let customer = Customer(identifier: "1", firstName: "Thuy", lastName: "Copeland", email: nil, phone: nil, billingAddress: billingAddress, shippingAddress: shippingAddress)
         let item = OrderItem(lineItemId: 1, name: "Belt", productID: 27, quantity: 1, sku: "", subtotal: "55.00", subtotalTax: "0.00", taxClass: "", total: "55.00", totalTax: "", variationID: 0)
         let order = Order(identifier: "190", number: "190", status: .processing, customer: customer, dateCreated: Date.init(), dateUpdated: Date.init(), shippingAddress: shippingAddress, billingAddress: billingAddress, items: [item], currency: "USD", total: 91.32, notes: [])
 

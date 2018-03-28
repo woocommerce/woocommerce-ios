6fe756a7ea52b3d673e21a160396d2686fc818e4
Thuy.Copeland <thuycopeland@gmail.com>
2018-03-28 11:31:42 -0500

    Remove styling from IB prototype cell and apply styling programmatically

--- WooCommerce/Classes/Extensions/UIFont+Helpers.swift
+++ WooCommerce/Classes/Extensions/UIFont+Helpers.swift
@@ -1,6 +1,22 @@
 import UIKit
 
 extension UIFont {
+    static var largeTitle: UIFont {
+        return UIFont.preferredFont(forTextStyle: .largeTitle)
+    }
+
+    static var title1: UIFont {
+        return UIFont.preferredFont(forTextStyle: .title1)
+    }
+
+    static var title2: UIFont {
+        return UIFont.preferredFont(forTextStyle: .title2)
+    }
+
+    static var title3: UIFont {
+        return UIFont.preferredFont(forTextStyle: .title3)
+    }
+
     static var headline: UIFont {
         return UIFont.preferredFont(forTextStyle: .headline)
     }
@@ -13,7 +29,19 @@ extension UIFont {
         return UIFont.preferredFont(forTextStyle: .body)
     }
 
+    static var callout: UIFont {
+        return UIFont.preferredFont(forTextStyle: .callout)
+    }
+
+    static var footnote: UIFont {
+        return UIFont.preferredFont(forTextStyle: .footnote)
+    }
+
     static var caption1: UIFont {
         return UIFont.preferredFont(forTextStyle: .caption1)
+    }
+
+    static var caption2: UIFont {
+        return UIFont.preferredFont(forTextStyle: .caption2)
     }
 }

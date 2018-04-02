8d85c81851e1bd71d7927ad14005f852dfdb1095
Thuy.Copeland <thuycopeland@gmail.com>
2018-04-02 10:11:53 -0500

    Configure and style search bar

--- WooCommerce/Classes/ViewRelated/Orders/OrdersViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/OrdersViewController.swift
@@ -5,11 +5,15 @@ class OrdersViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     var orders = [Order]()
+    var searchResults = [Order]()
+    let searchController = UISearchController(searchResultsController: nil)
+    var showSearchResults = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigation()
+        configureSearch()
 
         //FIXME: this is a hack so you can see a row working on the orders list table.
         let billingAddress = Address(firstName: "Thuy", lastName: "Copeland", company: "", address1: "500 Hollywood Blvd", address2: "", city: "Las Vegas", state: "NE", postcode: "90210", country: "US")
@@ -29,6 +33,24 @@ class OrdersViewController: UIViewController {
                                              action: #selector(rightButtonTapped))
         rightBarButton.tintColor = UIColor.white
         navigationItem.setRightBarButton(rightBarButton, animated: false)
+    }
+
+    func configureSearch() {
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = true
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = NSLocalizedString("Search all orders", comment: "Search placeholder text")
+        searchController.searchBar.sizeToFit() // get the proper size for displaying in table header
+
+        // MARK: This may need set app-wide in the future. Not yet.
+        searchController.searchBar.barTintColor = tableView.backgroundColor
+        searchController.searchBar.layer.borderWidth = 1
+        searchController.searchBar.layer.borderColor = tableView.backgroundColor?.cgColor
+
+        // add it to the table header, which leaves us room for pull-to-refresh control
+        tableView.tableHeaderView = searchController.searchBar
+        tableView.contentOffset = CGPoint(x: 0.0, y: searchController.searchBar.frame.size.height)
     }
 
     @objc func rightButtonTapped() {
@@ -66,12 +88,19 @@ class OrdersViewController: UIViewController {
 
 extension OrdersViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if showSearchResults {
+            return searchResults.count
+        }
         return orders.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: OrderListCell.reuseIdentifier, for: indexPath) as! OrderListCell
-        cell.configureCell(order: orders[indexPath.row])
+        if showSearchResults {
+            cell.configureCell(order: searchResults[indexPath.row])
+        } else {
+            cell.configureCell(order: orders[indexPath.row])
+        }
 
         return cell
     }
@@ -88,5 +117,39 @@ extension OrdersViewController: UITableViewDelegate {
             return 32
         }
         return 12
+    }
+}
+
+extension OrdersViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchString = searchController.searchBar.text {
+            // TODO: filter search results properly
+            searchResults = orders.filter({ (order) -> Bool in
+                let orderText: NSString = order.customer.firstName as NSString
+                return (orderText.range(of: searchString, options:NSString.CompareOptions.caseInsensitive).location) != NSNotFound
+            })
+            tableView.reloadData()
+        }
+    }
+}
+
+extension OrdersViewController: UISearchBarDelegate {
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        if searchBar.text == nil || searchBar.text == "" {
+            showSearchResults = false
+        } else {
+            showSearchResults = true
+        }
+
+        tableView.reloadData()
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        if showSearchResults {
+            showSearchResults = false
+            searchController.searchBar.text = nil
+        }
+        searchController.searchBar.resignFirstResponder()
+        tableView.reloadData()
     }
 }

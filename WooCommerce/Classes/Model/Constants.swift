9b71c09dc737b1f52ffb22575ea63afc531cdd13
Thuy.Copeland <thuycopeland@gmail.com>
2018-04-05 12:06:35 -0500

    Code review changes round 2
    - remove constants file and create extension inside of class
    - rename "Helpers" to "Styles"
    - Swiftify more code
    - remove extra spacing
    - adjust comments
    - trash boilerplate code in Notifications view controller

--- WooCommerce/Classes/Model/Constants.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-// MARK: Woo Constants
-//
-struct Constants {
-    static let groupedFirstSectionHeaderHeight: CGFloat = 32.0
-    static let groupedSectionHeaderHeight: CGFloat = 12.0
-}

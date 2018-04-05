09a7dc6d8c7a8f7ff9cbddfcec5deab56989783d
Thuy.Copeland <thuycopeland@gmail.com>
2018-04-05 15:02:40 -0500

    Add status label colors from Woo App Design Guide

--- WooCommerce/Classes/Styles/Style.swift
+++ WooCommerce/Classes/Styles/Style.swift
@@ -20,14 +20,14 @@ protocol Style {
 class DefaultStyle: Style {
     // Android uses flat colors with no alpha, so all alphas default to 1.0
     let wooCommerceBrandColor = UIColor(red: 0x96/255.0, green: 0x58/255.0, blue: 0x8A/255.0, alpha: 0xFF/255.0)
-    let statusDangerColor = UIColor(red: 251.0/255.0, green: 229.0/255.0, blue: 227.0/255.0, alpha: 1.0)
-    let statusDangerBoldColor = UIColor(red: 247.0/255.0, green: 204.0/255.0, blue: 201.0/255.0, alpha: 1.0)
-    let statusPrimaryColor = UIColor(red: 245.0/255.0, green: 250.0/255.0, blue: 250.0/255.0, alpha: 1.0)
-    let statusPrimaryBoldColor = UIColor(red: 198.0/255.0, green: 220.0/255.0, blue: 233.0/255.0, alpha: 1.0)
-    let statusSuccessColor = UIColor(red: 247.00/255.0, green: 255.0/255.0, blue: 247.0/255.0, alpha: 1.0)
-    let statusSuccessBoldColor = UIColor(red: 208.0/255.0, green: 231.0/255.0, blue: 185.0/255.0, alpha: 1.0)
-    let statusNotIdentifiedColor = UIColor(red: 232.0/255.0, green: 232.0/255.0, blue: 232.0/255.0, alpha: 1.0)
-    let statusNotIdentifiedBoldColor = UIColor(red: 221.0/255.0, green: 221.0/255.0, blue: 221.0/255.0, alpha: 1.0)
+    let statusDangerColor = UIColor(red: 255.0/255.0, green: 230.0/255.0, blue: 229.0/255.0, alpha: 1.0)
+    let statusDangerBoldColor = UIColor(red: 255.0/255.0, green: 197.0/255.0, blue: 195.0/255.0, alpha: 1.0)
+    let statusPrimaryColor = UIColor(red: 244.0/255.0, green: 249.0/255.0, blue: 251.0/255.0, alpha: 1.0)
+    let statusPrimaryBoldColor = UIColor(red: 188.0/255.0, green: 222.0/255.0, blue: 238.0/255.0, alpha: 1.0)
+    let statusSuccessColor = UIColor(red: 239.00/255.0, green: 249.0/255.0, blue: 230.0/255.0, alpha: 1.0)
+    let statusSuccessBoldColor = UIColor(red: 201.0/255.0, green: 233.0/255.0, blue: 169.0/255.0, alpha: 1.0)
+    let statusNotIdentifiedColor = UIColor(red: 235.0/255.0, green: 235.0/255.0, blue: 235.0/255.0, alpha: 1.0)
+    let statusNotIdentifiedBoldColor = UIColor(red: 226.0/255.0, green: 226.0/255.0, blue: 226.0/255.0, alpha: 1.0)
     let defaultTextColor = UIColor.black
 }
 
@@ -94,4 +94,3 @@ class StyleManager {
         }
     }
 }
-

bd4e0c1cb1d66a7001687b33ee8adf228a734108
Thuy.Copeland <thuycopeland@gmail.com>
2018-04-13 14:58:58 -0500

    Simplify Customer Info cell

--- WooCommerce/Classes/ViewRelated/Orders/SingleOrder/SingleOrderCustomerInfoCell.swift
+++ WooCommerce/Classes/ViewRelated/Orders/SingleOrder/SingleOrderCustomerInfoCell.swift
@@ -2,23 +2,14 @@ import UIKit
 
 class SingleOrderCustomerInfoCell: UITableViewCell {
     @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var addressLabel: UILabel!
-    @IBOutlet private weak var phoneLabel: UILabel!
-    @IBOutlet private weak var emailLabel: UILabel!
-    @IBOutlet private weak var moreButton: UIButton!
-    @IBOutlet private weak var emailButton: UIButton!
-    @IBOutlet private weak var borderLine: UIView!
-    @IBOutlet private weak var borderLine2: UIView!
-
-    var title: String!
-    var address: String!
-    var phone: String? = nil
-    var email: String? = nil
-    var displaysExtraBorders: Bool!
 
     static let reuseIdentifier = "SingleOrderCustomerInfoCell"
 
-    override func awakeFromNib() {
-        
+    func configureCell(title: String!, name: String!, address: String!, phone: String?, email: String?, displayExtraBorders: Bool!) {
+        titleLabel.text = title
+        nameLabel.text = name
+        addressLabel.text = address
     }
 }

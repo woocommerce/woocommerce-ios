8379a6bcc99a0f43701c9d234457eab3cc93dccc
Thuy.Copeland <thuycopeland@gmail.com>
2018-04-17 14:46:35 -0500

    Code review - remove Order extension file and move into Order model

--- WooCommerce/Classes/Model/Order.swift
+++ WooCommerce/Classes/Model/Order.swift
@@ -55,6 +55,60 @@ struct Order: Decodable {
     }
 }
 
+extension Order {
+    var status: OrderStatus {
+        get {
+            switch statusString {
+            case "pending":
+                return .pending
+            case "processing":
+                return .processing
+            case "on-hold":
+                return .onHold
+            case "completed":
+                return .completed
+            case "cancelled":
+                return .canceled
+            case "refunded":
+                return .refunded
+            case "failed":
+                return .failed
+            default:
+                NSLog("Custom order status: %@", statusString)
+                return .custom
+            }
+        }
+    }
+
+    var currencySymbol: String {
+        let locale = NSLocale(localeIdentifier: self.currency)
+        return locale.displayName(forKey: .currencySymbol, value: self.currency) ?? String()
+    }
+
+    enum OrderStructKeys: String, CodingKey {
+        case identifier = "id"
+        case number = "number"
+        case statusString = "status"
+        case customer = "customer"
+        case customerID = "customer_id"
+        case dateCreatedString = "date_created"
+        case dateUpdatedString = "date_modified"
+        case shippingAddress = "shipping"
+        case billingAddress = "billing"
+        case orderItems = "line_items"
+        case currency = "currency"
+        case total = "total"
+        case notes = "customer_note"
+    }
+
+    var dateCreated: Date {
+        get {
+            // TODO: use WordPressShared date helpers to convert dateCreatedString into a Date
+            return Date()
+        }
+    }
+}
+
 // MARK: -
 //
 enum OrderStatus: String {

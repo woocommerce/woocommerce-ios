21a5b67131304cb89b43c69372b02484b3a161af
Jorge Leandro Perez <jorge@lantean.co>
2018-04-30 18:10:49 -0300

    Implements JetpackEndpointTests

---
+++ Networking/NetworkingTests/Endpoint/JetpackEndpointTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Networking
+
+
+/// JetpackEndpoint Unit Tests
+///
+class JetpackEndpointTests: XCTestCase {
+    
+    func testExample() {
+        let test = JetpackEndpoint(wooApiVersion: .mark2, method: .get, siteID: 123, endpoint: "test")
+    }
+}

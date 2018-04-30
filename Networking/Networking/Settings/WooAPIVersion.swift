dcdc876e9dfb3c5f5b8ecd42cf2180aa6d927ec7
Jorge Leandro Perez <jorge@lantean.co>
2018-04-30 13:53:28 -0300

    Implements WooAPIVersion

---
+++ Networking/Networking/Settings/WooAPIVersion.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+
+/// Defines the supported Woo API Versions.
+///
+enum WooAPIVersion: String {
+
+    /// Woo Endpoint Mark 2
+    ///
+    case mark2 = "wc/v2/"
+
+    /// Returns the path for the current API Version
+    ///
+    var path: String {
+        return rawValue
+    }
+}

b3610fe3acbc9ac69e593552faf2f6ff8867384a
Jorge Leandro Perez <jorge@lantean.co>
2018-04-30 17:56:14 -0300

    Remote: Documenting Parameters

--- Networking/Networking/Remote/Remote.swift
+++ Networking/Networking/Remote/Remote.swift
@@ -2,31 +2,40 @@ import Foundation
 import Alamofire
 
 
-///
+/// Represents a collection of Remote Endpoints
 ///
 public class Remote {
 
-    ///
+    /// WordPress.com Credentials
     ///
     let credentials: Credentials
 
+    /// Designated Initializer.
     ///
+    /// - Parameter credentials: Credentials to be used in order to authenticate every request.
     ///
     public init(credentials: Credentials) {
         self.credentials = credentials
     }
 
+    /// Returns the HTTPHeaders containing our Authorization Token.
     ///
+    var headers: HTTPHeaders {
+        return ["Authorization": "Bearer \(credentials.authToken)"]
+    }
+
+    /// Submits a request over the Network!
     ///
-    func request<T>(endpoint: URLConvertible, method: HTTPMethod = .get, completion: @escaping (T) -> Void) {
-        Alamofire.request(endpoint, method: method)
+    /// - Parameters:
+    ///     - endpoint: Remote Endpoint that should be queried.
+    ///     - method: HTTP Method to use.
+    ///     - completion: Closure to be executed upon completion.
+    ///
+    func request(endpoint: URLConvertible, method: HTTPMethod = .get, completion: @escaping (Any?, Error?) -> Void) {
+        Alamofire.request(endpoint, method: method, headers: headers)
             .validate()
             .responseJSON { response in
-                guard let payload = response.result.value as? T else {
-                    return
-                }
-
-                completion(payload)
+                completion(response.result.value, response.result.error)
         }
     }
 }

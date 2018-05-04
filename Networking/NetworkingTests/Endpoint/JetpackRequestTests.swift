bd1d46c8273850fd65cfc6c184d194ecdbcef602
Jorge Leandro Perez <jorge@lantean.co>
2018-05-04 12:00:38 -0300

    Implements JetpackRequestTests

--- Networking/NetworkingTests/Endpoint/JetpackRequestTests.swift
+++ Networking/NetworkingTests/Endpoint/JetpackRequestTests.swift
@@ -1,7 +1,122 @@
 import XCTest
 @testable import Networking
+
+
 /// JetpackRequest Unit Tests
 ///
 class JetpackRequestTests: XCTestCase {
-    
+
+    /// Testing API Version
+    ///
+    private let sampleWooApiVersion = WooAPIVersion.mark2
+
+    /// Sample SiteID
+    ///
+    private let sampleSiteID = 1234
+
+    /// RPC Sample Method Path
+    ///
+    private let sampleRPC = "sample"
+
+    /// Sample Parameters
+    ///
+    private let sampleParameters = ["some": "thing", "yo": "semite"]
+
+    /// Base URL: Mapping the Sample Site + Jetpack Tunneling API
+    ///
+    private var jetpackEndpointBaseURL: String {
+        return DotcomRequest.wordpressApiBaseURL + JetpackRequest.wordpressApiVersion.path + "jetpack-blogs/" + String(sampleSiteID) + "/rest-api/"
+    }
+
+
+
+    /// Verifies that a POST JetpackRequest will query the Jetpack Tunneled WordPress.com API.
+    ///
+    func testPostRequestQueriesDotcomJetpackTunnelEndpointWithNoExtraQueryParameters() {
+        let request = JetpackRequest(wooApiVersion: .mark2, method: .post, siteID: sampleSiteID, path: sampleRPC, parameters: sampleParameters)
+
+        let expectedURL = URL(string: jetpackEndpointBaseURL)!
+        let generatedURL = try! request.asURLRequest().url!
+        XCTAssertEqual(generatedURL, expectedURL)
+    }
+
+    /// Verifies that a POST JetpackRequest will serialize all of the Tunneling Parameters in the request body.
+    ///
+    func testPostRequestQueriesDotcomJetpackTunnelEndpointWithItsParametersInTheBody() {
+        let request = JetpackRequest(wooApiVersion: .mark2, method: .post, siteID: sampleSiteID, path: sampleRPC, parameters: sampleParameters)
+
+        guard let urlRequest = try? request.asURLRequest(),
+            let generatedBodyAsData = urlRequest.httpBody,
+            let generatedBody = String(data: generatedBodyAsData, encoding: .utf8)
+        else {
+            XCTFail()
+            return
+        }
+
+        let expectedBody = httpBody(for: request)
+        XCTAssertEqual(expectedBody, generatedBody)
+    }
+
+    /// Verifis that a GET JetpackRequest will query the Jetpack Tunneled WordPress.com API, with the expected query parameters.
+    ///
+    func testGetRequestQueriesDotcomJetpackTunnelEndpointWithExpectedQueryParameters() {
+        let request = JetpackRequest(wooApiVersion: .mark2, method: .get, siteID: sampleSiteID, path: sampleRPC)
+
+        let expectedURL = URL(string: jetpackEndpointBaseURL + queryParameters(for: request))!
+        let generatedURL = try! request.asURLRequest().url!
+        XCTAssertEqual(generatedURL, expectedURL)
+    }
+
+    /// Verifis that a GET JetpackRequest will not serialize anything in the body.
+    ///
+    func testGetRequestDoesNotSerializeAnythingInTheBody() {
+        let request = JetpackRequest(wooApiVersion: .mark2, method: .get, siteID: sampleSiteID, path: sampleRPC, parameters: sampleParameters)
+
+        let output = try! request.asURLRequest()
+        XCTAssertNil(output.httpBody)
+    }
+}
+
+
+// Parameter Encoding Helpers
+//
+private extension JetpackRequestTests {
+
+    /// Returns the expected Query Parameters for a given JetpackRequest.
+    ///
+    func queryParameters(for request: JetpackRequest) -> String {
+        switch request.method {
+        case .get:
+            let parameters = concatenate(request.parameters).addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+            return "?_method=" + request.method.rawValue.lowercased() + "&json=true&path=" + sampleWooApiVersion.path + sampleRPC + parameters
+        default:
+            return String()
+        }
+    }
+
+    /// Returns the expected HTTP Body for a given Jetpack Request.
+    ///
+    func httpBody(for request: JetpackRequest) -> String {
+        guard request.method == .post else {
+            return String()
+        }
+
+        let parametersAsData = try! JSONEncoder().encode(request.parameters)
+        let parametersAsString = String(data: parametersAsData, encoding: .utf8)!
+        let parametersAsPercentEncoded = parametersAsString.addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+
+        return "_method=" + request.method.rawValue.lowercased() +
+            "&body=" + parametersAsPercentEncoded +
+            "&json=true" +
+            "&path=" + sampleWooApiVersion.path + sampleRPC
+
+    }
+
+    /// Concatenates the specified collection of Parameters for the URLRequest's httpBody.
+    ///
+    func concatenate(_ parameters: [String: String]) -> String {
+        return parameters.reduce("") { (output, parameter) in
+            return output + "&" + parameter.key + "=" + parameter.value
+        }
+    }
 }

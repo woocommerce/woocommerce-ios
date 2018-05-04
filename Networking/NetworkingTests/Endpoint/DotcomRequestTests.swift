4a833ed3b927a7032b7d8f722db349f5de004466
Jorge Leandro Perez <jorge@lantean.co>
2018-05-04 10:12:00 -0300

    DotcomRequestTests: New Unit Tests

--- Networking/NetworkingTests/Endpoint/DotcomRequestTests.swift
+++ Networking/NetworkingTests/Endpoint/DotcomRequestTests.swift
@@ -1,9 +1,9 @@
 import Foundation
-@testable import Networking
 import XCTest
+@testable import Networking
 
 
-/// WordPress.com Endpoint Unit Tests
+/// WordPress.com Requests Unit Tests
 ///
 class DotcomRequestTests: XCTestCase {
 
@@ -11,23 +11,76 @@ class DotcomRequestTests: XCTestCase {
     ///
     private let sampleRPC = "sample"
 
-
-    /// Verifies that the Endpoint's generated URL starts with WordPress.com API BaseURL.
+    /// Sample Parameters
     ///
-    func testEndpointUrlStartsWithWordPressBaseURL() {
-        let endpoint = DotcomRequest(wordpressApiVersion: .mark1_1, method: .get, path: sampleRPC)
-        let absoluteString = try! endpoint.asURLRequest().url!.absoluteString
+    private let sampleParameters = ["some": "thing", "yo": "semite"]
 
-        XCTAssertTrue(absoluteString.hasPrefix(endpoint.wordpressApiBaseURL))
+    /// Sample Parameters as a Query String
+    ///
+    private var sampleParametersForQuery: String {
+        return "?" + sampleParametersForBody
     }
 
-    /// Verifies that the Endpoint's generated URL has the API Version + Method Name as suffix.
+    /// Sample Parameters as a Body String
     ///
-    func testEndpointUrlEndsWithApiVersionAndMethod() {
-        let request = DotcomRequest(wordpressApiVersion: .mark1_1, method: .get, path: sampleRPC)
-        let absoluteString = try! request.asURLRequest().url!.absoluteString
+    private var sampleParametersForBody: String {
+        return encodeAsBodyString(parameters: sampleParameters)
+    }
 
-        let expectedSuffix = request.wordpressApiVersion.path + sampleRPC
-        XCTAssertTrue(absoluteString.hasSuffix(expectedSuffix))
+
+
+    /// Verifies that the DotcomRequest's generated URL contains the `BaseURL + API Version + RPC Name`.
+    ///
+    func testRequestUrlContainsExpectedComponents() {
+        let request = DotcomRequest(wordpressApiVersion: .mark1_1, method: .get, path: sampleRPC)
+
+        let expectedURL = URL(string: request.wordpressApiBaseURL + request.wordpressApiVersion.path + sampleRPC)
+        let generatedURL = try? request.asURLRequest().url
+        XCTAssertEqual(expectedURL, generatedURL)
+    }
+
+    /// Verifies that the DotcomRequest's generated URL contains the Parameters serialized as part of the query, when the method is `get`.
+    ///
+    func testParametersAreSerializedAsPartOfTheUrlQueryWhenMethodIsSetToGet() {
+        let request = DotcomRequest(wordpressApiVersion: .mark1_1, method: .get, path: sampleRPC, parameters: sampleParameters)
+
+        let expectedURL = URL(string: request.wordpressApiBaseURL + request.wordpressApiVersion.path + sampleRPC + sampleParametersForQuery)
+        let generatedURL = try? request.asURLRequest().url
+        XCTAssertEqual(expectedURL, generatedURL)
+    }
+
+    /// Verifies that the DotcomRequest's generated URL does NOT contain the Parameters serialized as part of the query, when the method is `post`.
+    ///
+    func testParametersAreNotSerializedAsPartOfTheUrlQueryWhenMethodIsSetToPost() {
+        let request = DotcomRequest(wordpressApiVersion: .mark1_1, method: .post, path: sampleRPC, parameters: sampleParameters)
+        let generatedURL = try? request.asURLRequest().url
+
+        let expectedURL = URL(string: request.wordpressApiBaseURL + request.wordpressApiVersion.path + sampleRPC)!
+        XCTAssertEqual(expectedURL, generatedURL)
+    }
+
+    /// Verifies that the DotcomRequest's generated httpBody contains the Parameters, serialized sa expected.
+    ///
+    func testParametersAreSerializedAsPartOfTheBodyWhenMethodIsSetToPost() {
+        let request = DotcomRequest(wordpressApiVersion: .mark1_1, method: .post, path: sampleRPC, parameters: sampleParameters)
+
+        let generatedBodyAsData = try! request.asURLRequest().httpBody
+        let generatedBodyAsString = String(data: generatedBodyAsData!, encoding: .utf8)
+        XCTAssertEqual(generatedBodyAsString, sampleParametersForBody)
+    }
+}
+
+
+/// Parameter Encoding Helpers
+///
+private extension DotcomRequestTests {
+
+    /// Encodes the specified collection of Parameters for the URLRequest's httpBody
+    ///
+    func encodeAsBodyString(parameters: [String: String]) -> String {
+        return parameters.reduce("") { (output, parameter) in
+            let prefix = output.isEmpty ? "" : "&"
+            return output + prefix + parameter.key + "=" + parameter.value
+        }
     }
 }

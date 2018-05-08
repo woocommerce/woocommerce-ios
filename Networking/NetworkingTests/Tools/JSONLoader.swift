9bae99c76c18474f70b6dfe5d88cb795de3b8d58
Jorge Leandro Perez <jorge@lantean.co>
2018-05-08 20:26:30 -0300

    JSONLoader: Simpler implementation

--- Networking/NetworkingTests/Tools/JSONLoader.swift
+++ Networking/NetworkingTests/Tools/JSONLoader.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+
 /// Loads + Parses a JSON-Encoded file.
 ///
 class JSONLoader {
@@ -9,38 +10,19 @@ class JSONLoader {
     static let defaultJsonExtension = "json"
 
 
-    /// Loads + Parses as JSON the specified filename.type.
+    /// Loads the specified filename.type Resource, and returns it's JSON Representation.
     ///
-    static func load<T>(filename: String, type: String = JSONLoader.defaultJsonExtension) -> T? {
-        guard let path = path(for: filename, ofType: type), let data = load(at: path) else {
+    static func load<T>(filename: String, ofType type: String = JSONLoader.defaultJsonExtension) -> T? {
+        guard let url = Bundle(for: self).url(forResource: filename, withExtension: type) else {
             return nil
         }
 
-        return parse(data: data) as? T
-    }
-
-    /// Returns the Path for the specified Filename.Type, in the current bundle.
-    ///
-    private static func path(for filename: String, ofType type: String) -> String? {
-        return Bundle(for: self).path(forResource: filename, ofType: type)
-    }
-
-    /// Loads the file at the specified path.
-    ///
-    private static func load(at path: String) -> Data? {
-        return try? Data(contentsOf: URL(fileURLWithPath: path))
-    }
-
-    /// Parses a given Data instance as JSON.
-    ///
-    private static func parse(data: Data) -> Any? {
-        var output: Any?
         do {
-            output = try JSONSerialization.jsonObject(with: data as Data, options: [.mutableContainers, .mutableLeaves])
+            return try JSONSerialization.jsonObject(with: Data(contentsOf: url), options: [.mutableContainers, .mutableLeaves]) as? T
         } catch {
             NSLog("Parsing Error: \(error)")
         }
 
-        return output
+        return nil
     }
 }

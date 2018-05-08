68ee97faa7bd44009a6f6fa6813a52123b56c84d
Jorge Leandro Perez <jorge@lantean.co>
2018-05-08 14:36:38 -0300

    Mapper: Updates Signature

--- Networking/Networking/Mapper/Mapper.swift
+++ Networking/Networking/Mapper/Mapper.swift
@@ -11,7 +11,7 @@ protocol Mapper {
 
     /// Maps a Backend Response into a generic entity of Type `Output`. This method *can throw* errors.
     ///
-    func map(response: [String: Any]) throws -> Output
+    func map(response: Data) throws -> Output
 }
 
 

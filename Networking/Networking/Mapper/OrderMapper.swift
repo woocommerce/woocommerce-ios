7775d859a3d90d035bda2b912d0e51927d6fc27c
Jorge Leandro Perez <jorge@lantean.co>
2018-05-09 15:32:47 -0300

    Implements OrderMapper

---
+++ Networking/Networking/Mapper/OrderMapper.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+
+/// Mapper: Order
+///
+class OrderMapper: Mapper {
+
+    /// (Attempts) to convert a dictionary into [Order].
+    ///
+    func map(response: Data) throws -> Order {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(DateFormatter.Defaults.dateTimeFormatter)
+
+        return try decoder.decode(OrderEnvelope.self, from: response).order
+    }
+}
+
+
+
+/// OrdersEnvelope Disposable Entity:
+/// `Update Order` endpoint returns the updated order document in the `data` key. This entity
+/// allows us to do parse all the things with JSONDecoder.
+///
+private struct OrderEnvelope: Decodable {
+    let order: Order
+
+    private enum CodingKeys: String, CodingKey {
+        case order = "data"
+    }
+}

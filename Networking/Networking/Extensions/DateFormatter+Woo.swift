abc857c65dadc4ab746e1fc736834da05a2bfd73
Jorge Leandro Perez <jorge@lantean.co>
2018-05-09 10:34:03 -0300

    DateFormatter+Woo: Setting Timezone

--- Networking/Networking/Extensions/DateFormatter+Woo.swift
+++ Networking/Networking/Extensions/DateFormatter+Woo.swift
@@ -14,6 +14,7 @@ extension DateFormatter {
         static let dateTimeFormatter: DateFormatter = {
             let formatter = DateFormatter()
             formatter.locale = Locale(identifier: "en_US_POSIX")
+            formatter.timeZone = TimeZone(identifier: "GMT")
             formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH:mm:ss"
             return formatter
         }()

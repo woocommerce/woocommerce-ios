32af5b3331aca9302aacc0cd4f6f9ebcff643d1e
Jorge Leandro Perez <jorge@lantean.co>
2018-05-10 10:57:10 -0300

    Credentials: Implements Equatable

--- Networking/Networking/Settings/Credentials.swift
+++ Networking/Networking/Settings/Credentials.swift
@@ -3,9 +3,16 @@ import Foundation
 
 /// Authenticated Requests Credentials
 ///
-public struct Credentials {
+public struct Credentials: Equatable {
 
     /// WordPress.com Authentication Token
     ///
     public let authToken: String
+}
+
+
+/// Equatable Support
+///
+public func ==(lhs: Credentials, rhs: Credentials) -> Bool {
+    return lhs.authToken == rhs.authToken
 }

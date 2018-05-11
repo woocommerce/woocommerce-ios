1917ee94b0db135683fabbdbb4d33436c74fc441
Jorge Leandro Perez <jorge@lantean.co>
2018-05-11 10:46:31 -0300

    Remote: More Unit Tests! (#57)
    * Implements Networking.framework
    
    * Updates Podfile
    
    * Implements WordPressAPIVersion
    
    * Implements WooAPIVersion
    
    * Implements Endpoint
    
    * Implements JetpackEndpoint
    
    * Implements JetpackEndpointTests
    
    * Implements OrdersRemote
    
    * Implements Remote
    
    * Implements Settings
    
    * Implements Credentials
    
    * Updates Project(s)
    
    * Credentials: Updates visibility
    
    * OrdersRemote: Updates Visibility
    
    * Remote: Updates visibility modifiers
    
    * Implements EndpointTests
    
    * Implements EndpointTests
    
    * Updates Project
    
    * Remote: Documenting Parameters
    
    * JetpackEndpoint: Removing Extra Space
    
    * Updates Podfile
    
    * Implements JetpackEndpointTests
    
    * OrdersRemote: Mockup
    
    * Remote: Adding headers!
    
    * Updates Podfile.lock
    
    * Endpoint: Renames property
    
    * JetpackEndpoint: Cleanup
    
    * JetpackEndpoint: Few Updates
    
    * JetpackEndpoint: Fixing Path Encoding
    
    * JetpackEndpoint > JetpackRequest
    
    * JetpackEndpointTests > JetpackRequestTests
    
    * Endpoint > DotcomEndpoint
    
    * EndpointTests > DotcomEndpointTests
    
    * Updates Project
    
    * OrdersRemote: New Public Methods
    
    * Remote: Cleanup
    
    * Updates Requests
    
    * OrdersRemote: Updates API
    
    * Fixing swiftlint
    
    * DotcomRequest: Documenting Initializer
    
    * Implements AuthenticatedRequest
    
    * DotcomRequestTests: New Unit Tests
    
    * Implements JetpackRequestTests
    
    * Implements AuthenticatedRequestTests
    
    * Updates Project
    
    * JetpackRequest: Default Dotcom API is static
    
    * DotcomRequest: BaseURL is Static
    
    * OrdersRemote: Updating comments
    
    * UnitTesting Constants
    
    * Implements OrdersRemoteTests
    
    * Implementsa JSONLoader
    
    * Implements AlamofireWrapper
    
    * Updates Project
    
    * Implements Network Protocol
    
    * Remote: Dependency Injection Friendly API
    
    * JSONLoader: Switching to static
    
    * Adding orders-fetch-all.json
    
    * Updates Project
    
    * NetworkMockup: Simulated Response API
    
    * OrdersRemote: Updates Public API
    
    * RemoteError: New Type
    
    * Mapper Protocol
    
    * Remote: Mapper Support
    
    * OrdersRemote: Updates internal implementation(s)
    
    * Updates Project
    
    * OrdersRemoteTests: Fixing Unit  Test
    
    * RemoteOrderMapper:  Stub
    
    * Mapper: Updates Output Type Name
    
    * Remote: Wiring Mapper's Output Type
    
    * DateFormatter+Woo: Implements Default Formatters
    
    * Implements KeyedDecodingContainer+Woo
    
    * Updates Project
    
    * Mapper: Updates Signature
    
    * Network: New API
    
    * NetworkMockup: New API Conformance
    
    * Remote: Support for Data / JSON
    
    * Mapper: Cleanup
    
    * RemoteOrderMapper > OrderListMapper
    
    * OrdersRemote: Updates OrderListMapper Naming
    
    * RemoteOrder > Order
    
    * Implements OrderStatus
    
    * Order: Wiring OrderStatus
    
    * OrderListMapper: Simplifies Parsing
    
    * OrderStatus: Nukes unneeded code
    
    * Address + Order: Cleanup
    
    * Nukes dead code
    
    * Remote: Updates Documentation
    
    * JSONLoader: Simpler implementation
    
    * OrdersRemote: Documenting public methods
    
    * DateFormatter+Woo Unit Tests
    
    * DateFormatter+Woo: Setting Timezone
    
    * Order: Nukes non-gmt dates
    
    * JSONLoader > Loader
    
    * Relocates NetworkMockup
    
    * Implements OrderListMapper UnitTests
    
    * Order: Renaming few properties
    
    * Implementing OrderItem
    
    * Implements OrderMapper
    
    * OrderListMapper: Renames private struct
    
    * NetworkMockup: Pattern Matching now supports any kind of request
    
    * OrdersRemoteTests: updateOrder Tests
    
    * OrderMapper: Nukes extra space
    
    * Remote: Few Unit-Testing friendly adjustments
    
    * Credentials: Implements Equatable
    
    * Implements Remote Unit Tests
    
    * OrderListMapper: Fixing Typo

---
+++ Networking/NetworkingTests/Remote/RemoteTests.swift
@@ -0,0 +1,140 @@
+import XCTest
+import Alamofire
+@testable import Networking
+
+
+/// Remote UnitTests
+///
+class RemoteTests: XCTestCase {
+
+    /// Sample Credentials
+    ///
+    private let credentials = Credentials(authToken: "yosemite")
+
+    /// Sample Request
+    ///
+    private let request = try! URLRequest(url: "www.a8c.com", method: .get)
+
+
+    /// Verifies that, by default, the Remote Layer gets AlamofireWrapper set as the Networking Layer.
+    ///
+    func testRemoteGetsAlamofireNetworkByDefault() {
+        let remote = Remote(credentials: credentials)
+        XCTAssert(remote.network is AlamofireWrapper)
+    }
+
+    /// Verifies that `enqueue` properly wraps up the received request within an AuthenticatedRequest, with the remote credentials.
+    ///
+    func testEnqueueProperlyWrapsUpJsonRequestsIntoAuthenticatedRequestWithCredentials() {
+        let network = DummyNetwork()
+        let remote = Remote(credentials: credentials, network: network)
+        let expectation = self.expectation(description: "Enqueue")
+
+        remote.enqueue(request) { (payload, error) in
+            XCTAssertNil(payload)
+            XCTAssertEqual(error as! NetworkError, NetworkError.emptyResponse)
+
+            XCTAssert(network.requestsForResponseData.isEmpty)
+            XCTAssert(network.requestsForResponseJSON.count == 1)
+
+            let first = network.requestsForResponseJSON.first as! AuthenticatedRequest
+            XCTAssertNotNil(first)
+            XCTAssertEqual(first.credentials, self.credentials)
+            XCTAssertEqual(first.request as! URLRequest, self.request)
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+
+    /// Verifies that `enqueue:mapper:` properly wraps up the received request within an AuthenticatedRequest, with the remote credentials.
+    ///
+    func testEnqueueProperlyWrapsUpDataRequestsIntoAuthenticatedRequestWithCredentials() {
+        let network = DummyNetwork()
+        let mapper = DummyMapper()
+        let remote = Remote(credentials: credentials, network: network)
+        let expectation = self.expectation(description: "Enqueue with Mapper")
+
+        remote.enqueue(request, mapper: mapper) { (payload, error) in
+            XCTAssertNil(payload)
+            XCTAssertEqual(error as! NetworkError, NetworkError.emptyResponse)
+
+            XCTAssert(network.requestsForResponseJSON.isEmpty)
+            XCTAssert(network.requestsForResponseData.count == 1)
+
+            let first = network.requestsForResponseData.first as! AuthenticatedRequest
+            XCTAssertNotNil(first)
+            XCTAssertEqual(first.credentials, self.credentials)
+            XCTAssertEqual(first.request as! URLRequest, self.request)
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+
+    /// Verifies that `enqueue:mapper:` relays any received payload over to the Mapper.
+    ///
+    func testEnqueueWithMapperProperlyRelaysReceivedPayloadToMapper() {
+        let network = DummyNetwork()
+        let mapper = DummyMapper()
+        let remote = Remote(credentials: credentials, network: network)
+
+        let expectation = self.expectation(description: "Enqueue with Mapper")
+
+        let orderData = Loader.contentsOf("order")
+        network.dummyDataResponse = orderData
+
+        remote.enqueue(request, mapper: mapper) { (payload, error) in
+            XCTAssertEqual(mapper.input, orderData)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+}
+
+
+/// Dummy Mapper: Useful only for Remote UnitTests
+///
+private class DummyMapper: Mapper {
+
+    /// Contains the last received `response` (via the map method).
+    ///
+    var input: Data?
+
+    func map(response: Data) throws -> Any {
+        input = response
+        return response
+    }
+}
+
+
+/// Dummy Network: Useful only for Remote UnitTests
+///
+private class DummyNetwork: Network  {
+
+    /// Keeps a collection of all of the `responseJSON` requests.
+    ///
+    var requestsForResponseJSON = [URLRequestConvertible]()
+
+    /// Keeps a collection of all of the `responseData` requests.
+    ///
+    var requestsForResponseData = [URLRequestConvertible]()
+
+    /// Dummy Data Response
+    ///
+    var dummyDataResponse: Data?
+
+
+    func responseJSON(for request: URLRequestConvertible, completion: @escaping (Any?, Error?) -> Void) {
+        requestsForResponseJSON.append(request)
+        completion(nil, nil)
+    }
+
+    func responseData(for request: URLRequestConvertible, completion: @escaping (Data?, Error?) -> Void) {
+        requestsForResponseData.append(request)
+        completion(dummyDataResponse, nil)
+    }
+}

6d42c1193ddc36043dbef61c0af1d0052d03ebad
Jorge Leandro Perez <jorge@lantean.co>
2018-05-11 12:31:16 -0300

    Storage Layer Barebones (#59)
    * Storage Layer Barebones
    
    * Embedding internal frameworks
    
    * Project Cleanup
    
    * Fixing typo

---
+++ Storage/Storage/Extensions/NSObject+Storage.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+
+/// NSObject Helper Methods
+///
+extension NSObject {
+
+    /// Returns the receiver's classname as a string, not including the namespace.
+    ///
+    class func classNameWithoutNamespaces() -> String {
+        guard let name = NSStringFromClass(self).components(separatedBy: ".").last else {
+            fatalError()
+        }
+
+        return name
+    }
+}

998fa5229c460ff7ab16a057abd2c010046ed164
Thuy.Copeland <thuycopeland@gmail.com>
2018-05-11 14:37:49 -0500

    🔥

--- WooCommerce/Classes/ViewModels/ContactViewModel.swift
+++ WooCommerce/Classes/ViewModels/ContactViewModel.swift
@@ -9,7 +9,7 @@ enum ContactType {
 class ContactViewModel {
     let title: String
     let fullName: String
-    let formattedAddress: String?
+    var formattedAddress: String?
     let cleanedPhoneNumber: String?
     let phoneNumber: String?
     let email: String?
@@ -23,17 +23,9 @@ class ContactViewModel {
         }
         let contact = CNContact.from(address: address)
         fullName = CNContactFormatter.string(from: contact, style: .fullName) ?? address.firstName + " " + address.lastName
-
         phoneNumber = contact.phoneNumbers.first?.value.stringValue
-
         cleanedPhoneNumber = address.phone?.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
-
-        if let postalAddress = contact.postalAddresses.first?.value {
-            formattedAddress = CNPostalAddressFormatter.string(from: postalAddress, style: .mailingAddress)
-        } else {
-            formattedAddress = nil
-        }
-
+        formattedAddress = contact.postalAddresses.first?.value.formatted(as: .mailingAddress) ?? ""
         email = contact.emailAddresses.first?.value as String?
     }
 }

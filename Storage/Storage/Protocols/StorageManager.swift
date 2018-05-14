160c7be04042c617071a25b7ce9d37a915339675
Jorge Leandro Perez <jorge@lantean.co>
2018-05-14 18:00:35 -0300

    Documenting new Protocols

--- Storage/Storage/Protocols/StorageManager.swift
+++ Storage/Storage/Protocols/StorageManager.swift
@@ -1,15 +1,16 @@
 import Foundation
 
 
-///
+/// Defines the methods and properties implemented by any concrete StorageManager implementation.
 ///
 public protocol StorageManager {
 
-    ///
+    /// Returns the `Storage` associated to the main thread.
     ///
     var viewStorage: Storage { get }
 
-    ///
+    /// Performs a task in Background: a special `Storage` instance will be provided (which is expected to be used within the closure!).
+    /// Note that you must NEVER use the viewStorage within the backgroundClosure.
     ///
     func performBackgroundTask(_ closure: @escaping (Storage) -> Void)
 }

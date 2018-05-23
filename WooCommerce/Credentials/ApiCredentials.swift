efe4ce11c874ef4f064bc14ee15d7fd59ff8c340
Jorge Leandro Perez <jorge@lantean.co>
2018-05-23 16:02:44 -0300

    Implements ApiCredentials

---
+++ WooCommerce/Credentials/ApiCredentials.swift
@@ -0,0 +1,28 @@
+/// WooCommerce API Credentials. Generated on %{timestamp}
+///
+struct ApiCredentials {
+	
+	/// WordPress.com AppID
+	///
+	static let dotcomAppId = "%{dotcom_app_id}"
+
+	/// WordPress.com Secret
+	///
+	static let dotcomSecret = "%{dotcom_secret}"
+
+    /// Google SDK's ClientID
+    ///
+    static let googleClientId = "%{google_client_id}"
+
+    /// Google SDK's ServerID
+    ///
+    static let googleServerId = "%{google_server_id}"
+
+    /// Google SDK's UrlScheme
+    ///
+    static let googleUrlScheme = "%{google_url_scheme}"
+
+    /// Tracks Prefix
+    ///
+    static let tracksPrefix = "%{tracks_prefix}"
+}

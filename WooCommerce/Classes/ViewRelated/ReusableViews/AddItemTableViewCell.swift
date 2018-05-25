8857ee9c74927b9ea9fbd3521ae56808f408814d
Thuy.Copeland <thuycopeland@gmail.com>
2018-05-25 10:23:23 -0500

    Add reusable `item tableview cell`

---
+++ WooCommerce/Classes/ViewRelated/ReusableViews/AddItemTableViewCell.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class AddItemTableViewCell: UITableViewCell {
+
+    static let reuseIdentifier = "AddItemTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        imageView?.tintColor = StyleManager.wooCommerceBrandColor
+    }
+    
+}
+
+extension AddItemTableViewCell {
+    func configure(image: UIImage, text: String) {
+        imageView?.image = image
+        textLabel?.text = text
+    }
+}

5e0441dddd495c153902e1d01df3c4b8916d11b7
Jorge Leandro Perez <jorge@lantean.co>
2018-05-28 14:02:01 -0300

    LoginPrologueViewController: Adding new fields

--- WooCommerce/Classes/Authentication/Prologue/LoginPrologueViewController.swift
+++ WooCommerce/Classes/Authentication/Prologue/LoginPrologueViewController.swift
@@ -1,5 +1,6 @@
 import Foundation
 import UIKit
+import SafariServices
 import WordPressAuthenticator
 
 
@@ -10,6 +11,14 @@ class LoginPrologueViewController: UIViewController {
     /// Label to be displayed at the top of the Prologue.
     ///
     @IBOutlet var upperLabel: UILabel!
+
+    /// Disclaimer Label
+    ///
+    @IBOutlet var disclaimerTextView: UITextView!
+
+    /// Jetpack Logo ImageVIew
+    ///
+    @IBOutlet var jetpackImageView: UIImageView!
 
     /// Default Action Button.
     ///
@@ -28,6 +37,8 @@ class LoginPrologueViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMainView()
+        setupJetpackImage()
+        setupDisclaimerLabel()
         setupUpperLabel()
         setupLoginButton()
     }
@@ -35,11 +46,6 @@ class LoginPrologueViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 }
 
@@ -53,9 +59,18 @@ private extension LoginPrologueViewController {
     }
 
     func setupUpperLabel() {
-        upperLabel.text = NSLocalizedString("Check your WooCommerce store on\nthe go, fulfill your orders and get\nnotifications of new sales.", comment: "Login Prologue Legend")
+        upperLabel.text = NSLocalizedString("Check your WooCommerce store on the go, fulfill your orders and get notifications of new sales.", comment: "Login Prologue Legend")
         upperLabel.font = UIFont.font(forStyle: .subheadline, weight: .bold)
         upperLabel.textColor = .white
+    }
+
+    func setupJetpackImage() {
+        jetpackImageView.image = UIImage.jetpackLogoImage.imageWithTintColor(.jetpackGreen)
+    }
+
+    func setupDisclaimerLabel() {
+        disclaimerTextView.attributedText = disclaimerAttributedText
+        disclaimerTextView.textContainerInset = .zero
     }
 
     func setupLoginButton() {
@@ -65,14 +80,63 @@ private extension LoginPrologueViewController {
 }
 
 
+// MARK: - UITextViewDeletgate Conformance
+//
+extension LoginPrologueViewController: UITextViewDelegate {
+
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        displaySafariViewController(at: URL)
+        return false
+    }
+}
+
+
 // MARK: - Action Handlers
 //
 extension LoginPrologueViewController {
 
+    /// Opens SafariViewController at the specified URL.
+    ///
+    func displaySafariViewController(at url: URL) {
+        let safariViewController = SFSafariViewController(url: url)
+        safariViewController.modalPresentationStyle = .pageSheet
+        present(safariViewController, animated: true, completion: nil)
+    }
+
+    /// Proceeds with the Login Flow.
+    ///
     @IBAction func loginWasPressed() {
         let loginViewController = WordPressAuthenticator.signinForWordPress()
         loginViewController.restrictToWPCom = true
         loginViewController.offerSignupOption = false
+
         navigationController?.pushViewController(loginViewController, animated: true)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+}
+
+
+// MARK: - Private Methods
+//
+private extension LoginPrologueViewController {
+
+    /// Returns the Disclaimer Attributed Text (which contains a link to the Jetpack Setup URL).
+    ///
+    var disclaimerAttributedText: NSAttributedString {
+        let disclaimerText = NSLocalizedString("This app requires Jetpack to be able to connect to your WooCommerce Store.\nFor configuration instructions, ", comment: "Login Disclaimer Text")
+        let disclaimerAttributes: [NSAttributedStringKey: Any] =  [
+            .font: UIFont.font(forStyle: .caption1, weight: .thin),
+            .foregroundColor: UIColor.black
+        ]
+
+        let readText = NSLocalizedString("read here.", comment: "Login Disclaimer Linked Text")
+        var readAttributes = disclaimerAttributes
+        readAttributes[.link] = WooConstants.jetpackSetupUrl
+
+        let readAttrText = NSMutableAttributedString(string: readText, attributes: readAttributes)
+        let disclaimerAttrText = NSMutableAttributedString(string: disclaimerText, attributes: disclaimerAttributes)
+        disclaimerAttrText.append(readAttrText)
+
+        return disclaimerAttrText
     }
 }

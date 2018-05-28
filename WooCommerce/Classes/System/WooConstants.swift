fef9666008e3362f2fa255b65be2c347d2dc5f65
Jorge Leandro Perez <jorge@lantean.co>
2018-05-28 14:02:34 -0300

    Implements WooConstants

---
+++ WooCommerce/Classes/System/WooConstants.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+
+/// WooCommerce Constants
+///
+struct WooConstants {
+
+    /// Private: No-OP!
+    ///
+    private init() {}
+
+    /// Jetpack Setup URL
+    ///
+    static let jetpackSetupUrl = URL(string: "https://jetpack.com/support/getting-started-with-jetpack/")!
+}

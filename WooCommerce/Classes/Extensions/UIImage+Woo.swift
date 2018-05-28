8582160b4e38dd304411e343c5c39ddf103d259b
Jorge Leandro Perez <jorge@lantean.co>
2018-05-28 14:01:44 -0300

    UIImage+Woo: New Extension

---
+++ WooCommerce/Classes/Extensions/UIImage+Woo.swift
@@ -0,0 +1,14 @@
+import Foundation
+import UIKit
+
+
+/// WooCommerce UIImage Assets
+///
+extension UIImage  {
+
+    /// Jetpack Logo Image
+    ///
+    static var jetpackLogoImage: UIImage {
+        return UIImage(named: "icon-jetpack-gray")!
+    }
+}

1194987541d257ab5acc471f9080b6a688be1a66
Thuy.Copeland <thuycopeland@gmail.com>
2018-05-29 14:32:55 -0500

    Date formatting for order note

--- WooCommerce/Classes/ViewModels/OrderNoteViewModel.swift
+++ WooCommerce/Classes/ViewModels/OrderNoteViewModel.swift
@@ -4,7 +4,7 @@ import Gridicons
 class OrderNoteViewModel {
     let iconImage: UIImage
     let iconColor: UIColor
-    let dateCreated: String
+    private let date: Date
     let statusText: String
     let contents: String
 
@@ -17,7 +17,21 @@ class OrderNoteViewModel {
             iconColor = StyleManager.wooGreyMid
             statusText = NSLocalizedString("Private note", comment: "Labels an order note to let the user know it's private and not seen by the customer")
         }
-        dateCreated = orderNote.dateCreated // TODO: make this a fuzzy short date
+
+        // "date_created": "2017-03-21T16:46:41",
+        let format = DateFormatter()
+        format.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        date = format.date(from: orderNote.dateCreated)!
+
         contents = orderNote.contents
+    }
+
+    var dateCreated: String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+
+        let formattedDateString = formatter.string(from: date)
+        return formattedDateString
     }
 }

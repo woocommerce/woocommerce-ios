4051b79b61bbc329a5ddf5de9a00dd1fd4a21624
Jorge Leandro Perez <jorge@lantean.co>
2018-05-29 16:58:08 -0300

    AuthenticationManager: Wiring AccountStore Sync

--- WooCommerce/Classes/Authentication/AuthenticationManager.swift
+++ WooCommerce/Classes/Authentication/AuthenticationManager.swift
@@ -144,11 +144,16 @@ extension AuthenticationManager: WordPressAuthenticatorDelegate {
         // NO-OP: The current WC version does not support Signup.
     }
 
-    /// Synchronizes the specified WordPress Account.
+    /// Synchronizes the specified WordPress Account. Note: Only Dotcom is supported!
     ///
     func sync(credentials: WordPressCredentials, onCompletion: @escaping (Error?) -> Void) {
-        // TODO: Download all of the Account's metadata!
-        onCompletion(nil)
+        guard case let .wpcom(_, authToken, _, _) = credentials else {
+            fatalError("WordPress.org is not supported")
+        }
+
+        Mall.shared.accountStore.synchronizeDotcomAccount(with: authToken) { error in
+            onCompletion(error)
+        }
     }
 
     /// Tracks a given Analytics Event.

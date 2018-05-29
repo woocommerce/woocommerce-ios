c27e38240403797627af72b62db0f974697b900c
Jorge Leandro Perez <jorge@lantean.co>
2018-05-29 16:47:39 -0300

    Storage: Implement Account Type

---
+++ Storage/Storage/Model/Account+CoreDataProperties.swift
@@ -0,0 +1,16 @@
+import Foundation
+import CoreData
+
+
+extension Account {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Account> {
+        return NSFetchRequest<Account>(entityName: "Account")
+    }
+
+    @NSManaged public var displayName: String?
+    @NSManaged public var email: String?
+    @NSManaged public var username: String?
+    @NSManaged public var uuid: String?
+    @NSManaged public var userID: Int64
+}

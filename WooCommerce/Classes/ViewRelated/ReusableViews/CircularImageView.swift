6322158fda2ccdb9d849588124e54ac15f4ac066
Jorge Leandro Perez <jorge@lantean.co>
2018-05-29 14:05:20 -0300

    Implements CircularImageView

---
+++ WooCommerce/Classes/ViewRelated/ReusableViews/CircularImageView.swift
@@ -0,0 +1,24 @@
+import Foundation
+import UIKit
+
+
+/// UIImageView with a circular shape.
+///
+class CircularImageView: UIImageView {
+
+    override var frame: CGRect {
+        didSet {
+            refreshRadius()
+        }
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        refreshRadius()
+    }
+
+    private func refreshRadius() {
+        layer.cornerRadius = frame.width * 0.5
+        layer.masksToBounds = true
+    }
+}

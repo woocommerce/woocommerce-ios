ce068b87510d82c86d92208fb98b2fc5956429fb
Thuy.Copeland <thuycopeland@gmail.com>
2018-05-29 11:13:21 -0500

    Update with credentials

---
+++ WooCommerce/DerivedSources/ApiCredentials.swift
@@ -0,0 +1,32 @@
+/// WooCommerce API Credentials. Generated on May 25, 2018 at 15:24:42
+///
+struct ApiCredentials {
+
+    /// WordPress.com AppID
+    ///
+    static let dotcomAppId = "55584"
+
+    /// WordPress.com Secret
+    ///
+    static let dotcomSecret = "d180BhLfpzsHBXugtIGKkQNau4QRgKMAwjYghVMqyZhmtVZgLRHIJ6VPIS1ribNT"
+
+    /// WordPress.com Magic Link Scheme
+    ///
+    static let dotcomAuthScheme = "woocommerce"
+
+    /// Google SDK's ClientID
+    ///
+    static let googleClientId = "108380595987-jbbnnqi7cad7uu6eg8l8eg30dpupmcpa.apps.googleusercontent.com"
+
+    /// Google SDK's ServerID
+    ///
+    static let googleServerId = "108380595987-j91sm1alavcdgd81i2655kp8q1lbtvrc.apps.googleusercontent.com"
+
+    /// Google SDK's Auth Scheme
+    ///
+    static let googleAuthScheme = "com.googleusercontent.apps.108380595987-jbbnnqi7cad7uu6eg8l8eg30dpupmcpa"
+
+    /// Tracks Prefix
+    ///
+    static let tracksPrefix = "woocommerceios"
+}

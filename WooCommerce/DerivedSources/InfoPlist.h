485544daca404c49d3259f92e9ea7d979a24e383
Thuy.Copeland <thuycopeland@gmail.com>
2018-05-29 11:13:21 -0500

    Update with credentials

---
+++ WooCommerce/DerivedSources/InfoPlist.h
@@ -0,0 +1,14 @@
+/// WooCommerce Preprocessed InfoPlist.h. Generated on May 25, 2018 at 15:24:42
+///
+/// Note: the following constants are being translated into `InfoPlist.h`, and can be picked up by the App's
+/// Info.plist.
+///
+
+
+/// Google Auth Callback Scheme
+///
+#define GOOGLE_AUTH_SCHEME com.googleusercontent.apps.108380595987-jbbnnqi7cad7uu6eg8l8eg30dpupmcpa
+
+/// WordPress.com Auth Callback Scheme (AKA Magic Link!)
+///
+#define DOTCOM_AUTH_SCHEME woocommerce

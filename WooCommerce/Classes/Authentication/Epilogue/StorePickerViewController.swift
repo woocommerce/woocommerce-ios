5dc9953f18b2064ed2d028ac63e254d76b94f1ab
Jorge Leandro Perez <jorge@lantean.co>
2018-05-29 14:30:38 -0300

    StorePickerViewController: Adding new outlets

--- WooCommerce/Classes/Authentication/Epilogue/StorePickerViewController.swift
+++ WooCommerce/Classes/Authentication/Epilogue/StorePickerViewController.swift
@@ -1,33 +1,74 @@
 import Foundation
 import UIKit
+import WordPressAuthenticator
 
 
-///
+/// Allows the user to pick which WordPress.com (OR) Jetpack-Connected-Store we should set up as the Main Store.
 ///
 class StorePickerViewController: UIViewController {
 
     /// White-Background View, to be placed surrounding the bottom area.
     ///
-    @IBOutlet var backgroundView: UIView!
+    @IBOutlet private var backgroundView: UIView! {
+        didSet  {
+            backgroundView.layer.masksToBounds = false
+            backgroundView.layer.shadowOpacity = Constants.backgroundShadowOpacity
+        }
+    }
+
+    /// Spinner, to be displayed while the User Details are being retrieved.
+    ///
+    @IBOutlet private var activityIndicator: UIActivityIndicatorView!
+
+    /// Account's Gravatar.
+    ///
+    @IBOutlet private var gravatarImageView: UIImageView! {
+        didSet {
+            gravatarImageView.image = .gravatarPlaceholderImage
+        }
+    }
+
+    /// Account's Full Name.
+    ///
+    @IBOutlet private var fullnameLabel: UILabel!
+
+    /// Account's Username.
+    ///
+    @IBOutlet private var usernameLabel: UILabel!
 
     /// Default Action Button.
     ///
-    @IBOutlet var continueButton: UIButton!
+    @IBOutlet private var continueButton: UIButton! {
+        didSet {
+            continueButton.backgroundColor = .clear
+        }
+    }
+
+    ///
+    ///
+    var credentials: WordPressCredentials!
+
+    /// Closure to be executed upon dismissal.
+    ///
+    var onDismiss: (() -> Void)?
+
 
 
     // MARK: - Overriden Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupMainView()
-        setupBackgroundView()
-        setupContinueButton()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        onDismiss?()
     }
 }
 
@@ -38,15 +79,6 @@ private extension StorePickerViewController {
 
     func setupMainView() {
         view.backgroundColor = StyleManager.tableViewBackgroundColor
-    }
-
-    func setupBackgroundView() {
-        backgroundView.layer.masksToBounds = false
-        backgroundView.layer.shadowOpacity = 0.2
-    }
-
-    func setupContinueButton() {
-        continueButton.backgroundColor = .clear
     }
 }
 
@@ -59,5 +91,15 @@ extension StorePickerViewController {
     ///
     @IBAction func continueWasPressed() {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+
+// MARK: - Nested Types
+//
+extension StorePickerViewController {
+
+    struct Constants {
+        static let backgroundShadowOpacity = Float(0.2)
     }
 }

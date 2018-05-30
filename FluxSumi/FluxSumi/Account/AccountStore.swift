b536bdfbce240b8ceb1423a2510e0008ac6befd8
Jorge Leandro Perez <jorge@lantean.co>
2018-05-30 09:32:12 -0300

    AccountStore: Wrapping Up the Keychain

--- FluxSumi/FluxSumi/Account/AccountStore.swift
+++ FluxSumi/FluxSumi/Account/AccountStore.swift
@@ -1,6 +1,7 @@
 import Foundation
 import Networking
 import Storage
+import SAMKeychain
 
 
 // MARK: - AccountStore
@@ -23,7 +24,7 @@ extension AccountStore  {
 
     /// Synchronizes the WordPress.com account associated with a specified Authentication Token.
     ///
-    public func synchronizeDotcomAccount(with authToken: String, onCompletion: @escaping (Error?) -> Void) {
+    public func synchronizeDotcomAccount(username: String, authToken: String, onCompletion: @escaping (Error?) -> Void) {
         let credentials = Credentials(authToken: authToken)
         let remote = AccountRemote(credentials: credentials)
 
@@ -36,6 +37,55 @@ extension AccountStore  {
             self?.updateStoredAccount(remote: account)
             onCompletion(nil)
         }
+
+
+        storeAuthToken(authToken, for: username)
+        defaultUsername = username
+    }
+
+    ///
+    ///
+    public var defaultCredentials: Credentials? {
+        guard let username = defaultUsername, let authToken = loadAuthToken(for: username) else {
+            return nil
+        }
+
+        return Credentials(authToken: authToken)
+    }
+
+    ///
+    ///
+    public var defaultUsername: String? {
+        get  {
+            return UserDefaults.standard.string(forKey: Constants.defaultUsernameKey)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: Constants.defaultUsernameKey)
+        }
+    }
+}
+
+
+// MARK: - Keychain
+//
+extension AccountStore {
+
+    /// Stores the specified Authentication Token, associated to a given Username.
+    ///
+    func storeAuthToken(_ token: String, for username: String) {
+        SAMKeychain.setPassword(token, forService: Constants.keychainServiceName, account: username)
+    }
+
+    /// Returns the stored Authentication Token, if any.
+    ///
+    func loadAuthToken(for username: String) -> String? {
+        return SAMKeychain.password(forService: Constants.keychainServiceName, account: username)
+    }
+
+    /// Removes the Authentication Token for the specified username.
+    ///
+    func removeAuthToken(for username: String) {
+        SAMKeychain.deletePassword(forService: Constants.keychainServiceName, account: username)
     }
 }
 
@@ -72,3 +122,15 @@ extension AccountStore {
         return storage.firstObject(ofType: Storage.Account.self, matching: predicate)
     }
 }
+
+
+// MARK: - Nested Types
+//
+private extension AccountStore {
+
+    struct Constants {
+        static let keychainServiceName = "com.automattic.woocommerce.account"
+        static let defaultUsernameKey = "defaultUsernameKey"
+    }
+}
+

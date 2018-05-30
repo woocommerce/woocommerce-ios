55e1e436e7c6b3c61421849cc44c6bc9b70d0c9a
Thuy.Copeland <thuycopeland@gmail.com>
2018-05-30 16:22:43 -0500

    Customize section header view

--- WooCommerce/Classes/ViewRelated/ReusableViews/TwoColumnSectionHeaderView.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/TwoColumnSectionHeaderView.swift
@@ -1,8 +1,10 @@
 import UIKit
 
-class TwoColumnSectionHeaderView: UIView {
+class TwoColumnSectionHeaderView: UITableViewHeaderFooterView {
     @IBOutlet private weak var leftColumn: UILabel!
     @IBOutlet private weak var rightColumn: UILabel!
+
+    static let reuseIdentifier = "TwoColumnSectionHeaderView"
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -16,13 +18,15 @@ class TwoColumnSectionHeaderView: UIView {
         super.awakeFromNib()
         leftColumn.applyFootnoteStyle()
         rightColumn.applyFootnoteStyle()
-        backgroundColor = .clear
+        leftColumn.textColor = StyleManager.sectionTitleColor
+        rightColumn.textColor = StyleManager.sectionTitleColor
     }
 }
 
 extension TwoColumnSectionHeaderView {
-    func configure(leftText: String, rightText: String) {
+    func configure(leftText: String?, rightText: String?) {
         leftColumn.text = leftText
         rightColumn.text = rightText
+        tintColor = .clear
     }
 }

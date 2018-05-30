a130e252455816c2deed83e3ea3f983a7cca6cd9
Jorge Leandro Perez <jorge@lantean.co>
2018-05-30 11:57:01 -0300

    Fixing FluxSumi Header

--- FluxSumi/FluxSumi/FluxSumi.h
+++ FluxSumi/FluxSumi/FluxSumi.h
@@ -1,10 +1,10 @@
 #import <UIKit/UIKit.h>
 
 //! Project version number for Flux.
-FOUNDATION_EXPORT double FluxCVersionNumber;
+FOUNDATION_EXPORT double FluxSumiVersionNumber;
 
 //! Project version string for Flux.
-FOUNDATION_EXPORT const unsigned char FluxCVersionString[];
+FOUNDATION_EXPORT const unsigned char FluxSumiVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FluxSumi/PublicHeader.h>
 

9a9943b3dbd52f7f22406c02f0b2ca26174abb4b
Thuy.Copeland <thuycopeland@gmail.com>
2018-06-11 11:09:06 -0500

    Format using medium date style and short time style

--- WooCommerce/Classes/ViewModels/OrderDetailsViewModel.swift
+++ WooCommerce/Classes/ViewModels/OrderDetailsViewModel.swift
@@ -3,7 +3,7 @@ import UIKit
 
 class OrderDetailsViewModel {
     let summaryTitle: String
-    let dateCreated: String
+    private let dateCreatedString: String
     let paymentStatus: String
     let paymentBackgroundColor: UIColor
     let paymentBorderColor: CGColor
@@ -15,7 +15,7 @@ class OrderDetailsViewModel {
 
     init(order: Order) {
         summaryTitle = "#\(order.number) \(order.shippingAddress.firstName) \(order.shippingAddress.lastName)"
-        dateCreated = String.localizedStringWithFormat(NSLocalizedString("Created %@", comment: "Order created date"), order.dateCreatedString) //FIXME: use a formatted date instead of raw timestamp
+        dateCreatedString = order.dateCreatedString
         paymentStatus = order.status.description
         paymentBackgroundColor = order.status.backgroundColor // MVVM: who should own color responsibilities? Maybe address this down the road.
         paymentBorderColor = order.status.borderColor // same here
@@ -24,5 +24,28 @@ class OrderDetailsViewModel {
         shippingAddress = shippingViewModel.formattedAddress
         billingViewModel = ContactViewModel(with: order.billingAddress, contactType: ContactType.billing)
         billingAddress = billingViewModel.formattedAddress
+    }
+
+    var summaryDateCreated: String {
+        // "date_created": "2017-03-21T16:46:41",
+        let format = ISO8601DateFormatter()
+        let gmt = TimeZone(abbreviation: "GMT")
+        format.timeZone = gmt
+        format.formatOptions = [.withFullDate, .withTime, .withDashSeparatorInDate, .withColonSeparatorInTime]
+        let date = format.date(from: dateCreatedString)
+
+        let shortFormat = DateFormatter()
+        shortFormat.dateFormat = "HH:mm:ss"
+        shortFormat.timeStyle = .short
+
+        guard let orderDate = date else {
+            NSLog("Order date not found!")
+            return dateCreatedString
+        }
+
+        let time = shortFormat.string(from: orderDate)
+
+        let summaryDate = String.localizedStringWithFormat(NSLocalizedString("Updated on \(orderDate.mediumString()) at \(time)", comment: "Order created date"))
+        return summaryDate
     }
 }

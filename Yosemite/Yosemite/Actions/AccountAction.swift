d012a8a16dab910ab0905bf1b3c06005a3355c21
Jorge Leandro Perez <jorge@lantean.co>
2018-06-14 13:43:38 -0300

    New retrieveAccount Action

--- Yosemite/Yosemite/Actions/AccountAction.swift
+++ Yosemite/Yosemite/Actions/AccountAction.swift
@@ -1,8 +1,16 @@
 import Foundation
+import Networking
+
+
+
+// MARK: - Public Aliases
+//
+public typealias Account = Networking.Account
 
 
 // MARK: - AccountAction: Defines all of the Actions supported by the AccountStore.
 //
 public enum AccountAction: Action {
-    case synchronizeAccountDetails(onCompletion: (Error?) -> Void)
+    case synchronizeAccount(onCompletion: (Account?, Error?) -> Void)
+    case retrieveAccount(userId: Int, onCompletion: (Account?) -> Void)
 }

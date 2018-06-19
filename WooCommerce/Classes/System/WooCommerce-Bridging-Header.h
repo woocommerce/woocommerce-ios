d3ad62581f683f5395cd10dd61a66bbaba148696
Thuy.Copeland <thuycopeland@gmail.com>
2018-06-19 15:12:39 -0500

    CocoaLumberjack
    - explicitly add CocoaLumberjack pod to podfile
    - add bridging header to silence warning
    - import CocoaLumberjack.swift from WPiOS

---
+++ WooCommerce/Classes/System/WooCommerce-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  WooCommerce-Bridging-Header.h
+//  WooCommerce
+//
+//  Created by Thuy Copeland on 6/19/18.
+//  Copyright © 2018 Automattic. All rights reserved.
+//
+
+#ifndef WooCommerce_Bridging_Header_h
+#define WooCommerce_Bridging_Header_h
+
+#endif /* WooCommerce_Bridging_Header_h */

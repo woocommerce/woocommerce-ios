1c61fcf2618abd279fe735c6e325fa2fb2d6d320
Jorge Leandro Perez <jorge@lantean.co>
2018-07-03 11:21:58 -0300

    MockupStorage: New Convenience Method

--- Yosemite/YosemiteTests/Mockups/MockupStorage.swift
+++ Yosemite/YosemiteTests/Mockups/MockupStorage.swift
@@ -17,6 +17,11 @@ public class MockupStorageManager: StorageManagerType {
         return persistentContainer.viewContext
     }
 
+    /// Returns the NSManagedObjectContext associated with the Main Thread. Convenience helper!!
+    ///
+    public var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
 
     /// Persistent Container: Holds the full CoreData Stack
     ///

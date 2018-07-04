48cfaf8bfc06ad0529fe5218043ab78e3469cbc0
Jorge Leandro Perez <jorge@lantean.co>
2018-07-04 13:40:39 -0300

    Account+Extensions: Updates ReadOnlyConvertible Protocol

--- Yosemite/Yosemite/Model/Account+Extensions.swift
+++ Yosemite/Yosemite/Model/Account+Extensions.swift
@@ -3,25 +3,19 @@ import Storage
 import Networking
 
 
-// MARK: - Storage.Account Convenience Methods.
+// MARK: - Storage.Account: ReadOnlyConvertible
 //
-extension Storage.Account {
+extension Storage.Account: ReadOnlyConvertible {
 
-    /// Updates the Storage.Account with the Yosemite.Account's Payload.
+    /// Updates the Storage.Account with the a ReadOnly.
     ///
-    func update(with account: Yosemite.Account) {
+    public func update(with account: Yosemite.Account) {
         displayName = account.displayName
         email = account.email
         gravatarUrl = account.gravatarUrl
         userID = Int64(account.userID)
         username = account.username
     }
-}
-
-
-// MARK: - Storage.Account ReadOnlyConvertible
-//
-extension Storage.Account: ReadOnlyConvertible {
 
     /// Returns a ReadOnly version of the receiver.
     ///

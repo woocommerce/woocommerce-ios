05653695d4a5be507938f570c0606049eb70f35b
Jorge Leandro Perez <jorge@lantean.co>
2018-07-04 19:34:02 -0300

    UserDefaults+Woo: Canonical UserDefaults Keys Storage

---
+++ WooCommerce/Classes/Extensions/UserDefaults+Woo.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+
+
+// MARK: - WooCommerce UserDefaults Keys
+//
+extension UserDefaults {
+    enum Key: String {
+        case defaultUsername
+        case defaultStoreID
+    }
+}
+
+
+// MARK: - Convenience Methods
+//
+extension UserDefaults {
+
+    /// Returns the Object (if any) associated with the specified Key.
+    ///
+    func object<T>(forKey key: Key) -> T? {
+        return value(forKey: key.rawValue) as? T
+    }
+
+    /// Stores the Key/Value Pair.
+    ///
+    func set<T>(_ value: T?, forKey key: Key) {
+        set(value, forKey: key.rawValue)
+    }
+
+    /// Nukes any object associated with the specified Key.
+    ///
+    func removeObject(forKey key: Key) {
+        removeObject(forKey: key.rawValue)
+    }
+}

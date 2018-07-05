5f2dad62960db9ade0f019f15f17a39ccec03ac1
Jorge Leandro Perez <jorge@lantean.co>
2018-07-05 00:45:36 -0300

    Session > SessionManager

---
+++ WooCommerce/Classes/System/SessionManager.swift
@@ -0,0 +1,94 @@
+import Foundation
+import Networking
+import KeychainAccess
+
+
+
+/// SessionManager provides persistent storage for Session-Y Properties.
+///
+struct SessionManager {
+
+    /// Reference to the UserDefaults Instance that should be used.
+    ///
+    private let defaults: UserDefaults
+
+    /// KeychainAccess Wrapper.
+    ///
+    private let keychain: Keychain
+
+    /// Active Credentials.
+    ///
+    var credentials: Credentials? {
+        get {
+            return loadCredentials()
+        }
+        set {
+            guard let credentials = newValue else {
+                removeCredentials()
+                return
+            }
+
+            saveCredentials(credentials)
+        }
+    }
+
+    /// Active Store's Site ID.
+    ///
+    var storeID: Int? {
+        get {
+            return defaults[.defaultStoreID]
+        }
+        set {
+            defaults[.defaultStoreID] = storeID
+        }
+    }
+
+
+    /// Designated Initializer.
+    ///
+    init(defaults: UserDefaults, keychainServiceName: String) {
+        self.defaults = defaults
+        self.keychain = Keychain(service: keychainServiceName).accessibility(.afterFirstUnlock)
+    }
+
+    /// Nukes all of the known Session's properties.
+    ///
+    mutating func reset() {
+        credentials = nil
+        storeID = nil
+    }
+}
+
+
+// MARK: - Private Methods
+//
+private extension SessionManager {
+
+    /// Returns the Default Credentials, if any.
+    ///
+    func loadCredentials() -> Credentials? {
+        guard let username = defaults[.defaultUsername] as? String, let authToken = keychain[username] else {
+            return nil
+        }
+
+        return Credentials(username: username, authToken: authToken)
+    }
+
+    /// Persists the Credentials's authToken in the keychain, and username in User Settings.
+    ///
+    func saveCredentials(_ credentials: Credentials) {
+        defaults[.defaultUsername] = credentials.username
+        keychain[credentials.username] = credentials.authToken
+    }
+
+    /// Nukes both, the AuthToken and Default Username.
+    ///
+    func removeCredentials() {
+        guard let username = defaults[.defaultUsername] as? String else {
+            return
+        }
+
+        keychain[username] = nil
+        defaults[.defaultUsername] = nil
+    }
+}

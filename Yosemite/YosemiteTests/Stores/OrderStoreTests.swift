ce0c25a6d4b1f78b4f9a3129f80ef69206784f5c
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-07-05 10:19:23 -0500

    OrderStoreTests fixes from merge

--- Yosemite/YosemiteTests/Stores/OrderStoreTests.swift
+++ Yosemite/YosemiteTests/Stores/OrderStoreTests.swift
@@ -160,26 +160,27 @@ class OrderStoreTests: XCTestCase {
 //
 private extension OrderStoreTests {
     func sampleOrder() -> Networking.Order {
-        return Networking.Order(orderID: 963,
-                                parentID: 0,
-                                customerID: 11,
-                                number: "963",
-                                status: .processing,
-                                currency: "USD",
-                                customerNote: "",
-                                dateCreated: date(with: "2018-04-03T23:05:12"),
-                                dateModified: date(with: "2018-04-03T23:05:14"),
-                                datePaid: date(with: "2018-04-03T23:05:14"),
-                                discountTotal: "30.00",
-                                discountTax: "1.20",
-                                shippingTotal: "0.00",
-                                shippingTax: "0.00",
-                                total: "31.20",
-                                totalTax: "1.20",
-                                items: sampleItems(),
-                                billingAddress: sampleAddress(),
-                                shippingAddress: sampleAddress(),
-                                coupons: sampleCoupons())
+        return Order(orderID: 963,
+                     parentID: 0,
+                     customerID: 11,
+                     number: "963",
+                     status: .processing,
+                     currency: "USD",
+                     customerNote: "",
+                     dateCreated: date(with: "2018-04-03T23:05:12"),
+                     dateModified: date(with: "2018-04-03T23:05:14"),
+                     datePaid: date(with: "2018-04-03T23:05:14"),
+                     discountTotal: "30.00",
+                     discountTax: "1.20",
+                     shippingTotal: "0.00",
+                     shippingTax: "0.00",
+                     total: "31.20",
+                     totalTax: "1.20",
+                     paymentMethodTitle: "Credit Card (Stripe)",
+                     items: sampleItems(),
+                     billingAddress: sampleAddress(),
+                     shippingAddress: sampleAddress(),
+                     coupons: sampleCoupons())
     }
 
     func sampleAddress() -> Networking.Address {

d1b7b72180fe1bb6ce828240a0e32d4aea6115cf
Jorge Leandro Perez <jorge@lantean.co>
2018-07-06 18:31:54 -0300

    StoresManagerTests: Fixing Unit Tests

--- WooCommerce/WooCommerceTests/Yosemite/StoresManagerTests.swift
+++ WooCommerce/WooCommerceTests/Yosemite/StoresManagerTests.swift
@@ -28,7 +28,7 @@ class StoresManagerTests: XCTestCase {
     ///
     func testInitialStateIsAuthenticatedAssumingCredentialsWereNotMissing() {
         var session = SessionManager.testingInstance
-        session.credentials = Settings.credentials
+        session.defaultCredentials = Settings.credentials
 
         let manager = StoresManager.testingInstance
         XCTAssertTrue(manager.isAuthenticated)
@@ -39,7 +39,7 @@ class StoresManagerTests: XCTestCase {
     ///
     func testAuthenticateEffectivelyTogglesStoreManagerToAuthenticatedState() {
         let manager = StoresManager.testingInstance
-        manager.authenticate(username: Settings.credentials.username, authToken: Settings.credentials.authToken)
+        manager.authenticate(username: Settings.credentials.username, authToken: Settings.credentials.authToken, onCompletion: nil)
 
         XCTAssertTrue(manager.isAuthenticated)
     }
@@ -63,7 +63,7 @@ class StoresManagerTests: XCTestCase {
         manager.authenticate(username: Settings.credentials.username, authToken: Settings.credentials.authToken)
 
         let session = SessionManager.testingInstance
-        XCTAssertEqual(session.credentials, Settings.credentials)
+        XCTAssertEqual(session.defaultCredentials, Settings.credentials)
     }
 }
 

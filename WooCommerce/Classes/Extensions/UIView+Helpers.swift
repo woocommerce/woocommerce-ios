9ec333971050b3d9516a908322fbd6f61f80dd88
Jorge Leandro Perez <jorge@lantean.co>
2018-07-10 19:24:19 -0300

    UIView+Helpers: New API's

--- WooCommerce/Classes/Extensions/UIView+Helpers.swift
+++ WooCommerce/Classes/Extensions/UIView+Helpers.swift
@@ -6,11 +6,16 @@ import UIKit
 ///
 extension UIView {
 
+    /// Returns the Nib associated with the received: It's filename is expected to match the Class Name
+    ///
+    class func loadNib() -> UINib {
+        return UINib(nibName: classNameWithoutNamespaces, bundle: nil)
+    }
+
     /// Returns the first Object contained within the nib with a name whose name matches with the receiver's type.
     /// Note: On error this method is expected to break, by design!
     ///
-    class func loadFromNib<T>() -> T {
-        let nib = UINib(nibName: classNameWithoutNamespaces, bundle: nil)
-        return nib.instantiate(withOwner: nil, options: nil).first as! T
+    class func instantiateFromNib<T>() -> T {
+        return loadNib().instantiate(withOwner: nil, options: nil).first as! T
     }
 }

9b33629b59df93b4f70cd8744a494b199dc47903
Jorge Leandro Perez <jorge@lantean.co>
2018-07-11 16:02:28 -0300

    StorePickerViewController: Implementing States

--- WooCommerce/Classes/Authentication/Epilogue/StorePickerViewController.swift
+++ WooCommerce/Classes/Authentication/Epilogue/StorePickerViewController.swift
@@ -2,22 +2,20 @@ import Foundation
 import UIKit
 import WordPressAuthenticator
 import WordPressUI
+import Storage
+import Yosemite
+
 
 
 /// Allows the user to pick which WordPress.com (OR) Jetpack-Connected-Store we should set up as the Main Store.
 ///
 class StorePickerViewController: UIViewController {
 
-    /// No Results Placeholder Image
+    /// Represents the internal StorePicker State
     ///
-    @IBOutlet private var noResultsImageView: UIImageView!
-
-    /// No Results Placeholder Text
-    ///
-    @IBOutlet private var noResultsLabel: UILabel! {
+    private var state: StorePickerState = .empty {
         didSet {
-            noResultsLabel.font = UIFont.font(forStyle: .subheadline, weight: .regular)
-            noResultsLabel.textColor = StyleManager.wooGreyTextMin
+            stateWasUpdated()
         }
     }
 
@@ -27,13 +25,15 @@ class StorePickerViewController: UIViewController {
         return AccountHeaderView.instantiateFromNib()
     }()
 
-    /// Main tableView
+    /// ResultsController: Loads Sites from the Storage Layer.
     ///
-    @IBOutlet private var tableView: UITableView! {
-        didSet {
-            tableView.tableHeaderView = accountHeaderView
-        }
-    }
+    private let resultsController: ResultsController<Storage.Site> = {
+        let viewContext = CoreDataManager.global.viewContext
+        let predicate = NSPredicate(format: "isWordPressStore == YES")
+        let descriptor = NSSortDescriptor(key: "name", ascending: true)
+
+        return ResultsController(viewContext: viewContext, matching: predicate, sortedBy: [descriptor])
+    }()
 
     /// White-Background View, to be placed surrounding the bottom area.
     ///
@@ -52,6 +52,27 @@ class StorePickerViewController: UIViewController {
         }
     }
 
+    /// No Results Placeholder Image
+    ///
+    @IBOutlet private var noResultsImageView: UIImageView!
+
+    /// No Results Placeholder Text
+    ///
+    @IBOutlet private var noResultsLabel: UILabel! {
+        didSet {
+            noResultsLabel.font = UIFont.font(forStyle: .subheadline, weight: .regular)
+            noResultsLabel.textColor = StyleManager.wooGreyTextMin
+        }
+    }
+
+    /// Main tableView
+    ///
+    @IBOutlet private var tableView: UITableView! {
+        didSet {
+            tableView.tableHeaderView = accountHeaderView
+        }
+    }
+
     /// Closure to be executed upon dismissal.
     ///
     var onDismiss: (() -> Void)?
@@ -63,9 +84,10 @@ class StorePickerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setup(mainView: view)
-        registerTableViewCells()
-        displayAccountDetails(in: accountHeaderView)
+        setupMainView()
+        setupAccountHeader()
+        setupTableView()
+        refreshResults()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -84,13 +106,14 @@ class StorePickerViewController: UIViewController {
 //
 private extension StorePickerViewController {
 
-    func setup(mainView: UIView) {
-        mainView.backgroundColor = StyleManager.tableViewBackgroundColor
+    func setupMainView() {
+        view.backgroundColor = StyleManager.tableViewBackgroundColor
     }
 
-    func registerTableViewCells() {
+    func setupTableView() {
         let cells = [
-            EmptyStoresTableViewCell.reuseIdentifier: EmptyStoresTableViewCell.loadNib()
+            EmptyStoresTableViewCell.reuseIdentifier: EmptyStoresTableViewCell.loadNib(),
+            StoreTableViewCell.reuseIdentifier: StoreTableViewCell.loadNib()
         ]
 
         for (reuseIdentifier, nib) in cells {
@@ -98,14 +121,25 @@ private extension StorePickerViewController {
         }
     }
 
-    func displayAccountDetails(in headerView: AccountHeaderView) {
+    func setupAccountHeader() {
         guard let defaultAccount = StoresManager.shared.sessionManager.defaultAccount else {
             return
         }
 
-        headerView.username = "@" + defaultAccount.username
-        headerView.fullname = defaultAccount.displayName
-        headerView.downloadGravatar(with: defaultAccount.email)
+        accountHeaderView.username = "@" + defaultAccount.username
+        accountHeaderView.fullname = defaultAccount.displayName
+        accountHeaderView.downloadGravatar(with: defaultAccount.email)
+    }
+
+    func refreshResults() {
+        try? resultsController.performFetch()
+        state = StorePickerState(sites: resultsController.fetchedObjects)
+    }
+
+    func stateWasUpdated() {
+        tableView.separatorStyle = state.separatorStyle
+        actionButton.setTitle(state.actionTitle, for: .normal)
+        tableView.reloadData()
     }
 }
 
@@ -122,20 +156,45 @@ extension StorePickerViewController {
 }
 
 
-// MARK: - Action Handlers
+// MARK: - UITableViewDataSource Conformance
 //
 extension StorePickerViewController: UITableViewDataSource {
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return state.numberOfSections
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return state.numberOfRows
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return state.headerTitle?.uppercased()
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: EmptyStoresTableViewCell.reuseIdentifier, for: indexPath)
+        guard let site = state.site(at: indexPath) else {
+            return tableView.dequeueReusableCell(withIdentifier: EmptyStoresTableViewCell.reuseIdentifier, for: indexPath)
+        }
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: StoreTableViewCell.reuseIdentifier, for: indexPath) as? StoreTableViewCell else {
+            fatalError()
+        }
+
+        cell.name = site.name
+        cell.url = site.url
+
+        return cell
+    }
+}
+
+
+// MARK: - UITableViewDelegate Conformance
+//
+extension StorePickerViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
@@ -144,13 +203,100 @@ extension StorePickerViewController: UITableViewDataSource {
 //
 private enum StorePickerConstants {
     static let backgroundShadowOpacity = Float(0.2)
+    static let numberOfSections = 1
+    static let emptyStateRowCount = 1
 }
 
 
 // MARK: - Represents the StorePickerViewController's Internal State.
 //
 private enum StorePickerState {
+
+    /// No Stores onScreen
+    ///
     case empty
-    case single
-    case multiple
+
+    /// Stores Available!
+    ///
+    case available(sites: [Yosemite.Site])
+
+
+    /// Designated Initializer
+    ///
+    init(sites: [Yosemite.Site]) {
+        if sites.isEmpty {
+            self = .empty
+        } else {
+            self = .available(sites: sites)
+        }
+    }
+}
+
+
+// MARK: - StorePickerState Properties
+//
+private extension StorePickerState {
+
+    /// Action Button's Title
+    ///
+    var actionTitle: String {
+        switch self {
+        case .empty:
+            return NSLocalizedString("Try another account", comment: "")
+        default:
+            return NSLocalizedString("Continue", comment: "")
+        }
+    }
+
+    /// Results Table's Header Title
+    ///
+    var headerTitle: String? {
+        switch self {
+        case .empty:
+            return nil
+        case .available(let sites) where sites.count > 1:
+            return NSLocalizedString("Pick Store to Connect", comment: "Store Picker's Section Title: Displayed whenever there are multiple Stores.")
+        default:
+            return NSLocalizedString("Connected Store", comment: "Store Picker's Section Title: Displayed when there's a single pre-selected Store.")
+        }
+    }
+
+    /// Number of TableView Sections
+    ///
+    var numberOfSections: Int {
+        return StorePickerConstants.numberOfSections
+    }
+
+    /// Number of TableView Rows
+    ///
+    var numberOfRows: Int {
+        switch self {
+        case .available(let sites):
+            return sites.count
+        default:
+            return StorePickerConstants.emptyStateRowCount
+        }
+    }
+
+    /// Results Table's Separator Style
+    ///
+    var separatorStyle: UITableViewCellSeparatorStyle {
+        switch self {
+        case .empty:
+            return .none
+        default:
+            return .singleLine
+        }
+    }
+
+    /// Returns the site to be displayed at a given IndexPath
+    ///
+    func site(at indexPath: IndexPath) -> Yosemite.Site? {
+        switch self {
+        case .empty:
+            return nil
+        case .available(let sites):
+            return sites[indexPath.row]
+        }
+    }
 }

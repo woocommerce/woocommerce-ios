f7bbbd95772897bf379918d752c51577be7afc73
Jorge Leandro Perez <jorge@lantean.co>
2018-07-11 16:44:57 -0300

    UITableViewCellHelpersTests: Documents Unit Test

--- WooCommerce/WooCommerceTests/Extensions/UITableViewCellHelpersTests.swift
+++ WooCommerce/WooCommerceTests/Extensions/UITableViewCellHelpersTests.swift
@@ -6,6 +6,8 @@ import XCTest
 //
 class UITableViewCellHelpersTests: XCTestCase {
 
+    /// Verifies that `reuseIdentifier` class method effectively returns a string that doesn't contain the class's module.
+    ///
     func testReuseIdentifierEffectivelyReturnsClassnameWithNoNamespaces() {
         XCTAssertEqual(EmptyStoresTableViewCell.reuseIdentifier, "EmptyStoresTableViewCell")
     }

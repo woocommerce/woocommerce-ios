d11c702d831400862371235776b1d30a6ac667d0
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-07-12 15:44:34 -0500

    Added some ReadOnlyConvertibles and inits() to Yosmite models

---
+++ Yosemite/Yosemite/Model/OrderItem+ReadOnlyConvertible.swift
@@ -0,0 +1,40 @@
+import Foundation
+import Storage
+
+
+// MARK: - Storage.OrderItem: ReadOnlyConvertible
+//
+extension Storage.OrderItem: ReadOnlyConvertible {
+
+    /// Updates the Storage.OrderItem with the ReadOnly.
+    ///
+    public func update(with orderItem: Yosemite.OrderItem) {
+        itemID = Int64(orderItem.itemID)
+        name = orderItem.name
+        quantity = Int16(orderItem.quantity)
+        productID = Int64(orderItem.productID)
+        sku = orderItem.sku
+        subtotal = orderItem.subtotal
+        subtotalTax = orderItem.subtotalTax
+        taxClass = orderItem.taxClass
+        total = orderItem.total
+        totalTax = orderItem.totalTax
+        variationID = Int64(orderItem.variationID)
+    }
+
+    /// Returns a ReadOnly version of the receiver.
+    ///
+    public func toReadOnly() -> Yosemite.OrderItem {
+        return OrderItem(itemID: Int(itemID),
+                         name: name ?? "",
+                         productID: Int(productID),
+                         quantity: Int(quantity),
+                         sku: sku ?? "",
+                         subtotal: subtotal ?? "",
+                         subtotalTax: subtotalTax ?? "",
+                         taxClass: taxClass ?? "",
+                         total: total ?? "",
+                         totalTax: totalTax ?? "",
+                         variationID: Int(variationID))
+    }
+}

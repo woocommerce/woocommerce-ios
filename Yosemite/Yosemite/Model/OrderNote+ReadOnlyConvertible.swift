80108068bed91ed504875e1deb111673f58c97a4
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-07-13 12:00:14 -0500

    Added ReadOnlyConvertible for OrderNote; Renamed OrderNote ID var

---
+++ Yosemite/Yosemite/Model/OrderNote+ReadOnlyConvertible.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Storage
+
+
+// MARK: - Storage.OrderNote: ReadOnlyConvertible
+//
+extension Storage.OrderNote: ReadOnlyConvertible {
+
+    /// Updates the Storage.OrderCoupon with the ReadOnly.
+    ///
+    public func update(with orderNote: Yosemite.OrderNote) {
+        noteID = Int64(orderNote.noteID)
+        dateCreated = orderNote.dateCreated
+        note = orderNote.note
+        isCustomerNote = orderNote.isCustomerNote
+    }
+
+    /// Returns a ReadOnly version of the receiver.
+    ///
+    public func toReadOnly() -> Yosemite.OrderNote {
+        return OrderNote(noteId: Int(noteID),
+                         dateCreated: dateCreated ?? Date(),
+                         note: note ?? "",
+                         isCustomerNote: isCustomerNote)
+    }
+}

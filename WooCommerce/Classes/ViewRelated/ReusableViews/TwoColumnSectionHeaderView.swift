dbd1d1500c1df056317c903f84bd1d258c09eaa9
Jorge Leandro Perez <jorge@lantean.co>
2018-07-23 17:41:26 -0300

    TwoColumnSectionHeaderView: New public properties

--- WooCommerce/Classes/ViewRelated/ReusableViews/TwoColumnSectionHeaderView.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/TwoColumnSectionHeaderView.swift
@@ -1,8 +1,41 @@
 import UIKit
 
+
+/// Renders a Section Header/Footer with two labels [Left / Right].
+///
 class TwoColumnSectionHeaderView: UITableViewHeaderFooterView {
+
+    /// Left Label
+    ///
     @IBOutlet private weak var leftColumn: UILabel!
+
+    /// Right Label
+    ///
     @IBOutlet private weak var rightColumn: UILabel!
+
+    /// Left Label's Text
+    ///
+    var leftText: String? {
+        get {
+            return leftColumn.text
+        }
+        set {
+            leftColumn.text = newValue
+        }
+    }
+
+    /// Right Label's Text
+    ///
+    var rightText: String? {
+        get {
+            return rightColumn.text
+        }
+        set {
+            rightColumn.text = newValue
+        }
+    }
+
+    // MARK: - Overridden Methods
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -14,6 +47,9 @@ class TwoColumnSectionHeaderView: UITableViewHeaderFooterView {
     }
 }
 
+
+// MARK: - Public Methods
+//
 extension TwoColumnSectionHeaderView {
     func configure(leftText: String?, rightText: String?) {
         leftColumn.text = leftText

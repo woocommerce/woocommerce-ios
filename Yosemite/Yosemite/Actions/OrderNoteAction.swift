f9c87ea509bcf5dbd9a3e66e534d35b74fb4b92f
Thuy.Copeland <thuycopeland@gmail.com>
2018-07-23 11:01:38 -0500

    Create the .addOrderNote action. Create the addOrderNote network request.

--- Yosemite/Yosemite/Actions/OrderNoteAction.swift
+++ Yosemite/Yosemite/Actions/OrderNoteAction.swift
@@ -6,4 +6,5 @@ import Networking
 //
 public enum OrderNoteAction: Action {
     case retrieveOrderNotes(siteID: Int, orderID: Int, onCompletion: ([OrderNote]?, Error?) -> Void)
+    case addOrderNote(siteID: Int, orderID: Int, isCustomerNote: Bool, note: OrderNote, onCompletion: ([OrderNote]?, Error?) -> Void)
 }

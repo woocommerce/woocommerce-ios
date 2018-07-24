1a7ed2ed7eacdb064efc4e03ca9164a77968f75d
Thuy.Copeland <thuycopeland@gmail.com>
2018-07-24 14:42:09 -0500

    Add "Email note to customer" cell

--- WooCommerce/Classes/ViewRelated/Orders/OrderDetails/AddNote/AddaNoteViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/OrderDetails/AddNote/AddaNoteViewController.swift
@@ -9,12 +9,16 @@ class AddaNoteViewController: UIViewController {
 
     var order: Order!
 
+    private var sections = [Section]()
+
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigation()
         configureTableView()
+        registerTableViewCells()
+        loadSections()
     }
 
     func configureNavigation() {
@@ -51,9 +55,45 @@ class AddaNoteViewController: UIViewController {
 private extension AddaNoteViewController {
     /// Setup: TableView
     ///
-    func configureTableView() {
+    private func configureTableView() {
         tableView.estimatedRowHeight = Constants.rowHeight
         tableView.rowHeight = UITableViewAutomaticDimension
+    }
+
+    /// Registers all of the available TableViewCells
+    ///
+    private func registerTableViewCells() {
+        let cells = [
+            WriteCustomerNoteTableViewCell.self,
+            ToggleEmailCustomerTableViewCell.self
+        ]
+
+        for cell in cells {
+            tableView.register(cell.loadNib(), forCellReuseIdentifier: cell.reuseIdentifier)
+        }
+    }
+
+    /// Setup: Sections
+    ///
+    private func loadSections() {
+        let writeNoteSectionTitle = NSLocalizedString("WRITE NOTE", comment: "Add a note screen - Write Note section title")
+        let writeNoteSection = Section(title: writeNoteSectionTitle, rows: [.writeNote])
+        let emailCustomerSection = Section(title: nil, rows: [.emailCustomer])
+
+        sections = [writeNoteSection, emailCustomerSection]
+    }
+
+    /// Cell Configuration
+    ///
+    private func configure(_ cell: UITableViewCell, for row: Row, at indexPath: IndexPath) {
+        switch cell {
+        case let cell as WriteCustomerNoteTableViewCell:
+            cell.configure(isNoteToCustomer: false)
+        case let cell as ToggleEmailCustomerTableViewCell:
+            cell.awakeFromNib()
+        default:
+            fatalError("Unidentified add a note row type")
+        }
     }
 }
 
@@ -69,15 +109,14 @@ extension AddaNoteViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let row = sections[indexPath.section].rows[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: row.reuseIdentifier, for: indexPath)
+        configure(cell, for: row, at: indexPath)
+        return cell
     }
 
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 0 {
-            return NSLocalizedString("WRITE NOTE", comment: "Add a note screen - Write Note section title")
-        }
-
-        return ""
+        return sections[section].title
     }
 }
 
@@ -86,5 +125,24 @@ extension AddaNoteViewController: UITableViewDataSource {
 private extension AddaNoteViewController {
     struct Constants {
         static let rowHeight = CGFloat(44)
+    }
+
+    private struct Section {
+        let title: String?
+        let rows: [Row]
+    }
+
+    private enum Row {
+        case writeNote
+        case emailCustomer
+
+        var reuseIdentifier: String {
+            switch self {
+            case .writeNote:
+                return WriteCustomerNoteTableViewCell.reuseIdentifier
+            case .emailCustomer:
+                return ToggleEmailCustomerTableViewCell.reuseIdentifier
+            }
+        }
     }
 }

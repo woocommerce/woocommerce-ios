0990c993914c1ea9ed6e260f43911da6fbdd1bf6
Thuy.Copeland <thuycopeland@gmail.com>
2018-07-24 11:01:42 -0500

    Add tableview data source
    - grouped table style
    - section titles

--- WooCommerce/Classes/ViewRelated/Orders/OrderDetails/AddNote/AddaNoteViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/OrderDetails/AddNote/AddaNoteViewController.swift
@@ -3,11 +3,18 @@ import Yosemite
 
 class AddaNoteViewController: UIViewController {
 
+    // MARK: - Properties
+
+    @IBOutlet var tableView: UITableView!
+
     var order: Order!
+
+    // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigation()
+        configureTableView()
     }
 
     func configureNavigation() {
@@ -37,5 +44,47 @@ class AddaNoteViewController: UIViewController {
     @objc func addButtonTapped() {
         NSLog("Add button tapped!")
     }
+}
 
+// MARK: - TableView Configuration
+//
+private extension AddaNoteViewController {
+    /// Setup: TableView
+    ///
+    func configureTableView() {
+        tableView.estimatedRowHeight = Constants.rowHeight
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+}
+
+// MARK: - UITableViewDataSource Conformance
+//
+extension AddaNoteViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return NSLocalizedString("WRITE NOTE", comment: "Add a note screen - Write Note section title")
+        }
+
+        return ""
+    }
+}
+
+// MARK: - Constants
+//
+private extension AddaNoteViewController {
+    struct Constants {
+        static let rowHeight = CGFloat(44)
+    }
 }

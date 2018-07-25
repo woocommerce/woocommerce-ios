eb1f4656de29fc4b0b877741502e845e721fc27f
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-07-25 13:27:26 -0500

    Added unit tests for WooAnalytics

---
+++ WooCommerce/WooCommerceTests/Mockups/MockupAnalyticsProvider.swift
@@ -0,0 +1,25 @@
+import Foundation
+@testable import WooCommerce
+
+
+public class MockupAnalyticsProvider: AnalyticsProvider {
+    var receivedEvents = [String]()
+    var receivedProperties = [[AnyHashable : Any]]()
+}
+
+
+// MARK: - AnalyticsProvider Conformance
+//
+public extension MockupAnalyticsProvider {
+
+    func track(_ eventName: String) {
+        track(eventName, withProperties: nil)
+    }
+
+    func track(_ eventName: String, withProperties properties: [AnyHashable : Any]?) {
+        receivedEvents.append(eventName)
+        if let properties = properties {
+            receivedProperties.append(properties)
+        }
+    }
+}

8f9d029edb50b5f30f68011673b7363c66c1df01
Jorge Leandro Perez <jorge@lantean.co>
2018-07-26 16:39:11 -0300

    OrderAction.updateOrder: Updating completion signature

--- Yosemite/Yosemite/Actions/OrderAction.swift
+++ Yosemite/Yosemite/Actions/OrderAction.swift
@@ -8,5 +8,5 @@ import Networking
 public enum OrderAction: Action {
     case retrieveOrders(siteID: Int, onCompletion: ([Order]?, Error?) -> Void)
     case retrieveOrder(siteID: Int, orderID: Int, onCompletion: (Order?, Error?) -> Void)
-    case updateOrder(siteID: Int, orderID: Int, status: OrderStatus, onCompletion: (Order?, Error?) -> Void)
+    case updateOrder(siteID: Int, orderID: Int, status: OrderStatus, onCompletion: (Error?) -> Void)
 }

05e192384767d0688f8cf0ce5558464d126318f6
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-07-30 14:45:34 -0500

    WIP: OrderStats models and remote

---
+++ Networking/Networking/Remote/OrderStatsRemote.swift
@@ -0,0 +1,45 @@
+import Foundation
+import Alamofire
+
+
+/// OrderStats: Remote Endpoints
+///
+public class OrderStatsRemote: Remote {
+
+    /// Fetch the order stats for a given site up to the current day, month, or year (depending on the given granularity of the `unit` parameter).
+    ///
+    /// - Parameters:
+    ///   - siteID: The site ID
+    ///   - unit: Defines the granularity of the stats we are fetching (one of 'day', 'week', 'month', or 'year')
+    ///   - latestDateToInclude: The latest date to include in the results. This string should match the `granularity`, e.g.: 'day':'1955-11-05', 'week':'1955-W44', 'month':'1955-11', 'year':'1955'
+    ///   - quantity: How many `unit`s to fetch
+    ///   - completion: Closure to be executed upon completion.
+    ///
+    public func loadOrderStats(for siteID: Int, granularity: OrderStatGranularity, latestDateToInclude: String, quantity: String, completion: @escaping ([OrderStatItem]?, Error?) -> Void) {
+        let path = "\(Constants.sitesPath)/\(siteID)/\(Constants.orderStatsPath)/"
+        let parameters = [ParameterKeys.unit: granularity.rawValue,
+                          ParameterKeys.date: latestDateToInclude,
+                          ParameterKeys.quantity: quantity]
+        let request = DotcomRequest(wordpressApiVersion: .wpcomMark2, method: .get, path: path, parameters: parameters)
+
+        // FIXME: Add OrderStatsMapper
+        //let mapper = OrderStatsMapper()
+        //enqueue(request, mapper: mapper, completion: completion)
+    }
+}
+
+
+// MARK: - Constants!
+//
+private extension OrderStatsRemote {
+    enum Constants {
+        static let sitesPath: String        = "sites"
+        static let orderStatsPath: String   = "stats/orders"
+    }
+
+    enum ParameterKeys {
+        static let unit: String     = "unit"
+        static let date: String     = "date"
+        static let quantity: String = "page"
+    }
+}
--- Yosemite/Yosemite/Model/Model.swift
+++ Yosemite/Yosemite/Model/Model.swift
@@ -13,4 +13,6 @@ public typealias OrderItem = Networking.OrderItem
 public typealias OrderStatus = Networking.OrderStatus
 public typealias OrderCouponLine = Networking.OrderCouponLine
 public typealias OrderNote = Networking.OrderNote
+public typealias OrderStatItem = Networking.OrderStatItem
+public typealias OrderStatGranularity = Networking.OrderStatGranularity
 public typealias Site = Networking.Site

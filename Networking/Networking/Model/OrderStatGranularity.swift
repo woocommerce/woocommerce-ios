55dbe56bfafc25c3618b9d5a9de914941e90782d
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-08-02 13:43:27 -0500

    Cleaned up OrderStatGranularity

--- Networking/Networking/Model/OrderStatGranularity.swift
+++ Networking/Networking/Model/OrderStatGranularity.swift
@@ -3,45 +3,12 @@ import Foundation
 
 /// Represents the data granularity for a specific `OrderStats` instance (e.g. day, week, month, year)
 ///
-public enum OrderStatGranularity: Decodable {
+public enum OrderStatGranularity: String, Decodable {
     case day
     case week
     case month
     case year
 }
-
-
-// MARK: - RawRepresentable Conformance
-//
-extension OrderStatGranularity: RawRepresentable {
-
-    /// Designated Initializer.
-    ///
-    public init(rawValue: String) {
-        switch rawValue {
-        case Keys.week:
-            self = .week
-        case Keys.month:
-            self = .month
-        case Keys.year:
-            self = .year
-        default:
-            self = .day
-        }
-    }
-
-    /// Returns the current Enum Case's Raw Value
-    ///
-    public var rawValue: String {
-        switch self {
-        case .day:      return Keys.day
-        case .week:     return Keys.week
-        case .month:    return Keys.month
-        case .year:     return Keys.year
-        }
-    }
-}
-
 
 // MARK: - StringConvertible Conformance
 //
@@ -63,17 +30,3 @@ extension OrderStatGranularity: CustomStringConvertible {
     }
 }
 
-
-// MARK: - Constants!
-//
-private extension OrderStatGranularity {
-
-    /// Enum containing the 'Known' OrderStatGranularity Keys
-    ///
-    private enum Keys {
-        static let day      = "day"
-        static let week     = "week"
-        static let month    = "month"
-        static let year     = "year"
-    }
-}

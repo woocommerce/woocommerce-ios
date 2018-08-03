0fedb020225a29a7727eff60e3d487cef662c697
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-08-03 13:00:10 -0500

    Added FIXME comment

--- Yosemite/Yosemite/Actions/OrderStatsAction.swift
+++ Yosemite/Yosemite/Actions/OrderStatsAction.swift
@@ -5,5 +5,8 @@ import Networking
 // MARK: - OrderStatsAction: Defines all of the Actions supported by the OrderStatsStore.
 //
 public enum OrderStatsAction: Action {
+
+    // FIXME: We are returning OrderStats in the completion handler...this needs to eventually return an error/nil much like
+    // OrderAction.retrieveOrders. Update this once OrderStats storage is in place.
     case retrieveOrderStats(siteID: Int, granularity: OrderStatGranularity, latestDateToInclude: Date, quantity: Int, onCompletion: (OrderStats?, Error?) -> Void)
 }

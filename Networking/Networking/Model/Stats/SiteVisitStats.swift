6baaa6270faad69b802266602bdc25bd518dccc8
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-08-06 10:31:06 -0500

    Added SiteVisitStats remote + models

---
+++ Networking/Networking/Model/Stats/SiteVisitStats.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+
+/// Represents site visit stats over a specific period.
+///
+public struct SiteVisitStats: Decodable {
+    public let date: String
+    public let granularity: OrderStatGranularity
+    public let fields: [String]
+    public let items: [SiteVisitStatsItem]?
+
+    /// The public initializer for order stats.
+    ///
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let date = try container.decode(String.self, forKey: .date)
+        let granularity = try container.decode(OrderStatGranularity.self, forKey: .unit)
+
+        let fields = try container.decode([String].self, forKey: .fields)
+        let rawData: [[AnyCodable]] = try container.decode([[AnyCodable]].self, forKey: .data)
+
+        let items = rawData.map({ SiteVisitStatsItem(fieldNames: fields, rawData: $0) })
+
+        self.init(date: date, granularity: granularity, fields: fields, items: items)
+    }
+
+
+    /// OrderStats struct initializer.
+    ///
+    public init(date: String, granularity: OrderStatGranularity, fields: [String], items: [SiteVisitStatsItem]?) {
+        self.date = date
+        self.granularity = granularity
+        self.fields = fields
+        self.items = items
+    }
+}
+
+
+/// Defines all of the SiteVisitStats CodingKeys.
+///
+private extension SiteVisitStats {
+
+    enum CodingKeys: String, CodingKey {
+        case date = "date"
+        case unit = "unit"
+        case fields = "fields"
+        case data = "data"
+    }
+}
+
+
+// MARK: - Comparable Conformance
+//
+extension SiteVisitStats: Comparable {
+    public static func == (lhs: SiteVisitStats, rhs: SiteVisitStats) -> Bool {
+        return lhs.date == rhs.date &&
+            lhs.granularity == rhs.granularity &&
+            lhs.fields == rhs.fields &&
+            lhs.items == rhs.items
+    }
+
+    public static func < (lhs: SiteVisitStats, rhs: SiteVisitStats) -> Bool {
+        return lhs.date < rhs.date
+    }
+}

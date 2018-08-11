62b3ccb3b0664230632ddcbca767bc6cf7274aac
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-08-10 19:08:19 -0500

    Lint updates

--- WooCommerce/Classes/Extensions/Double+Woo.swift
+++ WooCommerce/Classes/Extensions/Double+Woo.swift
@@ -23,24 +23,24 @@ extension Double {
     ///
     func friendlyString() -> String {
         var num = Double(self)
-        let sign = ((num < 0) ? "-" : "" );
-        num = fabs(num);
+        let sign = ((num < 0) ? "-" : "" )
+        num = fabs(num)
         if -1000.0..<1000.0 ~= num {
             let intNum = Int(num)
             if intNum == 0 {
-                return "\(Int(num))";
+                return "\(Int(num))"
             }
-            return "\(sign)\(Int(num))";
+            return "\(sign)\(Int(num))"
         }
 
-        let exp = Int(log10(num) / 3.0 ); // log10(1000)
-        let units = ["k", "m", "b", "t"];
-        let roundedNum: Double = Foundation.round(10 * num / pow(1000.0, Double(exp))) / 10;
+        let exp = Int(log10(num) / 3.0 ) // log10(1000)
+        let units = ["k", "m", "b", "t"]
+        let roundedNum: Double = Foundation.round(10 * num / pow(1000.0, Double(exp))) / 10
 
         if roundedNum == 1000.0 {
-            return "\(sign)\(1.0)\(units[exp])";
+            return "\(sign)\(1.0)\(units[exp])"
         } else {
-            return "\(sign)\(roundedNum)\(units[exp-1])";
+            return "\(sign)\(roundedNum)\(units[exp-1])"
         }
     }
 

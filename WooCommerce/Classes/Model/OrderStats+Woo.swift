750955114e4ee49bbf108b9ef13410985243baa9
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-08-14 12:20:39 -0500

    Added missing space

--- WooCommerce/Classes/Model/OrderStats+Woo.swift
+++ WooCommerce/Classes/Model/OrderStats+Woo.swift
@@ -9,7 +9,7 @@ extension OrderStats {
     /// Returns the Currency Symbol associated with the current order stats.
     ///
     var currencySymbol: String {
-        guard let currency = items?.filter({ !$0.currency.isEmpty}).first?.currency else {
+        guard let currency = items?.filter({ !$0.currency.isEmpty }).first?.currency else {
             return ""
         }
         guard let identifier = Locale.availableIdentifiers.first(where: { Locale(identifier: $0).currencyCode == currency }) else {

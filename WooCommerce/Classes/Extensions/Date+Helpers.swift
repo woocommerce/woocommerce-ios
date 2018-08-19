a0fbc0f2448fcd2f2a63f7e99b624b58c8e0c8ff
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-08-18 19:17:36 -0500

    WIP date formatting for charting

--- WooCommerce/Classes/Extensions/Date+Helpers.swift
+++ WooCommerce/Classes/Extensions/Date+Helpers.swift
@@ -18,4 +18,50 @@ extension DateFormatter {
             return formatter
         }()
     }
+
+    /// Chart Formatters
+    ///
+    struct Charts {
+
+        /// Date formatter used for creating the date displayed on a chart axis for **day** granularity.
+        ///
+        public static let chartsDayFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+            formatter.timeZone = TimeZone(identifier: "GMT")
+            formatter.dateFormat = "MMM dd"
+            return formatter
+        }()
+
+        /// Date formatter used for creating the date displayed on a chart axis for **week** granularity.
+        ///
+        public static let chartsWeekFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+            formatter.timeZone = TimeZone(identifier: "GMT")
+            formatter.dateFormat = "MMM yyyy"
+            return formatter
+        }()
+
+        /// Date formatter used for creating the date displayed on a chart axis for **month** granularity.
+        ///
+        public static let chartsMonthFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+            formatter.timeZone = TimeZone(identifier: "GMT")
+            formatter.dateFormat = "MMM yyyy"
+            return formatter
+        }()
+
+        /// Date formatter used for creating the date displayed on a chart axis for **year** granularity.
+        ///
+        public static let chartsYearFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+            formatter.timeZone = TimeZone(identifier: "GMT")
+            formatter.dateFormat = "yyyy"
+            return formatter
+        }()
+    }
+
 }

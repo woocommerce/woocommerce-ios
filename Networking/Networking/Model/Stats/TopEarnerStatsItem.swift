d30348e52709b23efe26f0d0d1313be649505994
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-09-04 15:45:20 -0500

    WIP: Top earner model + remote+ mapper in Networking fmwk

---
+++ Networking/Networking/Model/Stats/TopEarnerStatsItem.swift
@@ -0,0 +1,84 @@
+import Foundation
+
+
+/// Represents a single top earner stat for a specific period.
+///
+public struct TopEarnerStatsItem: Decodable {
+
+    /// Product ID
+    ///
+    public let productID: Int
+
+    /// Product name
+    ///
+    public let pruductName: String
+
+    /// Quantity sold
+    ///
+    public let quantity: Int
+
+    /// Average price of item
+    ///
+    public let price: String
+
+    /// Total revenue from product
+    ///
+    public let total: String
+
+    /// Currency
+    ///
+    public let currency: String
+
+    /// Image URL for product
+    ///
+    public let imageUrl: String?
+
+
+    /// Designated Initializer.
+    ///
+    public init(productID: Int, pruductName: String, quantity: Int, price: String, total: String, currency: String, imageUrl: String?) {
+        self.productID = productID
+        self.pruductName = pruductName
+        self.quantity = quantity
+        self.price = price
+        self.total = total
+        self.currency = currency
+        self.imageUrl = imageUrl
+    }
+}
+
+
+/// Defines all of the TopEarnerStatsItem CodingKeys.
+///
+private extension TopEarnerStatsItem {
+    enum CodingKeys: String, CodingKey {
+        case productID = "ID"
+        case pruductName = "name"
+        case total = "total"
+        case quantity = "quantity"
+        case price = "price"
+        case imageUrl = "image"
+        case currency = "currency"
+    }
+}
+
+
+// MARK: - Comparable Conformance
+//
+extension TopEarnerStatsItem: Comparable {
+    public static func == (lhs: TopEarnerStatsItem, rhs: TopEarnerStatsItem) -> Bool {
+        return lhs.productID == rhs.productID &&
+            lhs.pruductName == rhs.pruductName &&
+            lhs.quantity == rhs.quantity &&
+            lhs.price == rhs.price &&
+            lhs.total == rhs.total &&
+            lhs.currency == rhs.currency &&
+            lhs.imageUrl == rhs.imageUrl
+    }
+
+    public static func < (lhs: TopEarnerStatsItem, rhs: TopEarnerStatsItem) -> Bool {
+        return lhs.productID < rhs.productID ||
+            (lhs.productID == rhs.productID && lhs.pruductName < rhs.pruductName) ||
+            (lhs.productID == rhs.productID && lhs.pruductName < rhs.pruductName && lhs.quantity < rhs.quantity)
+    }
+}

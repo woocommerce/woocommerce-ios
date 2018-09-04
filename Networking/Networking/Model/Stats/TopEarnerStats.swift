38c1fc1bf21b94efc5596dcf723a892b4ab9bf71
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-09-04 15:45:20 -0500

    WIP: Top earner model + remote+ mapper in Networking fmwk

---
+++ Networking/Networking/Model/Stats/TopEarnerStats.swift
@@ -0,0 +1,64 @@
+import Foundation
+
+
+/// Represents Top Earner (aka top performer) stats over a specific period.
+///
+public struct TopEarnerStats: Decodable {
+    public let date: String
+    public let granularity: StatGranularity
+    public let limit: String
+    public let items: [TopEarnerStatsItem]?
+
+
+    /// The public initializer for top earner stats.
+    ///
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let date = try container.decode(String.self, forKey: .date)
+        let granularity = try container.decode(StatGranularity.self, forKey: .unit)
+        let limit = try container.decode(String.self, forKey: .limit)
+        let items = try container.decode([TopEarnerStatsItem].self, forKey: .data)
+
+        self.init(date: date, granularity: granularity, limit: limit, items: items)
+    }
+
+
+    /// TopEarnerStats struct initializer.
+    ///
+    public init(date: String, granularity: StatGranularity, limit: String, items: [TopEarnerStatsItem]?) {
+        self.date = date
+        self.granularity = granularity
+        self.limit = limit
+        self.items = items
+    }
+}
+
+
+/// Defines all of the TopEarnerStats CodingKeys.
+///
+private extension TopEarnerStats {
+    enum CodingKeys: String, CodingKey {
+        case date = "date"
+        case unit = "unit"
+        case limit = "limit"
+        case data = "data"
+    }
+}
+
+
+// MARK: - Comparable Conformance
+//
+extension TopEarnerStats: Comparable {
+    public static func == (lhs: TopEarnerStats, rhs: TopEarnerStats) -> Bool {
+        return lhs.date == rhs.date &&
+            lhs.granularity == rhs.granularity &&
+            lhs.limit == rhs.limit &&
+            lhs.items == rhs.items
+    }
+
+    public static func < (lhs: TopEarnerStats, rhs: TopEarnerStats) -> Bool {
+        return lhs.date < rhs.date ||
+            (lhs.date == rhs.date && lhs.limit < rhs.limit)
+    }
+}

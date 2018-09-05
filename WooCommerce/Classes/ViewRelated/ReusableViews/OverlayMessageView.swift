033b11ecd99bb7dd858734d294805e8b7ca7ea7b
Jorge Leandro Perez <jorge@lantean.co>
2018-09-05 13:37:19 -0300

    Implements OverlayMessageView

---
+++ WooCommerce/Classes/ViewRelated/ReusableViews/OverlayMessageView.swift
@@ -0,0 +1,87 @@
+import Foundation
+import UIKit
+
+
+/// OverlayMessageView: Displays an `Image + Text + Action` in a given superview.
+///
+class OverlayMessageView: UIView {
+
+    /// Overlay's ImageView.
+    ///
+    @IBOutlet private var imageView: UIImageView!
+
+    /// Overlay's Message Label.
+    ///
+    @IBOutlet private var messageLabel: UILabel!
+
+    /// Overlay's Action Button.
+    ///
+    @IBOutlet private var actionButton: UIButton!
+
+    /// Overlay's Top Image.
+    ///
+    var messageImage: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+        }
+    }
+
+    /// Message to be displayed below the Image.
+    ///
+    var messageText: String? {
+        get {
+            return messageLabel.text
+        }
+        set {
+            messageLabel.text = newValue
+        }
+    }
+
+    /// Action Button's Text.
+    ///
+    var actionText: String? {
+        get {
+            return actionButton.titleLabel?.text
+        }
+        set {
+            actionButton.setTitle(newValue, for: .normal)
+        }
+    }
+
+
+    /// Closure to be executed whenever the Action Button is pressed.
+    ///
+    var onAction: (() -> Void)?
+
+
+
+    // MARK: - Overridden Methods
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        backgroundColor = StyleManager.tableViewBackgroundColor
+        messageLabel.applyBodyStyle()
+        actionButton.applySecondaryButtonStyle()
+    }
+
+
+    // MARK: - Public Methods
+
+    /// Displays the receiver in a given superview. The Overlay will be attached to all edges.
+    ///
+    func attach(to superview: UIView) {
+        superview.addSubview(self)
+        superview.pinSubviewToAllEdges(self)
+    }
+
+
+    // MARK: - IBActions
+
+    @IBAction func buttonWasPressed() {
+        onAction?()
+    }
+}

3546d24a8c013bb49b01ce934ad206f19bf79952
Jorge Leandro Perez <jorge@lantean.co>
2018-09-12 11:37:25 -0300

    Revert to this commit to test Error State

--- WooCommerce/Classes/ViewRelated/Orders/OrdersViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/OrdersViewController.swift
@@ -218,10 +218,10 @@ extension OrdersViewController: SyncingCoordinatorDelegate {
                 return
             }
 
-            if let error = error {
-                DDLogError("⛔️ Error synchronizing orders: \(error)")
+//            if let error = error {
+//                DDLogError("⛔️ Error synchronizing orders: \(error)")
                 self.displaySyncingErrorNotice(retryPageNumber: pageNumber)
-            }
+//            }
 
             self.state.transitionToResultsUpdatedState(isEmpty: self.isEmpty, isFiltered: self.isFiltered)
             onCompletion?(error == nil)
@@ -498,15 +498,15 @@ private extension OrdersViewController {
         /// Transitions to `.results` / `.emptyFiltered` / `.emptyUnfiltered` accordingly.
         ///
         mutating func transitionToResultsUpdatedState(isEmpty: Bool, isFiltered: Bool) {
-//            if isEmpty == false {
-//                self = .results
-//                return
-//            }
-//
-//            if isFiltered {
-//                self = .emptyFiltered
-//                return
-//            }
+            if isEmpty == false {
+                self = .results
+                return
+            }
+
+            if isFiltered {
+                self = .emptyFiltered
+                return
+            }
 
             self = .emptyUnfiltered
         }

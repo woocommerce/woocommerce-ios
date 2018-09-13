7cbefb80d80857b078b60dadc1be3542e6250ae8
Jorge Leandro Perez <jorge@lantean.co>
2018-09-13 12:39:40 -0300

    ContactViewModel: Nukes type property

--- WooCommerce/Classes/ViewModels/ContactViewModel.swift
+++ WooCommerce/Classes/ViewModels/ContactViewModel.swift
@@ -4,26 +4,16 @@ import Gridicons
 import Contacts
 import Yosemite
 
-enum ContactType {
-    case billing
-    case shipping
-}
+
 
 class ContactViewModel {
-    let title: String
     let fullName: String
     let formattedAddress: String?
     let cleanedPhoneNumber: String?
     let phoneNumber: String?
     let email: String?
 
-    init(with address: Address, contactType: ContactType) {
-        switch contactType {
-        case .billing:
-            title = NSLocalizedString("Billing details", comment: "Billing title for customer info cell")
-        case .shipping:
-            title = NSLocalizedString("Shipping details", comment: "Shipping title for customer info cell")
-        }
+    init(with address: Address) {
         let contact = CNContact.from(address: address)
         fullName = CNContactFormatter.string(from: contact, style: .fullName) ?? address.firstName + " " + address.lastName
         phoneNumber = contact.phoneNumbers.first?.value.stringValue

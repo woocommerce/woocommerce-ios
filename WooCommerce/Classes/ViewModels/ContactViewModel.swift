955089e9e355f2f4e0f865f3372e7edcc315d806
Jorge Leandro Perez <jorge@lantean.co>
2018-09-13 17:07:10 -0300

    ContactViewModel: Removes unused properties

--- WooCommerce/Classes/ViewModels/ContactViewModel.swift
+++ WooCommerce/Classes/ViewModels/ContactViewModel.swift
@@ -7,18 +7,14 @@ import Yosemite
 
 
 class ContactViewModel {
-    let fullName: String
-    let formattedAddress: String?
     let cleanedPhoneNumber: String?
     let phoneNumber: String?
     let email: String?
 
     init(with address: Address) {
         let contact = CNContact.from(address: address)
-        fullName = CNContactFormatter.string(from: contact, style: .fullName) ?? address.firstName + " " + address.lastName
         phoneNumber = contact.phoneNumbers.first?.value.stringValue
         cleanedPhoneNumber = address.phone?.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
-        formattedAddress = contact.postalAddresses.first?.value.formatted(as: .mailingAddress) ?? ""
         email = contact.emailAddresses.first?.value as String?
     }
 }

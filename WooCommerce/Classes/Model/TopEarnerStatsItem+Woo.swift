78e9cefc1495c035144ed54eccf5b20eea37000b
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-09-13 16:03:49 -0500

    Top Perf: Added formatted price string to table

---
+++ WooCommerce/Classes/Model/TopEarnerStatsItem+Woo.swift
@@ -0,0 +1,27 @@
+import Foundation
+import Yosemite
+
+
+// MARK: - TopEarnerStatsItem Helper Methods
+//
+extension TopEarnerStatsItem {
+
+    /// Returns the Currency Symbol associated with the current TopEarnerStatsItem.
+    ///
+    var currencySymbol: String {
+        guard !currency.isEmpty else {
+            return ""
+        }
+        guard let identifier = Locale.availableIdentifiers.first(where: { Locale(identifier: $0).currencyCode == currency }) else {
+            return currency
+        }
+
+        return Locale(identifier: identifier).currencySymbol ?? currency
+    }
+
+    /// Returns the a friendly-formatted price string including the currency symbol
+    ///
+    var formattedPriceString: String {
+        return currencySymbol + price.friendlyString()
+    }
+}

a09cf5ae810dc46f0053544f44f0dce5d03a4430
Thuy.Copeland <thuycopeland@gmail.com>
2018-09-14 12:29:56 -0500

    Change to public struct. Change NumberFormatter to a private property. (Missing: a single instance to use everywhere.)

--- WooCommerce/Classes/Tools/MoneyFormatter.swift
+++ WooCommerce/Classes/Tools/MoneyFormatter.swift
@@ -1,11 +1,20 @@
 import Foundation
 
-struct MoneyFormatter {
+public struct MoneyFormatter {
+
+    private var currencyFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currencyISOCode
+
+        return formatter
+    }
 
     /// Returns a localized and formatted currency string, including zero values.
     ///
-    func format(value: String, currencyCode: String) -> String {
-        let formatter = currencyFormatter(currencyCode: currencyCode)
+    func format(value: String, currencyCode: String, locale: Locale = Locale.current) -> String {
+        currencyFormatter.locale = locale
+        currencyFormatter.currencyCode = currencyCode
         let decimalValue = Decimal(string: value) ?? Decimal(string: "0.0")
 
         guard let decimal = decimalValue else {
@@ -13,7 +22,7 @@ struct MoneyFormatter {
         }
 
         let decimalNumber = NSDecimalNumber(decimal: decimal)
-        guard let numberValue = formatter.string(from: decimalNumber) else {
+        guard let numberValue = currencyFormatter.string(from: decimalNumber) else {
             fatalError()
         }
 
@@ -22,10 +31,11 @@ struct MoneyFormatter {
 
     /// Returns a localized and formatted currency string, including zero values.
     ///
-    func format(value: Decimal, currencyCode: String) -> String {
-        let formatter = currencyFormatter(currencyCode: currencyCode)
+    func format(value: Decimal, currencyCode: String, locale: Locale = Locale.current) -> String {
+        currencyFormatter.locale = locale
+        currencyFormatter.currencyCode = currencyCode
         let decimalNumber = NSDecimalNumber(decimal: value)
-        guard let numberValue = formatter.string(from: decimalNumber) else {
+        guard let numberValue = currencyFormatter.string(from: decimalNumber) else {
             fatalError()
         }
 
@@ -34,43 +44,32 @@ struct MoneyFormatter {
 
     /// Returns a localized and formatted currency string, or nil if empty or zero.
     ///
-    func formatIfNonZero(value: String, currencyCode: String) -> String? {
+    func formatIfNonZero(value: String, currencyCode: String, locale: Locale = Locale.current) -> String? {
+        currencyFormatter.locale = locale
+        currencyFormatter.currencyCode = currencyCode
         guard value.isEmpty == false else {
             return nil
         }
+        currencyFormatter.currencyCode = currencyCode
 
         let decimalNumber = NSDecimalNumber(string: value)
         if decimalNumber.decimalValue.isZero {
             return nil
         }
 
-        let formatter = currencyFormatter(currencyCode: currencyCode)
-
-        return formatter.string(from: decimalNumber)
+        return currencyFormatter.string(from: decimalNumber)
     }
 
     /// Returns a localized and formatted currency string, or nil if value is zero.
     ///
-    func formatIfNonZero(value: Decimal, currencyCode: String) -> String? {
+    func formatIfNonZero(value: Decimal, currencyCode: String, locale: Locale = Locale.current) -> String? {
+        currencyFormatter.locale = locale
+        currencyFormatter.currencyCode = currencyCode
         let decimalNumber = NSDecimalNumber(decimal: value)
         if decimalNumber.decimalValue.isZero {
             return nil
         }
 
-        let formatter = currencyFormatter(currencyCode: currencyCode)
-
-        return formatter.string(from: decimalNumber)
-    }
-
-    /// Returns a currency formatter instance based on ISO 4217 currency codes.
-    ///
-    func currencyFormatter(currencyCode: String) -> NumberFormatter {
-        let currencyFormatter = NumberFormatter()
-        currencyFormatter.usesGroupingSeparator = true
-        currencyFormatter.numberStyle = .currencyISOCode
-        currencyFormatter.locale = Locale.current
-        currencyFormatter.currencyCode = currencyCode
-
-        return currencyFormatter
+        return currencyFormatter.string(from: decimalNumber)
     }
 }

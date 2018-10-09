92ab17c697755702cdb38b77103e6e36c79f45c1
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-10-09 16:42:29 -0500

    Added SiteVisitStats ReadOnlyConvertible and ReadOnlyType

--- Storage/Storage/Model/SiteVisitStats+CoreDataProperties.swift
+++ Storage/Storage/Model/SiteVisitStats+CoreDataProperties.swift
@@ -8,8 +8,8 @@ extension SiteVisitStats {
         return NSFetchRequest<SiteVisitStats>(entityName: "SiteVisitStats")
     }
 
-    @NSManaged public var date: String?
-    @NSManaged public var granularity: String?
+    @NSManaged public var date: String
+    @NSManaged public var granularity: String
     @NSManaged public var items: Set<SiteVisitStatsItem>?
 }
 

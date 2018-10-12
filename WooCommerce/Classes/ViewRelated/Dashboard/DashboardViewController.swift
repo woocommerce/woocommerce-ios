c9bf7a80f39369061fe2e57211cf273bb71c3fbe
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-10-12 14:22:24 -0500

    Added DispatchGroups to stats syncing logic

--- WooCommerce/Classes/ViewRelated/Dashboard/DashboardViewController.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/DashboardViewController.swift
@@ -146,10 +146,26 @@ private extension DashboardViewController {
 
     func reloadData() {
         DDLogInfo("♻️ Requesting dashboard data be reloaded...")
-        storeStatsViewController.syncAllStats()
-        newOrdersViewController.syncNewOrders()
-        topPerformersViewController.syncTopPerformers()
-        refreshControl.endRefreshing()
+        let group = DispatchGroup()
+
+        group.enter()
+        storeStatsViewController.syncAllStats() {
+            group.leave()
+        }
+
+        group.enter()
+        newOrdersViewController.syncNewOrders() {
+            group.leave()
+        }
+
+        group.enter()
+        topPerformersViewController.syncTopPerformers() {
+            group.leave()
+        }
+
+        group.notify(queue: .main) { [weak self] in
+            self?.refreshControl.endRefreshing()
+        }
     }
 
     func applyUnhideAnimation(for view: UIView) {

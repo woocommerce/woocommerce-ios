fd82db9dce81587760e9a911ae7c45bb4934edc0
Thuy.Copeland <thuycopeland@gmail.com>
2018-10-12 10:30:24 -0500

    Add privacy settings view controller

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/PrivacySettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PrivacySettingsViewController.swift
+//  WooCommerce
+//
+//  Created by Thuy Copeland on 10/12/18.
+//  Copyright © 2018 Automattic. All rights reserved.
+//
+
+import UIKit
+
+class PrivacySettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

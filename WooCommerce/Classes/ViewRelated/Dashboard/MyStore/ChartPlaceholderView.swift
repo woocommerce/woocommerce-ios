15d8f4ba9f48a37f22eb63800abafb25c8f3d5e6
Jorge Leandro Perez <jorge@lantean.co>
2018-10-12 14:28:57 -0300

    Implements ChartPlaceholderView

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/MyStore/ChartPlaceholderView.swift
@@ -0,0 +1,46 @@
+import Foundation
+import UIKit
+
+
+// ChartPlaceholderView: Charts Mockup UI!
+//
+class ChartPlaceholderView: UIView {
+
+    /// Top Container View
+    ///
+    @IBOutlet private var topStackView: UIStackView!
+
+    /// Bars Container View
+    ///
+    @IBOutlet private var barsStackView: UIStackView!
+
+    // MARK: - Overridden Methods
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupSubviews()
+    }
+}
+
+
+// MARK: - Private Methods
+//
+private extension ChartPlaceholderView {
+
+    /// Applies Rounded Style to the upper views.
+    ///
+    func setupSubviews() {
+        let subviews = barsStackView.subviews + topStackView.subviews.compactMap { $0.subviews.first }
+        for view in subviews {
+            view.layer.cornerRadius = Settings.cornerRadius
+            view.layer.masksToBounds = true
+        }
+    }
+}
+
+
+// MARK: - Private Types
+//
+private enum Settings {
+    static let cornerRadius = CGFloat(6)
+}

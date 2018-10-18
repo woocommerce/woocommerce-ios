53fcdd5c64b7991919db1156f4845b5ff60bc101
Thuy.Copeland <thuycopeland@gmail.com>
2018-10-18 15:43:35 -0500

    Set up Crashlytics methods to allow users to opt-out of tracking

--- WooCommerce/Classes/Tools/Fabric/FabricManager.swift
+++ WooCommerce/Classes/Tools/Fabric/FabricManager.swift
@@ -19,7 +19,18 @@ class FabricManager {
     /// Initializes the Fabric SDK.
     ///
     func initialize() {
-        Fabric.with([Crashlytics.self])
+        startCrashlyticsIfNeeded()
+    }
+
+    /// Starts Crashlytics
+    ///
+    func startCrashlyticsIfNeeded() {
+        initializeOptOutTracking()
+
+        if !userHasOptedOut() {
+            Fabric.with([Crashlytics.self])
+            startListeningToAuthNotifications()
+        }
     }
 
     /// Starts listening to Authentication Notifications: Fabric's metadata will be refreshed accordingly.
@@ -27,6 +38,13 @@ class FabricManager {
     func startListeningToAuthNotifications() {
         let nc = NotificationCenter.default
         nc.addObserver(self, selector: #selector(defaultAccountWasUpdated), name: .defaultAccountWasUpdated, object: nil)
+    }
+
+    /// Stops listening to Authentication Notifications
+    /// after tracking opt-out event
+    func stopListeningToAuthNotifications() {
+        let nc = NotificationCenter.default
+        nc.removeObserver(self, name: .defaultAccountWasUpdated, object: nil)
     }
 
     /// Handles the `.sessionWasAuthenticated` notification.
@@ -43,6 +61,67 @@ class FabricManager {
             DDLogInfo("🌡 Fabric Account: [\(username)]")
         } else {
             DDLogInfo("🌡 Fabric Account Nuked!")
+        }
+    }
+
+    /// Clears Crashlytics data after opt-out of tracking event
+    ///
+    func clearCrashlyticsParameters() {
+        let crashlytics = Crashlytics.sharedInstance()
+
+        crashlytics.setUserName(nil)
+        crashlytics.setUserEmail(nil)
+        crashlytics.setUserIdentifier(nil)
+    }
+}
+
+
+// Mark: - Tracking Opt Out
+//
+extension FabricManager {
+    /// Initialize the opt-out tracking
+    ///
+    func initializeOptOutTracking() {
+        if userHasOptedOutIsSet() {
+            // We've already configured the opt out setting
+            return
+        }
+
+        // set the default to no, user has not opted out yet
+        setUserHasOptedOutValue(false)
+    }
+
+    func userHasOptedOutIsSet() -> Bool {
+        return UserDefaults.standard.object(forKey: UserDefaults.Key.userOptedOutOfCrashlytics) != nil
+    }
+
+    /// This method just sets the user defaults value for UserOptedOut and doesn't
+    /// do any additional configuration of sessions or trackers.
+    func setUserHasOptedOutValue(_ optedOut: Bool) {
+        UserDefaults.standard.set(optedOut, forKey: UserDefaults.Key.userOptedOutOfCrashlytics)
+    }
+
+    func userHasOptedOut() -> Bool {
+        return UserDefaults.standard.bool(forKey: UserDefaults.Key.userOptedOutOfCrashlytics.rawValue)
+    }
+
+    func setUserHasOptedOut(_ optedOut: Bool) {
+        if userHasOptedOutIsSet() {
+            let currentValue = userHasOptedOut()
+            if currentValue == optedOut {
+                return
+            }
+        }
+
+        // store the preference
+        setUserHasOptedOutValue(optedOut)
+
+        // now take action on the preference
+        if (optedOut) {
+            clearCrashlyticsParameters()
+            stopListeningToAuthNotifications()
+        } else {
+            startCrashlyticsIfNeeded()
         }
     }
 }

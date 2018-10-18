eedce34e381085f8ad27f24cfb01b92a0f98e5f6
Thuy.Copeland <thuycopeland@gmail.com>
2018-10-18 09:21:44 -0500

    Fix separator line bug in TopLeftImage table view cell. Launch web view for cookie and privacy policy URLs.

--- WooCommerce/Classes/ViewRelated/ReusableViews/TopLeftImageTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/TopLeftImageTableViewCell.swift
@@ -1,22 +1,27 @@
 import UIKit
 
 /// Vertically aligns the image to the top of the cell instead of basic cell's center alignment.
-
 ///
 class TopLeftImageTableViewCell: UITableViewCell {
-
-    @IBOutlet public var leftImageView: UIImageView! {
-        didSet {
-            let leftInset = leftImageView.frame.origin.x + leftImageView.frame.width + leadingLabelConstraint.constant
-            separatorInset = UIEdgeInsets(top: 0, left: leftInset, bottom: 0, right: 0)
-        }
-    }
-    @IBOutlet public var label: UILabel!
-    @IBOutlet private var leadingLabelConstraint: NSLayoutConstraint!
 
     override func awakeFromNib() {
         super.awakeFromNib()
 
-        label?.applyBodyStyle()
+        textLabel?.applyBodyStyle()
+        textLabel?.numberOfLines = 0
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView?.frame.size = CGSize(width: Constants.iconW, height: Constants.iconH)
+        imageView?.frame.origin.y = Constants.iconY
+    }
+}
+
+extension TopLeftImageTableViewCell {
+    struct Constants {
+        static let iconW = CGFloat(24)
+        static let iconH = CGFloat(24)
+        static let iconY = CGFloat(11)
     }
 }

f749e227d7885d804349df56232c765e99c7e947
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-10-19 17:18:06 -0500

    Added unit tests for SiteSettingsMapper

--- Networking/Networking/Model/SiteSetting.swift
+++ Networking/Networking/Model/SiteSetting.swift
@@ -1,33 +1,46 @@
 import Foundation
 
 
-/// Represents a specific setting Entity for a site.
+/// Represents a specific setting entity for a specific site.
 ///
 public struct SiteSetting: Decodable {
+    public let siteID: Int
     public let settingID: String
     public let label: String
-    public let description: String
+    public let settingDescription: String
     public let value: String
 
     /// OrderNote struct initializer.
     ///
-    public init(settingID: String, label: String, description: String, value: String) {
+    public init(siteID: Int, settingID: String, label: String, description: String, value: String) {
+        self.siteID = siteID
         self.settingID = settingID
         self.label = label
-        self.description = description
+        self.settingDescription = description
         self.value = value
     }
 
-    /// The public initializer for OrderNote.
+    /// The public initializer for SiteSetting.
     ///
     public init(from decoder: Decoder) throws {
+        guard let siteID = decoder.userInfo[.siteID] as? Int else {
+            throw SiteSettingError.missingSiteID
+        }
+
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let settingID = try container.decode(String.self, forKey: .settingID)
-        let label = try container.decode(String.self, forKey: .label)
-        let description = try container.decode(String.self, forKey: .description)
-        let value = try container.decode(String.self, forKey: .value)
+        let label = try container.decodeIfPresent(String.self, forKey: .label) ?? ""
+        let settingDescription = try container.decodeIfPresent(String.self, forKey: .settingDescription) ?? ""
 
-        self.init(settingID: settingID, label: label, description: description, value: value) // initialize the struct
+        // Note: `value` is a mixed type per the documentation — usually a String but could be an Array, Int, etc
+        // For the specific settings we are interested in, it is a String type.
+        // See: https://woocommerce.github.io/woocommerce-rest-api-docs/#setting-options for more details.
+        var value = ""
+        if let stringValue = try? container.decode(String.self, forKey: .value) {
+            value = stringValue
+        }
+
+        self.init(siteID: siteID, settingID: settingID, label: label, description: settingDescription, value: value) // initialize the struct
     }
 }
 
@@ -37,10 +50,10 @@ public struct SiteSetting: Decodable {
 private extension SiteSetting {
 
     enum CodingKeys: String, CodingKey {
-        case settingID      = "id"
-        case label          = "label"
-        case description    = "description"
-        case value          = "value"
+        case settingID          = "id"
+        case label              = "label"
+        case settingDescription = "description"
+        case value              = "value"
     }
 }
 
@@ -51,7 +64,7 @@ extension SiteSetting: Comparable {
     public static func == (lhs: SiteSetting, rhs: SiteSetting) -> Bool {
         return lhs.settingID == rhs.settingID &&
             lhs.label == rhs.label &&
-            lhs.description == rhs.description &&
+            lhs.settingDescription == rhs.settingDescription &&
             lhs.value == rhs.value
     }
 
@@ -59,4 +72,11 @@ extension SiteSetting: Comparable {
         return lhs.settingID < rhs.settingID ||
             (lhs.settingID == rhs.settingID && lhs.label < rhs.label)
     }
+}
+
+
+// MARK: - Decoding Errors
+//
+enum SiteSettingError: Error {
+    case missingSiteID
 }

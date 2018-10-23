c84f0016376487fa4b45f9075faba4d16a37efa6
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-10-23 14:23:45 -0500

    Added all of the Storage fmwk stuff for SiteSetting

---
+++ Storage/Storage/Model/SiteSetting+CoreDataProperties.swift
@@ -0,0 +1,17 @@
+import Foundation
+import CoreData
+
+
+extension SiteSetting {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<SiteSetting> {
+        return NSFetchRequest<SiteSetting>(entityName: "SiteSetting")
+    }
+
+    @NSManaged public var siteID: Int64
+    @NSManaged public var settingID: String?
+    @NSManaged public var label: String?
+    @NSManaged public var settingDescription: String?
+    @NSManaged public var value: String?
+
+}

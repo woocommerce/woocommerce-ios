5db7657a9d9ae29c7d4df24aec188f5a9c08c76f
Jorge Leandro Perez <jorge@lantean.co>
2018-10-24 21:01:19 -0300

    NotificationsRemoteTests: New Unit Tests

--- Networking/NetworkingTests/Remote/NotificationsRemoteTests.swift
+++ Networking/NetworkingTests/Remote/NotificationsRemoteTests.swift
@@ -72,4 +72,70 @@ class NotificationsRemoteTests: XCTestCase {
 
         wait(for: [expectation], timeout: Constants.expectationTimeout)
     }
+
+    /// Verifies that `updateLastSeen` correctly returns a Dotcom Error, whenever the request failed.
+    ///
+    func testUpdateLastSeenProperlyParsesErrorResponses() {
+        let remote = NotificationsRemote(network: network)
+        let expectation = self.expectation(description: "Error Handling")
+
+        network.simulateResponse(requestUrlSuffix: "notifications/seen", filename: "generic_error")
+
+        remote.updateLastSeen("") { error in
+            let error = error as? DotcomError
+            XCTAssertEqual(error?.code, "unknown_token")
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+
+    /// Verifies that `updateLastSeen` correctly parses a successful response.
+    ///
+    func testUpdateLastSeenProperlyParsesSuccessResponses() {
+        let remote = NotificationsRemote(network: network)
+        let expectation = self.expectation(description: "Error Handling")
+
+        network.simulateResponse(requestUrlSuffix: "notifications/seen", filename: "generic_success")
+
+        remote.updateLastSeen("") { error in
+            XCTAssertNil(error)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+
+    /// Verifies that `updateReadStatus` correctly returns a Dotcom Error, whenever the request failed.
+    ///
+    func testUpdateReadStatusProperlyParsesErrorResponses() {
+        let remote = NotificationsRemote(network: network)
+        let expectation = self.expectation(description: "Error Handling")
+
+        network.simulateResponse(requestUrlSuffix: "notifications/read", filename: "generic_error")
+
+        remote.updateReadStatus("", read: true) { error in
+            let error = error as? DotcomError
+            XCTAssertEqual(error?.code, "unknown_token")
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+
+    /// Verifies that `updateReadStatus` correctly parses a successful response.
+    ///
+    func testUpdateReadStatusProperlyParsesSuccessResponses() {
+        let remote = NotificationsRemote(network: network)
+        let expectation = self.expectation(description: "Error Handling")
+
+        network.simulateResponse(requestUrlSuffix: "notifications/read", filename: "generic_success")
+
+        remote.updateReadStatus("", read: true) { error in
+            XCTAssertNil(error)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
 }

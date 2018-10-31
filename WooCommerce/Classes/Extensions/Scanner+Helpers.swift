79c933cb7bce24b944fc07e9bdd8ed198dcde34c
Jorge Leandro Perez <jorge@lantean.co>
2018-10-31 11:15:24 -0300

    Scanner+Helpers: Quoted Text Scanner

---
+++ WooCommerce/Classes/Extensions/Scanner+Helpers.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+
+/// Scanner: Helper Methods
+///
+extension Scanner {
+
+    /// Returns the NSRange(s) for all of the quotes contained within the scanned string.
+    ///
+    func scanQuotedRanges() -> [NSRange] {
+        var output = [NSRange]()
+        let marker = "\""
+
+        while isAtEnd == false {
+            // Find + Drop the first quotation mark
+            scanUpTo(marker, into: nil)
+            scanString(marker, into: nil)
+
+            // Scan the actual quoted text
+            let start = scanLocation
+            scanUpTo(marker, into: nil)
+            let end = scanLocation
+
+            // Drop the closing mark
+            scanString(marker, into: nil)
+
+            // Build the Range
+            guard start >= 0 && end < string.count && start < end else {
+                continue
+            }
+
+            let foundationRange = NSRange(location: start, length: end - start)
+            output.append(foundationRange)
+        }
+
+        return output
+    }
+}

d7bb151a1132875b82a3f8acc88ffdcc8d225e78
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-11-01 14:26:51 -0500

    Added a failing unit test 😃

--- Yosemite/YosemiteTests/Stores/NotificationStoreTests.swift
+++ Yosemite/YosemiteTests/Stores/NotificationStoreTests.swift
@@ -42,13 +42,39 @@ class NotificationStoreTests: XCTestCase {
         let expectation = self.expectation(description: "Sync notifications")
         let notificationStore = NotificationStore(dispatcher: dispatcher, storageManager: storageManager, network: network)
 
-        // FIXME: Need a way to "stack" responses to the same endpoint
         network.simulateResponse(requestUrlSuffix: "notifications", filename: "notifications-load-hashes")
         network.simulateResponse(requestUrlSuffix: "notifications", filename: "notifications-load-all")
         XCTAssertEqual(viewStorage.countObjects(ofType: Storage.Note.self), 0)
         let action = NotificationAction.synchronizeNotifications() { (error) in
             XCTAssertNil(error)
             XCTAssertEqual(self.viewStorage.countObjects(ofType: Storage.Note.self), 40)
+
+            if let note = self.viewStorage.loadNotification(noteID: 123456, noteHash: 987654)?.toReadOnly() {
+                // Plain Fields
+                XCTAssertEqual(note.noteId, 123456)
+                XCTAssertEqual(note.hash, 987654)
+                XCTAssertEqual(note.read, false)
+                XCTAssert(note.icon == "https://gravatar.tld/some-hash")
+                XCTAssert(note.noticon == "\u{f408}")
+                XCTAssertEqual(note.timestamp, "2018-10-22T18:51:33+00:00")
+                XCTAssertEqual(note.type, "comment_like")
+                XCTAssertEqual(note.kind, .commentLike)
+                XCTAssert(note.url == "https://someurl.sometld")
+                XCTAssert(note.title == "3 Likes")
+
+                // Blocks
+                XCTAssertEqual(note.subject.count, 1)
+                XCTAssertEqual(note.header.count, 2)
+                XCTAssertEqual(note.body.count, 3)
+
+                // Meta
+                XCTAssertEqual(note.meta.identifier(forKey: .site), 123456)
+                XCTAssertEqual(note.meta.identifier(forKey: .post), 2996)
+                XCTAssertEqual(note.meta.link(forKey: .post), "https://public-someurl.sometld")
+            } else {
+                XCTFail()
+            }
+
             expectation.fulfill()
         }
 

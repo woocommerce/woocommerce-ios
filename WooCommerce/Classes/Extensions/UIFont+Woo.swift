682b1cb413523b7f296c14bea6ed1208b39aa3d0
Jorge Leandro Perez <jorge@lantean.co>
2018-11-02 16:05:48 -0300

    UIFont+Woo: Renames variables

--- WooCommerce/Classes/Extensions/UIFont+Woo.swift
+++ WooCommerce/Classes/Extensions/UIFont+Woo.swift
@@ -9,22 +9,22 @@ extension UIFont {
     /// Returns the receiver *Bold* version.
     ///
     var bold: UIFont {
-        guard let italicDescriptor = fontDescriptor.withSymbolicTraits(.traitBold) else {
+        guard let descriptor = fontDescriptor.withSymbolicTraits(.traitBold) else {
             DDLogError("# Error: Cannot toggle font to Bold: [\(self)]")
             return self
         }
 
-        return UIFont(descriptor: italicDescriptor, size: pointSize)
+        return UIFont(descriptor: descriptor, size: pointSize)
     }
 
     /// Returns the receiver *Italics* version.
     ///
     var italics: UIFont {
-        guard let italicDescriptor = fontDescriptor.withSymbolicTraits(.traitItalic) else {
+        guard let descriptor = fontDescriptor.withSymbolicTraits(.traitItalic) else {
             DDLogError("# Error: Cannot toggle font to Italics: [\(self)]")
             return self
         }
 
-        return UIFont(descriptor: italicDescriptor, size: pointSize)
+        return UIFont(descriptor: descriptor, size: pointSize)
     }
 }

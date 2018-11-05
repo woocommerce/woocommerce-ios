540eefed12da692de9e000356f63c14e3536474f
Jorge Leandro Perez <jorge@lantean.co>
2018-11-05 14:13:57 -0300

    Order+Woo: Nukes (Hidden) Age Entity

--- WooCommerce/Classes/Model/Order+Woo.swift
+++ WooCommerce/Classes/Model/Order+Woo.swift
@@ -15,26 +15,3 @@ extension Order {
         return age.description
     }
 }
-
-enum Age: String {
-    case months     = "0"
-    case weeks      = "2"
-    case days       = "4"
-    case yesterday  = "5"
-    case today      = "6"
-
-    var description: String {
-        switch self {
-        case .months:
-            return NSLocalizedString("Older than a Month", comment: "Notifications Months Section Header")
-        case .weeks:
-            return NSLocalizedString("Older than a Week", comment: "Notifications Weeks Section Header")
-        case .days:
-            return NSLocalizedString("Older than 2 days", comment: "Notifications +2 Days Section Header")
-        case .yesterday:
-            return NSLocalizedString("Yesterday", comment: "Notifications Yesterday Section Header")
-        case .today:
-            return NSLocalizedString("Today", comment: "Notifications Today Section Header")
-        }
-    }
-}

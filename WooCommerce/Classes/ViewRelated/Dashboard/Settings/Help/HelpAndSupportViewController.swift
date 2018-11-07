06fd29362fedbdc4563eaf3838a7bee9e1d09356
Thuy.Copeland <thuycopeland@gmail.com>
2018-11-06 18:30:23 -0600

    Add classes for UI creation: view controller, footer view, value 1 style cell

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Help/HelpAndSupportViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HelpAndSupportViewController.swift
+//  WooCommerce
+//
+//  Created by Thuy Copeland on 11/6/18.
+//  Copyright © 2018 Automattic. All rights reserved.
+//
+
+import UIKit
+
+class HelpAndSupportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Help/VersionFooterView.swift
@@ -0,0 +1,21 @@
+//
+//  VersionFooterView.swift
+//  WooCommerce
+//
+//  Created by Thuy Copeland on 11/6/18.
+//  Copyright © 2018 Automattic. All rights reserved.
+//
+
+import UIKit
+
+class VersionFooterView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

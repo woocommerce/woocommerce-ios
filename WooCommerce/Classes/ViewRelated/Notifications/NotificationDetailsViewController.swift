1289a8688b957312793f18b3cc328c82333f0d74
Jorge Leandro Perez <jorge@lantean.co>
2018-11-08 20:02:06 -0300

    NotificationDetailsViewController: Wiring NoteDetailsRow(s)

--- WooCommerce/Classes/ViewRelated/Notifications/NotificationDetailsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Notifications/NotificationDetailsViewController.swift
@@ -11,9 +11,25 @@ class NotificationDetailsViewController: UIViewController {
     ///
     @IBOutlet private var tableView: UITableView!
 
+    /// EntityListener: Update / Deletion Notifications.
+    ///
+    private lazy var entityListener: EntityListener<Note> = {
+        return EntityListener(storageManager: AppDelegate.shared.storageManager, readOnlyEntity: note)
+    }()
+
     /// Note to be displayed!
     ///
-    private var note: Note!
+    private var note: Note! {
+        didSet {
+            rebuildDetailRows(from: note)
+        }
+    }
+
+    /// DetailsRow(s): Each Row is mapped to a single UI Entity!
+    ///
+    private var rows = [NoteDetailsRow]()
+
+
 
     /// Designated Initializer
     ///
@@ -37,15 +53,16 @@ class NotificationDetailsViewController: UIViewController {
 
         configureNavigationItem()
         configureMainView()
+        configureEntityListener()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
 
-/// Header: [.header]
-/// Comment: [OLD Comment Cell + Text + Actions]
-/// Regular: [.image, .text, .user]
+/// Header:     [.header]
+/// Comment:    [OLD Comment Cell + Text + Actions]
+/// Regular:    [.image, .text, .user]
     }
 }
 
@@ -60,13 +77,53 @@ private extension NotificationDetailsViewController {
         title = note.title
 
         // Don't show the Notifications title in the next-view's back button
-        let backButton = UIBarButtonItem(title: String(), style: .plain, target: nil, action: nil)
-        navigationItem.backBarButtonItem = backButton
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: String(), style: .plain, target: nil, action: nil)
     }
 
     /// Setup: Main View
     ///
     func configureMainView() {
         view.backgroundColor = StyleManager.tableViewBackgroundColor
+    }
+
+    /// Setup: EntityListener
+    ///
+    func configureEntityListener() {
+        entityListener.onUpsert = { [weak self] note in
+            self?.rebuildDetailRows(from: note)
+        }
+
+        entityListener.onDelete = { [weak self] in
+            guard let `self` = self else {
+                return
+            }
+
+            self.navigationController?.popViewController(animated: true)
+            self.displayNoteDeletedNotice()
+        }
+    }
+}
+
+
+// MARK: - Private Methods
+//
+private extension NotificationDetailsViewController {
+
+    /// Reloads all of the Notification Detail Rows!
+    ///
+    func rebuildDetailRows(from note: Note) {
+        rows = NoteDetailsRow.details(from: note)
+        tableView.reloadData()
+    }
+
+
+    /// Displays a Notice onScreen, indicating that the current Note has been deleted from the Store.
+    ///
+    func displayNoteDeletedNotice() {
+        let title = NSLocalizedString("Deleted Notification!", comment: "Deleted Notification's Title")
+        let message = NSLocalizedString("The notification has been removed!", comment: "Displayed whenever a Notification that was onscreen got deleted.")
+        let notice = Notice(title: title, message: message, feedbackType: .error)
+
+        AppDelegate.shared.noticePresenter.enqueue(notice: notice)
     }
 }

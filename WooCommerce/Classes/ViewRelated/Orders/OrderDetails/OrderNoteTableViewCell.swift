f6def41b62462adcabe954b9d8d6ae098783b475
Jorge Leandro Perez <jorge@lantean.co>
2018-11-13 14:36:47 -0300

    OrderNoteTableViewCell: New Public API

--- WooCommerce/Classes/ViewRelated/Orders/OrderDetails/OrderNoteTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/Orders/OrderDetails/OrderNoteTableViewCell.swift
@@ -1,25 +1,41 @@
 import UIKit
+import Gridicons
 
+
+// MARK: - OrderNoteTableViewCell
+//
 class OrderNoteTableViewCell: UITableViewCell {
+
+    /// Top Left Icon
+    ///
+    @IBOutlet private var iconButton: UIButton!
+
+    /// Top Right Label
+    ///
+    @IBOutlet private var dateLabel: UILabel!
+
+    /// Secondary Top Label
+    ///
+    @IBOutlet private var statusLabel: UILabel!
+
+    /// Bottom Label
+    ///
+    @IBOutlet private var noteLabel: UILabel!
+
+
+    // MARK: - View Lifecycle
+
     override func awakeFromNib() {
         super.awakeFromNib()
 
-        dateLabel.applyBodyStyle()
-        statusLabel.applyBodyStyle()
-        noteLabel.applyBodyStyle()
-
-        statusLabel.textColor = StyleManager.wooGreyMid
-        iconButton.layer.cornerRadius = iconButton.frame.width / 2
-        iconButton.tintColor = .white
+        configureLabels()
+        configureIconButton()
     }
 
-    @IBOutlet public var iconButton: UIButton!
 
-    @IBOutlet private var dateLabel: UILabel!
-    @IBOutlet private var statusLabel: UILabel!
-    @IBOutlet private var noteLabel: UILabel!
-
-    public var dateCreated: String? {
+    /// Date of Creation: To be displayed at the top of the cell
+    ///
+    var dateCreated: String? {
         get {
             return dateLabel.text
         }
@@ -28,21 +44,57 @@ class OrderNoteTableViewCell: UITableViewCell {
         }
     }
 
-    public var statusText: String? {
-        get {
-            return statusLabel.text
-        }
-        set {
-            statusLabel.text = newValue
-        }
-    }
-
-    public var contents: String? {
+    /// Note's Payload
+    ///
+    var contents: String? {
         get {
             return noteLabel.text
         }
         set {
             noteLabel.text = newValue
         }
+    }
+
+    /// Indicates if the note is visible to the Customer (or it's set to private!)
+    ///
+    var isCustomerNote: Bool = false {
+        didSet {
+            privacyWasUpdated()
+        }
+    }
+}
+
+
+// MARK: - Private Methods
+//
+private extension OrderNoteTableViewCell {
+
+    /// Updates the Status Label + Icon's Color
+    ///
+    func privacyWasUpdated() {
+        if isCustomerNote {
+            iconButton.backgroundColor = StyleManager.statusPrimaryBoldColor
+            statusLabel.text = NSLocalizedString("Note to customer", comment: "Labels an order note to let user know it's visible to the customer")
+        } else {
+            iconButton.backgroundColor = StyleManager.wooGreyMid
+            statusLabel.text = NSLocalizedString("Private note", comment: "Labels an order note to let the user know it's private and not seen by the customer")
+        }
+    }
+
+    /// Setup: Labels
+    ///
+    func configureLabels() {
+        dateLabel.applyBodyStyle()
+        statusLabel.applyBodyStyle()
+        noteLabel.applyBodyStyle()
+    }
+
+    /// Setup: Icon Button
+    ///
+    func configureIconButton() {
+        let image = Gridicon.iconOfType(.aside)
+        iconButton.setImage(image, for: .normal)
+        iconButton.layer.cornerRadius = iconButton.frame.width / 2
+        iconButton.tintColor = .white
     }
 }

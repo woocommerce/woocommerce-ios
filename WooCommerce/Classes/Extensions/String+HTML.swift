0f198ca31d638de183d35feffde3599f998d1117
Jorge Leandro Perez <jorge@lantean.co>
2018-11-23 15:27:31 -0300

    New String+HTML Extension

---
+++ WooCommerce/Classes/Extensions/String+HTML.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+
+///// String: HTML Stripping
+///
+extension String {
+
+    /// Returns the HTML Stripped version of the receiver.
+    ///
+    var strippedHTML: String {
+        guard let data = data(using: .utf8) else {
+            return self
+        }
+
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ]
+
+        guard let attributedString = try? NSAttributedString(data: data, options: options, documentAttributes: nil) else {
+            return self
+        }
+
+        return attributedString.string
+    }
+}

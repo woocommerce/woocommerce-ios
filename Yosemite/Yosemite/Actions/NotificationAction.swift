e30e8af97c9d7fcb7afe802ee72a1325d29b6581
Jorge Leandro Perez <jorge@lantean.co>
2018-11-26 17:05:17 -0300

    NotificationAction: Documents Actions

--- Yosemite/Yosemite/Actions/NotificationAction.swift
+++ Yosemite/Yosemite/Actions/NotificationAction.swift
@@ -6,9 +6,23 @@ import Networking
 // MARK: - NotificationAction: Defines all of the Actions supported by the NotificationStore.
 //
 public enum NotificationAction: Action {
+    /// Synchronizes the full Notifications collection.
+    ///
     case synchronizeNotifications(onCompletion: (Error?) -> Void)
+
+    /// Synchronizes a specified Notification.
+    ///
     case synchronizeNotification(noteId: Int64, onCompletion: (Error?) -> Void)
+
+    /// Updates the WordPress.com Last Seen field.
+    ///
     case updateLastSeen(timestamp: String, onCompletion: (Error?) -> Void)
+
+    /// Updates a given Notification's read flag.
+    ///
     case updateReadStatus(noteId: Int64, read: Bool, onCompletion: (Error?) -> Void)
+
+    /// Updates, in batch, the Notification's read flag.
+    ///
     case updateMultipleReadStatus(noteIds: [Int64], read: Bool, onCompletion: (Error?) -> Void)
 }

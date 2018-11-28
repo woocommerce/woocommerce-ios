cd30651c201cedc205f46395c7493e4336b478a3
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-11-28 13:03:48 -0600

    FancyAlert style updates

--- WooCommerce/Classes/ViewRelated/Fancy Alerts/FancyAlertViewController+Upgrade.swift
+++ WooCommerce/Classes/ViewRelated/Fancy Alerts/FancyAlertViewController+Upgrade.swift
@@ -1,15 +1,9 @@
 import Foundation
 import WordPressUI
+import SafariServices
 
 
 public extension FancyAlertViewController {
-
-    private struct Strings {
-        static let titleText = NSLocalizedString("Update to WooCommerce 3.5 to keep using this app", comment: "Title of alert warning users to upgrade to WC 3.5.")
-        static let bodyText = NSLocalizedString("This app requires that you install WooCommerce 3.5 on your server, and won't work properly without it. Update as soon as possible to continue using this app.",
-                                                comment: "Body text of alert warning users to upgrade to WC 3.5.")
-        static let dismissButtonText = NSLocalizedString("Got it!", comment: "Dismiss button title shown in alert warning users to upgrade to WC 3.5.")
-    }
 
     /// Create the fancy alert controller for the WC 3.5 upgrade alert
     ///
@@ -22,16 +16,42 @@ public extension FancyAlertViewController {
             controller.dismiss(animated: true)
         }
 
+        let moreInfoButton = FancyAlertViewController.Config.ButtonConfig(Strings.moreInfoButtonText) { controller, _ in
+            guard let url = URL(string: Strings.moreInfoURLString) else {
+                return
+            }
+
+            let safariViewController = SFSafariViewController(url: url)
+            safariViewController.modalPresentationStyle = .pageSheet
+            controller.present(safariViewController, animated: true)
+        }
+
         let config = FancyAlertViewController.Config(titleText: Strings.titleText,
                                                      bodyText: Strings.bodyText,
                                                      headerImage: nil,
                                                      dividerPosition: .bottom,
                                                      defaultButton: dismissButton,
                                                      cancelButton:  nil,
-                                                     appearAction: nil,
-                                                     dismissAction: nil)
+                                                     moreInfoButton: moreInfoButton,
+                                                     dismissAction: {})
 
         let controller = FancyAlertViewController.controllerWithConfiguration(configuration: config)
         return controller
     }
 }
+
+
+// MARK: - Constants
+//
+private extension FancyAlertViewController {
+
+    struct Strings {
+        static let titleText          = NSLocalizedString("Update to WooCommerce 3.5 to keep using this app", comment: "Title of alert warning users to upgrade to WC 3.5.")
+        static let bodyText           = NSLocalizedString("This app requires that you install WooCommerce 3.5 on your server, and won't work properly without it. Update as soon as possible to continue using this app.",
+                                                         comment: "Body text of alert warning users to upgrade to WC 3.5.")
+        static let moreInfoButtonText = NSLocalizedString("Update Instructions", comment: "Update instructions button title shown in alert warning users to upgrade to WC 3.5.")
+        static let moreInfoURLString  = "https://docs.woocommerce.com/document/how-to-update-woocommerce/"
+        static let dismissButtonText  = NSLocalizedString("Dismiss", comment: "Dismiss button title shown in alert warning users to upgrade to WC 3.5.")
+    }
+}
+

a28ac8c771594fbb4dccd2f3ff2d61b456301cdd
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-11-28 16:50:55 -0600

    Added a nukeme comment

--- WooCommerce/Classes/AppDelegate.swift
+++ WooCommerce/Classes/AppDelegate.swift
@@ -114,6 +114,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 
+        // TODO: Nuke this 👇👇 after the WCiOS v0.12 release!!
         displayWooUpgradeAlertIfNeeded()
     }
 

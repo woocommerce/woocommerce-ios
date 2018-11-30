b89d5761940a7e4b5831aa016b6c5da7c7d43aac
Thuy.Copeland <thuycopeland@gmail.com>
2018-11-30 13:11:43 -0600

    Remove the `isJetpackInstalled` attribute from the site entity

--- Storage/Storage/Model/Site+CoreDataProperties.swift
+++ Storage/Storage/Model/Site+CoreDataProperties.swift
@@ -12,7 +12,6 @@ extension Site {
     @NSManaged public var tagline: String?
     @NSManaged public var url: String?
     @NSManaged public var plan: String?
-    @NSManaged public var isJetpackInstalled: NSNumber?
     @NSManaged public var isWooCommerceActive: NSNumber?
     @NSManaged public var isWordPressStore: NSNumber?
 }

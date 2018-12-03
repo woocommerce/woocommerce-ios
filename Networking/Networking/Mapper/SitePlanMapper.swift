83f681d8c291650c6536d9d2fdbfadaeeb58e9e7
Thuy.Copeland <thuycopeland@gmail.com>
2018-12-03 10:21:35 -0600

    The `sites/$site` endpoint doesn't use a `data` field to wrap the response.

--- Networking/Networking/Mapper/SitePlanMapper.swift
+++ Networking/Networking/Mapper/SitePlanMapper.swift
@@ -5,24 +5,10 @@ import Foundation
 ///
 class SitePlanMapper: Mapper {
 
-    /// (Attempts) to convert a dictionary into a [SitePlan].
+    /// (Attempts) to convert a dictionary into a site plan attribute for the site entity.
     ///
-    func map(response: Data) throws -> [SitePlan] {
+    func map(response: Data) throws -> SitePlan {
         let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .formatted(DateFormatter.Defaults.dateTimeFormatter)
-
-        return try decoder.decode(SitePlanEnvelope.self, from: response).plan
-    }
-}
-
-/// SitePlanEnvelope Disposable Entity:
-/// The sites/site endpoint returns the plan response within a `data` key.
-/// This entity allows us to parse all the things with JSONDecoder.
-///
-private struct SitePlanEnvelope: Decodable {
-    let plan: [SitePlan]
-
-    private enum CodingKeys: String, CodingKey {
-        case plan = "data"
+        return try decoder.decode(SitePlan.self, from: response)
     }
 }

50cd2031b4bc14475964476029cd7f8a526fedaa
Jorge Leandro Perez <jorge@lantean.co>
2018-12-03 19:06:54 -0300

    Implements MockupStoresManager

---
+++ WooCommerce/WooCommerceTests/Mockups/MockupStoresManager.swift
@@ -0,0 +1,38 @@
+import Foundation
+import Yosemite
+@testable import WooCommerce
+
+
+/// MockupStoresManager: MockupStoresManager Mockup!
+///
+class MockupStoresManager: MockupStoresManager {
+
+    /// Contains all of the dispatched Actions
+    ///
+    private(set) var receivedActions = [Action]()
+
+    /// Indicates if the Actions should be dispatched for real (or do nothing!)
+    ///
+    var shouldDispatchActionsForReal = false
+
+
+    // MARK: - Overridden Methods
+
+    override func dispatch(_ action: Action) {
+        receivedActions.append(action)
+
+        guard shouldDispatchActionsForReal else {
+            return
+        }
+
+        super.dispatch(action)
+    }
+
+    // MARK: - Public Methods
+
+    /// Restores the initial state
+    ///
+    func reset() {
+        receivedActions = []
+    }
+}

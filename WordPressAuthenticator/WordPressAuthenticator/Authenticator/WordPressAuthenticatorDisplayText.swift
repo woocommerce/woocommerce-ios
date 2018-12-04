6a295553d5d3984fbdb0fb85c54d00ab5309e869
Thuy.Copeland <thuycopeland@gmail.com>
2018-12-04 11:18:26 -0600

    Initial display text file

---
+++ WordPressAuthenticator/WordPressAuthenticator/Authenticator/WordPressAuthenticatorDisplayText.swift
@@ -0,0 +1,9 @@
+//
+//  WordPressAuthenticatorDisplayText.swift
+//  WordPressAuthenticator
+//
+//  Created by Thuy Copeland on 12/4/18.
+//  Copyright © 2018 Automattic. All rights reserved.
+//
+
+import Foundation

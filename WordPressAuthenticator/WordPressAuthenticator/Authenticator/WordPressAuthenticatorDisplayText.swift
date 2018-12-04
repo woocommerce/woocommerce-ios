110bdec812895d6becc94b54f7394cfe7411067b
Thuy.Copeland <thuycopeland@gmail.com>
2018-12-04 14:14:41 -0600

    Add another text override option in the email login instructions

--- WordPressAuthenticator/WordPressAuthenticator/Authenticator/WordPressAuthenticatorDisplayText.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Authenticator/WordPressAuthenticatorDisplayText.swift
@@ -8,20 +8,23 @@ public struct WordPressAuthenticatorDisplayText {
     ///
     public let emailLoginInstructions: String
 
-    public let siteLoginInstructions: String
+    public let jetpackLoginInstructions: String
 
+    public let siteLoginInstructions: String
 
     /// Designated initializer.
     ///
-    public init(emailLoginInstructions: String, siteLoginInstructions: String) {
+    public init(emailLoginInstructions: String, jetpackLoginInstructions: String, siteLoginInstructions: String) {
         self.emailLoginInstructions = emailLoginInstructions
+        self.jetpackLoginInstructions = jetpackLoginInstructions
         self.siteLoginInstructions = siteLoginInstructions
     }
 }
 
 public extension WordPressAuthenticatorDisplayText {
     public static var defaultText: WordPressAuthenticatorDisplayText {
-        return WordPressAuthenticatorDisplayText(emailLoginInstructions: NSLocalizedString("Log in to the WordPress.com account you used to connect Jetpack.", comment: "Instruction text on the login's email address screen."),
+        return WordPressAuthenticatorDisplayText(emailLoginInstructions: NSLocalizedString("Log in to WordPress.com using an email address to manage all your WordPress sites.", comment: "Instruction text on the login's email address screen."),
+                                                 jetpackLoginInstructions: NSLocalizedString("Log in to the WordPress.com account you used to connect Jetpack.", comment: "Instruction text on the login's email address screen."),
                                                  siteLoginInstructions: NSLocalizedString("Enter the address of your WordPress site you'd like to connect.", comment: "Instruction text on the login's site addresss screen."))
     }
 }

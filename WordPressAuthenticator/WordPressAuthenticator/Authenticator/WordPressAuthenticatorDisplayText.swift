a7088cd5d2e2fd0bf77d910bcc2063901fa7b34e
Thuy.Copeland <thuycopeland@gmail.com>
2018-12-04 11:19:21 -0600

    Add text overrides to the display text struct

--- WordPressAuthenticator/WordPressAuthenticator/Authenticator/WordPressAuthenticatorDisplayText.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Authenticator/WordPressAuthenticatorDisplayText.swift
@@ -1,9 +1,27 @@
-//
-//  WordPressAuthenticatorDisplayText.swift
-//  WordPressAuthenticator
-//
-//  Created by Thuy Copeland on 12/4/18.
-//  Copyright © 2018 Automattic. All rights reserved.
-//
-
 import Foundation
+
+
+// MARK: - WordPress Authenticator Display Text
+//
+public struct WordPressAuthenticatorDisplayText {
+    /// Text: Login instructions.
+    ///
+    public let emailLoginInstructions: String
+
+    public let siteLoginInstructions: String
+
+
+    /// Designated initializer.
+    ///
+    public init(emailLoginInstructions: String, siteLoginInstructions: String) {
+        self.emailLoginInstructions = emailLoginInstructions
+        self.siteLoginInstructions = siteLoginInstructions
+    }
+}
+
+public extension WordPressAuthenticatorDisplayText {
+    public static var defaultText: WordPressAuthenticatorDisplayText {
+        return WordPressAuthenticatorDisplayText(emailLoginInstructions: NSLocalizedString("Log in to the WordPress.com account you used to connect Jetpack.", comment: "Instruction text on the login's email address screen."),
+                                                 siteLoginInstructions: NSLocalizedString("Enter the address of your WordPress site you'd like to connect.", comment: "Instruction text on the login's site addresss screen."))
+    }
+}

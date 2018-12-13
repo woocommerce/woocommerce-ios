6dda32190d6568907f9ddc30505edcfead8ea5fd
Thuy.Copeland <thuycopeland@gmail.com>
2018-12-13 11:47:16 -0600

    Handle other incoming remote push notification taps

--- WooCommerce/Classes/Notifications/InteractiveNotificationsManager.swift
+++ WooCommerce/Classes/Notifications/InteractiveNotificationsManager.swift
@@ -32,17 +32,21 @@ extension InteractiveNotificationsManager: UNUserNotificationCenterDelegate {
         let userInfo = notification.request.content.userInfo as NSDictionary
 
         // If the app is open, and a Zendesk view is being shown, Zendesk will display an alert allowing the user to view the updated ticket.
-        handleZendeskNotification(userInfo: userInfo)
+        // -TODO: check to see if alert display is still true, since we have the latest ZD SDK.
+        let _ = handleZendeskNotification(userInfo: userInfo)
 
         completionHandler([])
     }
 
-    private func handleZendeskNotification(userInfo: NSDictionary) {
+    private func handleZendeskNotification(userInfo: NSDictionary) -> Bool {
         if let type = userInfo.string(forKey: ZendeskManager.PushNotificationIdentifiers.key),
             type == ZendeskManager.PushNotificationIdentifiers.type,
         let payload = userInfo as? [AnyHashable : Any] {
             ZendeskManager.shared.handlePushNotification(payload)
+            return true
         }
+
+        return false
     }
 
     /// This method is only called when the app is in the background
@@ -51,8 +55,30 @@ extension InteractiveNotificationsManager: UNUserNotificationCenterDelegate {
                                 didReceive response: UNNotificationResponse,
                                 withCompletionHandler completionHandler: @escaping () -> Void) {
         let userInfo = response.notification.request.content.userInfo as NSDictionary
-        // other notifications
-        handleZendeskNotification(userInfo: userInfo)
+        let isHandled = handleZendeskNotification(userInfo: userInfo)
+
+        if isHandled == false {
+            // TODO:
+            // =====
+            // Refactor both PushNotificationsManager + InteractiveNotificationsManager:
+            //
+            //  -   InteractiveNotificationsManager should no longer be a singleton. Perhaps we could convert it into a struct.
+            //      Plus int should probably be renamed into something more meaningful (and match the new framework's naming)
+            //  -   New `NotificationsManager` class:
+            //      -   Would inherit `PushNotificationsManager.handleNotification`
+            //      -   Would deal with UserNotifications.framework
+            //      -   Would use InteractiveNotificationsManager!
+            //  -   Nuke `PushNotificationsManager`
+            //
+            //
+            guard let payload = userInfo as? [AnyHashable : Any] else {
+                return
+            }
+
+            AppDelegate.shared.pushNotesManager.handleNotification(payload) { _ in
+                completionHandler()
+            }
+        }
 
         completionHandler()
     }

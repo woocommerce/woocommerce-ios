06eefbef5315371ad6eb402d2150fea90e83d686
Thuy.Copeland <thuycopeland@gmail.com>
2018-12-13 17:55:58 -0600

    Delete the `InteractiveNotificationsManager`, it's not needed for the scope of the ZD task.

--- WooCommerce/Classes/AppDelegate.swift
+++ WooCommerce/Classes/AppDelegate.swift
@@ -89,7 +89,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        InteractiveNotificationsManager.shared.registerForUserNotifications()
         return true
     }
 

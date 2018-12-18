8b0f18ce538a1773e01c4169903ab2197943119e
Jorge Leandro Perez <jorge@lantean.co>
2018-12-18 09:58:40 -0300

    DotcomError: New Initializer

--- Networking/Networking/Validators/DotcomValidator.swift
+++ Networking/Networking/Validators/DotcomValidator.swift
@@ -10,4 +10,14 @@ struct DotcomValidator {
     static func error(from response: Data) -> Error? {
         return try? JSONDecoder().decode(DotcomError.self, from: response)
     }
+
+    /// Returns the DotcomError contained in a given dictionary. If any!
+    ///
+    static func error(from json: Any) -> Error? {
+        guard let dictionary = json as? [AnyHashable: Any] else {
+            return nil
+        }
+
+        return DotcomError(dictionary: dictionary)
+    }
 }

4b7594b2e6f243a0067d18975c81186580a00483
Jorge Leandro Perez <jorge@lantean.co>
2018-12-18 11:30:38 -0300

    DotcomValidator: Updates comments

--- Networking/Networking/Validators/DotcomValidator.swift
+++ Networking/Networking/Validators/DotcomValidator.swift
@@ -5,16 +5,16 @@ import Foundation
 ///
 struct DotcomValidator {
 
-    /// Returns the DotcomError contained in a given Response, if any.
+    /// Returns the DotcomError contained in a given Data Instance (if any).
     ///
     static func error(from response: Data) -> Error? {
         return try? JSONDecoder().decode(DotcomError.self, from: response)
     }
 
-    /// Returns the DotcomError contained in a given dictionary. If any!
+    /// Returns the DotcomError contained in a given JSON Object (if any).
     ///
-    static func error(from json: Any) -> Error? {
-        guard let dictionary = json as? [AnyHashable: Any] else {
+    static func error(from jsonObject: Any) -> Error? {
+        guard let dictionary = jsonObject as? [AnyHashable: Any] else {
             return nil
         }
 

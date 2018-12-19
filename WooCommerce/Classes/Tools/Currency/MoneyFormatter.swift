c490059ff77b99178102a7951e34e7e33427d6ce
Thuy.Copeland <thuycopeland@gmail.com>
2018-12-19 08:50:31 -0600

    Remove extra functions from overloaded method. Assume API input for string values are clean.

--- WooCommerce/Classes/Tools/Currency/MoneyFormatter.swift
+++ WooCommerce/Classes/Tools/Currency/MoneyFormatter.swift
@@ -14,26 +14,13 @@ public class MoneyFormatter {
     /// Returns a formatted decimal value from a given string.
     /// - Parameters:
     ///   - stringValue: the string received from the API
-    ///   - isRounded: rounds a decimal value
-    ///   - scale: the number of decimal points to display
-    func formatDecimal(from stringValue: String, isRounded: Bool = false, with scale: Int16 = 2) -> NSDecimalNumber? {
+    ///
+    func formatDecimal(from stringValue: String) -> NSDecimalNumber? {
         let decimalValue = NSDecimalNumber(string: stringValue)
 
         guard decimalValue != NSDecimalNumber.notANumber else {
-            DDLogError("Error: string value could not be converted to decimal: \(stringValue)")
+            DDLogError("Error: string input is not a number: \(stringValue)")
             return nil
-        }
-
-        if isRounded == true {
-            let roundingBehavior = NSDecimalNumberHandler(roundingMode: .plain,
-                                                          scale: scale,
-                                                          raiseOnExactness: false,
-                                                          raiseOnOverflow: false,
-                                                          raiseOnUnderflow: false,
-                                                          raiseOnDivideByZero: false)
-            let roundedDecimal = decimalValue.rounding(accordingToBehavior: roundingBehavior)
-
-            return roundedDecimal
         }
 
         return decimalValue

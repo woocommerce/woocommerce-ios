8c286bfe793bff61dd1762a485d3e5a670af77e0
Aaron Douglas <astralbodies@gmail.com>
2018-12-21 08:26:59 -0600

    Make SwiftLint happy without trailing breaks

---
+++ WooCommerce/WooCommerceTests/Tools/MoneyFormatSettingsTests.swift
@@ -0,0 +1,9 @@
+//
+//  MoneyFormatSettingsTests.swift
+//  WooCommerceTests
+//
+//  Created by Aaron Douglas on 12/21/18.
+//  Copyright © 2018 Automattic. All rights reserved.
+//
+
+import Foundation

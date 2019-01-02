c79b3430c80e764efdf4616b2c762b11a24fcf23
Thuy.Copeland <thuycopeland@gmail.com>
2019-01-02 13:59:54 -0600

    Rename MoneyFormatterSettings to CurrencyOptions

--- WooCommerce/Classes/Yosemite/StoresManager.swift
+++ WooCommerce/Classes/Yosemite/StoresManager.swift
@@ -197,7 +197,7 @@ private extension StoresManager {
             return
         }
 
-        MoneyFormatSettings.shared.beginListeningToSiteSettingsUpdates()
+        CurrencyOptions.shared.beginListeningToSiteSettingsUpdates()
 
         let action = SettingAction.retrieveSiteSettings(siteID: siteID) { error in
             if let error = error {

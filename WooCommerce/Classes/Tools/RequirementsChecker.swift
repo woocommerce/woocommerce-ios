4038724cf6e156751b1f9566ccf9576f54fda0b0
Matt Bumgardner <matt.bumgardner@gmail.com>
2019-01-18 10:33:07 -0600

    Fixed linter issue

--- WooCommerce/Classes/Tools/RequirementsChecker.swift
+++ WooCommerce/Classes/Tools/RequirementsChecker.swift
@@ -57,7 +57,7 @@ class RequirementsChecker {
             case .invalidWCVersion:
                 displayWCVersionAlert()
             default:
-                break            
+                break
             }
         }
     }

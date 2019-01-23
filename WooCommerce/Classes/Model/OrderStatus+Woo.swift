b9017cb1884654fd01287ce8cbc7a8c23df4c891
Thuy.Copeland <thuycopeland@gmail.com>
2019-01-23 09:16:05 -0600

    Restore `On Hold` in the filter action menu

--- WooCommerce/Classes/Model/OrderStatus+Woo.swift
+++ WooCommerce/Classes/Model/OrderStatus+Woo.swift
@@ -9,7 +9,6 @@ extension OrderStatus {
     /// Returns a collection of all of the known Order Status
     ///
     static var knownStatus: [OrderStatus] {
-        // .onHold is an existing status but we don't want it to display.
-        return [.pending, .processing, .failed, .cancelled, .completed, .refunded]
+        return [.pending, .processing, .onHold, .failed, .cancelled, .completed, .refunded]
     }
 }

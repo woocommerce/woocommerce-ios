aace6d92cc45c56311fed9af39258c531d3d1926
Thuy.Copeland <thuycopeland@gmail.com>
2019-01-28 15:15:20 -0600

    Add convenience method to convert HTML to NSMutableAttributedString

--- WooCommerce/Classes/Extensions/String+HTML.swift
+++ WooCommerce/Classes/Extensions/String+HTML.swift
@@ -25,4 +25,15 @@ extension String {
 
         return attributedString.string
     }
+
+    /// Convert HTML to an attributed string
+    ///
+    var htmlToAttributedString: NSMutableAttributedString? {
+        guard let data = data(using: .utf8) else { return NSMutableAttributedString() }
+        do {
+            return try NSMutableAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            return NSMutableAttributedString()
+        }
+    }
 }

41752af78ffc2b3625dea55fdd9d32c19eee03c2
Thuy.Copeland <thuycopeland@gmail.com>
2019-01-29 15:09:57 -0600

    Add the "Share menu" button and action
    - UIActivityViewController as popover, excluding `copyToPasteboard` and `mail` activities
    - add all activities to ShareHelper class
    - add share icon to nav bar

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Help/ApplicationLogDetailViewController.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Help/ApplicationLogDetailViewController.swift
@@ -1,30 +1,82 @@
-//
-//  ApplicationLogDetailViewController.swift
-//  WooCommerce
-//
-//  Created by Thuy Copeland on 1/29/19.
-//  Copyright © 2019 Automattic. All rights reserved.
-//
-
 import UIKit
 
 class ApplicationLogDetailViewController: UIViewController {
 
+    /// The contents of the log file.
+    ///
+    let logText: String
+
+    /// The log file's creation date as a formatted string.
+    ///
+    let logDate: String
+
+
+    // MARK: - Overridden Methods
+    //
+    
+    /// Designated initializer
+    ///
+    init(with contents: String, for date: String) {
+        self.logText = contents
+        self.logDate = date
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    /// Unsupported: NSCoder
+    ///
+    required init?(coder aDecoder: NSCoder) {
+        assertionFailure()
+        return nil
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureNavigation()
+        configureMainView()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    /// Add styles and buttons to nav bar
+    ///
+    func configureNavigation() {
+        title = NSLocalizedString("Application Log", comment: "Application Log navigation bar title - this screen is where users view a single log file.")
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // Don't show the Application Log title in the next-view's back button
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: String(), style: .plain, target: nil, action: nil)
+
+        let shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(showShareActivity))
+        navigationItem.rightBarButtonItem = shareButton
     }
-    */
 
+    /// Apply Woo styles.
+    ///
+    func configureMainView() {
+        view.backgroundColor = StyleManager.tableViewBackgroundColor
+    }
+
+    /// Display the share activity
+    /// Uses @objc attribute because this function is called from a #selector
+    ///
+    @objc
+    func showShareActivity(_ sender: UIBarButtonItem) {
+        let activityVC = UIActivityViewController(activityItems: [logText], applicationActivities: nil)
+        activityVC.modalPresentationStyle = .popover
+        activityVC.popoverPresentationController?.barButtonItem = sender
+        activityVC.excludedActivityTypes = assembleExcludedSupportTypes()
+        present(activityVC, animated: true, completion: nil)
+    }
+
+    /// Specifies the activity types that should be excluded.
+    /// - returns: all supported types except `.copyToPasteboard` and `.mail`.
+    ///
+    func assembleExcludedSupportTypes() -> [UIActivity.ActivityType] {
+        let activityTypes = NSMutableSet(array: SharingHelper.allActivityTypes())
+        let unsupportedTypes = Set(arrayLiteral: [UIActivity.ActivityType.copyToPasteboard,
+                                                  UIActivity.ActivityType.mail])
+
+        activityTypes.minus(unsupportedTypes)
+
+        return activityTypes.allObjects as! [UIActivity.ActivityType]
+    }
 }

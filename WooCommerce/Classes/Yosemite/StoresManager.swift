f07fcc4eaf6abbb2b8ea1d8252c14e15150e70ed
Thuy.Copeland <thuycopeland@gmail.com>
2019-02-05 13:15:38 -0600

    Clean up resetAuthentication method

--- WooCommerce/Classes/Yosemite/StoresManager.swift
+++ WooCommerce/Classes/Yosemite/StoresManager.swift
@@ -106,10 +106,8 @@ class StoresManager {
     /// Switches the primary store and remains Authenticated.
     ///
     func resetAuthentication() {
-//        sessionManager.reset()
         WooAnalytics.shared.refreshUserData()
         ZendeskManager.shared.reset()
-//        AppDelegate.shared.storageManager.reset()
     }
 
     /// Switches the state to a Deauthenticated one.

1632c74a8443dc7b4baeece8869b266cbefda85d
Matt Bumgardner <matt.bumgardner@gmail.com>
2019-02-22 12:39:48 -0600

    Filled out the ShipmentStore unit tests a little more

--- Yosemite/YosemiteTests/Stores/ShipmentStoreTests.swift
+++ Yosemite/YosemiteTests/Stores/ShipmentStoreTests.swift
@@ -46,9 +46,9 @@ class ShipmentStoreTests: XCTestCase {
 
     // MARK: - ShipmentAction.synchronizeShipmentTrackingData
 
-    /// Verifies that ShipmentAction.synchronizeShipmentTrackingData returns the expected shipment tracking data.
+    /// Verifies that `ShipmentAction.synchronizeShipmentTrackingData` effectively persists any retrieved tracking data.
     ///
-    func testRetrieveShipmentTrackingListReturnsExpectedFields() {
+    func testRetrieveShipmentTrackingListEffectivelyPersistsRetrievedShipmentTrackingData() {
         let expectation = self.expectation(description: "Retrieve shipment tracking list")
         let shipmentStore = ShipmentStore(dispatcher: dispatcher, storageManager: storageManager, network: network)
 
@@ -56,10 +56,115 @@ class ShipmentStoreTests: XCTestCase {
         let action = ShipmentAction.synchronizeShipmentTrackingData(siteID: sampleSiteID, orderID: sampleOrderID) { error in
             XCTAssertNil(error)
             XCTAssertEqual(self.viewStorage.countObjects(ofType: Storage.ShipmentTracking.self), 4)
+
+            let storageTracking1 = self.viewStorage.loadShipmentTracking(siteID: self.sampleSiteID, orderID: self.sampleOrderID, trackingID: self.sampleShipmentTracking1().trackingID)
+            XCTAssertNotNil(storageTracking1)
+            XCTAssertEqual(storageTracking1?.toReadOnly(), self.sampleShipmentTracking1())
+
+            let storageTracking2 = self.viewStorage.loadShipmentTracking(siteID: self.sampleSiteID, orderID: self.sampleOrderID, trackingID: self.sampleShipmentTracking2().trackingID)
+            XCTAssertNotNil(storageTracking2)
+            XCTAssertEqual(storageTracking2?.toReadOnly(), self.sampleShipmentTracking2())
+
+            let storageTracking3 = self.viewStorage.loadShipmentTracking(siteID: self.sampleSiteID, orderID: self.sampleOrderID, trackingID: self.sampleShipmentTracking3().trackingID)
+            XCTAssertNotNil(storageTracking3)
+            XCTAssertEqual(storageTracking3?.toReadOnly(), self.sampleShipmentTracking3())
+
+            let storageTracking4 = self.viewStorage.loadShipmentTracking(siteID: self.sampleSiteID, orderID: self.sampleOrderID, trackingID: self.sampleShipmentTracking4().trackingID)
+            XCTAssertNotNil(storageTracking4)
+            XCTAssertEqual(storageTracking4?.toReadOnly(), self.sampleShipmentTracking4())
+
+            // For grins, lets all check that viewStorage.loadShipmentTrackingList returns the same results 😇
+            let storageTrackingList = self.viewStorage.loadShipmentTrackingList(siteID: self.sampleSiteID, orderID: self.sampleOrderID)
+            XCTAssertNotNil(storageTrackingList)
+            let readOnlyList = storageTrackingList?.map({ $0.toReadOnly() })
+            XCTAssertEqual(readOnlyList?.sorted(), self.sampleShipmentTrackingList().sorted())
+
             expectation.fulfill()
         }
 
         shipmentStore.onAction(action)
         wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+
+    /// Verifies that `ShipmentAction.synchronizeShipmentTrackingData` returns an error whenever there is an error response from the backend.
+    ///
+    func testRetrieveShipmentTrackingListReturnsErrorUponReponseError() {
+        let expectation = self.expectation(description: "Retrieve shipment tracking list error response")
+        let shipmentStore = ShipmentStore(dispatcher: dispatcher, storageManager: storageManager, network: network)
+
+        network.simulateResponse(requestUrlSuffix: "orders/\(sampleOrderID)/shipment-trackings/", filename: "shipment_tracking_plugin_not_active")
+        let action = ShipmentAction.synchronizeShipmentTrackingData(siteID: sampleSiteID, orderID: sampleOrderID) { error in
+            XCTAssertNotNil(error)
+            expectation.fulfill()
+        }
+
+        shipmentStore.onAction(action)
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+
+    /// Verifies that `ShipmentAction.synchronizeShipmentTrackingData` returns an error whenever there is no backend response.
+    ///
+    func testRetrieveShipmentTrackingListReturnsErrorUponEmptyResponse() {
+        let expectation = self.expectation(description: "Retrieve shipment tracking list empty response")
+        let shipmentStore = ShipmentStore(dispatcher: dispatcher, storageManager: storageManager, network: network)
+
+        let action = ShipmentAction.synchronizeShipmentTrackingData(siteID: sampleSiteID, orderID: sampleOrderID) { error in
+            XCTAssertNotNil(error)
+            expectation.fulfill()
+        }
+
+        shipmentStore.onAction(action)
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+}
+
+// MARK: - Private Methods
+//
+private extension ShipmentStoreTests {
+
+    //  MARK: - ShipmentTracking Samples
+
+    func sampleShipmentTracking1() -> Networking.ShipmentTracking {
+        return ShipmentTracking(siteID: sampleSiteID,
+                                orderID: sampleOrderID,
+                                trackingID: "b1b94eecb1eb1c1edf3fa041efffd015",
+                                trackingNumber: "345645674567",
+                                trackingProvider: "USPS",
+                                trackingURL: "https://tools.usps.com/go/TrackConfirmAction_input?qtc_tLabels1=345645674567",
+                                dateShipped: DateFormatter.Defaults.yearMonthDayDateFormatter.date(from: "2019-02-15"))
+    }
+
+    func sampleShipmentTracking2() -> Networking.ShipmentTracking {
+        return ShipmentTracking(siteID: sampleSiteID,
+                                orderID: sampleOrderID,
+                                trackingID: "6f2c2fb15474c97eae8ed6287f7f33f9",
+                                trackingNumber: "56734563456",
+                                trackingProvider: "UPS",
+                                trackingURL: "http://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=56734563456",
+                                dateShipped: DateFormatter.Defaults.yearMonthDayDateFormatter.date(from: "2019-02-14"))
+    }
+
+    func sampleShipmentTracking3() -> Networking.ShipmentTracking {
+        return ShipmentTracking(siteID: sampleSiteID,
+                                orderID: sampleOrderID,
+                                trackingID: "6fc204bea3bf7f5565985e1b127a5d8d",
+                                trackingNumber: "456345623453454352323452345235",
+                                trackingProvider: "Flybynight Courriers",
+                                trackingURL: "",
+                                dateShipped: DateFormatter.Defaults.yearMonthDayDateFormatter.date(from: "2019-02-11"))
+    }
+
+    func sampleShipmentTracking4() -> Networking.ShipmentTracking {
+        return ShipmentTracking(siteID: sampleSiteID,
+                                orderID: sampleOrderID,
+                                trackingID: "2222",
+                                trackingNumber: "asdfasdf7787775756786789",
+                                trackingProvider: nil,
+                                trackingURL: nil,
+                                dateShipped: nil)
+    }
+
+    func sampleShipmentTrackingList() -> [Networking.ShipmentTracking] {
+        return [sampleShipmentTracking1(), sampleShipmentTracking2(), sampleShipmentTracking3(), sampleShipmentTracking4()]
     }
 }

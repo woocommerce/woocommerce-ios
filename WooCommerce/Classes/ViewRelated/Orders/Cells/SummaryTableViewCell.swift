7d98f6ba106fdfb1c2948215429e1d39581205b6
Thuy.Copeland <thuycopeland@gmail.com>
2019-03-04 09:45:45 -0600

    Fix payment label bug

--- WooCommerce/Classes/ViewRelated/Orders/Cells/SummaryTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Cells/SummaryTableViewCell.swift
@@ -66,6 +66,11 @@ class SummaryTableViewCell: UITableViewCell {
             super.setHighlighted(highlighted, animated: animated)
         }
     }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        paymentStatusLabel.layer.borderColor = UIColor.clear.cgColor
+    }
 }
 
 
@@ -77,10 +82,12 @@ private extension SummaryTableViewCell {
     ///
     func preserveLabelColors(action: () -> Void) {
         let paymentColor = paymentStatusLabel.backgroundColor
+        let borderColor = paymentStatusLabel.layer.borderColor
 
         action()
 
         paymentStatusLabel.backgroundColor = paymentColor
+        paymentStatusLabel.layer.borderColor = borderColor
     }
 
     /// Setup: Labels

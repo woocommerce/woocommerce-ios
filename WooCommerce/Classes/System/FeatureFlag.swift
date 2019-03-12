8e39d9d57a1745e0916e0ffaee2aba1a97be0e4f
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-03-12 08:43:01 +0800

    Add a feature flag to hide the UI elements

--- WooCommerce/Classes/System/FeatureFlag.swift
+++ WooCommerce/Classes/System/FeatureFlag.swift
@@ -4,10 +4,14 @@ enum FeatureFlag: Int {
     /// Throwaway case, to prevent a compiler error:
     /// `An enum with no cases cannot declare a raw type`
     case null
+    case manualTracking
 
     /// Returns a boolean indicating if the feature is enabled
     var enabled: Bool {
         switch self {
+        case .manualTracking:
+            return BuildConfiguration.current == .localDeveloper
+
         default:
             return true
         }

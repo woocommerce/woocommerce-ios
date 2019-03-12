b774ddacbb253fe7bbe433b8d4ab58f97ae91576
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-03-12 13:14:52 +0800

    Add some header docs

--- Networking/Networking/Remote/ShipmentsRemote.swift
+++ Networking/Networking/Remote/ShipmentsRemote.swift
@@ -23,6 +23,15 @@ public final class ShipmentsRemote: Remote {
         enqueue(request, mapper: mapper, completion: completion)
     }
 
+    /// Creates a new shipment tracking for a given order.
+    ///
+    /// - Parameters:
+    ///   - siteID: Site which hosts the Order
+    ///   - orderID: Identifier of the Order
+    ///   - trackingProvider: The name of the tracking provider
+    ///   - trackingNumber: The tracking number
+    ///   - completion: Closure to be executed upon completion
+    ///
     public func createShipmentTracking(for siteID: Int, orderID: Int, trackingProvider: String, trackingNumber: String, completion: @escaping (ShipmentTracking?, Error?) -> Void) {
         let path = "\(Constants.ordersPath)/" + String(orderID) + "/" + "\(Constants.shipmentPath)/"
 
@@ -35,6 +44,16 @@ public final class ShipmentsRemote: Remote {
         enqueue(request, mapper: mapper, completion: completion)
     }
 
+    /// Creates a new shipment tracking with a custom provider for a given order.
+    ///
+    /// - Parameters:
+    ///   - siteID: Site which hosts the Order
+    ///   - orderID: Identifier of the Order
+    ///   - trackingProvider: The name of the tracking provider
+    ///   - trackingNumber: The tracking number
+    ///   - trackingLink: The custom url offered by this provider to track shipments
+    ///   - completion: Closure to be executed upon completion
+    ///
     public func createShipmentTrackingWithCustomProvider(for siteID: Int, orderID: Int, trackingProvider: String, trackingNumber: String, trackingLink: String, completion: @escaping (ShipmentTracking?, Error?) -> Void) {
         let path = "\(Constants.ordersPath)/" + String(orderID) + "/" + "\(Constants.shipmentPath)/"
 
@@ -48,6 +67,14 @@ public final class ShipmentsRemote: Remote {
         enqueue(request, mapper: mapper, completion: completion)
     }
 
+    /// Deletes a shipment tracking.
+    ///
+    /// - Parameters:
+    ///   - siteID: Site which hosts the Order
+    ///   - orderID: Identifier of the Order
+    ///   - trackingID: The tracking identifier
+    ///   - completion: Closure to be executed upon completion
+    ///
     public func deleteShipmentTracking(for siteID: Int, orderID: Int, trackingID: String, completion: @escaping (ShipmentTracking?, Error?) -> Void) {
         let path = "\(Constants.ordersPath)/" + String(orderID) + "/" + "\(Constants.shipmentPath)/" + trackingID
 

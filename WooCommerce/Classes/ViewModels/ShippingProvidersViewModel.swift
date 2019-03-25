74dd600988c13583aaae8096a6b0c94eb64bd3d2
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-03-25 07:09:45 -0700

    Attempt to create a resultscontroller to fetch shipmenttracking provider groups
    Fix build error

--- WooCommerce/Classes/ViewModels/ShippingProvidersViewModel.swift
+++ WooCommerce/Classes/ViewModels/ShippingProvidersViewModel.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Yosemite
 
 final class ShippingProvidersViewModel {
     let title = NSLocalizedString("Shipping Providers",
@@ -6,10 +7,10 @@ final class ShippingProvidersViewModel {
 
     /// ResultsController: Surrounds us. Binds the galaxy together. And also, keeps the UITableView <> (Stored) Orders in sync.
     ///
-//    private(set) lazy var resultsController: ResultsController<ShipmentTrackingProviderGroup> = {
-//        let storageManager = AppDelegate.shared.storageManager
-//        let descriptor = NSSortDescriptor(keyPath: \StorageOrder.dateCreated, ascending: false)
-//
-//        return ResultsController<ShipmentTrackingProviderGroup>(storageManager: storageManager, sectionNameKeyPath: "normalizedAgeAsString", sortedBy: [descriptor])
-//    }()
+    private(set) lazy var resultsController: ResultsController<StorageShipmentTrackingProviderGroup> = {
+        let storageManager = AppDelegate.shared.storageManager
+        let descriptor = NSSortDescriptor(keyPath: \ShipmentTrackingProviderGroup.name, ascending: false)
+
+        return ResultsController<StorageShipmentTrackingProviderGroup>(storageManager: storageManager, sectionNameKeyPath: "name", sortedBy: [descriptor])
+    }()
 }

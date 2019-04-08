5f260361213eeea1ad354ff6bda7f10e0bb3d92b
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-04-08 16:31:24 +0800

    Unit test date picker cell

--- WooCommerce/Classes/ViewRelated/ReusableViews/DatePickerTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/DatePickerTableViewCell.swift
@@ -1,10 +1,19 @@
 import UIKit
 
+/// Cell containing a UIDatePicker, configured as Year / Month / Day
+///
 final class DatePickerTableViewCell: UITableViewCell {
 
     var onDateSelected: ((Date) -> Void)?
+    @IBOutlet private weak var picker: UIDatePicker!
 
     @IBAction func dateChanged(_ sender: UIDatePicker) {
         onDateSelected?(sender.date)
+    }
+}
+
+extension DatePickerTableViewCell {
+    func getPicker() -> UIDatePicker {
+        return picker
     }
 }

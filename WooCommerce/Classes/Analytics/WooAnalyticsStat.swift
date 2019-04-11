31bbf921e065a7e9a17295acfa8ca04f583f80d9
Thuy.Copeland <thuycopeland@gmail.com>
2019-04-11 14:44:57 -0500

    Truncate event name
    Correct name is `privacy_settings_share_info_link_tapped`

--- WooCommerce/Classes/Analytics/WooAnalyticsStat.swift
+++ WooCommerce/Classes/Analytics/WooAnalyticsStat.swift
@@ -110,7 +110,7 @@ public enum WooAnalyticsStat: String {
     case settingsCollectInfoToggled             = "settings_privacy_settings_collect_info_toggled"
     case settingsReportCrashesToggled           = "settings_privacy_settings_report_crashes_toggled"
     case settingsPrivacyPolicyTapped            = "settings_privacy_settings_privacy_policy_link_tapped"
-    case settingsShareInfoLearnMoreTapped       = "settings_privacy_settings_share_info_link_tapped"
+    case settingsShareInfoLearnMoreTapped       = "privacy_settings_share_info_link_tapped"
     case settingsThirdPartyLearnMoreTapped      = "privacy_settings_third_party_tracking_info_link_tapped"
     case settingsLicensesLinkTapped             = "settings_about_open_source_licenses_link_tapped"
     case settingsAboutLinkTapped                = "settings_about_woocommerce_link_tapped"

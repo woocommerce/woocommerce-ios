f230383040713ec65c16cabaa517551f79a1708b
Diego Rey Mendez <diegoreymendez@gmail.com>
2019-04-24 18:58:30 -0300

    Removes a method that was commited by mistake.

--- WordPressAuthenticator/WordPressAuthenticator/Model/LoginFields+Validation.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Model/LoginFields+Validation.swift
@@ -1,15 +1,3 @@
-extension String {
-    func isValidURL() -> Bool {
-        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-        if let match = detector.firstMatch(in: self, options: [], range: NSRange(location: 0, length: self.utf16.count)) {
-            // it is a link, if the match covers the whole string
-            return match.range.length == self.utf16.count
-        } else {
-            return false
-        }
-    }
-}
-
 // MARK: - LoginFields Validation Methods
 //
 extension LoginFields {

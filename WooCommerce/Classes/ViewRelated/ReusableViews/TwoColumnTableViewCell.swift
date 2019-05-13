f91c0e6889e23e55b2b786cf1f30b85c714d9749
Thuy.Copeland <thuycopeland@gmail.com>
2019-05-13 16:37:28 -0500

    Add a two-column tableview cell with Body styling

---
+++ WooCommerce/Classes/ViewRelated/ReusableViews/TwoColumnTableViewCell.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+/// A two column table view cell with Body styling.
+///
+final class TwoColumnTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var leftLabel: UILabel!
+    @IBOutlet weak var rightLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}

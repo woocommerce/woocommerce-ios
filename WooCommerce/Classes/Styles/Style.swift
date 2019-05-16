78d00f68e9c89b43cbd5b70100d4c3e42ff72ea5
Thuy.Copeland <thuycopeland@gmail.com>
2019-05-16 13:59:49 -0500

    Modify review star color

--- WooCommerce/Classes/Styles/Style.swift
+++ WooCommerce/Classes/Styles/Style.swift
@@ -153,7 +153,7 @@ private extension DefaultStyle {
         static let wooGreyTextMin        = UIColor(red: 89.0/255.0, green: 89.0/255.0, blue: 89.0/255.0, alpha: 1.0)
 
         static let goldStarColor         = UIColor(red: 238.0/255.0, green: 180.0/255.0, blue: 34.0/255.0, alpha: 1.0)
-        static let grayStarColor         = UIColor(red: 0, green: 0, blue: 0, alpha: 0.54)
+        static let grayStarColor         = UIColor(red: 89.0/255.0, green: 89.0/255.0, blue: 89.0/255.0, alpha: 1.0)
     }
 }
 

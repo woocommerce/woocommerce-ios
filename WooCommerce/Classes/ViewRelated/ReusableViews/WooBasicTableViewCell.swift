f7fa7d52ad344065081b6372f923416d35a5a30a
Thuy.Copeland <thuycopeland@gmail.com>
2019-05-23 15:30:12 -0500

    Use relative margins for WooBasicTableViewCell

--- WooCommerce/Classes/ViewRelated/ReusableViews/WooBasicTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/WooBasicTableViewCell.swift
@@ -5,12 +5,12 @@ import UIKit
 
 /// A UITableViewCell with bonus purple Woo styling.
 ///
-/// For reasons I cannot figure out, a `BasicTableViewCell` has
+/// So `BasicTableViewCell` uses springs and struts, causing
 /// a different leading margin ("left margin") measurement than
-/// a custom one. Custom cells follow the superview margin. Custom
-/// cells sets a 30 point leading margin on an iPhone XS Max.
-/// But a BasicTableViewCell sets the leading margin to 20 points.
-/// So here we are, building a basic table view cell as a custom cell,
+/// a custom cell. Custom cells follow the superview margin. Custom
+/// cells set a 20 point leading margin on an iPhone XS Max.
+/// But a BasicTableViewCell sets the leading margin to 16 points.
+/// So here we are, building a `BasicTableViewCell` as a custom cell,
 /// so that the margins match. --- ¯\_(ツ)_/¯ 21.05.2019 tc
 ///
 class WooBasicTableViewCell: UITableViewCell {

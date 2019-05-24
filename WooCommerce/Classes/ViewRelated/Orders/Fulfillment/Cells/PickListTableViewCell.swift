f20dea91dc737781f6b027fe17c9021a185a2627
Thuy.Copeland <thuycopeland@gmail.com>
2019-05-24 11:14:45 -0500

    Add a pick list table view cell

---
+++ WooCommerce/Classes/ViewRelated/Orders/Fulfillment/Cells/PickListTableViewCell.swift
@@ -0,0 +1,99 @@
+import UIKit
+import Gridicons
+import Yosemite
+
+
+/// Pick List: Renders a row that displays a single Product.
+///
+class PickListTableViewCell: UITableViewCell {
+
+    /// ImageView
+    ///
+    @IBOutlet private var productImageView: UIImageView!
+
+    /// Label: Name
+    ///
+    @IBOutlet private var nameLabel: UILabel!
+
+    /// Label: Quantity
+    ///
+    @IBOutlet private var quantityLabel: UILabel!
+
+    /// Label: SKU
+    ///
+    @IBOutlet private var skuLabel: UILabel!
+
+    /// Product Name
+    ///
+    var name: String? {
+        get {
+            return nameLabel?.text
+        }
+        set {
+            nameLabel?.text = newValue
+        }
+    }
+
+    /// Number of Items
+    ///
+    var quantity: String? {
+        get {
+            return quantityLabel?.text
+        }
+        set {
+            quantityLabel?.text = newValue
+        }
+    }
+
+    /// Item's SKU
+    ///
+    var sku: String? {
+        get {
+            return skuLabel?.text
+        }
+        set {
+            skuLabel?.text = newValue
+        }
+    }
+
+    // MARK: - Overridden Methods
+
+    required init?(coder aDecoder: NSCoder) {
+        // initializers don't call property observers,
+        // so don't set the default for mode here.
+        super.init(coder: aDecoder)
+    }
+
+    class func makeFromNib() -> PickListTableViewCell {
+        return Bundle.main.loadNibNamed("ProductDetailsTableViewCell", owner: self, options: nil)?.first as! PickListTableViewCell
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        productImageView.image = Gridicon.iconOfType(.product)
+        productImageView.tintColor = StyleManager.wooGreyBorder
+        selectionStyle = .none
+
+        nameLabel.applyBodyStyle()
+        quantityLabel.applyBodyStyle()
+        skuLabel.applySecondaryFootnoteStyle()
+
+        nameLabel?.text = ""
+        quantityLabel?.text = ""
+        skuLabel?.text = ""
+    }
+}
+
+
+// MARK: - Public Methods
+//
+extension PickListTableViewCell {
+    func configure(item: OrderItemViewModel, image: UIImage? = nil) {
+        if let itemImage = image {
+            productImageView.image = itemImage
+        }
+        name = item.name
+        quantity = item.quantity
+        sku = item.sku
+    }
+}

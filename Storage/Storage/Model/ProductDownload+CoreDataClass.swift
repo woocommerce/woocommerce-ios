9bace4e26c9fd9ee0a1c575e0cd105e68f763ae2
Thuy.Copeland <thuycopeland@gmail.com>
2019-05-29 18:15:36 -0500

    Storage - generate the core data classes for ProductDownload entity

---
+++ Storage/Storage/Model/ProductDownload+CoreDataClass.swift
@@ -0,0 +1,8 @@
+import Foundation
+import CoreData
+
+
+@objc(ProductDownload)
+public class ProductDownload: NSManagedObject {
+
+}
---
+++ Storage/Storage/Model/ProductDownload+CoreDataProperties.swift
@@ -0,0 +1,16 @@
+import Foundation
+import CoreData
+
+
+extension ProductDownload {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ProductDownload> {
+        return NSFetchRequest<ProductDownload>(entityName: "ProductDownload")
+    }
+
+    @NSManaged public var downloadID: String?
+    @NSManaged public var name: String?
+    @NSManaged public var fileURL: String?
+    @NSManaged public var product: Product?
+
+}

9cbbc72733857155aeee65072e705133245ad58f
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-06-11 15:46:45 +0800

    Add tests to cover the icons

---
+++ WooCommerce/WooCommerceTests/Extensions/IconsTests.swift
@@ -0,0 +1,148 @@
+import XCTest
+@testable import WooCommerce
+
+final class IconsTests: XCTestCase {
+    func testAddOutlineIconIsNotNil() {
+        XCTAssertNotNil(UIImage.addOutlineImage)
+    }
+
+    func testAsideIconIsNotNil() {
+        XCTAssertNotNil(UIImage.asideImage)
+    }
+
+    func testBellIconIsNotNil() {
+        XCTAssertNotNil(UIImage.bellImage)
+    }
+
+    func testCameraIconIsNotNil() {
+        XCTAssertNotNil(UIImage.cameraImage)
+    }
+
+    func testCheckmarkImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.checkmarkImage)
+    }
+
+    func testCheckmarkStyledImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.checkmarkStyledImage)
+    }
+
+    func testChevronImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.chevronImage)
+    }
+
+    func testChevronDownImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.chevronDownImage)
+    }
+
+    func testChevronUpImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.chevronUpImage)
+    }
+
+    func testCogImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.cogImage)
+    }
+
+    func testDeleteImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.deleteImage)
+    }
+
+    func testEllipsisImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.ellipsisImage)
+    }
+
+    func testErrorStateImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.errorStateImage)
+    }
+
+    func testExternalImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.externalImage)
+    }
+
+    func testFilterImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.filterImage)
+    }
+
+    func testHeartOutlineImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.heartOutlineImage)
+    }
+
+    func testJetpackLogoImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.jetpackLogoImage)
+    }
+
+    func testInvisibleImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.invisibleImage)
+    }
+
+    func testMailImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.mailImage)
+    }
+
+    func testMoreImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.moreImage)
+    }
+
+    func testProductPlaceholderImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.productPlaceholderImage)
+    }
+
+    func testProductImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.productImage)
+    }
+
+    func testPencilImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.pencilImage)
+    }
+
+    func testQuoteImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.quoteImage)
+    }
+
+    func testPagesImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.pagesImage)
+    }
+
+    func testSearchImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.searchImage)
+    }
+
+    func testSpamImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.spamImage)
+    }
+
+    func testStarImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.starImage(size: 1, tintColor: .clear))
+    }
+
+    func testStarImageMatchesExpectedSize() {
+        let size = Double(1)
+        let starImage = UIImage.starImage(size: size, tintColor: .clear)
+        XCTAssertEqual(starImage.size, CGSize(width: size, height: size))
+    }
+
+    func testStatsImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.statsImage)
+    }
+
+    func testStatsAltImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.statsAltImage)
+    }
+
+    func testTrashImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.trashImage)
+    }
+
+    func testWooLogoImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.wooLogoImage())
+    }
+
+    func testWooLogoImageMatchesExpectedSize() {
+        let size = CGSize(width: 20, height: 20)
+        let image = UIImage.wooLogoImage(withSize: size, tintColor: .clear)
+        XCTAssertEqual(size, image!.size)
+    }
+
+    func testWaitingForCustomersImageIconIsNotNil() {
+        XCTAssertNotNil(UIImage.waitingForCustomersImage)
+    }
+}

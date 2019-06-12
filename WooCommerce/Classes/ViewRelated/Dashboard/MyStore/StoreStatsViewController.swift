ebeba2533ca238464b3913c727656ad5618585e0
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-06-12 13:09:38 +0800

    Flip horizontal scroller for store stats and top performers on RTL languages

--- WooCommerce/Classes/ViewRelated/Dashboard/MyStore/StoreStatsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/MyStore/StoreStatsViewController.swift
@@ -41,6 +41,17 @@ class StoreStatsViewController: ButtonBarPagerTabStripViewController {
         ensureGhostContentIsAnimated()
     }
 
+    // MARK: - RTL support
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        /// ButtonBarView is a collection view, and it should flip to support
+        /// RTL languages automatically. And yet it doesn't.
+        /// So, for RTL languages, we flip it. This also flips the cells
+        if traitCollection.layoutDirection == .rightToLeft {
+            buttonBarView.transform = CGAffineTransform(scaleX: -1, y: 1)
+        }
+    }
 
     // MARK: - PagerTabStripDataSource
 
@@ -52,6 +63,11 @@ class StoreStatsViewController: ButtonBarPagerTabStripViewController {
         /// Hide the ImageView:
         /// We don't use it, and if / when "Ghostified" produces a quite awful placeholder UI!
         cell.imageView.isHidden = true
+
+        /// Flip the cells back to their proper state for RTL languages.
+        if traitCollection.layoutDirection == .rightToLeft {
+            cell.transform = CGAffineTransform(scaleX: -1, y: 1)
+        }
     }
 }
 

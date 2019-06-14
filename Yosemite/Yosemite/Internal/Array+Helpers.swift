f20936d0ffc6a9292bc587482f9ebbcb67c5de1a
Thuy.Copeland <thuycopeland@gmail.com>
2019-06-14 12:56:56 -0500

    Delete unnecessary comment

--- Yosemite/Yosemite/Internal/Array+Helpers.swift
+++ Yosemite/Yosemite/Internal/Array+Helpers.swift
@@ -1,11 +1,9 @@
 import Foundation
 
 
-/// Not mine. Sourced from: https://stackoverflow.com/a/46354989/4150507
-///
 public extension Array where Element: Hashable {
 
-    /// Non-mutation function that removes duplicates in an array.
+    /// Non-mutated function that removes duplicates in an array.
     ///
     func uniqued() -> [Element] {
         var seen = Set<Element>()

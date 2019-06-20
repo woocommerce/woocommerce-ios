7513ea90dc094e2e0d3aca00e02cb791928c671d
Jaclyn Chen <watertranquil@gmail.com>
2019-06-20 16:01:54 -0400

    Override `textRect(forBounds bounds: CGRect, ...)` to specify where text is rendered.

--- WooCommerce/Classes/Styles/PaddedLabel.swift
+++ WooCommerce/Classes/Styles/PaddedLabel.swift
@@ -22,6 +22,16 @@ class PaddedLabel: UILabel {
         intrinsicSuperViewContentSize.width += textInsets.left + textInsets.right
         return intrinsicSuperViewContentSize
     }
+
+    override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let insetRect = bounds.inset(by: textInsets)
+        let textRect = super.textRect(forBounds: insetRect, limitedToNumberOfLines: numberOfLines)
+        let invertedInsets = UIEdgeInsets(top: -textInsets.top,
+                                          left: -textInsets.left,
+                                          bottom: -textInsets.bottom,
+                                          right: -textInsets.right)
+        return textRect.inset(by: invertedInsets)
+    }
 }
 
 

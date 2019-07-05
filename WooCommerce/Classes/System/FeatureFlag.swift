4b490c836c9616cff17aadc0f5cfde02a3aa71e1
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-07-05 11:33:48 +0800

    Add a feature flag for stats

--- WooCommerce/Classes/System/FeatureFlag.swift
+++ WooCommerce/Classes/System/FeatureFlag.swift
@@ -5,11 +5,14 @@ enum FeatureFlag: Int {
     /// Throwaway case, to prevent a compiler error:
     /// `An enum with no cases cannot declare a raw type`
     case null
+    case stats
 
     /// Returns a boolean indicating if the feature is enabled
     ///
     var enabled: Bool {
         switch self {
+        case .stats:
+            return BuildConfiguration.current == .localDeveloper
         default:
             return true
         }

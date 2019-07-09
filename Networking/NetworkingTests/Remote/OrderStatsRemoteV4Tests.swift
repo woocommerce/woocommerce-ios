08641b3af752f04c23683bd427621ced10d6231e
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-07-09 15:13:31 -0400

    Unit test OrderStatsRemoteV4

--- Networking/NetworkingTests/Remote/OrderStatsRemoteV4Tests.swift
+++ Networking/NetworkingTests/Remote/OrderStatsRemoteV4Tests.swift
@@ -1,6 +1,8 @@
 import XCTest
 @testable import Networking
 
+/// OrderStatsRemote Unit Tests
+///
 final class OrderStatsRemoteV4Tests: XCTestCase {
 
     /// Dummy Network Wrapper
@@ -17,9 +19,10 @@ final class OrderStatsRemoteV4Tests: XCTestCase {
         network.removeAllSimulatedResponses()
     }
 
-    /// Verifies that loadOrderStats properly parses the `OrderStatsV4` sample response.
+    /// Verifies that loadOrderStats properly parses the `OrderStatsV4` sample response
+    /// when requesting the hourly stats
     ///
-    func testLoadOrderStatsProperlyReturnsParsedStats() {
+    func testLoadOrderStatsProperlyReturnsParsedStatsForHourlyStats() {
         let remote = OrderStatsRemoteV4(network: network)
         let expectation = self.expectation(description: "Load order stats")
 
@@ -33,6 +36,43 @@ final class OrderStatsRemoteV4Tests: XCTestCase {
                                 XCTAssertNotNil(orderStatsV4)
                                 XCTAssertEqual(orderStatsV4?.intervals.count, 24)
                                 expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+
+    /// Verifies that loadOrderStats properly parses the `OrderStatsV4` sample response
+    /// when requesting the weekly stats
+    ///
+    func testLoadOrderStatsProperlyReturnsParsedStatsForWeeklyStats() {
+        let remote = OrderStatsRemoteV4(network: network)
+        let expectation = self.expectation(description: "Load order stats")
+
+        network.simulateResponse(requestUrlSuffix: "reports/revenue/stats", filename: "order-stats-v4-defaults")
+
+        remote.loadOrderStats(for: sampleSiteID,
+                              unit: .weekly,
+                              latestDateToInclude: "1955-11-05",
+                              quantity: 2) { (orderStatsV4, error) in
+                                XCTAssertNil(error)
+                                XCTAssertNotNil(orderStatsV4)
+                                XCTAssertEqual(orderStatsV4?.intervals.count, 2)
+                                expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+
+    /// Verifies that loadOrderStats properly relays Networking Layer errors.
+    ///
+    func testLoadOrderStatsProperlyRelaysNetwokingErrors() {
+        let remote = OrderStatsRemoteV4(network: network)
+        let expectation = self.expectation(description: "Load order stats contains errors")
+
+        remote.loadOrderStats(for: sampleSiteID, unit: .daily, latestDateToInclude: "1955-11-05", quantity: 31) { (orderStats, error) in
+            XCTAssertNil(orderStats)
+            XCTAssertNotNil(error)
+            expectation.fulfill()
         }
 
         wait(for: [expectation], timeout: Constants.expectationTimeout)

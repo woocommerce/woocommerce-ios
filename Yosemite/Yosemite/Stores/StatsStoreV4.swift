52dc0fa6d074f98ee8965899ab8a7089127d499b
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-07-17 16:35:33 -0400

    Add missing methods to coredata extensions and finalise first implementation of store

--- Yosemite/Yosemite/Stores/StatsStoreV4.swift
+++ Yosemite/Yosemite/Stores/StatsStoreV4.swift
@@ -56,20 +56,65 @@ public extension StatsStoreV4 {
         let remote = OrderStatsRemoteV4(network: network)
 
         remote.loadOrderStats(for: siteID, unit: granularity, latestDateToInclude: date, quantity: quantity) { [weak self] (orderStatsV4, error) in
-            //
+            guard let orderStatsV4 = orderStatsV4 else {
+                onCompletion(error)
+                return
+            }
+
+            self?.upsertStoredOrderStats(readOnlyStats: orderStatsV4)
+            onCompletion(nil)
+        }
+    }
+}
+
+
+// MARK: - Persistence
+//
+extension StatsStoreV4 {
+    /// Updates (OR Inserts) the specified ReadOnly OrderStatsV4 Entity into the Storage Layer.
+    ///
+    func upsertStoredOrderStats(readOnlyStats: Networking.OrderStatsV4) {
+        assert(Thread.isMainThread)
+
+        let storage = storageManager.viewStorage
+        let storageOrderStats = storage.loadOrderStatsV4(siteID: String(readOnlyStats.siteID), granularity: readOnlyStats.granularity.rawValue) ?? storage.insertNewObject(ofType: Storage.OrderStatsV4.self)
+        storageOrderStats.update(with: readOnlyStats)
+        handleOrderStatsItems(readOnlyStats, storageOrderStats, storage)
+        storage.saveIfNeeded()
+    }
+
+    /// Updates the provided StorageOrderStats items using the provided read-only OrderStats items
+    ///
+    private func handleOrderStatsItems(_ readOnlyStats: Networking.OrderStatsV4, _ storageStats: Storage.OrderStatsV4, _ storage: StorageType) {
+
+        let readOnlyIntervals = readOnlyStats.intervals
+
+        if readOnlyIntervals.isEmpty {
+            // No items in the read-only order stats, so remove all the intervals in Storage.OrderStatsV4
+            storageStats.intervals?.forEach {
+                storageStats.removeFromIntervals($0)
+                storage.deleteObject($0)
+            }
+            return
         }
 
-//        let remote = OrderStatsRemote(network: network)
-//        let formattedDateString = StatsStore.buildDateString(from: latestDateToInclude, with: granularity)
-//
-//        remote.loadOrderStats(for: siteID, unit: granularity, latestDateToInclude: formattedDateString, quantity: quantity) { [weak self] (orderStats, error) in
-//            guard let orderStats = orderStats else {
-//                onCompletion(error)
-//                return
-//            }
-//
-//            self?.upsertStoredOrderStats(readOnlyStats: orderStats)
-//            onCompletion(nil)
-//        }
+        // Upsert the items from the read-only order stats item
+        for readOnlyInterval in readOnlyIntervals {
+            if let existingStorageInterval = storage.loadOrderStatsInterval(interval: readOnlyInterval.interval) {
+                existingStorageInterval.update(with: readOnlyInterval)
+            } else {
+                let newStorageInterval = storage.insertNewObject(ofType: Storage.OrderStatsV4Interval.self)
+                newStorageInterval.update(with: readOnlyInterval)
+                storageStats.addToIntervals(newStorageInterval)
+            }
+        }
+
+        // Now, remove any objects that exist in storageStats.intervals but not in readOnlyStats.intervals
+        storageStats.intervals?.forEach({ storageInterval in
+            if readOnlyIntervals.first(where: { $0.interval == storageInterval.interval } ) == nil {
+                storageStats.removeFromIntervals(storageInterval)
+                storage.deleteObject(storageInterval)
+            }
+        })
     }
 }

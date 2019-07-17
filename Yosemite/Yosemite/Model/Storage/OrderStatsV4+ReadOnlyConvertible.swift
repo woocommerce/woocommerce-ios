7832c8753ad40bf2b19560b4356f4d9a4e6c64d6
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-07-17 16:14:48 -0400

    Add all model objects and readonly extensions

---
+++ Yosemite/Yosemite/Model/Storage/OrderStatsV4+ReadOnlyConvertible.swift
@@ -0,0 +1,42 @@
+import Foundation
+import Storage
+
+
+// MARK: - Storage.OrderStatsV4: ReadOnlyConvertible
+//
+extension Storage.OrderStatsV4: ReadOnlyConvertible {
+    /// Updates the Storage.OrderStatsV4 with the ReadOnly.
+    ///
+    public func update(with stats: Yosemite.OrderStatsV4) {
+        siteID = stats.siteID
+        granularity = stats.granularity.rawValue
+    }
+
+    /// Returns a ReadOnly version of the receiver.
+    ///
+    public func toReadOnly() -> Yosemite.OrderStatsV4 {
+        let statsIntervals = intervals?.map { $0.toReadOnly()} ?? [Yosemite.OrderStatsV4Interval]()
+        
+        return OrderStatsV4(siteID: siteID,
+                            granularity: StatsGranularityV4(rawValue: granularity) ?? .hourly,
+                            totals: totals?.toReadOnly() ?? createReadOnlySubTotals(),
+                            intervals: statsIntervals)
+
+
+    }
+
+    // MARK: - Private Helpers
+    //
+    private func createReadOnlySubTotals() -> Yosemite.OrderStatsV4Totals {
+        return OrderStatsV4Totals(totalOrders: 0,
+                                  totalItemsSold: 0,
+                                  grossRevenue: 0,
+                                  couponDiscount: 0,
+                                  totalCoupons: 0,
+                                  refunds: 0,
+                                  taxes: 0,
+                                  shipping: 0,
+                                  netRevenue: 0,
+                                  totalProducts: 0)
+    }
+}

09aab5b1fcb2cd09f30d8136d7b0bf2bddfd60e7
Jaclyn Chen <watertranquil@gmail.com>
2019-07-24 07:38:04 +0800

    Dashboard: handle error when when user has no permission to view site visit stats (#1119)
    * Pass error message to `DotcomError` `unauthorized` case.
    
    * Add `noPermission` error case to `SiteVisitStatsStoreError` associated with the error message from an unauthorized remote error.
    
    * Handle `noPermission` site visit stats error case the same as when stats module is disabled - hide visitor stats UI without alert toast.
    
    * Update release notes.
    
    * Add `DotcomError` extension in `NetworkingTests` to allow case comparison without associated values. Fix tests that compare to `DotcomError.unauthorized`.
    
    * Revert "Add `DotcomError` extension in `NetworkingTests` to allow case comparison without associated values. Fix tests that compare to `DotcomError.unauthorized`."
    
    This reverts commit e73077a702025af33122a2fdad1d881bdef001e5.
    
    * Move not-so-cute error message checks to `DotcomError` initializer so that API implementation details are encapsulated in the Networking layer.
    
    * Style fixes.
    
    * Remove string type code.
    
    * Add unit tests to verify the new `DotcomError` cases with mock responses.
    
    * Prettify enum cases with message comparison.

--- Yosemite/Yosemite/Stores/StatsStore.swift
+++ Yosemite/Yosemite/Stores/StatsStore.swift
@@ -285,23 +285,15 @@ extension StatsStore {
 }
 
 /// An error that occurs while fetching site visit stats.
-/// API documentation of possible errors:
-/// https://developer.wordpress.com/docs/api/1.1/get/sites/%24site/stats/
 ///
+/// - noPermission: the user has no permission to view site stats.
 /// - statsModuleDisabled: Jetpack site stats module is disabled for the site.
 /// - unknown: other error cases.
 ///
 public enum SiteVisitStatsStoreError: Error {
     case statsModuleDisabled
+    case noPermission
     case unknown
-
-    private enum ErrorIdentifiers {
-        static let invalidBlog: String = "invalid_blog"
-    }
-
-    private enum ErrorMessages {
-        static let statsModuleDisabled: String = "This blog does not have the Stats module enabled"
-    }
 
     init(error: Error) {
         guard let dotcomError = error as? DotcomError else {
@@ -309,12 +301,10 @@ public enum SiteVisitStatsStoreError: Error {
             return
         }
         switch dotcomError {
-        case .unknown(let code, let message):
-            if code == ErrorIdentifiers.invalidBlog && message == ErrorMessages.statsModuleDisabled {
-                self = .statsModuleDisabled
-            } else {
-                self = .unknown
-            }
+        case .noStatsPermission:
+            self = .noPermission
+        case .statsModuleDisabled:
+            self = .statsModuleDisabled
         default:
             self = .unknown
         }

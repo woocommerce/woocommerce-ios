81ae118cef15cfa78ada2ec09484ad3e72ebea15
Jaclyn Chen <watertranquil@gmail.com>
2019-07-29 11:33:53 +0800

    Add unit test.

---
+++ WooCommerce/WooCommerceTests/Tools/AsyncDictionaryTests.swift
@@ -0,0 +1,27 @@
+import XCTest
+@testable import WooCommerce
+
+class AsyncDictionaryTests: XCTestCase {
+    private let asyncDictionary = AsyncDictionary<Int, String>()
+
+    func testAsyncValueCalculationAndUpdate() {
+        let key = 107
+        let value = "Woo!"
+
+        let expectationForCalculation = self.expectation(description: "Wait for calculation to be called not on the main thread")
+        let calculation = { () -> String in
+            XCTAssertFalse(Thread.current.isMainThread)
+            expectationForCalculation.fulfill()
+            return value
+        }
+        let expectationForOnUpdate = self.expectation(description: "Wait for update callback to be called on the main thread")
+        let onUpdate = { (updatedValue: String) in
+            XCTAssertEqual(updatedValue, value)
+            XCTAssertTrue(Thread.current.isMainThread)
+            expectationForOnUpdate.fulfill()
+        }
+        asyncDictionary.calculateAsynchronouslyAndUpdateValue(forKey: key, calculation: calculation, onUpdate: onUpdate)
+        waitForExpectations(timeout: 0.5, handler: nil)
+        XCTAssertEqual(asyncDictionary.value(forKey: key), value)
+    }
+}

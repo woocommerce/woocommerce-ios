b6e9ccedc5e369561ccde5e403039cecdfe87c96
Jaclyn Chen <watertranquil@gmail.com>
2019-08-06 17:27:32 +0800

    Remove enum code that is moved to `Yosemite`.

--- WooCommerce/Classes/ViewRelated/Dashboard/Stats v4/StatsGranularityV4+UI.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Stats v4/StatsGranularityV4+UI.swift
@@ -1,43 +1,6 @@
 import Yosemite
 
-extension StatsGranularityV4 {
-//    private func numberOfSeconds(numberOfIntervals: Int) -> TimeInterval {
-//        switch self {
-//        case .hourly:
-//            return 3600 * Double(numberOfIntervals)
-//        case .daily:
-//            return 86400 * Double(numberOfIntervals)
-//        case .weekly:
-//            return 86400 * 7 * Double(numberOfIntervals)
-//        case .monthly:
-//            return 86400 * 7
-//        default:
-//            fatalError("This case is not supported: \(self.rawValue)")
-//        }
-//    }
-}
-
-enum StatsTimeRangeV4 {
-    case today
-    case thisWeek
-    case thisMonth
-    case thisYear
-}
-
 extension StatsTimeRangeV4 {
-    var intervalGranularity: StatsGranularityV4 {
-        switch self {
-        case .today:
-            return .hourly
-        case .thisWeek:
-            return .daily
-        case .thisMonth:
-            return .daily
-        case .thisYear:
-            return .monthly
-        }
-    }
-
     // TODO-jc: more calculation later
     var intervalQuantity: Int {
         switch self {

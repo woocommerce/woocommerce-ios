bf8c4567c2981b27a4fcaddc620889e2b06fd5a1
Paolo Musolino <info@codeido.com>
2019-08-13 12:53:41 +0200

    Swift lint corrections

--- Networking/NetworkingTests/Mapper/OrderRefundsMapperTests.swift
+++ Networking/NetworkingTests/Mapper/OrderRefundsMapperTests.swift
@@ -5,35 +5,35 @@ import XCTest
 /// OrderRefundsMapper Unit Tests
 ///
 class OrderRefundsMapperTests: XCTestCase {
-    
+
     /// Dummy Site ID.
     ///
     private let dummySiteID = 33334444
-    
+
     /// Verifies that all of the Order Refunds fields are parsed correctly.
     ///
     func testOrderRefundsFieldsAreProperlyParsed() {
         let orderRefunds = mapLoadAllOrderRefundsResponse()
         XCTAssertEqual(orderRefunds.count, 2)
-        
+
         let firstOrderRefunds = orderRefunds[0]
         XCTAssertEqual(firstOrderRefunds.siteID, dummySiteID)
         XCTAssertEqual(firstOrderRefunds.refundID, 726)
-        
+
         let dateCreated = DateFormatter.Defaults.dateTimeFormatter.date(from: "2017-03-21T20:07:11")
         XCTAssertEqual(firstOrderRefunds.dateCreated, dateCreated)
-        
+
         XCTAssertEqual(firstOrderRefunds.amount, "10.00")
         XCTAssertEqual(firstOrderRefunds.reason, "Product No Longer Needed")
         XCTAssertEqual(firstOrderRefunds.refunded_by, 1)
         XCTAssertEqual(firstOrderRefunds.refunded_payment, false)
     }
-    
+
     /// Verifies that all of the Order Refunds Items are parsed correctly.
     ///
     func testOrderRefundsItemsAreCorrectlyParsed() {
         let orderRefunds = mapLoadAllOrderRefundsResponse()
-        
+
         let item = orderRefunds[0].items[0]
         XCTAssertEqual(item.itemID, 888)
         XCTAssertEqual(item.name, "Poster (Product Add-On)")
@@ -54,17 +54,17 @@ class OrderRefundsMapperTests: XCTestCase {
 /// Private Methods.
 ///
 private extension OrderRefundsMapperTests {
-    
+
     /// Returns the OrderRefundsMapper output upon receiving `filename` (Data Encoded)
     ///
     func mapOrderRefunds(from filename: String) -> [OrderRefund] {
         guard let response = Loader.contentsOf(filename) else {
             return []
         }
-        
+
         return try! OrderRefundsMapper(siteID: dummySiteID).map(response: response)
     }
-    
+
     /// Returns the OrderRefundsMapper output upon receiving `order-refunds-list`
     ///
     func mapLoadAllOrderRefundsResponse() -> [OrderRefund] {
--- Networking/NetworkingTests/Remote/RefundsRemoteTests.swift
+++ Networking/NetworkingTests/Remote/RefundsRemoteTests.swift
@@ -8,15 +8,15 @@ class RefundsRemoteTests: XCTestCase {
     /// Dummy Network Wrapper
     ///
     let network = MockupNetwork()
-    
+
     /// Dummy Site ID
     ///
     let sampleSiteID = 1234
-    
+
     /// Dummy Order ID
     ///
     let sampleOrderID = 1467
-    
+
     /// Repeat always!
     ///
     override func setUp() {
@@ -24,22 +24,22 @@ class RefundsRemoteTests: XCTestCase {
     }
 
     // MARK: - Load All Order Refunds Tests
-    
+
     /// Verifies that loadOrderRefunds properly parses the `order-refunds-list` sample response.
     ///
     func testLoadAllOrderRefundsProperlyReturnsParsedModels() {
         let remote = RefundsRemote(network: network)
         let expectation = self.expectation(description: "Load All Order Refunds")
-        
+
         network.simulateResponse(requestUrlSuffix: "orders/\(sampleOrderID)/refunds", filename: "order-refunds-list")
-        
+
         remote.loadOrderRefunds(for: sampleSiteID, by: sampleOrderID) { refunds, error in
             XCTAssertNil(error)
             XCTAssertNotNil(refunds)
             XCTAssert(refunds?.count == 2)
             expectation.fulfill()
         }
-        
+
         wait(for: [expectation], timeout: Constants.expectationTimeout)
     }
 
@@ -48,13 +48,13 @@ class RefundsRemoteTests: XCTestCase {
     func testLoadAllOrderRefundsProperlyRelaysNetwokingErrors() {
         let remote = RefundsRemote(network: network)
         let expectation = self.expectation(description: "Load All Order Refunds")
-        
+
         remote.loadOrderRefunds(for: sampleSiteID, by: sampleOrderID) { refunds, error in
             XCTAssertNil(refunds)
             XCTAssertNotNil(error)
             expectation.fulfill()
         }
-        
+
         wait(for: [expectation], timeout: Constants.expectationTimeout)
     }
 

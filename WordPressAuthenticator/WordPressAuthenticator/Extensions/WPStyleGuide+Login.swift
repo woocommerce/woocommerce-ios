245f3f2896fe61775ee9f257ff47e0e8086482a8
Stephenie Harris <scoutharris@gmail.com>
2019-08-13 15:20:55 -0600

    Style selfHostedLoginButton differently depending on which Log In flow is displayed.

--- WordPressAuthenticator/WordPressAuthenticator/Extensions/WPStyleGuide+Login.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Extensions/WPStyleGuide+Login.swift
@@ -116,12 +116,24 @@ extension WPStyleGuide {
     ///
     /// - Returns: A properly styled UIButton
     ///
-    class func selfHostedLoginButton() -> UIButton {
-        let baseString =  NSLocalizedString("Log in by entering your site address.", comment: "Label for button to log in using your site address.")
+    class func selfHostedLoginButton(alignment: UIControl.NaturalContentHorizontalAlignment = .leading) -> UIButton {
+        
+        let style = WordPressAuthenticator.shared.style
+        
+        if WordPressAuthenticator.shared.configuration.showNewLoginFlow {
+            let baseString =  NSLocalizedString("Or log in by _entering your site address_.", comment: "Label for button to log in using site address. Underscores _..._ denote underline.")
+            
+            let attrStrNormal = baseString.underlined(color: style.subheadlineColor, underlineColor: style.textButtonColor)
+            let attrStrHighlight = baseString.underlined(color: style.subheadlineColor, underlineColor: style.textButtonHighlightColor)
+            let font = WPStyleGuide.mediumWeightFont(forStyle: .caption2)
+            
+            return textButton(normal: attrStrNormal, highlighted: attrStrHighlight, font: font, alignment: alignment)
+        }
+        
+        let baseString = NSLocalizedString("Log in by entering your site address.", comment: "Label for button to log in using your site address.")
 
-        let attrStrNormal = selfHostedButtonString(baseString, linkColor:  WordPressAuthenticator.shared.style.textButtonColor)
-        let attrStrHighlight = selfHostedButtonString(baseString, linkColor: WordPressAuthenticator.shared.style.textButtonHighlightColor)
-
+        let attrStrNormal = selfHostedButtonString(baseString, linkColor:  style.textButtonColor)
+        let attrStrHighlight = selfHostedButtonString(baseString, linkColor: style.textButtonHighlightColor)
         let font = WPStyleGuide.mediumWeightFont(forStyle: .subheadline)
 
         return textButton(normal: attrStrNormal, highlighted: attrStrHighlight, font: font)

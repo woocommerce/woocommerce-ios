7ab53207e0f1259f120dd30d0bfa080b6ed656fb
Paolo Musolino <info@codeido.com>
2019-08-16 17:40:13 +0200

    Added "api_refund" param in Refund model

--- Networking/Networking/Model/Refund/Refund.swift
+++ Networking/Networking/Model/Refund/Refund.swift
@@ -12,18 +12,23 @@ public struct Refund {
     ///
     public let reason: String?
     
+    /// If true, the automatic refund is used. When false, manual refund process is used.
+    ///
+    public let apiRefund: Bool
+    
     /// Optional Line items to be refunded
     ///
     public let items: [LineItemRefund]?
     
-    public init(amount: String, reason: String?, items: [LineItemRefund]?) {
+    public init(amount: String, reason: String?, apiRefund: Bool = true, items: [LineItemRefund]?) {
         self.amount = amount
         self.reason = reason
         self.items = items
+        self.apiRefund = apiRefund
     }
     
     public func toDictionary() -> [String: Any]{
-        var dict: [String: Any] = ["amount": amount]
+        var dict: [String: Any] = ["amount": amount, "api_refund": apiRefund]
         
         if reason != nil{
             dict["reason"] = reason
@@ -42,6 +47,7 @@ extension Refund: Comparable {
     public static func == (lhs: Refund, rhs: Refund) -> Bool {
         return lhs.amount == rhs.amount &&
             lhs.reason == rhs.reason &&
+            lhs.apiRefund == rhs.apiRefund &&
             NSDictionary(dictionary: lhs.toDictionary()).isEqual(to: rhs.toDictionary()) &&
             (lhs.items != nil && rhs.items != nil) ? lhs.items!.count == rhs.items!.count &&
                 lhs.items!.sorted() == rhs.items!.sorted() : true
--- Networking/NetworkingTests/Models/RefundTests.swift
+++ Networking/NetworkingTests/Models/RefundTests.swift
@@ -8,7 +8,7 @@ final class RefundTests: XCTestCase {
 
     func testFullRefund(){
         
-        let dictExpected: [String: Any] = ["amount": "10.00", "reason": "Product No Longer Needed"]
+        let dictExpected: [String: Any] = ["amount": "10.00", "api_refund": true, "reason": "Product No Longer Needed"]
         
         let refund = Refund(amount: "10.00", reason: "Product No Longer Needed", items: nil)
         let refundDict = refund.toDictionary()
@@ -19,6 +19,7 @@ final class RefundTests: XCTestCase {
     func testPartialRefundForSingleProductIncludingTax(){
         let dictExpected: [String: Any] = ["amount": "10.00",
                                            "reason": "Product No Longer Needed",
+                                           "api_refund": true,
                                            "line_items": ["123": ["qty": 1,
                                                                   "refund_total": "8.00",
                                                                   "refund_tax":["789":"2.00"]]]]
@@ -32,6 +33,7 @@ final class RefundTests: XCTestCase {
     
     func testPartialRefundForSingleProductOnlyTax(){
         let dictExpected: [String: Any] = ["amount": "2.00",
+                                           "api_refund": true,
                                            "line_items": ["123": ["qty": 1,
                                                                   "refund_tax":["789":"2.00"]]]]
         
@@ -44,12 +46,13 @@ final class RefundTests: XCTestCase {
     
     func testPartialRefundForSingleProductExcludingTax(){
         let dictExpected: [String: Any] = ["amount": "10.00",
+                                           "api_refund": false,
                                            "reason": "Product No Longer Needed",
                                            "line_items": ["123": ["qty": 1,
                                                                   "refund_total": "8.00"]]]
         
         let itemRefund = LineItemRefund(itemID: "123", quantity: 1, refundTotal: "8.00", refundTax: nil)
-        let refund = Refund(amount: "10.00", reason: "Product No Longer Needed", items: [itemRefund])
+        let refund = Refund(amount: "10.00", reason: "Product No Longer Needed", apiRefund: false, items: [itemRefund])
         let refundDict = refund.toDictionary()
         
         XCTAssert(NSDictionary(dictionary: dictExpected).isEqual(to: refundDict))

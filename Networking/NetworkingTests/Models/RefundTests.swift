761490d01ce789d14b523da9742330e059c0f9aa
Paolo Musolino <info@codeido.com>
2019-08-18 21:10:17 +0200

    Fix spaces in RefundTests

--- Networking/NetworkingTests/Models/RefundTests.swift
+++ Networking/NetworkingTests/Models/RefundTests.swift
@@ -6,55 +6,55 @@ import XCTest
 ///
 final class RefundTests: XCTestCase {
 
-    func testFullRefund(){
-        
+    func testFullRefund() {
+
         let dictExpected: [String: Any] = ["amount": "10.00", "api_refund": true, "reason": "Product No Longer Needed"]
-        
+
         let refund = Refund(amount: "10.00", reason: "Product No Longer Needed", items: nil)
         let refundDict = refund.toDictionary()
-        
+
         XCTAssert(NSDictionary(dictionary: dictExpected).isEqual(to: refundDict))
     }
 
-    func testPartialRefundForSingleProductIncludingTax(){
+    func testPartialRefundForSingleProductIncludingTax() {
         let dictExpected: [String: Any] = ["amount": "10.00",
                                            "reason": "Product No Longer Needed",
                                            "api_refund": true,
                                            "line_items": ["123": ["qty": 1,
                                                                   "refund_total": "8.00",
-                                                                  "refund_tax":["789":"2.00"]]]]
-        
+                                                                  "refund_tax": ["789": "2.00"]]]]
+
         let itemRefund = LineItemRefund(itemID: "123", quantity: 1, refundTotal: "8.00", refundTax: [TaxRefund(taxIDLineItem: "789", amount: "2.00")])
         let refund = Refund(amount: "10.00", reason: "Product No Longer Needed", items: [itemRefund])
         let refundDict = refund.toDictionary()
-        
+
         XCTAssert(NSDictionary(dictionary: dictExpected).isEqual(to: refundDict))
     }
-    
-    func testPartialRefundForSingleProductOnlyTax(){
+
+    func testPartialRefundForSingleProductOnlyTax() {
         let dictExpected: [String: Any] = ["amount": "2.00",
                                            "api_refund": true,
                                            "line_items": ["123": ["qty": 1,
-                                                                  "refund_tax":["789":"2.00"]]]]
-        
+                                                                  "refund_tax": ["789": "2.00"]]]]
+
         let itemRefund = LineItemRefund(itemID: "123", quantity: 1, refundTotal: nil, refundTax: [TaxRefund(taxIDLineItem: "789", amount: "2.00")])
         let refund = Refund(amount: "2.00", reason: nil, items: [itemRefund])
         let refundDict = refund.toDictionary()
-        
+
         XCTAssert(NSDictionary(dictionary: dictExpected).isEqual(to: refundDict))
     }
-    
-    func testPartialRefundForSingleProductExcludingTax(){
+
+    func testPartialRefundForSingleProductExcludingTax() {
         let dictExpected: [String: Any] = ["amount": "10.00",
                                            "api_refund": false,
                                            "reason": "Product No Longer Needed",
                                            "line_items": ["123": ["qty": 1,
                                                                   "refund_total": "8.00"]]]
-        
+
         let itemRefund = LineItemRefund(itemID: "123", quantity: 1, refundTotal: "8.00", refundTax: nil)
         let refund = Refund(amount: "10.00", reason: "Product No Longer Needed", apiRefund: false, items: [itemRefund])
         let refundDict = refund.toDictionary()
-        
+
         XCTAssert(NSDictionary(dictionary: dictExpected).isEqual(to: refundDict))
     }
 }

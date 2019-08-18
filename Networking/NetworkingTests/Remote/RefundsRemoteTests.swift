4b2a2e6911c265ea3daddff7563c61307d1d9110
Paolo Musolino <info@codeido.com>
2019-08-18 21:47:38 +0200

    Tests for createRefund() method

--- Networking/NetworkingTests/Remote/RefundsRemoteTests.swift
+++ Networking/NetworkingTests/Remote/RefundsRemoteTests.swift
@@ -58,4 +58,39 @@ final class RefundsRemoteTests: XCTestCase {
         wait(for: [expectation], timeout: Constants.expectationTimeout)
     }
 
+    ///Verifies that createRefund properly parses the `order-refund` sample response.
+    ///
+    func testPartialRefundForSingleProductIncludingTax() {
+        let remote = RefundsRemote(network: network)
+        let expectation = self.expectation(description: "Partial refund for single product including tax")
+
+        network.simulateResponse(requestUrlSuffix: "orders/\(sampleOrderID)/refunds", filename: "order-refund")
+        
+        let itemRefund = LineItemRefund(itemID: "123", quantity: 1, refundTotal: "8.00", refundTax: [TaxRefund(taxIDLineItem: "789", amount: "2.00")])
+        let refund = Refund(amount: "10.00", reason: "Product No Longer Needed", items: [itemRefund])
+        remote.createRefund(for: sampleSiteID, by: sampleOrderID, refund: refund) { (orderRefund, error) in
+            XCTAssertNil(error)
+            XCTAssertNotNil(orderRefund)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+    
+    /// Verifies that createRefund properly relays Networking Layer errors.
+    ///
+    func testPartialRefundForSingleProductIncludingTaxRelaysNetwokingErrors() {
+        let remote = RefundsRemote(network: network)
+        let expectation = self.expectation(description: "Partial refund for single product including tax")
+        
+        let itemRefund = LineItemRefund(itemID: "123", quantity: 1, refundTotal: "8.00", refundTax: [TaxRefund(taxIDLineItem: "789", amount: "2.00")])
+        let refund = Refund(amount: "10.00", reason: "Product No Longer Needed", items: [itemRefund])
+        remote.createRefund(for: sampleSiteID, by: sampleOrderID, refund: refund) { (orderRefund, error) in
+            XCTAssertNil(orderRefund)
+            XCTAssertNotNil(error)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
 }

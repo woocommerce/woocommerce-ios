ca30fb9ff3f31bd2bdb5f0511b037ec8c32640eb
Paolo Musolino <info@codeido.com>
2019-08-20 01:04:34 +0200

    Extracted the creation of the expectation dictionary for RefundTests in private methods.

--- Networking/NetworkingTests/Models/RefundTests.swift
+++ Networking/NetworkingTests/Models/RefundTests.swift
@@ -7,45 +7,36 @@ import XCTest
 final class RefundTests: XCTestCase {
 
     func testFullRefund() {
-
-        let dictExpected: [String: Any] = ["amount": "10.00", "api_refund": true, "reason": "Product No Longer Needed"]
-
+        let expectation = dictionaryFullRefund()
+        
         let refund = Refund(amount: "10.00", reason: "Product No Longer Needed", items: nil)
         let refundDict = refund.toDictionary()
 
-        XCTAssert(NSDictionary(dictionary: dictExpected).isEqual(to: refundDict))
+        XCTAssert(NSDictionary(dictionary: expectation).isEqual(to: refundDict))
     }
 
     func testPartialRefundForSingleProductIncludingTax() {
-        let dictExpected: [String: Any] = ["amount": "10.00",
-                                           "reason": "Product No Longer Needed",
-                                           "api_refund": true,
-                                           "line_items": ["123": ["qty": 1,
-                                                                  "refund_total": "8.00",
-                                                                  "refund_tax": ["789": "2.00"]]]]
+        let expectation = dictionaryRefundForSingleProductIncludingTax()
 
         let itemRefund = LineItemRefund(itemID: "123", quantity: 1, refundTotal: "8.00", refundTax: [TaxRefund(taxIDLineItem: "789", amount: "2.00")])
         let refund = Refund(amount: "10.00", reason: "Product No Longer Needed", items: [itemRefund])
         let refundDict = refund.toDictionary()
 
-        XCTAssert(NSDictionary(dictionary: dictExpected).isEqual(to: refundDict))
+        XCTAssert(NSDictionary(dictionary: expectation).isEqual(to: refundDict))
     }
 
     func testPartialRefundForSingleProductOnlyTax() {
-        let dictExpected: [String: Any] = ["amount": "2.00",
-                                           "api_refund": true,
-                                           "line_items": ["123": ["qty": 1,
-                                                                  "refund_tax": ["789": "2.00"]]]]
+        let expectation: [String: Any] = dictionaryPartialRefundForSingleProductOnlyTax()
 
         let itemRefund = LineItemRefund(itemID: "123", quantity: 1, refundTotal: nil, refundTax: [TaxRefund(taxIDLineItem: "789", amount: "2.00")])
         let refund = Refund(amount: "2.00", reason: nil, items: [itemRefund])
         let refundDict = refund.toDictionary()
-
-        XCTAssert(NSDictionary(dictionary: dictExpected).isEqual(to: refundDict))
+        
+        XCTAssert(NSDictionary(dictionary: expectation).isEqual(to: refundDict))
     }
 
     func testPartialRefundForSingleProductExcludingTax() {
-        let dictExpected: [String: Any] = ["amount": "10.00",
+        let expectation: [String: Any] = ["amount": "10.00",
                                            "api_refund": false,
                                            "reason": "Product No Longer Needed",
                                            "line_items": ["123": ["qty": 1,
@@ -55,6 +46,47 @@ final class RefundTests: XCTestCase {
         let refund = Refund(amount: "10.00", reason: "Product No Longer Needed", apiRefund: false, items: [itemRefund])
         let refundDict = refund.toDictionary()
 
-        XCTAssert(NSDictionary(dictionary: dictExpected).isEqual(to: refundDict))
+        XCTAssert(NSDictionary(dictionary: expectation).isEqual(to: refundDict))
+    }
+}
+
+/// Private Methods.
+///
+private extension RefundTests {
+    
+    /// Returns the expected dictionary for a full refund.
+    ///
+    func dictionaryFullRefund() -> [String: Any]{
+        return ["amount": "10.00", "api_refund": true, "reason": "Product No Longer Needed"]
+    }
+    
+    /// Returns the expected dictionary for a refund for single product including taxes.
+    ///
+    func dictionaryRefundForSingleProductIncludingTax() -> [String: Any]{
+        return ["amount": "10.00",
+                "reason": "Product No Longer Needed",
+                "api_refund": true,
+                "line_items": ["123": ["qty": 1,
+                                       "refund_total": "8.00",
+                                       "refund_tax": ["789": "2.00"]]]]
+    }
+    
+    /// Returns the expected dictionary for a partial refund for single product including only taxes.
+    ///
+    func dictionaryPartialRefundForSingleProductOnlyTax() -> [String: Any]{
+        return ["amount": "2.00",
+                "api_refund": true,
+                "line_items": ["123": ["qty": 1,
+                                       "refund_tax": ["789": "2.00"]]]]
+    }
+    
+    /// Returns the expected dictionary for a partial refund for single product excluding taxes.
+    ///
+    func dictionaryPartialRefundForSingleProductExcludingTax() -> [String: Any]{
+        return ["amount": "10.00",
+                "api_refund": false,
+                "reason": "Product No Longer Needed",
+                "line_items": ["123": ["qty": 1,
+                                       "refund_total": "8.00"]]]
     }
 }

a7ab6188ab8b53d6bf1c2b091f49a6642a86945d
Jaclyn Chen <watertranquil@gmail.com>
2019-08-19 20:53:31 +0800

    Set timezone of the expected date formatter in tests.

--- WooCommerce/WooCommerceTests/ViewRelated/Dashboard/StoreStatsV4ChartAxisHelperTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Dashboard/StoreStatsV4ChartAxisHelperTests.swift
@@ -3,17 +3,6 @@ import XCTest
 
 class StoreStatsV4ChartAxisHelperTests: XCTestCase {
     private let helper = StoreStatsV4ChartAxisHelper()
-    private lazy var dayOfMonthFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.setLocalizedDateFormatFromTemplate("d")
-        return formatter
-    }()
-
-    private lazy var dayMonthFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.setLocalizedDateFormatFromTemplate("MMM d")
-        return formatter
-    }()
 
     func testDatesAcrossTwoMonths() {
         // GMT: Saturday, June 1, 2019 12:29:29 AM
@@ -23,12 +12,27 @@ class StoreStatsV4ChartAxisHelperTests: XCTestCase {
         // GMT: Thursday, August 1, 2019 12:29:29 AM
         let dateInAugust = Date(timeIntervalSince1970: 1561940969)
         let dates = [dateInAugust, dateInJune, secondDateInJune]
-        let text = helper.generateLabelText(for: dates, timeRange: .thisWeek, siteTimezone: TimeZone(identifier: "GMT")!)
+        let timezone = TimeZone(identifier: "GMT")!
+        let text = helper.generateLabelText(for: dates, timeRange: .thisWeek, siteTimezone: timezone)
         let textOfDateInAugust = text[0]
         let textOfFirstDateInJune = text[1]
         let textOfSecondDateInJune = text[2]
-        XCTAssertEqual(textOfDateInAugust, dayMonthFormatter.string(from: dateInAugust))
-        XCTAssertEqual(textOfFirstDateInJune, dayMonthFormatter.string(from: dateInJune))
-        XCTAssertEqual(textOfSecondDateInJune, dayOfMonthFormatter.string(from: secondDateInJune))
+        XCTAssertEqual(textOfDateInAugust, dayMonthFormatter(timezone: timezone).string(from: dateInAugust))
+        XCTAssertEqual(textOfFirstDateInJune, dayMonthFormatter(timezone: timezone).string(from: dateInJune))
+        XCTAssertEqual(textOfSecondDateInJune, dayOfMonthFormatter(timezone: timezone).string(from: secondDateInJune))
+    }
+
+    private func dayOfMonthFormatter(timezone: TimeZone) -> DateFormatter {
+        let formatter = DateFormatter()
+        formatter.setLocalizedDateFormatFromTemplate("d")
+        formatter.timeZone = timezone
+        return formatter
+    }
+
+    private func dayMonthFormatter(timezone: TimeZone) -> DateFormatter {
+        let formatter = DateFormatter()
+        formatter.setLocalizedDateFormatFromTemplate("MMM d")
+        formatter.timeZone = timezone
+        return formatter
     }
 }

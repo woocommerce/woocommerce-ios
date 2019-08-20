f3e41d67b61cde6e7002ae9afa47deef72e6f6ca
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-08-20 14:57:57 +0800

    Add a test for loading all reviews (happy path )

---
+++ Networking/NetworkingTests/Remote/ProductReviewsRemoteTests.swift
@@ -0,0 +1,47 @@
+import XCTest
+@testable import Networking
+
+
+/// ProductReviewsRemoteTests
+///
+final class ProductReviewsRemoteTests: XCTestCase {
+
+    /// Dummy Network Wrapper
+    ///
+    let network = MockupNetwork()
+
+    /// Dummy Site ID
+    ///
+    let sampleSiteID = 1234
+
+    /// Dummy Product ID
+    ///
+    let sampleProductID = 282
+
+    /// Repeat always!
+    ///
+    override func setUp() {
+        network.removeAllSimulatedResponses()
+    }
+
+    // MARK: - Load all product reviews tests
+
+    /// Verifies that loadAllProductReviews properly parses the `reviews-all` sample response.
+    ///
+
+    func testLoadAllProductReviewsProperlyReturnsParsedProductReviews() {
+        let remote = ProductReviewsRemote(network: network)
+        let expectation = self.expectation(description: "Load All Product Reviews")
+
+        network.simulateResponse(requestUrlSuffix: "products/reviews", filename: "reviews-all")
+
+        remote.loadAllProductReviews(for: sampleSiteID) { reviews, error in
+            XCTAssertNil(error)
+            XCTAssertNotNil(reviews)
+            XCTAssertEqual(reviews?.count, 2)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+}

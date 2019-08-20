b745274b2f56b71b304603429d4a11a339af8678
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-08-21 06:00:49 +0800

    Guard against running outside the tests in the setters

--- WooCommerce/Classes/ServiceLocator/ServiceLocator.swift
+++ WooCommerce/Classes/ServiceLocator/ServiceLocator.swift
@@ -7,7 +7,7 @@ final class ServiceLocator {
     private static var _analytics: Analytics = WooAnalytics(analyticsProvider: TracksProvider())
 
     /// Provides the access point to the analytics.
-    /// - Returns: An iumplementation of the Analytics protocol. It defaults to WooAnalytics
+    /// - Returns: An implementation of the Analytics protocol. It defaults to WooAnalytics
     static var analytics: Analytics {
         return _analytics
     }
@@ -19,6 +19,17 @@ final class ServiceLocator {
 /// The setters declared in this extension are meant to be used only from the test bundle
 extension ServiceLocator {
     static func setAnalytics(_ mock: Analytics) {
+        guard isRunningTests() else {
+            return
+        }
+
         _analytics = mock
+    }
+}
+
+
+private extension ServiceLocator {
+    static func isRunningTests() -> Bool {
+        return NSClassFromString("XCTestCase") != nil
     }
 }

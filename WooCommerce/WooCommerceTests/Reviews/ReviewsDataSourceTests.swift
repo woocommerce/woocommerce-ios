351b90ce9fb5031c6eccd01462d515618f0ade71
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-09-03 14:58:00 +0800

    Add tests to the dataSource

---
+++ WooCommerce/WooCommerceTests/Reviews/ReviewsDataSourceTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+@testable import WooCommerce
+
+final class ReviewsDataSourceTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}

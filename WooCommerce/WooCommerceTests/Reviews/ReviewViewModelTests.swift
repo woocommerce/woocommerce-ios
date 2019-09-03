85383afe4e62e26969c0ca42168333b2bc7507f4
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-09-03 14:19:10 +0800

    Test Product name is included

--- WooCommerce/WooCommerceTests/Reviews/ReviewViewModelTests.swift
+++ WooCommerce/WooCommerceTests/Reviews/ReviewViewModelTests.swift
@@ -6,28 +6,40 @@ final class ReviewViewModelTests: XCTestCase {
     private let mocks = MockReviews()
     private var subject: ReviewViewModel!
     private var review: ProductReview!
+    private var product: Product!
 
     override func setUp() {
         super.setUp()
         review = mocks.review()
-        subject = ReviewViewModel(review: review, product: nil)
+        product = mocks.product()
+        subject = ReviewViewModel(review: review, product: product)
     }
 
     override func tearDown() {
         subject = nil
         review = nil
+        product = nil
         super.tearDown()
     }
 
-    func testReviewViewModelReturnsSubject() {
-        XCTAssertEqual(subject.subject, subjectWithoutProduct())
+    func testReviewViewModelReturnsSubjectWithoutProductNameWhenProductIsNil() {
+        let viewModel = ReviewViewModel(review: review, product: nil)
+        XCTAssertEqual(viewModel.subject, reviewWithoutProduct())
+    }
+
+    func testReviewViewModelReturnsSubjectWithProductName() {
+        XCTAssertEqual(subject.subject, reviewWithProduct())
     }
 }
 
 
 private extension ReviewViewModelTests {
-    private func subjectWithoutProduct() -> String {
+    private func reviewWithoutProduct() -> String {
         return String(format: Strings.subjectFormat, mocks.reviewer, "")
+    }
+
+    private func reviewWithProduct() -> String {
+        return String(format: Strings.subjectFormat, mocks.reviewer, mocks.productName)
     }
 
     enum Strings {

ded1d4515ca43f1dc7789506c4bd7b7b23715fef
Jaclyn Chen <watertranquil@gmail.com>
2019-09-07 15:33:55 +0800

    Create product cell details based on status, stock status and variations.

--- WooCommerce/Classes/ViewRelated/Products/View Models/ProductsTabProductViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/View Models/ProductsTabProductViewModel.swift
@@ -3,8 +3,58 @@ import Yosemite
 
 private extension Product {
     func createDetailsAttributedString() -> NSAttributedString {
-        let attributedString = NSAttributedString(string: productStatus.description, attributes: [.foregroundColor: StyleManager.defaultTextColor])
+        let statusText = createStatusText()
+        let stockText = createStockText()
+        let variationsText = createVariationsText()
+
+        let detailsText = [statusText, stockText, variationsText]
+            .compactMap({ $0 })
+            .joined(separator: " • ")
+
+        let attributedString = NSMutableAttributedString(string: detailsText,
+                                                         attributes: [
+                                                            .foregroundColor: StyleManager.wooGreyMid,
+                                                            .font: StyleManager.footerLabelFont
+            ])
+        if let statusText = statusText {
+            attributedString.addAttributes([.foregroundColor : StyleManager.highlightTextColor],
+                                           range: NSRange(location: 0, length: statusText.count))
+        }
         return attributedString
+    }
+
+    func createStatusText() -> String? {
+        switch productStatus {
+        case .pending, .draft:
+            return productStatus.description
+        default:
+            return nil
+        }
+    }
+
+    func createStockText() -> String? {
+        switch productStockStatus {
+        case .inStock:
+            if let stockQuantity = stockQuantity {
+                let format = NSLocalizedString("%ld in stock", comment: "Label about product's inventory stock status shown on Products tab")
+                return String.localizedStringWithFormat(format, stockQuantity)
+            } else {
+                return NSLocalizedString("In stock", comment: "Label about product's inventory stock status shown on Products tab")
+            }
+        default:
+            return productStockStatus.description
+        }
+    }
+
+    func createVariationsText() -> String? {
+        guard !variations.isEmpty else {
+            return nil
+        }
+        let numberOfVariations = variations.count
+        let singularFormat = NSLocalizedString("%ld variant", comment: "Label about one product variation shown on Products tab")
+        let pluralFormat = NSLocalizedString("%ld variants", comment: "Label about number of variations shown on Products tab")
+        let format = String.pluralize(numberOfVariations, singular: singularFormat, plural: pluralFormat)
+        return String.localizedStringWithFormat(format, numberOfVariations)
     }
 }
 

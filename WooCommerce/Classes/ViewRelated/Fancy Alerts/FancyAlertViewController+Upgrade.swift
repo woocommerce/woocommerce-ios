233236763bd44463e4b50ea68837c275b3d250e1
Lorenzo Mattei <lore.mattei@gmail.com>
2019-09-25 10:28:40 +0200

    Make Hound happy

--- WooCommerce/Classes/ViewRelated/Fancy Alerts/FancyAlertViewController+Upgrade.swift
+++ WooCommerce/Classes/ViewRelated/Fancy Alerts/FancyAlertViewController+Upgrade.swift
@@ -93,7 +93,8 @@ private extension FancyAlertViewController {
         )
 
         // Body
-        static let bodyText = NSLocalizedString("This app requires that you install WooCommerce 3.5 on your server, and won't work properly without it. Update as soon as possible to continue using this app.",
+        static let bodyText = NSLocalizedString("This app requires that you install WooCommerce 3.5 on your server, and won't work properly without it." +
+            " Update as soon as possible to continue using this app.",
             comment: "Body text of alert warning users to upgrade to WC 3.5."
         )
 

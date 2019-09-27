d5805767775a86e97241374f5e12bcf063ebeb4d
Thuy.Copeland <thuycopeland@gmail.com>
2019-09-27 16:27:57 -0500

    Remove the siteID, it's not needed

--- Yosemite/Yosemite/Model/Storage/OrderRefundCondensed+ReadOnlyConvertible.swift
+++ Yosemite/Yosemite/Model/Storage/OrderRefundCondensed+ReadOnlyConvertible.swift
@@ -9,17 +9,15 @@ extension Storage.OrderRefundCondensed: ReadOnlyConvertible {
     /// Updates the Storage.OrderRefundCondensed with the ReadOnly.
     ///
     public func update(with orderRefundCondensed: Yosemite.OrderRefundCondensed) {
-        siteID = Int64(orderRefundCondensed.siteID)
-        refundID = Int64(orderRefundCondensed.orderID)
+        refundID = Int64(orderRefundCondensed.refundID)
         reason = orderRefundCondensed.reason
         total = orderRefundCondensed.total
     }
 
     /// Returns a ReadOnly version of the receiver.
     ///
-    public func toReadOnly() -> Yosemite.Order {
-        return OrderRefundCondensed(siteID: Int(siteID),
-                                    refundID: Int(refundID),
+    public func toReadOnly() -> Yosemite.OrderRefundCondensed {
+        return OrderRefundCondensed(refundID: Int(refundID),
                                     reason: reason ?? "",
                                     total: total ?? "")
     }

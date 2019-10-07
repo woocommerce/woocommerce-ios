25f909fa9e20a3fa3c471e6def93e674629623dd
Thuy.Copeland <thuycopeland@gmail.com>
2019-10-07 12:03:59 -0500

    Restore Paolo's RefundMapper

---
+++ Networking/Networking/Mapper/RefundMapper.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+
+/// Mapper: Refund
+///
+struct RefundMapper: Mapper {
+
+    /// Site Identifier associated to the order refund that will be parsed.
+    ///
+    /// We're injecting this field via `JSONDecoder.userInfo` because SiteID is not returned in any of the Order Endpoints.
+    ///
+    let siteID: Int
+
+
+    /// (Attempts) to convert a dictionary into a single Refund.
+    ///
+    func map(response: Data) throws -> Refund {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(DateFormatter.Defaults.dateTimeFormatter)
+        decoder.userInfo = [
+            .siteID: siteID
+        ]
+
+        return try decoder.decode(RefundEnvelope.self, from: response).refund
+    }
+
+    /// (Attempts) to encode a Refund object into JSONEncoded data.
+    ///
+    func map(refund: Refund) throws -> Data {
+        let encoder = JSONEncoder()
+
+        return try encoder.encode(refund)
+    }
+}
+
+
+/// RefundEnvelope Disposable Entity
+///
+/// `Load Refund` endpoint returns the requested order refund document in the `data` key. This entity
+/// allows us to do parse all the things with JSONDecoder.
+///
+private struct RefundEnvelope: Decodable {
+    let refund: Refund
+
+    private enum CodingKeys: String, CodingKey {
+        case refund = "data"
+    }
+}

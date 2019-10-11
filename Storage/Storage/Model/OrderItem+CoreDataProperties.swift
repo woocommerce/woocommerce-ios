dffcd5348ee3d022d30cdbdc4ebf4cd66ba7d1a5
Thuy.Copeland <thuycopeland@gmail.com>
2019-10-11 12:02:49 -0500

    Generate CoreData classes and properties

--- Storage/Storage/Model/OrderItem+CoreDataProperties.swift
+++ Storage/Storage/Model/OrderItem+CoreDataProperties.swift
@@ -10,9 +10,9 @@ extension OrderItem {
 
     @NSManaged public var itemID: Int64
     @NSManaged public var name: String?
-    @NSManaged public var quantity: NSDecimalNumber
     @NSManaged public var price: NSDecimalNumber?
     @NSManaged public var productID: Int64
+    @NSManaged public var quantity: NSDecimalNumber
     @NSManaged public var sku: String?
     @NSManaged public var subtotal: String?
     @NSManaged public var subtotalTax: String?
@@ -21,4 +21,23 @@ extension OrderItem {
     @NSManaged public var totalTax: String?
     @NSManaged public var variationID: Int64
     @NSManaged public var order: Order
+    @NSManaged public var tax: Set<OrderItemTax>?
+
+}
+
+// MARK: Generated accessors for tax
+extension OrderItem {
+
+    @objc(addTaxObject:)
+    @NSManaged public func addToTax(_ value: OrderItemTax)
+
+    @objc(removeTaxObject:)
+    @NSManaged public func removeFromTax(_ value: OrderItemTax)
+
+    @objc(addTax:)
+    @NSManaged public func addToTax(_ values: NSSet)
+
+    @objc(removeTax:)
+    @NSManaged public func removeFromTax(_ values: NSSet)
+
 }

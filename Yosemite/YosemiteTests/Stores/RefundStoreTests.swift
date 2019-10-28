e83f0e815b34375da781db19291abd4521e7282e
Thuy.Copeland <thuycopeland@gmail.com>
2019-10-28 14:31:41 -0500

    RefundStoreTests: add unit test to check all Refund fields are persisted

--- Yosemite/YosemiteTests/Stores/RefundStoreTests.swift
+++ Yosemite/YosemiteTests/Stores/RefundStoreTests.swift
@@ -34,6 +34,10 @@ class RefundStoreTests: XCTestCase {
     ///
     private let sampleOrderID = 560
 
+    /// Testing RefundID
+    ///
+    private let sampleRefundID = 590
+
     /// Testing Page Number
     ///
     private let defaultPageNumber = 1
@@ -78,5 +82,82 @@ class RefundStoreTests: XCTestCase {
 
         refundStore.onAction(action)
         wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+
+    /// Verifies that `RefundAction.synchronizeRefunds` effectively persists all of the refund fields
+    /// correctly across the related `Refund` entities (OrderItemRefund, for example).
+    ///
+    func testRetrieveRefundsEffectivelyPersistsRefundFieldsAndRelatedObjects() {
+        let expectation = self.expectation(description: "Persist refunds list")
+        let refundStore = RefundStore(dispatcher: dispatcher, storageManager: storageManager, network: network)
+        let remoteRefund = sampleRefund()
+
+        network.simulateResponse(requestUrlSuffix: "refunds", filename: "refunds-all")
+        XCTAssertEqual(viewStorage.countObjects(ofType: Storage.Refund.self), 0)
+
+        let action = RefundAction.synchronizeRefunds(siteID: sampleSiteID, orderID: sampleOrderID, pageNumber: defaultPageNumber, pageSize: defaultPageSize) { error in
+            XCTAssertNil(error)
+
+            let storedRefund = self.viewStorage.loadRefund(siteID: self.sampleSiteID, orderID: self.sampleOrderID, refundID: self.sampleRefundID)
+            let readOnlyStoredRefund = storedRefund?.toReadOnly()
+            XCTAssertNotNil(storedRefund)
+            XCTAssertNotNil(readOnlyStoredRefund)
+            XCTAssertEqual(readOnlyStoredRefund, remoteRefund)
+
+            expectation.fulfill()
+        }
+
+        refundStore.onAction(action)
+        wait(for: [expectation], timeout: Constants.expectationTimeout)
+    }
+}
+
+
+// MARK: - Private Helpers
+//
+private extension RefundStoreTests {
+
+    /// Generate a sample Refund
+    ///
+    func sampleRefund(_ siteID: Int? = nil) -> Networking.Refund {
+        let testSiteID = siteID ?? sampleSiteID
+        let testDate = date(with: "2019-10-09T16:18:23")
+        return Refund(refundID: sampleRefundID,
+                      orderID: sampleOrderID,
+                      siteID: testSiteID,
+                      dateCreated: testDate,
+                      amount: "18.00",
+                      reason: "Only 1 black hoodie left. Inventory count was off. My bad!",
+                      refundedByUserID: 1,
+                      isAutomated: true,
+                      createAutomated: false,
+                      items: [sampleOrderItem()])
+    }
+
+    /// Generate a sample OrderItem
+    ///
+    func sampleOrderItem() -> Networking.OrderItemRefund {
+        return OrderItemRefund(itemID: 73,
+                               name: "Ninja Silhouette",
+                               productID: 22,
+                               variationID: 0,
+                               quantity: -1,
+                               price: 18,
+                               sku: "T-SHIRT-NINJA-SILHOUETTE",
+                               subtotal: "-18.00",
+                               subtotalTax: "0.00",
+                               taxClass: "",
+                               taxes: [],
+                               total: "-18.00",
+                               totalTax: "0.00")
+    }
+
+    /// Format GMT string to Date type
+    ///
+    func date(with dateString: String) -> Date {
+        guard let date = DateFormatter.Defaults.dateTimeFormatter.date(from: dateString) else {
+            return Date()
+        }
+        return date
     }
 }

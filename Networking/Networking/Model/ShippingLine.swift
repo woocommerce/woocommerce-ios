6be756151df97e5a201291595f1544132e861de3
Paolo Musolino <info@codeido.com>
2019-11-05 11:04:41 +0100

    Refactor ShippingLine model properties

--- Networking/Networking/Model/ShippingLine.swift
+++ Networking/Networking/Model/ShippingLine.swift
@@ -4,23 +4,23 @@ import Foundation
 /// Represents a Shipping Line Entity.
 ///
 public struct ShippingLine: Decodable {
-    public let shippingId: Int
+    public let shippingID: Int
     public let methodTitle: String
-    public let methodId: String
+    public let methodID: String
     public let total: String
     public let totalTax: String
     
     /// Shipping Method struct initializer.
     ///
-    public init(shippingId: Int,
+    public init(shippingID: Int,
                 methodTitle: String,
-                methodId: String,
+                methodID: String,
                 total: String,
                 totalTax: String) {
         
-        self.shippingId = shippingId
+        self.shippingID = shippingID
         self.methodTitle = methodTitle
-        self.methodId = methodId
+        self.methodID = methodID
         self.total = total
         self.totalTax = totalTax
     }
@@ -31,15 +31,15 @@ public struct ShippingLine: Decodable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
-        let shippingId = try container.decode(Int.self, forKey: .shippingId)
+        let shippingID = try container.decode(Int.self, forKey: .shippingID)
         let methodTitle = try container.decode(String.self, forKey: .methodTitle)
-        let methodId = try container.decode(String.self, forKey: .methodId)
+        let methodID = try container.decode(String.self, forKey: .methodID)
         let total = try container.decode(String.self, forKey: .total)
         let totalTax = try container.decode(String.self, forKey: .totalTax)
         
-        self.init(shippingId: shippingId,
+        self.init(shippingID: shippingID,
                   methodTitle: methodTitle,
-                  methodId: methodId,
+                  methodID: methodID,
                   total: total,
                   totalTax: totalTax)
     }
@@ -51,9 +51,9 @@ public struct ShippingLine: Decodable {
 private extension ShippingLine {
     
     enum CodingKeys: String, CodingKey {
-        case shippingId            = "id"
+        case shippingID            = "id"
         case methodTitle           = "method_title"
-        case methodId              = "method_id"
+        case methodID              = "method_id"
         case total                 = "total"
         case totalTax              = "total_tax"
     }
@@ -64,16 +64,16 @@ private extension ShippingLine {
 //
 extension ShippingLine: Comparable {
     public static func == (lhs: ShippingLine, rhs: ShippingLine) -> Bool {
-        return lhs.shippingId == rhs.shippingId &&
+        return lhs.shippingID == rhs.shippingID &&
             lhs.methodTitle == rhs.methodTitle &&
-            lhs.methodId == rhs.methodId &&
+            lhs.methodID == rhs.methodID &&
             lhs.total == rhs.total &&
             lhs.totalTax == rhs.totalTax
     }
     
     public static func < (lhs: ShippingLine, rhs: ShippingLine) -> Bool {
-        return lhs.shippingId == rhs.shippingId &&
-            lhs.methodId == rhs.methodId &&
+        return lhs.shippingID == rhs.shippingID &&
+            lhs.methodID == rhs.methodID &&
             lhs.methodTitle < rhs.methodTitle
     }
 }

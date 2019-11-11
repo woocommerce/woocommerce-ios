9fd763eab2f9144f1b22d6eec9ebb80db467878b
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-11-11 11:27:14 +0800

    Start testing MurielColor
    And complete tests

---
+++ WooCommerce/WooCommerceTests/ViewRelated/MurielColorTests.swift
@@ -0,0 +1,55 @@
+import XCTest
+@testable import WooCommerce
+
+final class MurielColorTests: XCTestCase {
+    func testPinkColorMatchesAssetName() {
+        XCTAssertTrue(colorMatchesExpectation(color: MurielColor.pink, expectation: Expectations.pink))
+    }
+
+    func testWooCommercePurpleMatchesAssetName() {
+        XCTAssertTrue(colorMatchesExpectation(color: MurielColor.wooCommercePurple, expectation: Expectations.wooCommercePurple))
+    }
+
+    func testRedMatchesAssetName() {
+        XCTAssertTrue(colorMatchesExpectation(color: MurielColor.red, expectation: Expectations.red))
+    }
+
+    func testGrayMatchesAssetName() {
+        XCTAssertTrue(colorMatchesExpectation(color: MurielColor.gray, expectation: Expectations.gray))
+    }
+
+    func testBlueMatchesAssetName() {
+        XCTAssertTrue(colorMatchesExpectation(color: MurielColor.blue, expectation: Expectations.blue))
+    }
+
+    func testGreenMatchesAssetName() {
+        XCTAssertTrue(colorMatchesExpectation(color: MurielColor.green, expectation: Expectations.green))
+    }
+
+    func testWarningMatchesAssetName() {
+        XCTAssertTrue(colorMatchesExpectation(color: MurielColor.warning, expectation: Expectations.warning))
+    }
+
+    func testTextMatchesAssetName() {
+        XCTAssertTrue(colorMatchesExpectation(color: MurielColor.text, expectation: Expectations.text))
+    }
+
+    private func colorMatchesExpectation(color: MurielColor, expectation: String) -> Bool {
+        let assetName = color.assetName()
+
+        return assetName == expectation
+    }
+}
+
+extension MurielColorTests {
+    enum Expectations {
+        static let pink = "Pink50"
+        static let wooCommercePurple = "WooCommercePurple50"
+        static let red = "Red50"
+        static let gray = "Gray50"
+        static let blue = "Blue50"
+        static let green = "Green50"
+        static let warning = "Yellow50"
+        static let text = "Gray80"
+    }
+}

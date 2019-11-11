14e2e3e78347849d4d7d9f5b31b3b665d284543e
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-11-11 14:01:18 +0800

    Test all pure colors

--- WooCommerce/WooCommerceTests/ViewRelated/UIColor+Muriel-Tests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/UIColor+Muriel-Tests.swift
@@ -2,8 +2,48 @@ import XCTest
 @testable import WooCommerce
 
 final class UIColor_Muriel_Tests: XCTestCase {
+    func testBlueColorObtainedMatchesMurielSpec() {
+        let murielColor = MurielColor(name: .blue)
+        XCTAssertTrue(colorAssetObtainedMatchesMuriel(murielColor))
+    }
+
+    func testCeladonColorObtainedMatchesMurielSpec() {
+        let murielColor = MurielColor(name: .celadon)
+        XCTAssertTrue(colorAssetObtainedMatchesMuriel(murielColor))
+    }
+
     func testGrayColorObtainedMatchesMurielSpec() {
-        let murielColor = MurielColor.gray
+        let murielColor = MurielColor(name: .gray)
+        XCTAssertTrue(colorAssetObtainedMatchesMuriel(murielColor))
+    }
+
+    func testGreenColorObtainedMatchesMurielSpec() {
+        let murielColor = MurielColor(name: .green)
+        XCTAssertTrue(colorAssetObtainedMatchesMuriel(murielColor))
+    }
+
+    func testOrangeColorObtainedMatchesMurielSpec() {
+        let murielColor = MurielColor(name: .orange)
+        XCTAssertTrue(colorAssetObtainedMatchesMuriel(murielColor))
+    }
+
+    func testPinkColorObtainedMatchesMurielSpec() {
+        let murielColor = MurielColor(name: .pink)
+        XCTAssertTrue(colorAssetObtainedMatchesMuriel(murielColor))
+    }
+
+    func testPurpleColorObtainedMatchesMurielSpec() {
+        let murielColor = MurielColor(name: .purple)
+        XCTAssertTrue(colorAssetObtainedMatchesMuriel(murielColor))
+    }
+
+    func testYellowColorObtainedMatchesMurielSpec() {
+        let murielColor = MurielColor(name: .yellow)
+        XCTAssertTrue(colorAssetObtainedMatchesMuriel(murielColor))
+    }
+
+    func testWooCommercePurpleColorObtainedMatchesMurielSpec() {
+        let murielColor = MurielColor(name: .wooCommercePurple)
         XCTAssertTrue(colorAssetObtainedMatchesMuriel(murielColor))
     }
 

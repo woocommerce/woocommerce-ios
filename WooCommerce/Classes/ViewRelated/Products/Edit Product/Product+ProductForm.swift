13a6290faae119e4fed6f40c74a48be8e67d10e5
Jaclyn Chen <watertranquil@gmail.com>
2019-11-12 22:33:13 +0800

    Move HTML tags removal helper to `String+HTML` with basic tests.

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Product+ProductForm.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Product+ProductForm.swift
@@ -1,17 +1,10 @@
 import Yosemite
 
-extension String {
-    func HTMLTagsRemoved() -> String {
-        let string = replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
-        return string
-    }
-}
-
 extension Product {
     var singleLineFullDescription: String? {
         guard let description = fullDescription else {
             return nil
         }
-        return description.HTMLTagsRemoved()
+        return description.removedHTMLTags
     }
 }

45a83a103ce58f2da85eb0b8e4eaadabf39d8d4b
Thuy.Copeland <thuycopeland@gmail.com>
2019-11-15 09:06:22 -0600

    Create a new cell: TwoColumnHeadline *Attributed* Footnote cell
    Modifying the original TwoColumnHeadlineFootnote cell violates the Open / Closed principle.

---
+++ WooCommerce/Classes/ViewRelated/ReusableViews/TwoColumnHeadlineAttributedFootnoteTableViewCell.swift
@@ -0,0 +1,77 @@
+import UIKit
+
+
+/// Represents a cell with a two-column title "row"
+/// and an attributed string footnote "row" below the titles
+///
+final class TwoColumnHeadlineAttributedFootnoteTableViewCell: UITableViewCell {
+
+    /// We want this reusable cell to have a consistent appearance
+    /// everywhere it's used, so we closed access to the labels.
+    ///
+    @IBOutlet private weak var leftTitleLabel: UILabel!
+    @IBOutlet private weak var rightTitleLabel: UILabel!
+    @IBOutlet private weak var footnoteLabel: UILabel!
+
+    // Expose the properties of a label that should be available for configuration.
+
+    /// Left title label text
+    ///
+    var leftText: String? {
+        get {
+            return leftTitleLabel?.text
+        }
+        set {
+            leftTitleLabel?.text = newValue
+        }
+    }
+
+    /// Right title label text
+    ///
+    var rightText: String? {
+        get {
+            return rightTitleLabel?.text
+        }
+        set {
+            rightTitleLabel?.text = newValue
+        }
+    }
+
+    /// Footnote label text
+    ///
+    var footnoteAttributedText: NSAttributedString? {
+        get {
+            return footnoteLabel?.attributedText
+        }
+        set {
+            footnoteLabel?.attributedText = newValue
+        }
+    }
+
+    /// Cell setup
+    ///
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureBackground()
+        configureLabels()
+    }
+}
+
+// MARK: - Private Methods
+//
+private extension TwoColumnHeadlineAttributedFootnoteTableViewCell {
+
+    /// Setup: Cell background
+    ///
+    func configureBackground() {
+        applyDefaultBackgroundStyle()
+    }
+
+    /// Setup: Labels
+    ///
+    func configureLabels() {
+        leftTitleLabel.applyHeadlineStyle()
+        rightTitleLabel.applyHeadlineStyle()
+        footnoteLabel.applyFootnoteStyle()
+    }
+}

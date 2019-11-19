f7503508ac88f4b015ba53835f434efbba7faca4
Jaclyn Chen <watertranquil@gmail.com>
2019-11-19 12:13:49 +0800

    Use the new WC Analytics API namespace, and update the banner message when we revert from v4 to v3.

--- WooCommerce/Classes/ViewRelated/Dashboard/Factories/DashboardTopBannerFactory.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Factories/DashboardTopBannerFactory.swift
@@ -40,7 +40,7 @@ private extension DashboardTopBannerFactory {
 
     enum V4ToV3BannerConstants {
         static let title: String? = nil
-        static let info = NSLocalizedString("We have reverted to the old stats as we couldn’t find the WooCommerce Admin plugin",
+        static let info = NSLocalizedString("We have reverted to the old stats. To use the beta stats, please activate the WooCommerce Admin plugin with version 0.22 or higher",
                                             comment: "The info of the top banner on Dashboard that indicates new stats is unavailable")
         static let icon = UIImage.infoImage
         static let actionTitle = NSLocalizedString("Learn more",

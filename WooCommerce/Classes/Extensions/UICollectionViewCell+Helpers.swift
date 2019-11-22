b85eeeae5d760ad50bce66e0be65dcc3aa45740a
Paolo Musolino <info@codeido.com>
2019-11-22 11:44:13 +0100

    UICollectionViewCell helpers

---
+++ WooCommerce/Classes/Extensions/UICollectionViewCell+Helpers.swift
@@ -0,0 +1,20 @@
+import Foundation
+import UIKit
+
+
+/// UICollectionViewCell Helpers
+///
+extension UICollectionViewCell {
+
+    /// Returns a reuseIdentifier that matches the receiver's classname (non namespaced).
+    ///
+    class var reuseIdentifier: String {
+        return classNameWithoutNamespaces
+    }
+
+    /// Applies the default background color
+    ///
+    func applyDefaultBackgroundStyle() {
+        backgroundColor = StyleManager.wooWhite
+    }
+}

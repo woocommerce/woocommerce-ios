9961bdd4936119ac436928166b47ed98b3db9600
Paolo Musolino <info@codeido.com>
2019-11-22 15:52:49 +0100

    Improvements to collection view layout

--- WooCommerce/Classes/ViewRelated/Products/Cells/Product Images/ProductImagesFlowLayout.swift
+++ WooCommerce/Classes/ViewRelated/Products/Cells/Product Images/ProductImagesFlowLayout.swift
@@ -9,7 +9,8 @@ class ProductImagesFlowLayout: UICollectionViewFlowLayout {
         
         self.scrollDirection = .horizontal
         self.minimumInteritemSpacing = 12.0
-        self.minimumLineSpacing = 0.0
+        self.minimumLineSpacing = 12.0
         self.estimatedItemSize = CGSize(width: 128.0, height: 128.0)
+        self.sectionInset = UIEdgeInsets(top: 0.0, left: 12.0, bottom: 0.0, right: 0.0)
     }
 }

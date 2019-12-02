06bed1820bdea720c0188a10c7bf6fcd00e86d74
Thuy.Copeland <thuycopeland@gmail.com>
2019-12-02 12:54:56 -0600

    Code formatting to make Hound happy

--- WooCommerce/Classes/ViewRelated/Editor/FormatBar/FormatBarItemViewProperties.swift
+++ WooCommerce/Classes/ViewRelated/Editor/FormatBar/FormatBarItemViewProperties.swift
@@ -110,41 +110,59 @@ extension FormattingIdentifier: FormatBarItemViewProperties {
     var accessibilityHint: String {
         switch self {
         case .p:
-            return NSLocalizedString("Tap to toggle paragraph style", comment: "Accessibility hint for selecting paragraph style button on formatting toolbar.")
+            return NSLocalizedString("Tap to toggle paragraph style",
+                                     comment: "Accessibility hint for selecting paragraph style button on formatting toolbar.")
         case .bold:
-            return NSLocalizedString("Tap to toggle Bold text", comment: "Accessibility hint for bold button on formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Bold text",
+                                     comment: "Accessibility hint for bold button on formatting toolbar.")
         case .italic:
-            return NSLocalizedString("Tap to toggle Italic text", comment: "Accessibility hint for italic button on formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Italic text",
+                                     comment: "Accessibility hint for italic button on formatting toolbar.")
         case .underline:
-            return NSLocalizedString("Tap to toggle Underline", comment: "Accessibility hint for underline button on formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Underline",
+                                     comment: "Accessibility hint for underline button on formatting toolbar.")
         case .strikethrough:
-            return NSLocalizedString("Tap to toggle Strike Through", comment: "Accessibility hint for strikethrough button on formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Strike Through",
+                                     comment: "Accessibility hint for strikethrough button on formatting toolbar.")
         case .blockquote:
-            return NSLocalizedString("Tap to toggle Block Quote", comment: "Accessibility hint for block quote button on formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Block Quote",
+                                     comment: "Accessibility hint for block quote button on formatting toolbar.")
         case .orderedlist:
-            return NSLocalizedString("Tap to toggle Ordered List", comment: "Accessibility hint for Ordered list button on formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Ordered List",
+                                     comment: "Accessibility hint for Ordered list button on formatting toolbar.")
         case .unorderedlist:
-            return NSLocalizedString("Tap to toggle Unordered List", comment: "Accessibility hint for unordered list button on formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Unordered List",
+                                     comment: "Accessibility hint for unordered list button on formatting toolbar.")
         case .link:
-            return NSLocalizedString("Tap to insert a Link", comment: "Accessibility hint for insert link button on formatting toolbar.")
+            return NSLocalizedString("Tap to insert a Link",
+                                     comment: "Accessibility hint for insert link button on formatting toolbar.")
         case .horizontalruler:
-            return NSLocalizedString("Tap to insert a Horizontal Ruler", comment: "Accessibility hint for insert horizontal ruler button on formatting toolbar.")
+            return NSLocalizedString("Tap to insert a Horizontal Ruler",
+                                     comment: "Accessibility hint for insert horizontal ruler button on formatting toolbar.")
         case .sourcecode:
-            return NSLocalizedString("Tap to toggle HTML mode", comment: "Accessibility hint for HTML button on formatting toolbar.")
+            return NSLocalizedString("Tap to toggle HTML mode",
+                                     comment: "Accessibility hint for HTML button on formatting toolbar.")
         case .more:
-            return NSLocalizedString("Tap for more formatting options", comment: "Accessibility hint for the More button on formatting toolbar.")
+            return NSLocalizedString("Tap for more formatting options",
+                                     comment: "Accessibility hint for the More button on formatting toolbar.")
         case .header1:
-            return NSLocalizedString("Tap to toggle Header 1", comment: "Accessibility hint for selecting h1 paragraph style button on the formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Header 1",
+                                     comment: "Accessibility hint for selecting h1 paragraph style button on the formatting toolbar.")
         case .header2:
-            return NSLocalizedString("Tap to toggle Header 2", comment: "Accessibility hint for selecting h2 paragraph style button on the formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Header 2",
+                                     comment: "Accessibility hint for selecting h2 paragraph style button on the formatting toolbar.")
         case .header3:
-            return NSLocalizedString("Tap to toggle Header 3", comment: "Accessibility hint for selecting h3 paragraph style button on the formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Header 3",
+                                     comment: "Accessibility hint for selecting h3 paragraph style button on the formatting toolbar.")
         case .header4:
-            return NSLocalizedString("Tap to toggle Header 4", comment: "Accessibility hint for selecting h4 paragraph style button on the formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Header 4",
+                                     comment: "Accessibility hint for selecting h4 paragraph style button on the formatting toolbar.")
         case .header5:
-            return NSLocalizedString("Tap to toggle Header 5", comment: "Accessibility hint for selecting h5 paragraph style button on the formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Header 5",
+                                     comment: "Accessibility hint for selecting h5 paragraph style button on the formatting toolbar.")
         case .header6:
-            return NSLocalizedString("Tap to toggle Header 6", comment: "Accessibility hint for selecting h6 paragraph style button on the formatting toolbar.")
+            return NSLocalizedString("Tap to toggle Header 6",
+                                     comment: "Accessibility hint for selecting h6 paragraph style button on the formatting toolbar.")
         default:
             return ""
         }

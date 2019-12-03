3892083695395bb2e5a1c6953de7c5abc414f1d9
Jaclyn Chen <watertranquil@gmail.com>
2019-12-03 18:05:57 +0800

    Add a relationship between `Product` and `ProductShippingClass`.

--- Storage/Storage/Model/ProductShippingClass+CoreDataProperties.swift
+++ Storage/Storage/Model/ProductShippingClass+CoreDataProperties.swift
@@ -14,5 +14,23 @@ extension ProductShippingClass {
     @NSManaged public var descriptionHTML: String?
     @NSManaged public var count: Int64
     @NSManaged public var siteID: Int64
+    @NSManaged public var products: Set<Product>?
+
+}
+
+// MARK: Generated accessors for products
+extension ProductShippingClass {
+
+    @objc(addProductsObject:)
+    @NSManaged public func addToProducts(_ value: Product)
+
+    @objc(removeProductsObject:)
+    @NSManaged public func removeFromProducts(_ value: Product)
+
+    @objc(addProducts:)
+    @NSManaged public func addToProducts(_ values: NSSet)
+
+    @objc(removeProducts:)
+    @NSManaged public func removeFromProducts(_ values: NSSet)
 
 }

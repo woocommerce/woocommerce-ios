e5e7982c5fd9e1b2995989aec70e646351a68cd9
Jaclyn Chen <watertranquil@gmail.com>
2019-12-04 14:44:48 +0800

    Create `PaginatedListSelectorViewController` as a generic paginated list selector view controller.

---
+++ WooCommerce/Classes/ViewRelated/ListSelector/PaginatedListSelectorViewController.swift
@@ -0,0 +1,352 @@
+import UIKit
+import WordPressUI
+import Yosemite
+
+protocol PaginatedListSelectorDataSource {
+    associatedtype StorageModel: ResultsControllerMutableType
+
+    var selected: StorageModel.ReadOnlyType? { get }
+
+    func createResultsController() -> ResultsController<StorageModel>
+
+    mutating func handleSelectedChange(selected: StorageModel.ReadOnlyType)
+
+    func configureCell(cell: BasicTableViewCell, model: StorageModel.ReadOnlyType)
+
+    func sync(pageNumber: Int, pageSize: Int, onCompletion: ((Bool) -> Void)?)
+}
+
+final class PaginatedListSelectorViewController<DataSource: PaginatedListSelectorDataSource, Model, StorageModel>:
+UIViewController, UITableViewDataSource, UITableViewDelegate, SyncingCoordinatorDelegate
+where DataSource.StorageModel == StorageModel, Model == DataSource.StorageModel.ReadOnlyType, Model: Equatable {
+    private let viewProperties: PaginatedListSelectorViewProperties
+    private var dataSource: DataSource
+    private let onDismiss: (_ selected: Model?) -> Void
+
+    private let rowType = BasicTableViewCell.self
+
+    @IBOutlet private weak var tableView: UITableView!
+
+    /// Pull To Refresh Support.
+    ///
+    private lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(pullToRefresh(sender:)), for: .valueChanged)
+        return refreshControl
+    }()
+
+    /// Footer "Loading More" Spinner.
+    ///
+    private lazy var footerSpinnerView = {
+        return FooterSpinnerView(tableViewStyle: tableView.style)
+    }()
+
+    private lazy var footerEmptyView = {
+        return UIView(frame: .zero)
+    }()
+
+    /// ResultsController: Surrounds us. Binds the galaxy together. And also, keeps the UITableView <> (Stored) Product Variations in sync.
+    ///
+    private lazy var resultsController: ResultsController<StorageModel> = {
+        let resultsController = dataSource.createResultsController()
+        configureResultsController(resultsController) { [weak self] in
+            self?.tableView.reloadData()
+        }
+        return resultsController
+    }()
+
+    /// SyncCoordinator: Keeps tracks of which pages have been refreshed, and encapsulates the "What should we sync now" logic.
+    ///
+    private let syncingCoordinator = SyncingCoordinator()
+
+    private lazy var stateCoordinator: PaginatedListViewControllerStateCoordinator = {
+        let stateCoordinator = PaginatedListViewControllerStateCoordinator(onLeavingState: { [weak self] state in
+            self?.didLeave(state: state)
+            }, onEnteringState: { [weak self] state in
+                self?.didEnter(state: state)
+        })
+        return stateCoordinator
+    }()
+
+    /// Indicates if there are no results onscreen.
+    ///
+    private var isEmpty: Bool {
+        return resultsController.isEmpty
+    }
+
+    // MARK: - Constants
+    //
+    let placeholderRowsPerSection: [Int] = [3]
+
+    init(viewProperties: PaginatedListSelectorViewProperties,
+         dataSource: DataSource,
+         onDismiss: @escaping (_ selected: Model?) -> Void) {
+        self.viewProperties = viewProperties
+        self.dataSource = dataSource
+        self.onDismiss = onDismiss
+        super.init(nibName: "ListSelectorViewController", bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureNavigation()
+        configureMainView()
+        configureTableView()
+        configureSyncingCoordinator()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        syncingCoordinator.synchronizeFirstPage()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        onDismiss(dataSource.selected)
+        super.viewWillDisappear(animated)
+    }
+
+    // MARK: UITableViewDataSource
+    //
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return resultsController.sections.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultsController.sections[section].numberOfObjects
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: rowType.reuseIdentifier,
+                                                       for: indexPath) as? BasicTableViewCell else {
+                                                        fatalError()
+        }
+        let model = resultsController.object(at: indexPath)
+        dataSource.configureCell(cell: cell, model: model)
+
+        cell.accessoryType = model == dataSource.selected ? .checkmark: .none
+
+        return cell
+    }
+
+    // MARK: UITableViewDelegate
+    //
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let selected = resultsController.object(at: indexPath)
+        dataSource.handleSelectedChange(selected: selected)
+        tableView.reloadData()
+    }
+
+    // MARK: SyncingCoordinatorDelegate
+    //
+    func sync(pageNumber: Int, pageSize: Int, onCompletion: ((Bool) -> Void)?) {
+        transitionToSyncingState()
+        dataSource.sync(pageNumber: pageNumber, pageSize: pageSize) { [weak self] isCompleted in
+            guard let self = self else {
+                return
+            }
+
+            guard isCompleted else {
+                DDLogError("⛔️ Error synchronizing models")
+                self.displaySyncingErrorNotice(pageNumber: pageNumber, pageSize: pageSize)
+                return
+            }
+
+            self.transitionToResultsUpdatedState()
+            onCompletion?(isCompleted)
+        }
+    }
+
+    // MARK: actions
+    //
+    @objc private func pullToRefresh(sender: UIRefreshControl) {
+        ServiceLocator.analytics.track(.productVariationListPulledToRefresh)
+
+        syncingCoordinator.synchronizeFirstPage {
+            sender.endRefreshing()
+        }
+    }
+}
+
+// MARK: - View Configuration
+//
+private extension PaginatedListSelectorViewController {
+
+    func configureNavigation() {
+        title = viewProperties.navigationBarTitle
+    }
+
+    func configureMainView() {
+        view.backgroundColor = .listBackground
+    }
+
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.backgroundColor = .listBackground
+
+        registerTableViewCells()
+    }
+
+    func registerTableViewCells() {
+        tableView.register(rowType.loadNib(), forCellReuseIdentifier: rowType.reuseIdentifier)
+    }
+
+    /// Setup: Sync'ing Coordinator
+    ///
+    func configureSyncingCoordinator() {
+        syncingCoordinator.delegate = self
+    }
+}
+
+// MARK: - Finite State Machine Management
+//
+private extension PaginatedListSelectorViewController {
+
+    func didEnter(state: PaginatedListViewControllerState) {
+        switch state {
+        case .noResultsPlaceholder:
+            displayNoResultsOverlay()
+        case .syncing(let withExistingData):
+            if withExistingData {
+                ensureFooterSpinnerIsStarted()
+            } else {
+                displayPlaceholderProducts()
+            }
+        case .results:
+            break
+        }
+    }
+
+    func didLeave(state: PaginatedListViewControllerState) {
+        switch state {
+        case .noResultsPlaceholder:
+            removeAllOverlays()
+        case .syncing:
+            ensureFooterSpinnerIsStopped()
+            removePlaceholderProducts()
+        case .results:
+            break
+        }
+    }
+
+    func transitionToSyncingState() {
+        stateCoordinator.transitionToSyncingState(withExistingData: !isEmpty)
+    }
+
+    func transitionToResultsUpdatedState() {
+        stateCoordinator.transitionToResultsUpdatedState(hasData: !isEmpty)
+    }
+}
+
+// MARK: - Placeholders
+//
+private extension PaginatedListSelectorViewController {
+
+    /// Renders the Placeholder Orders: For safety reasons, we'll also halt ResultsController <> UITableView glue.
+    ///
+    func displayPlaceholderProducts() {
+        let options = GhostOptions(reuseIdentifier: BasicTableViewCell.reuseIdentifier, rowsPerSection: placeholderRowsPerSection)
+        tableView.displayGhostContent(options: options,
+        style: .wooDefaultGhostStyle)
+
+        resultsController.stopForwardingEvents()
+    }
+
+    /// Removes the Placeholder Products (and restores the ResultsController <> UITableView link).
+    ///
+    func removePlaceholderProducts() {
+        tableView.removeGhostContent()
+        resultsController.startForwardingEvents(to: tableView)
+        tableView.reloadData()
+    }
+
+    /// Displays the Error Notice.
+    ///
+    func displaySyncingErrorNotice(pageNumber: Int, pageSize: Int) {
+        let message = NSLocalizedString("Unable to refresh list", comment: "Refresh Action Failed")
+        let actionTitle = NSLocalizedString("Retry", comment: "Retry Action")
+        let notice = Notice(title: message, feedbackType: .error, actionTitle: actionTitle) { [weak self] in
+            self?.sync(pageNumber: pageNumber, pageSize: pageSize, onCompletion: nil)
+        }
+
+        ServiceLocator.noticePresenter.enqueue(notice: notice)
+    }
+
+    /// Displays the overlay when there are no results.
+    ///
+    func displayNoResultsOverlay() {
+        let overlayView: OverlayMessageView = OverlayMessageView.instantiateFromNib()
+        overlayView.messageImage = nil
+        overlayView.messageText = viewProperties.noResultsPlaceholderText
+        overlayView.actionVisible = false
+        overlayView.attach(to: view)
+    }
+
+    /// Removes all of the the OverlayMessageView instances in the view hierarchy.
+    ///
+    func removeAllOverlays() {
+        for subview in view.subviews where subview is OverlayMessageView {
+            subview.removeFromSuperview()
+        }
+    }
+}
+
+// MARK: - Spinner Helpers
+//
+extension PaginatedListSelectorViewController {
+
+    /// Starts the Footer Spinner animation, whenever `mustStartFooterSpinner` returns *true*.
+    ///
+    private func ensureFooterSpinnerIsStarted() {
+        guard mustStartFooterSpinner() else {
+            return
+        }
+
+        tableView.tableFooterView = footerSpinnerView
+        footerSpinnerView.startAnimating()
+    }
+
+    /// Whenever we're sync'ing an Products Page that's beyond what we're currently displaying, this method will return *true*.
+    ///
+    private func mustStartFooterSpinner() -> Bool {
+        guard let highestPageBeingSynced = syncingCoordinator.highestPageBeingSynced else {
+            return false
+        }
+
+        return highestPageBeingSynced * SyncingCoordinator.Defaults.pageSize > resultsController.numberOfObjects
+    }
+
+    /// Stops animating the Footer Spinner.
+    ///
+    private func ensureFooterSpinnerIsStopped() {
+        footerSpinnerView.stopAnimating()
+        tableView.tableFooterView = footerEmptyView
+    }
+}
+
+// MARK: - ResultsController
+//
+private extension PaginatedListSelectorViewController {
+
+    func configureResultsController(_ resultsController: ResultsController<StorageModel>, onReload: @escaping () -> Void) {
+        resultsController.onDidChangeContent = {
+            onReload()
+        }
+
+        resultsController.onDidResetContent = {
+            onReload()
+        }
+
+        try? resultsController.performFetch()
+    }
+}

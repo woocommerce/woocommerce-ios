6461f4a5034bc969ed64baea8d3c3eec73cf6e1b
Thuy.Copeland <thuycopeland@gmail.com>
2019-12-05 16:19:46 -0600

    Delete the fixMe unit test and write others to check the paymentSummary

--- WooCommerce/WooCommerceTests/ViewRelated/OrderPaymentDetailsViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/OrderPaymentDetailsViewModelTests.swift
@@ -4,75 +4,134 @@ import XCTest
 
 final class OrderPaymentDetailsViewModelTests: XCTestCase {
     private var order: Order!
-    private var subject: OrderPaymentDetailsViewModel!
+    private var viewModel: OrderPaymentDetailsViewModel!
+
+    /// Broken Orders
+    ///
+    private var brokenOrder: Order!
+    private var anotherBrokenOrder: Order!
+    private var brokenOrderViewModel: OrderPaymentDetailsViewModel!
+    private var anotherBrokenOrderViewModel: OrderPaymentDetailsViewModel!
 
     override func setUp() {
         super.setUp()
         order = MockOrders().sampleOrder()
-        subject = OrderPaymentDetailsViewModel(order: order!)
+        viewModel = OrderPaymentDetailsViewModel(order: order!)
+
+        brokenOrder = MockOrders().brokenOrder()
+        brokenOrderViewModel = OrderPaymentDetailsViewModel(order: brokenOrder)
+
+        anotherBrokenOrder = MockOrders().unpaidOrder()
+        anotherBrokenOrderViewModel = OrderPaymentDetailsViewModel(order: anotherBrokenOrder)
     }
 
     override func tearDown() {
-        subject = nil
+        anotherBrokenOrderViewModel = nil
+        anotherBrokenOrder = nil
+        brokenOrderViewModel = nil
+        brokenOrder = nil
+        viewModel = nil
         order = nil
         super.tearDown()
     }
 
     func testSubtotalMatchesExpectation() {
-        XCTAssertEqual(subject.subtotal, 0)
+        XCTAssertEqual(viewModel.subtotal, 0)
     }
 
     func testSubtotalValueMatchesExpectation() {
         let expectedValue = CurrencyFormatter().formatAmount(0, with: order.currency) ?? String()
-        XCTAssertEqual(subject.subtotalValue, expectedValue)
+        XCTAssertEqual(viewModel.subtotalValue, expectedValue)
     }
 
     func testDiscountTextMatchesExpectation() {
-        XCTAssertNil(subject.discountText)
+        XCTAssertNil(viewModel.discountText)
     }
 
     func testDiscountValueMatchesExpectation() {
         let expectedValue = "-" + CurrencyFormatter().formatAmount(order.discountTotal, with: order.currency)!
-        XCTAssertEqual(subject.discountValue, expectedValue)
+        XCTAssertEqual(viewModel.discountValue, expectedValue)
     }
 
     func testShippingValueMatchesExpectation() {
         let expectedValue = CurrencyFormatter().formatAmount(order.shippingTotal, with: order.currency)
-        XCTAssertEqual(subject.shippingValue, expectedValue)
+        XCTAssertEqual(viewModel.shippingValue, expectedValue)
     }
 
     func testTaxesValueMatchesExpectation() {
         let expectedValue = CurrencyFormatter().formatAmount(order.totalTax, with: order.currency)
-        XCTAssertEqual(subject.taxesValue, expectedValue)
+        XCTAssertEqual(viewModel.taxesValue, expectedValue)
     }
 
     func testTotalValueMatchedExpectation() {
         let expectedValue = CurrencyFormatter().formatAmount(order.total,
                                                              with: order.currency)
-        XCTAssertEqual(subject.totalValue, expectedValue)
+        XCTAssertEqual(viewModel.totalValue, expectedValue)
     }
 
     func testPaymentTotalMatchedExpectation() {
         let expectedValue = CurrencyFormatter().formatAmount(order.total,
                                                              with: order.currency)
-        XCTAssertEqual(subject.totalValue, expectedValue)
+        XCTAssertEqual(viewModel.totalValue, expectedValue)
     }
 
-    // FIXME: - This unit test.
-    /// Test the payment summary format and ensure it's localizing properly.
+    /// Test the `paymentSummary` calculated property
+    /// returns nil if the payment method title is an empty string
     ///
-    func testPaymentSummaryMatchesExpectation() {
-        // There isn't a good way to test the composite sentence and
-        // ensure the that the translated, formatted sentence is correct.
+    func testOrderPaymentMethodTitleReturnsNilIfPaymentMethodTitleIsBlank() {
+        let expected = ""
+        XCTAssertEqual(brokenOrder.paymentMethodTitle, expected)
+        XCTAssertNil(brokenOrderViewModel.paymentSummary)
+    }
 
-        // You can get the paymentSummary from the order,
-        // BUT you can't create a static expected result string that matches every translated language,
-        // WITHOUT using the exact same localization and string manipulation method
-        // OR translating a literal string in every language first.
-        // I even tried to get the current existing translated strings from the Bundle by trying to  `Localizable.strings` files. No luck. - tc
+    /// The `paymentMethodTitle` is used in the `paymentSummary`.
+    /// Test that the `paymentSummary` calculated property
+    /// does not return nil as long as the `paymentMethodTitle` is present
+    ///
+    func testOrderPaymentMethodTitleDoesNotReturnNilWhenPresentAndNotBlank() {
+        guard !order.paymentMethodTitle.isEmpty else {
+            XCTFail("Expected a payment_method_title, not a blank or nil value")
+            return
+        }
+
+        XCTAssertNotNil(viewModel.paymentSummary)
+    }
+
+    func testPaymentSummaryContainsAwaitingPaymentMessageWhenDatePaidIsNull() {
+        let awaitingPayment = String.localizedStringWithFormat(
+            NSLocalizedString(
+                "Awaiting payment via %@",
+                comment: "A unit test string. It reads: " +
+                "Awaiting payment via <payment method title>." +
+                "A payment method example is: 'Credit Card (Stripe)'"
+            ),
+            anotherBrokenOrder.paymentMethodTitle
+        )
+
+        XCTAssertEqual(anotherBrokenOrder.paymentMethodTitle, "Cash on Delivery")
+        XCTAssertNil(anotherBrokenOrder.datePaid)
+
+        guard let paymentSummary = anotherBrokenOrderViewModel.paymentSummary else {
+            XCTFail("The payment summary should not be nil or blank.")
+            return
+        }
+
+        XCTAssertTrue(paymentSummary.contains(awaitingPayment))
     }
 
     func testCouponLinesMatchesExpectation() {
-        XCTAssertEqual(subject.couponLines, order.coupons)
+        XCTAssertEqual(viewModel.couponLines, order.coupons)
+    }
+}
+
+/// Private Methods.
+///
+private extension OrderPaymentDetailsViewModelTests {
+
+    /// Returns the OrderMapper output upon receiving `filename` (Data Encoded)
+    ///
+    func mapOrder(from filename: String) throws -> Order {
+        let response = Loader.contentsOf(filename)!
+        return try OrderMapper(siteID: 545).map(response: response)
     }
 }

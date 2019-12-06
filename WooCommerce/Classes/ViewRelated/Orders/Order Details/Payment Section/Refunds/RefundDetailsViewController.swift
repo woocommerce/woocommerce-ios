ddd7dc6e4e4539db7a4b65d9dda996662b0a90ad
Thuy.Copeland <thuycopeland@gmail.com>
2019-12-06 15:37:11 -0600

    Add the Refund Details VC to the project

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Payment Section/Refunds/RefundDetailsViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+
+// MARK: - RefundDetailsViewController: Displays the details for a given Refund.
+//
+class RefundDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}

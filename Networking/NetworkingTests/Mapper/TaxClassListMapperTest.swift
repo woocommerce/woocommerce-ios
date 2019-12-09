678dd47bdb82dbdf6607ade595f3bcbe4d0c4a72
Paolo Musolino <info@codeido.com>
2019-12-09 15:17:08 +0100

    TaxClassListMapper Unit Tests + json example response

---
+++ Networking/NetworkingTests/Mapper/TaxClassListMapperTest.swift
@@ -0,0 +1,42 @@
+import XCTest
+@testable import Networking
+
+
+/// TaxClassListMapper Unit Tests
+///
+class TaxClassListMapperTest: XCTestCase {
+
+    /// Verifies that all of the Tax Class Fields are parsed correctly.
+    ///
+    func testTaxClassFieldsAreProperlyParsed() {
+        let taxClasses = mapLoadAllTaxClassResponse()
+        XCTAssertEqual(taxClasses.count, 3)
+        
+
+        let firstTaxClass = taxClasses[0]
+        XCTAssertEqual(firstTaxClass.slug, "standard")
+        XCTAssertEqual(firstTaxClass.name, "Standard Rate")
+    }
+}
+
+
+/// Private Methods.
+///
+private extension TaxClassListMapperTest {
+
+    /// Returns the TaxClassListMapper output upon receiving `filename` (Data Encoded)
+    ///
+    func mapTaxClasses(from filename: String) -> [TaxClass] {
+        guard let response = Loader.contentsOf(filename) else {
+            return []
+        }
+
+        return try! TaxClassListMapper().map(response: response)
+    }
+
+    /// Returns the TaxClassListMapper output upon receiving `taxes-classes`
+    ///
+    func mapLoadAllTaxClassResponse() -> [TaxClass] {
+        return mapTaxClasses(from: "taxes-classes")
+    }
+}

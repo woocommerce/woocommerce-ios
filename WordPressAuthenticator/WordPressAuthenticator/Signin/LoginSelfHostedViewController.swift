6228d84076d3b8a2a128f01a9daa43dbf6121149
Shiki <jayson@basanes.net>
2019-12-13 12:08:28 -0700

    LoginSelfHostedVC: Make forgotPassword a link
    The forgotPasswordButton opens Safari when tapped. Buttons with behaviors like this should be announced as a link by VoiceOver.

--- WordPressAuthenticator/WordPressAuthenticator/Signin/LoginSelfHostedViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Signin/LoginSelfHostedViewController.swift
@@ -106,6 +106,8 @@ class LoginSelfHostedViewController: LoginViewController, NUXKeyboardResponder {
             usernameField.placeholder = nil
             passwordField.placeholder = nil
         }
+
+        forgotPasswordButton.accessibilityTraits = .link
     }
 
 

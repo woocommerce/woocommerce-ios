1cee2ab0e2e79e933f5e9f0eecc9fd5701c8efdf
Paolo Musolino <info@codeido.com>
2019-12-16 13:28:25 +0100

    Fix registerSupportedActions

--- Yosemite/Yosemite/Stores/TaxClassStore.swift
+++ Yosemite/Yosemite/Stores/TaxClassStore.swift
@@ -14,7 +14,7 @@ public class TaxClassStore: Store {
     /// Registers for supported Actions.
     ///
     override public func registerSupportedActions(in dispatcher: Dispatcher) {
-        dispatcher.register(processor: self, for: ProductAction.self)
+        dispatcher.register(processor: self, for: TaxClassAction.self)
     }
 
     /// Receives and executes Actions.

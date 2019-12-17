11767a4d0556cfca957a8cf1c57c3522337fac15
Paolo Musolino <info@codeido.com>
2019-12-17 11:31:40 +0100

    TableView in controller + init

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Edit Price/ProductEditPriceSettingsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Edit Price/ProductEditPriceSettingsViewController.swift
@@ -1,7 +1,22 @@
 import UIKit
+import Yosemite
 
 class ProductEditPriceSettingsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let product: Product
+    
+    init(product: Product) {
+        self.product = product
+
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 

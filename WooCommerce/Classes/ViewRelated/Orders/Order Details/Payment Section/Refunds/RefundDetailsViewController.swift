b9b23cf812eacf71cceff20f78567b29af859af1
Thuy.Copeland <thuycopeland@gmail.com>
2019-12-18 11:29:17 -0600

    Set up navigation inside of Refund Details

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Payment Section/Refunds/RefundDetailsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Payment Section/Refunds/RefundDetailsViewController.swift
@@ -1,13 +1,38 @@
 import UIKit
+import Yosemite
 
 
 // MARK: - RefundDetailsViewController: Displays the details for a given Refund.
 //
 class RefundDetailsViewController: UIViewController {
 
+    /// Refund
+    ///
+    private let refund: Refund
+
+
+    /// Designated Initializer
+    ///
+    init(refund: Refund) {
+        self.refund = refund
+        super.init(nibName: type(of: self).nibName, bundle: nil)
+    }
+
+     /// NSCoder Conformance
+     ///
+     required init?(coder aDecoder: NSCoder) {
+         fatalError("init(coder:) is not supported")
+     }
+
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUpNavigation()
+    }
+
+    func setUpNavigation() {
+        let refundTitle = NSLocalizedString("Refund #%ld", comment: "It reads: Refund #<refund ID>")
+        title = String.localizedStringWithFormat(refundTitle, refund.refundID)
     }
 }

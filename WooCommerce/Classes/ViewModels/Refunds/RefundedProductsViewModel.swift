adafddec406cfcd0319b0f2c1e0843d2d8c0897f
Thuy.Copeland <thuycopeland@gmail.com>
2020-01-06 15:14:41 -0600

    Change viewModel to be a class, not a struct

--- WooCommerce/Classes/ViewModels/Refunds/RefundedProductsViewModel.swift
+++ WooCommerce/Classes/ViewModels/Refunds/RefundedProductsViewModel.swift
@@ -4,7 +4,7 @@ import Yosemite
 
 // MARK: - Refunded Products View Model
 //
-struct RefundedProductsViewModel {
+final class RefundedProductsViewModel {
     /// Order we're observing.
     ///
     private let order: Order
@@ -24,5 +24,42 @@ struct RefundedProductsViewModel {
     init(order: Order, refunds: [Refund]) {
         self.order = order
         self.refunds = refunds
+    }
+}
+
+
+// MARK: - Register table view cells
+//
+extension RefundedProductsViewModel {
+    /// Registers all of the available TableViewCells
+    ///
+    func registerTableViewCells(_ tableView: UITableView) {
+        let cells = [
+            ProductDetailsTableViewCell.self
+        ]
+
+        for cell in cells {
+            tableView.register(cell.loadNib(), forCellReuseIdentifier: cell.reuseIdentifier)
+        }
+    }
+
+    /// Registers all of the available TableViewHeaderFooters
+    ///
+    func registerTableViewHeaderFooters(_ tableView: UITableView) {
+        let headersAndFooters = [
+            TwoColumnSectionHeaderView.self
+        ]
+
+        for kind in headersAndFooters {
+            tableView.register(kind.loadNib(), forHeaderFooterViewReuseIdentifier: kind.reuseIdentifier)
+        }
+    }
+}
+
+
+// MARK: - Sections
+extension RefundedProductsViewModel {
+    func reloadSections() {
+        dataSource.reloadSections()
     }
 }

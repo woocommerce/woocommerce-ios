a7b22245bd735a4e002670308aebabf6b85958c0
Thuy.Copeland <thuycopeland@gmail.com>
2020-01-06 14:41:55 -0600

    Introduce the new data source for the refunded products table

---
+++ WooCommerce/Classes/ViewModels/Refunds/RefundedProductsDataSource.swift
@@ -0,0 +1,11 @@
+import Foundation
+import UIKit
+import Yosemite
+
+
+/// The main file for Refunded Products data.
+/// Must conform to NSObject so it can be the UITableViewDataSource.
+///
+final class RefundedProductsDataSource: NSObject {
+
+}

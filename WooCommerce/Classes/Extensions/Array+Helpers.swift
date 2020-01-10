cc8d2452ed8fa5cba641a4f73bb81ff81f901ac6
Thuy.Copeland <thuycopeland@gmail.com>
2020-01-10 14:43:25 -0600

    Add suggested helpers

--- WooCommerce/Classes/Extensions/Array+Helpers.swift
+++ WooCommerce/Classes/Extensions/Array+Helpers.swift
@@ -4,7 +4,6 @@ import Foundation
 // MARK: - Array Helpers
 //
 extension Array {
-
     /// Removes and returns the first element in the array. If any!
     ///
     mutating func popFirst() -> Element? {
@@ -17,11 +16,33 @@ extension Array {
 }
 
 
+// MARK: - Collection Helpers
+//
 extension Collection {
-
     /// Returns the element at the specified index if it is within bounds, otherwise nil.
     ///
     subscript (safe index: Index) -> Element? {
         return indices.contains(index) ? self[index] : nil
     }
+}
+
+
+// MARK: - Sequence Helpers
+//
+extension Sequence {
+    /// Get the keypaths for a elemtents in a sequence.
+    ///
+    func map<T>(_ keyPath: KeyPath<Element, T>) -> [T] {
+        return map { $0[keyPath: keyPath] }
+    }
+
+    /// Sum a sequence of elements by keypath.
+    func sum<T: Numeric>(_ keyPath: KeyPath<Element, T>) -> T {
+        return map(keyPath).sum()
+    }
+}
+
+extension Sequence where Element: Numeric {
+    /// Returns the sum of all elements in the collection.
+    func sum() -> Element { return reduce(0, +) }
 }

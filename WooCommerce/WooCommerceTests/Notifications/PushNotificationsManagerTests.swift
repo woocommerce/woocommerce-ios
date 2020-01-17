a862969f49950b92be674727d08927391c5da776
Jaclyn Chen <watertranquil@gmail.com>
2020-01-17 15:28:06 +0800

    Update unit tests.

--- WooCommerce/WooCommerceTests/Notifications/PushNotificationsManagerTests.swift
+++ WooCommerce/WooCommerceTests/Notifications/PushNotificationsManagerTests.swift
@@ -234,7 +234,7 @@ class PushNotificationsManagerTests: XCTestCase {
     ///
     func testHandleNotificationUpdatesApplicationsBadgeNumber() {
         let updatedBadgeNumber = 10
-        let userInfo = notificationPayload(badgeCount: updatedBadgeNumber)
+        let userInfo = notificationPayload(badgeCount: updatedBadgeNumber, type: .comment)
 
         XCTAssertEqual(application.applicationIconBadgeNumber, Int.min)
 
@@ -330,13 +330,14 @@ private extension PushNotificationsManagerTests {
 
     /// Returns a Sample Notification Payload
     ///
-    func notificationPayload(badgeCount: Int = 0, noteID: Int64 = 1234) -> [String: Any] {
+    func notificationPayload(badgeCount: Int = 0, noteID: Int64 = 1234, type: Note.Kind = .comment) -> [String: Any] {
         return [
             "aps": [
                 "badge": badgeCount,
                 "alert": Sample.defaultMessage
             ],
-            "note_id": noteID
+            "note_id": noteID,
+            "type": type.rawValue
         ]
     }
 }

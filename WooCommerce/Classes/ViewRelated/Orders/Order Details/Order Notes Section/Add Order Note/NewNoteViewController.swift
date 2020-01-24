6c7cf360bd553af0aa4b75a7df257d00c6d5eb36
Leonardo D'Amato <devleo.apps@gmail.com>
2020-01-24 22:02:18 +1100

    Icon now changing color when adding a new note to an order

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Order Notes Section/Add Order Note/NewNoteViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Order Notes Section/Add Order Note/NewNoteViewController.swift
@@ -16,6 +16,8 @@ class NewNoteViewController: UIViewController {
 
     private var noteText: String = ""
 
+    private var emailCustomerSwitch: Bool = false
+
     /// Dedicated NoticePresenter (use this here instead of ServiceLocator.noticePresenter)
     ///
     private lazy var noticePresenter: DefaultNoticePresenter = {
@@ -134,6 +136,7 @@ private extension NewNoteViewController {
 
         cell.iconImage = .asideImage
         cell.iconTint = isCustomerNote ? .listIcon : .textSubtle
+        cell.iconTint = emailCustomerSwitch == true ? .primary : .listIcon
         cell.iconImage?.accessibilityLabel = isCustomerNote ?
             NSLocalizedString("Note to customer",
                               comment: "Spoken accessibility label for an icon image that indicates it's a note to the customer.") :
@@ -171,7 +174,6 @@ private extension NewNoteViewController {
             }
 
             self.isCustomerNote = newValue
-            self.refreshTextViewCell()
 
             cell.accessibilityLabel = String.localizedStringWithFormat(
                 NSLocalizedString("Email note to customer %@", comment: ""),
@@ -182,6 +184,9 @@ private extension NewNoteViewController {
 
             let stateValue = newValue ? "on" : "off"
             ServiceLocator.analytics.track(.orderNoteEmailCustomerToggled, withProperties: ["state": stateValue])
+
+            self.emailCustomerSwitch = stateValue == "on" ? true : false
+            self.refreshTextViewCell()
         }
     }
 

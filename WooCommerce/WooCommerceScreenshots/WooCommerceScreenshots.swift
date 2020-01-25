bd55a3d314e0a8a968137a4e48efbeb01290e585
Jeremy Massel <jeremy.massel@automattic.com>
2020-01-24 22:50:38 -0700

    Add the ability to dismiss the top banner view

--- WooCommerce/WooCommerceScreenshots/WooCommerceScreenshots.swift
+++ WooCommerce/WooCommerceScreenshots/WooCommerceScreenshots.swift
@@ -25,5 +25,6 @@ class WooCommerceScreenshots: XCTestCase {
             .proceedWithPassword()
             .proceedWith(password: "")
             .continueWithSelectedSite()
+            .dismissTopBannerIfNeeded()
     }
 }

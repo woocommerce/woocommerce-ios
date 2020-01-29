7a2efee63817423b2d58e91f318a08473c7caef0
Shiki <jayson@basanes.net>
2020-01-29 14:11:31 -0700

    Delete unused file

--- WooCommerce/Classes/ViewRelated/Search/SearchEmptyLabelViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SearchEmptyLabelViewController.swift
-//  WooCommerce
-//
-//  Created by Shiki on 2020-01-29.
-//  Copyright © 2020 Automattic. All rights reserved.
-//
-
-import Foundation

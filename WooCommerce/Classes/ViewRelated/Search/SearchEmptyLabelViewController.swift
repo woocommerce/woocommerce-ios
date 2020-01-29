c07b0da5fa994eed914b0a307a1b5b70a0fd7d72
Shiki <jayson@basanes.net>
2020-01-29 13:45:03 -0700

    SearchUICommand: Add createStarterViewController

---
+++ WooCommerce/Classes/ViewRelated/Search/SearchEmptyLabelViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchEmptyLabelViewController.swift
+//  WooCommerce
+//
+//  Created by Shiki on 2020-01-29.
+//  Copyright © 2020 Automattic. All rights reserved.
+//
+
+import Foundation

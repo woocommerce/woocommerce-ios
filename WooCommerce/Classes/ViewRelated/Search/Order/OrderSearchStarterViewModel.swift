dec773d3931f78b80c6e557217b294fd6bd8a084
Shiki <jayson@basanes.net>
2020-01-29 19:54:57 -0700

    OrderSearchStarter: Add loading of data
    This should show all order statuses in the table.

--- WooCommerce/Classes/ViewRelated/Search/Order/OrderSearchStarterViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Search/Order/OrderSearchStarterViewModel.swift
@@ -7,13 +7,16 @@ final class OrderSearchStarterViewModel {
     private lazy var dataSource = DataSource()
 
     func configureDataSource(for tableView: UITableView) {
+        tableView.dataSource = dataSource
+
         dataSource.startForwardingEvents(to: tableView)
+        try? dataSource.performFetch()
     }
 }
 
 
-extension OrderSearchStarterViewModel {
-    class DataSource: NSObject, UITableViewDataSource {
+private extension OrderSearchStarterViewModel {
+    final class DataSource: NSObject, UITableViewDataSource {
         private lazy var storageManager = ServiceLocator.storageManager
 
         private lazy var resultsController: ResultsController<StorageOrderStatus> = {
@@ -22,16 +25,29 @@ extension OrderSearchStarterViewModel {
                                                          sortedBy: [descriptor])
         }()
 
+        func performFetch() throws {
+            try resultsController.performFetch()
+        }
+
         func startForwardingEvents(to tableView: UITableView) {
             resultsController.startForwardingEvents(to: tableView)
         }
 
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return 0
+            resultsController.numberOfObjects
         }
 
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            return UITableViewCell()
+            let status = resultsController.object(at: indexPath)
+
+            let cell = UITableViewCell()
+            cell.textLabel?.text = status.name
+
+            return cell
+        }
+
+        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            NSLocalizedString("Order Status", comment: "The section title for the list of Order statuses in the Order Search.")
         }
     }
 }

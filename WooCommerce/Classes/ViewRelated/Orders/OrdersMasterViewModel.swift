751c8a9c72c61695267258ee2bda6cd78545efc5
Shiki <jayson@basanes.net>
2020-02-03 15:52:48 -0700

    OrdersMasterVM: Add filter by siteID

--- WooCommerce/Classes/ViewRelated/Orders/OrdersMasterViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/OrdersMasterViewModel.swift
@@ -7,6 +7,7 @@ import Yosemite
 final class OrdersMasterViewModel {
 
     private lazy var storageManager = ServiceLocator.storageManager
+    private lazy var stores = ServiceLocator.stores
 
     /// ResultsController: Handles all things order status
     ///
@@ -23,7 +24,26 @@ final class OrdersMasterViewModel {
 
     /// Start all the operations that this `ViewModel` is responsible for.
     ///
+    /// This should only be called once in the lifetime of `OrdersMasterViewController`.
+    ///
     func activate() {
+        refreshStatusPredicate()
         try? statusResultsController.performFetch()
+    }
+
+    /// Update the `siteID` predicate of `statusResultsController` to the current `siteID`.
+    ///
+    private func refreshStatusPredicate() {
+        // Bugfix for https://github.com/woocommerce/woocommerce-ios/issues/751.
+        // Because we are listening for default account changes,
+        // this will also fire upon logging out, when the account
+        // is set to nil. So let's protect against multi-threaded
+        // access attempts if the account is indeed nil.
+        guard stores.isAuthenticated,
+            stores.needsDefaultStore == false else {
+                return
+        }
+
+        statusResultsController.predicate = NSPredicate(format: "siteID == %lld", stores.sessionManager.defaultStoreID ?? Int.min)
     }
 }

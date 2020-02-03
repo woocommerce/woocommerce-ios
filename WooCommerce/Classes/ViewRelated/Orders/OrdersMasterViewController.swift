3822fade0497dc133df1b1ca6dd1660e7d8427a9
Shiki <jayson@basanes.net>
2020-02-03 14:27:26 -0700

    Add initial OrdersMasterViewController

---
+++ WooCommerce/Classes/ViewRelated/Orders/OrdersMasterViewController.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+/// The main Orders view controller that is shown when the Orders tab is accessed.
+///
+/// TODO This should contain the tabs "Processing" and "All Orders".
+///
+final class OrdersMasterViewController: UIViewController {
+
+}

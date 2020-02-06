e69264bc8a6ea9457f777e86fc1fab51514bafcb
rachelmcr <rachel@automattic.com>
2020-02-06 22:21:32 +0000

    Calculate API base URL once

--- Networking/Networking/Settings/Settings.swift
+++ Networking/Networking/Settings/Settings.swift
@@ -11,11 +11,11 @@ public struct Settings {
 
     /// WordPress.com API Base URL
     ///
-    public static var wordpressApiBaseURL: String {
+    public static var wordpressApiBaseURL: String = {
         if ProcessInfo.processInfo.arguments.contains("mocked-wpcom-api") {
             return "http://localhost:8282/"
         }
 
         return "https://public-api.wordpress.com/"
-    }
+    }()
 }

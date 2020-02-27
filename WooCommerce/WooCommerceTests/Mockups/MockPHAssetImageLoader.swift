a24b6266fcf81870fb72901a9ca541bf4c115fa9
Jaclyn Chen <watertranquil@gmail.com>
2020-02-27 21:34:42 +0800

    Fix Hound warnings.

--- WooCommerce/WooCommerceTests/Mockups/MockPHAssetImageLoader.swift
+++ WooCommerce/WooCommerceTests/Mockups/MockPHAssetImageLoader.swift
@@ -5,11 +5,11 @@ import UIKit
 final class MockPHAssetImageLoader: PHAssetImageLoader {
     // Mocks in-memory cache.
     private let imagesByAsset: [PHAsset: UIImage]
-    
+
     init(imagesByAsset: [PHAsset: UIImage]) {
         self.imagesByAsset = imagesByAsset
     }
-    
+
     func requestImage(for asset: PHAsset,
                       targetSize: CGSize,
                       contentMode: PHImageContentMode,

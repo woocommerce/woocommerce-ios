cfeb722a042e86cd6fd6dea9ab25f822513cc18a
Jaclyn Chen <watertranquil@gmail.com>
2020-03-09 17:16:25 +0800

    Create `InventoryScannerViewController` that detects bar codes. TODOs: output image orientation, highlight text, support tap gesture.

---
+++ WooCommerce/Classes/ViewRelated/Products/InventoryScannerViewController.swift
@@ -0,0 +1,317 @@
+import AVFoundation
+import UIKit
+import Vision
+
+enum BoxAnchorLocation {
+    case bottomLeft
+    case topLeft
+    case topRight
+    case bottomRight
+    case none
+}
+
+final class InventoryScannerViewController: UIViewController {
+    @IBOutlet private weak var videoOutputImageView: UIImageView!
+
+    private let textBoxSize: CGSize = CGSize(width: 200.0, height: 200.0)
+    private let contentFormatting: [NSAttributedString.Key: Any] = [
+        .foregroundColor : UIColor.black,
+        .font : UIFont.systemFont(ofSize: 18.0, weight: .medium)
+    ]
+
+    private var session = AVCaptureSession()
+    private var requests = [VNRequest]()
+
+    private var totalNumberOfTextBoxes: Int = 0
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureNavigation()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        startLiveVideo()
+        startBarcodeDetection()
+    }
+
+    override func viewDidLayoutSubviews() {
+        self.videoOutputImageView.layer.sublayers?[0].frame = videoOutputImageView.bounds
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    func startLiveVideo() {
+        // Enable live stream video
+        self.session.sessionPreset = AVCaptureSession.Preset.photo
+
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video),
+            let deviceInput = try? AVCaptureDeviceInput(device: captureDevice) else {
+                return
+        }
+
+        let deviceOutput = AVCaptureVideoDataOutput()
+        deviceOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA)]
+        // Set the quality of the video
+        deviceOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global(qos: DispatchQoS.QoSClass.default))
+        // What the camera is seeing
+        self.session.addInput(deviceInput)
+        // What we will display on the screen
+        self.session.addOutput(deviceOutput)
+
+        // Show the video as it's being captured
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        let deviceOrientation: UIDeviceOrientation = UIDevice.current.orientation
+        // Orientation is reversed
+        switch (deviceOrientation) {
+        case .landscapeLeft:
+            previewLayer.connection?.videoOrientation = .landscapeRight
+        case .landscapeRight:
+            previewLayer.connection?.videoOrientation = .landscapeLeft
+        default:
+            previewLayer.connection?.videoOrientation = .landscapeRight
+        }
+        previewLayer.videoGravity = .resizeAspectFill
+        previewLayer.frame = self.videoOutputImageView.bounds
+        self.videoOutputImageView.layer.addSublayer(previewLayer)
+
+        self.session.startRunning()
+    }
+
+    func startBarcodeDetection() {
+        let barcodeRequest = VNDetectBarcodesRequest(completionHandler: self.detectBarcodeHandler)
+        print("!! \(VNDetectBarcodesRequest.supportedSymbologies)")
+        self.requests = [barcodeRequest]
+    }
+
+    // TODO-jc: remove?
+    @IBAction func handleVideoTapGesture(recognizer: UITapGestureRecognizer) {
+        let touchPoint: CGPoint = recognizer.location(in: self.videoOutputImageView)
+        let sublayers: [CALayer] = self.videoOutputImageView.layer.sublayers!
+        let textBoxLayers: [CALayer] = Array(sublayers[(sublayers.count - self.totalNumberOfTextBoxes)...])
+
+        for textBox in textBoxLayers {
+            if textBox.frame.contains(touchPoint) {
+                if self.session.isRunning {
+                    self.session.stopRunning()
+                }
+                return
+            }
+        }
+
+        self.toggleSession()
+    }
+
+    // TODO-jc: remove?
+    func toggleSession() {
+        if session.isRunning {
+            session.stopRunning()
+        } else {
+            session.startRunning()
+        }
+    }
+
+    // Handles barcode detection requests
+    func detectBarcodeHandler(request: VNRequest, error: Error?) {
+        if let error = error {
+            print(error)
+        }
+        guard let barcodes = request.results, barcodes.isNotEmpty else {
+            return
+        }
+
+        // Perform UI updates on the main thread
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else {
+                return
+            }
+
+            guard self.session.isRunning else {
+                return
+            }
+
+            self.videoOutputImageView.layer.sublayers?.removeSubrange(1...)
+            self.totalNumberOfTextBoxes = 0
+
+            // This will be used to eliminate duplicate findings
+            var barcodeObservations: [String: VNBarcodeObservation] = [:]
+            for barcode in barcodes.compactMap({ $0 as? VNBarcodeObservation }) {
+                guard let barcodeString = barcode.payloadStringValue else {
+                    continue
+                }
+                barcodeObservations[barcodeString] = barcode
+            }
+
+            for (_, barcodeObservation) in barcodeObservations {
+                self.highlightQRCode(barcode: barcodeObservation)
+            }
+            for (barcodeContent, barcodeObservation) in barcodeObservations {
+                self.drawTextBox(barcodeObservation: barcodeObservation, content: barcodeContent)
+            }
+        }
+    }
+
+    // Draw a box around each QRCode
+    func highlightQRCode(barcode: VNBarcodeObservation) {
+        let barcodeBounds = self.adjustBoundsToScreen(barcode: barcode)
+
+        let outline = CALayer()
+        outline.frame = barcodeBounds
+        outline.borderWidth = 2.0
+        outline.borderColor = UIColor.red.cgColor
+
+        // We are inserting the highlights at the beginning of the sublayer queue
+        // To avoid overlapping with the textboxes
+        self.videoOutputImageView.layer.insertSublayer(outline, at: 1)
+    }
+
+    func drawTextBox(barcodeObservation: VNBarcodeObservation, content: String) {
+        let barcodeBounds = self.adjustBoundsToScreen(barcode: barcodeObservation)
+
+        let textLayerFrame: CGRect = CGRect(x: barcodeBounds.origin.x + barcodeBounds.size.width, y: barcodeBounds.origin.y - self.textBoxSize.height,
+                                            width: textBoxSize.width, height: textBoxSize.height)
+        if self.videoOutputImageView.bounds.contains(textLayerFrame) {
+            // Readjust box locations so that there aren't any overlapping ones...
+            guard let readjustedFrame = self.readjustBoxLocationBasedOnExistingLayers(originalBox: textLayerFrame, barcodeSize: barcodeBounds.size) else {
+                return
+            }
+
+            let textBox = self.createTextLayer(content: content, frame: readjustedFrame)
+            textBox.name = content
+
+            self.videoOutputImageView.layer.addSublayer(textBox)
+            self.totalNumberOfTextBoxes += 1
+        }
+    }
+
+    func createTextLayer(content: String, frame: CGRect) -> CATextLayer {
+        let textBox = CATextLayer()
+        textBox.frame = frame
+        textBox.backgroundColor = UIColor.white.cgColor
+        textBox.cornerRadius = 6.0
+
+        textBox.shadowRadius = 3.0
+        textBox.shadowOffset = CGSize(width: 0, height: 0)
+        textBox.shadowColor = UIColor.black.cgColor
+        textBox.shadowOpacity = 0.9
+
+        let textBoxString = NSAttributedString(string: content, attributes: self.contentFormatting)
+        textBox.string = textBoxString
+
+        textBox.contentsScale = UIScreen.main.scale
+        textBox.isWrapped = true
+
+        return textBox
+    }
+
+    func adjustBoundsToScreen(barcode: VNBarcodeObservation) -> CGRect {
+        // Current origin is on the bottom-left corner
+        let xCord = barcode.boundingBox.origin.x * self.videoOutputImageView.frame.size.width
+        var yCord = (1 - barcode.boundingBox.origin.y) * self.videoOutputImageView.frame.size.height
+        let width = barcode.boundingBox.size.width * self.videoOutputImageView.frame.size.width
+        var height = -1 * barcode.boundingBox.size.height * self.videoOutputImageView.frame.size.height
+
+        // Re-adjust origin to be on the top-left corner, so that calculations can be standardized
+        yCord += height
+        height *= -1
+
+        return CGRect(x: xCord, y: yCord, width: width, height: height)
+    }
+
+    // Re-adjusts the given box's location based on other boxes that exist on other layers, so that boxes don't overlap
+    // Returns nil if there is nowhere to place the box on
+    func readjustBoxLocationBasedOnExistingLayers(originalBox: CGRect, barcodeSize: CGSize) -> CGRect? {
+        guard let videoOutputLayers: [CALayer] = self.videoOutputImageView.layer.sublayers else {
+            return nil
+        }
+
+        // Skip the first layer (i.e. the video layer) and outline layers
+        let textBoxLayers: [CALayer] = Array(videoOutputLayers[(videoOutputLayers.count - self.totalNumberOfTextBoxes)...]);
+
+        let bottomLeftAnchorBox = originalBox;
+        let topLeftAnchorBox = CGRect(x: originalBox.origin.x, y: originalBox.origin.y + originalBox.size.height + barcodeSize.height,
+                                      width: originalBox.size.width, height: originalBox.size.height)
+        let topRightAnchorBox = CGRect(x: originalBox.origin.x - barcodeSize.width - originalBox.size.width, y: originalBox.origin.y + barcodeSize.height + originalBox.size.height,
+                                       width: originalBox.size.width, height: originalBox.size.height)
+        let bottomRightAnchorBox = CGRect(x: originalBox.origin.x - barcodeSize.width - originalBox.size.width, y: originalBox.origin.y,
+                                          width: originalBox.size.width, height: originalBox.size.height)
+        var potentialBoxes: [BoxAnchorLocation : CGRect] = [
+            .bottomLeft : bottomLeftAnchorBox,
+            .topLeft : topLeftAnchorBox,
+            .topRight : topRightAnchorBox,
+            .bottomRight : bottomRightAnchorBox
+        ]
+
+        var potentialBoxToUse: (CGRect?, BoxAnchorLocation) = (nil, .none)
+        for layer in textBoxLayers {
+            for (type, potentialBox) in potentialBoxes {
+                if self.videoOutputImageView.bounds.contains(potentialBox) && !potentialBox.intersects(layer.frame) {
+                    potentialBoxToUse = (potentialBox, type)
+                } else {
+                    if potentialBoxToUse.1 == type {
+                        potentialBoxToUse = (nil, .none)
+                    }
+                    potentialBoxes.removeValue(forKey: type)
+                }
+            }
+        }
+
+        if textBoxLayers.count < 1 {
+            potentialBoxToUse = (originalBox, .bottomLeft)
+        }
+
+        return potentialBoxToUse.0
+    }
+}
+
+
+// MARK: Configurations
+//
+private extension InventoryScannerViewController {
+    func configureNavigation() {
+        title = NSLocalizedString("Inventory scanner", comment: "")
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonTapped))
+    }
+}
+
+// MARK: Navigation
+//
+private extension InventoryScannerViewController {
+    @objc func cancelButtonTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension InventoryScannerViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    // Run Vision code with live stream
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+
+        var requestOptions: [VNImageOption : Any] = [:]
+
+        if let camData = CMGetAttachment(sampleBuffer, key: kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, attachmentModeOut: nil) {
+            requestOptions = [.cameraIntrinsics: camData]
+        }
+
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .up, options: requestOptions)
+
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
+    }
+}

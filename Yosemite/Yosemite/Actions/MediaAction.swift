e435b8c05b3a62d52b6362fb92ab07ba8a035b46
Jaclyn Chen <watertranquil@gmail.com>
2020-03-09 09:06:23 +0800

    Add comments for each parameter in `MediaAction.retrieveMediaLibrary` action.

--- Yosemite/Yosemite/Actions/MediaAction.swift
+++ Yosemite/Yosemite/Actions/MediaAction.swift
@@ -5,6 +5,13 @@ import Foundation
 public enum MediaAction: Action {
     /// Retrieves media from the site's WP Media Library.
     ///
+    /// - Parameters:
+    ///   - siteID: Site for which we'll load the media from.
+    ///   - pageFirstIndex: The index of the first page from the caller's perspective, which `pageNumber` is based on.
+    ///   - pageNumber: The index of the page of media data to load from.
+    ///   - pageSize: The maximum number of media items to return per page.
+    ///   - onCompletion: Closure to be executed upon completion.
+    ///
     case retrieveMediaLibrary(siteID: Int64,
         pageFirstIndex: Int,
         pageNumber: Int,

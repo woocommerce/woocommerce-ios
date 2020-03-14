c48f284359876a2582be88c7d4bc4ace4a5f08c5
Ernesto Carrión <ernesto-carrion@cookpad.com>
2020-03-13 22:42:26 -0500

    Fixes tab-bar invisible border in iOS 13.1

--- WooCommerce/Classes/Extensions/UITabBar+Appearance.swift
+++ WooCommerce/Classes/Extensions/UITabBar+Appearance.swift
@@ -10,5 +10,29 @@ extension UITabBar {
         let appearance = Self.appearance()
         appearance.barTintColor = .appTabBar
         appearance.tintColor = .text
+
+        /// Some iOS 13.x versions don't render the tabbar shadow color correcly while in dark mode.
+        /// To fix it, we have to specifically set it in the `standardAppearance` object.
+        /// Which forces us to set other properties like `badgeBackgroundColor` and `badgeTextAttributes` in the `UITabBarItemAppearance` layouts.
+        ///
+        if #available(iOS 13.0, *) {
+            let standardAppearance = UITabBarAppearance()
+            standardAppearance.backgroundColor = .appTabBar
+            standardAppearance.shadowColor = .systemColor(.separator)
+            applyItemWooAppearance(to: standardAppearance.inlineLayoutAppearance)
+            applyItemWooAppearance(to: standardAppearance.stackedLayoutAppearance)
+            applyItemWooAppearance(to: standardAppearance.compactInlineLayoutAppearance)
+            appearance.standardAppearance = standardAppearance
+        }
+    }
+
+    @available(iOS 13.0, *)
+    private class func applyItemWooAppearance(to layoutAppearance: UITabBarItemAppearance) {
+        layoutAppearance.normal.badgeTextAttributes = [.foregroundColor: UIColor.textInverted]
+        layoutAppearance.selected.badgeTextAttributes = [.foregroundColor: UIColor.textInverted]
+        layoutAppearance.disabled.badgeTextAttributes = [.foregroundColor: UIColor.textInverted]
+        layoutAppearance.normal.badgeBackgroundColor = .primary
+        layoutAppearance.selected.badgeBackgroundColor = .primary
+        layoutAppearance.disabled.badgeBackgroundColor = .primary
     }
 }

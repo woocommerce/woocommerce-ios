9976d6737ed933b914e1f8c158aff129a580b4c1
Shiki <jayson@basanes.net>
2020-03-19 11:32:33 -0600

    OrdersVM: Add more test scenarios
    The last scenario fails because I failed to implement it.

--- WooCommerce/WooCommerceTests/ViewRelated/Orders/OrdersViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Orders/OrdersViewModelTests.swift
@@ -24,8 +24,10 @@ final class OrdersViewModelTests: XCTestCase {
     // 2. fetching both the filtered list and the "all orders" list
     //
     func testPullingToRefreshOnFilteredListItDeletesAndPerformsDualFetch() {
+        // Arrange
         let viewModel = OrdersViewModel()
 
+        // Act
         let action = viewModel.synchronizationAction(
             siteID: siteID,
             statusKey: OrderStatusEnum.processing.rawValue,
@@ -34,12 +36,68 @@ final class OrdersViewModelTests: XCTestCase {
             reason: SyncReason.pullToRefresh,
             completionHandler: unimportantCompletionHandler)
 
+        // Assert
         guard case .fetchFilteredAndAllOrders(_, let statusKey, let deleteAllBeforeSaving, _, _) = action else {
-            XCTFail("Unexpected OrderAction type")
+            XCTFail("Unexpected OrderAction type: \(action)")
             return
         }
 
         XCTAssertTrue(deleteAllBeforeSaving)
         XCTAssertEqual(statusKey, OrderStatusEnum.processing.rawValue)
+    }
+
+    // Test that when fetching the first page for reasons other than pull-to-refresh
+    // (e.g. `viewWillAppear`), the action returned will only be for dual fetching of the
+    // filtered list and the all orders list.
+    //
+    func testFirstPageLoadOnFilteredListWithNonPullToRefreshReasonsWillOnlyPerformDualFetch() {
+        // Arrange
+        let viewModel = OrdersViewModel()
+
+        // Act
+        let action = viewModel.synchronizationAction(
+            siteID: siteID,
+            statusKey: OrderStatusEnum.processing.rawValue,
+            pageNumber: Defaults.pageFirstIndex,
+            pageSize: pageSize,
+            reason: nil,
+            completionHandler: unimportantCompletionHandler)
+
+        // Assert
+        guard case .fetchFilteredAndAllOrders(_, let statusKey, let deleteAllBeforeSaving, _, _) = action else {
+            XCTFail("Unexpected OrderAction type: \(action)")
+            return
+        }
+
+        XCTAssertFalse(deleteAllBeforeSaving)
+        XCTAssertEqual(statusKey, OrderStatusEnum.processing.rawValue)
+    }
+
+    // Test that when pulling to refresh on the All Orders tab, the action returned will be for:
+    //
+    // 1. Deleting all the orders
+    // 2. Fetching the first page of all orders (any status)
+    //
+    func testPullingToRefreshOnAllOrdersListDeletesAndFetchesFirstPage() {
+        // Arrange
+        let viewModel = OrdersViewModel()
+
+        // Act
+        let action = viewModel.synchronizationAction(
+            siteID: siteID,
+            statusKey: nil,
+            pageNumber: Defaults.pageFirstIndex,
+            pageSize: pageSize,
+            reason: SyncReason.pullToRefresh,
+            completionHandler: unimportantCompletionHandler)
+
+        // Assert
+        guard case .fetchFilteredAndAllOrders(_, let statusKey, let deleteAllBeforeSaving, _, _) = action else {
+            XCTFail("Unexpected OrderAction type: \(action)")
+            return
+        }
+
+        XCTAssertTrue(deleteAllBeforeSaving)
+        XCTAssertNil(statusKey)
     }
 }

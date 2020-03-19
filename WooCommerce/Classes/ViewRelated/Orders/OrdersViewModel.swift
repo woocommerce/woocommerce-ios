0af052d9fc34553fe0af49721bf0f77eb6f3d914
Shiki <jayson@basanes.net>
2020-03-19 09:20:29 -0600

    Add OrdersViewModel

---
+++ WooCommerce/Classes/ViewRelated/Orders/OrdersViewModel.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+
+/// ViewModel for `OrdersViewController`.
+///
+/// This is an incremental WIP. Eventually, we should move all the data loading in here.
+///
+final class OrdersViewModel {
+
+}

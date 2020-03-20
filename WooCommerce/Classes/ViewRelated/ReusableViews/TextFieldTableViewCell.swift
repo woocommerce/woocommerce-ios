1d28bbf49ab310a4314caea1d127aa5dd479a5fe
Jaclyn Chen <watertranquil@gmail.com>
2020-03-20 17:11:27 +0800

    Create `TextFieldTableViewCell` for product name.

---
+++ WooCommerce/Classes/ViewRelated/ReusableViews/TextFieldTableViewCell.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+/// Contains a text field.
+///
+final class TextFieldTableViewCell: UITableViewCell {
+    struct ViewModel {
+        let text: String?
+        let placeholder: String?
+        let onTextChange: ((_ text: String?) -> Void)?
+    }
+
+    @IBOutlet private weak var textField: UITextField!
+
+    private var onTextChange: ((_ text: String?) -> Void)?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        configureTextField()
+        applyDefaultBackgroundStyle()
+    }
+
+    func configure(viewModel: ViewModel) {
+        onTextChange = viewModel.onTextChange
+
+        textField.text = viewModel.text
+        textField.placeholder = viewModel.placeholder
+        textField.borderStyle = .none
+        textField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
+    }
+}
+
+private extension TextFieldTableViewCell {
+    func configureTextField() {
+        textField.clearButtonMode = .whileEditing
+        textField.applyBodyStyle()
+    }
+}
+
+private extension TextFieldTableViewCell {
+    @objc func textFieldDidChange(textField: UITextField) {
+        onTextChange?(textField.text)
+    }
+}

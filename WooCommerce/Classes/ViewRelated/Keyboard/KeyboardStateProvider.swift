2e567c721133e58e0e893c36bfbffd7f0835f164
Shiki <jayson@basanes.net>
2020-03-31 11:33:16 -0600

    KeyboardStateProvider: Add comments

--- WooCommerce/Classes/ViewRelated/Keyboard/KeyboardStateProvider.swift
+++ WooCommerce/Classes/ViewRelated/Keyboard/KeyboardStateProvider.swift
@@ -1,18 +1,40 @@
 
 import UIKit
 
+/// Keyboard information usually received from `Notifications`.
+///
 struct KeyboardState {
+    /// True if the keyboard is visible.
     let isVisible: Bool
+    /// The frame of the keyboard when it is fully shown or hidden.
+    ///
+    /// The value is usually from `UIResponder.keyboardFrameEndUserInfoKey`.
     let frameEnd: CGRect
 }
 
+/// Provides the last known state of the keyboard.
+///
 protocol KeyboardStateProviding {
+    /// The last known state of the keyboard.
     var state: KeyboardState { get }
 }
 
+/// Provides and tracks the last known state of the keyboard.
+///
+/// This does not work very well as a one-off instance. It has to be instantiated and **kept**
+/// when the app is started (i.e. `UIApplicationDelegate`) so it can _track_ the keyboard changes.
+/// Only then will this be able to accurately provide the last known state of the keyboard.
+///
+/// Initially, this assumes that the keyboard is not visible.
+///
 final class KeyboardStateProvider: KeyboardStateProviding {
+    /// The last known state.
+    ///
+    /// This is kept up to date whenever we receive keyboard notifications.
+    ///
     private(set) var state: KeyboardState = KeyboardState(isVisible: false, frameEnd: .zero)
 
+    /// NSNotification observers that will be removed on deinit
     private var observations = [Any]()
 
     init() {
@@ -40,6 +62,8 @@ final class KeyboardStateProvider: KeyboardStateProviding {
 }
 
 private extension Notification {
+    /// The `CGRect` value of `UIResponder.keyboardFrameEndUserInfoKey` from self.
+    ///
     var keyboardFrameEnd: CGRect? {
         guard let rectAsValue = userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
             return nil

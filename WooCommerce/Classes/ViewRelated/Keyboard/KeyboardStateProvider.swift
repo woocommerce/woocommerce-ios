4c9538f43db3f86e4c3458ba67819a077d518a4a
Shiki <jayson@basanes.net>
2020-03-31 10:00:24 -0600

    KeyboardStateProvider: Include `frameEnd` in `KeyboardState`

--- WooCommerce/Classes/ViewRelated/Keyboard/KeyboardStateProvider.swift
+++ WooCommerce/Classes/ViewRelated/Keyboard/KeyboardStateProvider.swift
@@ -1,9 +1,9 @@
 
 import UIKit
 
-enum KeyboardState {
-    case hidden
-    case shown
+struct KeyboardState {
+    let isVisible: Bool
+    let frameEnd: CGRect
 }
 
 protocol KeyboardStateProviding {
@@ -11,7 +11,7 @@ protocol KeyboardStateProviding {
 }
 
 final class KeyboardStateProvider: KeyboardStateProviding {
-    private(set) var state: KeyboardState = .hidden
+    private(set) var state: KeyboardState = KeyboardState(isVisible: false, frameEnd: .zero)
 
     private var observations = [Any]()
 
@@ -19,19 +19,36 @@ final class KeyboardStateProvider: KeyboardStateProviding {
         let nc = NotificationCenter.default
 
         observations.append(
-            nc.addObserver(forName: UIResponder.keyboardDidShowNotification, object: nil, queue: nil) { [weak self] _ in
-                self?.state = .shown
+            nc.addObserver(forName: UIResponder.keyboardDidShowNotification, object: nil, queue: nil) { [weak self] notification in
+                self?.updateState(from: notification)
             }
         )
         observations.append(
-            nc.addObserver(forName: UIResponder.keyboardDidHideNotification, object: nil, queue: nil) { [weak self] _ in
-                self?.state = .hidden
+            nc.addObserver(forName: UIResponder.keyboardDidHideNotification, object: nil, queue: nil) { [weak self] notification in
+                self?.updateState(from: notification)
             }
+        )
+    }
+
+    private func updateState(from notification: Notification) {
+        state = KeyboardState(
+            isVisible: notification.name == UIResponder.keyboardDidShowNotification,
+            frameEnd: notification.keyboardFrameEnd ?? .zero
         )
     }
 
     deinit {
         observations.forEach(NotificationCenter.default.removeObserver)
+    }
+}
+
+private extension Notification {
+    var keyboardFrameEnd: CGRect? {
+        guard let rectAsValue = userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return nil
+        }
+
+        return rectAsValue.cgRectValue
     }
 }
 

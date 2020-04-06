acb51d6f164814088707c4f844414d815c6262f1
Paolo Musolino <info@codeido.com>
2020-04-06 16:23:23 +0200

    Fixed spaces

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Slug/ProductSlugViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Slug/ProductSlugViewController.swift
@@ -4,16 +4,16 @@ import Yosemite
 final class ProductSlugViewController: UIViewController {
 
     @IBOutlet weak private var tableView: UITableView!
-    
+
     // Completion callback
     //
     typealias Completion = (_ productSettings: ProductSettings) -> Void
     private let onCompletion: Completion
 
     private let productSettings: ProductSettings
-    
+
     private let sections: [Section]
-    
+
     /// Init
     ///
     init(settings: ProductSettings, completion: @escaping Completion) {
@@ -24,18 +24,18 @@ final class ProductSlugViewController: UIViewController {
         onCompletion = completion
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationBar()
         configureMainView()
         configureTableView()
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         onCompletion(productSettings)

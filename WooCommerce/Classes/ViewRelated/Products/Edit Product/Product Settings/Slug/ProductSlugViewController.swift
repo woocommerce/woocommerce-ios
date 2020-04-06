6fb7d0797576587e32af76db217e2efa18d0053b
Paolo Musolino <info@codeido.com>
2020-04-06 16:12:02 +0200

    ProductSlugViewController basic configuration

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Slug/ProductSlugViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Slug/ProductSlugViewController.swift
@@ -1,12 +1,61 @@
 import UIKit
+import Yosemite
 
-class ProductSlugViewController: UIViewController {
+final class ProductSlugViewController: UIViewController {
 
     @IBOutlet weak private var tableView: UITableView!
+    
+    // Completion callback
+    //
+    typealias Completion = (_ productSettings: ProductSettings) -> Void
+    private let onCompletion: Completion
+
+    private let productSettings: ProductSettings
+    
+    /// Init
+    ///
+    init(settings: ProductSettings, completion: @escaping Completion) {
+        productSettings = settings
+        onCompletion = completion
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        onCompletion(productSettings)
+    }
 
+}
+
+// MARK: - View Configuration
+//
+private extension ProductSlugViewController {
+
+    func configureNavigationBar() {
+        title = NSLocalizedString("Slug", comment: "Product Slug navigation title")
+
+        removeNavigationBackBarButtonText()
+    }
+
+    func configureMainView() {
+        view.backgroundColor = .listBackground
+    }
+
+    func configureTableView() {
+
+        //tableView.dataSource = self
+        //tableView.delegate = self
+
+        tableView.backgroundColor = .listBackground
+        tableView.removeLastCellSeparator()
+    }
 }

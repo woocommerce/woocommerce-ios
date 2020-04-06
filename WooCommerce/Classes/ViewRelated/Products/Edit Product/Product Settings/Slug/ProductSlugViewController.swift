24ce5a8ce57d6bde5c8a2a3cf136a85780ef2c5e
Paolo Musolino <info@codeido.com>
2020-04-06 16:06:34 +0200

    Generated ProductSlugViewController

---
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Slug/ProductSlugViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ProductSlugViewController: UIViewController {
+
+    @IBOutlet weak private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}

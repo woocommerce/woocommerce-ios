53421690796b57b6931de0606e9751a9646bd841
Shiki <jayson@basanes.net>
2020-04-07 15:38:54 -0600

    String+HTMLTests: Fix whitespace violations

--- Networking/NetworkingTests/Extensions/String+HTMLTests.swift
+++ Networking/NetworkingTests/Extensions/String+HTMLTests.swift
@@ -75,12 +75,12 @@ private extension String_HTMLTests {
                 """
                 <h1>HTML Ipsum Presents</h1>
 
-                <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis 
-                        egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu 
-                        libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat 
-                        eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, 
-                        condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget 
-                        tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. 
+                <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis
+                        egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu
+                        libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat
+                        eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi,
+                        condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget
+                        tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
                         <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
 
                 <h2>Header Level 2</h2>
@@ -91,8 +91,8 @@ private extension String_HTMLTests {
                 </ol>
 
                 <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis
-                        aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at 
-                        sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus 
+                        aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at
+                        sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus
                         pretium ornare est.</p></blockquote>
 
                 <h3>Header Level 3</h3>
@@ -115,12 +115,12 @@ private extension String_HTMLTests {
                 """
                 HTML Ipsum Presents
 
-                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis 
-                        egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu 
-                        libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat 
-                        eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, 
-                        condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget 
-                        tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. 
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+                        egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu
+                        libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat
+                        eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi,
+                        condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget
+                        tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
                         Donec non enim in turpis pulvinar facilisis. Ut felis.
 
                 Header Level 2
@@ -128,8 +128,8 @@ private extension String_HTMLTests {
                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
                    Aliquam tincidunt mauris eu risus.
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis
-                        aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at 
-                        sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus 
+                        aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at
+                        sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus
                         pretium ornare est.
 
                 Header Level 3

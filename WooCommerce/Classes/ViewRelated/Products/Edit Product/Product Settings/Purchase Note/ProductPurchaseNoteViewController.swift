9c807bf3464c8b4958622bb8b7cd14054f4c08f0
Paolo Musolino <info@codeido.com>
2020-04-13 17:41:56 +0200

    Added the new ProductPurchaseNoteViewController

---
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Purchase Note/ProductPurchaseNoteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProductPurchaseNoteViewController.swift
+//  WooCommerce
+//
+//  Created by Paolo Musolino on 13/04/2020.
+//  Copyright © 2020 Automattic. All rights reserved.
+//
+
+import UIKit
+
+class ProductPurchaseNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

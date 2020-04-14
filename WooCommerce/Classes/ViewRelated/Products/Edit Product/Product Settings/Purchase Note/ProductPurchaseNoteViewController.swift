2688eaba2814b86af35d1ce699128a6606573aa0
Paolo Musolino <info@codeido.com>
2020-04-14 14:49:38 +0200

    ProductPurchaseNoteViewController init and sections

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Purchase Note/ProductPurchaseNoteViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Purchase Note/ProductPurchaseNoteViewController.swift
@@ -1,16 +1,33 @@
-//
-//  ProductPurchaseNoteViewController.swift
-//  WooCommerce
-//
-//  Created by Paolo Musolino on 13/04/2020.
-//  Copyright © 2020 Automattic. All rights reserved.
-//
-
 import UIKit
+import Yosemite
 
 class ProductPurchaseNoteViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView!
+    
+    // Completion callback
+    //
+    typealias Completion = (_ productSettings: ProductSettings) -> Void
+    private let onCompletion: Completion
+
+    private let productSettings: ProductSettings
+    
+    private let sections: [Section]
+    
+    /// Init
+    ///
+    init(settings: ProductSettings, completion: @escaping Completion) {
+        productSettings = settings
+        let footerText = NSLocalizedString("An optional note to send the customer after purchase",
+                                           comment: "Footer text in Product Purchase Note screen")
+        sections = [Section(footer: footerText, rows: [.purchaseNote])]
+        onCompletion = completion
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,15 +35,35 @@ class ProductPurchaseNoteViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: - Constants
+//
+private extension ProductPurchaseNoteViewController {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /// Table Rows
+    ///
+    enum Row {
+        /// Listed in the order they appear on screen
+        case purchaseNote
+
+        var reuseIdentifier: String {
+            switch self {
+            case .purchaseNote:
+                return TextViewTableViewCell.reuseIdentifier
+            }
+        }
     }
-    */
 
+    /// Table Sections
+    ///
+    struct Section {
+        let footer: String?
+        let rows: [Row]
+
+        init(footer: String? = nil, rows: [Row]) {
+            self.footer = footer
+            self.rows = rows
+        }
+    }
 }

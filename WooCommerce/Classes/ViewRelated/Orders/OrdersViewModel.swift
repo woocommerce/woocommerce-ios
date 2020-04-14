e357ee059f3b18e48d1391f48ec6c391e1bd8f66
Shiki <jayson@basanes.net>
2020-04-14 10:29:49 -0600

    OrdersVM: Make resultsController private

--- WooCommerce/Classes/ViewRelated/Orders/OrdersViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/OrdersViewModel.swift
@@ -26,7 +26,7 @@ final class OrdersViewModel {
 
     /// Should be bound to the UITableView to auto-update the list of Orders.
     ///
-    private(set) lazy var resultsController: ResultsController<StorageOrder> = {
+    private lazy var resultsController: ResultsController<StorageOrder> = {
         let storageManager = ServiceLocator.storageManager
         let descriptor = NSSortDescriptor(keyPath: \StorageOrder.dateCreated, ascending: false)
 

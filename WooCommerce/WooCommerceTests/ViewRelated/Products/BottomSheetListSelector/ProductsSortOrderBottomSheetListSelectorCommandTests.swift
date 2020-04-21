55a1972194d311857571fdc7c6d9fa6874ba2be9
Jaclyn Chen <watertranquil@gmail.com>
2020-04-21 16:30:19 +0800

    Update variable names in `ProductsSortOrderBottomSheetListSelectorCommandTests` and fix build warnings.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/BottomSheetListSelector/ProductsSortOrderBottomSheetListSelectorCommandTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/BottomSheetListSelector/ProductsSortOrderBottomSheetListSelectorCommandTests.swift
@@ -5,42 +5,42 @@ import XCTest
 final class ProductsSortOrderBottomSheetListSelectorCommandTests: XCTestCase {
     func testInitialSelectedValue() {
         let selected = ProductsSortOrder.nameAscending
-        let dataSource = ProductsSortOrderBottomSheetListSelectorCommand(selected: selected)
-        XCTAssertEqual(dataSource.selected, selected)
+        let command = ProductsSortOrderBottomSheetListSelectorCommand(selected: selected)
+        XCTAssertEqual(command.selected, selected)
     }
 
     func testSelectedValueAfterChange() {
         // Arrange
         let selected = ProductsSortOrder.nameAscending
-        var dataSource = ProductsSortOrderBottomSheetListSelectorCommand(selected: selected)
+        let command = ProductsSortOrderBottomSheetListSelectorCommand(selected: selected)
 
         // Action
         let newSelected = ProductsSortOrder.dateDescending
-        dataSource.handleSelectedChange(selected: newSelected)
+        command.handleSelectedChange(selected: newSelected)
 
         // Assert
-        XCTAssertEqual(dataSource.selected, newSelected)
+        XCTAssertEqual(command.selected, newSelected)
     }
 
     func testIsSelectedValueAfterChange() {
         // Arrange
         let selected = ProductsSortOrder.nameAscending
         let notSelected: [ProductsSortOrder] = [.nameDescending, .dateAscending, .dateDescending]
-        var dataSource = ProductsSortOrderBottomSheetListSelectorCommand(selected: selected)
-        XCTAssertTrue(dataSource.isSelected(model: selected))
+        let command = ProductsSortOrderBottomSheetListSelectorCommand(selected: selected)
+        XCTAssertTrue(command.isSelected(model: selected))
         notSelected.forEach { notSelectedSortOrder in
-            XCTAssertFalse(dataSource.isSelected(model: notSelectedSortOrder))
+            XCTAssertFalse(command.isSelected(model: notSelectedSortOrder))
         }
 
         // Action
         let newSelected = ProductsSortOrder.dateDescending
         let newNotSelected: [ProductsSortOrder] = [.nameDescending, .nameAscending, .dateAscending]
-        dataSource.handleSelectedChange(selected: newSelected)
+        command.handleSelectedChange(selected: newSelected)
 
         // Assert
-        XCTAssertTrue(dataSource.isSelected(model: newSelected))
+        XCTAssertTrue(command.isSelected(model: newSelected))
         newNotSelected.forEach { notSelectedSortOrder in
-            XCTAssertFalse(dataSource.isSelected(model: notSelectedSortOrder))
+            XCTAssertFalse(command.isSelected(model: notSelectedSortOrder))
         }
     }
 }

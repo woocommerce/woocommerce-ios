da595f366eadb95601d386039396b32d0c8d5635
Jaclyn Chen <watertranquil@gmail.com>
2020-04-21 15:10:13 +0800

    Move `ProductsSortOrder`'s `actionSheetTitle` to its usage and make it private.

--- WooCommerce/Classes/ViewRelated/Products/BottomSheetListSelector/ProductsSortOrderBottomSheetListSelectorCommand.swift
+++ WooCommerce/Classes/ViewRelated/Products/BottomSheetListSelector/ProductsSortOrderBottomSheetListSelectorCommand.swift
@@ -1,5 +1,22 @@
 import Yosemite
 
+private extension ProductsSortOrder {
+    /// Title shown on the action sheet.
+    ///
+    var actionSheetTitle: String {
+        switch self {
+        case .dateAscending:
+            return NSLocalizedString("Date: Oldest to Newest", comment: "Action sheet option to sort products from the oldest to the newest")
+        case .dateDescending:
+            return NSLocalizedString("Date: Newest to Oldest", comment: "Action sheet option to sort products from the newest to the oldest")
+        case .nameAscending:
+            return NSLocalizedString("Title: A to Z", comment: "Action sheet option to sort products by ascending product name")
+        case .nameDescending:
+            return NSLocalizedString("Title: Z to A", comment: "Action sheet option to sort products by descending product name")
+        }
+    }
+}
+
 /// `BottomSheetListSelectorCommand` for selecting a sort order for the Products tab.
 ///
 struct ProductsSortOrderBottomSheetListSelectorCommand: BottomSheetListSelectorCommand {

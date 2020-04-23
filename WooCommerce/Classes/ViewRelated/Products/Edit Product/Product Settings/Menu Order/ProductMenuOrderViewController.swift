15575e785ac6b3eb1d4506b3516e74a85bf3add5
Paolo Musolino <info@codeido.com>
2020-04-23 16:42:37 +0200

    New ProductMenuOrderViewController

---
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Menu Order/ProductMenuOrderViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+import Yosemite
+
+final class ProductMenuOrderViewController: UIViewController {
+
+    @IBOutlet weak private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

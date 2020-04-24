f5d50c4019194edb6caed0ac80f68df3d1dc12a1
Shiki <jayson@basanes.net>
2020-04-24 07:29:51 -0600

    PublishSubject: Add tests to prove it's replaceable with Combine

--- WooCommerce/WooCommerceTests/Tools/Observables/PublishSubjectTests.swift
+++ WooCommerce/WooCommerceTests/Tools/Observables/PublishSubjectTests.swift
@@ -3,6 +3,8 @@ import XCTest
 import Foundation
 @testable import WooCommerce
 
+import Combine
+
 /// Tests cases for `PublishSubject`
 ///
 final class PublishSubjectTests: XCTestCase {
@@ -103,6 +105,154 @@ final class PublishSubjectTests: XCTestCase {
         // When
         // Cancel the observer
         observationToken.cancel()
+
+        // This will not be emitted anymore
+        subject.send("amet")
+
+        // Then
+        XCTAssertEqual(emittedValues, ["dicta"])
+    }
+
+    // MARK: - Proof of Compatibility with Combine's PassthroughSubject
+
+    func testCombinePassthroughSubjectEmitsValuesToAnObserver() throws {
+        guard #available(iOS 13.0, *) else {
+            try XCTSkipIf(true, "This test is for iOS 13.0+ only")
+            return
+        }
+
+        // Given
+        let subject = PassthroughSubject<String, Error>()
+        var disposeBag = Set<AnyCancellable>()
+
+        var emittedValues = [String]()
+        subject.sink(receiveCompletion: { _ in
+            // noop
+        }) { value in
+            emittedValues.append(value)
+        }.store(in: &disposeBag)
+
+        // When
+        subject.send("dicta")
+
+        // Then
+        XCTAssertEqual(emittedValues, ["dicta"])
+    }
+
+    func testCombinePassthroughSubjectContinuouslyEmitsValuesToAnObserver() throws {
+        guard #available(iOS 13.0, *) else {
+            try XCTSkipIf(true, "This test is for iOS 13.0+ only")
+            return
+        }
+
+        // Given
+        let subject = PassthroughSubject<String, Error>()
+        var disposeBag = Set<AnyCancellable>()
+
+        var emittedValues = [String]()
+        subject.sink(receiveCompletion: { _ in
+            // noop
+        }) { value in
+            emittedValues.append(value)
+        }.store(in: &disposeBag)
+
+        // When
+        subject.send("dicta")
+        subject.send("amet")
+        subject.send("dolor")
+
+        // Then
+        XCTAssertEqual(emittedValues, ["dicta", "amet", "dolor"])
+    }
+
+    func testCombinePassthroughSubjectDoesNotEmitValuesBeforeTheSubscription() throws {
+        guard #available(iOS 13.0, *) else {
+            try XCTSkipIf(true, "This test is for iOS 13.0+ only")
+            return
+        }
+
+        // Given
+        let subject = PassthroughSubject<String, Error>()
+        var disposeBag = Set<AnyCancellable>()
+
+        var emittedValues = [String]()
+
+        // When
+        // These are not emitted because there's no observer yet
+        subject.send("dicta")
+        subject.send("amet")
+
+        // Add the observer
+        subject.sink(receiveCompletion: { _ in
+            // noop
+        }) { value in
+            emittedValues.append(value)
+        }.store(in: &disposeBag)
+
+        // This will be emitted to the observer
+        subject.send("dolor")
+
+        // Then
+        XCTAssertEqual(emittedValues, ["dolor"])
+    }
+
+    func testCombinePassthroughSubjectEmitsValuesToAllObservers() throws {
+        guard #available(iOS 13.0, *) else {
+            try XCTSkipIf(true, "This test is for iOS 13.0+ only")
+            return
+        }
+
+        // Given
+        let subject = PassthroughSubject<String, Error>()
+        var disposeBag = Set<AnyCancellable>()
+
+        var emittedValues = [String]()
+
+        // First observer
+        subject.sink(receiveCompletion: { _ in
+            // noop
+        }) { value in
+            emittedValues.append(value)
+        }.store(in: &disposeBag)
+
+        // Second observer
+        subject.sink(receiveCompletion: { _ in
+            // noop
+        }) { value in
+            emittedValues.append(value)
+        }.store(in: &disposeBag)
+
+        // When
+        subject.send("dicta")
+        subject.send("amet")
+
+        // Then
+        // We'll receive two values for each observer
+        XCTAssertEqual(emittedValues, ["dicta", "dicta", "amet", "amet"])
+    }
+
+    func testCombinePassthroughSubjectDoesNotEmitValuesToCancelledObservers() throws {
+        guard #available(iOS 13.0, *) else {
+            try XCTSkipIf(true, "This test is for iOS 13.0+ only")
+            return
+        }
+
+        // Given
+        let subject = PassthroughSubject<String, Error>()
+
+        var emittedValues = [String]()
+        let cancellable = subject.sink(receiveCompletion: { _ in
+            // noop
+        }) { value in
+            emittedValues.append(value)
+        }
+
+        // This will be emitted because the observer is active
+        subject.send("dicta")
+
+        // When
+        // Cancel the observer
+        cancellable.cancel()
 
         // This will not be emitted anymore
         subject.send("amet")

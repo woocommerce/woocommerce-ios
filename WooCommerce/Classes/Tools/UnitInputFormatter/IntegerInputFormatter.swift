a749ca7b78dc103e4f30c0c3a4139e747b1f9f1f
Paolo Musolino <info@codeido.com>
2020-04-24 14:28:34 +0200

    Updated comment

--- WooCommerce/Classes/Tools/UnitInputFormatter/IntegerInputFormatter.swift
+++ WooCommerce/Classes/Tools/UnitInputFormatter/IntegerInputFormatter.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-/// `UnitInputFormatter` implementation for integer number input (positive, zero, negative)
+/// `UnitInputFormatter` implementation for integer number input (positive, negative, or zero)
 ///
 struct IntegerInputFormatter: UnitInputFormatter {
     private let numberFormatter: NumberFormatter = {

156149e85abfcf42b68c9bb2343becdcdcddd670
Shiki <jayson@basanes.net>
2020-04-24 10:13:34 -0600

    Add more comments to ForegroundNotification

--- WooCommerce/Classes/ServiceLocator/ForegroundNotification.swift
+++ WooCommerce/Classes/ServiceLocator/ForegroundNotification.swift
@@ -6,7 +6,13 @@ import struct Yosemite.Note
 /// the app is active.
 ///
 struct ForegroundNotification {
+    /// The `note_id` value received from the Remote Notification's `userInfo`.
+    ///
     let noteID: Int
+    /// The `type` value received from the Remote Notification's `userInfo`.
+    ///
     let kind: Note.Kind
+    /// The `alert` value received from the Remote Notification's `userInfo`.
+    ///
     let message: String
 }

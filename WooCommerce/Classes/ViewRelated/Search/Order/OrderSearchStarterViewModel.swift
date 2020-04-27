f87b7a98c175b5c6df41da568eb1c93a6a3a2469
Shiki <jayson@basanes.net>
2020-04-27 12:30:37 -0600

    OrderSearchStarterVM: Remove unnecessary DataSource

--- WooCommerce/Classes/ViewRelated/Search/Order/OrderSearchStarterViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Search/Order/OrderSearchStarterViewModel.swift
@@ -8,7 +8,16 @@ import Yosemite
 /// This encapsulates all the `OrderStatus` data loading and `UITableViewCell` presentation.
 ///
 final class OrderSearchStarterViewModel {
-    private lazy var dataSource = DataSource()
+    private let storageManager = ServiceLocator.storageManager
+    private let siteID = ServiceLocator.stores.sessionManager.defaultStoreID ?? Int64.min
+
+    private lazy var resultsController: ResultsController<StorageOrderStatus> = {
+        let descriptor = NSSortDescriptor(key: "slug", ascending: true)
+        let predicate = NSPredicate(format: "siteID == %lld", siteID)
+        return ResultsController<StorageOrderStatus>(storageManager: storageManager,
+                                                     matching: predicate,
+                                                     sortedBy: [descriptor])
+    }()
 
     /// Start all the operations that this `ViewModel` is responsible for.
     ///
@@ -18,9 +27,9 @@ final class OrderSearchStarterViewModel {
     ///     - tableView: The table to use for the results. This is not retained by this class.
     ///
     func activate(using tableView: UITableView) {
-        dataSource.startForwardingEvents(to: tableView)
+        resultsController.startForwardingEvents(to: tableView)
 
-        try? dataSource.performFetch()
+        try? resultsController.performFetch()
     }
 }
 
@@ -30,43 +39,12 @@ extension OrderSearchStarterViewModel {
     /// The number of DB results
     ///
     var numberOfObjects: Int {
-        dataSource.resultsController.numberOfObjects
+        resultsController.numberOfObjects
     }
 
     /// The `OrderStatus` located at `indexPath`.
     ///
     func orderStatus(at indexPath: IndexPath) -> OrderStatus {
-        dataSource.resultsController.object(at: indexPath)
-    }
-}
-
-private extension OrderSearchStarterViewModel {
-    /// Encpsulates data loading and presentation of the `UITableViewCells`.
-    ///
-    final class DataSource: NSObject {
-        private let storageManager = ServiceLocator.storageManager
-        private let siteID = ServiceLocator.stores.sessionManager.defaultStoreID ?? Int64.min
-
-        private(set) lazy var resultsController: ResultsController<StorageOrderStatus> = {
-            let descriptor = NSSortDescriptor(key: "slug", ascending: true)
-            let predicate = NSPredicate(format: "siteID == %lld", siteID)
-            return ResultsController<StorageOrderStatus>(storageManager: storageManager,
-                                                         matching: predicate,
-                                                         sortedBy: [descriptor])
-        }()
-
-        /// Run the query to fetch all the `OrderStatus`.
-        ///
-        func performFetch() throws {
-            try resultsController.performFetch()
-        }
-
-        /// Attach events so the `tableView` is always kept up to date.
-        ///
-        /// This should only be called once.
-        ///
-        func startForwardingEvents(to tableView: UITableView) {
-            resultsController.startForwardingEvents(to: tableView)
-        }
+        resultsController.object(at: indexPath)
     }
 }

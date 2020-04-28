b95c6b00f1737ebb08167e663f284fe2974e81e9
Shiki <jayson@basanes.net>
2020-04-28 13:34:12 -0600

    Move MainTabViewModel.readableCount to reusable method
    This will be used later.

---
+++ WooCommerce/WooCommerceTests/Extensions/NumberFormatter+LocalizedUpToNinetyNineTests.swift
@@ -0,0 +1,25 @@
+
+import XCTest
+
+@testable import WooCommerce
+
+final class NumberFormatterLocalizedUpToNinetyNineTests: XCTestCase {
+    
+    func testItReturnsNinetyNinePlusIfTheNumberIsGreaterThanNinetyNine() {
+        let localized = NumberFormatter.localizedUpToNinetyNine(100)
+
+        XCTAssertEqual(localized, NSLocalizedString("99+", comment: ""))
+    }
+
+    func testItReturnsTheLocalizedNumberIfTheNumberIsLessThanNinetyNine() {
+        let localized = NumberFormatter.localizedUpToNinetyNine(98)
+
+        XCTAssertEqual(localized, NumberFormatter.localizedString(from: NSNumber(value: 98), number: .none))
+    }
+
+    func testItReturnsTheLocalizedNumberIfTheNumberIsNinetyNine() {
+        let localized = NumberFormatter.localizedUpToNinetyNine(99)
+
+        XCTAssertEqual(localized, NumberFormatter.localizedString(from: NSNumber(value: 99), number: .none))
+    }
+}

93f3640381176be586a7989bde04494613a7a911
Jaclyn Chen <watertranquil@gmail.com>
2020-04-28 10:29:59 +0800

    Filter Products commands: create `FilterProductListCommand` that implements `FilterListCommand` and `FilterProductListSelectorCommand` that implements `ListSelectorCommand`.

---
+++ WooCommerce/Classes/ViewRelated/Products/Filters/FilterProductListCommand.swift
@@ -0,0 +1,148 @@
+import UIKit
+import Yosemite
+
+/// `FilterListCommand` for filtering a list of products.
+final class FilterProductListCommand: FilterListCommand {
+    /// Aggregates the filter values that can be updated in the Filter Products UI.
+    struct Filters {
+        let stockStatus: ProductStockStatus?
+        let productStatus: ProductStatus?
+        let productType: ProductType?
+    }
+
+    typealias ListSelectorCommand = FilterProductListSelectorCommand
+
+    let filterActionTitle = NSLocalizedString("Show Products", comment: "Button title for applying filters to a list of products.")
+
+    /// Set by `FilterListViewController` in `viewDidLoad`.
+    var onClearAllActionVisibilityChanged: ((_ isClearAllActionVisible: Bool) -> Void)?
+
+    /// The Clear All CTA is only visible when at least one filter is applied.
+    var isClearAllActionVisible: Bool {
+        return hasAppliedAnyFilters(filters: filterListSelectorCommand.filters)
+    }
+
+    /// Used to configure the list of filters, with a callback when the user taps on a filter row.
+    private(set) lazy var filterListSelectorCommand: ListSelectorCommand = {
+        FilterProductListSelectorCommand(filters: originalFilters) { [weak self] filter, viewController in
+            guard let self = self else {
+                return
+            }
+            self.onFilterSelected(filter, viewController: viewController)
+        }
+    }()
+
+    /// Used to initialize `filterListSelectorCommand` and also compare for outstanding changes on the filters for the discard changes action sheet.
+    private let originalFilters: Filters
+
+    /// Used to dismiss the Filter Products UI.
+    private let sourceViewController: UIViewController
+
+    typealias FilterCompletion = (_ filters: Filters) -> Void
+    private let onFilterCompletion: FilterCompletion
+
+    /// - Parameters:
+    ///   - sourceViewController: the view controller that presents the Filter Products UI, used to dismiss the screen upon user actions.
+    ///   - filters: the filters to be applied initially.
+    ///   - onFilterCompletion: called when the user taps the Filter CTA to apply the latest filters to the product list.
+    init(sourceViewController: UIViewController, filters: Filters, onFilterCompletion: @escaping FilterCompletion) {
+        self.sourceViewController = sourceViewController
+        self.originalFilters = filters
+        self.onFilterCompletion = onFilterCompletion
+    }
+
+    func onFilterSelected(_ filter: ProductListFilter, viewController: ListSelectorCommand.ViewController) {
+        let filterOptionListSelector: UIViewController
+        switch filter {
+        case .stockStatus:
+            let command = FilterProductsByStockStatusListSelectorCommand(selected: filterListSelectorCommand.filters.stockStatus) { [weak self] selected in
+                guard let self = self else {
+                    return
+                }
+
+                /// TODO: update to use `Copiable`.
+                let currentFilters = self.filterListSelectorCommand.filters
+                let newFilters = Filters(stockStatus: selected ?? nil, productStatus: currentFilters.productStatus, productType: currentFilters.productType)
+                self.onFiltersUpdate(filters: newFilters) { [weak viewController] in
+                    viewController?.reloadData()
+                }
+            }
+            filterOptionListSelector = ListSelectorViewController(command: command, onDismiss: { _ in })
+        case .productStatus:
+            let command = FilterProductsByProductStatusListSelectorCommand(selected: filterListSelectorCommand.filters.productStatus) { [weak self] selected in
+                guard let self = self else {
+                    return
+                }
+
+                /// TODO: update to use `Copiable`.
+                let currentFilters = self.filterListSelectorCommand.filters
+                let newFilters = Filters(stockStatus: currentFilters.stockStatus, productStatus: selected ?? nil, productType: currentFilters.productType)
+                self.onFiltersUpdate(filters: newFilters) { [weak viewController] in
+                    viewController?.reloadData()
+                }
+            }
+            filterOptionListSelector = ListSelectorViewController(command: command, onDismiss: { _ in })
+        default:
+            return
+        }
+
+        viewController.navigationController?.pushViewController(filterOptionListSelector, animated: true)
+    }
+
+    func onFilterActionTapped() {
+        sourceViewController.dismiss(animated: true) { [weak self] in
+            guard let self = self else {
+                return
+            }
+            let filters = self.filterListSelectorCommand.filters
+            self.onFilterCompletion(filters)
+        }
+    }
+
+    func onDismissActionTapped() {
+        sourceViewController.dismiss(animated: true, completion: nil)
+    }
+
+    func onClearAllActionTapped(onCompletion: @escaping () -> Void) {
+        onFiltersUpdate(filters: Filters(stockStatus: nil, productStatus: nil, productType: nil), onCompletion: onCompletion)
+    }
+}
+
+private extension FilterProductListCommand {
+    func onFiltersUpdate(filters: Filters, onCompletion: @escaping () -> Void) {
+        defer {
+            onCompletion()
+        }
+
+        guard filters != filterListSelectorCommand.filters else {
+            return
+        }
+
+        filterListSelectorCommand.filters = filters
+
+        let isClearAllActionVisible = hasAppliedAnyFilters(filters: filterListSelectorCommand.filters)
+        onClearAllActionVisibilityChanged?(isClearAllActionVisible)
+    }
+
+    func hasAppliedAnyFilters(filters: Filters) -> Bool {
+        return filters.stockStatus != nil || filters.productStatus != nil || filters.productType != nil
+    }
+}
+
+extension FilterProductListCommand {
+    /// Rows listed in the order they appear on screen
+    ///
+    enum ProductListFilter {
+        case stockStatus
+        case productStatus
+        case productType
+    }
+}
+
+extension FilterProductListCommand.Filters: Equatable {
+    static func == (lhs: FilterProductListCommand.Filters, rhs: FilterProductListCommand.Filters) -> Bool {
+        return lhs.stockStatus == rhs.stockStatus &&
+        lhs.productStatus == rhs.productStatus &&
+        lhs.productType == rhs.productType
+    }
+}

f0664ff2b3ab97171fec3c518922ed661d2de8c3
Paolo Musolino <info@codeido.com>
2020-05-05 16:22:17 +0200

    Added password to ProductSettings

--- Yosemite/Yosemite/Model/Products/ProductSettings.swift
+++ Yosemite/Yosemite/Model/Products/ProductSettings.swift
@@ -6,23 +6,26 @@ import Foundation
 public final class ProductSettings {
     public var status: ProductStatus
     public var featured: Bool
+    public var password: String
     public var catalogVisibility: ProductCatalogVisibility
     public var slug: String
     public var purchaseNote: String?
     public var menuOrder: Int
 
-    public init(status: ProductStatus, featured: Bool, catalogVisibility: ProductCatalogVisibility, slug: String, purchaseNote: String?, menuOrder: Int) {
+    public init(status: ProductStatus, featured: Bool, password: String, catalogVisibility: ProductCatalogVisibility, slug: String, purchaseNote: String?, menuOrder: Int) {
         self.status = status
         self.featured = featured
+        self.password = password
         self.catalogVisibility = catalogVisibility
         self.slug = slug
         self.purchaseNote = purchaseNote
         self.menuOrder = menuOrder
     }
 
-    public convenience init(from product: Product) {
+    public convenience init(from product: Product, password: String) {
         self.init(status: product.productStatus,
                   featured: product.featured,
+                  password: password,
                   catalogVisibility: product.productCatalogVisibility,
                   slug: product.slug,
                   purchaseNote: product.purchaseNote,
@@ -36,6 +39,7 @@ extension ProductSettings: Equatable {
     public static func == (lhs: ProductSettings, rhs: ProductSettings) -> Bool {
         return lhs.status.rawValue == rhs.status.rawValue &&
             lhs.featured == rhs.featured &&
+            lhs.password == rhs.password &&
             lhs.catalogVisibility.rawValue == rhs.catalogVisibility.rawValue &&
             lhs.slug == rhs.slug &&
             lhs.purchaseNote == rhs.purchaseNote &&

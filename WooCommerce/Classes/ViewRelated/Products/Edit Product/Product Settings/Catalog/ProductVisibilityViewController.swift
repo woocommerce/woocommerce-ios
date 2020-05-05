898770643ddda50abb50aff959e29077d09572c7
Paolo Musolino <info@codeido.com>
2020-05-05 14:42:31 +0200

    Created ProductVisibilityViewController + xib

---
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Catalog/ProductVisibilityViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProductVisibilityViewController.swift
+//  WooCommerce
+//
+//  Created by Paolo Musolino on 05/05/2020.
+//  Copyright © 2020 Automattic. All rights reserved.
+//
+
+import UIKit
+
+class ProductVisibilityViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

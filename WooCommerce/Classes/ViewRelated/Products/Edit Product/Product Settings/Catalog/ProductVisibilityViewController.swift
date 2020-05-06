bee0f6670432b98d83ea26e522721a7545f86297
Paolo Musolino <info@codeido.com>
2020-05-06 17:44:20 +0200

    ProductVisibilityViewController conformance to table view protocols

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Catalog/ProductVisibilityViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Catalog/ProductVisibilityViewController.swift
@@ -13,18 +13,25 @@ final class ProductVisibilityViewController: UIViewController {
     private let onCompletion: Completion
 
     private let productSettings: ProductSettings
+    
+    private var isPasswordSelected: Bool = false
+    
+    private var visibility: ProductVisibility = .publicVisibility
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationBar()
         configureMainView()
         configureTableView()
+        visibility = getProductVisibility(productSettings)
+        reloadSections()
     }
 
     /// Init
     ///
     init(settings: ProductSettings, completion: @escaping Completion) {
         productSettings = settings
+        isPasswordSelected = productSettings.password.isNotEmpty
         onCompletion = completion
         super.init(nibName: nil, bundle: nil)
     }
@@ -33,11 +40,32 @@ final class ProductVisibilityViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
-//    func reloadSections() {
-//        if productSettings.password.isEmpty {
-//            sections = [Section(rows: [.publicVisibility, .passwordVisibility, .privateVisibility])]
-//        }
-//    }
+    func reloadSections() {
+        if isPasswordSelected {
+            sections = [Section(rows: [.publicVisibility, .passwordVisibility, .passwordField, .privateVisibility])]
+        }
+        else {
+            sections = [Section(rows: [.publicVisibility, .passwordVisibility, .privateVisibility])]
+        }
+        tableView.reloadData()
+    }
+    
+    /**
+    * The visibility is determined by the status and the password. If the password isn't empty, then
+    * visibility is `passwordProtected`. If there's no password and the product status is `private`
+    * then the visibility is `privateVisibility`, otherwise it's `publicVisibility`.
+    */
+    func getProductVisibility(_ productSettings: ProductSettings) -> ProductVisibility {
+        if productSettings.password.isNotEmpty {
+            return .passwordProtected
+        }
+        else if productSettings.status == .privateStatus {
+            return .privateVisibility
+        }
+        else {
+            return .publicVisibility
+        }
+    }
 }
 
 // MARK: - View Configuration
@@ -57,11 +85,72 @@ private extension ProductVisibilityViewController {
     func configureTableView() {
         tableView.register(BasicTableViewCell.loadNib(), forCellReuseIdentifier: BasicTableViewCell.reuseIdentifier)
 
-        //tableView.dataSource = self
-        //tableView.delegate = self
+        tableView.dataSource = self
+        tableView.delegate = self
 
         tableView.backgroundColor = .listBackground
         tableView.removeLastCellSeparator()
+    }
+}
+
+// MARK: - UITableViewDataSource Conformance
+//
+extension ProductVisibilityViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].rows.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = sections[indexPath.section].rows[indexPath.row]
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: row.reuseIdentifier, for: indexPath)
+        configure(cell, for: row, at: indexPath)
+
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate Conformance
+//
+extension ProductVisibilityViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let row = sections[indexPath.section].rows[indexPath.row]
+        visibility = row.visibility
+        reloadSections()
+    }
+}
+
+// MARK: - Support for UITableViewDataSource
+//
+private extension ProductVisibilityViewController {
+    
+    /// Configure cellForRowAtIndexPath:
+    ///
+   func configure(_ cell: UITableViewCell, for row: Row, at indexPath: IndexPath) {
+        switch cell {
+        case let cell as BasicTableViewCell:
+            configureVisibilityCell(cell: cell, indexPath: indexPath)
+        default:
+            fatalError("Unidentified product catalog visibility row type")
+        }
+    }
+
+    func configureVisibilityCell(cell: BasicTableViewCell, indexPath: IndexPath) {
+        let row = sections[indexPath.section].rows[indexPath.row]
+        cell.selectionStyle = .default
+        cell.textLabel?.text = row.description
+        if row.editable {
+            cell.accessoryType = .none
+        }
+        else {
+            cell.accessoryType = row.visibility == visibility ? .checkmark : .none
+        }
     }
 }
 
@@ -97,6 +186,26 @@ extension ProductVisibilityViewController {
                 return NSLocalizedString("Private", comment: "One of the possible options in Product Visibility")
             }
         }
+        
+        var editable: Bool {
+            switch self {
+            case .passwordField:
+                return true
+            default:
+                return false
+            }
+        }
+        
+        var visibility: ProductVisibility {
+            switch self {
+            case .publicVisibility:
+                return .publicVisibility
+            case .passwordVisibility, .passwordField:
+                return .passwordProtected
+            case .privateVisibility:
+                return .privateVisibility
+            }
+        }
     }
 
     /// Table Sections
@@ -113,3 +222,37 @@ extension ProductVisibilityViewController {
         }
     }
 }
+
+
+// MARK: - Represents a ProductVisibility Entity
+//
+
+/// Represents a ProductVisibility Entity.
+///
+enum ProductVisibility {
+    
+    case publicVisibility
+    case passwordProtected
+    case privateVisibility
+}
+
+/// RawRepresentable Conformance
+///
+extension ProductVisibility {
+    
+    /// Designated Initializer.
+    ///
+    public init(status: ProductStatus, password: String) {
+        if password.isNotEmpty {
+            self = .passwordProtected
+        }
+        else if status == .privateStatus {
+            self = .privateVisibility
+        }
+        else {
+            self = .publicVisibility
+        }
+    }
+}
+    
+

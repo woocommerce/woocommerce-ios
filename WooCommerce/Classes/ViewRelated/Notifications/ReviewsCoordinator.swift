ed3674e51e316792a3286b7b990eb33edf3f9e28
Shiki <jayson@basanes.net>
2020-05-08 13:55:36 -0600

    ReviewsCoordinator: Observe "review" push notifications

--- WooCommerce/Classes/ViewRelated/Notifications/ReviewsCoordinator.swift
+++ WooCommerce/Classes/ViewRelated/Notifications/ReviewsCoordinator.swift
@@ -3,15 +3,35 @@ import Foundation
 import UIKit
 
 /// Coordinator for the Reviews tab.
-/// 
+///
 final class ReviewsCoordinator: Coordinator {
     var navigationController: UINavigationController
 
-    init() {
+    private let pushNotificationsManager: PushNotesManager
+
+    private var observationToken: ObservationToken?
+
+    init(pushNotificationsManager: PushNotesManager = ServiceLocator.pushNotesManager) {
+        self.pushNotificationsManager = pushNotificationsManager
+
         self.navigationController = WooNavigationController(rootViewController: ReviewsViewController())
     }
 
+    deinit {
+        observationToken?.cancel()
+    }
+
     func start() {
-        // noop
+        observationToken = pushNotificationsManager.inactiveNotifications.subscribe { [weak self] in
+            self?.handleInactiveNotification($0)
+        }
+    }
+
+    private func handleInactiveNotification(_ notification: ForegroundNotification) {
+        guard notification.kind == .comment else {
+            return
+        }
+
+
     }
 }

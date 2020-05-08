43ecd3e2eaa59def81751da3fa718aadf12f8088
Shiki <jayson@basanes.net>
2020-05-08 17:03:03 -0600

    XCTestCase+waitForExpectation: Use Constant as default TimeInterval

--- Networking/NetworkingTests/Testing/XCTestCase+Wait.swift
+++ Networking/NetworkingTests/Testing/XCTestCase+Wait.swift
@@ -7,6 +7,26 @@ extension XCTestCase {
     /// Example usage:
     ///
     /// ```
+    /// waitForExpectation(timeout: TimeInterval(10)) { expectation in
+    ///     doSomethingInTheBackground {
+    ///         expectation.fulfill()
+    ///     }
+    /// }
+    /// ```
+    ///
+    public func waitForExpectation(description: String? = nil,
+                                   timeout: TimeInterval,
+                                   _ block: (XCTestExpectation) -> ()) {
+        let exp = expectation(description: description ?? "")
+        block(exp)
+        wait(for: [exp], timeout: timeout)
+    }
+
+    /// Creates an XCTestExpectation and waits for `block` to call `fulfill()`.
+    ///
+    /// Example usage:
+    ///
+    /// ```
     /// waitForExpectation { expectation in
     ///     doSomethingInTheBackground {
     ///         expectation.fulfill()
@@ -15,10 +35,9 @@ extension XCTestCase {
     /// ```
     ///
     public func waitForExpectation(description: String? = nil,
-                                   timeout: TimeInterval = TimeInterval(10),
                                    _ block: (XCTestExpectation) -> ()) {
         let exp = expectation(description: description ?? "")
         block(exp)
-        wait(for: [exp], timeout: timeout)
+        wait(for: [exp], timeout: Constants.expectationTimeout)
     }
 }

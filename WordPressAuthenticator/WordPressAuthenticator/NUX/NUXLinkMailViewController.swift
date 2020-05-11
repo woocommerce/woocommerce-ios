142b3a9bfbc505d2caf39c9efd155a9bd45cd31c
Thuy.Copeland <thuycopeland@gmail.com>
2020-05-11 14:12:23 -0500

    Move button function into existing connected IBAction

--- WordPressAuthenticator/WordPressAuthenticator/NUX/NUXLinkMailViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/NUX/NUXLinkMailViewController.swift
@@ -21,18 +21,6 @@ class NUXLinkMailViewController: LoginViewController {
         }
     }
 
-    @IBAction func usePasswordButtonPressed(_ sender: Any) {
-        guard let vc = LoginWPComViewController.instantiate(from: .login) else {
-            DDLogError("Failed to navigate to LoginWPComViewController from NUXLinkMailViewController")
-            return
-        }
-
-        vc.loginFields = loginFields
-        vc.dismissBlock = dismissBlock
-        vc.errorToPresent = errorToPresent
-
-        navigationController?.pushViewController(vc, animated: true)
-    }
 
 
     // MARK: - Lifecycle Methods
@@ -119,6 +107,16 @@ class NUXLinkMailViewController: LoginViewController {
 
     @IBAction func handleUsePasswordTapped(_ sender: UIButton) {
         WordPressAuthenticator.track(.loginMagicLinkExited)
+        guard let vc = LoginWPComViewController.instantiate(from: .login) else {
+            DDLogError("Failed to navigate to LoginWPComViewController from NUXLinkMailViewController")
+            return
+        }
+
+        vc.loginFields = loginFields
+        vc.dismissBlock = dismissBlock
+        vc.errorToPresent = errorToPresent
+
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 

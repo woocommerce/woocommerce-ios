c5cebebb74f64726ae75a944391beee300b08afa
Shiki <jayson@basanes.net>
2020-05-12 16:28:35 -0600

    Make Networking's waitForExpectation module-level only
    I was wrong in the previous change where I made this `public` so Yosemite
    can use it. It turns out that doesn't work because we're only importing
    the main Networking target in Yosemite.

--- Networking/NetworkingTests/Testing/XCTestCase+Wait.swift
+++ Networking/NetworkingTests/Testing/XCTestCase+Wait.swift
@@ -14,30 +14,11 @@ extension XCTestCase {
     /// }
     /// ```
     ///
-    public func waitForExpectation(description: String? = nil,
-                                   timeout: TimeInterval,
-                                   _ block: (XCTestExpectation) -> ()) {
+    func waitForExpectation(description: String? = nil,
+                            timeout: TimeInterval = Constants.expectationTimeout,
+                            _ block: (XCTestExpectation) -> ()) {
         let exp = expectation(description: description ?? "")
         block(exp)
         wait(for: [exp], timeout: timeout)
-    }
-
-    /// Creates an XCTestExpectation and waits for `block` to call `fulfill()`.
-    ///
-    /// Example usage:
-    ///
-    /// ```
-    /// waitForExpectation { expectation in
-    ///     doSomethingInTheBackground {
-    ///         expectation.fulfill()
-    ///     }
-    /// }
-    /// ```
-    ///
-    public func waitForExpectation(description: String? = nil,
-                                   _ block: (XCTestExpectation) -> ()) {
-        let exp = expectation(description: description ?? "")
-        block(exp)
-        wait(for: [exp], timeout: Constants.expectationTimeout)
     }
 }

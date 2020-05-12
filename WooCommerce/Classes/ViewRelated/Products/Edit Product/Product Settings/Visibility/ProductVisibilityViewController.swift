df526380b1e3d11e6746c11731247627721eea4b
Paolo Musolino <info@codeido.com>
2020-05-12 20:08:01 +0200

    Fixed right image appearance on cell for product settings password

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Visibility/ProductVisibilityViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Product Settings/Visibility/ProductVisibilityViewController.swift
@@ -235,13 +235,13 @@ private extension ProductVisibilityViewController {
         let placeholder = NSLocalizedString("Enter password", comment: "Enter password placeholder in Product Visibility")
         let viewModel = TitleAndTextFieldWithImageTableViewCell.ViewModel(title: row.description, text: productSettings.password,
                                                                           placeholder: placeholder, image: .passwordFieldImage) { [weak self] (text) in
-            cell.rightImageViewIsHidden = text?.isEmpty == true
+            cell.rightImageViewIsHidden = text?.isEmpty == false
             self?.productSettings.password = text
         }
         cell.configure(viewModel: viewModel)
 
         /// Hides the image when there is a password, or show it if the password is empty
-        cell.rightImageViewIsHidden = (productSettings.password?.isEmpty == true || productSettings.password == nil)
+        cell.rightImageViewIsHidden = (productSettings.password?.isEmpty == false || productSettings.password == nil)
 
         cell.selectionStyle = .none
     }

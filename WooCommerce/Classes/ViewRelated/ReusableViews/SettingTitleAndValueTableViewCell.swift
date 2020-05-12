b799ff184793daf4176708b716ad578708b95078
Paolo Musolino <info@codeido.com>
2020-05-12 20:33:25 +0200

    Updated SettingTitleAndValueTableViewCell

--- WooCommerce/Classes/ViewRelated/ReusableViews/SettingTitleAndValueTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/SettingTitleAndValueTableViewCell.swift
@@ -7,8 +7,8 @@ final class SettingTitleAndValueTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        configureBackground()
-        configureLabels()
+        applyDefaultBackgroundStyle()
+        applyDefaultLabelsStyle()
     }
 }
 
@@ -23,12 +23,8 @@ extension SettingTitleAndValueTableViewCell {
 
 // MARK: Configurations
 //
-private extension SettingTitleAndValueTableViewCell {
-    func configureBackground() {
-        applyDefaultBackgroundStyle()
-    }
-
-    func configureLabels() {
+extension SettingTitleAndValueTableViewCell {
+    func applyDefaultLabelsStyle() {
         titleLabel.applyBodyStyle()
         titleLabel.textColor = .text
 

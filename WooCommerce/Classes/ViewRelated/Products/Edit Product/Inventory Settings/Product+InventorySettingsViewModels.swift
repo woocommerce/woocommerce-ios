af65253e40e2c9a3058ec813027e8619aae15ee2
Paolo Musolino <info@codeido.com>
2020-05-13 17:40:35 +0200

    Fixed Hound CI warnings

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Inventory Settings/Product+InventorySettingsViewModels.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Inventory Settings/Product+InventorySettingsViewModels.swift
@@ -14,14 +14,14 @@ extension Product {
     static func createStockQuantityViewModel(stockQuantity: Int?, onInputChange: @escaping (_ input: String?) -> Void) -> UnitInputViewModel {
         let title = NSLocalizedString("Quantity", comment: "Title of the cell in Product Inventory Settings > Quantity")
         let value = "\(stockQuantity ?? 0)"
-
+        let accessibilityHint = NSLocalizedString(
+            "The stock quantity for this product. Editable.",
+            comment: "VoiceOver accessibility hint, informing the user that the cell shows the stock quantity information for this product.")
         return UnitInputViewModel(title: title,
                                   unit: "",
                                   value: value,
                                   placeholder: "0",
-                                  accessibilityHint: NSLocalizedString(
-                                  "The stock quantity for this product. Editable.",
-                                  comment: "VoiceOver accessibility hint, informing the user that the cell shows the stock quantity information for this product."),
+                                  accessibilityHint: accessibilityHint,
                                   unitPosition: .none,
                                   keyboardType: .numberPad,
                                   inputFormatter: IntegerInputFormatter(),

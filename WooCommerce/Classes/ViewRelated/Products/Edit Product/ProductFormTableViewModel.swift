d74e10cc7965c8a3df84d789f4f7adc80cd11776
Jaclyn Chen <watertranquil@gmail.com>
2020-05-13 18:42:24 +0800

    Move `ProductFormSection.SettingsRow` extension to where the enum is defined.

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormTableViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormTableViewModel.swift
@@ -85,3 +85,28 @@ extension ProductFormSection.SettingsRow.ViewModel: Equatable {
             lhs.details == rhs.details
     }
 }
+
+extension ProductFormSection.SettingsRow {
+    func isVisible(product: Product) -> Bool {
+        guard let bottomSheetAction = bottomSheetAction else {
+            // If there is no corresponding bottom sheet action, the settings row is visible by default.
+            return true
+        }
+        return bottomSheetAction.isVisible(product: product) == false
+    }
+
+    private var bottomSheetAction: ProductFormBottomSheetAction? {
+        switch self {
+        case .inventory:
+            return .editInventorySettings
+        case .shipping:
+            return .editShippingSettings
+        case .categories:
+            return .editCategories
+        case .briefDescription:
+            return .editBriefDescription
+        default:
+            return nil
+        }
+    }
+}

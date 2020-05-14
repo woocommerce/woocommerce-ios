34aad0fd389fb3314b23d38b5a3f8fc41c22f829
Shiki <jayson@basanes.net>
2020-05-14 15:10:10 -0600

    SummaryTVCTests: Simplify test data
    These sample data are not really needed because they are not used anyway.
    They just add to the noise. :|

--- WooCommerce/WooCommerceTests/ViewRelated/SummaryTableViewCellTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/SummaryTableViewCellTests.swift
@@ -75,9 +75,9 @@ private extension SummaryTableViewCellTests {
                      statusKey: "automattic",
                      currency: "USD",
                      customerNote: "",
-                     dateCreated: date(with: "2018-04-03T23:05:12"),
-                     dateModified: date(with: "2018-04-03T23:05:14"),
-                     datePaid: date(with: "2018-04-03T23:05:14"),
+                     dateCreated: Date(),
+                     dateModified: Date(),
+                     datePaid: Date(),
                      discountTotal: "30.00",
                      discountTax: "1.20",
                      shippingTotal: "0.00",
@@ -85,156 +85,12 @@ private extension SummaryTableViewCellTests {
                      total: "31.20",
                      totalTax: "1.20",
                      paymentMethodTitle: "Credit Card (Stripe)",
-                     items: sampleItems(),
-                     billingAddress: sampleAddress(),
-                     shippingAddress: sampleAddress(),
-                     shippingLines: sampleShippingLines(),
-                     coupons: sampleCoupons(),
+                     items: [],
+                     billingAddress: nil,
+                     shippingAddress: nil,
+                     shippingLines: [],
+                     coupons: [],
                      refunds: [])
-    }
-
-    func sampleAddress() -> Address {
-        return Address(firstName: "Johnny",
-                       lastName: "Appleseed",
-                       company: "",
-                       address1: "234 70th Street",
-                       address2: "",
-                       city: "Niagara Falls",
-                       state: "NY",
-                       postcode: "14304",
-                       country: "US",
-                       phone: "333-333-3333",
-                       email: "scrambled@scrambled.com")
-    }
-
-    func sampleShippingLines() -> [ShippingLine] {
-        return [ShippingLine(shippingID: 123,
-        methodTitle: "International Priority Mail Express Flat Rate",
-        methodID: "usps",
-        total: "133.00",
-        totalTax: "0.00")]
-    }
-
-    func sampleCoupons() -> [OrderCouponLine] {
-        let coupon1 = OrderCouponLine(couponID: 894,
-                                      code: "30$off",
-                                      discount: "30",
-                                      discountTax: "1.2")
-        return [coupon1]
-    }
-
-    func sampleCouponsMutated() -> [OrderCouponLine] {
-        let coupon1 = OrderCouponLine(couponID: 894,
-                                      code: "30$off",
-                                      discount: "20",
-                                      discountTax: "12.2")
-        let coupon2 = OrderCouponLine(couponID: 12,
-                                      code: "hithere!",
-                                      discount: "50",
-                                      discountTax: "0.66")
-        return [coupon1, coupon2]
-    }
-
-    func sampleItems() -> [OrderItem] {
-        let item1 = OrderItem(itemID: 890,
-                              name: "Fruits Basket (Mix & Match Product)",
-                              productID: 52,
-                              variationID: 0,
-                              quantity: 2,
-                              price: NSDecimalNumber(integerLiteral: 30),
-                              sku: "",
-                              subtotal: "50.00",
-                              subtotalTax: "2.00",
-                              taxClass: "",
-                              taxes: [],
-                              total: "30.00",
-                              totalTax: "1.20")
-
-        let item2 = OrderItem(itemID: 891,
-                              name: "Fruits Bundle",
-                              productID: 234,
-                              variationID: 0,
-                              quantity: 1.5,
-                              price: NSDecimalNumber(integerLiteral: 0),
-                              sku: "5555-A",
-                              subtotal: "10.00",
-                              subtotalTax: "0.40",
-                              taxClass: "",
-                              taxes: [],
-                              total: "0.00",
-                              totalTax: "0.00")
-
-        return [item1, item2]
-    }
-
-    func sampleItemsMutated() -> [OrderItem] {
-        let item1 = OrderItem(itemID: 890,
-                              name: "Fruits Basket (Mix & Match Product) 2",
-                              productID: 52,
-                              variationID: 0,
-                              quantity: 10,
-                              price: NSDecimalNumber(integerLiteral: 30),
-                              sku: "",
-                              subtotal: "60.00",
-                              subtotalTax: "4.00",
-                              taxClass: "",
-                              taxes: [],
-                              total: "64.00",
-                              totalTax: "4.00")
-
-        let item2 = OrderItem(itemID: 891,
-                              name: "Fruits Bundle 2",
-                              productID: 234,
-                              variationID: 0,
-                              quantity: 3,
-                              price: NSDecimalNumber(integerLiteral: 0),
-                              sku: "5555-A",
-                              subtotal: "30.00",
-                              subtotalTax: "0.40",
-                              taxClass: "",
-                              taxes: [],
-                              total: "30.40",
-                              totalTax: "0.40")
-
-        let item3 = OrderItem(itemID: 23,
-                              name: "Some new product",
-                              productID: 12,
-                              variationID: 0,
-                              quantity: 1,
-                              price: NSDecimalNumber(integerLiteral: 10),
-                              sku: "QWE123",
-                              subtotal: "130.00",
-                              subtotalTax: "10.40",
-                              taxClass: "",
-                              taxes: [],
-                              total: "140.40",
-                              totalTax: "10.40")
-
-        return [item1, item2, item3]
-    }
-
-    func sampleItemsMutated2() -> [OrderItem] {
-        let item1 = OrderItem(itemID: 890,
-                              name: "Fruits Basket (Mix & Match Product) 2",
-                              productID: 52,
-                              variationID: 0,
-                              quantity: 10,
-                              price: NSDecimalNumber(integerLiteral: 10),
-                              sku: "",
-                              subtotal: "60.00",
-                              subtotalTax: "4.00",
-                              taxClass: "",
-                              taxes: [],
-                              total: "64.00",
-                              totalTax: "4.00")
-        return [item1]
-    }
-
-    func date(with dateString: String) -> Date {
-        guard let date = DateFormatter.Defaults.dateTimeFormatter.date(from: dateString) else {
-            return Date()
-        }
-        return date
     }
 }
 

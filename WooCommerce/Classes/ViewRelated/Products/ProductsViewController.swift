b3860d1f19509a6e97dfb3e1f78643b20a78f499
Paolo Musolino <info@codeido.com>
2020-05-14 16:25:00 +0200

    Fixed Hound CI warning

--- WooCommerce/Classes/ViewRelated/Products/ProductsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/ProductsViewController.swift
@@ -463,8 +463,7 @@ private extension ProductsViewController {
                                                                             return
                                                                         }
                                                                         self?.sortOrder = selectedSortOrder
-                                                                        ServiceLocator.analytics.track(.productListViewSortingOptionsTapped,
-                                                                                                       withProperties: ["order": selectedSortOrder.analyticsDescription])
+         ServiceLocator.analytics.track(.productListViewSortingOptionsTapped, withProperties: ["order": selectedSortOrder.analyticsDescription])
         }
         sortOrderListPresenter.show(from: self, sourceView: sender, arrowDirections: .up)
     }

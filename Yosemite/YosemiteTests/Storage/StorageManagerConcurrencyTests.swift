d268dc5d610d123644939edf93e16d82b7b32785
Shiki <jayson@basanes.net>
2020-05-18 15:25:42 -0600

    Storage*ConcurrencyTests: Add comments. Lots of em.

--- Yosemite/YosemiteTests/Storage/StorageManagerConcurrencyTests.swift
+++ Yosemite/YosemiteTests/Storage/StorageManagerConcurrencyTests.swift
@@ -5,6 +5,50 @@ import Networking
 
 @testable import Yosemite
 
+/// Proofs and documentation for **possible** concurrency issues that we may run into in the future.
+///
+/// Currently, every Yosemite `Store` creates their own `StorageType` for background saving.
+/// For example, in `OrderStore` and `ProductStore`, we declare them like this:
+///
+/// ```
+/// private lazy var sharedDerivedStorage: StorageType = {
+///     return storageManager.newDerivedStorage()
+/// }()
+/// ```
+///
+/// The `newDerivedStorage()` method typically creates a child `StorageType`
+/// (`NSManagedObjectContext`). These children:
+///
+/// - Have no idea they have siblings (derived `StorageType` in other `Stores`)
+/// - Do not see a siblings' data until a `saveIfNeeded()` is executed on that sibling.
+///
+/// This can mean that if two or more derived `StorageTypes` do similar things, it may be
+/// possible to upsert the same records, leading to duplicate data. For example, in `OrderStore`, we
+/// upsert records like this:
+///
+/// ```
+/// // Fetch an existing record. If it doesn't exist, create a new one.
+/// let storageOrder = storage.loadOrder(orderID: readOnlyOrder.orderID)
+///             ?? storage.insertNewObject(ofType: Storage.Order.self)
+///
+/// // Update the new or existing record with the newly fetched data from the API
+/// storageOrder.update(with: readOnlyOrder)
+/// ```
+///
+/// If by coincidence, the same operation happens **at the same time** with the same order on a
+/// **different** derived `StorageType`, we could be inserting the same order twice. And because we
+/// currently don't have unique key constraints, two records would be saved in Core Data.
+///
+/// ## Is it a Problem?
+///
+/// This is probably not a problem at the moment. We are careful with using a single `StorageType`
+/// for every `Store`. It would be a problem if:
+///
+/// - We have `Store` instances that are not deallocated, still running and doing something
+///   in the background while another newly created `Store` is doing the same thing.
+/// - The app grows and we need to do complex cross-feature operations. For example, saving
+///   Products and Product Categories simultaneously as a single transaction.
+///
 final class StorageManagerConcurrencyTests: XCTestCase {
 
     private var storageManager: StorageManagerType!
@@ -26,6 +70,9 @@ final class StorageManagerConcurrencyTests: XCTestCase {
         super.tearDown()
     }
 
+    /// Simulate what will happen if `perform()` happens at the same time but the `saveDerivedType`
+    /// happens in sequence. We end up with the same record inserted twice.
+    ///
     func testWhenSequentiallySavingItCanAllowSavingOfDuplicates() {
         // Given
         let firstDerivedStorage = storageManager.newDerivedStorage()
@@ -54,6 +101,9 @@ final class StorageManagerConcurrencyTests: XCTestCase {
         XCTAssertEqual(viewStorage.countObjects(ofType: Storage.OrderStatus.self), 2)
     }
 
+    /// Simulate what will happen if both the `perform()` and `saveDerivedType` happens at the
+    /// same time. We end up with the same record inserted twice.
+    ///
     func testWhenConcurrentlySavingItCanAllowSavingOfDuplicates() {
         // Given
         let firstDerivedStorage = storageManager.newDerivedStorage()
@@ -72,10 +122,8 @@ final class StorageManagerConcurrencyTests: XCTestCase {
                 self.upsert(orderStatus: orderStatus, using: derivedStorage)
             }
 
-            derivedStorage.perform {
-                self.storageManager.saveDerivedType(derivedStorage: derivedStorage) {
-                    exp.fulfill()
-                }
+            self.storageManager.saveDerivedType(derivedStorage: derivedStorage) {
+                exp.fulfill()
             }
         }
 
@@ -85,6 +133,10 @@ final class StorageManagerConcurrencyTests: XCTestCase {
         XCTAssertEqual(viewStorage.countObjects(ofType: Storage.OrderStatus.self), 2)
     }
 
+    /// Simulate what will happen if `perform()` and `saveDerivedType` called under a single
+    /// `perform()` block happens at the same time on two `StorageTypes`. We end up with the same
+    /// record inserted twice.
+    ///
     func testWhenConcurrentlySavingUsingASinglePerformBlockItCanAllowSavingOfDuplicates() {
         // Given
         let firstDerivedStorage = storageManager.newDerivedStorage()
@@ -114,6 +166,10 @@ final class StorageManagerConcurrencyTests: XCTestCase {
         XCTAssertEqual(viewStorage.countObjects(ofType: Storage.OrderStatus.self), 2)
     }
 
+    /// Simulate what will happen if we wait for the `perform()` and `saveDerivedType` to finish
+    /// before saving the same record on a sibling `StorageType`. The record is correctly
+    /// inserted only once.
+    ///
     func testWhenSequentiallySavingAndWaitingThenNoDuplicatesAreSaved() {
         // Given
         let firstDerivedStorage = storageManager.newDerivedStorage()
@@ -140,6 +196,9 @@ final class StorageManagerConcurrencyTests: XCTestCase {
         XCTAssertEqual(viewStorage.countObjects(ofType: Storage.OrderStatus.self), 1)
     }
 
+    /// Prove that siblings will not be able to access any upserted record from a modified
+    /// sibling (Sibling-X) if Sibling-X never calls `saveIfNeeded()`.
+    ///
     func testWhenNotSavingThenADerivedStorageWillNotShareDataToItsSiblings() {
         // Given
         let firstDerivedStorage = storageManager.newDerivedStorage()
@@ -159,12 +218,16 @@ final class StorageManagerConcurrencyTests: XCTestCase {
         }
 
         // Then
+        // No records are accessible from siblings and the `viewStorage`
         XCTAssertEqual(secondDerivedStorage.countObjects(ofType: Storage.OrderStatus.self), 0)
         XCTAssertEqual(viewStorage.countObjects(ofType: Storage.OrderStatus.self), 0)
 
         XCTAssertEqual(firstDerivedStorage.countObjects(ofType: Storage.OrderStatus.self), 1)
     }
 
+    /// Prove that siblings will be able to access the upserted record from a modified sibling
+    /// (Sibling-X) if Sibling-X calls `saveIfNeeded()`.
+    ///
     func testWhenSavedThenADerivedStorageWillShareDataToItsSiblings() {
         // Given
         let firstDerivedStorage = storageManager.newDerivedStorage()
@@ -186,6 +249,8 @@ final class StorageManagerConcurrencyTests: XCTestCase {
         }
 
         // Then
+        // The record is accessible from siblings and `viewStorage` even if we never persisted
+        // the change by calling `saveIfNeeded` on the `viewStorage`.
         XCTAssertEqual(secondDerivedStorage.countObjects(ofType: Storage.OrderStatus.self), 1)
         XCTAssertEqual(viewStorage.countObjects(ofType: Storage.OrderStatus.self), 1)
 

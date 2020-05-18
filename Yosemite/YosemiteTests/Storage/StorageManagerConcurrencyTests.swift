007cbfd061b17f2c977feee03a4af67a08ff705d
Shiki <jayson@basanes.net>
2020-05-18 13:03:38 -0600

    Add test to prove that duplicate records may occur

---
+++ Yosemite/YosemiteTests/Storage/StorageManagerConcurrencyTests.swift
@@ -0,0 +1,61 @@
+
+import XCTest
+import Storage
+import Networking
+
+@testable import Yosemite
+
+final class StorageManagerConcurrencyTests: XCTestCase {
+
+    private var storageManager: StorageManagerType!
+
+    private var viewStorage: StorageType {
+        storageManager.viewStorage
+    }
+
+    override func setUp() {
+        super.setUp()
+        // Using the real Sqlite-based StorageManagerType to be closer to the production operations
+        storageManager = CoreDataManager(name: "WooCommerce")
+        storageManager.reset()
+    }
+
+    override func tearDown() {
+        storageManager.reset()
+        storageManager = nil
+        super.tearDown()
+    }
+
+    func testTheConcurrencyArchitectureCanAllowSavingOfDuplicates() {
+        // Given
+        let firstDerivedStorage = storageManager.newDerivedStorage()
+        let secondDerivedStorage = storageManager.newDerivedStorage()
+
+        let orderStatus = Networking.OrderStatus(name: "In Space", siteID: 1_998, slug: "in-space", total: 9)
+
+        XCTAssertEqual(viewStorage.countObjects(ofType: Storage.OrderStatus.self), 0)
+
+        // When
+        [firstDerivedStorage, secondDerivedStorage].forEach { derivedContext in
+            derivedContext.perform {
+                if let existing = derivedContext.loadOrderStatus(siteID: orderStatus.siteID, slug: orderStatus.slug) {
+                    existing.update(with: orderStatus)
+                } else {
+                    let newStorageOrderStatus = derivedContext.insertNewObject(ofType: Storage.OrderStatus.self)
+                    newStorageOrderStatus.update(with: orderStatus)
+                }
+            }
+        }
+
+        [firstDerivedStorage, secondDerivedStorage].forEach { derivedContext in
+            waitForExpectation { exp in
+                storageManager.saveDerivedType(derivedStorage: derivedContext) {
+                    exp.fulfill()
+                }
+            }
+        }
+
+        // Then
+        XCTAssertEqual(viewStorage.countObjects(ofType: Storage.OrderStatus.self), 2)
+    }
+}

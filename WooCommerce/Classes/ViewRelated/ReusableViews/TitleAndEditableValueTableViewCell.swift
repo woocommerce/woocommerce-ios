24fef53a699dddb37a05dc85b2de46768c36a88f
Paolo Musolino <info@codeido.com>
2020-05-21 17:10:45 +0200

    fix: cell spacing setup in code

--- WooCommerce/Classes/ViewRelated/ReusableViews/TitleAndEditableValueTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/TitleAndEditableValueTableViewCell.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 final class TitleAndEditableValueTableViewCell: UITableViewCell {
+    @IBOutlet private weak var stackView: UIStackView!
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var value: UITextField!
 
@@ -10,6 +11,7 @@ final class TitleAndEditableValueTableViewCell: UITableViewCell {
         configureAsNonSelectable()
         styleTitle()
         styleValue()
+        configureStackView()
     }
 }
 
@@ -23,11 +25,24 @@ private extension TitleAndEditableValueTableViewCell {
         selectionStyle = .none
     }
 
+    func configureStackView() {
+        stackView.spacing = Constants.stackViewSpacing
+    }
+
     func styleTitle() {
         title.applyFootnoteStyle()
     }
 
     func styleValue() {
         value.applyBodyStyle()
+    }
+}
+
+// MARK: - Constants!
+//
+private extension TitleAndEditableValueTableViewCell {
+
+    enum Constants {
+        static let stackViewSpacing: CGFloat = 7
     }
 }

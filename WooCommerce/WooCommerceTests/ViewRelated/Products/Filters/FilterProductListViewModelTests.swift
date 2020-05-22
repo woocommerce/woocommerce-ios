8a103d5262069c107c87077d4246d17f4f63bdbd
Jaclyn Chen <watertranquil@gmail.com>
2020-05-22 16:48:24 +0800

    Add Given-When-Then comments.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Filters/FilterProductListViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Filters/FilterProductListViewModelTests.swift
@@ -3,23 +3,38 @@ import XCTest
 
 final class FilterProductListViewModelTests: XCTestCase {
     func testCriteriaWithDefaultFilters() {
+        // Given
         let filters = FilterProductListViewModel.Filters()
+
+        // When
         let viewModel = FilterProductListViewModel(filters: filters)
+
+        // Then
         let expectedCriteria = FilterProductListViewModel.Filters(stockStatus: nil, productStatus: nil, productType: nil, numberOfActiveFilters: 0)
         XCTAssertEqual(viewModel.criteria, expectedCriteria)
     }
 
     func testCriteriaWithNonNilFilters() {
+        // Given
         let filters = FilterProductListViewModel.Filters(stockStatus: .inStock, productStatus: .draft, productType: .grouped, numberOfActiveFilters: 3)
+
+        // When
         let viewModel = FilterProductListViewModel(filters: filters)
+
+        // Then
         let expectedCriteria = filters
         XCTAssertEqual(viewModel.criteria, expectedCriteria)
     }
 
     func testCriteriaAfterClearingAllNonNilFilters() {
+        // Given
         let filters = FilterProductListViewModel.Filters(stockStatus: .inStock, productStatus: .draft, productType: .grouped, numberOfActiveFilters: 3)
+
+        // When
         let viewModel = FilterProductListViewModel(filters: filters)
         viewModel.clearAll()
+
+        // Then
         let expectedCriteria = FilterProductListViewModel.Filters(stockStatus: nil, productStatus: nil, productType: nil, numberOfActiveFilters: 0)
         XCTAssertEqual(viewModel.criteria, expectedCriteria)
     }

b2f3e31d3f2632d49c3d83094a732e6871242b32
Paolo Musolino <info@codeido.com>
2020-05-26 16:39:33 +0200

    fix: managed the case where sections or rows are empty

--- WooCommerce/Classes/Extensions/UITableView+Helpers.swift
+++ WooCommerce/Classes/Extensions/UITableView+Helpers.swift
@@ -2,10 +2,18 @@ import UIKit
 
 extension UITableView {
 
-    /// Return the last Index Path (the last row of the last section)
-    func lastIndexPath() -> IndexPath {
+    /// Return the last Index Path (the last row of the last section) if available
+    func lastIndexPath() -> IndexPath? {
+        guard numberOfSections > 0 else {
+            return nil
+        }
         let section = max(numberOfSections - 1, 0)
+        
+        guard numberOfRows(inSection: section) > 0 else {
+            return nil
+        }
         let row = max(numberOfRows(inSection: section) - 1, 0)
+        
         return IndexPath(row: row, section: section)
     }
 }

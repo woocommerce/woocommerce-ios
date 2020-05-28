28e2c20ba771e1364ed7d94f27daf13ef20227ec
Shiki <jayson@basanes.net>
2020-05-28 10:38:44 -0600

    MockupTableView: Call super

--- WooCommerce/WooCommerceTests/Mockups/MockupTableView.swift
+++ WooCommerce/WooCommerceTests/Mockups/MockupTableView.swift
@@ -39,30 +39,37 @@ class MockupTableView: UITableView {
     // MARK: - Overridden Methods
 
     override func beginUpdates() {
+        super.beginUpdates()
         onBeginUpdates?()
     }
 
     override func endUpdates() {
+        super.endUpdates()
         onEndUpdates?()
     }
 
     override func insertRows(at indexPaths: [IndexPath], with animation: UITableView.RowAnimation) {
+        super.insertRows(at: indexPaths, with: animation)
         onInsertedRows?(indexPaths)
     }
 
     override func deleteRows(at indexPaths: [IndexPath], with animation: UITableView.RowAnimation) {
+        super.deleteRows(at: indexPaths, with: animation)
         onDeletedRows?(indexPaths)
     }
 
     override func reloadRows(at indexPaths: [IndexPath], with animation: UITableView.RowAnimation) {
+        super.reloadRows(at: indexPaths, with: animation)
         onReloadRows?(indexPaths)
     }
 
     override func deleteSections(_ sections: IndexSet, with animation: UITableView.RowAnimation) {
+        super.deleteSections(sections, with: animation)
         onDeletedSections?(sections)
     }
 
     override func insertSections(_ sections: IndexSet, with animation: UITableView.RowAnimation) {
+        super.insertSections(sections, with: animation)
         onInsertedSections?(sections)
     }
 }

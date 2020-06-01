a65bf31e5213a6c6e13c1ba67c7ebf9075fa9c3b
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-01 15:55:33 -0500

    Add siteAddressPrimaryButton title to DisplayStrings struct

--- WordPressAuthenticator/WordPressAuthenticator/Authenticator/WordPressAuthenticatorDisplayStrings.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Authenticator/WordPressAuthenticatorDisplayStrings.swift
@@ -12,19 +12,34 @@ public struct WordPressAuthenticatorDisplayStrings {
 
     public let siteLoginInstructions: String
 
+    /// Strings: primary call-to-action button titles.
+    ///
+    public let siteAddressPrimaryButton: String
+
     /// Designated initializer.
     ///
-    public init(emailLoginInstructions: String, jetpackLoginInstructions: String, siteLoginInstructions: String) {
+    public init(emailLoginInstructions: String,
+                jetpackLoginInstructions: String,
+                siteLoginInstructions: String,
+                siteAddressPrimaryButton: String) {
         self.emailLoginInstructions = emailLoginInstructions
         self.jetpackLoginInstructions = jetpackLoginInstructions
         self.siteLoginInstructions = siteLoginInstructions
+        self.siteAddressPrimaryButton = siteAddressPrimaryButton
     }
 }
 
 public extension WordPressAuthenticatorDisplayStrings {
     static var defaultStrings: WordPressAuthenticatorDisplayStrings {
-        return WordPressAuthenticatorDisplayStrings(emailLoginInstructions: NSLocalizedString("Log in to your WordPress.com account with your email address.", comment: "Instruction text on the login's email address screen."),
-                                                    jetpackLoginInstructions: NSLocalizedString("Log in to the WordPress.com account you used to connect Jetpack.", comment: "Instruction text on the login's email address screen."),
-                                                    siteLoginInstructions: NSLocalizedString("Enter the address of the WordPress site you'd like to connect.", comment: "Instruction text on the login's site addresss screen."))
+        return WordPressAuthenticatorDisplayStrings(
+            emailLoginInstructions: NSLocalizedString("Log in to your WordPress.com account with your email address.",
+                                                      comment: "Instruction text on the login's email address screen."),
+            jetpackLoginInstructions: NSLocalizedString("Log in to the WordPress.com account you used to connect Jetpack.",
+                                                        comment: "Instruction text on the login's email address screen."),
+            siteLoginInstructions: NSLocalizedString("Enter the address of the WordPress site you'd like to connect.",
+                                                     comment: "Instruction text on the login's site addresss screen."),
+            siteAddressPrimaryButton: NSLocalizedString("Continue",
+                                                        comment: "The primary call-to-action button on the unified site address screen.")
+        )
     }
 }

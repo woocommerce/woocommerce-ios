031beb1ba0c8029a5e48d5159aa62258dc654ca3
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-01 15:41:28 -0500

    Connect tableview delegate and datasource

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
@@ -13,3 +13,21 @@ final class SiteAddressViewController: UIViewController {
         super.viewDidLoad()
     }
 }
+
+
+// MARK: - UITableViewDataSource
+extension SiteAddressViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+
+// MARK: - UITableViewDelegate conformance
+extension SiteAddressViewController: UITableViewDelegate {
+
+}

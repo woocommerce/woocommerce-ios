afbd7132d70976010608d96f85f34019f80392ec
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-01 10:05:45 -0500

    Subclass UIViewController and name it SiteAddressViewController

---
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+
+/// SiteAddressViewController: log in by Site Address.
+///
+class SiteAddressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

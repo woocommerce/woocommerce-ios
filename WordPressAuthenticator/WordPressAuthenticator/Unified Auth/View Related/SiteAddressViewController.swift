f199b5aa7c372963379227d3d7a728d15097164f
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-01 15:21:02 -0500

    Attach the tableView and continueButton outlets

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
@@ -3,7 +3,11 @@ import UIKit
 
 /// SiteAddressViewController: log in by Site Address.
 ///
-class SiteAddressViewController: UIViewController {
+final class SiteAddressViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+
+    @IBOutlet private weak var continueButton: NUXButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()

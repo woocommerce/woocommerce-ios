fba9618114b496861df3bb4446352cb0adb3435e
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-02 13:05:15 -0500

    Add table row cells and conform to NUXKeyboardResponder

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
@@ -6,6 +6,7 @@ import UIKit
 final class SiteAddressViewController: LoginViewController {
 
     @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet var bottomContentConstraint: NSLayoutConstraint?
 
     var displayStrings: WordPressAuthenticatorDisplayStrings {
         return WordPressAuthenticator.shared.displayStrings
@@ -28,10 +29,34 @@ final class SiteAddressViewController: LoginViewController {
 // MARK: - UITableViewDataSource
 extension SiteAddressViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 4
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            return tableView.dequeueReusableCell(withIdentifier: "largetitlecell") ?? UITableViewCell()
+        }
+
+        if indexPath.row == 1 {
+            return tableView.dequeueReusableCell(withIdentifier: "emailcell")  ?? UITableViewCell()
+        }
+
+        if indexPath.row == 2 {
+            return tableView.dequeueReusableCell(withIdentifier: "instructionscell") ?? UITableViewCell()
+        }
+
+        if indexPath.row == 3 {
+            return tableView.dequeueReusableCell(withIdentifier: "textfieldcell") ?? UITableViewCell()
+        }
+
+        if indexPath.row == 4 {
+            return tableView.dequeueReusableCell(withIdentifier: "errorcell") ?? UITableViewCell()
+        }
+
+        if indexPath.row == 5 {
+            return tableView.dequeueReusableCell(withIdentifier: "secondaryhelperbuttoncell") ?? UITableViewCell()
+        }
+
         return UITableViewCell()
     }
 }
@@ -40,4 +65,21 @@ extension SiteAddressViewController: UITableViewDataSource {
 // MARK: - UITableViewDelegate conformance
 extension SiteAddressViewController: UITableViewDelegate {
 
+}
+
+// MARK: - Keyboard Notifications
+extension SiteAddressViewController: NUXKeyboardResponder {
+    var verticalCenterConstraint: NSLayoutConstraint? {
+        // no-op
+        return nil
+    }
+
+    @objc func handleKeyboardWillShow(_ notification: Foundation.Notification) {
+        keyboardWillShow(notification)
+    }
+
+
+    @objc func handleKeyboardWillHide(_ notification: Foundation.Notification) {
+        keyboardWillHide(notification)
+    }
 }

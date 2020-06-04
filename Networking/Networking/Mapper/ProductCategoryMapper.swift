c50187fd6060e46941930886ee0ad9a3c099ef06
Paolo Musolino <info@codeido.com>
2020-06-04 12:54:09 +0200

    feat: implemented ProductCategoryMapper

---
+++ Networking/Networking/Mapper/ProductCategoryMapper.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCategoryMapper.swift
+//  Networking
+//
+//  Created by Paolo Musolino on 04/06/2020.
+//  Copyright © 2020 Automattic. All rights reserved.
+//
+
+import UIKit
+
+class ProductCategoryMapper: NSObject {
+
+}

6f1bfec7f3b4836eff4933331162d1e9e6709c2f
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-04 16:03:12 -0500

    Make SiteAddressViewController conform to NUXKeyboardResponder

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
@@ -6,15 +6,27 @@ import UIKit
 final class SiteAddressViewController: LoginViewController {
 
     @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet var bottomContentConstraint: NSLayoutConstraint?
+
+    // Required property declaration for `NUXKeyboardResponder` but unused here.
+    var verticalCenterConstraint: NSLayoutConstraint?
 
     var displayStrings: WordPressAuthenticatorDisplayStrings {
         return WordPressAuthenticator.shared.displayStrings
     }
 
+    // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         localizePrimaryButton()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        registerForKeyboardEvents(keyboardWillShowAction: #selector(handleKeyboardWillShow(_:)),
+                                  keyboardWillHideAction: #selector(handleKeyboardWillHide(_:)))
     }
 
     func localizePrimaryButton() {
@@ -50,4 +62,16 @@ extension SiteAddressViewController: UITableViewDataSource {
 // MARK: - UITableViewDelegate conformance
 extension SiteAddressViewController: UITableViewDelegate {
 
+}
+
+
+// MARK: - Keyboard Notifications
+extension SiteAddressViewController: NUXKeyboardResponder {
+    @objc func handleKeyboardWillShow(_ notification: Foundation.Notification) {
+        keyboardWillShow(notification)
+    }
+
+    @objc func handleKeyboardWillHide(_ notification: Foundation.Notification) {
+        keyboardWillHide(notification)
+    }
 }

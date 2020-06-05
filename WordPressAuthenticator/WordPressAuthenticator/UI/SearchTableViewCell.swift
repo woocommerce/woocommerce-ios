0602ad77fdd95d981432fe528692a6069d0823b5
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-05 16:36:34 -0500

    Reuse identifiers no longer need declared separately

--- WordPressAuthenticator/WordPressAuthenticator/UI/SearchTableViewCell.swift
+++ WordPressAuthenticator/WordPressAuthenticator/UI/SearchTableViewCell.swift
@@ -13,10 +13,6 @@ public protocol SearchTableViewCellDelegate: class {
 //
 open class SearchTableViewCell: UITableViewCell {
 
-    /// UITableView's Reuse Identifier
-    ///
-    public static let reuseIdentifier = "SearchTableViewCell"
-
     /// Search 'UITextField's reference!
     ///
     @IBOutlet public var textField: LoginTextField!

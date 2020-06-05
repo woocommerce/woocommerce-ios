f2d2505ee50cf82e0813fc61bae531819f1e95e1
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-05 16:21:51 -0500

    Add UITableViewCell Helper

---
+++ WordPressAuthenticator/WordPressAuthenticator/Extensions/UITableViewCell+Helpers.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+
+/// UITableViewCell Helpers
+///
+extension UITableViewCell {
+    /// Returns a reuseIdentifier that matches the receiver's classname (non namespaced).
+    ///
+    class var reuseIdentifier: String {
+        return classNameWithoutNamespaces
+    }
+}

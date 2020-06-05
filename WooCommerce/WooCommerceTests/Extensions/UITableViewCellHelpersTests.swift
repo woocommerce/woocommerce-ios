9cc5216836f4b6ebfb4c6183caea73710de5b0df
Shiki <jayson@basanes.net>
2020-06-04 20:00:10 -0600

    Add tests for findSectionTopSeparator()

--- WooCommerce/WooCommerceTests/Extensions/UITableViewCellHelpersTests.swift
+++ WooCommerce/WooCommerceTests/Extensions/UITableViewCellHelpersTests.swift
@@ -11,4 +11,41 @@ final class UITableViewCellHelpersTests: XCTestCase {
     func testReuseIdentifierEffectivelyReturnsClassnameWithNoNamespaces() {
         XCTAssertEqual(EmptyStoresTableViewCell.reuseIdentifier, "EmptyStoresTableViewCell")
     }
+
+    func testFindSectionTopSeparatorReturnsTheSeparatorView() {
+        // Given
+        let cell = UITableViewCell()
+
+        // Dummy subviews
+        cell.addSubview(UIView(frame: .init(x: 0, y: 0, width: 263, height: 300)))
+        cell.addSubview(UIView(frame: .init(x: 100, y: 20, width: 120, height: 76)))
+
+        // We'll only be simulating the separator view because it looks like it's the
+        // `UITableView` itself that creates and adds the separator view as a child.
+        let actualSeparatorView =
+            UIView(frame: .init(origin: .zero, size: .init(width: cell.bounds.width, height: 0.5)))
+        cell.addSubview(actualSeparatorView)
+
+        // When
+        let foundSeparatorView = cell.findSectionTopSeparator()
+
+        // Then
+        XCTAssertNotNil(foundSeparatorView)
+        assertThat(actualSeparatorView, isIdenticalTo: foundSeparatorView)
+    }
+
+    func testFindSectionTopSeparatorReturnsNilIfThereIsNoMatchingSeparatorView() {
+        // Given
+        let cell = UITableViewCell()
+
+        // Dummy subviews
+        cell.addSubview(UIView(frame: .init(x: 0, y: 0, width: 263, height: 300)))
+        cell.addSubview(UIView(frame: .init(x: 100, y: 20, width: 120, height: 76)))
+
+        // When
+        let foundSeparatorView = cell.findSectionTopSeparator()
+
+        // Then
+        XCTAssertNil(foundSeparatorView)
+    }
 }
--- WooCommerce/WooCommerceTests/Testing/XCTestCase+Assertions.swift
+++ WooCommerce/WooCommerceTests/Testing/XCTestCase+Assertions.swift
@@ -10,4 +10,13 @@ extension XCTestCase {
     func assertEmpty<Element>(_ array: [Element], file: StaticString = #file, line: UInt = #line) {
         XCTAssertTrue(array.isEmpty, "Expected array \(array) to be empty.", file: file, line: line)
     }
+
+    /// Asserts that `lhs` has the same pointer address as `rhs`.
+    ///
+    func assertThat(_ lhs: AnyObject?, isIdenticalTo rhs: AnyObject?, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertTrue(lhs === rhs,
+                      "Expected object \(String(describing: lhs)) to have the same pointer address as \(String(describing: rhs)).",
+                      file: file,
+                      line: line)
+    }
 }

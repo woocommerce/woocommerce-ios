e8a178173c47f4dac3df5e019d9206c1a720980a
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-05 12:29:47 -0500

    Add IBOutlet and configuration function

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/LargeTitleHeaderView.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/LargeTitleHeaderView.swift
@@ -4,5 +4,13 @@ import UIKit
 /// LargeTitleHeaderView: Header view for the entire table. Not a section header.
 ///
 final class LargeTitleHeaderView: UIView {
+    @IBOutlet private weak var titleLabel: UILabel?
 
+    public func setTitle(_ titleText: String?, with font: UIFont? = nil) {
+        titleLabel?.text = titleText
+
+        if let font = font {
+            titleLabel?.font = font
+        }
+    }
 }

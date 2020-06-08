1aab388723d28eae058644fa5e491e7e509927b5
Shiki <jayson@basanes.net>
2020-06-08 10:27:58 -0600

    Add generated Models+Copiable.generated.swift file

---
+++ Networking/Networking/Model/Copiable/Models+Copiable.generated.swift
@@ -0,0 +1,29 @@
+// Generated using Sourcery 0.18.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+extension ProductImage {
+    public func copy(
+        imageID: CopiableProp<Int64> = .copy,
+        dateCreated: CopiableProp<Date> = .copy,
+        dateModified: NullableCopiableProp<Date> = .copy,
+        src: CopiableProp<String> = .copy,
+        name: NullableCopiableProp<String> = .copy,
+        alt: NullableCopiableProp<String> = .copy
+    ) -> Self {
+        let imageID = imageID ?? self.imageID
+        let dateCreated = dateCreated ?? self.dateCreated
+        let dateModified = dateModified ?? self.dateModified
+        let src = src ?? self.src
+        let name = name ?? self.name
+        let alt = alt ?? self.alt
+
+        return ProductImage(
+            imageID: imageID,
+            dateCreated: dateCreated,
+            dateModified: dateModified,
+            src: src,
+            name: name,
+            alt: alt
+        )
+    }
+}

6e58590fafa4e96bd4abb38fb4d6313b915dda3a
Paolo Musolino <info@codeido.com>
2020-06-08 15:04:36 +0200

    feat: generated new target TodayStatsWidget

---
+++ WooCommerce/TodayStatsWidget/TodayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodayViewController.swift
+//  TodayStatsWidget
+//
+//  Created by Paolo Musolino on 08/06/2020.
+//  Copyright © 2020 Automattic. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}

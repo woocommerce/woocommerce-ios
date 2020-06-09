3451bfcb4385fd126fa5f5bd17fc9d3b39434248
Paolo Musolino <info@codeido.com>
2020-06-09 12:51:57 +0200

    feat: new cell SelectedWebsiteInTodayWidgetTableViewCell

---
+++ WooCommerce/TodayStatsWidget/Cells/SelectedWebsiteInTodayWidgetTableViewCell.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// MARK: - SelectedWebsiteInTodayWidgetTableViewCell
+//
+class SelectedWebsiteInTodayWidgetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    
+}
+
+private extension SelectedWebsiteInTodayWidgetTableViewCell {
+    
+//    func configureHeaderLabels() {
+//        visitorsHeaderLabel.applyHeadlineStyle()
+//        ordersHeaderLabel.applyHeadlineStyle()
+//        revenueHeaderLabel.applyHeadlineStyle()
+//    }
+//
+//    func configureBodyLabels() {
+//        visitorsBodyLabel.applySecondaryBodyStyle()
+//        ordersBodyLabel.applySecondaryBodyStyle()
+//        revenueBodyLabel.applySecondaryBodyStyle()
+//    }
+}

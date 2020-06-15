ab61d0265906021232b14ccf5ac6de69c2acb81f
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-15 10:15:54 -0500

    Manually report Instruction cell's reuse identifier

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/InstructionTableViewCell.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/InstructionTableViewCell.swift
@@ -5,6 +5,8 @@ import UIKit
 ///
 public class InstructionTableViewCell: UITableViewCell {
 
+    public static let reuseIdentifier = "InstructionTableViewCell"
+
     @IBOutlet public var instructionLabel: UILabel!
 
     public override func awakeFromNib() {

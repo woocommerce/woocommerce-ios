41b354f0c3910ba98bcedc43963611a74e5ede1d
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-16 14:42:14 -0500

    Declare borderView and textField outlets. Connect outlets to nib file

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/TextFieldTableViewCell.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/TextFieldTableViewCell.swift
@@ -1,11 +1,14 @@
 import UIKit
 
 
-/// TextFieldTableViewCell: a textfield in a UITableViewCell.
+/// TextFieldTableViewCell: a textfield with a custom border line in a UITableViewCell.
 ///
 class TextFieldTableViewCell: UITableViewCell {
 
     public static let reuseIdentifier = "TextFieldTableViewCell"
+
+    @IBOutlet var borderView: UIView!
+    @IBOutlet var textField: UITextField!
 
     override func awakeFromNib() {
         super.awakeFromNib()

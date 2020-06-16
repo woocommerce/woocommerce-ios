0e4618055f67442206d0ea5aeb3f51ff6bc85994
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-16 14:29:50 -0500

    Add TextFieldTableViewCell to the project

---
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/TextFieldTableViewCell.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+
+/// TextFieldTableViewCell: a textfield in a UITableViewCell.
+///
+class TextFieldTableViewCell: UITableViewCell {
+
+    public static let reuseIdentifier = "TextFieldTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

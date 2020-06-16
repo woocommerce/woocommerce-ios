4bd9c37dd6002e0bfca97b950c47fbff54512cb0
Paolo Musolino <info@codeido.com>
2020-06-16 18:02:27 +0200

    feat: ProductCategory model now conform to GeneratedCopiable

--- Networking/Networking/Model/Product/ProductCategory.swift
+++ Networking/Networking/Model/Product/ProductCategory.swift
@@ -3,7 +3,7 @@ import Foundation
 
 /// Represents a ProductCategory entity.
 ///
-public struct ProductCategory: Decodable {
+public struct ProductCategory: Decodable, GeneratedCopiable {
     public let categoryID: Int64
     public let siteID: Int64
     public let parentID: Int64

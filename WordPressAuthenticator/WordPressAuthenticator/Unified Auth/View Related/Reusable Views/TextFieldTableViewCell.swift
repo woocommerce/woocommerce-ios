457f1f6ca0f44e5bf12871403a207f8d8c784966
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-17 16:15:08 -0500

    Adjust border line height constraint

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/TextFieldTableViewCell.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/TextFieldTableViewCell.swift
@@ -7,7 +7,12 @@ class TextFieldTableViewCell: UITableViewCell {
 
     public static let reuseIdentifier = "TextFieldTableViewCell"
 
+    private var hairlineBorderWidth: CGFloat {
+        return 1.0 / UIScreen.main.scale
+    }
+
     @IBOutlet var borderView: UIView!
+    @IBOutlet var borderWidth: NSLayoutConstraint!
     @IBOutlet var textField: UITextField!
 
     override func awakeFromNib() {
@@ -15,5 +20,6 @@ class TextFieldTableViewCell: UITableViewCell {
 
         let borderColor = WordPressAuthenticator.shared.unifiedStyle?.borderColor ?? WordPressAuthenticator.shared.style.primaryNormalBorderColor
         borderView.backgroundColor = borderColor
+        borderWidth.constant = hairlineBorderWidth
     }
 }

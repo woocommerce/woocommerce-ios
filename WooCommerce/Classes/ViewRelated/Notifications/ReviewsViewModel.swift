b9e887f84d54041a02bb28318a94f578834e37cb
Shiki <jayson@basanes.net>
2020-06-18 10:19:01 -0600

    ReviewsVM: reload table after performFetch

--- WooCommerce/Classes/ViewRelated/Notifications/ReviewsViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Notifications/ReviewsViewModel.swift
@@ -56,6 +56,9 @@ final class ReviewsViewModel {
         } catch {
             CrashLogging.logError(error)
         }
+
+        // Reload table because observeReviews() executes performFetch()
+        tableView.reloadData()
     }
 
     func refreshResults() {

77ed1965be5bbb6c1753c85cb56e14623e0b4e41
Paolo Musolino <info@codeido.com>
2020-06-18 16:27:23 +0200

    feat: added Section struct and Row enum in AddProductCategoryViewController

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Edit Categories/Add Category/AddProductCategoryViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Edit Categories/Add Category/AddProductCategoryViewController.swift
@@ -11,10 +11,37 @@ final class AddProductCategoryViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    
+}
+
+
+// MARK: - Private Types
+//
+private extension AddProductCategoryViewController {
+
+    struct Section {
+        let rows: [Row]
+    }
+
+    enum Row: CaseIterable {
+        case title
+        case parentCategory
+
+        var type: UITableViewCell.Type {
+            switch self {
+            case .title:
+                return UnitInputTableViewCell.self
+            case .parentCategory:
+                return SwitchTableViewCell.self
+            }
+        }
+
+        var reuseIdentifier: String {
+            return type.reuseIdentifier
+        }
+    }
 }

2c9c1334b882fa1aecfd3bc94e5ae8149d1237f3
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-19 10:50:33 -0500

    Add PlainTextButtonTableViewCell class and nib file

---
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/PlainTextButtonTableViewCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+
+/// PlainTextButtonTableViewCell: a plain text button with default styles.
+///
+class PlainTextButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}

6ee51f530a26d6093cbd658638ddfdf37b11dccb
Shiki <jayson@basanes.net>
2020-06-19 19:02:43 -0600

    swiftlint.sh: Only lint the modified and untracked files
    Based on manual testing, if we run swiftlint on all the files,
    it takes 0.4 seconds. If we only run on the changed files, it can
    take up to 0.3 seconds. Sometimes 0.1 seconds.
    
    I removed `set -e` because we want to handle when to exit by ourselves.

--- Scripts/build-phases/swiftlint.sh
+++ Scripts/build-phases/swiftlint.sh
@@ -4,8 +4,6 @@
 # Runs SwiftLint on the whole workspace
 #
 
-set -e
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Temporarily move to the root directory so that SwiftLint can correctly
@@ -21,7 +19,27 @@ if ! which $SWIFTLINT >/dev/null; then
   exit 1
 fi
 
-$SWIFTLINT --config $CONFIG_FILE --quiet
+# Run SwiftLint on the modified files.
+#
+# The `|| true` at the end is to stop `grep` from returning a non-zero exit if there
+# are no matches. Xcode's build will fail if we don't do this.
+#
+MODIFIED_FILES=`git diff --name-only --diff-filter=d HEAD | grep -v Pods | grep -G "\.swift$" || true`
+echo $MODIFIED_FILES | xargs $SWIFTLINT --config $CONFIG_FILE --quiet
+MODIFIED_FILES_LINT_RESULT=$?
+
+# Run SwiftLint on the added files
+ADDED_FILES=`git ls-files --others --exclude-standard | grep -G "\.swift$" || true`
+echo $ADDED_FILES | xargs $SWIFTLINT --config $CONFIG_FILE --quiet
+ADDED_FILES_LINT_RESULT=$?
 
 # Restore the previous directory
 popd > /dev/null
+
+# Exit with non-zero if SwiftLint found a serious violation in the linted files. 
+#
+# This stops Xcode from complaining about "...did not return a nonzero exit code...".
+#
+if [ $MODIFIED_FILES_LINT_RESULT -ne 0 ] || [ $ADDED_FILES_LINT_RESULT -ne 0 ] ; then 
+  exit 1
+fi

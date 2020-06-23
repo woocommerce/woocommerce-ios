4bd6b2e25a5ac81a3b5e57309eaede9883352b44
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-23 11:41:36 -0500

    Change the TextFieldTableViewCell to be configurable
    Don't allow direct access to IBOutlet properties

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/TextFieldTableViewCell.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/TextFieldTableViewCell.swift
@@ -5,21 +5,69 @@ import UIKit
 ///
 final class TextFieldTableViewCell: UITableViewCell {
 
+    /// Private properties
+    ///
+    @IBOutlet private weak var borderView: UIView!
+    @IBOutlet private weak var borderWidth: NSLayoutConstraint!
+    @IBOutlet private weak var textField: UITextField!
+
+    /// Public properties
+    ///
     public static let reuseIdentifier = "TextFieldTableViewCell"
 
     private var hairlineBorderWidth: CGFloat {
         return 1.0 / UIScreen.main.scale
     }
 
-    @IBOutlet private weak var borderView: UIView!
-    @IBOutlet private weak var borderWidth: NSLayoutConstraint!
-    @IBOutlet weak var textField: UITextField!
-
     override func awakeFromNib() {
         super.awakeFromNib()
+        styleBorder()
+    }
 
+    public func configureTextFieldStyle(with style: TextFieldStyle = .url, and placeholder: String?) {
+        applyTextFieldStyle(style)
+        textField.placeholder = placeholder
+    }
+}
+
+
+// MARK: - Private methods
+private extension TextFieldTableViewCell {
+
+    /// Style the bottom cell border, called borderView.
+    ///
+    func styleBorder() {
         let borderColor = WordPressAuthenticator.shared.unifiedStyle?.borderColor ?? WordPressAuthenticator.shared.style.primaryNormalBorderColor
         borderView.backgroundColor = borderColor
         borderWidth.constant = hairlineBorderWidth
+    }
+
+    /// Style the textField.
+    ///
+    func applyTextFieldStyle(_ style: TextFieldStyle) {
+        switch style {
+        case .url:
+            textField.keyboardType = .URL
+            textField.returnKeyType = .continue
+            textField.autocorrectionType = .no
+            textField.becomeFirstResponder()
+        default:
+            textField.returnKeyType = .continue
+            textField.autocorrectionType = .no
+            textField.becomeFirstResponder()
+        }
+    }
+}
+
+
+// MARK: - Constants
+extension TextFieldTableViewCell {
+
+    /// TextField configuration options.
+    ///
+    enum TextFieldStyle {
+        case url
+        case username
+        case password
     }
 }

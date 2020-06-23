42df150e008aebb1979aebf2312e72b6f0fe76c2
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-06-23 11:45:49 -0500

    Fixes string that went too long

--- WooCommerce/Classes/ViewRelated/Dashboard/Factories/DashboardTopBannerFactory.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Factories/DashboardTopBannerFactory.swift
@@ -18,7 +18,8 @@ private extension DashboardTopBannerFactory {
     enum DeprecatedStatsConstants {
         static let title = NSLocalizedString("Upgrade to keep seeing your stats", comment: "Banner title in my store when stats will be deprecated")
         static let info = NSLocalizedString(
-            "We’ve rolled out improvements to our analytics. Upgrade to WooCommerce 4.0 or above or install WooCommerce Admin plugin to keep seeing your stats after September 1, 2020.",
+            "We’ve rolled out improvements to our analytics. Upgrade to WooCommerce 4.0 or above or install WooCommerce Admin plugin to keep seeing " +
+            "your stats after September 1, 2020.",
             comment: "Banner caption in my store when the stats will be deprecated")
         static let icon = UIImage.syncImage
     }

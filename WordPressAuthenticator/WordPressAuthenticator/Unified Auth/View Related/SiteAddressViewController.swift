1ef5cb168a292d50114aca53fdaee09d43db51e9
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-24 16:42:49 -0500

    Import more logic from LoginSiteAddressViewController

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
@@ -241,7 +241,155 @@ private extension SiteAddressViewController {
                 return TextFieldTableViewCell.reuseIdentifier
             case .findSiteAddress:
                 return TextLinkButtonTableViewCell.reuseIdentifier
+// MARK: - Instance Methods
+extension SiteAddressViewController {
+
+    /// Validates what is entered in the various form fields and, if valid,
+    /// proceeds with the submit action.
+    ///
+    @objc func validateForm() {
+        view.endEditing(true)
+        displayError(message: "")
+
+        // We need to to this here because before this point we need the URL to be pre-validated
+        // exactly as the user inputs it, and after this point we need the URL to be the base site URL.
+        // This isn't really great, but it's the only sane solution I could come up with given the current
+        // architecture of this pod.
+        loginFields.siteAddress = WordPressAuthenticator.baseSiteURL(string: loginFields.siteAddress)
+
+        configureViewLoading(true)
+
+        let facade = WordPressXMLRPCAPIFacade()
+        facade.guessXMLRPCURL(forSite: loginFields.siteAddress, success: { [weak self] (url) in
+            // Success! We now know that we have a valid XML-RPC endpoint.
+            // At this point, we do NOT know if this is a WP.com site or a self-hosted site.
+            if let url = url {
+                self?.loginFields.meta.xmlrpcURL = url as NSURL
             }
+            // Let's try to grab site info in preparation for the next screen.
+            self?.fetchSiteInfo()
+
+        }, failure: { [weak self] (error) in
+            guard let error = error, let self = self else {
+                return
+            }
+            DDLogError(error.localizedDescription)
+            WordPressAuthenticator.track(.loginFailedToGuessXMLRPC, error: error)
+            WordPressAuthenticator.track(.loginFailed, error: error)
+            self.configureViewLoading(false)
+
+            let err = self.originalErrorOrError(error: error as NSError)
+
+            if let xmlrpcValidatorError = err as? WordPressOrgXMLRPCValidatorError {
+                self.displayError(message: xmlrpcValidatorError.localizedDescription, moveVoiceOverFocus: true)
+
+            } else if (err.domain == NSURLErrorDomain && err.code == NSURLErrorCannotFindHost) ||
+                (err.domain == NSURLErrorDomain && err.code == NSURLErrorNetworkConnectionLost) {
+                // NSURLErrorNetworkConnectionLost can be returned when an invalid URL is entered.
+                let msg = NSLocalizedString(
+                    "The site at this address is not a WordPress site. For us to connect to it, the site must use WordPress.",
+                    comment: "Error message shown a URL does not point to an existing site.")
+                self.displayError(message: msg, moveVoiceOverFocus: true)
+
+            } else {
+                self.displayError(error as NSError, sourceTag: self.sourceTag)
+            }
+        })
+    }
+
+    @objc func fetchSiteInfo() {
+        let baseSiteUrl = WordPressAuthenticator.baseSiteURL(string: loginFields.siteAddress)
+        let service = WordPressComBlogService()
+        let successBlock: (WordPressComSiteInfo) -> Void = { [weak self] siteInfo in
+            guard let self = self else {
+                return
+            }
+            self.configureViewLoading(false)
+            if siteInfo.isWPCom && WordPressAuthenticator.shared.delegate?.allowWPComLogin == false {
+                // Hey, you have to log out of your existing WP.com account before logging into another one.
+                self.promptUserToLogoutBeforeConnectingWPComSite()
+                return
+            }
+            self.presentNextControllerIfPossible(siteInfo: siteInfo)
         }
+        service.fetchUnauthenticatedSiteInfoForAddress(for: baseSiteUrl, success: successBlock, failure: { [weak self] error in
+            self?.configureViewLoading(false)
+            guard let self = self else {
+                return
+            }
+            self.presentNextControllerIfPossible(siteInfo: nil)
+        })
+    }
+
+    func presentNextControllerIfPossible(siteInfo: WordPressComSiteInfo?) {
+        WordPressAuthenticator.shared.delegate?.shouldPresentUsernamePasswordController(for: siteInfo, onCompletion: { (error, isSelfHosted) in
+            guard let originalError = error else {
+
+                if isSelfHosted {
+                    self.showSelfHostedUsernamePassword()
+                    return
+                }
+
+                self.showWPUsernamePassword()
+                return
+            }
+
+            self.displayError(message: originalError.localizedDescription)
+        })
+    }
+
+    @objc func originalErrorOrError(error: NSError) -> NSError {
+        guard let err = error.userInfo[XMLRPCOriginalErrorKey] as? NSError else {
+            return error
+        }
+        return err
+    }
+
+    /// Here we will continue with the self-hosted flow.
+    ///
+    @objc func showSelfHostedUsernamePassword() {
+        configureViewLoading(false)
+        guard let vc = LoginSelfHostedViewController.instantiate(from: .login) else {
+           DDLogError("Failed to navigate from LoginEmailViewController to LoginSelfHostedViewController")
+               return
+           }
+
+       vc.loginFields = loginFields
+       vc.dismissBlock = dismissBlock
+       vc.errorToPresent = errorToPresent
+
+       navigationController?.pushViewController(vc, animated: true)
+    }
+
+    /// Break away from the self-hosted flow.
+    /// Display a username / password login screen for WP.com sites.
+    ///
+    @objc func showWPUsernamePassword() {
+        configureViewLoading(false)
+
+        guard let vc = LoginUsernamePasswordViewController.instantiate(from: .login) else {
+            DDLogError("Failed to navigate from LoginSiteAddressViewController to LoginUsernamePasswordViewController")
+                return
+        }
+
+        vc.loginFields = loginFields
+        vc.dismissBlock = dismissBlock
+        vc.errorToPresent = errorToPresent
+
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+    /// Whether the form can be submitted.
+    ///
+    @objc func canSubmit() -> Bool {
+        return loginFields.validateSiteForSignin()
+    }
+
+    @objc private func promptUserToLogoutBeforeConnectingWPComSite() {
+        let acceptActionTitle = NSLocalizedString("OK", comment: "Alert dismissal title")
+        let message = NSLocalizedString("Please log out before connecting to a different wordpress.com site", comment: "Message for alert to prompt user to logout before connecting to a different wordpress.com site.")
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alertController.addDefaultActionWithTitle(acceptActionTitle)
+        present(alertController, animated: true)
     }
 }

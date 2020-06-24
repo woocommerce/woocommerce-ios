a8cc7cdd19205ca9aec660b75b2acb1a88b510cb
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-24 10:27:14 -0500

    Rename InstructionTableViewCell to TextLabelTableViewCell

---
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/TextLabelTableViewCell.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+
+/// TextLabelTableViewCell: a text label in a UITableViewCell.
+///
+public final class TextLabelTableViewCell: UITableViewCell {
+
+    /// Private properties
+    ///
+    @IBOutlet private weak var label: UILabel!
+
+    /// Public properties
+    ///
+    public static let reuseIdentifier = "TextLabelTableViewCell"
+
+    public func configureLabel(text: String?, style: TextLabelStyle = .body) {
+        label.text = text
+
+        switch style {
+        case .body:
+            label.textColor = WordPressAuthenticator.shared.unifiedStyle?.textColor ?? WordPressAuthenticator.shared.style.instructionColor
+            label.font = UIFont.preferredFontForTextStyle(.body)
+        }
+    }
+}
+
+public extension TextLabelTableViewCell {
+    /// The label style to display
+    ///
+    enum TextLabelStyle {
+        case body
+    }
+}

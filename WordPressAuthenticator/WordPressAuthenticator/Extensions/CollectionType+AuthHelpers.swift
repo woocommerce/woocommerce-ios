230b7c6f4e4cd109ef3ccc69c7a4226b8d783772
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-24 11:31:58 -0500

    Add a helper method to access indices safely on Collections

---
+++ WordPressAuthenticator/WordPressAuthenticator/Extensions/CollectionType+AuthHelpers.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+
+/// Collection Helpers for Authenticator.
+///
+extension Collection {
+    /// Returns the element at the specified index if it is within bounds, otherwise nil.
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}

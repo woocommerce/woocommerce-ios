80e1e552842502607bdff4061b1b657ed480e113
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-24 13:02:26 -0500

    Add TextLinkButtonTableViewCell

---
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/TextLinkButtonTableViewCell.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+
+/// TextLinkButtonTableViewCell: a plain button made to look like a text link.
+///
+class TextLinkButtonTableViewCell: UITableViewCell {
+
+    /// Private properties
+    ///
+    @IBOutlet private weak var button: UIButton!
+    @IBAction private func textLinkButtonTapped(_ sender: UIButton) {
+        actionHandler?()
+    }
+
+    /// Public properties
+    ///
+    public static let reuseIdentifier = "TextLinkButtonTableViewCell"
+
+    public var actionHandler: (() -> Void)?
+
+    public func configureButton(text: String?) {
+        button.setTitle(text, for: .normal)
+
+        let buttonTitleColor = WordPressAuthenticator.shared.unifiedStyle?.textButtonColor ?? WordPressAuthenticator.shared.style.textButtonColor
+        let buttonHighlightColor = WordPressAuthenticator.shared.unifiedStyle?.textButtonHighlightColor ?? WordPressAuthenticator.shared.style.textButtonHighlightColor
+        button.setTitleColor(buttonTitleColor, for: .normal)
+        button.setTitleColor(buttonHighlightColor, for: .highlighted)
+
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .callout)
+        button.titleLabel?.adjustsFontForContentSizeCategory = true
+    }
+}

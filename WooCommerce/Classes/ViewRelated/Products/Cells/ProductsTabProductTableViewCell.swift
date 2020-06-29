e0fb7edf7469e1bbcb0a800c76aae93d095fa101
Jaclyn Chen <watertranquil@gmail.com>
2020-06-29 16:56:12 +0800

    Move `Product` SwiftUI preview helper to a separate extension.

--- WooCommerce/Classes/ViewRelated/Products/Cells/ProductsTabProductTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/Products/Cells/ProductsTabProductTableViewCell.swift
@@ -146,7 +146,7 @@ private extension ProductsTabProductTableViewCell {
             // This multiplier matches the required size(37.5pt) for a 375pt(as per designs) content view width
             productImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.1),
             productImageView.widthAnchor.constraint(equalTo: productImageView.heightAnchor)
-            ])
+        ])
     }
 
     func configureBottomBorderView() {
@@ -204,69 +204,8 @@ private struct ProductsTabProductTableViewCellRepresentable: UIViewRepresentable
 
 @available(iOS 13.0, *)
 struct ProductsTabProductTableViewCell_Previews: PreviewProvider {
-    private static let product = Product(siteID: 1,
-                                  productID: 2,
-                                  name: "Woo super cool shiny product",
-                                  slug: "",
-                                  permalink: "",
-                                  dateCreated: Date(),
-                                  dateModified: Date(),
-                                  dateOnSaleStart: Date(),
-                                  dateOnSaleEnd: Date(),
-                                  productTypeKey: ProductType.variable.rawValue,
-                                  statusKey: ProductStatus.draft.rawValue,
-                                  featured: false,
-                                  catalogVisibilityKey: ProductCatalogVisibility.hidden.rawValue,
-                                  fullDescription: "",
-                                  briefDescription: "",
-                                  sku: "sku",
-                                  price: "0",
-                                  regularPrice: "32.5",
-                                  salePrice: "17.6",
-                                  onSale: false,
-                                  purchasable: true,
-                                  totalSales: 0,
-                                  virtual: false,
-                                  downloadable: false,
-                                  downloads: [],
-                                  downloadLimit: -1,
-                                  downloadExpiry: -1,
-                                  buttonText: "",
-                                  externalURL: "",
-                                  taxStatusKey: ProductTaxStatus.taxable.rawValue,
-                                  taxClass: "",
-                                  manageStock: true,
-                                  stockQuantity: 32,
-                                  stockStatusKey: ProductStockStatus.inStock.rawValue,
-                                  backordersKey: ProductBackordersSetting.allowed.rawValue,
-                                  backordersAllowed: false,
-                                  backordered: false,
-                                  soldIndividually: true,
-                                  weight: "2.9",
-                                  dimensions: ProductDimensions(length: "12", width: "26", height: "16"),
-                                  shippingRequired: false,
-                                  shippingTaxable: false,
-                                  shippingClass: "",
-                                  shippingClassID: 0,
-                                  productShippingClass: nil,
-                                  reviewsAllowed: true,
-                                  averageRating: "4.30",
-                                  ratingCount: 23,
-                                  relatedIDs: [31, 22, 369, 414, 56],
-                                  upsellIDs: [99, 1234566],
-                                  crossSellIDs: [1234, 234234, 3],
-                                  parentID: 0,
-                                  purchaseNote: "Thank you!",
-                                  categories: [],
-                                  tags: [],
-                                  images: [],
-                                  attributes: [],
-                                  defaultAttributes: [],
-                                  variations: [],
-                                  groupedProducts: [],
-                                  menuOrder: 0)
-    private static var nonSelectedViewModel = ProductsTabProductViewModel(product: product, isSelected: false)
-    private static var selectedViewModel = ProductsTabProductViewModel(product: product.copy(statusKey: ProductStatus.pending.rawValue),
+    private static var nonSelectedViewModel = ProductsTabProductViewModel(product: Product(), isSelected: false)
+    private static var selectedViewModel = ProductsTabProductViewModel(product: Product().copy(statusKey: ProductStatus.pending.rawValue),
                                                                        isSelected: true)
 
     private static func makeStack() -> some View {

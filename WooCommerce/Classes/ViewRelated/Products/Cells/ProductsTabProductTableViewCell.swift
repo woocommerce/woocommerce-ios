8201634a2737413a4a673e1553f987309741f1bc
Jaclyn Chen <watertranquil@gmail.com>
2020-06-29 16:48:00 +0800

    Add SwiftUI preview for non-selected and selected states.

--- WooCommerce/Classes/ViewRelated/Products/Cells/ProductsTabProductTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/Products/Cells/ProductsTabProductTableViewCell.swift
@@ -1,5 +1,4 @@
 import UIKit
-import Kingfisher
 
 final class ProductsTabProductTableViewCell: UITableViewCell {
     private lazy var productImageView: UIImageView = {
@@ -184,3 +183,117 @@ private extension ProductsTabProductTableViewCell {
         static let imageBackgroundColor = UIColor.listForeground
     }
 }
+
+#if canImport(SwiftUI) && DEBUG
+
+import SwiftUI
+
+import Yosemite
+
+private struct ProductsTabProductTableViewCellRepresentable: UIViewRepresentable {
+    let viewModel: ProductsTabProductViewModel
+
+    func makeUIView(context: Context) -> ProductsTabProductTableViewCell {
+        .init(style: .default, reuseIdentifier: "cell")
+    }
+
+    func updateUIView(_ view: ProductsTabProductTableViewCell, context: Context) {
+        view.update(viewModel: viewModel, imageService: ServiceLocator.imageService)
+    }
+}
+
+@available(iOS 13.0, *)
+struct ProductsTabProductTableViewCell_Previews: PreviewProvider {
+    private static let product = Product(siteID: 1,
+                                  productID: 2,
+                                  name: "Woo super cool shiny product",
+                                  slug: "",
+                                  permalink: "",
+                                  dateCreated: Date(),
+                                  dateModified: Date(),
+                                  dateOnSaleStart: Date(),
+                                  dateOnSaleEnd: Date(),
+                                  productTypeKey: ProductType.variable.rawValue,
+                                  statusKey: ProductStatus.draft.rawValue,
+                                  featured: false,
+                                  catalogVisibilityKey: ProductCatalogVisibility.hidden.rawValue,
+                                  fullDescription: "",
+                                  briefDescription: "",
+                                  sku: "sku",
+                                  price: "0",
+                                  regularPrice: "32.5",
+                                  salePrice: "17.6",
+                                  onSale: false,
+                                  purchasable: true,
+                                  totalSales: 0,
+                                  virtual: false,
+                                  downloadable: false,
+                                  downloads: [],
+                                  downloadLimit: -1,
+                                  downloadExpiry: -1,
+                                  buttonText: "",
+                                  externalURL: "",
+                                  taxStatusKey: ProductTaxStatus.taxable.rawValue,
+                                  taxClass: "",
+                                  manageStock: true,
+                                  stockQuantity: 32,
+                                  stockStatusKey: ProductStockStatus.inStock.rawValue,
+                                  backordersKey: ProductBackordersSetting.allowed.rawValue,
+                                  backordersAllowed: false,
+                                  backordered: false,
+                                  soldIndividually: true,
+                                  weight: "2.9",
+                                  dimensions: ProductDimensions(length: "12", width: "26", height: "16"),
+                                  shippingRequired: false,
+                                  shippingTaxable: false,
+                                  shippingClass: "",
+                                  shippingClassID: 0,
+                                  productShippingClass: nil,
+                                  reviewsAllowed: true,
+                                  averageRating: "4.30",
+                                  ratingCount: 23,
+                                  relatedIDs: [31, 22, 369, 414, 56],
+                                  upsellIDs: [99, 1234566],
+                                  crossSellIDs: [1234, 234234, 3],
+                                  parentID: 0,
+                                  purchaseNote: "Thank you!",
+                                  categories: [],
+                                  tags: [],
+                                  images: [],
+                                  attributes: [],
+                                  defaultAttributes: [],
+                                  variations: [],
+                                  groupedProducts: [],
+                                  menuOrder: 0)
+    private static var nonSelectedViewModel = ProductsTabProductViewModel(product: product, isSelected: false)
+    private static var selectedViewModel = ProductsTabProductViewModel(product: product.copy(statusKey: ProductStatus.pending.rawValue),
+                                                                       isSelected: true)
+
+    private static func makeStack() -> some View {
+        VStack {
+            ProductsTabProductTableViewCellRepresentable(viewModel: nonSelectedViewModel)
+            ProductsTabProductTableViewCellRepresentable(viewModel: selectedViewModel)
+        }
+        .background(Color(UIColor.listForeground))
+    }
+
+    static var previews: some View {
+        Group {
+            makeStack()
+                .previewLayout(.fixed(width: 320, height: 150))
+                .previewDisplayName("Light")
+
+            makeStack()
+                .previewLayout(.fixed(width: 320, height: 150))
+                .environment(\.colorScheme, .dark)
+                .previewDisplayName("Dark")
+
+            makeStack()
+                .previewLayout(.fixed(width: 320, height: 400))
+                .environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
+                .previewDisplayName("Large Font")
+        }
+    }
+}
+
+#endif

6e34638fcf61b71b2a567e9f6d267c8ad001b85b
Paolo Musolino <info@codeido.com>
2020-06-30 11:34:57 +0200

    feat: Added file product-tags-empty.json

---
+++ Yosemite/YosemiteTests/Stores/ProductTagStoreTests.swift
@@ -0,0 +1,13 @@
+//
+//  ProductTagStoreTests.swift
+//  YosemiteTests
+//
+//  Created by Paolo Musolino on 30/06/2020.
+//  Copyright © 2020 Automattic. All rights reserved.
+//
+
+import UIKit
+
+class ProductTagStoreTests: XCTestCase {
+
+}

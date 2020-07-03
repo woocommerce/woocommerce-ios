f85c310400e72657fc14fa5c0019fdfc36317017
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-07-03 12:41:42 -0500

    Update TopBannerView to be actionable and expandable

--- WooCommerce/Classes/ViewRelated/Top Banner/TopBannerViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Top Banner/TopBannerViewModel.swift
@@ -3,30 +3,41 @@ import UIKit
 /// View model for `TopBannerView`.
 ///
 struct TopBannerViewModel {
+
+    enum TopButtonType {
+        case chevron(handler: (() -> Void)?)
+        case dismiss(handler: (() -> Void)?)
+
+        var handler: (() -> Void)? {
+            switch self {
+            case let .chevron(handler), let .dismiss(handler):
+                return handler
+            }
+        }
+    }
+
     let title: String?
     let infoText: String?
     let icon: UIImage?
     let actionButtonTitle: String?
     let isExpanded: Bool
     let actionHandler: (() -> Void)?
-    let dismissHandler: (() -> Void)?
-    let expandedStateChangeHandler: (() -> Void)?
+    let topButton: TopButtonType
 
     /// Used when the top banner is not actionable.
     ///
     init(title: String?,
          infoText: String?,
          icon: UIImage?,
-         isExpanded: Bool = true,
-         expandedStateChangeHandler: (() -> Void)?) {
+         isExpanded: Bool,
+         topButton: TopButtonType) {
         self.title = title
         self.infoText = infoText
         self.icon = icon
         self.isExpanded = isExpanded
         self.actionButtonTitle = nil
         self.actionHandler = nil
-        self.dismissHandler = nil
-        self.expandedStateChangeHandler = expandedStateChangeHandler
+        self.topButton = topButton
     }
 
     /// Used when the top banner is actionable.
@@ -37,14 +48,13 @@ struct TopBannerViewModel {
          actionButtonTitle: String,
          isExpanded: Bool = true,
          actionHandler: @escaping () -> Void,
-         dismissHandler: @escaping () -> Void) {
+         topButton: TopButtonType) {
         self.title = title
         self.infoText = infoText
         self.icon = icon
         self.actionButtonTitle = actionButtonTitle
         self.isExpanded = isExpanded
         self.actionHandler = actionHandler
-        self.dismissHandler = dismissHandler
-        self.expandedStateChangeHandler = nil
+        self.topButton = topButton
     }
 }

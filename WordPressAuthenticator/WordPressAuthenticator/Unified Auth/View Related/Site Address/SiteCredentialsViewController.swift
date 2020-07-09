4e536a75dfbb76dff3df08f922722d8d8b195450
Thuy.Copeland <thuycopeland@gmail.com>
2020-07-09 11:20:41 -0500

    Inherit from LoginViewController

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Site Address/SiteCredentialsViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Site Address/SiteCredentialsViewController.swift
@@ -4,7 +4,7 @@ import UIKit
 /// Part two of the self-hosted sign in flow: username + password. Used by WPiOS and NiOS.
 /// A valid site address should be acquired before presenting this view controller.
 ///
-class SiteCredentialsViewController: UIViewController {
+class SiteCredentialsViewController: LoginViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

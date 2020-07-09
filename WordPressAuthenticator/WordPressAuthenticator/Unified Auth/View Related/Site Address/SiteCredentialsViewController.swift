d07a52fb433a8d72b54816d407fbb668dd41acb8
Thuy.Copeland <thuycopeland@gmail.com>
2020-07-09 11:19:09 -0500

    Add a blank SiteCredentialsViewController file to the project

---
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Site Address/SiteCredentialsViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+
+/// Part two of the self-hosted sign in flow: username + password. Used by WPiOS and NiOS.
+/// A valid site address should be acquired before presenting this view controller.
+///
+class SiteCredentialsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

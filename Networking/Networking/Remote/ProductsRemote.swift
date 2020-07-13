ca9b29ce41413c389f44ccc31b6c99621a6bdb7b
Jaclyn Chen <watertranquil@gmail.com>
2020-07-13 17:20:25 +0800

    Add `GroupedProductListSelectorDataSourceTests` that are similar to `GroupedProductsViewModelTests`, and delete `GroupedProductsViewModel`.

--- Networking/Networking/Remote/ProductsRemote.swift
+++ Networking/Networking/Remote/ProductsRemote.swift
@@ -70,9 +70,15 @@ public final class ProductsRemote: Remote, ProductsEndpointsProviding {
     /// - Parameters:
     ///     - siteID: We are fetching remote products for this site.
     ///     - productIDs: The array of product IDs that are requested.
+    ///     - pageNumber: Number of page that should be retrieved.
+    ///     - pageSize: Number of products to be retrieved per page.
     ///     - completion: Closure to be executed upon completion.
     ///
-    public func loadProducts(for siteID: Int64, by productIDs: [Int64], pageNumber: Int = Default.pageNumber, pageSize: Int = Default.pageSize, completion: @escaping (Result<[Product], Error>) -> Void) {
+    public func loadProducts(for siteID: Int64,
+                             by productIDs: [Int64],
+                             pageNumber: Int = Default.pageNumber,
+                             pageSize: Int = Default.pageSize,
+                             completion: @escaping (Result<[Product], Error>) -> Void) {
         guard productIDs.isEmpty == false else {
             completion(.success([]))
             return

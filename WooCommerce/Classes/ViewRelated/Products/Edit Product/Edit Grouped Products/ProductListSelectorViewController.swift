80a0e81b6d8ea4f244d27fc74484d4748bd88117
Jaclyn Chen <watertranquil@gmail.com>
2020-07-16 16:13:07 +0800

    Remove future code.

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Edit Grouped Products/ProductListSelectorViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Edit Grouped Products/ProductListSelectorViewController.swift
@@ -62,16 +62,6 @@ private extension ProductListSelectorViewController {
 
     @objc func searchButtonTapped() {
         // TODO-2199: search products
-        let productIDsToExclude = (excludedProductIDs + productIDs).removingDuplicates()
-        let searchProductsCommand = ProductListSelectorSearchUICommand(siteID: siteID,
-                                                                       excludedProductIDs: productIDsToExclude) { [weak self] productIDs in
-                                                                        self?.didSelectProductsFromSearch(ids: productIDs)
-        }
-        let searchViewController = SearchViewController(storeID: siteID,
-                                                        command: searchProductsCommand,
-                                                        cellType: ProductsTabProductTableViewCell.self)
-        let navigationController = WooNavigationController(rootViewController: searchViewController)
-        present(navigationController, animated: true, completion: nil)
     }
 
     func didSelectProductsFromSearch(ids: [Int64]) {

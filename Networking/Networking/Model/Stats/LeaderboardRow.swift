0168a69d689c9b71ed41763d407c33cd2398ff53
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-07-20 22:35:05 -0500

    Adds LeaderboardRow type

---
+++ Networking/Networking/Model/Stats/LeaderboardRow.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+/// Represents a leaderboard item - Top Product
+///
+public struct LeaderboardRow: Decodable {
+
+    enum CodingKeys: String, CodingKey {
+        case subject
+        case subjectValue
+        case value
+    }
+
+    /// The subject of the leaderboard. - Could be: Product, Category, Customer, etc
+    ///
+    public let subject: LeaderboardRowContent<String>
+
+    /// Value associated with the subject
+    ///
+    public let subjectValue: LeaderboardRowContent<Int>
+
+    /// Value of te subject in the leaderboard
+    ///
+    public let value: LeaderboardRowContent<Int>
+}

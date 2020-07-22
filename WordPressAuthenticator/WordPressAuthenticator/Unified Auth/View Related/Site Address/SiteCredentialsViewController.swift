0368c4254be7eb2b99cdfc898f72b7a7c0eeaa6e
Stephenie Harris <scoutharris@gmail.com>
2020-07-22 16:37:48 -0600

    Set the Site Address table size per the view size.

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Site Address/SiteCredentialsViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Site Address/SiteCredentialsViewController.swift
@@ -29,6 +29,10 @@ class SiteCredentialsViewController: LoginViewController {
         navigationItem.title = WordPressAuthenticator.shared.displayStrings.logInTitle
         styleNavigationBar(forUnified: true)
 
+        // Store default margin, and size table for the view.
+        defaultTableViewMargin = tableViewLeadingConstraint?.constant ?? 0
+        setTableViewMargins(forWidth: view.frame.width)
+
 		localizePrimaryButton()
 		registerTableViewCells()
 		loadRows()

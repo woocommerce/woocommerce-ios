5c9b7021387daeafa290aa8d371831c3e65a9a76
Jaclyn Chen <watertranquil@gmail.com>
2020-07-24 13:42:56 +0800

    Move sections logic and date picker state handling from `ProductPriceSettingsViewController` to `ProductPriceSettingsViewModel`.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Edit Product/ProductPriceSettingsViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Edit Product/ProductPriceSettingsViewModelTests.swift
@@ -580,4 +580,114 @@ final class ProductPriceSettingsViewModelTests: XCTestCase {
         // Assert
         XCTAssertFalse(viewModel.hasUnsavedChanges())
     }
+
+    // MARK: - Sections
+
+    typealias Section = ProductPriceSettingsViewModel.Section
+
+    func testInitialSectionsWithoutSaleDates() {
+        // Arrange
+        let saleStartDate: Date? = nil
+        let saleEndDate: Date? = nil
+        let product = MockProduct().product(dateOnSaleStart: saleStartDate, dateOnSaleEnd: saleEndDate)
+        let viewModel = ProductPriceSettingsViewModel(product: product)
+
+        // Act
+        let sections = viewModel.sections
+
+        // Assert
+        let initialSections: [Section] = [
+            Section(title: Strings.priceSectionTitle, rows: [.price, .salePrice]),
+            Section(title: nil, rows: [.scheduleSale]),
+            Section(title: Strings.taxSectionTitle, rows: [.taxStatus, .taxClass])
+        ]
+        XCTAssertEqual(sections, initialSections)
+    }
+
+    func testTappingScheduleSaleFromRowTogglesPickerRowInSalesSection() {
+        // Arrange
+        let saleStartDate: Date? = nil
+        let saleEndDate = Date().addingTimeInterval(numberOfSecondsPerDay)
+        let product = MockProduct().product(dateOnSaleStart: saleStartDate, dateOnSaleEnd: saleEndDate)
+        let viewModel = ProductPriceSettingsViewModel(product: product)
+        let initialSections: [Section] = [
+            Section(title: Strings.priceSectionTitle, rows: [.price, .salePrice]),
+            Section(title: nil, rows: [.scheduleSale, .scheduleSaleFrom, .scheduleSaleTo, .removeSaleTo]),
+            Section(title: Strings.taxSectionTitle, rows: [.taxStatus, .taxClass])
+        ]
+        XCTAssertEqual(viewModel.sections, initialSections)
+
+        // Act
+        viewModel.didTapScheduleSaleFromRow()
+        let sectionsAfterTheFirstTap = viewModel.sections
+        viewModel.didTapScheduleSaleFromRow()
+        let sectionsAfterTheSecondTap = viewModel.sections
+
+        // Assert
+        XCTAssertEqual(sectionsAfterTheFirstTap, [
+            Section(title: Strings.priceSectionTitle, rows: [.price, .salePrice]),
+            Section(title: nil, rows: [.scheduleSale, .scheduleSaleFrom, .datePickerSaleFrom, .scheduleSaleTo, .removeSaleTo]),
+            Section(title: Strings.taxSectionTitle, rows: [.taxStatus, .taxClass])
+        ])
+        XCTAssertEqual(sectionsAfterTheSecondTap, initialSections)
+    }
+
+    func testTappingScheduleSaleToRowTogglesPickerRowInSalesSection() {
+        // Arrange
+        let saleStartDate: Date? = nil
+        let saleEndDate = Date().addingTimeInterval(numberOfSecondsPerDay)
+        let product = MockProduct().product(dateOnSaleStart: saleStartDate, dateOnSaleEnd: saleEndDate)
+        let viewModel = ProductPriceSettingsViewModel(product: product)
+        let initialSections: [Section] = [
+            Section(title: Strings.priceSectionTitle, rows: [.price, .salePrice]),
+            Section(title: nil, rows: [.scheduleSale, .scheduleSaleFrom, .scheduleSaleTo, .removeSaleTo]),
+            Section(title: Strings.taxSectionTitle, rows: [.taxStatus, .taxClass])
+        ]
+        XCTAssertEqual(viewModel.sections, initialSections)
+
+        // Act
+        viewModel.didTapScheduleSaleToRow()
+        let sectionsAfterTheFirstTap = viewModel.sections
+        viewModel.didTapScheduleSaleToRow()
+        let sectionsAfterTheSecondTap = viewModel.sections
+
+        // Assert
+        XCTAssertEqual(sectionsAfterTheFirstTap, [
+            Section(title: Strings.priceSectionTitle, rows: [.price, .salePrice]),
+            Section(title: nil, rows: [.scheduleSale, .scheduleSaleFrom, .scheduleSaleTo, .datePickerSaleTo, .removeSaleTo]),
+            Section(title: Strings.taxSectionTitle, rows: [.taxStatus, .taxClass])
+        ])
+        XCTAssertEqual(sectionsAfterTheSecondTap, initialSections)
+    }
+
+    func testRemovingSaleEndDateDeletesRemoveSaleToRow() {
+        // Arrange
+        let saleStartDate: Date? = nil
+        let saleEndDate = Date().addingTimeInterval(numberOfSecondsPerDay)
+        let product = MockProduct().product(dateOnSaleStart: saleStartDate, dateOnSaleEnd: saleEndDate)
+        let viewModel = ProductPriceSettingsViewModel(product: product)
+        let initialSections: [Section] = [
+            Section(title: Strings.priceSectionTitle, rows: [.price, .salePrice]),
+            Section(title: nil, rows: [.scheduleSale, .scheduleSaleFrom, .scheduleSaleTo, .removeSaleTo]),
+            Section(title: Strings.taxSectionTitle, rows: [.taxStatus, .taxClass])
+        ]
+        XCTAssertEqual(viewModel.sections, initialSections)
+
+        // Act
+        viewModel.handleSaleEndDateChange(nil)
+
+        // Assert
+        XCTAssertEqual(viewModel.sections, [
+            Section(title: Strings.priceSectionTitle, rows: [.price, .salePrice]),
+            Section(title: nil, rows: [.scheduleSale, .scheduleSaleFrom, .scheduleSaleTo]),
+            Section(title: Strings.taxSectionTitle, rows: [.taxStatus, .taxClass])
+        ])
+    }
+}
+
+private extension ProductPriceSettingsViewModelTests {
+    enum Strings {
+        static let priceSectionTitle = NSLocalizedString("Price", comment: "Section header title for product price")
+        static let taxSectionTitle = NSLocalizedString("Tax Settings", comment: "Section header title for product tax settings")
+    }
 }

3abc0fb82ba05bab62d93f37ed6b0bb7c3c29a62
Stephenie Harris <scoutharris@gmail.com>
2020-07-27 15:06:59 -0600

    Adding new 2FA class and view.

---
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/2FA/TwoFAViewController.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+final class TwoFAViewController: LoginViewController {
+
+    // MARK: - Properties
+    @IBOutlet private weak var tableView: UITableView!
+    
+    // support tag
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = WordPressAuthenticator.shared.displayStrings.logInTitle
+        styleNavigationBar(forUnified: true)
+        
+        // Store default margin, and size table for the view.
+        defaultTableViewMargin = tableViewLeadingConstraint?.constant ?? 0
+        setTableViewMargins(forWidth: view.frame.width)
+        
+        localizePrimaryButton()
+    }
+    
+    // MARK: - Overrides
+
+    /// Style individual ViewController backgrounds, for now.
+    ///
+    override func styleBackground() {
+        guard let unifiedBackgroundColor = WordPressAuthenticator.shared.unifiedStyle?.viewControllerBackgroundColor else {
+            super.styleBackground()
+            return
+        }
+
+        view.backgroundColor = unifiedBackgroundColor
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return WordPressAuthenticator.shared.unifiedStyle?.statusBarStyle ??
+               WordPressAuthenticator.shared.style.statusBarStyle
+    }
+
+}
+
+// MARK: - UITableViewDataSource
+
+extension TwoFAViewController: UITableViewDataSource {
+    /// Returns the number of rows in a section.
+    ///
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO: update when real cells are added.
+        return 1
+    }
+
+    /// Configure cells delegate method.
+    ///
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO: update when real cells are added.
+        return UITableViewCell()
+    }
+}

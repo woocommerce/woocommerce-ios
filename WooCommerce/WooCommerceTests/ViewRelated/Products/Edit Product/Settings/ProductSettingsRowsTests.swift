b133d8d64d602c5cf10572b54be38b191d0d7281
Paolo Musolino <info@codeido.com>
2020-07-28 18:04:35 +0200

    feat: ProductSettingsRows tests

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Edit Product/Settings/ProductSettingsRowsTests.swift
@@ -0,0 +1,65 @@
+import XCTest
+@testable import WooCommerce
+import Yosemite
+
+/// Test cases for `ProductSettingsRows`.
+///
+final class ProductSettingsRowsTests: XCTestCase {
+
+    var settings = ProductSettings(status: .draft,
+                                   featured: false,
+                                   password: nil,
+                                   catalogVisibility: .catalog,
+                                   virtual: false,
+                                   reviewsAllowed: false,
+                                   slug: "slug",
+                                   purchaseNote: nil,
+                                   menuOrder: 0)
+
+    override func setUp() {
+        super.setUp()
+
+        // Given
+        settings = ProductSettings(status: .draft,
+                                       featured: false,
+                                       password: nil,
+                                       catalogVisibility: .catalog,
+                                       virtual: false,
+                                       reviewsAllowed: false,
+                                       slug: "",
+                                       purchaseNote: nil,
+                                       menuOrder: 0)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testVirtualProductRowChangedWhenUpdated() {
+        // Given
+        let virtualProduct = ProductSettingsRows.VirtualProduct(settings)
+
+        let cell = SwitchTableViewCell()
+        virtualProduct.configure(cell: cell)
+
+        // When
+        cell.onChange?(true)
+
+        // Then
+        XCTAssertEqual(settings.virtual, true)
+    }
+
+    func testReviewsAllowedRowChangedWhenUpdated() {
+        // Given
+        let reviewsAllowed = ProductSettingsRows.ReviewsAllowed(settings)
+
+        let cell = SwitchTableViewCell()
+        reviewsAllowed.configure(cell: cell)
+
+        // When
+        cell.onChange?(true)
+
+        // Then
+        XCTAssertEqual(settings.reviewsAllowed, true)
+    }
+}

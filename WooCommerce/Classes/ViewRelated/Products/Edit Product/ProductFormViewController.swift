55a1f13fa8573d4bf66a0359e920e2acaf78d97f
Jaclyn Chen <watertranquil@gmail.com>
2020-07-29 10:54:19 +0800

    Updates based on CR feedback.

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewController.swift
@@ -132,7 +132,7 @@ final class ProductFormViewController<ViewModel: ProductFormViewModelProtocol>: 
     // MARK: - Navigation actions handling
 
     override func shouldPopOnBackButton() -> Bool {
-        if viewModel.hasUnsavedChanges() {
+        guard viewModel.hasUnsavedChanges() == false else {
             presentBackNavigationActionSheet()
             return false
         }
@@ -257,10 +257,7 @@ final class ProductFormViewController<ViewModel: ProductFormViewModelProtocol>: 
                 break
             case .variations:
                 // TODO-2509 Edit Product M3 analytics
-                guard let product = product as? Product else {
-                    return
-                }
-                guard product.variations.isNotEmpty else {
+                guard let product = product as? Product, product.variations.isNotEmpty else {
                     return
                 }
                 let variationsViewController = ProductVariationsViewController(siteID: product.siteID,

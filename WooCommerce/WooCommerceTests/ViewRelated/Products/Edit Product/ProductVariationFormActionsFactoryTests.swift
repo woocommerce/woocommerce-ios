25a5abb24bc6ec2acc2a166388bbe87d9cefee08
Jaclyn Chen <watertranquil@gmail.com>
2020-07-30 10:43:56 +0800

    Add a test case missing shipping data and rename test methods.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Edit Product/ProductVariationFormActionsFactoryTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Edit Product/ProductVariationFormActionsFactoryTests.swift
@@ -4,7 +4,7 @@ import XCTest
 @testable import Yosemite
 
 final class ProductVariationFormActionsFactoryTests: XCTestCase {
-    func testViewModelForPhysicalSimpleProductWithoutImages() {
+    func testActionsForAPhysicalProductVariationWithoutImages() {
         // Arrange
         let productVariation = Fixtures.physicalProductVariationWithoutImages
 
@@ -24,7 +24,7 @@ final class ProductVariationFormActionsFactoryTests: XCTestCase {
         XCTAssertEqual(factory.bottomSheetActions(), expectedBottomSheetActions)
     }
 
-    func testViewModelForPhysicalSimpleProductWithImages() {
+    func testActionsForAPhysicalProductVariationWithImages() {
         // Arrange
         let productVariation = Fixtures.physicalProductVariationWithImages
 
@@ -44,7 +44,26 @@ final class ProductVariationFormActionsFactoryTests: XCTestCase {
         XCTAssertEqual(factory.bottomSheetActions(), expectedBottomSheetActions)
     }
 
-    func testViewModelForDownloadableSimpleProduct() {
+    func testActionsForAPhysicalProductVariationWithImagesMissingShippingData() {
+        // Arrange
+        let productVariation = Fixtures.physicalProductVariationWithImagesWithoutShipping
+
+        // Action
+        let factory = ProductVariationFormActionsFactory(productVariation: productVariation)
+
+        // Assert
+        let expectedPrimarySectionActions: [ProductFormEditAction] = [.images, .name, .description]
+        XCTAssertEqual(factory.primarySectionActions(), expectedPrimarySectionActions)
+
+        let expectedSettingsSectionActions: [ProductFormEditAction] = [.priceSettings,
+                                                                       .inventorySettings]
+        XCTAssertEqual(factory.settingsSectionActions(), expectedSettingsSectionActions)
+
+        let expectedBottomSheetActions: [ProductFormBottomSheetAction] = [.editShippingSettings]
+        XCTAssertEqual(factory.bottomSheetActions(), expectedBottomSheetActions)
+    }
+
+    func testActionsForADownloadableProductVariation() {
         // Arrange
         let productVariation = Fixtures.downloadableProductVariation
 
@@ -62,7 +81,7 @@ final class ProductVariationFormActionsFactoryTests: XCTestCase {
         XCTAssertEqual(factory.bottomSheetActions(), expectedBottomSheetActions)
     }
 
-    func testViewModelForVirtualSimpleProduct() {
+    func testActionsForAVirtualProductVariation() {
         // Arrange
         let productVariation = Fixtures.virtualProductVariation
 
@@ -99,6 +118,10 @@ private extension ProductVariationFormActionsFactoryTests {
             .copy(image: image, sku: "uks", virtual: false, downloadable: false,
                   manageStock: true, stockQuantity: nil,
                   weight: "2", dimensions: ProductDimensions(length: "", width: "", height: ""))
+        // downloadable: false, virtual: false, with inventory and without shipping
+        static let physicalProductVariationWithImagesWithoutShipping = MockProductVariation().productVariation()
+            .copy(image: image, sku: "uks", virtual: false, downloadable: false,
+                  manageStock: true, stockQuantity: nil)
         // downloadable: false, virtual: true, with inventory/shipping
         static let virtualProductVariation = MockProductVariation().productVariation()
             .copy(image: nil, sku: "uks", virtual: true, downloadable: false,

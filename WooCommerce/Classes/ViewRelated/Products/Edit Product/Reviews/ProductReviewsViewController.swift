12e922a4e74f630f8aae125ae5f785913fb86304
Paolo Musolino <info@codeido.com>
2020-08-05 16:03:37 +0200

    feat: added init method + tableview in ProductReviewsViewController

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Reviews/ProductReviewsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Reviews/ProductReviewsViewController.swift
@@ -1,21 +1,25 @@
 import UIKit
+import Yosemite
 
+/// The UI that shows the approved Reviews related to a specific product.
 final class ProductReviewsViewController: UIViewController {
+
+    private let product: Product
+
+    @IBOutlet private weak var tableView: UITableView!
+    
+    init(product: Product) {
+        self.product = product
+        super.init(nibName: type(of: self).nibName, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

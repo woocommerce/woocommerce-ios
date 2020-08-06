2fa1c80360c513e6c26112862c6dfeba51537802
Shiki <jayson@basanes.net>
2020-08-06 17:38:35 -0600

    StoreStatsAndTopPerf*VC: Use loadInAppFeedbackCardVisibility action

--- WooCommerce/Classes/ViewRelated/Dashboard/Stats v4/StoreStatsAndTopPerformersPeriodViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Stats v4/StoreStatsAndTopPerformersPeriodViewModel.swift
@@ -1,5 +1,5 @@
-
-import Foundation
+import Yosemite
+import class AutomatticTracks.CrashLogging
 
 /// ViewModel for `StoreStatsAndTopPerformersPeriodViewController`.
 ///
@@ -17,10 +17,10 @@ final class StoreStatsAndTopPerformersPeriodViewModel {
     var isInAppFeedbackCardVisible: Observable<Bool> {
         isInAppFeedbackCardVisibleSubject
     }
-
     private let isInAppFeedbackCardVisibleSubject = BehaviorSubject(false)
 
     private let featureFlagService: FeatureFlagService
+    private let storesManager: StoresManager
 
     /// Create an instance of `self`.
     ///
@@ -29,16 +29,33 @@ final class StoreStatsAndTopPerformersPeriodViewModel {
     ///     But setting this to `false`, will ensure that it will never be presented.
     ///
     init(canDisplayInAppFeedbackCard: Bool,
-         featureFlagService: FeatureFlagService = ServiceLocator.featureFlagService) {
+         featureFlagService: FeatureFlagService = ServiceLocator.featureFlagService,
+         storesManager: StoresManager = ServiceLocator.stores) {
         self.canDisplayInAppFeedbackCard = canDisplayInAppFeedbackCard
         self.featureFlagService = featureFlagService
+        self.storesManager = storesManager
 
         updateIsInAppFeedbackCardVisibleValue()
     }
 
     /// Calculates and updates the value of `isInAppFeedbackCardVisibleSubject`.
     private func updateIsInAppFeedbackCardVisibleValue() {
-        let isVisible = canDisplayInAppFeedbackCard && featureFlagService.isFeatureFlagEnabled(.inAppFeedback)
-        isInAppFeedbackCardVisibleSubject.send(isVisible)
+        // Abort right away if we don't need to calculate the real value.
+        let isEnabled = canDisplayInAppFeedbackCard && featureFlagService.isFeatureFlagEnabled(.inAppFeedback)
+        if !isEnabled {
+            return isInAppFeedbackCardVisibleSubject.send(isEnabled)
+        }
+
+        let action = AppSettingsAction.loadInAppFeedbackCardVisibility { result in
+            switch result {
+            case .success(let shouldBeVisible):
+                self.isInAppFeedbackCardVisibleSubject.send(shouldBeVisible)
+            case .failure(let error):
+                CrashLogging.logError(error)
+                // We'll just send a `false` value. I think this is the safer bet.
+                self.isInAppFeedbackCardVisibleSubject.send(false)
+            }
+        }
+        storesManager.dispatch(action)
     }
 }
--- WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Stats V4/StoreStatsAndTopPerformersPeriodViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Stats V4/StoreStatsAndTopPerformersPeriodViewModelTests.swift
@@ -1,16 +1,30 @@
 import XCTest
 
 @testable import WooCommerce
+import Yosemite
 
 final class StoreStatsAndTopPerformersPeriodViewModelTest: XCTestCase {
+
+    private var storesManager: MockupStoresManager!
+
+    override func setUp() {
+        super.setUp()
+        storesManager = MockupStoresManager(sessionManager: SessionManager.testingInstance)
+    }
+
+    override func tearDown() {
+        storesManager = nil
+        super.tearDown()
+    }
 
     func test_isInAppFeedbackCardVisible_is_false_if_feature_flag_is_off() throws {
         // Given
         let featureFlagService = MockFeatureFlagService(isInAppFeedbackOn: false)
+
+        // When
         let viewModel = StoreStatsAndTopPerformersPeriodViewModel(canDisplayInAppFeedbackCard: true,
                                                                   featureFlagService: featureFlagService)
 
-        // When
         var isVisible: Bool?
         _ = viewModel.isInAppFeedbackCardVisible.subscribe { value in
             isVisible = value
@@ -23,10 +37,83 @@ final class StoreStatsAndTopPerformersPeriodViewModelTest: XCTestCase {
     func test_isInAppFeedbackCardVisible_is_false_if_canDisplayInAppFeedbackCard_is_false() throws {
         // Given
         let featureFlagService = MockFeatureFlagService(isInAppFeedbackOn: true)
+
+        // When
         let viewModel = StoreStatsAndTopPerformersPeriodViewModel(canDisplayInAppFeedbackCard: false,
                                                                   featureFlagService: featureFlagService)
 
+        var isVisible: Bool?
+        _ = viewModel.isInAppFeedbackCardVisible.subscribe { value in
+            isVisible = value
+        }
+
+        // Then
+        XCTAssertFalse(try XCTUnwrap(isVisible))
+    }
+
+    func test_isInAppFeedbackCardVisible_is_true_if_the_AppSettingsAction_returns_true() throws {
+        // Given
+        let featureFlagService = MockFeatureFlagService(isInAppFeedbackOn: true)
+
+        storesManager.whenReceivingAction(ofType: AppSettingsAction.self) { action in
+            if case let AppSettingsAction.loadInAppFeedbackCardVisibility(onCompletion) = action {
+                onCompletion(.success(true))
+            }
+        }
+
         // When
+        let viewModel = StoreStatsAndTopPerformersPeriodViewModel(canDisplayInAppFeedbackCard: true,
+                                                                  featureFlagService: featureFlagService,
+                                                                  storesManager: storesManager)
+
+        var isVisible: Bool?
+        _ = viewModel.isInAppFeedbackCardVisible.subscribe { value in
+            isVisible = value
+        }
+
+        // Then
+        XCTAssertTrue(try XCTUnwrap(isVisible))
+    }
+
+    func test_isInAppFeedbackCardVisible_is_false_if_the_AppSettingsAction_returns_false() throws {
+        // Given
+        let featureFlagService = MockFeatureFlagService(isInAppFeedbackOn: true)
+
+        storesManager.whenReceivingAction(ofType: AppSettingsAction.self) { action in
+            if case let AppSettingsAction.loadInAppFeedbackCardVisibility(onCompletion) = action {
+                onCompletion(.success(false))
+            }
+        }
+
+        // When
+        let viewModel = StoreStatsAndTopPerformersPeriodViewModel(canDisplayInAppFeedbackCard: true,
+                                                                  featureFlagService: featureFlagService,
+                                                                  storesManager: storesManager)
+
+        var isVisible: Bool?
+        _ = viewModel.isInAppFeedbackCardVisible.subscribe { value in
+            isVisible = value
+        }
+
+        // Then
+        XCTAssertFalse(try XCTUnwrap(isVisible))
+    }
+
+    func test_isInAppFeedbackCardVisible_is_false_if_the_AppSettingsAction_returns_an_error() throws {
+        // Given
+        let featureFlagService = MockFeatureFlagService(isInAppFeedbackOn: true)
+
+        storesManager.whenReceivingAction(ofType: AppSettingsAction.self) { action in
+            if case let AppSettingsAction.loadInAppFeedbackCardVisibility(onCompletion) = action {
+                onCompletion(.failure(NSError(domain: "", code: 0, userInfo: nil)))
+            }
+        }
+
+        // When
+        let viewModel = StoreStatsAndTopPerformersPeriodViewModel(canDisplayInAppFeedbackCard: true,
+                                                                  featureFlagService: featureFlagService,
+                                                                  storesManager: storesManager)
+
         var isVisible: Bool?
         _ = viewModel.isInAppFeedbackCardVisible.subscribe { value in
             isVisible = value

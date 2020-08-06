12cd57b264c547fea24a352869499e7d4dc14e17
Jaclyn Chen <watertranquil@gmail.com>
2020-08-06 10:44:38 +0800

    Update unit tests.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Edit Product/EditableProductVariationModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Edit Product/EditableProductVariationModelTests.swift
@@ -6,8 +6,8 @@ final class EditableProductVariationModelTests: XCTestCase {
     func test_a_variation_with_any_attribute_has_name_that_consists_of_all_attributes() {
         // Arrange
         let allAttributes: [ProductAttribute] = [
-            ProductAttribute(attributeID: 0, name: "Color", position: 0, visible: true, variation: true, options: ["Orange", "Green"]),
-            ProductAttribute(attributeID: 0, name: "Brand", position: 1, visible: true, variation: true, options: ["Unknown", "House"])
+            ProductAttribute(attributeID: 0, name: "Brand", position: 1, visible: true, variation: true, options: ["Unknown", "House"]),
+            ProductAttribute(attributeID: 0, name: "Color", position: 0, visible: true, variation: true, options: ["Orange", "Green"])
         ]
         // The variation only has one attribute specified - Color.
         let variationAttributes: [ProductVariationAttribute] = [
@@ -29,10 +29,10 @@ final class EditableProductVariationModelTests: XCTestCase {
     func test_a_variation_with_full_attributes_has_name_that_consists_of_all_attributes() {
         // Arrange
         let allAttributes: [ProductAttribute] = [
-            ProductAttribute(attributeID: 0, name: "Color", position: 0, visible: true, variation: true, options: ["Orange", "Green"]),
-            ProductAttribute(attributeID: 0, name: "Brand", position: 1, visible: true, variation: true, options: ["Unknown", "House"])
+            ProductAttribute(attributeID: 0, name: "Brand", position: 1, visible: true, variation: true, options: ["Unknown", "House"]),
+            ProductAttribute(attributeID: 0, name: "Color", position: 0, visible: true, variation: true, options: ["Orange", "Green"])
         ]
-        // The variation only has one attribute specified - Color.
+        // The variation has both attributes.
         let variationAttributes: [ProductVariationAttribute] = [
             ProductVariationAttribute(id: 0, name: "Brand", option: "House"),
             ProductVariationAttribute(id: 0, name: "Color", option: "Orange")

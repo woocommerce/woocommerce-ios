24a991e99475364b51d1c85e3c1e5bb1f844a12f
Shiki <jayson@basanes.net>
2020-08-06 17:05:00 -0600

    StoreStatsAndTopPerformers*VM: Remove feature flag test
    This is no longer applicable because all "true" results should be determined
    by the `GeneralAppSettings`.

--- WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Stats V4/StoreStatsAndTopPerformersPeriodViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Stats V4/StoreStatsAndTopPerformersPeriodViewModelTests.swift
@@ -20,22 +20,6 @@ final class StoreStatsAndTopPerformersPeriodViewModelTest: XCTestCase {
         XCTAssertFalse(try XCTUnwrap(isVisible))
     }
 
-    func test_isInAppFeedbackCardVisible_is_true_if_feature_flag_is_on() throws {
-        // Given
-        let featureFlagService = MockFeatureFlagService(isInAppFeedbackOn: true)
-        let viewModel = StoreStatsAndTopPerformersPeriodViewModel(canDisplayInAppFeedbackCard: true,
-                                                                  featureFlagService: featureFlagService)
-
-        // When
-        var isVisible: Bool?
-        _ = viewModel.isInAppFeedbackCardVisible.subscribe { value in
-            isVisible = value
-        }
-
-        // Then
-        XCTAssertTrue(try XCTUnwrap(isVisible))
-    }
-
     func test_isInAppFeedbackCardVisible_is_false_if_canDisplayInAppFeedbackCard_is_false() throws {
         // Given
         let featureFlagService = MockFeatureFlagService(isInAppFeedbackOn: true)

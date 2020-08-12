e0434a35d66bbe44eb7f92fcf2177c63db494d0f
Diego Rey Mendez <diego@reymendez.com>
2020-08-12 19:52:47 -0300

    Migrates some code to Swift, to avoid adding new ObjC code.

---
+++ WordPressAuthenticator/WordPressAuthenticator/Services/LoginFacade.swift
@@ -0,0 +1,45 @@
+//
+//  LoginFacade.swift
+//  WordPressAuthenticator
+//
+//  Created by Diego Rey Mendez on 12/08/2020.
+//  Copyright © 2020 Automattic. All rights reserved.
+//
+
+import Foundation
+
+extension LoginFacade {
+    func requestOneTimeCode(with loginFields: LoginFields) {
+        wordpressComOAuthClientFacade.requestOneTimeCode(withUsername: loginFields.username, password: loginFields.password, success: {
+            if AuthenticatorAnalyticsTracker.shared.shouldUseLegacyTracker() {
+                WordPressAuthenticator.track(.twoFactorSentSMS)
+            }
+        }) { _ in
+            DDLogError("Failed to request one time code")
+        }
+    }
+
+    func requestSocial2FACode(with loginFields: LoginFields) {
+        guard let nonce = loginFields.nonceInfo?.nonceSMS else {
+            return
+        }
+        
+        wordpressComOAuthClientFacade.requestSocial2FACode(
+            withUserID: loginFields.nonceUserID,
+            nonce: nonce,
+            success: { newNonce in
+                if let newNonce = newNonce {
+                    loginFields.nonceInfo?.nonceSMS = newNonce
+                }
+            
+                if AuthenticatorAnalyticsTracker.shared.shouldUseLegacyTracker() {
+                    WordPressAuthenticator.track(.twoFactorSentSMS)
+                }
+        }) { (error, newNonce) in
+            if let newNonce = newNonce {
+                loginFields.nonceInfo?.nonceSMS = newNonce
+            }
+            DDLogError("Failed to request one time code");
+        }
+    }
+}

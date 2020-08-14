2cbb52006cf9637ce6a6fbb6eae4fed3cb732c76
Jaclyn Chen <watertranquil@gmail.com>
2020-08-14 13:40:06 +0800

    Fix UI test: add a step to tap on the login with email button from the login sheet.

--- WooCommerce/WooCommerceUITests/Screens/Login/WelcomeScreen.swift
+++ WooCommerce/WooCommerceUITests/Screens/Login/WelcomeScreen.swift
@@ -5,17 +5,21 @@ final class WelcomeScreen: BaseScreen {
 
     private struct ElementStringIDs {
         static let loginButton = "Prologue Log In Button"
+        static let loginWithEmailButton = "Log in with Email Button"
     }
 
     private let logInButton: XCUIElement
+    private let logInWithEmailButton: XCUIElement
 
     init() {
         logInButton = XCUIApplication().buttons[ElementStringIDs.loginButton]
+        logInWithEmailButton = XCUIApplication().buttons[ElementStringIDs.loginWithEmailButton]
         super.init(element: logInButton)
     }
 
     func selectLogin() -> LoginEmailScreen {
         logInButton.tap()
+        logInWithEmailButton.tap()
         return LoginEmailScreen()
     }
 

a25cefe41cfebfbfca34489d02ed5848cf24f8fd
Stephenie Harris <scoutharris@gmail.com>
2020-08-17 16:58:14 -0600

    Update to use new UI.

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Google/GoogleSignupConfirmationViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Google/GoogleSignupConfirmationViewController.swift
@@ -4,7 +4,10 @@ class GoogleSignupConfirmationViewController: LoginViewController {
 
     // MARK: - Properties
     
-    @IBOutlet weak var emailField: LoginTextField!
+    @IBOutlet private weak var tableView: UITableView!
+    private var rows = [Row]()
+    private var errorMessage: String?
+    private var shouldChangeVoiceOverFocus: Bool = false
 
     override var sourceTag: WordPressSupportSourceTag {
         get {
@@ -16,42 +19,183 @@ class GoogleSignupConfirmationViewController: LoginViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        localizeControls()
+        
+        navigationItem.title = WordPressAuthenticator.shared.displayStrings.signUpTitle
+        styleNavigationBar(forUnified: true)
+
+        defaultTableViewMargin = tableViewLeadingConstraint?.constant ?? 0
+        setTableViewMargins(forWidth: view.frame.width)
+
+        localizePrimaryButton()
+        registerTableViewCells()
+        loadRows()
+        configureForAccessibility()
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    // MARK: - Overrides
 
-        emailField.text = loginFields.emailAddress
-        emailField.isEnabled = false
-        emailField.backgroundColor = WordPressAuthenticator.shared.style.viewControllerBackgroundColor
-        
-        configureSubmitButton(animating: false)
+    /// Style individual ViewController backgrounds, for now.
+    ///
+    override func styleBackground() {
+        guard let unifiedBackgroundColor = WordPressAuthenticator.shared.unifiedStyle?.viewControllerBackgroundColor else {
+            super.styleBackground()
+            return
+        }
+
+        view.backgroundColor = unifiedBackgroundColor
+    }
+
+    /// Style individual ViewController status bars.
+    ///
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return WordPressAuthenticator.shared.unifiedStyle?.statusBarStyle ?? WordPressAuthenticator.shared.style.statusBarStyle
+    }
+
+    /// Override the title on 'submit' button
+    ///
+    override func localizePrimaryButton() {
+        submitButton?.setTitle(WordPressAuthenticator.shared.displayStrings.createAccountButtonTitle, for: .normal)
+    }
+
+    override func displayError(message: String, moveVoiceOverFocus: Bool = false) {
+        if errorMessage != message {
+            errorMessage = message
+            shouldChangeVoiceOverFocus = moveVoiceOverFocus
+            loadRows()
+            tableView.reloadData()
+        }
     }
     
+}
+
+// MARK: - UITableViewDataSource
+
+extension GoogleSignupConfirmationViewController: UITableViewDataSource {
+
+    /// Returns the number of rows in a section.
+    ///
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rows.count
+    }
+
+    /// Configure cells delegate method.
+    ///
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = rows[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: row.reuseIdentifier, for: indexPath)
+        configure(cell, for: row, at: indexPath)
+
+        return cell
+    }
 }
 
 // MARK: - Private Extension
 
 private extension GoogleSignupConfirmationViewController {
     
-    func localizeControls() {
-        instructionLabel?.text = NSLocalizedString("We'll use this email address to create your new WordPress.com account.", comment: "Text confirming email address to be used for new account.")
-        emailField.accessibilityIdentifier = "Google Signup Email Address"
-        emailField.contentInsets = WPStyleGuide.edgeInsetForLoginTextFields()
-
-        let submitButtonTitle = NSLocalizedString("Next", comment: "Title of a button. The text should be capitalized.").localizedCapitalized
-        submitButton?.setTitle(submitButtonTitle, for: .normal)
-        submitButton?.setTitle(submitButtonTitle, for: .highlighted)
-        submitButton?.accessibilityIdentifier = "Google Signup Email Next Button"
-    }
-    
     // MARK: - Button Handling
 
     @IBAction func handleSubmit() {
+        tracker.track(click: .submit)
+        tracker.track(click: .createAccount)
+        
         configureSubmitButton(animating: true)
         GoogleAuthenticator.sharedInstance.delegate = self
+        
+        // here
         GoogleAuthenticator.sharedInstance.createGoogleAccount(loginFields: loginFields)
+    }
+
+    // MARK: - Table Management
+    
+    /// Registers all of the available TableViewCells.
+    ///
+    func registerTableViewCells() {
+        let cells = [
+            GravatarEmailTableViewCell.reuseIdentifier: GravatarEmailTableViewCell.loadNib(),
+            TextLabelTableViewCell.reuseIdentifier: TextLabelTableViewCell.loadNib()
+        ]
+
+        for (reuseIdentifier, nib) in cells {
+            tableView.register(nib, forCellReuseIdentifier: reuseIdentifier)
+        }
+    }
+
+    /// Describes how the tableView rows should be rendered.
+    ///
+    func loadRows() {
+        rows = [.gravatarEmail, .instructions]
+
+        if let errorText = errorMessage, !errorText.isEmpty {
+            rows.append(.errorMessage)
+        }
+    }
+
+    /// Configure cells.
+    ///
+    func configure(_ cell: UITableViewCell, for row: Row, at indexPath: IndexPath) {
+        switch cell {
+        case let cell as GravatarEmailTableViewCell:
+            configureGravatarEmail(cell)
+        case let cell as TextLabelTableViewCell where row == .instructions:
+            configureInstructionLabel(cell)
+        case let cell as TextLabelTableViewCell where row == .errorMessage:
+            configureErrorLabel(cell)
+        default:
+            DDLogError("Error: Unidentified tableViewCell type found.")
+        }
+    }
+
+    /// Configure the gravatar + email cell.
+    ///
+    func configureGravatarEmail(_ cell: GravatarEmailTableViewCell) {
+        cell.configure(withEmail: loginFields.username)
+    }
+
+    /// Configure the instruction cell.
+    ///
+    func configureInstructionLabel(_ cell: TextLabelTableViewCell) {
+        cell.configureLabel(text: WordPressAuthenticator.shared.displayStrings.googleSignupInstructions, style: .body)
+    }
+
+    /// Configure the error message cell.
+    ///
+    func configureErrorLabel(_ cell: TextLabelTableViewCell) {
+        cell.configureLabel(text: errorMessage, style: .error)
+        if shouldChangeVoiceOverFocus {
+            UIAccessibility.post(notification: .layoutChanged, argument: cell)
+        }
+    }
+
+    /// Sets up accessibility elements in the order which they should be read aloud
+    /// and chooses which element to focus on at the beginning.
+    ///
+    func configureForAccessibility() {
+        view.accessibilityElements = [
+            tableView,
+            submitButton as Any
+        ]
+
+        UIAccessibility.post(notification: .screenChanged, argument: tableView)
+    }
+    
+    // MARK: - Private Constants
+
+    /// Rows listed in the order they were created.
+    ///
+    enum Row {
+        case gravatarEmail
+        case instructions
+        case errorMessage
+
+        var reuseIdentifier: String {
+            switch self {
+            case .gravatarEmail:
+                return GravatarEmailTableViewCell.reuseIdentifier
+            case .instructions, .errorMessage:
+                return TextLabelTableViewCell.reuseIdentifier
+            }
+        }
     }
 
 }
@@ -75,11 +219,7 @@ extension GoogleSignupConfirmationViewController: GoogleAuthenticatorDelegate {
     func googleSignupFailed(error: Error, loginFields: LoginFields) {
         configureSubmitButton(animating: false)
         self.loginFields = loginFields
-
-        // Display generic inline error.
-        displayError(message: NSLocalizedString("Google sign up failed.", comment: "Message shown on screen after the Google sign up process failed."))
-        // Display the API error in a Fancy Alert.
-        displayError(error as NSError, sourceTag: .wpComSignup)
+        displayError(message: error.localizedDescription, moveVoiceOverFocus: true)
     }
     
     // MARK: - Login

f4d770f76895206450dc58e3c4e37bbeda5c5e51
Paolo Musolino <info@codeido.com>
2020-08-18 17:10:09 +0200

    fix: updated ProductTypeBottomSheetListSelectorCommand tests

--- WooCommerce/WooCommerceTests/ViewRelated/Products/BottomSheetListSelector/ProductTypeBottomSheetListSelectorCommandTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/BottomSheetListSelector/ProductTypeBottomSheetListSelectorCommandTests.swift
@@ -8,21 +8,19 @@ final class ProductTypeBottomSheetListSelectorCommandTests: XCTestCase {
     func test_callback_is_called_on_selection() {
         // Arrange
         var selectedActions = [ProductType]()
-        let command = ProductTypeBottomSheetListSelectorCommand { (selected) in
+        let command = ProductTypeBottomSheetListSelectorCommand(selected: .simple) { (selected) in
             selectedActions.append(selected)
         }
 
         // Action
         command.handleSelectedChange(selected: .grouped)
         command.handleSelectedChange(selected: .variable)
-        command.handleSelectedChange(selected: .simple)
         command.handleSelectedChange(selected: .affiliate)
 
         // Assert
         let expectedActions: [ProductType] = [
             .grouped,
             .variable,
-            .simple,
             .affiliate
         ]
         XCTAssertEqual(selectedActions, expectedActions)

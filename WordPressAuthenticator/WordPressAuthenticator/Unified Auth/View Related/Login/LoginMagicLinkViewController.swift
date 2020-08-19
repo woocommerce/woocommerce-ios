5172147f62dac8e54a0a2a8577867ac78867fee4
Thuy.Copeland <thuycopeland@gmail.com>
2020-08-19 15:53:22 -0500

    Create the LoginMagicLink VC and Storyboard

---
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Login/LoginMagicLinkViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+
+/// Unified LoginMagicLinkViewController: login to .com with a magic link
+///
+final class LoginMagicLinkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}

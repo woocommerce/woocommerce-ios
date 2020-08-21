a90399aafb5d3886a47911cc4871b5f4599f80db
Shiki <jayson@basanes.net>
2020-08-21 11:32:59 -0600

    SurveyCoordinatingController: Track surveyScreen canceled event

--- WooCommerce/Classes/ViewRelated/Survey/SurveyCoordinatingController.swift
+++ WooCommerce/Classes/ViewRelated/Survey/SurveyCoordinatingController.swift
@@ -12,6 +12,9 @@ final class SurveyCoordinatingController: WooNavigationController {
 
     private let analytics: Analytics
 
+    /// Is true when `self` is being dismissed because the user has finished the survey.
+    private var receivedSurveyFinishedEvent: Bool = false
+
     /// What kind of survey to present.
     private let survey: SurveyViewController.Source
 
@@ -30,6 +33,14 @@ final class SurveyCoordinatingController: WooNavigationController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        if isBeingDismissed && !receivedSurveyFinishedEvent {
+            analytics.track(event: .surveyScreen(context: survey.feedbackContextForEvents, action: .canceled))
+        }
+    }
 }
 
 // MARK: Navigation
@@ -45,6 +56,7 @@ private extension SurveyCoordinatingController {
                 return
             }
 
+            self.receivedSurveyFinishedEvent = true
             self.analytics.track(event: .surveyScreen(context: self.survey.feedbackContextForEvents,
                                                       action: .completed))
 

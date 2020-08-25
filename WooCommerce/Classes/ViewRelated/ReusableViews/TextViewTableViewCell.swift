71ab93623c6090ba12b252da5c95d8d2a0b98231
Paolo Musolino <info@codeido.com>
2020-08-25 23:31:45 +0200

    refactor: TextViewTableViewCell now is configured using a ViewModel

--- WooCommerce/Classes/ViewRelated/ReusableViews/TextViewTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/TextViewTableViewCell.swift
@@ -2,13 +2,30 @@ import UIKit
 import Gridicons
 
 class TextViewTableViewCell: UITableViewCell {
+    struct ViewModel {
+        var icon: UIImage? = nil
+        var iconAccessibilityLabel: String? = nil
+        var iconTint: UIColor? = nil
+        var text: String? = nil
+        var placeholder: String? = nil
+        var textViewMinimumHeight: CGFloat? = nil
+        var isScrollEnabled: Bool = true
+        var onTextChange: ((_ text: String) -> Void)? = nil
+        var onTextDidBeginEditing: (() -> Void)? = nil
+        var keyboardType: UIKeyboardType = .default
+        var style: Style = .body
+    }
 
     @IBOutlet private weak var noteIconView: UIView!
-    @IBOutlet var noteIconButton: UIButton!
+    @IBOutlet private var noteIconButton: UIButton!
 
-    @IBOutlet var noteTextView: EnhancedTextView!
+    @IBOutlet private var noteTextView: EnhancedTextView!
 
-    var iconImage: UIImage? {
+    @IBOutlet private weak var textViewHeightConstraint: NSLayoutConstraint!
+
+    private var viewModel: ViewModel?
+
+    private var iconImage: UIImage? {
         get {
             return noteIconButton.image(for: .normal)
         }
@@ -20,7 +37,7 @@ class TextViewTableViewCell: UITableViewCell {
         }
     }
 
-    var iconTint: UIColor? {
+    private var iconTint: UIColor? {
         get {
             return noteIconButton.backgroundColor
         }
@@ -37,8 +54,54 @@ class TextViewTableViewCell: UITableViewCell {
 
         noteIconButton.accessibilityTraits = .image
     }
+
+    func configure(viewModel: ViewModel) {
+        self.viewModel = viewModel
+
+        iconImage = viewModel.icon
+        iconImage?.accessibilityLabel = viewModel.iconAccessibilityLabel
+        iconTint = viewModel.iconTint
+        noteTextView.text = viewModel.text
+        noteTextView.placeholder = viewModel.placeholder
+        if let minimumHeight = viewModel.textViewMinimumHeight {
+            textViewHeightConstraint.constant = minimumHeight
+        }
+        noteTextView.isScrollEnabled = viewModel.isScrollEnabled
+        noteTextView.onTextChange = viewModel.onTextChange
+        noteTextView.onTextDidBeginEditing = viewModel.onTextDidBeginEditing
+        noteTextView.keyboardType = viewModel.keyboardType
+        self.applyStyle(style: viewModel.style)
+    }
+
+    @discardableResult
+    override func becomeFirstResponder() -> Bool {
+        noteTextView.becomeFirstResponder()
+    }
 }
 
+// Styles
+extension TextViewTableViewCell {
+
+    enum Style {
+        case body
+        case headline
+    }
+
+    func applyStyle(style: Style) {
+        switch style {
+        case .body:
+            noteTextView.adjustsFontForContentSizeCategory = true
+            noteTextView.font = .body
+            noteTextView.textColor = .text
+        case .headline:
+            noteTextView.adjustsFontForContentSizeCategory = true
+            noteTextView.font = .headline
+            noteTextView.textColor = .text
+        }
+    }
+}
+
+// Private methods
 private extension TextViewTableViewCell {
     func configureBackground() {
         applyDefaultBackgroundStyle()

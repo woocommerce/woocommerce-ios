967cafeb83fd2f5ec42280dc06565c9a94cbad57
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-09-02 08:14:53 -0500

    Remove StatsV3 code from DashboardUIFactory

--- WooCommerce/Classes/ViewRelated/Dashboard/Factories/DashboardUIFactory.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Factories/DashboardUIFactory.swift
@@ -27,17 +27,12 @@ final class DashboardUIFactory {
     private let siteID: Int64
     private let statsVersionCoordinator: StatsVersionCoordinator
 
-    /// Function that generates the current system date
-    private let currentDateProvider: () -> Date
-
-    private var lastStatsV3DashboardUI: (DashboardUI & TopBannerPresenter)?
     private var lastStatsV4DashboardUI: DashboardUI?
     private lazy var deprecatedStatsViewController = DeprecatedDashboardStatsViewController()
 
     init(siteID: Int64, currentDateProvider: @escaping () -> Date = Date.init) {
         self.siteID = siteID
         self.statsVersionCoordinator = StatsVersionCoordinator(siteID: siteID)
-        self.currentDateProvider = currentDateProvider
     }
 
     func reloadDashboardUI(onUIUpdate: @escaping (_ dashboardUI: DashboardUI) -> Void) {
@@ -47,15 +42,6 @@ final class DashboardUIFactory {
                                        onUIUpdate: onUIUpdate)
         }
         statsVersionCoordinator.loadLastShownVersionAndCheckV4Eligibility()
-    }
-
-    private func statsV3DashboardUI() -> DashboardUI & TopBannerPresenter {
-        if let lastStatsV3DashboardUI = lastStatsV3DashboardUI {
-            return lastStatsV3DashboardUI
-        }
-        let dashboardUI = DashboardStatsV3ViewController(nibName: nil, bundle: nil)
-        lastStatsV3DashboardUI = dashboardUI
-        return dashboardUI
     }
 
     private func statsV4DashboardUI() -> DashboardUI {
@@ -70,11 +56,8 @@ final class DashboardUIFactory {
     private func dashboardUI(statsVersion: StatsVersion) -> DashboardUI {
         switch statsVersion {
         case .v3:
-            // Return an stats-empty view controller if current system date is greater or equal than our deprecation trigger date(09/01/2020)
-            if let deprecatedStatsDate = Date.september1st2020, currentDateProvider() >= deprecatedStatsDate {
-                return deprecatedStatsViewController
-            }
-            return statsV3DashboardUI()
+            // Return an stats-empty view controller for v3 stats
+            return deprecatedStatsViewController
         case .v4:
             return statsV4DashboardUI()
         }
@@ -94,31 +77,5 @@ private extension DashboardUIFactory {
 
         let updatedDashboardUI = dashboardUI(statsVersion: currentVersion)
         onUIUpdate(updatedDashboardUI)
-
-        if let topBannerPresenter = updatedDashboardUI as? TopBannerPresenter {
-            switch currentVersion {
-            case .v3:
-                let topBannerView = DashboardTopBannerFactory.deprecatedStatsBannerView {
-                    updatedDashboardUI.remindStatsUpgradeLater()
-                }
-                topBannerPresenter.hideTopBanner(animated: false)
-                topBannerPresenter.showTopBanner(topBannerView)
-            case .v4:
-                topBannerPresenter.hideTopBanner(animated: true)
-            }
-        }
-    }
-}
-
-private extension Date {
-
-    /// Returns a date object that corresponds to September 1st of 2020
-    static var september1st2020: Date? {
-        var dateComponents = DateComponents()
-        dateComponents.year = 2020
-        dateComponents.month = 9
-        dateComponents.day = 1
-
-        return Calendar(identifier: .gregorian).date(from: dateComponents)
     }
 }

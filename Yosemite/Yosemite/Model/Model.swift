99cdcb6cb6e1331a67e3eed061bdb1d84afaa35c
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-09-02 10:17:18 -0500

    Removes OrderStatsItem

--- Yosemite/Yosemite/Model/Model.swift
+++ Yosemite/Yosemite/Model/Model.swift
@@ -33,7 +33,6 @@ public typealias OrderStatusEnum = Networking.OrderStatusEnum
 public typealias OrderCouponLine = Networking.OrderCouponLine
 public typealias OrderNote = Networking.OrderNote
 public typealias OrderRefundCondensed = Networking.OrderRefundCondensed
-public typealias OrderStatsItem = Networking.OrderStatsItem
 public typealias OrderStatsV4 = Networking.OrderStatsV4
 public typealias OrderStatsV4Interval = Networking.OrderStatsV4Interval
 public typealias OrderStatsV4Totals = Networking.OrderStatsV4Totals

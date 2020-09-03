fcecf60366b362f59fcacb8d9548d059b2bd85be
Shiki <jayson@basanes.net>
2020-09-03 13:15:33 -0600

    SnapshotsProvider: Remove "update" bugfix for now
    I'll submit this as a separate PR later.

--- Yosemite/Yosemite/SnapshotsProvider/FetchResultSnapshotsProvider.swift
+++ Yosemite/Yosemite/SnapshotsProvider/FetchResultSnapshotsProvider.swift
@@ -44,7 +44,6 @@ public final class FetchResultSnapshotsProvider<ResultType: FetchResultSnapshots
     }()
 
     private let snapshotSubject = CurrentValueSubject<Snapshot, Never>(Snapshot())
-    private var observationToken: Any?
 
     public var snapshot: AnyPublisher<Snapshot, Never> {
         snapshotSubject.eraseToAnyPublisher()
@@ -57,39 +56,6 @@ public final class FetchResultSnapshotsProvider<ResultType: FetchResultSnapshots
 
     public func start() throws {
         try wrappedController.performFetch()
-
-        #warning("fix cast")
-        let context = storage as! NSManagedObjectContext
-        let nc = NotificationCenter.default
-
-        if let token = observationToken {
-            nc.removeObserver(token)
-        }
-
-        observationToken = nc.addObserver(forName: .NSManagedObjectContextObjectsDidChange, object: context, queue: nil) { [weak self] notification in
-            guard let self = self else {
-                return
-            }
-
-            let structuredNotification = ObjectsDidChangeNotification(notification)
-
-            #warning("also add filter using the correct entity")
-            let currentSnapshot = self.snapshotSubject.value
-            let managedObjectIDs = structuredNotification.updatedObjects.map(\.objectID).filter { managedObjectID in
-                currentSnapshot.indexOfItem(managedObjectID) != nil
-            }
-            if !managedObjectIDs.isEmpty {
-                var newSnapshot = currentSnapshot
-                newSnapshot.reloadItems(managedObjectIDs)
-                self.snapshotSubject.send(newSnapshot)
-            }
-        }
-    }
-
-    deinit {
-        if let token = observationToken {
-            NotificationCenter.default.removeObserver(token)
-        }
     }
 
     public func object(withID objectID: ObjectID) -> Order? {
@@ -105,19 +71,5 @@ public final class FetchResultSnapshotsProvider<ResultType: FetchResultSnapshots
                     didChangeContentWith snapshot: NSDiffableDataSourceSnapshotReference) {
         let snapshot = snapshot as Snapshot
         snapshotSubject.send(snapshot)
-    }
-}
-
-private struct ObjectsDidChangeNotification {
-
-    private let notification: Notification
-
-    init(_ notification: Notification) {
-        assert(notification.name == .NSManagedObjectContextObjectsDidChange)
-        self.notification = notification
-    }
-
-    var updatedObjects: Set<NSManagedObject> {
-        (notification.userInfo?[NSUpdatedObjectsKey] as? Set<NSManagedObject>) ?? Set()
     }
 }

4f57bcc3a19770475f0922f33b4e8362619174f5
Shiki <jayson@basanes.net>
2020-09-03 10:59:20 -0600

    Move remaining XCTestCase+Assertions to TestKit

--- TestKit/Sources/TestKit/Assertions.swift
+++ TestKit/Sources/TestKit/Assertions.swift
@@ -14,3 +14,30 @@ public func assertThat(_ lhs: AnyObject?, isIdenticalTo rhs: AnyObject?, file: S
                   file: file,
                   line: line)
 }
+
+/// Asserts that `subject` contains the given string.
+///
+public func assertThat(_ subject: String, contains value: String, file: StaticString = #file, line: UInt = #line) {
+    XCTAssertTrue(subject.contains(value),
+                  "Expected “\(subject)” to contain “\(value)”.",
+                  file: file,
+                  line: line)
+}
+
+/// Asserts that `subject`'s type is **exactly** `expectedType`.
+///
+/// If `subject`'s type is just a subclass of `expectedType`, then this will fail.
+///
+public func assertThat<T>(_ subject: Any?, isAnInstanceOf expectedType: T.Type, file: StaticString = #file, line: UInt = #line) {
+    guard let subject = subject else {
+        XCTFail("Expected nil to be an instance of \(expectedType)",
+                file: file,
+                line: line)
+        return
+    }
+
+    XCTAssertTrue(type(of: subject) == expectedType,
+                  "Expected \(subject) to be an instance of \(expectedType)",
+                  file: file,
+                  line: line)
+}

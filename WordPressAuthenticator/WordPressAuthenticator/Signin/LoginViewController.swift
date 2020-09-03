c888ada32f3189be6c59ae3e3f67e22ba4cf36d1
Stephenie Harris <scoutharris@gmail.com>
2020-09-03 09:47:55 -0600

    Use correct config flag.

--- WordPressAuthenticator/WordPressAuthenticator/Signin/LoginViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Signin/LoginViewController.swift
@@ -211,7 +211,7 @@ open class LoginViewController: NUXViewController, LoginFacadeDelegate {
         let unifiedGoogle = WordPressAuthenticator.shared.configuration.enableUnifiedGoogle && loginFields.meta.socialService == .google
         let unifiedApple = WordPressAuthenticator.shared.configuration.enableUnifiedApple && loginFields.meta.socialService == .apple
         let unifiedSiteAddress = WordPressAuthenticator.shared.configuration.enableUnifiedSiteAddress && !loginFields.siteAddress.isEmpty
-        let unifiedWordPress = WordPressAuthenticator.shared.configuration.enableUnifiedSiteAddress && loginFields.meta.userIsDotCom
+        let unifiedWordPress = WordPressAuthenticator.shared.configuration.enableUnifiedWordPress && loginFields.meta.userIsDotCom
         
         guard (unifiedGoogle || unifiedApple || unifiedSiteAddress || unifiedWordPress) else {
             presentLogin2FA()

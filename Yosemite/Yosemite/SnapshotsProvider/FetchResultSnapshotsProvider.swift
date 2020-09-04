b24693637b724f5d22a2135f51ffc7844660c23d
Shiki <jayson@basanes.net>
2020-09-04 09:35:54 -0600

    Add comments for SnapshotsProvider

--- Yosemite/Yosemite/SnapshotsProvider/FetchResultSnapshotsProvider.swift
+++ Yosemite/Yosemite/SnapshotsProvider/FetchResultSnapshotsProvider.swift
@@ -8,15 +8,55 @@ public typealias FetchResultSnapshotObjectID = NSManagedObjectID
 @available(iOS 13.0, *)
 public typealias FetchResultSnapshot = NSDiffableDataSourceSnapshot<String, FetchResultSnapshotObjectID>
 
+/// Emits `FetchResultSnapshot` objects for Core Data fetch results. The snapshot can be used
+/// by `DiffableDataSource` consumers like `UITableViewDiffableDataSource`.
+///
+/// This will continuously emit snapshots whenever the data changes.
+///
+/// ## Example Usage
+///
+/// ```
+/// // Create the snapshots provider
+/// let query = FetchResultSnapshotsProvider<StorageOrder>.Query(
+///     sortDescriptor: .init(keyPath: \StorageOrder.number, ascending: false)
+/// )
+/// let provider = FetchResultSnapshotsProvider(storageManager: storageManager, query: query)
+///
+/// // Set up the TableView and DataSource
+/// let dataSource =
+///    UITableViewDiffableDataSource<String, FetchResultSnapshotObjectID>(tableView: tableView,
+///                                                                       cellProvider: self.makeCellProvider())
+/// tableView.dataSource = dataSource
+///
+/// // Apply the emitted snapshots to the TableView's DataSource
+/// provider.snapshot.sink { snapshot in
+///     dataSource.apply(snapshot)
+/// }.store(in: &self.cancellables)
+///
+/// provider.start()
+/// ```
+///
+/// That's it! The `UITableView` should be automatically updated whenever the data changes.
+///
 @available(iOS 13.0, *)
 public final class FetchResultSnapshotsProvider<MutableType: FetchResultSnapshotsProviderMutableType>: NSObject, NSFetchedResultsControllerDelegate {
-
+    
+    /// Defines the conditions for fetching the results.
     public struct Query {
+        /// Defines how to sort the results.
+        ///
+        /// This is required because `NSFetchedResultsController` requires it.
+        ///
         /// This needs to be extended to allow an array. However, we have to add protection that
-        /// there will always be at least one sort descriptor. It's required by ResultsController.
-        /// It'd be great if we can check this requirement during compile-time.
+        /// there will always be at least one `NSSortDescriptor`. It'd be great if we can check
+        /// this requirement during compile-time.
         public let sortDescriptor: NSSortDescriptor
+        /// An optional predicate to filter the results.
         public let predicate: NSPredicate?
+        /// A keypath that returns the value to use for grouping results into sections.
+        ///
+        /// The values will be accessible in `FetchResultSnapshot.sectionIdentifiers`. In the UI,
+        /// those values can be converted to a user-friendly value.
         public let sectionNameKeyPath: String?
 
         init(sortDescriptor: NSSortDescriptor, predicate: NSPredicate? = nil, sectionNameKeyPath: String? = nil) {
@@ -26,8 +66,22 @@ public final class FetchResultSnapshotsProvider<MutableType: FetchResultSnapshot
         }
     }
 
+    /// The `StorageType` to perform the fetch on.
+    ///
+    /// This is currently forced to be the `viewStorage` (main thread). In the future, we can
+    /// probably move this to a background `StorageType` since `UITableViewDiffableDataSource`
+    /// allows consuming snapshots in the background.
     private let storage: StorageType
+    /// The conditions to use when fetching the results.
+    ///
+    /// In the future, we can allow this to be mutable if necessary.
     private let query: Query
+
+    /// The publisher that emits snapshots when the fetch results arrive and when the data changes.
+    public var snapshot: AnyPublisher<FetchResultSnapshot, Never> {
+        snapshotSubject.eraseToAnyPublisher()
+    }
+    private let snapshotSubject = CurrentValueSubject<FetchResultSnapshot, Never>(FetchResultSnapshot())
 
     private lazy var wrappedController: NSFetchedResultsController<MutableType> = {
         let fetchRequest = NSFetchRequest<MutableType>(entityName: MutableType.entityName)
@@ -43,21 +97,34 @@ public final class FetchResultSnapshotsProvider<MutableType: FetchResultSnapshot
         return resultsController
     }()
 
-    private let snapshotSubject = CurrentValueSubject<FetchResultSnapshot, Never>(FetchResultSnapshot())
-
-    public var snapshot: AnyPublisher<FetchResultSnapshot, Never> {
-        snapshotSubject.eraseToAnyPublisher()
-    }
-
     public init(storageManager: StorageManagerType, query: Query) {
         self.storage = storageManager.viewStorage
         self.query = query
     }
 
+    /// Start fetching and emitting snapshots.
     public func start() throws {
         try wrappedController.performFetch()
     }
 
+    /// Retrieve the immutable type pointed to by `objectID`.
+    ///
+    /// This is typically used in the `UITableViewDiffableDataSource`'s `CellProvider` in order to
+    /// display the row's information. Example:
+    ///
+    /// ```
+    /// private func makeCellProvider() -> UITableViewDiffableDataSource<String, FetchResultSnapshotObjectID>.CellProvider {
+    ///    return { [weak self] tableView, indexPath, objectID in
+    ///        guard let self = self, let cell = tableView.dequeueReusableCell(withIdentifier: ...) else {
+    ///            fatalError()
+    ///        }
+    ///
+    ///        let order = provider.object(withID: objectID)
+    ///        cell.configure(using: order)
+    ///        return cell
+    ///    }
+    ///}
+    /// ```
     public func object(withID objectID: FetchResultSnapshotObjectID) -> MutableType.ReadOnlyType? {
         assert(!objectID.isTemporaryID, "Expected objectID \(objectID) to be a permanent NSManagedObjectID.")
 
@@ -68,6 +135,10 @@ public final class FetchResultSnapshotsProvider<MutableType: FetchResultSnapshot
         }
     }
 
+    /// Converts the `NSFetchedResultsController` results to a snapshot that is then emitted
+    /// to observers.
+    ///
+    /// This will be hidden later. It should not be publicly accessible.
     public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
                     didChangeContentWith snapshot: NSDiffableDataSourceSnapshotReference) {
         let snapshot = snapshot as FetchResultSnapshot

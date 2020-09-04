c1041360328cfa55746eaeffd6d17d17ff9a7619
Diego Rey Mendez <diego@reymendez.com>
2020-09-04 15:34:12 -0300

    Removes prologue flow tracking until we have the new prologue screen.

--- WordPressAuthenticator/WordPressAuthenticator/Signin/LoginPrologueViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Signin/LoginPrologueViewController.swift
@@ -19,12 +19,6 @@ class LoginPrologueViewController: LoginViewController {
     private lazy var storedCredentialsAuthenticator = StoredCredentialsAuthenticator()
     
     @IBOutlet private weak var topContainerView: UIView!
-    
-    /// We can't rely on `isMovingToParent` to know if we need to track the `.prologue` step
-    /// because for the root view in an App, it's always `false`.  We're relying this variiable
-    /// instead, since the `.prologue` step only needs to be tracked once.
-    ///
-    private var prologueFlowTracked = false
 
     // MARK: - Lifecycle Methods
 
@@ -51,15 +45,6 @@ class LoginPrologueViewController: LoginViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        tracker.set(flow: .prologue)
-        
-        if !prologueFlowTracked {
-            tracker.track(step: .prologue)
-            prologueFlowTracked = true
-        } else {
-            tracker.set(step: .prologue)
-        }
         
         WordPressAuthenticator.track(.loginPrologueViewed)
         

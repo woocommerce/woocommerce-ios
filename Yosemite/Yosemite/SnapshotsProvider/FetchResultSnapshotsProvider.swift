d5dc567f3ddee8e30640808ddc80059042ad7311
Shiki <jayson@basanes.net>
2020-09-17 08:05:21 -0600

    SnapshotsProvider: Stop observing StorageManagerDidReset on deinit

--- Yosemite/Yosemite/SnapshotsProvider/FetchResultSnapshotsProvider.swift
+++ Yosemite/Yosemite/SnapshotsProvider/FetchResultSnapshotsProvider.swift
@@ -144,6 +144,7 @@ public final class FetchResultSnapshotsProvider<MutableType: FetchResultSnapshot
     }
 
     deinit {
+        stopObservingStorageManagerDidResetNotifications()
         stopObservingObjectsDidChangeNotifications()
     }
 

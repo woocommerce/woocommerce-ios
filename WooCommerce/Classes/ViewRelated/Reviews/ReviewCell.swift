a8617d30923d3a9b6dcfd7ca58aed6b202b8710d
Paolo Musolino <info@codeido.com>
2020-09-21 11:15:09 +0200

    feat: added viewModel in SwiftUI ReviewCell

--- WooCommerce/Classes/ViewRelated/Reviews/ReviewCell.swift
+++ WooCommerce/Classes/ViewRelated/Reviews/ReviewCell.swift
@@ -1,25 +1,30 @@
 
 #if canImport(SwiftUI) && DEBUG
 import SwiftUI
+import Networking
 import Gridicons
 
 @available(iOS 13.0, *)
 struct ReviewCell: View {
 
+    var viewModel: ReviewViewModel
 
     var body: some View {
         HStack {
             Rectangle()
                 .padding(.vertical, 0.0)
                 .frame(width: 2)
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .foregroundColor(viewModel.read ? Color(.clear) : Color(.accent))
 
             Spacer().frame(width: 12)
 
-            Text("\u{f300}")
+            VStack {
+                Text("\u{f300}")
                 .frame(width: 25, height: 25)
                 .padding(.top, 11.0)
 
+                Spacer(minLength: 0)
+            }
             Spacer().frame(width: 16)
 
             VStack(alignment: .leading, spacing: 3.0) {
@@ -42,7 +47,9 @@ struct ReviewCell: View {
 @available(iOS 13.0, *)
 struct ReviewCell_Previews: PreviewProvider {
     static var previews: some View {
-        ReviewCell()
+
+        ReviewViewModel(review: ProductReview, product: <#T##Product?#>, notification: <#T##Note?#>)
+        ReviewCell(viewModel: <#T##ReviewViewModel#>)
     }
 }
 

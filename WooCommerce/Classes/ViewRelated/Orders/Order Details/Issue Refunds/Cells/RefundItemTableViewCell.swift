a8894ab6e7e1468c2ddbedc65382b88d53bcbe6c
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-09-23 15:24:27 -0500

    Adds empty RefundItemTableViewCell

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundItemTableViewCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+/// Displays an item to be refunded
+///
+final class RefundItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}

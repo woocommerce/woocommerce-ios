8e8c6d2b436db553253f696092488e2eacbb53ba
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-09-23 16:11:51 -0500

    Updates RefundItemTableViewCell with a basic UI structure

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundItemTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundItemTableViewCell.swift
@@ -4,7 +4,30 @@ import UIKit
 ///
 final class RefundItemTableViewCell: UITableViewCell {
 
+    /// Item image view: Product image
+    ///
+    @IBOutlet private var itemImageView: UIImageView!
+
+    /// Item title: Product name
+    ///
+    @IBOutlet private var itemTitle: UILabel!
+
+    /// Item caption: Product quanity and price
+    ///
+    @IBOutlet private var itemCaption: UILabel!
+
+    /// Quantity button: Quantity to be refunded
+    ///
+    @IBOutlet private var itemQuantityButton: UIButton!
+
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
+}
+
+// MARK: Actions
+private extension RefundItemTableViewCell {
+    @IBAction func quantityButtonPressed(_ sender: Any) {
+        print("Item quantity button pressed")
     }
 }

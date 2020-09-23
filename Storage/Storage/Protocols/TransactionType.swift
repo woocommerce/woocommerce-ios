9eb76b9338268a0959316b7467cc94a86906ee2e
Shiki <jayson@basanes.net>
2020-09-23 15:54:38 -0600

    Add TransactioType protocol

---
+++ Storage/Storage/Protocols/TransactionType.swift
@@ -0,0 +1,52 @@
+import class CoreData.NSManagedObject
+
+public protocol TransactionType: class {
+    /// Returns all of the available objects of a given Type, matching the specified Predicate (and sorted with a given collection of
+    /// SortDescriptors).
+    ///
+    func allObjects<T: Object>(ofType type: T.Type, matching predicate: NSPredicate?, sortedBy descriptors: [NSSortDescriptor]?) -> [T]
+
+    /// Returns the count of all of the available objects, of a given Type.
+    ///
+    func countObjects<T: Object>(ofType type: T.Type) -> Int
+
+    /// Returns the count of objects, of a given Type, matching a specified Predicate.
+    ///
+    func countObjects<T: Object>(ofType type: T.Type, matching predicate: NSPredicate?) -> Int
+
+    /// Nukes the specified Object.
+    ///
+    func deleteObject<T: Object>(_ object: T)
+
+    /// Nukes all of the objects of the specified kind.
+    ///
+    func deleteAllObjects<T: Object>(ofType type: T.Type)
+
+    /// Returns the first available object, of a given kind, matching the specified Predicate.
+    ///
+    func firstObject<T: Object>(ofType type: T.Type) -> T?
+
+    /// Returns the first available object, of a given kind, matching the specified Predicate.
+    ///
+    func firstObject<T: Object>(ofType type: T.Type, matching predicate: NSPredicate?) -> T?
+
+    /// Inserts a new object of the given Type.
+    ///
+    func insertNewObject<T: Object>(ofType type: T.Type) -> T
+
+    /// Loads an object, of the specified Type, with a given ObjectID (if any).
+    ///
+    func loadObject<T: Object>(ofType type: T.Type, with objectID: T.ObjectID) -> T?
+
+    /// Obtain permanent `ObjectID` for the given `NSManagedObjects`.
+    ///
+    /// This is temporarily exposed since `NSFetchedResultsController`'s `DiffableDataSource`
+    /// support exposes temporary IDs in snapshots and immediately converts them to permanent IDs.
+    /// This causes an undesirable confusing animation (dual refresh) when the subsequent
+    /// snapshots are applied to the `UITableView`.
+    ///
+    /// We will find a better solution for this later. My current idea is to use a separate
+    /// **read-only** `NSManagedObjectContext` to use for the `NSFetchedResultsController`.
+    /// This will ensure that `NSFetchedResultsController` will not ever receive temporary IDs.
+    func obtainPermanentIDs(for objects: [NSManagedObject]) throws
+}

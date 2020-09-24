4e4ceead6b7678bbe75560b1d1b2ce74b0a9f895
Shiki <jayson@basanes.net>
2020-09-24 09:23:51 -0600

    Add documentation for Attribute

--- Storage/Storage/Model/Attribute+CoreDataClass.swift
+++ Storage/Storage/Model/Attribute+CoreDataClass.swift
@@ -1,6 +1,11 @@
 import Foundation
 import CoreData
 
+/// A generic Attribute class with 3 core properties: `id`, `key`, and `value`.
+///
+/// This is currently only used as the corresponding storage for `ProductVariationAttribute` but
+/// we might be able to use it for others later. 
+///
 @objc(Attribute)
 public class Attribute: NSManagedObject {
 

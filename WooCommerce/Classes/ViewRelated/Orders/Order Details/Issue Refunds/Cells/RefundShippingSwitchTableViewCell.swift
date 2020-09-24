0f29fdfbe74b526e716ee0680d36ec22f8cf9d69
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-09-24 17:46:13 -0500

    Adds RefundShippingSwitchTableViewCell empty class and xib

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundShippingSwitchTableViewCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+/// Displays a switch to enable shipping refunds
+///
+final class RefundShippingSwitchTableViewCell: UITableViewCell {
+
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}

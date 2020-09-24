9a962db30bc163d73adf910559968640b1fe3729
Partho Biswas <partho.maple@gmail.com>
2020-09-24 23:28:55 +0600

    update MockProduct for downloadable files

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Edit Product/Downloadable Files/FIle List/ProductDownloadListViewModelTests.swift
@@ -0,0 +1,33 @@
+//
+//  ProductDownloadListViewModelTests.swift
+//  WooCommerceTests
+//
+//  Created by Partho Biswas on 9/24/20.
+//  Copyright © 2020 Automattic. All rights reserved.
+//
+
+import XCTest
+
+class ProductDownloadListViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

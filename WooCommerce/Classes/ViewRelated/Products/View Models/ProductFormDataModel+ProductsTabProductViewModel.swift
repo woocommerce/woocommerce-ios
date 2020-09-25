c9b802a0e3b76014acd9327d751cf53b85eceb62
Jaclyn Chen <watertranquil@gmail.com>
2020-09-25 11:09:46 +0800

    Add stock status to each variation row in variation list.

---
+++ WooCommerce/Classes/ViewRelated/Products/View Models/ProductFormDataModel+ProductsTabProductViewModel.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+/// Helpers for `ProductsTabProductViewModel` from `ProductFormDataModel`.
+extension ProductFormDataModel {
+    /// Create a description text based on a product data model's stock status/quantity.
+    func createStockText() -> String {
+        switch stockStatus {
+        case .inStock:
+            if let stockQuantity = stockQuantity {
+                let format = NSLocalizedString("%ld in stock", comment: "Label about product's inventory stock status shown on Products tab")
+                return String.localizedStringWithFormat(format, stockQuantity)
+            } else {
+                return NSLocalizedString("In stock", comment: "Label about product's inventory stock status shown on Products tab")
+            }
+        default:
+            return stockStatus.description
+        }
+    }
+}

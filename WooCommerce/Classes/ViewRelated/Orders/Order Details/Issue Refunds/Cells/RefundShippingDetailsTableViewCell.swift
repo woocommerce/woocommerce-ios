c124b407ee70bc8acb288d814392af764c158b3b
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-09-25 12:31:32 -0500

    Adds empty RefundShippingDetailsTableViewCell

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundShippingDetailsTableViewCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+/// TableViewCell that displays all information regarding shipping refunds
+///
+final class RefundShippingDetailsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}

b9e087a8a708841d0943d4ce65840e75a5106e38
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-09-25 14:14:32 -0500

    Update RefundShippingDetailsTableViewCell with SwiftUI previews

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundShippingDetailsTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundShippingDetailsTableViewCell.swift
@@ -44,3 +44,57 @@ final class RefundShippingDetailsTableViewCell: UITableViewCell {
         super.awakeFromNib()
     }
 }
+
+// MARK: - Previews
+#if canImport(SwiftUI) && DEBUG
+
+import SwiftUI
+
+private struct RefundShippingDetailsTableViewCellRepresentable: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let nib = UINib(nibName: "RefundShippingDetailsTableViewCell", bundle: nil)
+        guard let cell = nib.instantiate(withOwner: self, options: nil).first as? RefundShippingDetailsTableViewCell else {
+            fatalError("Could not create RefundShippingDetailsTableViewCell")
+        }
+        return cell
+    }
+
+    func updateUIView(_ view: UIView, context: Context) {
+        // no op
+    }
+}
+
+@available(iOS 13.0, *)
+struct RefundShippingDetailsTableViewCell_Previews: PreviewProvider {
+
+    private static func makeStack() -> some View {
+        VStack {
+            RefundShippingDetailsTableViewCellRepresentable()
+        }
+    }
+
+    static var previews: some View {
+        Group {
+            makeStack()
+                .previewLayout(.fixed(width: 359, height: 209))
+                .previewDisplayName("Light")
+
+            makeStack()
+                .previewLayout(.fixed(width: 359, height: 209))
+                .environment(\.colorScheme, .dark)
+                .previewDisplayName("Dark")
+
+//            makeStack()
+//                .previewLayout(.fixed(width: 359, height: 300))
+//                .environment(\.sizeCategory, .accessibilityMedium)
+//                .previewDisplayName("Large Font")
+//
+//            makeStack()
+//                .previewLayout(.fixed(width: 359, height: 620))
+//                .environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
+//                .previewDisplayName("Extra Large Font")
+        }
+    }
+}
+
+#endif

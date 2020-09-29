a7ba42cbe9b14591c73875df3aafd6b01b5a34e1
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-09-29 14:59:47 -0500

    Update RefundProductsTotalTableViewCell wuth xib content and iboutlets

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundProductsTotalTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundProductsTotalTableViewCell.swift
@@ -4,6 +4,30 @@ import UIKit
 ///
 final class RefundProductsTotalTableViewCell: UITableViewCell {
 
+    /// Displays `Subtotal` title
+    ///
+    @IBOutlet private var subtotalTitleLabel: UILabel!
+
+    /// Displays the subtotal value
+    ///
+    @IBOutlet private var subtotalPriceLabel: UILabel!
+
+    /// Displays `Tax` title
+    ///
+    @IBOutlet private var taxTitleLabel: UILabel!
+
+    /// Displays the tax value
+    ///
+    @IBOutlet private var taxPriceLabel: UILabel!
+
+    /// Displays `Products Refund` title
+    ///
+    @IBOutlet private var productsRefundTitleLabel: UILabel!
+
+    /// Displays the total products costs to be refunded
+    ///
+    @IBOutlet private var productsRefundPriceLabel: UILabel!
+
     override class func awakeFromNib() {
         super.awakeFromNib()
     }

52d6e9af25560e7d9bd6baccfcfb63c2a78c99de
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-09-29 15:02:59 -0500

    Update RefundProductsTotalTableViewCell wuth SwiftUI previews

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundProductsTotalTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundProductsTotalTableViewCell.swift
@@ -32,3 +32,52 @@ final class RefundProductsTotalTableViewCell: UITableViewCell {
         super.awakeFromNib()
     }
 }
+
+// MARK: - Previews
+#if canImport(SwiftUI) && DEBUG
+
+import SwiftUI
+
+private struct RefundProductsTotalTableViewCellRepresentable: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let nib = UINib(nibName: "RefundProductsTotalTableViewCell", bundle: nil)
+        guard let cell = nib.instantiate(withOwner: self, options: nil).first as? RefundProductsTotalTableViewCell else {
+            fatalError("Could not create RefundProductsTotalTableViewCell")
+        }
+        return cell
+    }
+
+    func updateUIView(_ view: UIView, context: Context) {
+        // no op
+    }
+}
+
+@available(iOS 13.0, *)
+struct RefundProductsTotalTableViewCell_Previews: PreviewProvider {
+
+    private static func makeStack() -> some View {
+        VStack {
+            RefundProductsTotalTableViewCellRepresentable()
+        }
+    }
+
+    static var previews: some View {
+        Group {
+            makeStack()
+                .previewLayout(.fixed(width: 359, height: 128))
+                .previewDisplayName("Light")
+
+            makeStack()
+                .previewLayout(.fixed(width: 359, height: 128))
+                .environment(\.colorScheme, .dark)
+                .previewDisplayName("Dark")
+
+            makeStack()
+                .previewLayout(.fixed(width: 359, height: 200))
+                .environment(\.sizeCategory, .accessibilityMedium)
+                .previewDisplayName("Large Font")
+        }
+    }
+}
+
+#endif

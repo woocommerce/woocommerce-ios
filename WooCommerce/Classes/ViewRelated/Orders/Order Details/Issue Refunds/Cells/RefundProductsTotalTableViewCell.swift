02a7712f047081072a86babf2354754f953190fd
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-09-29 14:34:10 -0500

    Adds empty RefundProductsTotalTableViewCell

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundProductsTotalTableViewCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+/// `TableViewCell` that displays the products amount to be refunded
+///
+final class RefundProductsTotalTableViewCell: UITableViewCell {
+
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}

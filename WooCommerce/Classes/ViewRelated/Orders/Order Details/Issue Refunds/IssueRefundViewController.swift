8823a01ab6a260b1b41e5886cc67d07389d457ef
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-09-30 11:51:41 -0500

    Adds Empty IssueRefundViewController

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/IssueRefundViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class IssueRefundViewControlle: UIViewController {
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) is not supported")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

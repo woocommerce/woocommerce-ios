7c089619f1240541239d80b5a6d792eae7d04e91
Shiki <jayson@basanes.net>
2020-10-01 15:51:06 -0600

    Add comments for RefundConfirmationVM

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/RefundConfirmationViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/RefundConfirmationViewModel.swift
@@ -1,11 +1,14 @@
 import Foundation
 
+/// ViewModel for presenting refund confirmation to the user.
+///
 final class RefundConfirmationViewModel {
 
-    // This will be computed later :D
+    /// This will be computed later :D
     let refundAmount = "$87.50"
 
-    private(set) var sections: [Section]
+    /// The sections and rows to display in the `UITableView`.
+    let sections: [Section]
 
     init() {
         sections = [
@@ -31,6 +34,7 @@ final class RefundConfirmationViewModel {
 
 // MARK: - Sections and Rows
 
+/// A base protocol for the row types used by `RefundConfirmationViewModel`.
 protocol RefundConfirmationViewModelRow {
 
 }
@@ -41,17 +45,21 @@ extension RefundConfirmationViewModel {
         let rows: [RefundConfirmationViewModelRow]
     }
 
+    /// A row that shows a title on the left and a value on the right.
     struct TwoColumnRow: RefundConfirmationViewModelRow {
         let title: String
         let value: String
+        /// Indicates the style to use. If true, the view should use an emphasized (bold) style.
         let isHeadline: Bool
     }
 
+    /// A row that shows a title and a text field below it.
     struct TitleAndEditableValueRow: RefundConfirmationViewModelRow {
         let title: String
         let placeholder: String
     }
 
+    /// A row that shows a title and a paragraph (label) below it.
     struct TitleAndBodyRow: RefundConfirmationViewModelRow {
         let title: String
         let body: String?
@@ -62,11 +70,18 @@ extension RefundConfirmationViewModel {
 
 private extension RefundConfirmationViewModel {
     enum Localization {
-        static let previouslyRefunded = NSLocalizedString("Previously Refunded", comment: "")
-        static let refundAmount = NSLocalizedString("Refund Amount", comment: "")
-        static let refundVia = NSLocalizedString("Refund Via", comment: "")
-        static let reasonForRefund = NSLocalizedString("Reason for Refund (Optional)", comment: "")
-        static let reasonForRefundingOrder = NSLocalizedString("Reason for refunding order", comment: "")
+        static let previouslyRefunded = NSLocalizedString("Previously Refunded",
+                                                          comment: "A label representing the amount that was previously refunded for the order.")
+        static let refundAmount = NSLocalizedString("Refund Amount",
+                                                    comment: "A label representing the amount that will be refunded if the user confirms to proceed with the refund.")
+        static let refundVia = NSLocalizedString("Refund Via",
+                                                 comment: "The title of the section containing information about how the refund will be processed.")
+        static let reasonForRefund =
+            NSLocalizedString("Reason for Refund (Optional)",
+                              comment: "A label for the text field that the user can edit to indicate why they are issuing a refund.")
+        static let reasonForRefundingOrder =
+            NSLocalizedString("Reason for refunding order",
+                              comment: "A placeholder for the text field that the user can edit to indicate why they are issuing a refund.")
 
         static func manualRefund(via paymentMethod: String) -> String {
             let format = NSLocalizedString(

207e85cd1d593fa7800a05075879c0cdf85af46f
Shiki <jayson@basanes.net>
2020-10-01 12:45:03 -0600

    RefundConfirmationVM: Add Sections and Rows structure

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/RefundConfirmationViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/RefundConfirmationViewModel.swift
@@ -1,4 +1,39 @@
 import Foundation
 
 final class RefundConfirmationViewModel {
+    private(set) var sections: [Section]
+
+    init() {
+        sections = [Section(title: nil, rows: [
+            TwoColumnRow(title: Localization.previouslyRefunded, value: "$0.01"),
+            TwoColumnRow(title: Localization.refundAmount, value: "$1.23")
+        ])]
+    }
+}
+
+// MARK: - Sections and Rows
+
+protocol RefundConfirmationViewModelRow {
+
+}
+
+extension RefundConfirmationViewModel {
+    struct Section {
+        let title: String?
+        let rows: [RefundConfirmationViewModelRow]
+    }
+
+    struct TwoColumnRow: RefundConfirmationViewModelRow {
+        let title: String
+        let value: String
+    }
+}
+
+// MARK: - Localization
+
+private extension RefundConfirmationViewModel {
+    enum Localization {
+        static let previouslyRefunded = NSLocalizedString("Previously Refunded", comment: "")
+        static let refundAmount = NSLocalizedString("Refund Amount", comment: "")
+    }
 }

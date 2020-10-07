ea51b35e079ecd02621fe0e6627ba5070f2f1f13
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-10-07 13:31:13 -0500

    Update IssueRefundViewModel to represent real order item content

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/IssueRefundViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/IssueRefundViewModel.swift
@@ -20,27 +20,45 @@ final class IssueRefundViewModel {
     let selectedItemsTitle: String = "3 items selected"
 
     /// The sections and rows to display in the `UITableView`.
-    /// This is temporary data, will be removed after implementing https://github.com/woocommerce/woocommerce-ios/issues/2842
     ///
-    let sections: [Section] = [
-        Section(rows: [
-            RefundItemViewModel(productImage: nil, productTitle: "Item 1", productQuantityAndPrice: "2 x $30.27 each", quantityToRefund: "1"),
-            RefundItemViewModel(productImage: nil, productTitle: "Item 2", productQuantityAndPrice: "4 x $20.00 each", quantityToRefund: "2"),
-            RefundItemViewModel(productImage: nil, productTitle: "Item 3", productQuantityAndPrice: "3 x $15.99 each", quantityToRefund: "0"),
-            RefundProductsTotalViewModel(productsTax: "$13.45", productsSubtotal: "$66.26", productsTotal: "$79.71")
-        ]),
-        Section(rows: [
-            ShippingSwitchViewModel(title: "Refund Shipping", isOn: true),
-            RefundShippingDetailsViewModel(carrierRate: "USPS Flat Rate",
-                                           carrierCost: "$10.0",
-                                           shippingTax: "$2.99",
-                                           shippingSubtotal: "$10.0",
-                                           shippingTotal: "$12.99")
-        ])
-    ]
+    private(set) var sections: [Section] = []
 
-    init(order: Order) {
+    /// Products related to this order. Needed to build `RefundItemViewModel` rows
+    ///
+    lazy var products: [Product] = {
+        let resultsController = createProductsResultsController()
+        try? resultsController.performFetch()
+        return resultsController.fetchedObjects
+    }()
+
+    init(order: Order, currencySettings: CurrencySettings) {
         self.order = order
+
+        // This is temporary data, will be removed after implementing https://github.com/woocommerce/woocommerce-ios/issues/2842
+        sections = [
+            createItemsToRefundSection(currencySettings: currencySettings),
+            Section(rows: [
+                ShippingSwitchViewModel(title: "Refund Shipping", isOn: true),
+                RefundShippingDetailsViewModel(carrierRate: "USPS Flat Rate",
+                                               carrierCost: "$10.0",
+                                               shippingTax: "$2.99",
+                                               shippingSubtotal: "$10.0",
+                                               shippingTotal: "$12.99")
+            ])
+        ]
+    }
+}
+
+// MARK: Results Controller
+private extension IssueRefundViewModel {
+
+    /// Results controller that fetches the products related to this order
+    ///
+    func createProductsResultsController() -> ResultsController<StorageProduct> {
+        let siteID = ServiceLocator.stores.sessionManager.defaultStoreID ?? Int64.min
+        let itemsIDs = order.items.map { $0.productID }
+        let predicate = NSPredicate(format: "siteID == %lld AND productID IN %@", siteID, itemsIDs)
+        return ResultsController<StorageProduct>(storageManager: ServiceLocator.storageManager, matching: predicate, sortedBy: [])
     }
 }
 
@@ -60,6 +78,20 @@ extension IssueRefundViewModel {
     struct ShippingSwitchViewModel: IssueRefundRow {
         let title: String
         let isOn: Bool
+    }
+
+    /// Returns a section with the order items that can be refunded
+    ///
+    private func createItemsToRefundSection(currencySettings: CurrencySettings) -> Section {
+        let itemsRows = order.items.map { item -> RefundItemViewModel in
+            let product = products.filter { $0.productID == item.productID }.first
+            return RefundItemViewModel(item: item, product: product, currency: order.currency, currencySettings: currencySettings)
+        }
+
+        // This is temporary data, will be removed after implementing https://github.com/woocommerce/woocommerce-ios/issues/2842
+        let sumaryRow = RefundProductsTotalViewModel(productsTax: "$13.45", productsSubtotal: "$66.26", productsTotal: "$79.71")
+
+        return Section(rows: itemsRows + [sumaryRow])
     }
 }
 

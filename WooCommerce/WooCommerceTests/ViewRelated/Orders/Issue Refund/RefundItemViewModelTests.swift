c595821ed0d0f69dd407f9367cd5d4e1e78f637d
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-10-07 15:56:10 -0500

    Added MockOrderItem

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Orders/Issue Refund/RefundItemViewModelTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+import Yosemite
+
+@testable import WooCommerce
+
+
+/// Test cases for `RefundItemViewModel`
+///
+final class RefundItemViewModelTests: XCTestCase {
+    func test_something() {
+        Mockor
+    }
+}

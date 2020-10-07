058bc9c975eb71b755c51fa5a0c99f0f3b67894f
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-10-07 13:28:25 -0500

    Update RefundItemTableViewCell to allow the download of remote images

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundItemViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundItemViewModel.swift
@@ -1,9 +1,10 @@
 import Foundation
+import Yosemite
 
 /// Represents an order item to be refunded. Meant to be rendered by `RefundItemTableViewCell`
 ///
 struct RefundItemViewModel {
-    let productImage: URL?
+    let productImage: String?
     let productTitle: String
     let productQuantityAndPrice: String
     let quantityToRefund: String

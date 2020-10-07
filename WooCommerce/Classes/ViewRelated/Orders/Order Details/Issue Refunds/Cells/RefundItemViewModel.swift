54dc0bbb2c1a058c7eda7cc8b86d844cfec1fad1
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-10-07 13:30:07 -0500

    Update RefundItemViewModel to be created from an order item and it's product

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundItemViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundItemViewModel.swift
@@ -9,3 +9,29 @@ struct RefundItemViewModel {
     let productQuantityAndPrice: String
     let quantityToRefund: String
 }
+
+// MARK: Convenience Initializers
+extension RefundItemViewModel {
+
+    /// Creates a `RefundItemViewModel` based on a `OrderItem`, it's related product and it's currency..
+    /// `QuantityToRefund` is set to 0.
+    ///
+    init(item: OrderItem, product: Product?, currency: String, currencySettings: CurrencySettings) {
+        productImage = product?.images.first?.src
+        productTitle = item.name
+        quantityToRefund = String(UInt.min)
+        productQuantityAndPrice = {
+            let quantity = NumberFormatter.localizedString(from: item.quantity as NSDecimalNumber, number: .decimal)
+            let currencyFormatter = CurrencyFormatter(currencySettings: currencySettings)
+            let price = currencyFormatter.formatAmount(item.price, with: currency ) ?? ""
+            return String(format: Localization.quantityAndPriceFormat, quantity, price)
+        }()
+    }
+}
+
+// MARK: Constant
+private extension RefundItemViewModel {
+    enum Localization {
+        static let quantityAndPriceFormat = NSLocalizedString("%@ x %@ each", comment: "Refund item price and quantity format. EG: 2 x $10.00 each")
+    }
+}

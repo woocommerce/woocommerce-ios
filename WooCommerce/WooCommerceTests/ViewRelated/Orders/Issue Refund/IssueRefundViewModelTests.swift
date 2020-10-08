23de7f50750a4b952a7dfe9532de843a549e1e44
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-10-08 12:08:14 -0500

    Update IssueRefundViewModel with tests

--- WooCommerce/WooCommerceTests/ViewRelated/Orders/Issue Refund/IssueRefundViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Orders/Issue Refund/IssueRefundViewModelTests.swift
@@ -7,4 +7,48 @@ import Yosemite
 ///
 final class IssueRefundViewModelTests: XCTestCase {
 
+    func test_viewModel_does_not_have_shippping_section_on_order_without_shipping() {
+        // Given
+        let currencySettings = CurrencySettings()
+        let order = MockOrders().makeOrder(shippingLines: [])
+
+        // When
+        let viewModel = IssueRefundViewModel(order: order, currencySettings: currencySettings)
+
+        // Then
+        let rows = viewModel.sections.flatMap { $0.rows }
+        XCTAssertFalse(rows.isEmpty)
+        rows.forEach { viewModel in
+            XCTAssertFalse(viewModel is IssueRefundViewModel.ShippingSwitchViewModel)
+            XCTAssertFalse(viewModel is RefundShippingDetailsViewModel)
+        }
+    }
+
+    func test_viewModel_does_have_shipping_section_on_order_with_shipping() throws {
+        // Given
+        let currencySettings = CurrencySettings()
+        let order = MockOrders().makeOrder(shippingLines: MockOrders.sampleShippingLines())
+
+        // When
+        let viewModel = IssueRefundViewModel(order: order, currencySettings: currencySettings)
+
+        // Then
+        let shippingSwitchRow = try XCTUnwrap(viewModel.sections[safe: 1]?.rows[safe: 0])
+        XCTAssertTrue(shippingSwitchRow is IssueRefundViewModel.ShippingSwitchViewModel)
+    }
+
+    func test_viewModel_inserts_shipping_details_after_toggling_switch() throws {
+        // Given
+        let currencySettings = CurrencySettings()
+        let order = MockOrders().makeOrder(shippingLines: MockOrders.sampleShippingLines())
+        let viewModel = IssueRefundViewModel(order: order, currencySettings: currencySettings)
+        XCTAssertNil(viewModel.sections[safe: 1]?.rows[safe: 1]) // No shipping details
+
+        // When
+        viewModel.toggleRefundShipping()
+
+        // Then
+        let shippingDetailsRow = try XCTUnwrap(viewModel.sections[safe: 1]?.rows[safe: 1])
+        XCTAssertTrue(shippingDetailsRow is RefundShippingDetailsViewModel)
+    }
 }

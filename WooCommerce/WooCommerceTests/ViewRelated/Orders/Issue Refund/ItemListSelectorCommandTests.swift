1e376388a7bc53860f1b9baa1921ecd71a21c629
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-10-08 15:05:41 -0500

    Update ItemListSelectorCommandTests with tests

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Orders/Issue Refund/ItemListSelectorCommandTests.swift
@@ -0,0 +1,32 @@
+import XCTest
+import Yosemite
+
+@testable import WooCommerce
+
+/// Test cases for `ItemListSelectorCommand`
+///
+final class ItemListSelectorCommandTests: XCTestCase {
+    func test_commanmd_produces_correct_data_when_max_quantity_is_0() {
+        // When
+        let command = ItemListSelectorCommand(maxRefundQuantity: 0, currentQuantity: 0)
+
+        // Then
+        XCTAssertEqual(command.data, [0])
+    }
+
+    func test_commanmd_produces_correct_data_when_max_quantity_is_bigger_than_0() {
+        // When
+        let command = ItemListSelectorCommand(maxRefundQuantity: 5, currentQuantity: 0)
+
+        // Then
+        XCTAssertEqual(command.data, [0, 1, 2, 3, 4, 5])
+    }
+
+    func test_command_computes_selected_correctly() {
+        // When
+        let command = ItemListSelectorCommand(maxRefundQuantity: 5, currentQuantity: 3)
+
+        // Then
+        XCTAssertEqual(command.selected, 3)
+    }
+}

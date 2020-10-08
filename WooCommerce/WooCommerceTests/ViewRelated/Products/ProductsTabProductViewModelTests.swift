9f78b8dde1dfcca652b47df1c8d34865791f8c02
Jaclyn Chen <watertranquil@gmail.com>
2020-10-08 16:43:37 +0800

    Add/update unit tests for stock status details.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/ProductsTabProductViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/ProductsTabProductViewModelTests.swift
@@ -6,49 +6,87 @@ final class ProductsTabProductViewModelTests: XCTestCase {
 
     // MARK: Stock status
 
-    func testDetailsForProductInStockWithoutQuantity() {
-        let product = productMock(name: "Yay", stockQuantity: nil, stockStatus: .inStock)
+    func test_details_contain_stock_status_without_quantity_when_quantity_is_nil_and_manage_stock_is_enabled() {
+        // Arrange
+        let product = productMock().copy(manageStock: true, stockQuantity: nil, stockStatusKey: ProductStockStatus.inStock.rawValue)
+
+        // Action
         let viewModel = ProductsTabProductViewModel(product: product)
         let detailsText = viewModel.detailsAttributedString.string
+
+        // Assert
         let expectedStockDetail = NSLocalizedString("In stock", comment: "Label about product's inventory stock status shown on Products tab")
         XCTAssertTrue(detailsText.contains(expectedStockDetail))
     }
 
-    func testDetailsForProductInStockWithQuantity() {
+    func test_details_contain_stock_status_with_quantity_when_manage_stock_is_enabled() {
+        // Arrange
         let stockQuantity: Int64 = 6
-        let product = productMock(name: "Yay", stockQuantity: stockQuantity, stockStatus: .inStock)
+        let product = productMock().copy(manageStock: true, stockQuantity: stockQuantity, stockStatusKey: ProductStockStatus.inStock.rawValue)
+
+        // Action
         let viewModel = ProductsTabProductViewModel(product: product)
         let detailsText = viewModel.detailsAttributedString.string
+
+        // Assert
         let format = NSLocalizedString("%ld in stock", comment: "Label about product's inventory stock status shown on Products tab")
         let expectedStockDetail = String.localizedStringWithFormat(format, stockQuantity)
         XCTAssertTrue(detailsText.contains(expectedStockDetail))
     }
 
-    func testDetailsForProductOutOfStock() {
-        let product = productMock(name: "Yay", stockQuantity: 1099, stockStatus: .outOfStock)
+    func test_details_contain_stock_status_without_quantity_when_manage_stock_is_disabled() {
+        // Arrange
+        let stockQuantity: Int64 = 6
+        let product = productMock().copy(manageStock: false, stockQuantity: stockQuantity, stockStatusKey: ProductStockStatus.inStock.rawValue)
+
+        // Action
         let viewModel = ProductsTabProductViewModel(product: product)
         let detailsText = viewModel.detailsAttributedString.string
+
+        // Assert
+        XCTAssertTrue(detailsText.contains(ProductStockStatus.inStock.description))
+    }
+
+    func test_details_contain_stock_status_when_product_is_out_of_stock() {
+        // Arrange
+        let product = productMock(name: "Yay", stockQuantity: 1099, stockStatus: .outOfStock)
+
+        // Action
+        let viewModel = ProductsTabProductViewModel(product: product)
+        let detailsText = viewModel.detailsAttributedString.string
+
+        // Assert
         let expectedStockDetail = NSLocalizedString("Out of stock", comment: "Display label for the product's inventory stock status")
         XCTAssertTrue(detailsText.contains(expectedStockDetail))
     }
 
     // MARK: Variations
 
-    func testDetailsForProductWithOneVariation() {
+    func test_details_contain_singular_variant_format_when_product_has_one_variation() {
+        // Arrange
         let variations: [Int64] = [134]
         let product = productMock(name: "Yay", variations: variations)
+
+        // Action
         let viewModel = ProductsTabProductViewModel(product: product)
         let detailsText = viewModel.detailsAttributedString.string
+
+        // Assert
         let singularFormat = NSLocalizedString("%ld variant", comment: "Label about one product variation shown on Products tab")
         let expectedStockDetail = String.localizedStringWithFormat(singularFormat, variations.count)
         XCTAssertTrue(detailsText.contains(expectedStockDetail))
     }
 
-    func testDetailsForProductWithMultipleVariations() {
+    func test_details_contain_plural_variant_format_when_product_has_multiple_variations() {
+        // Arrange
         let variations: [Int64] = [201, 134]
         let product = productMock(name: "Yay", variations: variations)
+
+        // Action
         let viewModel = ProductsTabProductViewModel(product: product)
         let detailsText = viewModel.detailsAttributedString.string
+
+        // Assert
         let pluralFormat = NSLocalizedString("%ld variants", comment: "Label about number of variations shown on Products tab")
         let expectedStockDetail = String.localizedStringWithFormat(pluralFormat, variations.count)
         XCTAssertTrue(detailsText.contains(expectedStockDetail))

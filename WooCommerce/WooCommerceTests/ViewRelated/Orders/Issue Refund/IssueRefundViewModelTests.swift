78d6e873e33661f0178dfd2378be1c8b438ba759
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-10-08 16:52:19 -0500

    Update IssueRefundViewModelTests with more test cases

--- WooCommerce/WooCommerceTests/ViewRelated/Orders/Issue Refund/IssueRefundViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Orders/Issue Refund/IssueRefundViewModelTests.swift
@@ -71,4 +71,25 @@ final class IssueRefundViewModelTests: XCTestCase {
         XCTAssertEqual(viewModel.quantityAvailableForRefundForItemAtIndex(2), 1)
         XCTAssertEqual(viewModel.quantityAvailableForRefundForItemAtIndex(3), nil)
     }
+
+    func test_viewModel_returns_0_current_refund_quantity_for_a_clean_order() {
+        // Given
+        let currencySettings = CurrencySettings()
+        let items = [
+            MockOrderItem.sampleItem(itemID: 1, quantity: 3),
+            MockOrderItem.sampleItem(itemID: 2, quantity: 2),
+            MockOrderItem.sampleItem(itemID: 3, quantity: 1),
+        ]
+        let order = MockOrders().makeOrder(items: items)
+
+        // When
+        let viewModel = IssueRefundViewModel(order: order, currencySettings: currencySettings)
+
+        // Then
+        XCTAssertEqual(viewModel.currentQuantityForItemAtIndex(0), 0)
+        XCTAssertEqual(viewModel.currentQuantityForItemAtIndex(1), 0)
+        XCTAssertEqual(viewModel.currentQuantityForItemAtIndex(2), 0)
+        XCTAssertEqual(viewModel.currentQuantityForItemAtIndex(3), nil)
+    }
+
 }

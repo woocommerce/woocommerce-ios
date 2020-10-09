2a08e19ac615dc3c3fbd13b5768b08ed71058bfe
Shiki <jayson@basanes.net>
2020-10-08 19:35:42 -0600

    Implement Order.totalRefunded()

--- Yosemite/Yosemite/Model/Orders/Order+Refunds.swift
+++ Yosemite/Yosemite/Model/Orders/Order+Refunds.swift
@@ -1,8 +1,13 @@
 import Networking
 
-extension Order {
-    /// Sums up the `OrderRefundCondensed.total` inside `self.refunds`.
-    var totalRefunded: Double {
-        0
+public extension Order {
+    /// Sums up the `OrderRefundCondensed.total` inside `self.refunds`. This is returned as a
+    /// **negative** number because that's how the API returns them as.
+    ///
+    func totalRefunded(using currencyFormatter: CurrencyFormatter) -> NSDecimalNumber {
+        refunds.reduce(NSDecimalNumber(decimal: .zero)) { result, refundCondensed -> NSDecimalNumber in
+            let totalAsDecimal = currencyFormatter.convertToDecimal(from: refundCondensed.total) ?? .zero
+            return result.adding(totalAsDecimal)
+        }
     }
 }

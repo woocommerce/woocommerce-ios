92d3a5dae04747054b2304d79d7619878d2dca2f
Shiki <jayson@basanes.net>
2020-10-08 20:21:26 -0600

    Add tests for Order+Refunds

--- Yosemite/Yosemite/Model/Orders/Order+Refunds.swift
+++ Yosemite/Yosemite/Model/Orders/Order+Refunds.swift
@@ -4,9 +4,9 @@ public extension Order {
     /// Sums up the `OrderRefundCondensed.total` inside `self.refunds`. This is returned as a
     /// **negative** number because that's how the API returns them as.
     ///
-    func totalRefunded(using currencyFormatter: CurrencyFormatter) -> NSDecimalNumber {
-        refunds.reduce(NSDecimalNumber(decimal: .zero)) { result, refundCondensed -> NSDecimalNumber in
-            let totalAsDecimal = currencyFormatter.convertToDecimal(from: refundCondensed.total) ?? .zero
+    func totalRefunded(using formatter: CurrencyFormatter, locale: Locale = .current) -> NSDecimalNumber {
+        refunds.reduce(NSDecimalNumber.zero) { result, refundCondensed -> NSDecimalNumber in
+            let totalAsDecimal = formatter.convertToDecimal(from: refundCondensed.total, locale: locale) ?? .zero
             return result.adding(totalAsDecimal)
         }
     }

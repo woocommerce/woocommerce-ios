e2eefc45fe5160a02bf346188ce4f174d5b51b70
Shiki <jayson@basanes.net>
2020-10-12 15:21:18 -0600

    TotalRefundedUseCase: Inject Locale

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/TotalRefundedCalculationUseCase.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/TotalRefundedCalculationUseCase.swift
@@ -7,17 +7,19 @@ struct TotalRefundedCalculationUseCase {
 
     private let order: Order
     private let currencyFormatter: CurrencyFormatter
+    private let locale: Locale
 
-    init(order: Order, currencyFormatter: CurrencyFormatter) {
+    init(order: Order, currencyFormatter: CurrencyFormatter, locale: Locale = .current) {
         self.order = order
         self.currencyFormatter = currencyFormatter
+        self.locale = locale
     }
 
     /// Sums up the `OrderRefundCondensed.total` inside `self.order`. This is returned as a
     /// **negative** number because that's how the API returns them as.
     func totalRefunded() -> NSDecimalNumber {
-        order.refunds.reduce(NSDecimalNumber(decimal: .zero)) { result, refundCondensed -> NSDecimalNumber in
-            let totalAsDecimal = currencyFormatter.convertToDecimal(from: refundCondensed.total) ?? .zero
+        order.refunds.reduce(NSDecimalNumber.zero) { result, refundCondensed -> NSDecimalNumber in
+            let totalAsDecimal = currencyFormatter.convertToDecimal(from: refundCondensed.total, locale: locale) ?? .zero
             return result.adding(totalAsDecimal)
         }
     }

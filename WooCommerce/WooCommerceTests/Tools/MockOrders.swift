9ba04d2a525488074cc78f310ca50c6a75523b37
Shiki <jayson@basanes.net>
2020-10-12 15:13:17 -0600

    Add MockOrders.empty()

--- WooCommerce/WooCommerceTests/Tools/MockOrders.swift
+++ WooCommerce/WooCommerceTests/Tools/MockOrders.swift
@@ -4,6 +4,36 @@ final class MockOrders {
     let siteID: Int64 = 1234
     let orderID: Int64 = 5678
 
+    /// Returns an `Order` with empty values. Use `copy()` to modify them.
+    func empty() -> Order {
+        Order(
+            siteID: 0,
+            orderID: 0,
+            parentID: 0,
+            customerID: 0,
+            number: "",
+            status: .pending,
+            currency: "",
+            customerNote: nil,
+            dateCreated: Date(),
+            dateModified: Date(),
+            datePaid: nil,
+            discountTotal: "",
+            discountTax: "",
+            shippingTotal: "",
+            shippingTax: "",
+            total: "",
+            totalTax: "",
+            paymentMethodTitle: "",
+            items: [],
+            billingAddress: nil,
+            shippingAddress: nil,
+            shippingLines: [],
+            coupons: [],
+            refunds: []
+        )
+    }
+
     func makeOrder(status: OrderStatusEnum = .processing, items: [OrderItem] = [], shippingLines: [ShippingLine] = sampleShippingLines()) -> Order {
         return Order(siteID: siteID,
                      orderID: orderID,

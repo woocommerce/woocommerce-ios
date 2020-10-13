ad0f6e8d9af459201668fc8a9b77a9d84658caac
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-10-13 11:33:08 -0500

    RefundShippingDetailsViewModel to use RefundShippingCalculationUseCase internally

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundShippingDetailsViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundShippingDetailsViewModel.swift
@@ -22,18 +22,8 @@ extension RefundShippingDetailsViewModel {
         self.shippingTax = currencyFormatter.formatAmount(shippingLine.totalTax, with: currency) ?? ""
         self.shippingSubtotal = currencyFormatter.formatAmount(shippingLine.total, with: currency) ?? ""
         self.shippingTotal = {
-            let calculatedTotal = Self.calculateShippingTotal(of: shippingLine, using: currencyFormatter)
-            return currencyFormatter.formatAmount(calculatedTotal, with: currency) ?? ""
+            let useCase = RefundShippingCalculationUseCase(shippingLine: shippingLine, currencyFormatter: currencyFormatter)
+            return currencyFormatter.formatAmount(useCase.calculateRefundValue(), with: currency) ?? ""
         }()
-    }
-
-    /// Calculates the shipping total by adding the shipping cost + the shipping tax
-    ///
-    private static func calculateShippingTotal(of shippingLine: ShippingLine, using currencyFormatter: CurrencyFormatter) -> NSDecimalNumber {
-        guard let cost = currencyFormatter.convertToDecimal(from: shippingLine.total),
-            let tax = currencyFormatter.convertToDecimal(from: shippingLine.totalTax) else {
-                return .zero
-        }
-        return cost.adding(tax)
     }
 }

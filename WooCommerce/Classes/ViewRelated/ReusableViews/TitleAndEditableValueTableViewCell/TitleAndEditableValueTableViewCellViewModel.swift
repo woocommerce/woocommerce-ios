8ac04e577a831e92dcd49cacc23d3d20d1de8418
Shiki <jayson@basanes.net>
2020-10-13 14:17:22 -0600

    TitleAndEditableValue: Accept initialValue

--- WooCommerce/Classes/ViewRelated/ReusableViews/TitleAndEditableValueTableViewCell/TitleAndEditableValueTableViewCellViewModel.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/TitleAndEditableValueTableViewCell/TitleAndEditableValueTableViewCellViewModel.swift
@@ -7,7 +7,7 @@ final class TitleAndEditableValueTableViewCellViewModel {
     let title: String?
     let placeholder: String?
 
-    private let valueSubject = BehaviorSubject<String?>(nil)
+    private let valueSubject: BehaviorSubject<String?>
 
     var value: Observable<String?> {
         valueSubject
@@ -17,9 +17,11 @@ final class TitleAndEditableValueTableViewCellViewModel {
         valueSubject.value
     }
 
-    init(title: String?, placeholder: String?) {
+    init(title: String?, placeholder: String?, initialValue: String? = nil) {
         self.title = title
         self.placeholder = placeholder
+
+        valueSubject = BehaviorSubject(initialValue)
     }
 
     func update(value: String?) {

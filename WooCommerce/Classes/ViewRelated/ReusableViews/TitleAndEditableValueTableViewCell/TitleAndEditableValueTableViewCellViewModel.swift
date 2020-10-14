c0b3da5f861fc866414953204faeda3b0026ccce
Shiki <jayson@basanes.net>
2020-10-14 11:23:39 -0600

    Add comments for TitleAndEditableValueTableViewCellViewModel

--- WooCommerce/Classes/ViewRelated/ReusableViews/TitleAndEditableValueTableViewCell/TitleAndEditableValueTableViewCellViewModel.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/TitleAndEditableValueTableViewCell/TitleAndEditableValueTableViewCellViewModel.swift
@@ -2,18 +2,27 @@ import Foundation
 
 /// Observable ViewModel for `TitleAndEditableValueTableViewCell`.
 ///
+/// Represents a view that has a title label and an editable text field.
+///
 final class TitleAndEditableValueTableViewCellViewModel {
 
+    /// The `String` used for the title label.
     let title: String?
+    /// The placeholder for the editable text field.
     let placeholder: String?
+    /// If `false`, the text field will be disabled. Defaults to `true`.
     let allowsEditing: Bool
 
     private let valueSubject: BehaviorSubject<String?>
 
+    /// Emits values whenever the text field changes.
+    ///
+    /// This is connected with the view's text field through the `update(value:)`.
     var value: Observable<String?> {
         valueSubject
     }
 
+    /// The last value of the text field.
     var currentValue: String? {
         valueSubject.value
     }
@@ -26,6 +35,9 @@ final class TitleAndEditableValueTableViewCellViewModel {
         valueSubject = BehaviorSubject(initialValue)
     }
 
+    /// Updates the value of the `self.value` `Observable`.
+    ///
+    /// This is generally used by the view represented by this `ViewModel`.
     func update(value: String?) {
         valueSubject.send(value)
     }

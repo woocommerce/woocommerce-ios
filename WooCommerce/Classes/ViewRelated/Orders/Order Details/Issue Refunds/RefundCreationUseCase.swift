36d8fcfc4f5232e2dd6cd761ee12c00ab414eb50
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-10-20 11:55:37 -0500

    Adds 1st try of RefundCreationUseCase + tests

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/RefundCreationUseCase.swift
@@ -0,0 +1,81 @@
+import Foundation
+import Yosemite
+
+/// Creates a `Refund` object ready to be used  on `RefundStore.createRefund` action
+///
+struct RefundCreationUseCase {
+
+    /// Total amount to be refunded.
+    ///
+    let amount: String
+
+    /// Reason of the refund.
+    ///
+    let reason: String?
+
+    /// Whether payment refund on the payment gateway should be attempted or not.
+    ///
+    let automaticallyRefundsPayment: Bool
+
+    /// Line items to be refunded, currently only order items are suported
+    ///
+    let items: [IssueRefundViewModel.RefundableOrderItem]
+
+    /// Currency formatted needed for decimal calculations
+    ///
+    let currencyFormatter: CurrencyFormatter
+
+    /// Creates a `Refund` object ready to be used  on `RefundStore.createRefund` action
+    ///
+    func createRefundObject() -> Refund {
+
+        let refundItems = items.map { refundableItem -> OrderItemRefund in
+
+            let taxes = refundableItem.item.taxes.map { tax -> OrderItemTaxRefund in
+                let taxLineToRefund: Decimal = {
+                    let totalTax = currencyFormatter.convertToDecimal(from: tax.total) ?? 0
+                    let itemTax = (totalTax as Decimal) / refundableItem.item.quantity
+                    return itemTax * Decimal(refundableItem.quantity)
+                }()
+                return OrderItemTaxRefund(taxID: tax.taxID,
+                                          subtotal: "",
+                                          total: currencyFormatter.localize(taxLineToRefund) ?? "\(taxLineToRefund)")
+            }
+
+            let total = refundableItem.item.price.multiplying(by: NSDecimalNumber(value: refundableItem.quantity))
+
+            // TODO: Calculate item total tax
+            let totalTax: Decimal = {
+                let totalItemTax = currencyFormatter.convertToDecimal(from: refundableItem.item.totalTax) ?? 0
+                let itemTax = (totalItemTax as Decimal) / refundableItem.item.quantity
+                return itemTax * Decimal(refundableItem.quantity)
+            }()
+
+            return OrderItemRefund(itemID: refundableItem.item.itemID,
+                                   name: "",
+                                   productID: .min,
+                                   variationID: .min,
+                                   quantity: Decimal(refundableItem.quantity),
+                                   price: .zero,
+                                   sku: nil,
+                                   subtotal: "",
+                                   subtotalTax: "",
+                                   taxClass: "",
+                                   taxes: taxes,
+                                   total: currencyFormatter.localize(total) ?? "\(total)",
+                                   totalTax: currencyFormatter.localize(totalTax) ?? "\(totalTax)")
+        }
+
+        return Refund(refundID: .min,
+                      orderID: .min,
+                      siteID: .min,
+                      dateCreated: .distantPast,
+                      amount: amount,
+                      reason: reason ?? "",
+                      refundedByUserID: .min,
+                      isAutomated: nil,
+                      createAutomated: automaticallyRefundsPayment,
+                      items: refundItems)
+    }
+
+}
---
+++ WooCommerce/WooCommerceTests/ViewRelated/Orders/Issue Refund/RefundCreationUseCaseTests.swift
@@ -0,0 +1,93 @@
+import XCTest
+import Yosemite
+
+@testable import WooCommerce
+
+/// Test cases for `RefundCreationUseCase`
+///
+final class RefundCreationUseCaseTests: XCTestCase {
+
+    /// Default currency formatter
+    ///
+    private let formatter = CurrencyFormatter(currencySettings: CurrencySettings())
+
+    func test_refund_top_properties_are_correcly_translated_to_the_refund_object() {
+        // Given
+        let useCase = RefundCreationUseCase(amount: "10.0",
+                                            reason: "Test Reason",
+                                            automaticallyRefundsPayment: true,
+                                            items: [],
+                                            currencyFormatter: formatter)
+
+        // When
+        let refund = useCase.createRefundObject()
+
+        // Then
+        XCTAssertEqual(refund.amount, "10.0")
+        XCTAssertEqual(refund.reason, "Test Reason")
+        XCTAssertEqual(refund.createAutomated, true)
+    }
+
+    func test_refund_order_items_values_are_transformed_with_no_taxes() {
+        // Given
+        let items: [IssueRefundViewModel.RefundableOrderItem] = [
+            .init(item: MockOrderItem.sampleItem(itemID: 1, quantity: 2, price: 5.1, totalTax: "0.0"), quantity: 1),
+            .init(item: MockOrderItem.sampleItem(itemID: 2, quantity: 2, price: 6.3, totalTax: "0.0"), quantity: 2)
+        ]
+        let useCase = RefundCreationUseCase(amount: "17.60",
+                                            reason: nil,
+                                            automaticallyRefundsPayment: false,
+                                            items: items,
+                                            currencyFormatter: formatter)
+
+        // When
+        let refund = useCase.createRefundObject()
+
+        // Then
+        XCTAssertEqual(refund.items.count, items.count)
+        XCTAssertEqual(refund.items[0].itemID, 1)
+        XCTAssertEqual(refund.items[1].itemID, 2)
+
+        XCTAssertEqual(refund.items[0].quantity, 1)
+        XCTAssertEqual(refund.items[1].quantity, 2)
+
+        XCTAssertEqual(refund.items[0].total, "5.1")
+        XCTAssertEqual(refund.items[1].total, "12.6")
+
+        XCTAssertEqual(refund.items[0].totalTax, "0.0")
+        XCTAssertEqual(refund.items[1].totalTax, "0.0")
+
+        XCTAssertEqual(refund.items[0].taxes, [])
+        XCTAssertEqual(refund.items[0].taxes, [])
+    }
+
+    func test_refund_order_items_values_are_transformed_with_taxes() {
+        // Given
+        let taxes = [
+            OrderItemTax(taxID: 11, subtotal: "", total: "0.60"),
+            OrderItemTax(taxID: 12, subtotal: "", total: "3.30")
+        ]
+        let items: [IssueRefundViewModel.RefundableOrderItem] = [
+            .init(item: MockOrderItem.sampleItem(itemID: 1, quantity: 3, price: 5.1, taxes: taxes, totalTax: "3.90"), quantity: 2)
+        ]
+        let useCase = RefundCreationUseCase(amount: "19.20",
+                                            reason: nil,
+                                            automaticallyRefundsPayment: false,
+                                            items: items,
+                                            currencyFormatter: formatter)
+
+        // When
+        let refund = useCase.createRefundObject()
+
+        // Then
+        XCTAssertEqual(refund.items.count, items.count)
+        XCTAssertEqual(refund.items[0].itemID, 1)
+        XCTAssertEqual(refund.items[0].quantity, 2)
+        XCTAssertEqual(refund.items[0].total, "10.20")
+        XCTAssertEqual(refund.items[0].totalTax, "2.60")
+        XCTAssertEqual(refund.items[0].taxes[0].taxID, 11)
+        XCTAssertEqual(refund.items[0].taxes[0].total, "0.40")
+        XCTAssertEqual(refund.items[0].taxes[1].taxID, 12)
+        XCTAssertEqual(refund.items[0].taxes[1].total, "2.20")
+    }
+}

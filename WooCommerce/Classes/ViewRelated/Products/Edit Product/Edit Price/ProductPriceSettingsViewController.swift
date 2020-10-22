da3601f3c47fdc1d066754d679bab45f31f81a6d
Paolo Musolino <info@codeido.com>
2020-10-22 13:38:41 +0200

    fix: updated how the DatePicker height is handled in ProductPriceSettingsViewController

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Edit Price/ProductPriceSettingsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Edit Price/ProductPriceSettingsViewController.swift
@@ -287,7 +287,7 @@ extension ProductPriceSettingsViewController: UITableViewDelegate {
         let row = rowAtIndexPath(indexPath)
 
         if row == .datePickerSaleFrom || row == .datePickerSaleTo {
-            return Constants.pickerRowHeight
+            return DatePickerTableViewCell.getDefaultCellHeight()
         }
 
         return UITableView.automaticDimension
@@ -444,7 +444,7 @@ private extension ProductPriceSettingsViewController {
 //
 extension ProductPriceSettingsViewController {
 
-    struct Section: Equatable {
+    struct Section: RowIterable, Equatable {
         let title: String?
         let rows: [Row]
     }
@@ -488,5 +488,4 @@ extension ProductPriceSettingsViewController {
 
 private struct Constants {
     static let sectionHeight = CGFloat(44)
-    static let pickerRowHeight = CGFloat(216)
 }

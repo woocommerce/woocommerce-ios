99c038c992a861bdb4783d17bb6cfcbb884d5176
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-10-22 16:02:43 -0500

    Update RefundStote with tests for stale refunds

--- Yosemite/YosemiteTests/Stores/RefundStoreTests.swift
+++ Yosemite/YosemiteTests/Stores/RefundStoreTests.swift
@@ -417,6 +417,30 @@ class RefundStoreTests: XCTestCase {
 
         wait(for: [backgroundSaveExpectation], timeout: Constants.expectationTimeout)
     }
+
+    func test_stale_refunds_are_deleted_when_retrieving_new_refunds() {
+        // Given
+        let refundStore = RefundStore(dispatcher: dispatcher, storageManager: storageManager, network: network)
+        refundStore.upsertStoredRefund(readOnlyRefund: self.sampleRefund(refundID: 999), in: self.viewStorage)
+        XCTAssertEqual(viewStorage.countObjects(ofType: Storage.Refund.self), 1)
+
+        network.simulateResponse(requestUrlSuffix: "refunds", filename: "refunds-all")
+
+        // When
+        var retrieveError: Error?
+        waitForExpectation { exp in
+            let action = RefundAction.retrieveRefunds(siteID: sampleSiteID, orderID: sampleOrderID, refundIDs: [sampleRefundID, refundID]) { error in
+                retrieveError = error
+                exp.fulfill()
+            }
+            refundStore.onAction(action)
+        }
+
+        // Then
+        let storedRefunds = viewStorage.loadRefunds(siteID: sampleSiteID, orderID: sampleOrderID).map { $0.toReadOnly() }
+        XCTAssertEqual(storedRefunds, [sampleRefund(), sampleRefund2()])
+        XCTAssertNil(retrieveError)
+    }
 }
 
 
@@ -426,10 +450,10 @@ private extension RefundStoreTests {
 
     /// Generate a sample Refund
     ///
-    func sampleRefund(_ siteID: Int64? = nil) -> Networking.Refund {
+    func sampleRefund(_ siteID: Int64? = nil, refundID: Int64? = nil) -> Networking.Refund {
         let testSiteID = siteID ?? sampleSiteID
         let testDate = date(with: "2019-10-09T16:18:23")
-        return Refund(refundID: sampleRefundID,
+        return Refund(refundID: refundID ?? sampleRefundID,
                       orderID: sampleOrderID,
                       siteID: testSiteID,
                       dateCreated: testDate,

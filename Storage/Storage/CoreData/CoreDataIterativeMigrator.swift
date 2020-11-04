a92b9a68ee56448e21aac6122fa11c3a6779e440
Shiki <jayson@basanes.net>
2020-11-04 10:03:02 -0700

    Migrator: Simplify models(for:)

--- Storage/Storage/CoreData/CoreDataIterativeMigrator.swift
+++ Storage/Storage/CoreData/CoreDataIterativeMigrator.swift
@@ -288,7 +288,7 @@ private extension CoreDataIterativeMigrator {
     }
 
     func models(for modelVersions: [ManagedObjectModelsInventory.ModelVersion]) throws -> [NSManagedObjectModel] {
-        let models = try modelVersions.map { version -> NSManagedObjectModel in
+        try modelVersions.map { version -> NSManagedObjectModel in
             guard let model = self.modelsInventory.model(for: version) else {
                 let description = "No model found for \(version.name)"
                 throw NSError(domain: "IterativeMigrator", code: 110, userInfo: [NSLocalizedDescriptionKey: description])
@@ -296,7 +296,5 @@ private extension CoreDataIterativeMigrator {
 
             return model
         }
-
-        return models
     }
 }

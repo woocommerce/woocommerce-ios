c34c4d8d657394ec70573d03c679a57e3a9869e6
Jaclyn Chen <watertranquil@gmail.com>
2020-11-04 13:03:43 +0800

    Prep for merge.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/PresenceThresholdFilterTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/PresenceThresholdFilterTests.swift
@@ -2,16 +2,21 @@ import XCTest
 @testable import WooCommerce
 
 final class PresenceThresholdFilterTests: XCTestCase {
-
-    func testExample() {
-        let sequence = PresenceThresholdFilter<[String]>(threshold: 0.5, outOf: 10)
-        XCTAssertEqual(sequence.append(value: ["hello"]), ["hello"])
-        XCTAssertEqual(sequence.append(value: ["hello"]), nil)
-        XCTAssertEqual(sequence.append(value: ["hey"]), ["hey"])
-        XCTAssertEqual(sequence.append(value: ["hello"]), nil)
-        XCTAssertEqual(sequence.append(value: ["hey"]), ["hey"])
-        XCTAssertEqual(sequence.append(value: ["hey"]), nil)
-        XCTAssertEqual(sequence.append(value: ["hello"]), nil)
+    func test_appending_a_sequence_of_values() {
+        let sequence = PresenceThresholdFilter<String>(threshold: 0.5, outOf: 6)
+        // ["hello"]
+        XCTAssertEqual(sequence.append(value: "hello"), "hello")
+        // ["hello", "hello"] repeat frequency: 1.0
+        XCTAssertEqual(sequence.append(value: "hello"), nil)
+        // ["hello", "hello", "hey"] repeat frequency: 0
+        XCTAssertEqual(sequence.append(value: "hey"), "hey")
+        // ["hello", "hello", "hey", "hello"] repeat frequency: 3/4 = 0.75
+        XCTAssertEqual(sequence.append(value: "hello"), nil)
+        // ["hello", "hello", "hey", "hello", "hey"] repeat frequency: 2/5 = 0.4
+        XCTAssertEqual(sequence.append(value: "hey"), "hey")
+        // ["hello", "hello", "hey", "hello", "hey", "hey"] repeat frequency: 3/6 = 0.5 (max)
+        XCTAssertEqual(sequence.append(value: "hey"), "hey")
+        // ["hello", "hey", "hello", "hey", "hey", "hello"] repeat frequency: 3/6 = 0.5 (max)
+        XCTAssertEqual(sequence.append(value: "hello"), "hello")
     }
-
 }

98fa53c122601a936293eca646fca77fd2da2790
Jaclyn Chen <watertranquil@gmail.com>
2020-11-04 09:00:45 +0800

    Add comments to `OrderItemAttribute` and why we skip attribute with name `_reduced_stock`.

--- Networking/Networking/Model/OrderItem.swift
+++ Networking/Networking/Model/OrderItem.swift
@@ -73,6 +73,7 @@ public struct OrderItem: Decodable, Hashable {
             name = try ((container.decodeIfPresent(String.self, forKey: .variationParentName))
                         ?? container.decode(String.self, forKey: .name)).strippedHTML
             let allAttributes = (try? container.decodeIfPresent([OrderItemAttribute].self, forKey: .attributes)) ?? []
+            // Skips any attribute if the name is `_reduced_stock` because this is a known attribute added by core.
             attributes = allAttributes.filter { $0.name != "_reduced_stock" }
 
             // Logs error for unexpected attributes.
--- Networking/Networking/Model/OrderItemAttribute.swift
+++ Networking/Networking/Model/OrderItemAttribute.swift
@@ -1,3 +1,5 @@
+/// Represents an attribute of an `OrderItem` in its `attributes` property.
+/// Currently, the use case is when an order item is a variation and the attributes are its variation attributes.
 public struct OrderItemAttribute: Decodable, Hashable, Equatable {
     public let metaID: Int64
     public let name: String

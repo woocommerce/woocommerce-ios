6e462865ed61dbe65e0de14519f1f7added62968
Shiki <jayson@basanes.net>
2020-11-04 19:27:13 -0700

    Require NSPersistentStoreCoordinator in CoreDataIterativeMigrator
    The coordinator will be used later to replace file operations.

--- Storage/Storage/CoreData/CoreDataIterativeMigrator.swift
+++ Storage/Storage/CoreData/CoreDataIterativeMigrator.swift
@@ -6,11 +6,21 @@ import CoreData
 ///
 final class CoreDataIterativeMigrator {
 
-    private let fileManager: FileManagerProtocol
-
+    /// The coordinator instance whose functions will be used for replacing the existing
+    /// store with the migrated one.
+    ///
+    /// The coordinator instance can be retrieved from `NSPersistentContainer.persistentStoreCoordinator`.
+    ///
+    private let persistentStoreCoordinator: NSPersistentStoreCoordinator
+    /// The model versions that will be used for migration.
     private let modelsInventory: ManagedObjectModelsInventory
 
-    init(modelsInventory: ManagedObjectModelsInventory, fileManager: FileManagerProtocol = FileManager.default) {
+    private let fileManager: FileManagerProtocol
+
+    init(coordinator: NSPersistentStoreCoordinator,
+         modelsInventory: ManagedObjectModelsInventory,
+         fileManager: FileManagerProtocol = FileManager.default) {
+        persistentStoreCoordinator = coordinator
         self.modelsInventory = modelsInventory
         self.fileManager = fileManager
     }
--- Storage/StorageTests/CoreData/CoreDataIterativeMigratorTests.swift
+++ Storage/StorageTests/CoreData/CoreDataIterativeMigratorTests.swift
@@ -64,18 +64,20 @@ final class CoreDataIterativeMigratorTests: XCTestCase {
             // Given
             let currentModel = try XCTUnwrap(modelsByVersionName[currentVersion.name])
 
+            let persistentContainer =
+                makePersistentContainer(storeURL: storeURL, storeType: storeType, model: currentModel)
+
             // When
             // Migrate to the currentVersion if this is not the first version in the list.
             if modelsInventory.versions.first != currentVersion {
-                let migrator = CoreDataIterativeMigrator(modelsInventory: modelsInventory)
+                let migrator = CoreDataIterativeMigrator(coordinator: persistentContainer.persistentStoreCoordinator,
+                                                         modelsInventory: modelsInventory)
                 let (isMigrationSuccessful, _) =
                     try migrator.iterativeMigrate(sourceStore: storeURL, storeType: storeType, to: currentModel)
                 XCTAssertTrue(isMigrationSuccessful)
             }
 
             // Load the persistent container
-            let persistentContainer =
-                makePersistentContainer(storeURL: storeURL, storeType: storeType, model: currentModel)
             let loadingError: Error? = try waitFor { promise in
                 persistentContainer.loadPersistentStores { _, error in
                     promise(error)
@@ -110,7 +112,10 @@ final class CoreDataIterativeMigratorTests: XCTestCase {
 
         fileManager.whenCheckingIfFileExists(atPath: databaseURL.path, thenReturn: false)
 
-        let migrator = CoreDataIterativeMigrator(modelsInventory: modelsInventory, fileManager: fileManager)
+        // Using a fake `NSPersistentStoreCoordinator` is apparently inconsequential.
+        let migrator = CoreDataIterativeMigrator(coordinator: NSPersistentStoreCoordinator(),
+                                                 modelsInventory: modelsInventory,
+                                                 fileManager: fileManager)
 
         // When
         let result = try migrator.iterativeMigrate(sourceStore: databaseURL,
@@ -168,7 +173,7 @@ final class CoreDataIterativeMigratorTests: XCTestCase {
 
         // When
         do {
-            let iterativeMigrator = CoreDataIterativeMigrator(modelsInventory: modelsInventory)
+            let iterativeMigrator = CoreDataIterativeMigrator(coordinator: psc, modelsInventory: modelsInventory)
             let (result, _) = try iterativeMigrator.iterativeMigrate(sourceStore: storeURL,
                                                                      storeType: NSSQLiteStoreType,
                                                                      to: model10)
@@ -192,11 +197,13 @@ final class CoreDataIterativeMigratorTests: XCTestCase {
         let storeFileName = "WooMigrationDeletionUnitTest.sqlite"
         let storeURL = try urlForStore(withName: storeFileName, deleteIfExists: true)
         // Start a container so the SQLite files will be created.
-        _ = try startPersistentContainer(storeURL: storeURL, storeType: storeType, model: sourceModel)
+        let container = try startPersistentContainer(storeURL: storeURL, storeType: storeType, model: sourceModel)
 
         let fileManager = FileManager()
         let spyFileManager = SpyFileManager(fileManager)
-        let iterativeMigrator = CoreDataIterativeMigrator(modelsInventory: modelsInventory, fileManager: spyFileManager)
+        let iterativeMigrator = CoreDataIterativeMigrator(coordinator: container.persistentStoreCoordinator,
+                                                          modelsInventory: modelsInventory,
+                                                          fileManager: spyFileManager)
 
         // Create a file (e.g. WooCommerce.sqlite.~) that shouldn't be included in the deletion.
         let legacyBackupFileURL = storeURL.appendingPathExtension("~")
@@ -233,10 +240,12 @@ final class CoreDataIterativeMigratorTests: XCTestCase {
         let storeFileName = "WooMigrationMoveUnitTest.sqlite"
         let storeURL = try urlForStore(withName: storeFileName, deleteIfExists: true)
         // Start a container so the SQLite files will be created.
-        _ = try startPersistentContainer(storeURL: storeURL, storeType: storeType, model: sourceModel)
+        let container = try startPersistentContainer(storeURL: storeURL, storeType: storeType, model: sourceModel)
 
         let spyFileManager = SpyFileManager()
-        let iterativeMigrator = CoreDataIterativeMigrator(modelsInventory: modelsInventory, fileManager: spyFileManager)
+        let iterativeMigrator = CoreDataIterativeMigrator(coordinator: container.persistentStoreCoordinator,
+                                                          modelsInventory: modelsInventory,
+                                                          fileManager: spyFileManager)
 
         // When
         let (result, _) = try iterativeMigrator.iterativeMigrate(sourceStore: storeURL,

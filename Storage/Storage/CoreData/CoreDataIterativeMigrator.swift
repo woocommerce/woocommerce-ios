66a46d1992fe51726e1d6d63f3afe78f603333ac
Shiki <jayson@basanes.net>
2020-11-04 20:38:11 -0700

    Migrator: Delete the last temporary store URL after completion

--- Storage/Storage/CoreData/CoreDataIterativeMigrator.swift
+++ Storage/Storage/CoreData/CoreDataIterativeMigrator.swift
@@ -110,6 +110,13 @@ final class CoreDataIterativeMigrator {
                                                                   withPersistentStoreFrom: lastTempDestinationURL,
                                                                   sourceOptions: nil,
                                                                   ofType: storeType)
+
+            // Final cleanuup. Destroy the store at `lastTempDestinationURL` since it should have
+            // been copied over to `sourceStore` above.
+            try persistentStoreCoordinator.destroyPersistentStore(at: lastTempDestinationURL,
+                                                                  ofType: storeType,
+                                                                  options: nil)
+
             return (true, debugMessages)
         } catch {
             let errorInfo = (error as NSError?)?.userInfo ?? [:]

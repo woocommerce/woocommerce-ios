e216f4c05bcac80f2261113be180350ee7c90ff5
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-11-08 16:45:51 -0500

    Move fakes.generated into Fakes package

---
+++ Fakes/Sources/Fakes/Fakes.generated.swift
@@ -0,0 +1,17 @@
+// Generated using Sourcery 0.18.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+import Networking
+
+extension OrderItemTax {
+    /// Returns a "ready to use" type filled with fake values.
+    ///
+    public static func fake() -> OrderItemTax {
+        OrderItemTax(
+            taxID: .fake(),
+            subtotal: .fake(),
+            total: .fake()
+        )
+    }
+}
+

fa5a5250a8d9123014638f3807bae58c49ec9046
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-11-10 15:22:15 -0500

    Add PaymentGateway+ReadOnlyConvertible.swift

--- Networking/Networking/Model/PaymentGateway.swift
+++ Networking/Networking/Model/PaymentGateway.swift
@@ -34,7 +34,16 @@ public struct PaymentGateway: Equatable {
 
     /// List of features the payment gateway supports.
     ///
-    public let features: [Feature]
+    public let features: [Features]
+
+    public init(siteID: Int64, gatewayID: String, title: String, description: String, enabled: Bool, features: [Features]) {
+        self.siteID = siteID
+        self.gatewayID = gatewayID
+        self.title = title
+        self.description = description
+        self.enabled = enabled
+        self.features = features
+    }
 }
 
 // MARK: Gateway Decodable

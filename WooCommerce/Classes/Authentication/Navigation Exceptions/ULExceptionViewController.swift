1e9d820208bff9775e544355439f707dfbcef4f6
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2020-11-23 10:59:16 +0800

    Add a placeholder view controller, to be inserted into WPAuthenticator's navigation flow, just to test the idea of relying on navigation commands provided by WPAuthenticator

---
+++ WooCommerce/Classes/Authentication/Navigation Exceptions/ULExceptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ULExceptionViewController.swift
+//  WooCommerce
+//
+//  Created by Cesar Tardaguila on 23/11/2020.
+//  Copyright © 2020 Automattic. All rights reserved.
+//
+
+import UIKit
+
+class ULExceptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

0e55d6819d4ee4393be40f217983b4255a4a78cf
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2020-11-23 12:30:04 +0800

    Implement a first draft of navigation

--- WordPressAuthenticator/WordPressAuthenticator/Navigation/NavigateToEnterAccount.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Navigation/NavigateToEnterAccount.swift
@@ -4,5 +4,20 @@ public struct NavigateToEnterAccount: NavigationCommand {
     public init() {}
     public func execute(with: UINavigationController?) {
         print("Off we go to Enter a New WordPress.com account")
+        continueWithDotCom(navigationController: with)
+    }
+}
+
+
+private extension NavigateToEnterAccount {
+    /// Unified "Continue with WordPress.com" prologue button action.
+    ///
+    private func continueWithDotCom(navigationController: UINavigationController?) {
+        guard let vc = GetStartedViewController.instantiate(from: .getStarted) else {
+            DDLogError("Failed to navigate from LoginPrologueViewController to GetStartedViewController")
+            return
+        }
+
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
--- WordPressAuthenticator/WordPressAuthenticator/Navigation/NavigateToEnterSite.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Navigation/NavigateToEnterSite.swift
@@ -5,5 +5,20 @@ public struct NavigateToEnterSite: NavigationCommand {
     public init() {}
     public func execute(with: UINavigationController?) {
         print("Off we go to Enter a New Site Address")
+
+        presentUnifiedSiteAddressView(navigationController: with)
+    }
+}
+
+private extension NavigateToEnterSite {
+    /// Navigates to the unified site address login flow.
+    ///
+    func presentUnifiedSiteAddressView(navigationController: UINavigationController?) {
+        guard let vc = SiteAddressViewController.instantiate(from: .siteAddress) else {
+            DDLogError("Failed to navigate from LoginViewController to SiteAddressViewController")
+            return
+        }
+
+        navigationController?.pushViewController(vc, animated: true)
     }
 }

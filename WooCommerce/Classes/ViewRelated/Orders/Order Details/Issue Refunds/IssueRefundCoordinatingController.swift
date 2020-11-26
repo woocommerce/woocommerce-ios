6ece4b6c07ea95daf843d5bbf3708aa2a75bca18
Ernesto Carrión <ernesto.carrion@automattic.com>
2020-11-26 11:46:59 -0500

    Adds nextAction and selectItem action

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/IssueRefundCoordinatingController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/IssueRefundCoordinatingController.swift
@@ -5,7 +5,7 @@ import Yosemite
 ///
 final class IssueRefundCoordinatingController: WooNavigationController {
 
-    /// Order to be refunded
+    /// Order to be refunded.
     ///
     private let order: Order
 
@@ -32,6 +32,34 @@ private extension IssueRefundCoordinatingController {
     ///
     func startRefundNavigation() {
         let issueRefundViewController = IssueRefundViewController(order: order, refunds: refunds)
+        issueRefundViewController.onSelectQuantityAction = { [weak self] command in
+            self?.navigateToItemQuantitySelection(using: command)
+
+        }
+        issueRefundViewController.onNextAction = { [weak self] viewModel in
+            self?.navigateToRefundConfirmation(with: viewModel)
+        }
+
         setViewControllers([issueRefundViewController], animated: false)
+    }
+
+    /// Navigates to `ListSelectorViewController` with the provided command.
+    ///
+    func navigateToItemQuantitySelection(using command: RefundItemQuantityListSelectorCommand) {
+        let selectorViewController = ListSelectorViewController(command: command, tableViewStyle: .plain) { [weak self] selectedQuantity in
+            guard let selectedQuantity = selectedQuantity else {
+                return
+            }
+            // TODO: Handle Selection
+//            self?.viewModel.updateRefundQuantity(quantity: selectedQuantity, forItemAtIndex: indexPath.row)
+        }
+        show(selectorViewController, sender: nil)
+    }
+
+    /// Navigates to `RefundConfirmationViewController` with the provided view model.
+    ///
+    func navigateToRefundConfirmation(with viewModel: RefundConfirmationViewModel) {
+        let refundConfirmationViewController = RefundConfirmationViewController(viewModel: viewModel)
+        show(refundConfirmationViewController, sender: nil)
     }
 }
--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/IssueRefundViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/IssueRefundViewController.swift
@@ -17,6 +17,14 @@ final class IssueRefundViewController: UIViewController {
 
     private let viewModel: IssueRefundViewModel
 
+    /// Closure invoked when the next button is tapped
+    ///
+    var onNextAction: ((RefundConfirmationViewModel) -> Void)?
+
+    /// Closure invoked when the the quantity button of an item is pressed
+    ///
+    var onSelectQuantityAction: ((RefundItemQuantityListSelectorCommand) -> Void)?
+
     init(order: Order,
          refunds: [Refund],
          currencySettings: CurrencySettings = ServiceLocator.currencySettings,
@@ -65,7 +73,7 @@ private extension IssueRefundViewController {
 private extension IssueRefundViewController {
     @IBAction func nextButtonWasPressed(_ sender: Any) {
         let confirmationViewModel = viewModel.createRefundConfirmationViewModel()
-        show(RefundConfirmationViewController(viewModel: confirmationViewModel), sender: self)
+        onNextAction?(confirmationViewModel)
 
         viewModel.trackNextButtonTapped()
     }
@@ -86,13 +94,8 @@ private extension IssueRefundViewController {
         }
 
         let command = RefundItemQuantityListSelectorCommand(maxRefundQuantity: refundQuantity, currentQuantity: currentQuantity)
-        let selectorViewController = ListSelectorViewController(command: command, tableViewStyle: .plain) { [weak self] selectedQuantity in
-            guard let selectedQuantity = selectedQuantity else {
-                return
-            }
-            self?.viewModel.updateRefundQuantity(quantity: selectedQuantity, forItemAtIndex: indexPath.row)
-        }
-        show(selectorViewController, sender: nil)
+        onSelectQuantityAction?(command)
+
         viewModel.trackQuantityButtonTapped()
     }
 }

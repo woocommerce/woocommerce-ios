52ac0baf92918f27c19afa509d5efcfdef4c80d1
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2020-11-26 08:16:18 +0800

    Remove the first attempt at generalising the error view

--- WooCommerce/Classes/Authentication/Navigation Exceptions/NavigationExceptionContext.swift
+++
@@ -1,8 +0,0 @@
-import WordPressAuthenticator
-
-struct NavigationExceptionContext {
-    let primaryButtontitle: String
-    let secondaryButtonTitle: String
-    let primaryButtonAction: NavigationCommand
-    let secondaryButtonAction: NavigationCommand
-}

0d02cb04f105cba317ffd9dcd60da9a6de0c6c27
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2020-11-27 11:59:05 +0800

    Declare icon for error screen. Add tests

--- WooCommerce/Classes/Extensions/UIImage+Woo.swift
+++ WooCommerce/Classes/Extensions/UIImage+Woo.swift
@@ -297,10 +297,16 @@ extension UIImage {
         return UIImage(named: "login-site-address-info")!
     }
 
-    /// Login error: no jetpack
+    /// Login error: no Jetpack
     ///
     static var loginNoJetpackError: UIImage {
         return UIImage(named: "woo-no-jetpack-error")!
+    }
+
+    /// Login error: no WordPress
+    ///
+    static var loginNoWordPressError: UIImage {
+        return UIImage(named: "woo-wp-no-site")!
     }
 
     /// Mail Icon

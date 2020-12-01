52ce1aa9d2eaebbecbc3943073188309e557e80a
Jeremy Massel <jeremy.massel@automattic.com>
2020-12-01 13:39:34 -0700

    Allow mocking the network layer with a launch arg

--- WooCommerce/Classes/Yosemite/DefaultStoresManager.swift
+++ WooCommerce/Classes/Yosemite/DefaultStoresManager.swift
@@ -71,7 +71,14 @@ class DefaultStoresManager: StoresManager {
     ///
     init(sessionManager: SessionManagerProtocol) {
         _sessionManager = sessionManager
-        self.state = AuthenticatedState(sessionManager: sessionManager) ?? DeauthenticatedState()
+
+        if ProcessInfo.processInfo.arguments.contains("mocked-network-layer") {
+            let screenshotObjectGraph = ScreenshotObjects()
+            self.state = MockAuthenticatedState(objectGraph: screenshotObjectGraph)
+        }
+        else {
+            self.state = AuthenticatedState(sessionManager: sessionManager) ?? DeauthenticatedState()
+        }
 
         isLoggedInSubject.send(isAuthenticated)
 

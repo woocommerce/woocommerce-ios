1eaf4fac75ba52326ba84d8be908888acb919362
Shiki <jayson@basanes.net>
2020-12-02 10:14:16 -0700

    Add more comments for Migrator.modelsToMigrate()

--- Storage/Storage/CoreData/CoreDataIterativeMigrator.swift
+++ Storage/Storage/CoreData/CoreDataIterativeMigrator.swift
@@ -251,6 +251,30 @@ private extension CoreDataIterativeMigrator {
     }
 
     /// Returns an inclusive list of models between the source and target models.
+    ///
+    /// For example, if `sourceModel` is `"Model 13"` and `targetModel` is `"Model 16"`, then this
+    /// will return this list of `NSManagedObjectModels` in order:
+    ///
+    /// - Model 13
+    /// - Model 14
+    /// - Model 15
+    /// - Model 16
+    ///
+    /// This also works if the `targetModel` is lower than the `sourceModel`. For example, if the
+    /// `sourceModel` is `"Model 16"` and `targetModel` is `"Model 13"`, then this list will
+    /// be returned:
+    ///
+    /// - Model 16
+    /// - Model 15
+    /// - Model 14
+    /// - Model 13
+    ///
+    /// We don't really use the descending list at the moment. I'm just keeping this logic
+    /// as is for now so I don't accidentally introduce regressions. Somebody, one brave soul
+    /// will refactor this and remove the descending logic.
+    ///
+    /// - Returns: The list of models to be used for migration, including the `sourceModel` and
+    ///            the `targetModel`.
     func modelsToMigrate(from sourceModel: NSManagedObjectModel,
                          to targetModel: NSManagedObjectModel) throws -> [NSManagedObjectModel] {
         // Get NSManagedObjectModels for each of the model names given.

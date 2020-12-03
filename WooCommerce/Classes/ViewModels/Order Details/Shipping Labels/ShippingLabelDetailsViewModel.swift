656c61251a7bb90191f9189c9187c3bc5fad1b5e
Jaclyn Chen <watertranquil@gmail.com>
2020-12-03 10:20:28 +0800

    ShippingLabelDetailsViewController: show name and formatted address in origin and destination address rows.

--- WooCommerce/Classes/ViewModels/Order Details/Shipping Labels/ShippingLabelDetailsViewModel.swift
+++ WooCommerce/Classes/ViewModels/Order Details/Shipping Labels/ShippingLabelDetailsViewModel.swift
@@ -1,5 +1,7 @@
+import Contacts
 import Foundation
 import struct Yosemite.ShippingLabel
+import struct Yosemite.ShippingLabelAddress
 
 /// Provides UI display data for `ShippingLabelDetailsViewController`.
 struct ShippingLabelDetailsViewModel {
@@ -10,8 +12,8 @@ struct ShippingLabelDetailsViewModel {
     let paymentMethod: String
 
     init(shippingLabel: ShippingLabel, currencyFormatter: CurrencyFormatter) {
-        self.originAddress = shippingLabel.originAddress.formattedPostalAddress ?? ""
-        self.destinationAddress = shippingLabel.destinationAddress.formattedPostalAddress ?? ""
+        self.originAddress = shippingLabel.originAddress.nameAndFormattedAddress
+        self.destinationAddress = shippingLabel.destinationAddress.nameAndFormattedAddress
         self.packageName = shippingLabel.packageName
         self.carrierAndRate = {
             let serviceName = shippingLabel.serviceName
@@ -25,5 +27,27 @@ struct ShippingLabelDetailsViewModel {
 private extension ShippingLabelDetailsViewModel {
     enum Localization {
         static let creditCardPaymentMethod = NSLocalizedString("Credit card", comment: "Credit card payment method for shipping label.")
+    }
+}
+
+private extension ShippingLabelAddress {
+    var nameAndFormattedAddress: String {
+        [name, formattedAddress].compactMap { $0 }.joined(separator: "\n")
+    }
+
+    /// Returns the postal address, formatted and ready for display.
+    var formattedAddress: String? {
+        return postalAddress.formatted(as: .mailingAddress)
+    }
+
+    var postalAddress: CNPostalAddress {
+        let address = CNMutablePostalAddress()
+        address.street = [address1, address2].joined(separator: " ")
+        address.city = city
+        address.state = state
+        address.postalCode = postcode
+        address.country = country
+        address.isoCountryCode = country
+        return address
     }
 }

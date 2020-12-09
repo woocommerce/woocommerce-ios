35f160cd8bcfced95f4a4e40ff7202925e261ed2
Paolo Musolino <info@codeido.com>
2020-12-09 16:54:00 +0100

    feat: delete product attribute tests

--- Yosemite/YosemiteTests/Stores/ProductAttributeStoreTests.swift
+++ Yosemite/YosemiteTests/Stores/ProductAttributeStoreTests.swift
@@ -261,120 +261,70 @@ final class ProductAttributeStoreTests: XCTestCase {
         XCTAssertEqual(storedProductAttributesCount, 0)
     }
 
+    func test_delete_product_attribute_stored_attribute_upon_successful_response() throws {
+        // Given
+        let initialAttribute = sampleProductAttribute(attributeID: 1)
+        storageManager.insertSampleProductAttribute(readOnlyProductAttribute: initialAttribute)
+        network.simulateResponse(requestUrlSuffix: "products/attributes/1", filename: "product-attribute-delete")
+        XCTAssertEqual(storedProductAttributesCount, 1)
 
-//    func testAddProductTagReturnsErrorUponEmptyResponse() {
-//        // Given an empty network response
-//        XCTAssertEqual(storedProductTagsCount, 0)
-//
-//        // When dispatching a `addProductTags` action
-//        var result: Result<[Networking.ProductTag], Error>?
-//        waitForExpectation { (exp) in
-//            let action = ProductTagAction.addProductTags(siteID: sampleSiteID, tags: ["Round toe", "Flat"]) { (aResult) in
-//                result = aResult
-//                exp.fulfill()
-//            }
-//            store.onAction(action)
-//        }
-//
-//        // Then no tags should be stored
-//        XCTAssertEqual(storedProductTagsCount, 0)
-//        XCTAssertNotNil(result?.failure)
-//    }
-//
-//    func testSynchronizeProductTagsDeletesUnusedTags() {
-//        // Given some stored product tags without product relationships
-//        let sampleTags = (1...5).map { id in
-//            return sampleTag(tagID: id)
-//        }
-//        sampleTags.forEach { tag in
-//            storageManager.insertSampleProductTag(readOnlyProductTag: tag)
-//        }
-//        XCTAssertEqual(storedProductTagsCount, sampleTags.count)
-//
-//        // When dispatching a `synchronizeAllProductTags` action
-//        network.simulateResponse(requestUrlSuffix: "products/tags", filename: "product-tags-all")
-//        network.simulateResponse(requestUrlSuffix: "products/tags", filename: "product-tags-empty")
-//
-//        var errorResponse: ProductTagActionError?
-//        waitForExpectation { (exp) in
-//            let action = ProductTagAction.synchronizeAllProductTags(siteID: sampleSiteID) { error in
-//                errorResponse = error
-//                exp.fulfill()
-//            }
-//            store.onAction(action)
-//        }
-//
-//        // Then new tag should be stored and old tags should be deleted
-//        XCTAssertEqual(storedProductTagsCount, 4)
-//        XCTAssertNil(errorResponse)
-//    }
-//
-//    func testDeleteProductTagDeleteStoredTagSuccessfulResponse() {
-//        // Given a stubed product tag network response and a product tag stored locally
-//        network.simulateResponse(requestUrlSuffix: "products/tags/batch", filename: "product-tags-deleted")
-//        storageManager.insertSampleProductTag(readOnlyProductTag: sampleTag(tagID: 35))
-//
-//        XCTAssertEqual(storedProductTagsCount, 1)
-//
-//        // When dispatching a `deleteProductTags` action
-//        var result: Result<[Yosemite.ProductTag], Error>?
-//        waitForExpectation { (exp) in
-//            let action = ProductTagAction.deleteProductTags(siteID: sampleSiteID, ids: [35]) { (aResult) in
-//                result = aResult
-//                exp.fulfill()
-//            }
-//            store.onAction(action)
-//        }
-//
-//        // Then the tag should be removed
-//        XCTAssertEqual(storedProductTagsCount, 0)
-//        XCTAssertNil(result?.failure)
-//    }
-//
-//    func testDeleteProductTagReturnsErrorUponResponseError() {
-//        // Given a stubed generic-error network response
-//        network.simulateResponse(requestUrlSuffix: "products/tags/batch", filename: "generic_error")
-//        storageManager.insertSampleProductTag(readOnlyProductTag: sampleTag(tagID: 35))
-//        XCTAssertEqual(storedProductTagsCount, 1)
-//
-//        // When dispatching a `deleteProductTags` action
-//        var result: Result<[Yosemite.ProductTag], Error>?
-//        waitForExpectation { (exp) in
-//            let action = ProductTagAction.deleteProductTags(siteID: sampleSiteID, ids: [35]) { (aResult) in
-//                result = aResult
-//                exp.fulfill()
-//            }
-//            store.onAction(action)
-//        }
-//
-//        // Then one tags should be continue to be stored
-//        XCTAssertEqual(storedProductTagsCount, 1)
-//        XCTAssertNotNil(result?.failure)
-//    }
-//
-//    func testDeleteProductTagReturnsErrorUponEmptyResponse() {
-//        // Given an empty network response
-//        storageManager.insertSampleProductTag(readOnlyProductTag: sampleTag(tagID: 35))
-//        XCTAssertEqual(storedProductTagsCount, 1)
-//
-//        // When dispatching a `deleteProductTags` action
-//        var result: Result<[Yosemite.ProductTag], Error>?
-//        waitForExpectation { (exp) in
-//            let action = ProductTagAction.deleteProductTags(siteID: sampleSiteID, ids: [35]) { (aResult) in
-//                result = aResult
-//                exp.fulfill()
-//            }
-//            store.onAction(action)
-//        }
-//
-//        // Then one tags should be continue to be stored
-//        XCTAssertEqual(storedProductTagsCount, 1)
-//        XCTAssertNotNil(result?.failure)
-//    }
+        // When
+        let result: Result<Networking.ProductAttribute, Error> = try waitFor { promise in
+            let action = ProductAttributeAction.deleteProductAttribute(siteID: self.sampleSiteID, productAttributeID: 1) { result in
+                promise(result)
+            }
+            self.store.onAction(action)
+        }
+
+        // Then
+        XCTAssertTrue(result.isSuccess)
+        XCTAssertEqual(storedProductAttributesCount, 0)
+    }
+
+    func test_delete_product_attribute_returns_error_upon_response_error() throws {
+        // Given
+        network.simulateResponse(requestUrlSuffix: "products/attributes/1", filename: "generic_error")
+        XCTAssertEqual(storedProductAttributesCount, 0)
+
+        // When
+        let result: Result<Networking.ProductAttribute, Error> = try waitFor { promise in
+            let action = ProductAttributeAction.deleteProductAttribute(siteID: self.sampleSiteID, productAttributeID: 1) { result in
+                promise(result)
+            }
+            self.store.onAction(action)
+        }
+
+        // Then
+        XCTAssertTrue(result.isFailure)
+        XCTAssertEqual(storedProductAttributesCount, 0)
+    }
+
+    func test_delete_product_attribute_returns_error_upon_empty_response() throws {
+        // Given
+        XCTAssertEqual(storedProductAttributesCount, 0)
+
+        // When
+        let result: Result<Networking.ProductAttribute, Error> = try waitFor { promise in
+            let action = ProductAttributeAction.deleteProductAttribute(siteID: self.sampleSiteID, productAttributeID: 1) { result in
+                promise(result)
+            }
+            self.store.onAction(action)
+        }
+
+        // Then
+        XCTAssertTrue(result.isFailure)
+        XCTAssertEqual(storedProductAttributesCount, 0)
+    }
 }
 
 private extension ProductAttributeStoreTests {
     func sampleProductAttribute(attributeID: Int64) -> Networking.ProductAttribute {
-        return Networking.ProductAttribute(siteID: sampleSiteID, attributeID: attributeID, name: "Sample", position: 0, visible: false, variation: false, options: [])
+        return Networking.ProductAttribute(siteID: sampleSiteID,
+                                           attributeID: attributeID,
+                                           name: "Sample",
+                                           position: 0,
+                                           visible: false,
+                                           variation: false,
+                                           options: [])
     }
 }

26021b4ee81027083160a1349e7712f9297ba342
Jaclyn Chen <watertranquil@gmail.com>
2020-12-10 18:05:25 +0800

    Create `ReprintShippingLabelViewController` that has a top `UIScrollView` but it turns out to be hard for the paper size row.

---
+++ WooCommerce/Classes/ViewModels/Order Details/Shipping Labels/ReprintShippingLabelViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ReprintShippingLabelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}

702b73f79b3d9ce6b40bc8e7c8bf7d68d7e2fc5b
Jeremy Massel <jeremy.massel@automattic.com>
2020-12-11 10:04:19 -0700

    Fix broken order merging

--- Yosemite/Yosemite/Model/Mocks/ActionHandlers/MockOrderActionHandler.swift
+++ Yosemite/Yosemite/Model/Mocks/ActionHandlers/MockOrderActionHandler.swift
@@ -10,26 +10,39 @@ struct MockOrderActionHandler: MockActionHandler {
 
     func handle(action: ActionType) {
         switch action {
-            case .fetchFilteredAndAllOrders(let siteId, _, _, _, _, let onCompletion):
-                saveOrders(siteId: siteId, onCompletion: onCompletion)
+            case .fetchFilteredAndAllOrders(let siteID, _, _, _, _, let onCompletion):
+                fetchFilteredAndAllOrders(siteID: siteID, onCompletion: onCompletion)
             case .countProcessingOrders(let siteID, let onCompletion):
-                countOrders(status: .processing, siteId: siteID, onCompletion: onCompletion)
+                countOrders(status: .processing, siteID: siteID, onCompletion: onCompletion)
 
             default: unimplementedAction(action: action)
         }
     }
 
-    private func countOrders(status: OrderStatusEnum, siteId: Int64, onCompletion: (OrderCount?, Error?) -> Void) {
-        let count = objectGraph.orders(withStatus: status, forSiteId: siteId).count
+    func fetchFilteredAndAllOrders(siteID: Int64, onCompletion: @escaping (Error?) -> ()) {
+        saveOrders(orders: objectGraph.orders(forSiteId: siteID)) {
+            onCompletion(nil)
+        }
+    }
 
-        onCompletion(OrderCount(siteID: siteId, items: [
+    func countOrders(status: OrderStatusEnum, siteID: Int64, onCompletion: (OrderCount?, Error?) -> Void) {
+        let count = objectGraph.orders(withStatus: status, forSiteId: siteID).count
+
+        onCompletion(OrderCount(siteID: siteID, items: [
             OrderCountItem(slug: "", name: "", total: count)
         ]), nil)
     }
 
-    private func saveOrders(siteId: Int64, onCompletion: @escaping (Error?) -> ()) {
-        let updater = OrdersUpsertUseCase(storage: storageManager.newDerivedStorage())
-        updater.upsert(objectGraph.orders(forSiteId: siteId))
-        onCompletion(nil)
+    private func saveOrders(orders: [Order], onCompletion: @escaping () -> ()) {
+        let storage = storageManager.newDerivedStorage()
+
+        storage.perform {
+            let updater = OrdersUpsertUseCase(storage: storage)
+            updater.upsert(orders)
+        }
+
+        storageManager.saveDerivedType(derivedStorage: storage) {
+            DispatchQueue.main.async(execute: onCompletion)
+        }
     }
 }

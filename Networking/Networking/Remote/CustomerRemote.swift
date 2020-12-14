1437120b221fff4ad0489a4d5aa5997706298d5f
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2020-12-14 12:14:27 +0300

    Add optional parameters for getAllCustomers networking remote endpoint

--- Networking/Networking/Remote/CustomerRemote.swift
+++ Networking/Networking/Remote/CustomerRemote.swift
@@ -8,12 +8,25 @@ public class CustomerRemote: Remote {
     ///
     /// - Parameters:
     ///     - siteID: Site for which we'll fetch remote customers.
+    ///     - context: view or edit. Scope under which the request is made;
+    ///                determines fields present in response. Default is view.
+    ///     - pageNumber: Number of page that should be retrieved.
+    ///     - pageSize: Number of Customers to be retrieved per page.
     ///     - completion: Closure to be executed upon completion.
     ///
     public func getAllCustomers(for siteID: Int64,
+                                context: ContextType = Default.context,
+                                pageNumber: Int = Default.pageNumber,
+                                pageSize: Int = Default.pageSize,
                                 completion: @escaping (Result<[Customer], Error>) -> Void) {
+        let parameters = [
+            ParameterKey.context: context.rawValue,
+            ParameterKey.page: String(pageNumber),
+            ParameterKey.perPage: String(pageSize)
+        ]
+
         let path = Path.customers
-        let request = JetpackRequest(wooApiVersion: .mark3, method: .get, siteID: siteID, path: path)
+        let request = JetpackRequest(wooApiVersion: .mark3, method: .get, siteID: siteID, path: path, parameters: parameters)
         let mapper = CustomersListMapper(siteID: siteID)
         enqueue(request, mapper: mapper, completion: completion)
     }
@@ -22,9 +35,26 @@ public class CustomerRemote: Remote {
 
 // MARK: - Constants
 //
-private extension CustomerRemote {
+public extension CustomerRemote {
+
+    enum ContextType: String {
+        case view
+        case edit
+    }
+
+    enum Default {
+        public static let context: ContextType = .view
+        public static let pageSize: Int        = 25
+        public static let pageNumber: Int      = Remote.Default.firstPageNumber
+    }
 
     private enum Path {
         static let customers = "customers"
+    }
+
+    private enum ParameterKey {
+        static let context: String = "context"
+        static let page: String    = "page"
+        static let perPage: String = "per_page"
     }
 }

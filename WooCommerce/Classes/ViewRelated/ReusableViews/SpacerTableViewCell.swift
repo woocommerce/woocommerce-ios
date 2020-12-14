ed764cb8d3ed313ec9cb88d702828211b6294a8f
Jaclyn Chen <watertranquil@gmail.com>
2020-12-14 11:00:17 +0800

    Create `SpacerTableViewCell` for vertical spacing between cells.

---
+++ WooCommerce/Classes/ViewRelated/ReusableViews/SpacerTableViewCell.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+/// Used for variable vertical spacing between two table view cells.
+/// The cell currently has default cell background color (`.listForeground`).
+final class SpacerTableViewCell: UITableViewCell {
+    /// The height of the spacer cell.
+    var height: CGFloat = 0 {
+        didSet {
+            heightConstraint.constant = height
+        }
+    }
+
+    @IBOutlet private weak var heightConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var view: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        applyDefaultBackgroundStyle()
+        hideSeparator()
+        view.backgroundColor = .clear
+    }
+}

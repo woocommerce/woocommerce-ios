68385b5d5928228f27e56bbe660f2e84c7c622c9
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2020-12-15 14:00:23 +0800

    Add a test to login with wordpress.com

--- WooCommerce/WooCommerceUITests/Screens/Login/LoginSiteAddressScreen.swift
+++ WooCommerce/WooCommerceUITests/Screens/Login/LoginSiteAddressScreen.swift
@@ -37,14 +37,6 @@ class LoginSiteAddressScreen: BaseScreen {
         return LoginUsernamePasswordScreen()
     }
 
-//    func proceedWithWP(siteUrl: String) -> GetStartedScreen {
-//        siteAddressTextField.tap()
-//        siteAddressTextField.typeText(siteUrl)
-//        nextButton.tap()
-//
-//        return GetStartedScreen()
-//    }
-
     static func isLoaded() -> Bool {
         return XCUIApplication().buttons[ElementStringIDs.nextButton].exists
     }
--- WooCommerce/WooCommerceUITests/Tests/LoginTests.swift
+++ WooCommerce/WooCommerceUITests/Tests/LoginTests.swift
@@ -11,93 +11,12 @@ class LoginTests: XCTestCase {
         app.launch()
     }
 
-//    func testEmailPasswordLoginLogout() {
-//        // Log in with email and password
-//        WelcomeScreen()
-//        .selectLogin()
-//        .proceedWith(email: TestCredentials.emailAddress)
-//        .proceedWithPassword()
-//        .proceedWith(password: TestCredentials.password)
-//
-//        // Login epilogue
-//        .verifyEpilogueDisplays(displayName: TestCredentials.displayName, siteUrl: TestCredentials.siteUrl)
-//        .continueWithSelectedSite()
-//
-//        // Log out
-//        .openSettingsPane()
-//        .verifySelectedStoreDisplays(siteUrl: TestCredentials.siteUrl, displayName: TestCredentials.displayName)
-//        .logOut()
-//
-//        XCTAssert(WelcomeScreen.isLoaded())
-//    }
-
-
-    //Modified
-//    func testEmailPasswordLoginLogout() {
-//        // Log in with email and password
-//        PrologueScreen()
-//        .selectLogin()
-//        .proceedWith(email: TestCredentials.emailAddress)
-//        .proceedWithPassword()
-//        .proceedWith(password: TestCredentials.password)
-//
-//        // Login epilogue
-//        .verifyEpilogueDisplays(displayName: TestCredentials.displayName, siteUrl: TestCredentials.siteUrl)
-//        .continueWithSelectedSite()
-//
-//        // Log out
-//        .openSettingsPane()
-//        .verifySelectedStoreDisplays(siteUrl: TestCredentials.siteUrl, displayName: TestCredentials.displayName)
-//        .logOut()
-//
-//        XCTAssert(WelcomeScreen.isLoaded())
-//    }
-
-    func testEmailPasswordLoginLogout() {
-        let prologue = PrologueScreen().selectSiteAddress()
-            .proceedWith(siteUrl: TestCredentials.siteUrl)
-            .proceedWith(username: TestCredentials.emailAddress, password: TestCredentials.password)
-            .verifyEpilogueDisplays(displayName: TestCredentials.displayName, siteUrl: TestCredentials.siteUrl)
-            .continueWithSelectedSite()
-
-            // Log out
-            .openSettingsPane()
-            .verifySelectedStoreDisplays(siteUrl: TestCredentials.siteUrl, displayName: TestCredentials.displayName)
-            .logOut()
-
-        XCTAssert(prologue.isLoaded())
-    }
-
-    /**
-    // Unified WordPress.com login/out
-    // Replaces testWpcomUsernamePasswordLogin
-    func testWpcomLogin() {
-        let prologue = PrologueScreen().selectSiteAddress()
-            .proceedWithWP(siteUrl: TestCredentials.siteUrl)
-            //.proceedWith(username: TestCredentials.emailAddress, password: TestCredentials.password)
-            .proceedWith(email: TestCredentials.emailAddress)
-            .proceedWith(password: TestCredentials.password)
-            .verifyEpilogueDisplays(displayName: TestCredentials.displayName, siteUrl: TestCredentials.siteUrl)
-            .continueWithSelectedSite()
-            //.dismissNotificationAlertIfNeeded()
-
-        //XCTAssert(MySiteScreen().isLoaded())
-
-            // Log out
-            .openSettingsPane()
-            .verifySelectedStoreDisplays(siteUrl: TestCredentials.siteUrl, displayName: TestCredentials.displayName)
-            .logOut()
-
-        XCTAssert(prologue.isLoaded())
-    }
-    */
-
-    func testWPComLoginLogout() {
+    func testWordPressLoginLogout() {
         let prologue = PrologueScreen().selectContinue()
             .proceedWith(email: TestCredentials.emailAddress)
             .proceedWith(password: TestCredentials.password)
+            .verifyEpilogueDisplays(displayName: TestCredentials.displayName, siteUrl: TestCredentials.siteUrl)
             .continueWithSelectedSite()
-            //.dismissNotificationAlertIfNeeded()
 
             // Log out
             .openSettingsPane()

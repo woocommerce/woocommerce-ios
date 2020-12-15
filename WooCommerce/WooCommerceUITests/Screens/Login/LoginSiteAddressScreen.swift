95a203bce5f7524c689133b17e0af1c467aa67ce
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2020-12-15 11:47:24 +0800

    Current implementation

--- WooCommerce/WooCommerceUITests/Screens/Login/LoginSiteAddressScreen.swift
+++ WooCommerce/WooCommerceUITests/Screens/Login/LoginSiteAddressScreen.swift
@@ -3,14 +3,22 @@ import XCTest
 
 private struct ElementStringIDs {
     static let navBar = "WordPressAuthenticator.LoginSiteAddressView"
-    static let siteAddressTextField = "usernameField"
     static let nextButton = "Site Address Next Button"
+
+    // TODO: clean up comments when unifiedSiteAddress is permanently enabled.
+
+    // For original Site Address. This matches accessibilityIdentifier in Login.storyboard.
+    // Leaving here for now in case unifiedSiteAddress is disabled.
+    // static let siteAddressTextField = "usernameField"
+
+    // For unified Site Address. This matches TextFieldTableViewCell.accessibilityIdentifier.
+    static let siteAddressTextField = "Site address"
 }
 
-final class LoginSiteAddressScreen: BaseScreen {
-    private let navBar: XCUIElement
-    private let siteAddressTextField: XCUIElement
-    private let nextButton: XCUIElement
+class LoginSiteAddressScreen: BaseScreen {
+    let navBar: XCUIElement
+    let siteAddressTextField: XCUIElement
+    let nextButton: XCUIElement
 
     init() {
         let app = XCUIApplication()
@@ -21,12 +29,20 @@ final class LoginSiteAddressScreen: BaseScreen {
         super.init(element: siteAddressTextField)
     }
 
-    func proceedWith(siteUrl: String) -> LoginUsernamePasswordScreen {
+//    func proceedWith(siteUrl: String) -> LoginUsernamePasswordScreen {
+//        siteAddressTextField.tap()
+//        siteAddressTextField.typeText(siteUrl)
+//        nextButton.tap()
+//
+//        return LoginUsernamePasswordScreen()
+//    }
+
+    func proceedWithWP(siteUrl: String) -> GetStartedScreen {
         siteAddressTextField.tap()
         siteAddressTextField.typeText(siteUrl)
         nextButton.tap()
 
-        return LoginUsernamePasswordScreen()
+        return GetStartedScreen()
     }
 
     static func isLoaded() -> Bool {
--- WooCommerce/WooCommerceUITests/Tests/LoginTests.swift
+++ WooCommerce/WooCommerceUITests/Tests/LoginTests.swift
@@ -11,23 +11,67 @@ class LoginTests: XCTestCase {
         app.launch()
     }
 
-    func testEmailPasswordLoginLogout() {
-        // Log in with email and password
-        WelcomeScreen()
-        .selectLogin()
-        .proceedWith(email: TestCredentials.emailAddress)
-        .proceedWithPassword()
-        .proceedWith(password: TestCredentials.password)
+//    func testEmailPasswordLoginLogout() {
+//        // Log in with email and password
+//        WelcomeScreen()
+//        .selectLogin()
+//        .proceedWith(email: TestCredentials.emailAddress)
+//        .proceedWithPassword()
+//        .proceedWith(password: TestCredentials.password)
+//
+//        // Login epilogue
+//        .verifyEpilogueDisplays(displayName: TestCredentials.displayName, siteUrl: TestCredentials.siteUrl)
+//        .continueWithSelectedSite()
+//
+//        // Log out
+//        .openSettingsPane()
+//        .verifySelectedStoreDisplays(siteUrl: TestCredentials.siteUrl, displayName: TestCredentials.displayName)
+//        .logOut()
+//
+//        XCTAssert(WelcomeScreen.isLoaded())
+//    }
 
-        // Login epilogue
-        .verifyEpilogueDisplays(displayName: TestCredentials.displayName, siteUrl: TestCredentials.siteUrl)
-        .continueWithSelectedSite()
 
-        // Log out
-        .openSettingsPane()
-        .verifySelectedStoreDisplays(siteUrl: TestCredentials.siteUrl, displayName: TestCredentials.displayName)
-        .logOut()
+    //Modified
+//    func testEmailPasswordLoginLogout() {
+//        // Log in with email and password
+//        PrologueScreen()
+//        .selectLogin()
+//        .proceedWith(email: TestCredentials.emailAddress)
+//        .proceedWithPassword()
+//        .proceedWith(password: TestCredentials.password)
+//
+//        // Login epilogue
+//        .verifyEpilogueDisplays(displayName: TestCredentials.displayName, siteUrl: TestCredentials.siteUrl)
+//        .continueWithSelectedSite()
+//
+//        // Log out
+//        .openSettingsPane()
+//        .verifySelectedStoreDisplays(siteUrl: TestCredentials.siteUrl, displayName: TestCredentials.displayName)
+//        .logOut()
+//
+//        XCTAssert(WelcomeScreen.isLoaded())
+//    }
 
-        XCTAssert(WelcomeScreen.isLoaded())
+    // Unified WordPress.com login/out
+    // Replaces testWpcomUsernamePasswordLogin
+    func testWpcomLogin() {
+        let prologue = PrologueScreen().selectSiteAddress()
+            .proceedWithWP(siteUrl: TestCredentials.siteUrl)
+            //.proceedWith(username: TestCredentials.emailAddress, password: TestCredentials.password)
+            .proceedWith(email: TestCredentials.emailAddress)
+            .proceedWith(password: TestCredentials.password)
+            .verifyEpilogueDisplays(displayName: TestCredentials.displayName, siteUrl: TestCredentials.siteUrl)
+            .continueWithSelectedSite()
+            //.dismissNotificationAlertIfNeeded()
+
+        //XCTAssert(MySiteScreen().isLoaded())
+
+            // Log out
+            .openSettingsPane()
+            .verifySelectedStoreDisplays(siteUrl: TestCredentials.siteUrl, displayName: TestCredentials.displayName)
+            .logOut()
+
+        XCTAssert(prologue.isLoaded())
     }
 }

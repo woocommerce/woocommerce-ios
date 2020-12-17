486ae740137f26fb5f62c27c2cdf86a2edd92799
Paolo Musolino <info@codeido.com>
2020-12-17 13:44:19 +0100

    feat: AddAttributeOptionsViewController init method

--- WooCommerce/Classes/ViewRelated/Products/Variations/Add Attributes/AddAttributeOptionsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Variations/Add Attributes/AddAttributeOptionsViewController.swift
@@ -2,6 +2,16 @@ import UIKit
 
 class AddAttributeOptionsViewController: UIViewController {
 
+    /// Init
+    ///
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 

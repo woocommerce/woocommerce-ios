c7c7f0a17b19706378a6666cb2f653ca1e3c6bf5
Evgeny Shurakov <eshurakov@users.noreply.github.com>
2020-12-18 09:09:34 +0100

    Update OrderListCellViewModelTests.swift

--- WooCommerce/WooCommerceTests/ViewModels/OrderListCellViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewModels/OrderListCellViewModelTests.swift
@@ -6,7 +6,7 @@ import TestKit
 
 final class OrderListCellViewModelTests: XCTestCase {
 
-    func test_formatted_date_created_does_not_include_the_year_for_dates_with_the_same_year() {
+    func test_dateCreated_does_not_include_the_year_for_dates_with_the_same_year() {
         // Given
         let order = MockOrders().sampleOrderCreatedInCurrentYear()
         let viewModel = OrderListCellViewModel(order: order, status: nil)
@@ -18,7 +18,7 @@ final class OrderListCellViewModelTests: XCTestCase {
         XCTAssertFalse(formatted.contains(expectedYearString))
     }
 
-    func test_formatted_date_created_includes_the_year_for_dates_that_are_not_in_the_same_year() {
+    func test_dateCreated_includes_the_year_for_dates_that_are_not_in_the_same_year() {
         // Given
         let order = MockOrders().sampleOrder()
         let viewModel = OrderListCellViewModel(order: order, status: nil)
@@ -28,5 +28,29 @@ final class OrderListCellViewModelTests: XCTestCase {
         let formatted = viewModel.dateCreated
 
         XCTAssertTrue(formatted.contains(expectedYearString))
+    }
+
+    func test_OrderStatus_is_used_when_passed_to_view_model() {
+        // Given
+        let order = MockOrders().sampleOrder()
+        let orderStatus = OrderStatus(name: UUID().uuidString,
+                                      siteID: 0,
+                                      slug: UUID().uuidString,
+                                      total: 0)
+        let viewModel = OrderListCellViewModel(order: order, status: orderStatus)
+
+        // Then
+        XCTAssertEqual(viewModel.status, .custom(orderStatus.slug))
+        XCTAssertEqual(viewModel.statusString, orderStatus.name)
+    }
+
+    func test_status_from_order_is_used_when_OrderStatus_is_nil() {
+        // Given
+        let order = MockOrders().sampleOrder()
+        let viewModel = OrderListCellViewModel(order: order, status: nil)
+
+        // Then
+        XCTAssertEqual(viewModel.status, order.status)
+        XCTAssertEqual(viewModel.statusString, order.status.rawValue)
     }
 }

0008b8a9827a05aef5238b5e6d0d0c20d55bf77a
Allen Snook <allendav@automattic.com>
2021-01-05 14:46:29 -0800

    Replace brand(purple) help nav bar button color with accent(pink)

--- WooCommerce/Classes/Authentication/Epilogue/AccountHeaderView.swift
+++ WooCommerce/Classes/Authentication/Epilogue/AccountHeaderView.swift
@@ -101,8 +101,8 @@ private extension AccountHeaderView {
     func setupHelpButton() {
         helpButton.setTitle(Strings.helpButtonTitle, for: .normal)
         helpButton.setTitle(Strings.helpButtonTitle, for: .highlighted)
-        helpButton.setTitleColor(.primary, for: .normal)
-        helpButton.setTitleColor(.textSubtle, for: .highlighted)
+        helpButton.setTitleColor(.accent, for: .normal)
+        helpButton.setTitleColor(.accent, for: .highlighted)
         helpButton.on(.touchUpInside) { [weak self] control in
             ServiceLocator.analytics.track(.sitePickerHelpButtonTapped)
             self?.handleHelpButtonTapped(control)

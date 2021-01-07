eb169888a185467f781ca094345f1a0ac864328c
Jeremy Massel <jeremy.massel@gmail.com>
2021-01-07 10:33:39 -0700

    Update Yosemite/Yosemite/Model/Mocks/Helpers/ProductStockStatus+Mocks.swift
    Co-authored-by: Olivier Halligon <olivier.halligon@automattic.com>

--- Yosemite/Yosemite/Model/Mocks/Helpers/ProductStockStatus+Mocks.swift
+++ Yosemite/Yosemite/Model/Mocks/Helpers/ProductStockStatus+Mocks.swift
@@ -3,11 +3,11 @@ import Foundation
 extension ProductStockStatus {
     public static func from(quantity: Int64) -> ProductStockStatus {
         switch quantity {
-            case Int64.min ..< 0:
+            case ..<0:
                 return .onBackOrder
             case 0:
                 return .outOfStock
-            case 1...Int64.max:
+            case 1...:
                 return .inStock
             default:
                 preconditionFailure("Unable to parse quantity")

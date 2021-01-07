33d29a64a81d068d3bd0a886789d3d39b02fe408
Paolo Musolino <info@codeido.com>
2021-01-07 14:39:26 +0100

    feat: implemented ProductVariationInBatch model + ListMapper

---
+++ Networking/Networking/Model/Product/ProductVariationInBatch.swift
@@ -0,0 +1,9 @@
+//
+//  ProductVariationInBatch.swift
+//  Networking
+//
+//  Created by Paolo Musolino on 07/01/2021.
+//  Copyright © 2021 Automattic. All rights reserved.
+//
+
+import Foundation

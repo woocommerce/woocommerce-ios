64158704fe661a2f778dd9d2cdbe5fb977e56ab3
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-01-07 00:46:56 -0500

    Added ProductAttributeTermStore - not tested -

--- Yosemite/Yosemite/Actions/ProductAttributeTermAction.swift
+++ Yosemite/Yosemite/Actions/ProductAttributeTermAction.swift
@@ -8,7 +8,7 @@ public enum ProductAttributeTermAction: Action {
     /// Synchronizes all ProductAttributeTerm objects for a given attribute.
     /// `onCompletion` will be invoked when the sync operation finishes. `error` will be nil if the operation succeed.
     ///
-    case synchronizeProductAttributeTerms(siteID: Int64, attributeID: Int64, onCompletion: (Result<[ProductAttributeTerm], Error>) -> Void)
+    case synchronizeProductAttributeTerms(siteID: Int64, attributeID: Int64, onCompletion: (Result<Void, ProductAttributeTermActionError>) -> Void)
 }
 
 /// Defines all errors that a `ProductAttributeTermAction` can return

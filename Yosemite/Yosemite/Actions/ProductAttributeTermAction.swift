9bb8fadc1f29d4c94c3b8e817d0107f305a9af99
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-01-11 21:23:24 -0500

    Adds create attribute term action case

--- Yosemite/Yosemite/Actions/ProductAttributeTermAction.swift
+++ Yosemite/Yosemite/Actions/ProductAttributeTermAction.swift
@@ -9,6 +9,11 @@ public enum ProductAttributeTermAction: Action {
     /// `onCompletion` will be invoked when the sync operation finishes. `error` will be nil if the operation succeed.
     ///
     case synchronizeProductAttributeTerms(siteID: Int64, attributeID: Int64, onCompletion: (Result<Void, ProductAttributeTermActionError>) -> Void)
+
+    /// Create a new product attribute term associated with a given Site ID.
+    /// `onCompletion` will be invoked when the add operation finishes.
+    ///
+    case createProductAttributeTerm(siteID: Int64, attributeID: Int64, onCompletion: (Result<ProductAttributeTerm, Error>) -> Void)
 }
 
 /// Defines all errors that a `ProductAttributeTermAction` can return

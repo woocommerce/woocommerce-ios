a10c260bcef5a1a88f20de8b4cc703f304abd3fb
Jaclyn Chen <watertranquil@gmail.com>
2021-01-27 14:03:45 +0800

    Remove unused code.

--- Networking/Networking/Model/Stats/StatGranularity.swift
+++ Networking/Networking/Model/Stats/StatGranularity.swift
@@ -8,41 +8,4 @@ public enum StatGranularity: String, Decodable {
     case week
     case month
     case year
-
-    public var pluralizedString: String {
-        switch self {
-        case .day:
-            return NSLocalizedString("Days", comment: "Plural of 'day' — a statistical unit")
-        case .week:
-            return NSLocalizedString("Weeks", comment: "Plural of 'week' — a statistical unit")
-        case .month:
-            return NSLocalizedString("Months", comment: "Plural of 'month' — a statistical unit")
-        case .year:
-            return NSLocalizedString("Years", comment: "Plural of 'year' — a statistical unit")
-        }
-    }
-
-    public var accessibilityIdentifier: String {
-        return "granularity-\(self.rawValue)"
-    }
-}
-
-// MARK: - StringConvertible Conformance
-//
-extension StatGranularity: CustomStringConvertible {
-
-    /// Returns a user-freindly, localized string describing the stat granularity
-    ///
-    public var description: String {
-        switch self {
-        case .day:
-            return NSLocalizedString("Day", comment: "Statistical unit - a single day")
-        case .week:
-            return NSLocalizedString("Week", comment: "Statistical unit - a single week")
-        case .month:
-            return NSLocalizedString("Month", comment: "Statistical unit - a single week")
-        case .year:
-            return NSLocalizedString("Year", comment: "Statistical unit - a single year")
-        }
-    }
 }

c745c02f6cdcf052496dbe51c221bd96cceecd3d
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2021-02-08 18:14:36 +0300

    Add comments in AttributePickerViewModel

--- WooCommerce/Classes/ViewRelated/Products/Variations/Edit Attributes/AttributePickerViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Variations/Edit Attributes/AttributePickerViewModel.swift
@@ -13,24 +13,30 @@ final class AttributePickerViewModel {
 
     // MARK: DataSource
 
+    /// All attributes for a product variation, including "any"
     var attributes: [ProductAttribute] {
         variationModel.allAttributes
     }
 
+    /// Returns whether there are unsaved changes.
+    /// Comparison of arrays uses sorting because order doesn't matter.
     var isChanged: Bool {
         variationModel.productVariation.attributes.sorted { $0.id < $1.id } != editableAttributes.sorted { $0.id < $1.id }
     }
 
+    /// Attributes set (excluding "any") for a product variation after all updates
     var resultAttributes: [ProductVariationAttribute] {
         editableAttributes
     }
 
+    /// Selected option for variation attribute. Will be `nil` if option is "any"
     func selectedOption(for attribute: ProductAttribute) -> ProductVariationAttribute? {
         return editableAttributes.first(where: { $0.id == attribute.attributeID && $0.name == attribute.name })
     }
 
     // MARK: Delegate
 
+    /// Replace old option for attribute with updated one. For "any" options use `nil`.
     func update(oldAttribute: ProductVariationAttribute?, to newAttribute: ProductVariationAttribute?) {
         switch (oldAttribute, newAttribute) {
         case (nil, let newAttribute?):

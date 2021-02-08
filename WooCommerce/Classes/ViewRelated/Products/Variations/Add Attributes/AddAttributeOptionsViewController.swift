6d4482c97929af40c075d2b7f85650f6efd1d09b
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-02-08 14:07:44 -0500

    Show notice when the product update fails

--- WooCommerce/Classes/ViewRelated/Products/Variations/Add Attributes/AddAttributeOptionsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Variations/Add Attributes/AddAttributeOptionsViewController.swift
@@ -9,6 +9,8 @@ final class AddAttributeOptionsViewController: UIViewController {
 
     private let viewModel: AddAttributeOptionsViewModel
 
+    private let noticePresenter: NoticePresenter
+
     /// Keyboard management
     ///
     private lazy var keyboardFrameObserver: KeyboardFrameObserver = KeyboardFrameObserver { [weak self] keyboardFrame in
@@ -17,8 +19,9 @@ final class AddAttributeOptionsViewController: UIViewController {
 
     /// Init
     ///
-    init(viewModel: AddAttributeOptionsViewModel) {
+    init(viewModel: AddAttributeOptionsViewModel, noticePresenter: NoticePresenter = ServiceLocator.noticePresenter) {
         self.viewModel = viewModel
+        self.noticePresenter = noticePresenter
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -309,7 +312,17 @@ extension AddAttributeOptionsViewController: KeyboardScrollable {
 extension AddAttributeOptionsViewController {
 
     @objc private func nextButtonPressed() {
-        viewModel.updateProductAttributes(onCompletion: { _ in })
+        viewModel.updateProductAttributes { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(product):
+                // TODO: Navigate to attributes screen, maybe clean the stack? #3536
+                print(product.attributes)
+            case let .failure(error):
+                self.noticePresenter.enqueue(notice: .init(title: Localization.updateAttributeError, feedbackType: .error))
+                DDLogError(error.localizedDescription)
+            }
+        }
     }
 }
 
@@ -348,5 +361,7 @@ private extension AddAttributeOptionsViewController {
         static let nextNavBarButton = NSLocalizedString("Next", comment: "Next nav bar button title in Add Product Attribute Options screen")
         static let optionNameCellPlaceholder = NSLocalizedString("Option name",
                                                             comment: "Placeholder of cell presenting the title of the new attribute option.")
+        static let updateAttributeError = NSLocalizedString("The attribute couldn't be saved.",
+                                                            comment: "Error title when trying to update or create an attribute remotely.")
     }
 }

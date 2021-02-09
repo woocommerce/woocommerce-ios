3782f1cd89a2528a7d0a449a0b2dd4360c0f0541
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-02-09 15:27:20 -0500

    Add simple EditAttributesViewController

---
+++ WooCommerce/Classes/ViewRelated/Products/Variations/Edit Attributes/EditAttributesViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+/// EditAttributesViewController: Displays the list of attributes for a product.
+///
+final class EditAttributesViewController: UIViewController {
+
+    @IBOutlet private weak var addButton: UIButton!
+    @IBOutlet private weak var addButtonSeparator: UIView!
+    @IBOutlet private var tableView: UITableView!
+}

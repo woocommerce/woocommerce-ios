36af9cf13377ecaba66327d8328faecc07dba959
Paolo Musolino <info@codeido.com>
2021-02-09 19:33:51 +0100

    feat: added errors fields under ShippingLabelAddressValidationResponse model

--- Networking/Networking/Model/ShippingLabel/ShippingLabelAddressValidationResponse.swift
+++ Networking/Networking/Model/ShippingLabel/ShippingLabelAddressValidationResponse.swift
@@ -1,20 +1,23 @@
 import Foundation
 
-/// Represents Shipping Label Address that has been validated.
+/// Represents Shipping Label Address that has been validated or that generated an error.
 ///
 public struct ShippingLabelAddressValidationResponse: Equatable {
-    public let address: ShippingLabelAddress
+    public let address: ShippingLabelAddress?
+    public let errors: ShippingLabelAddressValidationError?
 
-    public init(address: ShippingLabelAddress) {
+    public init(address: ShippingLabelAddress?, errors: ShippingLabelAddressValidationError?) {
         self.address = address
+        self.errors = errors
     }
 }
 
 extension ShippingLabelAddressValidationResponse: Decodable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        let address = try container.decode(ShippingLabelAddress.self, forKey: .address)
-        self.init(address: address)
+        let address = try container.decodeIfPresent(ShippingLabelAddress.self, forKey: .address)
+        let errors = try container.decodeIfPresent(ShippingLabelAddressValidationError.self, forKey: .errors)
+        self.init(address: address, errors: errors)
     }
 }
 
@@ -23,5 +26,6 @@ extension ShippingLabelAddressValidationResponse: Decodable {
 private extension ShippingLabelAddressValidationResponse {
     enum CodingKeys: String, CodingKey {
         case address = "normalized"
+        case errors = "field_errors"
     }
 }

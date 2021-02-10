860ec2f93c4b783385d4c3f5086340f2a31734fe
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-02-10 15:01:35 -0500

    Add main product dependency

--- WooCommerce/Classes/ViewRelated/Products/Variations/Edit Attributes/EditAttributesViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Variations/Edit Attributes/EditAttributesViewController.swift
@@ -8,6 +8,17 @@ final class EditAttributesViewController: UIViewController {
     @IBOutlet private weak var addButtonSeparator: UIView!
     @IBOutlet private var tableView: UITableView!
 
+    private let viewModel: EditAttributesViewModel
+
+    init(viewModel: EditAttributesViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureAddButton()
--- WooCommerce/Classes/ViewRelated/Products/Variations/Edit Attributes/EditAttributesViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Variations/Edit Attributes/EditAttributesViewModel.swift
@@ -1,5 +1,28 @@
 import Foundation
+import Yosemite
 
 final class EditAttributesViewModel {
 
+    /// Main product dependency
+    ///
+    private var product: Product
+
+    /// If true, a done button will appear that allows the merchant to generate a variation
+    ///
+    private let allowVariationCreation: Bool
+
+    init(product: Product, allowVariationCreation: Bool) {
+        self.product = product
+        self.allowVariationCreation = allowVariationCreation
+    }
+}
+
+// MARK: View Controller inputs
+extension EditAttributesViewModel {
+
+    /// Updates the view model output properties based on the provided `product`.
+    ///
+    func updateProduct(_ product: Product) {
+        self.product = product
+    }
 }

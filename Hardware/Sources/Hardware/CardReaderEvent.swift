a4379fd1dc4ce28ad0fb1e49fe56ae510e22d1aa
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-02-10 15:18:15 -0500

    Add header docs

--- Hardware/Sources/Hardware/CardReaderEvent.swift
+++ Hardware/Sources/Hardware/CardReaderEvent.swift
@@ -1,7 +1,14 @@
 /// The possible events from a connected reader.
 public enum CardReaderEvent {
+    // A card was inserted.
     case cardInserted
+
+    // A card was removed.
     case cardRemoved
+
+    // Low battery warning.
     case lowBattery
+
+    // The card reader disconnected.
     case disconnected
 }

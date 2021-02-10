09d65cd5f3e49ce09e436227e80b557ece48a505
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-02-10 09:49:06 -0500

    Shift the API to Combine. That removes the discovery delegate, which was an implementation detail that we'd need to leak out of Hardware

--- Hardware/Sources/Hardware/CardReaderService.swift
+++ Hardware/Sources/Hardware/CardReaderService.swift
@@ -1,14 +1,16 @@
+import Combine
+
 /// Abstracts the integration with a Card Reader
 public protocol CardReaderService {
-    /// The service delegate.
-    var delegate: CardReaderServiceDelegate? { get set }
+    /// The publisher that emits the list of discovered readers whenever the service discovers a new reader.
+    var discoveredReaders: AnyPublisher<[CardReader], Error> { get }
 
-    /// Returns the connected reader, or nil if no reader is connected
-    var connectedReader: CardReader? { get }
+    /// The Publisher that emits the connected reader
+    var connectedReader: AnyPublisher<CardReader, Never> { get }
 
-    /// The CardReaderService's connection status
-    var connectionStatus: ConnectionStatus { get }
+    /// The Publisher that emits the connection status
+    var connectionStatus: AnyPublisher<ConnectionStatus, Never> { get }
 
-    /// The CardReaderService's current payment status
-    var paymentStatus: PaymentStatus { get }
+    /// The Publisher that emits the payment status
+    var paymentStatus: AnyPublisher<PaymentStatus, Never> { get }
 }
--- Hardware/Sources/Hardware/StripeCardReader/StripeCardReaderService.swift
+++ Hardware/Sources/Hardware/StripeCardReader/StripeCardReaderService.swift
@@ -1,18 +1,44 @@
+import Combine
 import StripeTerminal
 
 /// The adapter wrapping the Stripe Terminal SDK
-public final class StripeCardReaderService: CardReaderService {
-    public var delegate: CardReaderServiceDelegate? = nil
-
-    public let connectedReader: CardReader? = nil
-
-    public let connectionStatus: ConnectionStatus = .notConnected
-
-    public let paymentStatus: PaymentStatus = .notReady
-
+public final class StripeCardReaderService {
     private let tokenProvider: ConnectionTokenProvider
+
+    private let discoveredReadersSubject = CurrentValueSubject<[CardReader], Error>([])
+    private let connectedReaderSubject = PassthroughSubject<CardReader, Never>()
+    private let connectionStatusSubject = CurrentValueSubject<ConnectionStatus, Never>(.notConnected)
+
+    private let paymentStatusSubject = CurrentValueSubject<PaymentStatus, Never>(.notReady)
+
 
     public init(tokenProvider: ConnectionTokenProvider) {
         self.tokenProvider = tokenProvider
+    }
+}
+
+
+// MARK: - CardReaderService conformance
+extension StripeCardReaderService: CardReaderService {
+    public var discoveredReaders: AnyPublisher<[CardReader], Error> {
+        discoveredReadersSubject.eraseToAnyPublisher()
+    }
+
+
+    public var connectedReader: AnyPublisher<CardReader, Never> {
+        connectedReaderSubject.eraseToAnyPublisher()
+    }
+
+
+
+    public var connectionStatus: AnyPublisher<ConnectionStatus, Never> {
+        connectionStatusSubject.eraseToAnyPublisher()
+    }
+
+
+
+    /// The Publisher that emits the payment status
+    public var paymentStatus: AnyPublisher<PaymentStatus, Never> {
+        paymentStatusSubject.eraseToAnyPublisher()
     }
 }

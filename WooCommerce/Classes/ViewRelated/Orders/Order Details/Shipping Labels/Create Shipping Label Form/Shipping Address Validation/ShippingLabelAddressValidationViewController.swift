09076cbc1b6bc25cecc155421479baa21e4fbfa7
Paolo Musolino <info@codeido.com>
2021-02-12 15:14:01 +0100

    feat: added Sections and row in ShippingLabelAddressValidationViewController

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Shipping Address Validation/ShippingLabelAddressValidationViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Shipping Address Validation/ShippingLabelAddressValidationViewController.swift
@@ -23,3 +23,42 @@ final class ShippingLabelAddressValidationViewController: UIViewController {
 
     }
 }
+
+
+extension ShippingLabelAddressValidationViewController {
+
+    struct Section: Equatable {
+        let rows: [Row]
+    }
+
+    enum Row: CaseIterable {
+        case errorBanner
+
+        case name
+        case company
+        case phones
+        case address
+        case address2
+        case city
+        case postcode
+        case state
+        case country
+
+        case useAddressAsEntered
+
+        fileprivate var type: UITableViewCell.Type {
+            switch self {
+            case .errorBanner:
+                return BasicTableViewCell.self
+            case .name, .company, .phones, .address, .address2, .city, .postcode, .state, .country:
+                return NumberOfLinkedProductsTableViewCell.self
+            case .useAddressAsEntered:
+                return ButtonTableViewCell.self
+            }
+        }
+
+        fileprivate var reuseIdentifier: String {
+            return type.reuseIdentifier
+        }
+    }
+}

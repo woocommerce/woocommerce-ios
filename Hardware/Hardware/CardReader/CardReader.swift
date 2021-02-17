06554c49b25440bffb428a2ef3b6deec97165fe4
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-02-17 14:03:35 -0500

    First iteration of CardReader and CardReaderType

--- Hardware/Hardware/CardReader/CardReader.swift
+++ Hardware/Hardware/CardReader/CardReader.swift
@@ -1,7 +1,18 @@
 /// Models a Card Reader. This is the public struct that clients of
 /// Hardware are expected to consume.
-/// The exact properties are to be defined yet.
-/// For now, this is a placeholder
 public struct CardReader {
+    public let serial: String
+    public let vendorIdentifier: String?
+    public let name: String?
     public let status: CardReaderStatus
+    public let softwareVersion: String?
+    public let batteryLevel: Float?
+    public let readerType: CardReaderType
+}
+
+
+extension CardReader: Equatable {
+    public static func ==(lhs: CardReader, rhs: CardReader) -> Bool {
+        lhs.serial == rhs.serial
+    }
 }
---
+++ Hardware/Hardware/CardReader/StripeCardReader/CardReader+Stripe.swift
@@ -0,0 +1,18 @@
+import StripeTerminal
+
+/// Convenience intializer
+extension CardReader {
+    init(reader: Reader) {
+        self.serial = reader.serialNumber
+        self.vendorIdentifier = reader.stripeId
+        self.name = reader.label
+
+        let connected = reader.status == .online
+        self.status = CardReaderStatus(connected: connected, remembered: false)
+
+        self.softwareVersion = reader.deviceSoftwareVersion
+        self.batteryLevel = reader.batteryLevel?.floatValue
+
+        self.readerType = CardReaderType.with(readerType: reader.deviceType)
+    }
+}

c508709aec1ceda4df10ad8f0dae1da40a155279
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-02-18 10:46:46 -0500

    Add a test to hardware in order to generate a report

--- Hardware/HardwareTests/HardwareTests.swift
+++ Hardware/HardwareTests/HardwareTests.swift
@@ -2,5 +2,8 @@ import XCTest
 @testable import Hardware
 
 final class HardwareTests: XCTestCase {
-
+    // A test to check if the build is failing because it is missing a report.xcresult
+    func test_charge_status_exists() {
+        XCTAssertNotNil(ChargeStatus.failed)
+    }
 }

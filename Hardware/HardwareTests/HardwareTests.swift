d41dca81a2e5fdb3ab3e098f820b0c7cd6f8b90a
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-02-18 09:23:49 -0500

    Remove unecessary test

--- Hardware/HardwareTests/HardwareTests.swift
+++ Hardware/HardwareTests/HardwareTests.swift
@@ -2,7 +2,5 @@ import XCTest
 @testable import Hardware
 
 final class HardwareTests: XCTestCase {
-    func test_that_tests_run() {
-        XCTAssertEqual(Hardware().text, "Hello, World!")
-    }
+
 }

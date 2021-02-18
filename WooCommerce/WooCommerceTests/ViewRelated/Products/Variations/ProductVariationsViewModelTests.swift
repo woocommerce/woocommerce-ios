679b6c80be4bda0f620b1cdda9bc3e7d4fe0970c
rachelmcr <rachel@automattic.com>
2021-02-18 11:49:15 +0000

    Use Product() instead of MockProduct() for unit tests

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Variations/ProductVariationsViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Variations/ProductVariationsViewModelTests.swift
@@ -1,11 +1,12 @@
 import XCTest
 @testable import WooCommerce
+import Yosemite
 
 final class ProductVariationsViewModelTests: XCTestCase {
     func test_more_button_appears_when_product_is_not_empty_and_addProductVariations_feature_is_enabled() {
         // Arrange
         let variations: [Int64] = [101, 102]
-        let product = MockProduct().product(variations: variations)
+        let product = Product().copy(variations: variations)
         let viewModel = ProductVariationsViewModel(product: product, isAddProductVariationsEnabled: true)
 
         // Assert
@@ -15,7 +16,7 @@ final class ProductVariationsViewModelTests: XCTestCase {
     func test_more_button_does_not_appear_when_product_is_not_empty_and_addProductVariations_feature_is_disabled() {
         // Arrange
         let variations: [Int64] = [101, 102]
-        let product = MockProduct().product(variations: variations)
+        let product = Product().copy(variations: variations)
         let viewModel = ProductVariationsViewModel(product: product, isAddProductVariationsEnabled: false)
 
         // Assert
@@ -24,7 +25,7 @@ final class ProductVariationsViewModelTests: XCTestCase {
 
     func test_more_button_does_not_appear_when_product_is_empty_and_addProductVariations_feature_is_enabled() {
         // Arrange
-        let product = MockProduct().product()
+        let product = Product().copy()
         let viewModel = ProductVariationsViewModel(product: product, isAddProductVariationsEnabled: true)
 
         // Assert
@@ -33,7 +34,7 @@ final class ProductVariationsViewModelTests: XCTestCase {
 
     func test_more_button_does_not_appear_when_product_is_empty_and_addProductVariations_feature_is_disabled() {
         // Arrange
-        let product = MockProduct().product()
+        let product = Product().copy()
         let viewModel = ProductVariationsViewModel(product: product, isAddProductVariationsEnabled: false)
 
         // Assert

3fa50af8adb07bf46ce2ea23e80bbeb4afb90e9c
Paolo Musolino <info@codeido.com>
2021-02-18 17:14:36 +0100

    feat: implemented handleAddressValueChanges in ShippingLabelAddressFormViewModel

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Shipping Address Validation/ShippingLabelAddressFormViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Shipping Address Validation/ShippingLabelAddressFormViewModel.swift
@@ -7,11 +7,36 @@ final class ShippingLabelAddressFormViewModel: NSObject {
     typealias Row = ShippingLabelAddressFormViewController.Row
 
     let type: ShipType
-    let address: ShippingLabelAddress?
+    private (set) var address: ShippingLabelAddress?
 
     init(type: ShipType, address: ShippingLabelAddress?) {
         self.type = type
         self.address = address
+    }
+
+    func handleAddressValueChanges(row: Row, newValue: String?) {
+        switch row {
+        case .name:
+            address = address?.copy(name: newValue)
+        case .company:
+            address = address?.copy(company: newValue)
+        case .phone:
+            address = address?.copy(phone: newValue)
+        case .address:
+            address = address?.copy(address1: newValue)
+        case .address2:
+            address = address?.copy(address2: newValue)
+        case .city:
+            address = address?.copy(city: newValue)
+        case .postcode:
+            address = address?.copy(postcode: newValue)
+        case .state:
+            address = address?.copy(state: newValue)
+        case .country:
+            address = address?.copy(country: newValue)
+        default:
+            return
+        }
     }
 
     var sections: [Section] {

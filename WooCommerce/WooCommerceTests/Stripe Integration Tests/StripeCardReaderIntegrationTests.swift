ba8b81517ea44efbc9a359a88505ca1aaeb83db1
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-02-22 16:14:59 -0500

    Formalize first integration tests

--- WooCommerce/WooCommerceTests/Stripe Integration Tests/StripeCardReaderIntegrationTests.swift
+++ WooCommerce/WooCommerceTests/Stripe Integration Tests/StripeCardReaderIntegrationTests.swift
@@ -3,9 +3,11 @@ import Combine
 @testable import WooCommerce
 @testable import Hardware
 
-/// This test suite should belong in `Hardware`, but the Stripe Terminal SDK
-/// will crash, when running in test mode, if there is no Info.plist avaiable
-/// with a certain set of keys.
+
+/// Integration tests for the integration with the Stripe Terminal SDK.
+/// We want to reach into the ServiceLocator in all tests
+/// because these are integration test, and we do not want to mock anything,
+/// at this point, other than the actual hardware
 final class StripeCardReaderIntegrationTests: XCTestCase {
     private var cancellables: Set<AnyCancellable>!
 
@@ -15,12 +17,9 @@ final class StripeCardReaderIntegrationTests: XCTestCase {
     }
 
     // MARK: - Integration tests
-    func test_start_triggers_an_update_to_discovered_readers() {
+    func test_start_discovery_updates_discovered_readers() {
         let receivedReaders = expectation(description: "Discovered Readers publishes values after discovery process starts")
 
-
-        // We want to reach into the ServiceLocator because this is an integration test, and we do not want to mock 
-        // The
         let readerService = ServiceLocator.cardReaderService
 
         readerService.discoveredReaders.sink { completion in
@@ -32,4 +31,32 @@ final class StripeCardReaderIntegrationTests: XCTestCase {
         readerService.start()
         waitForExpectations(timeout: 5, handler: nil)
     }
+
+    func test_start_discovery_updates_discovered_readers__at_least_twice() {
+        let receivedReaders = expectation(description: "Discovered Readers publishes first, an empty array, and then the actual reader(s) discovered")
+
+        let readerService = ServiceLocator.cardReaderService
+
+        readerService.discoveredReaders.sink { completion in
+            receivedReaders.fulfill()
+        } receiveValue: { readers in
+            // The Stripe Terminal SDK published an empty list of discovered readers first
+            // and it will continue publishing as new readers are discovered.
+            // So we ignore the first call to receiveValue, and perform the test on the first call to
+            // receive value that is receiving a non-empty array.
+            guard !readers.isEmpty else {
+                return
+            }
+
+            // There should be at least one non nil reader
+            XCTAssertNotNil(readers.first)
+
+            // We blisfully ignore the actual values received (for now)
+            receivedReaders.fulfill()
+        }.store(in: &cancellables)
+
+        readerService.start()
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+
 }

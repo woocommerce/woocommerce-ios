0985f971fccec08a8008fd7ea7dd3fa796c75a77
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-02-25 11:48:16 -0500

    Specify CardReader is inmutable

--- Hardware/Hardware/CardReader/CardReader.swift
+++ Hardware/Hardware/CardReader/CardReader.swift
@@ -1,5 +1,6 @@
 /// Models a Card Reader. This is the public struct that clients of
 /// Hardware are expected to consume.
+/// CardReader is meant to be inmutable. 
 public struct CardReader {
 
     /// The CardReader serial number

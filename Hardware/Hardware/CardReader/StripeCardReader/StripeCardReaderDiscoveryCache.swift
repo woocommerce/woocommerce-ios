a674e699ee1b146e884dba16bbb60973ba0f964e
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-02-25 08:24:25 -0500

    Fix a couple of typos

--- Hardware/Hardware/CardReader/StripeCardReader/StripeCardReaderDiscoveryCache.swift
+++ Hardware/Hardware/CardReader/StripeCardReader/StripeCardReaderDiscoveryCache.swift
@@ -2,8 +2,8 @@ import StripeTerminal
 
 /// In memory, volatile cache
 final class StripeCardReaderDiscoveryCache {
-    /// CachedReaders. We will expose it for now,ntil we have some more clarity on what we
-    /// actually need from this class API.
+    /// CachedReaders. We will expose it for now until we have some more clarity on what we
+    /// actually need from this class' API.
     /// Typed as StripeCardReader so that we can unit test this class, which makes the class
     /// well, testable, but on the other hand, makes the API less clear
     private(set) var cachedReaders: [StripeCardReader] = []

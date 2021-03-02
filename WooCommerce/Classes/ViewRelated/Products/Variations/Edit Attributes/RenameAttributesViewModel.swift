e7265f4d118e7335e751d803d29415121f675f83
rachelmcr <rachel@automattic.com>
2021-03-02 10:11:49 +0000

    Add comments for documentation

--- WooCommerce/Classes/ViewRelated/Products/Variations/Edit Attributes/RenameAttributesViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Variations/Edit Attributes/RenameAttributesViewModel.swift
@@ -3,8 +3,12 @@ import Yosemite
 
 final class RenameAttributesViewModel {
 
+    /// Current name of the product attribute
+    ///
     let attributeName: String
 
+    /// New name of the product attribute
+    ///
     private(set) var newAttributeName: String?
 
     init(attributeName: String) {
@@ -16,10 +20,15 @@ final class RenameAttributesViewModel {
 // MARK: - Actions
 extension RenameAttributesViewModel {
 
+    /// Prevents the Done button from being enabled when the new attribute name is empty
+    ///
     var shouldEnableDoneButton: Bool {
         newAttributeName != ""
     }
 
+    /// Sets the new attribute name
+    ///
+    /// - Parameter name: New attribute name
     func handleAttributeNameChange(_ name: String?) {
         newAttributeName = name
     }

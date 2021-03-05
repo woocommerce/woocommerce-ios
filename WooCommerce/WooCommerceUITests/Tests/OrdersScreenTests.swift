0b2da79c23209d4245271ea185cc948433b2e5d8
thehenrybyrd <15107387+thehenrybyrd@users.noreply.github.com>
2021-03-05 11:44:52 +0200

    Implement method-chaining for readability

--- WooCommerce/WooCommerceUITests/Tests/OrdersScreenTests.swift
+++ WooCommerce/WooCommerceUITests/Tests/OrdersScreenTests.swift
@@ -4,13 +4,13 @@ class OrdersScreenTests: XCTestCase {
 
     override func setUp() {
         continueAfterFailure = false
-
+        
+        //comment out when testing from simulator already logged-in state:
         // UI tests must launch the application that they test.
         // let app = XCUIApplication()
         //comment out when testing without mocks:
-        //app.launchArguments = ["logout-at-launch", "disable-animations", "mocked-wpcom-api"]
-        // comment out when testing from simulator already logged-in state:
-        //app.launch()
+        // app.launchArguments = ["logout-at-launch", "disable-animations", "mocked-wpcom-api"]
+        // app.launch()
     }
 
     // Login, check that SingleOrderScreen loads
@@ -24,20 +24,8 @@ class OrdersScreenTests: XCTestCase {
   *          .verifyEpilogueDisplays(displayName: TestCredentials.displayName, siteUrl: TestCredentials.siteUrl)
   *          .continueWithSelectedSite()
   */
-
-        // Navigate to orders screen
-        // Find tabNavComponent, tap Orders button using gotoOrdersScreen function
-        let tabNavComponent = TabNavComponent()
-        tabNavComponent.gotoOrdersScreen()
-
-        // check that Orders screen is loaded
-        let orderScreen = OrdersScreen()
-        XCTAssert(orderScreen.isLoaded(), "Orders screen isn't loaded.")
-
-        // Select an order to get to SingleOrderScreen
-        // Assert that singleOrderScreen is loaded. This will check all the items asserted in singleOrderScreen's init.
-        orderScreen.selectOrder(atIndex: 0)
-        let singleOrderScreen = SingleOrderScreen()
-        XCTAssert(singleOrderScreen.isLoaded(), "Single Order Screen isn't loaded.")
+        //Select an order and verify the sections of the single order screen load.
+        let singleOrder = TabNavComponent().gotoOrdersScreen().selectOrder(atIndex: 0)
+        XCTAssert(singleOrder.isLoaded())
     }
 }

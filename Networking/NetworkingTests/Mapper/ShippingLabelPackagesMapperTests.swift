2b6471c4704ff468b0c0c7323a468eb482fdb5ab
Paolo Musolino <info@codeido.com>
2021-03-24 18:40:45 +0100

    feat: completed unit tests for Shipping Label Packages Mapper

--- Networking/NetworkingTests/Mapper/ShippingLabelPackagesMapperTests.swift
+++ Networking/NetworkingTests/Mapper/ShippingLabelPackagesMapperTests.swift
@@ -13,6 +13,7 @@ final class ShippingLabelPackagesMapperTests: XCTestCase {
 
         XCTAssertEqual(shippingLabelPackages.storeOptions, sampleShippingLabelStoreOptions())
         XCTAssertEqual(shippingLabelPackages.customPackages, sampleShippingLabelCustomPackages())
+        XCTAssertEqual(shippingLabelPackages.predefinedOptions, sampleShippingLabelPredefinedOptions())
     }
 }
 
@@ -42,13 +43,41 @@ private extension ShippingLabelPackagesMapperTests {
     }
 
     func sampleShippingLabelCustomPackages() -> [ShippingLabelCustomPackage] {
-        let customPackage1 = ShippingLabelCustomPackage(isUserDefined: true, title: "Krabica", isLetter: false, dimensions: "1 x 2 x 3", boxWeight: 1, maxWeight: 0)
-        let customPackage2 = ShippingLabelCustomPackage(isUserDefined: true, title: "Obalka", isLetter: true, dimensions: "2 x 3 x 4", boxWeight: 5, maxWeight: 0)
-        
+        let customPackage1 = ShippingLabelCustomPackage(isUserDefined: true,
+                                                        title: "Krabica",
+                                                        isLetter: false,
+                                                        dimensions: "1 x 2 x 3",
+                                                        boxWeight: 1,
+                                                        maxWeight: 0)
+        let customPackage2 = ShippingLabelCustomPackage(isUserDefined: true,
+                                                        title: "Obalka",
+                                                        isLetter: true,
+                                                        dimensions: "2 x 3 x 4",
+                                                        boxWeight: 5,
+                                                        maxWeight: 0)
+
         return [customPackage1, customPackage2]
     }
-    
+
     func sampleShippingLabelPredefinedOptions() -> [ShippingLabelPredefinedOption] {
-        return []
+        let predefinedPackages1 = [ShippingLabelPredefinedPackage(id: "small_flat_box",
+                                                                  title: "Small Flat Rate Box",
+                                                                  isLetter: false,
+                                                                  dimensions: "21.91 x 13.65 x 4.13"),
+                                  ShippingLabelPredefinedPackage(id: "medium_flat_box_top",
+                                                                 title: "Medium Flat Rate Box 1, Top Loading",
+                                                                 isLetter: false,
+                                                                 dimensions: "28.57 x 22.22 x 15.24")]
+        let predefinedOption1 = ShippingLabelPredefinedOption(title: "USPS Priority Mail Flat Rate Boxes",
+                                                              predefinedPackages: predefinedPackages1)
+
+        let predefinedPackages2 = [ShippingLabelPredefinedPackage(id: "LargePaddedPouch",
+                                                                  title: "Large Padded Pouch",
+                                                                  isLetter: true,
+                                                                  dimensions: "30.22 x 35.56 x 2.54")]
+        let predefinedOption2 = ShippingLabelPredefinedOption(title: "DHL Express",
+                                                              predefinedPackages: predefinedPackages2)
+
+        return [predefinedOption1, predefinedOption2]
     }
 }

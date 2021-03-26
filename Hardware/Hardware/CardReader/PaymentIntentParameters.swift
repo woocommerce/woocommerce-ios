c1ea6569727cf9725debda282609761e64c7fa31
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-03-26 12:42:37 -0400

    Silence liner warning

--- Hardware/Hardware/CardReader/PaymentIntentParameters.swift
+++ Hardware/Hardware/CardReader/PaymentIntentParameters.swift
@@ -31,7 +31,7 @@ public struct PaymentIntentParameters {
 
     /// Set of key-value pairs that you can attach to an object.
     /// This can be useful for storing additional information about the object in a structured format.
-    public let metadata: [AnyHashable : Any]?
+    public let metadata: [AnyHashable: Any]?
 
     public init(amount: UInt,
                 currency: String,

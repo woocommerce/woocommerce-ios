56f5d2b320c7f98b3ae427dff4f2b02612caace6
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-03-30 15:43:07 -0500

    Update navigation bar loading color

--- WooCommerce/Classes/Styles/UIColor+SemanticColors.swift
+++ WooCommerce/Classes/Styles/UIColor+SemanticColors.swift
@@ -266,7 +266,7 @@ extension UIColor {
     ///
     static var navigationBarLoadingIndicator: UIColor {
         if ServiceLocator.featureFlagService.isFeatureFlagEnabled(.largeTitles) {
-            return .accent
+            return .gray(.shade60)
         } else {
             return .white
         }

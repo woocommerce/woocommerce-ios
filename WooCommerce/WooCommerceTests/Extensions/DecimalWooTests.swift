208ed93758bf88896d2c5e004cf60d0cb13383dc
Paolo Musolino <info@codeido.com>
2021-04-01 17:49:11 +0200

    fix: Decimal unit tests

--- WooCommerce/WooCommerceTests/Extensions/DecimalWooTests.swift
+++ WooCommerce/WooCommerceTests/Extensions/DecimalWooTests.swift
@@ -8,11 +8,11 @@ import Foundation
 final class DecimalWooTests: XCTestCase {
 
     func test_intValue_returns_the_expected_result() {
-        XCTAssertEqual(Decimal(string: "1234.0"), 1234)
-        XCTAssertEqual(Decimal(string: "100.123456789123"), 100)
-        XCTAssertEqual(Decimal(string: "200"), 200)
-        XCTAssertEqual(Decimal(string: "2"), 2)
-        XCTAssertEqual(Decimal(string: "0"), 0)
-        XCTAssertEqual(Decimal(string: "0.2323921301"), 0)
+        XCTAssertEqual(Decimal(string: "1234.0")?.intValue, 1234)
+        XCTAssertEqual(Decimal(string: "100.123456789123")?.intValue, 100)
+        XCTAssertEqual(Decimal(string: "200")?.intValue, 200)
+        XCTAssertEqual(Decimal(string: "2")?.intValue, 2)
+        XCTAssertEqual(Decimal(string: "0")?.intValue, 0)
+        XCTAssertEqual(Decimal(string: "0.2323921301")?.intValue, 0)
     }
 }

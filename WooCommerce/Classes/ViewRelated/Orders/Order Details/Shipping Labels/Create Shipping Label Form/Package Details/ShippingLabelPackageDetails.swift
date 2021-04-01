46786991d3c38f0b5510a4ab8481947c66b61e5b
Paolo Musolino <info@codeido.com>
2021-04-01 17:24:28 +0200

    feat: updated ShippingLabelPackageDetails, where the viewModel now is an ObservedObject

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/ShippingLabelPackageDetails.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/ShippingLabelPackageDetails.swift
@@ -2,10 +2,10 @@ import SwiftUI
 import Yosemite
 
 struct ShippingLabelPackageDetails: View {
-    @State private var viewModel: ShippingLabelPackageDetailsViewModel
+    @ObservedObject private var viewModel: ShippingLabelPackageDetailsViewModel
 
     init(viewModel: ShippingLabelPackageDetailsViewModel) {
-        _viewModel = State(initialValue: viewModel)
+        _viewModel = ObservedObject(initialValue: viewModel)
     }
 
     var body: some View {
@@ -34,7 +34,7 @@ struct ShippingLabelPackageDetails: View {
                 TitleAndTextFieldRow(title: Localization.totalPackageWeight,
                                      placeholder: "0",
                                      text: .constant(""),
-                                     symbol: "oz",
+                                     symbol: viewModel.weightUnit,
                                      keyboardType: .decimalPad)
                 Divider()
 
@@ -64,8 +64,7 @@ struct ShippingLabelPackageDetails_Previews: PreviewProvider {
 
     static var previews: some View {
 
-        let viewModel = ShippingLabelPackageDetailsViewModel(items: ShippingLabelPackageDetails_Previews.sampleItems(),
-                                                             currency: ShippingLabelPackageDetails_Previews.sampleCurrency())
+        let viewModel = ShippingLabelPackageDetailsViewModel(order: ShippingLabelPackageDetails_Previews.sampleOrder())
 
         ShippingLabelPackageDetails(viewModel: viewModel)
             .environment(\.colorScheme, .light)
@@ -81,6 +80,67 @@ struct ShippingLabelPackageDetails_Previews: PreviewProvider {
 // MARK: - Private Methods
 //
 private extension ShippingLabelPackageDetails_Previews {
+
+    static func sampleOrder() -> Order {
+        return Order(siteID: 1234,
+                     orderID: 963,
+                     parentID: 0,
+                     customerID: 11,
+                     number: "963",
+                     status: .processing,
+                     currency: "USD",
+                     customerNote: "",
+                     dateCreated: date(with: "2018-04-03T23:05:12"),
+                     dateModified: date(with: "2018-04-03T23:05:14"),
+                     datePaid: date(with: "2018-04-03T23:05:14"),
+                     discountTotal: "30.00",
+                     discountTax: "1.20",
+                     shippingTotal: "0.00",
+                     shippingTax: "0.00",
+                     total: "31.20",
+                     totalTax: "1.20",
+                     paymentMethodID: "stripe",
+                     paymentMethodTitle: "Credit Card (Stripe)",
+                     items: sampleItems(),
+                     billingAddress: sampleAddress(),
+                     shippingAddress: sampleAddress(),
+                     shippingLines: sampleShippingLines(),
+                     coupons: sampleCoupons(),
+                     refunds: [],
+                     fees: [])
+    }
+
+    static func sampleAddress() -> Address {
+        return Address(firstName: "Johnny",
+                       lastName: "Appleseed",
+                       company: "",
+                       address1: "234 70th Street",
+                       address2: "",
+                       city: "Niagara Falls",
+                       state: "NY",
+                       postcode: "14304",
+                       country: "US",
+                       phone: "333-333-3333",
+                       email: "scrambled@scrambled.com")
+    }
+
+    static func sampleShippingLines() -> [ShippingLine] {
+        return [ShippingLine(shippingID: 123,
+                             methodTitle: "International Priority Mail Express Flat Rate",
+                             methodID: "usps",
+                             total: "133.00",
+                             totalTax: "0.00",
+                             taxes: [.init(taxID: 1, subtotal: "", total: "0.62125")])]
+    }
+
+    static func sampleCoupons() -> [OrderCouponLine] {
+        let coupon1 = OrderCouponLine(couponID: 894,
+                                      code: "30$off",
+                                      discount: "30",
+                                      discountTax: "1.2")
+
+        return [coupon1]
+    }
 
     static func sampleItems() -> [OrderItem] {
         let item1 = OrderItem(itemID: 890,
@@ -116,7 +176,14 @@ private extension ShippingLabelPackageDetails_Previews {
         return [item1, item2]
     }
 
-    static func sampleCurrency() -> String {
-        return "USD"
+    static func date(with dateString: String) -> Date {
+        guard let date = DateFormatter.Defaults.dateTimeFormatter.date(from: dateString) else {
+            return Date()
+        }
+        return date
+    }
+
+    static func taxes() -> [OrderItemTax] {
+        return [OrderItemTax(taxID: 75, subtotal: "0.45", total: "0.45")]
     }
 }

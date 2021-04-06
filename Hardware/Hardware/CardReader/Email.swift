d35505b9d5a7cfe39b28fd92e3f64efb232ed66c
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-04-06 07:21:24 -0400

    Point out the source of the email regex

--- Hardware/Hardware/CardReader/Email.swift
+++ Hardware/Hardware/CardReader/Email.swift
@@ -1,9 +1,10 @@
 /// A property wrapper to validate that a property is a valid email
-/// Property Wrappers can not throw, as far as I understand, so
+/// Property Wrappers can not throw, so
 /// what this wrapper does is return a nil when trying to set an invalid
 /// email address as a value of a property of type String.
 /// The reason to do this is add an extra layer of validation before passing
 /// an instance of PaymentIntentParameters to the Stripe Terminal SDK
+/// https://emailregex.com
 
 @propertyWrapper
 public struct Email<Value: StringProtocol> {
@@ -21,9 +22,9 @@ public struct Email<Value: StringProtocol> {
             value = newValue
         }
     }
-
     private func validate(email: Value?) -> Bool {
         guard let email = email else { return false }
+        // https://emailregex.com
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
         return emailPred.evaluate(with: email)

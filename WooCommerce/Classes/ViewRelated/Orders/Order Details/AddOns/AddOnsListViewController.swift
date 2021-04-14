05b2ddaf69391532aaa45e4efa82c1984000d8e6
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-04-14 18:13:56 -0500

    Move title to localization enum

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/AddOns/AddOnsListViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/AddOns/AddOnsListViewController.swift
@@ -7,12 +7,19 @@ import Yosemite
 class AddOnsListViewController: UIHostingController<AddOnsListI1View> {
     init(addOns: [OrderItemAttribute]) {
         super.init(rootView: AddOnsListI1View())
-        self.title = "Product Add-ons"
+        self.title = Localization.title
         addCloseNavigationBarButton()
     }
 
     @objc required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: Constants
+private extension AddOnsListViewController {
+    enum Localization {
+        static let title = NSLocalizedString("Product Add-ons", comment: "The title on the navigation bar when viewing an order item add-ons")
     }
 }
 

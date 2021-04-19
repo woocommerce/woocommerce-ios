0feca10203f294401031919d3d044f88a8e98f9b
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-04-19 08:04:57 -0400

    Implement first draft of ReceiptDetails

--- Hardware/Hardware/CardReader/ReceiptDetails.swift
+++ Hardware/Hardware/CardReader/ReceiptDetails.swift
@@ -1,4 +1,23 @@
 /// Receipt details associated with a card present transaction.
 public struct ReceiptDetails {
+    /// Also known as “Application Name”. Required on EMV receipts.
+    public let applicationPreferredName: String
 
+    /// Also known as “AID”. Required on EMV receipts.
+    public let dedicatedFileName: String
+
+    /// Authorization Response Code
+    public let authorizationResponseCode: String
+
+    /// Application Cryptogram
+    public let applicationCryptogram: String
+
+    /// TVR
+    public let terminalVerificationResults: String
+
+    /// TSI
+    public let transactionStatusInformation: String
+
+    /// The type of account being debited or credited
+    public let accountType: String?
 }
--- Hardware/Hardware/CardReader/StripeCardReader/ReceiptDetails+Stripe.swift
+++ Hardware/Hardware/CardReader/StripeCardReader/ReceiptDetails+Stripe.swift
@@ -8,10 +8,25 @@ extension ReceiptDetails {
         guard let details = receiptDetails else {
             return nil
         }
+
+        self.applicationPreferredName = details.applicationPreferredName
+        self.dedicatedFileName = details.dedicatedFileName
+        self.authorizationResponseCode = details.authorizationResponseCode
+        self.applicationCryptogram = details.applicationCryptogram
+        self.terminalVerificationResults = details.terminalVerificationResults
+        self.transactionStatusInformation = details.transactionStatusInformation
+        self.accountType = details.accountType
     }
 }
 
 protocol StripeReceiptDetails {
+    var applicationPreferredName: String { get }
+    var dedicatedFileName: String { get }
+    var authorizationResponseCode: String { get }
+    var applicationCryptogram: String { get }
+    var terminalVerificationResults: String { get }
+    var transactionStatusInformation: String { get }
+    var accountType: String? { get }
 }
 
 

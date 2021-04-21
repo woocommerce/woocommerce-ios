120bd6d1365fc075234562dbe0565bdc0f2eaefa
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-04-21 11:37:59 -0400

    Add tests for ReceiptStore

---
+++ Yosemite/YosemiteTests/Mocks/CardPresentPayments/MockReceiptPrinterService.swift
@@ -0,0 +1,16 @@
+@testable import Hardware
+
+
+/// Supports tests for ReceiptStore
+final class MockReceiptPrinterService: ReceiptPrinterService {
+    /// Boolean flag indicating metod printReceipt has been hit
+    var printWasCalled: Bool = false
+
+    /// The parameter passed to the printReceipt method
+    var contentProvided: ReceiptContent?
+
+    func printReceipt(content: ReceiptContent) {
+        printWasCalled = true
+        contentProvided = content
+    }
+}

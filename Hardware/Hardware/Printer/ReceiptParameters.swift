8f11f726148b1c720b775dc19b90b0bb8f3b05cb
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-04-21 11:51:55 -0400

    Add some more headerdocs

--- Hardware/Hardware/Printer/ReceiptParameters.swift
+++ Hardware/Hardware/Printer/ReceiptParameters.swift
@@ -1,6 +1,14 @@
+/// Encapsulates the information necessary to print a receipt for a
+/// card present payment
 public struct ReceiptParameters {
+    /// The total amount
     public let amount: UInt
+
+    /// The currency
     public let currency: String
+
+    /// Metadata provided by the payment processor. Contains the
+    /// to be added to the receipt required by the card networks.
     public let cardDetails: CardPresentDetails
 
     public init(amount: UInt, currency: String, cardDetails: CardPresentDetails) {
--- Yosemite/Yosemite/Actions/ReceiptAction.swift
+++ Yosemite/Yosemite/Actions/ReceiptAction.swift
@@ -1,5 +1,6 @@
 /// RefundAction: Defines all of the Actions supported by the ReceiptStore.
-///
+///  
 public enum ReceiptAction: Action {
+    /// Prints a receipt for a given `Order` with the given `ReceiptParameters`
     case print(order: Order, parameters: ReceiptParameters)
 }

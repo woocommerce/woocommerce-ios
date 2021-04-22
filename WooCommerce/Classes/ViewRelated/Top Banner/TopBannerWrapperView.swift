c6aa05ad49825dd01547432c11d08030875b17aa
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-04-22 11:19:53 -0500

    Move TopBannerViewWrapper into it's own class

---
+++ WooCommerce/Classes/ViewRelated/Top Banner/TopBannerWrapperView.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+/// Class that wraps a `TopBannerView` instance in order to provide an explicit `intrinsicContentSize`.
+/// Needed as `SwiftUI` fails to properly calculate it's dynamic height.
+///
+final class TopBannerWrapperView: UIView {
+    /// Desired `width` of the view. Needed to calculate the view dynamic `height`.
+    ///
+    let width: CGFloat
+
+    /// BannerView to wrap
+    ///
+    var bannerView: TopBannerView?
+
+    init(width: CGFloat) {
+        self.width = width
+        super.init(frame: .zero)
+    }
+
+    /// Sets the main banner view and adds it as a subview.
+    /// Discussion: The banner view is intentionally received as a function parameter(rather than in `init`) to allow consumer
+    /// references to `TopBannerWrapperView` in  view model closures.
+    ///
+    func setBanner(_ bannerView: TopBannerView) {
+        self.bannerView?.removeFromSuperview()
+
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(bannerView)
+        bannerView.pinSubviewToAllEdges(self)
+        self.bannerView = bannerView
+    }
+
+    /// Returns the preferred size of the view using on a fixed width.
+    ///
+    override var intrinsicContentSize: CGSize {
+        guard let bannerView = bannerView else {
+            return .zero
+        }
+
+        let targetSize =  CGSize(width: width, height: 0)
+        return bannerView.systemLayoutSizeFitting(targetSize, withHorizontalFittingPriority: .required, verticalFittingPriority: .defaultLow)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

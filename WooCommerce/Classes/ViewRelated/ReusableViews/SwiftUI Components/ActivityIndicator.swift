e7a0b72cab56ff5918c92327a66403a6403b3156
Paolo Musolino <info@codeido.com>
2021-04-23 14:23:21 +0200

    fix: ActivityIndicatorView become ActivityIndicator

--- WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/ActivityIndicator.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/ActivityIndicator.swift
@@ -2,23 +2,23 @@ import SwiftUI
 
 /// This is a wrapper view for using the UIActivityIndicator in SwiftUI. The native progress indicator is not available under iOS 13.
 ///
-struct ActivityIndicatorView: UIViewRepresentable {
+struct ActivityIndicator: UIViewRepresentable {
     @Binding var isAnimating: Bool
     let style: UIActivityIndicatorView.Style
 
-    func makeUIView(context: UIViewRepresentableContext<ActivityIndicatorView>) -> UIActivityIndicatorView {
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
         return UIActivityIndicatorView(style: style)
     }
 
-    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicatorView>) {
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
         isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
     }
 }
 
-struct ActivityIndicatorView_Previews: PreviewProvider {
+struct ActivityIndicator_Previews: PreviewProvider {
 
     static var previews: some View {
-        ActivityIndicatorView(isAnimating: .constant(true), style: .large)
+        ActivityIndicator(isAnimating: .constant(true), style: .large)
             .previewLayout(.fixed(width: 100, height: 100))
     }
 }

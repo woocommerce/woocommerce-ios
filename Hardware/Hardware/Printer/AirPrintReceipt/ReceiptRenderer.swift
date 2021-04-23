b4ad9a53230992b09a45f27b6ff2204b6826b383
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-04-23 05:56:27 -0400

    Exploe printing via HTML

--- Hardware/Hardware/Printer/AirPrintReceipt/ReceiptRenderer.swift
+++ Hardware/Hardware/Printer/AirPrintReceipt/ReceiptRenderer.swift
@@ -8,6 +8,8 @@ final class ReceiptRenderer: UIPrintPageRenderer {
 
     private let headerAttributes: [NSAttributedString.Key: Any] = [.font: UIFont(name: "HelveticaNeue", size: 24) as Any]
 
+    private let bodyTitleAttributes: [NSAttributedString.Key: Any] = [.font: UIFont(name: "HelveticaNeue", size: 12) as Any]
+
     private let bodyAttributes: [NSAttributedString.Key: Any] = [.font: UIFont(name: "HelveticaNeue", size: 12) as Any]
 
     init(content: ReceiptContent) {
@@ -41,31 +43,54 @@ final class ReceiptRenderer: UIPrintPageRenderer {
 
 
 private extension ReceiptRenderer {
-    enum Constants {
-        static let headerHeight: CGFloat = 80
-        static let footerHeight: CGFloat = 80
-        static let marging: CGFloat = 20
-    }
-
     private func configureHeaderAndFooter() {
         headerHeight = Constants.headerHeight
         footerHeight = Constants.footerHeight
     }
 
     private func configureFormatter() {
-        let formatter = UISimpleTextPrintFormatter(text: "\(parameters.amount / 100) \(parameters.currency.uppercased())")
+        let formatter = UIMarkupTextPrintFormatter(markupText: htmlContent())
+//        let formatter = UISimpleTextPrintFormatter(text: "\(parameters.amount / 100) \(parameters.currency.uppercased())")
         formatter.perPageContentInsets = .init(top: Constants.headerHeight, left: Constants.marging, bottom: Constants.footerHeight, right: Constants.marging)
 
         addPrintFormatter(formatter, startingAtPageAt: 0)
+    }
+
+    private func htmlContent() -> String {
+        return """
+            <p>
+                <h3>AMOUNT PAID</h3>
+                120 USD<br/>
+            </p>
+            <p>
+                <h3>DATE PAID</h3>
+                March 23, 2021<br/>
+            </p>
+            <p>
+                <h3>PAYMENT METHOD</h3>
+                - 4242<br/>
+            </p>
+        """
     }
 }
 
 
 private extension ReceiptRenderer {
+    enum Constants {
+        static let headerHeight: CGFloat = 80
+        static let footerHeight: CGFloat = 80
+        static let marging: CGFloat = 20
+    }
+
     enum Localization {
         static let receiptFromFormat = NSLocalizedString(
             "Receipt from %1$@",
             comment: "Title of receipt. Reads like Receipt from WooCommerce, Inc."
+        )
+
+        static let amountPaidSectionTitle = NSLocalizedString(
+            "Amount paid",
+            comment: "Title of 'Amount Paid' section in the receipt"
         )
     }
 }

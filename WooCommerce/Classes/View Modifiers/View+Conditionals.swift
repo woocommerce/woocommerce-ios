e97dc18d682be75eb507746b2b4eb4fdc95272d0
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-04-26 15:26:35 -0500

    Prefer Group isntead of AnyView for performance reasons

--- WooCommerce/Classes/View Modifiers/View+Conditionals.swift
+++ WooCommerce/Classes/View Modifiers/View+Conditionals.swift
@@ -5,9 +5,12 @@ extension View {
     /// If the `condition` is not met, an instance of `EmptyView` will be used in place of the receiver view.
     ///
     func renderedIf(_ condition: Bool) -> some View {
-        guard condition else {
-            return AnyView(EmptyView())
+        Group {
+            if condition {
+                self
+            } else {
+                EmptyView()
+            }
         }
-        return AnyView(self)
     }
 }

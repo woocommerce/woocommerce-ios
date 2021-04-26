0aeb66a9ed4eb405a4c1b54dc881f9acdb69c770
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-04-26 16:43:50 -0500

    renderedIf: Prefer nil instead of empty Group

--- WooCommerce/Classes/View Modifiers/View+Conditionals.swift
+++ WooCommerce/Classes/View Modifiers/View+Conditionals.swift
@@ -2,15 +2,12 @@ import SwiftUI
 
 extension View {
     /// Renders a view if the provided  `condition` is met.
-    /// If the `condition` is not met, an instance of `EmptyView` will be used in place of the receiver view.
+    /// If the `condition` is not met, an `nil`  will be used in place of the receiver view.
     ///
-    func renderedIf(_ condition: Bool) -> some View {
-        Group {
-            if condition {
-                self
-            } else {
-                EmptyView()
-            }
+    func renderedIf(_ condition: Bool) -> Self? {
+        guard condition else {
+            return nil
         }
+        return self
     }
 }

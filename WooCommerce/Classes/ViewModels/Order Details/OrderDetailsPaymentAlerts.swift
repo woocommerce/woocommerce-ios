923e5b51df0f202f92057be41994018ac15cd586
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-04-27 11:07:48 -0400

    Add second view model for when the reader requests a tap

--- WooCommerce/Classes/ViewModels/Order Details/OrderDetailsPaymentAlerts.swift
+++ WooCommerce/Classes/ViewModels/Order Details/OrderDetailsPaymentAlerts.swift
@@ -9,30 +9,27 @@ import WordPressUI
 /// https://github.com/woocommerce/woocommerce-ios/issues/3980
 final class OrderDetailsPaymentAlerts {
     //private var alertController: FancyAlertViewController?
-    private var modalController: UIViewController?
-    //private var name: String
-    //private var amount: String
+    private var modalController: CardPresentPaymentsModalViewController?
+    private var name: String = ""
+    private var amount: String = ""
 
     func readerIsReady(from: UIViewController, title: String, amount: String) {
-        //self.name = title
-        //self.amount = amount
+        self.name = title
+        self.amount = amount
 
         // Initial presentation of the modal view controller. We need to provide
         // a customer name and an amount.
-        let viewModel = readerIsReady(name: title, amount: amount)
+        let viewModel = readerIsReady()
         let newAlert = CardPresentPaymentsModalViewController(viewModel: viewModel)
         modalController = newAlert
         modalController?.modalPresentationStyle = .custom
         modalController?.transitioningDelegate = AppDelegate.shared.tabBarController
         from.present(newAlert, animated: true)
-//        let newAlert = FancyAlertViewController.makeCollectPaymentAlert(name: title, amount: amount, image: .cardPresentImage)
-//        alertController = newAlert
-//        alertController?.modalPresentationStyle = .custom
-//        alertController?.transitioningDelegate = AppDelegate.shared.tabBarController
-//        from.present(newAlert, animated: true)
     }
 
     func tapOrInsertCard() {
+        let viewModel = tapOrInsert()
+        modalController?.setViewModel(viewModel)
 //        let newConfiguraton = FancyAlertViewController.configurationForTappingCard(amount: amount ?? "")
 //        alertController?.setViewConfiguration(newConfiguraton, animated: false)
     }
@@ -62,7 +59,11 @@ final class OrderDetailsPaymentAlerts {
 
 
 private extension OrderDetailsPaymentAlerts {
-    func readerIsReady(name: String, amount: String) -> CardPresentPaymentsModalViewModel {
+    func readerIsReady() -> CardPresentPaymentsModalViewModel {
         CardPresentModalReaderIsReady(name: name, amount: amount)
+    }
+
+    func tapOrInsert() -> CardPresentPaymentsModalViewModel {
+        CardPresentModalTapCard(name: name, amount: amount)
     }
 }

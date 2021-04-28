5f7fccf4b66f80c28199fd3abf2b2d911cbbc11a
Huong Do <hi.huongdo@gmail.com>
2021-04-28 10:00:17 +0700

    Update check to show more button only when product has attributes but no variations

--- WooCommerce/Classes/ViewRelated/Products/Variations/ProductVariationsViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Variations/ProductVariationsViewModel.swift
@@ -46,6 +46,6 @@ extension ProductVariationsViewModel {
     /// Defines if the More Options button should be shown
     ///
     func shouldShowMoreButton(for product: Product) -> Bool {
-        product.attributesForVariations.isNotEmpty
+        product.variations.isEmpty && product.attributesForVariations.isNotEmpty
     }
 }

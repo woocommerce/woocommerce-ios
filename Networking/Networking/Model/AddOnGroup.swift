6748418b3b59b92bb3ee2f3d2040af15cdd230c2
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-04-29 11:28:35 -0500

    Update add-on properties

--- Networking/Networking/Model/AddOnGroup.swift
+++ Networking/Networking/Model/AddOnGroup.swift
@@ -3,4 +3,24 @@ import Foundation
 /// Represents a `AddOnGroup` entity that groups global add-ons.
 ///
 public struct AddOnGroup: Codable, Equatable, GeneratedCopiable, GeneratedFakeable {
+
+    /// SiteID
+    ///
+    let siteID: Int64
+
+    /// Add-on group ID
+    ///
+    let groupID: Int64
+
+    /// Name of the group
+    ///
+    let name: String
+
+    /// Priority of the group
+    ///
+    let priority: Int64
+
+    /// Associated global add-ons
+    ///
+    let addOns: [ProductAddOn]
 }

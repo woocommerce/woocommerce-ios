0b35ee9c9e1b5222d01cac098314398882fc83e4
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-04-29 17:27:29 -0500

    Added empty AddOnGroup remote tests

---
+++ Networking/NetworkingTests/Remote/AddOnGroupRemoteTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+@testable import Networking
+
+final class AddOnGroupRemoteTests: XCTestCase {
+
+    /// Dummy Network Wrapper
+    ///
+    private var network: MockNetwork!
+
+    /// Dummy Site ID
+    ///
+    private let sampleSiteID: Int64 = 1234
+
+    override func setUp() {
+        super.setUp()
+        network = MockNetwork()
+    }
+
+    override func tearDown() {
+        network = nil
+        super.tearDown()
+    }
+}

77c65531c65f1733668462da7fa4841d0b701a90
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-04-29 11:36:20 -0500

    Update GroupAddOn with custom decoding

--- Networking/Networking/Model/AddOnGroup.swift
+++ Networking/Networking/Model/AddOnGroup.swift
@@ -24,3 +24,31 @@ public struct AddOnGroup: Codable, Equatable, GeneratedCopiable, GeneratedFakeab
     ///
     let addOns: [ProductAddOn]
 }
+
+// MARK: Decoding
+extension AddOnGroup {
+    enum CodingKeys: String, CodingKey {
+        case groupID = "id"
+        case name
+        case priority
+        case addOns = "fields"
+    }
+
+    enum DecodingError: Error {
+        case missingSiteID
+    }
+
+    public init(from decoder: Decoder) throws {
+        guard let siteID = decoder.userInfo[.siteID] as? Int64 else {
+            throw ProductCategoryDecodingError.missingSiteID
+        }
+
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.init(siteID: siteID,
+                  groupID: try container.decode(Int64.self, forKey: .groupID),
+                  name: try container.decode(String.self, forKey: .name),
+                  priority: try container.decode(Int64.self, forKey: .priority),
+                  addOns: try container.decode([ProductAddOn].self, forKey: .addOns))
+    }
+}

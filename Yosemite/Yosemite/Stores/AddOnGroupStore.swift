0c8aae365757645ed6a8a62324e47e3cfb504c96
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-04-30 15:26:03 -0500

    Update AddOngroupStore to fetch add-on groups. Storing them is pending

--- Yosemite/Yosemite/Stores/AddOnGroupStore.swift
+++ Yosemite/Yosemite/Stores/AddOnGroupStore.swift
@@ -29,7 +29,47 @@ public final class AddOnGroupStore: Store {
 
         switch action {
         case let .synchronizeAddOnGroups(siteID, onCompletion):
-            break
+            synchronizeAddOnGroups(siteID: siteID, onCompletion: onCompletion)
         }
+    }
+}
+
+// MARK: Services
+private extension AddOnGroupStore {
+    /// Downloads and stores all add-on groups for a given `siteID`.
+    ///
+    func synchronizeAddOnGroups(siteID: Int64, onCompletion: @escaping (Result<Void, Error>) -> Void) {
+        remote.loadAddOnGroups(siteID: siteID) { [weak self] result in
+            switch result {
+            case .success(let groups):
+                self?.upsertAddOnGroupsInBackground(readOnlyAddOnGroups: groups, onCompletion: onCompletion)
+            case .failure(let error):
+                onCompletion(.failure(error))
+            }
+        }
+    }
+}
+
+// MARK: Storage
+private extension AddOnGroupStore {
+    /// Updates (OR Inserts) the provided ReadOnly `AddOnGroups` entities *in a background thread*.
+    /// onCompletion will be called on the main thread!
+    ///
+    func upsertAddOnGroupsInBackground(readOnlyAddOnGroups: [AddOnGroup], onCompletion: @escaping (Result<Void, Error>) -> Void) {
+        let derivedStorage = storageManager.writerDerivedStorage
+        derivedStorage.perform {
+            self.upsertAddOnGroups(readOnlyAddOnGroups: readOnlyAddOnGroups, in: derivedStorage)
+        }
+        storageManager.saveDerivedType(derivedStorage: derivedStorage) {
+            DispatchQueue.main.async {
+                onCompletion(.success(()))
+            }
+        }
+    }
+
+    /// Updates (OR Inserts) the specified ReadOnly `AddOnGroups` entities into the Storage Layer.
+    ///
+    func upsertAddOnGroups(readOnlyAddOnGroups: [AddOnGroup], in storage: StorageType) {
+        // TODO: Add implementation
     }
 }

43db83c2fa066bfab7dd352b7f0df78269e0d63e
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-04-30 15:04:38 -0500

    Adds empty AddOnGroupStore

--- Yosemite/Yosemite/Stores/AddOnGroupStore.swift
+++ Yosemite/Yosemite/Stores/AddOnGroupStore.swift
@@ -2,5 +2,34 @@ import Foundation
 import Networking
 import Storage
 
+/// Implements actions from `AddOnGroupAction`
+///
 public final class AddOnGroupStore: Store {
+
+    /// Remote source
+    ///
+    private let remote: AddOnGroupRemote
+
+    public override init(dispatcher: Dispatcher, storageManager: StorageManagerType, network: Network) {
+        self.remote = AddOnGroupRemote(network: network)
+        super.init(dispatcher: dispatcher, storageManager: storageManager, network: network)
+    }
+
+    override public func registerSupportedActions(in dispatcher: Dispatcher) {
+        dispatcher.register(processor: self, for: AddOnGroupAction.self)
+    }
+
+    /// Receives and executes actions.
+    ///
+    override public func onAction(_ action: Action) {
+        guard let action = action as? AddOnGroupAction else {
+            assertionFailure("ProductCategoryStore received an unsupported action")
+            return
+        }
+
+        switch action {
+        case let .synchronizeAddOnGroups(siteID, onCompletion):
+            break
+        }
+    }
 }

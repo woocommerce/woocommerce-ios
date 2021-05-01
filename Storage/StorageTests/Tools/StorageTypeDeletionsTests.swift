63f8d207f5c67b86cc0dd53c42d08b6079e92efb
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-05-01 15:33:04 -0500

    Adds function to delete stale add-on-goups

---
+++ Storage/StorageTests/Tools/StorageTypeDeletionsTests.swift
@@ -0,0 +1,50 @@
+import XCTest
+@testable import Storage
+
+class StorageTypeDeletionsTests: XCTestCase {
+
+    private let sampleSiteID: Int64 = 98765
+    private var storageManager: StorageManagerType!
+
+    private var storage: StorageType! {
+        storageManager.viewStorage
+    }
+
+    override func setUp() {
+        super.setUp()
+        storageManager = CoreDataManager(name: "WooCommerce", crashLogger: MockCrashLogger())
+    }
+
+    override func tearDown() {
+        storageManager.reset()
+        storageManager = nil
+        super.tearDown()
+    }
+
+    func test_deleteStaleAddOnGroups_does_not_delete_active_addOns() throws {
+        // Given
+        let initialGroups: [AddOnGroup] = [
+            createAddOnGroup(groupID: 123),
+            createAddOnGroup(groupID: 1234),
+            createAddOnGroup(groupID: 12345)
+        ]
+
+        // When
+        storage.deleteStaleAddOnGroups(siteID: sampleSiteID, activeGroupIDs: [123, 1234])
+
+        // Then
+        let activeGroups = storage.loadAddOnGroups(siteID: sampleSiteID)
+        XCTAssertEqual(activeGroups, initialGroups.dropLast())
+    }
+}
+
+private extension StorageTypeDeletionsTests {
+    /// Inserts and creates an `AddOnGroup` ready to be used on tests.
+    ///
+    func createAddOnGroup(groupID: Int64) -> AddOnGroup {
+        let addOnGroup = storage.insertNewObject(ofType: AddOnGroup.self)
+        addOnGroup.siteID = sampleSiteID
+        addOnGroup.groupID = groupID
+        return addOnGroup
+    }
+}

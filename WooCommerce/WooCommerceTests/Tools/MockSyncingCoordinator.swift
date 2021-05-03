1147862b3182c8a97dcfe867d2358e8503e6fe67
joshheald-a8c <80399824+joshheald-a8c@users.noreply.github.com>
2021-05-03 10:15:17 +0100

    Add MockSyncingCoordinator

---
+++ WooCommerce/WooCommerceTests/Tools/MockSyncingCoordinator.swift
@@ -0,0 +1,30 @@
+import Foundation
+import WooCommerce
+
+final class MockSyncingCoordinator: SyncingCoordinatorProtocol {
+    var spyDidCallEnsureNextPageIsSynchronized = false
+    var spyEnsureNextPageIsSynchronizedLastVisibleIndex: Int?
+
+    var spyDidCallResynchronize = false
+    var spyResynchronizeReason: String?
+
+    var spyDidCallSynchronizeFirstPage = false
+    var spySynchronizeFirstPageReason: String?
+
+    func ensureNextPageIsSynchronized(lastVisibleIndex: Int) {
+        spyDidCallEnsureNextPageIsSynchronized = true
+        spyEnsureNextPageIsSynchronizedLastVisibleIndex = lastVisibleIndex
+    }
+
+    func resynchronize(reason: String?, onCompletion: (() -> Void)?) {
+        spyDidCallResynchronize = true
+        spyResynchronizeReason = reason
+        onCompletion?()
+    }
+
+    func synchronizeFirstPage(reason: String?, onCompletion: (() -> Void)?) {
+        spyDidCallSynchronizeFirstPage = true
+        spySynchronizeFirstPageReason = reason
+        onCompletion?()
+    }
+}

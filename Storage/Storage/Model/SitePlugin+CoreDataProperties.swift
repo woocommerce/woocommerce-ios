05713c66aa92b034469f454c4bf86a44776cc436
Huong Do <huongdt29@gmail.com>
2021-05-05 22:52:34 +0700

    Make SitePlugin entity properties non-optional

--- Storage/Storage/Model/SitePlugin+CoreDataProperties.swift
+++ Storage/Storage/Model/SitePlugin+CoreDataProperties.swift
@@ -9,18 +9,18 @@ extension SitePlugin {
     }
 
     @NSManaged public var siteID: Int64
-    @NSManaged public var plugin: String?
-    @NSManaged public var status: String?
-    @NSManaged public var name: String?
-    @NSManaged public var pluginUri: String?
-    @NSManaged public var author: String?
-    @NSManaged public var authorUri: String?
-    @NSManaged public var descriptionRaw: String?
-    @NSManaged public var descriptionRendered: String?
-    @NSManaged public var version: String?
+    @NSManaged public var plugin: String
+    @NSManaged public var status: String
+    @NSManaged public var name: String
+    @NSManaged public var pluginUri: String
+    @NSManaged public var author: String
+    @NSManaged public var authorUri: String
+    @NSManaged public var descriptionRaw: String
+    @NSManaged public var descriptionRendered: String
+    @NSManaged public var version: String
     @NSManaged public var networkOnly: Bool
-    @NSManaged public var requiresWPVersion: String?
-    @NSManaged public var requiresPHPVersion: String?
-    @NSManaged public var textDomain: String?
+    @NSManaged public var requiresWPVersion: String
+    @NSManaged public var requiresPHPVersion: String
+    @NSManaged public var textDomain: String
 
 }

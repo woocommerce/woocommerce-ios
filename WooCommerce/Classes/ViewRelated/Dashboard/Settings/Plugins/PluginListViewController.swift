f4fee78ae8d6ca21294f4f66c237e25ab3748afb
Huong Do <hi.huongdo@gmail.com>
2021-05-08 17:09:56 +0700

    Inject view model to PluginListViewController through init

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Plugins/PluginListViewController.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Plugins/PluginListViewController.swift
@@ -2,6 +2,17 @@ import UIKit
 
 class PluginListViewController: UIViewController {
 
+    private let viewModel: PluginListViewModel
+
+    init?(coder: NSCoder, viewModel: PluginListViewModel) {
+        self.viewModel = viewModel
+        super.init(coder: coder)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("⛔️ You must create this view controller with a view model!")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }

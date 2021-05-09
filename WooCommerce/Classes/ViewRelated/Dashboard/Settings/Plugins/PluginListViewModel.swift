d1aa9b16ed4652d15a90b47f16f9a7c7b2551f23
Huong Do <huongdt29@gmail.com>
2021-05-09 12:26:39 +0700

    Update PluginListViewModel to provide data for table view

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Plugins/PluginListViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Plugins/PluginListViewModel.swift
@@ -14,7 +14,7 @@ final class PluginListViewModel {
     /// Reference to the StoresManager to dispatch Yosemite Actions.
     ///
     private let storesManager: StoresManager
-    
+
     /// Results controller for the plugin list
     ///
     private lazy var resultsController: ResultsController<StorageSitePlugin> = {
@@ -34,11 +34,50 @@ final class PluginListViewModel {
         self.siteID = siteID
         self.storesManager = storesManager
     }
+    
+    /// Start fetching plugin data from local storage.
+    ///
+    func activate() {
+        do {
+            try resultsController.performFetch()
+        } catch {
+            DDLogError("⛔️ Error fetching plugin list!")
+        }
+    }
 
-    func syncPlugins() {
+    /// Manually resync plugin list.
+    ///
+    func resyncPlugins() {
         let action = SitePluginAction.synchronizeSitePlugins(siteID: siteID) { [weak self] result in
             self?.shouldShowErrorState = result.isFailure
         }
         storesManager.dispatch(action)
     }
 }
+
+extension PluginListViewModel {
+    /// Number of sections to display on the table view.
+    ///
+    var numberOfSections: Int {
+        resultsController.sections.count
+    }
+
+    /// Title of table view section at specified index.
+    ///
+    func titleForSection(at index: Int) -> String? {
+        resultsController.sections[safe: index]?.name.capitalized
+    }
+
+    /// Number of rows in a specified table view section index.
+    ///
+    func numberOfRows(inSection sectionIndex: Int) -> Int {
+        resultsController.sections[safe: sectionIndex]?.objects.count ?? 0
+    }
+
+    /// View model for the table view cell at specified index path.
+    ///
+    func cellModelForRow(at indexPath: IndexPath) -> PluginListCellViewModel {
+        let plugin = resultsController.object(at: indexPath)
+        return PluginListCellViewModel(name: plugin.name, description: plugin.descriptionRaw)
+    }
+ }

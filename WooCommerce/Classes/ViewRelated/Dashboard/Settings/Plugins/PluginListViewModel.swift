59167d34fb5f132dc3f69a30d0c9db84a251923f
Huong Do <huongdt29@gmail.com>
2021-05-09 09:16:42 +0700

    Add syncPlugins method to PluginListViewModel

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Plugins/PluginListViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Plugins/PluginListViewModel.swift
@@ -3,11 +3,25 @@ import Yosemite
 
 final class PluginListViewModel {
 
+    /// Whether synchronization failed and error state should be displayed
+    ///
+    @Published var shouldShowErrorState: Bool = false
+
     /// Reference to the StoresManager to dispatch Yosemite Actions.
     ///
     private let storesManager: StoresManager
 
     init(storesManager: StoresManager = ServiceLocator.stores) {
         self.storesManager = storesManager
+    }
+
+    func syncPlugins() {
+        guard let id = storesManager.sessionManager.defaultStoreID else {
+            return
+        }
+        let action = SitePluginAction.synchronizeSitePlugins(siteID: id) { [weak self] result in
+            self?.shouldShowErrorState = result.isFailure
+        }
+        storesManager.dispatch(action)
     }
 }

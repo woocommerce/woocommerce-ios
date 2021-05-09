13e2c3ba83a2f447882217cd1ac3c1f684c4a6b1
Huong Do <huongdt29@gmail.com>
2021-05-09 09:38:03 +0700

    Add results controller for plugin list

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Plugins/PluginListViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Plugins/PluginListViewModel.swift
@@ -14,6 +14,21 @@ final class PluginListViewModel {
     /// Reference to the StoresManager to dispatch Yosemite Actions.
     ///
     private let storesManager: StoresManager
+    
+    /// Results controller for the plugin list
+    ///
+    private lazy var resultsController: ResultsController<StorageSitePlugin> = {
+        let storage = ServiceLocator.storageManager
+        let predicate = NSPredicate(format: "siteID = %ld", self.siteID)
+        let nameDescriptor = NSSortDescriptor(keyPath: \StorageSitePlugin.name, ascending: true)
+        let statusDescriptor = NSSortDescriptor(keyPath: \StorageSitePlugin.status, ascending: true)
+        return ResultsController<StorageSitePlugin>(
+            storageManager: storage,
+            sectionNameKeyPath: "status",
+            matching: predicate,
+            sortedBy: [nameDescriptor, statusDescriptor]
+        )
+    }()
 
     init(siteID: Int64, storesManager: StoresManager = ServiceLocator.stores) {
         self.siteID = siteID

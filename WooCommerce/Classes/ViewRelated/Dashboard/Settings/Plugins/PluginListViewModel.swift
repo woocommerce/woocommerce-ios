2a34908afcad792864983c2f56a3a5b213f30887
Huong Do <huongdt29@gmail.com>
2021-05-10 22:59:13 +0700

    Update PluginListViewModel with data source for plugin list

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Plugins/PluginListViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Plugins/PluginListViewModel.swift
@@ -15,7 +15,7 @@ final class PluginListViewModel {
     /// StorageManager to load plugins from storage
     ///
     private let storageManager: StorageManagerType
-    
+
     /// Results controller for the plugin list
     ///
     private lazy var resultsController: ResultsController<StorageSitePlugin> = {
@@ -38,7 +38,7 @@ final class PluginListViewModel {
         self.storesManager = storesManager
         self.storageManager = storageManager
     }
-    
+
     /// Start fetching plugin data from local storage.
     ///
     func activate(onDataChanged: @escaping () -> Void) {
@@ -51,3 +51,45 @@ final class PluginListViewModel {
         resultsController.onDidChangeContent = onDataChanged
     }
 }
+
+// MARK: - Data source for plugin list
+//
+extension PluginListViewModel {
+    /// Number of sections to display on the table view.
+    ///
+    var numberOfSections: Int {
+        resultsController.sections.count
+    }
+
+    /// Title of table view section at specified index.
+    ///
+    func titleForSection(at index: Int) -> String? {
+        // TODO: update this
+        return nil
+    }
+
+    /// Number of rows in a specified table view section index.
+    ///
+    func numberOfRows(inSection sectionIndex: Int) -> Int {
+        resultsController.sections[safe: sectionIndex]?.objects.count ?? 0
+    }
+
+    /// View model for the table view cell at specified index path.
+    ///
+    func cellModelForRow(at indexPath: IndexPath) -> PluginListCellViewModel {
+        let plugin = resultsController.object(at: indexPath)
+        // The description can sometimes contain HTML tags
+        // so it's best to be extra safe by removing those tags
+        return PluginListCellViewModel(
+            name: plugin.name,
+            description: plugin.descriptionRaw.removedHTMLTags
+        )
+    }
+}
+
+// MARK: - Model for plugin list cells
+//
+struct PluginListCellViewModel {
+     let name: String
+     let description: String
+ }

c4425ef784b8e0b85d451d4ff06064b75f731e38
Huong Do <huongdt29@gmail.com>
2021-05-10 23:19:50 +0700

    Add unit tests for PluginListViewModel

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Plugins/SitePluginListViewModelTests.swift
@@ -0,0 +1,69 @@
+import XCTest
+@testable import Storage
+@testable import WooCommerce
+@testable import Yosemite
+
+class PluginListViewModelTests: XCTestCase {
+
+    private let sampleSiteID: Int64 = 134
+
+    /// Mock Storage: InMemory
+    ///
+    private var storageManager: StorageManagerType!
+
+    /// View storage for tests
+    ///
+    private var storage: StorageType {
+        storageManager.viewStorage
+    }
+
+    override func setUp() {
+        super.setUp()
+        storageManager = MockStorageManager()
+    }
+
+    override func tearDown() {
+        storageManager = nil
+        super.tearDown()
+    }
+
+    func test_section_and_row_details_are_correct_after_activation() {
+        // Given
+        let activePlugin = SitePlugin.fake().copy(
+            siteID: sampleSiteID,
+            status: .active,
+            name: "BBB",
+            descriptionRaw: "Lorem ipsum <strong>random HTML content</strong>"
+        )
+        insert(activePlugin)
+
+        let inactivePlugin1 = SitePlugin.fake().copy(siteID: sampleSiteID, status: .inactive, name: "CCC")
+        insert(inactivePlugin1)
+
+        let inactivePlugin2 = SitePlugin.fake().copy(siteID: sampleSiteID, status: .inactive, name: "AAA")
+        insert(inactivePlugin2)
+
+        let viewModel = PluginListViewModel(siteID: sampleSiteID, storageManager: storageManager)
+
+        // When
+        viewModel.activate {}
+
+        // Then
+        XCTAssertEqual(viewModel.numberOfSections, 2)
+        XCTAssertEqual(viewModel.titleForSection(at: 0), "Active Plugins")
+        XCTAssertEqual(viewModel.titleForSection(at: 1), "Inactive Plugins")
+        XCTAssertEqual(viewModel.numberOfRows(inSection: 0), 1)
+        XCTAssertEqual(viewModel.numberOfRows(inSection: 1), 2)
+        XCTAssertEqual(viewModel.cellModelForRow(at: IndexPath(row: 0, section: 0)).name, activePlugin.name)
+        XCTAssertEqual(viewModel.cellModelForRow(at: IndexPath(row: 0, section: 0)).description, "Lorem ipsum random HTML content")
+        XCTAssertEqual(viewModel.cellModelForRow(at: IndexPath(row: 0, section: 1)).name, inactivePlugin2.name)
+        XCTAssertEqual(viewModel.cellModelForRow(at: IndexPath(row: 1, section: 1)).name, inactivePlugin1.name)
+    }
+}
+
+private extension PluginListViewModelTests {
+    func insert(_ readOnlyPlugin: Yosemite.SitePlugin) {
+        let plugin = storage.insertNewObject(ofType: StorageSitePlugin.self)
+        plugin.update(with: readOnlyPlugin)
+    }
+}

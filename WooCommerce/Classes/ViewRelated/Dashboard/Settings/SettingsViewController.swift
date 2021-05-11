e811f7aeffd0a284baf1368677c9468946e2f4ad
Huong Do <huongdt29@gmail.com>
2021-05-11 20:34:30 +0700

    Revert "Update UIStoryboard extension with method to accept creator block"
    This reverts commit 5191d3cb08a0f1d8d878b6680b7196a3d6a91069.

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/SettingsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/SettingsViewController.swift
@@ -390,31 +390,41 @@ private extension SettingsViewController {
 
     func supportWasPressed() {
         ServiceLocator.analytics.track(.settingsContactSupportTapped)
-        let viewController = UIStoryboard.dashboard.instantiateViewController(ofClass: HelpAndSupportViewController.self)
+        guard let viewController = UIStoryboard.dashboard.instantiateViewController(ofClass: HelpAndSupportViewController.self) else {
+            fatalError("Cannot instantiate `HelpAndSupportViewController` from Dashboard storyboard")
+        }
         show(viewController, sender: self)
     }
 
     func cardReadersWasPressed() {
         ServiceLocator.analytics.track(.settingsCardReadersTapped)
-        let viewController = UIStoryboard.dashboard.instantiateViewController(ofClass: CardReaderSettingsViewController.self)
+        guard let viewController = UIStoryboard.dashboard.instantiateViewController(ofClass: CardReaderSettingsViewController.self) else {
+            fatalError("Cannot instantiate `CardReaderSettingsViewController` from Dashboard storyboard")
+        }
         show(viewController, sender: self)
     }
 
     func privacyWasPressed() {
         ServiceLocator.analytics.track(.settingsPrivacySettingsTapped)
-        let viewController = UIStoryboard.dashboard.instantiateViewController(ofClass: PrivacySettingsViewController.self)
+        guard let viewController = UIStoryboard.dashboard.instantiateViewController(ofClass: PrivacySettingsViewController.self) else {
+            fatalError("Cannot instantiate `PrivacySettingsViewController` from Dashboard storyboard")
+        }
         show(viewController, sender: self)
     }
 
     func aboutWasPressed() {
         ServiceLocator.analytics.track(.settingsAboutLinkTapped)
-        let viewController = UIStoryboard.dashboard.instantiateViewController(ofClass: AboutViewController.self)
+        guard let viewController = UIStoryboard.dashboard.instantiateViewController(ofClass: AboutViewController.self) else {
+            fatalError("Cannot instantiate `AboutViewController` from Dashboard storyboard")
+        }
         show(viewController, sender: self)
     }
 
     func licensesWasPressed() {
         ServiceLocator.analytics.track(.settingsLicensesLinkTapped)
-        let viewController = UIStoryboard.dashboard.instantiateViewController(ofClass: LicensesViewController.self)
+        guard let viewController = UIStoryboard.dashboard.instantiateViewController(ofClass: LicensesViewController.self) else {
+            fatalError("Cannot instantiate `LicensesViewController` from Dashboard storyboard")
+        }
         show(viewController, sender: self)
     }
 

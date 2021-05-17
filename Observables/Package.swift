e8a91fcf2fa713b1a3980e8b4d25da513f0137d3
Jorge Bernal <jorge@automattic.com>
2021-05-17 10:44:54 +0200

    Bump deployment target to iOS 14.0

--- Observables/Package.swift
+++ Observables/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Observables",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+        .macOS(.v10_14), .iOS(.v14), .tvOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.

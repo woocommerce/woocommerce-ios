2755f97001bdd9f20cda92b3a5bd470e1dddb772
rachelmcr <rachel@automattic.com>
2021-05-19 18:55:56 +0100

    Add reusable SwiftUI row with title and toggle

---
+++ WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/TitleAndToggleRow.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+/// Renders a row with a label on the left side, and a toggle switch on the right side
+///
+struct TitleAndToggleRow: View {
+    let title: String
+    @Binding var isOn: Bool
+
+    var body: some View {
+        Toggle(title, isOn: $isOn)
+            .bodyStyle()
+            .padding(Constants.padding)
+    }
+}
+
+private extension TitleAndToggleRow {
+    enum Constants {
+        static let padding: CGFloat = 16
+    }
+}
+
+struct TitleAndToggleRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleAndToggleRow(title: "Toggle this setting", isOn: .constant(false))
+            .previewLayout(.fixed(width: 375, height: 100))
+            .previewDisplayName("Single line of text")
+
+        TitleAndToggleRow(title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum ullamcorper enim.", isOn: .constant(true))
+            .previewLayout(.fixed(width: 375, height: 100))
+            .previewDisplayName("Multiple lines of text")
+    }
+}

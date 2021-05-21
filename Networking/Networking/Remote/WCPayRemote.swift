61790500c8f33b2d9961970e434d6de7c88617bd
Allen Snook <allendav@automattic.com>
2021-05-21 10:55:27 -0700

    Request and parse the card_present_eligible flag

--- Networking/Networking/Remote/WCPayRemote.swift
+++ Networking/Networking/Remote/WCPayRemote.swift
@@ -74,7 +74,7 @@ private extension WCPayRemote {
     enum AccountParameterValues {
         static let fieldValues: String = """
             status,has_pending_requirements,has_overdue_requirements,current_deadline,\
-            statement_descriptor,store_currencies,country
+            statement_descriptor,store_currencies,country,card_present_eligible
             """
     }
 

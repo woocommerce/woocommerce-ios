07427103fa0bf735841c82e986d984ce2cf94db5
Allen Snook <allendav@automattic.com>
2021-06-09 13:42:14 -0700

    Add capture action to store

--- Yosemite/Yosemite/Actions/PaymentGatewayAccountAction.swift
+++ Yosemite/Yosemite/Actions/PaymentGatewayAccountAction.swift
@@ -4,7 +4,13 @@ import Networking
 /// Defines all of the `actions` supported by the `PaymentGatewayAccountStore`.
 ///
 public enum PaymentGatewayAccountAction: Action {
-    /// Retrieves and stores payment gateway accounts for the provided `siteID`
+    /// Retrieves and stores payment gateway account(s) for the provided `siteID`
     ///
-    case loadAccounts(siteID: Int64, onCompletion: (Result<[PaymentGatewayAccount], Error>) -> Void)
+    case loadAccounts(siteID: Int64, onCompletion: (Result<Void, Error>) -> Void)
+
+    /// Captures a payment intent ID, associated to an order and site
+    case captureOrderPayment(siteID: Int64,
+                             orderID: Int64,
+                             paymentIntentID: String,
+                             completion: (Result<Void, Error>) -> Void)
 }
--- Yosemite/Yosemite/Stores/PaymentGatewayAccountStore.swift
+++ Yosemite/Yosemite/Stores/PaymentGatewayAccountStore.swift
@@ -31,18 +31,27 @@ public final class PaymentGatewayAccountStore: Store {
         }
 
         switch action {
-        case let .loadAccounts(siteID, onCompletion):
-            loadAccounts(siteID: siteID, onCompletion: onCompletion)
+        case .loadAccounts(let siteID, let onCompletion):
+            loadAccounts(siteID: siteID,
+                         onCompletion: onCompletion)
+        case .captureOrderPayment(let siteID,
+                                  let orderID,
+                                  let paymentIntentID,
+                                  let onCompletion):
+            captureOrderPayment(siteID: siteID,
+                                orderID: orderID,
+                                paymentIntentID: paymentIntentID,
+                                onCompletion: onCompletion)
         }
     }
 }
 
-// MARK: Storage Methods
+// MARK: Networking Methods
 private extension PaymentGatewayAccountStore {
-
-    func loadAccounts(siteID: Int64, onCompletion: @escaping (Result<[PaymentGatewayAccount], Error>) -> Void) {
-
-        /// The only accounts we know about right now are WCPayAccounts
+    func loadAccounts(siteID: Int64, onCompletion: @escaping (Result<Void, Error>) -> Void) {
+        /// The only plugin we support payment gateway accounts for right now is WooCommerce Payments.
+        /// And there is only one account per site for that plugin.  In the future we will need to support remotes for
+        /// other plugins and it might be possible for there to be multiple accounts for a single site then.
         ///
         remote.loadAccount(for: siteID) { [weak self] result in
             guard let self = self else {
@@ -53,17 +62,43 @@ private extension PaymentGatewayAccountStore {
             case .success(let wcpayAccount):
                 let account = wcpayAccount.toPaymentGatewayAccount(siteID: siteID)
                 self.upsertStoredAccountInBackground(readonlyAccount: account)
-                onCompletion(.success([account]))
+                onCompletion(.success(()))
                     return
             case .failure(let error):
                 self.deleteStaleAccount(siteID: siteID, gatewayID: "woocommerce-payments") // TODO make a constant/enum
                 onCompletion(.failure(error))
                 return
             }
-
         }
     }
 
+    func captureOrderPayment(siteID: Int64,
+                             orderID: Int64,
+                             paymentIntentID: String,
+                             onCompletion: @escaping (Result<Void, Error>) -> Void) {
+        /// The only plugin we support capturing payments with right now is WooCommerce Payments.
+        /// In the future we will need to support remotes for other plugins.
+        ///
+        remote.captureOrderPayment(for: siteID, orderID: orderID, paymentIntentID: paymentIntentID, completion: { result in
+            switch result {
+            case .success(let intent):
+                guard intent.status == .succeeded else {
+                    DDLogDebug("Unexpected payment intent status \(intent.status) after attempting capture")
+                    onCompletion(.failure(CardReaderServiceError.paymentCapture()))
+                    return
+                }
+
+                onCompletion(.success(()))
+            case .failure(let error):
+                onCompletion(.failure(error))
+                return
+            }
+        })
+    }
+}
+
+// MARK: Storage Methods
+private extension PaymentGatewayAccountStore {
     func upsertStoredAccountInBackground(readonlyAccount: PaymentGatewayAccount) {
         let storage = storageManager.viewStorage
         let storageAccount = storage.loadPaymentGatewayAccount(siteID: readonlyAccount.siteID, gatewayID: readonlyAccount.gatewayID) ??

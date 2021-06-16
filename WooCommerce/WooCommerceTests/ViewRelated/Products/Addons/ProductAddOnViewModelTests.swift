f99b50714b95888f73cdbbca87116a651fc3bf84
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-06-16 16:36:45 -0500

    Add producrt add-on view model tests

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Addons/ProductAddOnViewModelTests.swift
@@ -0,0 +1,31 @@
+import XCTest
+import TestKit
+
+@testable import WooCommerce
+
+class ProductAddOnViewModelTests: XCTestCase {
+
+    func test_view_model_hides_description_and_price_when_empty() {
+        // Given
+        let viewModel = ProductAddOnViewModel(name: "", description: "", price: "", options: [
+            .init(name: "", price: "")
+        ])
+
+        // Then & When
+        XCTAssertFalse(viewModel.showDescription)
+        XCTAssertFalse(viewModel.showPrice)
+        XCTAssertFalse(viewModel.options[0].showPrice)
+    }
+
+    func test_view_model_shows_description_and_price_when_not_empty() {
+        // Given
+        let viewModel = ProductAddOnViewModel(name: "", description: "Description", price: "$20.99", options: [
+            .init(name: "", price: "$20.99")
+        ])
+
+        // Then & When
+        XCTAssertTrue(viewModel.showDescription)
+        XCTAssertTrue(viewModel.showPrice)
+        XCTAssertTrue(viewModel.options[0].showPrice)
+    }
+}

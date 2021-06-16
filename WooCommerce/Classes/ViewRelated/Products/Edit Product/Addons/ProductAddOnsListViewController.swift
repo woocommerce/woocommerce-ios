9298e2086aaf25608c59b86965fe9247f1a35ec8
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-06-16 09:26:46 -0500

    Added empty ProductAddOnsList view

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Addons/ProductAddOnsListViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Addons/ProductAddOnsListViewController.swift
@@ -1,2 +1,34 @@
+import UIKit
+import SwiftUI
 
-import Foundation
+// MARK: Hosting Controller
+
+/// Hosting controller that wraps an `ProductAddOnsList` view.
+///
+final class ProductAddOnsListViewController: UIHostingController<ProductAddOnsList> {
+    init() {
+        super.init(rootView: ProductAddOnsList())
+    }
+
+    required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: Views
+
+/// Renders a list of product add-ons
+///
+struct ProductAddOnsList: View {
+    var body: some View {
+        Text("WIP")
+    }
+}
+
+// MARK: Previews
+struct ProductAddOnsList_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductAddOnsList()
+            .environment(\.colorScheme, .light)
+    }
+}

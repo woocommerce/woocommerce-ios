6234509ea908691fe7174a33624435e2d2b1c982
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-06-16 16:49:24 -0500

    Integrate simple add-on view with view model

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Addons/ProductAddOn.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Addons/ProductAddOn.swift
@@ -3,24 +3,28 @@ import SwiftUI
 /// Renders a product add-on
 ///
 struct ProductAddOn: View {
+
+    /// Representation of the view state.
+    ///
+    let viewModel: ProductAddOnViewModel
+
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             Divider()
 
-            //Text(viewModel.title)
-            Text("Add-on")
+            Text(viewModel.name)
                 .headlineStyle()
                 .padding([.leading, .trailing])
 
             HStack(alignment: .bottom) {
-                //Text(viewModel.content)
-                Text("Description")
+                Text(viewModel.description)
                     .bodyStyle()
                     .frame(maxWidth: .infinity, alignment: .leading)
+                    .renderedIf(viewModel.showDescription)
 
-                //Text(viewModel.price)
-                Text("$5.0")
+                Text(viewModel.price)
                     .secondaryBodyStyle()
+                    .renderedIf(viewModel.showPrice)
             }
             .padding([.leading, .trailing])
 
@@ -32,8 +36,27 @@ struct ProductAddOn: View {
 
 // MARK: Previews
 struct ProductAddOn_Previews: PreviewProvider {
+
+    static let toppingViewModel = ProductAddOnViewModel(name: "Pizza Topping",
+                                                 description: "Select your favorite topping",
+                                                 price: "",
+                                                 options: [
+                                                    .init(name: "Peperoni", price: "$2.99"),
+                                                    .init(name: "Salami", price: "$1.99"),
+                                                    .init(name: "Ham", price: "$1.99"),
+                                                 ])
+    static let deliveryViewModel = ProductAddOnViewModel(name: "Delivery",
+                                                 description: "Weather you need delivery or not",
+                                                 price: "$6.00",
+                                                 options: [])
+
     static var previews: some View {
-        ProductAddOn()
-            .environment(\.colorScheme, .light)
+        VStack {
+            ProductAddOn(viewModel: toppingViewModel)
+                .environment(\.colorScheme, .light)
+
+            ProductAddOn(viewModel: deliveryViewModel)
+                .environment(\.colorScheme, .light)
+        }
     }
 }

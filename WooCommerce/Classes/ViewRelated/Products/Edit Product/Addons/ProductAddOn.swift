93b8df3821e85b51ec44c188cdad4778aeb60ced
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-06-16 10:55:43 -0500

    Adds empty ProductAddOn view

---
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Addons/ProductAddOn.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+/// Renders a product add-on
+///
+struct ProductAddOn: View {
+    var body: some View {
+        Text("WIP")
+    }
+}
+
+// MARK: Previews
+struct ProductAddOn_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductAddOn()
+            .environment(\.colorScheme, .light)
+    }
+}

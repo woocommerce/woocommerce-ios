fafc9e220b5dcbdc144e9cde9613d0e80d86698a
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-06-16 16:11:43 -0500

    Adds basic information for ProductAddOn view model

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Addons/ProductAddOnViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Addons/ProductAddOnViewModel.swift
@@ -1,1 +1,53 @@
+import Foundation
 
+/// ViewModel for `ProductAddOn`
+///
+struct ProductAddOnViewModel {
+
+    /// Represents an Add-on option
+    ///
+    struct Option {
+
+        /// Option name
+        ///
+        let name: String
+
+        /// Option optional price.
+        ///
+        let price: String
+
+        /// Determines the option price visibility
+        ///
+        var showPrice: Bool {
+            price.isNotEmpty
+        }
+    }
+
+    /// Add-on name
+    ///
+    let name: String
+
+    /// Add-on optional description.
+    ///
+    let description: String
+
+    /// Add-on optional price.
+    ///
+    let price: String
+
+    /// Add-on options.
+    ///
+    let options: [Option]
+
+    /// Determines the main description visibility
+    ///
+    var showDescription: Bool {
+        description.isNotEmpty
+    }
+
+    /// Determines the main price visibility
+    ///
+    var showPrice: Bool {
+        price.isNotEmpty
+    }
+}

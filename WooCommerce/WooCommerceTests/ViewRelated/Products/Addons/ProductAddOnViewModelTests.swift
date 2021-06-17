45c8c19e8aa6c23a175e16a335e521959bc39f05
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-06-17 14:47:35 -0500

    Update tests

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Addons/ProductAddOnViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Addons/ProductAddOnViewModelTests.swift
@@ -5,6 +5,15 @@ import TestKit
 
 class ProductAddOnViewModelTests: XCTestCase {
 
+    func test_view_model_shows_description_when_price_is_not_empty() {
+        // Given
+        let viewModel = ProductAddOnViewModel(name: "", description: "", price: "$20.99", options: [])
+
+        // Then & When
+        XCTAssertTrue(viewModel.showDescription)
+        XCTAssertTrue(viewModel.showPrice)
+    }
+
     func test_view_model_hides_description_and_price_when_empty() {
         // Given
         let viewModel = ProductAddOnViewModel(name: "", description: "", price: "", options: [

d184df8d22a4f9fd60b59f8fb83a1e015df9a37f
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-06-17 11:05:06 -0500

    Adds disclaimer info view

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Addons/ProductAddOnsListViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Addons/ProductAddOnsListViewController.swift
@@ -7,7 +7,7 @@ import SwiftUI
 ///
 final class ProductAddOnsListViewController: UIHostingController<ProductAddOnsList> {
     init(viewModel: ProductAddOnsListViewModel) {
-        super.init(rootView: ProductAddOnsList())
+        super.init(rootView: ProductAddOnsList(viewModel: viewModel))
         title = viewModel.title
     }
 
@@ -21,15 +21,39 @@ final class ProductAddOnsListViewController: UIHostingController<ProductAddOnsLi
 /// Renders a list of product add-ons
 ///
 struct ProductAddOnsList: View {
+
+    /// View model that directs the view content.
+    ///
+    let viewModel: ProductAddOnsListViewModel
+
     var body: some View {
-        Text("WIP")
+        AddOnListNotice(updateText: viewModel.infoNotice)
     }
 }
+
+/// Renders a info notice with an icon
+///
+private struct AddOnListNotice: View {
+
+    /// Content to be rendered next to the info icon.
+    ///
+    let updateText: String
+
+    var body: some View {
+        HStack {
+            Image(uiImage: .infoOutlineImage)
+            Text(updateText)
+        }
+        .footnoteStyle()
+        .padding([.leading, .trailing]).padding(.top, 4)
+    }
+}
+
 
 // MARK: Previews
 struct ProductAddOnsList_Previews: PreviewProvider {
     static var previews: some View {
-        ProductAddOnsList()
+        ProductAddOnsList(viewModel: ProductAddOnsListViewModel())
             .environment(\.colorScheme, .light)
     }
 }
--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Addons/ProductAddOnsListViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Addons/ProductAddOnsListViewModel.swift
@@ -7,11 +7,17 @@ final class ProductAddOnsListViewModel {
     /// View title
     ///
     let title = Localization.title
+
+    /// View info notice
+    ///
+    let infoNotice = Localization.infoNotice
 }
 
 // MARK: Constants
 extension ProductAddOnsListViewModel {
     enum Localization {
         static let title = NSLocalizedString("Product Add-ons", comment: "Title for the product add-ons screen")
+        static let infoNotice = NSLocalizedString("You can edit product add-ons in the web dashboard",
+                                                  comment: "Info notice at the bottom of the product add-ons screen")
     }
 }

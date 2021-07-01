dc70b707583634ffa3053441574c86542c3fd079
Huong Do <huongdt29@gmail.com>
2021-07-01 17:28:31 +0700

    Add new Review Order view controller

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Review Order/ReviewOrderViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class ReviewOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

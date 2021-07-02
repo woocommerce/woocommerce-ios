4b35d50f2e1517071d73bc48b217d17538a1c07f
Huong Do <huongdt29@gmail.com>
2021-07-02 10:04:27 +0700

    Navigate from order details to review order screen

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Review Order/ReviewOrderViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Review Order/ReviewOrderViewController.swift
@@ -4,18 +4,18 @@ import UIKit
 /// This screen is shown when Mark Order Complete button is tapped
 ///
 final class ReviewOrderViewController: UIViewController {
-    
+
     private let viewModel: ReviewOrderViewModel
-    
+
     init(viewModel: ReviewOrderViewModel) {
         self.viewModel = viewModel
         super.init(nibName: Self.nibName, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 

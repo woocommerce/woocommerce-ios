0ece78497267f29a0d4b091033fc460b2fa3730a
Huong Do <huongdt29@gmail.com>
2021-07-02 09:56:44 +0700

    Update Review Order view control with view model

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Review Order/ReviewOrderViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Review Order/ReviewOrderViewController.swift
@@ -1,7 +1,21 @@
 import UIKit
 
+/// View Control for Review Order screen
+/// This screen is shown when Mark Order Complete button is tapped
+///
 final class ReviewOrderViewController: UIViewController {
-
+    
+    private let viewModel: ReviewOrderViewModel
+    
+    init(viewModel: ReviewOrderViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: Self.nibName, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 

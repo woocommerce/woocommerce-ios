4bff435c07c4f4f77f41374e51c12a71b056edb9
Huong Do <huongdt29@gmail.com>
2021-07-02 09:50:09 +0700

    Add view model for review order

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Review Order/ReviewOrderViewModel.swift
@@ -0,0 +1,25 @@
+import Foundation
+import Yosemite
+import protocol Storage.StorageManagerType
+
+final class ReviewOrderViewModel {
+    /// The order for review
+    ///
+    private let order: Order
+    
+    /// StorageManager to load details of order from storage
+    ///
+    private let storageManager: StorageManagerType
+    
+    /// Reference to the StoresManager to dispatch Yosemite Actions.
+    ///
+    private let stores: StoresManager
+    
+    init(order: Order,
+         stores: StoresManager = ServiceLocator.stores,
+         storageManager: StorageManagerType = ServiceLocator.storageManager) {
+        self.order = order
+        self.stores = stores
+        self.storageManager = storageManager
+    }
+}

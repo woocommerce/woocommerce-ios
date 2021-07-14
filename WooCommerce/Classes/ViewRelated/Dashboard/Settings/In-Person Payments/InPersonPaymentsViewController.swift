55b9688ed348db5ce46e7b8da930f47ba2b5218a
Jorge Bernal <jbernal@gmail.com>
2021-07-14 13:59:31 +0200

    Remove duplicate strings

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsViewController.swift
@@ -22,16 +22,6 @@ private enum Localization {
         "In-Person Payments",
         comment: "Title for the In-Person Payments settings screen"
     )
-
-    static let unavailable = NSLocalizedString(
-        "In-Person Payments is currently unavailable",
-        comment: "Title for the error screen when In-Person Payments is unavailable"
-    )
-
-    static let acceptCash = NSLocalizedString(
-        "You can still accept in-person cash payments by enabling the “Cash on Delivery” payment method on your store.",
-        comment: "Generic error message when In-Person Payments is unavailable"
-    )
 }
 
 struct InPersonPaymentsView_Previews: PreviewProvider {

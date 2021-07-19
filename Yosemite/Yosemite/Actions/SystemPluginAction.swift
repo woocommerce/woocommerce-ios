5fb1811e972f6d6891275fe3a1cc2476c50a882a
Fernando <fernando.fgfer@gmail.com>
2021-07-19 14:07:48 +0200

    Add SystemPluginAction

---
+++ Yosemite/Yosemite/Actions/SystemPluginAction.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+/// Defines all actions supported by `SystemPluginStore`
+///
+public enum SystemPluginAction: Action {
+
+    /// Synchronize all system plugins for a site given its ID
+    ///
+    case synchronizeSystemPlugins(siteID: Int64, onCompletion: (Result<Void, Error>) -> Void)
+    
+    /// Fetch all system plugins for a site given its ID
+    ///
+    case fetchSystemPlugins(siteID: Int64, onCompletion: ([SystemPlugin]?, Error) -> Void)
+}

b04637a7be0cd11fc52ee171804ac6a04168409c
Fernando <fernando.fgfer@gmail.com>
2021-07-19 12:41:59 +0200

    Add SystemPluginsRemote

---
+++ Networking/Networking/Remote/SystemPluginsRemote.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+/// SystemPlugins: Remote Endpoints
+///
+public class SystemPluginsRemote: Remote {
+
+    /// Retrieves all of the `SystemPlugin`s for a given site.
+    ///
+    /// - Parameters:
+    ///   - siteID: Site for which we'll fetch the system plugins.
+    ///   - completion: Closure to be executed upon completion.
+    ///
+    public func loadSystemPlugins(for siteID: Int64,
+                            completion: @escaping (Result<[SystemPlugin], Error>) -> Void) {
+        let path = Constants.systemPluginsPath
+        let request = JetpackRequest(wooApiVersion: .none, method: .get, siteID: siteID, path: path, parameters: nil)
+        let mapper = SystemPluginsMapper(siteID: siteID)
+
+        enqueue(request, mapper: mapper, completion: completion)
+    }
+}
+
+
+// MARK: - Constants!
+//
+private extension SystemPluginsRemote {
+    enum Constants {
+        static let systemPluginsPath: String = "wc/v3/system_status"
+    }
+}

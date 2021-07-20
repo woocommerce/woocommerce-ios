3edbd88b583e7a1d0947ec024ac3b163d5a664fe
Fernando <fernando.fgfer@gmail.com>
2021-07-21 00:01:31 +0200

    Remove warning

--- Yosemite/Yosemite/Actions/SystemPluginAction.swift
+++ Yosemite/Yosemite/Actions/SystemPluginAction.swift
@@ -7,7 +7,7 @@ public enum SystemPluginAction: Action {
     /// Synchronize all system plugins for a site given its ID
     ///
     case synchronizeSystemPlugins(siteID: Int64, onCompletion: (Result<Void, Error>) -> Void)
-    
+
     /// Fetch all system plugins for a site given its ID
     ///
     case fetchSystemPlugins(siteID: Int64, onCompletion: ([SystemPlugin]?) -> Void)

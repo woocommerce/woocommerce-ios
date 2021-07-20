fa07dfacb90924b632dc1bdf75cd52eced5601a2
rachelmcr <rachel@automattic.com>
2021-07-20 12:28:00 +0100

    Round decimals up by default

--- WooCommerce/Classes/Extensions/Decimal+Helpers.swift
+++ WooCommerce/Classes/Extensions/Decimal+Helpers.swift
@@ -2,20 +2,20 @@ import Foundation
 
 extension Decimal {
 
-    /// Returns the int value of a decimal. We ensure we round down our Decimal before converting it to an Int, using NSDecimalRound.
+    /// Returns the int value of a decimal. We ensure we round up our Decimal before converting it to an Int, using NSDecimalRound.
     ///
     var intValue: Int {
         NSDecimalNumber(decimal: whole).intValue
     }
 
-    private func rounded(_ roundingMode: NSDecimalNumber.RoundingMode = .down, scale: Int = 0) -> Self {
+    private func rounded(_ roundingMode: NSDecimalNumber.RoundingMode = .up, scale: Int = 0) -> Self {
         var result = Self()
         var number = self
         NSDecimalRound(&result, &number, scale, roundingMode)
         return result
     }
 
-    private var whole: Self { rounded( self < 0 ? .up : .down) }
+    private var whole: Self { rounded( self < 0 ? .down : .up) }
 
     private var fraction: Self { self - whole }
 

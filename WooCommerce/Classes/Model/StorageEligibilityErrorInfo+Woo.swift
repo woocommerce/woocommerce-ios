e3373a94bf0110b1b31c7f74cf6686dea4b0238d
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2021-07-21 16:55:17 +0700

    Add docs to EligibilityErrorInfo extension

--- WooCommerce/Classes/Model/StorageEligibilityErrorInfo+Woo.swift
+++ WooCommerce/Classes/Model/StorageEligibilityErrorInfo+Woo.swift
@@ -1,5 +1,7 @@
 import Yosemite
 
+/// Encapsulates Storage.EligibilityErrorInfo interface helpers.
+///
 extension StorageEligibilityErrorInfo {
     /// Convenience method that converts the roles to a display-friendly format.
     /// e.g. ["author", "shop_manager"] -> "Author, Shop Manager"

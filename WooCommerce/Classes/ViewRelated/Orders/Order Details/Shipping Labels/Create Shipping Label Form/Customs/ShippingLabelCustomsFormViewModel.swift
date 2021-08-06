eb65d3af7a96235b9dbfafc29b182fbce22cd52c
Huong Do <hi.huongdo@gmail.com>
2021-08-06 14:29:21 +0700

    Add new view model and SwiftUI view for customs form

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Customs/ShippingLabelCustomsFormViewModel.swift
@@ -0,0 +1,34 @@
+import Foundation
+import Yosemite
+import protocol Storage.StorageManagerType
+
+/// View model for ShippingLabelsCustomsFormInput
+///
+final class ShippingLabelCustomsFormViewModel: ObservableObject {
+
+    /// Associated order of the shipping label.
+    ///
+    private let order: Order
+
+    /// Input customs forms of the shipping label if added initially.
+    ///
+    private let customsForms: [ShippingLabelCustomsForm]
+
+    /// Stores to sync data of products and variations.
+    ///
+    private let stores: StoresManager
+
+    /// Storage to fetch products and variations.
+    ///
+    private let storageManager: StorageManagerType
+
+    init(order: Order,
+         customsForms: [ShippingLabelCustomsForm],
+         stores: StoresManager = ServiceLocator.stores,
+         storageManager: StorageManagerType = ServiceLocator.storageManager) {
+        self.order = order
+        self.customsForms = customsForms
+        self.stores = stores
+        self.storageManager = storageManager
+    }
+}

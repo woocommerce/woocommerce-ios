cb85fc55db44e3d476933f4ba7b133eaa8575096
rachelmcr <rachel@automattic.com>
2021-08-09 14:11:07 +0100

    Add Create New Package button to bottom of screen

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/Package Selection/ShippingLabelPackageList.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/Package Selection/ShippingLabelPackageList.swift
@@ -8,57 +8,72 @@ struct ShippingLabelPackageList: View {
     var body: some View {
         GeometryReader { geometry in
             NavigationView {
-                ScrollView {
-                    LazyVStack(spacing: 0) {
-                        /// Custom Packages
-                        ///
-                        if viewModel.showCustomPackagesHeader {
-                            ListHeaderView(text: Localization.customPackageHeader.uppercased(), alignment: .left)
-                                .padding(.horizontal, insets: geometry.safeAreaInsets)
-                        }
-                        ForEach(viewModel.customPackages, id: \.title) { package in
-                            let selected = package == viewModel.selectedCustomPackage
-                            SelectableItemRow(title: package.title, subtitle: package.dimensions + " \(viewModel.dimensionUnit)", selected: selected)
-                                .onTapGesture {
-                                    viewModel.didSelectPackage(package.title)
-                                    ServiceLocator.analytics.track(.shippingLabelPurchaseFlow, withProperties: ["state": "packages_selected"])
-                                }
-                                .padding(.horizontal, insets: geometry.safeAreaInsets)
-                                .background(Color(.systemBackground))
-                            Divider().padding(.leading, Constants.dividerPadding)
-                        }
-
-                        /// Predefined Packages
-                        ///
-                        ForEach(viewModel.predefinedOptions, id: \.title) { option in
-
-                            ListHeaderView(text: option.title.uppercased(), alignment: .left)
-                                .padding(.horizontal, insets: geometry.safeAreaInsets)
-                            ForEach(option.predefinedPackages) { package in
-                                let selected = package == viewModel.selectedPredefinedPackage
-                                SelectableItemRow(title: package.title,
-                                                  subtitle: package.dimensions + " \(viewModel.dimensionUnit)",
-                                                  selected: selected).onTapGesture {
-                                                    viewModel.didSelectPackage(package.id)
-                                                    ServiceLocator.analytics.track(.shippingLabelPurchaseFlow, withProperties: ["state": "packages_selected"])
-                                                  }
+                ZStack(alignment: .bottom) {
+                    ScrollView {
+                        LazyVStack(spacing: 0) {
+                            /// Custom Packages
+                            ///
+                            if viewModel.showCustomPackagesHeader {
+                                ListHeaderView(text: Localization.customPackageHeader.uppercased(), alignment: .left)
+                                    .padding(.horizontal, insets: geometry.safeAreaInsets)
+                            }
+                            ForEach(viewModel.customPackages, id: \.title) { package in
+                                let selected = package == viewModel.selectedCustomPackage
+                                SelectableItemRow(title: package.title, subtitle: package.dimensions + " \(viewModel.dimensionUnit)", selected: selected)
+                                    .onTapGesture {
+                                        viewModel.didSelectPackage(package.title)
+                                        ServiceLocator.analytics.track(.shippingLabelPurchaseFlow, withProperties: ["state": "packages_selected"])
+                                    }
                                     .padding(.horizontal, insets: geometry.safeAreaInsets)
                                     .background(Color(.systemBackground))
                                 Divider().padding(.leading, Constants.dividerPadding)
                             }
+
+                            /// Predefined Packages
+                            ///
+                            ForEach(viewModel.predefinedOptions, id: \.title) { option in
+
+                                ListHeaderView(text: option.title.uppercased(), alignment: .left)
+                                    .padding(.horizontal, insets: geometry.safeAreaInsets)
+                                ForEach(option.predefinedPackages) { package in
+                                    let selected = package == viewModel.selectedPredefinedPackage
+                                    SelectableItemRow(title: package.title,
+                                                      subtitle: package.dimensions + " \(viewModel.dimensionUnit)",
+                                                      selected: selected).onTapGesture {
+                                                        viewModel.didSelectPackage(package.id)
+                                                        ServiceLocator.analytics.track(.shippingLabelPurchaseFlow,
+                                                                                       withProperties: ["state": "packages_selected"])
+                                                      }
+                                        .padding(.horizontal, insets: geometry.safeAreaInsets)
+                                        .background(Color(.systemBackground))
+                                    Divider().padding(.leading, Constants.dividerPadding)
+                                }
+                            }
                         }
                     }
-                }
-                .background(Color(.listBackground))
-                .ignoresSafeArea(.container, edges: .horizontal)
-                .navigationTitle(Localization.title)
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationBarItems(trailing: Button(action: {
-                    viewModel.confirmPackageSelection()
-                    presentation.wrappedValue.dismiss()
-                }, label: {
-                    Text(Localization.doneButton)
+                    .background(Color(.listBackground))
+                    .ignoresSafeArea(.container, edges: .horizontal)
+                    .navigationTitle(Localization.title)
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationBarItems(trailing: Button(action: {
+                        viewModel.confirmPackageSelection()
+                        presentation.wrappedValue.dismiss()
+                    }, label: {
+                        Text(Localization.doneButton)
                 }))
+
+                    Button(action: {
+                        // TODO-3909: Navigate to create custom package screen
+                    }) {
+                        HStack {
+                            Image(uiImage: .plusImage)
+                            Text(Localization.createPackageButton)
+                            Spacer()
+                        }.padding(.bottom, geometry.safeAreaInsets.bottom)
+                    }
+                    .buttonStyle(LinkButtonStyle())
+                    .background(Color(.listForeground))
+                }.edgesIgnoringSafeArea([.bottom])
             }
         }
     }
@@ -70,6 +85,7 @@ private extension ShippingLabelPackageList {
         static let doneButton = NSLocalizedString("Done", comment: "Done navigation button under the Package Selected screen in Shipping Label flow")
         static let customPackageHeader = NSLocalizedString("CUSTOM PACKAGES",
                                                            comment: "Header for the Custom Packages section in Shipping Label Package listing")
+        static let createPackageButton = NSLocalizedString("Create new package", comment: "Button to create a new package in Shipping Label Package screen")
     }
 
     enum Constants {

1b001fd45d188834346fbea22eefb6ab94de0a96
Gio Lodi <giovanni.lodi42@gmail.com>
2021-08-09 21:01:46 +1000

    Make UI test initializing `PrologueScreen` `throws` instead of `try!`
    With `try!` they would crash if the `PrologueScreen` init throws. This
    way, they'll fail normally.

--- WooCommerce/WooCommerceUITests/Tests/LoginTests.swift
+++ WooCommerce/WooCommerceUITests/Tests/LoginTests.swift
@@ -12,8 +12,8 @@ final class LoginTests: XCTestCase {
     }
 
     // Login with Store Address and log out.
-    func test_site_address_login_logout() {
-        let prologue = try! PrologueScreen().selectSiteAddress()
+    func test_site_address_login_logout() throws {
+        let prologue = try PrologueScreen().selectSiteAddress()
             .proceedWith(siteUrl: TestCredentials.siteUrl)
             .proceedWith(email: TestCredentials.emailAddress)
             .proceedWith(password: TestCredentials.password)
@@ -30,8 +30,8 @@ final class LoginTests: XCTestCase {
     }
 
     //Login with WordPress.com account and log out
-    func test_WordPress_login_logout() {
-        let prologue = try! PrologueScreen().selectContinueWithWordPress()
+    func test_WordPress_login_logout() throws {
+        let prologue = try PrologueScreen().selectContinueWithWordPress()
             .proceedWith(email: TestCredentials.emailAddress)
             .proceedWith(password: TestCredentials.password)
             .verifyEpilogueDisplays(displayName: TestCredentials.displayName, siteUrl: TestCredentials.siteUrl)
@@ -45,8 +45,8 @@ final class LoginTests: XCTestCase {
         XCTAssert(prologue.isLoaded)
     }
 
-    func test_WordPress_unsuccessfull_login() {
-        _ = try! PrologueScreen().selectContinueWithWordPress()
+    func test_WordPress_unsuccessfull_login() throws {
+        _ = try PrologueScreen().selectContinueWithWordPress()
             .proceedWith(email: TestCredentials.emailAddress)
             .tryProceed(password: "invalidPswd")
             .verifyLoginError()

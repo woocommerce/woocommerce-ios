b7b7b8a19480d08dcd111cba3ccd97e8c86de308
Huong Do <hi.huongdo@gmail.com>
2021-08-09 20:18:18 +0700

    Remove check for domestic US territories for customs form requirement

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/ShippingLabelFormViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/ShippingLabelFormViewModel.swift
@@ -124,13 +124,7 @@ final class ShippingLabelFormViewModel {
             return true
         }
 
-        if originAddress.country == destinationAddress.country {
-            return false
-        }
-
-        // Shipments between US, Puerto Rico and Virgin Islands don't need Customs, everything else does
-        return !Constants.domesticUSTerritories.contains(originAddress.country) ||
-            !Constants.domesticUSTerritories.contains(destinationAddress.country)
+        return originAddress.country != destinationAddress.country
     }
 
     private let stores: StoresManager
@@ -793,9 +787,6 @@ private extension ShippingLabelFormViewModel {
         /// These US states are a special case because they represent military bases. They're considered "domestic",
         /// but they require a Customs form to ship from/to them.
         static let usMilitaryStates = ["AA", "AE", "AP"]
-
-        // Packages shipping to or from the US, Puerto Rico and Virgin Islands don't need a Customs form
-        static let domesticUSTerritories = ["US", "PR", "VI"]
 
         // These destination countries require an ITN regardless of shipment value
         static let uspsITNRequiredDestination = ["IR", "SY", "KP", "CU", "SD"]

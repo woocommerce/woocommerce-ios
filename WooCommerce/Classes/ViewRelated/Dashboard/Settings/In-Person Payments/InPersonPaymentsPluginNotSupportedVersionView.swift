921b995bd3046e9b50e812451b9e48be00370ba7
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-08-09 09:13:18 -0400

    First draft of error view for plugin version not supported

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsPluginNotSupportedVersionView.swift
@@ -0,0 +1,56 @@
+import SwiftUI
+
+struct InPersonPaymentsPluginNotSupportedVersionView: View {
+    let onRefresh: () -> Void
+
+    var body: some View {
+        VStack {
+            Spacer()
+
+            VStack(alignment: .center, spacing: 42) {
+                Text(Localization.title)
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+                Image(uiImage: .paymentsPlugin)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 108.0)
+                Text(Localization.message)
+                    .font(.callout)
+                    .multilineTextAlignment(.center)
+            }
+
+            Spacer()
+
+            Button(Localization.primaryButton, action: onRefresh)
+                .buttonStyle(PrimaryButtonStyle())
+                .padding(.bottom, 24.0)
+            InPersonPaymentsLearnMore()
+        }
+        .padding(24.0)
+    }
+}
+
+private enum Localization {
+    static let title = NSLocalizedString(
+        "Unsupported WooCommerce Payments version",
+        comment: "Title for the error screen when the installed version of WooCommerce Payments is unsupported"
+    )
+
+    static let message = NSLocalizedString(
+        "The WooCommerce Payments extension is installed on your store, but the installed version does not support In-Person Payments. "
+            + "Please update WooCommerce Payments to the most recent version.",
+        comment: "Error message when WooCommerce Payments is installed but the version is not supported"
+    )
+
+    static let primaryButton = NSLocalizedString(
+        "Refresh After Updating",
+        comment: "Button to reload plugin data after updating the WooCommerce Payments plugin"
+    )
+}
+
+struct InPersonPaymentsPluginNotSupportedVersionView_Previews: PreviewProvider {
+    static var previews: some View {
+        InPersonPaymentsPluginNotSupportedVersionView(onRefresh: {})
+    }
+}

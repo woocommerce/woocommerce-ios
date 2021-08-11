a77eaccc4da3aff16e270c8fb0a684f0823acfe5
Huong Do <hi.huongdo@gmail.com>
2021-08-11 09:54:13 +0700

    Add localization for customs form restriction type

--- WooCommerce/Classes/Extensions/ShippingLabelCustomsForm+Localization.swift
+++ WooCommerce/Classes/Extensions/ShippingLabelCustomsForm+Localization.swift
@@ -17,7 +17,7 @@ extension ShippingLabelCustomsForm.ContentsType {
         }
     }
 
-    enum Localization {
+    private enum Localization {
         static let contentTypeMerchandise = NSLocalizedString("Merchandise",
                                                               comment: "Type Merchandise of content to be declared for the customs form in Shipping Label flow")
         static let contentTypeDocuments = NSLocalizedString("Documents",
@@ -28,5 +28,31 @@ extension ShippingLabelCustomsForm.ContentsType {
                                                          comment: "Type Sample of content to be declared for the customs form in Shipping Label flow")
         static let contentTypeOther = NSLocalizedString("Other",
                                                         comment: "Type Other of content to be declared for the customs form in Shipping Label flow")
+    }
+}
+
+extension ShippingLabelCustomsForm.RestrictionType {
+    var localizedName: String {
+        switch self {
+        case .none:
+            return Localization.restrictionTypeNone
+        case .quarantine:
+            return Localization.restrictionTypeQuarantine
+        case .sanitaryOrPhytosanitaryInspection:
+            return Localization.restrictionTypeSanitary
+        case .other:
+            return Localization.restrictionTypeOther
+        }
+    }
+
+    private enum Localization {
+        static let restrictionTypeNone = NSLocalizedString("None",
+                                                           comment: "Restriction type None for contents to be declared in the customs form for Shipping Label flow")
+        static let restrictionTypeQuarantine = NSLocalizedString("Quarantine",
+                                                                 comment: "Restriction type Quarantine for contents to be declared in the customs form for Shipping Label flow")
+        static let restrictionTypeSanitary = NSLocalizedString("Sanitary / Phytosanitary Inspection",
+                                                               comment: "Restriction type Sanitary / Phytosanitary Inspection for contents to be declared in the customs form for Shipping Label flow")
+        static let restrictionTypeOther = NSLocalizedString("Other",
+                                                            comment: "Restriction type Other for contents to be declared in the customs form for Shipping Label flow")
     }
 }

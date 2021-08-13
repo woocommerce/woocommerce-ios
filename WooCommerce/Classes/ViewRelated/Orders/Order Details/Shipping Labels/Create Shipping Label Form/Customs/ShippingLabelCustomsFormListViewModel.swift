c3cfc3ad1059ef74d640a674de497456c0e442ca
Huong Do <hi.huongdo@gmail.com>
2021-08-13 17:46:36 +0700

    Update item details based on fetched products and variations

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Customs/ShippingLabelCustomsFormListViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Customs/ShippingLabelCustomsFormListViewModel.swift
@@ -17,11 +17,7 @@ final class ShippingLabelCustomsFormListViewModel: ObservableObject {
 
     /// Input customs forms of the shipping label if added initially.
     ///
-    @Published var customsForms: [ShippingLabelCustomsForm] {
-        didSet {
-            inputViewModels = customsForms.map { .init(customsForm: $0, countries: allCountries, currency: order.currency) }
-        }
-    }
+    @Published var customsForms: [ShippingLabelCustomsForm]
 
     /// Associated order of the shipping label.
     ///
@@ -39,6 +35,18 @@ final class ShippingLabelCustomsFormListViewModel: ObservableObject {
     ///
     private let allCountries: [Country]
 
+    /// Reusing Package Details results controllers since we're interested in the same models.
+    ///
+    private var resultsControllers: ShippingLabelPackageDetailsResultsControllers?
+
+    /// Products contained inside the Order and fetched from Core Data
+    ///
+    @Published private var products: [Product] = []
+
+    /// ProductVariations contained inside the Order and fetched from Core Data
+    ///
+    @Published private var productVariations: [ProductVariation] = []
+
     init(order: Order,
          customsForms: [ShippingLabelCustomsForm],
          countries: [Country],
@@ -50,5 +58,88 @@ final class ShippingLabelCustomsFormListViewModel: ObservableObject {
         self.storageManager = storageManager
         self.allCountries = countries
         self.inputViewModels = customsForms.map { .init(customsForm: $0, countries: countries, currency: order.currency) }
+
+        configureResultsControllers()
+        updateItemDetails()
+    }
+}
+
+// MARK: - Fetching and updating item details.
+//
+private extension ShippingLabelCustomsFormListViewModel {
+    /// Update item details for current customs forms
+    /// with every change in products and product variations.
+    ///
+    func updateItemDetails() {
+        $products.combineLatest($productVariations) { [weak self] (products, variations) -> [ShippingLabelCustomsForm] in
+            self?.updateCustomsForms(products: products, productVariations: variations) ?? []
+        }
+        .handleEvents(receiveOutput: { [weak self] customsForms in
+            guard let self = self else { return }
+            self.inputViewModels = customsForms.map { .init(customsForm: $0, countries: self.allCountries, currency: self.order.currency) }
+        })
+        .assign(to: &$customsForms)
+    }
+
+    /// Configure result controllers for products and product variations.
+    ///
+    func configureResultsControllers() {
+        resultsControllers = ShippingLabelPackageDetailsResultsControllers(siteID: order.siteID,
+                                                                           orderItems: order.items,
+                                                                           storageManager: storageManager,
+           onProductReload: { [weak self] (products) in
+            self?.products = products
+        }, onProductVariationsReload: { [weak self] (productVariations) in
+            self?.productVariations = productVariations
+        })
+
+        products = resultsControllers?.products ?? []
+        productVariations = resultsControllers?.productVariations ?? []
+    }
+
+    /// Return copy of customs forms with updated item details based on fetched products and variations.
+    ///
+    func updateCustomsForms(products: [Product], productVariations: [ProductVariation]) -> [ShippingLabelCustomsForm] {
+        var updatedForms: [ShippingLabelCustomsForm] = []
+
+        for form in customsForms {
+            let updatedItems = form.items.map { item -> ShippingLabelCustomsForm.Item in
+                // Find the matching order item
+                guard let orderItem = order.items.first(where: { $0.variationID == item.productID || $0.productID == item.productID }) else {
+                    return item
+                }
+
+                let weight: Double = {
+                    if orderItem.variationID > 0,
+                       let productVariation = productVariations.first(where: { $0.productVariationID == orderItem.variationID }) {
+                        return Double(productVariation.weight ?? "") ?? 0
+                    } else if let product = products.first(where: { $0.productID == orderItem.productID }) {
+                        return Double(product.weight ?? "0") ?? 0
+                    }
+
+                    return 0
+                }()
+
+                return .init(description: orderItem.name,
+                             quantity: orderItem.quantity.intValue, // TODO: is this safe?
+                             value: orderItem.price.doubleValue,
+                             weight: weight,
+                             hsTariffNumber: "",
+                             originCountry: "",
+                             productID: item.productID)
+            }
+
+            // Append new form with updated items
+            updatedForms.append(.init(packageID: form.packageID,
+                                      packageName: form.packageName,
+                                      contentsType: form.contentsType,
+                                      contentExplanation: form.contentExplanation,
+                                      restrictionType: form.restrictionType,
+                                      restrictionComments: form.restrictionComments,
+                                      nonDeliveryOption: form.nonDeliveryOption,
+                                      itn: form.itn,
+                                      items: updatedItems))
+        }
+        return updatedForms
     }
 }

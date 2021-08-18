b5fbff2c6556f46ff6300b5047ab1b549bf37cba
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-08-18 11:02:27 -0500

    Add edit button interface

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Customer Section/CustomerNoteTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Customer Section/CustomerNoteTableViewCell.swift
@@ -6,6 +6,8 @@ final class CustomerNoteTableViewCell: UITableViewCell {
 
     @IBOutlet private weak var bodyLabel: UILabel!
 
+    @IBOutlet private weak var editButton: UIButton!
+
     /// Headline label text
     ///
     var headline: String? {
@@ -28,12 +30,27 @@ final class CustomerNoteTableViewCell: UITableViewCell {
         }
     }
 
+    /// Closure to be invoked when the edit icon is tapped
+    ///
+    var onEditTapped: (() -> Void)? {
+        didSet {
+            editButton.isHidden = onEditTapped == nil
+        }
+    }
+
     override func awakeFromNib() {
         super.awakeFromNib()
         configureBackground()
         configureLabels()
+        configureEditButton()
     }
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        headlineLabel.text = nil
+        bodyLabel.text = nil
+        onEditTapped = nil
+    }
 }
 
 
@@ -47,6 +64,15 @@ private extension CustomerNoteTableViewCell {
     func configureLabels() {
         headlineLabel.applyHeadlineStyle()
         bodyLabel.applyBodyStyle()
+    }
+
+    func configureEditButton() {
+        editButton.applyIconButtonStyle(icon: .pencilImage)
+        editButton.addTarget(self, action: #selector(editButtonTapped), for: .touchUpInside)
+    }
+
+    @objc func editButtonTapped() {
+        onEditTapped?()
     }
 }
 

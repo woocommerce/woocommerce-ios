e4ce46824cb68fe2066a91eb0b6b8fc406a33ce7
rachelmcr <rachel@automattic.com>
2021-08-18 11:31:54 +0100

    Simplify unit test setup

--- WooCommerce/WooCommerceTests/ViewRelated/Shipping Label/Create Shipping Label/ShippingLabelCustomPackageFormViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Shipping Label/Create Shipping Label/ShippingLabelCustomPackageFormViewModelTests.swift
@@ -1,15 +1,11 @@
 import XCTest
 @testable import WooCommerce
-@testable import Yosemite
-@testable import Storage
 
 class ShippingLabelCustomPackageFormViewModelTests: XCTestCase {
 
     func test_properties_return_expected_values_for_empty_form() {
         // Given
-        let shippingSettingsService = MockShippingSettingsService(dimensionUnit: "in", weightUnit: "oz")
-        ServiceLocator.setShippingSettingsService(shippingSettingsService)
-        let viewModel = ShippingLabelCustomPackageFormViewModel()
+        let viewModel = ShippingLabelCustomPackageFormViewModel(lengthUnit: "in", weightUnit: "oz")
 
         // Then
         XCTAssertEqual(viewModel.lengthUnit, "in")

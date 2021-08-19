12490ac38c0de6f2754403559eb4666eda700280
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-08-19 09:51:05 -0500

    Adds simple hosting controller

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Order Notes Section/Customer Note/EditCustomerNote.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Order Notes Section/Customer Note/EditCustomerNote.swift
@@ -1,1 +1,30 @@
 import Foundation
+import SwiftUI
+
+/// Hosting controller that wraps an `EditCustomerNote` view.
+///
+final class EditCustomerNoteHostingController: UIHostingController<EditCustomerNote> {
+    init() {
+        super.init(rootView: EditCustomerNote())
+    }
+
+    required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+/// Allows merchant to edit the customer provided note of an order.
+///
+struct EditCustomerNote: View {
+    var body: some View {
+        Text("Empty")
+    }
+}
+
+// MARK: Previews
+struct EditCustomerNote_Previews: PreviewProvider {
+    static var previews: some View {
+        EditCustomerNote()
+            .environment(\.colorScheme, .light)
+    }
+}

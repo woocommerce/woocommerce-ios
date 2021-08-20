7a318f3122a8ed6afd7e66820c76a3d04bd6729b
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2021-08-20 16:54:10 -0600

    Add Buildkite
    Fix copy files path
    
    Use Xcode 13
    
    XC13 Fix

---
+++ .buildkite/commands/build-for-testing.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eu
+
+# FIXIT-13.1: Temporary fix until we're on the Xcode 13.1 VM
+echo "--- :rubygems: Fixing Ruby Setup"
+gem install bundler
+
+echo "--- :rubygems: Setting up Gems"
+restore_cache "$(hash_file .ruby-version)-$(hash_file Gemfile.lock)"
+install_gems
+
+echo "--- :cocoapods: Setting up Pods"
+install_cocoapods
+
+echo "--- :writing_hand: Copy Files"
+cp -v fastlane/env/project.env.example .configure-files/project.env
+mkdir -pv ~/.configure/woocommerce-ios/secrets
+cp -v fastlane/env/project.env.example ~/.configure/woocommerce-ios/secrets/project.env
+
+echo "--- :hammer_and_wrench: Building"
+bundle exec fastlane build_for_testing
+
+echo "--- :arrow_up: Upload Build Products"
+tar -cf build-products.tar DerivedData/Build/Products/
+buildkite-agent artifact upload build-products.tar

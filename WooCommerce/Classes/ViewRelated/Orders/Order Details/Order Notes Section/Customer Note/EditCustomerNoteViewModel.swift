8fa4795c3856bc93b4392dab48a849dc6872b8c0
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-08-23 10:40:47 -0500

    Add initializer to EditCustomerNoteViewModel

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Order Notes Section/Customer Note/EditCustomerNoteViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Order Notes Section/Customer Note/EditCustomerNoteViewModel.swift
@@ -1,7 +1,26 @@
 import Foundation
+import Yosemite
 
 /// View Model for the Edit Customer Note screen
 ///
 final class EditCustomerNoteViewModel {
 
+    /// Original content of the order customer provided note
+    ///
+    private let originalNote: String
+
+    /// New content to submit
+    ///
+    private var newNote: String
+
+    /// True when there are changes to the `initialNote`. False otherwise.
+    ///
+    var doneEnabled: Bool {
+        originalNote != newNote
+    }
+
+    init(order: Order) {
+        self.originalNote = order.customerNote ?? ""
+        self.newNote = self.originalNote
+    }
 }

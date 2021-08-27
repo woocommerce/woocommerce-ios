626c15840eba72ff0f7be08b1efd713785077d99
Huong Do <hi.huongdo@gmail.com>
2021-08-27 09:45:27 +0700

    Update PrintCustomsFormsView with buttons

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Print Customs Form/PrintCustomsFormsView.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Print Customs Form/PrintCustomsFormsView.swift
@@ -14,12 +14,63 @@ struct PrintCustomsFormsView: View {
                     .multilineTextAlignment(.center)
             }
             .frame(width: 235)
+
+            if invoiceURLs.count == 1 {
+                VStack(spacing: 8) {
+                    Button(action: {}, label: {
+                        Text("Print customs form")
+                    })
+                    .buttonStyle(PrimaryButtonStyle())
+
+                    saveForLaterButton
+                }
+                .padding(.horizontal, 16)
+                .frame(maxWidth: .infinity)
+            } else {
+                VStack(spacing: 0) {
+                    ForEach(Array(invoiceURLs.enumerated()), id: \.element) { index, url in
+                        HStack {
+                            Text("Package \(index + 1)")
+                            Spacer()
+                            Button(action: {}, label: {
+                                Text("Print")
+                            })
+                            .buttonStyle(LinkButtonStyle())
+                        }
+                        .padding(.horizontal, 16)
+                        .frame(height: 48)
+                        Divider()
+                    }
+
+                    Spacer()
+
+                    saveForLaterButton
+                        .padding(.horizontal, 16)
+                        .frame(maxWidth: .infinity)
+                }
+                
+            }
+
+            Spacer()
         }
+    }
+
+    private var saveForLaterButton: some View {
+        Button(action: {}, label: {
+            Text("Save for later")
+        })
+        .buttonStyle(SecondaryButtonStyle())
     }
 }
 
 struct PrintCustomsFormsView_Previews: PreviewProvider {
     static var previews: some View {
-        PrintCustomsFormsView(invoiceURLs: ["http://woocommerce.com"])
+        PrintCustomsFormsView(invoiceURLs: ["https://woocommerce.com"])
+            .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+            .previewDisplayName("iPhone 12")
+        
+        PrintCustomsFormsView(invoiceURLs: ["https://woocommerce.com", "https://wordpress.com"])
+            .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro Max"))
+            .previewDisplayName("iPhone 12 Pro Max")
     }
 }

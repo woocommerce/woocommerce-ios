5356981f05cd79a53e89e8797db68506a4cb5387
Carlos Correa <caiobzen@gmail.com>
2021-08-31 10:46:30 +0100

    Simplify WhatsNewViewModel

--- WooCommerce/Classes/ViewModels/WhatsNew/WhatsNewViewModel.swift
+++ WooCommerce/Classes/ViewModels/WhatsNew/WhatsNewViewModel.swift
@@ -5,19 +5,11 @@ import Yosemite
 final class WhatsNewViewModel: ReportListPresentable {
     let items: [ReportItem]
     let onDismiss: () -> Void
-    let title = Localization.title
-    let ctaTitle = Localization.ctaTitle
+    let title = NSLocalizedString("What’s New in WooCommerce", comment: "Title of Whats New Component")
+    let ctaTitle = NSLocalizedString("Continue", comment: "Title of continue button")
 
     init(items: [Feature], onDismiss: @escaping () -> Void) {
         self.items = items.map { ReportItem(title: $0.title, subtitle: $0.subtitle, iconUrl: $0.iconUrl, iconBase64: $0.iconBase64)}
         self.onDismiss = onDismiss
-    }
-}
-
-// MARK: - Private data structures
-private extension WhatsNewViewModel {
-    enum Localization {
-        static let title = NSLocalizedString("What’s New in WooCommerce", comment: "Title of Whats New Component")
-        static let ctaTitle = NSLocalizedString("Continue", comment: "Title of continue button")
     }
 }

fa7f4d5510cfa46fc1491f3add1491bba77b437e
Carlos Correa <caiobzen@gmail.com>
2021-09-01 23:12:10 +0100

    Add tests to WhatsNewFactory

--- WooCommerce/Classes/ViewRelated/WhatsNew/WhatsNewFactory.swift
+++ WooCommerce/Classes/ViewRelated/WhatsNew/WhatsNewFactory.swift
@@ -20,15 +20,20 @@ struct WhatsNewFactory {
     /// Transform Features into ReportItem models
     private static func mapFeaturesToItems(_ features: [Feature]) -> [ReportItem] {
         features.map {
-            var icon: IconListItem.Icon?
-            if let base64String = $0.iconBase64,
-               let imageData = Data(base64Encoded: base64String),
-               let image = UIImage(data: imageData) {
-                icon = .base64(image)
-            } else if let url = URL(string: $0.iconUrl) {
-                icon = .remote(url)
-            }
-            return ReportItem(title: $0.title, subtitle: $0.subtitle, icon: icon)
+            return ReportItem(title: $0.title, subtitle: $0.subtitle, icon: icon(for: $0))
         }
+    }
+
+    /// Get IconListItem.Icon from a Feature
+    private static func icon(for feature: Feature) -> IconListItem.Icon? {
+        var icon: IconListItem.Icon?
+        if let base64String = feature.iconBase64,
+           let imageData = Data(base64Encoded: base64String),
+           let image = UIImage(data: imageData) {
+            icon = .base64(image)
+        } else if let url = URL(string: feature.iconUrl) {
+            icon = .remote(url)
+        }
+        return icon
     }
 }

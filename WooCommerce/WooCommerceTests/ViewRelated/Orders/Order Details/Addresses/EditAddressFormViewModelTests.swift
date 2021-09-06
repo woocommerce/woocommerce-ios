0e60453142188085f821880f34bf4636c46a45f9
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2021-09-06 19:42:02 +0300

    Add tests for EditAddressFormViewModel

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Orders/Order Details/Addresses/EditAddressFormViewModelTests.swift
@@ -0,0 +1,72 @@
+import XCTest
+import Yosemite
+import TestKit
+@testable import WooCommerce
+
+final class EditAddressFormViewModelTests: XCTestCase {
+
+    func test_creating_with_address_prefills_fields_with_correct_data() {
+        // Given
+        let address = sampleAddress()
+        let viewModel = EditAddressFormViewModel(address: address)
+
+        // Then
+        XCTAssertEqual(viewModel.firstName, address.firstName)
+        XCTAssertEqual(viewModel.lastName, address.lastName)
+        XCTAssertEqual(viewModel.email, address.email ?? "")
+        XCTAssertEqual(viewModel.phone, address.phone ?? "")
+
+        XCTAssertEqual(viewModel.company, address.company ?? "")
+        XCTAssertEqual(viewModel.address1, address.address1)
+        XCTAssertEqual(viewModel.address2, address.address2 ?? "")
+        XCTAssertEqual(viewModel.city, address.city)
+        XCTAssertEqual(viewModel.postcode, address.postcode)
+
+        XCTAssertFalse(viewModel.isDoneButtonEnabled)
+    }
+
+    func test_updating_fields_enables_done_button() {
+        // Given
+        let address = sampleAddress()
+        let viewModel = EditAddressFormViewModel(address: address)
+        XCTAssertFalse(viewModel.isDoneButtonEnabled)
+
+        // When
+        viewModel.firstName = "John"
+
+        // Then
+        XCTAssertTrue(viewModel.isDoneButtonEnabled)
+    }
+
+    func test_updating_fields_back_to_original_values_disables_done_button() {
+        // Given
+        let address = sampleAddress()
+        let viewModel = EditAddressFormViewModel(address: address)
+        XCTAssertFalse(viewModel.isDoneButtonEnabled)
+
+        // When
+        viewModel.firstName = "John"
+        viewModel.lastName = "Ipsum"
+        viewModel.firstName = "Johnny"
+        viewModel.lastName = "Appleseed"
+
+        // Then
+        XCTAssertFalse(viewModel.isDoneButtonEnabled)
+    }
+}
+
+private extension EditAddressFormViewModelTests {
+    func sampleAddress() -> Address {
+        return Address(firstName: "Johnny",
+                       lastName: "Appleseed",
+                       company: nil,
+                       address1: "234 70th Street",
+                       address2: nil,
+                       city: "Niagara Falls",
+                       state: "NY",
+                       postcode: "14304",
+                       country: "US",
+                       phone: "333-333-3333",
+                       email: "scrambled@scrambled.com")
+    }
+}

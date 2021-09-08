bb7145a830e44bec75a1ce8d01de45db10636687
Huong Do <hi.huongdo@gmail.com>
2021-09-08 09:47:46 +0700

    Update comments for ShippingLabelPackageInfo

--- WooCommerce/Classes/Model/ShippingLabelPackageInfo.swift
+++ WooCommerce/Classes/Model/ShippingLabelPackageInfo.swift
@@ -3,7 +3,13 @@ import Foundation
 /// A structure to hold the basic details for a selected package on Package Details screen of Shipping Label purchase flow.
 ///
 struct ShippingLabelPackageInfo: Hashable {
+
+    /// ID of the selected package.
     let packageID: String
+
+    /// Total weight of the package in string value.
     let totalWeight: String
+
+    /// List of product or variation IDs for items in the package.
     let productIDs: [Int64]
 }

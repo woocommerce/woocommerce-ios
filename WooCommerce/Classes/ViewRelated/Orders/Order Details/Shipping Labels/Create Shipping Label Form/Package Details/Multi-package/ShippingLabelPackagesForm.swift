813b0edbe5de22efbd860543923166b01fd532e8
Huong Do <hi.huongdo@gmail.com>
2021-09-09 11:30:40 +0700

    Add new view ShippingLabelPackagesForm

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/Multi-package/ShippingLabelPackagesForm.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ShippingLabelPackagesForm: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ShippingLabelPackagesForm_Previews: PreviewProvider {
+    static var previews: some View {
+        ShippingLabelPackagesForm()
+    }
+}

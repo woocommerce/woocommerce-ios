3eddb033dfd30cb16ab68226de4f7167a7695969
Huong Do <hi.huongdo@gmail.com>
2021-09-09 15:42:48 +0700

    Configure package selection for package item

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/Multi-package/ShippingLabelPackateItemViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/Multi-package/ShippingLabelPackateItemViewModel.swift
@@ -9,6 +9,39 @@ final class ShippingLabelPackageItemViewModel: ObservableObject {
     ///
     let selectedPackageID: String
 
+    var dimensionUnit: String {
+        return packagesResponse?.storeOptions.dimensionUnit ?? ""
+    }
+    var customPackages: [ShippingLabelCustomPackage] {
+        return packagesResponse?.customPackages ?? []
+    }
+    var predefinedOptions: [ShippingLabelPredefinedOption] {
+        return packagesResponse?.predefinedOptions ?? []
+    }
+
+    /// Whether there are saved custom or predefined packages to select from.
+    ///
+    var hasCustomOrPredefinedPackages: Bool {
+        return customPackages.isNotEmpty || predefinedOptions.isNotEmpty
+    }
+
+    /// The title of the selected package, if any.
+    ///
+    var selectedPackageName: String {
+        if let selectedCustomPackage = selectedCustomPackage {
+            return selectedCustomPackage.title
+        }
+        else if let selectedPredefinedPackage = selectedPredefinedPackage {
+            return selectedPredefinedPackage.title
+        }
+        else {
+            return Localization.selectPackagePlaceholder
+        }
+    }
+    @Published private(set) var selectedCustomPackage: ShippingLabelCustomPackage?
+    @Published private(set) var selectedPredefinedPackage: ShippingLabelPredefinedPackage?
+    @Published var totalWeight: String = ""
+
     private let order: Order
     private let orderItems: [OrderItem]
     private let currency: String
@@ -38,5 +71,70 @@ final class ShippingLabelPackageItemViewModel: ObservableObject {
         self.weightUnit = weightUnit
         self.packagesResponse = packagesResponse
         self.selectedPackageID = selectedPackageID
+
+        didSelectPackage(selectedPackageID)
+    }
+}
+
+// MARK: - Package Selection
+extension ShippingLabelPackageItemViewModel {
+    func didSelectPackage(_ id: String) {
+        selectCustomPackage(id)
+        selectPredefinedPackage(id)
+    }
+
+    private func selectCustomPackage(_ id: String) {
+        guard let packagesResponse = packagesResponse else {
+            return
+        }
+
+        for customPackage in packagesResponse.customPackages {
+            if customPackage.title == id {
+                selectedCustomPackage = customPackage
+                selectedPredefinedPackage = nil
+                return
+            }
+        }
+    }
+
+    private func selectPredefinedPackage(_ id: String) {
+        guard let packagesResponse = packagesResponse else {
+            return
+        }
+
+        for option in packagesResponse.predefinedOptions {
+            for predefinedPackage in option.predefinedPackages {
+                if predefinedPackage.id == id {
+                    selectedCustomPackage = nil
+                    selectedPredefinedPackage = predefinedPackage
+                    return
+                }
+            }
+        }
+    }
+}
+
+private extension ShippingLabelPackageItemViewModel {
+    enum Localization {
+        static let subtitleFormat =
+            NSLocalizedString("%1$@", comment: "In Shipping Labels Package Details,"
+                                + " the pattern used to show the weight of a product. For example, “1lbs”.")
+        static let subtitleWithAttributesFormat =
+            NSLocalizedString("%1$@・%2$@", comment: "In Shipping Labels Package Details if the product has attributes,"
+                                + " the pattern used to show the attributes and weight. For example, “purple, has logo・1lbs”."
+                                + " The %1$@ is the list of attributes (e.g. from variation)."
+                                + " The %2$@ is the weight with the unit.")
+        static func subtitle(weight: String?, weightUnit: String, attributes: [VariationAttributeViewModel]) -> String {
+            let attributesText = attributes.map { $0.nameOrValue }.joined(separator: ", ")
+            let formatter = WeightFormatter(weightUnit: weightUnit)
+            let weight = formatter.formatWeight(weight: weight)
+            if attributes.isEmpty {
+                return String.localizedStringWithFormat(subtitleFormat, weight, weightUnit)
+            } else {
+                return String.localizedStringWithFormat(subtitleWithAttributesFormat, attributesText, weight)
+            }
+        }
+        static let selectPackagePlaceholder = NSLocalizedString("Select a package",
+                                                                comment: "Placeholder for the selected package in the Shipping Labels Package Details screen")
     }
 }

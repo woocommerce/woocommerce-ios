09d1a3f04a8478825363c87a5742f845d8d8db3f
Huong Do <hi.huongdo@gmail.com>
2021-09-09 11:37:58 +0700

    Configure products and variations for packages form view model

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/Multi-package/ShippingLabelPackagesFormViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/Multi-package/ShippingLabelPackagesFormViewModel.swift
@@ -17,6 +17,14 @@ final class ShippingLabelPackagesFormViewModel: ObservableObject {
     /// List of selected package with basic info.
     ///
     @Published private var selectedPackages: [ShippingLabelPackageAttributes] = []
+    
+    /// Products contained inside the Order and fetched from Core Data
+    ///
+    @Published private var products: [Product] = []
+
+    /// ProductVariations contained inside the Order and fetched from Core Data
+    ///
+    @Published private var productVariations: [ProductVariation] = []
 
     init(order: Order,
          packagesResponse: ShippingLabelPackagesResponse?,
@@ -29,5 +37,55 @@ final class ShippingLabelPackagesFormViewModel: ObservableObject {
         self.stores = stores
         self.storageManager = storageManager
         self.selectedPackages = selectedPackages
+
+        configureResultsControllers()
+        syncProducts()
+        syncProductVariations()
+    }
+
+    private func configureResultsControllers() {
+        resultsControllers = ShippingLabelPackageDetailsResultsControllers(siteID: order.siteID,
+                                                                           orderItems: order.items,
+                                                                           storageManager: storageManager,
+           onProductReload: { [weak self] (products) in
+            guard let self = self else { return }
+            self.products = products
+        }, onProductVariationsReload: { [weak self] (productVariations) in
+            guard let self = self else { return }
+            self.productVariations = productVariations
+        })
+
+        products = resultsControllers?.products ?? []
+        productVariations = resultsControllers?.productVariations ?? []
+    }
+}
+
+/// API Requests
+///
+private extension ShippingLabelPackagesFormViewModel {
+    func syncProducts(onCompletion: ((Error?) -> ())? = nil) {
+        let action = ProductAction.requestMissingProducts(for: order) { (error) in
+            if let error = error {
+                DDLogError("⛔️ Error synchronizing Products: \(error)")
+                onCompletion?(error)
+                return
+            }
+
+            onCompletion?(nil)
+        }
+
+        stores.dispatch(action)
+    }
+
+    func syncProductVariations(onCompletion: ((Error?) -> ())? = nil) {
+        let action = ProductVariationAction.requestMissingVariations(for: order) { error in
+            if let error = error {
+                DDLogError("⛔️ Error synchronizing missing variations in an Order: \(error)")
+                onCompletion?(error)
+                return
+            }
+            onCompletion?(nil)
+        }
+        stores.dispatch(action)
     }
 }

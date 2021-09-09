b8bd691502d67a1ab35fca62e75a5abc9d87a497
Huong Do <hi.huongdo@gmail.com>
2021-09-09 11:49:55 +0700

    Add view and view model for package item

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/Multi-package/ShippingLabelPackageItem.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ShippingLabelPackageItem: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ShippingLabelPackageItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ShippingLabelPackageItem()
+    }
+}

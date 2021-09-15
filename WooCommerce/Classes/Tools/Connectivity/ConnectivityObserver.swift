a3851ca399343664f158990075264958e7ec029a
Huong Do <hi.huongdo@gmail.com>
2021-09-15 12:28:31 +0700

    Remove `isNetworkReachable` in ConnectivityObserver and update cases for ConnectionType

--- WooCommerce/Classes/Tools/Connectivity/ConnectivityObserver.swift
+++ WooCommerce/Classes/Tools/Connectivity/ConnectivityObserver.swift
@@ -1,9 +1,6 @@
 import Foundation
 
 protocol ConnectivityObserver {
-    /// Gets current status of the connection.
-    var isNetworkReachable: Bool { get }
-
     /// Starts the observer with a listener.
     func startObserving(listener: @escaping (ConnectivityStatus) -> Void)
 
@@ -25,8 +22,10 @@ enum ConnectivityStatus: Equatable {
 /// Defines the various connection types detected.
 ///
 /// - ethernetOrWiFi: The connection type is either over Ethernet or WiFi.
-/// - wwan:           The connection type is a WWAN connection.
+/// - cellular:       The connection type is a cellular connection.
+/// - other:          The connection type is via a local loopback network, virtual network or other unknown types.
 enum ConnectionType: Equatable {
     case ethernetOrWiFi
-    case wwan
+    case cellular
+    case other
 }

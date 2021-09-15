5875e0af25cd51369994fbfbb2ebac25526b926d
Shiki <jayson@basanes.net>
2021-09-15 15:20:49 -0600

    Fix failing ULErrorViewControllerTests

--- WooCommerce/WooCommerceTests/Authentication/ULErrorViewControllerTests.swift
+++ WooCommerce/WooCommerceTests/Authentication/ULErrorViewControllerTests.swift
@@ -1,7 +1,18 @@
 import XCTest
+import WordPressAuthenticator
 @testable import WooCommerce
 
 final class ULErrorViewControllerTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+
+        initializeAuthenticator()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
 
     func test_viewcontroller_presents_image_provided_by_viewmodel() throws {
         // Given
@@ -154,5 +165,105 @@ private final class ErrorViewModel: ULErrorViewModel {
 
     func didTapAuxiliaryButton(in viewController: UIViewController?) {
         auxiliaryButtonTapped = true
+    }
+// MARK: - WordPressAuthenticator Initialization
+
+private extension ULErrorViewControllerTests {
+    /// Initialize `WordPressAuthenticator` with dummy configuration.
+    ///
+    /// For some reason, the tests in this class fail when the `.view` is loaded because
+    /// `NUXButton` instances used within `ULErrorViewController` seem to _phone home_ and access
+    /// the `WordPressAuthenticator` instance. And since the `WordPressAuthenticator` instance
+    /// was not initialized, I get a unit test fatal error:
+    ///
+    /// ```
+    /// WordPressAuthenticator wasn't initialized
+    /// ```
+    ///
+    /// Initializing with dummy data seems to work.
+    ///
+    /// There is no known way to tear down this instance. Once initialized, it's initialized. And
+    /// I don't believe we use `WordPressAuthenticator` anywhere else. So hopefully this will not
+    /// bleed into the other unit tests. ¯\(°_o)/¯
+    func initializeAuthenticator() {
+        let configuration = WordPressAuthenticatorConfiguration(wpcomClientId: "",
+                                                                wpcomSecret: "",
+                                                                wpcomScheme: "",
+                                                                wpcomTermsOfServiceURL: "",
+                                                                wpcomAPIBaseURL: "",
+                                                                googleLoginClientId: "",
+                                                                googleLoginServerClientId: "",
+                                                                googleLoginScheme: "",
+                                                                userAgent: "",
+                                                                showLoginOptions: true,
+                                                                enableSignUp: false,
+                                                                enableSignInWithApple: false,
+                                                                enableSignupWithGoogle: false,
+                                                                enableUnifiedAuth: true,
+                                                                continueWithSiteAddressFirst: true)
+
+        let style = WordPressAuthenticatorStyle(primaryNormalBackgroundColor: .red,
+                                                primaryNormalBorderColor: .red,
+                                                primaryHighlightBackgroundColor: .red,
+                                                primaryHighlightBorderColor: .red,
+                                                secondaryNormalBackgroundColor: .red,
+                                                secondaryNormalBorderColor: .red,
+                                                secondaryHighlightBackgroundColor: .red,
+                                                secondaryHighlightBorderColor: .red,
+                                                disabledBackgroundColor: .red,
+                                                disabledBorderColor: .red,
+                                                primaryTitleColor: .primaryButtonTitle,
+                                                secondaryTitleColor: .red,
+                                                disabledTitleColor: .red,
+                                                disabledButtonActivityIndicatorColor: .red,
+                                                textButtonColor: .red,
+                                                textButtonHighlightColor: .red,
+                                                instructionColor: .red,
+                                                subheadlineColor: .red,
+                                                placeholderColor: .red,
+                                                viewControllerBackgroundColor: .red,
+                                                textFieldBackgroundColor: .red,
+                                                buttonViewBackgroundColor: .red,
+                                                buttonViewTopShadowImage: nil,
+                                                navBarImage: UIImage(),
+                                                navBarBadgeColor: .red,
+                                                navBarBackgroundColor: .red,
+                                                prologueTopContainerChildViewController: nil,
+                                                statusBarStyle: .default)
+
+        let displayStrings = WordPressAuthenticatorDisplayStrings(emailLoginInstructions: "",
+                                                                  getStartedInstructions: "",
+                                                                  jetpackLoginInstructions: "",
+                                                                  siteLoginInstructions: "",
+                                                                  usernamePasswordInstructions: "",
+                                                                  continueWithWPButtonTitle: "",
+                                                                  enterYourSiteAddressButtonTitle: "",
+                                                                  findSiteButtonTitle: "",
+                                                                  signupTermsOfService: "",
+                                                                  getStartedTitle: "")
+
+        let unifiedStyle = WordPressAuthenticatorUnifiedStyle(borderColor: .red,
+                                                              errorColor: .red,
+                                                              textColor: .red,
+                                                              textSubtleColor: .red,
+                                                              textButtonColor: .red,
+                                                              textButtonHighlightColor: .red,
+                                                              viewControllerBackgroundColor: .red,
+                                                              prologueButtonsBackgroundColor: .red,
+                                                              prologueViewBackgroundColor: .red,
+                                                              navBarBackgroundColor: .red,
+                                                              navButtonTextColor: .red,
+                                                              navTitleTextColor: .red)
+
+        let displayImages = WordPressAuthenticatorDisplayImages(
+            magicLink: UIImage(),
+            siteAddressModalPlaceholder: UIImage()
+        )
+
+        WordPressAuthenticator.initialize(configuration: configuration,
+                                          style: style,
+                                          unifiedStyle: unifiedStyle,
+                                          displayImages: displayImages,
+                                          displayStrings: displayStrings)
     }
 }

5866b3a3fd252281ca2103f4a42cb5c7550c59e4
Shiki <jayson@basanes.net>
2021-09-15 16:28:02 -0600

    Add login_what_is_jetpack_help_screen_learn_more_button_tapped event

--- WooCommerce/Classes/Analytics/WooAnalyticsStat.swift
+++ WooCommerce/Classes/Analytics/WooAnalyticsStat.swift
@@ -39,6 +39,7 @@ public enum WooAnalyticsStat: String {
     case loginJetpackRequiredViewInstructionsButtonTapped = "login_jetpack_required_view_instructions_button_tapped"
     case loginWhatIsJetpackHelpScreenViewed               = "login_what_is_jetpack_help_screen_viewed"
     case loginWhatIsJetpackHelpScreenOkButtonTapped       = "login_what_is_jetpack_help_screen_ok_button_tapped"
+    case loginWhatIsJetpackHelpScreenLearnMoreButtonTapped = "login_what_is_jetpack_help_screen_learn_more_button_tapped"
     case loginMagicLinkOpenEmailClientViewed              = "login_magic_link_open_email_client_viewed"
     case loginMagicLinkRequestFormViewed                  = "login_magic_link_request_form_viewed"
     case loginMagicLinkExited                             = "login_magic_link_exited"

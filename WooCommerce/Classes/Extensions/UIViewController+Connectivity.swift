b21d1961b8f948f42dba6600709b9c52636bbf45
Huong Do <hi.huongdo@gmail.com>
2021-09-16 11:14:20 +0700

    Add extension for UIViewController to configure offline banner

---
+++ WooCommerce/Classes/Extensions/UIViewController+Connectivity.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+extension UIViewController {
+    /// Content of offline banner
+    ///
+    var offlineContentView: UIView {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 3
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .center
+
+        let imageView = UIImageView(image: .lightningImage)
+        imageView.tintColor = .white
+        imageView.contentMode = .scaleAspectFit
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalToConstant: 24),
+            imageView.heightAnchor.constraint(equalToConstant: 24)
+        ])
+
+        let messageLabel = UILabel()
+        messageLabel.text = NSLocalizedString("Offline - using cached data", comment: "Message for offline banner")
+        messageLabel.applySecondaryBodyStyle()
+        messageLabel.textColor = .white
+
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(messageLabel)
+        return stackView
+    }
+
+    /// Set up toolbar for the view controller to display the offline message,
+    /// and listen to connectivity status changes to change the toolbar's visibility.
+    ///
+    func configureOfflineBanner() {
+        let offlineItem = UIBarButtonItem(customView: offlineContentView)
+        let spaceItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbarItems = [spaceItem, offlineItem, spaceItem]
+        navigationController?.toolbar.barTintColor = .gray
+
+        ServiceLocator.connectivityObserver.startObserving { [weak self] status in
+            self?.navigationController?.setToolbarHidden(status != .notReachable, animated: true)
+        }
+    }
+}

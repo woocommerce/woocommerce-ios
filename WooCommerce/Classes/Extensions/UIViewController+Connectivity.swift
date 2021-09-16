0bd664318c412c59fb14581e6c578f10c1af7712
Huong Do <hi.huongdo@gmail.com>
2021-09-16 15:14:21 +0700

    Update configuring offline banner in UIViewController extension

--- WooCommerce/Classes/Extensions/UIViewController+Connectivity.swift
+++ WooCommerce/Classes/Extensions/UIViewController+Connectivity.swift
@@ -37,7 +37,10 @@ extension UIViewController {
         toolbarItems = [spaceItem, offlineItem, spaceItem]
         navigationController?.toolbar.barTintColor = .gray
 
-        ServiceLocator.connectivityObserver.startObserving { [weak self] status in
+        let connected = ServiceLocator.connectivityObserver.isConnectivityAvailable
+        navigationController?.setToolbarHidden(connected, animated: true)
+
+        ServiceLocator.connectivityObserver.updateListener { [weak self] status in
             guard let self = self,
                   self.isViewOnScreen() else { return }
             self.navigationController?.setToolbarHidden(status != .notReachable, animated: true)

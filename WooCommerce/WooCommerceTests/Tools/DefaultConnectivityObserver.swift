fcf09d0ffa2cc8eb0a3875f19701d726d1714441
Huong Do <hi.huongdo@gmail.com>
2021-09-17 10:10:41 +0700

    Add unit test for network update

--- WooCommerce/WooCommerceTests/Tools/DefaultConnectivityObserver.swift
+++ WooCommerce/WooCommerceTests/Tools/DefaultConnectivityObserver.swift
@@ -51,6 +51,31 @@ final class DefaultConnectivityObserverTests: XCTestCase {
         // Then
         XCTAssertFalse(observer.isConnectivityAvailable)
     }
+
+    func test_updateListener_returns_correct_status_in_callback_closure() {
+        // Given
+        let network = MockNetwork(status: .satisfied, currentInterface: .wifi)
+        let networkMonitor = MockNetworkMonitor(currentNetwork: network)
+        let networkUpdate = MockNetwork(status: .satisfied, currentInterface: .cellular)
+        let statusExpectation = expectation(description: "Status in callback closure")
+
+        // When
+        let observer = DefaultConnectivityObserver(networkMonitor: networkMonitor)
+        var result: ConnectivityStatus = .unknown
+        observer.updateListener { status in
+            result = status
+            statusExpectation.fulfill()
+        }
+        networkMonitor.fakeNetworkUpdate(network: networkUpdate)
+
+        // Then
+        waitForExpectations(timeout: 0.3, handler: nil)
+        if case .reachable(let type) = result {
+            XCTAssertEqual(type, .cellular)
+        } else {
+            XCTFail("Incorrect result status in callback closure")
+        }
+    }
 }
 
 final class MockNetworkMonitor: NetworkMonitoring {
@@ -63,6 +88,10 @@ final class MockNetworkMonitor: NetworkMonitoring {
 
     init(currentNetwork: NetworkMonitorable) {
         self.currentNetwork = currentNetwork
+    }
+
+    func fakeNetworkUpdate(network: NetworkMonitorable) {
+        networkUpdateHandler?(network)
     }
 
     func start(queue: DispatchQueue) {

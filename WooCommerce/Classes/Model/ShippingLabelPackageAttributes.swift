7f178a2954824c4c928a85ebf0b72eb6e279b3b9
Huong Do <hi.huongdo@gmail.com>
2021-09-23 16:05:09 +0700

    Update ShippingLabelPackageAttributes with optional length, width and height

--- WooCommerce/Classes/Model/ShippingLabelPackageAttributes.swift
+++ WooCommerce/Classes/Model/ShippingLabelPackageAttributes.swift
@@ -4,6 +4,9 @@ import Foundation
 ///
 struct ShippingLabelPackageAttributes: Hashable, Equatable {
 
+    /// Default box ID for original packages.
+    static let originalPackagingBoxID = "individual"
+
     /// ID of the selected package.
     let packageID: String
 
@@ -12,4 +15,22 @@ struct ShippingLabelPackageAttributes: Hashable, Equatable {
 
     /// List of product or variation IDs for items in the package.
     let productIDs: [Int64]
+
+    /// Length of the package, required if the package is original packaging.
+    let length: Double?
+
+    /// Width of the package, required if the package is original packaging.
+    let width: Double?
+
+    /// Height of the package, required if the package is original packaging.
+    let height: Double?
+
+    init(packageID: String, totalWeight: String, productIDs: [Int64], length: Double? = nil, width: Double? = nil, height: Double? = nil) {
+        self.packageID = packageID
+        self.totalWeight = totalWeight
+        self.productIDs = productIDs
+        self.length = length
+        self.width = width
+        self.height = height
+    }
 }

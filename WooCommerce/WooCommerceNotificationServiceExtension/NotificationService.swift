1176d5f5a722be8519190ccfce5dc5a814b27a09
Jaclyn Chen <watertranquil@gmail.com>
2021-09-28 17:21:46 +0800

    Update notification content for new order and review notifications.

--- WooCommerce/WooCommerceNotificationServiceExtension/NotificationService.swift
+++ WooCommerce/WooCommerceNotificationServiceExtension/NotificationService.swift
@@ -1,6 +1,6 @@
 import UserNotifications
 
-class NotificationService: UNNotificationServiceExtension {
+final class NotificationService: UNNotificationServiceExtension {
 
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
@@ -8,15 +8,39 @@ class NotificationService: UNNotificationServiceExtension {
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
+
+        guard let content = request.content.mutableCopy() as? UNMutableNotificationContent else {
+            return
         }
+
+        guard let type = NotificationType(rawValue: content.categoryIdentifier) else {
+            bestAttemptContent = content
+            contentHandler(content)
+            return
+        }
+
+        if let siteID = content.userInfo[NotificationKey.siteID] as? Int {
+            content.threadIdentifier = "\(type.rawValue) \(siteID)"
+        }
+
+        switch type {
+        case .storeOrder:
+            if let message = content.userInfo[NotificationKey.message] as? String {
+                content.title = NSLocalizedString("You have 1 new order! 🎉", comment: "Title of new order push notification.")
+                content.body = message
+            }
+        case .storeReview:
+            if let message = content.userInfo[NotificationKey.message] as? String {
+                content.title = NSLocalizedString("You have 1 new review! ⭐️", comment: "Title of new order push notification.")
+                let prompt = content.body
+                content.body = "\(prompt): \(message)" // TODO: localize
+            }
+        }
+
+        bestAttemptContent = content
+        contentHandler(content)
     }
-    
+
     override func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
@@ -24,5 +48,16 @@ class NotificationService: UNNotificationServiceExtension {
             contentHandler(bestAttemptContent)
         }
     }
+}
 
+// MARK: - Private Types
+//
+private enum NotificationKey {
+    static let siteID = "blog"
+    static let message = "message"
+}
+
+private enum NotificationType: String {
+    case storeOrder = "store_order"
+    case storeReview = "store_review"
 }

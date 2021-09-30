b5f69080c5c3225df04e21ad3c761992060a0024
Gio Lodi <giovanni.lodi42@gmail.com>
2021-09-30 15:12:35 +1000

    Convert `BetaFeaturesScreen` to be a `ScreenObject` subclass

--- WooCommerce/UITestsFoundation/Screens/Settings/BetaFeaturesScreen.swift
+++ WooCommerce/UITestsFoundation/Screens/Settings/BetaFeaturesScreen.swift
@@ -1,16 +1,24 @@
+import ScreenObject
 import XCTest
 
-class BetaFeaturesScreen: BaseScreen {
+// This screen is currently unused. Given the purpose of UITestsFoundation is to provide developers
+// with an easy to use API to write tests, I'm going to keep the screen in the framework anyway, so
+// it'll be already available if someone needs to work with it or on it in the future.
+class BetaFeaturesScreen: ScreenObject {
 
     struct ElementStringIDs {
         static let enableProductsButton = "beta-features-products-cell"
     }
 
-    private let enableProductsButton = XCUIApplication().cells[ElementStringIDs.enableProductsButton]
+    // `expectedElement` is a `ScreenObject` utility to get the first element for the
+    // `expectedElementGetters` list.
+    private var enableProductsButton: XCUIElement { expectedElement }
 
-    init() {
-        super.init(element: enableProductsButton)
-        XCTAssert(enableProductsButton.waitForExistence(timeout: 3))
+    init(app: XCUIApplication = XCUIApplication()) throws {
+        try super.init(
+            expectedElementGetters: [ { $0.cells["beta-features-products-cell"] } ],
+            app: app
+        )
     }
 
     @discardableResult

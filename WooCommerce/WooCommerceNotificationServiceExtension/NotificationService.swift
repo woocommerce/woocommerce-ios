1194152fd01af5f1cbf0dfda18899eccd2f3d3d4
Jaclyn Chen <watertranquil@gmail.com>
2021-10-01 15:46:27 +0800

    Update extension's `NotificationService` to call `NotificationServiceExtension.NotificationService`.

--- WooCommerce/WooCommerceNotificationServiceExtension/NotificationService.swift
+++ WooCommerce/WooCommerceNotificationServiceExtension/NotificationService.swift
@@ -1,86 +1,14 @@
-import Experiments
 import UserNotifications
+import NotificationServiceExtension
 
 final class NotificationService: UNNotificationServiceExtension {
-
-    private var contentHandler: ((UNNotificationContent) -> Void)?
-    private var bestAttemptContent: UNNotificationContent?
-
-    // `UNNotificationServiceExtension` does not allow a custom initializer, thus the feature flag service is a public property for unit testing.
-    var featureFlagService: FeatureFlagService = DefaultFeatureFlagService()
+    private lazy var service: UNNotificationServiceExtension = NotificationServiceExtension.NotificationService()
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-
-        guard let content = request.content.mutableCopy() as? UNMutableNotificationContent else {
-            return
-        }
-
-        guard featureFlagService.isFeatureFlagEnabled(.pushNotificationsForAllStores) else {
-            setContent(content)
-            return
-        }
-
-        guard let type = NotificationType(rawValue: content.categoryIdentifier),
-              let siteID = content.userInfo[NotificationKey.siteID] as? Int64 else {
-            setContent(content)
-            return
-        }
-
-        content.threadIdentifier = "\(type.rawValue) \(siteID)"
-
-        switch type {
-        case .storeOrder:
-            if let message = content.userInfo[NotificationKey.message] as? String {
-                content.title = Localization.orderNotificationTitle
-                content.body = message
-            }
-        case .storeReview:
-            if let message = content.userInfo[NotificationKey.message] as? String {
-                content.title = Localization.reviewNotificationTitle
-                let prompt = content.body
-                content.body = String.localizedStringWithFormat(Localization.reviewNotificationBodyFormat, prompt, message)
-            }
-        }
-
-        setContent(content)
+        service.didReceive(request, withContentHandler: contentHandler)
     }
 
     override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
-    }
-}
-
-private extension NotificationService {
-    func setContent(_ content: UNNotificationContent) {
-        bestAttemptContent = content
-        contentHandler?(content)
-    }
-}
-
-// MARK: - Private Types
-//
-private extension NotificationService {
-    enum NotificationKey {
-        static let siteID = "blog"
-        static let message = "message"
-    }
-
-    enum NotificationType: String {
-        case storeOrder = "store_order"
-        case storeReview = "store_review"
-    }
-
-    enum Localization {
-        static let orderNotificationTitle = NSLocalizedString("You have 1 new order! 🎉", comment: "Title of new order push notification.")
-        static let reviewNotificationTitle = NSLocalizedString("You have 1 new review! ⭐️", comment: "Title of new review push notification.")
-        static let reviewNotificationBodyFormat = NSLocalizedString("%1$@: %2$@", comment: "Body format of new review push notification."
-                                                                        + " %1$@ reads like 'User name left a review on product name."
-                                                                        + " %2$@ is the review content.")
+        service.serviceExtensionTimeWillExpire()
     }
 }

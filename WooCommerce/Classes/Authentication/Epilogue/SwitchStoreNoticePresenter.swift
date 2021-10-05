4889b04c21b283be4f98146b7ba607117bd91174
Jaclyn Chen <watertranquil@gmail.com>
2021-10-05 15:22:24 +0800

    Update notice title after switching stores based on feature flag.

--- WooCommerce/Classes/Authentication/Epilogue/SwitchStoreNoticePresenter.swift
+++ WooCommerce/Classes/Authentication/Epilogue/SwitchStoreNoticePresenter.swift
@@ -7,11 +7,14 @@ final class SwitchStoreNoticePresenter {
 
     private let sessionManager: SessionManagerProtocol
     private let noticePresenter: NoticePresenter
+    private let featureFlagService: FeatureFlagService
 
     init(sessionManager: SessionManagerProtocol = ServiceLocator.stores.sessionManager,
-         noticePresenter: NoticePresenter = ServiceLocator.noticePresenter) {
+         noticePresenter: NoticePresenter = ServiceLocator.noticePresenter,
+         featureFlagService: FeatureFlagService = ServiceLocator.featureFlagService) {
         self.sessionManager = sessionManager
         self.noticePresenter = noticePresenter
+        self.featureFlagService = featureFlagService
     }
 
     /// Present the switch notice to the user, with the new configured store name.
@@ -24,13 +27,25 @@ final class SwitchStoreNoticePresenter {
             return
         }
 
-        let messageFormat = NSLocalizedString("Switched to %1$@. You will only receive notifications from this store.",
-                                              comment: "Message presented after users switch to a new store. "
-                                                + "Reads like: Switched to {store name}. You will only receive notifications from this store. "
-                                                + "Parameters: %1$@ - store name")
-        let message = String.localizedStringWithFormat(messageFormat, newStoreName)
-        let notice = Notice(title: message, feedbackType: .success)
+        let titleFormat = featureFlagService.isFeatureFlagEnabled(.pushNotificationsForAllStores) ?
+            Localization.titleFormat: Localization.titleFormatWithPushNotificationsForAllStoresDisabled
+        let title = String.localizedStringWithFormat(titleFormat, newStoreName)
+        let notice = Notice(title: title, feedbackType: .success)
 
         noticePresenter.enqueue(notice: notice)
+    }
+}
+
+extension SwitchStoreNoticePresenter {
+    enum Localization {
+        static let titleFormatWithPushNotificationsForAllStoresDisabled =
+            NSLocalizedString("Switched to %1$@. You will only receive notifications from this store.",
+                              comment: "Message presented after users switch to a new store when multi-store push notifications are not supported. "
+                                + "Reads like: Switched to {store name}. You will only receive notifications from this store. "
+                                + "Parameters: %1$@ - store name")
+        static let titleFormat = NSLocalizedString("Switched to %1$@.",
+                                                   comment: "Message presented after users switch to a new store. "
+                                                    + "Reads like: Switched to {store name}. You will only receive notifications from this store. "
+                                                    + "Parameters: %1$@ - store name")
     }
 }

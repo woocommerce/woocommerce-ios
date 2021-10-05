cabb2edc7a122654e2e5b07e85113bddcd2564bd
Paolo Musolino <info@codeido.com>
2021-10-05 16:52:09 +0200

    update: code style

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Carriers and Rates/ShippingLabelCarriersSectionViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Carriers and Rates/ShippingLabelCarriersSectionViewModel.swift
@@ -4,5 +4,5 @@ struct ShippingLabelCarriersSectionViewModel: Identifiable {
     internal let id = UUID()
 
     let packageNumber: Int
-    var rows: [ShippingLabelCarrierRowViewModel] = []
+    let rows: [ShippingLabelCarrierRowViewModel]
 }

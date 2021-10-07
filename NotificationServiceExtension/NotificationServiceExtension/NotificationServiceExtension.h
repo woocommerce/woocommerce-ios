b80997c24d3b109e0ee1d7e961d2d5b27af9b37a
Jaclyn Chen <watertranquil@gmail.com>
2021-10-07 11:46:35 +0800

    Add a new framework project NotificationServiceExtension without any changes.

---
+++ NotificationServiceExtension/NotificationServiceExtension/NotificationServiceExtension.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for NotificationServiceExtension.
+FOUNDATION_EXPORT double NotificationServiceExtensionVersionNumber;
+
+//! Project version string for NotificationServiceExtension.
+FOUNDATION_EXPORT const unsigned char NotificationServiceExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificationServiceExtension/PublicHeader.h>
+
+

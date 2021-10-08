e6784577d7161bbe3b90eea4470474e38622279b
Jaclyn Chen <watertranquil@gmail.com>
2021-10-08 09:31:19 +0800

    Create `NotificationService` in NotificationServiceExtension framework. In the extension, call functions from `NotificationService` in the framework.

---
+++ NotificationServiceExtension/NotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,6 @@
+import UserNotifications
+
+/// Modifies notification content based on payload from the server to support multi-store push notifications.
+public class NotificationService: UNNotificationServiceExtension {
+    // TODO-5032: will be implemented in a separate PR
+}
--- WooCommerce/WooCommerceNotificationServiceExtension/NotificationService.swift
+++ WooCommerce/WooCommerceNotificationServiceExtension/NotificationService.swift
@@ -1,5 +1,16 @@
 import UserNotifications
+import NotificationServiceExtension
 
+/// A UNNotificationServiceExtension subclass must be defined in the extension target and is set in Info.plist's `NSExtensionPrincipalClass` field.
+/// The main logic is in `NotificationServiceExtension.NotificationService`, which is in a separate framework for unit testing.
 final class NotificationService: UNNotificationServiceExtension {
-    // TODO-5032: will be implemented in a separate PR
+    private let service: UNNotificationServiceExtension = NotificationServiceExtension.NotificationService()
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        service.didReceive(request, withContentHandler: contentHandler)
+    }
+
+    override func serviceExtensionTimeWillExpire() {
+        service.serviceExtensionTimeWillExpire()
+    }
 }

2d81feeaa151322336277814742faa8491e669ba
Huong Do <hi.huongdo@gmail.com>
2021-10-14 15:59:25 +0700

    Add action sheet for ItemToFulfillRow

--- WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/ItemToFulfillRow.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/ItemToFulfillRow.swift
@@ -7,9 +7,46 @@ struct ItemToFulfillRow: View, Identifiable {
     let productOrVariationID: Int64
     let title: String
     let subtitle: String
+    var moveItemActionSheetTitle: String = ""
+    var moveItemActionSheetButtons: [ActionSheet.Button] = []
+    @State private var showingMoveItemDialog: Bool = false
 
     var body: some View {
-        TitleAndSubtitleRow(title: title, subtitle: subtitle)
+        if ServiceLocator.featureFlagService.isFeatureFlagEnabled(.shippingLabelsInternational) {
+            HStack {
+                TitleAndSubtitleRow(title: title, subtitle: subtitle)
+                Spacer()
+                Button(action: {
+                    guard !showingMoveItemDialog else {
+                        showingMoveItemDialog = false
+                        return
+                    }
+                    showingMoveItemDialog = true
+                    ServiceLocator.analytics.track(.shippingLabelMoveItemTapped)
+                }, label: {
+                    Text(Localization.moveButton)
+                        .font(.footnote)
+                        .foregroundColor(Color(UIColor(color: .accent)))
+                })
+                .padding(.trailing, Constants.horizontalPadding)
+                .actionSheet(isPresented: $showingMoveItemDialog, content: {
+                    ActionSheet(title: Text(moveItemActionSheetTitle),
+                                buttons: moveItemActionSheetButtons)
+                })
+            }
+        } else {
+            TitleAndSubtitleRow(title: title, subtitle: subtitle)
+        }
+    }
+}
+
+private extension ItemToFulfillRow {
+    enum Localization {
+        static let moveButton = NSLocalizedString("Move", comment: "Button on each order item of the Package Details screen in Shipping Labels flow.")
+    }
+
+    enum Constants {
+        static let horizontalPadding: CGFloat = 16
     }
 }
 

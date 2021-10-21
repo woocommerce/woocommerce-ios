839e6899cc0784e6442a91f51d156116c6a1fb5f
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2021-10-21 17:02:45 +0300

    Clarify error about missing endpoint for Add-Ons

--- WooCommerce/Classes/Yosemite/DefaultStoresManager.swift
+++ WooCommerce/Classes/Yosemite/DefaultStoresManager.swift
@@ -2,6 +2,7 @@ import Combine
 import Foundation
 import Yosemite
 import Observables
+import enum Networking.DotcomError
 
 // MARK: - DefaultStoresManager
 //
@@ -358,7 +359,11 @@ private extension DefaultStoresManager {
     func synchronizeAddOnsGroups(siteID: Int64) {
         let action = AddOnGroupAction.synchronizeAddOnGroups(siteID: siteID) { result in
             if let error = result.failure {
-                DDLogError("⛔️ Failed to sync add-on groups for siteID: \(siteID). Error: \(error)")
+                if error as? DotcomError == .noRestRoute {
+                    DDLogError("⚠️ Endpoint for add-on groups is unreachable for siteID: \(siteID). WC Product Add-Ons plugin may be missing.")
+                } else {
+                    DDLogError("⛔️ Failed to sync add-on groups for siteID: \(siteID). Error: \(error)")
+                }
             }
         }
         dispatch(action)

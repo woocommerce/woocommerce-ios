0adc7fb689079c2312d1ce2a4c8b24e1e957469c
Paolo Musolino <info@codeido.com>
2021-10-22 13:59:17 +0200

    fix: OrderDateRangeFilterEnum no more conforms to `Decodable`

--- WooCommerce/Classes/ViewRelated/Orders/Order Filters/OrderDateRangeFilterEnum.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Filters/OrderDateRangeFilterEnum.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// Represents all of the possible Order Date Ranges in enum form
 ///
-enum OrderDateRangeFilterEnum: Decodable, Hashable {
+enum OrderDateRangeFilterEnum: Hashable {
     case today
     case last2Days
     case thisWeek

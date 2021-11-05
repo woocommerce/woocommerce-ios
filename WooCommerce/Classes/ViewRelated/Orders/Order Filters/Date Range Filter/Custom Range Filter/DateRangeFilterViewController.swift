f46ef60cd1bdc38d083a48447ad392d225cf2108
Paolo Musolino <info@codeido.com>
2021-11-05 15:50:40 +0100

    feat: introduced DateRangeFilterViewController

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Filters/Date Range Filter/Custom Range Filter/DateRangeFilterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DateRangeFilterViewController.swift
+//  WooCommerce
+//
+//  Created by Paolo Musolino on 04/11/21.
+//  Copyright © 2021 Automattic. All rights reserved.
+//
+
+import UIKit
+
+class DateRangeFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

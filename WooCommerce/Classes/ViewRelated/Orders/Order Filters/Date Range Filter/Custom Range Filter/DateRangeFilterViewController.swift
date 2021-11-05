06f4b769c21c5a61b495c04e8181ed3d89607c39
Paolo Musolino <info@codeido.com>
2021-11-05 15:51:50 +0100

    feat: improvements on DateRangeFilterViewController

--- WooCommerce/Classes/ViewRelated/Orders/Order Filters/Date Range Filter/Custom Range Filter/DateRangeFilterViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Filters/Date Range Filter/Custom Range Filter/DateRangeFilterViewController.swift
@@ -1,30 +1,231 @@
-//
-//  DateRangeFilterViewController.swift
-//  WooCommerce
-//
-//  Created by Paolo Musolino on 04/11/21.
-//  Copyright © 2021 Automattic. All rights reserved.
-//
-
 import UIKit
+import Yosemite
 
-class DateRangeFilterViewController: UIViewController {
+// MARK: - DateRangeFilterViewController
+//
+final class DateRangeFilterViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+
+
+    // Completion callback
+    //
+    typealias Completion = (Date?, Date?) -> Void
+    private let onCompletion: Completion
+
+    private var startDate: Date?
+    private var endDate: Date?
+
+    // Indicate when the start or the end date picker is expanded
+    //
+    private var startDateExpanded: Bool = false
+    private var endDateExpanded: Bool = false
+
+    private var rows: [Row] = []
+
+    /// Init
+    ///
+    init(startDate: Date?,
+         endDate: Date?,
+         completion: @escaping Completion) {
+        self.startDate = startDate
+        self.endDate = endDate
+        onCompletion = completion
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureNavigationBar()
+        configureMainView()
+        configureRows()
+        configureTableView()
     }
 
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: - View Configuration
+//
+private extension DateRangeFilterViewController {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureNavigationBar() {
+        title = Localization.navigationBarTitle
     }
-    */
 
+    func configureMainView() {
+        view.backgroundColor = .listBackground
+    }
+
+    func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        /// Registers all of the available TableViewCells
+        ///
+        for row in Row.allCases {
+            tableView.registerNib(for: row.type)
+        }
+
+        tableView.backgroundColor = .listBackground
+        tableView.removeLastCellSeparator()
+    }
+
+    func configureRows() {
+        var tempRows: [Row] = [.startDateTitle]
+        if startDateExpanded {
+            tempRows.append(.startDatePicker)
+        }
+        tempRows.append(.endDateTitle)
+        if endDateExpanded {
+            tempRows.append(.endDatePicker)
+        }
+        rows = tempRows
+    }
+}
+
+// MARK: - UITableViewDataSource Conformance
+//
+extension DateRangeFilterViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rows.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = rows[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: row.reuseIdentifier, for: indexPath)
+        configure(cell, for: row, at: indexPath)
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate Conformance
+//
+extension DateRangeFilterViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        switch rows[indexPath.row] {
+        case .startDateTitle:
+            startDateExpanded = !startDateExpanded
+            configureRows()
+            tableView.reloadData()
+        case .endDateTitle:
+            endDateExpanded = !endDateExpanded
+            configureRows()
+            tableView.reloadData()
+        default:
+            return
+        }
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+
+// MARK: - Cell configuration
+//
+private extension DateRangeFilterViewController {
+    /// Cells currently configured in the order they appear on screen
+    ///
+    func configure(_ cell: UITableViewCell, for row: Row, at indexPath: IndexPath) {
+        switch cell {
+        case let cell as TitleAndValueTableViewCell where row == .startDateTitle:
+            configureStartDateTitle(cell: cell)
+            break
+        case let cell as DatePickerTableViewCell where row == .startDatePicker:
+            configureStartDatePicker(cell: cell)
+            break
+        case let cell as TitleAndValueTableViewCell where row == .endDateTitle:
+            configureEndDateTitle(cell: cell)
+            break
+        case let cell as DatePickerTableViewCell where row == .endDatePicker:
+            configureEndDatePicker(cell: cell)
+            break
+        default:
+            fatalError()
+            break
+        }
+    }
+
+    func configureStartDateTitle(cell: TitleAndValueTableViewCell) {
+        cell.updateUI(title: Localization.startDateTitle, value: startDate?.toString(dateStyle: .medium, timeStyle: .none))
+        cell.selectionStyle = .none
+    }
+
+    func configureStartDatePicker(cell: DatePickerTableViewCell) {
+        if let startDate = startDate {
+            cell.getPicker().setDate(startDate, animated: false)
+        }
+        cell.getPicker().maximumDate = endDate
+        cell.getPicker().preferredDatePickerStyle = .inline
+        cell.onDateSelected = { [weak self] date in
+            guard let self = self else {
+                return
+            }
+            self.startDate = date
+            self.tableView.reloadData()
+            self.onCompletion(self.startDate, self.endDate)
+        }
+    }
+
+    func configureEndDateTitle(cell: TitleAndValueTableViewCell) {
+        cell.updateUI(title: Localization.endDateTitle, value: endDate?.toString(dateStyle: .medium, timeStyle: .none))
+        cell.selectionStyle = .none
+    }
+
+    func configureEndDatePicker(cell: DatePickerTableViewCell) {
+        if let endDate = endDate {
+            cell.getPicker().setDate(endDate, animated: false)
+        }
+        cell.getPicker().minimumDate = startDate
+        cell.getPicker().preferredDatePickerStyle = .inline
+        cell.onDateSelected = { [weak self] date in
+            guard let self = self else {
+                return
+            }
+            self.endDate = date
+            self.tableView.reloadData()
+            self.onCompletion(self.startDate, self.endDate)
+        }
+    }
+}
+
+// MARK: - Private Types
+//
+private extension DateRangeFilterViewController {
+
+    enum Row: CaseIterable {
+        case startDateTitle
+        case startDatePicker
+        case endDateTitle
+        case endDatePicker
+
+        fileprivate var type: UITableViewCell.Type {
+            switch self {
+            case .startDateTitle, .endDateTitle:
+                return TitleAndValueTableViewCell.self
+            case .startDatePicker, .endDatePicker:
+                return DatePickerTableViewCell.self
+            }
+        }
+
+        fileprivate var reuseIdentifier: String {
+            return type.reuseIdentifier
+        }
+    }
+}
+
+private extension DateRangeFilterViewController {
+    enum Localization {
+        static let navigationBarTitle = NSLocalizedString("Custom Range",
+                                                          comment: "Navigation title of the orders filter selector screen for custom date range")
+        static let startDateTitle = NSLocalizedString("Start Date", comment: "Label for one of the filters in order custom date range")
+        static let endDateTitle = NSLocalizedString("End Date", comment: "Label for one of the filters in order custom date range")
+    }
 }

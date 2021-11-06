438f2195cb9ab57b95106f42ab362f5880c1c6ca
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-11-05 19:14:15 -0500

    Basic UI components for sumary view

--- WooCommerce/Classes/ViewRelated/Orders/Simple Payments/QuickOrderSummary.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Simple Payments/QuickOrderSummary.swift
@@ -4,6 +4,84 @@ import SwiftUI
 ///
 struct QuickOrderSummary: View {
     var body: some View {
-        Text("Holi")
+        VStack {
+            ScrollView {
+                VStack(spacing: 10) {
+
+                    HStack {
+                        Rectangle()
+                            .fill()
+                            .foregroundColor(.gray)
+                            .frame(width: 48, height: 48)
+
+                        Text("Custom Amount")
+
+                        Spacer()
+
+                        Text("$40.00")
+                    }
+                    .background(Color.white)
+
+
+                    TitleAndTextFieldRow(title: "Email", placeholder: "Enter Email", text: .constant(""))
+                        .background(Color.white)
+
+                    VStack(alignment: .leading) {
+                        Text("Payment")
+
+                        TitleAndValueRow(title: "Subtotal", value: .content("$40.0"), selectable: false) {}
+
+                        TitleAndToggleRow(title: "Charge Taxes", isOn: .constant(false))
+
+                        TitleAndValueRow(title: "Subtotal", value: .content("$40.0"), selectable: false) {}
+                    }
+                    .background(Color.white)
+
+                    VStack(alignment: .leading) {
+                        Text("Order Note")
+
+                        Button(action: {
+                            print("Tapped add note")
+                        }, label: {
+                            HStack {
+                                Rectangle()
+                                    .fill()
+                                    .frame(width: 24, height: 24)
+
+                                Text("Add Note")
+
+                                Spacer()
+                            }
+                            .foregroundColor(Color(.accent))
+                        })
+                        .frame(maxWidth: .infinity)
+
+
+                    }
+                    .background(Color.white)
+
+                }
+                .background(Color(.listBackground))
+            }
+
+            VStack {
+                Divider()
+
+                Button("Take Payment ($40.0)", action: {
+
+                })
+                .buttonStyle(PrimaryButtonStyle())
+            }
+        }
+    }
+}
+
+// MARK: Previews
+
+struct QuickOrderSummary_Preview: PreviewProvider {
+    static var previews: some View {
+        QuickOrderSummary()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+            .previewDisplayName("iPhone 12")
     }
 }

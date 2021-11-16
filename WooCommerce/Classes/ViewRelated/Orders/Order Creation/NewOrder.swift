90de306c8b3ae218f42309d196c675d44bac8795
rachelmcr <rachel@automattic.com>
2021-11-16 10:16:54 +0000

    Add empty New Order view

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/NewOrder.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct NewOrder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewOrder_Previews: PreviewProvider {
+    static var previews: some View {
+        NewOrder()
+    }
+}

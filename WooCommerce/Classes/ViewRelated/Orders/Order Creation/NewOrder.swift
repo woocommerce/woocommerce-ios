291fd7617b6a3d462a912bfb22109af070c73657
rachelmcr <rachel@automattic.com>
2021-11-16 11:06:34 +0000

    Add navigation bar to New Order screen

--- WooCommerce/Classes/ViewRelated/Orders/Order Creation/NewOrder.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/NewOrder.swift
@@ -1,5 +1,7 @@
 import SwiftUI
 
+/// View to create a new manual order
+///
 struct NewOrder: View {
     var body: some View {
         ScrollView {
@@ -7,11 +9,32 @@ struct NewOrder: View {
         }
         .background(Color(.listBackground))
             .ignoresSafeArea(.container, edges: [.horizontal, .bottom])
+        .navigationTitle(Localization.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .confirmationAction) {
+                Button(action: {
+                    // TODO-5405 - Add create order action
+                }, label: {
+                    Text(Localization.createButton)
+                })
+            }
+        }
+    }
+}
+
+// MARK: Constants
+private extension NewOrder {
+    enum Localization {
+        static let title = NSLocalizedString("New Order", comment: "Title for the order creation screen")
+        static let createButton = NSLocalizedString("Create", comment: "Button to create an order on the New Order screen")
     }
 }
 
 struct NewOrder_Previews: PreviewProvider {
     static var previews: some View {
-        NewOrder()
+        NavigationView {
+            NewOrder()
+        }
     }
 }

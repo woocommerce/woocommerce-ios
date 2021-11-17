b89e54be3394947aa816598211858d172773b478
Paolo Musolino <info@codeido.com>
2021-11-17 17:15:59 +0100

    feat: OrderStatusFilterViewController

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Filters/Order Status Filter/OrderStatusFilterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OrderStatusFilterViewController.swift
+//  WooCommerce
+//
+//  Created by Paolo Musolino on 17/11/21.
+//  Copyright © 2021 Automattic. All rights reserved.
+//
+
+import UIKit
+
+class OrderStatusFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

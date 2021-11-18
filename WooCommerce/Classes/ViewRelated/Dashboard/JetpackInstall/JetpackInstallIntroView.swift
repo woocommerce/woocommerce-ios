7aaad7e39f0c0928a38f47e1d465b19f8ce8938b
Huong Do <hi.huongdo@gmail.com>
2021-11-18 15:26:47 +0700

    Add attributed text for description of Install Jetpack intro

--- WooCommerce/Classes/ViewRelated/Dashboard/JetpackInstall/JetpackInstallIntroView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/JetpackInstall/JetpackInstallIntroView.swift
@@ -4,9 +4,27 @@ struct JetpackInstallIntroView: View {
     // Closure invoked when Close button is tapped
     var dismissAction: () -> Void = {}
 
-    /// Closure invoked when the install button is tapped
-    ///
-    var installAction: () -> Void = {}
+    let siteURL: String
+
+    private var descriptionAttributedString: NSAttributedString {
+        let font: UIFont = .body
+        let boldFont: UIFont = font.bold
+        let siteName = siteURL.trimHTTPScheme()
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+
+        let attributedString = NSMutableAttributedString(
+            string: String(format: Localization.installDescription, siteName),
+            attributes: [.font: font,
+                         .foregroundColor: UIColor.primary,
+                         .paragraphStyle: paragraphStyle
+                        ]
+        )
+        let boldSiteAddress = NSAttributedString(string: siteName, attributes: [.font: boldFont])
+        attributedString.replaceFirstOccurrence(of: siteName, with: boldSiteAddress)
+        return attributedString
+    }
 
     var body: some View {
         VStack {
@@ -19,22 +37,32 @@ struct JetpackInstallIntroView: View {
 
             Spacer()
 
-            VStack {
+            VStack(spacing: Constants.contentSpacing) {
                 Image(uiImage: .jetpackLogoImage)
                     .resizable()
                     .renderingMode(.template)
                     .foregroundColor(Color(.jetpackGreen))
                     .frame(width: Constants.jetpackLogoSize, height: Constants.jetpackLogoSize)
+
+                Text(Localization.installTitle)
+                    .font(.largeTitle)
+                    .foregroundColor(.primary)
+
+                AttributedText(descriptionAttributedString)
             }
+            .padding(.horizontal, Constants.contentHorizontalMargin)
+            .scrollVerticallyIfNeeded()
 
             Spacer()
 
             // Primary Button to install Jetpack
-            Button(Localization.installAction, action: installAction)
+            Button(Localization.installAction, action: {
+                // TODO: Show main install screen
+            })
                 .buttonStyle(PrimaryButtonStyle())
                 .fixedSize(horizontal: false, vertical: true)
-                .padding(.horizontal, Constants.horizontalMargin)
-                .padding(.bottom, Constants.installJetpackBottomMargin)
+                .padding(.horizontal, Constants.actionButtonHorizontalMargin)
+                .padding(.bottom, Constants.actionButtonBottomMargin)
         }
     }
 }
@@ -42,23 +70,29 @@ struct JetpackInstallIntroView: View {
 private extension JetpackInstallIntroView {
     enum Constants {
         static let jetpackLogoSize: CGFloat = 120
-        static let horizontalMargin: CGFloat = 16
-        static let installJetpackBottomMargin: CGFloat = 28
+        static let actionButtonHorizontalMargin: CGFloat = 16
+        static let actionButtonBottomMargin: CGFloat = 28
+        static let contentHorizontalMargin: CGFloat = 40
+        static let contentSpacing: CGFloat = 8
+        static let jetpackLogoBottomMargin: CGFloat = 24
     }
 
     enum Localization {
         static let closeButton = NSLocalizedString("Close", comment: "Title of the Close action on the Jetpack Install view")
-        static let installAction = NSLocalizedString("Install Jetpack", comment: "Title of install action in the Jetpack benefits view.")
+        static let installAction = NSLocalizedString("Get Started", comment: "Title of install action in the Jetpack benefits view.")
+        static let installTitle = NSLocalizedString("Install Jetpack", comment: "Title of the Install Jetpack intro view")
+        static let installDescription = NSLocalizedString("Install the free Jetpack plugin to %1$@ and experience the best mobile experience.",
+                                                          comment: "Description of the Jetpack Install flow for the specified site")
     }
 }
 
 struct JetpackInstallIntroView_Previews: PreviewProvider {
     static var previews: some View {
-        JetpackInstallIntroView()
+        JetpackInstallIntroView(siteURL: "automattic.com")
             .preferredColorScheme(.light)
             .previewLayout(.fixed(width: 414, height: 780))
 
-        JetpackInstallIntroView()
+        JetpackInstallIntroView(siteURL: "automattic.com")
             .preferredColorScheme(.dark)
             .previewLayout(.fixed(width: 800, height: 300))
     }

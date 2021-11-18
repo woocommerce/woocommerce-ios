662dcf79772fa5467f9f45df94a23e268b14781b
Jaclyn Chen <watertranquil@gmail.com>
2021-11-18 14:26:57 +0800

    Pass JCP feature flag to `AccountAction.synchronizeSites` and `AccountAction.loadAndSynchronizeSiteIfNeeded` for JCP workaround.

--- Yosemite/Yosemite/Actions/AccountAction.swift
+++ Yosemite/Yosemite/Actions/AccountAction.swift
@@ -7,10 +7,10 @@ import Networking
 //
 public enum AccountAction: Action {
     case loadAccount(userID: Int64, onCompletion: (Account?) -> Void)
-    case loadAndSynchronizeSiteIfNeeded(siteID: Int64, onCompletion: (Result<Site, Error>) -> Void)
+    case loadAndSynchronizeSiteIfNeeded(siteID: Int64, isJetpackConnectionPackageSupported: Bool, onCompletion: (Result<Site, Error>) -> Void)
     case synchronizeAccount(onCompletion: (Result<Account, Error>) -> Void)
     case synchronizeAccountSettings(userID: Int64, onCompletion: (Result<AccountSettings, Error>) -> Void)
-    case synchronizeSites(selectedSiteID: Int64?, onCompletion: (Result<Void, Error>) -> Void)
+    case synchronizeSites(selectedSiteID: Int64?, isJetpackConnectionPackageSupported: Bool, onCompletion: (Result<Void, Error>) -> Void)
     case synchronizeSitePlan(siteID: Int64, onCompletion: (Result<Void, Error>) -> Void)
     case updateAccountSettings(userID: Int64, tracksOptOut: Bool, onCompletion: (Result<Void, Error>) -> Void)
 }

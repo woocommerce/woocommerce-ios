8cfc88bbd1b3c3072fc8e5a451924b612c748d01
Huong Do <hi.huongdo@gmail.com>
2021-11-19 14:47:23 +0700

    Add done button to JetpackInstallStepsView

--- WooCommerce/Classes/ViewRelated/Dashboard/JetpackInstall/JetpackInstallStepsView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/JetpackInstall/JetpackInstallStepsView.swift
@@ -30,45 +30,55 @@ struct JetpackInstallStepsView: View {
     }
 
     var body: some View {
-        VStack(alignment: .leading, spacing: Constants.contentSpacing) {
-            // Header
-            HStack(spacing: 8) {
-                Image(uiImage: .jetpackGreenLogoImage)
-                    .resizable()
-                    .frame(width: Constants.logoSize * scale, height: Constants.logoSize * scale)
-                Image(uiImage: .connectionImage)
-                    .resizable()
-                    .frame(width: Constants.connectionIconSize * scale, height: Constants.connectionIconSize * scale)
-
-                if let image = UIImage.wooLogoImage(tintColor: .white) {
-                    Circle()
-                        .foregroundColor(Color(.withColorStudio(.wooCommercePurple, shade: .shade60)))
+        VStack {
+            // Main content
+            VStack(alignment: .leading, spacing: Constants.contentSpacing) {
+                // Header
+                HStack(spacing: 8) {
+                    Image(uiImage: .jetpackGreenLogoImage)
+                        .resizable()
                         .frame(width: Constants.logoSize * scale, height: Constants.logoSize * scale)
-                        .overlay(
-                            Image(uiImage: image)
-                                .resizable()
-                                .frame(width: Constants.wooIconSize.width * scale, height: Constants.wooIconSize.height * scale)
-                        )
+                    Image(uiImage: .connectionImage)
+                        .resizable()
+                        .frame(width: Constants.connectionIconSize * scale, height: Constants.connectionIconSize * scale)
+
+                    if let image = UIImage.wooLogoImage(tintColor: .white) {
+                        Circle()
+                            .foregroundColor(Color(.withColorStudio(.wooCommercePurple, shade: .shade60)))
+                            .frame(width: Constants.logoSize * scale, height: Constants.logoSize * scale)
+                            .overlay(
+                                Image(uiImage: image)
+                                    .resizable()
+                                    .frame(width: Constants.wooIconSize.width * scale, height: Constants.wooIconSize.height * scale)
+                            )
+                    }
+
+                    Spacer()
                 }
+                .padding(.top, Constants.contentTopMargin)
 
-                Spacer()
+                // Title and description
+                VStack(alignment: .leading, spacing: Constants.textSpacing) {
+                    Text(Localization.installTitle)
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(Color(.text))
+
+                    AttributedText(descriptionAttributedString)
+                }
             }
-            .padding(.top, Constants.contentTopMargin)
-
-            // Title and description
-            VStack(alignment: .leading, spacing: Constants.textSpacing) {
-                Text(Localization.installTitle)
-                    .font(.largeTitle)
-                    .bold()
-                    .foregroundColor(Color(.text))
-
-                AttributedText(descriptionAttributedString)
-            }
-
+            .padding(.horizontal, Constants.contentHorizontalMargin)
+            .scrollVerticallyIfNeeded()
 
             Spacer()
+
+            // Done Button to dismiss Install Jetpack
+            Button(Localization.doneButton, action: dismissAction)
+                .buttonStyle(PrimaryButtonStyle())
+                .fixedSize(horizontal: false, vertical: true)
+                .padding(.horizontal, Constants.actionButtonMargin)
+                .padding(.bottom, Constants.actionButtonMargin)
         }
-        .padding(.horizontal, Constants.contentHorizontalMargin)
     }
 }
 
@@ -81,12 +91,14 @@ private extension JetpackInstallStepsView {
         static let wooIconSize: CGSize = .init(width: 30, height: 18)
         static let connectionIconSize: CGFloat = 10
         static let textSpacing: CGFloat = 12
+        static let actionButtonMargin: CGFloat = 16
     }
 
     enum Localization {
         static let installTitle = NSLocalizedString("Install Jetpack", comment: "Title of the Install Jetpack view")
         static let installDescription = NSLocalizedString("Please wait while we connect your site %1$@ with Jetpack.",
                                                           comment: "Message on the Jetpack Install Progress screen. The %1$@ is the site address.")
+        static let doneButton = NSLocalizedString("Done", comment: "Done button on the Jetpack Install Progress screen.")
     }
 }
 

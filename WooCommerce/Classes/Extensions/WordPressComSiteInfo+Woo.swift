2a3a0bb78cb008f8f62b4c04a3f2165207baf347
Jaclyn Chen <watertranquil@gmail.com>
2021-11-19 09:38:27 +0800

    Only check `isJetpackConnected` for valid Jetpack connection.

--- WooCommerce/Classes/Extensions/WordPressComSiteInfo+Woo.swift
+++ WooCommerce/Classes/Extensions/WordPressComSiteInfo+Woo.swift
@@ -5,8 +5,6 @@ extension WordPressComSiteInfo {
     /// Encapsulates the rules that declare a WordPress site as having a valid
     /// Jetpack installation
     var hasValidJetpack: Bool {
-        return hasJetpack &&
-            isJetpackConnected &&
-            isJetpackActive
+        isJetpackConnected
     }
 }

0a7ca20b9cf8289562cc2483cdf9a3c2d5a32ecb
Jaclyn Chen <watertranquil@gmail.com>
2021-11-22 14:40:32 +0800

    Remove new code to reduce the PR to reviewable size.

--- Networking/Networking/Remote/MediaRemote.swift
+++ Networking/Networking/Remote/MediaRemote.swift
@@ -35,36 +35,6 @@ public class MediaRemote: Remote {
         enqueue(request, mapper: mapper, completion: completion)
     }
 
-    /// Loads an array of media from the site's WP Media Library via WordPress site API.
-    /// API reference: https://developer.wordpress.org/rest-api/reference/media/#list-media
-    ///
-    /// - Parameters:
-    ///   - siteID: Site for which we'll load the media from.
-    ///   - pageNumber: The index of the page of media data to load from, starting from 1.
-    ///   - pageSize: The number of media items to return.
-    ///   - completion: Closure to be executed upon completion.
-    public func loadMediaLibraryFromWordPressSite(siteID: Int64,
-                                                  pageNumber: Int = Default.pageNumber,
-                                                  pageSize: Int = 25,
-                                                  context: String = Default.context,
-                                                  completion: @escaping (Result<[WordPressMedia], Error>) -> Void) {
-        let parameters: [String: Any] = [
-            ParameterKey.pageSize: pageSize,
-            ParameterKey.pageNumber: pageNumber,
-            ParameterKey.fieldsWordPressSite: Default.wordPressMediaFields,
-            ParameterKey.mimeType: "image"
-        ]
-
-        let path = "sites/\(siteID)/media"
-        let request = DotcomRequest(wordpressApiVersion: .wpMark2,
-                                    method: .get,
-                                    path: path,
-                                    parameters: parameters)
-        let mapper = WordPressMediaListMapper()
-
-        enqueue(request, mapper: mapper, completion: completion)
-    }
-
     /// Uploads an array of media in the local file system.
     /// API reference: https://developer.wordpress.com/docs/api/1.1/post/sites/%24site/media/new/
     ///
@@ -104,40 +74,6 @@ public class MediaRemote: Remote {
             mediaItems.forEach { mediaItem in
                 multipartFormData.append(mediaItem.localURL,
                                          withName: "media[]",
-                                         fileName: mediaItem.filename,
-                                         mimeType: mediaItem.mimeType)
-            }
-        }, completion: completion)
-    }
-
-    /// Uploads an array of media in the local file system to the WordPress site.via WordPress site API
-    /// API reference: https://developer.wordpress.org/rest-api/reference/media/#create-a-media-item
-    ///
-    /// - Parameters:
-    ///   - siteID: Site for which we'll upload the media to.
-    ///   - productID: Product for which the media items are first added to.
-    ///   - mediaItems: An array of uploadable media items.
-    ///   - completion: Closure to be executed upon completion.
-    public func uploadMediaToWordPressSite(siteID: Int64,
-                                           productID: Int64,
-                                           mediaItems: [UploadableMedia],
-                                           completion: @escaping (Result<WordPressMedia, Error>) -> Void) {
-        let formParameters: [String: String] = [
-            "post": "\(productID)",
-            ParameterKey.fieldsWordPressSite: Default.wordPressMediaFields,
-        ]
-        let path = "sites/\(siteID)/media"
-        let request = DotcomRequest(wordpressApiVersion: .wpMark2, method: .post, path: path, parameters: nil)
-        let mapper = WordPressMediaMapper()
-
-        enqueueMultipartFormDataUpload(request, mapper: mapper, multipartFormData: { multipartFormData in
-            formParameters.forEach { (key, value) in
-                multipartFormData.append(Data(value.utf8), withName: key)
-            }
-
-            mediaItems.forEach { mediaItem in
-                multipartFormData.append(mediaItem.localURL,
-                                         withName: ParameterValue.mediaUploadName,
                                          fileName: mediaItem.filename,
                                          mimeType: mediaItem.mimeType)
             }

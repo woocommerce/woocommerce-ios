abeaaa064d2dbd81ac796e871c45b2c0aecb2a57
Allen Snook <allendav@automattic.com>
2021-11-24 10:23:10 -0800

    Add localized descriptions for each decline reason

--- Hardware/Hardware/CardReader/DeclineReason.swift
+++ Hardware/Hardware/CardReader/DeclineReason.swift
@@ -64,4 +64,52 @@ enum DeclineReason {
     /// payment method.
     ///
     case testCard
+
+    /// The card was declined for an unknown reason. Tell the user this and
+    /// ask them to try another payment method.
+    ///
+    case unknown
+}
+
+extension DeclineReason {
+    public var localizedDescription: String? {
+        switch self {
+        case .temporary:
+            return NSLocalizedString("Trying again may succeed, or try another means of payment",
+                                     comment: "Message when a card is declined due to a potentially temporary problem.")
+        case .fraud:
+            return NSLocalizedString("Try another means of payment",
+                                     comment: "Message when a lost or stolen card is presented for payment. Do NOT disclose fraud.")
+        case .cardNotSupported:
+            return NSLocalizedString("The card does not support this type of purchase. Try another means of payment",
+                                     comment: "Message when the card presented does not allow this type of purchase.")
+        case .currencyNotSupported:
+            return NSLocalizedString("The card does not support this currency. Try another means of payment",
+                                     comment: "Message when the card presented does not support the order currency.")
+        case .duplicateTransaction:
+            return NSLocalizedString("An identical transaction was submitted recently. If you wish to continue, try another means of payment",
+                                     comment: "Message when it looks like a duplicate transaction is being attempted.")
+        case .expiredCard:
+            return NSLocalizedString("The card has expired. Try another means of payment",
+                                     comment: "Message when the presented card is past its expiration date.")
+        case .incorrectPostalCode:
+            return NSLocalizedString("The transaction postal code does not match that of the card presented. Correct the postal code or try another means of payment",
+                                     comment: "Message when the presented card postal code doesn't match the order postal code.")
+        case .insufficientFunds:
+            return NSLocalizedString("Payment declined due to insufficient funds. Try another means of payment",
+                                     comment: "Message when the presented card remaining credit or balance is insufficient for the purchase.")
+        case .invalidAmount:
+            return NSLocalizedString("The payment amount is not allowed for the card presented. Try another means of payment.",
+                                     comment: "Message when the presented card does not allow the purchase amount.")
+        case .pinRequired:
+            return NSLocalizedString("This card requires a PIN code and thus cannot be processed. Try another means of payment",
+                                     comment: "Message when a card requires a PIN code and we have no means of entering such a code.")
+        case .testCard:
+            return NSLocalizedString("System test cards are not permitted for payment. Try another means of payment",
+                                     comment: "Message when attempting to pay for a live transaction with a test card.")
+        case .unknown:
+            return NSLocalizedString("Payment was declined for an unknown reason. Try another means of payment",
+                                     comment: "Message when we don't know exactly why the payment was declined.")
+        }
+    }
 }

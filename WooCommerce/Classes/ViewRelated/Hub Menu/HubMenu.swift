688cf1c3d828d9406751dd672f924294ea653894
Paolo Musolino <info@codeido.com>
2021-11-25 16:14:43 +0100

    feat: wip text in HubMenu

--- WooCommerce/Classes/ViewRelated/Hub Menu/HubMenu.swift
+++ WooCommerce/Classes/ViewRelated/Hub Menu/HubMenu.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct HubMenu: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Work in progress")
     }
 }
 

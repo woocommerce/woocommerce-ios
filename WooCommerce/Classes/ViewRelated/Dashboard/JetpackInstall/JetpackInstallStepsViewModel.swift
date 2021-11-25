518e79a29ed460dc3539ca419d192d7f45b45470
Huong Do <hi.huongdo@gmail.com>
2021-11-25 16:39:02 +0700

    Implement first step: installing the jetpack plugin

--- WooCommerce/Classes/ViewRelated/Dashboard/JetpackInstall/JetpackInstallStepsViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/JetpackInstall/JetpackInstallStepsViewModel.swift
@@ -14,10 +14,42 @@ final class JetpackInstallStepsViewModel: ObservableObject {
     private let stores: StoresManager
 
     /// Current step of the installation
-    @Published private(set) var currentStep: JetpackInstallStep = .activation
+    ///
+    @Published private(set) var currentStep: JetpackInstallStep = .installation
 
     init(siteID: Int64, stores: StoresManager = ServiceLocator.stores) {
         self.siteID = siteID
         self.stores = stores
+    }
+
+    /// Starts the steps by installing the Jetpack plugin.
+    ///
+    func startInstallation() {
+        let installationAction = SitePluginAction.installSitePlugin(siteID: siteID, slug: Constants.jetpackSlug) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success:
+                self.activateJetpack()
+            case .failure:
+                // TODO-5365: handle failure with an error message
+                break
+            }
+        }
+        stores.dispatch(installationAction)
+    }
+
+    private func activateJetpack() {
+        // TODO:
+    }
+
+    private func checkSiteConnection() {
+        // TODO:
+    }
+}
+
+private extension JetpackInstallStepsViewModel {
+    enum Constants {
+        static let jetpackSlug: String = "jetpack"
+        static let jetpackPluginName: String = "jetpack/jetpack"
     }
 }

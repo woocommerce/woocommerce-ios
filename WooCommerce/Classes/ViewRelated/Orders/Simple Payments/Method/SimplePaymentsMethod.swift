99a70617dedcfc104b96336743e74eba786ea174
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-11-26 15:23:07 -0500

    Added empty SimpleyPaymentsMethod view

---
+++ WooCommerce/Classes/ViewRelated/Orders/Simple Payments/Method/SimplePaymentsMethod.swift
@@ -0,0 +1,11 @@
+import Foundation
+import SwiftUI
+
+/// View to choose what payment method will be used with the simple payments order.
+///
+struct SimplePaymentsMethod: View {
+
+    var body: some View {
+        Text("Payment method")
+    }
+}

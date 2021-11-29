b724e4d85606d61ae0af334af2691fa6ecd27c23
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-11-29 08:03:21 -0500

    Adds empty Methods ViewModel

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Orders/Simple Payments/SimplePaymentsMethodsViewModelTests.swift
@@ -0,0 +1,11 @@
+import Foundation
+import XCTest
+import Combine
+
+@testable import WooCommerce
+@testable import Yosemite
+
+final class SimplePaymentsMethodsViewModelTests: XCTestCase {
+
+    var subscriptions = Set<AnyCancellable>()
+}

4fa39d64456a1db502ea4285e0cc97ed8977896b
Jaclyn Chen <watertranquil@gmail.com>
2021-11-30 11:19:24 +0800

    Add a comment to `jetpackConnectionActivePlugins` property.

--- Networking/Networking/Model/Site.swift
+++ Networking/Networking/Model/Site.swift
@@ -41,6 +41,9 @@ public struct Site: Decodable, Equatable, GeneratedFakeable, GeneratedCopiable {
     ///
     public let isWordPressStore: Bool
 
+    /// For Jetpack CP sites (connected to Jetpack with Jetpack Connection Package instead of Jetpack-the-plugin), this property contains a list of active plugins with Jetpack Connection Package
+    /// (e.g. WooCommerce Payments, Jetpack Backup).
+    ///
     public let jetpackConnectionActivePlugins: [String]
 
     /// Time zone identifier of the site (TZ database name).

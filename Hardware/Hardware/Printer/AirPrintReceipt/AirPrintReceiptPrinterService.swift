6efed3fc0b4cb0c53b86ba89ecadbb5fedec7ea1
Allen Snook <allendav@automattic.com>
2021-12-01 15:15:31 -0800

    Pass a fresh print info to each print job

--- Hardware/Hardware/Printer/AirPrintReceipt/AirPrintReceiptPrinterService.swift
+++ Hardware/Hardware/Printer/AirPrintReceipt/AirPrintReceiptPrinterService.swift
@@ -3,24 +3,19 @@ import UIKit
 /// Barebones Implementation of the ReceiptPrinterService that integrates with AirPrint
 /// Will be iterated in https://github.com/woocommerce/woocommerce-ios/issues/3982
 public final class AirPrintReceiptPrinterService: PrinterService {
-    private let printInfo: UIPrintInfo = {
+    public init() { }
+
+    public func printReceipt(content: ReceiptContent, completion: @escaping (PrintingResult) -> Void) {
         let info = UIPrintInfo(dictionary: nil)
         // Will be localized in #3982
         info.jobName = "Order Receipt"
         info.orientation = .portrait
         info.duplex = .longEdge
 
-        return info
-    }()
-
-    public init() { }
-
-    public func printReceipt(content: ReceiptContent, completion: @escaping (PrintingResult) -> Void) {
-        let printController = UIPrintInteractionController.shared
-
-        printController.printInfo = printInfo
-
         let renderer = ReceiptRenderer(content: content)
+
+        let printController = UIPrintInteractionController.shared
+        printController.printInfo = info
         printController.printPageRenderer = renderer
 
         printController.present(animated: true) { (controller, completed, error) in

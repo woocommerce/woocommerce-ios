bad75178614d5335f6b576fa3825f9d7ba5cc99e
rachelmcr <rachel@automattic.com>
2021-12-01 20:25:20 +0000

    Make product configuration more explicit

--- WooCommerce/Classes/ViewRelated/Orders/Order Creation/ProductsSection/AddProductViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/ProductsSection/AddProductViewModel.swift
@@ -7,12 +7,21 @@ final class AddProductViewModel: ObservableObject {
     private let siteID: Int64
     private let storageManager: StorageManagerType
 
+    /// Product types excluded from the product list.
+    /// For now, only non-variable product types are supported.
+    ///
+    private let excludedProductTypes: [ProductType] = [.variable]
+
+    /// Product statuses included in the product list.
+    /// Only published or private products can be added to an order.
+    ///
+    private let includedProductStatuses: [ProductStatus] = [.publish, .privateStatus]
+
     /// All products that can be added to an order.
-    /// Includes all non-variable products with published or private status. (Variable products to be added in a future milestone.)
     ///
     private var products: [Product] {
         return productsResultsController.fetchedObjects.filter { product in
-            product.productType != .variable && ( product.productStatus == .publish || product.productStatus == .privateStatus )
+            !excludedProductTypes.contains(product.productType) && includedProductStatuses.contains(product.productStatus)
         }
     }
 
@@ -28,18 +37,24 @@ final class AddProductViewModel: ObservableObject {
         let predicate = NSPredicate(format: "siteID == %lld", siteID)
         let descriptor = NSSortDescriptor(key: "name", ascending: true)
         let resultsController = ResultsController<StorageProduct>(storageManager: storageManager, matching: predicate, sortedBy: [descriptor])
-
-        do {
-            try resultsController.performFetch()
-        } catch {
-            DDLogError("⛔️ Error fetching products: \(error)")
-        }
-
         return resultsController
     }()
 
     init(siteID: Int64, storageManager: StorageManagerType = ServiceLocator.storageManager) {
         self.siteID = siteID
         self.storageManager = storageManager
+
+        configureProductsResultsController()
+    }
+}
+
+// MARK: - Configuration
+private extension AddProductViewModel {
+    func configureProductsResultsController() {
+        do {
+            try productsResultsController.performFetch()
+        } catch {
+            DDLogError("⛔️ Error fetching products for new order: \(error)")
+        }
     }
 }

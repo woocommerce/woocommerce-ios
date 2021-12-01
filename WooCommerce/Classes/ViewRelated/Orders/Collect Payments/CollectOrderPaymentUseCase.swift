b568e8e686ff719978b5c1b19af5e0f9bf071a27
Ernesto Carrión <ernesto.carrion@automattic.com>
2021-12-01 14:58:28 -0500

    Add empty collect payment use case

---
+++ WooCommerce/Classes/ViewRelated/Orders/Collect Payments/CollectOrderPaymentUseCase.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+/// Use case to collect payments from an order.
+/// Orchestrates reader connection, payment, UI alerts and analytics.
+///
+final class CollectOrderPaymentUseCase {
+
+}

f94c78848e3af8c495b81c6f2118f3a0918e339c
Paolo Musolino <info@codeido.com>
2021-12-01 18:11:59 +0100

    feat: title of every item in the hub menu

--- WooCommerce/Classes/ViewRelated/Hub Menu/HubMenuViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Hub Menu/HubMenuViewModel.swift
@@ -18,5 +18,25 @@ extension HubMenuViewModel {
         case woocommerceAdmin
         case viewStore
         case reviews
+
+        var title: String {
+            switch self {
+            case .woocommerceAdmin:
+                return Localization.woocommerceAdmin
+            case .viewStore:
+                return Localization.viewStore
+            case .reviews:
+                return Localization.reviews
+            }
+        }
+    }
+
+    private enum Localization {
+        static let woocommerceAdmin = NSLocalizedString("Hub Menu",
+                                     comment: "Navigation bar title of hub menu view")
+        static let viewStore = NSLocalizedString("Hub Menu",
+                                     comment: "Navigation bar title of hub menu view")
+        static let reviews = NSLocalizedString("Hub Menu",
+                                     comment: "Navigation bar title of hub menu view")
     }
 }

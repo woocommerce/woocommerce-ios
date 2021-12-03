37910a6d4ecba9ffde4083a5ee46f6bc103c881c
rachelmcr <rachel@automattic.com>
2021-12-03 00:13:10 +0000

    Update results controller to filter products based on status and type

--- WooCommerce/Classes/ViewRelated/Orders/Order Creation/ProductsSection/AddProductViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/ProductsSection/AddProductViewModel.swift
@@ -10,19 +10,17 @@ final class AddProductViewModel: ObservableObject {
     /// Product types excluded from the product list.
     /// For now, only non-variable product types are supported.
     ///
-    private let excludedProductTypes: [ProductType] = [.variable]
+    private let excludedProductTypeKeys: [String] = [ProductType.variable.rawValue]
 
     /// Product statuses included in the product list.
     /// Only published or private products can be added to an order.
     ///
-    private let includedProductStatuses: [ProductStatus] = [.publish, .privateStatus]
+    private let includedProductStatusKeys: [String] = [ProductStatus.publish.rawValue, ProductStatus.privateStatus.rawValue]
 
     /// All products that can be added to an order.
     ///
     private var products: [Product] {
-        return productsResultsController.fetchedObjects.filter { product in
-            !excludedProductTypes.contains(product.productType) && includedProductStatuses.contains(product.productStatus)
-        }
+        return productsResultsController.fetchedObjects
     }
 
     /// View models for each product row
@@ -34,7 +32,8 @@ final class AddProductViewModel: ObservableObject {
     /// Products Results Controller.
     ///
     private lazy var productsResultsController: ResultsController<StorageProduct> = {
-        let predicate = NSPredicate(format: "siteID == %lld", siteID)
+        let predicate = NSPredicate(format: "siteID == %lld AND statusKey IN %@ AND NOT(productTypeKey IN %@)",
+                                    siteID, includedProductStatusKeys, excludedProductTypeKeys)
         let descriptor = NSSortDescriptor(key: "name", ascending: true)
         let resultsController = ResultsController<StorageProduct>(storageManager: storageManager, matching: predicate, sortedBy: [descriptor])
         return resultsController

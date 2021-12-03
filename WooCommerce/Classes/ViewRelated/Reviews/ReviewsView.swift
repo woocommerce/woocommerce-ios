d05da62e44deb73729e33ec4395eddc3ebe42f37
Paolo Musolino <info@codeido.com>
2021-12-03 14:41:11 +0100

    fix: navigation bar fix in ReviewsView

--- WooCommerce/Classes/ViewRelated/Reviews/ReviewsView.swift
+++ WooCommerce/Classes/ViewRelated/Reviews/ReviewsView.swift
@@ -7,11 +7,27 @@ struct ReviewsView: UIViewControllerRepresentable {
 
     typealias UIViewControllerType = ReviewsViewController
 
-    func makeUIViewController(context: Context) -> ReviewsViewController {
-        ReviewsViewController(siteID: siteID)
+    class Coordinator {
+        var parentObserver: NSKeyValueObservation?
+    }
+
+    /// This is a UIKit solution for fixing Navigation Title and Bar Button Items ignored in NavigationView.
+    /// This solution doesn't require making internal changes to the destination `UIViewController`
+    /// and should be called once, when wrapped.
+    /// Solution proposed here: https://stackoverflow.com/q/59295507/7241994
+    ///
+    func makeUIViewController(context: Self.Context) -> ReviewsViewController {
+        let viewController = ReviewsViewController(siteID: siteID)
+        context.coordinator.parentObserver = viewController.observe(\.parent, changeHandler: { vc, _ in
+            vc.parent?.title = vc.title
+            vc.parent?.navigationItem.rightBarButtonItems = vc.navigationItem.rightBarButtonItems
+        })
+        return viewController
     }
 
     func updateUIViewController(_ uiViewController: ReviewsViewController, context: Context) {
         // nothing to do here
     }
+
+    func makeCoordinator() -> Self.Coordinator { Coordinator() }
 }

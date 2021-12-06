d2a314dc819ccac4f2cc6cf36f46041bc1d3956f
rachelmcr <rachel@automattic.com>
2021-12-06 16:49:49 +0000

    Add ABTest to define experiments and start testing

---
+++ Experiments/Experiments/ABTest.swift
@@ -0,0 +1,29 @@
+import AutomatticTracks
+
+/// ABTest defines AB testing experiments and runs the tests based on their variations from the ExPlat service.
+///
+enum ABTest: String, CaseIterable {
+    /// Throwaway case, to prevent a compiler error:
+    /// `An enum with no cases cannot declare a raw type`
+    case null
+
+    /// Returns a variation for the given experiment
+    var variation: Variation {
+        return ExPlat.shared?.experiment(self.rawValue) ?? .control
+    }
+}
+
+extension ABTest {
+    /// Start the AB Testing platform if any experiment exists
+    ///
+    static func start() {
+        guard ABTest.allCases.count > 1 else {
+            return
+        }
+
+        let experimentNames = ABTest.allCases.filter { $0 != .null }.map { $0.rawValue }
+        ExPlat.shared?.register(experiments: experimentNames)
+
+        ExPlat.shared?.refresh()
+    }
+}

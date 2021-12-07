8714e5f734420918a11dea7c21008f73d56763eb
Hassaan El-Garem <hassaansalehelgarem@gmail.com>
2021-12-07 17:15:55 +0200

    Revert "Clean up GravatarEmailTableViewCell"
    This reverts commit 587da268ef424dfb5e36dd96b8fd4d013a50b669.

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Password/PasswordViewController.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Password/PasswordViewController.swift
@@ -11,7 +11,6 @@ class PasswordViewController: LoginViewController {
     @IBOutlet var bottomContentConstraint: NSLayoutConstraint?
 
     private weak var passwordField: UITextField?
-    private weak var emailField: UITextField?
     private var rows = [Row]()
     private var errorMessage: String?
     private var shouldChangeVoiceOverFocus: Bool = false
@@ -205,30 +204,6 @@ extension PasswordViewController: UITextFieldDelegate {
         return true
     }
 
-    func textFieldDidChangeSelection(_ textField: UITextField) {
-        switch textField {
-        case let textField where textField === emailField:
-            handleEmailFieldDidChange(textField)
-            break
-        default:
-            DDLogError("Error: Unidentifed textfield found")
-        }
-    }
-
-    private func handleEmailFieldDidChange(_ textfield: UITextField) {
-        // The email can only be changed via a password manager.
-        // In this case, don't update username for social accounts.
-        // This prevents inadvertent account linking.
-        if loginFields.meta.socialService != nil {
-            textfield.text = loginFields.username
-        } else {
-            loginFields.username = textfield.nonNilTrimmedText()
-            loginFields.emailAddress = textfield.nonNilTrimmedText()
-        }
-
-        configureSubmitButton(animating: false)
-    }
-
 }
 
 // MARK: - UITableViewDataSource
@@ -326,10 +301,20 @@ private extension PasswordViewController {
     ///
     func configureGravatarEmail(_ cell: GravatarEmailTableViewCell) {
         cell.configure(withEmail: loginFields.username)
-        
-        // Save a reference of email textfield to identify it when delegate methods are called
-        emailField = cell.emailLabel
-        cell.emailLabel?.delegate = self
+
+        cell.onChangeSelectionHandler = { [weak self] textfield in
+            // The email can only be changed via a password manager.
+            // In this case, don't update username for social accounts.
+            // This prevents inadvertent account linking.
+            if self?.loginFields.meta.socialService != nil {
+                cell.updateEmailAddress(self?.loginFields.username)
+            } else {
+                self?.loginFields.username = textfield.nonNilTrimmedText()
+                self?.loginFields.emailAddress = textfield.nonNilTrimmedText()
+            }
+
+            self?.configureSubmitButton(animating: false)
+        }
     }
 
     /// Configure the instruction cell.

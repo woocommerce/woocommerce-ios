b206226b8ee9af5a8ada83f44b9562fe10c28dbd
ThomazFB <thomazfcortez@gmail.com>
2021-12-10 04:26:33 -0300

    Define basic calculation to acquire fees total, subtotal and taxes

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundFeesDetailsViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundFeesDetailsViewModel.swift
@@ -14,8 +14,18 @@ extension RefundFeesDetailsViewModel {
     /// Creates a `RefundFeesDetailsViewModel` based on a `[OrderFeeLine]`
     ///
     init(fees: [OrderFeeLine], currency: String, currencySettings: CurrencySettings) {
-        self.feesTaxes = ""
-        self.feesSubtotal = ""
-        self.feesTotal = ""
+        let currencyFormatter = CurrencyFormatter(currencySettings: currencySettings)
+
+        let totalTaxes = fees.compactMap {
+            currencyFormatter.convertToDecimal(from: $0.totalTax) as Decimal?
+        }.reduce(0, +)
+
+        let total = fees.compactMap {
+            currencyFormatter.convertToDecimal(from: $0.total) as Decimal?
+        }.reduce(0, +)
+
+        self.feesTaxes = "\(totalTaxes)"
+        self.feesSubtotal = "\(total - totalTaxes)"
+        self.feesTotal = "\(total)"
     }
 }

755771ed55ef99b14e76ef6f30f6fbd786482547
ThomazFB <thomazfcortez@gmail.com>
2021-12-10 04:14:10 -0300

    Connect View and Class outlet fields for RefundsFeesTableViewCell

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundFeesDetailsTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/Cells/RefundFeesDetailsTableViewCell.swift
@@ -1,5 +1,27 @@
 import UIKit
 
 final class RefundFeesDetailsTableViewCell: UITableViewCell {
-    
+    /// Displays `Subtotal` title
+    ///
+    @IBOutlet private var subtotalTitleLabel: UILabel!
+
+    /// Displays the subtotal value
+    ///
+    @IBOutlet private var subtotalPriceLabel: UILabel!
+
+    /// Displays `Tax` title
+    ///
+    @IBOutlet private var taxTitleLabel: UILabel!
+
+    /// Displays the tax value
+    ///
+    @IBOutlet private var taxPriceLabel: UILabel!
+
+    /// Displays `Total` title
+    ///
+    @IBOutlet private var totalTitleLabel: UILabel!
+
+    /// DIsplays the total value
+    ///
+    @IBOutlet private var totalPriceLabel: UILabel!
 }

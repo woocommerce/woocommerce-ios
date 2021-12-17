948a610def278cd869a948faa7a2b36f242d2b1d
Paolo Musolino <info@codeido.com>
2021-12-17 15:47:16 +0100

    feat: improvements to errorDescription

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Site Health Status Checker/SiteHealthStatusCheckerDetail.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Site Health Status Checker/SiteHealthStatusCheckerDetail.swift
@@ -1,8 +1,17 @@
 import SwiftUI
 
+/// Detail View of a request
+///
 struct SiteHealthStatusCheckerDetail: View {
 
     let request: SiteHealthStatusCheckerRequest
+
+    private var errorDescription: String {
+        if let error = request.error {
+            return String(describing: error)
+        }
+        return "-"
+    }
 
     var body: some View {
         ScrollView {
@@ -13,7 +22,7 @@ struct SiteHealthStatusCheckerDetail: View {
                 Divider()
                 TitleAndSubtitleRow(title: Localization.successTitle, subtitle: request.success ? Localization.successResult : Localization.failureResult)
                 Divider()
-                TitleAndSubtitleRow(title: Localization.errorTitle, subtitle: request.error?.localizedDescription ?? "-")
+                TitleAndSubtitleRow(title: Localization.errorTitle, subtitle: errorDescription)
                 Divider()
                 TitleAndSubtitleRow(title: Localization.totalDurationTitle, subtitle: formatTimeIntervalInSeconds(request.time))
                 Divider()

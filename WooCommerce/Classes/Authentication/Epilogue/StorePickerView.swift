b5f5316dbc8f28d296db28acbc6a504a45b49123
Paolo Musolino <info@codeido.com>
2021-12-23 13:56:35 +0100

    feat: StorePickerView

---
+++ WooCommerce/Classes/Authentication/Epilogue/StorePickerView.swift
@@ -0,0 +1,21 @@
+//
+//  StorePickerView.swift
+//  WooCommerce
+//
+//  Created by Paolo Musolino on 22/12/21.
+//  Copyright © 2021 Automattic. All rights reserved.
+//
+
+import SwiftUI
+
+struct StorePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StorePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        StorePickerView()
+    }
+}

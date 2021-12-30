b4fd99fce3c8a57e40fc6e0b191c7672cea56e3e
Ernesto Carrion <ernesto.carrion@automattic.com>
2021-12-29 22:13:31 -0500

    Adde Safe Area Insets key envorinment

---
+++ WooCommerce/Classes/Environment Keys/SafeAreaInsetsKey.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+/// Environment key that allow use to obtain safe areas insets from a environment value.
+/// By `default` it provides the first window safe areas insets.
+/// Provide your custom inset with  the `environment(_:_:)` modifier when dealing with multiple windows or specific screens.
+///
+struct SafeAreaInsetsKey: EnvironmentKey {
+    /// Returns the safe areas of the main window.
+    ///
+    static var defaultValue: EdgeInsets {
+        guard let window = UIApplication.shared.windows.first else {
+            return .zero
+        }
+
+        // Converts the non-directional UIEdgeInstets into direcional EdgeInsets
+        let safeInsets = window.safeAreaInsets
+        if UIView.userInterfaceLayoutDirection(for: window.semanticContentAttribute) == .rightToLeft {
+            return EdgeInsets(top: safeInsets.top, leading: safeInsets.left, bottom: safeInsets.bottom, trailing: safeInsets.right)
+        } else {
+            return EdgeInsets(top: safeInsets.top, leading: safeInsets.right, bottom: safeInsets.bottom, trailing: safeInsets.left)
+        }
+    }
+}
+
+extension EnvironmentValues {
+    /// Sets a custom safe area inset.
+    ///
+    var safeAreaInsets: EdgeInsets {
+        get {
+            self[SafeAreaInsetsKey.self]
+        }
+        set {
+            self[SafeAreaInsetsKey.self] = newValue
+        }
+    }
+}

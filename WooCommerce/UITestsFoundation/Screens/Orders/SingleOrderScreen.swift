939942d0eeb9885d8a1b9b8589bd07dd81d545c9
thehenrybyrd <15107387+thehenrybyrd@users.noreply.github.com>
2021-12-30 16:18:48 -0500

    Add SingleOrderScreen

--- WooCommerce/UITestsFoundation/Screens/Orders/SingleOrderScreen.swift
+++ WooCommerce/UITestsFoundation/Screens/Orders/SingleOrderScreen.swift
@@ -1,16 +1,46 @@
-import ScreenObject
 import XCTest
+import Foundation
 
-public final class SingleOrderScreen: ScreenObject {
+public final class SingleOrderScreen: BaseScreen {
 
     // TODO: Remove force `try` once `ScreenObject` migration is completed
     let tabBar = try! TabNavComponent()
 
-    init(app: XCUIApplication = XCUIApplication()) throws {
-        try super.init(
-            expectedElementGetters: [ { $0.staticTexts["summary-table-view-cell-title-label"] } ],
-            app: app
-        )
+    struct ElementStringIDs {
+           static let summaryTitleLabel = "summary-table-view-cell-title-label"
+           static let productsSection = "single-product-cell"
+           static let paymentsSection = "single-order-payment-cell"
+           static let customerSection = "customer-shipping-details-address"
+       }
+
+       //let tabBar = TabNavComponent()
+       private let summaryTitleLabel: XCUIElement
+       private let productsSection: XCUIElement
+       private let paymentsSection: XCUIElement
+       private let customerSection: XCUIElement
+
+       static var isVisible: Bool {
+           let summaryTitleLabel = XCUIApplication().staticTexts[ElementStringIDs.summaryTitleLabel]
+           return summaryTitleLabel.exists && summaryTitleLabel.isHittable
+       }
+
+       init() {
+           summaryTitleLabel = XCUIApplication().staticTexts[ElementStringIDs.summaryTitleLabel]
+           productsSection = XCUIApplication().cells[ElementStringIDs.productsSection]
+           paymentsSection = XCUIApplication().cells[ElementStringIDs.paymentsSection]
+           customerSection = XCUIApplication().cells[ElementStringIDs.customerSection]
+
+           super.init(element: summaryTitleLabel)
+
+           XCTAssert(productsSection.exists)
+           XCTAssert(paymentsSection.exists)
+           XCTAssert(customerSection.exists)
+       }
+
+    @discardableResult
+    public func verifySingleOrderScreenLoaded() throws -> Self {
+        XCTAssertTrue(isLoaded())
+        return self
     }
 
     @discardableResult

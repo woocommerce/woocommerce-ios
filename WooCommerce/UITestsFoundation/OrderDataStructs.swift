f31fa12a22efbd33ec7834a55c954a0e6c1528cf
thehenrybyrd <15107387+thehenrybyrd@users.noreply.github.com>
2022-01-03 16:40:04 -0500

    Additions to OrderDataStructs

--- WooCommerce/UITestsFoundation/OrderDataStructs.swift
+++ WooCommerce/UITestsFoundation/OrderDataStructs.swift
@@ -15,6 +15,17 @@ public struct OrderData: Codable {
     public let id: Int
     public let number: String
     public var status: String
-    public let total: String
-    public let product_id: String
+//    public let total: String    // doesn't work because the number isn't formatted in the mock, but is in the UI
+    public let line_items: [LineItems]
+    public let billing: BillingInformation
+}
+
+public struct LineItems: Codable {
+    public let name: String
+    public let product_id: Int
+}
+
+public struct BillingInformation: Codable {
+    public let first_name: String
+    public let last_name: String
 }

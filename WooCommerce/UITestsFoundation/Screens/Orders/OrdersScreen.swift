57f031d5ff46f67f0a9e20b96f51507d42619604
thehenrybyrd <15107387+thehenrybyrd@users.noreply.github.com>
2022-01-03 16:40:22 -0500

    Attempts to fix verifyOrdersList

--- WooCommerce/UITestsFoundation/Screens/Orders/OrdersScreen.swift
+++ WooCommerce/UITestsFoundation/Screens/Orders/OrdersScreen.swift
@@ -37,13 +37,19 @@ public final class OrdersScreen: ScreenObject {
         searchButton.tap()
         return try OrderSearchScreen()
     }
-    
-    
+
     public func verifyOrdersList(orders: [OrderData]) throws -> Self {
-        app.assertTextVisibilityCount(textToFind: orders[0].number)
-        app.assertElementExistsOnCell(mainCell: orders[0].number, elementToFind: orders[0].product_id)
-        app.assertCorrectCellCountDisplayed(expectedCount: orders.count, actualCount: app.tables.cells.count)
-        
+      //  app.assertCorrectCellCountDisplayed(expectedCount: orders.count, actualCount: app.tables.cells.count)
+        XCTAssertEqual(orders.count, app.tables.cells.count, "Expecting \(orders.count) orders, got \(app.tables.cells.count) instead!") //orders.count counts the orders in the "orders" variable (from the mocks), while tables.cells.count counts the cells in the table, in the UI itself.
+        app.assertTextVisibilityCount(textToFind: String(orders[0].id))
+        //check an item from the mocks, make sure it appears on the screen. something like order id is good here, but it has to be a string
+        //app.assertTextVisibilityCount(textToFind: String(orders[0].total)) // doesn't work because the Total isn't formatted in the mock, but is in the UI.
+        app.assertTwoTextsAppearOnSameLabel(firstSubstring: String(orders[0].id), secondSubstring: orders[0].billing.first_name)
+        // check that the accessibilityID has the correct id and first name
+//        app.assertTextVisibilityCount(textToFind: orders[0].total)
+
+        //        app.assertElementExistsOnCell(mainCell: orders[0].number, elementToFind: orders[0].product_id) // this one might not work, depending on the structure. Oh, nope. the product id won't display on this screen, it's on singleOrder screen.
+
  //       let TextVisibilityCount = try app.getTextVisibilityCount(text: orders[0].number)
 //
   //      XCTAssertTrue(numberVisibilityCount == 1, "Expecting order number to appear once, appeared \(nameVisibilityCount) times instead!")
@@ -52,7 +58,7 @@ public final class OrdersScreen: ScreenObject {
 
         return self
     }
-    
+
     public func verifyOrdersScreenLoaded() throws -> Self {
         XCTAssertTrue(isLoaded)
         return self

7b64644bf5cb849f3bd6288b853740bc5b9d65ab
thehenrybyrd <15107387+thehenrybyrd@users.noreply.github.com>
2022-01-03 16:40:56 -0500

    Attempts to fix verifySingleOrder

--- WooCommerce/UITestsFoundation/Screens/Orders/SingleOrderScreen.swift
+++ WooCommerce/UITestsFoundation/Screens/Orders/SingleOrderScreen.swift
@@ -8,11 +8,25 @@ public final class SingleOrderScreen: ScreenObject {
 
     init(app: XCUIApplication = XCUIApplication()) throws {
         try super.init(
-            expectedElementGetters: [ { $0.staticTexts["summary-table-view-cell-title-label"] } ],
+            expectedElementGetters: [
+                { $0.staticTexts["summary-table-view-cell-title-label"] },
+                { $0.cells["single-product-cell"] } ],
             app: app
         )
     }
 
+    @discardableResult
+    public func verifySingleOrder(order: OrderData) throws -> Self {
+        app.assertTextVisibilityCount(textToFind: order.status)
+        app.assertTextVisibilityCount(textToFind: order.billing.first_name)
+  //      app.assertTextVisibilityCount(textToFind: order.total)
+        XCTAssertTrue(app.textViews[order.line_items[0].name].isFullyVisibleOnScreen(), "First product name is not visible on screen!")
+        
+//        app.elementIsFullyVisibleOnScreen(element: "single-product-cell")
+        return self
+        // we could also use accessibiltyIDs for this, possibly via elementIsFullyVisibleOnScreen
+    }
+    
     @discardableResult
     public func goBackToOrdersScreen() throws -> OrdersScreen {
         pop()

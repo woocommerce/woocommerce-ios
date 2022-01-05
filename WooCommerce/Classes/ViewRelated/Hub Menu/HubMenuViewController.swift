4e14c542a982b3d4713d596ac4c3077a93c97e35
Paolo Musolino <info@codeido.com>
2022-01-05 16:53:38 +0100

    feat: present review detail from HubMenuCoordinator

--- WooCommerce/Classes/ViewRelated/Hub Menu/HubMenuViewController.swift
+++ WooCommerce/Classes/ViewRelated/Hub Menu/HubMenuViewController.swift
@@ -1,16 +1,26 @@
 import SwiftUI
 import UIKit
+import Yosemite
 
 /// Displays a grid view of all available menu in the "Menu" tab (eg. View Store, Reviews, Coupons, etc...)
 final class HubMenuViewController: UIHostingController<HubMenu> {
+    private var hubMenu: HubMenu
+
     init(siteID: Int64, navigationController: UINavigationController?) {
-        super.init(rootView: HubMenu(siteID: siteID, navigationController: navigationController))
+        hubMenu = HubMenu(siteID: siteID, navigationController: navigationController)
+        super.init(rootView: hubMenu)
         configureNavigationBar()
         configureTabBarItem()
     }
 
     required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    /// Present the specific Review Details View from a push notification
+    ///
+    func pushReviewDetailsViewController(using parcel: ProductReviewFromNoteParcel) {
+        hubMenu.pushReviewDetailsViewController(using: parcel)
     }
 }
 
@@ -24,7 +34,6 @@ private extension HubMenuViewController {
         tabBarItem.image = .hubMenu
         tabBarItem.accessibilityIdentifier = "tab-bar-menu-item"
     }
-
 }
 
 private extension HubMenuViewController {

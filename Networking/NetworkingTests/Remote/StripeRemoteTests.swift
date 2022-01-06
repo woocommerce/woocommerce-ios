fa94cc58f6f49b527491f9d1eb1785429720be2f
Allen Snook <allendav@automattic.com>
2022-01-06 12:14:17 -0800

    Add given, when, then to unit tests

--- Networking/NetworkingTests/Remote/StripeRemoteTests.swift
+++ Networking/NetworkingTests/Remote/StripeRemoteTests.swift
@@ -24,16 +24,19 @@ final class StripeRemoteTests: XCTestCase {
     /// statement descriptor, currencies and country here.
     ///
     func test_loadAccount_properly_returns_account() throws {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-complete")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isSuccess)
         let account = try result.get()
         XCTAssertEqual(account.status, .complete)
@@ -47,16 +50,19 @@ final class StripeRemoteTests: XCTestCase {
     /// Verifies that loadAccount properly handles the rejected - fraud response
     ///
     func test_loadAccount_properly_handles_rejected_fraud_account() throws {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-rejected-fraud")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isSuccess)
         let account = try result.get()
         XCTAssertEqual(account.status, .rejectedFraud)
@@ -65,16 +71,19 @@ final class StripeRemoteTests: XCTestCase {
     /// Verifies that loadAccount properly handles the rejected - terms of service response
     ///
     func test_loadAccount_properly_handles_rejected_terms_of_service_account() throws {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-rejected-terms-of-service")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isSuccess)
         let account = try result.get()
         XCTAssertEqual(account.status, .rejectedTermsOfService)
@@ -83,16 +92,19 @@ final class StripeRemoteTests: XCTestCase {
     /// Verifies that loadAccount properly handles the rejected - listed response
     ///
     func test_loadAccount_properly_handles_rejected_listed_account() throws {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-rejected-listed")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isSuccess)
         let account = try result.get()
         XCTAssertEqual(account.status, .rejectedListed)
@@ -101,16 +113,19 @@ final class StripeRemoteTests: XCTestCase {
     /// Verifies that loadAccount properly handles the rejected - other response
     ///
     func test_loadAccount_properly_handles_rejected_other_account() throws {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-rejected-other")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isSuccess)
         let account = try result.get()
         XCTAssertEqual(account.status, .rejectedOther)
@@ -119,16 +134,19 @@ final class StripeRemoteTests: XCTestCase {
     /// Verifies that loadAccount properly handles the restricted (review) response
     ///
     func test_loadAccount_properly_handles_restricted_review_account() throws {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-restricted")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isSuccess)
         let account = try result.get()
         XCTAssertEqual(account.status, .restricted)
@@ -139,16 +157,19 @@ final class StripeRemoteTests: XCTestCase {
     /// Verifies that loadAccount properly handles the restricted - pending response
     ///
     func test_loadAccount_properly_handles_restricted_pending_account() throws {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-restricted-pending")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isSuccess)
         let account = try result.get()
         XCTAssertEqual(account.status, .restricted)
@@ -160,16 +181,19 @@ final class StripeRemoteTests: XCTestCase {
     /// Verifies that loadAccount properly handles the restricted - overdue response
     ///
     func test_loadAccount_properly_handles_restricted_overdue_account() throws {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-restricted-overdue")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isSuccess)
         let account = try result.get()
         XCTAssertEqual(account.status, .restricted)
@@ -180,16 +204,19 @@ final class StripeRemoteTests: XCTestCase {
     /// Verifies that loadAccount properly handles an unrecognized status response
     ///
     func test_loadAccount_properly_handles_unrecognized_status_account() throws {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-unknown-status")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isSuccess)
         let account = try result.get()
         XCTAssertEqual(account.status, .unknown)
@@ -198,16 +225,19 @@ final class StripeRemoteTests: XCTestCase {
     /// Verifies that loadAccount properly handles unexpected fields in the response (resulting in a Decoding Error)
     ///
     func test_loadAccount_properly_handles_unexpected_fields_in_response() {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-wrong-json")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isFailure)
         let error = result.failure
         XCTAssertTrue(error is DecodingError)
@@ -216,16 +246,19 @@ final class StripeRemoteTests: XCTestCase {
     /// Properly decodes live account in live mode stripe-account-live-live
     ///
     func test_loadAccount_properly_handles_live_account_in_live_mode() throws {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-live-live")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isSuccess)
         let account = try result.get()
         XCTAssertEqual(account.isLiveAccount, true)
@@ -235,16 +268,19 @@ final class StripeRemoteTests: XCTestCase {
     /// Properly decodes live account in test mode stripe-account-live-test
     ///
     func test_loadAccount_properly_handles_live_account_in_test_mode() throws {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-live-test")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isSuccess)
         let account = try result.get()
         XCTAssertEqual(account.isLiveAccount, true)
@@ -254,16 +290,19 @@ final class StripeRemoteTests: XCTestCase {
     /// Properly decodes developer account in test mode stripe-account-dev-test
     ///
     func test_loadAccount_properly_handles_dev_account_in_test_mode() throws {
+        // Given
         let remote = StripeRemote(network: network)
 
         network.simulateResponse(requestUrlSuffix: "wc_stripe/account/summary", filename: "stripe-account-dev-test")
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isSuccess)
         let account = try result.get()
         XCTAssertEqual(account.isLiveAccount, false)
@@ -273,17 +312,20 @@ final class StripeRemoteTests: XCTestCase {
     /// Verifies that loadAccount properly handles networking errors
     ///
     func test_loadAccount_properly_handles_networking_errors() throws {
+        // Given
         let remote = StripeRemote(network: network)
         let expectedError = NSError(domain: #function, code: 0, userInfo: nil)
 
         network.simulateError(requestUrlSuffix: "wc_stripe/account/summary", error: expectedError)
 
+        // When
         let result: Result<StripeAccount, Error> = waitFor { promise in
             remote.loadAccount(for: self.sampleSiteID) { result in
                 promise(result)
             }
         }
 
+        // Then
         XCTAssertTrue(result.isFailure)
         let error = try XCTUnwrap(result.failure) as NSError
         XCTAssertEqual(expectedError, error)

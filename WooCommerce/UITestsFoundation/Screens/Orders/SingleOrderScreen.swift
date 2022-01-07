bdd01c9ea24ac4fe70aa283023c870c02e6272e4
thehenrybyrd <15107387+thehenrybyrd@users.noreply.github.com>
2022-01-07 12:03:32 -0500

    Fix element on cell check
    1. use assertElementExistsOnCell instead of verifyChildElementOnParentCell
    2. use string interpolation to reference first_name + last_name

--- WooCommerce/UITestsFoundation/Screens/Orders/SingleOrderScreen.swift
+++ WooCommerce/UITestsFoundation/Screens/Orders/SingleOrderScreen.swift
@@ -17,6 +17,9 @@ public final class SingleOrderScreen: ScreenObject {
     public func verifySingleOrder(order: OrderData) throws -> Self {
         app.assertTextVisibilityCount(textToFind: order.status, expectedCount: 1) //checks that the Mock order status appears on the screen
         app.assertTextVisibilityCount(textToFind: order.billing.first_name, expectedCount: 2) // Check that the Mock first name ("Mira") appears twice - because this order has both billing and shipping details
+        // try app.verifyChildElementOnParentCell(parent: "single-product-cell", child: "Mira") //this shouldn't work! "single-product-cell" doesn't include the text "Mira"
+        app.assertElementExistsOnCell(mainCell: "summary-table-view-cell", elementToFind: "\(order.billing.first_name) \(order.billing.last_name)")
+        //  "\(billing.first_name) + \(billing.last_name)"  "Mira Workman"
         app.assertTextVisibilityCount(textToFind: order.total, expectedCount: 1)
         XCTAssertTrue(app.staticTexts[order.line_items[0].name].isFullyVisibleOnScreen(), "First product name is not visible on screen!") //Black Coral shades is first in the mocks, that's what we're referencing, even though Malaya shades appear first on the screen.
 

d7afe726af0dd14b200e3ee4c07334b73dfe8036
thehenrybyrd <15107387+thehenrybyrd@users.noreply.github.com>
2022-01-07 12:01:42 -0500

    Add comments explaining the asserts
    To help me remember what each assert does. May removed later.

--- WooCommerce/UITestsFoundation/Screens/Orders/SingleOrderScreen.swift
+++ WooCommerce/UITestsFoundation/Screens/Orders/SingleOrderScreen.swift
@@ -15,9 +15,8 @@ public final class SingleOrderScreen: ScreenObject {
 
     @discardableResult
     public func verifySingleOrder(order: OrderData) throws -> Self {
-        app.assertTextVisibilityCount(textToFind: order.status, expectedCount: 1)
-        app.assertTextVisibilityCount(textToFind: order.billing.first_name, expectedCount: 2) // the first name "Mira" appears twice
-        try app.verifyChildElementOnParentCell(parent: "single-product-cell", child: "Mira")
+        app.assertTextVisibilityCount(textToFind: order.status, expectedCount: 1) //checks that the Mock order status appears on the screen
+        app.assertTextVisibilityCount(textToFind: order.billing.first_name, expectedCount: 2) // Check that the Mock first name ("Mira") appears twice - because this order has both billing and shipping details
         app.assertTextVisibilityCount(textToFind: order.total, expectedCount: 1)
         XCTAssertTrue(app.staticTexts[order.line_items[0].name].isFullyVisibleOnScreen(), "First product name is not visible on screen!") //Black Coral shades is first in the mocks, that's what we're referencing, even though Malaya shades appear first on the screen.
 

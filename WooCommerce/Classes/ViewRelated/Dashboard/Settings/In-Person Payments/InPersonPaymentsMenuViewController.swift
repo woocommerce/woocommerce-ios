82d68c29b675e472b4c98b81811d9e0288fb6540
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2022-01-11 09:57:26 -0500

    Bring back link to Chipper's manual

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsMenuViewController.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsMenuViewController.swift
@@ -29,6 +29,7 @@ private extension InPersonPaymentsMenuViewController {
         rows = [
             .orderCardReader,
             .manageCardReader,
+            .bbposChipper2XBTManual,
             .stripeM2Manual
         ]
     }
@@ -54,6 +55,8 @@ private extension InPersonPaymentsMenuViewController {
             configureOrderCardReader(cell: cell)
         case let cell as LeftImageTableViewCell where row == .manageCardReader:
             configureManageCardReader(cell: cell)
+        case let cell as LeftImageTableViewCell where row == .bbposChipper2XBTManual:
+            configureChipper2XManual(cell: cell)
         case let cell as LeftImageTableViewCell where row == .stripeM2Manual:
             configureStripeM2Manual(cell: cell)
         default:
@@ -75,11 +78,18 @@ private extension InPersonPaymentsMenuViewController {
         cell.configure(image: .creditCardIcon, text: Localization.manageCardReader)
     }
 
+    func configureChipper2XManual(cell: LeftImageTableViewCell) {
+        cell.imageView?.tintColor = .text
+        cell.accessoryType = .disclosureIndicator
+        cell.selectionStyle = .default
+        cell.configure(image: .cardReaderManualIcon, text: Localization.chipperCardReaderManual)
+    }
+
     func configureStripeM2Manual(cell: LeftImageTableViewCell) {
         cell.imageView?.tintColor = .text
         cell.accessoryType = .disclosureIndicator
         cell.selectionStyle = .default
-        cell.configure(image: .cardReaderManualIcon, text: Localization.cardReaderManual)
+        cell.configure(image: .cardReaderManualIcon, text: Localization.stripeM2CardReaderManual)
     }
 }
 
@@ -107,6 +117,10 @@ extension InPersonPaymentsMenuViewController {
         let viewModelsAndViews = CardReaderSettingsViewModelsOrderedList()
         viewController.configure(viewModelsAndViews: viewModelsAndViews)
         show(viewController, sender: self)
+    }
+
+    func bbposChipper2XBTManualWasPressed() {
+            WebviewHelper.launch(Constants.bbposChipper2XBTManualURL, with: self)
     }
 
     func stripeM2ManualWasPressed() {
@@ -145,6 +159,8 @@ extension InPersonPaymentsMenuViewController {
             orderCardReaderWasPressed()
         case .manageCardReader:
             manageCardReaderWasPressed()
+        case .bbposChipper2XBTManual:
+            bbposChipper2XBTManualWasPressed()
         case .stripeM2Manual:
             stripeM2ManualWasPressed()
         }
@@ -165,9 +181,14 @@ private extension InPersonPaymentsMenuViewController {
             comment: "Navigates to Card Reader management screen"
         )
 
-        static let cardReaderManual = NSLocalizedString(
-            "Card reader manual",
-            comment: "Navigates to Card Reader manual"
+        static let chipperCardReaderManual = NSLocalizedString(
+            "Chipper 2X card reader manual",
+            comment: "Navigates to Chipper Card Reader manual"
+        )
+
+        static let stripeM2CardReaderManual = NSLocalizedString(
+            "Stripe M2 card reader manual",
+            comment: "Navigates to Stripe M2 Card Reader manual"
         )
     }
 }
@@ -175,17 +196,11 @@ private extension InPersonPaymentsMenuViewController {
 private enum Row: CaseIterable {
     case orderCardReader
     case manageCardReader
+    case bbposChipper2XBTManual
     case stripeM2Manual
 
     var type: UITableViewCell.Type {
-        switch self {
-        case .orderCardReader:
-            return LeftImageTableViewCell.self
-        case .manageCardReader:
-            return LeftImageTableViewCell.self
-        case .stripeM2Manual:
-            return LeftImageTableViewCell.self
-        }
+        LeftImageTableViewCell.self
     }
 
     var reuseIdentifier: String {
@@ -195,6 +210,7 @@ private enum Row: CaseIterable {
 
 private enum Constants {
     static let woocommercePurchaseCardReaderURL = URL(string: "https://woocommerce.com/products/m2-card-reader/")!
+    static let bbposChipper2XBTManualURL = URL(string: "https://developer.bbpos.com/quick_start_guide/Chipper%202X%20BT%20Quick%20Start%20Guide.pdf")!
     static let stripeM2ManualURL = URL(string: "https://stripe.com/files/docs/terminal/m2_product_sheet.pdf")!
 }
 

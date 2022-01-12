6f1e951edf4cefa473c6bbd61ccaf92c295c1d11
Ernesto Carrion <ernesto.carrion@automattic.com>
2022-01-12 08:18:24 -0500

    Added empty notice modifier trype

--- WooCommerce/Classes/View Modifiers/View+NoticesModifier.swift
+++ WooCommerce/Classes/View Modifiers/View+NoticesModifier.swift
@@ -1,1 +1,24 @@
 import SwiftUI
+
+/// View Modifier to show a notice in front of a view.
+///
+struct NoticeModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+
+            Text("Holi")
+        }
+    }
+}
+
+// MARK: View Extension
+
+extension View {
+    /// Shows a notice in front of the view.
+    ///
+    func notice() -> some View {
+        self.modifier(NoticeModifier())
+    }
+}
+

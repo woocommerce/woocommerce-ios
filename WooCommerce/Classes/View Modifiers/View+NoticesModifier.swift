a44f9a3d4640f84e5d37026c2f016bd95b93da77
Ernesto Carrion <ernesto.carrion@automattic.com>
2022-01-12 15:35:32 -0500

    Added NoticeView swiftui wrapper

--- WooCommerce/Classes/View Modifiers/View+NoticesModifier.swift
+++ WooCommerce/Classes/View Modifiers/View+NoticesModifier.swift
@@ -1,14 +1,115 @@
 import SwiftUI
-import Lottie
+import UIKit
 
-/// View Modifier to show a notice in front of a view.
+/// View Modifier that shows a notice in front of a view.
 ///
 struct NoticeModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        ZStack {
-            content
 
-            Text("Holi")
+    /// Notice object to render
+    ///
+    let notice: Notice
+
+    func body(content: Content) -> some View {
+        // Geometry reader to provide the correct view width.
+        GeometryReader { geometry in
+            ZStack {
+
+                // Main content
+                content
+
+                // VStack with spacer to push content to the bottom
+                VStack {
+                    Spacer()
+
+                    // NoticeView wrapper
+                    NoticeAlert(notice: notice, width: geometry.size.width)
+                        .fixedSize(horizontal: false, vertical: true)
+                }
+            }
+        }
+    }
+}
+
+// MARK: Custom Views
+
+/// `SwiftUI` representable type for `NoticeView`.
+///
+private struct NoticeAlert: UIViewRepresentable {
+
+    /// Notice object render
+    ///
+    let notice: Notice
+
+    /// Desired width of the view.
+    ///
+    let width: CGFloat
+
+    func makeUIView(context: Context) -> NoticeWrapper {
+        let noticeView = NoticeView(notice: notice)
+        let wrapperView = NoticeWrapper(noticeView: noticeView)
+        wrapperView.translatesAutoresizingMaskIntoConstraints = false
+        return wrapperView
+    }
+
+    func updateUIView(_ uiView: NoticeWrapper, context: Context) {
+        uiView.width = width
+    }
+}
+
+
+private extension NoticeAlert {
+    /// Wrapper type to force the underlying `NoticeView` to fixed width
+    ///
+    class NoticeWrapper: UIView {
+        /// Underlying notice view
+        ///
+        let noticeView: NoticeView
+
+        /// Fixed width constraint.
+        ///
+        var width: CGFloat = 0 {
+            didSet {
+                noticeViewWidthConstraint.constant = width
+            }
+        }
+
+        /// Width constraint for the notice view.
+        ///
+        private var noticeViewWidthConstraint = NSLayoutConstraint()
+
+        /// Notice view padding.
+        ///
+        let defaultInsets = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+
+        init(noticeView: NoticeView) {
+            self.noticeView = noticeView
+            super.init(frame: .zero)
+
+            // Add notice view to edges
+            noticeView.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(noticeView)
+
+            layoutMargins = defaultInsets
+            pinSubviewToAllEdgeMargins(noticeView)
+
+            noticeViewWidthConstraint = widthAnchor.constraint(equalToConstant: width)
+            noticeViewWidthConstraint.isActive = true
+        }
+
+        /// Returns the preferred size of the view using the fixed width.
+        ///
+        override var intrinsicContentSize: CGSize {
+            let targetSize =  CGSize(width: width - defaultInsets.left - defaultInsets.right, height: 0)
+            let noticeHeight = noticeView.systemLayoutSizeFitting(
+                targetSize,
+                withHorizontalFittingPriority: .required,
+                verticalFittingPriority: .defaultLow
+            ).height
+            return CGSize(width: width, height: noticeHeight + defaultInsets.top + defaultInsets.bottom)
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
         }
     }
 }
@@ -16,10 +117,10 @@ struct NoticeModifier: ViewModifier {
 // MARK: View Extension
 
 extension View {
-    /// Shows a notice in front of the view.
+    /// Shows the provided notice in front of the view.
     ///
-    func notice() -> some View {
-        self.modifier(NoticeModifier())
+    func notice(_ notice: Notice) -> some View {
+        self.modifier(NoticeModifier(notice: notice))
     }
 }
 
@@ -28,7 +129,17 @@ extension View {
 struct NoticeModifier_Previews: PreviewProvider {
     static var previews: some View {
         Text("")
-            .notice()
+            .notice(
+                .init(title: "API Error",
+                      subtitle: "Restricted Access",
+                      message: "Your photos could not be downloaded, please ask for the correct permissions!",
+                      feedbackType: .error,
+                      notificationInfo: nil,
+                      actionTitle: "Retry",
+                      actionHandler: {
+                          print("Retry")
+                      })
+            )
             .environment(\.colorScheme, .light)
             .previewDisplayName("Light Content")
     }

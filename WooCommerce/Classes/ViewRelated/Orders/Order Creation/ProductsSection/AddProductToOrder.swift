45977982d0a239b11b0546ab9a56e70ecc03c708
rachelmcr <rachel@automattic.com>
2022-01-14 13:38:03 +0000

    Use a lazy navigation link to open each variable product's variation list

--- WooCommerce/Classes/ViewRelated/Orders/Order Creation/ProductsSection/AddProductToOrder.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/ProductsSection/AddProductToOrder.swift
@@ -54,13 +54,12 @@ struct AddProductToOrder: View {
         .wooNavigationBarStyle()
     }
 
+    /// Creates the `ProductRow` for a product, depending on whether the product is variable.
+    ///
     @ViewBuilder private func createProductRow(rowViewModel: ProductRowViewModel) -> some View {
-        if rowViewModel.numberOfVariations > 0 {
-            NavigationLink {
-                if let addVariationToOrderVM = viewModel.getVariationsViewModel(for: rowViewModel.productOrVariationID) {
-                    AddProductVariationToOrder(isPresented: $isPresented, viewModel: addVariationToOrderVM)
-                }
-            } label: {
+        if rowViewModel.numberOfVariations > 0,
+           let addVariationToOrderVM = viewModel.getVariationsViewModel(for: rowViewModel.productOrVariationID) {
+            LazyNavigationLink(destination: AddProductVariationToOrder(isPresented: $isPresented, viewModel: addVariationToOrderVM)) {
                 ProductRow(viewModel: rowViewModel)
             }
         } else {

1f65bb1aa9b1f18aa0344ead74919d5162b65516
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2022-01-17 13:05:46 -0500

    Add toggles, make them work as a group

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/Onboarding Errors/InPersonPaymentsSelectPluginView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/Onboarding Errors/InPersonPaymentsSelectPluginView.swift
@@ -3,17 +3,37 @@ import SwiftUI
 struct InPersonPaymentsSelectPlugin: View {
     let onRefresh: () -> Void
 
+    @State private var activateWCPay = true
+    @State private var activateStripe = true
+
     var body: some View {
-        InPersonPaymentsOnboardingError(
-            title: Localization.unavailable,
-            message: Localization.message,
-            image: InPersonPaymentsOnboardingError.ImageInfo(
-                image: .paymentErrorImage,
-                height: 180.0
-            ),
-            supportLink: false,
-            learnMore: true
-        )
+        VStack {
+            InPersonPaymentsOnboardingError(
+                title: Localization.unavailable,
+                message: Localization.message,
+                image: InPersonPaymentsOnboardingError.ImageInfo(
+                    image: .paymentErrorImage,
+                    height: 180.0
+                ),
+                supportLink: false,
+                learnMore: false
+            )
+
+            VStack {
+                // Switch to activate WooCommercePayments
+                Toggle(Localization.wcPay, isOn: $activateWCPay)
+                    .onChange(of: activateWCPay, perform: { value in
+                        activateStripe = !value
+                    })
+
+                // Switch to activate Stripe extension
+                Toggle(Localization.stripeExtension, isOn: $activateStripe)
+                    .onChange(of: activateStripe, perform: { value in
+                        activateWCPay = !value
+                    })
+            }
+            .padding([.leading, .trailing, .bottom])
+        }
     }
 }
 
@@ -26,6 +46,16 @@ private enum Localization {
     static let message = NSLocalizedString(
         "Choose between WooCommerce Payments and Stripe Extension",
         comment: "Message requesting merchants to select between available payments processors"
+    )
+
+    static let stripeExtension = NSLocalizedString(
+        "Stripe Extension",
+        comment: "Message asking merchants whether the Stripe Extension should be active"
+    )
+
+    static let wcPay = NSLocalizedString(
+        "WooCommerce Payments",
+        comment: "Message asking merchants whether the WooCommerce Payments should be active"
     )
 }
 

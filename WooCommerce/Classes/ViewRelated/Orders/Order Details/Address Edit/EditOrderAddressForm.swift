e15990f685db61d012a8f344ffe0df6219e67ca6
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2022-01-24 16:46:17 +0300

    Move navigation links to ScrollView

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Address Edit/EditOrderAddressForm.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Address Edit/EditOrderAddressForm.swift
@@ -232,31 +232,31 @@ struct EditOrderAddressForm<ViewModel: AddressFormViewModelProtocol>: View {
                         .padding(.horizontal, insets: safeAreaInsets)
                         .background(Color(.systemBackground))
                 }
+
+                // Go to edit country
+                LazyNavigationLink(destination: FilterListSelector(viewModel: viewModel.createCountryViewModel()), isActive: $showCountrySelector) {
+                    EmptyView()
+                }
+
+                // Go to edit state
+                LazyNavigationLink(destination: FilterListSelector(viewModel: viewModel.createStateViewModel()), isActive: $showStateSelector) {
+                    EmptyView()
+                }
+
+                ///
+                /// iOS 14.5 has a bug where
+                /// Pushing a view while having "exactly two" navigation links makes the pushed view to be popped when the initial view changes its state.
+                /// EG: AddressForm -> CountrySelector -> Country is selected -> AddressForm updates country -> CountrySelector is popped automatically.
+                /// Adding an extra and useless navigation link fixes the problem but throws a warning in the console.
+                /// Ref: https://forums.swift.org/t/14-5-beta3-navigationlink-unexpected-pop/45279
+                ///
+                NavigationLink(destination: EmptyView()) {
+                    EmptyView()
+                }
             }
             .disableAutocorrection(true)
             .background(Color(.listBackground))
             .ignoresSafeArea(.container, edges: [.horizontal, .bottom])
-
-            // Go to edit country
-            LazyNavigationLink(destination: FilterListSelector(viewModel: viewModel.createCountryViewModel()), isActive: $showCountrySelector) {
-                EmptyView()
-            }
-
-            // Go to edit state
-            LazyNavigationLink(destination: FilterListSelector(viewModel: viewModel.createStateViewModel()), isActive: $showStateSelector) {
-                EmptyView()
-            }
-
-            ///
-            /// iOS 14.5 has a bug where
-            /// Pushing a view while having "exactly two" navigation links makes the pushed view to be popped when the initial view changes its state.
-            /// EG: AddressForm -> CountrySelector -> Country is selected -> AddressForm updates country -> CountrySelector is popped automatically.
-            /// Adding an extra and useless navigation link fixes the problem but throws a warning in the console.
-            /// Ref: https://forums.swift.org/t/14-5-beta3-navigationlink-unexpected-pop/45279
-            ///
-            NavigationLink(destination: EmptyView()) {
-                EmptyView()
-            }
         }
         .navigationTitle(viewModel.viewTitle)
         .navigationBarTitleDisplayMode(.inline)

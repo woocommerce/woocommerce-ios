00748db1ffec9355e26edeac6d9ad3d630456041
Huong Do <hi.huongdo@gmail.com>
2022-01-25 14:31:08 +0700

    Update relationships between coupons and search results

--- Storage/Storage/Model/CouponSearchResult+CoreDataProperties.swift
+++ Storage/Storage/Model/CouponSearchResult+CoreDataProperties.swift
@@ -8,6 +8,23 @@ extension CouponSearchResult {
     }
 
     @NSManaged public var keyword: String?
-    @NSManaged public var coupon: Coupon?
+    @NSManaged public var coupons: NSSet?
+
+}
+
+// MARK: Generated accessors for coupons
+extension CouponSearchResult {
+
+    @objc(addCouponsObject:)
+    @NSManaged public func addToCoupons(_ value: Coupon)
+
+    @objc(removeCouponsObject:)
+    @NSManaged public func removeFromCoupons(_ value: Coupon)
+
+    @objc(addCoupons:)
+    @NSManaged public func addToCoupons(_ values: NSSet)
+
+    @objc(removeCoupons:)
+    @NSManaged public func removeFromCoupons(_ values: NSSet)
 
 }

1208e17ec07078b9370ec3e811617c43dc3e0a0e
Paolo Musolino <info@codeido.com>
2022-01-27 16:02:43 +0100

    fix: Inbox Mapper

--- Networking/Networking/Mapper/InboxNoteListMapper.swift
+++ Networking/Networking/Mapper/InboxNoteListMapper.swift
@@ -4,10 +4,16 @@ import Foundation
 ///
 struct InboxNoteListMapper: Mapper {
 
-    /// (Attempts) to convert a dictionary into a Inbox Note Array.
+    /// Site we're parsing `InboxNote`s for
+    /// We're injecting this field by copying it in after parsing responses, because `siteID` is not returned in any of the Inbox Note endpoints.
+    ///
+    let siteID: Int64
+
+    /// (Attempts) to convert a dictionary into an Inbox Note Array.
     ///
     func map(response: Data) throws -> [InboxNote] {
         let decoder = JSONDecoder()
-        return try decoder.decode([InboxNote].self, from: response)
+        let inboxNotes = try decoder.decode([InboxNote].self, from: response)
+        return inboxNotes.map { $0.copy(siteID: siteID) }
     }
 }

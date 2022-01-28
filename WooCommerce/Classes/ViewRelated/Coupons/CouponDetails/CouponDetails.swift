464f516796c2a3db78dc207b3321d24d62927eb9
Huong Do <hi.huongdo@gmail.com>
2022-01-28 12:00:42 +0700

    Add content for coupon details

--- WooCommerce/Classes/ViewRelated/Coupons/CouponDetails/CouponDetails.swift
+++ WooCommerce/Classes/ViewRelated/Coupons/CouponDetails/CouponDetails.swift
@@ -9,7 +9,33 @@ struct CouponDetails: View {
     }
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                Text(Localization.detailSectionTitle)
+                    .bold()
+                    .padding(Constants.margin)
+                Divider()
+                    .padding(.leading, Constants.margin)
+                TitleAndValueRow(title: Localization.couponCode,
+                                 value: .content(viewModel.couponCode),
+                                 selectable: true) {}
+            }
+            .background(Color(.listForeground))
+        }
+        .background(Color(.listBackground))
+        .navigationTitle(Localization.navigationTitle)
+    }
+}
+
+private extension CouponDetails {
+    enum Constants {
+        static let margin: CGFloat = 16
+    }
+
+    enum Localization {
+        static let navigationTitle = NSLocalizedString("Coupon", comment: "Title of Coupon Details screen")
+        static let detailSectionTitle = NSLocalizedString("Coupon Details", comment: "Title of Details section in Coupon Details screen")
+        static let couponCode = NSLocalizedString("Coupon Code", comment: "Title of the Coupon Code row in Coupon Details screen")
     }
 }
 

b286eb333012d64fea07d29a582b2da79fc33e60
Josh Heald <joshheald@users.noreply.github.com>
2022-02-02 17:41:09 +0000

    5976 Test card and cardPresent charges separately

--- Networking/NetworkingTests/Mapper/WCPayChargeMapperTests.swift
+++ Networking/NetworkingTests/Mapper/WCPayChargeMapperTests.swift
@@ -20,10 +20,10 @@ class WCPayChargeMapperTests: XCTestCase {
         XCTAssertEqual(wcpayCharge.siteID, dummySiteID)
     }
 
-    /// Verifies that the fields are all parsed correctly
+    /// Verifies that the fields are all parsed correctly for a card present payment
     ///
-    func test_WCPayCharge_map_parses_all_fields_in_result() throws {
-        let wcpayCharge = try mapRetrieveWCPayChargeResponse()
+    func test_WCPayCharge_map_parses_all_fields_in_result_for_card_present() throws {
+        let wcpayCharge = try mapRetrieveWCPayChargeResponse(responseName: .cardPresent)
 
         let expectedCreatedDate = Date.init(timeIntervalSince1970: 1643280767) //2022-01-27 10:52:47 UTC
 
@@ -52,6 +52,34 @@ class WCPayChargeMapperTests: XCTestCase {
                                               status: .succeeded)
         XCTAssertEqual(wcpayCharge, expectedWcpayCharge)
     }
+
+    /// Verifies that the fields are all parsed correctly for a card payment
+    ///
+    func test_WCPayCharge_map_parses_all_fields_in_result_for_card() throws {
+        let wcpayCharge = try mapRetrieveWCPayChargeResponse(responseName: .card)
+
+        let expectedCreatedDate = Date.init(timeIntervalSince1970: 1643378348) //2022-01-28 13:59:08 UTC
+
+        let expectedPaymentMethodDetails = WCPayPaymentMethodDetails.card(
+            details: .init(brand: .amex, last4: "1111", funding: .credit))
+
+        let expectedWcpayCharge = WCPayCharge(siteID: dummySiteID,
+                                              id: "ch_3KMuym2EdyGr1FMV0uQZeFqm",
+                                              amount: 3330,
+                                              amountCaptured: 3330,
+                                              amountRefunded: 0,
+                                              authorizationCode: nil,
+                                              captured: true,
+                                              created: expectedCreatedDate,
+                                              currency: "usd",
+                                              paid: true,
+                                              paymentIntentID: "pi_3KMuym2EdyGr1FMV0TtJrfac",
+                                              paymentMethodID: "pm_1KMuyk2EdyGr1FMVYAz48aiQ",
+                                              paymentMethodDetails: expectedPaymentMethodDetails,
+                                              refunded: false,
+                                              status: .succeeded)
+        XCTAssertEqual(wcpayCharge, expectedWcpayCharge)
+    }
 }
 
 
@@ -71,9 +99,14 @@ private extension WCPayChargeMapperTests {
 
     /// Returns the CouponMapper output from `coupon.json`
     ///
-    func mapRetrieveWCPayChargeResponse() throws -> WCPayCharge {
-        return try mapWCPayCharge(from: "wcpay-charge")
+    func mapRetrieveWCPayChargeResponse(responseName: ChargeResponse = .cardPresent) throws -> WCPayCharge {
+        return try mapWCPayCharge(from: responseName.rawValue)
     }
 
     struct FileNotFoundError: Error {}
+
+    enum ChargeResponse: String {
+        case cardPresent = "wcpay-charge-card-present"
+        case card = "wcpay-charge-card"
+    }
 }

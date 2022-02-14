054ca52b7b56d029165b7b09d33a3112f76f10cd
Josh Heald <joshheald@users.noreply.github.com>
2022-02-14 16:44:30 +0000

    Revert "5976 Make receipt non-optional"
    This reverts commit 21b631e532427588b3058c3e9109095d56aaf8ad.

--- Storage/Storage/Model/WCPayCardPresentPaymentDetails+CoreDataProperties.swift
+++ Storage/Storage/Model/WCPayCardPresentPaymentDetails+CoreDataProperties.swift
@@ -11,7 +11,7 @@ extension WCPayCardPresentPaymentDetails {
     @NSManaged public var brand: String
     @NSManaged public var last4: String
     @NSManaged public var funding: String
-    @NSManaged public var receipt: WCPayCardPresentReceiptDetails
+    @NSManaged public var receipt: WCPayCardPresentReceiptDetails?
     @NSManaged public var charge: WCPayCharge?
 
 }

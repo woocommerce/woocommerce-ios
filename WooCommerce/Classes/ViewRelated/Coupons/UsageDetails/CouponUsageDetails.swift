6ae36420c45f92fd8db120a0fe07f84dbf25b4c6
Huong Do <hi.huongdo@gmail.com>
2022-02-15 16:19:32 +0700

    Add usage restrictions section to usage details screen

--- WooCommerce/Classes/ViewRelated/Coupons/UsageDetails/CouponUsageDetails.swift
+++ WooCommerce/Classes/ViewRelated/Coupons/UsageDetails/CouponUsageDetails.swift
@@ -2,20 +2,65 @@ import SwiftUI
 
 struct CouponUsageDetails: View {
 
-    @ObservedObject private var viewModel: CouponDetailsViewModel
+    @ObservedObject private var viewModel: CouponUsageDetailsViewModel
 
-    init(viewModel: CouponDetailsViewModel) {
+    init(viewModel: CouponUsageDetailsViewModel) {
         self.viewModel = viewModel
     }
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(alignment: .leading, spacing: 0) {
+                    ListHeaderView(text: Localization.usageRestriction, alignment: .left)
+                    VStack(alignment: .leading, spacing: 0) {
+                        Divider()
+                        TitleAndValueRow(title: String.localizedStringWithFormat(Localization.minimumSpend, viewModel.currencySymbol),
+                                         value: .content(viewModel.minimumSpend),
+                                         selectable: false,
+                                         action: {})
+                            .padding(.horizontal, Constants.margin)
+                            .padding(.horizontal, insets: geometry.safeAreaInsets)
+                        Divider()
+                            .padding(.leading, Constants.margin)
+                            .padding(.leading, insets: geometry.safeAreaInsets)
+                        TitleAndValueRow(title: String.localizedStringWithFormat(Localization.maximumSpend, viewModel.currencySymbol),
+                                         value: .content(viewModel.maximumSpend),
+                                         selectable: false,
+                                         action: {})
+                            .padding(.horizontal, Constants.margin)
+                            .padding(.horizontal, insets: geometry.safeAreaInsets)
+                        Divider()
+                            .padding(.leading, Constants.margin)
+                            .padding(.leading, insets: geometry.safeAreaInsets)
+                        TitleAndValueRow(title: Localization.usageLimitPerCoupon,
+                                         value: .content(viewModel.usageLimitPerCoupon),
+                                         selectable: false,
+                                         action: {})
+                            .padding(.horizontal, Constants.margin)
+                            .padding(.horizontal, insets: geometry.safeAreaInsets)
+                        Divider()
+                    }
+                    .background(Color(.listForeground))
+                }
+            }
+            .background(Color(.listBackground))
+            .ignoresSafeArea(.container, edges: [.horizontal, .bottom])
+        }
     }
 }
 
 private extension CouponUsageDetails {
+    enum Constants {
+        static let margin: CGFloat = 16
+        static let verticalSpacing: CGFloat = 8
+    }
+
     enum Localization {
-        static let usageRestriction = NSLocalizedString("Usage Restrictions", comment: "Title for the usage restrictions section on coupon usage details screen")
+        static let usageRestriction = NSLocalizedString(
+            "Usage Restrictions",
+            comment: "Title for the usage restrictions section on coupon usage details screen"
+        )
         static let minimumSpend = NSLocalizedString(
             "Minimum Spend (%1$@)",
             comment: "Title for the minimum spend row on coupon usage details screen with currency symbol within the brackets. " +
@@ -54,6 +99,9 @@ private extension CouponUsageDetails {
 struct CouponUsageDetails_Previews: PreviewProvider {
     static var previews: some View {
         CouponUsageDetails(viewModel: .init(coupon: .sampleCoupon))
+
+        CouponUsageDetails(viewModel: .init(coupon: .sampleCoupon))
+            .previewLayout(.fixed(width: 715, height: 320))
     }
 }
 #endif

b250a48e851204a25dbb82e3cd62a4994fa8a53d
Ernesto Carrion <ernesto.carrion@automattic.com>
2022-02-16 14:31:57 -0500

    Adds helper to define which order status a new order should begint with

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/Synchronizer/NewOrderInitialStatusResolver.swift
@@ -0,0 +1,42 @@
+import Yosemite
+
+/// Helper that defines which `status` a new order should initially have.
+///
+struct NewOrderInitialStatusResolver {
+
+    /// Current site ID
+    ///
+    private let siteID: Int64
+
+    /// Stores.
+    ///
+    private let stores: StoresManager
+
+    /// Defines the WC version where `auto-draft` should be available.
+    ///
+    private let draftMinSupportedVersion = "6.3.0"
+
+    /// WooCommerce plugin name.
+    ///
+    private let wcPluginName = "WooCommerce"
+
+
+    /// Decides the initial `status` for a new order based on the current store version.
+    ///
+    func resolve(onCompletion: @escaping (OrderStatusEnum) -> ()) {
+        let action = SystemStatusAction.fetchSystemPlugin(siteID: siteID, systemPluginName: wcPluginName) { wooPlugin in
+            guard let wooPlugin = wooPlugin else {
+                return onCompletion(.pending)
+            }
+
+            // auto-draft should exists in versions greater than `6.3.0`
+            switch draftMinSupportedVersion.compare(wooPlugin.version, options: .numeric) {
+            case .orderedAscending, .orderedSame:
+                onCompletion(.autoDraft)
+            case .orderedDescending:
+                onCompletion(.pending)
+            }
+        }
+        ServiceLocator.stores.dispatch(action)
+    }
+}

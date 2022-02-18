fcd6bc7084b8b849466caa933c6aff5de37c3eb1
Ernesto Carrion <ernesto.carrion@automattic.com>
2022-02-17 22:54:31 -0500

    Adds unit tests

--- WooCommerce/WooCommerceTests/ViewRelated/Orders/Order Creation/Synchronizer/RemoteOrderSynchronizerTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Orders/Order Creation/Synchronizer/RemoteOrderSynchronizerTests.swift
@@ -1,6 +1,7 @@
 import XCTest
 import TestKit
 import Fakes
+import Combine
 
 @testable import WooCommerce
 @testable import Yosemite
@@ -11,6 +12,12 @@ class RemoteOrderSynchronizerTests: XCTestCase {
     private let sampleProductID: Int64 = 234
     private let sampleInputID: Int64 = 345
     private let sampleShippingID: Int64 = 456
+    private var subscriptions = Set<AnyCancellable>()
+
+    override func setUp() {
+        super.setUp()
+        subscriptions.removeAll()
+    }
 
     func test_sending_status_input_updates_local_order() throws {
         // Given
@@ -133,5 +140,211 @@ class RemoteOrderSynchronizerTests: XCTestCase {
 
         // Then
         XCTAssertEqual(synchronizer.order.shippingLines, [])
+    }
+
+    func test_sending_product_input_triggers_order_creation() {
+        // Given
+        let product = Product.fake().copy(productID: sampleProductID)
+        let stores = MockStoresManager(sessionManager: .testingInstance)
+        let synchronizer = RemoteOrderSynchronizer(siteID: sampleSiteID, stores: stores)
+
+        // When
+        let orderCreationInvoked: Bool = waitFor { promise in
+            stores.whenReceivingAction(ofType: OrderAction.self) { action in
+                switch action {
+                case .createOrder:
+                    promise(true)
+                default:
+                    promise(false)
+                }
+            }
+
+            let input = OrderSyncProductInput(product: .product(product), quantity: 1)
+            synchronizer.setProduct.send(input)
+        }
+
+        // Then
+        XCTAssertTrue(orderCreationInvoked)
+    }
+
+    func test_sending_addresses_input_triggers_order_creation() {
+        // Given
+        let address = Address.fake().copy(firstName: "Woo", lastName: "Customer")
+        let stores = MockStoresManager(sessionManager: .testingInstance)
+        let synchronizer = RemoteOrderSynchronizer(siteID: sampleSiteID, stores: stores)
+
+        // When
+        let orderCreationInvoked: Bool = waitFor { promise in
+            stores.whenReceivingAction(ofType: OrderAction.self) { action in
+                switch action {
+                case .createOrder:
+                    promise(true)
+                default:
+                    promise(false)
+                }
+            }
+
+            let input = OrderSyncAddressesInput(billing: address, shipping: address)
+            synchronizer.setAddresses.send(input)
+        }
+
+        // Then
+        XCTAssertTrue(orderCreationInvoked)
+    }
+
+    func test_sending_shipping_input_triggers_order_creation() {
+        // Given
+        let shippingLine = ShippingLine.fake().copy(shippingID: sampleShippingID)
+        let stores = MockStoresManager(sessionManager: .testingInstance)
+        let synchronizer = RemoteOrderSynchronizer(siteID: sampleSiteID, stores: stores)
+
+        // When
+        let orderCreationInvoked: Bool = waitFor { promise in
+            stores.whenReceivingAction(ofType: OrderAction.self) { action in
+                switch action {
+                case .createOrder:
+                    promise(true)
+                default:
+                    promise(false)
+                }
+            }
+
+            synchronizer.setShipping.send(shippingLine)
+        }
+
+        // Then
+        XCTAssertTrue(orderCreationInvoked)
+    }
+
+    func test_sending_fee_input_triggers_order_creation() {
+        // Given
+        let fee = OrderFeeLine.fake().copy()
+        let stores = MockStoresManager(sessionManager: .testingInstance)
+        let synchronizer = RemoteOrderSynchronizer(siteID: sampleSiteID, stores: stores)
+
+        // When
+        let orderCreationInvoked: Bool = waitFor { promise in
+            stores.whenReceivingAction(ofType: OrderAction.self) { action in
+                switch action {
+                case .createOrder:
+                    promise(true)
+                default:
+                    promise(false)
+                }
+            }
+
+            synchronizer.setFee.send(fee)
+        }
+
+        // Then
+        XCTAssertTrue(orderCreationInvoked)
+    }
+
+    func test_states_are_properly_set_upon_success_order_creation() {
+        // Given
+        let product = Product.fake().copy(productID: sampleProductID)
+        let stores = MockStoresManager(sessionManager: .testingInstance)
+        let synchronizer = RemoteOrderSynchronizer(siteID: sampleSiteID, stores: stores)
+        stores.whenReceivingAction(ofType: OrderAction.self) { action in
+            switch action {
+            case .createOrder(_, _, let completion):
+                completion(.success(.fake()))
+            default:
+                XCTFail("Unexpected action: \(action)")
+            }
+        }
+
+        // When
+        let input = OrderSyncProductInput(product: .product(product), quantity: 1)
+        synchronizer.setProduct.send(input)
+
+        let states: [OrderSyncState] = waitFor { promise in
+            synchronizer.statePublisher
+                .dropFirst()
+                .collect(2)
+                .sink { states in
+                    promise(states)
+                }
+                .store(in: &self.subscriptions)
+        }
+
+        // Then
+        XCTAssertEqual(states, [.syncing, .synced])
+    }
+
+    func test_states_are_properly_set_upon_failing_order_creation() {
+        // Given
+        let product = Product.fake().copy(productID: sampleProductID)
+        let error = NSError(domain: "", code: 0, userInfo: nil)
+        let stores = MockStoresManager(sessionManager: .testingInstance)
+        let synchronizer = RemoteOrderSynchronizer(siteID: sampleSiteID, stores: stores)
+        stores.whenReceivingAction(ofType: OrderAction.self) { action in
+            switch action {
+            case .createOrder(_, _, let completion):
+                completion(.failure(error))
+            default:
+                XCTFail("Unexpected action: \(action)")
+            }
+        }
+
+        // When
+        let input = OrderSyncProductInput(product: .product(product), quantity: 1)
+        synchronizer.setProduct.send(input)
+
+        let states: [OrderSyncState] = waitFor { promise in
+            synchronizer.statePublisher
+                .dropFirst()
+                .collect(2)
+                .sink { states in
+                    promise(states)
+                }
+                .store(in: &self.subscriptions)
+        }
+
+        // Then
+        assertEqual(states, [.syncing, .error(error)])
+    }
+
+    func test_sending_double_input_triggers_only_one_order_creation() {
+        // Given
+        let product = Product.fake().copy(productID: sampleProductID)
+        let stores = MockStoresManager(sessionManager: .testingInstance)
+        let synchronizer = RemoteOrderSynchronizer(siteID: sampleSiteID, stores: stores)
+
+        // When
+        let exp = expectation(description: #function)
+        exp.expectedFulfillmentCount = 1
+        exp.assertForOverFulfill = true
+
+        stores.whenReceivingAction(ofType: OrderAction.self) { action in
+            switch action {
+            case .createOrder:
+                exp.fulfill()
+            default:
+                break
+            }
+        }
+
+        let input1 = OrderSyncProductInput(product: .product(product), quantity: 1)
+        synchronizer.setProduct.send(input1)
+
+        let input2 = OrderSyncProductInput(product: .product(product), quantity: 2)
+        synchronizer.setProduct.send(input2)
+
+        // Then
+        wait(for: [exp], timeout: 1.0)
+    }
+}
+
+extension OrderSyncState: Equatable {
+    public static func == (lhs: OrderSyncState, rhs: OrderSyncState) -> Bool {
+        switch (lhs, rhs) {
+        case (.syncing, .syncing), (.synced, .synced):
+            return true
+        case (.error(let error1), .error(let error2)):
+            return error1 as NSError == error2 as NSError
+        default:
+            return false
+        }
     }
 }

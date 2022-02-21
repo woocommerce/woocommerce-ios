7db6908aab8e6c3dd5efd0dec426c22e23a74f87
Christos Koninis <christos.koninis@gmail.com>
2022-02-21 09:36:38 +0200

    Updated BulkUpdateFormDataModel to remove unused code

--- WooCommerce/Classes/ViewRelated/Products/Variations/Bulk Update/BulkUpdateViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Variations/Bulk Update/BulkUpdateViewModel.swift
@@ -28,17 +28,6 @@ final class BulkUpdateViewModel {
     /// Called when syncing state changes
     var observeSyncState: ((SyncState) -> Void)?
 
-    /// Product Variations Controller.
-    ///
-    private lazy var resultsController: ResultsController<StorageProductVariation> = {
-        let predicate = NSPredicate(format: "siteID == %lld", siteID)
-        let resultsController = ResultsController<StorageProductVariation>(storageManager: storageManager, matching: predicate, sortedBy: [])
-
-        return resultsController
-    }()
-
-    private var bulkUpdateFormDataModel: BulkUpdateFormDataModel?
-
     init(siteID: Int64,
          productID: Int64,
          storageManager: StorageManagerType = ServiceLocator.storageManager,
@@ -53,7 +42,7 @@ final class BulkUpdateViewModel {
     /// This is the main activation method for this view model.
     ///
     /// This should only be called when the corresponding view was loaded.
-    /// 
+    ///
     func activate() {
         synchAllProductVariations()
     }
@@ -76,29 +65,12 @@ final class BulkUpdateViewModel {
                     self.syncState = .syncingError
 
                     DDLogError("⛔️ Error synchronizing product variations: \(error)")
+                } else {
+                    self.syncState = .syncedResults
                 }
-                self.configureResultsController()
             }
 
         storesManager.dispatch(action)
-    }
-
-    /// Setup: Results Controller
-    ///
-    private func configureResultsController() {
-        resultsController.onDidChangeContent = { [weak self] in
-            guard let self = self else {
-                return
-            }
-            self.bulkUpdateFormDataModel = BulkUpdateFormDataModel(productVariations: self.resultsController.fetchedObjects)
-            self.syncState = .syncedResults
-        }
-
-        do {
-            try resultsController.performFetch()
-        } catch {
-            DDLogError("⛔️ Error fetching product variations: \(error)")
-        }
     }
 }
 
@@ -113,52 +85,3 @@ extension BulkUpdateViewModel {
         static let numberOfObjects = 100
     }
 }
-
-// Holds an array of product variations and provides caggregate data to for the bulk update form
-///
-final class BulkUpdateFormDataModel {
-
-    /// The aggregate of a single value over all product variations
-    ///
-    enum BulkValue<T> {
-        /// All variations have the same value
-        case value(T)
-        /// Variations have mixed values
-        case mixed
-//        case mixed(min: T, max: T)
-        /// None of the variation has a value
-        case none
-    }
-
-    let productVariations: [ProductVariation]
-
-    init(productVariations: [ProductVariation]) {
-        self.productVariations = productVariations
-    }
-
-    func regularPrice() -> BulkValue<String> {
-        let results = productVariations.map(\.regularPrice).compactMap({ $0 }).uniqued()
-
-        if let samePrice = results.first {
-            return BulkValue.value(samePrice)
-        } else if results.count != 1 {
-            return BulkValue.mixed
-        } else {
-            return BulkValue.none
-        }
-    }
-
-    func salePrice() -> BulkValue<String> {
-        let results = productVariations.map(\.salePrice).compactMap({ $0 }).uniqued()
-
-        if let samePrice = results.first {
-            return BulkValue.value(samePrice)
-        } else if results.count != 1 {
-            return BulkValue.mixed
-        } else {
-            return BulkValue.none
-        }
-    }
-}
-
-

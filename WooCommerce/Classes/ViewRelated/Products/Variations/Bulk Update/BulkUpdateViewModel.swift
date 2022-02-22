391ca593770f9d54f31728bbfc117d76d02e116e
Christos Koninis <christos.koninis@gmail.com>
2022-02-22 07:53:56 +0200

    Replaced closure with combine publisher to notify for state events

--- WooCommerce/Classes/ViewRelated/Products/Variations/Bulk Update/BulkUpdateViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Variations/Bulk Update/BulkUpdateViewModel.swift
@@ -19,14 +19,7 @@ final class BulkUpdateViewModel {
     private let productID: Int64
 
     /// The state of synching all variations
-    private(set) var syncState: SyncState {
-        didSet {
-            observeSyncState?(syncState)
-        }
-    }
-
-    /// Called when syncing state changes
-    var observeSyncState: ((SyncState) -> Void)?
+    @Published private(set) var syncState: SyncState 
 
     init(siteID: Int64,
          productID: Int64,

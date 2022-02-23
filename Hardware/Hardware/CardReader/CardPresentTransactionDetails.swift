1cc4e57e19e58711eb5f994e687be1f2e9cedca6
Josh Heald <joshheald@users.noreply.github.com>
2022-02-23 14:12:38 +0000

    Add interac-only test code

--- Hardware/Hardware/CardReader/CardPresentTransactionDetails.swift
+++ Hardware/Hardware/CardReader/CardPresentTransactionDetails.swift
@@ -48,6 +48,32 @@ public struct CardPresentTransactionDetails: Codable, Equatable {
         self.receipt = receipt
         self.emvAuthData = emvAuthData
     }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let last4 = try container.decode(String.self, forKey: .last4)
+        let expMonth = try container.decode(Int.self, forKey: .expMonth)
+        let expYear = try container.decode(Int.self, forKey: .expYear)
+        let cardholderName = try container.decodeIfPresent(String.self, forKey: .cardholderName)
+
+        #warning("Don't merge this or everything will be Interac on our IPP receipts!")
+        let brand = CardBrand.interac
+
+        let fingerprint = try container.decode(String.self, forKey: .fingerprint)
+        let generatedCard = try container.decodeIfPresent(String.self, forKey: .generatedCard)
+        let receipt = try container.decodeIfPresent(ReceiptDetails.self, forKey: .receipt)
+        let emvAuthData = try container.decodeIfPresent(String.self, forKey: .emvAuthData)
+        self.init(last4: last4,
+                  expMonth: expMonth,
+                  expYear: expYear,
+                  cardholderName: cardholderName,
+                  brand: brand,
+                  fingerprint: fingerprint,
+                  generatedCard: generatedCard,
+                  receipt: receipt,
+                  emvAuthData: emvAuthData)
+    }
 }
 
 extension CardPresentTransactionDetails {

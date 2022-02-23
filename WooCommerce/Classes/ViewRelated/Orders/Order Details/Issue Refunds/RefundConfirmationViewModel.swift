18fdc37a7a4ee4ce6440422873b508a56bd06a50
Josh Heald <joshheald@users.noreply.github.com>
2022-02-23 12:47:20 +0000

    6265 Remove US-specific voiceover for card brand

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/RefundConfirmationViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Issue Refunds/RefundConfirmationViewModel.swift
@@ -318,15 +318,11 @@ private extension WCPayCardBrand {
         let localizedDescription = String(format: Localization.cardAccessibilityDescriptionFormat, cardBrandName(), last4)
         let attributedLocalizedDescription = NSMutableAttributedString(string: localizedDescription)
 
-        guard let brandNameRange = localizedDescription.range(of: cardBrandName()),
-              let last4Range = localizedDescription.range(of: last4)
-        else {
+        guard let last4Range = localizedDescription.range(of: last4) else {
             return attributedLocalizedDescription
         }
 
-        let brandNameNSRange = NSRange(brandNameRange, in: localizedDescription)
         let last4NSRange = NSRange(last4Range, in: localizedDescription)
-        attributedLocalizedDescription.setAttributes([.accessibilitySpeechLanguage: "en-US"], range: brandNameNSRange)
         attributedLocalizedDescription.setAttributes([.accessibilitySpeechSpellOut: true], range: last4NSRange)
 
         return attributedLocalizedDescription

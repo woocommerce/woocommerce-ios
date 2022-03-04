9649ac983fdc0844caf2d99e6d1d2d37e82647bb
Huong Do <hi.huongdo@gmail.com>
2022-03-04 10:20:20 +0700

    Update unit tests for ReviewsViewModel

--- WooCommerce/WooCommerceTests/Reviews/ReviewsViewModelTests.swift
+++ WooCommerce/WooCommerceTests/Reviews/ReviewsViewModelTests.swift
@@ -4,86 +4,113 @@ import XCTest
 @testable import Yosemite
 
 final class ReviewsViewModelTests: XCTestCase {
-    private var mockDataSource: ReviewsDataSource!
-    private var viewModel: ReviewsViewModel!
 
-    override func setUp() {
-        super.setUp()
-        mockDataSource = MockReviewsDataSource()
-        viewModel = ReviewsViewModel(siteID: 1, data: mockDataSource)
-    }
-
-    override func tearDown() {
-        viewModel = nil
-        mockDataSource = nil
-        super.tearDown()
-    }
+    private let sampleSiteID: Int64 = 1334
 
     func testDataSourceReturnsInjectedReviewsDataSource() {
+        // Given
+        let mockDataSource = MockReviewsDataSource()
+        let viewModel = ReviewsViewModel(siteID: sampleSiteID, data: mockDataSource)
+
+        // When
         let dataSource = viewModel.dataSource
+
+        // Then
         XCTAssertNotNil(dataSource as? MockReviewsDataSource)
     }
 
     func testDelegateReturnsInjectedReviewsDelegate() {
+        // Given
+        let mockDataSource = MockReviewsDataSource()
+        let viewModel = ReviewsViewModel(siteID: sampleSiteID, data: mockDataSource)
+
+        // When
         let delegate = viewModel.delegate
+
+        // Then
         XCTAssertNotNil(delegate as? MockReviewsDataSource)
     }
 
     func testIsEmptyReturnsTheSameAsTheDataSource() {
+        // Given
+        let mockDataSource = MockReviewsDataSource()
+        let viewModel = ReviewsViewModel(siteID: sampleSiteID, data: mockDataSource)
+
+        // Then
         XCTAssertEqual(viewModel.isEmpty, mockDataSource.isEmpty)
     }
 
     func testDisplayPlaceHolderReviewsStopsForWardingEventsInDataSource() {
+        // Given
         let table = UITableView()
-        let ds = mockDataSource as! MockReviewsDataSource
-
+        let mockDataSource = MockReviewsDataSource()
+        let viewModel = ReviewsViewModel(siteID: sampleSiteID, data: mockDataSource)
         viewModel.displayPlaceholderReviews(tableView: table)
 
-        XCTAssertTrue(ds.stopsForwardingEventsWasHit)
+        // Then
+        XCTAssertTrue(mockDataSource.stopsForwardingEventsWasHit)
     }
 
     func testRemovePlaceHolderReviewsStartsForWardingEventsInDataSource() {
+        // Given
         let table = UITableView()
-        let ds = mockDataSource as! MockReviewsDataSource
+        let mockDataSource = MockReviewsDataSource()
+        let viewModel = ReviewsViewModel(siteID: sampleSiteID, data: mockDataSource)
 
+        // When
         viewModel.removePlaceholderReviews(tableView: table)
 
-        XCTAssertTrue(ds.startForwardingEventsWasHit)
+        // Then
+        XCTAssertTrue(mockDataSource.startForwardingEventsWasHit)
     }
 
     func testConfigureResultsControllerStartsForWardingEventsAndStartsObservingReviewsInDataSource() {
+        // Given
         let table = UITableView()
-        let ds = mockDataSource as! MockReviewsDataSource
+        let mockDataSource = MockReviewsDataSource()
+        let viewModel = ReviewsViewModel(siteID: sampleSiteID, data: mockDataSource)
 
+        // When
         viewModel.configureResultsController(tableView: table)
 
-        XCTAssertTrue(ds.startForwardingEventsWasHit && ds.startObservingWasHit)
+        // Then
+        XCTAssertTrue(mockDataSource.startForwardingEventsWasHit && mockDataSource.startObservingWasHit)
     }
 
     func testSyncDataHitsExpectedReviewsAndProductsActions() {
+        // Given
         let storesManager = MockReviewsStoresManager()
-        ServiceLocator.setStores(storesManager)
+        let mockDataSource = MockReviewsDataSource()
+        let viewModel = ReviewsViewModel(siteID: sampleSiteID, data: mockDataSource, stores: storesManager)
 
-        let expec = expectation(description: "Wait for synchronizeReviews to complete")
-
+        // Then
+        let expectation = expectation(description: "Wait for synchronizeReviews to complete")
         viewModel.synchronizeReviews(pageNumber: 1, pageSize: 25) {
             let allTargetsHit = storesManager.syncReviewsIsHit && storesManager.retrieveProductsIsHit
             XCTAssertTrue(allTargetsHit)
             if allTargetsHit {
-                expec.fulfill()
+                expectation.fulfill()
             } else {
                 XCTFail()
             }
         }
-
         waitForExpectations(timeout: 10, handler: nil)
     }
 
     func test_hasErrorLoadingData_false_after_successful_sync() {
         // Given
+        let mockDataSource = MockReviewsDataSource()
+        let storesManager = MockStoresManager(sessionManager: .testingInstance)
+        storesManager.whenReceivingAction(ofType: ProductReviewAction.self) { action in
+            switch action {
+            case .synchronizeProductReviews(_, _, _, _, _, let onCompletion):
+                onCompletion(nil)
+            default:
+                return
+            }
+        }
+        let viewModel = ReviewsViewModel(siteID: sampleSiteID, data: mockDataSource, stores: storesManager)
         viewModel.hasErrorLoadingData = true
-        let storesManager = MockReviewsStoresManager()
-        ServiceLocator.setStores(storesManager)
 
         // When
         viewModel.synchronizeReviews(pageNumber: 1, pageSize: 25, onCompletion: nil)
@@ -94,7 +121,7 @@ final class ReviewsViewModelTests: XCTestCase {
 
     func test_hasErrorLoadingData_true_after_failed_sync() {
         // Given
-        viewModel.hasErrorLoadingData = false
+        let mockDataSource = MockReviewsDataSource()
         let storesManager = MockStoresManager(sessionManager: .testingInstance)
         storesManager.whenReceivingAction(ofType: ProductReviewAction.self) { action in
             switch action {
@@ -104,13 +131,70 @@ final class ReviewsViewModelTests: XCTestCase {
                 return
             }
         }
-        ServiceLocator.setStores(storesManager)
+        let viewModel = ReviewsViewModel(siteID: sampleSiteID, data: mockDataSource, stores: storesManager)
+        viewModel.hasErrorLoadingData = false
 
         // When
         viewModel.synchronizeReviews(pageNumber: 1, pageSize: 25, onCompletion: nil)
 
         // Then
         XCTAssertTrue(viewModel.hasErrorLoadingData)
+    }
+
+    func test_synchronizeReviews_triggers_retrieveProducts_with_all_reviewsProductIDs() {
+        // Given
+        let mockDataSource = MockReviewsDataSource()
+        let mocks = MockReviews()
+        let sampleReviews: [ProductReview] = [
+            mocks.review(injectedReviewID: sampleSiteID, injectedProductID: 55),
+            mocks.review(injectedReviewID: sampleSiteID, injectedProductID: 668),
+            mocks.review(injectedReviewID: sampleSiteID, injectedProductID: 789)
+        ]
+
+        let mockStores = MockStoresManager(sessionManager: .makeForTesting())
+        mockStores.whenReceivingAction(ofType: ProductReviewAction.self) { action in
+            switch action {
+            case let .synchronizeProductReviews(_, _, _, _, _, onCompletion):
+                // simulate the delay in persisting reviews locally
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    onCompletion(nil)
+                    mockDataSource.reviews = sampleReviews
+                }
+            default:
+                break
+            }
+        }
+
+        mockStores.whenReceivingAction(ofType: NotificationAction.self) { action in
+            switch action {
+            case .synchronizeNotifications(let onCompletion):
+                onCompletion(nil)
+            default:
+                break
+            }
+        }
+
+        var retrievedProductIDs: [Int64] = []
+        mockStores.whenReceivingAction(ofType: ProductAction.self) { action in
+            switch action {
+            case let .retrieveProducts(_, productIDs, _, _, onCompletion):
+                retrievedProductIDs = productIDs
+                onCompletion(.success((products: [], hasNextPage: false)))
+            default:
+                break
+            }
+        }
+        let viewModel = ReviewsViewModel(siteID: sampleSiteID, data: mockDataSource, stores: mockStores)
+
+        // When
+        let expectation = expectation(description: "Wait for synchronizeReviews to complete")
+        viewModel.synchronizeReviews(pageNumber: 1, pageSize: 25) {
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+
+        // Then
+        XCTAssertEqual(retrievedProductIDs, [55, 668, 789])
     }
 }
 
@@ -119,11 +203,7 @@ final class ReviewsViewModelTests: XCTestCase {
 
 final class MockReviewsDataSource: NSObject, ReviewsDataSource {
 
-    private lazy var reviews: [ProductReview] = {
-        let mocks = MockReviews()
-        let mockReview = mocks.review()
-        return [mockReview, mockReview]
-    }()
+    var reviews: [ProductReview] = []
 
     var isEmpty: Bool {
         return reviews.isEmpty

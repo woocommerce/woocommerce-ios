715595b892f72ff09da51b380f117388b6fc09fd
Huong Do <hi.huongdo@gmail.com>
2022-03-15 17:25:09 +0700

    Add new endpoints for site setting remote

--- Networking/Networking/Remote/SiteSettingsRemote.swift
+++ Networking/Networking/Remote/SiteSettingsRemote.swift
@@ -46,6 +46,36 @@ public class SiteSettingsRemote: Remote {
 
         enqueue(request, mapper: mapper, completion: completion)
     }
+
+    /// Retrieve setting for whether coupons are enabled for a given site
+    ///
+    /// - Parameters:
+    ///   - siteID: Site for which we'll fetch the coupon setting.
+    ///   - completion: Closure to be executed upon completion.
+    ///
+    public func loadCouponSetting(for siteID: Int64, completion: @escaping (Result<SiteSetting, Error>) -> Void) {
+        let path = Constants.siteSettingsPath + Constants.generalSettingsGroup + "/" + Constants.couponEnabledSettingID
+        let request = JetpackRequest(wooApiVersion: .mark3, method: .get, siteID: siteID, path: path, parameters: nil)
+        let mapper = SiteSettingMapper(siteID: siteID, settingsGroup: SiteSettingGroup.general)
+
+        enqueue(request, mapper: mapper, completion: completion)
+    }
+
+    /// Update setting for coupon for a given site
+    ///
+    /// - Parameters:
+    ///   - siteID: Site for which we'll update the coupon setting.
+    ///   - value: New value for the setting.
+    ///   - completion: Closure to be executed upon completion.
+    ///
+    public func updateCouponSetting(for siteID: Int64, value: String, completion: @escaping (Result<SiteSetting, Error>) -> Void) {
+        let parameters: [String: Any] = [Constants.valueParameter: value]
+        let path = Constants.siteSettingsPath + Constants.generalSettingsGroup + "/" + Constants.couponEnabledSettingID
+        let request = JetpackRequest(wooApiVersion: .mark3, method: .put, siteID: siteID, path: path, parameters: parameters)
+        let mapper = SiteSettingMapper(siteID: siteID, settingsGroup: SiteSettingGroup.general)
+
+        enqueue(request, mapper: mapper, completion: completion)
+    }
 }
 
 
@@ -57,5 +87,7 @@ private extension SiteSettingsRemote {
         static let generalSettingsGroup: String   = "general"
         static let productSettingsGroup: String   = "products"
         static let advancedSettingsGroup: String   = "advanced"
+        static let couponEnabledSettingID: String = "woocommerce_enable_coupons"
+        static let valueParameter: String = "value"
     }
 }

c1e1d25b4b34bfe0ab2212d09afefe94e74e8694
Huong Do <hi.huongdo@gmail.com>
2022-03-28 11:50:43 +0700

    Add status view

---
+++ WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/StatusView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+/// A view to display a status label accompanied with a background color
+///
+struct StatusView: View {
+    /// The label on the status view
+    private let label: String
+
+    /// The background color of the view
+    private let backgroundColor: UIColor
+
+    init(label: String, backgroundColor: UIColor) {
+        self.label = label
+        self.backgroundColor = backgroundColor
+    }
+
+    var body: some View {
+        Text(label)
+            .font(.caption)
+            .padding(.horizontal, Constants.horizontalMargin)
+            .background(Color(backgroundColor))
+            .cornerRadius(Constants.cornerRadius)
+    }
+}
+
+private extension StatusView {
+    enum Constants {
+        static let horizontalMargin: CGFloat = 4
+        static let cornerRadius: CGFloat = 2
+    }
+}
+
+struct StatusView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatusView(label: "Active", backgroundColor: .withColorStudio(.celadon, shade: .shade5))
+    }
+}

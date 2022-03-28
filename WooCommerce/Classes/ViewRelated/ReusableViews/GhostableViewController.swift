82281da2dad8a8c266467f44ef18484f6a1bd38f
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2022-03-28 16:00:18 +0200

    Create GhostableViewController and move logic to it

---
+++ WooCommerce/Classes/ViewRelated/ReusableViews/GhostableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  GhostableViewController.swift
+//  WooCommerce
+//
+//  Created by César Vargas Casaseca on 28/3/22.
+//  Copyright © 2022 Automattic. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Make your `UIViewController` instance implement this protocol if you want to display/hide a ghost animation on top of it
+protocol GhostableViewController: UIViewController {
+    /// The `GhostTableViewController` to be displayed on top of the view. Configure as wished.
+    var ghostTableViewController: GhostTableViewController { get }
+}
+
+extension GhostableViewController {
+    /// Displays the animated ghost view by adding the `GhostTableViewController` as child
+    func displayGhostContent() {
+        guard let ghostView = ghostTableViewController.view else {
+            return
+        }
+
+        ghostView.translatesAutoresizingMaskIntoConstraints = false
+        addChild(ghostTableViewController)
+        view.addSubview(ghostView)
+        view.pinSubviewToAllEdges(ghostView)
+        ghostTableViewController.didMove(toParent: self)
+    }
+
+    /// Removes the animated ghost
+    func removeGhostContent() {
+        guard let ghostView = ghostTableViewController.view else {
+            return
+        }
+
+        ghostTableViewController.willMove(toParent: nil)
+        ghostView.removeFromSuperview()
+        ghostTableViewController.removeFromParent()
+    }
+}

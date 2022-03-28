2a7c7689f3536c98d14d6078317d084efb68a426
Paolo Musolino <info@codeido.com>
2022-03-28 16:54:37 +0200

    feat: AddEditCouponViewModel tests

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Coupons/AddEditCouponViewModelTests.swift
@@ -0,0 +1,34 @@
+import XCTest
+@testable import Yosemite
+@testable import WooCommerce
+
+final class AddEditCouponViewModelTests: XCTestCase {
+
+    func test_titleView_property_return_expected_values_on_creation_depending_on_discountType() {
+        let viewModel1 = AddEditCouponViewModel(siteID: 123, discountType: .percent)
+        XCTAssertEqual(viewModel1.titleView, "Create percentage discount")
+
+        let viewModel2 = AddEditCouponViewModel(siteID: 123, discountType: .fixedCart)
+        XCTAssertEqual(viewModel2.titleView, "Create fixed card discount")
+
+        let viewModel3 = AddEditCouponViewModel(siteID: 123, discountType: .fixedProduct)
+        XCTAssertEqual(viewModel3.titleView, "Create fixed product discount")
+
+        let viewModel4 = AddEditCouponViewModel(siteID: 123, discountType: .other)
+        XCTAssertEqual(viewModel4.titleView, "Create discount")
+    }
+
+    func test_titleView_property_return_expected_values_on_editing_depending_on_discountType() {
+        let viewModel1 = AddEditCouponViewModel(existingCoupon: Coupon.sampleCoupon.copy(discountType: .percent))
+        XCTAssertEqual(viewModel1.titleView, "Edit percentage discount")
+
+        let viewModel2 = AddEditCouponViewModel(existingCoupon: Coupon.sampleCoupon.copy(discountType: .fixedCart))
+        XCTAssertEqual(viewModel2.titleView, "Edit fixed card discount")
+
+        let viewModel3 = AddEditCouponViewModel(existingCoupon: Coupon.sampleCoupon.copy(discountType: .fixedProduct))
+        XCTAssertEqual(viewModel3.titleView, "Edit fixed product discount")
+
+        let viewModel4 = AddEditCouponViewModel(existingCoupon: Coupon.sampleCoupon.copy(discountType: .other))
+        XCTAssertEqual(viewModel4.titleView, "Edit discount")
+    }
+}

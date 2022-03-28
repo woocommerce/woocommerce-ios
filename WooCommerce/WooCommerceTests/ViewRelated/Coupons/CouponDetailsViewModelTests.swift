4adcfd7a3b0aab75a4edf0cad800f38c5348343b
Huong Do <hi.huongdo@gmail.com>
2022-03-28 12:24:31 +0700

    Update unit tests for coupon details

--- WooCommerce/WooCommerceTests/ViewRelated/Coupons/CouponDetailsViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Coupons/CouponDetailsViewModelTests.swift
@@ -4,42 +4,58 @@ import XCTest
 
 final class CouponDetailsViewModelTests: XCTestCase {
 
-    func test_coupon_details_are_correct_for_fixedProduct_discount_type() {
+    func test_amount_is_correct_for_fixedProduct_discount_type() {
         // Given
         let sampleCoupon = Coupon.fake().copy(
-            code: "AGK32FD",
             amount: "10.00",
-            discountType: .fixedProduct,
-            description: "Coupon description",
-            dateExpires: Date(timeIntervalSince1970: 1642755825), // GMT: January 21, 2022
-            productIds: []
+            discountType: .fixedProduct
         )
         let viewModel = CouponDetailsViewModel(coupon: sampleCoupon, currencySettings: CurrencySettings())
 
         // Then
-        XCTAssertEqual(viewModel.couponCode, "AGK32FD")
         XCTAssertEqual(viewModel.amount, "$10.00")
-        XCTAssertEqual(viewModel.description, "Coupon description")
-        XCTAssertEqual(viewModel.expiryDate, "January 21, 2022")
     }
 
-    func test_coupon_details_are_correct_for_percentage_discount_type() {
+    func test_amount_is_correct_for_percentage_discount_type() {
         // Given
+        let sampleCoupon = Coupon.fake().copy(
+            amount: "10.00",
+            discountType: .percent
+        )
+        let viewModel = CouponDetailsViewModel(coupon: sampleCoupon)
+
+        // Then
+        XCTAssertEqual(viewModel.amount, "10%")
+    }
+
+    func test_coupon_details_are_correct() {
         let sampleCoupon = Coupon.fake().copy(
             code: "AGK32FD",
             amount: "10.00",
             discountType: .percent,
             description: "Coupon description",
             dateExpires: Date(timeIntervalSince1970: 1642755825), // GMT: January 21, 2022
-            productIds: []
+            productIds: [],
+            usageLimitPerUser: 3,
+            freeShipping: true,
+            excludeSaleItems: false,
+            minimumAmount: "5.00",
+            emailRestrictions: ["*@a8c.com", "someone.else@example.com"]
         )
-        let viewModel = CouponDetailsViewModel(coupon: sampleCoupon)
+        let viewModel = CouponDetailsViewModel(coupon: sampleCoupon, currencySettings: .init())
 
         // Then
         XCTAssertEqual(viewModel.couponCode, "AGK32FD")
         XCTAssertEqual(viewModel.amount, "10%")
         XCTAssertEqual(viewModel.description, "Coupon description")
         XCTAssertEqual(viewModel.expiryDate, "January 21, 2022")
+        XCTAssertEqual(viewModel.discountType, NSLocalizedString("Percentage Discount", comment: ""))
+        XCTAssertFalse(viewModel.excludeSaleItems)
+        XCTAssertTrue(viewModel.allowsFreeShipping)
+        XCTAssertEqual(viewModel.usageLimitPerUser, 3)
+        XCTAssertEqual(viewModel.minimumAmount, "$5.00")
+        XCTAssertEqual(viewModel.maximumAmount, "")
+        XCTAssertEqual(viewModel.emailRestrictions, ["*@a8c.com", "someone.else@example.com"])
     }
 
     func test_coupon_is_updated_after_synchronizing() {

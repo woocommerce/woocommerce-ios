fc6f8ef6b33b32f8a97330de1c0a686846134ccc
Paolo Musolino <info@codeido.com>
2022-03-30 17:29:17 +0200

    feat: Coupon Details section

--- WooCommerce/Classes/ViewRelated/Coupons/Add and Edit Coupons/AddEditCoupon.swift
+++ WooCommerce/Classes/ViewRelated/Coupons/Add and Edit Coupons/AddEditCoupon.swift
@@ -17,8 +17,37 @@ struct AddEditCoupon: View {
         NavigationView {
             GeometryReader { geometry in
                 ScrollView {
-                    ListHeaderView(text: Localization.headerCouponDetails.uppercased(), alignment: .left)
-                        .padding(.horizontal, insets: geometry.safeAreaInsets)
+                    VStack (alignment: .leading) {
+                        ListHeaderView(text: Localization.headerCouponDetails.uppercased(), alignment: .left)
+                            .padding(.horizontal, insets: geometry.safeAreaInsets)
+
+                        TitleAndTextFieldRow(title: Localization.couponAmountPercentage,
+                                             placeholder: Localization.couponAmountPercentage,
+                                             text: $viewModel.amountField,
+                                             editable: false,
+                                             fieldAlignment: .leading,
+                                             keyboardType: .asciiCapableNumberPad)
+                            .padding(.horizontal, insets: geometry.safeAreaInsets)
+                        Divider()
+                            .padding([.leading], insets: geometry.safeAreaInsets)
+                        Text(Localization.footerCouponAmountPercentage)
+                            .subheadlineStyle()
+                            .padding(.horizontal, insets: geometry.safeAreaInsets)
+
+                        TitleAndTextFieldRow(title: Localization.couponCode,
+                                             placeholder: Localization.couponCode,
+                                             text: $viewModel.codeField,
+                                             editable: false,
+                                             fieldAlignment: .leading,
+                                             keyboardType: .asciiCapableNumberPad)
+                            .padding(.horizontal, insets: geometry.safeAreaInsets)
+                        Divider()
+                            .padding([.leading], insets: geometry.safeAreaInsets)
+                        Text(Localization.footerCouponCode)
+                            .subheadlineStyle()
+                            .padding(.horizontal, insets: geometry.safeAreaInsets)
+                    }
+
                 }
             }
             .toolbar {
@@ -45,6 +74,18 @@ private extension AddEditCoupon {
         static let headerCouponDetails = NSLocalizedString(
             "Coupon details",
             comment: "Header of the coupon details in the view for adding or editing a coupon.")
+        static let couponAmountPercentage = NSLocalizedString(
+            "Amount (%)",
+            comment: "Text field Amount in percentage in the view for adding or editing a coupon.")
+        static let footerCouponAmountPercentage = NSLocalizedString(
+            "Set the percentage of the discount you want to offer.",
+            comment: "The footer of the text field Amount in percentage in the view for adding or editing a coupon.")
+        static let couponCode = NSLocalizedString(
+            "Coupon Code",
+            comment: "Text field coupon code in the view for adding or editing a coupon.")
+        static let footerCouponCode = NSLocalizedString(
+            "Customers need to enter this code to use the coupon.",
+            comment: "The footer of the text field coupon code in the view for adding or editing a coupon.")
     }
 }
 

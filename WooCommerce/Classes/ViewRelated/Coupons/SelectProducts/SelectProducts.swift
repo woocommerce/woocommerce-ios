8cc0c0b706fcd8c9ca7bd07ce8df0fcf658a2955
Huong Do <hi.huongdo@gmail.com>
2022-04-06 11:17:38 +0700

    Add localized strings

--- WooCommerce/Classes/ViewRelated/Coupons/SelectProducts/SelectProducts.swift
+++ WooCommerce/Classes/ViewRelated/Coupons/SelectProducts/SelectProducts.swift
@@ -7,22 +7,30 @@ struct SelectProducts: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
-                SearchHeader(filterText: $query, filterPlaceholder: "Search Products")
+                SearchHeader(filterText: $query, filterPlaceholder: Localization.searchBarPlaceholder)
             }
             HStack {
-                Button("Select All") {
+                Button(Localization.selectAll) {
                     // TODO: select all item
                 }
                 .buttonStyle(LinkButtonStyle())
                 .fixedSize()
                 Spacer()
-                Button("Filter") {
+                Button(Localization.filter) {
                     // TODO: show filter view
                 }
                 .buttonStyle(LinkButtonStyle())
                 .fixedSize()
             }
         }
+    }
+}
+
+private extension SelectProducts {
+    enum Localization {
+        static let searchBarPlaceholder = NSLocalizedString("Search Products", comment: "Placeholder for the search bar in the Select Products screen")
+        static let selectAll = NSLocalizedString("Select All", comment: "Action button on the Select Products screen to select all products in the list")
+        static let filter = NSLocalizedString("Filter", comment: "Action button on the Select Products screen to filter items in the product list.")
     }
 }
 

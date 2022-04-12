6065156e882e0ddfa6862148b85dad8135fbde74
Paolo Musolino <info@codeido.com>
2022-04-12 12:44:53 +0200

    fix: localized string

--- WooCommerce/Classes/ViewRelated/Coupons/Add and Edit Coupons/CouponExpiryDateView.swift
+++ WooCommerce/Classes/ViewRelated/Coupons/Add and Edit Coupons/CouponExpiryDateView.swift
@@ -32,7 +32,7 @@ struct CouponExpiryDateView: View {
 private extension CouponExpiryDateView {
 
     enum Localization {
-        static let title = NSLocalizedString("Select an expiry date",
+        static let title = NSLocalizedString("Coupon expiry date",
                                              comment: "Title of the view for selecting an expiry date for a coupon.")
         static let resetButton = NSLocalizedString("Reset",
                                                    comment: "Reset button in the view for selecting an expiry date for a coupon.")

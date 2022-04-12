a0220f8d4e339e61284d75a7821341357ce5fc4c
Juanpe Catalán <juanpecm@gmail.com>
2022-04-12 14:07:22 +0200

    Revert validation if any image has moved after reordering

--- WooCommerce/Classes/ViewRelated/Products/Media/ProductImagesViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Media/ProductImagesViewController.swift
@@ -240,20 +240,8 @@ private extension ProductImagesViewController {
     }
 
     func handleProductImageStatusesReordering(_ reorderedProductImageStatuses: [ProductImageStatus]) {
-        // We have to validate if the order of the product images has been changed.
-        // The user can make some changes but the list may end up with the old order.
-        hasMovedAnyImages = validateIfAnyImagesHasMovedAfterReordering(reorderedProductImageStatuses)
-
+        hasMovedAnyImages = true
         productImageActionHandler.updateProductImageStatusesAfterReordering(reorderedProductImageStatuses)
-    }
-
-    /// Check if the given product images are in the same order as the original ones.
-    ///
-    func validateIfAnyImagesHasMovedAfterReordering(_ reorderedItems: [ProductImageStatus]) -> Bool {
-        let zippedProductImages = zip(reorderedItems, self.product.imageStatuses)
-        return zippedProductImages.contains(where: { reordered, original -> Bool in
-            reordered.dragItemIdentifier != original.dragItemIdentifier
-        })
     }
 }
 

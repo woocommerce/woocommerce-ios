4051f41ac843fa71442105c27c7b8be059f3076a
thehenrybyrd <15107387+thehenrybyrd@users.noreply.github.com>
2022-04-13 11:56:43 +0300

    Create new Flow for adding product

--- WooCommerce/WooCommerceUITests/Flows/NewOrderFlow.swift
+++ WooCommerce/WooCommerceUITests/Flows/NewOrderFlow.swift
@@ -13,4 +13,14 @@ class NewOrderFlow {
             .selectOrderStatus(atIndex: 1)
             .confirmSelectedOrderStatus()
     }
+
+
+    /// Select the first product from the addProductScreen
+    /// - Returns: New Order screen object.
+    @discardableResult
+    static func addProduct() throws -> NewOrderScreen {
+        let products = try GetMocks.readProductsData()  // if we need use this in other functions in this flow, it'll have to be a private function
+        return try NewOrderScreen().openAddProductScreen()
+            .selectProduct(byName: products[0].name)
+    }
 }

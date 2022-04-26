32386caae7d9103a9756cbc7cd59cbd84acf09fb
Juanpe Catalán <juanpecm@gmail.com>
2022-04-26 11:14:17 +0200

    Add `isUseUpdateOrderAddressOptimisticallyIfAvaiableOn` to the `MockFeatureFlagService`

--- WooCommerce/WooCommerceTests/Mocks/MockFeatureFlagService.swift
+++ WooCommerce/WooCommerceTests/Mocks/MockFeatureFlagService.swift
@@ -7,17 +7,20 @@ struct MockFeatureFlagService: FeatureFlagService {
     private let isInboxOn: Bool
     private let isSplitViewInOrdersTabOn: Bool
     private let isUpdateOrderOptimisticallyOn: Bool
+    private let isUseUpdateOrderAddressOptimisticallyIfAvaiableOn: Bool
 
     init(isJetpackConnectionPackageSupportOn: Bool = false,
          isHubMenuOn: Bool = false,
          isInboxOn: Bool = false,
          isSplitViewInOrdersTabOn: Bool = false,
-         isUpdateOrderOptimisticallyOn: Bool = false) {
+         isUpdateOrderOptimisticallyOn: Bool = false,
+         isUseUpdateOrderAddressOptimisticallyIfAvaiableOn: Bool = false) {
         self.isJetpackConnectionPackageSupportOn = isJetpackConnectionPackageSupportOn
         self.isHubMenuOn = isHubMenuOn
         self.isInboxOn = isInboxOn
         self.isSplitViewInOrdersTabOn = isSplitViewInOrdersTabOn
         self.isUpdateOrderOptimisticallyOn = isUpdateOrderOptimisticallyOn
+        self.isUseUpdateOrderAddressOptimisticallyIfAvaiableOn = isUseUpdateOrderAddressOptimisticallyIfAvaiableOn
     }
 
     func isFeatureFlagEnabled(_ featureFlag: FeatureFlag) -> Bool {
@@ -32,6 +35,8 @@ struct MockFeatureFlagService: FeatureFlagService {
             return isSplitViewInOrdersTabOn
         case .updateOrderOptimistically:
             return isUpdateOrderOptimisticallyOn
+        case .useUpdateOrderAddressOptimisticallyIfAvailable:
+            return isUseUpdateOrderAddressOptimisticallyIfAvaiableOn
         default:
             return false
         }

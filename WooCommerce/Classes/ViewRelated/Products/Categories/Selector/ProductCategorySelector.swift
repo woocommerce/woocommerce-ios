263d64d471f87b7109df50007fc8b4fa88e07b4e
Huong Do <hi.huongdo@gmail.com>
2022-04-29 14:38:49 +0700

    Add config for category selector

--- WooCommerce/Classes/ViewRelated/Products/Categories/Selector/ProductCategorySelector.swift
+++ WooCommerce/Classes/ViewRelated/Products/Categories/Selector/ProductCategorySelector.swift
@@ -3,17 +3,37 @@ import SwiftUI
 /// View showing a list of product categories to select from.
 ///
 struct ProductCategorySelector: View {
+    @Binding private var isPresented: Bool
     @ObservedObject private var viewModel: ProductCategorySelectorViewModel
 
-    init(viewModel: ProductCategorySelectorViewModel) {
+    private let config: Configuration
+
+    private var doneButtonTitle: String {
+        let itemsCount = viewModel.listViewModel.selectedCategories.count
+        if itemsCount == 0 {
+            return Localization.doneButton
+        } else {
+            return String.pluralize(
+                itemsCount,
+                singular: config.doneButtonSingularFormat,
+                plural: config.doneButtonPluralFormat
+            )
+        }
+    }
+
+    init(isPresented: Binding<Bool>,
+         config: Configuration,
+         viewModel: ProductCategorySelectorViewModel) {
         self.viewModel = viewModel
+        self.config = config
+        self._isPresented = isPresented
     }
 
     var body: some View {
         NavigationView {
             VStack(spacing: 0) {
                 ProductCategoryList(viewModel: viewModel.listViewModel)
-                Button("Done") {
+                Button(doneButtonTitle) {
                     // TODO
                 }
                 .buttonStyle(PrimaryButtonStyle())
@@ -21,12 +41,12 @@ struct ProductCategorySelector: View {
             }
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
-                    Button("Cancel") {
-                        // TODO
+                    Button(Localization.cancelButton) {
+                        isPresented.toggle()
                     }
                 }
             }
-            .navigationTitle("Select Products")
+            .navigationTitle(config.title)
             .navigationBarTitleDisplayMode(.large)
             .navigationViewStyle(.stack)
             .wooNavigationBarStyle()
@@ -34,9 +54,31 @@ struct ProductCategorySelector: View {
     }
 }
 
+extension ProductCategorySelector {
+    struct Configuration {
+        let title: String
+        var doneButtonSingularFormat: String
+        var doneButtonPluralFormat: String
+    }
+}
+
+private extension ProductCategorySelector {
+    enum Localization {
+        static let doneButton = NSLocalizedString("Done", comment: "Button to submit selection on Select Categories screen")
+        static let cancelButton = NSLocalizedString("Cancel", comment: "Button to dismiss Select Categories screen")
+    }
+}
+
 struct ProductCategorySelector_Previews: PreviewProvider {
     static var previews: some View {
         let viewModel = ProductCategorySelectorViewModel(siteID: 123) { _ in }
-        ProductCategorySelector(viewModel: viewModel)
+        let config = ProductCategorySelector.Configuration(
+            title: "Select Categories",
+            doneButtonSingularFormat: "",
+            doneButtonPluralFormat: ""
+        )
+        ProductCategorySelector(isPresented: .constant(true),
+                                config: config,
+                                viewModel: viewModel)
     }
 }

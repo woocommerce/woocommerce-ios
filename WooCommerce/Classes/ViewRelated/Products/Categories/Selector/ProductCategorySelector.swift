73bec3e6dc70c6127ecd35d1650b2b7b16ac7926
Huong Do <hi.huongdo@gmail.com>
2022-04-29 11:33:55 +0700

    Add ProductCategorySelector view

---
+++ WooCommerce/Classes/ViewRelated/Products/Categories/Selector/ProductCategorySelector.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// View showing a list of product categories to select from.
+///
+struct ProductCategorySelector: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ProductCategorySelector_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductCategorySelector()
+    }
+}

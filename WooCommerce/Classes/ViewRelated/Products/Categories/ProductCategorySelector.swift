217b259719041d3e7e99b1f75419141638d35552
Huong Do <hi.huongdo@gmail.com>
2022-04-29 11:30:37 +0700

    Add SwiftUI wrapper for ProductCategoryListViewController

---
+++ WooCommerce/Classes/ViewRelated/Products/Categories/ProductCategorySelector.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+/// SwiftUI wrapper of `ProductCategoryListViewController`.
+///
+struct ProductCategorySelector: UIViewControllerRepresentable {
+    private let viewModel: ProductCategoryListViewModel
+
+    init(viewModel: ProductCategoryListViewModel) {
+        self.viewModel = viewModel
+    }
+
+    func makeUIViewController(context: Context) -> ProductCategoryListViewController {
+        return ProductCategoryListViewController(viewModel: viewModel)
+    }
+
+    func updateUIViewController(_ uiViewController: ProductCategoryListViewController, context: Context) {
+        // no=op
+    }
+}

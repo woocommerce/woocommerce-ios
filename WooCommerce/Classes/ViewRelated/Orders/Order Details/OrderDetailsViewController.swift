b37c443bec66f62c7b2c7aa6702722eb21ebadf3
Josh Heald <joshheald@users.noreply.github.com>
2022-05-04 10:13:35 +0100

    Revert "6607 Remove Collect Payment gateway account check"
    This reverts commit 318440cd0ef2053c8378c54565cfdc74b901575f.

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/OrderDetailsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/OrderDetailsViewController.swift
@@ -382,6 +382,10 @@ private extension OrderDetailsViewController {
         }
 
         group.enter()
+        refreshCardPresentPaymentEligibility()
+        group.leave()
+
+        group.enter()
         syncSavedReceipts {_ in
             group.leave()
         }

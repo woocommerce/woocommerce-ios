cca5f492463be3ed46fe57c66ee30c386c4767b1
Jaclyn Chen <watertranquil@gmail.com>
2022-05-10 21:00:33 +0800

    Add comments for dashboard event functions.

--- WooCommerce/Classes/Analytics/WooAnalyticsEvent+Dashboard.swift
+++ WooCommerce/Classes/Analytics/WooAnalyticsEvent+Dashboard.swift
@@ -7,18 +7,26 @@ extension WooAnalyticsEvent {
             static let range = "range"
         }
 
+        /// Tracked when the store stats are loaded with fresh data either via first load, event driven refresh, or manual refresh.
+        /// - Parameter timeRange: the range of store stats (e.g. Today, This Week, This Month, This Year).
         static func dashboardMainStatsLoaded(timeRange: StatsTimeRangeV4) -> WooAnalyticsEvent {
             WooAnalyticsEvent(statName: .dashboardMainStatsLoaded, properties: [Keys.range: timeRange.analyticsValue])
         }
 
+        /// Tracked when the date range on the store stats view changes.
+        /// - Parameter timeRange: the range of store stats (e.g. Today, This Week, This Month, This Year).
         static func dashboardMainStatsDate(timeRange: StatsTimeRangeV4) -> WooAnalyticsEvent {
             WooAnalyticsEvent(statName: .dashboardMainStatsDate, properties: [Keys.range: timeRange.analyticsValue])
         }
 
+        /// Tracked when the top performers are loaded with fresh data either via first load, event driven refresh, or manual refresh.
+        /// - Parameter timeRange: the range of store stats (e.g. Today, This Week, This Month, This Year).
         static func dashboardTopPerformersLoaded(timeRange: StatsTimeRangeV4) -> WooAnalyticsEvent {
             WooAnalyticsEvent(statName: .dashboardTopPerformersLoaded, properties: [Keys.range: timeRange.analyticsValue])
         }
 
+        /// Tracked when the date range on the top performers view changes.
+        /// - Parameter timeRange: the range of store stats (e.g. Today, This Week, This Month, This Year).
         static func dashboardTopPerformersDate(timeRange: StatsTimeRangeV4) -> WooAnalyticsEvent {
             WooAnalyticsEvent(statName: .dashboardTopPerformersDate, properties: [Keys.range: timeRange.analyticsValue])
         }

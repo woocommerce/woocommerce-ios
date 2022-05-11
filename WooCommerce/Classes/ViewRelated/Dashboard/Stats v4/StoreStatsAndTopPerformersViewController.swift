4f1bc583c81d407f3625987d2e51a241ecbb383c
Jaclyn Chen <watertranquil@gmail.com>
2022-05-11 09:49:08 +0800

    Fix Hound warning.

--- WooCommerce/Classes/ViewRelated/Dashboard/Stats v4/StoreStatsAndTopPerformersViewController.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Stats v4/StoreStatsAndTopPerformersViewController.swift
@@ -407,7 +407,8 @@ private extension StoreStatsAndTopPerformersViewController {
                                                           onCompletion: { result in
                                                             switch result {
                                                             case .success:
-                                                                ServiceLocator.analytics.track(event: .Dashboard.dashboardTopPerformersLoaded(timeRange: timeRange))
+                                                                ServiceLocator.analytics.track(event:
+                                                                        .Dashboard.dashboardTopPerformersLoaded(timeRange: timeRange))
                                                             case .failure(let error):
                                                                 DDLogError("⛔️ Dashboard (Top Performers) — Error synchronizing top earner stats: \(error)")
                                                             }

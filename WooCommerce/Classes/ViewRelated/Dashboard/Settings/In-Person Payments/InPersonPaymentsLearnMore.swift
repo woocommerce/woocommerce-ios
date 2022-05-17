0f4b87ac1bce162d0a152996b43ae0f2e3494d9e
Andrey <andrey.kidinov@a8c.com>
2022-05-17 15:45:51 +0400

    Reverted formatting changes

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsLearnMore.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsLearnMore.swift
@@ -9,17 +9,17 @@ struct InPersonPaymentsLearnMore: View {
     var body: some View {
         HStack(spacing: 16) {
             Image(uiImage: .infoOutlineImage)
-                    .resizable()
-                    .foregroundColor(Color(.neutral(.shade60)))
-                    .frame(width: iconSize, height: iconSize)
+                .resizable()
+                .foregroundColor(Color(.neutral(.shade60)))
+                .frame(width: iconSize, height: iconSize)
             AttributedText(learnMoreAttributedString)
         }
-                .padding(.vertical, Constants.verticalPadding)
-                .onTapGesture {
-                    ServiceLocator.analytics.track(event: WooAnalyticsEvent.InPersonPayments
-                            .cardPresentOnboardingLearnMoreTapped(countryCode: cardPresentConfiguration.countryCode))
-                    customOpenURL?(InPersonPaymentsLearnMore.learnMoreURL)
-                }
+            .padding(.vertical, Constants.verticalPadding)
+            .onTapGesture {
+                ServiceLocator.analytics.track(event: WooAnalyticsEvent.InPersonPayments
+                                                .cardPresentOnboardingLearnMoreTapped(countryCode: cardPresentConfiguration.countryCode))
+                customOpenURL?(InPersonPaymentsLearnMore.learnMoreURL)
+            }
     }
 
     var iconSize: CGFloat {
@@ -80,6 +80,6 @@ private enum Localization {
 struct InPersonPaymentsLearnMore_Previews: PreviewProvider {
     static var previews: some View {
         InPersonPaymentsLearnMore()
-                .padding()
+            .padding()
     }
 }

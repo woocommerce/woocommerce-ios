2f881f58f6df07210d88b57bef640b6f7e2eff58
ThomazFB <thomazfcortez@gmail.com>
2022-05-24 18:42:40 -0300

    Fix CouponCodeInputFormatterTests

--- WooCommerce/WooCommerceTests/ViewRelated/Coupons/CouponCodeInputFormatterTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Coupons/CouponCodeInputFormatterTests.swift
@@ -7,13 +7,13 @@ final class CouponCodeInputFormatterTests: XCTestCase {
 
     // MARK: test cases for `isValid(input:)`
     func test_a_string_is_a_valid_coupon() {
-        let input = "ejfdklmda,349292!òàèù"
+        let input = "eJfDkLmDa,349292!òàèù"
         XCTAssertTrue(formatter.isValid(input: input))
     }
 
     // MARK: test cases for `format(input:)`
     func test_a_coupon_code_will_be_formatted_uppercased() {
-        let input = "abcdefd"
-        XCTAssertEqual(formatter.format(input: input), "ABCDEFD")
+        let input = "AbCdEfD"
+        XCTAssertEqual(formatter.format(input: input), "abcdefd")
     }
 }

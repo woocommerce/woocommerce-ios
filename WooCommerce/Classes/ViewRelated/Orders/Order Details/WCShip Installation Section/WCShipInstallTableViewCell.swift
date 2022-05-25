92f710f81288d3cf094f25582eb16aea258247be
Paolo Musolino <info@codeido.com>
2022-05-25 15:17:59 +0200

    feat: WCShipInstallTableViewCell implementation

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/WCShip Installation Section/WCShipInstallTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/WCShip Installation Section/WCShipInstallTableViewCell.swift
@@ -1,24 +1,50 @@
-//
-//  WCShipInstallTableViewCell.swift
-//  WooCommerce
-//
-//  Created by Paolo Musolino on 25/05/22.
-//  Copyright © 2022 Automattic. All rights reserved.
-//
-
 import UIKit
 
-class WCShipInstallTableViewCell: UITableViewCell {
+/// Displays the banner in the Order Detail for suggesting to the user to install/activate the WCShip extension.
+///
+final class WCShipInstallTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var body: UILabel!
+    @IBOutlet weak var action: UILabel!
+    @IBOutlet weak var placeholderImageView: UIImageView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        configureBackground()
+        configureTitleLabel()
+        configureBodyLabel()
+        configureActionLabel()
+    }
+}
+
+private extension WCShipInstallTableViewCell {
+
+    func configureBackground() {
+        applyDefaultBackgroundStyle()
+
+        //Background when selected
+        selectedBackgroundView = UIView()
+        selectedBackgroundView?.backgroundColor = .listBackground
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configurePlaceholderImageView() {
+        placeholderImageView.image = UIImage.productPlaceholderImage
+        placeholderImageView.contentMode = .scaleAspectFit
+        placeholderImageView.clipsToBounds = true
     }
-    
+
+    func configureTitleLabel() {
+        title.applyTitleStyle()
+    }
+
+    func configureBodyLabel() {
+        body.applyBodyStyle()
+        body.numberOfLines = 0
+    }
+
+    func configureActionLabel() {
+        action.applyActionableStyle()
+    }
+
 }

21cd505e3e3e2079884cf8f0a134199f40e17e4f
Huong Do <hi.huongdo@gmail.com>
2022-05-25 17:08:24 +0700

    Add tests for isSelected of DiscountTypeBottomSheetListSelectorCommand

--- WooCommerce/WooCommerceTests/ViewRelated/Coupons/DiscountTypeBottomSheetListSelectorCommandTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Coupons/DiscountTypeBottomSheetListSelectorCommandTests.swift
@@ -4,23 +4,45 @@ import XCTest
 
 final class DiscountTypeBottomSheetListSelectorCommandTests: XCTestCase {
     func test_callback_is_called_on_selection() {
-        // Arrange
+        // Given
         var selectedActions = [Coupon.DiscountType]()
         let command = DiscountTypeBottomSheetListSelectorCommand(selected: .percent) { (selected) in
             selectedActions.append(selected)
         }
 
-        // Action
+        // When
         command.handleSelectedChange(selected: .fixedProduct)
         command.handleSelectedChange(selected: .fixedCart)
         command.handleSelectedChange(selected: .percent)
 
-        // Assert
+        // Then
         let expectedActions: [Coupon.DiscountType] = [
             .fixedProduct,
             .fixedCart,
             .percent
         ]
         XCTAssertEqual(selectedActions, expectedActions)
+    }
+
+    func test_isSelected_returns_true_if_given_the_initial_value() {
+        // Given
+        let command = DiscountTypeBottomSheetListSelectorCommand(selected: .percent) { _ in }
+
+        // When
+        let isSelected = command.isSelected(model: .percent)
+
+        // Then
+        XCTAssertTrue(isSelected)
+    }
+
+    func test_isSelected_returns_false_if_given_a_different_value() {
+        // Given
+        let command = DiscountTypeBottomSheetListSelectorCommand(selected: .percent) { _ in }
+
+        // When
+        let isSelected = command.isSelected(model: .fixedCart)
+
+        // Then
+        XCTAssertFalse(isSelected)
     }
 }

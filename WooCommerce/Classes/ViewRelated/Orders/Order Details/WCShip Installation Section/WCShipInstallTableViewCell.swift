19e5d4888ca08066513812439de987cc4726da63
Paolo Musolino <info@codeido.com>
2022-05-25 15:17:34 +0200

    feat: WCShipInstallTableViewCell

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/WCShip Installation Section/WCShipInstallTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WCShipInstallTableViewCell.swift
+//  WooCommerce
+//
+//  Created by Paolo Musolino on 25/05/22.
+//  Copyright © 2022 Automattic. All rights reserved.
+//
+
+import UIKit
+
+class WCShipInstallTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

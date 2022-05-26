e8c2329d6dd2fa90bf44a36f469d4d61b7713dd3
Jorge Bernal <jorge@automattic.com>
2022-05-26 12:03:42 +0200

    Fix typo

--- WooCommerce/Classes/Tools/In-Person Payments/CardPresentConfigurationLoader.swift
+++ WooCommerce/Classes/Tools/In-Person Payments/CardPresentConfigurationLoader.swift
@@ -3,7 +3,7 @@ import Yosemite
 
 final class CardPresentConfigurationLoader {
     init(stores: StoresManager = ServiceLocator.stores) {
-        // This initialized is kept since this is where we'd check for
+        // This initializer is kept since this is where we'd check for
         // feature flags while developing support for a new country
         // See https://github.com/woocommerce/woocommerce-ios/pull/6954
     }

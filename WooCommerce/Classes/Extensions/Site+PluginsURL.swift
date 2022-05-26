b8ded4d89969960c9f602880d71fd2d56ab76608
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2022-05-26 12:48:45 +0200

    Add documentation

--- WooCommerce/Classes/Extensions/Site+PluginsURL.swift
+++ WooCommerce/Classes/Extensions/Site+PluginsURL.swift
@@ -12,11 +12,17 @@ private extension CardPresentPaymentsPlugin {
     }
 }
 
+/// Encapsulates the logic related to the provision of the Site URLs that point to plugins related content
+///
 extension Site {
+    /// Site's plugins section in wp-admin.
+    ///
     var pluginsURL: String {
         adminURL + "/plugins.php"
     }
 
+    /// Payment plugin settings in wp-admin. This can be helpful when the plugin needs to be setup completely.
+    /// 
     func pluginSettingsSectionURL(from plugin: CardPresentPaymentsPlugin) -> String {
         adminURL + "admin.php?page=wc-settings&tab=checkout&section=" + plugin.setupURLSectionPath
     }

b008f92cac09de110ffe99eecdb41a7c773820b9
Huong Do <hi.huongdo@gmail.com>
2022-05-31 16:43:09 +0700

    Add unit tests for CouponAllowedEmailsViewModel

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Coupons/CouponAllowedEmailsViewModelTests.swift
@@ -0,0 +1,38 @@
+import XCTest
+@testable import WooCommerce
+
+final class CouponAllowedEmailsViewModelTests: XCTestCase {
+
+    func test_completion_block_is_triggered_when_address_validation_succeeds() {
+        // Given
+        var savedAddresses: String?
+        let completionBlock: (String) -> Void = { email in
+            savedAddresses = email
+        }
+        let viewModel = CouponAllowedEmailsViewModel(allowedEmails: "", onCompletion: completionBlock)
+
+        // When
+        viewModel.emailPatterns = "*@mail.com"
+        viewModel.validateEmails {}
+
+        // Then
+        XCTAssertEqual(savedAddresses, "*@mail.com")
+        XCTAssertNil(viewModel.notice)
+    }
+
+    func test_completion_block_is_not_triggered_and_notice_is_not_nil_when_address_validation_fails() {
+        var savedAddresses: String?
+        let completionBlock: (String) -> Void = { email in
+            savedAddresses = email
+        }
+        let viewModel = CouponAllowedEmailsViewModel(allowedEmails: "", onCompletion: completionBlock)
+
+        // When
+        viewModel.emailPatterns = "*@mail"
+        viewModel.validateEmails {}
+
+        // Then
+        XCTAssertNil(savedAddresses)
+        XCTAssertNotNil(viewModel.notice)
+    }
+}

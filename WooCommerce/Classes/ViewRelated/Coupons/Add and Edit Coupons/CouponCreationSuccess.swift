8442421fef42f91dabfb78506774d1cc8b86a657
Huong Do <hi.huongdo@gmail.com>
2022-06-08 13:39:44 +0700

    Add new view for coupon creation success

---
+++ WooCommerce/Classes/ViewRelated/Coupons/Add and Edit Coupons/CouponCreationSuccess.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// A view to be displayed when a coupon is created successfully.
+///
+struct CouponCreationSuccess: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CouponCreationSuccess_Previews: PreviewProvider {
+    static var previews: some View {
+        CouponCreationSuccess()
+    }
+}

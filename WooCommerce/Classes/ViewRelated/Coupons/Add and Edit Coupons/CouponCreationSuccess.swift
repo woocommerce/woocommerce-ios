aff21453743486719fb973970aae2ba2624fd578
Huong Do <hi.huongdo@gmail.com>
2022-06-08 14:35:38 +0700

    Add success label and image

--- WooCommerce/Classes/ViewRelated/Coupons/Add and Edit Coupons/CouponCreationSuccess.swift
+++ WooCommerce/Classes/ViewRelated/Coupons/Add and Edit Coupons/CouponCreationSuccess.swift
@@ -3,13 +3,25 @@ import SwiftUI
 /// A view to be displayed when a coupon is created successfully.
 ///
 struct CouponCreationSuccess: View {
+    let couponCode: String
+
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading, spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
+                Image(uiImage: UIImage.checkSuccessImage)
+                    .padding(.bottom, 40)
+                Text("Your new coupon was created!")
+                    .font(.largeTitle)
+                    .bold()
+                Text(couponCode)
+                    .font(.largeTitle)
+            }
+        }
     }
 }
 
 struct CouponCreationSuccess_Previews: PreviewProvider {
     static var previews: some View {
-        CouponCreationSuccess()
+        CouponCreationSuccess(couponCode: "34sdfg")
     }
 }

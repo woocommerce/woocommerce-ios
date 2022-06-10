c7aed068732428a91af8909654470e4fa0798701
ThomazFB <thomazfcortez@gmail.com>
2022-06-09 21:32:53 -0300

    Define Coupon+String

---
+++ WooCommerce/Classes/ViewRelated/Coupons/Coupon+String.swift
@@ -0,0 +1,29 @@
+import Foundation
+import Yosemite
+
+extension Coupon {
+    /// The message to be shared about the coupon
+    ///
+    var shareMessage: String {
+        if productIds.isNotEmpty ||
+                   productCategories.isNotEmpty ||
+                   excludedProductIds.isNotEmpty ||
+                   excludedProductCategories.isNotEmpty {
+            return String.localizedStringWithFormat(Localization.shareMessageSomeProducts, amount, code)
+        }
+        return String.localizedStringWithFormat(Localization.shareMessageAllProducts, amount, code)
+    }
+}
+
+private extension Coupon {
+    enum Localization {
+        static let shareMessageAllProducts = NSLocalizedString(
+                "Apply %1$@ off to all products with the promo code “%2$@”.",
+                comment: "Message to share the coupon code if it is applicable to all products. " +
+                        "Reads like: Apply 10% off to all products with the promo code “20OFF”.")
+        static let shareMessageSomeProducts = NSLocalizedString(
+                "Apply %1$@ off to some products with the promo code “%2$@”.",
+                comment: "Message to share the coupon code if it is applicable to some products. " +
+                        "Reads like: Apply 10% off to some products with the promo code “20OFF”.")
+    }
+}

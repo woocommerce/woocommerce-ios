c29f0065bab6b9418a798a437fc55f752b3f93d9
ThomazFB <thomazfcortez@gmail.com>
2022-06-10 00:07:34 -0300

    Remove unnecessary Localization declaration in CouponDetailsViewModel

--- WooCommerce/Classes/ViewRelated/Coupons/CouponDetails/CouponDetailsViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Coupons/CouponDetails/CouponDetailsViewModel.swift
@@ -299,14 +299,4 @@ private extension CouponDetailsViewModel {
     enum Constants {
         static let noLimit: Int64 = -1
     }
-    enum Localization {
-        static let shareMessageAllProducts = NSLocalizedString(
-            "Apply %1$@ off to all products with the promo code “%2$@”.",
-            comment: "Message to share the coupon code if it is applicable to all products. " +
-            "Reads like: Apply 10% off to all products with the promo code “20OFF”.")
-        static let shareMessageSomeProducts = NSLocalizedString(
-            "Apply %1$@ off to some products with the promo code “%2$@”.",
-            comment: "Message to share the coupon code if it is applicable to some products. " +
-            "Reads like: Apply 10% off to some products with the promo code “20OFF”.")
-    }
 }

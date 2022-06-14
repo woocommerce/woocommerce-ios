db1b62955b84ddb157b22719a415ee518091bc4b
Gabriel Maldonado <gabrielmaldonado.art@gmail.com>
2022-06-14 11:33:44 +0700

    Modified dateFromString func to accept injected parameters

--- TestKit/Sources/TestKit/DateFormatter+Helpers.swift
+++ TestKit/Sources/TestKit/DateFormatter+Helpers.swift
@@ -6,13 +6,16 @@ extension DateFormatter {
 
     /// Date And Time Formatter. Converts String to Date type
     ///
-    static public func dateFromString(with dateString: String) -> Date {
+    static public func dateFromString(with dateString: String,
+                                      locale: Locale? = .init(identifier: "en_US_POSIX"),
+                                      timeZone: TimeZone? = .init(identifier: "GMT"),
+                                      dateFormat: String? = "yyyy'-'MM'-'dd'T'HH:mm:ss") -> Date {
 
         let formatter = DateFormatter()
 
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.timeZone = TimeZone(identifier: "GMT")
-        formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH:mm:ss"
+        formatter.locale = locale
+        formatter.timeZone = timeZone
+        formatter.dateFormat = dateFormat
 
         guard let date = formatter.date(from: dateString) else {
             return Date()

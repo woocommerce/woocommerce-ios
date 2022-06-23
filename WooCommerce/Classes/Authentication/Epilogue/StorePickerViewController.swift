6a21f2a778ec91cd78c28efd684cf295c72fe7cf
Jaclyn Chen <watertranquil@gmail.com>
2022-06-23 15:02:52 -0400

    Update Yosemite action to `closeAccount`.

--- WooCommerce/Classes/Authentication/Epilogue/StorePickerViewController.swift
+++ WooCommerce/Classes/Authentication/Epilogue/StorePickerViewController.swift
@@ -716,8 +716,7 @@ private extension StorePickerViewController {
     func removeAppleIDAccess() async -> Result<Void, Error> {
         await withCheckedContinuation { [weak self] continuation in
             guard let self = self else { return }
-            let action = AccountAction.removeAppleIDAccess(dotcomAppID: ApiCredentials.dotcomAppId,
-                                                           dotcomSecret: ApiCredentials.dotcomSecret) { result in
+            let action = AccountAction.closeAccount { result in
                 continuation.resume(returning: result)
             }
             self.stores.dispatch(action)

c6ce7f50881a880e15f137f2bc6c5cb433c37936
Ernesto Carrion <ernesto.carrion@automattic.com>
2022-06-23 00:12:10 -0500

    Update unit tests

--- WooCommerce/WooCommerceTests/ViewRelated/Orders/Order Creation/EditableOrderViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Orders/Order Creation/EditableOrderViewModelTests.swift
@@ -1130,7 +1130,7 @@ final class EditableOrderViewModelTests: XCTestCase {
         let order = Order.fake().copy(isEditable: false)
 
         // When
-        let viewModel = NewOrderViewModel(siteID: sampleSiteID, initialOrder: order)
+        let viewModel = NewOrderViewModel(siteID: sampleSiteID, flow: .editing(initialOrder: order))
 
         // Then
         XCTAssertTrue(viewModel.shouldShowNonEditableBanner)
@@ -1141,7 +1141,7 @@ final class EditableOrderViewModelTests: XCTestCase {
         let order = Order.fake().copy(isEditable: true)
 
         // When
-        let viewModel = NewOrderViewModel(siteID: sampleSiteID, initialOrder: order)
+        let viewModel = NewOrderViewModel(siteID: sampleSiteID, flow: .editing(initialOrder: order))
 
         // Then
         XCTAssertFalse(viewModel.shouldShowNonEditableBanner)

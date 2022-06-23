347a7ec716e0f95971a182a9daa8f99440c7312c
Jaclyn Chen <watertranquil@gmail.com>
2022-06-23 14:56:28 -0400

    Update close account alerts with a username confirmation text field.

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/RemoveAppleIDAccessCoordinator.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/RemoveAppleIDAccessCoordinator.swift
@@ -1,10 +1,12 @@
 import UIKit
+import protocol Yosemite.StoresManager
 
 /// Coordinates navigation for removing Apple ID access from various entry points (settings and empty stores screens).
-final class RemoveAppleIDAccessCoordinator {
+@MainActor final class RemoveAppleIDAccessCoordinator {
     private let sourceViewController: UIViewController
     private let removeAction: () async -> Result<Void, Error>
     private let onRemoveSuccess: () -> Void
+    private let stores: StoresManager
 
     /// - Parameters:
     ///   - sourceViewController: the view controller that presents the in-progress UI and alerts.
@@ -12,10 +14,12 @@ final class RemoveAppleIDAccessCoordinator {
     ///   - onRemoveSuccess: called when the removal is successful.
     init(sourceViewController: UIViewController,
          removeAction: @escaping () async -> Result<Void, Error>,
-         onRemoveSuccess: @escaping () -> Void) {
+         onRemoveSuccess: @escaping () -> Void,
+         stores: StoresManager = ServiceLocator.stores) {
         self.sourceViewController = sourceViewController
         self.removeAction = removeAction
         self.onRemoveSuccess = onRemoveSuccess
+        self.stores = stores
     }
 
     func start() {
@@ -26,11 +30,15 @@ final class RemoveAppleIDAccessCoordinator {
 private extension RemoveAppleIDAccessCoordinator {
     func presentConfirmationAlert() {
         // TODO: 7068 - analytics
+
+        let username = stores.sessionManager.defaultAccount?.username ?? ""
+        let message = String.localizedStringWithFormat(Localization.ConfirmationAlert.alertMessageFormat, username)
         let alertController = UIAlertController(title: Localization.ConfirmationAlert.alertTitle,
-                                                message: Localization.ConfirmationAlert.alertMessage,
+                                                message: message,
                                                 preferredStyle: .alert)
         alertController.addActionWithTitle(Localization.ConfirmationAlert.cancelButtonTitle, style: .cancel) { _ in }
-        alertController.addActionWithTitle(Localization.ConfirmationAlert.removeButtonTitle, style: .destructive) { [weak self] _ in
+        let closeAccountAction = alertController.addActionWithTitle(Localization.ConfirmationAlert.removeButtonTitle,
+                                                                    style: .destructive) { [weak self] _ in
             guard let self = self else { return }
 
             // TODO: 7068 - analytics
@@ -46,12 +54,24 @@ private extension RemoveAppleIDAccessCoordinator {
                     case .success:
                         self.onRemoveSuccess()
                     case .failure(let error):
-                        DDLogError("⛔️ Cannot remove Apple ID access: \(error)")
-                        self.presentErrorAlert()
+                        DDLogError("⛔️ Cannot close account: \(error)")
+                        self.presentErrorAlert(error: error)
                     }
                 }
             }
         }
+
+        // Enables close account button based on whether the username matches the user input.
+        closeAccountAction.isEnabled = false
+
+        // Username text field.
+        alertController.addTextField { textField in
+            textField.clearButtonMode = .always
+            textField.on(.editingChanged) { textField in
+                closeAccountAction.isEnabled = textField.text == username
+            }
+        }
+
         sourceViewController.present(alertController, animated: true)
     }
 
@@ -66,12 +86,47 @@ private extension RemoveAppleIDAccessCoordinator {
         sourceViewController.dismiss(animated: true, completion: completion)
     }
 
-    func presentErrorAlert() {
+    func presentErrorAlert(error: Error) {
         let alertController = UIAlertController(title: Localization.ErrorAlert.alertTitle,
-                                                message: Localization.ErrorAlert.alertMessage,
+                                                message: generateLocalizedMessage(error: error),
                                                 preferredStyle: .alert)
+        alertController.addActionWithTitle(Localization.ErrorAlert.contactSupportButtonTitle, style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            ZendeskProvider.shared.showNewRequestIfPossible(from: self.sourceViewController, with: nil)
+        }
         alertController.addActionWithTitle(Localization.ErrorAlert.dismissButtonTitle, style: .cancel) { _ in }
         sourceViewController.present(alertController, animated: true)
+    }
+
+    func generateLocalizedMessage(error: Error) -> String {
+        let errorCode = errorCode(error: error)
+
+        switch errorCode {
+        case "unauthorized":
+            return NSLocalizedString("You're not authorized to close the account.",
+                                     comment: "Error message displayed when unable to close user account due to being unauthorized.")
+        case "atomic-site":
+            return NSLocalizedString("To close this account now, contact our support team.",
+                                     comment: "Error message displayed when unable to close user account due to having active atomic site.")
+        case "chargebacked-site":
+            return NSLocalizedString("This user account cannot be closed if there are unresolved chargebacks.",
+                                     comment: "Error message displayed when unable to close user account due to unresolved chargebacks.")
+        case "active-subscriptions":
+            return NSLocalizedString("This user account cannot be closed while it has active subscriptions.",
+                                     comment: "Error message displayed when unable to close user account due to having active subscriptions.")
+        case "active-memberships":
+            return NSLocalizedString("This user account cannot be closed while it has active purchases.",
+                                     comment: "Error message displayed when unable to close user account due to having active purchases.")
+        default:
+            return NSLocalizedString("An error occured while closing account.",
+                                     comment: "Default error message displayed when unable to close user account.")
+        }
+    }
+
+    func errorCode(error: Error) -> String? {
+        let userInfo = (error as NSError).userInfo
+        let errorCode = userInfo[Constants.dotcomAPIErrorCodeKey] as? String
+        return errorCode
     }
 }
 
@@ -79,46 +134,51 @@ private extension RemoveAppleIDAccessCoordinator {
     enum Localization {
         enum RemoveAppleIDAccessInProgressView {
             static let title = NSLocalizedString(
-                "Removing Apple ID Access...",
-                comment: "Title of the Remove Apple ID Access in-progress view."
+                "Closing account...",
+                comment: "Title of the Close Account in-progress view."
             )
         }
 
         enum ConfirmationAlert {
             static let alertTitle = NSLocalizedString(
-                "Remove Apple ID Access",
-                comment: "Remove Apple ID Access confirmation alert title - confirms and revokes Apple ID token."
+                "Confirm Close Account",
+                comment: "Close Account confirmation alert title."
             )
-            static let alertMessage = NSLocalizedString(
-                "This will log you out and reset your Sign In With Apple access token. " +
-                "You will be asked to re-enter your WordPress.com password if you Sign In With Apple again.",
-                comment: "Alert message to confirm a user meant to remove Apple ID access."
+            static let alertMessageFormat = NSLocalizedString(
+                "To confirm, please re-enter your username before closing.\n\n%1$@",
+                comment: "Close Account confirmation alert message. The %1$@ is the user's WordPress.com username."
             )
             static let cancelButtonTitle = NSLocalizedString(
                 "Cancel",
-                comment: "Alert button title - dismisses alert, which cancels the Remove Apple ID Access attempt."
+                comment: "Close Account button title - dismisses alert, which cancels the Close Account attempt."
             )
 
             static let removeButtonTitle = NSLocalizedString(
-                "Remove",
-                comment: "Remove Apple ID Access button title - confirms and revokes Apple ID token."
+                "Permanently Close Account",
+                comment: "Close Account button title - confirms and closes user's WordPress.com account."
             )
         }
 
         enum ErrorAlert {
             static let alertTitle = NSLocalizedString(
-                "Error Removing Apple ID Access",
-                comment: "Remove Apple ID Access error alert title."
+                "Couldn’t close account automatically",
+                comment: "Error title displayed when unable to close user account."
             )
-            static let alertMessage = NSLocalizedString(
-                "Please try again or contact us for support.",
-                comment: "Remove Apple ID Access error alert message."
+            static let contactSupportButtonTitle = NSLocalizedString(
+                "Contact Support",
+                comment: "Close Account error alert button title - navigates the user to contact support."
             )
             static let dismissButtonTitle = NSLocalizedString(
-                "OK",
-                comment: "Alert button title - dismisses Remove Apple ID Access error alert."
+                "Cancel",
+                comment: "Close Account error alert button title - dismisses error alert."
             )
         }
+    }
+}
+
+private extension RemoveAppleIDAccessCoordinator {
+    enum Constants {
+        static let dotcomAPIErrorCodeKey = "WordPressComRestApiErrorCodeKey"
     }
 }
 

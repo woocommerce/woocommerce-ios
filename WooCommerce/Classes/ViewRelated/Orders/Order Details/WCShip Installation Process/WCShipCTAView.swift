dd61e56018244f4e814cb4908ab6c5f3158cec1b
Paolo Musolino <info@codeido.com>
2022-07-01 16:49:01 +0200

    fix: removed viewDidLoad

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/WCShip Installation Process/WCShipCTAView.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/WCShip Installation Process/WCShipCTAView.swift
@@ -9,10 +9,6 @@ final class WCShipCTAHostingController: UIHostingController<WCShipCTAView> {
     required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 }
 
 

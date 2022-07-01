32acd70ce89f4354e648aa0ce58e863eb76db64e
ThomazFB <thomazfcortez@gmail.com>
2022-07-01 00:23:20 -0300

    Cover non-percent discount type gets ignored test scenario

--- WooCommerce/WooCommerceTests/ViewRelated/Coupons/AddEditCouponViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Coupons/AddEditCouponViewModelTests.swift
@@ -369,7 +369,20 @@ final class AddEditCouponViewModelTests: XCTestCase {
     }
     
     func test_validatePercentageAmountInput_ignores_validation_when_discountType_is_not_percent() {
-        
+        // Given
+        let coupon = Coupon.sampleCoupon.copy(amount: "200", discountType: .fixedCart)
+        let viewModel = AddEditCouponViewModel(
+                existingCoupon: coupon,
+                inputWarningDurationInSeconds: 0.1,
+                onSuccess: { _ in }
+        )
+
+        // When
+        let error = viewModel.validatePercentageAmountInput(withWarning: false)
+
+        // Then
+        XCTAssertNil(error)
+        XCTAssertEqual(viewModel.amountField, "200")
     }
     
     func test_validatePercentageAmountInput_returns_nil_if_amount_is_valid() {

ac36c0ed11f574f158e21ae6a05027738788fd64
Ernesto Carrion <ernesto.carrion@automattic.com>
2022-07-02 00:09:14 -0500

    Add view model support to show info messages about multiple lines

--- WooCommerce/Classes/ViewRelated/Orders/Order Creation/EditableOrderViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/EditableOrderViewModel.swift
@@ -104,6 +104,10 @@ final class EditableOrderViewModel: ObservableObject {
     /// Defines if the non editable indicators (banners, locks, fields) should be shown.
     @Published private(set) var shouldShowNonEditableIndicators: Bool = false
 
+    /// Defines the multiple lines info message to show.
+    ///
+    @Published private(set) var multipleLinesMessage: String? = nil
+
     /// Status Results Controller.
     ///
     private lazy var statusResultsController: ResultsController<StorageOrderStatus> = {
@@ -266,6 +270,7 @@ final class EditableOrderViewModel: ObservableObject {
         configurePaymentDataViewModel()
         configureCustomerNoteDataViewModel()
         configureNonEditableIndicators()
+        configureMultipleLinesMessage()
         resetAddressForm()
     }
 
@@ -725,6 +730,29 @@ private extension EditableOrderViewModel {
             .assign(to: &$shouldShowNonEditableIndicators)
     }
 
+    /// Binds the order state to the `multipleLineMessage` property.
+    ///
+    func configureMultipleLinesMessage() {
+        Publishers.CombineLatest(orderSynchronizer.orderPublisher, Just(flow))
+            .map { order, flow -> String? in
+                switch (flow, order.shippingLines.count, order.fees.count) {
+                case (.creation, _, _):
+                    return nil
+                case (.editing, 2...Int.max, 0...1): // Multiple shipping lines
+                    return Localization.multipleShippingLines
+                case (.editing, 0...1, 2...Int.max): // Multiple fee lines
+                    return Localization.multipleFeeLines
+                case (.editing, 2...Int.max, 2...Int.max): // Multiple shipping & fee lines
+                    return Localization.multipleFeesAndShippingLines
+                case (.editing, _, _): // Negative count - Should not happen!
+                    return nil
+                }
+            }
+            .assign(to: &$multipleLinesMessage)
+    }
+
+
+
     /// Tracks when customer details have been added
     ///
     func trackCustomerDetailsAdded() {
@@ -948,5 +976,15 @@ private extension EditableOrderViewModel {
         static let invalidBillingSuggestion =
         NSLocalizedString("Please make sure you are running the latest version of WooCommerce and try again later.",
                           comment: "Recovery suggestion when we fail to update an address when creating or editing an order")
+
+        static let multipleShippingLines = NSLocalizedString("Shipping details are incomplete.\n" +
+                                                             "To edit all shipping details, browse the order in your WooCommerce store admin.",
+                                                             comment: "Info message shown when the order contains multiple shipping lines")
+        static let multipleFeeLines = NSLocalizedString("Fees are incomplete.\n" +
+                                                        "To edit all fees, browse the order in your WooCommerce store admin.",
+                                                        comment: "Info message shown when the order contains multiple fee lines")
+        static let multipleFeesAndShippingLines = NSLocalizedString("Fees & Shipping details are incomplete.\n" +
+                                                                    "To edit all the details, browse the order in your WooCommerce store admin.",
+                                                                    comment: "Info message shown when the order contains multiple fees and shipping lines")
     }
 }
--- WooCommerce/WooCommerceTests/ViewRelated/Orders/Order Creation/EditableOrderViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Orders/Order Creation/EditableOrderViewModelTests.swift
@@ -1195,6 +1195,61 @@ final class EditableOrderViewModelTests: XCTestCase {
         // Then
         XCTAssertFalse(viewModel.shouldShowNonEditableIndicators)
     }
+
+    func test_zero_fees_and_shipping_lines_order_displays_nil_message() {
+        // Given
+        let order = Order.fake()
+
+        // When
+        let viewModel = EditableOrderViewModel(siteID: sampleSiteID, flow: .editing(initialOrder: order))
+
+        // Then
+        XCTAssertNil(viewModel.multipleLinesMessage)
+    }
+
+    func test_one_shipping_and_fee_line_order_displays_nil_message() {
+        // Given
+        let order = Order.fake().copy(shippingLines: [.fake()], fees: [.fake()])
+
+        // When
+        let viewModel = EditableOrderViewModel(siteID: sampleSiteID, flow: .editing(initialOrder: order))
+
+        // Then
+        XCTAssertNil(viewModel.multipleLinesMessage)
+    }
+
+    func test_multiple_shipping_line_order_displays_info_message() {
+        // Given
+        let order = Order.fake().copy(shippingLines: [.fake(), .fake()])
+
+        // When
+        let viewModel = EditableOrderViewModel(siteID: sampleSiteID, flow: .editing(initialOrder: order))
+
+        // Then
+        XCTAssertNotNil(viewModel.multipleLinesMessage)
+    }
+
+    func test_multiple_fee_line_order_displays_info_message() {
+        // Given
+        let order = Order.fake().copy(fees: [.fake(), .fake()])
+
+        // When
+        let viewModel = EditableOrderViewModel(siteID: sampleSiteID, flow: .editing(initialOrder: order))
+
+        // Then
+        XCTAssertNotNil(viewModel.multipleLinesMessage)
+    }
+
+    func test_multiple_shipping_and_fee_line_order_displays_info_message() {
+        // Given
+        let order = Order.fake().copy(shippingLines: [.fake(), .fake()], fees: [.fake(), .fake()])
+
+        // When
+        let viewModel = EditableOrderViewModel(siteID: sampleSiteID, flow: .editing(initialOrder: order))
+
+        // Then
+        XCTAssertNotNil(viewModel.multipleLinesMessage)
+    }
 }
 
 private extension MockStorageManager {

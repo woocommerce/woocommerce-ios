0f424f18e584536608f97feeb0d2d28de33d9f6b
ThomazFB <thomazfcortez@gmail.com>
2022-07-06 21:02:19 -0300

    Create OrderMetadata SwiftUI file

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Order Metadata/OrderMetadata.swift
@@ -0,0 +1,21 @@
+//
+//  OrderMetadata.swift
+//  WooCommerce
+//
+//  Created by Thomaz F B Cortez on 06/07/22.
+//  Copyright © 2022 Automattic. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderMetadata: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderMetadata_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderMetadata()
+    }
+}

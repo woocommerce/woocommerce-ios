100a3ebd312457f9fa027b3b848e784ab9446b50
ThomazFB <thomazfcortez@gmail.com>
2022-07-07 20:41:20 -0300

    Rename OrderMetadata SwiftUI view to OrderMetadataDetails

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Order Metadata/OrderMetadataDetails.swift
@@ -0,0 +1,14 @@
+import Yosemite
+import SwiftUI
+
+struct OrderMetadataDetails: View {
+    var body: some View {
+        Text("Order metadata goes here")
+    }
+}
+
+struct OrderMetadataDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderMetadataDetails()
+    }
+}

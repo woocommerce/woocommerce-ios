19ba98c4f6fe138b3bf5847f1533c360000c968f
rachelmcr <rachel@automattic.com>
2022-07-07 14:16:50 +0100

    Add Order customFields relationship to codegen properties

---
+++ Storage/Storage/Model/OrderMetaData+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+import Foundation
+import CoreData
+
+
+extension OrderMetaData {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<OrderMetaData> {
+        return NSFetchRequest<OrderMetaData>(entityName: "OrderMetaData")
+    }
+
+    @NSManaged public var key: String?
+    @NSManaged public var value: String?
+    @NSManaged public var metadataID: Int64
+    @NSManaged public var order: Order?
+
+}
+
+extension OrderMetaData : Identifiable {
+
+}

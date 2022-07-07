af8db26f4b70425c9adddb9821de386db2e0fcff
Jaclyn Chen <watertranquil@gmail.com>
2022-07-07 10:13:45 -0400

    Update the temporary URL to point to a guide on WC.com.

--- WooCommerce/Classes/Authentication/Prologue/LoginPrologueViewController.swift
+++ WooCommerce/Classes/Authentication/Prologue/LoginPrologueViewController.swift
@@ -127,7 +127,7 @@ private extension LoginPrologueViewController {
     enum Constants {
         static let spacingBetweenCarouselAndNewToWooCommerceButton: CGFloat = 20
         // TODO: 7231 - update URL if needed
-        static let newToWooCommerceURL = "https://wordpress.com/support/introduction-to-woocommerce"
+        static let newToWooCommerceURL = "https://woocommerce.com/guides/new-store"
     }
 
     enum Localization {

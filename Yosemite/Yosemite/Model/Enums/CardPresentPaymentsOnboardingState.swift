47f432c54bf1fd747a09a9f173070120a885f851
Gabriel Maldonado <gabrielmaldonado.art@gmail.com>
2022-07-08 16:51:29 +0700

    Typo in “multiple_payment_providers_conflict”

--- Yosemite/Yosemite/Model/Enums/CardPresentPaymentsOnboardingState.swift
+++ Yosemite/Yosemite/Model/Enums/CardPresentPaymentsOnboardingState.swift
@@ -84,7 +84,7 @@ extension CardPresentPaymentOnboardingState {
         case .completed:
             return nil
         case .selectPlugin:
-            return "multiple_payment_provider_conflict"
+            return "multiple_payment_providers_conflict"
         case .pluginShouldBeDeactivated:
             return "plugin_should_be_deactivated"
         case .countryNotSupported(countryCode: _):

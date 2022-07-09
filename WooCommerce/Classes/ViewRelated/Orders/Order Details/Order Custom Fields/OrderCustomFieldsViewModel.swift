3b04c9e4ddd74b9626e4128806868d9e885d65e5
ThomazFB <thomazfcortez@gmail.com>
2022-07-09 01:51:34 -0300

    Expand OrderCustomFieldsViewModel initializers

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Order Custom Fields/OrderCustomFieldsViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Order Custom Fields/OrderCustomFieldsViewModel.swift
@@ -4,18 +4,26 @@ import Yosemite
 /// ViewModel for `OrderCustomFieldsDetails`
 struct OrderCustomFieldsViewModel: Identifiable {
     let id: Int64
-    
+
     /// The title for the Custom Field mapped from the metadata key
     ///
     let title: String
-    
+
     /// The content for the Custom Field mapped from the metadata value
     ///
     let content: String
-    
+
+    init(id: Int64, title: String, content: String) {
+        self.id = id
+        self.title = title
+        self.content = content
+    }
+
     init(metadata: OrderMetaData) {
-        self.id = metadata.metadataID
-        self.title = metadata.key
-        self.content = metadata.value
+        self.init(
+            id: metadata.metadataID,
+            title: metadata.key,
+            content: metadata.value
+        )
     }
 }

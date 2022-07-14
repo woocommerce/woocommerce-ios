18b4ef040a67f6604b10e9a788c059df9d92053b
Jaclyn Chen <watertranquil@gmail.com>
2022-07-14 10:43:55 -0400

    Create a new view controller for login onboarding.

---
+++ WooCommerce/Classes/Authentication/Prologue/LoginOnboardingViewController.swift
@@ -0,0 +1,136 @@
+import UIKit
+
+/// Contains a feature carousel with buttons that end up on the login prologue screen.
+final class LoginOnboardingViewController: UIViewController {
+    private let stackView: UIStackView = .init()
+    private let onDismiss: () -> Void
+
+    init(onDismiss: @escaping () -> Void) {
+        self.onDismiss = onDismiss
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureMainView()
+        configureCurvedImageView()
+        configureWooLogo()
+        configureStackView()
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIDevice.isPad() ? .all : .portrait
+    }
+}
+
+private extension LoginOnboardingViewController {
+    func configureMainView() {
+        view.backgroundColor = .basicBackground
+
+        let bottomView = UIView(frame: .zero)
+        bottomView.backgroundColor = .authPrologueBottomBackgroundColor
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bottomView)
+        NSLayoutConstraint.activate([
+            bottomView.leadingAnchor.constraint(equalTo: view.safeLeadingAnchor),
+            bottomView.trailingAnchor.constraint(equalTo: view.safeTrailingAnchor),
+            bottomView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3),
+            bottomView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+    func configureCurvedImageView() {
+        let imageView = UIImageView(image: .curvedRectangle)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: view.safeTopAnchor, constant: 52),
+            imageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.7)
+        ])
+    }
+
+    func configureWooLogo() {
+        let imageView = UIImageView(image: .wooLogoPrologueImage)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 176),
+            imageView.heightAnchor.constraint(equalToConstant: 36),
+            imageView.topAnchor.constraint(equalTo: view.safeTopAnchor, constant: 28)
+        ])
+    }
+
+    func configureStackView() {
+        configureStackViewSubviews()
+
+        stackView.axis = .vertical
+        stackView.spacing = Constants.stackViewSpacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        view.pinSubviewToSafeArea(stackView, insets: Constants.stackViewInsets)
+    }
+
+    func configureStackViewSubviews() {
+        let carousel = createFeatureCarousel()
+
+        let continueButton = createContinueButton()
+        let skipButton = createSkipButton()
+        let buttonStackView: UIStackView = {
+            let stackView = UIStackView(arrangedSubviews: [continueButton, skipButton])
+            stackView.spacing = Constants.buttonStackViewSpacing
+            stackView.axis = .vertical
+            return stackView
+        }()
+
+        stackView.addArrangedSubviews([carousel, buttonStackView])
+    }
+
+    func createFeatureCarousel() -> UIView {
+        let carousel = LoginProloguePageViewController()
+        carousel.view.translatesAutoresizingMaskIntoConstraints = false
+
+        addChild(carousel)
+        return carousel.view
+    }
+
+    func createContinueButton() -> UIButton {
+        let button = UIButton(frame: .zero)
+        button.applyPrimaryButtonStyle()
+        button.setTitle(Localization.continueButtonTitle, for: .normal)
+        button.on(.touchUpInside) { [weak self] _ in
+            self?.onDismiss()
+        }
+        return button
+    }
+
+    func createSkipButton() -> UIButton {
+        let button = UIButton(frame: .zero)
+        button.applyLinkButtonStyle()
+        button.setTitle(Localization.skipButtonTitle, for: .normal)
+        button.on(.touchUpInside) { [weak self] _ in
+            self?.onDismiss()
+        }
+        return button
+    }
+}
+
+private extension LoginOnboardingViewController {
+    enum Localization {
+        static let continueButtonTitle = NSLocalizedString("Next", comment: "The button title on the login onboarding screen to continue to the next step.")
+        static let skipButtonTitle = NSLocalizedString("Skip", comment: "The button title on the login onboarding screen to skip to the prologue screen.")
+    }
+
+    enum Constants {
+        static let stackViewInsets: UIEdgeInsets = .init(top: 0, left: 16, bottom: 20, right: 16)
+        static let stackViewSpacing: CGFloat = 40
+        static let buttonStackViewSpacing: CGFloat = 16
+    }
+}

ffc2e57e3a8e9e3825027823eabf715ade9a1b2f
Huong Do <hi.huongdo@gmail.com>
2022-07-15 16:27:13 +0700

    Add mobile redirect handler

--- WooCommerce/Classes/Authentication/Navigation Exceptions/JetpackConnectionWebViewController.swift
+++ WooCommerce/Classes/Authentication/Navigation Exceptions/JetpackConnectionWebViewController.swift
@@ -32,24 +32,35 @@ private extension JetpackConnectionWebViewController {
         view.pinSubviewToSafeArea(webView)
 
         guard let escapedSiteURL = siteURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-              let url = URL(string: String(format: Strings.jetpackInstallString, escapedSiteURL)) else {
+              let url = URL(string: String(format: Constants.jetpackInstallString, escapedSiteURL, Constants.mobileRedirectURL)) else {
             return
         }
 
         let request = URLRequest(url: url)
         webView.load(request)
     }
+
+    func handleMobileRedirect() {
+        // TODO-7269
+    }
 }
 
 extension JetpackConnectionWebViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        DDLogInfo("🔵 URL: \(navigationAction.request.url?.absoluteString ?? "")")
-        decisionHandler(.allow)
+        let navigationURL = navigationAction.request.url?.absoluteString
+        switch navigationURL {
+        case let .some(url) where url == Constants.mobileRedirectURL:
+            decisionHandler(.cancel)
+            handleMobileRedirect()
+        default:
+            decisionHandler(.allow)
+        }
     }
 }
 
 private extension JetpackConnectionWebViewController {
-    enum Strings {
-        static let jetpackInstallString = "https://wordpress.com/jetpack/connect?url=%@&from=mobile"
+    enum Constants {
+        static let jetpackInstallString = "https://wordpress.com/jetpack/connect?url=%@&mobie-redirect=%@from=mobile"
+        static let mobileRedirectURL = "woocommerce://jetpack-connected"
     }
 }

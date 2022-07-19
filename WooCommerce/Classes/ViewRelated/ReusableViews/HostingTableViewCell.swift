07bf695a741dd643e810bf87b4b56db6306e27a6
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2022-07-19 15:25:40 +0200

    Add comments and missing import

--- WooCommerce/Classes/ViewRelated/ReusableViews/HostingTableViewCell.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/HostingTableViewCell.swift
@@ -1,5 +1,8 @@
 import SwiftUI
+import UIKit
 
+/// Use this cell to host a SwiftUI view into a `UITableViewCell` so it can be displayed in a `UITableView` instance
+///
 class HostingTableViewCell<Content: View>: UITableViewCell {
     private weak var controller: UIHostingController<Content>?
 
@@ -8,52 +11,59 @@ class HostingTableViewCell<Content: View>: UITableViewCell {
             controller.rootView = view
             controller.view.layoutIfNeeded()
         } else {
-            let swiftUICellViewController = UIHostingController(rootView: view)
-
-            guard let swiftUICellView = swiftUICellViewController.view else {
-                return
-            }
-
-            controller = swiftUICellViewController
-            swiftUICellView.backgroundColor = .clear
-
-            layoutIfNeeded()
-
-            parent.addChild(swiftUICellViewController)
-            contentView.addSubview(swiftUICellView)
-            swiftUICellView.translatesAutoresizingMaskIntoConstraints = false
-
-            contentView.addConstraint(NSLayoutConstraint(item: swiftUICellView,
-                                                         attribute: NSLayoutConstraint.Attribute.leading,
-                                                         relatedBy: NSLayoutConstraint.Relation.equal,
-                                                         toItem: contentView,
-                                                         attribute: NSLayoutConstraint.Attribute.leading,
-                                                         multiplier: 1.0,
-                                                         constant: 0.0))
-            contentView.addConstraint(NSLayoutConstraint(item: swiftUICellView,
-                                                         attribute: NSLayoutConstraint.Attribute.trailing,
-                                                         relatedBy: NSLayoutConstraint.Relation.equal,
-                                                         toItem: contentView,
-                                                         attribute: NSLayoutConstraint.Attribute.trailing,
-                                                         multiplier: 1.0,
-                                                         constant: 0.0))
-            contentView.addConstraint(NSLayoutConstraint(item: swiftUICellView,
-                                                         attribute: NSLayoutConstraint.Attribute.top,
-                                                         relatedBy: NSLayoutConstraint.Relation.equal,
-                                                         toItem: contentView,
-                                                         attribute: NSLayoutConstraint.Attribute.top,
-                                                         multiplier: 1.0,
-                                                         constant: 0.0))
-            contentView.addConstraint(NSLayoutConstraint(item: swiftUICellView,
-                                                         attribute: NSLayoutConstraint.Attribute.bottom,
-                                                         relatedBy: NSLayoutConstraint.Relation.equal,
-                                                         toItem: contentView,
-                                                         attribute: NSLayoutConstraint.Attribute.bottom,
-                                                         multiplier: 1.0,
-                                                         constant: 0.0))
-
-            swiftUICellViewController.didMove(toParent: parent)
-            swiftUICellView.layoutIfNeeded()
+            setupController(with: view, parent: parent)
         }
+    }
+
+    private func setupController(with view: Content, parent: UIViewController) {
+        let swiftUICellViewController = UIHostingController(rootView: view)
+
+        guard let swiftUICellView = swiftUICellViewController.view else {
+            return
+        }
+
+        controller = swiftUICellViewController
+        swiftUICellView.backgroundColor = .clear
+
+        parent.addChild(swiftUICellViewController)
+        contentView.addSubview(swiftUICellView)
+
+        setupConstraints(for: swiftUICellView)
+
+        swiftUICellViewController.didMove(toParent: parent)
+        swiftUICellView.layoutIfNeeded()
+    }
+
+    private func setupConstraints(for view: UIView) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        contentView.addConstraint(NSLayoutConstraint(item: view,
+                                                     attribute: NSLayoutConstraint.Attribute.leading,
+                                                     relatedBy: NSLayoutConstraint.Relation.equal,
+                                                     toItem: contentView,
+                                                     attribute: NSLayoutConstraint.Attribute.leading,
+                                                     multiplier: 1.0,
+                                                     constant: 0.0))
+        contentView.addConstraint(NSLayoutConstraint(item: view,
+                                                     attribute: NSLayoutConstraint.Attribute.trailing,
+                                                     relatedBy: NSLayoutConstraint.Relation.equal,
+                                                     toItem: contentView,
+                                                     attribute: NSLayoutConstraint.Attribute.trailing,
+                                                     multiplier: 1.0,
+                                                     constant: 0.0))
+        contentView.addConstraint(NSLayoutConstraint(item: view,
+                                                     attribute: NSLayoutConstraint.Attribute.top,
+                                                     relatedBy: NSLayoutConstraint.Relation.equal,
+                                                     toItem: contentView,
+                                                     attribute: NSLayoutConstraint.Attribute.top,
+                                                     multiplier: 1.0,
+                                                     constant: 0.0))
+        contentView.addConstraint(NSLayoutConstraint(item: view,
+                                                     attribute: NSLayoutConstraint.Attribute.bottom,
+                                                     relatedBy: NSLayoutConstraint.Relation.equal,
+                                                     toItem: contentView,
+                                                     attribute: NSLayoutConstraint.Attribute.bottom,
+                                                     multiplier: 1.0,
+                                                     constant: 0.0))
     }
 }

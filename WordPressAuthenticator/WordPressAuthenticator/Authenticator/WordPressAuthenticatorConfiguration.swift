91a073cd34c6dcb22a64bf1d51f83e796efa29f1
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-07-26 12:34:12 +0530

    Remove comment about WP and WC iOS apps behaviour.

--- WordPressAuthenticator/WordPressAuthenticator/Authenticator/WordPressAuthenticatorConfiguration.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Authenticator/WordPressAuthenticatorConfiguration.swift
@@ -101,7 +101,6 @@ public struct WordPressAuthenticatorConfiguration {
     let enableSiteCredentialsLoginInGetStartedScreen: Bool
 
     /// If enabled, we will ask for WPCOM login after signing in using .org site credentials.
-    ///  WordPress iOS app can proceed without WPCOM login. But, WooCommerce iOS app needs WPCOM login.
     ///  Disabled by default
     let isWordPressComCredentialsRequired: Bool
 

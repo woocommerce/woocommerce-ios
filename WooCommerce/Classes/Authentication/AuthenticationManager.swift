587ebb1cfc3e791f0400d7db6029e48888bbeb93
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-07-26 12:44:04 +0530

    Rename WPCOM login config property.

--- WooCommerce/Classes/Authentication/AuthenticationManager.swift
+++ WooCommerce/Classes/Authentication/AuthenticationManager.swift
@@ -47,7 +47,7 @@ class AuthenticationManager: Authentication {
                                                                 enableUnifiedAuth: true,
                                                                 continueWithSiteAddressFirst: true,
                                                                 enableSiteCredentialsLoginForSelfHostedSites: true,
-                                                                isWordPressComCredentialsRequired: true)
+                                                                isWPComLoginRequiredForSiteCredentialsLogin: true)
 
         let systemGray3LightModeColor = UIColor(red: 199/255.0, green: 199/255.0, blue: 204/255.0, alpha: 1)
         let systemLabelLightModeColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)

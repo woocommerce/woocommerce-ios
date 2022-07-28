81b81346d1df796d360f5542516b5e24918ecf5d
Gabriel Maldonado <gabrielmaldonado.art@gmail.com>
2022-07-28 11:27:47 +0200

    Remove Collection+extensions from array+woo

--- WooFoundation/WooFoundation/Extensions/Collection+Extensions.swift
+++ WooFoundation/WooFoundation/Extensions/Collection+Extensions.swift
@@ -3,7 +3,7 @@ import Foundation
 extension Collection {
     /// Returns the element at the specified index if it is within bounds, otherwise nil.
     ///
-    subscript (safe index: Index) -> Element? {
+    public subscript (safe index: Index) -> Element? {
         return indices.contains(index) ? self[index] : nil
     }
 }

f6c9570f9815a7ead6d94dae3dca1f84b3c724c7
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2022-07-28 14:44:03 +0200

    Add a feature flag for payments in hub menu so we can hide this feature until it is complete

--- Experiments/Experiments/FeatureFlag.swift
+++ Experiments/Experiments/FeatureFlag.swift
@@ -101,4 +101,8 @@ public enum FeatureFlag: Int {
     /// Local notifications scheduled 24 hours after certain login errors
     ///
     case loginErrorNotifications
+
+    /// Payments Section in the Hub Menu
+    ///
+    case paymentsHubMenuSection
 }

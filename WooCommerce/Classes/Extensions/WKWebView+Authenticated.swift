7693c158d2381c6d332f7e7b4ac557583a9157d8
Huong Do <hi.huongdo@gmail.com>
2022-08-02 17:59:36 +0700

    Remove sandbox env configuration

--- WooCommerce/Classes/Extensions/WKWebView+Authenticated.swift
+++ WooCommerce/Classes/Extensions/WKWebView+Authenticated.swift
@@ -9,7 +9,6 @@ import class Networking.UserAgent
 extension WKWebView {
     func authenticateForWPComAndRedirect(to url: URL, credentials: Credentials?) {
         customUserAgent = UserAgent.defaultUserAgent
-        configureForSandboxEnvironment()
         do {
             try load(authenticatedPostData(with: credentials, redirectTo: url))
         } catch {
@@ -32,23 +31,5 @@ extension WKWebView {
                           "authorization": "Bearer " + token]
 
         return try URLEncoding.default.encode(request, with: parameters)
-    }
-
-    /// For all test cases, to test against the staging server
-    /// please apply the following patch after replacing [secret] with a sandbox secret from the secret store.
-    ///
-    private func configureForSandboxEnvironment() {
-        #if DEBUG
-        if let cookie = HTTPCookie(properties: [
-            .domain: ".wordpress.com",
-            .path: "/",
-            .name: "store_sandbox",
-            .value: "[secret]",
-            .secure: "TRUE"
-        ]) {
-            configuration.websiteDataStore.httpCookieStore.setCookie(cookie) {
-            }
-        }
-        #endif
     }
 }

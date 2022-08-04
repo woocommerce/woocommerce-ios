926c37a8c5580b8a6f4b8d6485966d194c374979
ThomazFB <thomazfcortez@gmail.com>
2022-08-04 00:07:51 -0300

    Define WaitingTimeTracker State and basic interface

--- WooCommerce/Classes/Analytics/WaitingTimeTracker.swift
+++ WooCommerce/Classes/Analytics/WaitingTimeTracker.swift
@@ -1,5 +1,24 @@
 import Foundation
+import Combine
 
 class WaitingTimeTracker {
+    @Published var currentState: State = .Idle(0.0)
 
+    func onWaitingStarted() {
+    }
+
+    func onWaitingEnded() {
+    }
+
+    enum State {
+        let creationTimestamp: TimeInterval
+
+        init(creationTimestamp: TimeInterval? = nil) {
+            self.creationTimestamp = creationTimestamp ?? NSDate().timeIntervalSince1970
+        }
+
+        case Idle
+        case Waiting
+        case Done
+    }
 }

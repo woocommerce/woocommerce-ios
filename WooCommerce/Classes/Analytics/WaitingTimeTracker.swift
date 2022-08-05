c96d83db6e64f1869490480352905101207ca904
ThomazFB <thomazfcortez@gmail.com>
2022-08-05 02:12:57 -0300

    Simplify WaitingTimeTracker

--- WooCommerce/Classes/Analytics/WaitingTimeTracker.swift
+++ WooCommerce/Classes/Analytics/WaitingTimeTracker.swift
@@ -2,14 +2,13 @@ import Foundation
 import Combine
 
 /// Tracks the waiting time for a given scenario, allowing to evaluate as analytics
-/// how much time in seconds it took between the `start` and `end` function calls
+/// how much time in seconds it took between the init and `end` function call
 ///
 class WaitingTimeTracker {
     private let trackScenario: WooAnalyticsEvent.WaitingTime.Scenario
     private let currentTimeInMillis: () -> TimeInterval
     private let analyticsService: Analytics
-
-    private var waitingStartedTimestamp: TimeInterval? = nil
+    private let waitingStartedTimestamp: TimeInterval
 
     init(trackScenario: WooAnalyticsEvent.WaitingTime.Scenario,
          analyticsService: Analytics = ServiceLocator.analytics,
@@ -18,13 +17,10 @@ class WaitingTimeTracker {
         self.trackScenario = trackScenario
         self.analyticsService = analyticsService
         self.currentTimeInMillis = currentTimeInMillis
-    }
-
-    func start() {
         waitingStartedTimestamp = currentTimeInMillis()
     }
 
-    /// End the waiting time by evaluating the elapsed time between `start` and `end`,
+    /// End the waiting time by evaluating the elapsed time from the init,
     /// and sending it as an analytics event.
     ///
     func end() {

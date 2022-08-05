467bf4aac01ddb63333d62865881bf31a0a6f9b8
ThomazFB <thomazfcortez@gmail.com>
2022-08-05 00:56:54 -0300

    Define currentTimeInMillis closure for better testing capabilities

--- WooCommerce/Classes/Analytics/WaitingTimeTracker.swift
+++ WooCommerce/Classes/Analytics/WaitingTimeTracker.swift
@@ -3,13 +3,17 @@ import Combine
 
 class WaitingTimeTracker {
     private let trackEvent: WooAnalyticsStat
+    private let currentTimeInMillis: () -> TimeInterval
 
     private var waitingStartedTimestamp: TimeInterval? = nil
 
     /// Initialize the WaitingTimeTracker with a specific timeout, if none is provided it will set 30 seconds as the default
     ///
-    init(trackEvent: WooAnalyticsStat) {
+    init(trackEvent: WooAnalyticsStat,
+         currentTimeInMillis: @escaping () -> TimeInterval = { Date().timeIntervalSince1970 }
+    ) {
         self.trackEvent = trackEvent
+        self.currentTimeInMillis = currentTimeInMillis
     }
 
     /// Set the Tracker state to `.waiting`, triggering the waiting cycle
@@ -17,7 +21,7 @@ class WaitingTimeTracker {
     /// - parameter analyticsStat: The stat to be send to Tracks when the waiting time ends
     ///
     func onWaitingStarted(analyticsStat: WooAnalyticsStat) {
-        waitingStartedTimestamp = NSDate().timeIntervalSince1970
+        waitingStartedTimestamp = currentTimeInMillis()
     }
 
     /// Set the Tracker state to `.done`, ending the waiting cycle. Only accepts it if the current state is `.waiting`
@@ -28,7 +32,7 @@ class WaitingTimeTracker {
             return
         }
 
-        let elapsedTime = NSDate().timeIntervalSince1970 - waitingStartedTimestamp
+        let elapsedTime = currentTimeInMillis() - waitingStartedTimestamp
         ServiceLocator.analytics.track(trackEvent, withProperties: [
             "waiting_time": elapsedTime
         ])

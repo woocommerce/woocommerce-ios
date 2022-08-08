0f07ba5b252edf46f213142006ced0b5ad3d502a
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-08-08 18:41:14 +0530

    Remove unused method.

--- WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/TextLabelTableViewCell.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Unified Auth/View Related/Reusable Views/TextLabelTableViewCell.swift
@@ -25,16 +25,11 @@ public final class TextLabelTableViewCell: UITableViewCell {
         }
     }
 
-    public func configureLabel(attributedText: NSAttributedString) {
-        label.attributedText = attributedText
-    }
-
     /// Override methods
     ///
     public override func prepareForReuse() {
         super.prepareForReuse()
         label.text = nil
-        label.attributedText = nil
     }
 }
 

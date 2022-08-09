a3fca859f82c6d74b26029dfbbbfd019d0b52759
Huong Do <hi.huongdo@gmail.com>
2022-08-09 11:00:15 +0700

    Update results controller for store picker based on configuration

--- WooCommerce/Classes/Authentication/Epilogue/StorePickerViewModel.swift
+++ WooCommerce/Classes/Authentication/Epilogue/StorePickerViewModel.swift
@@ -8,10 +8,10 @@ final class StorePickerViewModel {
     /// ResultsController: Loads Sites from the Storage Layer.
     ///
     private lazy var resultsController: ResultsController<StorageSite> = {
-        let predicate = NSPredicate(format: "isWooCommerceActive == YES")
-        let descriptor = NSSortDescriptor(key: "name", ascending: true)
-
-        return ResultsController(storageManager: storageManager, matching: predicate, sortedBy: [descriptor])
+        return ResultsController(storageManager: storageManager,
+                                 sectionNameKeyPath: configuration.sectionNameKeyPath,
+                                 matching: configuration.predicate,
+                                 sortedBy: configuration.sortDescriptors)
     }()
 
     /// Selected configuration for the store picker
@@ -26,5 +26,42 @@ final class StorePickerViewModel {
          storageManager: StorageManagerType = ServiceLocator.storageManager) {
         self.configuration = configuration
         self.storageManager = storageManager
+    }
+
+    func refreshSites() {
+        try? resultsController.performFetch()
+    }
+}
+
+// MARK: - Extension to set up results controller for the store picker
+//
+private extension StorePickerConfiguration {
+    var sectionNameKeyPath: String? {
+        switch self {
+        case .switchingStores:
+            return nil
+        default:
+            return "isWooCommerceActive"
+        }
+    }
+
+    var predicate: NSPredicate? {
+        switch self {
+        case .switchingStores:
+            return NSPredicate(format: "isWooCommerceActive == YES")
+        default:
+            return nil
+        }
+    }
+
+    var sortDescriptors: [NSSortDescriptor] {
+        let nameDescriptor = NSSortDescriptor(keyPath: \StorageSite.name, ascending: true)
+        let wooDescriptor = NSSortDescriptor(keyPath: \StorageSite.isWooCommerceActive, ascending: false)
+        switch self {
+        case .switchingStores:
+            return [nameDescriptor]
+        default:
+            return [wooDescriptor, nameDescriptor]
+        }
     }
 }

1ce7b5e09c1d59dea2254d26214ad1694444c966
iamgabrielma <gabrielmaldonado.art@gmail.com>
2022-08-10 18:04:30 +0200

    Style BottomAnnouncement View

--- WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/BottomAnnouncementView.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/BottomAnnouncementView.swift
@@ -2,28 +2,52 @@ import SwiftUI
 
 struct BottomAnnouncementView: View {
     @Environment(\.presentationMode) private var presentation
+
     var completionHandler: (() -> Void)?
 
     var body: some View {
-        GeometryReader { geo in
-            VStack {
-                Text(Localization.title)
-                Text(Localization.message)
-                Button(Localization.okButton) {
-                    if let completionHandler = completionHandler {
-                        completionHandler()
-                    }
-                    presentation.wrappedValue.dismiss()
+        VStack(alignment: .leading) {
+            Spacer()
+            Text(Localization.title)
+                .font(.headline)
+                .padding(.bottom, Layout.titleFontSize)
+            Text(Localization.message)
+                .font(.caption)
+                .foregroundColor(.gray)
+                .padding(.bottom, Layout.messageFontSize)
+                .multilineTextAlignment(.leading)
+                .fixedSize(horizontal: false, vertical: true)
+            Button(Localization.confirmationButton) {
+                if let completionHandler = completionHandler {
+                    completionHandler()
                 }
+                presentation.wrappedValue.dismiss()
             }
+            .buttonStyle(SecondaryButtonStyle())
+            .padding(.bottom, Layout.bottomPadding)
         }
+        .padding(.leading, Layout.horizontalPadding)
+        .padding(.trailing, Layout.horizontalPadding)
+        .padding(.top, Layout.verticalPadding)
+        .padding(.bottom, Layout.verticalPadding)
+        .background(RoundedRectangle(cornerRadius: Layout.cornerRadius)
+            .fill(Color(.withColorStudio(.gray, shade: .shade0))))
     }
 }
 
 extension BottomAnnouncementView {
     enum Localization {
-        static let title = NSLocalizedString("Payments from the Menu tab", comment: "Text comment")
-        static let message = NSLocalizedString("Now you can quickly access In-Person Payments and other features with ease.", comment: "Comment")
-        static let okButton = NSLocalizedString("Got it!", comment: "comment")
+        static let title = NSLocalizedString("Payments from the Menu tab", comment: "Title of the bottom announcement modal when a merchant taps on Simple Payment")
+        static let message = NSLocalizedString("Now you can quickly access In-Person Payments and other features with ease.", comment: "Message of the bottom announcement modal when a merchant taps on Simple Payment")
+        static let confirmationButton = NSLocalizedString("Got it!", comment: "Confirmation text of the button on the bottom announcement modal when a merchant taps on Simple Payment")
+    }
+
+    enum Layout {
+        static let titleFontSize: CGFloat = 16
+        static let messageFontSize: CGFloat = 16
+        static let horizontalPadding: CGFloat = 6
+        static let verticalPadding: CGFloat = 4
+        static let bottomPadding: CGFloat = 6
+        static let cornerRadius: CGFloat = 12
     }
 }

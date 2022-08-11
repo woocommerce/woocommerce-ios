7082a0f43ce975e99a6bc6a8ab7705f0fe8a7af3
Jaclyn Chen <watertranquil@gmail.com>
2022-08-11 21:47:26 +0800

    Update A/A test case name to be consistent with the other A/A test with a design ref.

--- Experiments/Experiments/ABTest.swift
+++ Experiments/Experiments/ABTest.swift
@@ -13,7 +13,8 @@ public enum ABTest: String, CaseIterable {
     case aaTest202208 = "woocommerceios_explat_aa_test_202208"
 
     /// A/A test to make sure there is no bias in the logged out state.
-    case loggedOutAATest = "woocommerceios_explat_aa_test_logged_out_202208"
+    /// Experiment ref: pbxNRc-1S0-p2
+    case aaTestLoggedOut202208 = "woocommerceios_explat_aa_test_logged_out_202208"
 
     /// Returns a variation for the given experiment
     public var variation: Variation {

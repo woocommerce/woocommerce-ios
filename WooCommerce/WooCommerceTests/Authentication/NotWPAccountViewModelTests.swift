776280995c393382e853fab9dd1cfeb534649166
Jaclyn Chen <watertranquil@gmail.com>
2022-08-16 21:10:08 +0800

    NotWPAccountViewModel: update unit tests based on the button style changes.

--- WooCommerce/WooCommerceTests/Authentication/NotWPAccountViewModelTests.swift
+++ WooCommerce/WooCommerceTests/Authentication/NotWPAccountViewModelTests.swift
@@ -57,28 +57,52 @@ final class NotWPAccountViewModelTests: XCTestCase {
         let primaryButtonTitle = viewModel.primaryButtonTitle
 
         // Then
-        XCTAssertEqual(primaryButtonTitle, Expectations.primaryButtonTitle)
+        XCTAssertEqual(primaryButtonTitle, Expectations.loginWithSiteAddressTitle)
     }
 
-    // MARK: - `isPrimaryButtonHidden`
+    // MARK: - `primaryButtonTitle`
 
-    func test_viewmodel_primary_button_is_hidden_for_invalidWPComEmail_from_site_address_error() {
+    func test_primary_button_title_is_restart_login_for_invalidWPComEmail_from_site_address_error() {
         // Given
         let viewModel = NotWPAccountViewModel(error: SignInError.invalidWPComEmail(source: .wpComSiteAddress))
 
         // When
-        let isPrimaryButtonHidden = viewModel.isPrimaryButtonHidden
+        let primaryButtonTitle = viewModel.primaryButtonTitle
+
+        // Then
+        XCTAssertEqual(primaryButtonTitle, Expectations.restartLoginTitle)
+    }
+
+    func test_primary_button_title_is_login_with_site_address_for_invalidWPComEmail_from_wpCom_error() {
+        // Given
+        let viewModel = NotWPAccountViewModel(error: SignInError.invalidWPComEmail(source: .wpCom))
+
+        // When
+        let primaryButtonTitle = viewModel.primaryButtonTitle
+
+        // Then
+        XCTAssertEqual(primaryButtonTitle, Expectations.loginWithSiteAddressTitle)
+    }
+
+    // MARK: - `isSecondaryButtonHidden`
+
+    func test_secondary_button_is_hidden_for_invalidWPComEmail_from_site_address_error() {
+        // Given
+        let viewModel = NotWPAccountViewModel(error: SignInError.invalidWPComEmail(source: .wpComSiteAddress))
+
+        // When
+        let isPrimaryButtonHidden = viewModel.isSecondaryButtonHidden
 
         // Then
         XCTAssertTrue(isPrimaryButtonHidden)
     }
 
-    func test_viewmodel_primary_button_is_not_hidden_for_invalidWPComEmail_from_wpCom_error() {
+    func test_secondary_button_is_not_hidden_for_invalidWPComEmail_from_wpCom_error() {
         // Given
         let viewModel = NotWPAccountViewModel(error: SignInError.invalidWPComEmail(source: .wpCom))
 
         // When
-        let isPrimaryButtonHidden = viewModel.isPrimaryButtonHidden
+        let isPrimaryButtonHidden = viewModel.isSecondaryButtonHidden
 
         // Then
         XCTAssertFalse(isPrimaryButtonHidden)
@@ -94,7 +118,7 @@ final class NotWPAccountViewModelTests: XCTestCase {
         let secondaryButtonTitle = viewModel.secondaryButtonTitle
 
         // Then
-        XCTAssertEqual(secondaryButtonTitle, Expectations.secondaryButtonTitle)
+        XCTAssertEqual(secondaryButtonTitle, Expectations.restartLoginTitle)
     }
 }
 
@@ -106,13 +130,13 @@ private extension NotWPAccountViewModelTests {
                                                     comment: "Message explaining that an email is not associated with a WordPress.com account. "
                                                         + "Presented when logging in with an email address that is not a WordPress.com account")
 
-        static let primaryButtonTitle = NSLocalizedString("Log in with your store address",
-                                                          comment: "Action button linking to instructions for enter another store."
-                                                          + "Presented when logging in with an email address that is not a WordPress.com account")
+        static let loginWithSiteAddressTitle = NSLocalizedString("Log in with your store address",
+                                                                 comment: "Action button linking to instructions for enter another store."
+                                                                 + "Presented when logging in with an email address that is not a WordPress.com account")
 
-        static let secondaryButtonTitle = NSLocalizedString("Log in with another account",
-                                                            comment: "Action button that will restart the login flow."
-                                                            + "Presented when logging in with an email address that does not match a WordPress.com account")
+        static let restartLoginTitle = NSLocalizedString("Log in with another account",
+                                                         comment: "Action button that will restart the login flow."
+                                                         + "Presented when logging in with an email address that does not match a WordPress.com account")
     }
 }
 

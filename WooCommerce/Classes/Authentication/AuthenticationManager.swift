64c7f209a3c34f647fdf9fb8f7814dd5364daa64
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-08-25 11:36:53 +0530

    Present support for `Authentication` protocol method.

--- WooCommerce/Classes/Authentication/AuthenticationManager.swift
+++ WooCommerce/Classes/Authentication/AuthenticationManager.swift
@@ -376,30 +376,27 @@ extension AuthenticationManager: WordPressAuthenticatorDelegate {
         }
     }
 
-    func presentSupport(from sourceViewController: UIViewController, sourceTag: WordPressSupportSourceTag) {
-    }
-
     /// Presents the Support Interface from a given ViewController, with a specified SourceTag.
     ///
+    func presentSupport(from sourceViewController: UIViewController, sourceTag: WordPressSupportSourceTag) {
+        presentSupport(from: sourceViewController)
+    }
+
+    /// Presents the Support Interface from a given ViewController.
+    ///
+    /// - Parameters:
+    ///     - from: ViewController from which to present the support interface from
+    ///     - sourceTag: Support source tag of the view controller.
+    ///     - lastStep: Last `Step` tracked in `AuthenticatorAnalyticsTracker`
+    ///     - lastFlow: Last `Flow` tracked in `AuthenticatorAnalyticsTracker`
+    ///
     func presentSupport(from sourceViewController: UIViewController, sourceTag: WordPressSupportSourceTag, lastStep: String, lastFlow: String) {
-        let identifier = HelpAndSupportViewController.classNameWithoutNamespaces
-        let supportViewController = UIStoryboard.dashboard.instantiateViewController(identifier: identifier,
-                                                                                     creator: { coder -> HelpAndSupportViewController? in
-            guard let customHelpCenterContent = CustomHelpCenterContent(step: lastStep, flow: lastFlow) else {
-                /// Returning nil as we don't need to customise the HelpAndSupportViewController
-                /// In this case `instantiateViewController` method will use the default `HelpAndSupportViewController` created from storyboard.
-                ///
-                return nil
-            }
+        guard let customHelpCenterContent = CustomHelpCenterContent(step: lastStep, flow: lastFlow)else {
+            presentSupport(from: sourceViewController)
+            return
+        }
 
-            return HelpAndSupportViewController(customHelpCenterContent: customHelpCenterContent, coder: coder)
-        })
-        supportViewController.displaysDismissAction = true
-
-        let navController = WooNavigationController(rootViewController: supportViewController)
-        navController.modalPresentationStyle = .formSheet
-
-        sourceViewController.present(navController, animated: true, completion: nil)
+        presentSupport(from: sourceViewController, customHelpCenterContent: customHelpCenterContent)
     }
 
     /// Presents the Support new request, from a given ViewController, with a specified SourceTag.
@@ -733,5 +730,31 @@ private extension AuthenticationManager {
     func isSupportedError(_ error: Error) -> Bool {
         let wooAuthError = AuthenticationError.make(with: error)
         return wooAuthError != .unknown
+    }
+}
+
+// MARK: - Help and support helpers
+private extension AuthenticationManager {
+
+    func presentSupport(from sourceViewController: UIViewController,
+                        customHelpCenterContent: CustomHelpCenterContent? = nil) {
+        let identifier = HelpAndSupportViewController.classNameWithoutNamespaces
+        let supportViewController = UIStoryboard.dashboard.instantiateViewController(identifier: identifier,
+                                                                                     creator: { coder -> HelpAndSupportViewController? in
+            guard let customHelpCenterContent = customHelpCenterContent else {
+                /// Returning nil as we don't need to customise the HelpAndSupportViewController
+                /// In this case `instantiateViewController` method will use the default `HelpAndSupportViewController` created from storyboard.
+                ///
+                return nil
+            }
+
+            return HelpAndSupportViewController(customHelpCenterContent: customHelpCenterContent, coder: coder)
+        })
+        supportViewController.displaysDismissAction = true
+
+        let navController = WooNavigationController(rootViewController: supportViewController)
+        navController.modalPresentationStyle = .formSheet
+
+        sourceViewController.present(navController, animated: true, completion: nil)
     }
 }

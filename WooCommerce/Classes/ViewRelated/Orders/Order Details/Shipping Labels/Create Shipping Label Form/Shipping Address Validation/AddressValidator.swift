cfa402343e9c678373c008613b3d380a8ba5172f
ThomazFB <thomazfcortez@gmail.com>
2022-08-25 23:05:13 -0300

    Define AddressValidator usage interface

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Shipping Address Validation/AddressValidator.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Shipping Address Validation/AddressValidator.swift
@@ -1,6 +1,35 @@
-import Foundation
+import Yosemite
 
 class AddressValidator {
+    func validate(address: ShippingLabelAddress,
+                  onlyLocally: Bool,
+                  completion: @escaping (Result<Void, AddressValidationError>) -> Void) {
+
+    }
+
+    func validate(address: Address,
+                  onlyLocally: Bool,
+                  completion: @escaping (Result<Void, AddressValidationError>) -> Void) {
+
+    }
+
+    enum ValidationType {
+        case none
+        case local
+        case remote
+    }
+
+    enum LocalValidationError {
+        case name
+        case address
+        case city
+        case postcode
+        case state
+        case country
+        case missingPhoneNumber
+        case invalidPhoneNumber
+    }
+
     enum AddressValidationError: Error {
         case local
         case remote(Error)

668a3367770120eaa7a30eec3e9d47a7c9c77830
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-08-26 10:57:47 +0530

    Add unit test for "Enter store creds" case in CustomHelpCenterContent.

--- WooCommerce/WooCommerceTests/Model/CustomHelpCenterContentTests.swift
+++ WooCommerce/WooCommerceTests/Model/CustomHelpCenterContentTests.swift
@@ -69,4 +69,24 @@ final class CustomHelpCenterContentTests: XCTestCase {
         XCTAssertEqual(sut.trackingProperties[CustomHelpCenterContent.Key.flow.rawValue], flow.rawValue)
         XCTAssertEqual(sut.trackingProperties[CustomHelpCenterContent.Key.url.rawValue], helpContentURL.absoluteString)
     }
+
+    // MARK: Enter Store credentials screen (wp-admin creds)
+    //
+    func test_init_using_step_and_flow_returns_valid_instance_for_enter_store_creds_screen() throws {
+        // Given
+        let step: AuthenticatorAnalyticsTracker.Step = .usernamePassword
+        let flow: AuthenticatorAnalyticsTracker.Flow = .loginWithSiteAddress
+        let helpContentURL = WooConstants.URLs.helpCenterForEnterStoreCredentials.asURL()
+
+        // When
+        let sut = try XCTUnwrap(CustomHelpCenterContent(step: step, flow: flow))
+
+        // Then
+        XCTAssertEqual(sut.url, helpContentURL)
+
+        // Test the `trackingProperties` dictionary values
+        XCTAssertEqual(sut.trackingProperties[CustomHelpCenterContent.Key.step.rawValue], step.rawValue)
+        XCTAssertEqual(sut.trackingProperties[CustomHelpCenterContent.Key.flow.rawValue], flow.rawValue)
+        XCTAssertEqual(sut.trackingProperties[CustomHelpCenterContent.Key.url.rawValue], helpContentURL.absoluteString)
+    }
 }

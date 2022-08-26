437806848dcf0375b5886969e7758ded9b6f743f
ThomazFB <thomazfcortez@gmail.com>
2022-08-26 00:02:39 -0300

    Enable simple local validation to AddressValidator

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Shipping Address Validation/AddressValidator.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Shipping Address Validation/AddressValidator.swift
@@ -4,13 +4,49 @@ class AddressValidator {
     func validate(address: ShippingLabelAddress,
                   onlyLocally: Bool,
                   completion: @escaping (Result<Void, AddressValidationError>) -> Void) {
+        if validateAddressLocally(address: address).isNotEmpty {
+            completion(.failure(.local))
+            return
+        }
 
+        if onlyLocally {
+            completion(.success(()))
+            return
+        }
     }
 
     func validate(address: Address,
                   onlyLocally: Bool,
                   completion: @escaping (Result<Void, AddressValidationError>) -> Void) {
         return validate(address: convertAddress(address: address), onlyLocally: onlyLocally, completion: completion)
+    }
+
+    private func validateAddressLocally(address: ShippingLabelAddress) -> [LocalValidationError] {
+        var errors: [LocalValidationError] = []
+
+        if let addressToBeValidated = address {
+            if addressToBeValidated.name.isEmpty && nameRequired {
+                errors.append(.name)
+            }
+            if addressToBeValidated.address1.isEmpty {
+                errors.append(.address)
+            }
+            if addressToBeValidated.city.isEmpty {
+                errors.append(.city)
+            }
+            if addressToBeValidated.postcode.isEmpty {
+                errors.append(.postcode)
+            }
+            if addressToBeValidated.state.isEmpty && stateOfCountryRequired {
+                errors.append(.state)
+            }
+            if addressToBeValidated.country.isEmpty {
+                errors.append(.country)
+            }
+            //TODO: validate phone number
+        }
+
+        return errors
     }
 
     private func convertAddress(address: Address) -> ShippingLabelAddress {

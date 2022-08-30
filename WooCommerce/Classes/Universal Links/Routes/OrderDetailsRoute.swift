8e5949a8d60d7416cac07476e1bbb7f766a4daed
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2022-08-30 12:41:52 +0200

    Switch store if necessary when opening an universal link from an email

--- WooCommerce/Classes/Universal Links/Routes/OrderDetailsRoute.swift
+++ WooCommerce/Classes/Universal Links/Routes/OrderDetailsRoute.swift
@@ -1,11 +1,31 @@
 import Foundation
+import Yosemite
 
 /// Shows order details from a given universal link that matches the right path
 ///
 struct OrderDetailsRoute: Route {
     let path = "/orders/details"
 
+    private let switchStoreUseCase: SwitchStoreUseCaseProtocol
+
+    init(switchStoreUseCase: SwitchStoreUseCaseProtocol = SwitchStoreUseCase(stores: ServiceLocator.stores)) {
+        self.switchStoreUseCase = switchStoreUseCase
+    }
+
     func perform(with parameters: [String: String]) {
-        DDLogInfo("We received an order details universal link with parameters: \(parameters)")
+        guard let storeIdString = parameters[ParametersKeys.blogId],
+              let storeId = Int64(storeIdString),
+              let orderId = parameters[ParametersKeys.orderId] else {
+            return
+        }
+
+        switchStoreUseCase.switchStore(with: storeId, onCompletion: {_ in})
+    }
+}
+
+private extension OrderDetailsRoute {
+    enum ParametersKeys {
+        static let blogId = "blog_id"
+        static let orderId = "order_id"
     }
 }

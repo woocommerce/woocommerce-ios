f8362b2d82fd25e8f29448d1922fea0d87feb648
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-08-31 18:25:19 +0530

    Make `MockAuthenticationManager` confirm to `Authentication` protocol.

--- WooCommerce/WooCommerceTests/Mocks/MockAuthenticationManager.swift
+++ WooCommerce/WooCommerceTests/Mocks/MockAuthenticationManager.swift
@@ -1,11 +1,39 @@
 @testable import WooCommerce
-import UIKit
+import WordPressAuthenticator
 
-final class MockAuthenticationManager: AuthenticationManager {
-    private(set) var authenticationUIInvoked: Bool = false
+final class MockAuthenticationManager: Authentication {
+    private(set) var authenticationUIInvoked = false
+    private(set) var presentSupportInvoked = false
 
-    override func authenticationUI() -> UIViewController {
+    func authenticationUI() -> UIViewController {
         authenticationUIInvoked = true
         return UIViewController()
+    }
+
+    func presentSupport(from sourceViewController: UIViewController, screen: CustomHelpCenterContent.Screen) {
+        presentSupportInvoked = true
+    }
+
+    func presentSupport(from sourceViewController: UIViewController, sourceTag: WordPressSupportSourceTag) {
+        // no-op
+    }
+
+    func handleAuthenticationUrl(_ url: URL, options: [UIApplication.OpenURLOptionsKey: Any], rootViewController: UIViewController) -> Bool {
+        true
+    }
+
+    func initialize() {
+        // no-op
+    }
+
+    func setLoggedOutAppSettings(_ settings: LoggedOutAppSettingsProtocol) {
+        // no-op
+    }
+
+    func errorViewController(for siteURL: String,
+                             with matcher: ULAccountMatcher,
+                             navigationController: UINavigationController,
+                             onStorePickerDismiss: @escaping () -> Void) -> UIViewController? {
+        nil
     }
 }

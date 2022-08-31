34ff3bbcb80754dbde221d0dfb746cbd0b367b7f
Huong Do <hi.huongdo@gmail.com>
2022-08-31 14:57:31 +0700

    Add test for JetpackConnectionWebViewModel

---
+++ WooCommerce/WooCommerceTests/Authentication/JetpackConnectionWebViewModelTests.swift
@@ -0,0 +1,29 @@
+import XCTest
+@testable import WooCommerce
+
+final class JetpackConnectionWebViewModelTests: XCTestCase {
+
+    func test_completion_handler_returns_the_connected_email_from_url_query() async throws {
+        // Given
+        let siteURL = "https://test.com"
+        let expectedEmail = "test@mail.com"
+        var authorizedEmail: String?
+        let completionHandler: (String?) -> Void = { email in
+            authorizedEmail = email
+        }
+        let initialURL = try XCTUnwrap(URL(string: "https://jetpack.wordpress.com/jetpack.authorize/1/"))
+        let viewModel = JetpackConnectionWebViewModel(initialURL: initialURL, siteURL: siteURL, completion: completionHandler)
+
+        // When
+        let authorizeURL = try XCTUnwrap(URL(string: "https://jetpack.wordpress.com/jetpack.authorize?user_email=\(expectedEmail)"))
+        let authorizePolicy = await viewModel.decidePolicy(for: authorizeURL)
+        let finalUrl = try XCTUnwrap(URL(string: siteURL + "/wp-admin"))
+        let completionPolicy = await viewModel.decidePolicy(for: finalUrl)
+
+        // Then
+        XCTAssertEqual(authorizePolicy, .allow)
+        XCTAssertEqual(completionPolicy, .cancel)
+        XCTAssertEqual(authorizedEmail, expectedEmail)
+    }
+
+}

aecb50a14404d30876504b9fa827749cab8a0b3c
Jaclyn Chen <watertranquil@gmail.com>
2022-09-01 22:06:37 +0800

    Add `AppSettingsAction`s for loading and setting the last selected stats time range tab.

--- Storage/Storage/Model/GeneralStoreSettings.swift
+++ Storage/Storage/Model/GeneralStoreSettings.swift
@@ -37,16 +37,21 @@ public struct GeneralStoreSettings: Codable, Equatable, GeneratedCopiable {
     ///
     public let skippedCashOnDeliveryOnboardingStep: Bool
 
+    /// The raw value string of `StatsTimeRangeV4` that indicates the last selected time range tab in store stats.
+    public var lastSelectedStatsTimeRange: String
+
     public init(isTelemetryAvailable: Bool = false,
                 telemetryLastReportedTime: Date? = nil,
                 areSimplePaymentTaxesEnabled: Bool = false,
                 preferredInPersonPaymentGateway: String? = nil,
-                skippedCashOnDeliveryOnboardingStep: Bool = false) {
+                skippedCashOnDeliveryOnboardingStep: Bool = false,
+                lastSelectedStatsTimeRange: String) {
         self.isTelemetryAvailable = isTelemetryAvailable
         self.telemetryLastReportedTime = telemetryLastReportedTime
         self.areSimplePaymentTaxesEnabled = areSimplePaymentTaxesEnabled
         self.preferredInPersonPaymentGateway = preferredInPersonPaymentGateway
         self.skippedCashOnDeliveryOnboardingStep = skippedCashOnDeliveryOnboardingStep
+        self.lastSelectedStatsTimeRange = lastSelectedStatsTimeRange
     }
 }
 
@@ -62,6 +67,7 @@ extension GeneralStoreSettings {
         self.areSimplePaymentTaxesEnabled = try container.decodeIfPresent(Bool.self, forKey: .areSimplePaymentTaxesEnabled) ?? false
         self.preferredInPersonPaymentGateway = try container.decodeIfPresent(String.self, forKey: .preferredInPersonPaymentGateway)
         self.skippedCashOnDeliveryOnboardingStep = try container.decodeIfPresent(Bool.self, forKey: .skippedCashOnDeliveryOnboardingStep) ?? false
+        self.lastSelectedStatsTimeRange = try container.decodeIfPresent(String.self, forKey: .lastSelectedStatsTimeRange) ?? ""
 
         // Decode new properties with `decodeIfPresent` and provide a default value if necessary.
     }

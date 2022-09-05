131764904f1ecaf76734780fa98dd05a59fde02a
jos <17252150+jostnes@users.noreply.github.com>
2022-09-05 18:05:46 +0800

    update Screen Object version for testing

--- WooCommerce/UITestsFoundation/Screens/Orders/OrdersScreen.swift
+++ WooCommerce/UITestsFoundation/Screens/Orders/OrdersScreen.swift
@@ -2,10 +2,6 @@ import ScreenObject
 import XCTest
 
 public final class OrdersScreen: ScreenObject {
-
-    // TODO: Remove force `try` once `ScreenObject` migration is completed
-    public let tabBar = try! TabNavComponent()
-
     private let searchButtonGetter: (XCUIApplication) -> XCUIElement = {
         $0.buttons["order-search-button"]
     }

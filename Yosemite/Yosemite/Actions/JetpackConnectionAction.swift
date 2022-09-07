2810a514eb07fa4f202fdb1efa7132a1ae6dee2c
Huong Do <hi.huongdo@gmail.com>
2022-09-07 10:45:51 +0700

    Update comments on JetpackConnectionAction

--- Yosemite/Yosemite/Actions/JetpackConnectionAction.swift
+++ Yosemite/Yosemite/Actions/JetpackConnectionAction.swift
@@ -4,8 +4,8 @@ import WordPressKit
 
 /// Defines actions supported by `JetpackConnectionStore`.
 public enum JetpackConnectionAction: Action {
-    /// Fetches the URL used for setting up Jetpack connection using the given authenticator
+    /// Fetches the URL used for setting up Jetpack connection.
     case fetchJetpackConnectionURL(completion: (Result<URL, Error>) -> Void)
-    /// Fetches the user connection state with the given site's Jetpack and authenticator.
+    /// Fetches the user connection state with the given site's Jetpack.
     case fetchJetpackUser(completion: (Result<JetpackUser, Error>) -> Void)
 }

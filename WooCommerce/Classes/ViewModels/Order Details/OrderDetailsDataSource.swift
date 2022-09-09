d7c554613663b2c62b39a2d23c65e28bdda5595a
Josh Heald <joshheald@users.noreply.github.com>
2022-09-09 10:50:24 +0100

    Revert "Merge pull request #7311 from woocommerce/issue/7209-rely-on-needsPayment-for-collect-payment-button"
    This reverts commit a891a64cb2da4aafb8a3a7714790cc98bb573a7c, reversing
    changes made to 200a0145bd42b1ae15da0c9b53e7cd35c4f30c0f.
    
    # Conflicts:
    #	WooCommerce/WooCommerceTests/ViewRelated/Orders/Order Details/OrderDetailsDataSourceTests.swift

--- WooCommerce/Classes/ViewModels/Order Details/OrderDetailsDataSource.swift
+++ WooCommerce/Classes/ViewModels/Order Details/OrderDetailsDataSource.swift
@@ -47,7 +47,7 @@ final class OrderDetailsDataSource: NSObject {
     /// Whether the order is eligible for card present payment.
     ///
     var isEligibleForPayment: Bool {
-        order.needsPayment
+        return order.datePaid == nil
     }
 
     var isEligibleForRefund: Bool {

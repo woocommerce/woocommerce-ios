28826ced2859b76daf59566fbcc923c324261d1c
iamgabrielma <gabrielmaldonado.art@gmail.com>
2022-09-20 10:18:03 +0200

    Add billing & shipping params to Customer model

--- Networking/Networking/Model/Customer.swift
+++ Networking/Networking/Model/Customer.swift
@@ -13,42 +13,51 @@ struct Customer: Codable {
     let email: String
 
     /// Customer first name
-    let firstName: String
+    let firstName: String?
 
     /// Customer last name
-    let lastName: String
-    // TODO: Check why both billing and shipping are making the decoder fail.
+    let lastName: String?
+
     /// List of billing address data
-    //let billing: [String]
+    let billing: Address?
 
     /// List of shipping address data
-    //let shipping: [String]
+    let shipping: Address?
 
     /// Customer struct initializer
     ///
     public init(customerID: Int64,
                 email: String,
-                firstName: String,
-                lastName: String) {
+                firstName: String?,
+                lastName: String?,
+                billing: Address?,
+                shipping: Address?) {
         self.customerID = customerID
         self.email = email
         self.firstName = firstName
         self.lastName = lastName
+        self.billing = billing
+        self.shipping = shipping
     }
 
     /// Public initializer for the Customer
     ///
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+
         let customerID = try container.decode(Int64.self, forKey: .customerID)
         let email = try container.decode(String.self, forKey: .email)
         let firstName = try container.decodeIfPresent(String.self, forKey: .firstName) ?? ""
         let lastName = try container.decodeIfPresent(String.self, forKey: .lastName) ?? ""
+        let billing = try? container.decode(Address.self, forKey: .billing)
+        let shipping = try? container.decode(Address.self, forKey: .shipping)
 
         self.init(customerID: customerID,
-            email: email,
-            firstName: firstName,
-            lastName: lastName
+                  email: email,
+                  firstName: firstName,
+                  lastName: lastName,
+                  billing: billing,
+                  shipping: shipping
         )
     }
 }
@@ -61,5 +70,7 @@ extension Customer {
         case email
         case firstName =        "first_name"
         case lastName =         "last_name"
+        case billing
+        case shipping
     }
 }

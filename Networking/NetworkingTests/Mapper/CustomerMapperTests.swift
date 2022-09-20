9e29b05a1601a3873811e49e2c197cb73c0e0dbe
iamgabrielma <gabrielmaldonado.art@gmail.com>
2022-09-20 10:54:27 +0200

    Add CustomerMapper unit tests

--- Networking/NetworkingTests/Mapper/CustomerMapperTests.swift
+++ Networking/NetworkingTests/Mapper/CustomerMapperTests.swift
@@ -1,28 +1,82 @@
 import XCTest
 @testable import Networking
 
+/// CustomerMapper Unit Tests
+///
 class CustomerMapperTests: XCTestCase {
 
-    func test_data_is_mapped() {
-        let mapper = CustomerMapper(siteID: 123)
-        guard let data = Loader.contentsOf("customer") else {
+    /// Dummy Site ID.
+    ///
+    private let dummySiteID: Int64 = 123
+
+    /// Local file that holds Customer data representing the API endpoint
+    ///
+    private let filename: String = "customer"
+
+    /// Verifies that the Customer object can be mapped fron the Encoded data
+    ///
+    func test_Customer_is_mapped_from_encoded_data() {
+        // Given
+        let mapper = CustomerMapper(siteID: dummySiteID)
+        guard let data = Loader.contentsOf(filename) else {
             XCTFail("customer.json not found")
             return
         }
+
+        // When
         let customer = try? mapper.map(response: data)
+
+        // Then
         XCTAssertNotNil(mapper)
         XCTAssertNotNil(customer)
-        XCTAssertEqual(customer?.customerID, 25)
+        XCTAssertNotNil(customer?.customerID)
+        XCTAssertNotNil(customer?.email)
+        XCTAssertNotNil(customer?.firstName)
+        XCTAssertNotNil(customer?.lastName)
+        XCTAssertNotNil(customer?.billing)
+        XCTAssertNotNil(customer?.shipping)
+    }
+
+    /// Verifies that all of the Customer response values are parsed correctly
+    ///
+    func test_Customer_response_values_are_correctly_parsed() throws {
+        // Given
+        guard let customer = try mapCustomer(from: filename) else {
+            XCTFail()
+            return
+        }
+
+        // Then
+        XCTAssertNotNil(customer)
+        XCTAssertEqual(customer.customerID, 25)
+        XCTAssertEqual(customer.email, "john.doe@example.com")
+        XCTAssertEqual(customer.firstName, "John")
+        XCTAssertEqual(customer.lastName, "Doe")
+
+        let dummyAddresses = [customer.shipping, customer.billing].compactMap({ $0 })
+        XCTAssertEqual(dummyAddresses.count, 2)
+
+        for address in dummyAddresses {
+            XCTAssertEqual(address.firstName, "John")
+            XCTAssertEqual(address.lastName, "Doe")
+            XCTAssertEqual(address.company, "")
+            XCTAssertEqual(address.address1, "969 Market")
+            XCTAssertEqual(address.address2, "")
+            XCTAssertEqual(address.city, "San Francisco")
+            XCTAssertEqual(address.state, "CA")
+            XCTAssertEqual(address.postcode, "94103")
+            XCTAssertEqual(address.country, "US")
+        }
     }
 }
 
 private extension CustomerMapperTests {
-    func mapCustomer(from filename: String) throws -> Customer {
-        return Customer(
-            customerID: 25,
-            email: "",
-            firstName: "",
-            lastName: ""
-        )
+    /// Returns the CustomerMapper output upon receiving `filename` (Data Encoded)
+    ///
+    func mapCustomer(from filename: String) throws -> Customer? {
+        guard let response = Loader.contentsOf(filename) else {
+            return nil
+        }
+        return try! CustomerMapper(siteID: dummySiteID).map(response: response)
     }
 }

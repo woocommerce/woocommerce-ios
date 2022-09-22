d5a6d91f67ce11997bc02a07a7c64a2757ad146d
Huong Do <hi.huongdo@gmail.com>
2022-09-22 17:11:50 +0700

    Add test for showing account mismatch when site has active jetpack but not connected

--- WooCommerce/WooCommerceTests/Authentication/AuthenticationManagerTests.swift
+++ WooCommerce/WooCommerceTests/Authentication/AuthenticationManagerTests.swift
@@ -173,6 +173,28 @@ final class AuthenticationManagerTests: XCTestCase {
         XCTAssertTrue(rootController is ULErrorViewController)
     }
 
+    func test_it_shows_account_mismatch_upon_login_epilogue_if_the_site_has_active_jetpack_but_not_connected() {
+        // Given
+        let manager = AuthenticationManager()
+        let testSite = "http://test.com"
+        let siteInfo = WordPressComSiteInfo(remote: ["isWordPress": true,
+                                                     "hasJetpack": true,
+                                                     "urlAfterRedirects": testSite,
+                                                     "isJetpackActive": true,
+                                                     "isJetpackConnected": false])
+        let wpcomCredentials = WordPressComCredentials(authToken: "abc", isJetpackLogin: false, multifactor: false, siteURL: testSite)
+        let credentials = AuthenticatorCredentials(wpcom: wpcomCredentials, wporg: nil)
+        let navigationController = UINavigationController()
+
+        // When
+        manager.shouldPresentUsernamePasswordController(for: siteInfo, onCompletion: { _ in })
+        manager.presentLoginEpilogue(in: navigationController, for: credentials, onDismiss: {})
+
+        // Then
+        let rootController = navigationController.viewControllers.first
+        XCTAssertTrue(rootController is ULAccountMismatchViewController)
+    }
+
     func test_it_can_display_jetpack_error_for_org_site_credentials_sign_in() {
         // Given
         let manager = AuthenticationManager()

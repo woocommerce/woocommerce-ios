84bea3f84a9d534ba34d8e3a53bb8e92ee735ba4
iamgabrielma <gabrielmaldonado.art@gmail.com>
2022-09-26 13:04:47 +0900

    Resolve malformed retrieveCustomer JPRequest path

--- Networking/Networking/Remote/CustomerRemote.swift
+++ Networking/Networking/Remote/CustomerRemote.swift
@@ -9,7 +9,7 @@ public class CustomerRemote: Remote {
     ///     - completion: Closure to be executed upon completion.
     ///
     public func retrieveCustomer(for siteID: Int64, with customerID: Int64, completion: @escaping (Result<Customer, Error>) -> Void) {
-        let path = "/customers/\(customerID)"
+        let path = "customers/\(customerID)"
         let request = JetpackRequest(wooApiVersion: .mark3,
                                      method: .get,
                                      siteID: siteID,

94f1ebc260e3ad10ec8a993d338384894138d8b9
Gabriel Maldonado <gabrielmaldonado.art@gmail.com>
2022-09-28 07:48:43 +0900

    Add initial WCAnalyticsCustomer model, mapper, remote

---
+++ Networking/Networking/Model/WCAnalyticsCustomer.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+public struct WCAnalyticsCustomer: Codable {
+
+    /// Unique identifier for the user
+    public let userID: Int64
+
+    /// Customer name
+    public let name: String?
+
+    /// WCAnalyticsCustomer struct Initializer
+    ///
+    public init(userID: Int64, name: String?) {
+        self.userID = userID
+        self.name = name
+    }
+
+    /// Public initializer for WCAnalyticsCustomer
+    ///
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let userID = try container.decode(Int64.self, forKey: .userID)
+        let name = try container.decode(String.self, forKey: .name)
+
+        self.init(userID: userID, name: name)
+    }
+}
+
+extension WCAnalyticsCustomer {
+    enum CodingKeys: String, CodingKey {
+        case userID =   "id"
+        case name
+    }
+}
---
+++ Networking/Networking/Remote/WCAnalyticsCustomerRemote.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public class WCAnalyticsCustomerRemote: Remote {
+    /// Retrieves a `Customer` from `/wc-analytics/customers?search=`
+    ///
+    /// - Parameters:
+    ///     - siteID: Site for which we'll fetch the customer.
+    ///     - name: Name of the customer that will be retrieved
+    ///     - completion: Closure to be executed upon completion.
+    ///
+    func retrieveCustomerByName(for siteID: Int64, with name: String, completion: @escaping (Result<WCAnalyticsCustomer, Error>) -> Void) {
+
+        let path = "customers?search=\(name)"
+        let request = JetpackRequest(
+            wooApiVersion: .wcAnalytics,
+            method: .get,
+            siteID: siteID,
+            path: path,
+            parameters: nil
+        )
+
+        let mapper = WCAnalyticsCustomerMapper(siteID: siteID)
+        enqueue(request, mapper: mapper, completion: completion)
+    }
+}

32dfe2a623957706b5e8eb660430e8f2490c9d71
iamgabrielma <gabrielmaldonado.art@gmail.com>
2022-09-27 09:15:11 +0900

    Split CustomerRemoteTests

--- Networking/NetworkingTests/Remote/CustomerRemoteTests.swift
+++ Networking/NetworkingTests/Remote/CustomerRemoteTests.swift
@@ -7,6 +7,8 @@ class CustomerRemoteTests: XCTestCase {
     ///
     private var network: MockNetwork!
 
+    private var remote: CustomerRemote!
+
     /// Dummy Site ID
     ///
     private let sampleSiteID: Int64 = 123
@@ -17,30 +19,47 @@ class CustomerRemoteTests: XCTestCase {
     override func setUp() {
         super.setUp()
         network = MockNetwork()
+        remote = CustomerRemote(network: network)
     }
 
     override func tearDown() {
         network = nil
+        remote = nil
         super.tearDown()
     }
 
-    /// Verifies that retrieveCustomer  properly parses the `wc/v3/customers/{customerID}` endpoint sample response.
+    /// Verifies that retrieveCustomer simulated response is successful for a given customerID
     ///
-    func test_retrieveCustomer_returns_parsed_customer_successfully() throws {
+    func test_CustomerRemote_when_retrieveCustomer_then_returns_result_isSuccess() throws {
         // Given
-        let remote = CustomerRemote(network: network)
         network.simulateResponse(requestUrlSuffix: "customers/\(sampleCustomerID)", filename: "customer")
 
         // When
         let result = waitFor { promise in
-            remote.retrieveCustomer(for: self.sampleSiteID, with: self.sampleCustomerID) { result in
+            self.remote.retrieveCustomer(for: self.sampleSiteID, with: self.sampleCustomerID) { result in
                 promise(result)
             }
         }
 
         // Then
         XCTAssert(result.isSuccess)
+    }
+
+    /// Verifies that retrieveCustomer  properly parses the `wc/v3/customers/{customerID}` endpoint sample response.
+    ///
+    func test_retrieveCustomer_returns_parsed_customer_successfully() throws {
+        // Given
+        network.simulateResponse(requestUrlSuffix: "customers/\(sampleCustomerID)", filename: "customer")
+
+        // When
+        let result = waitFor { promise in
+            self.remote.retrieveCustomer(for: self.sampleSiteID, with: self.sampleCustomerID) { result in
+                promise(result)
+            }
+        }
         let customer = try XCTUnwrap(result.get())
+
+        // Then
         XCTAssertNotNil(customer)
     }
 }

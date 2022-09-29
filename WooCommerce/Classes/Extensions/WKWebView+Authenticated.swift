a72255e12a8b1440f4f526296ce835b4efa929c1
Huong Do <hi.huongdo@gmail.com>
2022-09-29 11:28:48 +0700

    Add method to authenticate with WP.org credentials

--- WooCommerce/Classes/Extensions/WKWebView+Authenticated.swift
+++ WooCommerce/Classes/Extensions/WKWebView+Authenticated.swift
@@ -1,12 +1,29 @@
 import Alamofire
 import Foundation
 import WebKit
+import WordPressAuthenticator
 import struct Yosemite.Credentials
 import class Networking.UserAgent
 
 /// An extension to authenticate WPCom automatically
 ///
 extension WKWebView {
+    static let wporgNoncePath = "/admin-ajax.php?action=rest-nonce"
+
+    func authenticateForWPOrg(with credentials: WordPressOrgCredentials) throws -> URLRequest {
+        var request = try URLRequest(url: credentials.loginURL.asURL(), method: .post)
+        request.httpShouldHandleCookies = true
+
+        let redirectLink = (credentials.adminURL + Self.wporgNoncePath)
+            .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+
+        let parameters = ["log": credentials.username,
+                          "pwd": credentials.password,
+                          "redirect_to": redirectLink ?? ""]
+
+        return try URLEncoding.default.encode(request, with: parameters)
+    }
+
     func authenticateForWPComAndRedirect(to url: URL, credentials: Credentials?) {
         customUserAgent = UserAgent.defaultUserAgent
         do {

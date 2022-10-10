17faec8e907551698d8c350f9ae583699e9d8551
iamgabrielma <gabrielmaldonado.art@gmail.com>
2022-10-10 12:34:28 +0900

    Add asserts for retrieveCustomer unit tests

--- Yosemite/YosemiteTests/Stores/CustomerStoreTests.swift
+++ Yosemite/YosemiteTests/Stores/CustomerStoreTests.swift
@@ -11,7 +11,7 @@ class CustomerStoreTests: XCTestCase {
     private var searchRemote: WCAnalyticsCustomerRemote!
     private var store: CustomerStore!
     private let dummySiteID: Int64 = 12345
-    private let dummyCustomerID: Int64 = 1
+    private let dummyCustomerID: Int64 = 25
     private let dummyKeyword: String = "John"
 
     override func setUp() {
@@ -30,7 +30,7 @@ class CustomerStoreTests: XCTestCase {
         )
     }
 
-    func test_retrieveCustomer_returns_Customer_upon_success() {
+    func test_retrieveCustomer_returns_Customer_upon_success() throws {
         // Given
         network.simulateResponse(requestUrlSuffix: "", filename: "customer")
 
@@ -44,6 +44,23 @@ class CustomerStoreTests: XCTestCase {
 
         // Then
         XCTAssertTrue(result.isSuccess)
+        let customer = try result.get()
+        XCTAssertEqual(customer.customerID, 25)
+        XCTAssertEqual(customer.firstName, "John")
+        XCTAssertEqual(customer.lastName, "Doe")
+        XCTAssertEqual(customer.email, "john.doe@example.com")
+        XCTAssertEqual(customer.billing?.firstName, "John" )
+        XCTAssertEqual(customer.billing?.lastName, "Doe" )
+        XCTAssertEqual(customer.billing?.address1, "969 Market" )
+        XCTAssertEqual(customer.billing?.address2, "" )
+        XCTAssertEqual(customer.billing?.city, "San Francisco" )
+        XCTAssertEqual(customer.billing?.state, "CA" )
+        XCTAssertEqual(customer.billing?.postcode, "94103" )
+        XCTAssertEqual(customer.billing?.country, "US" )
+        XCTAssertEqual(customer.billing?.email, "john.doe@example.com" )
+        XCTAssertEqual(customer.billing?.phone, "(555) 555-5555" )
+        XCTAssertEqual(customer.shipping?.firstName, "John" )
+        XCTAssertEqual(customer.shipping?.lastName, "Doe" )
     }
 
     func test_retrieveCustomer_returns_Error_upon_failure() {
@@ -64,29 +81,20 @@ class CustomerStoreTests: XCTestCase {
         XCTAssertEqual(result.failure as? NetworkError, expectedError)
     }
 
-    func test_mapSearchResultsToCustomerObject_returns_Customer_upon_success() throws {
+    func test_searchCustomers_returns_Success_upon_success() {
         // Given
-        //network.simulateResponse(requestUrlSuffix: "", filename: "wc-analytics-customers")
-        // Mock instead?
-        let searchResults = [
-            Networking.WCAnalyticsCustomer(userID: 1, name: "John"),
-            Networking.WCAnalyticsCustomer(userID: 2, name: "Paul"),
-            Networking.WCAnalyticsCustomer(userID: 3, name: "John.Merch")
-        ]
+        network.simulateResponse(requestUrlSuffix: "customers", filename: "wc-analytics-customers")
+        network.simulateResponse(requestUrlSuffix: "customers/25", filename: "customer")
 
-        // When
-        let result: Result<Customer, Error> = waitFor { promise in
-            let action = CustomerAction.mapSearchResultsToCustomerObject(
+        let result: Result<Void, Error> = waitFor { promise in
+            let action = CustomerAction.searchCustomers(
                 siteID: self.dummySiteID,
-                searchResults: searchResults) { result in
+                keyword: self.dummyKeyword) { result in
                     promise(result)
                 }
             self.store.onAction(action)
         }
-
-        // Then
         XCTAssertTrue(result.isSuccess)
-        XCTAssertEqual(searchResults[0].name, try result.get().firstName)
     }
 
     func test_searchCustomers_returns_Error_upon_failure() {

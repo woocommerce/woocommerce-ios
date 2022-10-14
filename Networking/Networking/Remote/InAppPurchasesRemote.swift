0c79ae2e376b6d2ace9bc996d83cdb678572d87e
Jorge Bernal <jorge@automattic.com>
2022-10-14 12:24:08 +0200

    Augment network requests with app id and offer async variants

--- Networking/Networking/Remote/InAppPurchasesRemote.swift
+++ Networking/Networking/Remote/InAppPurchasesRemote.swift
@@ -1,8 +1,10 @@
+import Alamofire
 import Foundation
 
 public class InAppPurchasesRemote: Remote {
-    public func loadProducts(completion: @escaping (Result<[String], Error>) -> Void) {
-        let request = DotcomRequest(wordpressApiVersion: .wpcomMark2, method: .get, path: Constants.productsPath)
+    public func loadProducts(completion: @escaping (Swift.Result<[String], Error>) -> Void) {
+        let dotComRequest = DotcomRequest(wordpressApiVersion: .wpcomMark2, method: .get, path: Constants.productsPath)
+        let request = augmentedRequestWithAppId(dotComRequest)
         let mapper = InAppPurchasesProductMapper()
         enqueue(request, mapper: mapper, completion: completion)
     }
@@ -13,11 +15,57 @@ public class InAppPurchasesRemote: Remote {
         productIdentifier: String,
         appStoreCountryCode: String,
         receiptData: Data,
-        completion: @escaping (Result<Int, Error>) -> Void) {
-            let request = DotcomRequest(wordpressApiVersion: .wpcomMark2, method: .post, path: Constants.ordersPath)
+        completion: @escaping (Swift.Result<Int, Error>) -> Void) {
+            let dotComRequest = DotcomRequest(wordpressApiVersion: .wpcomMark2, method: .post, path: Constants.ordersPath)
+            let request = augmentedRequestWithAppId(dotComRequest)
             let mapper = InAppPurchaseOrderResultMapper()
             enqueue(request, mapper: mapper, completion: completion)
         }
+}
+
+// MARK: - Async methods
+
+public extension InAppPurchasesRemote {
+    func loadProducts() async throws -> [String] {
+        try await withCheckedThrowingContinuation { continuation in
+            loadProducts { result in
+                continuation.resume(with: result)
+            }
+        }
+    }
+
+    func createOrder(
+        for siteID: Int64,
+        price: Int,
+        productIdentifier: String,
+        appStoreCountryCode: String,
+        receiptData: Data
+    ) async throws -> Int {
+        try await withCheckedThrowingContinuation { continuation in
+            createOrder(
+                for: siteID,
+                price: price,
+                productIdentifier: productIdentifier,
+                appStoreCountryCode: appStoreCountryCode,
+                receiptData: receiptData
+            ) { result in
+                continuation.resume(with: result)
+            }
+        }
+    }
+}
+
+private extension InAppPurchasesRemote {
+    func augmentedRequestWithAppId(_ request: URLRequestConvertible) -> URLRequestConvertible {
+        guard let bundleIdentifier = Bundle.main.bundleIdentifier,
+              var augmented = try? request.asURLRequest() else {
+            return request
+        }
+
+        augmented.setValue(bundleIdentifier, forHTTPHeaderField: "X-APP-ID")
+
+        return augmented
+    }
 }
 
 private extension InAppPurchasesRemote {

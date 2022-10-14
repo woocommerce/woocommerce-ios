1f2ad52ff240f964f8086d3bfedd91b203c9c4d4
Jorge Bernal <jorge@automattic.com>
2022-10-14 15:51:16 +0200

    Fix typo in unit test

--- Networking/NetworkingTests/Mapper/InAppPurchaseOrderResultMapperTests.swift
+++ Networking/NetworkingTests/Mapper/InAppPurchaseOrderResultMapperTests.swift
@@ -5,7 +5,7 @@ final class InAppPurchasesOrderResultMapperTests: XCTestCase {
     func test_iap_order_creation_is_decoded_from_json_response() throws {
         // Given
         let jsonData = try XCTUnwrap(Loader.contentsOf("iap-order-create"))
-        let expectedOrderId = 1234
+        let expectedOrderId = 12345
 
         // When
         let orderId = try InAppPurchaseOrderResultMapper().map(response: jsonData)

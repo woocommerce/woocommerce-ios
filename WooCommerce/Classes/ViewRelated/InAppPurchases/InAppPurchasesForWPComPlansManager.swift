0582234702093da0f4cd3660d73543cc1031f9b1
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2022-10-26 11:58:32 +0200

    Add logic to IAP manager

--- WooCommerce/Classes/ViewRelated/InAppPurchases/InAppPurchasesForWPComPlansManager.swift
+++ WooCommerce/Classes/ViewRelated/InAppPurchases/InAppPurchasesForWPComPlansManager.swift
@@ -1,18 +1,20 @@
 import Foundation
 import StoreKit
+import Yosemite
+
+protocol WPComPlanProduct {
+    var displayName: String { get }
+    var description: String { get }
+    var id: String { get }
+    var displayPrice: String { get }
+}
+
+extension StoreKit.Product: WPComPlanProduct {}
 
 enum WPComPlanProductTransactionStatus {
     case notStarted // Neither purchased through Apple nor the WPCom plan was unlocked
-    case pending // In-App purchase was successful but the WPCom plan unlock request failed
+    case pending // In-App purchase was successful but the WPCom plan unlock request is pending
     case finished // In-App purchase and WPCom plan unlock succesful
-}
-
-struct WPComPlanProduct {
-    let localizedTitle: String
-    let localizedDescription: String
-    let price: String
-    let currency: String
-    let status: WPComPlanProductTransactionStatus
 }
 
 protocol InAppPurchasesForWPComPlansProtocol {
@@ -22,14 +24,44 @@ protocol InAppPurchasesForWPComPlansProtocol {
 }
 
 final class InAppPurchasesForWPComPlansManager: InAppPurchasesForWPComPlansProtocol {
+    private let stores = ServiceLocator.stores
+    // ISO 3166-1 Alpha-3 country code representation.
+    private let supportedCountriesCodes = ["USA"]
+
     func fetchProducts() async throws -> [WPComPlanProduct] {
-        []
+        try await withCheckedThrowingContinuation { continuation in
+            stores.dispatch(InAppPurchaseAction.loadProducts(completion: { result in
+                switch result {
+                case .success(let products):
+                    continuation.resume(returning: products)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }))
+        }
+    }
+
+    func transactionStatus(for product: WPComPlanProduct) async -> WPComPlanProductTransactionStatus {
+        guard let result = await Transaction.latest(for: product.id) else {
+            return .notStarted
+        }
+
+        return await Transaction.unfinished.contains(result) ? .pending : .finished
     }
 
     func purchase(product: WPComPlanProduct, for remoteSiteId: Int64) async throws {
+        guard let storeKitProduct = product as? StoreKit.Product else {
+            return
+        }
+
+        stores.dispatch(InAppPurchaseAction.purchaseProduct(siteID: remoteSiteId, product: storeKitProduct, completion: { _ in }))
     }
 
     func inAppPurchasesAreSupported() async -> Bool {
-        await Storefront.current?.countryCode == "USA"
+        guard let countryCode = await Storefront.current?.countryCode else {
+            return false
+        }
+
+        return supportedCountriesCodes.contains(countryCode)
     }
 }

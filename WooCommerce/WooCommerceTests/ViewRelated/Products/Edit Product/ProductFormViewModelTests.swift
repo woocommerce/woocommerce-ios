a525828976e1f8518016f895910e74b3f8f3d2d1
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2022-10-27 20:02:23 +0300

    Revert "Update tests for actionButtons"
    223297d8d074c6bd2055c3895dbcf1b8c230d5fa

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Edit Product/ProductFormViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Edit Product/ProductFormViewModelTests.swift
@@ -277,7 +277,7 @@ final class ProductFormViewModelTests: XCTestCase {
         let actionButtons = viewModel.actionButtons
 
         // Then
-        XCTAssertEqual(actionButtons, [.preview, .publish, .more])
+        XCTAssertEqual(actionButtons, [.publish, .more])
     }
 
     func test_action_buttons_for_new_product_with_published_status_and_no_pending_changes() {
@@ -289,7 +289,7 @@ final class ProductFormViewModelTests: XCTestCase {
         let actionButtons = viewModel.actionButtons
 
         // Then
-        XCTAssertEqual(actionButtons, [.preview, .publish, .more])
+        XCTAssertEqual(actionButtons, [.publish, .more])
     }
 
     func test_action_buttons_for_new_product_with_different_status() {
@@ -305,7 +305,7 @@ final class ProductFormViewModelTests: XCTestCase {
         let actionButtons = viewModel.actionButtons
 
         // Then
-        XCTAssertEqual(actionButtons, [.preview, .save, .more])
+        XCTAssertEqual(actionButtons, [.save, .more])
     }
 
     func test_action_buttons_for_existing_published_product_and_pending_changes() {
@@ -343,7 +343,7 @@ final class ProductFormViewModelTests: XCTestCase {
         let actionButtons = viewModel.actionButtons
 
         // Then
-        XCTAssertEqual(actionButtons, [.preview, .save, .more])
+        XCTAssertEqual(actionButtons, [.save, .more])
     }
 
     func test_action_buttons_for_existing_draft_product_and_no_pending_changes() {
@@ -355,7 +355,7 @@ final class ProductFormViewModelTests: XCTestCase {
         let actionButtons = viewModel.actionButtons
 
         // Then
-        XCTAssertEqual(actionButtons, [.preview, .publish, .more])
+        XCTAssertEqual(actionButtons, [.publish, .more])
     }
 
     func test_action_buttons_for_existing_product_with_other_status_and_peding_changes() {

ac52d2df880c8972ee53b762055c5906ff8ee70b
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2022-10-28 17:21:05 +0300

    Add productsOnboarding support to MockFeatureFlagService

--- WooCommerce/WooCommerceTests/Mocks/MockFeatureFlagService.swift
+++ WooCommerce/WooCommerceTests/Mocks/MockFeatureFlagService.swift
@@ -8,19 +8,22 @@ struct MockFeatureFlagService: FeatureFlagService {
     private let shippingLabelsOnboardingM1: Bool
     private let isLoginPrologueOnboardingEnabled: Bool
     private let isSimplifiedLoginFlowI1Enabled: Bool
+    private let isProductsOnboardingEnabled: Bool
 
     init(isInboxOn: Bool = false,
          isSplitViewInOrdersTabOn: Bool = false,
          isUpdateOrderOptimisticallyOn: Bool = false,
          shippingLabelsOnboardingM1: Bool = false,
          isLoginPrologueOnboardingEnabled: Bool = false,
-         isSimplifiedLoginFlowI1Enabled: Bool = false) {
+         isSimplifiedLoginFlowI1Enabled: Bool = false,
+         isProductsOnboardingEnabled: Bool = false) {
         self.isInboxOn = isInboxOn
         self.isSplitViewInOrdersTabOn = isSplitViewInOrdersTabOn
         self.isUpdateOrderOptimisticallyOn = isUpdateOrderOptimisticallyOn
         self.shippingLabelsOnboardingM1 = shippingLabelsOnboardingM1
         self.isLoginPrologueOnboardingEnabled = isLoginPrologueOnboardingEnabled
         self.isSimplifiedLoginFlowI1Enabled = isSimplifiedLoginFlowI1Enabled
+        self.isProductsOnboardingEnabled = isProductsOnboardingEnabled
     }
 
     func isFeatureFlagEnabled(_ featureFlag: FeatureFlag) -> Bool {
@@ -37,6 +40,8 @@ struct MockFeatureFlagService: FeatureFlagService {
             return isLoginPrologueOnboardingEnabled
         case .simplifiedLoginFlowI1:
             return isSimplifiedLoginFlowI1Enabled
+        case .productsOnboarding:
+            return isProductsOnboardingEnabled
         default:
             return false
         }

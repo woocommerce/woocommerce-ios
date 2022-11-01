d9585c1fc97d89768b8df35058b1f5e8aa3df871
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-11-01 17:00:30 +0530

    Skip pasteboard tests if running iOS 16.0 and above. I think new `UIPasteControl` should be used for this feature.

--- WordPressAuthenticator/WordPressAuthenticatorTests/Authenticator/PasteboardTests.swift
+++ WordPressAuthenticator/WordPressAuthenticatorTests/Authenticator/PasteboardTests.swift
@@ -10,6 +10,10 @@ class PasteboardTests: XCTestCase {
     }
 
     func testNominalAuthCode() throws {
+        if #available(iOS 16.0, *) {
+            throw XCTSkip("Unsupported iOS version")
+        }
+
         guard #available(iOS 14.0, *) else {
             throw XCTSkip("Unsupported iOS version")
         }
@@ -32,6 +36,10 @@ class PasteboardTests: XCTestCase {
     }
 
     func testLeadingZeroInAuthCodePreserved() throws {
+        if #available(iOS 16.0, *) {
+            throw XCTSkip("Unsupported iOS version")
+        }
+
         guard #available(iOS 14.0, *) else {
             throw XCTSkip("Unsupported iOS version")
         }
@@ -51,4 +59,6 @@ class PasteboardTests: XCTestCase {
         }
 
         waitForExpectations(timeout: timeout, handler: nil)
-    }}
+    }
+
+}

ab8bf0343f2f1639f308d9822d36ea15c7e1d9b6
Jaclyn Chen <watertranquil@gmail.com>
2022-11-04 15:38:31 +0800

    Add a test case for `LoggedOutStoreCreationCoordinator`.

---
+++ WooCommerce/WooCommerceTests/Authentication/LoggedOutStoreCreationCoordinatorTests.swift
@@ -0,0 +1,36 @@
+import TestKit
+import XCTest
+@testable import WooCommerce
+
+final class LoggedOutStoreCreationCoordinatorTests: XCTestCase {
+    private var navigationController: UINavigationController!
+    private let window = UIWindow(frame: UIScreen.main.bounds)
+
+    override func setUp() {
+        super.setUp()
+
+        window.makeKeyAndVisible()
+        navigationController = .init()
+        window.rootViewController = navigationController
+    }
+
+    override func tearDown() {
+        navigationController = nil
+        window.resignKey()
+        window.rootViewController = nil
+
+        super.tearDown()
+    }
+
+    func test_start_shows_AccountCreationFormHostingController() throws {
+        // Given
+        let coordinator = LoggedOutStoreCreationCoordinator(source: .prologue, navigationController: navigationController)
+        XCTAssertNil(navigationController.topViewController)
+
+        // When
+        coordinator.start()
+
+        // Then
+        assertThat(navigationController.topViewController, isAnInstanceOf: AccountCreationFormHostingController.self)
+    }
+}

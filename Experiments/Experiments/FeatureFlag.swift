6da0cb21e3fd7eb91c62b4d34b1a19e73ea47d67
Huong Do <hi.huongdo@gmail.com>
2022-11-09 11:34:19 +0700

    Remove feature flag for loginPrologueOnboardingSurvey

--- Experiments/Experiments/FeatureFlag.swift
+++ Experiments/Experiments/FeatureFlag.swift
@@ -50,10 +50,6 @@ public enum FeatureFlag: Int {
     ///
     case loginErrorNotifications
 
-    /// Whether to show a survey at the end of the login onboarding screen after feature carousel
-    ///
-    case loginPrologueOnboardingSurvey
-
     /// Whether to prefer magic link to password in the login flow
     ///
     case loginMagicLinkEmphasis

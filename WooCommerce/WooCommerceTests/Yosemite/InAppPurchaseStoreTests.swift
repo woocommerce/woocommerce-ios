136a509a17da47bc5b2d13d6af403d285e2172fc
Jorge Bernal <jorge@automattic.com>
2022-11-09 13:16:53 +0100

    Add some more tests for InAppPurchaseStore

--- WooCommerce/WooCommerceTests/Yosemite/InAppPurchaseStoreTests.swift
+++ WooCommerce/WooCommerceTests/Yosemite/InAppPurchaseStoreTests.swift
@@ -21,6 +21,16 @@ final class InAppPurchaseStoreTests: XCTestCase {
     ///
     private let sampleSiteID: Int64 = 123
 
+    /// Testing Product ID
+    /// Should match the product ID in WooCommerce.storekit
+    ///
+    private let sampleProductID: String = "debug.woocommerce.ecommerce.monthly"
+
+    /// Testing Order ID
+    /// Should match the order ID in iap-order-create.json
+    ///
+    private let sampleOrderID: Int64 = 12345
+
     var store: InAppPurchaseStore!
 
 
@@ -33,6 +43,7 @@ final class InAppPurchaseStoreTests: XCTestCase {
 
     override func tearDown() {
         storeKitSession.resetToDefaultState()
+        storeKitSession.clearTransactions()
     }
 
     func test_iap_supported_in_us() throws {
@@ -67,7 +78,7 @@ final class InAppPurchaseStoreTests: XCTestCase {
         XCTAssertFalse(result)
     }
 
-    func test_load_products_loads_empty_products_response() throws {
+    func test_load_products_loads_products_response() throws {
         // Given
         network.simulateResponse(requestUrlSuffix: "iap/products", filename: "iap-products")
 
@@ -82,5 +93,118 @@ final class InAppPurchaseStoreTests: XCTestCase {
         // Then
         let products = try XCTUnwrap(result.get())
         XCTAssertFalse(products.isEmpty)
+        XCTAssertEqual(products.first?.id, sampleProductID)
+    }
+
+    func test_load_products_fails_if_iap_unsupported() throws {
+        // Given
+        storeKitSession.storefront = "CAN"
+        network.simulateResponse(requestUrlSuffix: "iap/products", filename: "iap-products")
+
+        // When
+        let result = waitFor { promise in
+            let action = InAppPurchaseAction.loadProducts { result in
+                    promise(result)
+                }
+            self.store.onAction(action)
+        }
+
+        // Then
+        XCTAssert(result.isFailure)
+    }
+
+    func test_purchase_product_completes_purchase() throws {
+        // Given
+        network.simulateResponse(requestUrlSuffix: "iap/orders", filename: "iap-order-create")
+
+        // When
+        let result = waitFor { promise in
+            let action = InAppPurchaseAction.purchaseProduct(siteID: self.sampleSiteID, productID: self.sampleProductID) { result in
+                    promise(result)
+                }
+            self.store.onAction(action)
+        }
+
+        // Then
+        let purchaseResult = try XCTUnwrap(result.get())
+        guard case let .success(verificationResult) = purchaseResult,
+              case let .verified(transaction) = verificationResult else {
+            return XCTFail()
+        }
+        XCTAssertEqual(transaction.productID, sampleProductID)
+        XCTAssertNotNil(transaction.appAccountToken)
+    }
+
+    @available(iOS 16.0, *)
+    func test_purchase_product_ensure_xcode_environment() throws {
+        // Given
+        network.simulateResponse(requestUrlSuffix: "iap/orders", filename: "iap-order-create")
+
+        // When
+        let result = waitFor { promise in
+            let action = InAppPurchaseAction.purchaseProduct(siteID: self.sampleSiteID, productID: self.sampleProductID) { result in
+                    promise(result)
+                }
+            self.store.onAction(action)
+        }
+
+        // Then
+        let purchaseResult = try XCTUnwrap(result.get())
+        guard case let .success(verificationResult) = purchaseResult,
+              case let .verified(transaction) = verificationResult else {
+            return XCTFail()
+        }
+        XCTAssertEqual(transaction.environment, .xcode)
+    }
+
+    func test_purchase_product_handles_api_errors() throws {
+        // Given
+        network.simulateResponse(requestUrlSuffix: "iap/orders", filename: "error-wp-rest-forbidden")
+
+        // When
+        let result = waitFor { promise in
+            let action = InAppPurchaseAction.purchaseProduct(siteID: self.sampleSiteID, productID: self.sampleProductID) { result in
+                    promise(result)
+                }
+            self.store.onAction(action)
+        }
+
+        // Then
+        XCTAssert(result.isFailure)
+        let error = try XCTUnwrap(result.failure)
+        XCTAssert(error is WordPressApiError)
+    }
+
+    func test_user_is_entitled_to_product_returns_false_when_not_entitled() throws {
+        // Given
+
+        // When
+        let result = waitFor { promise in
+            let action = InAppPurchaseAction.userIsEntitledToProduct(productID: self.sampleProductID) { result in
+                    promise(result)
+                }
+            self.store.onAction(action)
+        }
+
+        // Then
+        let isEntitled = try XCTUnwrap(result.get())
+        XCTAssertFalse(isEntitled)
+    }
+
+    func test_user_is_entitled_to_product_returns_true_when_entitled() throws {
+        // Given
+        try storeKitSession.buyProduct(productIdentifier: sampleProductID)
+
+        // When
+        let result = waitFor { promise in
+            let action = InAppPurchaseAction.userIsEntitledToProduct(productID: self.sampleProductID) { result in
+                    promise(result)
+                }
+            self.store.onAction(action)
+        }
+
+        // Then
+        let isEntitled = try XCTUnwrap(result.get())
+        XCTAssertTrue(isEntitled)
     }
 }

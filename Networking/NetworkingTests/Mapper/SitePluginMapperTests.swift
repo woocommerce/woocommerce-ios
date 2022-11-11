387a766d50797e99f52e8fce5c6f7568254a075c
Huong Do <hi.huongdo@gmail.com>
2022-11-11 09:15:39 +0700

    Unwrap optionals for plugins in SitePluginMapperTests

--- Networking/NetworkingTests/Mapper/SitePluginMapperTests.swift
+++ Networking/NetworkingTests/Mapper/SitePluginMapperTests.swift
@@ -11,40 +11,38 @@ final class SitePluginMapperTests: XCTestCase {
 
     /// Verifies the SitePlugin fields are parsed correctly.
     ///
-    func test_SitePlugin_fields_are_properly_parsed() {
-        let plugin = mapPlugin(from: "plugin")
-        XCTAssertNotNil(plugin)
-        XCTAssertEqual(plugin?.plugin, "jetpack/jetpack")
-        XCTAssertEqual(plugin?.siteID, dummySiteID)
-        XCTAssertEqual(plugin?.status, .active)
-        XCTAssertEqual(plugin?.name, "Jetpack by WordPress.com")
-        XCTAssertEqual(plugin?.pluginUri, "https://jetpack.com")
-        XCTAssertEqual(plugin?.author, "Automattic")
-        XCTAssertEqual(plugin?.descriptionRaw, "Bring the power of the WordPress.com cloud to your self-hosted WordPress.")
-        XCTAssertEqual(plugin?.descriptionRendered, "Bring the power of the WordPress.com cloud to your self-hosted WordPress. " +
+    func test_SitePlugin_fields_are_properly_parsed() throws {
+        let plugin = try XCTUnwrap(mapPlugin(from: "plugin"))
+        XCTAssertEqual(plugin.plugin, "jetpack/jetpack")
+        XCTAssertEqual(plugin.siteID, dummySiteID)
+        XCTAssertEqual(plugin.status, .active)
+        XCTAssertEqual(plugin.name, "Jetpack by WordPress.com")
+        XCTAssertEqual(plugin.pluginUri, "https://jetpack.com")
+        XCTAssertEqual(plugin.author, "Automattic")
+        XCTAssertEqual(plugin.descriptionRaw, "Bring the power of the WordPress.com cloud to your self-hosted WordPress.")
+        XCTAssertEqual(plugin.descriptionRendered, "Bring the power of the WordPress.com cloud to your self-hosted WordPress. " +
                        "<cite>By <a href=\"https://jetpack.com\">Automattic</a>.</cite>")
-        XCTAssertEqual(plugin?.version, "9.5")
-        XCTAssertEqual(plugin?.textDomain, "jetpack")
+        XCTAssertEqual(plugin.version, "9.5")
+        XCTAssertEqual(plugin.textDomain, "jetpack")
     }
 
     /// Verifies the SitePlugin fields are parsed correctly when there's no data envelope wrapping the response.
     ///
-    func test_SitePlugin_fields_are_properly_parsed_for_response_without_data_envelope() {
-        let plugin = mapPluginWithoutEnvelope(from: "site-plugin-without-envelope")
-        XCTAssertNotNil(plugin)
-        XCTAssertEqual(plugin?.plugin, "jetpack/jetpack")
-        XCTAssertEqual(plugin?.siteID, -1)
-        XCTAssertEqual(plugin?.status, .active)
-        XCTAssertEqual(plugin?.name, "Jetpack")
-        XCTAssertEqual(plugin?.pluginUri, "https://jetpack.com")
-        XCTAssertEqual(plugin?.author, "Automattic")
-        XCTAssertEqual(plugin?.descriptionRaw, "Security, performance, and marketing tools made by WordPress experts. " +
+    func test_SitePlugin_fields_are_properly_parsed_for_response_without_data_envelope() throws {
+        let plugin = try XCTUnwrap(mapPluginWithoutEnvelope(from: "site-plugin-without-envelope"))
+        XCTAssertEqual(plugin.plugin, "jetpack/jetpack")
+        XCTAssertEqual(plugin.siteID, -1)
+        XCTAssertEqual(plugin.status, .active)
+        XCTAssertEqual(plugin.name, "Jetpack")
+        XCTAssertEqual(plugin.pluginUri, "https://jetpack.com")
+        XCTAssertEqual(plugin.author, "Automattic")
+        XCTAssertEqual(plugin.descriptionRaw, "Security, performance, and marketing tools made by WordPress experts. " +
                        "Jetpack keeps your site protected so you can focus on more important things.")
-        XCTAssertEqual(plugin?.descriptionRendered, "Security, performance, and marketing tools made by WordPress experts. " +
+        XCTAssertEqual(plugin.descriptionRendered, "Security, performance, and marketing tools made by WordPress experts. " +
                        "Jetpack keeps your site protected so you can focus on more important things. " +
                        "<cite>By <a href=\"https://jetpack.com\">Automattic</a>.</cite>")
-        XCTAssertEqual(plugin?.version, "11.5.1")
-        XCTAssertEqual(plugin?.textDomain, "jetpack")
+        XCTAssertEqual(plugin.version, "11.5.1")
+        XCTAssertEqual(plugin.textDomain, "jetpack")
     }
 }
 

b0b8bf98192e47653259d2e9e048aeabdc2f8130
Jaclyn Chen <watertranquil@gmail.com>
2022-11-15 13:44:43 +0800

    StoreCreationSummaryView: mark `init?(coder aDecoder: NSCoder)` as unavailable.

--- WooCommerce/Classes/Authentication/Store Creation/StoreCreationSummaryView.swift
+++ WooCommerce/Classes/Authentication/Store Creation/StoreCreationSummaryView.swift
@@ -14,6 +14,7 @@ final class StoreCreationSummaryHostingController: UIHostingController<StoreCrea
         }
     }
 
+    @available(*, unavailable)
     required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }

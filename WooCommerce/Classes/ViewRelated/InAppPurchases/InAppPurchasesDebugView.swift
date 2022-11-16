9282886d3c4160a55a33f7422a0da35e69a80a11
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2022-11-16 14:50:01 +0100

    Remove trailing whitespace.

--- WooCommerce/Classes/ViewRelated/InAppPurchases/InAppPurchasesDebugView.swift
+++ WooCommerce/Classes/ViewRelated/InAppPurchases/InAppPurchasesDebugView.swift
@@ -31,7 +31,6 @@ struct InAppPurchasesDebugView: View {
                         }
                     }
                 }
-            
                 Section("Products") {
                     if products.isEmpty {
                         Text("No products")

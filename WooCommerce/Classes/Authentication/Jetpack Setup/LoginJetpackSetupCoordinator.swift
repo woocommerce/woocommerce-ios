9793471f3b490ff4ece8536c5e5eb9bba5a6f021
Huong Do <hi.huongdo@gmail.com>
2022-11-17 11:22:53 +0700

    Present site credential screen from the coordinator

--- WooCommerce/Classes/Authentication/Jetpack Setup/LoginJetpackSetupCoordinator.swift
+++ WooCommerce/Classes/Authentication/Jetpack Setup/LoginJetpackSetupCoordinator.swift
@@ -5,18 +5,22 @@ import Yosemite
 final class LoginJetpackSetupCoordinator: Coordinator {
     let navigationController: UINavigationController
 
+    private let siteURL: String
     private let stores: StoresManager
     private let analytics: Analytics
 
-    init(navigationController: UINavigationController,
+    init(siteURL: String,
+         navigationController: UINavigationController,
          stores: StoresManager = ServiceLocator.stores,
          analytics: Analytics = ServiceLocator.analytics) {
+        self.siteURL = siteURL
         self.navigationController = navigationController
         self.stores = stores
         self.analytics = analytics
     }
 
     func start() {
-        // TODO
+        let siteCredentialUI = SiteCredentialLoginHostingViewController(siteURL: siteURL)
+        navigationController.present(UINavigationController(rootViewController: siteCredentialUI), animated: true)
     }
 }

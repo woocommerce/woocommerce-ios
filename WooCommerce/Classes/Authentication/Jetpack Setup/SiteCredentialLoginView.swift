49813561247611121dc7e3fdf532f8b46e130ce3
Huong Do <hi.huongdo@gmail.com>
2022-11-17 10:09:43 +0700

    Add new view for site credential login

---
+++ WooCommerce/Classes/Authentication/Jetpack Setup/SiteCredentialLoginView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// The view for inputing site credentials
+///
+struct SiteCredentialLoginView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SiteCredentialLoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        SiteCredentialLoginView()
+    }
+}

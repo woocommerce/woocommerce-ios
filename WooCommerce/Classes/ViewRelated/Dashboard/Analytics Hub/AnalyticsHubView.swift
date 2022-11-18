a09de3ecae45399699def6a04bdfb08dedfe3302
Ernesto Carrión <ernesto.carrion@automattic.com>
2022-11-18 14:09:09 -0500

    Adds Empty Analytics Hub View

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubView.swift
@@ -0,0 +1,19 @@
+import Foundation
+import SwiftUI
+
+/// Main Analytics Hub View
+///
+struct AnalyticsHubView: View {
+
+    var body: some View {
+        Text("ITS a ME MARIO")
+    }
+}
+
+// MARK: Preview
+
+struct AnalyticsHubPreview: PreviewProvider {
+    static var previews: some View {
+        AnalyticsHubView()
+    }
+}

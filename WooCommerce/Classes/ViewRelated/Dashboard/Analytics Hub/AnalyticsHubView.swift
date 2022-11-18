8a656e2dc0ccdfdbe1c7e8cc3c26b7f7a073e414
Ernesto Carrión <ernesto.carrion@automattic.com>
2022-11-18 14:58:12 -0500

    Added previews

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubView.swift
@@ -6,7 +6,16 @@ import SwiftUI
 struct AnalyticsHubView: View {
 
     var body: some View {
-        Text("ITS a ME MARIO")
+        Text("Content")
+            .navigationTitle(Localization.title)
+    }
+}
+
+/// Constants
+///
+private extension AnalyticsHubView {
+    struct Localization {
+        static let title = NSLocalizedString("Analytics", comment: "Title for the Analytics Hub screen.")
     }
 }
 
@@ -14,6 +23,8 @@ struct AnalyticsHubView: View {
 
 struct AnalyticsHubPreview: PreviewProvider {
     static var previews: some View {
-        AnalyticsHubView()
+        NavigationView {
+            AnalyticsHubView()
+        }
     }
 }

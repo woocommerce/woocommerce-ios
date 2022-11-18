6b7da8b3d5ca711c1dcf19b6a034345d51a6a4fa
ThomazFB <thomazfcortez@gmail.com>
2022-11-18 18:15:36 -0300

    Adapt AnalyticsHubView to work with AnalyticsHubViewModel

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubViewModel.swift
@@ -1,7 +1,7 @@
 import Yosemite
 
 class AnalyticsHubViewModel: ObservableObject {
-    private let selectedTimeRange: StatsTimeRangeV4
+    let selectedTimeRange: StatsTimeRangeV4
 
     init(selectedTimeRange: StatsTimeRangeV4) {
         self.selectedTimeRange = selectedTimeRange

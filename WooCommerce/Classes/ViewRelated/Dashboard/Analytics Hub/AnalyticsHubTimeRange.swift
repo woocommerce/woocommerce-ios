3cd4b9dedf5e962f7802f3e71909912edb1cd145
ThomazFB <thomazfcortez@gmail.com>
2022-11-19 16:07:38 -0300

    Add start of last month Calendar calculation

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubTimeRange.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubTimeRange.swift
@@ -14,4 +14,8 @@ extension Date {
     func startOfCurrentMonth() -> Date {
         return Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: self)))!
     }
+    
+    func startOfLastMonth() -> Date {
+        return Calendar.current.date(byAdding: .month, value: -1, to: self)!
+    }
 }

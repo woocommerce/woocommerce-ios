d130c1dfb6cc2bd74bbd206e4378c92c7c17b782
Ernesto Carrión <ernesto.carrion@automattic.com>
2022-11-19 10:19:38 -0500

    Adds Dummy view

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubView.swift
@@ -6,8 +6,47 @@ import SwiftUI
 struct AnalyticsHubView: View {
 
     var body: some View {
-        Text("Content")
-            .navigationTitle(Localization.title)
+        ScrollView {
+            VStack(alignment: .leading, spacing: Layout.vertialSpacing) {
+                VStack(spacing: 0) {
+                    Divider()
+                    Text("Placeholder for Revenue Card")
+                        .padding(.leading)
+                        .frame(maxWidth: .infinity, minHeight: 84, alignment: .leading)
+                        .background(Color(uiColor: .listForeground))
+
+                    Divider()
+                }
+
+
+                VStack(spacing: 0) {
+                    Divider()
+
+                    Text("Placeholder For Revenue Card")
+                        .padding(.leading)
+                        .frame(maxWidth: .infinity, minHeight: 220, alignment: .leading)
+                        .background(Color(uiColor: .listForeground))
+
+                    Divider()
+                }
+
+                VStack(spacing: 0) {
+                    Divider()
+
+                    Text("Placeholder For Orders Card")
+                        .padding(.leading)
+                        .frame(maxWidth: .infinity, minHeight: 220, alignment: .leading)
+                        .background(Color(uiColor: .listForeground))
+
+                    Divider()
+                }
+
+                Spacer()
+            }
+        }
+        .navigationTitle(Localization.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(Color(uiColor: .listBackground))
     }
 }
 
@@ -16,6 +55,10 @@ struct AnalyticsHubView: View {
 private extension AnalyticsHubView {
     struct Localization {
         static let title = NSLocalizedString("Analytics", comment: "Title for the Analytics Hub screen.")
+    }
+
+    struct Layout {
+        static let vertialSpacing: CGFloat = 26.0
     }
 }
 

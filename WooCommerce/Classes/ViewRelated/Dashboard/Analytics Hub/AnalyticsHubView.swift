b7ff566ac58313012e8b8af6c9a5ba2d29da4c7b
Ernesto Carrión <ernesto.carrion@automattic.com>
2022-11-19 11:51:31 -0500

    Include results controller

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubView.swift
@@ -1,5 +1,19 @@
 import Foundation
+import Yosemite
 import SwiftUI
+
+/// Hosting Controller for the `AnalyticsHubView` view.
+///
+final class AnalyticsHubHostingViewController: UIHostingController<AnalyticsHubView> {
+    init(timeRange: StatsTimeRangeV4) {
+        super.init(rootView: AnalyticsHubView())
+    }
+
+    @available(*, unavailable)
+    required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
 /// Main Analytics Hub View
 ///
@@ -10,7 +24,7 @@ struct AnalyticsHubView: View {
             VStack(alignment: .leading, spacing: Layout.vertialSpacing) {
                 VStack(spacing: 0) {
                     Divider()
-                    Text("Placeholder for Revenue Card")
+                    Text("Placeholder for Time Range Selection")
                         .padding(.leading)
                         .frame(maxWidth: .infinity, minHeight: 84, alignment: .leading)
                         .background(Color(uiColor: .listForeground))

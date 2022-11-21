1a31d240149ad4c312a15fc667917bd1fa37cf50
Ernesto Carrión <ernesto.carrion@automattic.com>
2022-11-21 17:05:31 -0500

    Extract growth class into their own type

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/StatsCard.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/StatsCard.swift
@@ -31,11 +31,7 @@ struct AnalyticsReportCard: View {
                     Text(totalSales)
                         .titleStyle()
 
-                    Text(totalGrowth)
-                        .foregroundColor(Color(.textInverted))
-                        .captionStyle()
-                        .padding(Layout.growthBackgroundPadding)
-                        .background(Color(totalGrowthColor))
+                    GrowthTag(value: totalGrowth, backgroundColor: totalGrowthColor)
 
                 }
 
@@ -48,14 +44,25 @@ struct AnalyticsReportCard: View {
                     Text(netSales)
                         .titleStyle()
 
-                    Text(netGrowth)
-                        .foregroundColor(Color(.textInverted))
-                        .captionStyle()
-                        .padding(Layout.growthBackgroundPadding)
-                        .background(Color(netGrowthColor))
+                    GrowthTag(value: netGrowth, backgroundColor: netGrowthColor)
                 }
             }
         }
+    }
+}
+
+private struct GrowthTag: View {
+
+    let value: String
+    let backgroundColor: UIColor
+
+    var body: some View {
+        Text(value)
+            .padding(AnalyticsReportCard.Layout.growthBackgroundPadding)
+            .foregroundColor(Color(.textInverted))
+            .captionStyle()
+            .background(Color(backgroundColor))
+            .cornerRadius(AnalyticsReportCard.Layout.growthCornerRadius)
     }
 }
 
@@ -68,6 +75,7 @@ private extension AnalyticsReportCard {
 
     enum Layout {
         static let growthBackgroundPadding = EdgeInsets(top: 2, leading: 8, bottom: 2, trailing: 8)
+        static let growthCornerRadius: CGFloat = 4.0
     }
 }
 
@@ -77,10 +85,10 @@ struct Previews: PreviewProvider {
         AnalyticsReportCard(title: "REVENUE",
                             totalSales: "$3.678",
                             totalGrowth: "+23%",
-                            totalGrowthColor: .systemGreen,
+                            totalGrowthColor: .withColorStudio(.green, shade: .shade40),
                             netSales: "$3.232",
                             netGrowth: "-3%",
-                            netGrowthColor: .systemRed)
+                            netGrowthColor: .withColorStudio(.red, shade: .shade40))
             .previewLayout(.sizeThatFits)
     }
 }

e094d8aab29206883c3a8d3ca9075b64cd13b7ad
Ernesto Carrión <ernesto.carrion@automattic.com>
2022-11-21 16:08:57 -0500

    Adds stats UI components

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/StatsCard.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/StatsCard.swift
@@ -5,7 +5,47 @@ import SwiftUI
 struct AnalyticsReportCard: View {
 
     var body: some View {
-        Text("Content")
+        VStack(alignment: .leading) {
+
+            Text("Title")
+                .foregroundColor(Color(.text))
+                .footnoteStyle()
+
+            HStack {
+                VStack(alignment: .leading) {
+
+                    Text("Left Subtitle")
+                        .font(.callout)
+                        .foregroundColor(Color(.textSubtle))
+
+                    Text("Left Value")
+                        .titleStyle()
+
+                    Text("Left Percentage")
+                        .font(.caption)
+                        .foregroundColor(Color(.textInverted))
+                        .padding(8)
+                        .background(Color(.systemGreen))
+
+                }
+
+                VStack(alignment: .leading) {
+
+                    Text("Right Subtitle")
+                        .font(.callout)
+                        .foregroundColor(Color(.textSubtle))
+
+                    Text("Right Value")
+                        .titleStyle()
+
+                    Text("Right Percentage")
+                        .font(.caption)
+                        .foregroundColor(Color(.textInverted))
+                        .padding(8)
+                        .background(Color(.systemGreen))
+                }
+            }
+        }
     }
 }
 
@@ -13,5 +53,6 @@ struct AnalyticsReportCard: View {
 struct Previews: PreviewProvider {
     static var previews: some View {
         AnalyticsReportCard()
+            .previewLayout(.sizeThatFits)
     }
 }

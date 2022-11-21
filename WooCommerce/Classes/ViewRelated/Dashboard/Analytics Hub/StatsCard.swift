017077ddfeed68bd72ca6727267fe94e655beba9
Ernesto Carrión <ernesto.carrion@automattic.com>
2022-11-21 12:12:18 -0500

    Adds empty report card class

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/StatsCard.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+/// Resuable report card made for the Analytics Hub.
+///
+struct AnalyticsReportCard: View {
+
+    var body: some View {
+        Text("Content")
+    }
+}
+
+// MARK: Previews
+struct Previews: PreviewProvider {
+    static var previews: some View {
+        AnalyticsReportCard()
+    }
+}

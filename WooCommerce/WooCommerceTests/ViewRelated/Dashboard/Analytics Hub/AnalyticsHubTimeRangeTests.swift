7c1ba30320ef31dd817b64170fd2f4884897cd6a
ThomazFB <thomazfcortez@gmail.com>
2022-11-22 17:43:14 -0300

    Finish AnalyticsHubTimeRangeTests coverage

--- WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubTimeRangeTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubTimeRangeTests.swift
@@ -76,6 +76,62 @@ final class AnalyticsHubTimeRangeTests: XCTestCase {
         XCTAssertEqual(previousTimeRange.end, dateFrom("2022-06-30"))
     }
 
+    func test_when_time_range_inits_with_thisYear_then_generate_expected_descriptions() {
+        // Given
+        let currentDate = dateFrom("2022-07-01")
+        let timeRange = AnalyticsHubTimeRange(selectedTimeRange: .thisYear, currentDate: currentDate)
+
+        // When
+        let currentRangeDescription = timeRange.currentRangeDescription
+        let previousRangeDescription = timeRange.previousRangeDescription
+
+        // Then
+        XCTAssertEqual(currentRangeDescription, "Jan 1 - Jul 1, 2022")
+        XCTAssertEqual(previousRangeDescription, "Jan 1 - Jul 1, 2021")
+    }
+
+    func test_when_time_range_inits_with_thisMonth_then_generate_expected_descriptions() {
+        // Given
+        let currentDate = dateFrom("2022-07-31")
+        let timeRange = AnalyticsHubTimeRange(selectedTimeRange: .thisMonth, currentDate: currentDate)
+
+        // When
+        let currentRangeDescription = timeRange.currentRangeDescription
+        let previousRangeDescription = timeRange.previousRangeDescription
+
+        // Then
+        XCTAssertEqual(currentRangeDescription, "Jul 1 - 31, 2022")
+        XCTAssertEqual(previousRangeDescription, "Jun 1 - 30, 2022")
+    }
+
+    func test_when_time_range_inits_with_thisWeek_then_generate_expected_descriptions() {
+        // Given
+        let currentDate = dateFrom("2022-07-29")
+        let timeRange = AnalyticsHubTimeRange(selectedTimeRange: .thisWeek, currentDate: currentDate)
+
+        // When
+        let currentRangeDescription = timeRange.currentRangeDescription
+        let previousRangeDescription = timeRange.previousRangeDescription
+
+        // Then
+        XCTAssertEqual(currentRangeDescription, "Jul 25 - 29, 2022")
+        XCTAssertEqual(previousRangeDescription, "Jul 18 - 22, 2022")
+    }
+
+    func test_when_time_range_inits_with_today_then_generate_expected_descriptions() {
+        // Given
+        let currentDate = dateFrom("2022-07-01")
+        let timeRange = AnalyticsHubTimeRange(selectedTimeRange: .today, currentDate: currentDate)
+
+        // When
+        let currentRangeDescription = timeRange.currentRangeDescription
+        let previousRangeDescription = timeRange.previousRangeDescription
+
+        // Then
+        XCTAssertEqual(currentRangeDescription, "Jul 1, 2022")
+        XCTAssertEqual(previousRangeDescription, "Jun 30, 2022")
+    }
+
     private func dateFrom(_ date: String) -> Date {
         return dateFormatter.date(from: date)!
     }

dd659ca01b039a44673b24565f22e017e52682ab
ThomazFB <thomazfcortez@gmail.com>
2022-11-22 16:35:39 -0300

    Improve AnalyticsHubTimeRangeTests date test strategy

--- WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubTimeRangeTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubTimeRangeTests.swift
@@ -10,7 +10,7 @@ final class AnalyticsHubTimeRangeTests: XCTestCase {
 
     func test_when_time_range_inits_with_thisYear_then_generate_expected_ranges() {
         // Given
-        let currentDate = dateFrom("2022-02-05")
+        let currentDate = dateFrom("2020-02-29")
         let timeRange = AnalyticsHubTimeRange(selectedTimeRange: .thisYear, currentDate: currentDate)
 
         // When
@@ -18,11 +18,28 @@ final class AnalyticsHubTimeRangeTests: XCTestCase {
         let previousTimeRange = timeRange.previousTimeRange
 
         // Then
-        XCTAssertEqual(currentTimeRange.start, dateFrom("2022-01-01"))
-        XCTAssertEqual(currentTimeRange.end, dateFrom("2022-02-05"))
+        XCTAssertEqual(currentTimeRange.start, dateFrom("2020-01-01"))
+        XCTAssertEqual(currentTimeRange.end, dateFrom("2020-02-29"))
 
-        XCTAssertEqual(previousTimeRange.start, dateFrom("2021-01-01"))
-        XCTAssertEqual(previousTimeRange.end, dateFrom("2021-02-05"))
+        XCTAssertEqual(previousTimeRange.start, dateFrom("2019-01-01"))
+        XCTAssertEqual(previousTimeRange.end, dateFrom("2019-02-28"))
+    }
+
+    func test_when_time_range_inits_with_thisMonth_then_generate_expected_ranges() {
+        // Given
+        let currentDate = dateFrom("2010-07-31")
+        let timeRange = AnalyticsHubTimeRange(selectedTimeRange: .thisMonth, currentDate: currentDate)
+
+        // When
+        let currentTimeRange = timeRange.currentTimeRange
+        let previousTimeRange = timeRange.previousTimeRange
+
+        // Then
+        XCTAssertEqual(currentTimeRange.start, dateFrom("2010-07-01"))
+        XCTAssertEqual(currentTimeRange.end, dateFrom("2010-07-31"))
+
+        XCTAssertEqual(previousTimeRange.start, dateFrom("2010-06-01"))
+        XCTAssertEqual(previousTimeRange.end, dateFrom("2010-06-30"))
     }
 
     private func dateFrom(_ date: String) -> Date {

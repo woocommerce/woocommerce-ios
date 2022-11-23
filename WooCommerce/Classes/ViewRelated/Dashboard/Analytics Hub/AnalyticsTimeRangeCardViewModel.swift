5565f128507be466a05665f33b38bf66a8c88b2c
ThomazFB <thomazfcortez@gmail.com>
2022-11-23 11:07:02 -0300

    Configure AnalyticsTimeRangeCardViewModel

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsTimeRangeCardViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsTimeRangeCardViewModel.swift
@@ -1,1 +1,18 @@
 import Foundation
+
+/// Analytics Hub Time Range Card ViewModel.
+/// Used to transmit analytics time range data.
+///
+struct AnalyticsTimeRangeCardViewModel {
+    /// Time Range Title.
+    ///
+    let selectedRangeTitle: String
+
+    /// Current Range Subtitle.
+    ///
+    let currentRangeSubtitle: String
+
+    /// Previous Range Subtitle.
+    ///
+    let previousRangeSubtitle: String
+}

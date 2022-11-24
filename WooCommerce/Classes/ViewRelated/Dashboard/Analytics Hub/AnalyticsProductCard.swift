1ed9152a2a25e333bd9cc6629f271731f7ded3f1
Ernesto Carrión <ernesto.carrion@automattic.com>
2022-11-24 15:43:35 -0500

    Adds Empty AnalyticsProductCard

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsProductCard.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+/// Products Card on the Analytics Hub
+///
+struct AnalyticsProductCard: View {
+    var body: some View {
+        Text("Empty")
+    }
+}
+
+
+// MARK: Previews
+struct AnalyticsProductCardPreviews: PreviewProvider {
+    static var previews: some View {
+        AnalyticsProductCard()
+            .previewLayout(.sizeThatFits)
+    }
+}

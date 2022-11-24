ac2850ccb716ebeabd0f12425be9041d904fb9c3
Jaclyn Chen <watertranquil@gmail.com>
2022-11-24 11:12:01 +0800

    Add unit tests for `PaymentRemote`.

--- Networking/Networking/Remote/PaymentRemote.swift
+++ Networking/Networking/Remote/PaymentRemote.swift
@@ -1,9 +1,18 @@
 import Foundation
 
+/// Protocol for `PaymentRemote` mainly used for mocking.
 public protocol PaymentRemoteProtocol {
+    /// Loads the WPCOM plan remotely that matches the product ID.
+    /// - Parameter productID: The ID of the WPCOM plan product.
+    /// - Returns: The WPCOM plan that matches the given product ID.
     func loadPlan(thatMatchesID productID: Int64) async throws -> WPComPlan
 
-    func createCart(siteID: Int64, productID: Int64) async throws -> CreateCartResponse
+    /// Creates a cart with the given product ID for the site ID.
+    /// - Parameters:
+    ///   - siteID: The ID of the site that the product is being added to.
+    ///   - productID: The ID of the product to be added to the site.
+    /// - Returns: The remote response from creating a cart.
+    func createCart(siteID: Int64, productID: Int64) async throws
 }
 
 /// WPCOM Payment Endpoints
@@ -19,7 +28,7 @@ public class PaymentRemote: Remote, PaymentRemoteProtocol {
         return plan
     }
 
-    public func createCart(siteID: Int64, productID: Int64) async throws -> CreateCartResponse {
+    public func createCart(siteID: Int64, productID: Int64) async throws {
         let path = "\(Path.cartCreation)/\(siteID)"
 
         let parameters: [String: Any] = [
@@ -29,14 +38,19 @@ public class PaymentRemote: Remote, PaymentRemoteProtocol {
                     "volume": 1
                 ]
             ],
+            // Necessary to create a persistent cart for later checkout, the default value is `true`.
             "temporary": false
         ]
         let request = DotcomRequest(wordpressApiVersion: .mark1_1, method: .post, path: path, parameters: parameters)
-        return try await enqueue(request)
+        let response: CreateCartResponse = try await enqueue(request)
+        guard response.products.contains(where: { $0.productID == productID }) else {
+            throw CreateCartError.noMatchingProduct
+        }
     }
 }
 
-public struct WPComPlan: Decodable {
+/// Contains necessary data for rendering a WPCOM plan in the app.
+public struct WPComPlan: Decodable, Equatable {
     public let productID: Int64
     public let name: String
     public let formattedPrice: String
@@ -48,11 +62,35 @@ public struct WPComPlan: Decodable {
     }
 }
 
+/// Possible error cases from loading a WPCOM plan.
 public enum LoadPlanError: Error {
     case noMatchingPlan
 }
 
-public struct CreateCartResponse: Decodable {}
+/// Possible error cases from creating cart for a site with a WPCOM plan.
+public enum CreateCartError: Error {
+    case noMatchingProduct
+}
+
+/// Contains necessary data for handling the remote response from creating a cart.
+private struct CreateCartResponse: Decodable {
+    let products: [Product]
+
+    private enum CodingKeys: String, CodingKey {
+        case products
+    }
+}
+
+private extension CreateCartResponse {
+    /// Describes a product in a cart.
+    struct Product: Decodable {
+        let productID: Int64
+
+        private enum CodingKeys: String, CodingKey {
+            case productID = "product_id"
+        }
+    }
+}
 
 // MARK: - Constants
 //
---
+++ Networking/NetworkingTests/Remote/PaymentRemoteTests.swift
@@ -0,0 +1,112 @@
+import XCTest
+import TestKit
+@testable import Networking
+
+final class PaymentRemoteTests: XCTestCase {
+    /// Mock network wrapper.
+    private var network: MockNetwork!
+
+    override func setUp() {
+        super.setUp()
+        network = MockNetwork()
+    }
+
+    override func tearDown() {
+        network = nil
+        super.tearDown()
+    }
+
+    // MARK: - `loadPlan`
+
+    func test_loadPlan_returns_plan_on_success() async throws {
+        // Given
+        let remote = PaymentRemote(network: network)
+        network.simulateResponse(requestUrlSuffix: "plans", filename: "load-plan-success")
+
+        // When
+        let plan = try await remote.loadPlan(thatMatchesID: Constants.planProductID)
+
+        // Then
+        XCTAssertEqual(plan, .init(productID: Constants.planProductID,
+                                   name: "WordPress.com eCommerce",
+                                   formattedPrice: "NT$2,230"))
+    }
+
+    func test_loadPlan_throws_noMatchingPlan_error_when_response_does_not_include_plan_with_given_id() async throws {
+        // Given
+        let remote = PaymentRemote(network: network)
+        network.simulateResponse(requestUrlSuffix: "plans", filename: "load-plan-success")
+
+        // When
+        await assertThrowsError {
+            _ = try await remote.loadPlan(thatMatchesID: 9)
+        } errorAssert: { error in
+            // Then
+            (error as? LoadPlanError) == .noMatchingPlan
+        }
+    }
+
+    func test_loadPlan_throws_notFound_error_when_no_response() async throws {
+        // Given
+        let remote = PaymentRemote(network: network)
+
+        // When
+        await assertThrowsError {
+            _ = try await remote.loadPlan(thatMatchesID: 9)
+        } errorAssert: { error in
+            // Then
+            (error as? NetworkError) == .notFound
+        }
+    }
+
+    // MARK: - `createCart`
+
+    func test_createCart_returns_on_success() async throws {
+        // Given
+        let siteID: Int64 = 606
+        let remote = PaymentRemote(network: network)
+        network.simulateResponse(requestUrlSuffix: "me/shopping-cart/\(siteID)", filename: "create-cart-success")
+
+        // When
+        do {
+            try await remote.createCart(siteID: siteID, productID: Constants.planProductID)
+        } catch {
+            // Then
+            XCTFail("Unexpected error: \(error)")
+        }
+    }
+
+    func test_createCart_throws_noMatchingProduct_error_when_response_does_not_include_plan_with_given_id() async throws {
+        // Given
+        let siteID: Int64 = 606
+        let remote = PaymentRemote(network: network)
+        network.simulateResponse(requestUrlSuffix: "me/shopping-cart/\(siteID)", filename: "create-cart-success")
+
+        // When
+        await assertThrowsError {
+            _ = try await remote.createCart(siteID: siteID, productID: 685)
+        } errorAssert: { error in
+            // Then
+            (error as? CreateCartError) == .noMatchingProduct
+        }
+    }
+
+    func test_createCart_throws_notFound_error_when_no_response() async throws {
+        // Given
+        let remote = PaymentRemote(network: network)
+
+        // When
+        await assertThrowsError {
+            _ = try await remote.createCart(siteID: 606, productID: 685)
+        } errorAssert: { error in
+            // Then
+            (error as? NetworkError) == .notFound
+        }
+    }
+}
+
+private extension PaymentRemoteTests {
+    enum Constants {
+        static let planProductID: Int64 = 1021
+    }
+}

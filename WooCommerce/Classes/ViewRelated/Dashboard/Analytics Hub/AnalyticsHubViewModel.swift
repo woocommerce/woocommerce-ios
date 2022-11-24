e1300aaec1248ee751aa37b314108b7a40d58770
rachelmcr <rachel@automattic.com>
2022-11-24 09:39:51 +0000

    Initialize orders card with placeholders and constants

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubViewModel.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Yosemite
+import class UIKit.UIColor
 
 /// Main View Model for the Analytics Hub.
 ///
@@ -19,15 +20,15 @@ final class AnalyticsHubViewModel: ObservableObject {
 
     /// Orders Card ViewModel
     ///
-    @Published var ordersCard = AnalyticsReportCardViewModel(title: "ORDERS",
-                                                             leadingTitle: "Total Orders",
-                                                             leadingValue: "145",
-                                                             leadingDelta: "+36%",
-                                                             leadingDeltaColor: .withColorStudio(.green, shade: .shade50),
-                                                             trailingTitle: "Average Order Value",
-                                                             trailingValue: "$57,99",
-                                                             trailingDelta: "-16%",
-                                                             trailingDeltaColor: .withColorStudio(.red, shade: .shade40))
+    @Published var ordersCard = AnalyticsReportCardViewModel(title: Localization.OrderCard.title,
+                                                             leadingTitle: Localization.OrderCard.leadingTitle,
+                                                             leadingValue: Constants.placeholderLabel,
+                                                             leadingDelta: Constants.placeholderLabel,
+                                                             leadingDeltaColor: Constants.deltaColor(for: .negative),
+                                                             trailingTitle: Localization.OrderCard.trailingTitle,
+                                                             trailingValue: Constants.placeholderLabel,
+                                                             trailingDelta: Constants.placeholderLabel,
+                                                             trailingDeltaColor: Constants.deltaColor(for: .negative))
 
     // MARK: Private data
 
@@ -38,4 +39,27 @@ final class AnalyticsHubViewModel: ObservableObject {
     /// Order stats for the previous time period (for comparison)
     ///
     @Published private var previousOrderStats: OrderStatsV4? = nil
+}
+
+// MARK: - Constants
+private extension AnalyticsHubViewModel {
+    enum Constants {
+        static let placeholderLabel = "-"
+        static func deltaColor(for direction: StatsDataTextFormatter.DeltaPercentage.Direction) -> UIColor {
+            switch direction {
+            case .positive:
+                return .withColorStudio(.green, shade: .shade50)
+            case .negative, .zero:
+                return .withColorStudio(.red, shade: .shade40)
+            }
+        }
+    }
+
+    enum Localization {
+        enum OrderCard {
+            static let title = NSLocalizedString("ORDERS", comment: "Title for order analytics section in the Analytics Hub")
+            static let leadingTitle = NSLocalizedString("Total Orders", comment: "Label for total number of orders in the Analytics Hub")
+            static let trailingTitle = NSLocalizedString("Average Order Value", comment: "Label for average value of orders in the Analytics Hub")
+        }
+    }
 }

8c832a14634504b8a5371c4a419259af89cd17c4
Jaclyn Chen <watertranquil@gmail.com>
2022-11-24 11:29:59 +0800

    Add unit tests for `PaymentStore`.

--- Yosemite/Yosemite/Actions/PaymentAction.swift
+++ Yosemite/Yosemite/Actions/PaymentAction.swift
@@ -3,13 +3,17 @@ import Foundation
 /// PaymentAction: Defines all of the Actions supported by the PaymentStore.
 ///
 public enum PaymentAction: Action {
+    /// Loads a specific WPCOM plan.
+    /// - Parameters:
+    ///   - productID: The ID of the WPCOM product to return.
+    ///   - completion: Invoked when the WPCOM plan that matches the given ID is loaded.
     case loadPlan(productID: Int64,
                   completion: (Result<WPComPlan, Error>) -> Void)
 
-    /// Creates a cart with a WPCOM product.
+    /// Creates a cart with a WPCOM plan.
     /// - Parameters:
-    ///   - productID: The ID of the WPCOM product.
-    ///   - siteID: The site ID for the WPCOM product to be attached to.
+    ///   - productID: The ID of the WPCOM plan product.
+    ///   - siteID: The site ID for the WPCOM plan to be attached to.
     ///   - completion: The result of cart creation.
     case createCart(productID: String,
                     siteID: Int64,

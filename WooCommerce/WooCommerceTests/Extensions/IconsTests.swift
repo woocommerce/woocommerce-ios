f0c2a37a200814f69709d64de84b88d3ba4a8a6f
ThomazFB <thomazfcortez@gmail.com>
2022-11-24 10:39:36 -0300

    Add new calendar icon test to IconsTests

--- WooCommerce/WooCommerceTests/Extensions/IconsTests.swift
+++ WooCommerce/WooCommerceTests/Extensions/IconsTests.swift
@@ -707,6 +707,9 @@ final class IconsTests: XCTestCase {
 
     func test_wpcomLogoImage_is_not_nil() {
         XCTAssertNotNil(UIImage.wpcomLogoImage)
+    }
 
+    func test_calendar_icon_is_not_nil() {
+        XCTAssertNotNil(UIImage.calendar)
     }
 }

a9a43522a676d318c668cdb0c5ee542397d82d01
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-11-25 09:43:00 +0530

    Add event to track user trying again.

--- WooCommerce/Classes/Analytics/WooAnalyticsStat.swift
+++ WooCommerce/Classes/Analytics/WooAnalyticsStat.swift
@@ -114,6 +114,8 @@ public enum WooAnalyticsStat: String {
 
     case loginJetpackSetupAuthorizedUsingDifferentWPCOMAccount = "login_jetpack_setup_authorized_using_different_wpcom_account"
 
+    case loginJetpackSetupScreenTryAgainButtonTapped = "login_jetpack_setup_try_again_button_tapped"
+
     // MARK: No matched site alert
     //
     case loginJetpackNoMatchedSiteErrorViewed = "login_jetpack_no_matched_site_error_viewed"

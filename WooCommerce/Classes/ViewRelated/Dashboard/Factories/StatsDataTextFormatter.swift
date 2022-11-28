0d89a69c543ef4dfabad76379d6938d62dd96029
rachelmcr <rachel@automattic.com>
2022-11-28 13:12:58 +0000

    Add stats helper to get chart data values

--- WooCommerce/Classes/ViewRelated/Dashboard/Factories/StatsDataTextFormatter.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Factories/StatsDataTextFormatter.swift
@@ -208,6 +208,33 @@ extension StatsDataTextFormatter {
             return lhs.dateStart(timeZone: siteTimezone) < rhs.dateStart(timeZone: siteTimezone)
         }) ?? []
     }
+
+    /// Returns the requested stats total data values for every interval in the provided order stats.
+    ///
+    /// Used to create a line chart with the returned values.
+    ///
+    static func getChartData(for dataType: StatsTotalData, from orderStats: OrderStatsV4?) -> [Double] {
+        let intervals = sortOrderStatsIntervals(from: orderStats)
+        return intervals.map { interval in
+            switch dataType {
+            case .totalRevenue:
+                return (interval.subtotals.grossRevenue as NSNumber).doubleValue
+            case .netRevenue:
+                return (interval.subtotals.netRevenue as NSNumber).doubleValue
+            case .orderCount:
+                return Double(interval.subtotals.totalOrders)
+            case .averageOrderValue:
+                return (interval.subtotals.averageOrderValue as NSNumber).doubleValue
+            }
+        }
+    }
+
+    enum StatsTotalData {
+        case totalRevenue
+        case netRevenue
+        case orderCount
+        case averageOrderValue
+    }
 }
 
 // MARK: - Private helpers

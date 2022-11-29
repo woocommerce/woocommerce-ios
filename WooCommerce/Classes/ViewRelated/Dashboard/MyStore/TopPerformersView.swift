3578a7545c3549c323233c7bf3035dfab318c5bb
Ernesto Carrión <ernesto.carrion@automattic.com>
2022-11-29 11:05:10 -0500

    Adds empty view

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/MyStore/TopPerformersView.swift
@@ -0,0 +1,20 @@
+import UIKit
+import SwiftUI
+
+/// View that displays a collection of top items organized in a list.
+///
+struct TopPerformersView: View {
+
+    var body: some View {
+        Text("View")
+    }
+}
+
+
+// MARK: Previews
+struct TopPerformersPreview: PreviewProvider {
+    static var previews: some View {
+        TopPerformersView()
+            .previewLayout(.sizeThatFits)
+    }
+}

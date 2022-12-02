7281857e54077deb1fc7f158fe43deb97444a9b2
Gio Lodi <gio@mokacoding.com>
2022-12-02 17:38:29 +0100

    Specialize previous OAuth token `URLRequest` logic for Google

---
+++ WordPressAuthenticator/WordPressAuthenticatorTests/GoogleSignIn/URLRequest+GoogleSignInTests.swift
@@ -0,0 +1,32 @@
+@testable import WordPressAuthenticator
+import XCTest
+
+class URLRequestOAuthTokenRequestTests: XCTestCase {
+
+    let testBody = OAuthTokenRequestBody(
+        clientId: "a",
+        clientSecret: "b",
+        code: "c",
+        codeVerifier: "d",
+        grantType: "e",
+        redirectURI: "f"
+    )
+
+    func testURL() throws {
+        let request = try URLRequest.googleSignInTokenRequest(body: testBody)
+        XCTAssertEqual(request.url, URL(string: "https://oauth2.googleapis.com/token")!)
+    }
+
+    func testMethodPost() throws {
+        let request = try URLRequest.googleSignInTokenRequest(body: testBody)
+        XCTAssertEqual(request.httpMethod, "POST")
+    }
+
+    func testContentTypeFormURLEncoded() throws {
+        let request = try URLRequest.googleSignInTokenRequest(body: testBody)
+        XCTAssertEqual(
+            request.value(forHTTPHeaderField: "Content-Type"),
+            "application/x-www-form-urlencoded; charset=UTF-8"
+        )
+    }
+}

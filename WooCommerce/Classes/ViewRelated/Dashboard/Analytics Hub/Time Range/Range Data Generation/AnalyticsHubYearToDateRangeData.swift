22b0b021d41017670d1c8f506536064763530dc8
ThomazFB <thomazfcortez@gmail.com>
2022-12-05 15:06:14 -0300

    Add documentation to the Range data structs

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/Time Range/Range Data Generation/AnalyticsHubYearToDateRangeData.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/Time Range/Range Data Generation/AnalyticsHubYearToDateRangeData.swift
@@ -1,5 +1,13 @@
 import Foundation
 
+/// Responsible for defining two ranges of data, one starting from January 1st  of the current year
+/// until the current date and the previous one, starting from January 1st of the last year
+/// until the same day on the in that year. E. g.
+///
+/// Today: 1 Jul 2022
+/// Current range: Jan 1 until Jul 1, 2022
+/// Previous range: Jan 1 until Jul 1, 2022
+///
 struct AnalyticsHubYearToDateRangeData: AnalyticsHubTimeRangeData {
     let currentDateStart: Date?
     let currentDateEnd: Date?

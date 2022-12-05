f8b6f16e966d3cfd15f445c5c76d933e60f3229a
ThomazFB <thomazfcortez@gmail.com>
2022-12-05 11:29:49 -0300

    Introduce expected SelectionTypes

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/Time Range/AnalyticsHubTimeRangeSelection.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/Time Range/AnalyticsHubTimeRangeSelection.swift
@@ -41,6 +41,12 @@ public class AnalyticsHubTimeRangeSelection {
             selectionData = AnalyticsHubTodayRangeData(referenceDate: currentDate, timezone: timezone, calendar: calendar)
         case .yesterday:
             selectionData = AnalyticsHubYesterdayRangeData(referenceDate: currentDate, timezone: timezone, calendar: calendar)
+        case .lastWeek:
+            selectionData = AnalyticsHubLastWeekRangeData(referenceDate: currentDate, timezone: timezone, calendar: calendar)
+        case .lastMonth:
+            selectionData = AnalyticsHubLastMonthRangeData(referenceDate: currentDate, timezone: timezone, calendar: calendar)
+        case .lastYear:
+            selectionData = AnalyticsHubLastYearRangeData(referenceDate: currentDate, timezone: timezone, calendar: calendar)
         case .weekToDate:
             selectionData = AnalyticsHubWeekToDateRangeData(referenceDate: currentDate, timezone: timezone, calendar: calendar)
         case .monthToDate:
@@ -89,6 +95,9 @@ extension AnalyticsHubTimeRangeSelection {
     enum SelectionType: CaseIterable {
         case today
         case yesterday
+        case lastWeek
+        case lastMonth
+        case lastYear
         case weekToDate
         case monthToDate
         case yearToDate
@@ -99,6 +108,12 @@ extension AnalyticsHubTimeRangeSelection {
                 return Localization.today
             case .yesterday:
                 return Localization.yesterday
+            case .lastWeek:
+                return Localization.lastWeek
+            case .lastMonth:
+                return Localization.lastMonth
+            case .lastYear:
+                return Localization.lastYear
             case .weekToDate:
                 return Localization.weekToDate
             case .monthToDate:
@@ -132,6 +147,9 @@ extension AnalyticsHubTimeRangeSelection {
     enum Localization {
         static let today = NSLocalizedString("Today", comment: "Title of the Analytics Hub Today's selection range")
         static let yesterday = NSLocalizedString("Yesterday", comment: "Title of the Analytics Hub Yesterday selection range")
+        static let lastWeek = NSLocalizedString("Last Week", comment: "Title of the Analytics Hub Last Week selection range")
+        static let lastMonth = NSLocalizedString("Last Month", comment: "Title of the Analytics Hub Last Month selection range")
+        static let lastYear = NSLocalizedString("Last Year", comment: "Title of the Analytics Hub Last Year selection range")
         static let weekToDate = NSLocalizedString("Week to Date", comment: "Title of the Analytics Hub Week to Date selection range")
         static let monthToDate = NSLocalizedString("Month to Date", comment: "Title of the Analytics Hub Month to Date selection range")
         static let yearToDate = NSLocalizedString("Year to Date", comment: "Title of the Analytics Hub Year to Date selection range")

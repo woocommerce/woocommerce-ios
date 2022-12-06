74b83515b4059ebaf905fece3fdb96a6698dbe5b
ThomazFB <thomazfcortez@gmail.com>
2022-12-06 16:54:34 -0300

    Adjust AnalyticsHubLastQuarterRangeData to correctly calculate the last quarter range

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/Time Range/Range Data Generation/AnalyticsHubLastQuarterRangeData.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/Time Range/Range Data Generation/AnalyticsHubLastQuarterRangeData.swift
@@ -7,10 +7,12 @@ struct AnalyticsHubLastQuarterRangeData: AnalyticsHubTimeRangeData {
     let previousDateEnd: Date?
 
     init(referenceDate: Date, timezone: TimeZone, calendar: Calendar) {
-        self.currentDateEnd = referenceDate.endOfQuarter(timezone: timezone, calendar: calendar)
-        self.currentDateStart = referenceDate.startOfQuarter(timezone: timezone, calendar: calendar)
-        let previousDateEnd = calendar.date(byAdding: .month, value: -3, to: referenceDate)
-        self.previousDateEnd = previousDateEnd?.endOfQuarter(timezone: timezone, calendar: calendar)
-        self.previousDateStart = previousDateEnd?.startOfQuarter(timezone: timezone, calendar: calendar)
+        let oneQuarterAgo = calendar.date(byAdding: .month, value: -3, to: referenceDate)
+        self.currentDateEnd = oneQuarterAgo?.endOfQuarter(timezone: timezone, calendar: calendar)
+        self.currentDateStart = oneQuarterAgo?.startOfQuarter(timezone: timezone, calendar: calendar)
+
+        let twoQuartersAgo = calendar.date(byAdding: .month, value: -6, to: referenceDate)
+        self.previousDateEnd = twoQuartersAgo?.endOfQuarter(timezone: timezone, calendar: calendar)
+        self.previousDateStart = twoQuartersAgo?.startOfQuarter(timezone: timezone, calendar: calendar)
     }
 }

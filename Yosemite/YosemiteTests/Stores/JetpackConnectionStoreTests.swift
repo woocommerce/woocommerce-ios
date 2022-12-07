dcb836d1df5fcea146f99008f55442c81ecea427
Gio Lodi <gio@mokacoding.com>
2022-12-07 20:12:59 +0100

    Remove an unnecessary `siteURL` re-declaration in a test

--- Yosemite/YosemiteTests/Stores/JetpackConnectionStoreTests.swift
+++ Yosemite/YosemiteTests/Stores/JetpackConnectionStoreTests.swift
@@ -229,7 +229,6 @@ final class JetpackConnectionStoreTests: XCTestCase {
 
     func test_fetchJetpackUser_properly_relays_errors() {
         // Given
-        let siteURL = "http://test.com"
         let urlSuffix = "/jetpack/v4/connection/data"
         let error = NetworkError.unacceptableStatusCode(statusCode: 500)
         network.simulateError(requestUrlSuffix: urlSuffix, error: error)

130a9f36f12ad512855c0b894a8bb3c08065711f
Huong Do <hi.huongdo@gmail.com>
2022-12-07 16:34:41 +0700

    Update WordPressOrgNetwork to handle cookie authentication

--- Networking/Networking/Network/WordPressOrgNetwork.swift
+++ Networking/Networking/Network/WordPressOrgNetwork.swift
@@ -1,104 +1,106 @@
-import Alamofire
 import Combine
 import Foundation
-import WordPressKit
+
+/// Configuration for handling cookie nonce authentication.
+///
+public struct CookieNonceAuthenticatorConfiguration {
+    let username: String
+    let password: String
+    let loginURL: URL
+    let adminURL: URL
+
+    public init(username: String, password: String, loginURL: URL, adminURL: URL) {
+        self.username = username
+        self.password = password
+        self.loginURL = loginURL
+        self.adminURL = adminURL
+    }
+}
 
 /// Class to handle WP.org REST API requests.
 ///
 public final class WordPressOrgNetwork: Network {
 
-    private let authenticator: Authenticator?
-    private let userAgent: String?
+    private var nonce: String?
+    private let configuration: CookieNonceAuthenticatorConfiguration
 
-    private lazy var sessionManager: Alamofire.SessionManager = {
-        let sessionConfiguration = URLSessionConfiguration.default
-        let sessionManager = makeSessionManager(configuration: sessionConfiguration)
-        return sessionManager
-    }()
-
-    private lazy var backgroundSessionManager: Alamofire.SessionManager = {
-        // A unique ID is included in the background session identifier so that the session does not get invalidated when the initializer is called multiple
-        // times (e.g. when logging in).
-        let uniqueID = UUID().uuidString
-        let sessionConfiguration = URLSessionConfiguration.background(withIdentifier: "com.automattic.woocommerce.backgroundsession.\(uniqueID)")
-        let sessionManager = makeSessionManager(configuration: sessionConfiguration)
-        return sessionManager
-    }()
+    private let sessionManager: SessionManager = .default
 
     public var session: URLSession { sessionManager.session }
 
-    public init(authenticator: Authenticator? = nil, userAgent: String = UserAgent.defaultUserAgent) {
-        self.authenticator = authenticator
-        self.userAgent = userAgent
+    public init(configuration: CookieNonceAuthenticatorConfiguration) {
+        self.configuration = configuration
     }
 
     public func responseData(for request: Request) async throws -> Data? {
-        return try await withCheckedThrowingContinuation { [weak self] continuation in
-            guard let self = self else { return }
-
-            self.sessionManager.request(request)
-                .validate()
-                .responseData(completionHandler: { (response) in
-                switch response.result {
-                case .success(let responseObject):
-                    continuation.resume(returning: responseObject)
-                case .failure(let error):
-                    DDLogWarn("⚠️ Error requesting \(request.urlRequest?.url?.absoluteString ?? ""): \(error.localizedDescription)")
-                    continuation.resume(throwing: error)
-                }
-
-            })
-        }
+        let request = try await createRequest(wrapping: request)
+        let data = try await sessionManager.request(request)
+        return data
     }
 
     /// Executes the specified Network Request. Upon completion, the payload will be sent back to the caller as a Data instance.
-    ///
-    /// - Important:
-    ///     - User agent and authenticator from the initializer will be injected through the `validate` call.
     ///
     /// - Parameters:
     ///     - request: Request that should be performed.
     ///     - completion: Closure to be executed upon completion.
     ///
     public func responseData(for request: Request, completion: @escaping (Data?, Error?) -> Void) {
-        sessionManager.request(request)
-            .validate()
-            .responseData { response in
-                completion(response.value, response.networkingError)
+        Task {
+            do {
+                let request = try await createRequest(wrapping: request)
+                let data = try await sessionManager.request(request)
+                await MainActor.run {
+                    completion(data, nil)
+                }
+            } catch {
+                await MainActor.run {
+                    completion(nil, error)
+                }
             }
+        }
     }
 
     /// Executes the specified Network Request. Upon completion, the payload will be sent back to the caller as a Data instance.
-    ///
-    /// - Important:
-    ///     - User agent and authenticator from the initializer will be injected through the `validate` call..
     ///
     /// - Parameters:
     ///     - request: Request that should be performed.
     ///     - completion: Closure to be executed upon completion.
     ///
-    public func responseData(for request: Request, completion: @escaping (Swift.Result<Data, Error>) -> Void) {
-        sessionManager.request(request)
-            .validate()
-            .responseData { response in
-                completion(response.result.toSwiftResult())
+    public func responseData(for request: Request, completion: @escaping (Result<Data, Error>) -> Void) {
+        Task {
+            do {
+                let request = try await createRequest(wrapping: request)
+                let data = try await sessionManager.request(request)
+                await MainActor.run {
+                    completion(.success(data))
+                }
+            } catch {
+                await MainActor.run {
+                    completion(.failure(error))
+                }
             }
+        }
     }
 
     /// Executes the specified Network Request. Upon completion, the payload or error will be emitted to the publisher.
     /// Only one value will be emitted and the request cannot be retried.
     ///
-    /// - Important:
-    ///     - User agent and authenticator from the initializer will be injected through the `validate` call..
-    ///
     /// - Parameter request: Request that should be performed.
     /// - Returns: A publisher that emits the result of the given request.
     public func responseDataPublisher(for request: Request) -> AnyPublisher<Swift.Result<Data, Error>, Never> {
-        return Future() { [weak self] promise in
-            guard let self = self else { return }
-            self.sessionManager.request(request).validate().responseData { response in
-                let result = response.result.toSwiftResult()
-                promise(Swift.Result.success(result))
+        return Future() { promise in
+            Task {
+                do {
+                    let request = try await self.createRequest(wrapping: request)
+                    let data = try await self.sessionManager.request(request)
+                    await MainActor.run {
+                        promise(Result.success(.success(data)))
+                    }
+                } catch {
+                    await MainActor.run {
+                        promise(Result.success(.failure(error)))
+                    }
+                }
             }
         }.eraseToAnyPublisher()
     }
@@ -106,33 +108,73 @@ public final class WordPressOrgNetwork: Network {
     public func uploadMultipartFormData(multipartFormData: @escaping (MultipartFormDataType) -> Void,
                                         to request: Request,
                                         completion: @escaping (Data?, Error?) -> Void) {
-        backgroundSessionManager.upload(multipartFormData: multipartFormData, with: request) { (encodingResult) in
-            switch encodingResult {
-            case .success(let upload, _, _):
-                upload.responseData { response in
-                    completion(response.value, response.error)
+        Task {
+            let request = try await createRequest(wrapping: request)
+            do {
+                let data = try await sessionManager.upload(multipartFormData: multipartFormData, with: request)
+                await MainActor.run {
+                    completion(data, nil)
                 }
-            case .failure(let error):
-                completion(nil, error)
+            } catch {
+                await MainActor.run {
+                    completion(nil, error)
+                }
             }
         }
     }
 }
 
 private extension WordPressOrgNetwork {
-    /// Creates a session manager with injected user agent and authenticator for handling cookie-nonce/token
-    ///
-    func makeSessionManager(configuration sessionConfiguration: URLSessionConfiguration) -> Alamofire.SessionManager {
-        var additionalHeaders: [String: AnyObject] = [:]
-        if let userAgent = self.userAgent {
-            additionalHeaders["User-Agent"] = userAgent as AnyObject?
+    func createRequest(wrapping request: Request) async throws -> Request {
+        try await handleLoginAndNonceRetrieval()
+        guard let nonce else {
+            throw NetworkError.nonceAuthenticationFailed
         }
+        var adaptedRequest = try request.asURLRequest()
+        adaptedRequest.addValue(nonce, forHTTPHeaderField: "X-WP-Nonce")
+        return adaptedRequest
+    }
 
-        sessionConfiguration.httpAdditionalHeaders = additionalHeaders
+    func handleLoginAndNonceRetrieval() async throws {
+        guard nonce == nil else {
+            return // No need to log in again
+        }
+        DDLogInfo("Starting Cookie+Nonce login sequence for \(configuration.loginURL)")
+        guard let nonceRetrievalURL = URL(string: "admin-ajax.php?action=rest-nonce", relativeTo: configuration.adminURL) else {
+            throw NetworkError.nonceAuthenticationFailed
+        }
+        let request = authenticatedRequest(redirectURL: nonceRetrievalURL)
+        let (data, _) = try await session.data(for: request)
 
-        let sessionManager = Alamofire.SessionManager(configuration: sessionConfiguration)
-        sessionManager.adapter = authenticator
-        sessionManager.retrier = authenticator
-        return sessionManager
+        DDLogInfo("Posted Login to \(configuration.loginURL), redirected to \(nonceRetrievalURL)")
+        guard let page = String(data: data, encoding: .utf8),
+              let nonce = readNonceFromAjaxAction(html: page) else {
+            throw NetworkError.nonceAuthenticationFailed
+        }
+        self.nonce = nonce
+    }
+
+    func readNonceFromAjaxAction(html: String) -> String? {
+        guard !html.isEmpty else {
+            return nil
+        }
+        return html
+    }
+
+    func authenticatedRequest(redirectURL: URL) -> URLRequest {
+        var request = URLRequest(url: configuration.loginURL)
+
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+
+        var parameters = [URLQueryItem]()
+        parameters.append(URLQueryItem(name: "log", value: configuration.username))
+        parameters.append(URLQueryItem(name: "pwd", value: configuration.password))
+        parameters.append(URLQueryItem(name: "rememberme", value: "true"))
+        parameters.append(URLQueryItem(name: "redirect_to", value: redirectURL.absoluteString))
+        var components = URLComponents()
+        components.queryItems = parameters
+        request.httpBody = components.percentEncodedQuery?.data(using: .utf8)
+        return request
     }
 }

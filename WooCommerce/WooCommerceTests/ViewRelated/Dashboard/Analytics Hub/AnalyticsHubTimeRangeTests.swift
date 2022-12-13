3bbadc6f8b984552e8f8465b133c1fdd08bfba86
ThomazFB <thomazfcortez@gmail.com>
2022-12-13 00:08:24 -0300

    Enhance AnalyticsHubTimeRangeTests

--- WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubTimeRangeTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubTimeRangeTests.swift
@@ -53,18 +53,16 @@ final class AnalyticsHubTimeRangeTests: XCTestCase {
 
     func test_when_time_range_is_in_different_years_then_fully_describe_both_dates() {
         // Given
-        let startDate = startDate(from: "2021-04-01")!
-        let endDate = endDate(from: "2022-07-15")!
+        let startDate = startDate(from: "2021-04-15")!
+        let endDate = endDate(from: "2022-04-15")!
         let timeRange = AnalyticsHubTimeRange(start: startDate, end: endDate)
 
         // When
         let description = timeRange.formatToString(simplified: false, timezone: testTimezone, calendar: testCalendar)
 
         //Then
-        XCTAssertEqual(description, "Apr 1, 2021 - Jul 15, 2022")
+        XCTAssertEqual(description, "Apr 15, 2021 - Apr 15, 2022")
     }
-    
-    //TODO different years same month
 
     private func startDate(from date: String) -> Date? {
         let dateFormatter = DateFormatter()

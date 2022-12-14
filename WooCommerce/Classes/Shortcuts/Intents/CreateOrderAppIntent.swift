84b9191fabe01b5ac0a0ba0a9ce68c4daef64dac
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2022-12-14 15:09:42 +0100

    Add intent to create an order without products.

--- WooCommerce/Classes/Shortcuts/Intents/CreateOrderAppIntent.swift
+++ WooCommerce/Classes/Shortcuts/Intents/CreateOrderAppIntent.swift
@@ -3,16 +3,33 @@ import Yosemite
 import Foundation
 
 @available(iOS 16, *)
-struct CreateOrderAppIntent: AppIntent {
-    static var title: LocalizedStringResource = "Create Order"
+struct CreateOrderWithProductAppIntent: AppIntent {
+    static var title: LocalizedStringResource = "Create Order with a Product"
     static var openAppWhenRun = true
 
     @IntentParameter(title: "Product", description: "The product to add to the order", requestValueDialog: IntentDialog("Which product would you like to add to the order?"))
     var product: ShortcutProductAppEntity
 
+    static var parameterSummary: some ParameterSummary {
+        Summary("Create order with \(\.$product)")
+    }
+
     @MainActor // <-- include if the code needs to be run on the main thread
     func perform() async throws -> some IntentResult {
         MainTabBarController.presentOrderCreationFlow(with: product.originalProduct)
+
+        return .result()
+    }
+}
+
+@available(iOS 16, *)
+struct CreateEmptyOrderAppIntent: AppIntent {
+    static var title: LocalizedStringResource = "Create Order"
+    static var openAppWhenRun = true
+
+    @MainActor // <-- include if the code needs to be run on the main thread
+    func perform() async throws -> some IntentResult {
+        MainTabBarController.presentOrderCreationFlow()
 
         return .result()
     }

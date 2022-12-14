c330d5fc052a804111f7d7d58ca3299f35a1fc57
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-12-14 08:17:49 +0530

    Introduce `DefaultApplicationPasswordUseCase` to perform application password actions.

--- Networking/Networking/ApplicationPassword/ApplicationPasswordUseCase.swift
+++ Networking/Networking/ApplicationPassword/ApplicationPasswordUseCase.swift
@@ -1,5 +1,6 @@
 import Foundation
 import WordPressShared
+import KeychainAccess
 
 struct ApplicationPassword {
     /// WordPress org username that the application password belongs to
@@ -27,4 +28,45 @@ protocol ApplicationPasswordUseCase {
     ///  Deletes locally and also sends an API request to delete it from the site
     ///
     func deletePassword() async throws
+}
+
+final class DefaultApplicationPasswordUseCase {
+    /// WordPress.com Credentials.
+    ///
+    private let credentials: Credentials
+
+    /// SiteID needed when using WPCOM credentials
+    ///
+    private let siteID: Int64
+
+    /// To generate and delete application password
+    ///
+    private let network: Network
+
+    /// Stores the application password
+    ///
+    private let keychain: Keychain
+
+    /// Used to name the password in wpadmin.
+    ///
+    private var applicationPasswordName: String {
+        get async {
+            await UIDevice.current.model
+        }
+    }
+
+    init(siteID: Int64,
+         networkcredentials: Credentials,
+         network: Network? = nil,
+         keychain: Keychain = Keychain(service: "com.automattic.woocommerce.applicationpassword")) {
+        self.siteID = siteID
+        self.credentials = networkcredentials
+        self.keychain = keychain
+
+        if let network {
+            self.network = network
+        } else {
+            self.network = ApplicationPasswordNetwork(credentials: networkcredentials)
+        }
+    }
 }

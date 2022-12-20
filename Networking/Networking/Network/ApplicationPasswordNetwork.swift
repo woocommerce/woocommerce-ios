8741de07b257a088d23b71f5c45fb1b8b8a1abb5
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-12-20 16:55:23 +0530

    Make `ApplicationPasswordNetwork` work with WPOrg credentials.

--- Networking/Networking/Network/ApplicationPasswordNetwork.swift
+++ Networking/Networking/Network/ApplicationPasswordNetwork.swift
@@ -1,23 +1,30 @@
+import Alamofire
 import Combine
 import Foundation
-import Alamofire
-
+import WordPressKit
 /// This Network is specific for generating and deleting application passwords
 ///
 ///  - We cannot use the AlamofireNetwork as we will be initiating the application password generation from there. (By listening to other API calls)
-///  - `ApplicationPasswordNetwork` currently takes in WPCOM credentials. In future it will also work with .org site credentials as well.
+///  - Uses WPOrg credentials to fetch the application password
 ///
 public class ApplicationPasswordNetwork: Network {
-    /// WordPress.com Credentials.
-    ///
-    private let credentials: Credentials
+    private let authenticator: Authenticator?
+    private let userAgent: String?
+
+    private lazy var sessionManager: Alamofire.SessionManager = {
+        let sessionConfiguration = URLSessionConfiguration.default
+        let sessionManager = makeSessionManager(configuration: sessionConfiguration)
+        return sessionManager
+    }()
 
     public var session: URLSession { SessionManager.default.session }
 
     /// Public Initializer
     ///
-    public required init(credentials: Credentials) {
-        self.credentials = credentials
+    @MainActor
+    public init(authenticator: Authenticator? = nil, userAgent: String = UserAgent.defaultUserAgent) {
+        self.authenticator = authenticator
+        self.userAgent = userAgent
     }
 
     /// Executes the specified Network Request. Upon completion, the payload will be sent back to the caller as a Data instance.
@@ -30,11 +37,11 @@ public class ApplicationPasswordNetwork: Network {
     ///     - completion: Closure to be executed upon completion.
     ///
     public func responseData(for request: URLRequestConvertible, completion: @escaping (Swift.Result<Data, Error>) -> Void) {
-        let request = AuthenticatedRequest(credentials: credentials, request: request)
-
-        Alamofire.request(request).responseData { response in
-            completion(response.result.toSwiftResult())
-        }
+        sessionManager.request(request)
+            .validate()
+            .responseData { response in
+                completion(response.result.toSwiftResult())
+            }
     }
 
     @available(*, deprecated, message: "Not implemented. Use the `Result` based method instead.")
@@ -49,4 +56,22 @@ public class ApplicationPasswordNetwork: Network {
     public func uploadMultipartFormData(multipartFormData: @escaping (MultipartFormData) -> Void,
                                         to request: URLRequestConvertible,
                                         completion: @escaping (Data?, Error?) -> Void) { }
+}
+
+private extension ApplicationPasswordNetwork {
+    /// Creates a session manager with injected user agent and authenticator for handling cookie-nonce/token
+    ///
+    func makeSessionManager(configuration sessionConfiguration: URLSessionConfiguration) -> Alamofire.SessionManager {
+        var additionalHeaders: [String: AnyObject] = [:]
+        if let userAgent = self.userAgent {
+            additionalHeaders["User-Agent"] = userAgent as AnyObject?
+        }
+
+        sessionConfiguration.httpAdditionalHeaders = additionalHeaders
+
+        let sessionManager = Alamofire.SessionManager(configuration: sessionConfiguration)
+        sessionManager.adapter = authenticator
+        sessionManager.retrier = authenticator
+        return sessionManager
+    }
 }

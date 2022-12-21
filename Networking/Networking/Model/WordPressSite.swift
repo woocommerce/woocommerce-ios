2edfc10dbce72dcbf5cfdaf6eb695656d3ffff63
Huong Do <hi.huongdo@gmail.com>
2022-12-21 17:57:45 +0700

    Add new model for WordPressSite

---
+++ Networking/Networking/Model/WordPressSite.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+/// Represents basic information for a WordPress site.
+///
+struct WordPressSite: Decodable, Equatable {
+    
+    /// Site's Name.
+    ///
+    public let name: String
+
+    /// Site's Description.
+    ///
+    public let description: String
+
+    /// Site's URL.
+    ///
+    public let url: String
+
+    /// Time zone identifier of the site (TZ database name).
+    ///
+    public let timezone: String
+
+    /// Return the website UTC time offset, showing the difference in hours and minutes from UTC, from the westernmost (−12:00) to the easternmost (+14:00).
+    ///
+    public let gmtOffset: Double
+
+    public init(name: String, description: String, url: String, timezone: String, gmtOffset: Double) {
+        self.name = name
+        self.description = description
+        self.url = url
+        self.timezone = timezone
+        self.gmtOffset = gmtOffset
+    }
+}
+
+/// Defines all of the WordPressSite CodingKeys
+///
+private extension WordPressSite {
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case description
+        case url
+        case timezone = "timezone_string"
+        case gmtOffset = "gmt_offset"
+    }
+}

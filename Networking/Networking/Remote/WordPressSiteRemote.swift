8aef5722e69e2c3f4483caa1e1cf99056f774ded
Huong Do <hi.huongdo@gmail.com>
2022-12-22 16:55:32 +0700

    Move root path to an enum

--- Networking/Networking/Remote/WordPressSiteRemote.swift
+++ Networking/Networking/Remote/WordPressSiteRemote.swift
@@ -4,12 +4,18 @@ import Foundation
 ///
 public final class WordPressSiteRemote: Remote {
     public func fetchSiteInfo(for siteURL: String) async throws -> WordPressSite {
-        let path = "/wp-json"
+        let path = Path.root
         guard let url = URL(string: siteURL + path) else {
             throw NetworkError.invalidURL
         }
         let request = try URLRequest(url: url, method: .get)
         let mapper = WordPressSiteMapper()
         return try await enqueue(request, mapper: mapper)
+    }
+}
+
+private extension WordPressSiteRemote {
+    enum Path {
+        static let root = "/wp-json"
     }
 }

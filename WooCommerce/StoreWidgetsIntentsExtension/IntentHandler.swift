7829c416d51ef355194af075e4286a3f034b16da
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2022-12-23 20:50:24 +0300

    Add single-store fallback

--- WooCommerce/StoreWidgetsIntentsExtension/IntentHandler.swift
+++ WooCommerce/StoreWidgetsIntentsExtension/IntentHandler.swift
@@ -5,7 +5,13 @@ class IntentHandler: INExtension, StoreWidgetsConfigIntentHandling {
     func provideStoreOptionsCollection(for intent: StoreWidgetsConfigIntent, searchTerm: String?) async throws -> INObjectCollection<IntentStore> {
         guard let sitesData = UserDefaults.group?[.sharedSitesData] as? Data,
               let sitesArray = try? JSONDecoder().decode([SharedSiteData].self, from: sitesData) else {
-            return INObjectCollection(items: [])
+            // Fallback to single-store implementation
+            if let storeID = UserDefaults.group?[.defaultStoreID] as? Int64,
+               let storeName = UserDefaults.group?[.defaultStoreName] as? String {
+                return INObjectCollection(items: [ IntentStore(identifier: String(storeID), display: storeName) ])
+            } else {
+                return INObjectCollection(items: [])
+            }
         }
 
         let sites = sitesArray.map { IntentStore(identifier: String($0.siteID), display: $0.siteName) }

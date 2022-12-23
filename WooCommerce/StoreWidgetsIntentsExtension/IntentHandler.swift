97c4634a1f8ac7802dcd326412d6fd32adec6f25
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2022-12-23 20:50:24 +0300

    Load sites data for widget configuration options

--- WooCommerce/StoreWidgetsIntentsExtension/IntentHandler.swift
+++ WooCommerce/StoreWidgetsIntentsExtension/IntentHandler.swift
@@ -3,7 +3,13 @@ import Intents
 class IntentHandler: INExtension, StoreWidgetsConfigIntentHandling {
 
     func provideStoreOptionsCollection(for intent: StoreWidgetsConfigIntent, searchTerm: String?) async throws -> INObjectCollection<IntentStore> {
-        return INObjectCollection(items: [IntentStore(identifier: "123123", display: "Test Store")])
+        guard let sitesData = UserDefaults.group?[.sharedSitesData] as? Data,
+              let sitesArray = try? JSONDecoder().decode([SharedSiteData].self, from: sitesData) else {
+            return INObjectCollection(items: [])
+        }
+
+        let sites = sitesArray.map { IntentStore(identifier: String($0.siteID), display: $0.siteName) }
+        return INObjectCollection(items: sites)
     }
 
     override func handler(for intent: INIntent) -> Any {

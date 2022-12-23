47d06c02aa278aea6ad5caf9bb4ba42da9310dc6
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2022-12-23 20:50:24 +0300

    Add search support in IntentHandler

--- WooCommerce/StoreWidgetsIntentsExtension/IntentHandler.swift
+++ WooCommerce/StoreWidgetsIntentsExtension/IntentHandler.swift
@@ -3,22 +3,32 @@ import Intents
 class IntentHandler: INExtension, StoreWidgetsConfigIntentHandling {
 
     func provideStoreOptionsCollection(for intent: StoreWidgetsConfigIntent, searchTerm: String?) async throws -> INObjectCollection<IntentStore> {
-        guard let sitesData = UserDefaults.group?[.sharedSitesData] as? Data,
-              let sitesArray = try? JSONDecoder().decode([SharedSiteData].self, from: sitesData) else {
-            // Fallback to single-store implementation
-            if let storeID = UserDefaults.group?[.defaultStoreID] as? Int64,
-               let storeName = UserDefaults.group?[.defaultStoreName] as? String {
-                return INObjectCollection(items: [ IntentStore(identifier: String(storeID), display: storeName) ])
-            } else {
-                return INObjectCollection(items: [])
-            }
+        var sitesArray = getAllStores()
+        if let searchTerm {
+            sitesArray = sitesArray.filter { $0.siteName.contains(searchTerm) }
         }
-
         let sites = sitesArray.map { IntentStore(identifier: String($0.siteID), display: $0.siteName) }
         return INObjectCollection(items: sites)
     }
 
     override func handler(for intent: INIntent) -> Any {
         return self
+    }
+}
+
+private extension IntentHandler {
+    func getAllStores() -> [SharedSiteData] {
+        guard let sitesData = UserDefaults.group?[.sharedSitesData] as? Data,
+              let sitesArray = try? JSONDecoder().decode([SharedSiteData].self, from: sitesData) else {
+            // Fallback to single-store implementation
+            if let storeID = UserDefaults.group?[.defaultStoreID] as? Int64,
+               let storeName = UserDefaults.group?[.defaultStoreName] as? String {
+                return [SharedSiteData(siteID: storeID, siteName: storeName)]
+            } else {
+                return []
+            }
+        }
+
+        return sitesArray
     }
 }

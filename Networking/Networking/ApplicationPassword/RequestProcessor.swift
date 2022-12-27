106817235c3744ca6781edbb39793d7ff69f1aa5
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-12-27 13:50:54 +0530

    `RequestProcessor` to authenticate and retry alamofire network requests.

---
+++ Networking/Networking/ApplicationPassword/RequestProcessor.swift
@@ -0,0 +1,96 @@
+import Alamofire
+import Foundation
+
+/// Authenticates and retries requests
+///
+final class RequestProcessor {
+    private var requestsToRetry = [RequestRetryCompletion]()
+
+    private var isAuthenticating = false
+
+    private let requestAuthenticator: RequestAuthenticator
+
+    init(credentials: Credentials?) {
+        requestAuthenticator = RequestAuthenticator(credentials: credentials)
+    }
+}
+
+// MARK: Request Authentication
+//
+extension RequestProcessor: RequestAdapter {
+    func adapt(_ urlRequest: URLRequest) throws -> URLRequest {
+        return try requestAuthenticator.authenticate(urlRequest)
+    }
+}
+
+// MARK: Retrying Request
+//
+extension RequestProcessor: RequestRetrier {
+    func should(_ manager: Alamofire.SessionManager,
+                retry request: Alamofire.Request,
+                with error: Error,
+                completion: @escaping Alamofire.RequestRetryCompletion) {
+        guard
+            request.retryCount == 0, // Only retry once
+            shouldRetry(request), // Retry only REST API requests that use application password
+            shouldRetry(error) // Retry only specific errors
+        else {
+            return completion(false, 0.0)
+        }
+
+        requestsToRetry.append(completion)
+        if !isAuthenticating {
+            generateApplicationPassword()
+        }
+    }
+}
+
+// MARK: Helpers
+//
+private extension RequestProcessor {
+    func generateApplicationPassword() {
+        Task(priority: .medium) {
+            isAuthenticating = true
+
+            do {
+                let _ = try await requestAuthenticator.generateApplicationPassword()
+                isAuthenticating = false
+                completeRequests(true)
+            } catch {
+                isAuthenticating = false
+                completeRequests(false)
+            }
+        }
+    }
+
+    func shouldRetry(_ request: Alamofire.Request) -> Bool {
+        guard case let .wporg(_, _, siteAddress) = requestAuthenticator.credentials,
+              let url = request.request?.url,
+              url.absoluteString.hasPrefix(siteAddress + "/" + RESTRequest.Settings.basePath) else {
+            return false
+        }
+
+        return true
+    }
+
+    func shouldRetry(_ error: Error) -> Bool {
+        // Need to generate application password
+        if .applicationPasswordNotAvailable == error as? RequestAuthenticatorError {
+            return true
+        }
+
+        // Failed authorization
+        if case .responseValidationFailed(reason: .unacceptableStatusCode(code: 401)) = error as? AFError {
+            return true
+        }
+
+        return false
+    }
+
+    func completeRequests(_ shouldRetry: Bool) {
+        requestsToRetry.forEach { (completion) in
+            completion(shouldRetry, 0.0)
+        }
+        requestsToRetry.removeAll()
+    }
+}

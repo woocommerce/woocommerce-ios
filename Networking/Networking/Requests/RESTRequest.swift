84313617437b2d0180160745315476b71e30cacb
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-12-27 14:11:07 +0530

    Revert "Use JSON encoding for post and put requests."
    This reverts commit 6e8efb99053d9de5b7acc69abf8ac4de39b81075.

--- Networking/Networking/Requests/RESTRequest.swift
+++ Networking/Networking/Requests/RESTRequest.swift
@@ -52,12 +52,7 @@ struct RESTRequest: URLRequestConvertible {
         let components = [siteURL, Settings.basePath, wooApiVersion.path, path].map { $0.trimSlashes() }
         let url = try components.joined(separator: "/").asURL()
         let request = try URLRequest(url: url, method: method)
-        switch method {
-        case .post, .put:
-            return try JSONEncoding.default.encode(request, with: parameters)
-        default:
-            return try URLEncoding.default.encode(request, with: parameters)
-        }
+        return try URLEncoding.default.encode(request, with: parameters)
     }
 }
 

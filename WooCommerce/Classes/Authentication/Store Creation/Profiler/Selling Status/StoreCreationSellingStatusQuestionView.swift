e526709f8993b62c78e4bf1259bcc9c46ce820cc
Jaclyn Chen <watertranquil@gmail.com>
2022-12-27 16:47:19 +0800

    When selecting the option of "already selling online", navigate to the selling platform selector.

--- WooCommerce/Classes/Authentication/Store Creation/Profiler/Selling Status/StoreCreationSellingStatusQuestionView.swift
+++ WooCommerce/Classes/Authentication/Store Creation/Profiler/Selling Status/StoreCreationSellingStatusQuestionView.swift
@@ -21,26 +21,46 @@ final class StoreCreationSellingStatusQuestionHostingController: UIHostingContro
 /// Shows the store selling status question in the store creation flow.
 struct StoreCreationSellingStatusQuestionView: View {
     @ObservedObject private var viewModel: StoreCreationSellingStatusQuestionViewModel
+    @ObservedObject private var platformsViewModel: StoreCreationSellingPlatformsQuestionViewModel
 
     init(storeName: String, onContinue: @escaping () -> Void, onSkip: @escaping () -> Void) {
         self.viewModel = StoreCreationSellingStatusQuestionViewModel(storeName: storeName, onContinue: onContinue, onSkip: onSkip)
+        self.platformsViewModel = StoreCreationSellingPlatformsQuestionViewModel(storeName: storeName, onContinue: onContinue, onSkip: onSkip)
     }
 
     var body: some View {
-        OptionalStoreCreationProfilerQuestionView(viewModel: viewModel) {
-            VStack(spacing: 16) {
-                ForEach(viewModel.sellingStatuses, id: \.self) { sellingStatus in
-                    Button(action: {
-                        viewModel.selectStatus(sellingStatus)
-                    }, label: {
-                        HStack {
-                            Text(sellingStatus.description)
-                            Spacer()
-                        }
-                    })
-                    .buttonStyle(SelectableSecondaryButtonStyle(isSelected: viewModel.selectedStatus == sellingStatus))
+        if viewModel.isAlreadySellingOnline {
+            return AnyView(OptionalStoreCreationProfilerQuestionView(viewModel: platformsViewModel) {
+                VStack(spacing: 16) {
+                    ForEach(platformsViewModel.platforms, id: \.self) { platform in
+                        Button(action: {
+                            platformsViewModel.selectPlatform(platform)
+                        }, label: {
+                            HStack {
+                                Text(platform.description)
+                                Spacer()
+                            }
+                        })
+                        .buttonStyle(SelectableSecondaryButtonStyle(isSelected: platformsViewModel.selectedPlatforms.contains(platform)))
+                    }
                 }
-            }
+            })
+        } else {
+            return AnyView(OptionalStoreCreationProfilerQuestionView(viewModel: viewModel) {
+                VStack(spacing: 16) {
+                    ForEach(viewModel.sellingStatuses, id: \.self) { sellingStatus in
+                        Button(action: {
+                            viewModel.selectStatus(sellingStatus)
+                        }, label: {
+                            HStack {
+                                Text(sellingStatus.description)
+                                Spacer()
+                            }
+                        })
+                        .buttonStyle(SelectableSecondaryButtonStyle(isSelected: viewModel.selectedStatus == sellingStatus))
+                    }
+                }
+            })
         }
     }
 }

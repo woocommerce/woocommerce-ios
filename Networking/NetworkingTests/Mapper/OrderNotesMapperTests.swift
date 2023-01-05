697130cc828b5de4b19a5f858709bb3a017fc58d
Huong Do <hi.huongdo@gmail.com>
2023-01-05 17:12:37 +0800

    Update tests for OrderNotesMapper

--- Networking/NetworkingTests/Mapper/OrderNotesMapperTests.swift
+++ Networking/NetworkingTests/Mapper/OrderNotesMapperTests.swift
@@ -31,6 +31,31 @@ class OrderNotesMapperTests: XCTestCase {
         XCTAssertEqual(secondNote.author, "someuser")
     }
 
+    /// Verifies that all of the OrderNote Fields are parsed correctly.
+    ///
+    func test_Note_fields_are_properly_parsed_when_response_has_no_data_envelope() {
+        let notes = mapLoadAllOrderNotesResponseWithoutDataEnvelope()
+        XCTAssertEqual(notes.count, 19)
+
+        let firstNote = notes[0]
+        let firstNoteCreated = DateFormatter.Defaults.dateTimeFormatter.date(from: "2018-06-23T16:05:55")
+
+        XCTAssertEqual(firstNote.noteID, 2260)
+        XCTAssertEqual(firstNote.dateCreated, firstNoteCreated)
+        XCTAssertEqual(firstNote.note, "This order is going to be a problem.")
+        XCTAssertEqual(firstNote.isCustomerNote, false)
+        XCTAssertEqual(firstNote.author, "someadmin")
+
+        let secondNote = notes[1]
+        let secondNoteCreated = DateFormatter.Defaults.dateTimeFormatter.date(from: "2018-06-23T17:06:55")
+
+        XCTAssertEqual(secondNote.noteID, 2261)
+        XCTAssertEqual(secondNote.dateCreated, secondNoteCreated)
+        XCTAssertEqual(secondNote.note, "I love your products!")
+        XCTAssertEqual(secondNote.isCustomerNote, true)
+        XCTAssertEqual(secondNote.author, "someuser")
+    }
+
     /// Verifies that an Note in a broken state does [gets default values] | [gets skipped while parsing]
     ///
     func test_Note_has_default_dateCreated_when_null_date_received() {
@@ -62,10 +87,16 @@ private extension OrderNotesMapperTests {
         return try! OrderNotesMapper().map(response: response)
     }
 
-    /// Returns the OrderNotesMapper output upon receiving `orders-load-all`
+    /// Returns the OrderNotesMapper output upon receiving `order-notes`
     ///
     func mapLoadAllOrderNotesResponse() -> [OrderNote] {
         return mapNotes(from: "order-notes")
+    }
+
+    /// Returns the OrderNotesMapper output upon receiving `order-notes-without-data`
+    ///
+    func mapLoadAllOrderNotesResponseWithoutDataEnvelope() -> [OrderNote] {
+        return mapNotes(from: "order-notes-without-data")
     }
 
     /// Returns the OrderNotesMapper output upon receiving `broken-order`

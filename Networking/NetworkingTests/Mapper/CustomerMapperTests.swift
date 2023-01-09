4a40e4d4351824f0f4189483763ab80ed8c6bc76
Huong Do <hi.huongdo@gmail.com>
2023-01-09 11:45:33 +0800

    Update tests for CustomerMapper

--- Networking/NetworkingTests/Mapper/CustomerMapperTests.swift
+++ Networking/NetworkingTests/Mapper/CustomerMapperTests.swift
@@ -12,6 +12,7 @@ class CustomerMapperTests: XCTestCase {
     /// Local file that holds Customer data representing the API endpoint
     ///
     private let filename: String = "customer"
+    private let fileNameWithoutDataEnvelope = "customer-without-data"
 
     /// Verifies that the Customer object can be mapped fron the Encoded data
     ///
@@ -36,6 +37,38 @@ class CustomerMapperTests: XCTestCase {
     func test_Customer_response_values_are_correctly_parsed() throws {
         // Given
         guard let customer = try mapCustomer(from: filename) else {
+            XCTFail()
+            return
+        }
+
+        // Then
+        XCTAssertNotNil(customer)
+        XCTAssertEqual(customer.customerID, 25)
+        XCTAssertEqual(customer.email, "john.doe@example.com")
+        XCTAssertEqual(customer.firstName, "John")
+        XCTAssertEqual(customer.lastName, "Doe")
+
+        let dummyAddresses = [customer.shipping, customer.billing].compactMap({ $0 })
+        XCTAssertEqual(dummyAddresses.count, 2)
+
+        for address in dummyAddresses {
+            XCTAssertEqual(address.firstName, "John")
+            XCTAssertEqual(address.lastName, "Doe")
+            XCTAssertEqual(address.company, "")
+            XCTAssertEqual(address.address1, "969 Market")
+            XCTAssertEqual(address.address2, "")
+            XCTAssertEqual(address.city, "San Francisco")
+            XCTAssertEqual(address.state, "CA")
+            XCTAssertEqual(address.postcode, "94103")
+            XCTAssertEqual(address.country, "US")
+        }
+    }
+
+    /// Verifies that all of the Customer response values are parsed correctly
+    ///
+    func test_Customer_response_values_are_correctly_parsed_when_response_has_no_data_envelope() throws {
+        // Given
+        guard let customer = try mapCustomer(from: fileNameWithoutDataEnvelope) else {
             XCTFail()
             return
         }

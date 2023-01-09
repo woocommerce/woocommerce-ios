218ba06237ee33f4d365a1ef20182b50e4e308ce
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-01-09 19:51:46 +0800

    Add resultsController to fetch all wcpay Orders

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsMenuViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsMenuViewModel.swift
@@ -41,7 +41,8 @@ final class InPersonPaymentsMenuViewModel {
         return codGateway.enabled
     }
 
-    private lazy var resultsController = createRecentIPPOrdersResultsController()
+    private lazy var resultsController = createIPPOrdersResultsController()
+    private lazy var recentIPPOrdersResultsController = createRecentIPPOrdersResultsController()
 
     // MARK: - Output properties
     @Published var showWebView: AuthenticatedWebViewModel? = nil
@@ -87,16 +88,18 @@ final class InPersonPaymentsMenuViewModel {
     ///
     func displayIPPFeedbackBannerIfEligible() {
         if isCODEnabled {
-            let resultsCount = resultsController.fetchedObjects.count
+            let hasResults = resultsController.fetchedObjects.isEmpty ? false : true
+            let resultsCount = recentIPPOrdersResultsController.fetchedObjects.count
             let numberOfTransactions = 10
 
+            // Debug: Remove before merging
+            print("hasResults? \(hasResults)")
             print("IPP transactions within 30 days: \(resultsCount)")
-            print(resultsController.fetchedObjects.map {
+            print(recentIPPOrdersResultsController.fetchedObjects.map {
                 ("OrderID: \($0.orderID) - PaymentMethodID: \($0.paymentMethodID) - DatePaid: \(String(describing: $0.datePaid))")
             })
 
-            if resultsCount == 0 {
-                // TODO: This option should use a different results controller or predicate, as we're looking for 0 orders historically, not within 30 days.
+            if !hasResults {
                 print("0 transactions. Banner 1 shown")
             } else if resultsCount < numberOfTransactions {
                 print("< 10 transactions within 30 days. Banner 2 shown")
@@ -109,6 +112,8 @@ final class InPersonPaymentsMenuViewModel {
     private func fetchIPPTransactions() {
         do {
             try resultsController.performFetch()
+            // TODO: Perform the 2nd fetch only if the first one was successful?
+            try recentIPPOrdersResultsController.performFetch()
         } catch {
             DDLogError("Error fetching IPP transactions: \(error)")
         }
@@ -116,7 +121,18 @@ final class InPersonPaymentsMenuViewModel {
 }
 
 private extension InPersonPaymentsMenuViewModel {
-    /// Results controller that fetches IPP transactions within the last 30 days
+    /// Results controller that fetches any IPP transactions via WooCommerce Payments
+    ///
+    func createIPPOrdersResultsController() -> ResultsController<StorageOrder> {
+        let paymentGateway = Constants.wcpay
+        let predicate = NSPredicate(
+            format: "siteID == %lld AND paymentMethodID == %@",
+            argumentArray: [siteID ?? 0, paymentGateway]
+            )
+        return ResultsController<StorageOrder>(storageManager: storage, matching: predicate, sortedBy: [])
+    }
+
+    /// Results controller that fetches IPP transactions  via WooCommerce Payments, within the last 30 days
     ///
     func createRecentIPPOrdersResultsController() -> ResultsController<StorageOrder> {
         let today = Date()

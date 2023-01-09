22efff17d773c2249d379766084f0d2b078da4f7
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-01-09 17:55:18 +0530

    Update `MockMediaRemote` with new REST API mock methods.

--- Yosemite/YosemiteTests/Mocks/Networking/Remote/MockMediaRemote.swift
+++ Yosemite/YosemiteTests/Mocks/Networking/Remote/MockMediaRemote.swift
@@ -15,17 +15,26 @@ final class MockMediaRemote {
     /// The results to return based on the given site ID in `loadMediaLibraryFromWordPressSite`
     private var loadMediaLibraryFromWordPressSiteResultsBySiteID = [Int64: Result<[WordPressMedia], Error>]()
 
+    /// The results to return based on the given site ID in `loadMediaLibraryUsingRestApi`
+    private var loadMediaLibraryUsingRestApiResultsBySiteURL = [String: Result<[WordPressMedia], Error>]()
+
     /// The results to return based on the given site ID in `uploadMedia`
     private var uploadMediaResultsBySiteID = [Int64: Result<[Media], Error>]()
 
     /// The results to return based on the given site ID in `uploadMediaToWordPressSite`
     private var uploadMediaToWordPressSiteResultsBySiteID = [Int64: Result<WordPressMedia, Error>]()
 
+    /// The results to return based on the given site ID in `uploadMediaUsingRestApi`
+    private var uploadMediaUsingRestApiResultsBySiteURL = [String: Result<WordPressMedia, Error>]()
+
     /// The results to return based on the given site ID in `updateProductID`
     private var updateProductIDResultsBySiteID = [Int64: Result<Media, Error>]()
 
     /// The results to return based on the given site ID in `updateProductIDToWordPressSite`
     private var updateProductIDToWordPressSiteResultsBySiteID = [Int64: Result<WordPressMedia, Error>]()
+
+    /// The results to return based on the given site ID in `updateProductIDUsingRestApi`
+    private var updateProductIDUsingRestApiResultsBySiteURL = [String: Result<WordPressMedia, Error>]()
 
     /// Returns the value as a publisher when `loadMediaLibrary` is called.
     func whenLoadingMediaLibrary(siteID: Int64, thenReturn result: Result<[Media], Error>) {
@@ -35,6 +44,11 @@ final class MockMediaRemote {
     /// Returns the value as a publisher when `loadMediaLibraryFromWordPressSite` is called.
     func whenLoadingMediaLibraryFromWordPressSite(siteID: Int64, thenReturn result: Result<[WordPressMedia], Error>) {
         loadMediaLibraryFromWordPressSiteResultsBySiteID[siteID] = result
+    }
+
+    /// Returns the value as a publisher when `loadMediaLibraryFromWordPressSite` is called.
+    func whenLoadingMediaLibraryUsingRestApi(siteURL: String, thenReturn result: Result<[WordPressMedia], Error>) {
+        loadMediaLibraryUsingRestApiResultsBySiteURL[siteURL] = result
     }
 
     /// Returns the value as a publisher when `uploadMedia` is called.
@@ -47,6 +61,11 @@ final class MockMediaRemote {
         uploadMediaToWordPressSiteResultsBySiteID[siteID] = result
     }
 
+    /// Returns the value as a publisher when `uploadMediaUsingRestApi` is called.
+    func whenUploadingMediaUsingRestApi(siteURL: String, thenReturn result: Result<WordPressMedia, Error>) {
+        uploadMediaUsingRestApiResultsBySiteURL[siteURL] = result
+    }
+
     /// Returns the value as a publisher when `updateProductID` is called.
     func whenUpdatingProductID(siteID: Int64, thenReturn result: Result<Media, Error>) {
         updateProductIDResultsBySiteID[siteID] = result
@@ -56,16 +75,24 @@ final class MockMediaRemote {
     func whenUpdatingProductIDToWordPressSite(siteID: Int64, thenReturn result: Result<WordPressMedia, Error>) {
         updateProductIDToWordPressSiteResultsBySiteID[siteID] = result
     }
+
+    /// Returns the value as a publisher when `updateProductIDUsingRestApi` is called.
+    func whenUpdatingProductIDUsingRestApi(siteURL: String, thenReturn result: Result<WordPressMedia, Error>) {
+        updateProductIDUsingRestApiResultsBySiteURL[siteURL] = result
+    }
 }
 
 extension MockMediaRemote {
     enum Invocation: Equatable {
         case loadMediaLibrary(siteID: Int64)
         case loadMediaLibraryFromWordPressSite(siteID: Int64)
+        case loadMediaLibraryUsingRestApi(siteURL: String)
         case uploadMedia(siteID: Int64)
         case uploadMediaToWordPressSite(siteID: Int64)
+        case uploadMediaUsingRestApi(siteURL: String)
         case updateProductID(siteID: Int64)
         case updateProductIDToWordPressSite(siteID: Int64)
+        case updateProductIDUsingRestApi(siteURL: String)
     }
 }
 
@@ -85,6 +112,18 @@ extension MockMediaRemote: MediaRemoteProtocol {
         invocations.append(.loadMediaLibraryFromWordPressSite(siteID: siteID))
         guard let result = loadMediaLibraryFromWordPressSiteResultsBySiteID[siteID] else {
             XCTFail("\(String(describing: self)) Could not find result for site ID: \(siteID)")
+            return
+        }
+        completion(result)
+    }
+
+    func loadMediaLibraryUsingRestApi(siteURL: String,
+                                      pageNumber: Int,
+                                      pageSize: Int,
+                                      completion: @escaping (Result<[Networking.WordPressMedia], Error>) -> Void) {
+        invocations.append(.loadMediaLibraryUsingRestApi(siteURL: siteURL))
+        guard let result = loadMediaLibraryUsingRestApiResultsBySiteURL[siteURL] else {
+            XCTFail("\(String(describing: self)) Could not find result for site URL: \(siteURL)")
             return
         }
         completion(result)
@@ -115,6 +154,18 @@ extension MockMediaRemote: MediaRemoteProtocol {
         completion(result)
     }
 
+    func uploadMediaUsingRestApi(siteURL: String,
+                                 productID: Int64,
+                                 mediaItems: [Networking.UploadableMedia],
+                                 completion: @escaping (Result<Networking.WordPressMedia, Error>) -> Void) {
+        invocations.append(.uploadMediaUsingRestApi(siteURL: siteURL))
+        guard let result = uploadMediaUsingRestApiResultsBySiteURL[siteURL] else {
+            XCTFail("\(String(describing: self)) Could not find result for site URL: \(siteURL)")
+            return
+        }
+        completion(result)
+    }
+
     func updateProductID(siteID: Int64,
                          productID: Int64,
                          mediaID: Int64,
@@ -134,6 +185,18 @@ extension MockMediaRemote: MediaRemoteProtocol {
         invocations.append(.updateProductIDToWordPressSite(siteID: siteID))
         guard let result = updateProductIDToWordPressSiteResultsBySiteID[siteID] else {
             XCTFail("\(String(describing: self)) Could not find result for site ID: \(siteID)")
+            return
+        }
+        completion(result)
+    }
+
+    func updateProductIDUsingRestApi(siteURL: String,
+                                     productID: Int64,
+                                     mediaID: Int64,
+                                     completion: @escaping (Result<Networking.WordPressMedia, Error>) -> Void) {
+        invocations.append(.updateProductIDUsingRestApi(siteURL: siteURL))
+        guard let result = updateProductIDUsingRestApiResultsBySiteURL[siteURL] else {
+            XCTFail("\(String(describing: self)) Could not find result for site URL: \(siteURL)")
             return
         }
         completion(result)

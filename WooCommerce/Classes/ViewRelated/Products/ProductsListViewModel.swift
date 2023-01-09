e90c6ed17e76e84865c3e25db704ba10a43bf358
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2023-01-09 21:30:21 +0300

    Add ProductsListViewModel

---
+++ WooCommerce/Classes/ViewRelated/Products/ProductsListViewModel.swift
@@ -0,0 +1,31 @@
+import Foundation
+import Yosemite
+
+/// View model for `ProductsViewController`. Only stores logic related to Bulk Editing.
+///
+class ProductListViewModel {
+    private var selectedProducts: Set<Product> = .init()
+
+    var selectedProductsCount: Int {
+        selectedProducts.count
+    }
+
+    func productIsSelected(_ productToCheck: Product) -> Bool {
+        return selectedProducts.contains(productToCheck)
+    }
+
+    func selectProduct(_ selectedProduct: Product) {
+        selectedProducts.insert(selectedProduct)
+//        if selectedProducts.contains(selectedProduct) {
+//        } else {
+//        }
+    }
+
+    func deselectProduct(_ selectedProduct: Product) {
+        selectedProducts.remove(selectedProduct)
+    }
+
+    func deselectAll() {
+        selectedProducts.removeAll()
+    }
+}

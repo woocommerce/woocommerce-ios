554d2faaa579614be6107935e3c63200720b9df7
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-01-09 17:40:38 +0530

    Unit tests for REST API + application password methods in `MediaRemote`

--- Networking/NetworkingTests/Remote/MediaRemoteTests.swift
+++ Networking/NetworkingTests/Remote/MediaRemoteTests.swift
@@ -10,6 +10,10 @@ final class MediaRemoteTests: XCTestCase {
     ///
     private let sampleSiteID: Int64 = 1234
 
+    /// Dummy Site URL
+    ///
+    private let sampleSiteURL = "http://test.com"
+
     /// Dummy Product ID
     ///
     private let sampleProductID: Int64 = 586
@@ -117,6 +121,60 @@ final class MediaRemoteTests: XCTestCase {
         XCTAssertTrue(result.isFailure)
     }
 
+    // MARK: - Load Media From Media Library `loadMediaLibraryUsingRestApi`
+
+    /// Verifies that `loadMediaLibraryUsingRestApi` properly parses the `media-library-from-wordpress-site` sample response.
+    ///
+    func test_loadMediaLibraryUsingRestApi_properly_returns_parsed_media_list() throws {
+        // Given
+        let remote = MediaRemote(network: network)
+        network.simulateResponse(requestUrlSuffix: "media", filename: "media-library-from-wordpress-site")
+
+        // When
+        let result = waitFor { promise in
+            remote.loadMediaLibraryUsingRestApi(siteURL: self.sampleSiteURL) { result in
+                promise(result)
+            }
+        }
+
+        // Then
+        let mediaItems = try XCTUnwrap(result.get())
+        XCTAssertEqual(mediaItems.count, 2)
+        let uploadedMedia = try XCTUnwrap(mediaItems.first)
+        XCTAssertEqual(uploadedMedia.mediaID, 22)
+        XCTAssertEqual(uploadedMedia.date, Date(timeIntervalSince1970: 1637546157))
+        XCTAssertEqual(uploadedMedia.slug, "img_0111-2")
+        XCTAssertEqual(uploadedMedia.mimeType, "image/jpeg")
+        XCTAssertEqual(uploadedMedia.src, "https://ninja.media/wp-content/uploads/2021/11/img_0111-2-scaled.jpeg")
+        XCTAssertEqual(uploadedMedia.alt, "Floral")
+        XCTAssertEqual(uploadedMedia.details?.width, 2560)
+        XCTAssertEqual(uploadedMedia.details?.height, 1920)
+        XCTAssertEqual(uploadedMedia.details?.fileName, "2021/11/img_0111-2-scaled.jpeg")
+        XCTAssertEqual(uploadedMedia.title, .init(rendered: "img_0111-2"))
+        XCTAssertEqual(uploadedMedia.details?.sizes["thumbnail"],
+                       .init(fileName: "img_0111-2-150x150.jpeg",
+                             src: "https://ninja.media/wp-content/uploads/2021/11/img_0111-2-150x150.jpeg",
+                             width: 150,
+                             height: 150))
+    }
+
+    /// Verifies that `loadMediaLibraryUsingRestApi` properly relays Networking Layer errors.
+    ///
+    func test_loadMediaLibraryUsingRestApi_properly_relays_networking_errors() throws {
+        // Given
+        let remote = MediaRemote(network: network)
+
+        // When
+        let result = waitFor { promise in
+            remote.loadMediaLibraryUsingRestApi(siteURL: self.sampleSiteURL) { result in
+                promise(result)
+            }
+        }
+
+        // Then
+        XCTAssertTrue(result.isFailure)
+    }
+
     // MARK: - uploadMedia
 
     /// Verifies that `uploadMedia` properly parses the `media-upload` sample response.
@@ -215,6 +273,61 @@ final class MediaRemoteTests: XCTestCase {
         XCTAssertTrue(result.isFailure)
     }
 
+    /// Verifies that `uploadMediaUsingRestApi` properly parses the `media-upload-to-wordpress-site` sample response.
+    ///
+    func test_uploadMediaUsingRestApi_properly_returns_parsed_media() throws {
+        // Given
+        let remote = MediaRemote(network: network)
+        let path = "media"
+        network.simulateResponse(requestUrlSuffix: path, filename: "media-upload-to-wordpress-site")
+
+        // When
+        let result = waitFor { promise in
+            remote.uploadMediaUsingRestApi(siteURL: self.sampleSiteURL,
+                                           productID: self.sampleProductID,
+                                           mediaItems: []) { result in
+                promise(result)
+            }
+        }
+
+        // Then
+        let uploadedMedia = try XCTUnwrap(result.get())
+        XCTAssertEqual(uploadedMedia.mediaID, 23)
+        XCTAssertEqual(uploadedMedia.date, Date(timeIntervalSince1970: 1637477423))
+        XCTAssertEqual(uploadedMedia.slug, "img_0005-1")
+        XCTAssertEqual(uploadedMedia.mimeType, "image/jpeg")
+        XCTAssertEqual(uploadedMedia.src, "https://ninja.media/wp-content/uploads/2021/11/img_0005-1-scaled.jpeg")
+        XCTAssertEqual(uploadedMedia.alt, "Floral")
+        XCTAssertEqual(uploadedMedia.details?.width, 2560)
+        XCTAssertEqual(uploadedMedia.details?.height, 1708)
+        XCTAssertEqual(uploadedMedia.details?.fileName, "2021/11/img_0005-1-scaled.jpeg")
+        XCTAssertEqual(uploadedMedia.title, .init(rendered: "img_0005-1"))
+        XCTAssertEqual(uploadedMedia.details?.sizes["thumbnail"],
+                       .init(fileName: "img_0005-1-150x150.jpeg",
+                             src: "https://ninja.media/wp-content/uploads/2021/11/img_0005-1-150x150.jpeg",
+                             width: 150,
+                             height: 150))
+    }
+
+    /// Verifies that `uploadMediaUsingRestApi` properly relays Networking Layer errors.
+    ///
+    func test_uploadMediaUsingRestApi_properly_relays_networking_errors() {
+        // Given
+        let remote = MediaRemote(network: network)
+
+        // When
+        let result = waitFor { promise in
+            remote.uploadMediaUsingRestApi(siteURL: self.sampleSiteURL,
+                                           productID: self.sampleProductID,
+                                           mediaItems: []) { result in
+                promise(result)
+            }
+        }
+
+        // Then
+        XCTAssertTrue(result.isFailure)
+    }
+
     // MARK: - updateProductID
 
     /// Verifies that `updateProductID` properly parses the `media-update-product-id` sample response.
@@ -293,6 +406,49 @@ final class MediaRemoteTests: XCTestCase {
             remote.updateProductIDToWordPressSite(siteID: self.sampleSiteID,
                                    productID: self.sampleProductID,
                                    mediaID: self.sampleMediaID) { result in
+                promise(result)
+            }
+        }
+
+        // Then
+        XCTAssertTrue(result.isFailure)
+    }
+
+    // MARK: - updateProductIDUsingRestApi
+
+    /// Verifies that `updateProductIDUsingRestApi` properly parses the `media-update-product-id-in-wordpress-site` sample response.
+    ///
+    func test_updateProductIDUsingRestApi_properly_returns_parsed_media() throws {
+        // Given
+        let remote = MediaRemote(network: network)
+        let path = "media/\(sampleMediaID)"
+        network.simulateResponse(requestUrlSuffix: path, filename: "media-update-product-id-in-wordpress-site")
+
+        // When
+        let result = waitFor { promise in
+            remote.updateProductIDUsingRestApi(siteURL: self.sampleSiteURL,
+                                               productID: self.sampleProductID,
+                                               mediaID: self.sampleMediaID) { result in
+                promise(result)
+            }
+        }
+
+        // Then
+        let media = try XCTUnwrap(result.get())
+        XCTAssertEqual(media.mediaID, sampleMediaID)
+    }
+
+    /// Verifies that `updateProductIDUsingRestApi` properly relays Networking Layer errors.
+    ///
+    func test_updateProductIDUsingRestApi_properly_relays_networking_errors() {
+        // Given
+        let remote = MediaRemote(network: network)
+
+        // When
+        let result = waitFor { promise in
+            remote.updateProductIDUsingRestApi(siteURL: self.sampleSiteURL,
+                                               productID: self.sampleProductID,
+                                               mediaID: self.sampleMediaID) { result in
                 promise(result)
             }
         }

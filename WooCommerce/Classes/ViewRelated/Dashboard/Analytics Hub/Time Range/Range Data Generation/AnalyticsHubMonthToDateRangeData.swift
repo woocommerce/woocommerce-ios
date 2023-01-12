57766ae6f19b63013636162155e39365f854430d
ThomazFB <thomazfcortez@gmail.com>
2023-01-12 20:19:36 -0300

    Update documentation for Today and `to date` ranges

--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/Time Range/Range Data Generation/AnalyticsHubMonthToDateRangeData.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/Time Range/Range Data Generation/AnalyticsHubMonthToDateRangeData.swift
@@ -4,9 +4,20 @@ import Foundation
 /// until the current date and the previous one, starting from the first day of the previous month
 /// until the same day of the previous month. E. g.
 ///
-/// Today: 31 Jul 2022
+/// Today: 20 Jul 2022
+///
 /// Current range: Jul 1 until Jul 31, 2022
-/// Previous range: Jun 1 until Jun 30, 2022
+/// Formatted current range for UI: Jul 1 until Jul 20, 2022
+/// 
+/// Previous range: Jun 1 until Jun 20, 2022
+/// Formatted previous range for UI: Jun 1 until Jun 20, 2022
+///
+/// The reason why there's a difference between the current range and the formatted current range
+/// is due to the My Store rule that creates the end date far in the future for each tab instead of using today's date.
+/// This behavior covers any time zone gap between the app and the store, always fetching as much data in “the future” as possible.
+///
+/// For data consistency, the Analytics Hub should follow the same for this range,
+/// but only for the current one, the previous should remain using the today's date as the reference for the end date. 
 ///
 struct AnalyticsHubMonthToDateRangeData: AnalyticsHubTimeRangeData {
     let currentDateStart: Date?
--- WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/Time Range/Range Data Generation/AnalyticsHubWeekToDateRangeData.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/Time Range/Range Data Generation/AnalyticsHubWeekToDateRangeData.swift
@@ -5,8 +5,19 @@ import Foundation
 /// until the same day of that week. E. g.
 ///
 /// Today: 29 Jul 2022
-/// Current range: Jul 25 until Jul 29, 2022
+///
+/// Current range: Jul 25 until Jul 31, 2022
+/// Formatted current range for UI: Jul 25 until Jul 29, 2022
+/// 
 /// Previous range: Jul 18 until Jul 22, 2022
+/// Formatted previous range for UI: Jul 18 until Jul 22, 2022
+///
+/// The reason why there's a difference between the current range and the formatted current range
+/// is due to the My Store rule that creates the end date far in the future for each tab instead of using today's date.
+/// This behavior covers any time zone gap between the app and the store, always fetching as much data in “the future” as possible.
+///
+/// For data consistency, the Analytics Hub should follow the same for this range,
+/// but only for the current one, the previous should remain using the today's date as the reference for the end date. 
 ///
 struct AnalyticsHubWeekToDateRangeData: AnalyticsHubTimeRangeData {
     let currentDateStart: Date?

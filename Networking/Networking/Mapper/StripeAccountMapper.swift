4fcb5861e13e5ff85c2ed76f1d9871f44d2aa603
Huong Do <hi.huongdo@gmail.com>
2023-01-16 21:27:01 +0700

    Update StripeAccountMapper to parse content without data envelope

--- Networking/Networking/Mapper/StripeAccountMapper.swift
+++ Networking/Networking/Mapper/StripeAccountMapper.swift
@@ -14,7 +14,11 @@ struct StripeAccountMapper: Mapper {
         /// can cross that bridge when we need those decoded.
         decoder.dateDecodingStrategy = .secondsSince1970
 
-        return try decoder.decode(StripeAccountEnvelope.self, from: response).account
+        do {
+            return try decoder.decode(StripeAccountEnvelope.self, from: response).account
+        } catch {
+            return try decoder.decode(StripeAccount.self, from: response)
+        }
     }
 }
 

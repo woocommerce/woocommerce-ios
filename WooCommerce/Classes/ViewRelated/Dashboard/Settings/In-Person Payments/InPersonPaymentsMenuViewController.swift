55d327963406cfb8597b20f09821aaa19abe057b
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-01-16 09:50:19 +0800

    Remove previous Payments View implementation

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsMenuViewController.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsMenuViewController.swift
@@ -67,7 +67,6 @@ final class InPersonPaymentsMenuViewController: UIViewController {
         runCardPresentPaymentsOnboardingIfPossible()
         configureWebViewPresentation()
         viewModel.viewDidLoad()
-        viewModel.displayIPPFeedbackBannerIfEligible()
     }
 }
 
--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsMenuViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsMenuViewModel.swift
@@ -1,20 +1,16 @@
 import Foundation
 import Yosemite
 import WooFoundation
-import protocol Storage.StorageManagerType
 
 final class InPersonPaymentsMenuViewModel {
     // MARK: - Dependencies
     struct Dependencies {
         let stores: StoresManager
-        let storage: StorageManagerType
         let analytics: Analytics
 
         init(stores: StoresManager = ServiceLocator.stores,
-             storage: StorageManagerType = ServiceLocator.storageManager,
              analytics: Analytics = ServiceLocator.analytics) {
             self.stores = stores
-            self.storage = storage
             self.analytics = analytics
         }
     }
@@ -25,18 +21,9 @@ final class InPersonPaymentsMenuViewModel {
         dependencies.stores
     }
 
-    private var storage: StorageManagerType {
-        dependencies.storage
-    }
-
     private var analytics: Analytics {
         dependencies.analytics
     }
-
-    // MARK: - ResultsControllers
-    private lazy var IPPOrdersResultsController = createIPPOrdersResultsController()
-
-    private lazy var recentIPPOrdersResultsController = createRecentIPPOrdersResultsController()
 
     // MARK: - Output properties
     @Published var showWebView: AuthenticatedWebViewModel? = nil
@@ -44,14 +31,6 @@ final class InPersonPaymentsMenuViewModel {
     // MARK: - Configuration properties
     private var siteID: Int64? {
         return stores.sessionManager.defaultStoreID
-    }
-
-    private var isCODEnabled: Bool {
-        guard let siteID = siteID,
-              let codGateway = dependencies.storage.viewStorage.loadPaymentGateway(siteID: siteID, gatewayID: "cod")?.toReadOnly() else {
-            return false
-        }
-        return codGateway.enabled
     }
 
     var isEligibleForCardPresentPayments: Bool {
@@ -64,8 +43,6 @@ final class InPersonPaymentsMenuViewModel {
          cardPresentPaymentsConfiguration: CardPresentPaymentsConfiguration = CardPresentConfigurationLoader().configuration) {
         self.dependencies = dependencies
         self.cardPresentPaymentsConfiguration = cardPresentPaymentsConfiguration
-
-        fetchIPPTransactions()
     }
 
     func viewDidLoad() {
@@ -89,86 +66,9 @@ final class InPersonPaymentsMenuViewModel {
             self?.showWebView = nil
         })
     }
-
-    /// This method is just a helper for debugging, we may use it for populating different Banner content based on the fetched objects count
-    ///
-    func displayIPPFeedbackBannerIfEligible() {
-        if isCODEnabled {
-            let hasResults = IPPOrdersResultsController.fetchedObjects.isEmpty ? false : true
-
-            /// In order to filter WCPay transactions processed through IPP within the last 30 days,
-            /// we check if these contain `receipt_url` in their metadata, unlike those processed through a website,
-            /// which doesn't
-            ///
-            let IPPTransactionsFound = recentIPPOrdersResultsController.fetchedObjects.filter({
-                $0.customFields.contains(where: {$0.key == Constants.receiptURLKey }) &&
-                $0.paymentMethodTitle == Constants.paymentMethodTitle})
-            let IPPresultsCount = IPPTransactionsFound.count
-
-            // TODO: Debug. Remove before merging
-            print("hasResults? \(hasResults)")
-            print("IPP transactions within 30 days: \(IPPresultsCount)")
-            print(recentIPPOrdersResultsController.fetchedObjects.map {
-                ("OrderID: \($0.orderID) - PaymentMethodID: \($0.paymentMethodID) (\($0.paymentMethodTitle) - DatePaid: \(String(describing: $0.datePaid))")
-            })
-
-            if !hasResults {
-                print("0 transactions. Banner 1 shown")
-            } else if IPPresultsCount < Constants.numberOfTransactions {
-                print("< 10 transactions within 30 days. Banner 2 shown")
-            } else if IPPresultsCount >= Constants.numberOfTransactions {
-                print(">= 10 transactions within 30 days. Banner 3 shown")
-            }
-        }
-    }
-
-    private func fetchIPPTransactions() {
-        do {
-            try IPPOrdersResultsController.performFetch()
-            try recentIPPOrdersResultsController.performFetch()
-        } catch {
-            DDLogError("Error fetching IPP transactions: \(error)")
-        }
-    }
-}
-
-private extension InPersonPaymentsMenuViewModel {
-    /// Results controller that fetches any IPP transactions via WooCommerce Payments
-    ///
-    func createIPPOrdersResultsController() -> ResultsController<StorageOrder> {
-        let paymentGateway = Constants.paymentMethodID
-        let predicate = NSPredicate(
-            format: "siteID == %lld AND paymentMethodID == %@",
-            argumentArray: [siteID ?? 0, paymentGateway]
-        )
-        return ResultsController<StorageOrder>(storageManager: storage, matching: predicate, sortedBy: [])
-    }
-
-    /// Results controller that fetches IPP transactions via WooCommerce Payments, within the last 30 days
-    ///
-    func createRecentIPPOrdersResultsController() -> ResultsController<StorageOrder> {
-        let today = Date()
-        let paymentGateway = Constants.paymentMethodID
-        let thirtyDaysBeforeToday = Calendar.current.date(
-            byAdding: .day,
-            value: -30,
-            to: today
-        )!
-
-        let predicate = NSPredicate(
-            format: "siteID == %lld AND paymentMethodID == %@ AND datePaid >= %@",
-            argumentArray: [siteID ?? 0, paymentGateway, thirtyDaysBeforeToday]
-        )
-
-        return ResultsController<StorageOrder>(storageManager: storage, matching: predicate, sortedBy: [])
-    }
 }
 
 private enum Constants {
     static let utmCampaign = "payments_menu_item"
     static let utmSource = "payments_menu"
-    static let paymentMethodID = "woocommerce_payments"
-    static let paymentMethodTitle = "WooCommerce In-Person Payments"
-    static let receiptURLKey = "receipt_url"
-    static let numberOfTransactions = 10
 }

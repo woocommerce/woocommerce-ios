9daeca0a72bed1d4467b38c027d3a669019c154f
Jaclyn Chen <watertranquil@gmail.com>
2023-01-17 12:52:51 +0800

    Implement `DomainAction.loadPaidDomainSuggestions` to load domain suggestions and products for paid domain selector.

--- Yosemite/YosemiteTests/Stores/DomainStoreTests.swift
+++ Yosemite/YosemiteTests/Stores/DomainStoreTests.swift
@@ -71,6 +71,65 @@ final class DomainStoreTests: XCTestCase {
         XCTAssertEqual(error as? NetworkError, .timeout)
     }
 
+    // MARK: - `loadPaidDomainSuggestions`
+
+    func test_loadPaidDomainSuggestions_returns_suggestions_on_success() throws {
+        // Given
+        remote.whenLoadingPaidDomainSuggestions(thenReturn: .success([.init(name: "paid.domain", productID: 203, supportsPrivacy: true)]))
+        remote.whenLoadingDomainProducts(thenReturn: .success([.init(productID: 203, term: "year", cost: "NT$610.00", saleCost: "NT$154.00")]))
+
+        // When
+        let result = waitFor { promise in
+            self.store.onAction(DomainAction.loadPaidDomainSuggestions(query: "domain") { result in
+                promise(result)
+            })
+        }
+
+        // Then
+        XCTAssertTrue(result.isSuccess)
+        let suggestions = try XCTUnwrap(result.get())
+        XCTAssertEqual(suggestions, [.init(productID: 203, name: "paid.domain", term: "year", cost: "NT$610.00", saleCost: "NT$154.00")])
+    }
+
+    func test_loadPaidDomainSuggestions_returns_empty_suggestions_from_failed_productID_mapping() throws {
+        // Given
+        remote.whenLoadingPaidDomainSuggestions(thenReturn: .success([.init(name: "paid.domain", productID: 203, supportsPrivacy: true)]))
+        // The product ID does not match the domain suggestion.
+        remote.whenLoadingDomainProducts(thenReturn: .success([.init(productID: 156, term: "year", cost: "NT$610.00", saleCost: "NT$154.00")]))
+
+        // When
+        let result = waitFor { promise in
+            self.store.onAction(DomainAction.loadPaidDomainSuggestions(query: "domain") { result in
+                promise(result)
+            })
+        }
+
+        // Then
+        XCTAssertTrue(result.isSuccess)
+        let suggestions = try XCTUnwrap(result.get())
+        XCTAssertEqual(suggestions, [])
+    }
+
+    func test_loadPaidDomainSuggestions_returns_error_on_failure() throws {
+        // Given
+        remote.whenLoadingPaidDomainSuggestions(thenReturn: .failure(NetworkError.invalidURL))
+        remote.whenLoadingDomainProducts(thenReturn: .failure(NetworkError.timeout))
+
+        // When
+        let result = waitFor { promise in
+            self.store.onAction(DomainAction.loadPaidDomainSuggestions(query: "domain") { result in
+                promise(result)
+            })
+        }
+
+        // Then
+        XCTAssertTrue(result.isFailure)
+        let error = try XCTUnwrap(result.failure)
+        // The error of `loadDomainProducts` is returned since it is the first async call.
+        XCTAssertEqual(error as? NetworkError, .timeout)
+    }
+
+
     // MARK: - `loadDomains`
 
     func test_loadDomains_returns_domains_on_success() throws {

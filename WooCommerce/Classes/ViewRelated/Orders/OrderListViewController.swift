523fe0a8ef8ecf31a8b08612138a2b7d5acc73d4
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-01-19 17:04:24 +0800

    Add dismiss upon tapping on survey button

--- WooCommerce/Classes/ViewRelated/Orders/OrderListViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/OrderListViewController.swift
@@ -796,6 +796,9 @@ private extension OrderListViewController {
     private func createIPPFeedbackTopBanner() -> TopBannerView {
         let shareIPPFeedbackAction = TopBannerViewModel.ActionButton(title: Localization.shareFeedbackButton, action: { _ in
             self.displayIPPFeedbackBannerSurvey()
+            // We dismiss the banner at this point as well, as we cannot know if was successfully submited or not at this point
+            // We will check the FeedbackStatus to know if we must display it again or not
+            self.viewModel.dismissIPPFeedbackBanner(remindLater: false, remindAfter: nil)
         })
 
         let viewModel = TopBannerViewModel(

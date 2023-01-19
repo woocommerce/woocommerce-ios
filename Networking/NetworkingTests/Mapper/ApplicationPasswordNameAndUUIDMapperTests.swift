ac7e2b414fc074ce7a559255006f8f1b20412722
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-01-19 16:07:41 +0530

    Mapper for fetching application password name and uuid.

---
+++ Networking/NetworkingTests/Mapper/ApplicationPasswordNameAndUUIDMapperTests.swift
@@ -0,0 +1,37 @@
+import XCTest
+@testable import Networking
+
+/// ApplicationPasswordNameAndUUIDMapper Unit Tests
+///
+final class ApplicationPasswordNameAndUUIDMapperTests: XCTestCase {
+
+    private let wpOrgUsername = "username"
+
+    /// Verifies that GET application password response is parsed properly
+    ///
+    func test_response_is_properly_parsed_when_loading_all_application_passwords() throws {
+        guard let passwords = mapGetApplicationPasswordsResponse() else {
+            XCTFail()
+            return
+        }
+
+        let password = try XCTUnwrap(passwords.first)
+        XCTAssertEqual(password.uuid, "42467857-579d-4bf3-8cc7-88bfb701d3a7")
+        XCTAssertEqual(password.name, "testest")
+    }
+}
+
+// MARK: - Private Methods.
+//
+private extension ApplicationPasswordNameAndUUIDMapperTests {
+
+    /// Returns the ApplicationPasswordNameAndUUIDMapper output upon receiving success response
+    ///
+    func mapGetApplicationPasswordsResponse() -> [ApplicationPasswordNameAndUUID]? {
+        guard let response = Loader.contentsOf("get-application-passwords-success") else {
+            return nil
+        }
+
+        return try? ApplicationPasswordNameAndUUIDMapper().map(response: response)
+    }
+}

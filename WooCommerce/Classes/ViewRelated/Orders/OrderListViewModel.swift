b8ad392ca8868eac0ea13b79e1798f5cd17b749f
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-01-23 13:33:37 +0800

    Remove testing method

--- WooCommerce/Classes/ViewRelated/Orders/OrderListViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/OrderListViewModel.swift
@@ -221,25 +221,17 @@ final class OrderListViewModel {
         stores.dispatch(action)
     }
 
-    // This is a temporary method in order to update the IPP feedback status to `.pending`, and
-    // then load feedback visibility. We need to reset the banner status on UserDefaults for
-    // the banner to appear again for testing purposes.
+    // Requests if the In-Person Payments feedback banner should be shown,
+    // in which case we proceed to sync the view model by fetching transactions
     private func syncIPPBannerVisibility() {
-        let action = AppSettingsAction.updateFeedbackStatus(type: .IPP, status: .pending) { _ in
-            self.loadIPPFeedbackBannerVisibility()
-            self.fetchIPPTransactions()
-        }
-        stores.dispatch(action)
-    }
-
-    private func loadIPPFeedbackBannerVisibility() {
-        let action = AppSettingsAction.loadFeedbackVisibility(type: .IPP) { [weak self] result in
-            switch result {
+        let action = AppSettingsAction.loadFeedbackVisibility(type: .IPP) { [weak self] visibility in
+            switch visibility {
             case .success(let visible):
                 self?.hideIPPFeedbackBanner = !visible
+                self?.fetchIPPTransactions()
             case .failure(let error):
                 self?.hideIPPFeedbackBanner = true
-                ServiceLocator.crashLogging.logError(error)
+                DDLogError("Couldn't load feedback visibility. \(error)")
             }
         }
         self.stores.dispatch(action)

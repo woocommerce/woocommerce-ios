0fcd33e334b7356c0764307827252c981ed0326e
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-01-25 16:50:14 +0800

    Add internal comments

--- WooCommerce/Classes/Extensions/UIView+Border.swift
+++ WooCommerce/Classes/Extensions/UIView+Border.swift
@@ -1,9 +1,10 @@
 import UIKit
 
 extension UIView {
-    /// Creates a border view with the given height and color.
+    /// Creates a border view with the given height, and color.
     /// - Parameter height: height of the border. Default to be 0.5px.
     /// - Parameter color: background color of the border. Default to be the divider color.
+    ///
     static func createBorderView(height: CGFloat = 0.5,
                                  color: UIColor = .systemColor(.separator)) -> UIView {
         let view = UIView(frame: .zero)
@@ -15,7 +16,11 @@ extension UIView {
         return view
     }
 
-    /// Creates an empty view with the given X
+    /// Creates an empty view with the given height, width, and color
+    /// - Parameter height: height of the separator. Default to be 0.5px.
+    /// - Parameter width: width of the separator. Default to be 0.5px.
+    /// - Parameter color: background color of the separator. Default to be `.clear`
+    /// 
     static func createSeparatorView(height: CGFloat = 0.5,
                                 width: CGFloat = 0.5,
                                 color: UIColor = .clear) -> UIView {

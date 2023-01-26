8fcfb6bd10d5f0b4229599620b019c9b16ad7bba
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-01-26 12:39:29 +0530

    Refactor unit tests to use firstIndex to find events.

--- WooCommerce/WooCommerceTests/AppCoordinatorTests.swift
+++ WooCommerce/WooCommerceTests/AppCoordinatorTests.swift
@@ -340,10 +340,12 @@ final class AppCoordinatorTests: XCTestCase {
         pushNotesManager.sendLocalNotificationResponse(response)
 
         // Then
-        XCTAssertEqual(analytics.receivedEvents, [WooAnalyticsStat.loginLocalNotificationTapped.rawValue])
-        let actionPropertyValue = try XCTUnwrap(analytics.receivedProperties.first?["action"] as? String)
+        let indexOfEvent = try XCTUnwrap(analytics.receivedEvents.firstIndex(where: { $0 == WooAnalyticsStat.loginLocalNotificationTapped.rawValue}))
+        let eventProperties = try XCTUnwrap(analytics.receivedProperties[indexOfEvent])
+
+        let actionPropertyValue = try XCTUnwrap(eventProperties["action"] as? String)
         XCTAssertEqual(actionPropertyValue, "contact_support")
-        let typePropertyValue = try XCTUnwrap(analytics.receivedProperties.first?["type"] as? String)
+        let typePropertyValue = try XCTUnwrap(eventProperties["type"] as? String)
         XCTAssertEqual(typePropertyValue, "site_address_error")
     }
 
@@ -364,10 +366,12 @@ final class AppCoordinatorTests: XCTestCase {
         pushNotesManager.sendLocalNotificationResponse(response)
 
         // Then
-        XCTAssertEqual(analytics.receivedEvents, [WooAnalyticsStat.loginLocalNotificationTapped.rawValue])
-        let actionPropertyValue = try XCTUnwrap(analytics.receivedProperties.first?["action"] as? String)
+        let indexOfEvent = try XCTUnwrap(analytics.receivedEvents.firstIndex(where: { $0 == WooAnalyticsStat.loginLocalNotificationTapped.rawValue}))
+        let eventProperties = try XCTUnwrap(analytics.receivedProperties[indexOfEvent])
+
+        let actionPropertyValue = try XCTUnwrap(eventProperties["action"] as? String)
         XCTAssertEqual(actionPropertyValue, "login_with_wpcom")
-        let typePropertyValue = try XCTUnwrap(analytics.receivedProperties.first?["type"] as? String)
+        let typePropertyValue = try XCTUnwrap(eventProperties["type"] as? String)
         XCTAssertEqual(typePropertyValue, "site_address_error")
     }
 
@@ -388,10 +392,12 @@ final class AppCoordinatorTests: XCTestCase {
         pushNotesManager.sendLocalNotificationResponse(response)
 
         // Then
-        XCTAssertEqual(analytics.receivedEvents, [WooAnalyticsStat.loginLocalNotificationTapped.rawValue])
-        let actionPropertyValue = try XCTUnwrap(analytics.receivedProperties.first?["action"] as? String)
+        let indexOfEvent = try XCTUnwrap(analytics.receivedEvents.firstIndex(where: { $0 == WooAnalyticsStat.loginLocalNotificationTapped.rawValue}))
+        let eventProperties = try XCTUnwrap(analytics.receivedProperties[indexOfEvent])
+
+        let actionPropertyValue = try XCTUnwrap(eventProperties["action"] as? String)
         XCTAssertEqual(actionPropertyValue, "default")
-        let typePropertyValue = try XCTUnwrap(analytics.receivedProperties.first?["type"] as? String)
+        let typePropertyValue = try XCTUnwrap(eventProperties["type"] as? String)
         XCTAssertEqual(typePropertyValue, "site_address_error")
     }
 
@@ -412,8 +418,10 @@ final class AppCoordinatorTests: XCTestCase {
         pushNotesManager.sendLocalNotificationResponse(response)
 
         // Then
-        XCTAssertEqual(analytics.receivedEvents, [WooAnalyticsStat.loginLocalNotificationDismissed.rawValue])
-        let typePropertyValue = try XCTUnwrap(analytics.receivedProperties.first?["type"] as? String)
+        let indexOfEvent = try XCTUnwrap(analytics.receivedEvents.firstIndex(where: { $0 == WooAnalyticsStat.loginLocalNotificationDismissed.rawValue}))
+        let eventProperties = try XCTUnwrap(analytics.receivedProperties[indexOfEvent])
+
+        let typePropertyValue = try XCTUnwrap(eventProperties["type"] as? String)
         XCTAssertEqual(typePropertyValue, "site_address_error")
     }
 }

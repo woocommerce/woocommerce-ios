8a2d974d9872a64455e32da7a1d6dce1e9edaf2d
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2023-01-30 10:58:23 +0100

    Use systemUpdate instead of Date to measure with more reliability.

--- WooCommerce/Classes/ViewRelated/Orders/Order Creation/DurationRecorder/OrderDurationRecorder.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/DurationRecorder/OrderDurationRecorder.swift
@@ -11,16 +11,16 @@ class OrderDurationRecorder {
     static let shared = OrderDurationRecorder()
     private var orderAddNewTimestamp: TimeInterval?
     private var cardPaymentStartedTimestamp: TimeInterval?
-    private static let timeout = 60*10
+    private static let timeout: TimeInterval = 60*10
 
     private init() { }
 
     func startRecording() {
-        orderAddNewTimestamp = Date().timeIntervalSince1970
+        orderAddNewTimestamp = ProcessInfo.processInfo.systemUptime
     }
 
     func recordCardPaymentStarted() {
-        cardPaymentStartedTimestamp = Date().timeIntervalSince1970
+        cardPaymentStartedTimestamp = ProcessInfo.processInfo.systemUptime
     }
 
     func reset() {
@@ -28,20 +28,20 @@ class OrderDurationRecorder {
         cardPaymentStartedTimestamp = nil
     }
 
-    func timeIntervalSinceOrderAddNew() throws -> Int64 {
-        try timeIntervalSince(orderAddNewTimestamp)
+    func timeIntervalSinceOrderAddNew() throws -> TimeInterval {
+        return try timeIntervalSince(orderAddNewTimestamp)
     }
 
-    func timeIntervalSinceCardPaymentStarted() throws -> Int64 {
+    func timeIntervalSinceCardPaymentStarted() throws -> TimeInterval {
         try timeIntervalSince(cardPaymentStartedTimestamp)
     }
 
-    private func timeIntervalSince(_ origin: TimeInterval?) throws -> Int64 {
+    private func timeIntervalSince(_ origin: TimeInterval?) throws -> TimeInterval {
         guard let startTimestamp = origin else {
             throw OrderDurationRecorderError.noStartRecordingTimestamp
         }
 
-        let timestamp = Int64(Date().timeIntervalSince1970 - startTimestamp)
+        let timestamp = ProcessInfo.processInfo.systemUptime - startTimestamp
 
         guard timestamp < OrderDurationRecorder.timeout else {
             throw OrderDurationRecorderError.durationExceededTimeout

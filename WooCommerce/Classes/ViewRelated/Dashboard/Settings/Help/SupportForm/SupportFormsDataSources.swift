6e7f01d06697ff1b61b0ecb7978375c3bf34742e
Ernesto Carrión <ernesto.carrion@automattic.com>
2023-02-03 09:23:59 -0500

    Adds empty Zendesk datasources

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Help/SupportForm/SupportFormsDataSources.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/// Provides general Zendesk metadata.
+///
+struct GeneralSupportDataSource {
+
+}
+
+/// Provides WCPay Zendesk metadata.
+///
+struct WCPaySupportDataSource {
+
+}

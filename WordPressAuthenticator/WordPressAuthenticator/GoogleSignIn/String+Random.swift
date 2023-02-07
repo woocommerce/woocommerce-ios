19a4d0549d07b1e69e99bed081b237245a3dc48e
Gio Lodi <gio@mokacoding.com>
2023-02-08 10:58:28 +1100

    Add `String` extension to generate random string

---
+++ WordPressAuthenticator/WordPressAuthenticator/GoogleSignIn/String+Random.swift
@@ -0,0 +1,12 @@
+extension String {
+
+    static func randomString(usingCharacters characters: String, withLenght length: Int) -> String {
+        let allowedCharactersCount = UInt32(characters.count)
+
+        return (0..<length).reduce("") { accumulator, _ in
+           let randomOffset = Int(arc4random_uniform(allowedCharactersCount))
+           let randomCharacter = characters[characters.index(characters.startIndex, offsetBy: randomOffset)]
+           return accumulator + String(randomCharacter)
+        }
+    }
+}
---
+++ WordPressAuthenticator/WordPressAuthenticatorTests/GoogleSignIn/String+RandomTests.swift
@@ -0,0 +1,36 @@
+@testable import WordPressAuthenticator
+import XCTest
+
+class StringRandomTests: XCTestCase {
+
+    func testRandomStringLengthIsAsRequested() {
+        XCTAssertEqual(
+            String.randomString(usingCharacters: "abc", withLenght: 10).count,
+            10
+        )
+    }
+
+    func testRandomStringUsesGivenCharactersOnly() {
+        // Using length 300 with 3 characters be relatively sure sure we'll get all of the
+        // characters at least once.
+        let randomString = String.randomString(usingCharacters: "abc", withLenght: 30)
+
+        XCTAssertEqual(
+            CharacterSet(charactersIn: randomString),
+            CharacterSet(charactersIn: "abc")
+        )
+    }
+
+    func testRandomStringsAreDifferent() {
+        let characters = "abcdefghijklmnopqrstuvwxyz"
+        let length = 100
+
+        // It is _possible_ for the strings to be equal. However, it should be so unlikely that a
+        // test failure will most definitely mean an error in the code, rather than a legit case of
+        // the same random string being generated twice.
+        XCTAssertNotEqual(
+            String.randomString(usingCharacters: characters, withLenght: length),
+            String.randomString(usingCharacters: characters, withLenght: length)
+        )
+    }
+}

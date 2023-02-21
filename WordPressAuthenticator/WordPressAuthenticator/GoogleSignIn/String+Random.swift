7a978d5799f99417511961f3ca99060ebf06381e
Gio Lodi <gio@mokacoding.com>
2023-02-21 14:19:53 +1100

    Use `SecRandomCopyBytes` to generate cryptographically secure string
    Notice I left a `fatalError` in there still to address

--- WordPressAuthenticator/WordPressAuthenticator/GoogleSignIn/String+Random.swift
+++ WordPressAuthenticator/WordPressAuthenticator/GoogleSignIn/String+Random.swift
@@ -3,10 +3,20 @@ extension String {
     static func randomString(using characters: Set<Character>, withLength length: Int) -> String {
         let allowedCharactersCount = UInt32(characters.count)
 
-        return (0..<length).reduce("") { accumulator, _ in
-           let randomOffset = Int(arc4random_uniform(allowedCharactersCount))
-           let randomCharacter = characters[characters.index(characters.startIndex, offsetBy: randomOffset)]
-           return accumulator + String(randomCharacter)
+        var randomBytes = [UInt8](repeating: 0, count: length)
+        // Use `SecRandomCopyBytes` to generate cryptographically secure random bytes to use as
+        // offsets to create the random string.
+        let status = SecRandomCopyBytes(kSecRandomDefault, length, &randomBytes)
+
+        // FIXME: Handle errors by returning nil
+        guard status == errSecSuccess else {
+            fatalError()
+        }
+
+        return randomBytes.reduce("") { accumulator, randomByte in
+            let randomOffset = Int(randomByte) % Int(allowedCharactersCount)
+            let randomCharacter = characters[characters.index(characters.startIndex, offsetBy: randomOffset)]
+            return accumulator + String(randomCharacter)
         }
     }
 }

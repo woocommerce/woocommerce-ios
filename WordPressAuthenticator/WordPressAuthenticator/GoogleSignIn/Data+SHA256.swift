03d67893021431b419fb0aa9c17a1a0a8cc999e3
Gio Lodi <gio@mokacoding.com>
2023-02-21 13:52:12 +1100

    Refactor `Data` SHA256 implementation to use CryptoKit
    See conversation at
    https://github.com/wordpress-mobile/WordPressAuthenticator-iOS/pull/740#discussion_r1110439162

--- WordPressAuthenticator/WordPressAuthenticator/GoogleSignIn/Data+SHA256.swift
+++ WordPressAuthenticator/WordPressAuthenticator/GoogleSignIn/Data+SHA256.swift
@@ -1,20 +1,12 @@
-import CommonCrypto
+import CryptoKit
 
 extension Data {
 
     func sha256Hashed() -> Data {
-        Data(sha256Hash())
+        Data(SHA256.hash(data: self))
     }
 
     func sha256Hashed() -> String {
-        sha256Hash().map { String(format: "%02x", $0) }.joined(separator: "")
-    }
-
-    private func sha256Hash() -> [UInt8] {
-        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-        withUnsafeBytes {
-            _ = CC_SHA256($0.baseAddress, CC_LONG(count), &hash)
-        }
-        return hash
+        SHA256.hash(data: self).map { String(format: "%02hhx", $0) }.joined()
     }
 }

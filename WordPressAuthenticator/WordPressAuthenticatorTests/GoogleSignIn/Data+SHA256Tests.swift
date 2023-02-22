6c05bcb5d9387830ed83e73c1875e06f14b34f08
Gio Lodi <gio@mokacoding.com>
2023-02-22 16:24:26 +1100

    Rewrite more tests with neater `Data("*".utf8)`
    See also 778fbaabca50d0974f723c2eb5feed1bcd69261d

--- WordPressAuthenticator/WordPressAuthenticatorTests/GoogleSignIn/Data+SHA256Tests.swift
+++ WordPressAuthenticator/WordPressAuthenticatorTests/GoogleSignIn/Data+SHA256Tests.swift
@@ -7,15 +7,15 @@ class DataSHA256Tests: XCTestCase {
         // It's fiddly to test the hashing `Data` against `Data`, so we use `String` against
         // `String` and we trust/hope that the underlying implementation is shared.
         XCTAssertEqual(
-            "foo".data(using: .utf8)!.sha256Hashed(),
+            Data("foo".utf8).sha256Hashed(),
             "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
         )
         XCTAssertEqual(
-            "Hello, World!".data(using: .utf8)!.sha256Hashed(),
+            Data("Hello, World!".utf8).sha256Hashed(),
             "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f"
         )
         XCTAssertEqual(
-            "abcABC-=/?".data(using: .utf8)!.sha256Hashed(),
+            Data("abcABC-=/?".utf8).sha256Hashed(),
             "e8552e6ddda2103b18158c28ecd834b1772c72794011547c9ef6d8fcb3419a23"
         )
     }

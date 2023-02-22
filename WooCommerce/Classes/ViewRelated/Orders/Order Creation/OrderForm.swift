65803d77a544a9e595f03577c6124f70d368979b
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-02-22 10:53:39 +0700

    Capitalize featureFlag

--- WooCommerce/Classes/ViewRelated/Orders/Order Creation/OrderForm.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/OrderForm.swift
@@ -352,7 +352,7 @@ struct OrderForm_Previews: PreviewProvider {
 
 private extension ProductSelectorView.Configuration {
     static let addProductToOrder: Self =
-        .init(multipleSelectionsEnabled: ServiceLocator.featureFlagService.isFeatureFlagEnabled(.productMultiselectionM1),
+        .init(multipleSelectionsEnabled: ServiceLocator.featureFlagService.isFeatureFlagEnabled(.productMultiSelectionM1),
               searchHeaderBackgroundColor: .listBackground,
               prefersLargeTitle: false,
               title: Localization.title,

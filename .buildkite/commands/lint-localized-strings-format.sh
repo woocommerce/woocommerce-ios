4693ef51bc94d9d29a40e49b365c44d8bbe8cb2b
Spencer Transier <spencer@transier.us>
2023-02-22 20:24:53 -0700

    Add Bash Cache action to the strings Linter

--- .buildkite/commands/lint-localized-strings-format.sh
+++ .buildkite/commands/lint-localized-strings-format.sh
@@ -5,4 +5,7 @@ SECRETS_DIR=~/.configure/woocommerce-ios/secrets
 mkdir -pv $SECRETS_DIR
 cp -v fastlane/env/project.env.example $SECRETS_DIR/project.env
 
+echo "--- :swift: Restoring SPM Cache"
+install_swiftpm_dependencies
+
 lint_localized_strings_format

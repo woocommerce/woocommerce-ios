19e0087766ecbdc8946191571b660a805870790b
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-02-24 19:55:00 +0530

    ReadSize extension for getting total available size in SwiftUI views.

---
+++ WooCommerce/Classes/View Modifiers/View+ReadSize.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+// From https://github.com/FiveStarsBlog/CodeSamples/blob/0d764ee68ee1cfd634f6726f2867dcba383ebe99/SwiftUI-read-a-view-size/View%2BreadSize.swift
+
+extension View {
+    func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
+        background(
+            GeometryReader { geometryProxy in
+                Color.clear
+                    .preference(key: SizePreferenceKey.self, value: geometryProxy.size)
+            }
+        )
+        .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
+    }
+}
+
+private struct SizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+}

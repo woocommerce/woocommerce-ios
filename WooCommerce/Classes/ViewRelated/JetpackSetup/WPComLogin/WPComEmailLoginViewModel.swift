ebeb0cdc912877cee5c082a0ea4492ada1c35ce4
Huong Do <hi.huongdo@gmail.com>
2023-02-27 14:34:51 +0700

    Add onSubmit closure for WPComEmailLoginViewModel

--- WooCommerce/Classes/ViewRelated/JetpackSetup/WPComLogin/WPComEmailLoginViewModel.swift
+++ WooCommerce/Classes/ViewRelated/JetpackSetup/WPComLogin/WPComEmailLoginViewModel.swift
@@ -3,4 +3,11 @@ import Foundation
 /// View model for `WPComEmailLoginView`
 final class WPComEmailLoginViewModel {
     @Published var emailAddress: String = ""
+
+    /// The closure to be triggered when the Install Jetpack button is tapped.
+    private let onSubmit: (String) -> Void
+
+    init(onSubmit: @escaping (String) -> Void) {
+        self.onSubmit = onSubmit
+    }
 }

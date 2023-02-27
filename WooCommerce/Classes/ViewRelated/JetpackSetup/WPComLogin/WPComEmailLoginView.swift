bea6d919d254e4f1ce343da1dcceb7283cc69317
Huong Do <hi.huongdo@gmail.com>
2023-02-27 14:34:59 +0700

    Add WPComEmailLoginHostingController

--- WooCommerce/Classes/ViewRelated/JetpackSetup/WPComLogin/WPComEmailLoginView.swift
+++ WooCommerce/Classes/ViewRelated/JetpackSetup/WPComLogin/WPComEmailLoginView.swift
@@ -1,5 +1,41 @@
 import SwiftUI
 
+/// Hosting controller for `WPComEmailLoginView`
+final class WPComEmailLoginHostingController: UIHostingController<WPComEmailLoginView> {
+    private lazy var noticePresenter: DefaultNoticePresenter = {
+        let noticePresenter = DefaultNoticePresenter()
+        noticePresenter.presentingViewController = self
+        return noticePresenter
+    }()
+
+    init(onSubmit: @escaping (String) -> Void) {
+        super.init(rootView: WPComEmailLoginView(viewModel: .init(onSubmit: onSubmit)))
+    }
+
+    @available(*, unavailable)
+    required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTransparentNavigationBar()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: Localization.cancel, style: .plain, target: self, action: #selector(dismissView))
+    }
+
+    @objc
+    private func dismissView() {
+        dismiss(animated: true)
+    }
+}
+
+private extension WPComEmailLoginHostingController {
+    enum Localization {
+        static let cancel = NSLocalizedString("Cancel", comment: "Button to dismiss the site credential login screen")
+    }
+}
+
+
 /// Screen for logging in to a WPCom account during the Jetpack setup flow
 /// This is presented for users authenticated with WPOrg credentials.
 struct WPComEmailLoginView: View {
@@ -10,20 +46,17 @@ struct WPComEmailLoginView: View {
     }
 
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: Constants.blockVerticalPadding) {
-                JetpackInstallHeaderView()
-                
-                // title and description
-                VStack(alignment: .leading, spacing: Constants.contentVerticalSpacing) {
-                    Text(Localization.installJetpack)
-                        .largeTitleStyle()
-                    Text(Localization.loginToInstall)
-                        .subheadlineStyle()
-                }
-    
-                Spacer()
+        ScrollableVStack(alignment: .leading, padding: Constants.contentHorizontalPadding, spacing: Constants.blockVerticalPadding) {
+            JetpackInstallHeaderView()
+
+            // title and description
+            VStack(alignment: .leading, spacing: Constants.contentVerticalSpacing) {
+                Text(Localization.installJetpack)
+                    .largeTitleStyle()
+                Text(Localization.loginToInstall)
+                    .subheadlineStyle()
             }
+            Spacer()
         }
     }
 }
@@ -32,6 +65,7 @@ private extension WPComEmailLoginView {
     enum Constants {
         static let blockVerticalPadding: CGFloat = 32
         static let contentVerticalSpacing: CGFloat = 8
+        static let contentHorizontalPadding: CGFloat = 16
     }
 
     enum Localization {
@@ -49,6 +83,6 @@ private extension WPComEmailLoginView {
 
 struct WPComEmailLoginView_Previews: PreviewProvider {
     static var previews: some View {
-        WPComEmailLoginView()
+        WPComEmailLoginView(viewModel: .init(onSubmit: { _ in }))
     }
 }

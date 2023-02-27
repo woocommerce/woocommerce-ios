2e1c9f2bad17cfd44344a70570af42754728db17
Huong Do <hi.huongdo@gmail.com>
2023-02-27 12:26:43 +0700

    Add tests for reloadRoles

--- WooCommerce/WooCommerceTests/ViewRelated/JetpackSetup/AdminRoleRequiredViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/JetpackSetup/AdminRoleRequiredViewModelTests.swift
@@ -2,6 +2,7 @@ import XCTest
 @testable import WooCommerce
 @testable import Yosemite
 
+@MainActor
 final class AdminRoleRequiredViewModelTests: XCTestCase {
 
     func test_username_is_correct() {
@@ -31,5 +32,67 @@ final class AdminRoleRequiredViewModelTests: XCTestCase {
 
         // Then
         assertEqual(NSLocalizedString("Shop Manager", comment: "User's Shop Manager role."), roleName)
+    }
+
+    func test_reloadRoles_returns_true_if_user_has_admin_role() async throws {
+        // Given
+        let stores = MockStoresManager(sessionManager: .makeForTesting(authenticated: true, isWPCom: false))
+        stores.whenReceivingAction(ofType: UserAction.self) { action in
+            switch action {
+            case .retrieveUser(_, let onCompletion):
+                let user = User.fake().copy(roles: [User.Role.administrator.rawValue])
+                onCompletion(.success(user))
+            }
+        }
+        let viewModel = AdminRoleRequiredViewModel(siteID: 123, stores: stores)
+
+        // When
+        let result = try await viewModel.reloadRoles()
+
+        // Then
+        XCTAssertTrue(result)
+    }
+
+    func test_reloadRoles_returns_false_if_user_does_not_have_admin_role() async throws {
+        // Given
+        let stores = MockStoresManager(sessionManager: .makeForTesting(authenticated: true, isWPCom: false))
+        stores.whenReceivingAction(ofType: UserAction.self) { action in
+            switch action {
+            case .retrieveUser(_, let onCompletion):
+                let user = User.fake().copy(roles: [User.Role.shopManager.rawValue])
+                onCompletion(.success(user))
+            }
+        }
+        let viewModel = AdminRoleRequiredViewModel(siteID: 123, stores: stores)
+
+        // When
+        let result = try await viewModel.reloadRoles()
+
+        // Then
+        XCTAssertFalse(result)
+    }
+
+    func test_reloadRoles_relays_error_if_the_fetch_fails() async {
+        // Given
+        let expectedError = NSError(domain: "Test", code: 500, userInfo: nil)
+        let stores = MockStoresManager(sessionManager: .makeForTesting(authenticated: true, isWPCom: false))
+        stores.whenReceivingAction(ofType: UserAction.self) { action in
+            switch action {
+            case .retrieveUser(_, let onCompletion):
+                onCompletion(.failure(expectedError))
+            }
+        }
+        let viewModel = AdminRoleRequiredViewModel(siteID: 123, stores: stores)
+
+        // When
+        var caughtError: NSError?
+        do {
+            _ = try await viewModel.reloadRoles()
+        } catch {
+            caughtError = error as NSError
+        }
+
+        // Then
+        assertEqual(expectedError, caughtError)
     }
 }

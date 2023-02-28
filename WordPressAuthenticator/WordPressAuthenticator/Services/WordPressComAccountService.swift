0e80225a85525aad367212084f55aa1d32dd5846
Huong Do <hi.huongdo@gmail.com>
2023-02-28 15:06:50 +0700

    Make WordPressComAccountService public along with 2 of its method to reuse in WCiOS

--- WordPressAuthenticator/WordPressAuthenticator/Services/WordPressComAccountService.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Services/WordPressComAccountService.swift
@@ -3,11 +3,11 @@ import WordPressKit
 
 // MARK: - WordPressComAccountService
 //
-class WordPressComAccountService {
+public class WordPressComAccountService {
 
     /// Indicates if a WordPress.com account is "PasswordLess": This kind of account must be authenticated via a Magic Link.
     ///
-    func isPasswordlessAccount(username: String, success: @escaping (Bool) -> Void, failure: @escaping (Error) -> Void) {
+    public func isPasswordlessAccount(username: String, success: @escaping (Bool) -> Void, failure: @escaping (Error) -> Void) {
         let remote = AccountServiceRemoteREST(wordPressComRestApi: anonymousAPI)
 
         remote.isPasswordlessAccount(username, success: { isPasswordless in
@@ -44,7 +44,7 @@ class WordPressComAccountService {
 
     /// Requests a WordPress.com Authentication Link to be sent to the specified email address.
     ///
-    func requestAuthenticationLink(for email: String, jetpackLogin: Bool, success: @escaping () -> Void, failure: @escaping (Error) -> Void) {
+    public func requestAuthenticationLink(for email: String, jetpackLogin: Bool, success: @escaping () -> Void, failure: @escaping (Error) -> Void) {
         let remote = AccountServiceRemoteREST(wordPressComRestApi: anonymousAPI)
 
         remote.requestWPComAuthLink(forEmail: email,

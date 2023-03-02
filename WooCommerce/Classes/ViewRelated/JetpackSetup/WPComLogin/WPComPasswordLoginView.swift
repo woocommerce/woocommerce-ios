b605176002ff6a832ea3e295f06f7903b8c88095
Huong Do <hi.huongdo@gmail.com>
2023-03-02 17:17:38 +0700

    Add view for password login

---
+++ WooCommerce/Classes/ViewRelated/JetpackSetup/WPComLogin/WPComPasswordLoginView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+/// Screen for entering the password for a WPCom account during the Jetpack setup flow
+/// This is presented for users authenticated with WPOrg credentials.
+struct WPComPasswordLoginView: View {
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: Constants.blockVerticalPadding) {
+                JetpackInstallHeaderView()
+
+                // title
+                Text("")
+                    .largeTitleStyle()
+
+                Spacer()
+            }
+            .padding(Constants.contentPadding)
+        }
+    }
+}
+
+private extension WPComPasswordLoginView {
+    enum Constants {
+        static let blockVerticalPadding: CGFloat = 32
+        static let contentVerticalSpacing: CGFloat = 8
+        static let contentPadding: CGFloat = 16
+    }
+}
+
+struct WPComPasswordLoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        WPComPasswordLoginView()
+    }
+}

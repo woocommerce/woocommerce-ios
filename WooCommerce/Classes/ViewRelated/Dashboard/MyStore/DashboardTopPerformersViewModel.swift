31fecc278eb519aaf8a9d1db319a6f1e25f57818
Jaclyn Chen <watertranquil@gmail.com>
2023-03-02 11:01:29 +0800

    Create `TopPerformersEmptyView` with a view model to be shown in the dashboard.

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/MyStore/DashboardTopPerformersViewModel.swift
@@ -0,0 +1,61 @@
+import SwiftUI
+import struct Yosemite.TopEarnerStatsItem
+
+/// View model for `DashboardTopPerformersView`.
+final class DashboardTopPerformersViewModel: ObservableObject {
+    /// UI state of the dashboard top performers.
+    enum State {
+        /// Shows placeholder rows.
+        case loading
+        /// Shows either an empty view or a list of top performing rows.
+        case loaded(rows: [TopEarnerStatsItem])
+    }
+
+    @Published private(set) var isRedacted: Bool = false
+    @Published private(set) var rows: [TopPerformersRow.Data] = []
+
+    private var state: State
+    private var onTap: (_ item: TopEarnerStatsItem) -> Void
+
+    private let placeholderRows: [TopPerformersRow.Data] = [
+        .init(imageURL: nil, name: "Placeholder", details: "", value: "Value"),
+        .init(imageURL: nil, name: "Placeholder", details: "", value: "Value"),
+        .init(imageURL: nil, name: "Placeholder", details: "", value: "Value")
+    ]
+
+    init(state: State, onTap: @escaping (_ item: TopEarnerStatsItem) -> Void) {
+        self.state = state
+        self.onTap = onTap
+        update(state: state)
+    }
+
+    /// Updates the state based on the data loading status.
+    func update(state: State) {
+        switch state {
+        case .loading:
+            isRedacted = true
+            rows = placeholderRows
+        case .loaded(let items):
+            isRedacted = false
+            let rows = items.map { item in
+                TopPerformersRow.Data(imageURL: URL(string: item.imageUrl ?? ""),
+                                      name: item.productName ?? "",
+                                      details: Localization.netSales(value: item.totalString),
+                                      value: "\(item.quantity)",
+                                      tapHandler: { [weak self] in
+                    self?.onTap(item)
+                })
+            }
+            self.rows = rows
+        }
+    }
+}
+
+private extension DashboardTopPerformersViewModel {
+    enum Localization {
+        static func netSales(value: String) -> String {
+            String.localizedStringWithFormat(NSLocalizedString("Net sales: %@", comment: "Label for the total sales of a product in the Analytics Hub"),
+                                             value)
+        }
+    }
+}

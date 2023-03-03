e220738933ce44f671396da7718ed76648396939
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-03-03 17:36:36 +0530

    More string to `Localication` enum for `StoreOnboardingTaskViewModel`

--- WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingTaskViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingTaskViewModel.swift
@@ -17,44 +17,20 @@ struct StoreOnboardingTaskViewModel: Identifiable, Equatable {
         switch task.type {
         case .addFirstProduct:
             icon = .productImage
-            title = NSLocalizedString(
-                "Add your first product",
-                comment: "Title of the store onboarding task to add the first product."
-            )
-            subtitle = NSLocalizedString(
-                "Start selling by adding products or services to your store.",
-                comment: "Subtitle of the store onboarding task to add the first product."
-            )
+            title = Localication.AddFirstProduct.title
+            subtitle = Localication.AddFirstProduct.subtitle
         case .launchStore:
             icon = .launchStoreImage
-            title = NSLocalizedString(
-                "Launch your store",
-                comment: "Title of the store onboarding task to launch the store."
-            )
-            subtitle = NSLocalizedString(
-                "Publish your site to the world anytime you want!",
-                comment: "Subtitle of the store onboarding task to launch the store."
-            )
+            title = Localication.LaunchStore.title
+            subtitle = Localication.LaunchStore.subtitle
         case .customizeDomains:
             icon = .domainsImage
-            title = NSLocalizedString(
-                "Customize your domain",
-                comment: "Title of the store onboarding task to customize the store domain."
-            )
-            subtitle = NSLocalizedString(
-                "Have a custom URL to host your store.",
-                comment: "Subtitle of the store onboarding task to customize the store domain."
-            )
+            title = Localication.CustomizeDomains.title
+            subtitle = Localication.CustomizeDomains.subtitle
         case .payments:
             icon = .currencyImage
-            title = NSLocalizedString(
-                "Get paid",
-                comment: "Title of the store onboarding task to get paid."
-            )
-            subtitle = NSLocalizedString(
-                "Give your customers an easy and convenient way to pay!",
-                comment: "Subtitle of the store onboarding task to get paid."
-            )
+            title = Localication.Payments.title
+            subtitle = Localication.Payments.subtitle
         case .unsupported:
             icon = .checkCircleImage
             title = ""
@@ -65,5 +41,54 @@ struct StoreOnboardingTaskViewModel: Identifiable, Equatable {
     static func placeHolder() -> Self {
         .init(task: .init(isComplete: true,
                           type: .launchStore))
+    }
+}
+
+
+extension StoreOnboardingTaskViewModel {
+    enum Localication {
+        enum AddFirstProduct {
+            static let title = NSLocalizedString(
+                "Add your first product",
+                comment: "Title of the store onboarding task to add the first product."
+            )
+            static let subtitle = NSLocalizedString(
+                "Start selling by adding products or services to your store.",
+                comment: "Subtitle of the store onboarding task to add the first product."
+            )
+        }
+
+        enum LaunchStore {
+            static let title = NSLocalizedString(
+                "Launch your store",
+                comment: "Title of the store onboarding task to launch the store."
+            )
+            static let subtitle = NSLocalizedString(
+                "Publish your site to the world anytime you want!",
+                comment: "Subtitle of the store onboarding task to launch the store."
+            )
+        }
+
+        enum CustomizeDomains {
+            static let title = NSLocalizedString(
+                "Customize your domain",
+                comment: "Title of the store onboarding task to customize the store domain."
+            )
+            static let subtitle = NSLocalizedString(
+                "Have a custom URL to host your store.",
+                comment: "Subtitle of the store onboarding task to customize the store domain."
+            )
+        }
+
+        enum Payments {
+            static let title = NSLocalizedString(
+                "Get paid",
+                comment: "Title of the store onboarding task to get paid."
+            )
+            static let subtitle = NSLocalizedString(
+                "Give your customers an easy and convenient way to pay!",
+                comment: "Subtitle of the store onboarding task to get paid."
+            )
+        }
     }
 }

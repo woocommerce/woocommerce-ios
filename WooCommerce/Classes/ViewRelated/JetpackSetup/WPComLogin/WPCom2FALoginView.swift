3cacfb237f6491d28f22d6f6cc7cc5719e3c1b0a
Huong Do <hi.huongdo@gmail.com>
2023-03-03 12:01:28 +0700

    Update UI for 2FA login screen

--- WooCommerce/Classes/ViewRelated/JetpackSetup/WPComLogin/WPCom2FALoginView.swift
+++ WooCommerce/Classes/ViewRelated/JetpackSetup/WPComLogin/WPCom2FALoginView.swift
@@ -2,13 +2,113 @@ import SwiftUI
 
 /// View for 2FA login screen of the custom WPCom login flow for Jetpack setup.
 struct WPCom2FALoginView: View {
+    @ObservedObject private var viewModel: WPCom2FALoginViewModel
+    @FocusState private var isFieldFocused: Bool
+    @State private var isPrimaryButtonLoading = false
+    @State private var isSMSRequestInProgress = false
+
+    /// The closure to be triggered when the Install Jetpack button is tapped.
+    private let onSubmit: (String) async -> Void
+
+    /// The closure to be triggered when the Text me a code button is tapped.
+    private let onSMSRequest: () async -> Void
+
+    init(viewModel: WPCom2FALoginViewModel,
+         onSubmit: @escaping (String) async -> Void,
+         onSMSRequest: @escaping () async -> Void) {
+        self.viewModel = viewModel
+        self.onSubmit = onSubmit
+        self.onSMSRequest = onSMSRequest
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            VStack(alignment: .leading, spacing: Constants.blockVerticalPadding) {
+                JetpackInstallHeaderView()
+
+                // title and description
+                VStack(alignment: .leading, spacing: Constants.contentVerticalSpacing) {
+                    Text(viewModel.titleString)
+                        .largeTitleStyle()
+                    Text(Localization.subtitleString)
+                        .subheadlineStyle()
+                }
+
+                // Verification field
+                AccountCreationFormFieldView(viewModel: .init(
+                    header: "",
+                    placeholder: Localization.verificationCode,
+                    keyboardType: .asciiCapableNumberPad,
+                    text: $viewModel.verificationCode,
+                    isSecure: true,
+                    errorMessage: nil,
+                    isFocused: isFieldFocused
+                ))
+                .focused($isFieldFocused)
+
+                Button(action: {
+                    Task { @MainActor in
+                        isSMSRequestInProgress = true
+                        await onSMSRequest()
+                        isSMSRequestInProgress = false
+                    }
+                }, label: {
+                    if isSMSRequestInProgress {
+                        ActivityIndicator(isAnimating: .constant(true), style: .medium)
+                    } else {
+                        Text(Localization.textMeACode)
+                            .linkStyle()
+                    }
+                })
+                Spacer()
+            }
+            .padding(Constants.contentPadding)
+        }
+        .safeAreaInset(edge: .bottom) {
+            VStack {
+                // Primary CTA
+                Button(viewModel.titleString) {
+                    Task { @MainActor in
+                        isPrimaryButtonLoading = true
+                        await onSubmit(viewModel.verificationCode)
+                        isPrimaryButtonLoading = false
+                    }
+                }
+                .buttonStyle(PrimaryLoadingButtonStyle(isLoading: isPrimaryButtonLoading))
+                .disabled(viewModel.verificationCode.isEmpty)
+            }
+            .padding(Constants.contentPadding)
+            .background(Color(uiColor: .systemBackground))
+        }
+    }
+}
+
+private extension WPCom2FALoginView {
+    enum Constants {
+        static let blockVerticalPadding: CGFloat = 32
+        static let contentVerticalSpacing: CGFloat = 8
+        static let contentPadding: CGFloat = 16
+    }
+
+    enum Localization {
+        static let subtitleString = NSLocalizedString(
+            "Almost there! Please enter the verification code from your Authentication app",
+            comment: "Instruction on the WPCom 2FA login screen of the Jetpack setup flow")
+        static let verificationCode = NSLocalizedString(
+            "Verification code",
+            comment: "Placeholder for the 2FA code field on the WPCom 2FA login screen of the Jetpack setup flow."
+        )
+        static let textMeACode = NSLocalizedString(
+            "Text me a code instead",
+            comment: "Button to request 2FA code via SMS on the WPCom 2FA login screen of the Jetpack setup flow."
+        )
     }
 }
 
 struct WPCom2FALoginView_Previews: PreviewProvider {
     static var previews: some View {
-        WPCom2FALoginView()
+        WPCom2FALoginView(viewModel: .init(requiresConnectionOnly: true),
+                          onSubmit: { _ in },
+                           onSMSRequest: {})
     }
 }
--- WooCommerce/Classes/ViewRelated/JetpackSetup/WPComLogin/WPCom2FALoginViewModel.swift
+++ WooCommerce/Classes/ViewRelated/JetpackSetup/WPComLogin/WPCom2FALoginViewModel.swift
@@ -2,7 +2,25 @@ import Foundation
 
 /// View model for `WPCom2FALoginView`.
 final class WPCom2FALoginViewModel: ObservableObject {
-    @Published var otp: String
+    @Published var verificationCode: String = ""
 
-    init(requiresConnectionOnly: Bool) {}
+    /// Title for the view
+    let titleString: String
+
+    init(requiresConnectionOnly: Bool) {
+        self.titleString = requiresConnectionOnly ? Localization.connectJetpack : Localization.installJetpack
+    }
+}
+
+extension WPCom2FALoginViewModel {
+    enum Localization {
+        static let installJetpack = NSLocalizedString(
+            "Install Jetpack",
+            comment: "Title for the WPCom 2FA login screen when Jetpack is not installed yet"
+        )
+        static let connectJetpack = NSLocalizedString(
+            "Connect Jetpack",
+            comment: "Title for the WPCom 2FA login screen when Jetpack is not connected yet"
+        )
+    }
 }

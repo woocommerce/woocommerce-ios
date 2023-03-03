e543a5e22806a4b022d83a160af14dba93bb0b9d
Huong Do <hi.huongdo@gmail.com>
2023-03-03 15:41:41 +0700

    Add tests for 2FA code validation

--- WooCommerce/WooCommerceTests/ViewRelated/JetpackSetup/WPComLogin/WPCom2FALoginViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/JetpackSetup/WPComLogin/WPCom2FALoginViewModelTests.swift
@@ -25,4 +25,58 @@ final class WPCom2FALoginViewModelTests: XCTestCase {
         assertEqual(WPCom2FALoginViewModel.Localization.connectJetpack, text)
     }
 
+    func test_strippedCode_removes_all_white_spaces_from_verification_code() {
+        // Given
+        let viewModel = WPCom2FALoginViewModel(requiresConnectionOnly: true)
+
+        // When
+        viewModel.verificationCode = "43 99 92"
+
+        // Then
+        assertEqual("439992", "439992")
+    }
+
+    func test_isValidCode_returns_false_when_verification_code_contains_non_digits() {
+        // Given
+        let viewModel = WPCom2FALoginViewModel(requiresConnectionOnly: true)
+
+        // When
+        viewModel.verificationCode = "43gd35"
+
+        //  Then
+        XCTAssertFalse(viewModel.isValidCode)
+    }
+
+    func test_isValidCode_returns_false_when_verification_code_is_empty() {
+        // Given
+        let viewModel = WPCom2FALoginViewModel(requiresConnectionOnly: true)
+
+        // When
+        viewModel.verificationCode = ""
+
+        //  Then
+        XCTAssertFalse(viewModel.isValidCode)
+    }
+
+    func test_isValidCode_returns_false_when_verification_code_is_too_long() {
+        // Given
+        let viewModel = WPCom2FALoginViewModel(requiresConnectionOnly: true)
+
+        // When
+        viewModel.verificationCode = "185787878"
+
+        //  Then
+        XCTAssertFalse(viewModel.isValidCode)
+    }
+
+    func test_isValidCode_returns_true_when_verification_has_acceptable_length() {
+        // Given
+        let viewModel = WPCom2FALoginViewModel(requiresConnectionOnly: true)
+
+        // When
+        viewModel.verificationCode = "185787"
+
+        //  Then
+        XCTAssertTrue(viewModel.isValidCode)
+    }
 }

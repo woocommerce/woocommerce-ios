8b2cb132ec2709e87335519c43df6c2b93b0b35f
Josh Heald <joshheald@users.noreply.github.com>
2023-03-09 11:01:16 +0000

    Open capture screen from AR menu

--- WooCommerce/Classes/ViewRelated/AugmentedReality/AugmentedRealityMenu.swift
+++ WooCommerce/Classes/ViewRelated/AugmentedReality/AugmentedRealityMenu.swift
@@ -1,15 +1,26 @@
 import SwiftUI
 
 struct AugmentedRealityMenu: View {
+    private let cameraViewModel = CameraViewModel()
+    @State var showCameraView = false
     var body: some View {
         List {
-            Text("Capture images")
+            Button {
+                showCameraView = true
+            } label: {
+                Text("Capture images")
+            }
+
+
             if #available(macCatalyst 16.0, *) {
                 NavigationLink(destination: AugmentedRealityCreateUSDZ()) {
                     Text("Create USDZ files")
                 }
             }
         }
+        .sheet(isPresented: $showCameraView, content: {
+            ContentView(model: cameraViewModel)
+        })
     }
 }
 

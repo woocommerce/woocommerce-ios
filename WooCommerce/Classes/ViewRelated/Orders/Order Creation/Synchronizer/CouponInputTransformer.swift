6d08649d901bbbc310debe59edff24b40294a409
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-03-09 10:12:28 +0530

    Add transformer to update order with coupon line.

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/Synchronizer/CouponInputTransformer.swift
@@ -0,0 +1,32 @@
+import Foundation
+import Yosemite
+
+/// Helper to update an `order` given an `OrderCouponLine` input type.
+///
+struct CouponInputTransformer {
+
+    /// Adds, deletes, or updates a coupon line input into an existing order.
+    ///
+    static func update(input: OrderCouponLine?, on order: Order) -> Order {
+        // If input is `nil`, then we remove the first existing coupon line.
+        guard let input = input else {
+            let updatedLines = order.coupons.enumerated().map { index, line -> OrderCouponLine? in
+                if index == 0 {
+                    return nil
+                }
+                return OrderFactory.newOrderCouponLine(code: line.code)
+            }.compactMap({ $0 })
+            return order.copy(coupons: updatedLines)
+        }
+
+        // If there is no existing coupon lines, we insert the input one.
+        guard order.coupons.count > 0 else {
+            return order.copy(coupons: [input])
+        }
+
+        // If there are existing coupon lines replace the first coupon line
+        var updatedCodes = order.coupons.map({ $0.code })
+        updatedCodes[0] = input.code
+        return order.copy(coupons: updatedCodes.map({ OrderFactory.newOrderCouponLine(code: $0) }))
+    }
+}

81cbe0dbdf710e0ea4cab05488e41abd9c3fe00f
Ernesto Carrión <ernesto.carrion@automattic.com>
2023-03-14 17:32:09 -0500

    Initial Subscription fetch on the dashboard

--- WooCommerce/Classes/ViewRelated/Dashboard/DashboardViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/DashboardViewModel.swift
@@ -159,6 +159,21 @@ final class DashboardViewModel {
         }
     }
 
+    /// Fetches the current site plan.
+    ///
+    func syncFreeTrialBanner(siteID: Int64) {
+        let action = PaymentAction.loadSiteCurrentPlan(siteID: siteID) { [weak self] result in
+            //guard let self = self else { return }
+            switch result {
+            case .success(let plan):
+                print(plan)
+            case .failure(let error):
+                DDLogError("⛔️ Error fetching the current site's plan information: \(error)")
+            }
+        }
+        stores.dispatch(action)
+    }
+
     /// Checks if a store is eligible for products onboarding -returning error otherwise- and prepares the onboarding announcement if needed.
     ///
     private func syncProductsOnboarding(for siteID: Int64) async throws {

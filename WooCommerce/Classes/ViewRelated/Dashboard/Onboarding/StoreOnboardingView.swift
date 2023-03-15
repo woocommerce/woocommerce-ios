2dc0343c431d56c3b95c86e8747dbdb042ad9316
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-03-15 16:49:47 +0530

    Refactor tasks count check for readability.

--- WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingView.swift
@@ -143,7 +143,7 @@ struct StoreOnboardingView: View {
 
                 // View all button
                 viewAllButton(action: viewAllTapped, text: String(format: Localization.viewAll, viewModel.taskViewModels.count))
-                    .renderedIf(!viewModel.isExpanded && !viewModel.isRedacted && viewModel.taskViewModels.count > 2)
+                    .renderedIf(!viewModel.isExpanded && !viewModel.isRedacted && !(viewModel.taskViewModels.count < 3))
 
                 Spacer()
                     .renderedIf(viewModel.isExpanded)

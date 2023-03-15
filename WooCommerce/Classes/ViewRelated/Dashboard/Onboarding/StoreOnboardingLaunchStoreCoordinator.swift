b47cd70d29aab8a907ea13be8d715f2bf371b46c
Jaclyn Chen <watertranquil@gmail.com>
2023-03-15 13:09:52 +0800

    Integrate launch site action to the UI and show an alert on error.

--- WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingLaunchStoreCoordinator.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingLaunchStoreCoordinator.swift
@@ -1,18 +1,31 @@
 import UIKit
 import SwiftUI
+import struct Yosemite.Site
 
 /// Coordinates navigation of the launch store action from store onboarding.
 final class StoreOnboardingLaunchStoreCoordinator: Coordinator {
     let navigationController: UINavigationController
-    private let siteURL: URL
+    private let site: Site
 
-    init(siteURL: URL, navigationController: UINavigationController) {
-        self.siteURL = siteURL
+    init(site: Site, navigationController: UINavigationController) {
+        self.site = site
         self.navigationController = navigationController
     }
 
     func start() {
-        let launchStoreController = StoreOnboardingLaunchStoreHostingController(viewModel: .init(siteURL: siteURL))
+        guard let siteURL = URL(string: site.url) else {
+            assertionFailure("The site does not have a valid URL to launch from store onboarding: \(site).")
+            return
+        }
+        let launchStoreController = StoreOnboardingLaunchStoreHostingController(viewModel: .init(siteURL: siteURL, siteID: site.siteID) { [weak self] in
+            self?.showLaunchedView()
+        })
         navigationController.present(WooNavigationController(rootViewController: launchStoreController), animated: true)
+    }
+}
+
+private extension StoreOnboardingLaunchStoreCoordinator {
+    func showLaunchedView() {
+        print("🚀 site launched")
     }
 }

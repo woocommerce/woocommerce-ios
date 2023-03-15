a65cce7f7471f896c0cf173caebf2566df67d198
Jaclyn Chen <watertranquil@gmail.com>
2023-03-15 11:41:19 +0800

    Yosemite layer changes for launching a site.

--- Yosemite/YosemiteTests/Mocks/Networking/Remote/MockSiteRemote.swift
+++ Yosemite/YosemiteTests/Mocks/Networking/Remote/MockSiteRemote.swift
@@ -7,9 +7,17 @@ final class MockSiteRemote {
     /// The results to return in `createSite`.
     private var createSiteResult: Result<SiteCreationResponse, Error>?
 
+    /// The results to return in `launchSite`.
+    private var launchSiteResult: Result<Void, Error>?
+
     /// Returns the value when `createSite` is called.
     func whenCreatingSite(thenReturn result: Result<SiteCreationResponse, Error>) {
         createSiteResult = result
+    }
+
+    /// Returns the value when `launchSite` is called.
+    func whenLaunchingSite(thenReturn result: Result<Void, Error>) {
+        launchSiteResult = result
     }
 }
 
@@ -17,6 +25,15 @@ extension MockSiteRemote: SiteRemoteProtocol {
     func createSite(name: String, domain: String) async throws -> SiteCreationResponse {
         guard let result = createSiteResult else {
             XCTFail("Could not find result for creating a site.")
+            throw NetworkError.notFound
+        }
+
+        return try result.get()
+    }
+
+    func launchSite(siteID: Int64) async throws {
+        guard let result = launchSiteResult else {
+            XCTFail("Could not find result for launching a site.")
             throw NetworkError.notFound
         }
 

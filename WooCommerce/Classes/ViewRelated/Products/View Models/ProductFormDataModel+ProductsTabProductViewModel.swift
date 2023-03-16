033688ebb15a31141a995d8f66469ef1c62780ef
rachelmcr <rachel@automattic.com>
2023-03-16 13:05:14 +0000

    Fix product bundle stock label when manageStock is false

--- WooCommerce/Classes/ViewRelated/Products/View Models/ProductFormDataModel+ProductsTabProductViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/View Models/ProductFormDataModel+ProductsTabProductViewModel.swift
@@ -13,13 +13,28 @@ extension ProductFormDataModel {
                 return self.stockStatus
             }
         }()
-        // When feature flag is enabled: If product is a product bundle with a bundle stock quantity, use that as the product stock quantity.
+        // When feature flag is enabled: If product is a product bundle, use the bundle stock quantity.
         let stockQuantity: Decimal? = {
             switch (productBundlesEnabled, productType, bundleStockQuantity) {
             case (true, .bundle, .some(let bundleStockQuantity)):
                 return Decimal(bundleStockQuantity)
+            case (true, .bundle, nil):
+                return nil
             default:
                 return self.stockQuantity
+            }
+        }()
+        // When feature flag is enabled:
+        // If product is a product bundle, use the bundle stock quantity to set the `manageStock` setting (override the product-level setting).
+        // Even if the product's `manageStock` setting is false, we want to show the bundle stock quantity when there is one.
+        let manageStock: Bool = {
+            switch (productBundlesEnabled, productType, bundleStockQuantity) {
+            case (true, .bundle, .some):
+                return true
+            case (true, .bundle, nil):
+                return false
+            default:
+                return self.manageStock
             }
         }()
 

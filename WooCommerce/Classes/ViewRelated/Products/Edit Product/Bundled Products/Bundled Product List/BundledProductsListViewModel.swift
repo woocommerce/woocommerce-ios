f8ea8c2b03a3cc765e9fa833c7d8fa0a84a75bde
rachelmcr <rachel@automattic.com>
2023-03-16 18:37:49 +0000

    Sync bundled products from remote if any product images are missing

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Bundled Products/Bundled Product List/BundledProductsListViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Bundled Products/Bundled Product List/BundledProductsListViewModel.swift
@@ -9,8 +9,11 @@ final class BundledProductsListViewModel {
     /// Represents a bundled product
     ///
     struct BundledProduct: Identifiable {
-        /// Bundled product ID
+        /// ID of the bundled product (item ID unique to the bundled product list)
         let id: Int64
+
+        /// Product ID of the bundled product
+        let productID: Int64
 
         /// Title of the bundled product
         let title: String
@@ -32,7 +35,7 @@ final class BundledProductsListViewModel {
 
     /// Bundled products
     ///
-    let bundledProducts: [BundledProduct]
+    var bundledProducts: [BundledProduct]
 
     init(bundledProducts: [BundledProduct]) {
         self.bundledProducts = bundledProducts
@@ -42,19 +45,33 @@ final class BundledProductsListViewModel {
 // MARK: Initializers
 extension BundledProductsListViewModel {
     convenience init(siteID: Int64, bundledProducts: [Yosemite.ProductBundleItem], storageManager: StorageManagerType = ServiceLocator.storageManager) {
-        let products = BundledProductsListViewModel.fetchProducts(for: siteID, including: bundledProducts.map { $0.productID }, storageManager: storageManager)
-        let viewModels = bundledProducts.map { bundledProduct in
-            BundledProduct(id: bundledProduct.bundledItemID,
-                           title: bundledProduct.title,
-                           stockStatus: bundledProduct.stockStatus.description,
-                           imageURL: products.first(where: { $0.productID == bundledProduct.productID })?.imageURL) // URL for bundled product's first image
-        }
+        let viewModels = BundledProductsListViewModel.getViewModels(for: bundledProducts, siteID: siteID, storageManager: storageManager)
         self.init(bundledProducts: viewModels)
+
+        // Re-sync bundled products to get product images if needed
+        synchronizeBundledProductsIfNeeded(for: siteID) { [weak self] in
+            self?.bundledProducts = BundledProductsListViewModel.getViewModels(for: bundledProducts, siteID: siteID, storageManager: storageManager)
+        }
     }
 }
 
 // MARK: Private helpers
 private extension BundledProductsListViewModel {
+    /// Creates `BundledProduct` view models for the provided Product Bundle Items
+    ///
+    static func getViewModels(for bundleItems: [ProductBundleItem], siteID: Int64, storageManager: StorageManagerType) -> [BundledProduct] {
+        let products = fetchProducts(for: siteID, including: bundleItems.map { $0.productID }, storageManager: storageManager)
+        return bundleItems.map { bundleItem in
+            BundledProduct(id: bundleItem.bundledItemID,
+                           productID: bundleItem.productID,
+                           title: bundleItem.title,
+                           stockStatus: bundleItem.stockStatus.description,
+                           imageURL: products.first(where: { $0.productID == bundleItem.productID })?.imageURL) // URL for bundle item's first image
+        }
+    }
+
+    /// Fetches the provided product IDs from storage
+    ///
     static func fetchProducts(for siteID: Int64, including productIDs: [Int64], storageManager: StorageManagerType) -> [Product] {
         let predicate = NSPredicate(format: "siteID == %lld AND productID IN %@", siteID, productIDs)
         let controller = ResultsController<StorageProduct>(storageManager: storageManager, matching: predicate, sortedBy: [])
@@ -66,6 +83,26 @@ private extension BundledProductsListViewModel {
         }
 
         return controller.fetchedObjects
+    }
+
+    /// Synchronizes the products matching the provided product IDs, to retrieve their product images
+    ///
+    func synchronizeBundledProductsIfNeeded(for siteID: Int64, onCompletion: @escaping () -> Void) {
+        // We only need to sync if the bundled products are missing images
+        guard bundledProducts.filter({ $0.imageURL == nil }).isNotEmpty else { return }
+
+        let action = ProductAction.retrieveProducts(siteID: siteID, productIDs: bundledProducts.map { $0.productID }) { result in
+            switch result {
+            case .success:
+                break
+            case .failure(let error):
+                DDLogError("⛔️ Error synchronizing products: \(error)")
+            }
+
+            onCompletion()
+        }
+
+        ServiceLocator.stores.dispatch(action)
     }
 }
 

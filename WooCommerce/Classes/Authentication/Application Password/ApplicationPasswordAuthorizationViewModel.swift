5aa2f2842e78acd1e83ce1c8f0bef19a20a0f621
Huong Do <hi.huongdo@gmail.com>
2023-03-17 11:02:18 +0700

    Inject site url and stores to app password auth view model

--- WooCommerce/Classes/Authentication/Application Password/ApplicationPasswordAuthorizationViewModel.swift
+++ WooCommerce/Classes/Authentication/Application Password/ApplicationPasswordAuthorizationViewModel.swift
@@ -1,7 +1,15 @@
 import Foundation
+import Yosemite
 
 /// View model for `ApplicationPasswordAuthorizationWebViewController`.
 ///
 final class ApplicationPasswordAuthorizationViewModel {
+    private let siteURL: String
+    private let stores: StoresManager
 
+    init(siteURL: String,
+         stores: StoresManager = ServiceLocator.stores) {
+        self.siteURL = siteURL
+        self.stores = stores
+    }
 }

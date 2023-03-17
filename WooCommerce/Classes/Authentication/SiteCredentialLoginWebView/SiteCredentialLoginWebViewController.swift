f7c500eda30bd4f3b24ee015fe0ba798ebb20f0d
Huong Do <hi.huongdo@gmail.com>
2023-03-17 10:51:12 +0700

    Add new web view for app password auth

---
+++ WooCommerce/Classes/Authentication/SiteCredentialLoginWebView/SiteCredentialLoginWebViewController.swift
@@ -0,0 +1,36 @@
+import UIKit
+import WebKit
+
+/// View with embedded web view to authorize application password for a site.
+///
+final class SiteCredentialLoginWebViewController: UIViewController {
+
+    /// Main web view
+    private lazy var webView: WKWebView = {
+        let webView = WKWebView(frame: .zero)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        return webView
+    }()
+
+    /// Progress bar for the web view
+    private lazy var progressBar: UIProgressView = {
+        let bar = UIProgressView(progressViewStyle: .bar)
+        bar.translatesAutoresizingMaskIntoConstraints = false
+        return bar
+    }()
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

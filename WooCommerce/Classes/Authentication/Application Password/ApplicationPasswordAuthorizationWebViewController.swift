8aac9e4883001c961f8e733d694b8e2122315f42
Huong Do <hi.huongdo@gmail.com>
2023-03-17 11:20:22 +0700

    Configure web view for app password auth

--- WooCommerce/Classes/Authentication/Application Password/ApplicationPasswordAuthorizationWebViewController.swift
+++ WooCommerce/Classes/Authentication/Application Password/ApplicationPasswordAuthorizationWebViewController.swift
@@ -1,5 +1,7 @@
+import Combine
 import UIKit
 import WebKit
+import struct Networking.ApplicationPassword
 
 /// View with embedded web view to authorize application password for a site.
 ///
@@ -20,9 +22,16 @@ final class ApplicationPasswordAuthorizationWebViewController: UIViewController 
     }()
 
     private let viewModel: ApplicationPasswordAuthorizationViewModel
+    private let successHandler: (ApplicationPassword, UINavigationController?) -> Void
+    private let failureHandler: () -> Void
+    private var subscriptions: Set<AnyCancellable> = []
 
-    init(viewModel: ApplicationPasswordAuthorizationViewModel) {
+    init(viewModel: ApplicationPasswordAuthorizationViewModel,
+         onSuccess: @escaping (ApplicationPassword, UINavigationController?) -> Void,
+         onFailure: @escaping () -> Void) {
         self.viewModel = viewModel
+        self.successHandler = onSuccess
+        self.failureHandler = onFailure
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -32,8 +41,73 @@ final class ApplicationPasswordAuthorizationWebViewController: UIViewController 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigationBar()
+        configureWebView()
+        configureProgressBar()
+    }
+}
 
-        // Do any additional setup after loading the view.
+private extension ApplicationPasswordAuthorizationWebViewController {
+    func configureNavigationBar() {
+        title = Localization.authorizeAppPassword
     }
 
+    func configureWebView() {
+        view.addSubview(webView)
+        NSLayoutConstraint.activate([
+            view.leadingAnchor.constraint(equalTo: webView.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: webView.trailingAnchor),
+            view.safeTopAnchor.constraint(equalTo: webView.topAnchor),
+            view.safeBottomAnchor.constraint(equalTo: webView.bottomAnchor),
+        ])
+
+        extendContentUnderSafeAreas()
+        webView.configureForSandboxEnvironment()
+
+        webView.publisher(for: \.estimatedProgress)
+            .sink { [weak self] progress in
+                if progress == 1 {
+                    self?.progressBar.setProgress(0, animated: false)
+                } else {
+                    self?.progressBar.setProgress(Float(progress), animated: true)
+                }
+            }
+            .store(in: &subscriptions)
+
+        webView.publisher(for: \.url)
+            .sink { [weak self] url in
+                guard let url, url.absoluteString.hasPrefix(Constants.successURL) else {
+                    return
+                }
+                self?.handleAuthorizationResponse(with: url)
+            }
+            .store(in: &subscriptions)
+    }
+
+    func extendContentUnderSafeAreas() {
+        webView.scrollView.clipsToBounds = false
+        view.backgroundColor = webView.underPageBackgroundColor
+    }
+
+    func configureProgressBar() {
+        view.addSubview(progressBar)
+        NSLayoutConstraint.activate([
+            view.leadingAnchor.constraint(equalTo: progressBar.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: progressBar.trailingAnchor),
+            view.safeTopAnchor.constraint(equalTo: progressBar.topAnchor)
+        ])
+    }
+
+    func handleAuthorizationResponse(with url: URL) {
+        // TODO
+    }
+}
+
+private extension ApplicationPasswordAuthorizationWebViewController {
+    enum Constants {
+        static let successURL = "woocommerce://application-password"
+    }
+    enum Localization {
+        static let authorizeAppPassword = "Authorize application password"
+    }
 }

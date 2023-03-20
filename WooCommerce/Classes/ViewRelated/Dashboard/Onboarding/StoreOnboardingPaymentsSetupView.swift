1c02d392738a3fa98b7ce38c2639d627b96b5909
Jaclyn Chen <watertranquil@gmail.com>
2023-03-20 09:56:44 +0800

    Update payments onboarding navigation bar to be transparent.

--- WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingPaymentsSetupView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingPaymentsSetupView.swift
@@ -14,6 +14,12 @@ final class StoreOnboardingPaymentsSetupHostingController: UIHostingController<S
     required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureTransparentNavigationBar()
+    }
 }
 
 /// Shows an overview of the payments setup with a CTA to set up payments in a webview.

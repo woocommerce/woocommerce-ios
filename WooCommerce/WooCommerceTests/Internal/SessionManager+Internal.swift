8f0ef43a14ce2b1d91b719631f07399c1233e523
Huong Do <hi.huongdo@gmail.com>
2023-03-22 09:28:32 +0700

    Update test for isAuthenticatedWithoutWPCom

--- WooCommerce/WooCommerceTests/Internal/SessionManager+Internal.swift
+++ WooCommerce/WooCommerceTests/Internal/SessionManager+Internal.swift
@@ -44,6 +44,7 @@ extension SessionManagerProtocol {
 enum SessionSettings {
     static let wpcomCredentials = Credentials(username: "username", authToken: "authToken", siteAddress: "siteAddress")
     static let wporgCredentials = Credentials(username: "username", password: "password", siteAddress: "siteAddress")
+    static let applicationPasswordCredentials = Credentials.applicationPassword(username: "username", password: "password", siteAddress: "siteAddress")
     static let defaults = UserDefaults(suiteName: "storesManagerTests")!
     static let keychainServiceName = "com.woocommerce.storesmanagertests"
 }

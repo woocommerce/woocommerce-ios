2774e4235cf074fa20ef22182edd632d61f2fe23
Gio Lodi <gio@mokacoding.com>
2023-03-27 22:44:13 +1100

    Expose `name` property in `IDToken`

--- WordPressAuthenticator/WordPressAuthenticatorTests/GoogleSignIn/JSONWebToken+Fixtures.swift
+++ WordPressAuthenticator/WordPressAuthenticatorTests/GoogleSignIn/JSONWebToken+Fixtures.swift
@@ -26,7 +26,33 @@ extension JSONWebToken {
     // }
     private(set) static var validJWTStringWithEmailOnly = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJ2YWx1ZSIsImVtYWlsIjoidGVzdEBlbWFpbC5jb20ifQ.b-2oTvjpc_qHM5dU6akk_ESe3eWUZwL21pvTsCmW2gE"
 
-    // For convenience, this exposes the email value used in validJWTStringWithEmail.
+    // Created with https://jwt.io/ with input:
+    //
+    // header: {
+    //   "alg": "HS256",
+    //   "typ": "JWT"
+    // }
+    // payload: {
+    //   "name": "John Doe",
+    //   "key": "value"
+    // }
+    private(set) static var validJWTStringWithNameOnly = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJrZXkiOiJ2YWx1ZSJ9.P7Se5_EMlFBg5q8PV4C2IQ1YojTTSgitCBX7FgmXZzs"
+
+    // Created with https://jwt.io/ with input:
+    //
+    // header: {
+    //   "alg": "HS256",
+    //   "typ": "JWT"
+    // }
+    // payload: {
+    //   "name": "John Doe",
+    //   "key": "value",
+    //   "email": "test@email.com"
+    // }
+    private(set) static var validJWTStringWithNameAndEmail = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJrZXkiOiJ2YWx1ZSIsImVtYWlsIjoidGVzdEBlbWFpbC5jb20ifQ.-xzg0r5mMnSZ8hE3hk7S93iCZHhOez1QFYdheSmDlx4"
+
+    // For convenience, this exposes the email and name value used in the fixtures.
     // This allows us to use raw strings in tests, rather than having to implement encoding the JWT from an arbitrary string.
     private(set) static var emailFromValidJWTStringWithEmail = "test@email.com"
+    private(set) static var nameFromValidJWTStringWithEmail = "John Doe"
 }

b4cbc7af6d0f3896860c606fc4502a1ecb249873
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-03-29 14:05:27 +0530

    Check eligibility to publish store by fetching site plan and checking for free trial.

--- WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingLaunchStoreViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingLaunchStoreViewModel.swift
@@ -7,19 +7,31 @@ final class StoreOnboardingLaunchStoreViewModel: ObservableObject {
 
     @Published private(set) var isLaunchingStore: Bool = false
     @Published var error: SiteLaunchError?
+    @Published private(set) var canPublishStore: Bool = false
 
     private let siteID: Int64
     private let stores: StoresManager
     private let onLaunch: () -> Void
 
+    /// Closure invoked when the merchants taps on the `Upgrade` button.
+    ///
+    private let onUpgradeTapped: () -> Void
+
     init(siteURL: URL,
          siteID: Int64,
          stores: StoresManager = ServiceLocator.stores,
-         onLaunch: @escaping () -> Void) {
+         onLaunch: @escaping () -> Void,
+         onUpgradeTapped: @escaping () -> Void) {
         self.siteURL = siteURL
         self.siteID = siteID
         self.stores = stores
         self.onLaunch = onLaunch
+        self.onUpgradeTapped = onUpgradeTapped
+    }
+
+    @MainActor
+    func checkEligibilityToPublishStore() async {
+        canPublishStore = !(await isFreeTrialPlan())
     }
 
     @MainActor
@@ -35,9 +47,35 @@ final class StoreOnboardingLaunchStoreViewModel: ObservableObject {
         }
         isLaunchingStore = false
     }
+
+    @MainActor
+    func didTapUpgrade() {
+        onUpgradeTapped()
+    }
 }
 
 private extension StoreOnboardingLaunchStoreViewModel {
+    @MainActor
+    func isFreeTrialPlan() async -> Bool {
+        // Only fetch free trial information if the site is a WPCom site.
+        guard stores.sessionManager.defaultSite?.isWordPressComStore == true else {
+            return false
+        }
+
+        return await withCheckedContinuation({ continuation in
+            let action = PaymentAction.loadSiteCurrentPlan(siteID: siteID) { result in
+                switch result {
+                case .success(let plan):
+                    return continuation.resume(returning: plan.isFreeTrial)
+                case .failure(let error):
+                    DDLogError("⛔️ Error fetching the current site's plan information: \(error)")
+                    return continuation.resume(returning: false)
+                }
+            }
+            stores.dispatch(action)
+        })
+    }
+
     @MainActor
     func launchStoreRemotely() async -> Result<Void, SiteLaunchError> {
         await withCheckedContinuation { continuation in
--- WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Onboarding/StoreOnboardingLaunchStoreViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Onboarding/StoreOnboardingLaunchStoreViewModelTests.swift
@@ -1,18 +1,22 @@
 import XCTest
-import Yosemite
 @testable import WooCommerce
+@testable import Yosemite
 
 final class StoreOnboardingLaunchStoreViewModelTests: XCTestCase {
     private let exampleURL: URL = WooConstants.URLs.privacy.asURL()
     private var stores: MockStoresManager!
+    private var sessionManager: SessionManager!
+    private let freeTrialID = "1052"
 
     override func setUp() {
         super.setUp()
-        stores = MockStoresManager(sessionManager: .makeForTesting())
+        sessionManager = .makeForTesting(authenticated: true)
+        stores = MockStoresManager(sessionManager: sessionManager)
     }
 
     override func tearDown() {
         stores = nil
+        sessionManager = nil
         super.tearDown()
     }
 
@@ -20,7 +24,7 @@ final class StoreOnboardingLaunchStoreViewModelTests: XCTestCase {
 
     func test_siteURL_is_set_from_init_value() throws {
         // Given
-        let viewModel = StoreOnboardingLaunchStoreViewModel(siteURL: exampleURL, siteID: 1, onLaunch: {})
+        let viewModel = StoreOnboardingLaunchStoreViewModel(siteURL: exampleURL, siteID: 1, onLaunch: {}, onUpgradeTapped: {})
 
         // Then
         XCTAssertEqual(viewModel.siteURL, exampleURL)
@@ -50,7 +54,8 @@ final class StoreOnboardingLaunchStoreViewModelTests: XCTestCase {
                                                                     onLaunch: {
                     // Then
                     promise(())
-                })
+                },
+                                                                    onUpgradeTapped: {})
                 await viewModel.launchStore()
             }
         }
@@ -68,7 +73,8 @@ final class StoreOnboardingLaunchStoreViewModelTests: XCTestCase {
         let viewModel = StoreOnboardingLaunchStoreViewModel(siteURL: self.exampleURL,
                                                             siteID: siteID,
                                                             stores: self.stores,
-                                                            onLaunch: {})
+                                                            onLaunch: {},
+                                                            onUpgradeTapped: {})
 
         // When
         await viewModel.launchStore()
@@ -76,4 +82,114 @@ final class StoreOnboardingLaunchStoreViewModelTests: XCTestCase {
         // Then
         XCTAssertEqual(viewModel.error, .alreadyLaunched)
     }
+
+    // MARK: - `canPublishStore`
+
+    func test_canPublishStore_is_false_by_default() async throws {
+        // Given
+        let siteID: Int64 = 134
+        let sut = StoreOnboardingLaunchStoreViewModel(siteURL: exampleURL,
+                                                      siteID: siteID,
+                                                      stores: stores,
+                                                      onLaunch: {},
+                                                      onUpgradeTapped: {})
+
+        // Then
+        XCTAssertFalse(sut.canPublishStore)
+    }
+
+    func test_canPublishStore_is_false_for_WPCOM_site_under_free_trail() async {
+        // Given
+        sessionManager.defaultSite = .fake().copy(isWordPressComStore: true)
+        sessionManager.defaultRoles = [.administrator]
+
+        let siteID: Int64 = 134
+        let sitePlan = WPComSitePlan(id: self.freeTrialID,
+                                     hasDomainCredit: false,
+                                     expiryDate: Date().addingDays(14))
+        mockLoadSiteCurrentPlan(result: .success(sitePlan))
+
+        let sut = StoreOnboardingLaunchStoreViewModel(siteURL: exampleURL,
+                                                      siteID: siteID,
+                                                      stores: stores,
+                                                      onLaunch: {},
+                                                      onUpgradeTapped: {})
+        // When
+        await sut.checkEligibilityToPublishStore()
+
+        // Then
+        XCTAssertFalse(sut.canPublishStore)
+    }
+
+    func test_canPublishStore_is_true_for_non_WPCOM_site() async {
+        // Given
+        sessionManager.defaultSite = .fake().copy(isWordPressComStore: false)
+        sessionManager.defaultRoles = [.administrator]
+        let siteID: Int64 = 134
+
+        let sut = StoreOnboardingLaunchStoreViewModel(siteURL: exampleURL,
+                                                      siteID: siteID,
+                                                      stores: stores,
+                                                      onLaunch: {},
+                                                      onUpgradeTapped: {})
+        // When
+        await sut.checkEligibilityToPublishStore()
+
+        // Then
+        XCTAssertTrue(sut.canPublishStore)
+    }
+
+    func test_canPublishStore_is_true_for_WPCOM_site_not_under_free_trial() async {
+        // Given
+        sessionManager.defaultSite = .fake().copy(isWordPressComStore: false)
+        sessionManager.defaultRoles = [.administrator]
+        let siteID: Int64 = 134
+
+        let sitePlan = WPComSitePlan(hasDomainCredit: false)
+        mockLoadSiteCurrentPlan(result: .success(sitePlan))
+
+        let sut = StoreOnboardingLaunchStoreViewModel(siteURL: exampleURL,
+                                                      siteID: siteID,
+                                                      stores: stores,
+                                                      onLaunch: {},
+                                                      onUpgradeTapped: {})
+        // When
+        await sut.checkEligibilityToPublishStore()
+
+        // Then
+        XCTAssertTrue(sut.canPublishStore)
+    }
+
+    func test_canPublishStore_is_true_for_WPCOM_site_when_checking_site_plan_fails() async {
+        // Given
+        sessionManager.defaultSite = .fake().copy(isWordPressComStore: false)
+        sessionManager.defaultRoles = [.administrator]
+        let siteID: Int64 = 134
+
+        mockLoadSiteCurrentPlan(result: .failure(MockError()))
+
+        let sut = StoreOnboardingLaunchStoreViewModel(siteURL: exampleURL,
+                                                      siteID: siteID,
+                                                      stores: stores,
+                                                      onLaunch: {},
+                                                      onUpgradeTapped: {})
+        // When
+        await sut.checkEligibilityToPublishStore()
+
+        // Then
+        XCTAssertTrue(sut.canPublishStore)
+    }
+}
+
+private extension StoreOnboardingLaunchStoreViewModelTests {
+    func mockLoadSiteCurrentPlan(result: Result<WPComSitePlan, Error>) {
+        stores.whenReceivingAction(ofType: PaymentAction.self) { action in
+            guard case let .loadSiteCurrentPlan(_, completion) = action else {
+                return XCTFail()
+            }
+            completion(result)
+        }
+    }
+
+    final class MockError: Error { }
 }

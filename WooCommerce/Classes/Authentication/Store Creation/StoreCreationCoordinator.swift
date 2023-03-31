5df63fd2c9302e213b0ef27c03a779dd7bda6c4e
ThomazFB <thomazfcortez@gmail.com>
2023-03-31 07:44:56 -0300

    Fix hound issues

--- WooCommerce/Classes/Authentication/Store Creation/StoreCreationCoordinator.swift
+++ WooCommerce/Classes/Authentication/Store Creation/StoreCreationCoordinator.swift
@@ -654,7 +654,10 @@ private extension StoreCreationCoordinator {
                 /// Free trial stores should land directly on the dashboard and not show any success view.
                 ///
                 if self.isFreeTrialCreation {
-                    self.analytics.track(event: .StoreCreation.siteCreated(source: self.source.analyticsValue, siteURL: site.url, flow: .native, isFreeTrial: true))
+                    self.analytics.track(event: .StoreCreation.siteCreated(source: self.source.analyticsValue,
+                                                                           siteURL: site.url,
+                                                                           flow: .native,
+                                                                           isFreeTrial: true))
                     self.continueWithSelectedSite(site: site)
                 } else {
                     self.showSuccessView(from: navigationController, site: site)

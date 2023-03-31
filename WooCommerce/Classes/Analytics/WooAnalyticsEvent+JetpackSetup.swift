e91b7f78d392cd21f01078cdb9e0c131bb87f859
Huong Do <hi.huongdo@gmail.com>
2023-03-31 09:54:53 +0700

    Define tracking events for login flow and setup flow

---
+++ WooCommerce/Classes/Analytics/WooAnalyticsEvent+JetpackSetup.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+extension WooAnalyticsEvent {
+    enum JetpackSetup {
+        private enum Key: String {
+            case isAlreadyConnected = "is_already_connected"
+            case requiresConnectionOnly = "requires_connection_only"
+            case tap
+            case step
+        }
+
+        enum LoginFlow {
+            enum TapTarget: String {
+                case submit
+                case dismiss
+            }
+
+            enum Step: String {
+                case emailAddress = "email_address"
+                case password
+                case magicLink = "magic_link"
+                case verificationCode = "verification_code"
+            }
+        }
+
+        enum SetupFlow {
+            enum TapTarget: String {
+                case dismiss
+                case support
+                case continueSetup = "continue_setup"
+                case goToStore = "go_to_store"
+            }
+
+            enum Step: String {
+                case fetchPluginDetail = "fetch_plugin_detail"
+                case install
+                case activate
+                case fetchConnectionURL = "fetch_connection_url"
+                case checkConnection = "check_connection"
+            }
+        }
+
+        static func connectionCheckCompleted(isAlreadyConnected: Bool, requiresConnectionOnly: Bool) -> WooAnalyticsEvent {
+            .init(statName: .jetpackSetupConnectionCheckCompleted, properties: [
+                Key.isAlreadyConnected.rawValue: isAlreadyConnected,
+                Key.requiresConnectionOnly.rawValue: requiresConnectionOnly
+            ])
+        }
+
+        static func loginFlow(step: LoginFlow.Step, tap: LoginFlow.TapTarget? = nil, failure: Error? = nil) -> WooAnalyticsEvent {
+            var properties: [String: WooAnalyticsEventPropertyType] = [Key.step.rawValue: step.rawValue]
+            if let tap {
+                properties[Key.tap.rawValue] = tap.rawValue
+            }
+            return .init(statName: .jetpackSetupLoginFlow, properties: properties, error: failure)
+        }
+
+        static func setupFlow(step: SetupFlow.Step, tap: SetupFlow.TapTarget? = nil, failure: Error? = nil) -> WooAnalyticsEvent {
+            var properties: [String: WooAnalyticsEventPropertyType] = [Key.step.rawValue: step.rawValue]
+            if let tap {
+                properties[Key.tap.rawValue] = tap.rawValue
+            }
+            return .init(statName: .jetpackSetupFlow, properties: properties, error: failure)
+        }
+    }
+}

e7336ea526db89a5977116e7d70bfd372be96b0c
Ernesto Carrión <ernesto.carrion@automattic.com>
2023-04-02 17:06:26 -0500

    Adds empty FreeTrialSummaryView

---
+++ WooCommerce/Classes/Authentication/Store Creation/Free Trial/FreeTrialSummaryView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+/// View to inform the benefits of a free trial
+///
+struct FreeTrialSummaryView: View {
+
+    var body: some View {
+        Text("Summary View")
+    }
+}
+
+
+struct FreeTrialSummaryView_Preview: PreviewProvider {
+    static var previews: some View {
+        FreeTrialSummaryView()
+    }
+}

a16cc8440390ab84f3d4ab00805494f93fecb6a3
Ernesto Carrión <ernesto.carrion@automattic.com>
2023-04-03 11:14:37 -0500

    Adds view main structure

--- WooCommerce/Classes/Authentication/Store Creation/Free Trial/FreeTrialSummaryView.swift
+++ WooCommerce/Classes/Authentication/Store Creation/Free Trial/FreeTrialSummaryView.swift
@@ -4,8 +4,72 @@ import SwiftUI
 ///
 struct FreeTrialSummaryView: View {
 
+    static let features: [String] = [
+        "Premium themes",
+        "Unlimited products",
+        "Shipping labels",
+        "Ecommerce reports",
+        "Multiple payment options",
+        "Subscriptions & product kits",
+        "Social advertising",
+        "Email marketing",
+        "24/7 support",
+        "Auto updates & backups",
+        "Site security",
+        "Fast to launch"
+    ]
+
+
     var body: some View {
-        Text("Summary View")
+        VStack(spacing: .zero) {
+            // Main Content
+            ScrollView {
+                VStack(alignment: .leading) {
+
+                    HStack {
+                        Spacer()
+                        Rectangle()
+                            .frame(width: 187, height: 158, alignment: .trailing)
+                    }
+
+                    Text("Launch in days, grow for years")
+                        .titleStyle()
+
+                    Text("We offer everything you need to build and grow an online store, powered by WooCommerce and hosted on WordPress.com.")
+                        .calloutStyle()
+
+                    Text("Try it free for 14 days.")
+                        .secondaryTitleStyle()
+
+                    ForEach(Self.features, id: \.self) { feature in
+                        HStack {
+                            Rectangle()
+                                .frame(width: 16, height: 16)
+
+                            Text(feature)
+                        }
+                    }
+                }
+            }
+
+            // Continue Footer
+            VStack() {
+                Divider()
+
+                Button {
+                    print("Continue Pressed")
+                } label: {
+                    Text("Try For Free")
+                }
+                .buttonStyle(PrimaryButtonStyle())
+                .padding()
+
+                Text("No credit card required.")
+                    .calloutStyle()
+                    .scenePadding(.bottom)
+            }
+            .background(Color(.listForeground(modal: false)))
+        }
     }
 }
 

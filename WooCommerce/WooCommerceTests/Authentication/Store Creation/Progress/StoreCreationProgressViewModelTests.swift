babdea8f280b8e6a771aa8ba09d2482e8b67eead
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-04-06 11:15:02 +0530

    Update time interval to a larger number.

--- WooCommerce/WooCommerceTests/Authentication/Store Creation/Progress/StoreCreationProgressViewModelTests.swift
+++ WooCommerce/WooCommerceTests/Authentication/Store Creation/Progress/StoreCreationProgressViewModelTests.swift
@@ -40,7 +40,7 @@ final class StoreCreationProgressViewModelTests: XCTestCase {
 
     func test_onAppear_increments_progressValue_at_expected_interval() {
         // Given
-        let timeInterval = 0.01
+        let timeInterval = 0.1
         let incrementInterval = 1.0
         let sut = StoreCreationProgressViewModel(incrementInterval: incrementInterval,
                                                  progressViewAnimationTimerInterval: timeInterval)
@@ -59,7 +59,7 @@ final class StoreCreationProgressViewModelTests: XCTestCase {
 
     func test_onAppear_increments_progressValue_only_upto_next_progress() {
         // Given
-        let timeInterval = 0.01
+        let timeInterval = 0.1
         let incrementInterval = 1.0
         let sut = StoreCreationProgressViewModel(incrementInterval: incrementInterval,
                                                  progressViewAnimationTimerInterval: timeInterval)
@@ -80,7 +80,7 @@ final class StoreCreationProgressViewModelTests: XCTestCase {
 
     func test_onAppear_increments_progressValue_as_expected_after_calling_incrementProgress() {
         // Given
-        let timeInterval = 0.01
+        let timeInterval = 0.1
         let incrementInterval = 1.0
         let sut = StoreCreationProgressViewModel(incrementInterval: incrementInterval,
                                                  progressViewAnimationTimerInterval: timeInterval)

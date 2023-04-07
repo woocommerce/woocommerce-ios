5f1661f56cd8c8afbac6c8f748c596869c3ca94f
jos <17252150+jostnes@users.noreply.github.com>
2023-04-07 16:06:02 +0800

    fix test on iPad

--- WooCommerce/UITestsFoundation/Screens/ExternalAppScreen.swift
+++ WooCommerce/UITestsFoundation/Screens/ExternalAppScreen.swift
@@ -39,8 +39,11 @@ public final class ExternalAppScreen {
         let safari = XCUIApplication(bundleIdentifier: "com.apple.mobilesafari")
         safari.launch()
 
+        // To accommodate elements on both iPhone and iPad
+        let searchBarElement = UIDevice.current.userInterfaceIdiom == .phone ? "TabBarItemTitle" : "UnifiedTabBarItemView?isSelected=true"
+
         // Go to Wiremock's HTML file with universal links
-        safari.textFields["TabBarItemTitle"].tap()
+        safari.textFields[searchBarElement].tap()
         safari.typeText("http://localhost:8282/links.html")
         safari.buttons["Go"].tap()
 

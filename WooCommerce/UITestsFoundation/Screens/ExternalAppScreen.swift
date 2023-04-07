58286ca407e768e41e10a2f5f6fe583bcd2dcfa9
jos <17252150+jostnes@users.noreply.github.com>
2023-04-07 17:58:24 +0800

    remove unused openUniversalLinkFromRemindersApp

--- WooCommerce/UITestsFoundation/Screens/ExternalAppScreen.swift
+++ WooCommerce/UITestsFoundation/Screens/ExternalAppScreen.swift
@@ -8,32 +8,7 @@ public final class ExternalAppScreen {
         app = XCUIApplication()
     }
 
-    // To open universal links without depending on mocks
-    public func openUniversalLinkFromRemindersApp(link: String) {
-
-        let reminders = XCUIApplication(bundleIdentifier: "com.apple.reminders")
-        reminders.launch()
-
-        // To dismiss the welcome screen if displayed
-        let continueButton = reminders.buttons["Continue"]
-        if continueButton.exists {
-            continueButton.tap()
-        }
-
-        // Add universal link as reminder
-        let remindersTable = reminders.otherElements["RemindersList.ID.RemindersTable"]
-        if !reminders.textFields["Title"].exists { remindersTable.tap() }
-        reminders.textFields["Title"].typeText("\(link)")
-        remindersTable.tap()
-
-        // Mark reminder as done to remove from list on next visit
-        reminders.buttons["CompleteOff"].tap()
-
-        // Tap first reminder on list
-        reminders.cells.links.firstMatch.tap()
-    }
-
-    // To open universal links using mocks
+    // To open universal links listed in mocked HTML file
     public func openUniversalLinkFromSafariApp(link: String) {
 
         let safari = XCUIApplication(bundleIdentifier: "com.apple.mobilesafari")

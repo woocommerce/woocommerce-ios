6bdd25eaef2b241d112a39b9471b9beb49638ccf
jos <17252150+jostnes@users.noreply.github.com>
2023-04-11 11:31:51 +0800

    update search bar element to work on iPhone/iPad and in different safari states

--- WooCommerce/UITestsFoundation/Screens/ExternalAppScreen.swift
+++ WooCommerce/UITestsFoundation/Screens/ExternalAppScreen.swift
@@ -9,20 +9,18 @@ public final class ExternalAppScreen {
     }
 
     // To open universal links listed in mocked HTML file
-    public func openUniversalLinkFromSafariApp(link: String) {
 
         let safari = XCUIApplication(bundleIdentifier: "com.apple.mobilesafari")
         safari.launch()
 
         // To accommodate elements on both iPhone and iPad
-        let searchBarElement = UIDevice.current.userInterfaceIdiom == .phone ? "TabBarItemTitle" : "UnifiedTabBarItemView?isSelected=true"
+        let searchBarElement = UIDevice.current.userInterfaceIdiom == .phone ? "CapsuleNavigationBar?isSelected=true" : "UnifiedTabBar"
 
         // Go to Wiremock's HTML file with universal links
-        safari.textFields[searchBarElement].tap()
+        safari.otherElements[searchBarElement].tap()
         safari.typeText("http://localhost:8282/links.html")
         safari.buttons["Go"].tap()
 
         // Tap on the universal link
-        if safari.staticTexts["TESTING LINKS"].exists { safari.links[link].tap() }
     }
 }

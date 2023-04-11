0e20662c4d4e630f39fa0500540104bb958125c3
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2023-04-11 10:10:37 +0200

    Pass feature flag service on init for better testability

--- WooCommerce/Classes/Tools/In-Person Payments/CardPresentConfigurationLoader.swift
+++ WooCommerce/Classes/Tools/In-Person Payments/CardPresentConfigurationLoader.swift
@@ -1,17 +1,23 @@
 import Foundation
+import Experiments
 import Yosemite
 
 final class CardPresentConfigurationLoader {
-    init(stores: StoresManager = ServiceLocator.stores) {
+    let shouldReturnConfigurationForGB: Bool
+
+    init(stores: StoresManager = ServiceLocator.stores,
+         featureFlagService: FeatureFlagService = ServiceLocator.featureFlagService) {
         // This initializer is kept since this is where we'd check for
         // feature flags while developing support for a new country
         // See https://github.com/woocommerce/woocommerce-ios/pull/6954
+
+        shouldReturnConfigurationForGB = featureFlagService.isFeatureFlagEnabled(.IPPUKExpansion)
     }
 
     var configuration: CardPresentPaymentsConfiguration {
         .init(
             country: SiteAddress().countryCode,
-            shouldReturnConfigurationForGB: ServiceLocator.featureFlagService.isFeatureFlagEnabled(.IPPUKExpansion)
+            shouldReturnConfigurationForGB: shouldReturnConfigurationForGB
         )
     }
 }

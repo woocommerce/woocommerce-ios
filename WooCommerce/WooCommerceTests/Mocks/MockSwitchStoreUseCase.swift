6f74105ef7021c53bdf1e395e50200f99851e841
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-04-12 14:01:17 +0530

    Mock `getAvailableStores` method.

--- WooCommerce/WooCommerceTests/Mocks/MockSwitchStoreUseCase.swift
+++ WooCommerce/WooCommerceTests/Mocks/MockSwitchStoreUseCase.swift
@@ -2,12 +2,15 @@ import Foundation
 import XCTest
 
 @testable import WooCommerce
+import struct Yosemite.Site
 
 /// Mock for `SwitchStoreUseCase`.
 ///
 /// This currently just calls `onCompletion()` with `true` as the `storeChanged` result.
 ///
 final class MockSwitchStoreUseCase: SwitchStoreUseCaseProtocol {
+
+    var availableStores: [Site] = []
 
     /// The IDs of stores that `switchStore` was called with.
     private(set) var destinationStoreIDs = [Int64]()
@@ -22,5 +25,9 @@ extension MockSwitchStoreUseCase {
         DispatchQueue.main.async {
             onCompletion(true)
         }
+    }
+
+    func getAvailableStores() -> [Site] {
+        availableStores
     }
 }

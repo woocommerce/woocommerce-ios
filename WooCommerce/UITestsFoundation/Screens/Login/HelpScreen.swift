6beb89818c5788fdfc448703298595c0f378d276
jos <17252150+jostnes@users.noreply.github.com>
2023-04-19 11:25:29 +0800

    reorder element declaration by screen

--- WooCommerce/UITestsFoundation/Screens/Login/HelpScreen.swift
+++ WooCommerce/UITestsFoundation/Screens/Login/HelpScreen.swift
@@ -3,6 +3,11 @@ import XCTest
 
 public final class HelpScreen: ScreenObject {
 
+    // "Help" screen elements
+    private let helpNavigationBarGetter: (XCUIApplication) -> XCUIElement = {
+        $0.navigationBars["Help"]
+    }
+
     private let contactSupportButtonGetter: (XCUIApplication) -> XCUIElement = {
         $0.staticTexts["Contact Support"]
     }
@@ -11,37 +16,37 @@ public final class HelpScreen: ScreenObject {
         $0.textFields["Email"]
     }
 
-    private let helpListFirstOptionGetter: (XCUIApplication) -> XCUIElement = {
-        $0.scrollViews.element.staticTexts.element(boundBy: 1)
-    }
-
-    private let helpNavigationBarGetter: (XCUIApplication) -> XCUIElement = {
-        $0.navigationBars["Help"]
-    }
-
-    private let messageTextFieldGetter: (XCUIApplication) -> XCUIElement = {
-        $0.scrollViews.element.textViews.element(boundBy: 0)
-    }
-
     private let okButtonGetter: (XCUIApplication) -> XCUIElement = {
         $0.buttons["OK"]
+    }
+
+    // "Contact Support" screen elements
+    private let helpListFirstOptionGetter: (XCUIApplication) -> XCUIElement = {
+        $0.scrollViews.element.staticTexts.element(boundBy: 1)
     }
 
     private let subjectTextFieldGetter: (XCUIApplication) -> XCUIElement = {
         $0.scrollViews.element.textFields.element(boundBy: 0)
     }
 
+    private let messageTextFieldGetter: (XCUIApplication) -> XCUIElement = {
+        $0.scrollViews.element.textViews.element(boundBy: 0)
+    }
+
     private let submitButtonGetter: (XCUIApplication) -> XCUIElement = {
         $0.buttons["Submit Support Request"]
     }
 
+    // "Help" screen elements
+    private var helpNavigationBar: XCUIElement { helpNavigationBarGetter(app) }
     private var contactSupportButton: XCUIElement { contactSupportButtonGetter(app) }
     private var emailTextField: XCUIElement { emailTextFieldGetter(app) }
-    private var helpListFirstOption: XCUIElement { helpListFirstOptionGetter(app) }
-    private var helpNavigationBar: XCUIElement { helpNavigationBarGetter(app) }
-    private var messageTextField: XCUIElement { messageTextFieldGetter(app) }
     private var okButton: XCUIElement { okButtonGetter(app) }
+
+    // "Contact Support" screen elements
+    private var helpListFirstOption: XCUIElement { helpListFirstOptionGetter(app) }
     private var subjectTextField: XCUIElement { subjectTextFieldGetter(app) }
+    private var messageTextField: XCUIElement { messageTextFieldGetter(app) }
     private var submitButton: XCUIElement { submitButtonGetter(app) }
 
     public init(app: XCUIApplication = XCUIApplication()) throws {

c7859634bb7521fea9512d8e8934be53374d7080
Rachel McRoberts <rachel@automattic.com>
2023-04-21 14:11:34 +0100

    Add support for retrieving subscriptions for renewal orders

--- Yosemite/YosemiteTests/Stores/SubscriptionStoreTests.swift
+++ Yosemite/YosemiteTests/Stores/SubscriptionStoreTests.swift
@@ -24,6 +24,10 @@ final class SubscriptionStoreTests: XCTestCase {
     ///
     private let sampleOrderID: Int64 = 12345
 
+    /// Sample Subscription ID
+    ///
+    private let sampleSubscriptionID: Int64 = 282
+
     override func setUp() {
         super.setUp()
         network = MockNetwork()
@@ -33,7 +37,57 @@ final class SubscriptionStoreTests: XCTestCase {
 
     // MARK: - loadSubscriptions
 
-    func test_loadSubscriptions_returns_subscriptions_on_success() throws {
+    func test_loadSubscriptions_returns_specific_subscription_for_renewal_order_on_success() throws {
+        // Given
+        network.simulateResponse(requestUrlSuffix: "subscriptions/\(sampleSubscriptionID)", filename: "subscription")
+        let store = SubscriptionStore(dispatcher: dispatcher, storageManager: storageManager, network: network)
+
+        // When
+        let renewalOrder = Order.fake().copy(siteID: self.sampleSiteID, renewalSubscriptionID: "\(sampleSubscriptionID)")
+        let result: Result<[Subscription], Error> = waitFor { promise in
+            let action = SubscriptionAction.loadSubscriptions(for: renewalOrder) { result in
+                promise(result)
+            }
+            store.onAction(action)
+        }
+
+        // Then
+        XCTAssertTrue(result.isSuccess)
+        let subscription = try XCTUnwrap(result.get().first)
+        let expectedSubscription = Subscription(siteID: sampleSiteID,
+                                                subscriptionID: sampleSubscriptionID,
+                                                parentID: 281,
+                                                status: .active,
+                                                currency: "USD",
+                                                billingPeriod: .week,
+                                                billingInterval: "1",
+                                                total: "14.50",
+                                                startDate: DateFormatter.dateFromString(with: "2023-01-31T16:29:46"),
+                                                endDate: DateFormatter.dateFromString(with: "2023-04-25T16:29:46"))
+        assertEqual(expectedSubscription, subscription)
+    }
+
+    func test_loadSubscriptions_returns_errors_for_renewal_order_on_failure() {
+        // Given
+        let error = NetworkError.unacceptableStatusCode(statusCode: 500)
+        network.simulateError(requestUrlSuffix: "subscriptions/\(sampleSubscriptionID)", error: error)
+        let store = SubscriptionStore(dispatcher: dispatcher, storageManager: storageManager, network: network)
+
+        // When
+        let renewalOrder = Order.fake().copy(siteID: self.sampleSiteID, renewalSubscriptionID: "\(sampleSubscriptionID)")
+        let result: Result<[Subscription], Error> = waitFor { promise in
+            let action = SubscriptionAction.loadSubscriptions(for: renewalOrder) { result in
+                promise(result)
+            }
+            store.onAction(action)
+        }
+
+        // Then
+        XCTAssertTrue(result.isFailure)
+        XCTAssertEqual(result.failure as? NetworkError, error)
+    }
+
+    func test_loadSubscriptions_returns_subscriptions_for_non_renewal_order_on_success() throws {
         // Given
         network.simulateResponse(requestUrlSuffix: "subscriptions", filename: "subscription-list")
         let store = SubscriptionStore(dispatcher: dispatcher, storageManager: storageManager, network: network)
@@ -52,7 +106,7 @@ final class SubscriptionStoreTests: XCTestCase {
         assertEqual(2, subscriptions.count)
     }
 
-    func test_loadSubscriptions_returns_errors_on_failure() {
+    func test_loadSubscriptions_returns_errors_for_non_renewal_order_on_failure() {
         // Given
         let error = NetworkError.unacceptableStatusCode(statusCode: 500)
         network.simulateError(requestUrlSuffix: "subscriptions", error: error)

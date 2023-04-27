8da311fed8d36452c5687a618bbd0952b443f6fb
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-04-27 21:34:02 +0530

    Feature flag for hide store onboarding list feature.

--- Experiments/Experiments/DefaultFeatureFlagService.swift
+++ Experiments/Experiments/DefaultFeatureFlagService.swift
@@ -93,6 +93,8 @@ public struct DefaultFeatureFlagService: FeatureFlagService {
             return true
         case .productDescriptionAI:
             return buildConfig == .localDeveloper || buildConfig == .alpha
+        case .hideStoreOnboardingTaskList:
+            return (buildConfig == .localDeveloper || buildConfig == .alpha) && !isUITesting
         default:
             return true
         }

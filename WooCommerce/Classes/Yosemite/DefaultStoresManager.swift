749d60a99dee45b922692cb25c1435066834c071
Huong Do <hi.huongdo@gmail.com>
2023-04-28 11:17:56 +0700

    Clear store phone number upon switching stores

--- WooCommerce/Classes/Yosemite/DefaultStoresManager.swift
+++ WooCommerce/Classes/Yosemite/DefaultStoresManager.swift
@@ -234,6 +234,7 @@ class DefaultStoresManager: StoresManager {
         // Because `defaultSite` is loaded or synced asynchronously, it is reset here so that any UI that calls this does not show outdated data.
         // For example, `sessionManager.defaultSite` is used to show site name in various screens in the app.
         sessionManager.defaultSite = nil
+        defaults[.storePhoneNumber] = nil
         defaults[.completedAllStoreOnboardingTasks] = nil
         restoreSessionSiteIfPossible()
         ServiceLocator.pushNotesManager.reloadBadgeCount()

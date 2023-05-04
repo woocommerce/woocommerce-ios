1f8b91e851d487acec8cf824c63b1ad8c085521a
ThomazFB <thomazfcortez@gmail.com>
2023-05-04 19:46:56 -0300

    Configure basic TopBannerView creation inside ShippingLabelsEUNoticeTopBannerFactory.swift

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/ShippingLabelsEUNoticeTopBannerFactory.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/ShippingLabelsEUNoticeTopBannerFactory.swift
@@ -1,6 +1,21 @@
 import Foundation
 
 final class ShippingLabelsEUNoticeTopBannerFactory {
-    func createTopBanner() {
+    func createTopBanner() -> TopBannerView {
+        let viewModel = TopBannerViewModel(title: Localization.title,
+                infoText: Localization.info,
+                icon: .infoImage,
+                topButton: .none)
+
+        return TopBannerView(viewModel: viewModel)
+    }
+}
+
+private extension ShippingLabelsEUNoticeTopBannerFactory {
+    enum Localization {
+        static let title = NSLocalizedString("")
+        static let info = NSLocalizedString("")
+        static let learnMore = NSLocalizedString("")
+        static let dismiss = NSLocalizedString("")
     }
 }

332bfd477cb9d6870126ef50fe23d21239624d01
ThomazFB <thomazfcortez@gmail.com>
2023-05-05 20:06:51 -0300

    Connect top banner call inside ShippingLabelFormViewController.swift

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/ShippingLabelFormViewController.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/ShippingLabelFormViewController.swift
@@ -47,6 +47,7 @@ final class ShippingLabelFormViewController: UIViewController {
         configureTableView()
         registerTableViewCells()
         registerTableViewHeaderFooters()
+        showTopBannerView()
         observeViewModel()
     }
 
--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/ShippingLabelsEUNoticeTopBannerFactory.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/ShippingLabelsEUNoticeTopBannerFactory.swift
@@ -2,9 +2,11 @@ import Foundation
 
 final class ShippingLabelsEUNoticeTopBannerFactory {
     func createTopBanner() -> TopBannerView {
-        let viewModel = TopBannerViewModel(title: Localization.title,
+        let viewModel = TopBannerViewModel(
+                title: Localization.title,
                 infoText: Localization.info,
                 icon: .infoImage,
+                isExpanded: true,
                 topButton: .none)
 
         return TopBannerView(viewModel: viewModel)

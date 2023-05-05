f892ea4955c7ffc655691a1e1adae9c8295d8c80
Josh Heald <joshheald@users.noreply.github.com>
2023-05-05 10:10:40 +0100

    9570 Remove top space on JITM when badge not set
    Now that the badge is optional on an announcement card, we collapse the space it would take up when it’s not set.
    
    Additionally, this commit corrects the padding between the `title` and `message`, and sets the CTA color to `primary` instead of `systemPink`.

--- WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/FeatureAnnouncementCardView.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/FeatureAnnouncementCardView.swift
@@ -26,93 +26,90 @@ struct FeatureAnnouncementCardView: View {
     }
 
     var content: some View {
-        VStack(spacing: 0) {
-            HStack(spacing: 0) {
-                if let badgeType = viewModel.badgeType {
+        VStack(alignment: .leading, spacing: Layout.smallSpacing) {
+            if let badgeType = viewModel.badgeType {
+                HStack(spacing: 0) {
                     BadgeView(type: badgeType)
-                        .padding(.leading, Layout.padding)
+                    Spacer()
+                }
+            }
+            Text(viewModel.title)
+                .headlineStyle()
+            HStack(alignment: .bottom, spacing: 0) {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(viewModel.message)
+                        .bodyStyle()
+                        .padding(.bottom, viewModel.buttonTitle == nil ? Layout.bottomNoButtonPadding : Layout.largeSpacing)
+
+                    if let buttonTitle = viewModel.buttonTitle {
+                        Button(buttonTitle) {
+                            viewModel.ctaTapped()
+                            callToAction?()
+                        }
+                        .padding(.bottom, Layout.bottomButtonPadding)
+                        .foregroundColor(Color(uiColor: .primary))
+                    }
                 }
                 Spacer()
-                if let dismiss = dismiss {
-                    Button(action: {
-                        if viewModel.showDismissConfirmation {
-                            showingDismissActionSheet = true
-                        } else {
-                            viewModel.dontShowAgainTapped()
-                            dismiss()
+                if let imageUrl = viewModel.imageUrl {
+                    AdaptiveAsyncImage(lightUrl: imageUrl, darkUrl: viewModel.imageDarkUrl, scale: 3) { imagePhase in
+                        switch imagePhase {
+                        case .failure:
+                            Image(uiImage: viewModel.image)
+                                .accessibilityHidden(true)
+                        case .success(let image):
+                            image.resizable()
+                                .scaledToFit()
+                                .accessibilityHidden(true)
+                        case .empty:
+                            AnimatedPlaceholder()
+                        @unknown default:
+                            EmptyView()
                         }
-                    }) {
-                        Image(systemName: "xmark")
-                            .foregroundColor(Color(.withColorStudio(.gray)))
                     }
-                    .padding(.trailing, Layout.padding)
-                    .actionSheet(isPresented: $showingDismissActionSheet) {
-                        ActionSheet(
-                            title: Text(viewModel.dismissAlertTitle),
-                            message: Text(viewModel.dismissAlertMessage),
-                            buttons: [
-                                .default(Text(Localization.remindLaterButton),
-                                         action: {
-                                             viewModel.remindLaterTapped()
-                                             dismiss()
-                                         }),
-                                .default(Text(Localization.dontShowAgainButton),
-                                         action: {
-                                             viewModel.dontShowAgainTapped()
-                                             dismiss()
-                                         }),
-                                .cancel()
-                            ]
-                        )
-                    }
+                } else {
+                    Image(uiImage: viewModel.image)
+                        .accessibilityHidden(true)
                 }
             }
-            .padding(.top, Layout.padding)
-
-            VStack(alignment: .leading, spacing: 0) {
-                Text(viewModel.title)
-                    .headlineStyle()
-                HStack(alignment: .bottom, spacing: 0) {
-                    VStack(alignment: .leading, spacing: 0) {
-                        Text(viewModel.message)
-                            .bodyStyle()
-                            .padding(.bottom, viewModel.buttonTitle == nil ? Layout.bottomNoButtonPadding : Layout.largeSpacing)
-
-                        if let buttonTitle = viewModel.buttonTitle {
-                            Button(buttonTitle) {
-                                viewModel.ctaTapped()
-                                callToAction?()
-                            }
-                            .padding(.bottom, Layout.bottomButtonPadding)
-                            .foregroundColor(Color(.withColorStudio(.pink)))
-                        }
-                    }
-                    Spacer()
-                    if let imageUrl = viewModel.imageUrl {
-                        AdaptiveAsyncImage(lightUrl: imageUrl, darkUrl: viewModel.imageDarkUrl, scale: 3) { imagePhase in
-                            switch imagePhase {
-                            case .failure:
-                                Image(uiImage: viewModel.image)
-                                    .accessibilityHidden(true)
-                            case .success(let image):
-                                image.resizable()
-                                    .scaledToFit()
-                                    .accessibilityHidden(true)
-                            case .empty:
-                                AnimatedPlaceholder()
-                            @unknown default:
-                                EmptyView()
-                            }
-                        }
-                    } else {
-                        Image(uiImage: viewModel.image)
-                            .accessibilityHidden(true)
-                    }
-                }
-            }
-            .padding(.top, Layout.smallSpacing)
-            .padding(.leading, Layout.padding)
         }
+        .overlay(alignment: .topTrailing) {
+            if let dismiss = dismiss {
+                Button(action: {
+                    if viewModel.showDismissConfirmation {
+                        showingDismissActionSheet = true
+                    } else {
+                        viewModel.dontShowAgainTapped()
+                        dismiss()
+                    }
+                }) {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.withColorStudio(.gray)))
+                }
+                .padding(.trailing, Layout.padding)
+                .actionSheet(isPresented: $showingDismissActionSheet) {
+                    ActionSheet(
+                        title: Text(viewModel.dismissAlertTitle),
+                        message: Text(viewModel.dismissAlertMessage),
+                        buttons: [
+                            .default(Text(Localization.remindLaterButton),
+                                     action: {
+                                         viewModel.remindLaterTapped()
+                                         dismiss()
+                                     }),
+                            .default(Text(Localization.dontShowAgainButton),
+                                     action: {
+                                         viewModel.dontShowAgainTapped()
+                                         dismiss()
+                                     }),
+                            .cancel()
+                        ]
+                    )
+                }
+            }
+        }
+        .padding(.top, Layout.padding)
+        .padding(.leading, Layout.padding)
         .onAppear {
             viewModel.onAppear()
         }

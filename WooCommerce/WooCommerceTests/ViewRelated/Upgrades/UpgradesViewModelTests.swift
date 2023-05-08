d2d36f3c3ee5d1054066fb79e664140a7f048a5f
ThomazFB <thomazfcortez@gmail.com>
2023-05-08 20:03:44 -0300

    Improve UpgradesViewModelTests.swift coverage

--- WooCommerce/WooCommerceTests/ViewRelated/Upgrades/UpgradesViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Upgrades/UpgradesViewModelTests.swift
@@ -110,6 +110,41 @@ final class UpgradesViewModelTests: XCTestCase {
         XCTAssertNil(viewModel.errorNotice)
     }
 
+    func test_active_upgrades_disabled_free_trial_has_correct_view_model_values() {
+        // Given
+        let expireDate = Date().addingDays(14)
+        let plan = WPComSitePlan(id: freeTrialID,
+                hasDomainCredit: false,
+                expiryDate: expireDate)
+
+        let session = SessionManager.testingInstance
+        let stores = MockStoresManager(sessionManager: session)
+        let featureFlags = MockFeatureFlagService(isFreeTrial: true)
+        session.defaultSite = sampleSite
+
+        stores.whenReceivingAction(ofType: PaymentAction.self) { action in
+            switch action {
+            case .loadSiteCurrentPlan(_, let completion):
+                completion(.success(plan))
+            default:
+                break
+            }
+        }
+
+        // When
+        let synchronizer = StorePlanSynchronizer(stores: stores)
+        let viewModel = UpgradesViewModel(stores: stores, storePlanSynchronizer: synchronizer, featureFlagService: featureFlags)
+        viewModel.loadPlan()
+
+        // Then
+        XCTAssertEqual(viewModel.planName, NSLocalizedString("Free Trial", comment: ""))
+        XCTAssertTrue(viewModel.planInfo.isNotEmpty)
+        XCTAssertFalse(viewModel.shouldShowUpgradeButton)
+        XCTAssertFalse(viewModel.shouldShowCancelTrialButton)
+        XCTAssertTrue(viewModel.shouldShowFreeTrialFeatures)
+        XCTAssertNil(viewModel.errorNotice)
+    }
+
     func test_WooExpress_is_removed_from_plan_name() {
         // Given
         let expireDate = Date().addingDays(300)

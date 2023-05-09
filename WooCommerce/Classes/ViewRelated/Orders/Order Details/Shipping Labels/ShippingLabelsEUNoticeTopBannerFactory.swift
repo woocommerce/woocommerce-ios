6fb36658559842645ecd8960f5fd12d7bb5a0c30
ThomazFB <thomazfcortez@gmail.com>
2023-05-09 20:36:53 -0300

    Adjust ShippingLabelsEUNoticeTopBannerFactory.swift to correctly create action buttons

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/ShippingLabelsEUNoticeTopBannerFactory.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/ShippingLabelsEUNoticeTopBannerFactory.swift
@@ -2,14 +2,24 @@ import Foundation
 
 final class ShippingLabelsEUNoticeTopBannerFactory {
     func createTopBanner() -> TopBannerView {
+        let learnMoreAction = TopBannerViewModel.ActionButton(title: Localization.learnMore) { _ in
+            NSLog("Learn more clicked")
+        }
+
+        let dismissAction = TopBannerViewModel.ActionButton(title: Localization.dismiss) { _ in
+            NSLog("Dismiss clicked")
+        }
+
         let viewModel = TopBannerViewModel(
                 title: Localization.title,
                 infoText: Localization.info,
                 icon: .infoImage,
                 isExpanded: true,
-                topButton: .none)
-
-        return TopBannerView(viewModel: viewModel)
+                topButton: .dismiss(handler: {}),
+                actionButtons: [learnMoreAction, dismissAction])
+        let topBannerView = TopBannerView(viewModel: viewModel)
+        topBannerView.translatesAutoresizingMaskIntoConstraints = false
+        return topBannerView
     }
 }
 

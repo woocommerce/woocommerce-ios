7da87adc14f6c9bc92cea2f87e5a65d71c8e4f46
Huong Do <hi.huongdo@gmail.com>
2023-05-10 11:16:26 +0700

    Add contents for other cases of local notification

--- WooCommerce/Classes/Notifications/LocalNotification.swift
+++ WooCommerce/Classes/Notifications/LocalNotification.swift
@@ -17,7 +17,7 @@ struct LocalNotification {
     /// Its raw value is used for the identifier of a local notification and also the event property for analytics.
     enum Scenario {
         case storeCreationComplete
-        case oneDayAfterStoreCreationNameWithoutFreeTrial
+        case oneDayAfterStoreCreationNameWithoutFreeTrial(storeName: String)
         case oneDayBeforeFreeTrialExpires(expiryDate: Date)
         case oneDayAfterFreeTrialExpires
         // The following notifications are deprecated and are canceled in the first release.
@@ -90,34 +90,85 @@ extension LocalNotification {
             return name
         }()
 
+        let title: String
+        let body: String
+        let actions: [Action]
+        let category: Category
+
         switch scenario {
+        case .storeCreationComplete:
+            title = Localization.StoreCreationComplete.title
+            body = String.localizedStringWithFormat(Localization.StoreCreationComplete.body, name)
+            actions = [.explore]
+            category = .storeCreation
+
+        case .oneDayAfterStoreCreationNameWithoutFreeTrial(let storeName):
+            title = Localization.OneDayAfterStoreCreationNameWithoutFreeTrial.title
+            body = String.localizedStringWithFormat(
+                Localization.OneDayAfterStoreCreationNameWithoutFreeTrial.body,
+                name,
+                storeName
+            )
+            category = .storeCreation
+            actions = [.subscribe]
+
         case .oneDayBeforeFreeTrialExpires(let expiryDate):
-            let title = String.localizedStringWithFormat(Localization.OneDayBeforeFreeTrialExpires.title, name)
+            title = String.localizedStringWithFormat(Localization.OneDayBeforeFreeTrialExpires.title, name)
             let dateFormatStyle = Date.FormatStyle()
                 .weekday(.wide)
                 .month(.wide)
                 .day(.defaultDigits)
             let displayDate = expiryDate.formatted(dateFormatStyle)
-            let message = String.localizedStringWithFormat(Localization.OneDayBeforeFreeTrialExpires.body, displayDate)
-            self.init(title: title,
-                      body: message,
-                      scenario: scenario,
-                      actions: .init(category: .storeCreation, actions: [.upgrade]))
+            body = String.localizedStringWithFormat(Localization.OneDayBeforeFreeTrialExpires.body, displayDate)
+            category = .storeCreation
+            actions = [.upgrade]
+
         case .oneDayAfterFreeTrialExpires:
-            let title = Localization.OneDayAfterFreeTrialExpires.title
-            let message = String.localizedStringWithFormat(Localization.OneDayAfterFreeTrialExpires.body, name)
-            self.init(title: title,
-                      body: message,
-                      scenario: scenario,
-                      actions: .init(category: .storeCreation, actions: [.upgrade]))
+            title = Localization.OneDayAfterFreeTrialExpires.title
+            body = String.localizedStringWithFormat(Localization.OneDayAfterFreeTrialExpires.body, name)
+            category = .storeCreation
+            actions = [.upgrade]
+
         default:
             return nil
         }
+
+        self.init(title: title,
+                  body: body,
+                  scenario: scenario,
+                  actions: .init(category: category, actions: actions))
     }
 }
 
 private extension LocalNotification {
     enum Localization {
+        enum StoreCreationComplete {
+            static let title = NSLocalizedString(
+                "Your store is ready!",
+                comment: "Title of the local notification about a newly created store"
+            )
+            static let body = NSLocalizedString(
+                "Hi %1$@, Welcome to your 14-day free trial of Woo Express – " +
+                "everything you need to start and grow a successful online business, " +
+                "all in one place. Ready to explore?",
+                comment: "Message on the local notification about a newly created store." +
+                "The placeholder is the name of the user."
+            )
+        }
+
+        enum OneDayAfterStoreCreationNameWithoutFreeTrial {
+            static let title = NSLocalizedString(
+                "Your store is waiting!",
+                comment: "Title of the local notification suggesting a trial plan subscription."
+            )
+            static let body = NSLocalizedString(
+                "Hi %1$@, %2$@ is ready for you! Start your 14-day free trial " +
+                "of Woo Express right in just one click to start your online business.",
+                comment: "Message on the local notification suggesting a trial plan subscription." +
+                "The placeholders are the name of the user and the store name."
+            )
+        }
+
         enum OneDayBeforeFreeTrialExpires {
             static let title = NSLocalizedString(
                 "Time’s almost up, %1$@!",

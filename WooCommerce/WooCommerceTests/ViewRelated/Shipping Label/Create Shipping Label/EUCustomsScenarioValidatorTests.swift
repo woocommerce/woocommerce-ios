ee6677e1efe9ca69c10b8073bd34199086dfe478
ThomazFB <thomazfcortez@gmail.com>
2023-05-12 17:24:46 -0300

    Create first failure scenario for EUCustomsScenarioValidatorTests.swift

--- WooCommerce/WooCommerceTests/ViewRelated/Shipping Label/Create Shipping Label/EUCustomsScenarioValidatorTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Shipping Label/Create Shipping Label/EUCustomsScenarioValidatorTests.swift
@@ -3,6 +3,10 @@ import XCTest
 @testable import Yosemite
 
 final class EUCustomsScenarioValidatorTests: XCTestCase {
+    func test_when_origin_country_is_US_and_destination_country_is_not_in_EU_new_custom_rules_then_return_false() {
+        assertValidationFailsFor(origin: "US", destination: "BR")
+    }
+
     private func assertValidationSucceedsFor(origin: String, destination: String) {
         // Given
         let originAddress = createShippingLabelAddressWith(country: origin)
@@ -13,6 +17,18 @@ final class EUCustomsScenarioValidatorTests: XCTestCase {
 
         // Then
         XCTAssertTrue(result)
+    }
+
+    private func assertValidationFailsFor(origin: String, destination: String) {
+        // Given
+        let originAddress = createShippingLabelAddressWith(country: origin)
+        let destinationAddress = createShippingLabelAddressWith(country: destination)
+
+        // When
+        let result = EUCustomsScenarioValidator.validate(origin: originAddress, destination: destinationAddress)
+
+        // Then
+        XCTAssertFalse(result)
     }
 
     private func createShippingLabelAddressWith(country: String) -> ShippingLabelAddress {

c1fbef5e1de478d7233ef8448fd7a8f3bc8c010e
ThomazFB <thomazfcortez@gmail.com>
2023-05-11 23:32:11 -0300

    Introduce EUCustomsScenarioValidator.swift

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/EUCustomsScenarioValidator.swift
@@ -0,0 +1,8 @@
+import Foundation
+import Yosemite
+
+class EUCustomsScenarioValidator {
+    static func validate(origin: ShippingLabelAddress, destination: ShippingLabelAddress) -> Bool {
+        return true
+    }
+}

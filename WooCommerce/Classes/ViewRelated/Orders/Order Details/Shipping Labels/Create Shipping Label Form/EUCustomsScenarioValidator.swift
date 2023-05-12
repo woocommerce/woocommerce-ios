b88badf36156b9b893bc1c121d7408f6af8fef92
ThomazFB <thomazfcortez@gmail.com>
2023-05-11 23:46:48 -0300

    Implement EUCustomsScenarioValidator.swift basic behavior

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/EUCustomsScenarioValidator.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/EUCustomsScenarioValidator.swift
@@ -3,6 +3,36 @@ import Yosemite
 
 class EUCustomsScenarioValidator {
     static func validate(origin: ShippingLabelAddress?, destination: ShippingLabelAddress?) -> Bool {
-        return true
+        origin?.country == "US" && countriesFollowingEUCustoms.contains(destination?.country ?? "")
     }
+
+    static let countriesFollowingEUCustoms = ["AT", "AUT",
+                                              "BE", "BEL",
+                                              "BG", "BGR",
+                                              "HR", "HRV",
+                                              "CY", "CYP",
+                                              "CZ", "CZE",
+                                              "DK", "DNK",
+                                              "EE", "EST",
+                                              "FI", "FIN",
+                                              "FR", "FRA",
+                                              "DE", "DEU",
+                                              "GR", "GRC",
+                                              "HU", "HUN",
+                                              "IE", "IRL",
+                                              "IT", "ITA",
+                                              "LV", "LVA",
+                                              "LT", "LTU",
+                                              "LU", "LUX",
+                                              "MT", "MLT",
+                                              "NL", "NLD",
+                                              "NO", "NOR",
+                                              "PL", "POL",
+                                              "PT", "PRT",
+                                              "RO", "ROU",
+                                              "SK", "SVK",
+                                              "SI", "SVN",
+                                              "ES", "ESP",
+                                              "SE", "SWE",
+                                              "CH", "CHE"]
 }

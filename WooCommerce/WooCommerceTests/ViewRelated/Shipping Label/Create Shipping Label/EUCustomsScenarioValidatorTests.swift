29c2ace86a5d98ce191000c28cf7b71c658f4027
ThomazFB <thomazfcortez@gmail.com>
2023-05-12 17:20:06 -0300

    Add basic test structure inside EUCustomsScenarioValidatorTests.swift

--- WooCommerce/WooCommerceTests/ViewRelated/Shipping Label/Create Shipping Label/EUCustomsScenarioValidatorTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Shipping Label/Create Shipping Label/EUCustomsScenarioValidatorTests.swift
@@ -1,42 +1,21 @@
-//
-//  EUCustomsScenarioValidatorTests.swift
-//  WooCommerceTests
-//
-//  Created by Thomaz F B Cortez on 12/05/23.
-//  Copyright © 2023 Automattic. All rights reserved.
-//
-
 import XCTest
+@testable import WooCommerce
+@testable import Yosemite
 
 final class EUCustomsScenarioValidatorTests: XCTestCase {
+    private func assertValidationSucceedsFor(origin: String, destination: String) {
+        // Given
+        let originAddress = createShippingLabelAddressWith(country: origin)
+        let destinationAddress = createShippingLabelAddressWith(country: destination)
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // When
+        let result = EUCustomsScenarioValidator.validate(origin: originAddress, destination: destinationAddress)
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        // Then
+        XCTAssertTrue(result)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    private func createShippingLabelAddressWith(country: String) -> ShippingLabelAddress {
+        ShippingLabelAddress(company: "", name: "", phone: "", country: country, state: "", address1: "", address2: "", city: "", postcode: "")
     }
 }

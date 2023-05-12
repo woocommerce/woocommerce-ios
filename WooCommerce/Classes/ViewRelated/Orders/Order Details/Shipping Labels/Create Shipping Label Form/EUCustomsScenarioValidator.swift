1893d5787103a5f936eb3a4aeca6836678ea9c3b
ThomazFB <thomazfcortez@gmail.com>
2023-05-12 17:43:50 -0300

    Refactor EUCustomsScenarioValidator.swift

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/EUCustomsScenarioValidator.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/EUCustomsScenarioValidator.swift
@@ -5,39 +5,39 @@ import Yosemite
 ///
 /// Refer to [USPS instructions](https://www.usps.com/international/new-eu-customs-rules.htm) for more context.
 ///
-class EUCustomsScenarioValidator {
+struct EUCustomsScenarioValidator {
     static func validate(origin: ShippingLabelAddress?, destination: ShippingLabelAddress?) -> Bool {
         origin?.country == usISOCode && countriesFollowingEUCustoms.contains(destination?.country ?? "")
     }
 
     private static let usISOCode = "US"
     private static let countriesFollowingEUCustoms: Set<String> = ["AT", "AUT", // Austria
-                                                           "BE", "BEL", // Belgium
-                                                           "BG", "BGR", // Bulgaria
-                                                           "HR", "HRV", // Croatia
-                                                           "CY", "CYP", // Cyprus
-                                                           "CZ", "CZE", // Czech Republic
-                                                           "DK", "DNK", // Denmark
-                                                           "EE", "EST", // Estonia
-                                                           "FI", "FIN", // Finland
-                                                           "FR", "FRA", // France
-                                                           "DE", "DEU", // Germany
-                                                           "GR", "GRC", // Greece
-                                                           "HU", "HUN", // Hungary
-                                                           "IE", "IRL", // Ireland
-                                                           "IT", "ITA", // Italy
-                                                           "LV", "LVA", // Latvia
-                                                           "LT", "LTU", // Lithuania
-                                                           "LU", "LUX", // Luxembourg
-                                                           "MT", "MLT", // Malta
-                                                           "NL", "NLD", // Netherlands
-                                                           "NO", "NOR", // Norway
-                                                           "PL", "POL", // Poland
-                                                           "PT", "PRT", // Portugal
-                                                           "RO", "ROU", // Romania
-                                                           "SK", "SVK", // Slovakia
-                                                           "SI", "SVN", // Slovenia
-                                                           "ES", "ESP", // Spain
-                                                           "SE", "SWE", // Sweden
-                                                           "CH", "CHE"] // Switzerland
+                                                                   "BE", "BEL", // Belgium
+                                                                   "BG", "BGR", // Bulgaria
+                                                                   "HR", "HRV", // Croatia
+                                                                   "CY", "CYP", // Cyprus
+                                                                   "CZ", "CZE", // Czech Republic
+                                                                   "DK", "DNK", // Denmark
+                                                                   "EE", "EST", // Estonia
+                                                                   "FI", "FIN", // Finland
+                                                                   "FR", "FRA", // France
+                                                                   "DE", "DEU", // Germany
+                                                                   "GR", "GRC", // Greece
+                                                                   "HU", "HUN", // Hungary
+                                                                   "IE", "IRL", // Ireland
+                                                                   "IT", "ITA", // Italy
+                                                                   "LV", "LVA", // Latvia
+                                                                   "LT", "LTU", // Lithuania
+                                                                   "LU", "LUX", // Luxembourg
+                                                                   "MT", "MLT", // Malta
+                                                                   "NL", "NLD", // Netherlands
+                                                                   "NO", "NOR", // Norway
+                                                                   "PL", "POL", // Poland
+                                                                   "PT", "PRT", // Portugal
+                                                                   "RO", "ROU", // Romania
+                                                                   "SK", "SVK", // Slovakia
+                                                                   "SI", "SVN", // Slovenia
+                                                                   "ES", "ESP", // Spain
+                                                                   "SE", "SWE", // Sweden
+                                                                   "CH", "CHE"] // Switzerland
 }

bc73a873e438ced32f300ba8b4f3cb50603c8d87
ThomazFB <thomazfcortez@gmail.com>
2023-05-15 16:10:22 -0300

    Expand EUShippingNoticeTopbannerFactory to support different creation configurations

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/EUShippingNoticeTopBannerFactory.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/EUShippingNoticeTopBannerFactory.swift
@@ -1,18 +1,29 @@
 import Foundation
 
 final class EUShippingNoticeTopBannerFactory {
-    static func createTopBanner(onDismissPressed: @escaping () -> Void,
-                                onLearnMorePressed: @escaping () -> Void) -> TopBannerView {
+    static func createWarningBanner(onDismissPressed: @escaping () -> Void,
+                                    onLearnMorePressed: @escaping () -> Void) -> TopBannerView {
+        return createTopBanner(contentText: Localization.warningInfo, onDismissPressed: onDismissPressed, onLearnMorePressed: onLearnMorePressed)
+    }
+    
+    static func createInstructionsBanner(onDismissPressed: @escaping () -> Void,
+                                         onLearnMorePressed: @escaping () -> Void) -> TopBannerView {
+        return createTopBanner(contentText: Localization.instructionsInfo, onDismissPressed: onDismissPressed, onLearnMorePressed: onLearnMorePressed)
+    }
+    
+    private static func createTopBanner(contentText: String,
+                                        onDismissPressed: @escaping () -> Void,
+                                        onLearnMorePressed: @escaping () -> Void) -> TopBannerView {
         let learnMoreAction = TopBannerViewModel.ActionButton(title: Localization.learnMore) { _ in
             onLearnMorePressed()
         }
-
+        
         let dismissAction = TopBannerViewModel.ActionButton(title: Localization.dismiss) { _ in
             onDismissPressed()
         }
-
+        
         let viewModel = TopBannerViewModel(title: nil,
-                                           infoText: Localization.info,
+                                           infoText: contentText,
                                            icon: .infoOutlineImage,
                                            iconTintColor: .accent,
                                            isExpanded: true,
@@ -26,10 +37,15 @@ final class EUShippingNoticeTopBannerFactory {
 
 private extension EUShippingNoticeTopBannerFactory {
     enum Localization {
-        static let info = NSLocalizedString("When shipping to countries that follow European Union (EU) customs rules, " +
-                                            "you must provide a clear, specific description of every item. Otherwise, " +
-                                            "shipments may be delayed or interrupted at customs.",
-                                            comment: "The EU notice banner content describing why some countries require special customs description")
+        static let warningInfo = NSLocalizedString("When shipping to countries that follow European Union (EU) customs rules, " +
+                                                   "you must provide a clear, specific description of every item. Otherwise, " +
+                                                   "shipments may be delayed or interrupted at customs.",
+                                                   comment: "The EU notice banner content describing why some countries require special customs description")
+        static let instructionsInfo = NSLocalizedString("Shipping to countries that follow European Union (EU) customs rules now " +
+                                                        "requires you clearly describe every item. For example, if you are sending " +
+                                                        "clothing, you must indicate what type of clothing (e.g., men’s shirts, girl’s vest, boy’s jacket) " +
+                                                        "for the description to be acceptable. Otherwise, shipments may be delayed or interrupted at customs. ",
+                                                        comment: "The EU notice banner content describing how the shipping customs shall be configured")
         static let learnMore = NSLocalizedString("Learn more", comment: "Label for the banner Learn more button")
         static let dismiss = NSLocalizedString("Dismiss", comment: "Label for the banner Dismiss button")
     }

f2f9c3a2f23ba5477fb63f4736017f42b69c4010
jos <17252150+jostnes@users.noreply.github.com>
2023-05-16 11:24:12 +0800

    use annotate test failures

--- .buildkite/commands/run-ui-tests.sh
+++ .buildkite/commands/run-ui-tests.sh
@@ -31,13 +31,16 @@ bundle exec fastlane test_without_building name:"$TEST_NAME" device:"$DEVICE"
 TESTS_EXIT_STATUS=$?
 set -e
 
-if [[ "$TESTS_EXIT_STATUS" -ne 0 ]]; then
-  # Keep the (otherwise collapsed) current "Testing" section open in Buildkite logs on error. See https://buildkite.com/docs/pipelines/managing-log-output#collapsing-output
-  echo "^^^ +++"
-  echo "UI Tests failed!"
-fi
-
 echo "--- 📦 Zipping test results"
 cd fastlane/test_output/ && zip -rq WooCommerce.xcresult.zip WooCommerce.xcresult && cd -
+
+echo "--- 🚦 Report Tests Status"
+if [[ $TESTS_EXIT_STATUS -eq 0 ]]; then
+  echo "UI Tests seems to have passed (exit code 0). All good 👍"
+else
+  echo "The UI Tests, ran during the '🧪 Testing' step above, have failed."
+  echo "For more details about the failed tests, check the Buildkite annotation, the logs under the '🧪 Testing' section and the \`.xcresult\` and test reports in Buildkite artifacts."
+fi
+annotate_test_failures "fastlane/test_output/WooCommerce.xml"
 
 exit $TESTS_EXIT_STATUS

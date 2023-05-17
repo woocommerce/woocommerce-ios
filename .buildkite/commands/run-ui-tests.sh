cd6fd0abe73efe3e9e4d5ef58929c46bdb7667b3
jos <17252150+jostnes@users.noreply.github.com>
2023-05-17 22:20:12 +0800

    revert previous removal to expand testing section if fail

--- .buildkite/commands/run-ui-tests.sh
+++ .buildkite/commands/run-ui-tests.sh
@@ -31,6 +31,12 @@ bundle exec fastlane test_without_building name:"$TEST_NAME" device:"$DEVICE"
 TESTS_EXIT_STATUS=$?
 set -e
 
+if [[ "$TESTS_EXIT_STATUS" -ne 0 ]]; then
+  # Keep the (otherwise collapsed) current "Testing" section open in Buildkite logs on error. See https://buildkite.com/docs/pipelines/managing-log-output#collapsing-output
+  echo "^^^ +++"
+  echo "UI Tests failed!"
+fi
+
 echo "--- 📦 Zipping test results"
 cd fastlane/test_output/ && zip -rq WooCommerce.xcresult.zip WooCommerce.xcresult && cd -
 
@@ -38,8 +44,6 @@ echo "--- 🚦 Report Tests Status"
 if [[ $TESTS_EXIT_STATUS -eq 0 ]]; then
   echo "UI Tests seems to have passed (exit code 0). All good 👍"
 else
-  # Keep the (otherwise collapsed) current "Testing" section open in Buildkite logs on error. See https://buildkite.com/docs/pipelines/managing-log-output#collapsing-output
-  echo "^^^ +++"
   echo "The UI Tests, which ran inside the '🧪 Testing' section above in the logs, have failed."
   echo "For more details about the failed tests, check the Buildkite annotation, the logs under the '🧪 Testing' section and the \`.xcresult\` and test reports in Buildkite artifacts."
 fi

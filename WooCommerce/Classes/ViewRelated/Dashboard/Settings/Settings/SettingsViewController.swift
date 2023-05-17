5c101fc74aade0e36429c96eaec208786ebe11da
Huong Do <hi.huongdo@gmail.com>
2023-05-17 15:33:32 +0700

    Fix typo

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Settings/SettingsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Settings/SettingsViewController.swift
@@ -455,7 +455,7 @@ private extension SettingsViewController {
 
     func logOutUser() {
         Task { @MainActor in
-            // Waits to track all the canceled notification before deauthenticating or the events will not be logged.
+            // Waits to track all the canceled notifications before deauthenticating or the events will not be logged.
             await pushNotesManager.cancelAllNotifications()
             ServiceLocator.stores.deauthenticate()
             navigationController?.popToRootViewController(animated: true)

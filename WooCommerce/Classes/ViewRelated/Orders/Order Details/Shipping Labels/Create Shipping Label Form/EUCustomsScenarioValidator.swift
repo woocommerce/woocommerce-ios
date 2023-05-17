31ac0bde59fac524571fdba36a60ca5d81caeeda
Ernesto Carrión <ernesto.carrion@automattic.com>
2023-05-17 08:57:44 -0500

    Revert to countriesFollowingEUCustoms names

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/EUCustomsScenarioValidator.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/EUCustomsScenarioValidator.swift
@@ -7,7 +7,7 @@ import Yosemite
 ///
 struct EUCustomsScenarioValidator {
     static func validate(origin: ShippingLabelAddress?, destination: ShippingLabelAddress?) -> Bool {
-        usCountryISOCode.contains(origin?.country ?? "") && Country.EUCountryCodes.contains(destination?.country ?? "")
+        usCountryISOCode.contains(origin?.country ?? "") && Country.countriesFollowingEUCustoms.contains(destination?.country ?? "")
     }
 
     private static let usCountryISOCode: Set<String> = ["US", "USA"] // United States of America
@@ -16,38 +16,38 @@ struct EUCustomsScenarioValidator {
 extension Country {
     /// EU Country Code definitions.
     ///
-    static let EUCountryCodes: Set<String> = ["AT", "AUT", // Austria
-                                              "BE", "BEL", // Belgium
-                                              "BG", "BGR", // Bulgaria
-                                              "HR", "HRV", // Croatia
-                                              "CY", "CYP", // Cyprus
-                                              "CZ", "CZE", // Czech Republic
-                                              "DK", "DNK", // Denmark
-                                              "EE", "EST", // Estonia
-                                              "FI", "FIN", // Finland
-                                              "FR", "FRA", // France
-                                              "DE", "DEU", // Germany
-                                              "GR", "GRC", // Greece
-                                              "HU", "HUN", // Hungary
-                                              "IE", "IRL", // Ireland
-                                              "IT", "ITA", // Italy
-                                              "LV", "LVA", // Latvia
-                                              "LT", "LTU", // Lithuania
-                                              "LU", "LUX", // Luxembourg
-                                              "MT", "MLT", // Malta
-                                              "NL", "NLD", // Netherlands
-                                              "NO", "NOR", // Norway
-                                              "PL", "POL", // Poland
-                                              "PT", "PRT", // Portugal
-                                              "RO", "ROU", // Romania
-                                              "SK", "SVK", // Slovakia
-                                              "SI", "SVN", // Slovenia
-                                              "ES", "ESP", // Spain
-                                              "SE", "SWE", // Sweden
-                                              "CH", "CHE"] // Switzerland
+    static let countriesFollowingEUCustoms: Set<String> = ["AT", "AUT", // Austria
+                                                           "BE", "BEL", // Belgium
+                                                           "BG", "BGR", // Bulgaria
+                                                           "HR", "HRV", // Croatia
+                                                           "CY", "CYP", // Cyprus
+                                                           "CZ", "CZE", // Czech Republic
+                                                           "DK", "DNK", // Denmark
+                                                           "EE", "EST", // Estonia
+                                                           "FI", "FIN", // Finland
+                                                           "FR", "FRA", // France
+                                                           "DE", "DEU", // Germany
+                                                           "GR", "GRC", // Greece
+                                                           "HU", "HUN", // Hungary
+                                                           "IE", "IRL", // Ireland
+                                                           "IT", "ITA", // Italy
+                                                           "LV", "LVA", // Latvia
+                                                           "LT", "LTU", // Lithuania
+                                                           "LU", "LUX", // Luxembourg
+                                                           "MT", "MLT", // Malta
+                                                           "NL", "NLD", // Netherlands
+                                                           "NO", "NOR", // Norway
+                                                           "PL", "POL", // Poland
+                                                           "PT", "PRT", // Portugal
+                                                           "RO", "ROU", // Romania
+                                                           "SK", "SVK", // Slovakia
+                                                           "SI", "SVN", // Slovenia
+                                                           "ES", "ESP", // Spain
+                                                           "SE", "SWE", // Sweden
+                                                           "CH", "CHE"] // Switzerland
 
     /// GDPR Country Code definitions.
     /// *Although the UK has departed from the EU as of January 2021, the GDPR was enacted before its withdrawal and is therefore considered a valid UK law.*
     ///
-    static let GDPRCountryCodes = EUCountryCodes + ["GB"]
+    static let GDPRCountryCodes = countriesFollowingEUCustoms + ["GB"]
 }

05761a0e44992b6215b009632301bfff512c7918
Josh Heald <joshheald@users.noreply.github.com>
2023-05-18 10:07:59 +0100

    9735 Set test site address in US for correct config

--- WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Settings/In-Person Payments/TapToPayReconnectionControllerTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Settings/In-Person Payments/TapToPayReconnectionControllerTests.swift
@@ -7,6 +7,7 @@ import Hardware
 final class TapToPayReconnectionControllerTests: XCTestCase {
 
     private var stores: MockStoresManager!
+    private var storageManager: MockStorageManager!
     private var connectionControllerFactory: MockBuiltInCardReaderConnectionControllerFactory!
     private var sut: TapToPayReconnectionController!
     private let sampleSiteID: Int64 = 12891
@@ -19,6 +20,25 @@ final class TapToPayReconnectionControllerTests: XCTestCase {
         connectionControllerFactory = MockBuiltInCardReaderConnectionControllerFactory()
         sut = TapToPayReconnectionController(stores: stores,
                                              connectionControllerFactory: connectionControllerFactory)
+        storageManager = MockStorageManager()
+        setSiteAddressCountry()
+    }
+
+    func setSiteAddressCountry() {
+        ServiceLocator.setSelectedSiteSettings(SelectedSiteSettings(stores: stores, storageManager: storageManager))
+        let setting = SiteSetting.fake()
+            .copy(
+                siteID: sampleSiteID,
+                settingID: "woocommerce_default_country",
+                value: "US:NY",
+                settingGroupKey: SiteSettingGroup.general.rawValue
+            )
+        storageManager.insertSampleSiteSetting(readOnlySiteSetting: setting)
+        ServiceLocator.selectedSiteSettings.refresh()
+    }
+
+    override func tearDown() {
+        ServiceLocator.setSelectedSiteSettings(SelectedSiteSettings())
     }
 
     func test_reconnectIfNeeded_calls_searchAndConnect_if_no_reader_connected_and_site_and_device_meet_requirements() throws {

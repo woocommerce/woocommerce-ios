8f19d4422162fc8139930548a3f8cdbe628f5ec6
Spencer Transier <spencer@transier.us>
2023-05-19 23:35:19 -0700

    Use localization key instead of English copy as key for long EU customs string

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/EUShippingNoticeTopBannerFactory.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/EUShippingNoticeTopBannerFactory.swift
@@ -48,10 +48,11 @@ extension EUShippingNoticeTopBannerFactory {
                                                    "you must provide a clear, specific description of every item. Otherwise, " +
                                                    "shipments may be delayed or interrupted at customs.",
                                                    comment: "The EU notice banner content describing why some countries require special customs description")
-        static let instructionsInfo = NSLocalizedString("Shipping to countries that follow European Union (EU) customs rules now " +
+        static let instructionsInfo = NSLocalizedString("eu_shipping_instructions_info",
+                                                        value: "Shipping to countries that follow European Union (EU) customs rules now " +
                                                         "requires you clearly describe every item. For example, if you are sending " +
                                                         "clothing, you must indicate what type of clothing (e.g., men’s shirts, girl’s vest, boy’s jacket) " +
-                                                        "for the description to be acceptable. Otherwise, shipments may be delayed or interrupted at customs. ",
+                                                        "for the description to be acceptable. Otherwise, shipments may be delayed or interrupted at customs.",
                                                         comment: "The EU notice banner content describing how the shipping customs shall be configured")
         static let learnMore = NSLocalizedString("Learn more", comment: "Label for the banner Learn more button")
         static let dismiss = NSLocalizedString("Dismiss", comment: "Label for the banner Dismiss button")

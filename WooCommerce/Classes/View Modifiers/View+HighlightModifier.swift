f553a6da471573a746653d051bb6dc3c70b5149d
ThomazFB <thomazfcortez@gmail.com>
2023-05-22 19:55:24 -0300

    Introduce View+HighlightModifier file

---
+++ WooCommerce/Classes/View Modifiers/View+HighlightModifier.swift
@@ -0,0 +1,21 @@
+//
+//  View+HighlightModifier.swift
+//  WooCommerce
+//
+//  Created by Thomaz F B Cortez on 22/05/23.
+//  Copyright © 2023 Automattic. All rights reserved.
+//
+
+import SwiftUI
+
+struct View_HighlightModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct View_HighlightModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        View_HighlightModifier()
+    }
+}

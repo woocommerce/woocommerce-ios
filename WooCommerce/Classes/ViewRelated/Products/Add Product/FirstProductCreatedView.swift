f736175ed0849b3aeccea846ddb56b3525154345
Huong Do <hi.huongdo@gmail.com>
2023-05-22 10:55:21 +0700

    Add a new view for celebrating first product

---
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/FirstProductCreatedView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// Celebratory screen after creating the first product 🎉
+///
+struct FirstProductCreatedView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct FirstProductCreatedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstProductCreatedView()
+    }
+}

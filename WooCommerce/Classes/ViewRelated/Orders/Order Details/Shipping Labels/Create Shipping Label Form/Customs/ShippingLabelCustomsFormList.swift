4331b3d6052deacbd926a610a08ede18dd093477
ThomazFB <thomazfcortez@gmail.com>
2023-05-22 19:44:51 -0300

    Introduce base blinking behavior modifier

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Customs/ShippingLabelCustomsFormList.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Customs/ShippingLabelCustomsFormList.swift
@@ -95,6 +95,39 @@ struct ShippingLabelCustomsFormList: View {
     }
 }
 
+struct BlinkingBorderModifier: ViewModifier {
+    let state: Binding<Bool>
+    let color: Color
+    let repeatCount: Int
+    let duration: Double
+
+    // internal wrapper is needed because there is no didFinish of Animation now
+    private var blinking: Binding<Bool> {
+        Binding<Bool>(get: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + self.duration) {
+                self.state.wrappedValue = false
+            }
+            return self.state.wrappedValue }, set: {
+            self.state.wrappedValue = $0
+        })
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .border(self.blinking.wrappedValue ? self.color : Color.clear, width: 3.0)
+            .animation(Animation.linear(duration: self.duration).repeatCount(self.repeatCount),
+                       value: blinking.wrappedValue)
+    }
+}
+
+extension View {
+    func blinkBorder(on state: Binding<Bool>, color: Color,
+                     repeatCount: Int = 3, duration: Double = 0.5) -> some View {
+        self.modifier(BlinkingBorderModifier(state: state, color: color,
+                                             repeatCount: repeatCount, duration: duration))
+    }
+}
+
 private extension ShippingLabelCustomsFormList {
     enum Localization {
         static let navigationTitle = NSLocalizedString("Customs", comment: "Navigation title for Customs screen in Shipping Label flow")

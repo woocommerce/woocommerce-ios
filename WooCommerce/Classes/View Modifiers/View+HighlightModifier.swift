d37b93fa7ec62a3b72c5801f2bfccb382ca5cbfe
ThomazFB <thomazfcortez@gmail.com>
2023-05-22 20:04:20 -0300

    Refactor and add documentation to HighlightModifier implementation

--- WooCommerce/Classes/View Modifiers/View+HighlightModifier.swift
+++ WooCommerce/Classes/View Modifiers/View+HighlightModifier.swift
@@ -1,33 +1,39 @@
 import SwiftUI
 
+/// Casts a blinking border highlight into any view given a Published `state` to control it's visibility
+///
 struct HighlightModifier: ViewModifier {
-    let state: Binding<Bool>
+    let highlighted: Binding<Bool>
     let color: Color
     let repeatCount: Int
     let duration: Double
 
-    private var blinking: Binding<Bool> {
+    private var animationActive: Binding<Bool> {
         Binding<Bool>(get: {
             DispatchQueue.main.asyncAfter(deadline: .now() + self.duration) {
-                self.state.wrappedValue = false
+                self.highlighted.wrappedValue = false
             }
-            return self.state.wrappedValue }, set: {
-            self.state.wrappedValue = $0
+            return self.highlighted.wrappedValue
+        }, set: {
+            self.highlighted.wrappedValue = $0
         })
     }
 
     func body(content: Content) -> some View {
         content
-            .border(self.blinking.wrappedValue ? self.color : Color.clear, width: 3.0)
+            .border(self.animationActive.wrappedValue ? self.color : Color.clear, width: 3.0)
             .animation(Animation.linear(duration: self.duration).repeatCount(self.repeatCount),
-                       value: blinking.wrappedValue)
+                       value: animationActive.wrappedValue)
     }
 }
 
+// MARK: View extension
 extension View {
-    func highlight(on state: Binding<Bool>, color: Color,
+    func highlight(on highlighted: Binding<Bool>, color: Color,
                      repeatCount: Int = 3, duration: Double = 0.5) -> some View {
-        self.modifier(HighlightModifier(state: state, color: color,
-                                             repeatCount: repeatCount, duration: duration))
+        self.modifier(HighlightModifier(highlighted: highlighted,
+                                        color: color,
+                                        repeatCount: repeatCount,
+                                        duration: duration))
     }
 }

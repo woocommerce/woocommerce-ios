f3ee5363b7d68d368fd31b640fa4b2ace7f95ccb
ThomazFB <thomazfcortez@gmail.com>
2023-05-22 19:55:34 -0300

    Configure HighlightModifier

--- WooCommerce/Classes/View Modifiers/View+HighlightModifier.swift
+++ WooCommerce/Classes/View Modifiers/View+HighlightModifier.swift
@@ -1,21 +1,33 @@
-//
-//  View+HighlightModifier.swift
-//  WooCommerce
-//
-//  Created by Thomaz F B Cortez on 22/05/23.
-//  Copyright © 2023 Automattic. All rights reserved.
-//
-
 import SwiftUI
 
-struct View_HighlightModifier: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct HighlightModifier: ViewModifier {
+    let state: Binding<Bool>
+    let color: Color
+    let repeatCount: Int
+    let duration: Double
+
+    private var blinking: Binding<Bool> {
+        Binding<Bool>(get: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + self.duration) {
+                self.state.wrappedValue = false
+            }
+            return self.state.wrappedValue }, set: {
+            self.state.wrappedValue = $0
+        })
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .border(self.blinking.wrappedValue ? self.color : Color.clear, width: 3.0)
+            .animation(Animation.linear(duration: self.duration).repeatCount(self.repeatCount),
+                       value: blinking.wrappedValue)
     }
 }
 
-struct View_HighlightModifier_Previews: PreviewProvider {
-    static var previews: some View {
-        View_HighlightModifier()
+extension View {
+    func highlight(on state: Binding<Bool>, color: Color,
+                     repeatCount: Int = 3, duration: Double = 0.5) -> some View {
+        self.modifier(HighlightModifier(state: state, color: color,
+                                             repeatCount: repeatCount, duration: duration))
     }
 }

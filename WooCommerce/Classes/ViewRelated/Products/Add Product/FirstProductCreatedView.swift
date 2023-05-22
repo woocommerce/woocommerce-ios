7512de018d88c6bcdd56df4f5350319b3b08df3e
Huong Do <hi.huongdo@gmail.com>
2023-05-22 11:50:51 +0700

    Add UI for the celebratory screen

--- WooCommerce/Classes/ViewRelated/Products/Add Product/FirstProductCreatedView.swift
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/FirstProductCreatedView.swift
@@ -4,12 +4,51 @@ import SwiftUI
 ///
 struct FirstProductCreatedView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: Layout.verticalSpacing) {
+            Spacer()
+            VStack {
+                Text(Localization.title)
+                    .titleStyle()
+                Text(Localization.message)
+                    .secondaryBodyStyle()
+            }
+            Image(uiImage: .welcomeImage)
+            Button(Localization.shareAction) {
+                // TODO
+            }
+            .buttonStyle(PrimaryButtonStyle())
+            Spacer()
+        }
+        .padding()
+        .background(Color(uiColor: .systemBackground))
+    }
+}
+
+private extension FirstProductCreatedView {
+    enum Layout {
+        static let verticalSpacing: CGFloat = 32
+    }
+    enum Localization {
+        static let title = NSLocalizedString(
+            "Congratulations 🎉",
+            comment: "Title of the celebratory screen after creating the first product"
+        )
+        static let message = NSLocalizedString(
+            "Great work on your first product!",
+            comment: "Message on the celebratory screen after creating first product"
+        )
+        static let shareAction = NSLocalizedString(
+            "Spread the word",
+            comment: "Title of the action button to share the first created product"
+        )
     }
 }
 
 struct FirstProductCreatedView_Previews: PreviewProvider {
     static var previews: some View {
         FirstProductCreatedView()
+            .environment(\.colorScheme, .light)
+        FirstProductCreatedView()
+            .environment(\.colorScheme, .dark)
     }
 }

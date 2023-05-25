31e3f04ceb828051b7d2f45ba738cdd19ff7009c
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-05-25 13:06:39 +0800

    Add fallback for retrieving variations via ID

--- WooCommerce/Classes/ViewRelated/Orders/Order Creation/EditableOrderViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/EditableOrderViewModel.swift
@@ -1327,7 +1327,7 @@ extension EditableOrderViewModel {
     func updateOrderWithProductID(_ productID: Int64) {
         guard currentOrderItems.contains(where: { $0.productOrVariationID == productID }) else {
             // If it's not part of the current order, send the correct productType to the synchronizer
-            if let productVariation = allProductVariations.first(where: { $0.productVariationID == productID }) {
+            if let productVariation = retrieveVariation(for: productID) {
                 orderSynchronizer.setProduct.send(.init(product: .variation(productVariation), quantity: 1))
             } else if let product = allProducts.first(where: { $0.productID == productID }) {
                 orderSynchronizer.setProduct.send(.init(product: .product(product), quantity: 1))
@@ -1398,6 +1398,14 @@ private extension EditableOrderViewModel {
     func retrieveVariation(for item: OrderItem) -> ProductVariation? {
         guard let variation = allProductVariations.first(where: { $0.productVariationID == item.variationID }) else {
             let product = allProducts.first(where: { $0.productID == item.variationID })
+            return product?.toProductVariation()
+        }
+        return variation
+    }
+
+    func retrieveVariation(for id: Int64) -> ProductVariation? {
+        guard let variation = allProductVariations.first(where: { $0.productVariationID == id }) else {
+            let product = allProducts.first(where: { $0.productID == id })
             return product?.toProductVariation()
         }
         return variation

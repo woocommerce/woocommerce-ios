e99ae592fbf61470404a8b95b32dda9cfd2ea4e9
Rachel McRoberts <rachel@automattic.com>
2023-05-30 11:00:54 +0100

    Add fallback when shipping line ID field value is a string

--- Networking/Networking/Model/ShippingLineTax.swift
+++ Networking/Networking/Model/ShippingLineTax.swift
@@ -24,6 +24,22 @@ public struct ShippingLineTax: Decodable, Hashable, GeneratedFakeable {
         self.subtotal = subtotal
         self.total = total
     }
+
+    /// The public initializer for ShippingLineTax.
+    ///
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        // Even though a plain install of WooCommerce Core provides Int values,
+        // some plugins alter the field value from Int to String.
+        let taxID = container.failsafeDecodeIfPresent(targetType: Int64.self,
+                                                       forKey: .taxID,
+                                                       alternativeTypes: [.string(transform: { Int64($0) ?? 0 })]) ?? 0
+        let subtotal = try container.decode(String.self, forKey: .subtotal)
+        let total = try container.decode(String.self, forKey: .total)
+
+        self.init(taxID: taxID, subtotal: subtotal, total: total)
+    }
 }
 
 /// Defines all of the ShippingLineTax CodingKeys.

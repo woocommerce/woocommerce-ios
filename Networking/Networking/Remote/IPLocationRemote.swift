b1f396f519d22885a431eac0625f2626c1283b26
Ernesto Carrión <ernesto.carrion@automattic.com>
2023-05-30 16:25:06 -0500

    Add documentation

--- Networking/Networking/Remote/IPLocationRemote.swift
+++ Networking/Networking/Remote/IPLocationRemote.swift
@@ -4,10 +4,11 @@ import Foundation
 ///
 public final class IPLocationRemote: Remote {
 
+    /// Fetches the country code from the device ip.
+    ///
     public func getIPCountryCode(onCompletion: @escaping (Result<String, Error>) -> Void) {
-
         guard let url = URL(string: "https://ipinfo.io/json") else {
-            return // DO SOMETHING
+            return onCompletion(.failure(IPLocationError.malformedURL)) // Should not happen.
         }
 
         let request = UnauthenticatedRequest(request: .init(url: url))
@@ -16,8 +17,20 @@ public final class IPLocationRemote: Remote {
     }
 }
 
+/// `IPLocationRemote` known errors
+///
+public extension IPLocationRemote {
+    enum IPLocationError: Error {
+        case malformedURL
+    }
+}
+
+/// Private mapper used to extract the country code from the `IPLocationRemote` response.
+///
 private struct IPCountryCodeMapper: Mapper {
 
+    /// Response envelope
+    ///
     struct Response: Decodable {
         enum CodingKeys: String, CodingKey {
             case countryCode = "country"

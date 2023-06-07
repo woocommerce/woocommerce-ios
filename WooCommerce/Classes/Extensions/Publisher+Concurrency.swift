28fa4083543b1c620aff7f94bdf9aa3af479553e
Jaclyn Chen <watertranquil@gmail.com>
2023-06-07 21:59:44 +0800

    Rename `Publisher.asyncMap` to `Publisher.tryAsyncMap` so that `Publisher.asyncMap` no longer throws an error.

--- WooCommerce/Classes/Extensions/Publisher+Concurrency.swift
+++ WooCommerce/Classes/Extensions/Publisher+Concurrency.swift
@@ -1,6 +1,24 @@
 import Combine
 
 extension Publisher {
+    /// Transforms the publisher with an async operator that does not throw an error.
+    ///
+    /// Original implementation:
+    /// https://www.swiftbysundell.com/articles/calling-async-functions-within-a-combine-pipeline
+    ///
+    /// - Parameter transform: a function that transforms the upstream publisher asynchronously that does not throw an error.
+    /// - Returns: a new publisher that is transformed by the given operator asynchronously.
+    func asyncMap<T>(_ transform: @escaping (Output) async -> T) ->
+    Publishers.FlatMap<Future<T, Never>, Self> {
+                           flatMap { value in
+                               Future { promise in
+                                   Task {
+                                       promise(.success(await transform(value)))
+                                   }
+                               }
+                           }
+                       }
+
     /// Transforms the publisher with an async throwable operator.
     ///
     /// Original implementation:
@@ -8,7 +26,7 @@ extension Publisher {
     ///
     /// - Parameter transform: a function that transforms the upstream publisher asynchronously with the option to throw an error.
     /// - Returns: a new publisher that is transformed by the given operator asynchronously.
-    func asyncMap<T>(_ transform: @escaping (Output) async throws -> T) ->
+    func tryAsyncMap<T>(_ transform: @escaping (Output) async throws -> T) ->
     Publishers.FlatMap<Future<T, Error>,
                        Publishers.SetFailureType<Self, Error>> {
                            flatMap { value in

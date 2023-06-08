626ab065fa3e702c0b171cc15ca1771064b56677
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2023-06-08 09:26:12 +0200

    Tweak logic and comments

--- WooCommerce/Classes/ViewRelated/Products/SKU Scanner/BarcodeSKUScannerProductFinder.swift
+++ WooCommerce/Classes/ViewRelated/Products/SKU Scanner/BarcodeSKUScannerProductFinder.swift
@@ -2,7 +2,7 @@ import Foundation
 import Yosemite
 import Vision
 
-/// Given a scanned barcode searches for the matching product, refining the barcode if necessary to handle the format exceptions
+/// Given a scanned barcode this struct searches for the matching product, refining the barcode if necessary to handle the format exceptions
 ///
 struct BarcodeSKUScannerProductFinder {
     private let stores: StoresManager
@@ -15,12 +15,12 @@ struct BarcodeSKUScannerProductFinder {
         do {
             return try await retrieveProduct(from: barcode.payloadStringValue, siteID: siteID)
         } catch {
-            // If we couldn't find the product, let's do tries by refining the SKU search
+            // If we couldn't find the product, let's keep trying by refining the SKU search
             guard (error as? ProductLoadError) == .notFound else {
                 throw error
             }
 
-            // Re-start the search in case we can remove the country code (Apple adds it by default, but merchants might not have it)
+            // Re-start the search in case we can remove the country code (Apple adds it by default for UPC-A, but merchants might not have it)
             if let refinedBarcode = barcode.removeCountryCodeIfPossible() {
                 return try await findProduct(from: refinedBarcode, siteID: siteID)
             } else if let refinedBarcode = barcode.removeCheckDigitIfPossible() {
@@ -61,9 +61,10 @@ private extension ScannedBarcode {
     }
 
     func removeCountryCodeIfPossible() -> ScannedBarcode? {
-        // When we have an 12 digit UPC-A format barcode Apple adds a zero at the beginning and returns an EAN-13 format
+        // When we have an 12 digit UPC-A format barcode Apple adds a zero at the beginning as the country code and returns an EAN-13 format
         // See https://nationwidebarcode.com/are-upc-a-and-ean-13-the-same/
         guard symbology == VNBarcodeSymbology.ean13,
+              payloadStringValue.characterCount == 13,
               payloadStringValue.hasPrefix("0") else {
             return nil
         }

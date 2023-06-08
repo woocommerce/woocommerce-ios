1003b138b8be356a1e1dd723b217d4177963ecb2
Rachel McRoberts <rachel@automattic.com>
2023-06-08 19:23:05 +0100

    Use notification center to track when app startup actions are completed

--- WooCommerce/Classes/Analytics/AppStartupWaitingTimeTracker.swift
+++ WooCommerce/Classes/Analytics/AppStartupWaitingTimeTracker.swift
@@ -10,21 +10,71 @@ final class AppStartupWaitingTimeTracker: WaitingTimeTracker {
     ///
     private var appStartupWaitingActions = AppStartupAction.allCases
 
+    /// NotificationCenter Tokens
+    ///
+    private var trackingObservers: [NSObjectProtocol]?
+
+    /// NotificationCenter
+    ///
+    private let notificationCenter: NotificationCenter
+
     /// All actions that must be waited for on app startup.
     ///
     enum AppStartupAction: CaseIterable {
-        case appCoordinatorStart
+        case validateRoleEligibility
+        case checkFeatureAnnouncements
         case restoreSessionSite
         case syncEntities
-        case checkWooVersion
+        case checkMinimumWooVersion
         case syncDashboardStats
-        case syncJITMs
+        case syncTopPerformers
         case fetchExperimentAssignments
-        case mainTabBarInit
+        case syncJITMs
+        case syncPaymentConnectionTokens
+
+        var notificationName: NSNotification.Name {
+            switch self {
+            case .validateRoleEligibility:
+                return .RoleEligibilityValidated
+            case .checkFeatureAnnouncements:
+                return .FeatureAnnouncementsChecked
+            case .restoreSessionSite:
+                return .SessionSiteRestored
+            case .syncEntities:
+                return .EntitiesSynchronized
+            case .checkMinimumWooVersion:
+                return .MinimumWooVersionChecked
+            case .syncDashboardStats:
+                return .DashboardStatsSynced
+            case .syncTopPerformers:
+                return .TopPerformersSynced
+            case .fetchExperimentAssignments:
+                return .ExperimentAssignmentsFetched
+            case .syncJITMs:
+                return .JITMsSynced
+            case .syncPaymentConnectionTokens:
+                return .PaymentConnectionTokensSynced
+            }
+        }
     }
 
-    init() {
+    init(notificationCenter: NotificationCenter = .default) {
+        self.notificationCenter = notificationCenter
+
         super.init(trackScenario: .appStartup)
+
+        startListeningToNotifications()
+    }
+
+    /// Starts listening for Notifications
+    ///
+    func startListeningToNotifications() {
+        for startupAction in AppStartupAction.allCases {
+            let observer = notificationCenter.addObserver(forName: startupAction.notificationName, object: nil, queue: .main) { [weak self] _ in
+                self?.end(startupAction)
+            }
+            trackingObservers?.append(observer)
+        }
     }
 
     /// End the waiting time for the provided startup action.
@@ -41,4 +91,19 @@ final class AppStartupWaitingTimeTracker: WaitingTimeTracker {
             // TODO: Call super.end() to fire the analytics event
         }
     }
+}
+
+// MARK: App Startup Notifications
+//
+extension NSNotification.Name {
+    static let RoleEligibilityValidated = NSNotification.Name(rawValue: "RoleEligibilityValidated")
+    static let FeatureAnnouncementsChecked = NSNotification.Name(rawValue: "FeatureAnnouncementsChecked")
+    static let SessionSiteRestored = NSNotification.Name(rawValue: "SessionSiteRestored")
+    static let EntitiesSynchronized = NSNotification.Name(rawValue: "EntitiesSynchronized")
+    static let MinimumWooVersionChecked = NSNotification.Name(rawValue: "MinimumWooVersionChecked")
+    static let DashboardStatsSynced = NSNotification.Name(rawValue: "DashboardStatsSynced")
+    static let TopPerformersSynced = NSNotification.Name(rawValue: "TopPerformersSynced")
+    static let ExperimentAssignmentsFetched = NSNotification.Name(rawValue: "ExperimentAssignmentsFetched")
+    static let JITMsSynced = NSNotification.Name(rawValue: "JITMsSynced")
+    static let PaymentConnectionTokensSynced = NSNotification.Name(rawValue: "PaymentConnectionTokensSynced")
 }

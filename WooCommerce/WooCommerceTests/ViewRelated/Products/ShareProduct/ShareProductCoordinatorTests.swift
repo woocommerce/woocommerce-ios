29b96ca835f6f5ac77b0158b8f9df9e2daf25462
Huong Do <hi.huongdo@gmail.com>
2023-06-09 17:36:57 +0700

    Update unit test for ShareProductCoordinator

--- WooCommerce/WooCommerceTests/ViewRelated/Products/ShareProduct/ShareProductCoordinatorTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/ShareProduct/ShareProductCoordinatorTests.swift
@@ -5,6 +5,8 @@ final class ShareProductCoordinatorTests: XCTestCase {
 
     private var navigationController: UINavigationController!
     private let productPath = "https://example.com"
+    private var analyticsProvider: MockAnalyticsProvider!
+    private var analytics: WooAnalytics!
 
     override func setUp() {
         navigationController = UINavigationController()
@@ -13,11 +15,16 @@ final class ShareProductCoordinatorTests: XCTestCase {
         window.rootViewController = UIViewController()
         window.makeKeyAndVisible()
         window.rootViewController = navigationController
+
+        analyticsProvider = MockAnalyticsProvider()
+        analytics = WooAnalytics(analyticsProvider: analyticsProvider)
         super.setUp()
     }
 
     override func tearDown() {
         navigationController = nil
+        analytics = nil
+        analyticsProvider = nil
         super.tearDown()
     }
 
@@ -40,4 +47,44 @@ final class ShareProductCoordinatorTests: XCTestCase {
         }
     }
 
+    func test_AI_sheet_is_displayed_when_AI_is_available() throws {
+        // Given
+        let checker = MockShareProductAIEligibilityChecker(canGenerateShareProductMessageUsingAI: true)
+        let coordinator = ShareProductCoordinator(siteID: 123,
+                                                  productURL: try XCTUnwrap(URL(string: productPath)),
+                                                  productName: "Test",
+                                                  shareSheetAnchorItem: UIBarButtonItem(systemItem: .done),
+                                                  shareProductEligibilityChecker: checker,
+                                                  navigationController: navigationController)
+
+        // When
+        coordinator.start()
+
+        // Then
+        waitUntil {
+            self.navigationController.presentedViewController is ProductSharingMessageGenerationHostingController
+        }
+    }
+
+    func test_analytics_is_tracked_when_AI_sheet_is_displayed() throws {
+        // Given
+        let checker = MockShareProductAIEligibilityChecker(canGenerateShareProductMessageUsingAI: true)
+        let coordinator = ShareProductCoordinator(siteID: 123,
+                                                  productURL: try XCTUnwrap(URL(string: productPath)),
+                                                  productName: "Test",
+                                                  shareSheetAnchorItem: UIBarButtonItem(systemItem: .done),
+                                                  shareProductEligibilityChecker: checker,
+                                                  navigationController: navigationController,
+                                                  analytics: analytics)
+
+        // When
+        coordinator.start()
+        waitUntil {
+            self.navigationController.presentedViewController is ProductSharingMessageGenerationHostingController
+        }
+
+        // Then
+        let firstEvent = try XCTUnwrap(analyticsProvider.receivedEvents.first)
+        XCTAssertEqual(firstEvent, "product_sharing_ai_displayed")
+    }
 }

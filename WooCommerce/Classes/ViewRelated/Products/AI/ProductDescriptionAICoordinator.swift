26b93109e45335433c9fd391e21fb6ad04879fb4
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-06-14 16:09:26 +0530

    Add a helper to build bottom sheet.

--- WooCommerce/Classes/ViewRelated/Products/AI/ProductDescriptionAICoordinator.swift
+++ WooCommerce/Classes/ViewRelated/Products/AI/ProductDescriptionAICoordinator.swift
@@ -12,7 +12,7 @@ final class ProductDescriptionAICoordinator: Coordinator {
     private let analytics: Analytics
     private let onApply: (ProductDescriptionGenerationOutput) -> Void
 
-    private var bottomSheetPresenter: BottomSheetPresenter?
+    private var productDescriptionGenerationBottomSheetPresenter: BottomSheetPresenter?
 
     init(product: ProductFormDataModel,
          navigationController: UINavigationController,
@@ -34,14 +34,7 @@ final class ProductDescriptionAICoordinator: Coordinator {
 // MARK: Navigation
 private extension ProductDescriptionAICoordinator {
     func presentAIBottomSheet() {
-        let presenter = BottomSheetPresenter(configure: { bottomSheet in
-            var sheet = bottomSheet
-            sheet.prefersEdgeAttachedInCompactHeight = true
-            sheet.largestUndimmedDetentIdentifier = .none
-            sheet.prefersGrabberVisible = true
-            sheet.detents = [.medium(), .large()]
-        })
-        bottomSheetPresenter = presenter
+        productDescriptionGenerationBottomSheetPresenter = buildBottomSheetPresenter()
 
         let controller = ProductDescriptionGenerationHostingController(viewModel:
                 .init(siteID: product.siteID,
@@ -54,11 +47,25 @@ private extension ProductDescriptionAICoordinator {
         }))
 
         navigationController.view.endEditing(true)
-        presenter.present(controller, from: navigationController)
+        productDescriptionGenerationBottomSheetPresenter?.present(controller, from: navigationController)
         analytics.track(event: .ProductFormAI.productDescriptionAIButtonTapped(source: source))
     }
 
     func dismissDescriptionGenerationBottomSheetIfNeeded() {
-        bottomSheetPresenter?.dismiss(onDismiss: {})
+        productDescriptionGenerationBottomSheetPresenter?.dismiss(onDismiss: {})
+    }
+}
+
+// MARK: Bottom sheet helpers
+//
+private extension ProductDescriptionAICoordinator {
+    func buildBottomSheetPresenter() -> BottomSheetPresenter {
+        BottomSheetPresenter(configure: { bottomSheet in
+            var sheet = bottomSheet
+            sheet.prefersEdgeAttachedInCompactHeight = true
+            sheet.largestUndimmedDetentIdentifier = .none
+            sheet.prefersGrabberVisible = true
+            sheet.detents = [.medium(), .large()]
+        })
     }
 }

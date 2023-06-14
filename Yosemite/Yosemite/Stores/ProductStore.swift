e230730e45357a5c7611d7c5018178d55edbdc64
Huong Do <hi.huongdo@gmail.com>
2023-06-14 11:52:58 +0700

    Update prompts to better identify language

--- Yosemite/Yosemite/Stores/ProductStore.swift
+++ Yosemite/Yosemite/Stores/ProductStore.swift
@@ -518,9 +518,9 @@ private extension ProductStore {
                                     features: String,
                                     completion: @escaping (Result<String, Error>) -> Void) {
         let prompt = [
-            "Write a product description for a product with title: \"\(name)\". More features about the product: \(features)",
+            "Write a description for a product with title \(name) and features: \(features).",
             "Some requirements for the description are:",
-            "- Identify the language used in the product title and features, and use the same language in your response.",
+            "- Identify the language used in the product title and features and use the same language in your response.",
             "- Make the description 50-60 words or less.",
             "- Use a 9th grade reading level."
         ].joined(separator: "\n")
@@ -538,11 +538,11 @@ private extension ProductStore {
                                        description: String,
                                        completion: @escaping (Result<String, Error>) -> Void) {
         let prompt = [
-            "Your task is to help a merchant create a message to share with their customers a product named \"\(name)\". More information about the product:",
+            "Your task is to help a merchant create a message to share with their customers a product named \(name). More information about the product:",
             "Product description: \"\(description)\"",
             "Product URL: \(url)",
             "Some requirements for the message are: ",
-            "- Identify the language used in the product name, and use the same language in your response.",
+            "- Identify the language used in the product name and use the same language in your response.",
             "- The length should be up to 3 sentences.",
             "- Use a 9th grade reading level.",
             "- Add related hashtags at the end of the message.",

6f7bc23b2463f52f29b2509778a747f9cc067104
Josh Heald <joshheald@users.noreply.github.com>
2023-06-15 10:12:24 +0100

    Add `isSiteOwner` to Site+ReadOnlyConvertible

--- Yosemite/Yosemite/Model/Storage/Site+ReadOnlyConvertible.swift
+++ Yosemite/Yosemite/Model/Storage/Site+ReadOnlyConvertible.swift
@@ -15,6 +15,7 @@ extension Storage.Site: ReadOnlyConvertible {
         url = site.url
         adminURL = site.adminURL
         loginURL = site.loginURL
+        isSiteOwner = site.isSiteOwner
         frameNonce = site.frameNonce
 //        plan = site.plan // We're not assigning the plan here because it's not sent on the intial API request.
         isJetpackThePluginInstalled = site.isJetpackThePluginInstalled
@@ -36,7 +37,7 @@ extension Storage.Site: ReadOnlyConvertible {
                     url: url ?? "",
                     adminURL: adminURL ?? "",
                     loginURL: loginURL ?? "",
-                    isSiteOwner: false,
+                    isSiteOwner: isSiteOwner,
                     frameNonce: frameNonce ?? "",
                     plan: plan ?? "",
                     isJetpackThePluginInstalled: isJetpackThePluginInstalled,

1e2712aadb954a4fb8fc16c81c5078add0693050
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-06-19 20:07:31 +0530

    Add extension to provide inverted colors for tool tip.

---
+++ WooCommerce/Classes/Extensions/UIColor+Tooltip.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+extension UIColor {
+    static var invertedSystem5: UIColor {
+        return UIColor(light: UIColor.systemGray5.color(for: UITraitCollection(userInterfaceStyle: .dark)),
+                       dark: UIColor.systemGray5.color(for: UITraitCollection(userInterfaceStyle: .light)))
+    }
+
+    static var invertedLabel: UIColor {
+        return UIColor(light: UIColor.label.color(for: UITraitCollection(userInterfaceStyle: .dark)),
+                       dark: UIColor.label.color(for: UITraitCollection(userInterfaceStyle: .light)))
+    }
+
+    static var invertedSecondaryLabel: UIColor {
+        return UIColor(light: UIColor.secondaryLabel.color(for: UITraitCollection(userInterfaceStyle: .dark)),
+                       dark: UIColor.secondaryLabel.color(for: UITraitCollection(userInterfaceStyle: .light)))
+    }
+
+    static var invertedLink: UIColor {
+        UIColor(light: .wooCommercePurple(.shade30), dark: .wooCommercePurple(.shade50))
+    }
+
+    static var invertedSeparator: UIColor {
+        return UIColor(light: UIColor.separator.color(for: UITraitCollection(userInterfaceStyle: .dark)),
+                       dark: UIColor.separator.color(for: UITraitCollection(userInterfaceStyle: .light)))
+    }
+}

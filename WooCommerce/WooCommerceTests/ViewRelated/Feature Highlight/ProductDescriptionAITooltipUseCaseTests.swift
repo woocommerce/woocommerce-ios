1ecd44ea9909d497df48e4a7208beb3a9c9c8002
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-06-21 12:12:04 +0530

    `ProductDescriptionAITooltipUseCase` - Unit test tool tip display condition logic.

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Feature Highlight/ProductDescriptionAITooltipUseCaseTests.swift
@@ -0,0 +1,84 @@
+import XCTest
+@testable import WooCommerce
+
+final class ProductDescriptionAITooltipUseCaseTests: XCTestCase {
+    // MARK: isDescriptionAIEnabled
+    func test_shouldShowTooltip_is_true_only_when_isDescriptionAIEnabled_is_true() {
+        var sut = ProductDescriptionAITooltipUseCase(isDescriptionAIEnabled: true,
+                                                     userDefaults: MockUserDefaults())
+        sut.hasDismissedWriteWithAITooltip = false
+        sut.numberOfTimesWriteWithAITooltipIsShown = 1
+
+        XCTAssertTrue(sut.shouldShowTooltip)
+    }
+
+    func test_shouldShowTooltip_is_true_when_isDescriptionAIEnabled_is_false() {
+        var sut = ProductDescriptionAITooltipUseCase(isDescriptionAIEnabled: false,
+                                                     userDefaults: MockUserDefaults())
+        sut.hasDismissedWriteWithAITooltip = false
+        sut.numberOfTimesWriteWithAITooltipIsShown = 1
+
+        XCTAssertFalse(sut.shouldShowTooltip)
+    }
+
+    // MARK: Counter
+
+    func test_shouldShowTooltip_is_true_only_when_counter_is_below_3() {
+        var sut = ProductDescriptionAITooltipUseCase(isDescriptionAIEnabled: true,
+                                                     userDefaults: MockUserDefaults())
+        sut.hasDismissedWriteWithAITooltip = false
+        sut.numberOfTimesWriteWithAITooltipIsShown = 2
+
+        XCTAssertTrue(sut.shouldShowTooltip)
+    }
+
+    func test_shouldShowTooltip_is_false_when_counter_is_3() {
+        var sut = ProductDescriptionAITooltipUseCase(isDescriptionAIEnabled: true,
+                                                     userDefaults: MockUserDefaults())
+        sut.hasDismissedWriteWithAITooltip = false
+        sut.numberOfTimesWriteWithAITooltipIsShown = 3
+
+        XCTAssertFalse(sut.shouldShowTooltip)
+    }
+
+    // MARK: Dismissed by user
+
+    func test_shouldShowTooltip_is_true_only_when_hasDismissedWriteWithAITooltip_is_false() {
+        var sut = ProductDescriptionAITooltipUseCase(isDescriptionAIEnabled: true,
+                                                     userDefaults: MockUserDefaults())
+        sut.hasDismissedWriteWithAITooltip = false
+        sut.numberOfTimesWriteWithAITooltipIsShown = 1
+
+        XCTAssertTrue(sut.shouldShowTooltip)
+    }
+
+    func test_shouldShowTooltip_is_false_when_hasDismissedWriteWithAITooltip_is_true() {
+        var sut = ProductDescriptionAITooltipUseCase(isDescriptionAIEnabled: true,
+                                                     userDefaults: MockUserDefaults())
+        sut.hasDismissedWriteWithAITooltip = true
+        sut.numberOfTimesWriteWithAITooltipIsShown = 1
+
+        XCTAssertFalse(sut.shouldShowTooltip)
+    }
+}
+
+private class MockUserDefaults: UserDefaults {
+    var hasDismissedWriteWithAITooltip = false
+    var numberOfTimesWriteWithAITooltipIsShown = 0
+
+    override func bool(forKey defaultName: String) -> Bool {
+        hasDismissedWriteWithAITooltip
+    }
+
+    override func set(_ value: Bool, forKey defaultName: String) {
+        hasDismissedWriteWithAITooltip = value
+    }
+
+    override func integer(forKey defaultName: String) -> Int {
+        numberOfTimesWriteWithAITooltipIsShown
+    }
+
+    override func set(_ integer: Int, forKey defaultName: String) {
+        numberOfTimesWriteWithAITooltipIsShown = integer
+    }
+}

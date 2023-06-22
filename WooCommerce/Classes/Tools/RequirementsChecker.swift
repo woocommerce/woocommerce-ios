17bd6e55c2edfbecab311501e13a76bff11dc6d9
Huong Do <hi.huongdo@gmail.com>
2023-06-22 13:06:20 +0700

    Update RequirementsChecker to check wpcom site plan if necessary

--- WooCommerce/Classes/Tools/RequirementsChecker.swift
+++ WooCommerce/Classes/Tools/RequirementsChecker.swift
@@ -15,6 +15,9 @@ enum RequirementCheckResult: Int, CaseIterable {
     /// The installed version of WC is NOT valid
     ///
     case invalidWCVersion
+
+    /// The site is running an expired free trial plan
+    case expiredFreeTrialPlan
 }
 
 
@@ -26,14 +29,27 @@ class RequirementsChecker {
     ///
     private init() { }
 
-
     /// This function checks the default site's API version and then displays a warning if the
     /// site's WC version is not valid.
     ///
-    /// NOTE: When checking the default site's WC version, if 1) an error occurs or 2) the server response is invalid,
-    ///       the WC version alert will *not* be displayed.
+    /// If the site is WPCom, the site plan is fetched first to determine if the site is running on an expired plan.
     ///
-    static func checkMinimumWooVersionForDefaultStore() {
+    static func checkSiteEligibility(for siteID: Int64, onCompletion: ((Result<RequirementCheckResult, Error>) -> Void)? = nil) {
+        switch siteID {
+        case WooConstants.placeholderStoreID:
+            // Skips site plan check for non-WPCom site
+            checkMinimumWooVersion(for: siteID, onCompletion: onCompletion)
+        default:
+            checkWPComSitePlan(for: siteID, onCompletion: onCompletion)
+        }
+    }
+
+    /// This function checks the default site's API version and then displays a warning
+    /// if the site's WC version is not valid or the site is running on an expired plan.
+    ///
+    /// NOTE: When checking the default site's WC version, if 1) an error occurs or 2) the server response is invalid, WC version alert will *not* be displayed.
+    ///
+    static func checkEligibilityForDefaultStore() {
         guard ServiceLocator.stores.isAuthenticated else {
             return
         }
@@ -42,11 +58,59 @@ class RequirementsChecker {
             return
         }
 
-        checkMinimumWooVersion(for: siteID) { result in
-            if case .success(.invalidWCVersion) = result {
+        checkSiteEligibility(for: siteID) { result in
+            switch result {
+            case .success(.invalidWCVersion):
                 displayWCVersionAlert()
+            case .success(.expiredFreeTrialPlan):
+                // TODO: display upgrade alert
+                break
+            default:
+                break
             }
         }
+    }
+}
+
+
+// MARK: - Private helpers
+//
+private extension RequirementsChecker {
+    static func checkWPComSitePlan(for siteID: Int64, onCompletion: ((Result<RequirementCheckResult, Error>) -> Void)? = nil) {
+        let action = PaymentAction.loadSiteCurrentPlan(siteID: siteID) { result in
+            switch result {
+            case .success(let plan):
+                // Normalize dates in the same timezone.
+                let today = Date().startOfDay(timezone: .current)
+                guard plan.isFreeTrial, let expiryDate = plan.expiryDate?.startOfDay(timezone: .current) else {
+                    return checkMinimumWooVersion(for: siteID, onCompletion: onCompletion)
+                }
+                let daysLeft = Calendar.current.dateComponents([.day], from: today, to: expiryDate).day ?? 0
+                if daysLeft <= 0 {
+                    onCompletion?(.success(.expiredFreeTrialPlan))
+                } else {
+                    checkMinimumWooVersion(for: siteID, onCompletion: onCompletion)
+                }
+            case .failure(LoadSiteCurrentPlanError.noCurrentPlan):
+                // Since this is a WPCom store, if it has no plan its plan must have expired or been cancelled.
+                // Generally, expiry is `.success(plan)` with a plan expiry date in the past, but in some cases, we just
+                // don't get any plans marked as `current` in the plans response.
+                onCompletion?(.success(.expiredFreeTrialPlan))
+            case .failure(let error):
+                onCompletion?(.failure(error))
+                DDLogError("⛔️ Error synchronizing WPCom plan: \(error)")
+            }
+        }
+        ServiceLocator.stores.dispatch(action)
+    }
+
+    /// Display the WC version alert
+    ///
+    static func displayWCVersionAlert() {
+        let fancyAlert = FancyAlertViewController.makeWooUpgradeAlertController()
+        fancyAlert.modalPresentationStyle = .custom
+        fancyAlert.transitioningDelegate = AppDelegate.shared.tabBarController
+        AppDelegate.shared.tabBarController?.present(fancyAlert, animated: true)
     }
 
     /// This function simply checks the provided site's API version. No warning will be displayed to the user.
@@ -57,21 +121,6 @@ class RequirementsChecker {
     static func checkMinimumWooVersion(for siteID: Int64, onCompletion: ((Result<RequirementCheckResult, Error>) -> Void)? = nil) {
         let action = retrieveSiteAPIAction(siteID: siteID, onCompletion: onCompletion)
         ServiceLocator.stores.dispatch(action)
-    }
-}
-
-
-// MARK: - Private helpers
-//
-private extension RequirementsChecker {
-
-    /// Display the WC version alert
-    ///
-    static func displayWCVersionAlert() {
-        let fancyAlert = FancyAlertViewController.makeWooUpgradeAlertController()
-        fancyAlert.modalPresentationStyle = .custom
-        fancyAlert.transitioningDelegate = AppDelegate.shared.tabBarController
-        AppDelegate.shared.tabBarController?.present(fancyAlert, animated: true)
     }
 
     /// Returns a `SettingAction.retrieveSiteAPI` action

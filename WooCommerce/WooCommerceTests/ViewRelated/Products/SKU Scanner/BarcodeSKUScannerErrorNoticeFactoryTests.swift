dc837fad4fefd90c49a63529de701c72305b00bd
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2023-06-26 16:01:02 +0200

    Adapt tests to latest change.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/SKU Scanner/BarcodeSKUScannerErrorNoticeFactoryTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/SKU Scanner/BarcodeSKUScannerErrorNoticeFactoryTests.swift
@@ -5,6 +5,7 @@ import Yosemite
 
 final class BarcodeSKUScannerErrorNoticeFactoryTests: XCTestCase {
     let barcode = ScannedBarcode(payloadStringValue: "test-sku", symbology: .ean13)
+    let noticeTitle = NSLocalizedString("Cannot add Product to Order.", comment: "")
 
     func test_notice_when_a_generic_error_is_passed_then_returns_generic_notice() {
         let error = NSError(domain: "disconnect", code: 134)
@@ -16,13 +17,15 @@ final class BarcodeSKUScannerErrorNoticeFactoryTests: XCTestCase {
     func test_notice_when_a_product_not_found_error_is_passed_then_returns_right_notice() {
         let result = BarcodeSKUScannerErrorNoticeFactory.notice(for: ProductLoadError.notFound, code: barcode, actionHandler: {})
 
-        XCTAssertEqual(result.title, NSLocalizedString("Product with SKU \"\(barcode.payloadStringValue)\" not found.", comment: ""))
+        XCTAssertEqual(result.title, noticeTitle)
+        XCTAssertEqual(result.message, NSLocalizedString("Product with SKU \"\(barcode.payloadStringValue)\" not found.", comment: ""))
     }
 
     func test_notice_when_a_product_not_purchasable_error_is_passed_then_returns_right_notice() {
         let result = BarcodeSKUScannerErrorNoticeFactory.notice(for: ProductLoadError.notPurchasable, code: barcode, actionHandler: {})
 
-        XCTAssertEqual(result.title, NSLocalizedString("Product with SKU \"\(barcode.payloadStringValue)\" is not purchasable.", comment: ""))
+        XCTAssertEqual(result.title, noticeTitle)
+        XCTAssertEqual(result.message, NSLocalizedString("Product with SKU \"\(barcode.payloadStringValue)\" is not purchasable.", comment: ""))
     }
 
     func test_notice_passes_right_action_handler() {

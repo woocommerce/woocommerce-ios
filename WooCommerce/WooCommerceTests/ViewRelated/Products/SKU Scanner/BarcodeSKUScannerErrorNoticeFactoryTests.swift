db23252afcdbfc0d935416dd7faa308a857e9f98
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2023-06-26 13:14:52 +0200

    Add unit tests for adding SKU to error messages after barcode scanning.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/SKU Scanner/BarcodeSKUScannerErrorNoticeFactoryTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/SKU Scanner/BarcodeSKUScannerErrorNoticeFactoryTests.swift
@@ -4,28 +4,30 @@ import Yosemite
 @testable import WooCommerce
 
 final class BarcodeSKUScannerErrorNoticeFactoryTests: XCTestCase {
+    let barcode = ScannedBarcode(payloadStringValue: "test-sku", symbology: .ean13)
+
     func test_notice_when_a_generic_error_is_passed_then_returns_generic_notice() {
         let error = NSError(domain: "disconnect", code: 134)
-        let result = BarcodeSKUScannerErrorNoticeFactory.notice(for: error, actionHandler: {})
+        let result = BarcodeSKUScannerErrorNoticeFactory.notice(for: error, code: barcode, actionHandler: {})
 
         XCTAssertEqual(result.title, NSLocalizedString("Cannot add Product to Order.", comment: ""))
     }
 
     func test_notice_when_a_product_not_found_error_is_passed_then_returns_right_notice() {
-        let result = BarcodeSKUScannerErrorNoticeFactory.notice(for: ProductLoadError.notFound, actionHandler: {})
+        let result = BarcodeSKUScannerErrorNoticeFactory.notice(for: ProductLoadError.notFound, code: barcode, actionHandler: {})
 
-        XCTAssertEqual(result.title, NSLocalizedString("Product not found. Failed to add Product to Order.", comment: ""))
+        XCTAssertEqual(result.title, NSLocalizedString("Product with SKU \"\(barcode.payloadStringValue)\" not found.", comment: ""))
     }
 
     func test_notice_when_a_product_not_purchasable_error_is_passed_then_returns_right_notice() {
-        let result = BarcodeSKUScannerErrorNoticeFactory.notice(for: ProductLoadError.notPurchasable, actionHandler: {})
+        let result = BarcodeSKUScannerErrorNoticeFactory.notice(for: ProductLoadError.notPurchasable, code: barcode, actionHandler: {})
 
-        XCTAssertEqual(result.title, NSLocalizedString("Product not purchasable. Failed to add Product to Order.", comment: ""))
+        XCTAssertEqual(result.title, NSLocalizedString("Product with SKU \"\(barcode.payloadStringValue)\" is not purchasable.", comment: ""))
     }
 
     func test_notice_passes_right_action_handler() {
         var actionHandlerIsCalled = false
-        let result = BarcodeSKUScannerErrorNoticeFactory.notice(for: ProductLoadError.notPurchasable, actionHandler: {
+        let result = BarcodeSKUScannerErrorNoticeFactory.notice(for: ProductLoadError.notPurchasable, code: barcode, actionHandler: {
             actionHandlerIsCalled = true
         })
 

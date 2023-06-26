af47cdec99848ed7de72b518cbf396771b3def1e
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2023-06-26 15:57:04 +0200

    Refactor Notice to hold longer messages.

--- WooCommerce/Classes/ViewRelated/Products/SKU Scanner/BarcodeSKUScannerErrorNoticeFactory.swift
+++ WooCommerce/Classes/ViewRelated/Products/SKU Scanner/BarcodeSKUScannerErrorNoticeFactory.swift
@@ -5,22 +5,23 @@ import Yosemite
 /// 
 struct BarcodeSKUScannerErrorNoticeFactory {
     static func notice(for error: Error, code: ScannedBarcode, actionHandler: @escaping ((() -> Void))) -> Notice {
-        Notice(title: noticeTitle(for: error, code: code),
+        Notice(title: Localization.defaultTitle,
+               message: noticeMessage(for: error, code: code),
                feedbackType: .error,
                actionTitle: Localization.retryActionTitle,
                actionHandler: actionHandler)
     }
 
-    private static func noticeTitle(for error: Error, code: ScannedBarcode) -> String {
+    private static func noticeMessage(for error: Error, code: ScannedBarcode) -> String {
         guard let productLoadError = error as? ProductLoadError else {
             return Localization.defaultTitle
         }
 
         switch productLoadError {
         case .notFound:
-            return String(format: Localization.productNotFoundTitle, code.payloadStringValue)
+            return String(format: Localization.productNotFoundMessage, code.payloadStringValue)
         case .notPurchasable:
-            return String(format: Localization.productNotPurchasableTitle, code.payloadStringValue)
+            return String(format: Localization.productNotPurchasableMessage, code.payloadStringValue)
         default:
             return Localization.defaultTitle
         }
@@ -31,9 +32,9 @@ private extension BarcodeSKUScannerErrorNoticeFactory {
     enum Localization {
         static let defaultTitle = NSLocalizedString("Cannot add Product to Order.",
                                                     comment: "Generic error when a product can't be added to an order after being scanned.")
-        static let productNotFoundTitle = NSLocalizedString("Product with SKU \"%@\" not found.",
+        static let productNotFoundMessage = NSLocalizedString("Product with SKU \"%@\" not found.",
                                                             comment: "Error message when the scanner cannot find a matching product. %@ is the SKU code.")
-        static let productNotPurchasableTitle = NSLocalizedString("Product with SKU \"%@\" is not purchasable.",
+        static let productNotPurchasableMessage = NSLocalizedString("Product with SKU \"%@\" is not purchasable.",
                                                                   comment: "Error message when the scanner found a product but isn't purchasable." +
                                                                   "%@ is the SKU code.")
         static let retryActionTitle = NSLocalizedString("Retry",

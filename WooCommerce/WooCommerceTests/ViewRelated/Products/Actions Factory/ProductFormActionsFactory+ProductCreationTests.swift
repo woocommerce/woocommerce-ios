c44465d86abd73b194ee88e385dd2e8f265ab115
Jaclyn Chen <watertranquil@gmail.com>
2023-06-26 15:00:15 -0400

    Update the product type row to be editable in the product creation form.

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Actions Factory/ProductFormActionsFactory+ProductCreationTests.swift
@@ -0,0 +1,66 @@
+import XCTest
+import Fakes
+
+@testable import WooCommerce
+@testable import Yosemite
+
+final class ProductFormActionsFactory_ProductCreationTests: XCTestCase {
+    func test_product_type_is_editable_for_core_product_types() {
+        // Product types supported in core.
+        let coreProductTypes: [ProductType] = [.simple, .variable, .affiliate, .grouped]
+
+        coreProductTypes.forEach { coreProductType in
+            // Given
+            let product = Product.fake().copy(productTypeKey: coreProductType.rawValue)
+            let model = EditableProductModel(product: product)
+
+            // When
+            let actions = Fixtures.actionsFactory(product: model, formType: .add).settingsSectionActions()
+
+            // Then
+            XCTAssertTrue(actions.contains(.productType(editable: true)))
+        }
+    }
+
+    func test_product_type_is_not_editable_for_non_core_product_types() {
+        // Product types supported in Woo extensions.
+        let nonCoreProductTypes: [ProductType] = [.bundle, .composite, .subscription, .variableSubscription, .custom("sub")]
+
+        nonCoreProductTypes.forEach { nonCoreProductType in
+            // Given
+            let product = Product.fake().copy(productTypeKey: nonCoreProductType.rawValue)
+            let model = EditableProductModel(product: product)
+
+            // When
+            let actions = Fixtures.actionsFactory(product: model, formType: .add).settingsSectionActions()
+
+            // Then
+            XCTAssertTrue(actions.contains(.productType(editable: false)))
+        }
+    }
+}
+
+private extension ProductFormActionsFactory_ProductCreationTests {
+    enum Fixtures {
+        // Factory with default feature settings.
+        static func actionsFactory(product: EditableProductModel,
+                                   formType: ProductFormType,
+                                   addOnsFeatureEnabled: Bool = false,
+                                   isLinkedProductsPromoEnabled: Bool = false,
+                                   isBundledProductsEnabled: Bool = false,
+                                   isCompositeProductsEnabled: Bool = false,
+                                   isSubscriptionProductsEnabled: Bool = false,
+                                   isMinMaxQuantitiesEnabled: Bool = false,
+                                   variationsPrice: ProductFormActionsFactory.VariationsPrice = .unknown) -> ProductFormActionsFactory {
+            ProductFormActionsFactory(product: product,
+                                      formType: formType,
+                                      addOnsFeatureEnabled: addOnsFeatureEnabled,
+                                      isLinkedProductsPromoEnabled: isLinkedProductsPromoEnabled,
+                                      isBundledProductsEnabled: isBundledProductsEnabled,
+                                      isCompositeProductsEnabled: isCompositeProductsEnabled,
+                                      isSubscriptionProductsEnabled: isSubscriptionProductsEnabled,
+                                      isMinMaxQuantitiesEnabled: isMinMaxQuantitiesEnabled,
+                                      variationsPrice: variationsPrice)
+        }
+    }
+}

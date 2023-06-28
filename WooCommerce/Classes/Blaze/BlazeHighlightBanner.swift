79bfcb8b05802fee5a74993a65113748d8ce2570
Huong Do <hi.huongdo@gmail.com>
2023-06-28 20:23:44 +0700

    Add content for the Blaze highlight banner

--- WooCommerce/Classes/Blaze/BlazeHighlightBanner.swift
+++ WooCommerce/Classes/Blaze/BlazeHighlightBanner.swift
@@ -2,7 +2,47 @@ import SwiftUI
 
 struct BlazeHighlightBanner: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: Layout.spacing) {
+            // Dismiss button
+            HStack {
+                Spacer()
+                Button {
+                    // TODO
+                } label: {
+                    Image(systemName: "xmark")
+                }
+                .buttonStyle(.plain)
+            }
+
+            // Blaze icon
+            Image(uiImage: .blaze)
+
+            // Title
+            Text("Promote products with Blaze")
+                .headlineStyle()
+
+            // Description
+            Text("Turn your products into ads that run across millions of sites on WordPress.com and Tumblr.")
+                .bodyStyle()
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, Layout.spacing)
+
+            // CTA
+            Button {
+                // TODO
+            } label: {
+                Text("Try Blaze now")
+                    .headlineLinkStyle()
+            }
+            .buttonStyle(LinkButtonStyle())
+        }
+        .padding(Layout.spacing)
+    }
+}
+
+extension BlazeHighlightBanner {
+    enum Layout {
+        static let spacing: CGFloat = 16
     }
 }
 

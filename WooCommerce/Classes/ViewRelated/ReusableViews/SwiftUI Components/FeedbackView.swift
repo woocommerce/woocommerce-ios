3afc15b3a3846a14eb2f9e54a9c2330d0f195db3
Huong Do <hi.huongdo@gmail.com>
2023-06-28 12:27:47 +0700

    Update feedback view style

--- WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/FeedbackView.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/FeedbackView.swift
@@ -4,30 +4,42 @@ struct FeedbackView: View {
     let onUpvote: () -> Void
     let onDownvote: () -> Void
 
+    /// Scale of the view based on accessibility changes
+    @ScaledMetric private var scale: CGFloat = 1.0
+
     var body: some View {
         HStack {
             Text(Localization.feedbackQuestion)
-                .secondaryBodyStyle()
+                .subheadlineStyle()
             Spacer()
-            Button {
-                onUpvote()
-            } label: {
-                Image(systemName: "hand.thumbsup")
-                    .secondaryBodyStyle()
+            HStack(spacing: Layout.buttonSpacing) {
+                Button {
+                    onUpvote()
+                } label: {
+                    Image(systemName: "hand.thumbsup")
+                        .resizable()
+                        .frame(width: Layout.iconSize * scale,
+                               height: Layout.iconSize * scale)
+                        .foregroundColor(.secondary)
+                }
+                .buttonStyle(.plain)
+
+                Button {
+                    onDownvote()
+                } label: {
+                    Image(systemName: "hand.thumbsdown")
+                        .resizable()
+                        .frame(width: Layout.iconSize * scale,
+                               height: Layout.iconSize * scale)
+                        .foregroundColor(.secondary)
+                }
+                .buttonStyle(.plain)
             }
-            .buttonStyle(.plain)
-            Button {
-                onDownvote()
-            } label: {
-                Image(systemName: "hand.thumbsdown")
-                    .secondaryBodyStyle()
-            }
-            .buttonStyle(.plain)
         }
         .padding(Layout.contentInsets)
         .background(
             RoundedRectangle(cornerRadius: Layout.cornerRadius)
-                .foregroundColor(.init(uiColor: .secondarySystemFill))
+                .foregroundColor(.init(uiColor: .systemGray5))
         )
     }
 }
@@ -36,6 +48,8 @@ private extension FeedbackView {
     enum Layout {
         static let contentInsets = EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)
         static let cornerRadius: CGFloat = 8
+        static let iconSize: CGFloat = 20
+        static let buttonSpacing: CGFloat = 16
     }
     enum Localization {
         static let feedbackQuestion = NSLocalizedString(

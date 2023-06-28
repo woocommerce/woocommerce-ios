3b82a5330cae0ca46a0553bb4e6e322cff24f6f9
Jaclyn Chen <watertranquil@gmail.com>
2023-06-28 17:32:51 -0400

    Create local announcement modal view, view model, and announcements provider.

---
+++ WooCommerce/Classes/LocalAnnouncements/LocalAnnouncementsProvider.swift
@@ -0,0 +1,52 @@
+import Experiments
+import Foundation
+import Yosemite
+
+/// Provides the local announcements to be displayed on the dashboard tab.
+///
+final class LocalAnnouncementsProvider {
+    private let stores: StoresManager
+    private let analytics: Analytics
+    private let featureFlagService: FeatureFlagService
+    // The order of the announcements is based on the priority.
+    private let announcements: [LocalAnnouncement] = [.productDescriptionAI]
+
+    init(stores: StoresManager = ServiceLocator.stores,
+         analytics: Analytics = ServiceLocator.analytics,
+         featureFlagService: FeatureFlagService = ServiceLocator.featureFlagService) {
+        self.stores = stores
+        self.analytics = analytics
+        self.featureFlagService = featureFlagService
+    }
+
+    func loadAnnouncement() async -> LocalAnnouncementViewModel? {
+        guard featureFlagService.isFeatureFlagEnabled(.productDescriptionAIFromStoreOnboarding) else {
+            return nil
+        }
+        for announcement in announcements {
+            guard await isVisible(announcement: announcement) else {
+                continue
+            }
+            return .init(announcement: announcement, stores: stores, analytics: analytics)
+        }
+        return nil
+    }
+}
+
+private extension LocalAnnouncementsProvider {
+    @MainActor
+    func isVisible(announcement: LocalAnnouncement) async -> Bool {
+        await withCheckedContinuation { continuation in
+            stores.dispatch(AppSettingsAction.getLocalAnnouncementVisibility(announcement: announcement) { isVisible in
+                continuation.resume(returning: isVisible)
+            })
+        }
+    }
+
+    func isEligible(announcement: LocalAnnouncement) -> Bool {
+        switch announcement {
+            case .productDescriptionAI:
+                return stores.sessionManager.defaultSite?.isWordPressComStore == true
+        }
+    }
+}

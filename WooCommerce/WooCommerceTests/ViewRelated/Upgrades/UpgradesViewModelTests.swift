9919cdb88bd9ffab9c1eb6fd32f57f3ba44313cc
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-06-28 15:01:07 +0700

    Add helpers for UpgradesViewModel state testing

--- WooCommerce/WooCommerceTests/ViewRelated/Upgrades/UpgradesViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Upgrades/UpgradesViewModelTests.swift
@@ -79,13 +79,13 @@ final class UpgradesViewModelTests: XCTestCase {
 
     func test_upgradeViewState_when_initialized_is_loading_state() {
         // Given, When
-        let sut = UpgradesViewModel(siteID: sampleSiteID)
+        // see `setUp`
 
         // Then
         XCTAssertEqual(sut.upgradeViewState, .loading)
     }
 
-    func test_upgradeViewState_when_initialized_by_non_owner_then_state_is_prepurchase_error_userNotAllowedToUpgrade() {
+    func test_upgradeViewState_when_initialized_by_non_owner_then_state_is_prepurchaseError_userNotAllowedToUpgrade() {
         // Given
         let site = Site.fake().copy(isSiteOwner: false)
         let stores = MockStoresManager(sessionManager: .makeForTesting(defaultSite: site))
@@ -93,5 +93,83 @@ final class UpgradesViewModelTests: XCTestCase {
 
         // Then
         XCTAssertEqual(sut.upgradeViewState, .prePurchaseError(.userNotAllowedToUpgrade))
+    }
+
+    func test_upgradeViewState_when_retrievePlanDetailsIfAvailable_fetching_error_then_upgradeViewState_is_prePurchaseError_fetchError() async {
+        // Given
+        let inAppPurchasesManager = MockInAppPurchasesForWPComPlansManager(plans: [])
+        let sut = UpgradesViewModel(siteID: sampleSiteID,
+                                    inAppPurchasesPlanManager: inAppPurchasesManager)
+
+        let expectedError: PrePurchaseError = .fetchError
+
+        // When
+        await sut.fetchPlans()
+
+        // Then
+        XCTAssertEqual(sut.upgradeViewState, .prePurchaseError(expectedError))
+    }
+
+    func test_upgradeViewState_when_retrievePlanDetailsIfAvailable_has_plans_then_upgradeViewState_is_loaded() async {
+        // Given
+        let expectedPlan = UpgradesViewModelTests.makeWPComPlanProduct()
+        let inAppPurchasesManager = MockInAppPurchasesForWPComPlansManager(plans: [expectedPlan])
+        let sut = UpgradesViewModel(siteID: sampleSiteID,
+                                    inAppPurchasesPlanManager: inAppPurchasesManager)
+
+        // When
+        await sut.fetchPlans()
+
+        // Then
+        XCTAssertEqual(sut.upgradeViewState.caseString, "loaded")
+    }
+}
+
+// MARK: Helpers
+//
+private extension UpgradeViewState {
+    var caseString: String {
+        switch self {
+        case .loaded:
+            return "loaded"
+        default:
+            return ""
+        }
+    }
+}
+private extension UpgradesViewModelTests {
+    struct MockWPComPlanProduct: WPComPlanProduct {
+        var displayName: String
+        var description: String
+        var id: String
+        var displayPrice: String
+    }
+
+    static func makeWPComPlanProduct() -> WPComPlanProduct {
+        MockInAppPurchasesForWPComPlansManager.Plan(
+                displayName: "Test awesome plan",
+                description: "All the Woo, all the time",
+                id: "debug.woocommerce.express.essential.monthly",
+                displayPrice: "$1.50")
+    }
+
+    static func makeWooWPComPlan() -> WooWPComPlan {
+        let woocomplan = MockInAppPurchasesForWPComPlansManager.Plan(
+                displayName: "Test awesome plan",
+                description: "All the Woo, all the time",
+                id: "debug.woocommerce.express.essential.monthly",
+                displayPrice: "$1.50")
+        let wooplan = WooPlan(id: "",
+                              name: "",
+                              shortName: "",
+                              planFrequency: WooPlan.PlanFrequency.month,
+                              planDescription: "",
+                              headerImageFileName: "",
+                              headerImageCardColor: .red,
+                              planFeatureGroups: [])
+
+        return WooWPComPlan(wpComPlan: woocomplan,
+                            wooPlan: wooplan,
+                            hardcodedPlanDataIsValid: true)
     }
 }

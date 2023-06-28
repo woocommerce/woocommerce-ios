62deb81df1c65a6c4335c9a3fe3efe9c5b855262
Huong Do <hi.huongdo@gmail.com>
2023-06-28 19:25:05 +0700

    Add new banner for highlighting Blaze

---
+++ WooCommerce/Classes/Blaze/BlazeHighlightBanner.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct BlazeHighlightBanner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlazeHighlightBanner_Previews: PreviewProvider {
+    static var previews: some View {
+        BlazeHighlightBanner()
+    }
+}

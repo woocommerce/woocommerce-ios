4f03ed1df7f13841c1ff450cef4526638b3f4d62
Huong Do <hi.huongdo@gmail.com>
2023-06-28 16:24:09 +0700

    Inject delay for dismissing feedback banner

--- WooCommerce/Classes/ViewRelated/Products/AI/ProductDescriptionGenerationViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/AI/ProductDescriptionGenerationViewModel.swift
@@ -32,12 +32,14 @@ final class ProductDescriptionGenerationViewModel: ObservableObject {
     private let stores: StoresManager
     private let analytics: Analytics
     private let onApply: (_ output: ProductDescriptionGenerationOutput) -> Void
+    private let delayBeforeDismissingFeedbackBanner: TimeInterval
 
     private var task: Task<Void, Error>?
 
     init(siteID: Int64,
          name: String,
          description: String,
+         delayBeforeDismissingFeedbackBanner: TimeInterval = 0.5,
          stores: StoresManager = ServiceLocator.stores,
          analytics: Analytics = ServiceLocator.analytics,
          onApply: @escaping (ProductDescriptionGenerationOutput) -> Void) {
@@ -48,6 +50,7 @@ final class ProductDescriptionGenerationViewModel: ObservableObject {
         self.analytics = analytics
         self.onApply = onApply
         self.isProductNameEditable = name.isEmpty
+        self.delayBeforeDismissingFeedbackBanner = delayBeforeDismissingFeedbackBanner
     }
 
     /// Generates product description async.
@@ -73,7 +76,7 @@ final class ProductDescriptionGenerationViewModel: ObservableObject {
     func handleFeedback(_ vote: FeedbackView.Vote) {
         analytics.track(event: .AIFeedback.feedbackSent(source: .productDescription,
                                                         isUseful: vote == .up))
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+        DispatchQueue.main.asyncAfter(deadline: .now() + delayBeforeDismissingFeedbackBanner) { [weak self] in
             self?.shouldShowFeedbackView = false
         }
     }

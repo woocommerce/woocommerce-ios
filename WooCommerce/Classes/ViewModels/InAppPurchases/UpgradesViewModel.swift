efa8568fe065305360cf8426c871f11ecd166df2
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-06-28 16:49:00 +0700

    Extract UpgradeViewState. Add partial Equatable conformance

--- WooCommerce/Classes/ViewModels/InAppPurchases/UpgradesViewModel.swift
+++ WooCommerce/Classes/ViewModels/InAppPurchases/UpgradesViewModel.swift
@@ -3,25 +3,6 @@ import SwiftUI
 import Yosemite
 import Combine
 
-enum UpgradeViewState {
-    case loading
-    case loaded(WooWPComPlan)
-    case purchasing(WooWPComPlan)
-    case waiting(WooWPComPlan)
-    case completed(WooWPComPlan)
-    case prePurchaseError(PrePurchaseError)
-    case purchaseUpgradeError(PurchaseUpgradeError)
-
-    var shouldShowPlanDetailsView: Bool {
-        switch self {
-        case .loading, .loaded, .purchasing, .prePurchaseError:
-            return true
-        default:
-            return false
-        }
-    }
-}
-
 enum PrePurchaseError: Error {
     case fetchError
     case entitlementsError

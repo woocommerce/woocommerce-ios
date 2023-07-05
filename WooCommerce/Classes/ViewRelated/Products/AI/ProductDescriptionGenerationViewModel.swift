74814746151ba8d74c1525899bbd7f23e5614dc7
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-07-05 19:13:53 +0530

    Detect and store language while generating product description.

--- WooCommerce/Classes/ViewRelated/Products/AI/ProductDescriptionGenerationViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/AI/ProductDescriptionGenerationViewModel.swift
@@ -36,6 +36,10 @@ final class ProductDescriptionGenerationViewModel: ObservableObject {
 
     private var task: Task<Void, Error>?
 
+    /// Language used in product identified by AI
+    ///
+    private var languageIdentifiedUsingAI: String?
+
     init(siteID: Int64,
          name: String,
          description: String,
@@ -86,13 +90,38 @@ final class ProductDescriptionGenerationViewModel: ObservableObject {
 private extension ProductDescriptionGenerationViewModel {
     @MainActor
     func generateProductDescription() async -> Result<String, Error> {
-        await withCheckedContinuation { continuation in
-            stores.dispatch(ProductAction.generateProductDescription(siteID: siteID,
-                                                                     name: name,
-                                                                     features: features) { result in
-                continuation.resume(returning: result)
-            })
+        do {
+            let language = try await detectLaunguage()
+            return await withCheckedContinuation { continuation in
+                stores.dispatch(ProductAction.generateProductDescription(siteID: siteID,
+                                                                         name: name,
+                                                                         features: features,
+                                                                         language: language) { result in
+                    continuation.resume(returning: result)
+                })
+            }
+        } catch {
+            return .failure(error)
         }
+    }
+
+    @MainActor
+    func detectLaunguage() async throws -> String {
+        if let languageIdentifiedUsingAI,
+           languageIdentifiedUsingAI.isNotEmpty {
+            return languageIdentifiedUsingAI
+        }
+
+        let language = try await withCheckedThrowingContinuation { continuation in
+            stores.dispatch(ProductAction.identifyLanguage(siteID: siteID,
+                                                           string: name + " " + features,
+                                                           feature: .productSharing,
+                                                           completion: { result in
+                continuation.resume(with: result)
+            }))
+        }
+        self.languageIdentifiedUsingAI = language
+        return language
     }
 
     @MainActor

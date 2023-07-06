79a99296f6dbd0f4d158689d1e9ea9769c9b9921
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-07-06 12:00:54 +0530

    Reset identified language if voted down.

--- WooCommerce/Classes/ViewRelated/Products/AI/ProductSharingMessageGenerationViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/AI/ProductSharingMessageGenerationViewModel.swift
@@ -101,6 +101,10 @@ final class ProductSharingMessageGenerationViewModel: ObservableObject {
     func handleFeedback(_ vote: FeedbackView.Vote) {
         analytics.track(event: .AIFeedback.feedbackSent(source: .productSharingMessage,
                                                         isUseful: vote == .up))
+        if vote == .down {
+            languageIdentifiedUsingAI = nil
+        }
+
         // Delay the disappearance of the banner for a better UX.
         DispatchQueue.main.asyncAfter(deadline: .now() + delayBeforeDismissingFeedbackBanner) { [weak self] in
             self?.shouldShowFeedbackView = false

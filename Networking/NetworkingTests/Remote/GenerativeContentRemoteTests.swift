0e24ae4ebe4fcc49112aa01c08c141a7f6509bc1
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-07-12 16:09:40 +0530

    Unit test token regeneration behaviour.

--- Networking/NetworkingTests/Remote/GenerativeContentRemoteTests.swift
+++ Networking/NetworkingTests/Remote/GenerativeContentRemoteTests.swift
@@ -53,6 +53,57 @@ final class GenerativeContentRemoteTests: XCTestCase {
         }
     }
 
+    func test_generateText_with_failure_returns_error_when_token_generation_fails() async throws {
+        // Given
+        let remote = GenerativeContentRemote(network: network)
+        network.simulateResponse(requestUrlSuffix: "sites/\(sampleSiteID)/jetpack-openai-query/jwt", filename: "jwt-token-failure")
+        network.simulateResponse(requestUrlSuffix: "text-completion", filename: "generative-text-failure")
+
+        // When
+        await assertThrowsError {
+            _ = try await remote.generateText(siteID: sampleSiteID,
+                                              base: "generate a product description for wapuu pencil",
+                                              feature: .productDescription)
+        } errorAssert: { error in
+            // Then
+            error as? WordPressApiError == .unknown(code: "oauth2_invalid_token", message: "The OAuth2 token is invalid.")
+        }
+    }
+
+    func test_generateText_retries_after_regenarating_token_upon_receiving_403_error() async throws {
+        // Given
+        let jwtRequestPath = "sites/\(sampleSiteID)/jetpack-openai-query/jwt"
+        let textCompletionPath = "text-completion"
+        let remote = GenerativeContentRemote(network: network)
+        network.simulateResponse(requestUrlSuffix: jwtRequestPath, filename: "jwt-token-success")
+        network.simulateResponse(requestUrlSuffix: textCompletionPath, filename: "generative-text-success")
+
+        // When
+        _ = try await remote.generateText(siteID: sampleSiteID,
+                                          base: "generate a product description for wapuu pencil",
+                                          feature: .productDescription)
+        // Then
+        XCTAssertEqual(numberOfJwtRequests(in: network.requestsForResponseData), 1)
+
+        // When
+        _ = try await remote.generateText(siteID: sampleSiteID,
+                                          base: "generate a product description for wapuu pencil",
+                                          feature: .productDescription)
+
+        // Then
+        XCTAssertEqual(numberOfJwtRequests(in: network.requestsForResponseData), 1)
+
+
+        // When
+        network.simulateResponse(requestUrlSuffix: textCompletionPath, filename: "generative-text-invalid-token")
+        _ = try? await remote.generateText(siteID: sampleSiteID,
+                                           base: "generate a product description for wapuu pencil",
+                                           feature: .productDescription)
+
+        // Then
+        XCTAssertEqual(numberOfJwtRequests(in: network.requestsForResponseData), 2)
+        XCTAssertEqual(numberOfTextCompletionRequests(in: network.requestsForResponseData), 4)
+    }
 
     // MARK: - `identifyLanguage`
 
@@ -86,5 +137,81 @@ final class GenerativeContentRemoteTests: XCTestCase {
             // Then
             error as? WordPressApiError == .unknown(code: "inactive", message: "OpenAI features have been disabled")
         }
+    }
+
+    func test_identifyLanguage_with_failure_returns_error_when_token_generation_fails() async throws {
+        // Given
+        let remote = GenerativeContentRemote(network: network)
+        network.simulateResponse(requestUrlSuffix: "sites/\(sampleSiteID)/jetpack-openai-query/jwt", filename: "jwt-token-failure")
+        network.simulateResponse(requestUrlSuffix: "text-completion", filename: "identify-language-failure")
+
+        // When
+        await assertThrowsError {
+            _ = try await remote.identifyLanguage(siteID: sampleSiteID,
+                                                  string: "Woo is awesome.",
+                                                  feature: .productDescription)
+        } errorAssert: { error in
+            // Then
+            error as? WordPressApiError == .unknown(code: "oauth2_invalid_token", message: "The OAuth2 token is invalid.")
+        }
+    }
+
+    func test_identifyLanguage_retries_after_regenarating_token_upon_receiving_403_error() async throws {
+        // Given
+        let jwtRequestPath = "sites/\(sampleSiteID)/jetpack-openai-query/jwt"
+        let textCompletionPath = "text-completion"
+        let remote = GenerativeContentRemote(network: network)
+        network.simulateResponse(requestUrlSuffix: jwtRequestPath, filename: "jwt-token-success")
+        network.simulateResponse(requestUrlSuffix: textCompletionPath, filename: "generative-text-success")
+
+        // When
+        _ = try await remote.identifyLanguage(siteID: sampleSiteID,
+                                              string: "Woo is awesome.",
+                                              feature: .productDescription)
+        // Then
+        XCTAssertEqual(numberOfJwtRequests(in: network.requestsForResponseData), 1)
+
+        // When
+        _ = try await remote.identifyLanguage(siteID: sampleSiteID,
+                                              string: "Woo is awesome.",
+                                              feature: .productDescription)
+
+        // Then
+        XCTAssertEqual(numberOfJwtRequests(in: network.requestsForResponseData), 1)
+
+
+        // When
+        network.simulateResponse(requestUrlSuffix: textCompletionPath, filename: "generative-text-invalid-token")
+        _ = try? await remote.identifyLanguage(siteID: sampleSiteID,
+                                               string: "Woo is awesome.",
+                                               feature: .productDescription)
+
+        // Then
+        XCTAssertEqual(numberOfJwtRequests(in: network.requestsForResponseData), 2)
+        XCTAssertEqual(numberOfTextCompletionRequests(in: network.requestsForResponseData), 4)
+    }
+}
+
+// MARK: - Helpers
+//
+private extension GenerativeContentRemoteTests {
+    func numberOfJwtRequests(in array: [URLRequestConvertible]) -> Int {
+        let jwtRequestPath = "sites/\(sampleSiteID)/jetpack-openai-query/jwt"
+        return array.filter({ request in
+            guard let dotcomRequest = request as? DotcomRequest else {
+                return false
+            }
+            return dotcomRequest.path == jwtRequestPath
+        }).count
+    }
+
+    func numberOfTextCompletionRequests(in array: [URLRequestConvertible]) -> Int {
+        let textCompletionPath = "text-completion"
+        return array.filter({ request in
+            guard let dotcomRequest = request as? DotcomRequest else {
+                return false
+            }
+            return dotcomRequest.path == textCompletionPath
+        }).count
     }
 }

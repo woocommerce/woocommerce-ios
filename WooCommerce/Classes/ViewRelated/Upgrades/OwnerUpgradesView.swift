18a7d44a76156e32e60f34d11ca3f22371f8f97b
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-07-17 19:23:52 +0200

    Added full feature list and initial UI styling

--- WooCommerce/Classes/ViewRelated/Upgrades/OwnerUpgradesView.swift
+++ WooCommerce/Classes/ViewRelated/Upgrades/OwnerUpgradesView.swift
@@ -16,16 +16,67 @@ struct FullFeatureListViewModel {
                                     "WooCommerce store",
                                     "WooCommerce mobile app",
                                     "WordPress CMS",
-                                    "WordPress mobile app"
-                                  ], performanceFeatures: []
+                                    "WordPress mobile app",
+                                    "Free SSL certificate",
+                                    "Generous storage",
+                                    "Automated backup + quick restore",
+                                    "Ad-free experience",
+                                    "Unlimited admin accounts",
+                                    "Live chat support",
+                                    "Email support",
+                                    "Premium themes included",
+                                    "Sales reports",
+                                    "Google Analytics"
+                                  ],
+                                  performanceFeatures: []
                                  ),
             FullFeatureListGroups(title: "Products",
                                   essentialFeatures: [
                                     "List unlimited products",
                                     "Gift cards",
-                                  ], performanceFeatures: [
-                                    "Min/Max order quantity"
-                                  ])
+                                  ],
+                                  performanceFeatures: [
+                                    "Min/Max order quantity",
+                                    "Product Bundles",
+                                    "Custom product kits",
+                                    "List products by brand",
+                                    "Product recommendations"
+                                  ]),
+            FullFeatureListGroups(title: "Payments",
+                                  essentialFeatures: [
+                                    "Integrated payments",
+                                    "International payments'",
+                                    "Automated sales taxes",
+                                    "Accept local payments'",
+                                    "Recurring payments'"
+                                  ],
+                                  performanceFeatures: []),
+
+            FullFeatureListGroups(title: "Marketing & Email",
+                                  essentialFeatures: [
+                                  "Promote on TikTok",
+                                  "Sync with Pinterest",
+                                  "Connect with Facebook",
+                                  "Advanced SEO tools",
+                                  "Advertise on Google",
+                                  "Custom order emails",
+                                  ],
+                                  performanceFeatures: [
+                                    "Back in stock emails",
+                                    "Marketing automation",
+                                    "Abandoned cart recovery",
+                                    "Referral programs",
+                                    "Customer birthday emails",
+                                    "Loyalty points programs"
+                                  ]),
+
+            FullFeatureListGroups(title: "Shipping",
+                                  essentialFeatures: [
+                                  "Shipment tracking",
+                                  "Live shipping rates",
+                                  "Discounted shipping²",
+                                  "Print shipping labels²"],
+                                  performanceFeatures: []),
         ]
     }
 }
@@ -34,22 +85,42 @@ struct FullFeatureListView: View {
     var featureListGroups = FullFeatureListViewModel.hardcodedFullFeatureList()
     var body: some View {
         ScrollView() {
-            ForEach(featureListGroups, id: \.title) { featureList in
-                Text(featureList.title)
-                    .font(.title)
-                    .bold()
-                ForEach(featureList.essentialFeatures, id: \.self) { feature in
-                    Text(feature)
-                        .font(.body)
+            VStack(alignment: .leading, spacing: 8.0) {
+                ForEach(featureListGroups, id: \.title) { featureList in
+                    Text(featureList.title)
+                        .font(.title)
+                        .bold()
+                    ForEach(featureList.essentialFeatures, id: \.self) { feature in
+                        Text(feature)
+                            .font(.body)
+                    }
+                    ForEach(featureList.performanceFeatures, id: \.self) { feature in
+                        Text(feature)
+                            .font(.body)
+                            .underline(color: .red)
+                    }
+                    Divider()
                 }
-                ForEach(featureList.performanceFeatures, id: \.self) { feature in
-                    Text(feature)
-                        .font(.body)
-                        .underline(color: .red)
-                }
-                Divider()
+                Text(Localization.disclaimer1)
+                    .font(.caption)
+                Text(Localization.disclaimer2)
+                    .font(.caption)
             }
+            .background(Color(.listBackground))
+            .navigationTitle("Full Feature List")
         }
+    }
+}
+
+private extension FullFeatureListView {
+    struct Localization {
+        static let disclaimer1 = NSLocalizedString(
+            "1. Available as standard in WooCommerce Payments (restrictions apply)." +
+            "Additional extensions may be required for other payment providers." ,
+            comment: "")
+        static let disclaimer2 = NSLocalizedString(
+        "2. Only available in the U.S. – an additional extension will be required for other countries.",
+        comment: "")
     }
 }
 

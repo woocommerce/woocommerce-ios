f363e4b2662a1d0103cd138f55bbcfa0ee118b9b
Rachel McRoberts <rachel@automattic.com>
2023-07-19 18:22:04 +0100

    Customize error banner title and message for Jetpack connection errors

--- WooCommerce/Classes/ViewRelated/Top Banner/ErrorTopBannerFactory.swift
+++ WooCommerce/Classes/ViewRelated/Top Banner/ErrorTopBannerFactory.swift
@@ -1,4 +1,5 @@
 import Yosemite
+import enum Networking.DotcomError
 
 /// Creates a top banner to be used when there is an error loading data on a screen.
 /// The banner has two action buttons: "Troubleshoot" and "Contact Support."
@@ -7,11 +8,19 @@ struct ErrorTopBannerFactory {
                                 expandedStateChangeHandler: @escaping () -> Void,
                                 onTroubleshootButtonPressed: @escaping () -> Void,
                                 onContactSupportButtonPressed: @escaping () -> Void) -> TopBannerView {
-        let errorType: ErrorType = error is DecodingError ? .decodingError : .generalError
+        let errorType: ErrorType = {
+            if error is DecodingError {
+                return .decodingError
+            } else if error as? DotcomError == .jetpackNotConnected {
+                return .jetpackConnectionError
+            } else {
+                return .generalError
+            }
+        }()
         let troubleshootAction = TopBannerViewModel.ActionButton(title: Localization.troubleshoot, action: { _ in onTroubleshootButtonPressed() })
         let contactSupportAction = TopBannerViewModel.ActionButton(title: Localization.contactSupport, action: { _ in onContactSupportButtonPressed() })
         let actions = [troubleshootAction, contactSupportAction]
-        let viewModel = TopBannerViewModel(title: Localization.title,
+        let viewModel = TopBannerViewModel(title: errorType.title,
                                            infoText: errorType.info,
                                            icon: .infoOutlineImage,
                                            isExpanded: true,
@@ -27,13 +36,33 @@ struct ErrorTopBannerFactory {
 
 extension ErrorTopBannerFactory {
     enum ErrorType {
+        /// An error decoding the JSON response
+        ///
         case decodingError
+
+        /// A broken Jetpack connection error
+        ///
+        case jetpackConnectionError
+
+        /// Any other error
+        ///
         case generalError
+
+        var title: String {
+            switch self {
+            case .jetpackConnectionError:
+                return Localization.jetpackConnectionTitle
+            default:
+                return Localization.generalTitle
+            }
+        }
 
         var info: String {
             switch self {
             case .decodingError:
                 return Localization.decodingInfo
+            case .jetpackConnectionError:
+                return Localization.jetpackConnectionInfo
             case .generalError:
                 return Localization.generalInfo
             }
@@ -41,13 +70,18 @@ extension ErrorTopBannerFactory {
     }
 
     enum Localization {
-        static let title = NSLocalizedString("We couldn't load your data",
-                                             comment: "The title of the Error Loading Data banner")
+        static let generalTitle = NSLocalizedString("We couldn't load your data",
+                                                    comment: "The title of the Error Loading Data banner")
+        static let jetpackConnectionTitle = NSLocalizedString("We couldn't connect to your store",
+                                                              comment: "The title of the Error Loading Data banner when there is a Jetpack connection error")
         static let generalInfo = NSLocalizedString("Please try again later or reach out to us and we'll be happy to assist you!",
                                                    comment: "The info of the Error Loading Data banner")
         static let decodingInfo = NSLocalizedString("This could be related to a conflict with a plugin. " +
                                                     "Please try again later or reach out to us and we'll be happy to assist you!",
                                                     comment: "The info on the Error Loading Data banner when there was a decoding error.")
+        static let jetpackConnectionInfo = NSLocalizedString("There is a problem with your store's Jetpack connection. " +
+                                                             "Please reconnect Jetpack or reach out to us and we'll be happy to assist you!",
+                                                             comment: "The info on the Error Loading Data banner when there was a Jetpack connection error.")
         static let troubleshoot = NSLocalizedString("Troubleshoot",
                                                     comment: "The title of the button to get troubleshooting information in the Error Loading Data banner")
         static let contactSupport = NSLocalizedString("Contact support",

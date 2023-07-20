667bf906e76ce253eb8fe64632133969ebbc67ff
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-07-20 19:23:07 +0530

    Make canceling async and track cancel event only if pending notification is present.

--- WooCommerce/Classes/Notifications/PushNotificationsManager.swift
+++ WooCommerce/Classes/Notifications/PushNotificationsManager.swift
@@ -363,11 +363,16 @@ extension PushNotificationsManager {
         await requestLocalNotification(notification, trigger: trigger)
     }
 
-    func cancelLocalNotification(scenarios: [LocalNotification.Scenario]) {
-        let center = UNUserNotificationCenter.current()
-        center.removePendingNotificationRequests(withIdentifiers: scenarios.map { $0.identifier })
-        scenarios.map(\.identifier).forEach { identifier in
-            analytics.track(.localNotificationCanceled, withProperties: ["type": LocalNotification.Scenario.identifierForAnalytics(identifier)])
+    func cancelLocalNotification(scenarios: [LocalNotification.Scenario]) async {
+        let center = configuration.userNotificationsCenter
+        let pending = await center.pendingNotificationRequests().filter {
+            scenarios.map { LocalNotification.Scenario.identifierForAnalytics($0.identifier) }
+                .contains(LocalNotification.Scenario.identifierForAnalytics($0.identifier))
+        }
+        center.removePendingNotificationRequests(withIdentifiers: pending.map { $0.identifier })
+        pending.forEach { request in
+            analytics.track(event: .LocalNotification.canceled(type: LocalNotification.Scenario.identifierForAnalytics(request.identifier),
+                                                               userInfo: request.content.userInfo))
         }
     }
 

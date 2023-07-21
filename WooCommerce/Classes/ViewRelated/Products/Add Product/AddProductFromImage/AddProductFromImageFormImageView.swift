7f773dae4e8898467e12389b2a7817bc7df403c7
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-07-21 14:11:59 +0530

    Move no text detected text inside `AddProductFromImageFormImageView`

--- WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductFromImage/AddProductFromImageFormImageView.swift
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductFromImage/AddProductFromImageFormImageView.swift
@@ -10,35 +10,43 @@ struct AddProductFromImageFormImageView: View {
     }
 
     var body: some View {
-        EditableImageView(imageState: viewModel.imageState,
-                          emptyContent: {
-            VStack(spacing: Layout.verticalSpacing) {
-                Image(uiImage: .addImage)
+        VStack {
+            EditableImageView(imageState: viewModel.imageState,
+                              emptyContent: {
+                VStack(spacing: Layout.verticalSpacing) {
+                    Image(uiImage: .addImage)
 
-                Label {
-                    Text(Localization.packagingImageTip)
-                } icon: {
-                    Image(uiImage: .sparklesImage)
-                }
-                .foregroundColor(.init(uiColor: .accent))
-                .fixedSize(horizontal: false, vertical: true)
-            }
-        })
-        .padding(Layout.padding)
-        .overlay(alignment: .topTrailing) {
-            Button {
-                isShowingActionSheet = true
-            } label: {
-                Image(systemName: "pencil.circle.fill")
-                    .symbolRenderingMode(.multicolor)
-                    .font(.system(size: Layout.editImageSize))
+                    Label {
+                        Text(Localization.packagingImageTip)
+                    } icon: {
+                        Image(uiImage: .sparklesImage)
+                    }
                     .foregroundColor(.init(uiColor: .accent))
-                    .renderedIf(viewModel.imageState.image != nil)
+                    .fixedSize(horizontal: false, vertical: true)
+                }
+            })
+            .padding(Layout.padding)
+            .overlay(alignment: .topTrailing) {
+                Button {
+                    isShowingActionSheet = true
+                } label: {
+                    Image(systemName: "pencil.circle.fill")
+                        .symbolRenderingMode(.multicolor)
+                        .font(.system(size: Layout.editImageSize))
+                        .foregroundColor(.init(uiColor: .accent))
+                        .renderedIf(viewModel.imageState.image != nil)
+                }
             }
+            .mediaSourceActionSheet(showsActionSheet: $isShowingActionSheet, selectMedia: { source in
+                viewModel.addImage(from: source)
+            })
+
+            Text(Localization.noTextDetected)
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundColor(Color(uiColor: .secondaryLabel))
+                .renderedIf(viewModel.shouldShowNoTextDetectedMessage)
         }
-        .mediaSourceActionSheet(showsActionSheet: $isShowingActionSheet, selectMedia: { source in
-            viewModel.addImage(from: source)
-        })
     }
 }
 
@@ -53,6 +61,10 @@ private extension AddProductFromImageFormImageView {
         static let packagingImageTip = NSLocalizedString(
             "Take a packaging photo to create product details with AI",
             comment: "Tip in the add product from image form to add a packaging image for AI-generated product details."
+        )
+        static let noTextDetected = NSLocalizedString(
+            "No text detected. Please select another packaging photo or enter product details manually.",
+            comment: "No text detected message on the add product from image form."
         )
     }
 }

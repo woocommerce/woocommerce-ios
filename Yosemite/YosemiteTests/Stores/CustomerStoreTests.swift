de6115b9079262ccb69f89e2be1096d7cc4eab7d
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2023-07-26 14:07:27 +0200

    Add tests for synchronizeCustomers

--- Yosemite/YosemiteTests/Stores/CustomerStoreTests.swift
+++ Yosemite/YosemiteTests/Stores/CustomerStoreTests.swift
@@ -91,6 +91,35 @@ final class CustomerStoreTests: XCTestCase {
         XCTAssertEqual(result.failure as? NetworkError, expectedError)
     }
 
+    func test_synchronizeCustomers_retrieves_wc_analytics_customers_and_parses_them_to_customers() {
+        network.simulateResponse(requestUrlSuffix: "customers", filename: "wc-analytics-customers")
+
+        // When
+        _ = waitFor { promise in
+            let action = CustomerAction.synchronizeCustomers(siteID: self.dummySiteID, pageNumber: 1, pageSize: 2) { result in
+                promise(result)
+            }
+            self.dispatcher.dispatch(action)
+        }
+
+        let customers = viewStorage
+            .loadAllCustomers(siteID: dummySiteID)
+            .map { $0.toReadOnly() }
+            .sorted(by: { $0.customerID < $1.customerID })
+
+        assertEqual(4, customers.count)
+        assertEqual(0, customers[0].customerID)
+        assertEqual(1, customers[1].customerID)
+        assertEqual(2, customers[2].customerID)
+        assertEqual(3, customers[3].customerID)
+        assertEqual("Matt", customers[0].firstName)
+        assertEqual("Unregistered", customers[0].lastName)
+        assertEqual("John", customers[1].firstName)
+        assertEqual("Paul", customers[2].firstName)
+        assertEqual("John", customers[3].firstName)
+        assertEqual("Doe", customers[3].lastName)
+    }
+
     func test_searchCustomers_when_three_results_found_then_returns_an_array_with_three_customer_entities() throws {
         // Given
         network.simulateResponse(requestUrlSuffix: "customers", filename: "wc-analytics-customers")

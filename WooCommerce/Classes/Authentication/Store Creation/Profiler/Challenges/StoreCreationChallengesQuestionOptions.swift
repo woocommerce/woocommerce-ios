7528a95242f596d573f84cdc824a39c93fba616b
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-08-03 10:36:24 +0530

    List of challenges as enum.

---
+++ WooCommerce/Classes/Authentication/Store Creation/Profiler/Challenges/StoreCreationChallengesQuestionOptions.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+extension StoreCreationChallengesQuestionViewModel {
+    // TODO: 10386 Align with Android and send these via tracks
+    enum Challenge: String, Equatable {
+        case settingUpTheOnlineStore = "setting-up-the-online-store"
+        case findingCustomers = "finding-customers"
+        case managingInventory = "managing-inventory"
+        case shippingAndLogistics = "shipping-and-logistics"
+        case other = "other"
+    }
+
+    var challenges: [Challenge] {
+        [
+            .settingUpTheOnlineStore,
+            .findingCustomers,
+            .managingInventory,
+            .shippingAndLogistics,
+            .other
+        ]
+    }
+}
+
+extension StoreCreationChallengesQuestionViewModel.Challenge {
+    var name: String {
+        switch self {
+        case .settingUpTheOnlineStore:
+            return NSLocalizedString("Setting up the online store", comment: "Challenge option in the store creation challenges question.")
+        case .findingCustomers:
+            return NSLocalizedString("Finding customers", comment: "Challenge option in the store creation challenges question.")
+        case .managingInventory:
+            return NSLocalizedString("Managing inventory", comment: "Challenge option in the store creation challenges question.")
+        case .shippingAndLogistics:
+            return NSLocalizedString("Shipping and logistics", comment: "Challenge option in the store creation challenges question.")
+        case .other:
+            return NSLocalizedString("Other", comment: "Challenge option in the store creation challenges question.")
+        }
+    }
+}

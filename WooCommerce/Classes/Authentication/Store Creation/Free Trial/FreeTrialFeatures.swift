b06acb52523d93098d043fa069378009a252bdd3
Huong Do <hi.huongdo@gmail.com>
2023-08-08 10:34:19 +0700

    Update content for free trial summary and subscriptions view

--- WooCommerce/Classes/Authentication/Store Creation/Free Trial/FreeTrialFeatures.swift
+++ WooCommerce/Classes/Authentication/Store Creation/Free Trial/FreeTrialFeatures.swift
@@ -7,44 +7,47 @@ struct FreeTrialFeatures {
     ///
     struct Feature {
         let title: String
-        let icon: UIImage
+        let contents: [String]
     }
 
     /// Free Trial Features
     ///
     static let features: [Feature] = [
-        .init(title: Localization.Feature.premiumThemes, icon: .premiumThemesIcon),
-        .init(title: Localization.Feature.unlimitedProducts, icon: .unlimitedProductsIcon),
-        .init(title: Localization.Feature.shippingLabels, icon: .shippingOutlineIcon),
-        .init(title: Localization.Feature.ecommerceReports, icon: .ecommerceIcon),
-        .init(title: Localization.Feature.paymentOptions, icon: .paymentOptionsIcon),
-        .init(title: Localization.Feature.subscriptions, icon: .giftIcon),
-        .init(title: Localization.Feature.advertising, icon: .advertisingIcon),
-        .init(title: Localization.Feature.marketing, icon: .emailOutlineIcon),
-        .init(title: Localization.Feature.support, icon: .supportIcon),
-        .init(title: Localization.Feature.backups, icon: .backupsIcon),
-        .init(title: Localization.Feature.security, icon: .siteSecurityIcon),
-        .init(title: Localization.Feature.launch, icon: .launchIcon)
+        .init(title: Localization.Title.startSellingQuickly, contents: [
+            Localization.Feature.themes,
+            Localization.Feature.storeManagement,
+            Localization.Feature.codeFree
+        ]),
+        .init(title: Localization.Title.growYourBusiness, contents: [
+            Localization.Feature.seo,
+            Localization.Feature.advertising,
+            Localization.Feature.email
+        ]),
+        .init(title: Localization.Title.hassleFreeStoreOwnership, contents: [
+            Localization.Feature.hosting,
+            Localization.Feature.backups,
+            Localization.Feature.support
+        ])
     ]
 }
 
 private extension FreeTrialFeatures {
     enum Localization {
+        enum Title {
+            static let startSellingQuickly = NSLocalizedString("Start selling quickly", comment: "Title of features on the Free Trial Summary screen")
+            static let growYourBusiness = NSLocalizedString("Grow your business", comment: "Title of features on the Free Trial Summary screen")
+            static let hassleFreeStoreOwnership = NSLocalizedString("Hassel-free store ownership", comment: "Title of features on the Free Trial Summary screen")
+        }
         enum Feature {
-            static let premiumThemes = NSLocalizedString("Premium themes", comment: "Premium Themes title feature on the Free Trial Summary Screen")
-            static let unlimitedProducts = NSLocalizedString("Unlimited products", comment: "Unlimited products title feature on the Free Trial Summary Screen")
-            static let shippingLabels = NSLocalizedString("Shipping labels", comment: "Shipping labels title feature on the Free Trial Summary Screen")
-            static let ecommerceReports = NSLocalizedString("Ecommerce reports", comment: "Ecommerce reports title feature on the Free Trial Summary Screen")
-            static let paymentOptions = NSLocalizedString("Multiple payment options",
-                    comment: "Multiple payment options title feature on the Free Trial Summary Screen")
-            static let subscriptions = NSLocalizedString("Subscriptions & product kits",
-                    comment: "Subscriptions & product kits title feature on the Free Trial Summary Screen")
+            static let themes = NSLocalizedString("Premium themes", comment: "Premium Themes title feature on the Free Trial Summary Screen")
+            static let storeManagement = NSLocalizedString("Store management tools", comment: "Store management title feature on the Free Trial Summary Screen")
+            static let codeFree = NSLocalizedString("Code-free design and editing", comment: "Code-free title feature on the Free Trial Summary Screen")
+            static let seo = NSLocalizedString("Built-in SEO", comment: "SEO title feature on the Free Trial Summary Screen")
             static let advertising = NSLocalizedString("Social advertising", comment: "Social advertising title feature on the Free Trial Summary Screen")
-            static let marketing = NSLocalizedString("Email marketing", comment: "Email marketing title feature on the Free Trial Summary Screen")
+            static let email = NSLocalizedString("Automated customer emails", comment: "Email title feature on the Free Trial Summary Screen")
+            static let hosting = NSLocalizedString("Best-in-class hosting", comment: "Hosting title feature on the Free Trial Summary Screen")
+            static let backups = NSLocalizedString("Automated site backups", comment: "Site backup title feature on the Free Trial Summary Screen")
             static let support = NSLocalizedString("24/7 support", comment: "24/7 support title feature on the Free Trial Summary Screen")
-            static let backups = NSLocalizedString("Auto updates & backups", comment: "Auto updates & backups title feature on the Free Trial Summary Screen")
-            static let security = NSLocalizedString("Site security", comment: "Site security title feature on the Free Trial Summary Screen")
-            static let launch = NSLocalizedString("Fast to launch", comment: "Fast to launch title feature on the Free Trial Summary Screen")
         }
     }
 }

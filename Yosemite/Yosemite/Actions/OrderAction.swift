91338d7e99bccf4a984d0265b32ec88c9e19663d
Huong Do <hi.huongdo@gmail.com>
2023-08-09 11:01:05 +0700

    Add new action to check if store has any orders

--- Yosemite/Yosemite/Actions/OrderAction.swift
+++ Yosemite/Yosemite/Actions/OrderAction.swift
@@ -109,4 +109,9 @@ public enum OrderAction: Action {
     /// Returns a publisher that emits inserted orders on the view storage context.
     ///
     case observeInsertedOrders(siteID: Int64, completion: (AnyPublisher<[Order], Never>) -> Void)
+
+    /// Checks if the store already has any orders.
+    /// Returns `false` if the store has no orders.
+    ///
+    case checkIfStoreHasOrders(siteID: Int64, onCompletion: (Result<Bool, Error>) -> Void)
 }

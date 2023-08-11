a0c7f41813a8a4c3a9ab094af114d194767bea8a
Huong Do <hi.huongdo@gmail.com>
2023-08-11 14:10:34 +0700

    Remove redundant MainActor attributes on StoreOnboardingCoordinator

--- WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingCoordinator.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingCoordinator.swift
@@ -34,7 +34,6 @@ final class StoreOnboardingCoordinator: Coordinator {
     }
 
     /// Navigates to the fullscreen store onboarding view.
-    @MainActor
     func start() {
         let onboardingNavigationController = UINavigationController()
         let onboardingViewController = StoreOnboardingViewHostingController(viewModel: .init(siteID: site.siteID,
@@ -48,7 +47,6 @@ final class StoreOnboardingCoordinator: Coordinator {
 
     /// Navigates to complete an onboarding task.
     /// - Parameter task: the task to complete.
-    @MainActor
     func start(task: StoreOnboardingTask) {
         switch task.type {
         case .storeDetails:
@@ -70,7 +68,6 @@ final class StoreOnboardingCoordinator: Coordinator {
 }
 
 private extension StoreOnboardingCoordinator {
-    @MainActor
     func showStoreDetails() {
         let coordinator = StoreOnboardingStoreDetailsCoordinator(site: site,
                                                                  navigationController: navigationController,
@@ -81,7 +78,6 @@ private extension StoreOnboardingCoordinator {
         coordinator.start()
     }
 
-    @MainActor
     func addProduct() {
         let coordinator = AddProductCoordinator(siteID: site.siteID,
                                                 source: .storeOnboarding,
@@ -95,7 +91,6 @@ private extension StoreOnboardingCoordinator {
         coordinator.start()
     }
 
-    @MainActor
     func showCustomDomains() {
         let coordinator = DomainSettingsCoordinator(source: .dashboardOnboarding,
                                                     site: site,
@@ -104,10 +99,11 @@ private extension StoreOnboardingCoordinator {
             self?.onTaskCompleted(.customizeDomains)
         })
         self.domainSettingsCoordinator = coordinator
-        coordinator.start()
+        Task { @MainActor in
+            coordinator.start()
+        }
     }
 
-    @MainActor
     func launchStore(task: StoreOnboardingTask) {
         let coordinator = StoreOnboardingLaunchStoreCoordinator(site: site,
                                                                 isLaunched: task.isComplete,
@@ -122,7 +118,6 @@ private extension StoreOnboardingCoordinator {
         coordinator.start()
     }
 
-    @MainActor
     func showWCPaySetup() {
         let coordinator = StoreOnboardingPaymentsSetupCoordinator(task: .wcPay,
                                                                   site: site,
@@ -134,7 +129,6 @@ private extension StoreOnboardingCoordinator {
         coordinator.start()
     }
 
-    @MainActor
     func showPaymentsSetup() {
         let coordinator = StoreOnboardingPaymentsSetupCoordinator(task: .payments,
                                                                   site: site,

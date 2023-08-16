f75e3c99e8cfb5d2992bb02f6326490b2abca811
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-08-16 13:45:23 +0530

    Remove TODO.

--- WooCommerce/Classes/Authentication/Store Creation/StoreCreationCoordinator.swift
+++ WooCommerce/Classes/Authentication/Store Creation/StoreCreationCoordinator.swift
@@ -86,7 +86,6 @@ private extension StoreCreationCoordinator {
         navigationController.isNavigationBarHidden = false
         let controller = StoreCreationProfilerQuestionContainerHostingController(viewModel: .init(storeName: storeName, onCompletion: { [weak self] answers in
             guard let self else { return }
-            // TODO: 10385 Upload profiler answers from `DashboardViewModel`
             if let answers {
                 self.analytics.track(event: .StoreCreation.siteCreationProfilerData(answers))
 

2052cc127284ddf43dc61c5ea44cacd8d3b703cd
Huong Do <hi.huongdo@gmail.com>
2023-08-16 16:53:07 +0700

    Remove hosting controller for StoreCreationChallengesQuestionView

--- WooCommerce/Classes/Authentication/Store Creation/Profiler/Challenges/StoreCreationChallengesQuestionView.swift
+++ WooCommerce/Classes/Authentication/Store Creation/Profiler/Challenges/StoreCreationChallengesQuestionView.swift
@@ -1,23 +1,5 @@
 import SwiftUI
 
-/// Hosting controller that wraps the `StoreCreationChallengesQuestionView`.
-final class StoreCreationChallengesQuestionHostingController: UIHostingController<StoreCreationChallengesQuestionView> {
-    init(viewModel: StoreCreationChallengesQuestionViewModel) {
-        super.init(rootView: StoreCreationChallengesQuestionView(viewModel: viewModel))
-    }
-
-    @available(*, unavailable)
-    required dynamic init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        configureTransparentNavigationBar()
-    }
-}
-
 /// Shows the store challenges question in the store creation flow.
 struct StoreCreationChallengesQuestionView: View {
     @ObservedObject private var viewModel: StoreCreationChallengesQuestionViewModel

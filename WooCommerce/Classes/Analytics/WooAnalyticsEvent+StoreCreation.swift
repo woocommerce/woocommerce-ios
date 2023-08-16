99a9c69107db1885a32a633d3f148c4696b80313
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-08-16 16:12:12 +0530

    Receive challenges and features and track it as properties.

--- WooCommerce/Classes/Analytics/WooAnalyticsEvent+StoreCreation.swift
+++ WooCommerce/Classes/Analytics/WooAnalyticsEvent+StoreCreation.swift
@@ -80,13 +80,17 @@ extension WooAnalyticsEvent {
                               properties: [Key.step: step.rawValue])
         }
 
-        /// Tracked when completing the last profiler question during the store creation flow when free trials are enabled.
-        static func siteCreationProfilerData(_ profilerData: StoreProfilerAnswers) -> WooAnalyticsEvent {
-            let properties = [
+        /// Tracked when completing the last profiler question during the store creation flow
+        static func siteCreationProfilerData(_ profilerData: StoreProfilerAnswers,
+                                             challenges: [StoreCreationChallengesAnswer],
+                                             features: [StoreCreationFeaturesAnswer]) -> WooAnalyticsEvent {
+            let properties: [String: WooAnalyticsEventPropertyType] = [
                 Key.category: profilerData.category,
                 Key.sellingStatus: profilerData.sellingStatus?.analyticsValue,
                 Key.sellingPlatforms: profilerData.sellingPlatforms,
-                Key.countryCode: profilerData.countryCode
+                Key.countryCode: profilerData.countryCode,
+                Key.challenges: challenges.map { $0.value }.joined(separator: ","),
+                Key.features: features.map { $0.value }.joined(separator: ",")
             ].compactMapValues({ $0 })
             return WooAnalyticsEvent(statName: .siteCreationProfilerData, properties: properties)
         }

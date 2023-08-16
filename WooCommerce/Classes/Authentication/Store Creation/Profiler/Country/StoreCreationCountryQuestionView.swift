8267aaa38a628771de2446a628ddbf58068405e9
Huong Do <hi.huongdo@gmail.com>
2023-08-16 16:52:42 +0700

    Remove hosting controller for StoreCreationCountryQuestionView

--- WooCommerce/Classes/Authentication/Store Creation/Profiler/Country/StoreCreationCountryQuestionView.swift
+++ WooCommerce/Classes/Authentication/Store Creation/Profiler/Country/StoreCreationCountryQuestionView.swift
@@ -1,23 +1,5 @@
 import SwiftUI
 
-/// Hosting controller that wraps the `StoreCreationCountryQuestionView`.
-final class StoreCreationCountryQuestionHostingController: UIHostingController<StoreCreationCountryQuestionView> {
-    init(viewModel: StoreCreationCountryQuestionViewModel) {
-        super.init(rootView: StoreCreationCountryQuestionView(viewModel: viewModel))
-    }
-
-    @available(*, unavailable)
-    required dynamic init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        configureTransparentNavigationBar()
-    }
-}
-
 /// Shows the store country question in the store creation flow.
 struct StoreCreationCountryQuestionView: View {
     @StateObject private var viewModel: StoreCreationCountryQuestionViewModel

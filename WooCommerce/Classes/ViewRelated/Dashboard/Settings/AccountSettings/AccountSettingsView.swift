d7a36de13837a8c0be1c2f0cfcbd0fdb809e79fc
Huong Do <hi.huongdo@gmail.com>
2023-08-18 17:50:00 +0700

    Update UI for the Account Settings screen

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/AccountSettings/AccountSettingsView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/AccountSettings/AccountSettingsView.swift
@@ -2,8 +2,8 @@ import SwiftUI
 
 /// Hosting controller for `AccountSettingsView`
 final class AccountSettingsHostingController: UIHostingController<AccountSettingsView> {
-    init(viewModel: AccountSettingsViewModel) {
-        super.init(rootView: AccountSettingsView(viewModel: viewModel))
+    init(onCloseAccount: @escaping () -> Void) {
+        super.init(rootView: AccountSettingsView(onCloseAccount: onCloseAccount))
     }
 
     required dynamic init?(coder aDecoder: NSCoder) {
@@ -13,19 +13,49 @@ final class AccountSettingsHostingController: UIHostingController<AccountSetting
 
 /// View for Account Settings
 struct AccountSettingsView: View {
-    private let viewModel: AccountSettingsViewModel
+    private let closeAccountHandler: () -> Void
 
-    init(viewModel: AccountSettingsViewModel) {
-        self.viewModel = viewModel
+    init(onCloseAccount: @escaping () -> Void) {
+        self.closeAccountHandler = onCloseAccount
     }
 
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            Button(action: closeAccountHandler) {
+                Text(Localization.closeAccount)
+                    .errorStyle()
+                    .padding(.vertical, Layout.buttonHorizontalPadding)
+            }
+            .frame(maxWidth: .infinity)
+            .cornerRadius(Layout.buttonCornerRadius)
+            .background(Color(.systemBackground))
+            .padding(.horizontal, Layout.contentHorizontalMargin)
+            .padding(.top, Layout.contentTopMargin)
+
+            Spacer()
+        }
+        .background(Color(.listBackground))
+        .navigationTitle(Localization.title)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+private extension AccountSettingsView {
+    enum Layout {
+        static let contentHorizontalMargin: CGFloat = 24
+        static let contentTopMargin: CGFloat = 32
+        static let buttonHorizontalPadding: CGFloat = 12
+        static let buttonCornerRadius: CGFloat = 8
+    }
+
+    enum Localization {
+        static let closeAccount = NSLocalizedString("Close Account", comment: "Button to close account on the Account Settings screen")
+        static let title = NSLocalizedString("Account Settings", comment: "Title of the Account Settings screen")
     }
 }
 
 struct AccountSettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        AccountSettingsView(viewModel: .init())
+        AccountSettingsView {}
     }
 }

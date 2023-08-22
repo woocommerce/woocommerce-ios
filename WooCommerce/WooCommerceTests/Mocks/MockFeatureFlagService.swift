7b3d51a959686fa04d2b0dbce0f03758ace0ff27
Huong Do <hi.huongdo@gmail.com>
2023-08-22 10:44:25 +0700

    Update MockFeatureFlagService

--- WooCommerce/WooCommerceTests/Mocks/MockFeatureFlagService.swift
+++ WooCommerce/WooCommerceTests/Mocks/MockFeatureFlagService.swift
@@ -6,7 +6,6 @@ struct MockFeatureFlagService: FeatureFlagService {
     private let isSplitViewInOrdersTabOn: Bool
     private let isUpdateOrderOptimisticallyOn: Bool
     private let shippingLabelsOnboardingM1: Bool
-    private let isLoginPrologueOnboardingEnabled: Bool
     private let isDomainSettingsEnabled: Bool
     private let isSupportRequestEnabled: Bool
     private let isDashboardStoreOnboardingEnabled: Bool
@@ -28,7 +27,6 @@ struct MockFeatureFlagService: FeatureFlagService {
          isSplitViewInOrdersTabOn: Bool = false,
          isUpdateOrderOptimisticallyOn: Bool = false,
          shippingLabelsOnboardingM1: Bool = false,
-         isLoginPrologueOnboardingEnabled: Bool = false,
          isDomainSettingsEnabled: Bool = false,
          isSupportRequestEnabled: Bool = false,
          isDashboardStoreOnboardingEnabled: Bool = false,
@@ -49,7 +47,6 @@ struct MockFeatureFlagService: FeatureFlagService {
         self.isSplitViewInOrdersTabOn = isSplitViewInOrdersTabOn
         self.isUpdateOrderOptimisticallyOn = isUpdateOrderOptimisticallyOn
         self.shippingLabelsOnboardingM1 = shippingLabelsOnboardingM1
-        self.isLoginPrologueOnboardingEnabled = isLoginPrologueOnboardingEnabled
         self.isDomainSettingsEnabled = isDomainSettingsEnabled
         self.isSupportRequestEnabled = isSupportRequestEnabled
         self.isDashboardStoreOnboardingEnabled = isDashboardStoreOnboardingEnabled
@@ -78,8 +75,6 @@ struct MockFeatureFlagService: FeatureFlagService {
             return isUpdateOrderOptimisticallyOn
         case .shippingLabelsOnboardingM1:
             return shippingLabelsOnboardingM1
-        case .loginPrologueOnboarding:
-            return isLoginPrologueOnboardingEnabled
         case .domainSettings:
             return isDomainSettingsEnabled
         case .supportRequests:

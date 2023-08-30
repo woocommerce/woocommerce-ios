9ab650fb88330a537bdfdcfed9678bf7f238e024
Huong Do <hi.huongdo@gmail.com>
2023-08-30 11:18:47 +0700

    Switch to new web view model for woo pay setup

--- WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingPaymentsSetupCoordinator.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingPaymentsSetupCoordinator.swift
@@ -59,7 +59,9 @@ private extension StoreOnboardingPaymentsSetupCoordinator {
             return assertionFailure("Invalid URL for onboarding payments setup: \(urlString)")
         }
 
-        let webViewModel = WPAdminWebViewModel(title: title, initialURL: url)
+        let webViewModel = WooPaymentSetupWebViewModel(title: title, initialURL: url) { [weak self] in
+            self?.dismissWebview()
+        }
         let webViewController = AuthenticatedWebViewController(viewModel: webViewModel)
         webViewController.navigationItem.leftBarButtonItem = .init(barButtonSystemItem: .done, target: self, action: #selector(dismissWebview))
         navigationController.show(webViewController, sender: navigationController)

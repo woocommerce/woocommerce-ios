1fff2330c618dcf9f02bbdeb25c87083700d448b
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2023-08-31 16:19:15 +0200

    Add a new model with the Tax Rate object

---
+++ Storage/Storage/Model/TaxRate+CoreDataClass.swift
@@ -0,0 +1,7 @@
+import Foundation
+import CoreData
+
+@objc(TaxRate)
+public class TaxRate: NSManagedObject {
+
+}
---
+++ Storage/Storage/Model/TaxRate+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+import Foundation
+import CoreData
+
+
+extension TaxRate {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<TaxRate> {
+        return NSFetchRequest<TaxRate>(entityName: "TaxRate")
+    }
+
+    @NSManaged public var id: Int64
+    @NSManaged public var country: String?
+    @NSManaged public var state: String?
+    @NSManaged public var postcode: String?
+    @NSManaged public var postcodes: NSObject?
+    @NSManaged public var priority: Int64
+    @NSManaged public var rate: String?
+    @NSManaged public var name: String?
+    @NSManaged public var order: Int64
+    @NSManaged public var taxRateClass: String?
+    @NSManaged public var shipping: Bool
+    @NSManaged public var compound: Bool
+    @NSManaged public var city: String?
+    @NSManaged public var cities: NSObject?
+
+}

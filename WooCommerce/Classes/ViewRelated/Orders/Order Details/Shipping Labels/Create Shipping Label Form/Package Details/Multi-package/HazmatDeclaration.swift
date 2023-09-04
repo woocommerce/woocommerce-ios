d997d7237113c37206c40142ae61beef9fab59d9
ThomazFB <thomazfcortez@gmail.com>
2023-09-04 14:01:13 -0300

    Define HazmatDeclaration file

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Create Shipping Label Form/Package Details/Multi-package/HazmatDeclaration.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct HazmatDeclaration: View {
+    var body: some View {
+        Text("Hazmat Declaration")
+    }
+}
+
+private extension HazmatDeclaration {
+    enum Localization {
+        
+    }
+}
+
+struct HazmatDeclaration_Previews: PreviewProvider {
+    static var previews: some View {
+        HazmatDeclaration()
+    }
+}

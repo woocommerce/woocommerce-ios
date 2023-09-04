8daf810dac88eb54411abdf6ddd64c32ee179e07
Ernesto Carrión <ernesto.carrion@automattic.com>
2023-09-04 14:58:17 -0500

    Adds Empty Order Notification Content extension

---
+++ WooCommerce/NotificationExtension/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  NotificationExtension
+//
+//  Created by Ernesto Carrion on 4/09/23.
+//  Copyright © 2023 Automattic. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}

00dcb466569eefa39d5e96e6fb143f5f2aed8b3a
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2023-09-07 17:45:58 +0200

    Enable feature flag for the manual taxes milestone 2

--- Experiments/Experiments/DefaultFeatureFlagService.swift
+++ Experiments/Experiments/DefaultFeatureFlagService.swift
@@ -86,7 +86,7 @@ public struct DefaultFeatureFlagService: FeatureFlagService {
         case .betterCustomerSelectionInOrder:
             return true
         case .manualTaxesInOrderM2:
-            return buildConfig == .localDeveloper || buildConfig == .alpha
+            return true
         case .hazmatShipping:
             return buildConfig == .localDeveloper || buildConfig == .alpha
         default:

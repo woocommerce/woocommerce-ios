3af3c462433fd685cda09756bd1c60edd031e24c
Huong Do <hi.huongdo@gmail.com>
2023-09-11 12:13:49 +0700

    Restore removed items when deleting category fails

--- WooCommerce/Classes/ViewRelated/Products/Categories/ProductCategoryListViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Categories/ProductCategoryListViewModel.swift
@@ -221,6 +221,7 @@ final class ProductCategoryListViewModel {
     @MainActor
     func deleteCategory(id: Int64) async {
         deletionFailure = nil
+        let selectedItem = selectedCategories.first(where: { $0.categoryID != id })
         do {
             // optimistic deletion
             categoryViewModels.removeAll(where: { $0.categoryID == id })
@@ -228,9 +229,15 @@ final class ProductCategoryListViewModel {
             // removes the category from the selected list if exists
             selectedCategories = selectedCategories.filter { $0.categoryID != id }
         } catch {
+            // restore removed items
+            updateViewModelsArray()
+            if let selectedItem {
+                selectedCategories.append(selectedItem)
+            }
             deletionFailure = error
+            onReloadNeeded?()
         }
-        // fetches list again to update UI
+        // fetches list again to update storage
         synchronizeAllCategories()
     }
 

1a11cb963f272e929d5b281011e2f4bd0fecf4a4
Josh Heald <joshheald@users.noreply.github.com>
2023-09-12 11:27:59 +0100

    Allow payment retry when there’s no PI to cancel
    Payments which fail before the payment intent is created can still be retried.
    
    They should follow the restart flow, which requires that the active payment intent be cancelled, and then a new PI be created for the retry.
    
    Previously, if cancelling the payment intent failed because there was no active payment intent, we prevented the retry – which didn’t make sense.
    
    If there’s no payment intent, it’s safe to make a new one. This commit adds an underlying error to that effect, and allows the retry to continue when this error is returned from a cancellation attempt.

--- WooCommerce/Classes/ViewRelated/Orders/Collect Payments/CollectOrderPaymentUseCase.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Collect Payments/CollectOrderPaymentUseCase.swift
@@ -410,7 +410,7 @@ private extension CollectOrderPaymentUseCase {
                                                    guard let self = self else { return }
 
                                                    switch result {
-                                                   case .success:
+                                                   case .success, .failure(CardReaderServiceError.paymentCancellation(.noActivePaymentIntent)):
                                                        // Retry payment
                                                        self.attemptPayment(alertProvider: paymentAlerts,
                                                                            paymentGatewayAccount: paymentGatewayAccount,

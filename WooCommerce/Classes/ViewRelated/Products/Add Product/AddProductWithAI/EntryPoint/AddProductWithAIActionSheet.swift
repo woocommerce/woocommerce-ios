c2ca702a9b9e06d35b341af553f4dbe959dda2a4
Huong Do <hi.huongdo@gmail.com>
2023-09-14 15:50:31 +0700

    Add legal text for AI feature

--- WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/EntryPoint/AddProductWithAIActionSheet.swift
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/EntryPoint/AddProductWithAIActionSheet.swift
@@ -3,22 +3,33 @@ import SwiftUI
 /// View to show options for adding a new product including one with AI assistance.
 ///
 struct AddProductWithAIActionSheet: View {
+    @Environment(\.customOpenURL) private var customOpenURL
+    @State private var legalURL: URL?
+
     var body: some View {
-        VStack(alignment: .leading, spacing: Layout.margin) {
+        VStack(alignment: .leading, spacing: Constants.margin) {
             Text(Localization.title)
                 .subheadlineStyle()
-                .padding(.vertical, Layout.margin)
+                .padding(.vertical, Constants.margin)
 
             // AI option
-            HStack(alignment: .top, spacing: Layout.margin) {
+            HStack(alignment: .top, spacing: Constants.margin) {
                 Image(uiImage: .sparklesImage)
                     .renderingMode(.template)
                     .foregroundColor(.accentColor)
-                VStack(alignment: .leading, spacing: Layout.verticalSpacing) {
+                VStack(alignment: .leading, spacing: Constants.verticalSpacing) {
                     Text(Localization.aiTitle)
                         .bodyStyle()
                     Text(Localization.aiDescription)
                         .subheadlineStyle()
+                    Text(.init(Localization.legalText))
+                        .font(.caption)
+                        .foregroundColor(Color(.secondaryLabel))
+                        .padding(.top, Constants.margin)
+                        .environment(\.customOpenURL) { url in
+                            legalURL = url
+                        }
+                        .safariSheet(url: $legalURL)
                 }
                 Spacer()
             }
@@ -26,11 +37,11 @@ struct AddProductWithAIActionSheet: View {
             Divider()
 
             // Manual option
-            HStack(alignment: .top, spacing: Layout.margin) {
+            HStack(alignment: .top, spacing: Constants.margin) {
                 Image(systemName: "plus.circle")
                     .font(.title3)
                     .foregroundColor(.secondary)
-                VStack(alignment: .leading, spacing: Layout.verticalSpacing) {
+                VStack(alignment: .leading, spacing: Constants.verticalSpacing) {
                     Text(Localization.manualTitle)
                         .bodyStyle()
                     Text(Localization.manualDescription)
@@ -39,14 +50,15 @@ struct AddProductWithAIActionSheet: View {
                 Spacer()
             }
         }
-        .padding(.horizontal, Layout.margin)
+        .padding(.horizontal, Constants.margin)
     }
 }
 
 private extension AddProductWithAIActionSheet {
-    enum Layout {
+    enum Constants {
         static let verticalSpacing: CGFloat = 8
         static let margin: CGFloat = 16
+        static let legalURL = "https://automattic.com/ai-guidelines/"
     }
 
     enum Localization {
@@ -61,6 +73,11 @@ private extension AddProductWithAIActionSheet {
         static let aiDescription = NSLocalizedString(
             "Quickly generate details for you",
             comment: "Description of the option to add new product with AI assistance"
+        )
+        static let legalText = NSLocalizedString(
+            "Powered by AI. [Learn more.](https://automattic.com/ai-guidelines/)",
+            comment: "Markdown content for the label to indicate AI-generated content on the product creation action sheet. " +
+            "Please translate the words while keeping the markdown format and URL"
         )
         static let manualTitle = NSLocalizedString(
             "Add manually",

d50de9abcce0f5c6e07798e552b21f0502d5d0d5
Huong Do <hi.huongdo@gmail.com>
2023-09-14 16:44:53 +0700

    Present new action sheet for product creation

--- WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingCoordinator.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Onboarding/StoreOnboardingCoordinator.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Experiments
 import SwiftUI
 import UIKit
 import struct Yosemite.Site
@@ -16,8 +17,10 @@ final class StoreOnboardingCoordinator: Coordinator {
     private var launchStoreCoordinator: StoreOnboardingLaunchStoreCoordinator?
     private var paymentsSetupCoordinator: StoreOnboardingPaymentsSetupCoordinator?
     private var wooPaySetupCelebrationViewBottomSheetPresenter: BottomSheetPresenter?
+    private var addProductWithAIBottomSheetPresenter: BottomSheetPresenter?
 
     private let site: Site
+    private let featureFlagService: FeatureFlagService
     private let onTaskCompleted: (_ task: TaskType) -> Void
     private let reloadTasks: () -> Void
     private let onUpgradePlan: (() -> Void)?
@@ -30,11 +33,13 @@ final class StoreOnboardingCoordinator: Coordinator {
 
     init(navigationController: UINavigationController,
          site: Site,
+         featureFlagService: FeatureFlagService = ServiceLocator.featureFlagService,
          onTaskCompleted: @escaping (_ task: TaskType) -> Void,
          reloadTasks: @escaping () -> Void,
          onUpgradePlan: (() -> Void)? = nil) {
         self.navigationController = navigationController
         self.site = site
+        self.featureFlagService = featureFlagService
         self.onTaskCompleted = onTaskCompleted
         self.reloadTasks = reloadTasks
         self.onUpgradePlan = onUpgradePlan
@@ -88,11 +93,37 @@ private extension StoreOnboardingCoordinator {
     }
 
     func addProduct() {
+        // TODO-10688: Replace this with eligibility check
+        if featureFlagService.isFeatureFlagEnabled(.productCreationAI) {
+            showAddProductWithAIActionSheet()
+        } else {
+            startRegularProductCreationFlow(showsTemplateOption: true)
+        }
+    }
+
+    func showAddProductWithAIActionSheet() {
+        let controller = AddProductWithAIActionSheetHostingController(onAIOption: { [weak self] in
+            self?.addProductWithAIBottomSheetPresenter?.dismiss {
+                self?.addProductWithAIBottomSheetPresenter = nil
+                // TODO-10688: start AI flow
+            }
+        }, onManualOption: { [weak self] in
+            self?.addProductWithAIBottomSheetPresenter?.dismiss {
+                self?.addProductWithAIBottomSheetPresenter = nil
+                self?.startRegularProductCreationFlow(showsTemplateOption: false)
+            }
+        })
+
+        addProductWithAIBottomSheetPresenter = buildBottomSheetPresenter(height: navigationController.view.frame.height * 0.35)
+        addProductWithAIBottomSheetPresenter?.present(controller, from: navigationController)
+    }
+
+    func startRegularProductCreationFlow(showsTemplateOption: Bool) {
         let coordinator = AddProductCoordinator(siteID: site.siteID,
                                                 source: .storeOnboarding,
                                                 sourceView: nil,
                                                 sourceNavigationController: navigationController,
-                                                isFirstProduct: true)
+                                                isFirstProduct: showsTemplateOption)
         self.addProductCoordinator = coordinator
         coordinator.onProductCreated = { [weak self] _ in
             self?.onTaskCompleted(.addFirstProduct)
@@ -183,13 +214,20 @@ private extension StoreOnboardingCoordinator {
 // MARK: Bottom sheet helpers
 //
 private extension StoreOnboardingCoordinator {
-    func buildBottomSheetPresenter() -> BottomSheetPresenter {
+    func buildBottomSheetPresenter(height: CGFloat? = nil) -> BottomSheetPresenter {
         BottomSheetPresenter(configure: { bottomSheet in
             var sheet = bottomSheet
             sheet.prefersEdgeAttachedInCompactHeight = true
             sheet.largestUndimmedDetentIdentifier = .none
             sheet.prefersGrabberVisible = true
-            sheet.detents = [.medium()]
+            if #available(iOS 16.0, *), let height {
+                let customHeight = UISheetPresentationController.Detent.custom { _ in
+                    height
+                }
+                sheet.detents = [.medium(), customHeight]
+            } else {
+                sheet.detents = [.medium()]
+            }
         })
     }
 }

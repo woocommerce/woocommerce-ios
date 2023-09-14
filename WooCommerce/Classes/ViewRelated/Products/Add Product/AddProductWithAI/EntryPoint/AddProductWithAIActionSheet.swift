c9e052b1af3b454e4748c021187c82bc737e0e45
Huong Do <hi.huongdo@gmail.com>
2023-09-14 14:06:01 +0700

    Add new view for entry point of product creation with AI

---
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/EntryPoint/AddProductWithAIActionSheet.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// View to show options for adding a new product including one with AI assistance.
+///
+struct AddProductWithAIActionSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddProductWithAIActionSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        AddProductWithAIActionSheet()
+    }
+}

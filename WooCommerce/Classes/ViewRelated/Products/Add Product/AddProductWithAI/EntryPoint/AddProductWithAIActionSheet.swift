b052d6925b909674fe27400546f6d711314dc6c9
Huong Do <hi.huongdo@gmail.com>
2023-09-14 14:12:13 +0700

    Add localized strings for new product creation sheet

--- WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/EntryPoint/AddProductWithAIActionSheet.swift
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/EntryPoint/AddProductWithAIActionSheet.swift
@@ -8,6 +8,36 @@ struct AddProductWithAIActionSheet: View {
     }
 }
 
+private extension AddProductWithAIActionSheet {
+    enum Layout {
+        static let verticalSpacing: CGFloat = 8
+        static let margin: CGFloat = 16
+    }
+
+    enum Localization {
+        static let title = NSLocalizedString(
+            "Add a product",
+            comment: "Title on the action sheet to select an option for adding new product"
+        )
+        static let aiTitle = NSLocalizedString(
+            "Create a product with AI",
+            comment: "Title of the option to add new product with AI assistance"
+        )
+        static let aiDescription = NSLocalizedString(
+            "Quickly generate details for you",
+            comment: "Description of the option to add new product with AI assistance"
+        )
+        static let manualTitle = NSLocalizedString(
+            "Add manually",
+            comment: "Title of the option to add new product manually"
+        )
+        static let manualDescription = NSLocalizedString(
+            "Add a product and the details manually",
+            comment: "Description of the option to add new product manually"
+        )
+    }
+}
+
 struct AddProductWithAIActionSheet_Previews: PreviewProvider {
     static var previews: some View {
         AddProductWithAIActionSheet()

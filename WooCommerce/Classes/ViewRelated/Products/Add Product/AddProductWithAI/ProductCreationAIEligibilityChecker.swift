44f3b7f1b2890c0b02a02613c369534f3320df1f
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-09-15 10:54:13 +0530

    Inject feature flag service. Receive store has products info as method param.

--- WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/ProductCreationAIEligibilityChecker.swift
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/ProductCreationAIEligibilityChecker.swift
@@ -4,22 +4,26 @@ import Experiments
 
 /// Protocol for checking "add product using AI" eligibility for easier unit testing.
 protocol ProductCreationAIEligibilityCheckerProtocol {
-    /// Checks if the user is eligible for the "add product from image" feature.
-    func isEligible() -> Bool
+    /// Checks if the user is eligible for the "add product using AI" feature.
+    func isEligible(storeHasProducts: Bool) -> Bool
 }
 
 /// Checks the eligibility for the "add product using AI" feature.
 final class ProductCreationAIEligibilityChecker: ProductCreationAIEligibilityCheckerProtocol {
     private let stores: StoresManager
-    private let storeHasProducts: Bool
+    private let featureFlagService: FeatureFlagService
 
     init(stores: StoresManager = ServiceLocator.stores,
-         storeHasProducts: Bool) {
+         featureFlagService: FeatureFlagService = ServiceLocator.featureFlagService) {
         self.stores = stores
-        self.storeHasProducts = storeHasProducts
+        self.featureFlagService = featureFlagService
     }
 
-    func isEligible() -> Bool {
+    func isEligible(storeHasProducts: Bool) -> Bool {
+        guard featureFlagService.isFeatureFlagEnabled(.productCreationAI) else {
+            return false
+        }
+
         guard let site = stores.sessionManager.defaultSite else {
             return false
         }

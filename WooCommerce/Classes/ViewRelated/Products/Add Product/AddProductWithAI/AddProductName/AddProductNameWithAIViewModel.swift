0ca04fbf5727eb32364011e55e1f3cc6c5700606
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-09-18 12:18:20 +0530

    Add AddProductNameWithAIView UI.

---
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/AddProductName/AddProductNameWithAIViewModel.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+final class AddProductNameWithAIViewModel: ObservableObject {
+    @Published var productNameContent: String = ""
+    private let onUsePackagePhoto: (String?) -> Void
+    private let onContinueWithProductName: (String) -> Void
+
+    init(siteID: Int64,
+         onUsePackagePhoto: @escaping (String?) -> Void,
+         onContinueWithProductName: @escaping (String) -> Void) {
+        self.onUsePackagePhoto = onUsePackagePhoto
+        self.onContinueWithProductName = onContinueWithProductName
+    }
+
+    func didTapUsePackagePhoto() {
+        let productName: String? = {
+            guard productNameContent.isNotEmpty else {
+                return nil
+            }
+            return productNameContent
+        }()
+        onUsePackagePhoto(productName)
+    }
+
+    func didTapSuggestName() {
+        // TODO: Present suggest name sheet
+    }
+
+    func didTapContinue() {
+        onContinueWithProductName(productNameContent)
+    }
+}

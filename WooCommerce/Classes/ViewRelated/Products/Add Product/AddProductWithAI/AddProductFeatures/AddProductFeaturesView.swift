efe28fcd027ab8f43fa28afe08637f9bd7aebd49
Huong Do <hi.huongdo@gmail.com>
2023-09-20 16:03:01 +0700

    Add new view for adding product features

---
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/AddProductFeatures/AddProductFeaturesView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// View for adding product features in the product creation with AI flow.
+///
+struct AddProductFeaturesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddProductDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddProductFeaturesView()
+    }
+}

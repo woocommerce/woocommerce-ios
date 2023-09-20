fafbb28fce346bb442d93e93f82b7a73476d10af
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-09-20 14:10:26 +0530

    Enable product description AI generation for self hosted sites with Jetpack AI

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Edit Product/ProductFormAIEligibilityCheckerTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Edit Product/ProductFormAIEligibilityCheckerTests.swift
@@ -28,6 +28,19 @@ final class ProductFormAIEligibilityCheckerTests: XCTestCase {
         XCTAssertFalse(isDescriptionAIEnabled)
     }
 
+    func test_description_feature_is_enabled_when_site_is_not_wpcom_and_ai_assistant_feature_is_active() throws {
+        // Given
+        let featureFlagService = MockFeatureFlagService(isProductDescriptionAIEnabled: true)
+        let checker = ProductFormAIEligibilityChecker(site: .fake().copy(isAIAssistantFeatureActive: true, isWordPressComStore: false),
+                                                      featureFlagService: featureFlagService)
+
+        // When
+        let isDescriptionAIEnabled = checker.isFeatureEnabled(.description)
+
+        // Then
+        XCTAssertTrue(isDescriptionAIEnabled)
+    }
+
     func test_description_feature_is_disabled_when_site_is_nil() throws {
         // Given
         let featureFlagService = MockFeatureFlagService(isProductDescriptionAIEnabled: true)

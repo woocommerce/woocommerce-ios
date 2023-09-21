b8bf29796e1c2d2fe9766bbaa14eaf6d23608f1a
Huong Do <hi.huongdo@gmail.com>
2023-09-21 13:27:41 +0700

    Add new view for previewing generated product details

---
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/Preview/ProductDetailPreviewView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// View for previewing product details generated with AI.
+///
+struct ProductDetailPreviewView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ProductDetailPreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailPreviewView()
+    }
+}

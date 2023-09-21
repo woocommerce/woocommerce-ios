8aedc71355548d80e47a8db2e746bc6ae06c622d
Huong Do <hi.huongdo@gmail.com>
2023-09-21 14:39:38 +0700

    Add title and subtitle for preview screen

--- WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/Preview/ProductDetailPreviewView.swift
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/Preview/ProductDetailPreviewView.swift
@@ -3,10 +3,47 @@ import SwiftUI
 /// View for previewing product details generated with AI.
 ///
 struct ProductDetailPreviewView: View {
+
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            VStack(alignment: .leading, spacing: Layout.blockVerticalSpacing) {
+                VStack(alignment: .leading, spacing: Layout.titleBlockSpacing) {
+                    // Title label.
+                    Text(Localization.title)
+                        .fontWeight(.bold)
+                        .titleStyle()
+
+                    // Subtitle label.
+                    Text(Localization.subtitle)
+                        .foregroundColor(Color(.secondaryLabel))
+                        .bodyStyle()
+                }
+
+            }
+            .padding(insets: Layout.insets)
+        }
     }
 }
+
+private extension ProductDetailPreviewView {
+    enum Layout {
+        static let insets: EdgeInsets = .init(top: 24, leading: 16, bottom: 16, trailing: 16)
+
+        static let blockVerticalSpacing: CGFloat = 40
+        static let titleBlockSpacing: CGFloat = 16
+    }
+    enum Localization {
+        static let title = NSLocalizedString(
+            "Preview",
+            comment: "Title on the add product with AI Preview screen."
+        )
+        static let subtitle = NSLocalizedString(
+            "Don't worry. You can always change those details later.",
+            comment: "Subtitle on the add product with AI Preview screen."
+        )
+    }
+}
+
 
 struct ProductDetailPreviewView_Previews: PreviewProvider {
     static var previews: some View {

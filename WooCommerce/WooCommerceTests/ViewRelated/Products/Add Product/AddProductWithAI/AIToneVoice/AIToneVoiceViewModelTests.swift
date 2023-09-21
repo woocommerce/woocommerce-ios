b7aab037c99d369383003b898d7232985bc4edae
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-09-21 16:13:40 +0530

    Unit tests for AIToneVoiceViewModel.

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Add Product/AddProductWithAI/AIToneVoice/AIToneVoiceViewModelTests.swift
@@ -0,0 +1,72 @@
+import TestKit
+import XCTest
+
+@testable import WooCommerce
+
+final class AIToneVoiceViewModelTests: XCTestCase {
+    private let siteID: Int64 = 123
+
+    func test_casual_is_the_default_tone() throws {
+        // Given
+        let uuid = UUID().uuidString
+        let defaults = try XCTUnwrap(UserDefaults(suiteName: uuid))
+
+        // When
+        let viewModel = AIToneVoiceViewModel(siteID: siteID, userDefaults: defaults)
+
+        // Then
+        XCTAssertEqual(viewModel.selectedTone, .casual)
+    }
+
+    func test_tone_is_restored_from_user_defaults() throws {
+        // Given
+        let uuid = UUID().uuidString
+        let defaults = try XCTUnwrap(UserDefaults(suiteName: uuid))
+        defaults.setAITone(.flowery, for: siteID)
+
+        // When
+        let viewModel = AIToneVoiceViewModel(siteID: siteID, userDefaults: defaults)
+
+        // Then
+        XCTAssertEqual(viewModel.selectedTone, .flowery)
+    }
+
+    func test_tone_gets_stored_on_selection() throws {
+        // Given
+        let uuid = UUID().uuidString
+        let defaults = try XCTUnwrap(UserDefaults(suiteName: uuid))
+        let viewModel = AIToneVoiceViewModel(siteID: siteID, userDefaults: defaults)
+
+        // When
+        viewModel.onSelectTone(.flowery)
+
+        // Then
+        XCTAssertEqual(try XCTUnwrap(defaults.aiTone(for: siteID)), .flowery)
+    }
+
+    // MARK: - AI tone helpers
+    func test_stored_tone_is_restored_as_expected() throws {
+        // Given
+        let uuid = UUID().uuidString
+        let defaults = try XCTUnwrap(UserDefaults(suiteName: uuid))
+        defaults[.aiPromptTone] = ["\(siteID)": AIToneVoice.convincing.rawValue]
+
+        // Then
+        XCTAssertEqual(try XCTUnwrap(defaults.aiTone(for: siteID)), .convincing)
+    }
+
+    func test_tone_gets_stored_as_expected() throws {
+        // Given
+        let uuid = UUID().uuidString
+        let defaults = try XCTUnwrap(UserDefaults(suiteName: uuid))
+        defaults[.aiPromptTone] = ["\(siteID)": AIToneVoice.convincing.rawValue]
+        XCTAssertEqual(try XCTUnwrap(defaults.aiTone(for: siteID)), .convincing)
+
+        // When
+        defaults.setAITone(.convincing, for: siteID)
+
+        // Then
+        let dictionary = try XCTUnwrap(defaults[.aiPromptTone] as? [String: String])
+        XCTAssertEqual(dictionary["\(siteID)"], AIToneVoice.convincing.rawValue)
+    }
+}

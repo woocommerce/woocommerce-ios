715dc6f5fd423bc9a0d8f44cfe376e7b9b8f6980
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-09-26 17:56:44 +0530

    Test that response with shipping object is parsed correctly.

--- Networking/NetworkingTests/Mapper/AIProductMapperTests.swift
+++ Networking/NetworkingTests/Mapper/AIProductMapperTests.swift
@@ -26,7 +26,31 @@ final class AIProductMapperTests: XCTestCase {
         XCTAssertEqual(product.dimensions.width, "10")
         XCTAssertEqual(product.dimensions.height, "5")
         XCTAssertEqual(product.price, "250")
-}
+    }
+
+    func test_it_maps_product_correctly_when_no_shipping_info_available() throws {
+        // Given
+        let data = try retrieveGenerateProductNoShippingInfoResponse()
+        let mapper = AIProductMapper(siteID: siteID,
+                                     existingCategories: [.fake(), .fake()],
+                                     existingTags: [.fake(), .fake()])
+
+        // When
+        let product = try mapper.map(response: data)
+
+        // Then
+        XCTAssertEqual(product.name, "Cookie")
+        // swiftlint:disable line_length
+        XCTAssertEqual(product.fullDescription, "Introducing Cookie, the ultimate crunchy and crispy treat that will satisfy your snacking cravings. Made with the finest ingredients, these irresistible cookies are baked to perfection, delivering a delightful texture with every bite. Whether you're enjoying them with a cup of tea or sharing them with friends, Cookie is the go-to snack for any casual occasion. Indulge in the mouthwatering flavors and experience a taste sensation that will leave you wanting more. Get your hands on Cookie today and discover why it's the ultimate snack companion.")
+        XCTAssertEqual(product.shortDescription, "The ultimate crunchy and crispy treat that will satisfy your snacking cravings. Made with the finest ingredients, these irresistible cookies are baked to perfection, delivering a delightful texture with every bite. Indulge in the mouthwatering flavors of Cookie today!")
+        // swiftlint:enable line_length
+        XCTAssertFalse(product.virtual)
+        XCTAssertEqual(product.weight, "")
+        XCTAssertEqual(product.dimensions.length, "")
+        XCTAssertEqual(product.dimensions.width, "")
+        XCTAssertEqual(product.dimensions.height, "")
+        XCTAssertEqual(product.price, "250")
+    }
 
     func test_it_maps_product_with_matching_existing_categories() throws {
         // Given
@@ -65,6 +89,14 @@ final class AIProductMapperTests: XCTestCase {
 private extension AIProductMapperTests {
     func retrieveGenerateProductResponse() throws -> Data {
         guard let response = Loader.contentsOf("generate-product-success") else {
+            throw FileNotFoundError()
+        }
+
+        return response
+    }
+
+    func retrieveGenerateProductNoShippingInfoResponse() throws -> Data {
+        guard let response = Loader.contentsOf("generate-product-success-no-shipping-info") else {
             throw FileNotFoundError()
         }
 

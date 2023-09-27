66eeed300f6a696ab5d6154ac26be736ff424e9a
Jaclyn Chen <watertranquil@gmail.com>
2023-09-27 22:07:12 +0800

    Add rounded border button style.

--- WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/ButtonStyles.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/ButtonStyles.swift
@@ -99,6 +99,15 @@ struct PlusButtonStyle: ButtonStyle {
     }
 }
 
+/// Button with a rounded border with default corner radius and a configurable color.
+struct RoundedBorderedStyle: ButtonStyle {
+    let borderColor: Color
+
+    func makeBody(configuration: Configuration) -> some View {
+        return RoundedBorderedButton(configuration: configuration, borderColor: borderColor)
+    }
+}
+
 /// Adds a primary button style while showing a progress view on top of the button when required.
 ///
 struct PrimaryLoadingButtonStyle: PrimitiveButtonStyle {
@@ -386,6 +395,22 @@ private struct PlusButton: View {
     }
 }
 
+private struct RoundedBorderedButton: View {
+    let configuration: ButtonStyleConfiguration
+    let borderColor: Color
+
+    var body: some View {
+        BaseButton(configuration: configuration)
+            .background(
+                RoundedRectangle(cornerRadius: Style.defaultCornerRadius)
+                    .strokeBorder(
+                        borderColor,
+                        lineWidth: Style.defaultBorderWidth
+                    )
+            )
+    }
+}
+
 private enum Style {
     static let defaultCornerRadius = CGFloat(8.0)
     static let defaultBorderWidth = CGFloat(1.0)
@@ -396,39 +421,55 @@ private enum Style {
 struct PrimaryButton_Previews: PreviewProvider {
     static var previews: some View {
         VStack(spacing: 20) {
-            Button("Primary button") {}
-                .buttonStyle(PrimaryButtonStyle())
+            Group {
+                Button("Primary button") {}
+                    .buttonStyle(PrimaryButtonStyle())
 
-            Button("Primary button (disabled)") {}
-                .buttonStyle(PrimaryButtonStyle())
-                .disabled(true)
+                Button("Primary button (disabled)") {}
+                    .buttonStyle(PrimaryButtonStyle())
+                    .disabled(true)
+            }
 
-            Button("Secondary button") {}
-                .buttonStyle(SecondaryButtonStyle())
+            Group {
+                Button("Secondary button") {}
+                    .buttonStyle(SecondaryButtonStyle())
+                
+                Button("Secondary button (disabled)") {}
+                    .buttonStyle(SecondaryButtonStyle())
+                    .disabled(true)
+            }
 
-            Button("Secondary button (disabled)") {}
-                .buttonStyle(SecondaryButtonStyle())
-                .disabled(true)
+            Group {
+                Button("Selectable secondary button (selected)") {}
+                    .buttonStyle(SelectableSecondaryButtonStyle(isSelected: true))
 
-            Button("Selectable secondary button (selected)") {}
-                .buttonStyle(SelectableSecondaryButtonStyle(isSelected: true))
+                Button("Selectable secondary button") {}
+                    .buttonStyle(SelectableSecondaryButtonStyle(isSelected: false))
+            }
 
-            Button("Selectable secondary button") {}
-                .buttonStyle(SelectableSecondaryButtonStyle(isSelected: false))
+            Group {
+                Button("Link button") {}
+                    .buttonStyle(LinkButtonStyle())
 
-            Button("Link button") {}
-                .buttonStyle(LinkButtonStyle())
+                Button("Link button (Disabled)") {}
+                    .buttonStyle(LinkButtonStyle())
+                    .disabled(true)
+            }
 
-            Button("Link button (Disabled)") {}
-            .buttonStyle(LinkButtonStyle())
-            .disabled(true)
+            Group {
+                Button("Plus button") {}
+                    .buttonStyle(PlusButtonStyle())
 
-            Button("Plus button") {}
-                .buttonStyle(PlusButtonStyle())
+                Button("Plus button (disabled)") {}
+                    .buttonStyle(PlusButtonStyle())
+                    .disabled(true)
+            }
 
-            Button("Plus button (disabled)") {}
-            .buttonStyle(PlusButtonStyle())
-            .disabled(true)
+            Group {
+                Button("Rounded bordered button") {}
+                    .foregroundColor(.init(uiColor: .error))
+                    .buttonStyle(RoundedBorderedStyle(borderColor: .init(uiColor: .error)))
+            }
         }
         .preferredColorScheme(.light)
         .padding()
@@ -485,6 +526,12 @@ struct PrimaryButton_Previews: PreviewProvider {
                 Button("Plus button (disabled)") {}
                     .buttonStyle(PlusButtonStyle())
                     .disabled(true)
+            }
+
+            Group {
+                Button("Rounded bordered button") {}
+                    .foregroundColor(.init(uiColor: .error))
+                    .buttonStyle(RoundedBorderedStyle(borderColor: .init(uiColor: .error)))
             }
         }
         .preferredColorScheme(.dark)

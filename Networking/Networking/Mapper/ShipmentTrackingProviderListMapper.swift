e66c3f2385d617944488f0d5a4b886e9980c8111
Gio Lodi <gio@mokacoding.com>
2023-09-28 11:56:36 -0700

    Replace `ShipmentTrackingProviderListEnvelope` with `Envelope`

--- Networking/Networking/Mapper/ShipmentTrackingProviderListMapper.swift
+++ Networking/Networking/Mapper/ShipmentTrackingProviderListMapper.swift
@@ -3,34 +3,23 @@ import Foundation
 /// (Attempts) to convert a dictionary into an ShipmentTrackingProviderGroup entity.
 ///
 struct ShipmentTrackingProviderListMapper: Mapper {
+
     private let siteID: Int64
 
     public init(siteID: Int64) {
         self.siteID = siteID
     }
 
+    private typealias RawData = [String: [String: String]]
+
     func map(response: Data) throws -> [ShipmentTrackingProviderGroup] {
         let decoder = JSONDecoder()
-        let rawDictionary: ShipmentTrackingProviderListEnvelope.RawData
+        let rawDictionary: RawData
         if hasDataEnvelope(in: response) {
-            rawDictionary = try decoder.decode(ShipmentTrackingProviderListEnvelope.self, from: response).rawData
+            rawDictionary = try decoder.decode(Envelope<RawData>.self, from: response).data
         } else {
-            rawDictionary = try decoder.decode(ShipmentTrackingProviderListEnvelope.RawData.self, from: response)
+            rawDictionary = try decoder.decode(RawData.self, from: response)
         }
         return rawDictionary.map({ ShipmentTrackingProviderGroup(name: $0.key, siteID: siteID, dictionary: $0.value) })
-    }
-}
-
-
-/// ShipmentTrackingProviderListEnvelope Disposable Entity: The shipment tracking provider endpoint returns
-/// the providers within a `data` key.
-///
-private struct ShipmentTrackingProviderListEnvelope: Decodable {
-    typealias RawData = [String: [String: String]]
-
-    let rawData: RawData
-
-    private enum CodingKeys: String, CodingKey {
-        case rawData = "data"
     }
 }

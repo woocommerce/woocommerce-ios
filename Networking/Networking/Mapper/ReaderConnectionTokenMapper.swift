b7e71095b52f32c7ff5ac72075adf4b44d4ee5ae
Gio Lodi <gio@mokacoding.com>
2023-09-28 16:24:31 -0700

    Replace `RemoteReaderLocationEnvelope` with `extract(from:decoder:)`

--- Networking/Networking/Mapper/ReaderConnectionTokenMapper.swift
+++ Networking/Networking/Mapper/ReaderConnectionTokenMapper.swift
@@ -5,8 +5,6 @@ struct ReaderConnectionTokenMapper: Mapper {
     /// (Attempts) to convert a dictionary into a connection token.
     ///
     func map(response: Data) throws -> ReaderConnectionToken {
-        let decoder = JSONDecoder()
-
         return try extract(from: response, using: JSONDecoder())
     }
 }
--- Networking/Networking/Mapper/RemoteReaderLocationMapper.swift
+++ Networking/Networking/Mapper/RemoteReaderLocationMapper.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 /// Mapper: WCPay Reader Location
 ///
 struct RemoteReaderLocationMapper: Mapper {
@@ -9,23 +7,6 @@ struct RemoteReaderLocationMapper: Mapper {
     func map(response: Data) throws -> RemoteReaderLocation {
         let decoder = JSONDecoder()
 
-        if hasDataEnvelope(in: response) {
-            return try decoder.decode(RemoteReaderLocationEnvelope.self, from: response).location
-        } else {
-            return try decoder.decode(RemoteReaderLocation.self, from: response)
-        }
-    }
-}
-
-/// WCPayLocationEnvelope Disposable Entity
-///
-/// Endpoint returns the location in the `data` key. This entity
-/// allows us to parse it with JSONDecoder.
-///
-private struct RemoteReaderLocationEnvelope: Decodable {
-    let location: RemoteReaderLocation
-
-    private enum CodingKeys: String, CodingKey {
-        case location = "data"
+        return try extract(from: response, using: decoder)
     }
 }

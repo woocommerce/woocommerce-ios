874e4f424c369d02c744f187c074c97697d4188b
Gio Lodi <gio@mokacoding.com>
2023-09-28 16:22:01 -0700

    Replace `ShippingLabelAddressValidationResponseEnvelope` with generic

--- Networking/Networking/Mapper/ShippingLabelAddressValidationSuccessMapper.swift
+++ Networking/Networking/Mapper/ShippingLabelAddressValidationSuccessMapper.swift
@@ -1,6 +1,3 @@
-import Foundation
-
-
 /// Mapper: Shipping Label Address Validation Response
 ///
 struct ShippingLabelAddressValidationSuccessMapper: Mapper {
@@ -8,25 +5,14 @@ struct ShippingLabelAddressValidationSuccessMapper: Mapper {
     ///
     func map(response: Data) throws -> ShippingLabelAddressValidationSuccess {
         let decoder = JSONDecoder()
-        let data: ShippingLabelAddressValidationResponse = try {
-            if hasDataEnvelope(in: response) {
-                return try decoder.decode(ShippingLabelAddressValidationResponseEnvelope.self, from: response).data
-            } else {
-                return try decoder.decode(ShippingLabelAddressValidationResponse.self, from: response)
-            }
-        }()
+
+        let data: ShippingLabelAddressValidationResponse
+        if hasDataEnvelope(in: response) {
+            data = try decoder.decode(Envelope<ShippingLabelAddressValidationResponse>.self, from: response).data
+        } else {
+            data = try decoder.decode(ShippingLabelAddressValidationResponse.self, from: response)
+        }
+
         return try data.result.get()
-    }
-}
-
-/// ShippingLabelAddressValidationResponseEnvelope Disposable Entity:
-/// `Normalize Address` endpoint returns the shipping label address document in the `data` key.
-/// This entity allows us to do parse all the things with JSONDecoder.
-///
-private struct ShippingLabelAddressValidationResponseEnvelope: Decodable {
-    let data: ShippingLabelAddressValidationResponse
-
-    private enum CodingKeys: String, CodingKey {
-        case data = "data"
     }
 }

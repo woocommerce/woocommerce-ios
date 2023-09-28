59e3082069879e26ec715192cb2e27b18f659ce0
Gio Lodi <gio@mokacoding.com>
2023-09-28 16:29:50 -0700

    Replace `ProductIDEnvelope` with generic `Envelope`

--- Networking/Networking/Mapper/ProductIDMapper.swift
+++ Networking/Networking/Mapper/ProductIDMapper.swift
@@ -1,4 +1,4 @@
-import Foundation
+private typealias ProductIDs = [[String: Int64]]
 
 /// Mapper: Product IDs
 ///
@@ -9,32 +9,13 @@ struct ProductIDMapper: Mapper {
     func map(response: Data) throws -> [Int64] {
         let decoder = JSONDecoder()
 
+        let ids: ProductIDs
         if hasDataEnvelope(in: response) {
-            return try decoder.decode(ProductIDEnvelope.self, from: response).productIDs.compactMap { $0[Constants.idKey] }
+            ids = try decoder.decode(Envelope<ProductIDs>.self, from: response).data
         } else {
-            return try decoder.decode(ProductIDEnvelope.ProductIDs.self, from: response).compactMap { $0[Constants.idKey] }
+            ids = try decoder.decode(ProductIDs.self, from: response)
         }
-    }
-}
 
-// MARK: Constants
-//
-private extension ProductIDMapper {
-    enum Constants {
-        static let idKey = "id"
-    }
-}
-
-/// ProductIDEnvelope Disposable Entity:
-/// `Products` endpoint returns if a product exists. This entity
-/// allows us to parse the product IDs with JSONDecoder.
-///
-private struct ProductIDEnvelope: Decodable {
-    typealias ProductIDs = [[String: Int64]]
-
-    let productIDs: ProductIDs
-
-    private enum CodingKeys: String, CodingKey {
-        case productIDs = "data"
+        return ids.compactMap { $0["id"] }
     }
 }

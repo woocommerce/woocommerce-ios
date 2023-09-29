77e4f875c646797f8461cef6ca68973f76a9ac61
Huong Do <hi.huongdo@gmail.com>
2023-09-29 09:23:42 +0700

    Remove deprecated tests for AddProductNameWithAIViewModelT

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Add Product/AddProductWithAI/AddProductNameWithAIViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Add Product/AddProductWithAI/AddProductNameWithAIViewModelTests.swift
@@ -3,45 +3,5 @@ import Yosemite
 @testable import WooCommerce
 
 final class AddProductNameWithAIViewModelTests: XCTestCase {
-
-    func test_productNameContent_is_updated_correctly_with_initialName() {
-        // Given
-        let expectedName = "iPhone 15"
-        let viewModel = AddProductNameWithAIViewModel(siteID: 123, initialName: expectedName, onUsePackagePhoto: { _ in }, onContinueWithProductName: { _ in })
-
-        // Then
-        XCTAssertEqual(viewModel.productNameContent, expectedName)
-    }
-
-    func test_onUsePackagePhoto_is_triggered_when_tapping_package_photo() {
-        // Given
-        var triggeredName: String?
-        let expectedName = "iPhone 15"
-        let viewModel = AddProductNameWithAIViewModel(siteID: 123,
-                                                      initialName: expectedName,
-                                                      onUsePackagePhoto: { triggeredName = $0 },
-                                                      onContinueWithProductName: { _ in })
-
-        // When
-        viewModel.didTapUsePackagePhoto()
-
-        // Then
-        XCTAssertEqual(triggeredName, expectedName)
-    }
-
-    func test_onContinueWithProductName_is_triggered_when_tapping_continue() {
-        // Given
-        var triggeredName: String?
-        let expectedName = "iPhone 15"
-        let viewModel = AddProductNameWithAIViewModel(siteID: 123,
-                                                      initialName: expectedName,
-                                                      onUsePackagePhoto: { _ in },
-                                                      onContinueWithProductName: { triggeredName = $0 })
-
-        // When
-        viewModel.didTapContinue()
-
-        // Then
-        XCTAssertEqual(triggeredName, expectedName)
-    }
+    // TODO: add tests for analytics
 }

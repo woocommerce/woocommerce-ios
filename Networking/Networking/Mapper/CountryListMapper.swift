ca797ad4cf8ba7a39964204dae3d2110611c5a39
Gio Lodi <gio@mokacoding.com>
2023-09-28 17:03:14 -0700

    Rewrite `CountryListMapper` to use `extract(from:,decoder:)`

--- Networking/Networking/Mapper/CountryListMapper.swift
+++ Networking/Networking/Mapper/CountryListMapper.swift
@@ -1,17 +1,8 @@
-import Foundation
-
-
-/// Mapper: Country Collection
-///
 struct CountryListMapper: Mapper {
 
     /// (Attempts) to convert an instance of Data into an array of Country Entities.
     ///
     func map(response: Data) throws -> [Country] {
-        if hasDataEnvelope(in: response) {
-            return try JSONDecoder().decode(Envelope<[Country]>.self, from: response).data
-        } else {
-            return try JSONDecoder().decode([Country].self, from: response)
-        }
+        try extract(from: response, using: JSONDecoder())
     }
 }

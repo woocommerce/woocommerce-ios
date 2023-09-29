58225c63d7791605656468447d4419e3ce3f0c70
Huong Do <hi.huongdo@gmail.com>
2023-09-29 09:23:51 +0700

    Add tests for canBeDismissed

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Add Product/AddProductWithAI/AddProductWithAIContainerViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Add Product/AddProductWithAI/AddProductWithAIContainerViewModelTests.swift
@@ -2,6 +2,45 @@ import XCTest
 @testable import WooCommerce
 
 final class AddProductWithAIContainerViewModelTests: XCTestCase {
+
+    // MARK: - `canBeDismissed`
+
+    func test_canBeDismissed_returns_true_if_current_step_is_productName_and_the_name_field_is_empty() {
+        // Given
+        let viewModel = AddProductWithAIContainerViewModel(siteID: 123, source: .productsTab, onCancel: {}, onCompletion: { _ in })
+
+        // Then
+        XCTAssertTrue(viewModel.canBeDismissed)
+    }
+
+    func test_canBeDismissed_returns_False_if_current_step_is_productName_and_the_name_field_is_not_empty() {
+        // Given
+        let viewModel = AddProductWithAIContainerViewModel(siteID: 123, source: .productsTab, onCancel: {}, onCompletion: { _ in })
+
+        // When
+        viewModel.addProductNameViewModel.productNameContent = "iPhone 15"
+
+        // Then
+        XCTAssertFalse(viewModel.canBeDismissed)
+    }
+
+    func test_canBeDismissed_returns_False_if_current_step_is_not_product_name() {
+        // Given
+        let viewModel = AddProductWithAIContainerViewModel(siteID: 123, source: .productsTab, onCancel: {}, onCompletion: { _ in })
+
+        // When
+        viewModel.onContinueWithProductName(name: "iPhone 15")
+
+        // Then
+        XCTAssertFalse(viewModel.canBeDismissed)
+
+        // When
+        viewModel.onProductFeaturesAdded(features: "No lightning jack")
+
+        // Then
+        XCTAssertFalse(viewModel.canBeDismissed)
+    }
+
     // MARK: `didGenerateDataFromPackage`
 
     func test_didGenerateDataFromPackage_sets_values_from_package_flow() {

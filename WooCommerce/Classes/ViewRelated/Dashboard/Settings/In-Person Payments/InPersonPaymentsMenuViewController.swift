17184bd5184824e38823342353eac0ba65c2b016
Josh Heald <joshheald@users.noreply.github.com>
2023-10-06 15:50:53 +0100

    10871 Add button for About Tap to Pay screen

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsMenuViewController.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsMenuViewController.swift
@@ -239,7 +239,7 @@ private extension InPersonPaymentsMenuViewController {
         guard featureFlagService.isFeatureFlagEnabled(.tapToPayOnIPhone) else {
             return nil
         }
-        var rows: [Row] = [.setUpTapToPayOnIPhone]
+        var rows: [Row] = [.setUpTapToPayOnIPhone, .aboutTapToPayOnIPhone]
 
         if shouldShowFeedbackRow {
             rows.append(.tapToPayOnIPhoneFeedback)
@@ -317,6 +317,8 @@ private extension InPersonPaymentsMenuViewController {
             configureToggleEnableCashOnDelivery(cell: cell)
         case let cell as BadgedLeftImageTableViewCell where row == .setUpTapToPayOnIPhone:
             configureSetUpTapToPayOnIPhone(cell: cell)
+        case let cell as LeftImageTableViewCell where row == .aboutTapToPayOnIPhone:
+            configureAboutTapToPayOnIPhone(cell: cell)
         case let cell as LeftImageTableViewCell where row == .tapToPayOnIPhoneFeedback:
             configureTapToPayOnIPhoneFeedback(cell: cell)
         case let cell as LeftImageTableViewCell where row == .depositOverview:
@@ -385,6 +387,13 @@ private extension InPersonPaymentsMenuViewController {
         cell.configure(image: .tapToPayOnIPhoneIcon,
                        text: Localization.tapToPayOnIPhone,
                        showBadge: viewModel.shouldBadgeTapToPayOnIPhone)
+    }
+
+    func configureAboutTapToPayOnIPhone(cell: LeftImageTableViewCell) {
+        prepareForReuse(cell)
+        cell.accessibilityIdentifier = "about-tap-to-pay"
+        cell.configure(image: .infoOutlineImage,
+                       text: Localization.aboutTapToPayOnIPhone)
     }
 
     func configureTapToPayOnIPhoneFeedback(cell: LeftImageTableViewCell) {
@@ -524,6 +533,10 @@ extension InPersonPaymentsMenuViewController {
         navigationController?.present(controller, animated: true)
     }
 
+    func aboutTapToPayOnIPhoneWasPressed() {
+        // TODO: open the new screen
+    }
+
     func tapToPayOnIPhoneFeedbackWasPressed() {
         let surveyNavigation = SurveyCoordinatingController(survey: .tapToPayFirstPayment)
         navigationController?.present(surveyNavigation, animated: true)
@@ -626,6 +639,8 @@ extension InPersonPaymentsMenuViewController: UITableViewDelegate {
             break
         case .setUpTapToPayOnIPhone:
             setUpTapToPayOnIPhoneWasPressed()
+        case .aboutTapToPayOnIPhone:
+            aboutTapToPayOnIPhoneWasPressed()
         case .tapToPayOnIPhoneFeedback:
             tapToPayOnIPhoneFeedbackWasPressed()
         case .depositOverview:
@@ -713,6 +728,11 @@ private extension InPersonPaymentsMenuViewController {
             comment: "Navigates to the Tap to Pay on iPhone set up flow. The full name is expected by Apple. " +
             "The destination screen also allows for a test payment, after set up.")
 
+        static let aboutTapToPayOnIPhone = NSLocalizedString(
+            "About Tap to Pay on iPhone",
+            comment: "Navigates to the About Tap to Pay on iPhone screen, which explains the capabilities and limits " +
+            "of Tap to Pay on iPhone, relevant to the store territory.")
+
         static let tapToPayOnIPhoneFeedback = NSLocalizedString(
             "Share Tap to Pay on iPhone Feedback",
             comment: "Navigates to a screen to share feedback about Tap to Pay on iPhone.")
@@ -755,6 +775,7 @@ private enum Row: CaseIterable {
     case collectPayment
     case toggleEnableCashOnDelivery
     case setUpTapToPayOnIPhone
+    case aboutTapToPayOnIPhone
     case tapToPayOnIPhoneFeedback
     case depositOverview
 

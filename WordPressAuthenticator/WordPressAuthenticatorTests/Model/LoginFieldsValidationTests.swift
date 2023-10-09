c8b3c61f519d4f8ad23d90a0bfb72c1caa048641
Ernesto Carrión <ernesto.carrion@automattic.com>
2023-10-09 16:02:53 -0500

    Re-enable failing test

--- WordPressAuthenticator/WordPressAuthenticatorTests/Model/LoginFieldsValidationTests.swift
+++ WordPressAuthenticator/WordPressAuthenticatorTests/Model/LoginFieldsValidationTests.swift
@@ -39,8 +39,7 @@ class LoginFieldsValidationTests: XCTestCase {
         loginFields.siteAddress = "https://hostname"
         XCTAssert(loginFields.validateSiteForSignin(), "Since we want to validate simple mistakes, to use a hostname you'll need an http:// or https:// prefix.")
 
-        // TODO: This is failing I don't know why
-        //loginFields.siteAddress = "host name.com"
-        //XCTAssertFalse(loginFields.validateSiteForSignin(), "Hostname with spaces should not validate.")
+        loginFields.siteAddress = "host name.com"
+        XCTAssertFalse(loginFields.validateSiteForSignin(), "Hostname with spaces should not validate.")
     }
 }

04c1b6c5278b3231706a4b90ba20f361d4c3a9fb
ThomazFB <thomazfcortez@gmail.com>
2023-10-09 20:35:16 -0300

    Add onDismiss handling to NavigateToEnterWPCOMPassword

--- WordPressAuthenticator/WordPressAuthenticator/Navigation/NavigateToEnterWPCOMPassword.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Navigation/NavigateToEnterWPCOMPassword.swift
@@ -4,25 +4,32 @@ import Foundation
 ///
 public struct NavigateToEnterWPCOMPassword: NavigationCommand {
     private let loginFields: LoginFields
+    private let onDismiss: () -> Void
 
-    public init(loginFields: LoginFields) {
+    public init(loginFields: LoginFields, onDismiss: (() -> Void)?) {
         self.loginFields = loginFields
+        self.onDismiss = onDismiss ?? {}
     }
     public func execute(from: UIViewController?) {
         let navigationController = (from as? UINavigationController) ?? from?.navigationController
         presentPasswordView(navigationController: navigationController,
-                            loginFields: loginFields)
+                            loginFields: loginFields,
+                            onDismiss: onDismiss)
     }
 }
 
 private extension NavigateToEnterWPCOMPassword {
-    func presentPasswordView(navigationController: UINavigationController?, loginFields: LoginFields) {
+    func presentPasswordView(navigationController: UINavigationController?, loginFields: LoginFields, onDismiss: @escaping () -> Void) {
         guard let controller = PasswordViewController.instantiate(from: .password) else {
             WPAuthenticatorLogError("Failed to navigate to PasswordViewController from GetStartedViewController")
             return
         }
 
         controller.loginFields = loginFields
+        controller.dismissBlock = { _ in
+            onDismiss()
+        }
+
         navigationController?.pushViewController(controller, animated: true)
     }
 }

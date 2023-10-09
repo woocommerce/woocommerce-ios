b47c5368f083c6de9a9db69f7c584786bae01fbf
ThomazFB <thomazfcortez@gmail.com>
2023-10-09 20:34:58 -0300

    Add onDismiss handling to NavigateToEnterSiteCredentials

--- WordPressAuthenticator/WordPressAuthenticator/Navigation/NavigateToEnterSiteCredentials.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Navigation/NavigateToEnterSiteCredentials.swift
@@ -4,25 +4,32 @@ import Foundation
 ///
 public struct NavigateToEnterSiteCredentials: NavigationCommand {
     private let loginFields: LoginFields
+    private let onDismiss: () -> Void
 
-    public init(loginFields: LoginFields) {
+    public init(loginFields: LoginFields, onDismiss: (() -> Void)?) {
         self.loginFields = loginFields
+        self.onDismiss = onDismiss ?? {}
     }
     public func execute(from: UIViewController?) {
         let navigationController = (from as? UINavigationController) ?? from?.navigationController
         presentSiteCredentialsView(navigationController: navigationController,
-                                   loginFields: loginFields)
+                                   loginFields: loginFields,
+                                   onDismiss: onDismiss)
     }
 }
 
 private extension NavigateToEnterSiteCredentials {
-    func presentSiteCredentialsView(navigationController: UINavigationController?, loginFields: LoginFields) {
+    func presentSiteCredentialsView(navigationController: UINavigationController?, loginFields: LoginFields, onDismiss: @escaping () -> Void) {
         guard let controller = SiteCredentialsViewController.instantiate(from: .siteAddress) else {
             WPAuthenticatorLogError("Failed to navigate to SiteCredentialsViewController")
             return
         }
 
         controller.loginFields = loginFields
+        controller.dismissBlock = { _ in
+            onDismiss()
+        }
+
         navigationController?.pushViewController(controller, animated: true)
     }
 }

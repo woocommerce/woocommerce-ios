56b13f392e9d975c368bd0deb8e77ff89e04165c
Huong Do <hi.huongdo@gmail.com>
2023-10-11 15:46:59 +0900

    Add BlazeStore

---
+++ Yosemite/Yosemite/Model/Storage/BlazeCampaign+ReadOnlyConvertible.swift
@@ -0,0 +1,34 @@
+import Foundation
+import Storage
+
+// MARK: - Storage.BlazeCampaign: ReadOnlyConvertible
+//
+extension Storage.BlazeCampaign: ReadOnlyConvertible {
+    /// Updates the `Storage.BlazeCampaign` from the ReadOnly representation (`Networking.BlazeCampaign`)
+    ///
+    public func update(with campaign: Yosemite.BlazeCampaign) {
+        siteID = campaign.siteID
+        campaignID = campaign.campaignID
+        name = campaign.name
+        rawStatus = campaign.uiStatus
+        contentClickURL = campaign.contentClickURL
+        contentImageURL = campaign.contentImageURL
+        totalBudget = campaign.totalBudget
+        totalClicks = campaign.totalClicks
+        totalImpressions = campaign.totalImpressions
+    }
+
+    /// Returns a ReadOnly (`Networking.BlazeCampaign`) version of the `Storage.BlazeCampaign`
+    ///
+    public func toReadOnly() -> BlazeCampaign {
+        BlazeCampaign(siteID: siteID,
+                      campaignID: campaignID,
+                      name: name,
+                      uiStatus: rawStatus,
+                      contentImageURL: contentImageURL,
+                      contentClickURL: contentClickURL,
+                      totalImpressions: totalImpressions,
+                      totalClicks: totalClicks,
+                      totalBudget: totalBudget)
+    }
+}

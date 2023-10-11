3b575b97f1d42c69de593088890559d9d8ce0354
Huong Do <hi.huongdo@gmail.com>
2023-10-11 16:40:44 +0900

    Add MockBlazeRemote

---
+++ Yosemite/YosemiteTests/Mocks/Networking/Remote/MockBlazeRemote.swift
@@ -0,0 +1,27 @@
+import XCTest
+import Networking
+
+/// Mock for `BlazeRemote`.
+///
+final class MockBlazeRemote {
+    private var loadingCampaignResult: Result<[Networking.BlazeCampaign], Error>?
+
+    func whenLoadingCampaign(thenReturn result: Result<[Networking.BlazeCampaign], Error>) {
+        loadingCampaignResult = result
+    }
+}
+
+extension MockBlazeRemote: BlazeRemoteProtocol {
+    func loadCampaigns(for siteID: Int64, pageNumber: Int) async throws -> [Networking.BlazeCampaign] {
+        guard let result = loadingCampaignResult else {
+            XCTFail("Could not find result for loading Blaze campaigns.")
+            throw NetworkError.notFound
+        }
+        switch result {
+        case .success(let campaigns):
+            return campaigns
+        case .failure(let error):
+            throw error
+        }
+    }
+}

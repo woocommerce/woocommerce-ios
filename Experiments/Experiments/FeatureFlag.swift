a1f727852461943d342db89f160e4393ebd19d6f
Jaclyn Chen <watertranquil@gmail.com>
2023-10-21 09:08:11 +0800

    Add a feature flag for bundle products in order form.

--- Experiments/Experiments/FeatureFlag.swift
+++ Experiments/Experiments/FeatureFlag.swift
@@ -221,4 +221,8 @@ public enum FeatureFlag: Int {
     /// Enables a new section on the My Store screen and a new entry point to the campaign list.
     ///
     case optimizedBlazeExperience
+
+    /// Enables bundle products support in order creation/editing.
+    ///
+    case bundleProductsInOrderForm
 }

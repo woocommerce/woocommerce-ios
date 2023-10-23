75aed3a1746efc209c2c18c93310a91c0009e9ae
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-10-23 16:42:59 +0530

    Enable optimized blaze list screen by default.

--- WooCommerce/Classes/ViewRelated/Hub Menu/HubMenuViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Hub Menu/HubMenuViewModel.swift
@@ -191,16 +191,10 @@ final class HubMenuViewModel: ObservableObject {
 
         ServiceLocator.analytics.track(event: .Blaze.blazeEntryPointTapped(source: .menu))
 
-        if featureFlagService.isFeatureFlagEnabled(.optimizedBlazeExperience) {
-            // shows campaign list for the new Blaze experience.
-            let controller = BlazeCampaignListHostingController(viewModel: .init(siteID: site.siteID))
-            navigationController?.show(controller, sender: self)
-            ServiceLocator.analytics.track(event: .Blaze.blazeCampaignListEntryPointSelected(source: .menu))
-        } else {
-            let viewModel = BlazeWebViewModel(source: .menu, siteURL: site.url, productID: nil)
-            let webViewController = AuthenticatedWebViewController(viewModel: viewModel)
-            navigationController?.show(webViewController, sender: self)
-        }
+        // shows campaign list for the new Blaze experience.
+        let controller = BlazeCampaignListHostingController(viewModel: .init(siteID: site.siteID))
+        navigationController?.show(controller, sender: self)
+        ServiceLocator.analytics.track(event: .Blaze.blazeCampaignListEntryPointSelected(source: .menu))
     }
 
     private func observeSiteForUIUpdates() {

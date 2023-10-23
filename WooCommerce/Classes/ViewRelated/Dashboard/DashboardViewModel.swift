ebd4e44749b392178e72940ea98e6c39d9a79303
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-10-23 16:40:01 +0530

    Remove legacy blaze banner.

--- WooCommerce/Classes/ViewRelated/Dashboard/DashboardViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/DashboardViewModel.swift
@@ -27,8 +27,6 @@ final class DashboardViewModel {
 
     @Published private(set) var showOnboarding: Bool = false
 
-    @Published private(set) var showBlazeBanner: Bool = false
-
     @Published private(set) var showBlazeCampaignView: Bool = false
 
     /// Trigger to start the Add Product flow
@@ -326,27 +324,9 @@ final class DashboardViewModel {
     }
 }
 
-// MARK: - Blaze banner
-extension DashboardViewModel {
-    /// Checks for Blaze eligibility and user defaults to show the banner if necessary.
-    ///
+private extension DashboardViewModel {
     @MainActor
-    func updateBlazeBannerVisibility() async {
-        showBlazeBanner = await isBlazeBannerVisible()
-    }
-
-    private func isBlazeBannerVisible() async -> Bool {
-        async let isSiteEligible = blazeEligibilityChecker.isSiteEligible()
-        async let storeHasPublishedProducts = (try? checkIfStoreHasProducts(siteID: siteID, status: .published)) ?? false
-        async let storeHasAnyOrders = (try? checkIfStoreHasOrders(siteID: siteID)) ?? false
-        guard await(isSiteEligible, storeHasPublishedProducts, storeHasAnyOrders) == (true, true, false) else {
-            return false
-        }
-        return !userDefaults.hasDismissedBlazeBanner(for: siteID)
-    }
-
-    @MainActor
-    private func checkIfStoreHasProducts(siteID: Int64, status: ProductStatus? = nil) async throws -> Bool {
+    func checkIfStoreHasProducts(siteID: Int64, status: ProductStatus? = nil) async throws -> Bool {
         try await withCheckedThrowingContinuation { continuation in
             stores.dispatch(ProductAction.checkIfStoreHasProducts(siteID: siteID, status: status, onCompletion: { result in
                 switch result {
@@ -358,28 +338,6 @@ extension DashboardViewModel {
                 }
             }))
         }
-    }
-
-    @MainActor
-    private func checkIfStoreHasOrders(siteID: Int64) async throws -> Bool {
-        try await withCheckedThrowingContinuation { continuation in
-            stores.dispatch(OrderAction.checkIfStoreHasOrders(siteID: siteID, onCompletion: { result in
-                switch result {
-                case .success(let hasOrders):
-                    continuation.resume(returning: hasOrders)
-                case .failure(let error):
-                    DDLogError("⛔️ Dashboard — Error fetching order to show the Blaze banner: \(error)")
-                    continuation.resume(throwing: error)
-                }
-            }))
-        }
-    }
-
-    /// Hides the banner and updates the user defaults to not show the banner again.
-    ///
-    func hideBlazeBanner() {
-        showBlazeBanner = false
-        userDefaults.setBlazeBannerDismissed(for: siteID)
     }
 }
 

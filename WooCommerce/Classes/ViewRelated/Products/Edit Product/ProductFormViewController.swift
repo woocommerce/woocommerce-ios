0d3ec0986ae84b17f56479147d8852db2252abe1
Hafiz Rahman <hfzrhmn@gmail.com>
2023-10-26 15:47:21 +0700

    Remove unneeded code related to blaze in more menu.
    Blaze does not show up in more menu anymore.

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewController.swift
@@ -321,14 +321,6 @@ final class ProductFormViewController<ViewModel: ProductFormViewModelProtocol>: 
             }
         }
 
-        if viewModel.canPromoteWithBlaze() {
-            actionSheet.addDefaultActionWithTitle(ActionSheetStrings.promoteWithBlaze) { [weak self] _ in
-                self?.displayBlaze()
-                ServiceLocator.analytics.track(event: .Blaze.blazeEntryPointTapped(source: .productMoreMenu))
-            }
-            ServiceLocator.analytics.track(event: .Blaze.blazeEntryPointDisplayed(source: .productMoreMenu))
-        }
-
         if viewModel.canEditProductSettings() {
             actionSheet.addDefaultActionWithTitle(ActionSheetStrings.productSettings) { [weak self] _ in
                 ServiceLocator.analytics.track(.productDetailViewSettingsButtonTapped)

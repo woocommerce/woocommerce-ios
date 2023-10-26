10997ef8b4e06dad8818a2d8b45c7575a9e3cdc8
Hafiz Rahman <hfzrhmn@gmail.com>
2023-10-26 15:33:21 +0700

    Add tracking for `blazeEntryPointDisplayed`

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewController.swift
@@ -738,6 +738,9 @@ private extension ProductFormViewController {
     func observeUpdateBlazeEligibility() {
         blazeEligibilitySubscription = viewModel.blazeEligibilityUpdate.sink { [weak self] in
             self?.updateFormTableContent()
+            if self?.viewModel.canPromoteWithBlaze() == true {
+                ServiceLocator.analytics.track(event: .Blaze.blazeEntryPointDisplayed(source: .productDetailPromoteButton))
+            }
         }
 
     }

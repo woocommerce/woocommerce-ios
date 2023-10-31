3b79ca2e064dcc6c1800f13f2753fac8b8274444
Josh Heald <joshheald@users.noreply.github.com>
2023-10-31 13:49:49 +0000

    11052 Fix auto-reconnect TTP on fresh launch
    Previously, the auto-reconnection to the Tap to Pay system was not working on first launch; no connection was attempted. This reconnection is intended to save merchants time when taking a payment with TTP, by avoiding the wait for connection.
    
    It was failing because in `reconnectIfNeeded` it checks for `case .completed = onboardingCache.value` in the guard. On first launch, this was always `nil`, because it hadn’t finished refreshing. Both the refresh and the reconnection are triggered by app launch and app foreground.
    
    In this commit, we wait until the onboarding cache has finished refreshing, before we auto-reconnect to the Tap to Pay system.

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/CardPresentPaymentsOnboardingIPPUsersRefresher.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/CardPresentPaymentsOnboardingIPPUsersRefresher.swift
@@ -1,12 +1,14 @@
 import CoreData
 import Yosemite
 import Storage
+import Combine
 
 /// Refreshes the CPP onboarding state if there are IPP transactions stored
 ///
 class CardPresentPaymentsOnboardingIPPUsersRefresher {
     private let stores: StoresManager
     private let cardPresentPaymentsOnboardingUseCase: CardPresentPaymentsOnboardingUseCaseProtocol
+    private var cancellables: Set<AnyCancellable> = []
 
     init(stores: StoresManager = ServiceLocator.stores,
          cardPresentPaymentsOnboardingUseCase: CardPresentPaymentsOnboardingUseCaseProtocol = CardPresentPaymentsOnboardingUseCase()) {
@@ -14,14 +16,26 @@ class CardPresentPaymentsOnboardingIPPUsersRefresher {
         self.cardPresentPaymentsOnboardingUseCase = cardPresentPaymentsOnboardingUseCase
     }
 
-    func refreshIPPUsersOnboardingState() {
+    func refreshIPPUsersOnboardingState(completion: @escaping (() -> Void)) {
         guard let siteID = stores.sessionManager.defaultStoreID else {
             return
         }
 
         let action = AppSettingsAction.loadSiteHasAtLeastOneIPPTransactionFinished(siteID: siteID) { [weak self] result in
             if result {
-                self?.cardPresentPaymentsOnboardingUseCase.refresh()
+                guard let self else { return }
+
+                self.cardPresentPaymentsOnboardingUseCase.statePublisher.sink { [weak self] state in
+                    guard state != .loading else {
+                        return
+                    }
+                    // Stop observing further state updates, to avoid memory cycles; this class is long-lived!
+                    _ = self?.cancellables.map { $0.cancel() }
+                    completion()
+                }
+                .store(in: &cancellables)
+
+                self.cardPresentPaymentsOnboardingUseCase.refresh()
             }
         }
 

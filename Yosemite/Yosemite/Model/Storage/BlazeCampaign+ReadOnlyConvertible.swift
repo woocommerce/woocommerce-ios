e9edbecadfb5106e6cb2418bb5cdcea00ea1f3ec
Hafiz Rahman <hfzrhmn@gmail.com>
2023-11-02 12:44:35 +0700

    Update blazeCampaign convertible (temporarily, to not break unit tests).

--- Yosemite/Yosemite/Model/Storage/BlazeCampaign+ReadOnlyConvertible.swift
+++ Yosemite/Yosemite/Model/Storage/BlazeCampaign+ReadOnlyConvertible.swift
@@ -23,7 +23,7 @@ extension Storage.BlazeCampaign: ReadOnlyConvertible {
     public func toReadOnly() -> BlazeCampaign {
         BlazeCampaign(siteID: siteID,
                       campaignID: campaignID,
-                      productID: 0, // TODO-11059: update storage
+                      productID: nil, // TODO-11059: update storage
                       name: name,
                       uiStatus: rawStatus,
                       contentImageURL: contentImageURL,

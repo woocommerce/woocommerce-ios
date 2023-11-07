cb5a319a94f5f56e0ee5460503ebce227f2f09d0
Josh Heald <joshheald@users.noreply.github.com>
2023-11-07 15:37:05 +0000

    Prevent new menu showing on production builds

--- WooCommerce/Classes/ViewRelated/Hub Menu/HubMenuViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Hub Menu/HubMenuViewModel.swift
@@ -95,7 +95,7 @@ final class HubMenuViewModel: ObservableObject {
 
     func viewDidAppear() {
         NotificationCenter.default.post(name: .hubMenuViewDidAppear, object: nil)
-        swiftUIPaymentsMenuEnabled = generalAppSettings.betaFeatureEnabled(.swiftUIPaymentsMenu)
+        swiftUIPaymentsMenuEnabled = featureFlagService.isFeatureFlagEnabled(.swiftUIPaymentsMenu) && generalAppSettings.betaFeatureEnabled(.swiftUIPaymentsMenu)
     }
 
     /// Resets the menu elements displayed on the menu.

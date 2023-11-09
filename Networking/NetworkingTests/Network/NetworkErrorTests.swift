2c700bc53c6c0745761871e221217ccd772e6d23
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-11-09 09:50:38 +0530

    Unit test that response data is stored as string when unacceptableStatusCode.

--- Networking/NetworkingTests/Network/NetworkErrorTests.swift
+++ Networking/NetworkingTests/Network/NetworkErrorTests.swift
@@ -4,19 +4,22 @@ import XCTest
 
 /// NetworkError Tests
 ///
-class NetworkErrorTests: XCTestCase {
+final class NetworkErrorTests: XCTestCase {
 
     /// Verifies that statusCode(s) within the "Successful Range" [200, 299) yield nil errors.
     ///
     func testSuccessStatusCodesYieldNilNetworkErrors() {
-        XCTAssertNil(NetworkError(from: 200))
-        XCTAssertNil(NetworkError(from: 299))
+        XCTAssertNil(NetworkError(responseData: nil,
+                                  statusCode: 200))
+        XCTAssertNil(NetworkError(responseData: nil,
+                                  statusCode: 299))
     }
 
     /// Verifies that (not defined) Network Errors are wrapped within a `.unacceptableStatusCode` case
     ///
     func testUnknownErrorsAreCaughtByUnacceptableEnumCase() {
-        guard case let .unacceptableStatusCode(statusCode)? = NetworkError(from: 300) else {
+        guard case let .unacceptableStatusCode(statusCode, _)? = NetworkError(responseData: nil,
+                                                                           statusCode: 300) else {
             XCTFail()
             return
         }
@@ -27,9 +30,29 @@ class NetworkErrorTests: XCTestCase {
     /// Verifies that NotFound is properly parsed.
     ///
     func testNotFoundErrorIsProperlyParsed() {
-        guard case .notFound? = NetworkError(from: 404) else {
+        guard case .notFound? = NetworkError(responseData: nil,
+                                             statusCode: 404) else {
             XCTFail()
             return
         }
+    }
+
+    /// Verifies that provided response data if stored as string
+    ///
+    func test_response_data_is_stored_as_string() throws {
+        // Given
+        let error = ["error": "Failed to load"]
+        let jsonData = try JSONSerialization.data(withJSONObject: error)
+
+        // When
+        guard case let .unacceptableStatusCode(statusCode, response)? = NetworkError(responseData: jsonData,
+                                                                                     statusCode: 300) else {
+            XCTFail()
+            return
+        }
+
+        // Then
+        XCTAssertEqual(statusCode, 300)
+        XCTAssertEqual(response, "{\"error\":\"Failed to load\"}")
     }
 }

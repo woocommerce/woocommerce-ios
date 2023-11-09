d9a35fe582c8608309270460f04150b1878eb97e
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-11-09 15:28:24 +0530

    Turn off feature flag until we can navigate to product detail screen after selecting product type.

--- Experiments/Experiments/DefaultFeatureFlagService.swift
+++ Experiments/Experiments/DefaultFeatureFlagService.swift
@@ -85,7 +85,7 @@ public struct DefaultFeatureFlagService: FeatureFlagService {
         case .customLoginUIForAccountCreation:
             return buildConfig == .localDeveloper || buildConfig == .alpha
         case .subscriptionProducts:
-            return buildConfig == .localDeveloper || buildConfig == .alpha
+            return false
         default:
             return true
         }

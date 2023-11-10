4b00a05072923ee48aa53d5f0b8769e64c81e51b
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-11-10 18:00:26 +0530

    Add user friendly error descriptions for NetworkError.

--- Networking/Networking/Network/NetworkError.swift
+++ Networking/Networking/Network/NetworkError.swift
@@ -54,3 +54,38 @@ extension NetworkError {
         static let timeout  = 408
     }
 }
+
+extension NetworkError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .notFound:
+            return NSLocalizedString(
+                "NetworkError.notFound",
+                value: "Sorry, we couldn't find what you were looking for. Please try again.",
+                comment: "Error message when we receive not found error")
+        case .timeout:
+            return NSLocalizedString(
+                "NetworkError.timeout",
+                value: "Sorry, the request took too long to process. Please try again later. (Error code: 408)",
+                comment: "Error message when a request times out.")
+        case let .unacceptableStatusCode(statusCode, responseData):
+            let response: String? = responseData.map { String(data: $0, encoding: .utf8) } ?? nil
+            let format = NSLocalizedString(
+                "NetworkError.unacceptableStatusCode",
+                value: "Sorry, there was an issue with the server. Please try again later. (Error code: %1$d). Response: %2$@",
+                comment: "Error message when the a network call fails with unacceptable status code." +
+                "%1$d is the status code like 500. %2$@ is the response string.")
+            return String.localizedStringWithFormat(format, statusCode, response ?? "")
+        case .invalidURL:
+            return NSLocalizedString(
+                "NetworkError.invalidURL",
+                value: "Sorry, the URL is not valid. Please try again.",
+                comment: "Error message when the URL is invalid.")
+        case .invalidCookieNonce:
+            return NSLocalizedString(
+                "NetworkError.invalidCookieNonce",
+                value: "Sorry, your session has expired. Please log in again.",
+                comment: "Error message when session cookie has expired.")
+        }
+    }
+}

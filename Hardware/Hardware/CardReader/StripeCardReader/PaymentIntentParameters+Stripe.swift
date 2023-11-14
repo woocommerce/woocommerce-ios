098f82ece16e3b0e1be94b1b4c6320ec9a8c01f3
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-11-14 16:54:55 +0900

    Extract PaymentIntent parameter builder to own method

--- Hardware/Hardware/CardReader/StripeCardReader/PaymentIntentParameters+Stripe.swift
+++ Hardware/Hardware/CardReader/StripeCardReader/PaymentIntentParameters+Stripe.swift
@@ -5,50 +5,18 @@ extension Hardware.PaymentIntentParameters {
     /// Initializes a StripeTerminal.PaymentIntentParameters from a
     /// Hardware.PaymentIntentParameters
     func toStripe(with cardReaderMetadata: CardReaderMetadata? = nil) -> StripeTerminal.PaymentIntentParameters? {
-        // Shortcircuit if we do not have a valid currency code
-        guard !currency.isEmpty else {
-            return nil
-        }
-
-        // Shortcircuit if we do not have a valid payment method
-        guard !paymentMethodTypes.isEmpty else {
+        // Shortcircuit if we do not have a valid currency code, or a valid payment method
+        guard !currency.isEmpty, !paymentMethodTypes.isEmpty else {
             return nil
         }
 
         let amountForStripe = prepareAmountForStripe(amount)
-
-        let paymentIntentParamBuilder: PaymentIntentParametersBuilder = StripeTerminal.PaymentIntentParametersBuilder(amount: amountForStripe,
-                                                                                                                      currency: currency)
+        let paymentIntentParametersBuilder = createPaymentIntentParametersBuilder(amount: amountForStripe, currency: currency)
 
         do {
-            paymentIntentParamBuilder.setPaymentMethodTypes(paymentMethodTypes)
-            paymentIntentParamBuilder.setStripeDescription(receiptDescription)
-
-            /// Stripe allows the credit card statement descriptor to be nil, but not an empty string
-            /// https://stripe.dev/stripe-terminal-ios/docs/Classes/SCPPaymentIntentParameters.html#/c:objc(cs)SCPPaymentIntentParameters(py)statementDescriptor
-            paymentIntentParamBuilder.setStatementDescriptor(nil)
-
-            let descriptor = statementDescription ?? ""
-            if !descriptor.isEmpty {
-                paymentIntentParamBuilder.setStatementDescriptor(descriptor)
-            }
-
-            if let applicationFee = applicationFee {
-                /// Stripe requires that "The amount must be provided as a boxed UInt in the currency's smallest unit."
-                /// Smallest-unit and UInt conversion is done in the same way as for the total amount, but that does not need to be boxed.
-                let applicationFeeForStripe = NSNumber(value: prepareAmountForStripe(applicationFee))
-                paymentIntentParamBuilder.setApplicationFeeAmount(applicationFeeForStripe)
-            }
-
-            paymentIntentParamBuilder.setReceiptEmail(receiptEmail)
-
-            let updatedMetadata = prepareMetadataForStripe(with: cardReaderMetadata)
-            paymentIntentParamBuilder.setMetadata(updatedMetadata)
-
-            // Return payment intent built config:
-            return try paymentIntentParamBuilder.build()
+            return try build(paymentIntentParametersBuilder, with: cardReaderMetadata)
         } catch {
-            // TODO: Better error handling
+            DDLogError("Failed to build PaymentIntentParameters. Error:\(error)")
             return nil
         }
     }
@@ -58,6 +26,40 @@ private extension Hardware.PaymentIntentParameters {
     func prepareAmountForStripe(_ amount: Decimal) -> UInt {
         let amountInSmallestUnit = amount * stripeSmallestCurrencyUnitMultiplier
         return NSDecimalNumber(decimal: amountInSmallestUnit).uintValue
+    }
+
+    func createPaymentIntentParametersBuilder(amount: UInt, currency: String) -> PaymentIntentParametersBuilder {
+        StripeTerminal.PaymentIntentParametersBuilder(amount: amount, currency: currency)
+    }
+
+    func build(_ builder: PaymentIntentParametersBuilder,
+               with cardReaderMetadata: CardReaderMetadata? = nil) throws -> StripeTerminal.PaymentIntentParameters? {
+            builder.setPaymentMethodTypes(paymentMethodTypes)
+            builder.setStripeDescription(receiptDescription)
+
+            /// Stripe allows the credit card statement descriptor to be nil, but not an empty string
+            /// https://stripe.dev/stripe-terminal-ios/docs/Classes/SCPPaymentIntentParameters.html#/c:objc(cs)SCPPaymentIntentParameters(py)statementDescriptor
+            builder.setStatementDescriptor(nil)
+
+            let descriptor = statementDescription ?? ""
+            if !descriptor.isEmpty {
+                builder.setStatementDescriptor(descriptor)
+            }
+
+            if let applicationFee = applicationFee {
+                /// Stripe requires that "The amount must be provided as a boxed UInt in the currency's smallest unit."
+                /// Smallest-unit and UInt conversion is done in the same way as for the total amount, but that does not need to be boxed.
+                let applicationFeeForStripe = NSNumber(value: prepareAmountForStripe(applicationFee))
+                builder.setApplicationFeeAmount(applicationFeeForStripe)
+            }
+
+            builder.setReceiptEmail(receiptEmail)
+
+            let updatedMetadata = prepareMetadataForStripe(with: cardReaderMetadata)
+            builder.setMetadata(updatedMetadata)
+
+            // Return payment intent built configuration:
+            return try builder.build()
     }
 
     func prepareMetadataForStripe(with meta: CardReaderMetadata? = nil) -> [String: String]? {

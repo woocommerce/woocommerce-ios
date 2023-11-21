ae1127754ca544779fde63c0d80b63409f7b6ecc
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2023-11-21 17:46:42 +0100

    Fix format

--- WooCommerce/Classes/ViewRelated/Orders/Order Creation/CustomAmounts/AddCustomAmountPercentageViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/CustomAmounts/AddCustomAmountPercentageViewModel.swift
@@ -6,30 +6,30 @@ import Yosemite
 final class AddCustomAmountPercentageViewModel: ObservableObject {
     let currencyFormatter: CurrencyFormatter
     let baseAmountForPercentage: Decimal
-
+    
     @Published var percentageCalculatedAmount: String = ""
     var baseAmountForPercentageString: String {
         guard let formattedAmount = currencyFormatter.formatAmount(baseAmountForPercentage) else {
             return ""
         }
-
+        
         return formattedAmount
     }
-
+    
     @Published var percentage = "" {
         didSet {
             guard oldValue != percentage else { return }
-
+            
             updateAmountBasedOnPercentage(percentage)
         }
     }
-
+    
     init(baseAmountForPercentage: Decimal, currencyFormatter: CurrencyFormatter) {
         self.baseAmountForPercentage = baseAmountForPercentage
         self.currencyFormatter = currencyFormatter
         percentageCalculatedAmount = "0"
     }
-
+    
     func updateAmountBasedOnPercentage(_ percentage: String) {
         guard percentage.isNotEmpty,
               let decimalInput = currencyFormatter.convertToDecimal(percentage) else {
@@ -40,12 +40,12 @@ final class AddCustomAmountPercentageViewModel: ObservableObject {
         let amountString = "\(baseAmountForPercentage * (decimalInput as Decimal) * 0.01)"
         percentageCalculatedAmount = currencyFormatter.formatAmount(amountString) ?? ""
     }
-
+    
     func preset(with fee: OrderFeeLine) {
         guard let totalDecimal = currencyFormatter.convertToDecimal(fee.total) else {
             return
         }
-
+        
         percentage = currencyFormatter.localize(((totalDecimal as Decimal / baseAmountForPercentage) * 100)) ?? "0"
         percentageCalculatedAmount = fee.total
     }

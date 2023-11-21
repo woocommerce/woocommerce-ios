87ca9124910d9052ce7c9398b17d44231b53c9a4
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2023-11-21 14:29:22 +0100

    Move class to its own file

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/CustomAmounts/AddCustomAmountPercentageView.swift
@@ -0,0 +1,85 @@
+import SwiftUI
+
+struct AddCustomAmountPercentageView: View {
+    @ObservedObject private(set) var viewModel: AddCustomAmountPercentageViewModel
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: Layout.mainVerticalSpacing) {
+            HStack() {
+                Text(Localization.percentageInputTitle)
+                    .font(.subheadline)
+                    .foregroundColor(Color(.textSubtle))
+
+                Spacer()
+
+                Text("(\(viewModel.baseAmountForPercentageString))")
+                    .font(.subheadline)
+                    .foregroundColor(Color(.textSubtle))
+            }
+
+            PercentageInputField(text: $viewModel.percentage)
+
+            Divider()
+                .padding(.bottom, Layout.mainVerticalSpacing)
+
+            HStack() {
+                Text(Localization.amountTitle)
+                    .font(.subheadline)
+                    .foregroundColor(Color(.textSubtle))
+
+                Spacer()
+
+                Text(viewModel.percentageCalculatedAmount)
+                    .font(.subheadline)
+                    .foregroundColor(Color(.textSubtle))
+            }
+        }
+    }
+}
+
+private extension AddCustomAmountPercentageView {
+    enum Layout {
+        static let mainVerticalSpacing: CGFloat = 8
+        static func percentageFontSize(scale: CGFloat) -> CGFloat {
+            56 * scale
+        }
+    }
+}
+
+private extension AddCustomAmountPercentageView {
+    enum Localization {
+        static let amountTitle = NSLocalizedString("Amount", comment: "Title above the amount field on the add custom amount view in orders.")
+
+        static let percentageInputTitle = NSLocalizedString("addCustomAmountView.percentageTextField.title",
+                                                            value: "Enter percentage of order total",
+                                                            comment: "Title for entering an custom amount through a percentage")
+        static let percentageInputPlaceholder = NSLocalizedString("addCustomAmountView.percentageTextField.placeholder",
+                                                                  value: "Enter percentage",
+                                                                  comment: "Placeholder for entering an custom amount through a percentage")
+    }
+}
+
+private extension AddCustomAmountPercentageView {
+    struct PercentageInputField: View {
+        @ScaledMetric private var scale: CGFloat = 1.0
+        @Binding var text: String
+
+        var body: some View {
+            HStack(spacing: 0) {
+                TextField("",
+                          text: $text,
+                          prompt: Text("0").foregroundColor(Color(.textSubtle))
+                )
+                .font(.system(size: Layout.percentageFontSize(scale: scale), weight: .bold))
+                .keyboardType(.decimalPad)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .fixedSize()
+
+                Text("%")
+                    .font(.system(size: Layout.percentageFontSize(scale: scale), weight: .bold))
+                    .foregroundColor(text.isEmpty ? Color(.textSubtle) : Color(.text))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+}
--- WooCommerce/Classes/ViewRelated/Orders/Order Creation/CustomAmounts/AddCustomAmountView.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/CustomAmounts/AddCustomAmountView.swift
@@ -24,12 +24,11 @@ struct AddCustomAmountView: View {
                                     .padding(.bottom, Layout.mainVerticalSpacing)
                             }
                         }
-//
+
                         if let percentageViewModel = viewModel.percentageViewModel {
                             AddCustomAmountPercentageView(viewModel: percentageViewModel)
                                 .padding(.bottom, Layout.mainVerticalSpacing)
                         }
-
 
                         Toggle(Localization.chargeTaxesToggleTitle, isOn: $viewModel.isTaxable)
                             .font(.title3)
@@ -75,9 +74,6 @@ struct AddCustomAmountView: View {
 private extension AddCustomAmountView {
     enum Layout {
         static let mainVerticalSpacing: CGFloat = 8
-        static func percentageFontSize(scale: CGFloat) -> CGFloat {
-            56 * scale
-        }
     }
 }
 
@@ -101,93 +97,5 @@ private extension AddCustomAmountView {
 
     enum AccessibilityIdentifiers {
         static let addCustomAmountButton = "order-add-custom-amount-view-add-custom-amount-button"
-    }
-}
-
-struct AddCustomAmountPercentageView: View {
-    @ObservedObject private(set) var viewModel: AddCustomAmountPercentageViewModel
-
-    var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            HStack() {
-                Text(Localization.percentageInputTitle)
-                    .font(.subheadline)
-                    .foregroundColor(Color(.textSubtle))
-
-                Spacer()
-
-                Text("(\(viewModel.baseAmountForPercentageString))")
-                    .font(.subheadline)
-                    .foregroundColor(Color(.textSubtle))
-            }
-
-            PercentageInputField(text: $viewModel.percentage)
-
-            Divider()
-                .padding(.bottom, 8)
-
-            HStack() {
-                Text(Localization.amountTitle)
-                    .font(.subheadline)
-                    .foregroundColor(Color(.textSubtle))
-
-                Spacer()
-
-                Text(viewModel.percentageCalculatedAmount)
-                    .font(.subheadline)
-                    .foregroundColor(Color(.textSubtle))
-            }
-
-        }
-    }
-}
-private extension AddCustomAmountPercentageView {
-    enum Layout {
-        static let mainVerticalSpacing: CGFloat = 8
-        static func percentageFontSize(scale: CGFloat) -> CGFloat {
-            56 * scale
-        }
-    }
-}
-
-private extension AddCustomAmountPercentageView {
-    enum Localization {
-        static let amountTitle = NSLocalizedString("Amount", comment: "Title above the amount field on the add custom amount view in orders.")
-
-        static let percentageInputTitle = NSLocalizedString("addCustomAmountView.percentageTextField.title",
-                                                             value: "Enter percentage of order total",
-                                                             comment: "Title for entering an custom amount through a percentage")
-        static let percentageInputPlaceholder = NSLocalizedString("addCustomAmountView.percentageTextField.placeholder",
-                                                             value: "Enter percentage",
-                                                             comment: "Placeholder for entering an custom amount through a percentage")
-    }
-
-    enum AccessibilityIdentifiers {
-        static let addCustomAmountButton = "order-add-custom-amount-view-add-custom-amount-button"
-    }
-}
-
-private extension AddCustomAmountPercentageView {
-    struct PercentageInputField: View {
-        @ScaledMetric private var scale: CGFloat = 1.0
-        @Binding var text: String
-
-        var body: some View {
-            HStack(spacing: 0) {
-                TextField("",
-                          text: $text,
-                          prompt: Text("0").foregroundColor(Color(.textSubtle))
-                )
-                .font(.system(size: Layout.percentageFontSize(scale: scale), weight: .bold))
-                .keyboardType(.decimalPad)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .fixedSize()
-
-                Text("%")
-                    .font(.system(size: Layout.percentageFontSize(scale: scale), weight: .bold))
-                    .foregroundColor(text.isEmpty ? Color(.textSubtle) : Color(.text))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-            }
-        }
     }
 }

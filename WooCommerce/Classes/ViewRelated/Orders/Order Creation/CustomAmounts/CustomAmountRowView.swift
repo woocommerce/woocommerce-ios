f8b025f20096f45d6c760e0c19214ae231de9ded
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-11-22 18:37:50 +0900

    Update CustomAmountRowView design

--- WooCommerce/Classes/ViewRelated/Orders/Order Creation/CustomAmounts/CustomAmountRowView.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/CustomAmounts/CustomAmountRowView.swift
@@ -10,65 +10,42 @@ struct CustomAmountRowView: View {
     let editable: Bool
 
     var body: some View {
-        HStack(alignment: .center) {
-            Image(systemName: "tag")
-                .resizable()
-                .foregroundColor(Color(uiColor: .secondaryLabel))
-                .frame(width: Layout.tagIconImageSize * scale, height: Layout.tagIconImageSize * scale)
-                .padding()
-                .overlay {
-                    RoundedRectangle(cornerRadius: Layout.tagBorderCornerRadius)
-                        .stroke(Color(.separator), lineWidth: Layout.tagBorderLineWidth)
-                }
-
-            VStack(alignment: .leading) {
-                HStack(alignment: .top) {
-                    Text(viewModel.name)
-                        .foregroundColor(Color(.wooCommercePurple(.shade60)))
-                        .bodyStyle()
-                    Button {
-                        viewModel.onEditCustomAmount()
-                    } label: {
-                        Image(systemName: "pencil")
-                            .resizable()
-                            .padding(.top, Layout.editIconTopPadding)
-                            .frame(width: Layout.editIconImageSize * scale,
-                                   height: Layout.editIconImageSize * scale)
-                            .renderedIf(editable)
-                    }
-                }
-
-                Text(viewModel.total)
-                    .subheadlineStyle()
-            }
+        HStack(alignment: .center, spacing: Layout.contentSpacing) {
+            Text(viewModel.name)
+                .foregroundColor(.init(UIColor.text))
+                .subheadlineStyle()
+                .multilineTextAlignment(.leading)
+                // Avoids the custom amount name to be truncated when it's long enough
+                .fixedSize(horizontal: false, vertical: true)
 
             Spacer()
 
-            Button {
-                viewModel.onRemoveCustomAmount()
-            } label: {
-                Image(systemName: "xmark")
-            }
-            .foregroundColor(Color(.secondaryLabel))
-            .tertiaryTitleStyle()
-            .frame(width: Layout.closeButtonImageSize * scale,
-                   height: Layout.closeButtonImageSize * scale, alignment: .trailing)
-            .renderedIf(editable)
+            Text(viewModel.total)
+                .foregroundColor(.init(UIColor.text))
+                .subheadlineStyle()
 
+            Image(uiImage: .pencilImage)
+                .resizable()
+                .flipsForRightToLeftLayoutDirection(true)
         }
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(.top, Layout.topPadding)
+        .padding(Layout.contentPadding)
+        .background(
+            RoundedRectangle(cornerRadius: Layout.cornerRadius)
+                .fill(Color(uiColor: .init(light: UIColor.clear,
+                                           dark: UIColor.systemGray5)))
+        )
+        .overlay {
+            RoundedRectangle(cornerRadius: Layout.cornerRadius)
+                .stroke(Color(uiColor: .separator), lineWidth: Layout.borderLineWidth)
+        }
     }
 }
 
 extension CustomAmountRowView {
     enum Layout {
-        static let tagBorderCornerRadius: CGFloat = 4
-        static let tagBorderLineWidth: CGFloat = 0.5
-        static let tagIconImageSize: CGFloat = 25
-        static let editIconImageSize: CGFloat = 16
-        static let editIconTopPadding: CGFloat = 3
-        static let closeButtonImageSize: CGFloat = 25
-        static let topPadding: CGFloat = 16
+        static let contentSpacing: CGFloat = 16
+        static let contentPadding: CGFloat = 16
+        static let cornerRadius: CGFloat = 8
+        static let borderLineWidth: CGFloat = 0.5
     }
 }

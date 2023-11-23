ed0229bf377e1a30a76a08b0fe96ada480afa9f3
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-11-23 12:18:41 +0530

    Move Woo subscription product types out of core product types test.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Actions Factory/ProductFormActionsFactory+ProductCreationTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Actions Factory/ProductFormActionsFactory+ProductCreationTests.swift
@@ -7,11 +7,28 @@ import Fakes
 final class ProductFormActionsFactory_ProductCreationTests: XCTestCase {
     func test_product_type_is_editable_for_core_product_types() {
         // Product types supported in core.
-        let coreProductTypes: [ProductType] = [.simple, .variable, .affiliate, .grouped, .subscription, .variableSubscription]
+        let coreProductTypes: [ProductType] = [.simple, .variable, .affiliate, .grouped]
 
         coreProductTypes.forEach { coreProductType in
             // Given
             let product = Product.fake().copy(productTypeKey: coreProductType.rawValue)
+            let model = EditableProductModel(product: product)
+
+            // When
+            let actions = Fixtures.actionsFactory(product: model, formType: .add).settingsSectionActions()
+
+            // Then
+            XCTAssertTrue(actions.contains(.productType(editable: true)))
+        }
+    }
+
+    func test_product_type_is_editable_for_woo_subscription_product_types() {
+        // Product types supported by Woo Subscription plugin.
+        let subscriptionProductTypes: [ProductType] = [.subscription, .variableSubscription]
+
+        subscriptionProductTypes.forEach { type in
+            // Given
+            let product = Product.fake().copy(productTypeKey: type.rawValue)
             let model = EditableProductModel(product: product)
 
             // When

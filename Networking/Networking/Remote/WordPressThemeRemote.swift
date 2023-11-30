f0b4d7156f2ffb40a7109065adb63e8402406b3a
Huong Do <hi.huongdo@gmail.com>
2023-11-30 11:21:06 +0700

    Hardcode queries in the path for suggest themes

--- Networking/Networking/Remote/WordPressThemeRemote.swift
+++ Networking/Networking/Remote/WordPressThemeRemote.swift
@@ -14,11 +14,7 @@ public final class WordPressThemeRemote: Remote, WordPressThemeRemoteProtocol {
 
     public func loadSuggestedThemes() async throws -> [WordPressTheme] {
         let path = Paths.suggestedThemes
-        let parameters: [String: Any] = [
-            Keys.filter: Values.suggestedThemeFilter,
-            Keys.number: Values.maximumThemeCount
-        ]
-        let request = DotcomRequest(wordpressApiVersion: .mark1_2, method: .get, path: path, parameters: parameters)
+        let request = DotcomRequest(wordpressApiVersion: .mark1_2, method: .get, path: path)
         let mapper = WordPressThemeListMapper()
         return try await enqueue(request, mapper: mapper)
             .filter { theme in
@@ -29,18 +25,10 @@ public final class WordPressThemeRemote: Remote, WordPressThemeRemoteProtocol {
 
 private extension WordPressThemeRemote {
     enum Paths {
-        // https://public-api.wordpress.com/rest/v1.2/themes?filter=subject:store&number=100
-        static let suggestedThemes = "themes"
-    }
-
-    enum Keys {
-        static let filter = "filter"
-        static let number =  "number"
+        static let suggestedThemes = "themes?filter=subject:store&number=100"
     }
 
     enum Values {
-        static let suggestedThemeFilter = "subject:store"
-        static let maximumThemeCount = 100
         static let filteredThemeIDs = ["tsubaki", "tazza", "amulet", "zaino", "thriving-artist", "attar"]
     }
 }

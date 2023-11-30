2d3dc3b1c101d2d674d3c1ffbe931b7363b8f90b
Huong Do <hi.huongdo@gmail.com>
2023-11-30 11:42:46 +0700

    Add WordPressThemeStore

---
+++ Yosemite/Yosemite/Stores/WordPressThemeStore.swift
@@ -0,0 +1,67 @@
+import Foundation
+import Networking
+import Storage
+
+// MARK: - WordPressThemeStore
+//
+public final class WordPressThemeStore: Store {
+    private let remote: WordPressThemeRemoteProtocol
+
+    init(dispatcher: Dispatcher,
+         storageManager: StorageManagerType,
+         network: Network,
+         remote: WordPressThemeRemoteProtocol) {
+        self.remote = remote
+        super.init(dispatcher: dispatcher, storageManager: storageManager, network: network)
+    }
+
+    /// Initializes a new WordPressThemeStore.
+    /// - Parameters:
+    ///   - dispatcher: The dispatcher used to subscribe to `WordPressThemeAction`.
+    ///   - network: The network layer used to fetch theme details
+    ///
+    public convenience override init(dispatcher: Dispatcher, storageManager: StorageManagerType, network: Network) {
+        self.init(dispatcher: dispatcher,
+                  storageManager: storageManager,
+                  network: network,
+                  remote: WordPressThemeRemote(network: network))
+    }
+
+    // MARK: - Actions
+
+    /// Registers for supported Actions.
+    ///
+    override public func registerSupportedActions(in dispatcher: Dispatcher) {
+        dispatcher.register(processor: self, for: BlazeAction.self)
+    }
+
+    /// Receives and executes Actions.
+    /// - Parameters:
+    ///   - action: An action to handle. Must be a `WordPressThemeAction`
+    ///
+    override public func onAction(_ action: Action) {
+        guard let action = action as? WordPressThemeAction else {
+            assertionFailure("BlazeStore received an unsupported action")
+            return
+        }
+
+        switch action {
+        case .loadSuggestedThemes(let onCompletion):
+            loadSuggestedThemes(onCompletion: onCompletion)
+        }
+    }
+}
+
+private extension WordPressThemeStore {
+
+    func loadSuggestedThemes(onCompletion: @escaping (Result<[WordPressTheme], Error>) -> Void) {
+        Task { @MainActor in
+            do {
+                let themes = try await remote.loadSuggestedThemes()
+                onCompletion(.success(themes))
+            } catch {
+                onCompletion(.failure(error))
+            }
+        }
+    }
+}

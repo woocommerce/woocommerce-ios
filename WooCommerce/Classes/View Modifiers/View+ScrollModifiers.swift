7ddc890bd1d6cfdebf69440c61f70c33119f5e02
Josh Heald <joshheald@users.noreply.github.com>
2023-11-30 16:14:53 +0000

    11334 Prevent ∞ loop in scrollVerticallyIfNeeded
    Previously, we checked the size of the content within the body of the scrollVerticallyIfNeeded view modifier, and set the `shouldScroll` state according to the size compared against the parent’s size.
    
    Since it was in `body`, this comparison happened during the layout pass, and because it resulted in a change of state which the same view depended on, it would result in a re-render of the view.
    
    For some sizes, particularly where `contentSize` was very close/equal to `parentGeometry.size`, this re-render would change the `shouldScroll` state back to its previous value, and thus invalidate the view again. That was the cause of the infinite layout loop.
    
    In this commit, we move the comparison out of the `body`, so we’re never changing `State` _during_ a layout pass, but instead we change it in response to a preference key change. `PreferenceKey` is a convenient way to pass data from child views to parent views.

--- WooCommerce/Classes/View Modifiers/View+ScrollModifiers.swift
+++ WooCommerce/Classes/View Modifiers/View+ScrollModifiers.swift
@@ -1,52 +1,59 @@
 import SwiftUI
 
-/// View modifier that conditionally wraps the `content` in a `Scrollview` if the `content` height exceeds the view height.
+/// Preference key for communicating sizes
+struct SizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize? = nil
+
+    static func reduce(value: inout CGSize?, nextValue: () -> CGSize?) {
+        // Take the response of the first child which updates the key. Disallow further updates from its children.
+        value = value ?? nextValue()
+    }
+}
+
+/// View modifier that conditionally wraps the `content` in a `ScrollView` if the `content` height exceeds the view height.
 ///
 struct ConditionalVerticalScrollModifier: ViewModifier {
-
-    /// Keeps track of the `content` rendered size
-    ///
-    @State private var contentSize: CGSize = .zero
-
     /// Defines if the content should scroll or not.
-    ///
     @State private var shouldScroll: Bool = false
-
 
     func body(content: Content) -> some View {
         GeometryReader { parentGeometry in
-            if shouldScroll {
-                ScrollView(.vertical, showsIndicators: false) {
-                    contentGeometryListener(content: content, parentGeometry: parentGeometry)
+            Group {
+                if shouldScroll {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        contentGeometryListener(content: content)
+                    }
+                } else {
+                    contentGeometryListener(content: content)
                 }
-            } else {
-                contentGeometryListener(content: content, parentGeometry: parentGeometry)
+            }
+            .onPreferenceChange(SizePreferenceKey.self) { contentSize in
+                /// Using `onPreferenceChange` avoid changing state (`shouldScroll`) during a layout pass.
+                /// Changing state that's used to layout the view during layout can cause an infinite loop and make the screen unresponsive.
+                if let contentSize = contentSize {
+                    shouldScroll = contentSize.height > parentGeometry.size.height
+                }
             }
         }
-        .frame(maxHeight: contentSize.height)
     }
 
-    /// Updates the `contentSize` property by adding a clear background to the `content` that has a `GeometryReader` attached to it.
-    /// Updates the `shouldScroll` property when the content vertical geometry is greater than the parent vertical geometry.
+    /// Updates the `SizePreferenceKey` by adding a clear background to the `content` that has a `GeometryReader` attached to it.
+    /// This is used in the parent to update the `shouldScroll` property when the content vertical geometry is greater than the parent vertical geometry.
     ///
-    private func contentGeometryListener(content: Content, parentGeometry: GeometryProxy) -> some View {
+    private func contentGeometryListener(content: Content) -> some View {
         content
             .background(
-                GeometryReader { contentGeometry -> Color in
-                    DispatchQueue.main.async {
-                        contentSize = contentGeometry.size
-                        shouldScroll = contentGeometry.size.height > parentGeometry.size.height
-                    }
-                    return .clear
-                }
-            )
+                GeometryReader { geometry in
+                    Color.clear
+                        .preference(key: SizePreferenceKey.self, value: geometry.size)
+                })
     }
 }
 
 // MARK: View Extensions
 
 extension View {
-    /// Allows the view to scroll vertically when the content height is greater than it's parent height.
+    /// Allows the view to scroll vertically when the content height is greater than its parent height.
     ///
     func scrollVerticallyIfNeeded() -> some View {
         self.modifier(ConditionalVerticalScrollModifier())

c111877a321b6fbad71919e1b222b3fe7f185bff
Hafiz Rahman <hfzrhmn@gmail.com>
2023-11-30 17:53:40 +0700

    Add carousel that loads image from URL.

--- WooCommerce/Classes/ViewRelated/Themes/ThemesPickerView.swift
+++ WooCommerce/Classes/ViewRelated/Themes/ThemesPickerView.swift
@@ -1,18 +1,65 @@
 import SwiftUI
+import Kingfisher
 
 struct ThemesPickerView: View {
+    /// Scale of the view based on accessibility changes
+    @ScaledMetric private var scale: CGFloat = 1.0
+
     var body: some View {
+        let urls = [
+            "google.com",
+            "facebook.com",
+            "microsoft.com",
+            "wordpress.com",
+            "amazon.com",
+            "apple.com"
+        ]
+
         ScrollView {
             VStack(alignment: .leading) {
+                Spacer()
+                    .frame(height: 40)
 
                 // Title label.
                 Text(Localization.chooseThemeHeading)
-                    .headlineStyle()
+                    .bold()
+                    .largeTitleStyle()
+                    .padding(.horizontal, Layout.contentPadding)
+
+                Spacer()
+                    .frame(height: 16)
 
                 // Subtitle label.
                 Text(Localization.chooseThemeSubtitle)
-                    .foregroundColor(Color(.secondaryLabel))
-                    .bodyStyle()
+                    .subheadlineStyle()
+                    .padding(.horizontal, Layout.contentPadding)
+
+                Spacer()
+                    .frame(height: 40)
+
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack() {
+                        ForEach(urls, id: \.self) { url in
+                            if let themeURL = getThemeUrl(themeUrl: url) {
+                                KFImage(themeURL)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: Layout.imageWidth * scale, height: Layout.imageHeight * scale)
+                                    .cornerRadius(Layout.cornerRadius)
+                                    .shadow(radius: Layout.shadowRadius, x: 0, y: 3)
+                                    .padding(.init(
+                                        top: Layout.imageVerticalPadding,
+                                        leading: Layout.imageHorizontalPadding,
+                                        bottom: Layout.imageVerticalPadding,
+                                        trailing: Layout.imageHorizontalPadding)
+                                    )
+
+                            }
+                        }
+                    }
+                }
+
+                Spacer()
             }
 
         }
@@ -28,28 +75,48 @@ struct ThemesPickerView: View {
     }
 }
 
-struct ThemesCarouselView_Previews: PreviewProvider {
+private extension ThemesPickerView {
+    private func getThemeUrl(themeUrl: String) -> URL? {
+        let urlStr = "https://s0.wp.com/mshots/v1/https://\(themeUrl)?demo=true/?w=1200&h=2400&vpw=400&vph=800"
+        return URL(string: urlStr)
+    }
+}
+
+struct ThemesPickerView_Previews: PreviewProvider {
     static var previews: some View {
         ThemesPickerView()
     }
 }
 
-private enum Localization {
-    static let skipButtonTitle = NSLocalizedString(
-        "ThemesPickerView.skipButtonTitle",
-        value: "Skip",
-        comment: "Title of the button to skip theme carousel screen."
-    )
+private extension ThemesPickerView {
+    private enum Layout {
+        static let contentPadding: CGFloat = 16
+        static let imageHorizontalPadding: CGFloat = 8
+        static let imageVerticalPadding: CGFloat = 8
+        static let imageWidth: CGFloat = 225
+        static let imageHeight: CGFloat = 450
+        static let cornerRadius: CGFloat = 8
+        static let shadowRadius: CGFloat = 2
+    }
 
-    static let chooseThemeHeading = NSLocalizedString(
-    "ThemesPickerView.chooseThemeHeading",
-    value: "Choose a theme",
-    comment: "Main heading on the theme carousel screen."
-    )
+    private enum Localization {
+        static let skipButtonTitle = NSLocalizedString(
+            "ThemesPickerView.skipButtonTitle",
+            value: "Skip",
+            comment: "Title of the button to skip theme carousel screen."
+        )
 
-    static let chooseThemeSubtitle = NSLocalizedString(
-    "ThemesPickerView.chooseThemeSubtitle",
-    value: "You can always change it later in the settings.",
-    comment: "Subtitle on the theme carousel screen."
-    )
+        static let chooseThemeHeading = NSLocalizedString(
+        "ThemesPickerView.chooseThemeHeading",
+        value: "Choose a theme",
+        comment: "Main heading on the theme carousel screen."
+        )
+
+        static let chooseThemeSubtitle = NSLocalizedString(
+        "ThemesPickerView.chooseThemeSubtitle",
+        value: "You can always change it later in the settings.",
+        comment: "Subtitle on the theme carousel screen."
+        )
+    }
+
 }

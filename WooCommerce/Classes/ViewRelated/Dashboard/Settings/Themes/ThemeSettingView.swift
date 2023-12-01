6fbb21a578e6f5a7b7cbedbc3059d7dd2c2044a2
Huong Do <hi.huongdo@gmail.com>
2023-12-01 10:03:51 +0700

    Add new view for theme settings

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Themes/ThemeSettingView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+/// View for selecting a new theme for the current site.
+///
+struct ThemeSettingView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ThemeSettingView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        Group {
+            ThemeSettingView()
+        }
+    }
+}

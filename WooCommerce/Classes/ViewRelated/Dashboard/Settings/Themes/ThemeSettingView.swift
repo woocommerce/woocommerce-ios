4b72d91166c90e133fb1b431108c1083771dbbed
Huong Do <hi.huongdo@gmail.com>
2023-12-01 11:14:04 +0700

    Add layout for theme setting view

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Themes/ThemeSettingView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Themes/ThemeSettingView.swift
@@ -1,10 +1,48 @@
 import SwiftUI
 
+final class ThemeSettingHostingController: UIHostingController<ThemeSettingView> {
+    init(siteID: Int64) {
+        super.init(rootView: ThemeSettingView())
+    }
+
+    required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 /// View for selecting a new theme for the current site.
 ///
 struct ThemeSettingView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section(Localization.currentSection) {
+                    HStack {
+                        Text(Localization.themeRow)
+                            .bodyStyle()
+                        Spacer()
+                        Text("Tsubaki")
+                            .secondaryBodyStyle()
+                    }
+                }
+
+                Section(Localization.tryOtherLook) {
+                    VStack {
+                        // TODO: add slider here
+                        Spacer()
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button(Localization.cancelButton) {
+                        // TODO: dismiss view
+                    }
+                }
+            }
+            .navigationTitle(Localization.title)
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 

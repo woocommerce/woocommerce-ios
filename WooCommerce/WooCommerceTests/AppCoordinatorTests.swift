29d3d655411daec9774cfeb66206338384345b40
Rachel McRoberts <rachel@automattic.com>
2023-12-07 10:52:26 +0000

    Attempt to fix crash with magic link login

--- WooCommerce/WooCommerceTests/AppCoordinatorTests.swift
+++ WooCommerce/WooCommerceTests/AppCoordinatorTests.swift
@@ -568,6 +568,27 @@ final class AppCoordinatorTests: XCTestCase {
         }
         XCTAssertNil(loginNavigationController.presentedViewController)
     }
+
+    func test_appCoordinator_start_resets_default_store_and_proceeds_to_login_when_isAuthenticated_and_needsDefaultStore_are_false() {
+        // Given
+        sessionManager = .makeForTesting(authenticated: false, defaultSite: Site.fake().copy(siteID: 123))
+        stores = MockStoresManager(sessionManager: sessionManager)
+        let appCoordinator = makeCoordinator(authenticationManager: authenticationManager,
+                                             loggedOutAppSettings: MockLoggedOutAppSettings(hasFinishedOnboarding: true))
+        coordinator = appCoordinator
+
+        XCTAssertFalse(stores.isAuthenticated)
+        XCTAssertFalse(stores.needsDefaultStore)
+
+        // When
+        appCoordinator.start()
+
+        // Then
+        waitUntil {
+            self.window.rootViewController is LoginNavigationController
+        }
+        XCTAssertTrue(stores.needsDefaultStore)
+    }
 }
 
 private extension AppCoordinatorTests {

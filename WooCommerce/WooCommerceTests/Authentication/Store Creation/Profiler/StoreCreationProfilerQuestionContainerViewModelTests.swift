d1dd45c5fd13fdfb0bf4316a97bcb8f85d7e8497
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-12-11 17:21:02 +0530

    Remove features and challenges questions related unit tests.

--- WooCommerce/WooCommerceTests/Authentication/Store Creation/Profiler/StoreCreationProfilerQuestionContainerViewModelTests.swift
+++ WooCommerce/WooCommerceTests/Authentication/Store Creation/Profiler/StoreCreationProfilerQuestionContainerViewModelTests.swift
@@ -80,19 +80,6 @@ final class StoreCreationProfilerQuestionContainerViewModelTests: XCTestCase {
         XCTAssertEqual(data.category, StoreCreationCategoryQuestionViewModel.Category.foodDrink.rawValue)
     }
 
-    func test_saveCountry_updates_currentQuestion_to_challenges() {
-        // Given
-        let viewModel = StoreCreationProfilerQuestionContainerViewModel(storeName: "Test",
-                                                                        onCompletion: {},
-                                                                        uploadAnswersUseCase: StoreCreationProfilerUploadAnswersUseCase(siteID: 123))
-
-        // When
-        viewModel.saveCountry(.US)
-
-        // Then
-        XCTAssertEqual(viewModel.currentQuestion, .challenges)
-    }
-
     func test_saveCountry_saves_answer() throws {
         // Given
         let usecase = MockStoreCreationProfilerUploadAnswersUseCase()
@@ -108,7 +95,7 @@ final class StoreCreationProfilerQuestionContainerViewModelTests: XCTestCase {
         XCTAssertEqual(data.countryCode, "AG")
     }
 
-    func test_saveFeatures_triggers_onCompletion() throws {
+    func test_saveCountry_triggers_onCompletion() throws {
         // Given
         var triggeredCompletion = false
         let viewModel = StoreCreationProfilerQuestionContainerViewModel(storeName: "Test",
@@ -116,7 +103,7 @@ final class StoreCreationProfilerQuestionContainerViewModelTests: XCTestCase {
                                                                         uploadAnswersUseCase: MockStoreCreationProfilerUploadAnswersUseCase())
 
         // When
-        viewModel.saveFeatures([])
+        viewModel.saveCountry(.US)
 
         // Then
         XCTAssertTrue(triggeredCompletion)
@@ -171,38 +158,6 @@ final class StoreCreationProfilerQuestionContainerViewModelTests: XCTestCase {
         // Then
         XCTAssertFalse(triggeredCompletion)
         XCTAssertEqual(viewModel.currentQuestion, .category)
-    }
-
-    func test_backtrackOrDismissProfiler_sets_current_question_to_country_if_current_question_is_challenges() {
-        // Given
-        var triggeredCompletion = false
-        let viewModel = StoreCreationProfilerQuestionContainerViewModel(storeName: "Test",
-                                                                        onCompletion: { triggeredCompletion = true },
-                                                                        uploadAnswersUseCase: StoreCreationProfilerUploadAnswersUseCase(siteID: 123))
-        viewModel.saveCountry(.US)
-
-        // When
-        viewModel.backtrackOrDismissProfiler()
-
-        // Then
-        XCTAssertFalse(triggeredCompletion)
-        XCTAssertEqual(viewModel.currentQuestion, .country)
-    }
-
-    func test_backtrackOrDismissProfiler_sets_current_question_to_challenges_if_current_question_is_features() {
-        // Given
-        var triggeredCompletion = false
-        let viewModel = StoreCreationProfilerQuestionContainerViewModel(storeName: "Test",
-                                                                        onCompletion: { triggeredCompletion = true },
-                                                                        uploadAnswersUseCase: StoreCreationProfilerUploadAnswersUseCase(siteID: 123))
-        viewModel.saveChallenges([])
-
-        // When
-        viewModel.backtrackOrDismissProfiler()
-
-        // Then
-        XCTAssertFalse(triggeredCompletion)
-        XCTAssertEqual(viewModel.currentQuestion, .challenges)
     }
 
     // MARK: - Analytics
@@ -303,71 +258,6 @@ final class StoreCreationProfilerQuestionContainerViewModelTests: XCTestCase {
         XCTAssertEqual(eventProperties["step"] as? String, "store_profiler_country")
     }
 
-
-    func test_saveCountry_tracks_site_creation_event_for_country_features() throws {
-        // Given
-        let viewModel = StoreCreationProfilerQuestionContainerViewModel(storeName: "Test",
-                                                                        analytics: analytics,
-                                                                        onCompletion: {},
-                                                                        uploadAnswersUseCase: StoreCreationProfilerUploadAnswersUseCase(siteID: 123))
-
-        // When
-        viewModel.saveCountry(.AD)
-
-        // Then
-        let indexOfEvent = try XCTUnwrap(analyticsProvider.receivedEvents.firstIndex(where: { $0 == "site_creation_step" }))
-        let eventProperties = try XCTUnwrap(analyticsProvider.receivedProperties[indexOfEvent])
-        XCTAssertEqual(eventProperties["step"] as? String, "store_profiler_challenges")
-    }
-
-    func test_saveChallenges_tracks_skip_event_for_challenges_questions() throws {
-        // Given
-        let viewModel = StoreCreationProfilerQuestionContainerViewModel(storeName: "Test",
-                                                                        analytics: analytics,
-                                                                        onCompletion: {},
-                                                                        uploadAnswersUseCase: StoreCreationProfilerUploadAnswersUseCase(siteID: 123))
-
-        // When
-        viewModel.saveChallenges([])
-
-        // Then
-        let indexOfEvent = try XCTUnwrap(analyticsProvider.receivedEvents.firstIndex(where: { $0 == "site_creation_profiler_question_skipped" }))
-        let eventProperties = try XCTUnwrap(analyticsProvider.receivedProperties[indexOfEvent])
-        XCTAssertEqual(eventProperties["step"] as? String, "store_profiler_challenges")
-    }
-
-    func test_saveChallenges_tracks_site_creation_event_for_features_question() throws {
-        // Given
-        let viewModel = StoreCreationProfilerQuestionContainerViewModel(storeName: "Test",
-                                                                        analytics: analytics,
-                                                                        onCompletion: {},
-                                                                        uploadAnswersUseCase: StoreCreationProfilerUploadAnswersUseCase(siteID: 123))
-
-        // When
-        viewModel.saveChallenges([])
-
-        // Then
-        let indexOfEvent = try XCTUnwrap(analyticsProvider.receivedEvents.firstIndex(where: { $0 == "site_creation_step" }))
-        let eventProperties = try XCTUnwrap(analyticsProvider.receivedProperties[indexOfEvent])
-        XCTAssertEqual(eventProperties["step"] as? String, "store_profiler_features")
-    }
-
-    func test_saveFeatures_tracks_skip_event_for_challenges_questions() throws {
-        // Given
-        let viewModel = StoreCreationProfilerQuestionContainerViewModel(storeName: "Test",
-                                                                        analytics: analytics,
-                                                                        onCompletion: {},
-                                                                        uploadAnswersUseCase: StoreCreationProfilerUploadAnswersUseCase(siteID: 123))
-
-        // When
-        viewModel.saveFeatures([])
-
-        // Then
-        let indexOfEvent = try XCTUnwrap(analyticsProvider.receivedEvents.firstIndex(where: { $0 == "site_creation_profiler_question_skipped" }))
-        let eventProperties = try XCTUnwrap(analyticsProvider.receivedProperties[indexOfEvent])
-        XCTAssertEqual(eventProperties["step"] as? String, "store_profiler_features")
-    }
-
     func test_profiler_data_is_tracked_onCompletion() throws {
         // Given
         let viewModel = StoreCreationProfilerQuestionContainerViewModel(storeName: "Test",
@@ -381,14 +271,6 @@ final class StoreCreationProfilerQuestionContainerViewModelTests: XCTestCase {
         viewModel.saveCategory(.init(name: StoreCreationCategoryQuestionViewModel.Category.clothingAccessories.name,
                                      value: StoreCreationCategoryQuestionViewModel.Category.clothingAccessories.rawValue))
         viewModel.saveCountry(.US)
-        viewModel.saveChallenges([.init(name: StoreCreationChallengesQuestionViewModel.Challenge.findingCustomers.name,
-                                        value: StoreCreationChallengesQuestionViewModel.Challenge.findingCustomers.rawValue),
-                                  .init(name: StoreCreationChallengesQuestionViewModel.Challenge.shippingAndLogistics.name,
-                                        value: StoreCreationChallengesQuestionViewModel.Challenge.shippingAndLogistics.rawValue)])
-        viewModel.saveFeatures([.init(name: StoreCreationFeaturesQuestionViewModel.Feature.productManagementAndInventoryTracking.name,
-                                      value: StoreCreationFeaturesQuestionViewModel.Feature.productManagementAndInventoryTracking.rawValue),
-                                .init(name: StoreCreationFeaturesQuestionViewModel.Feature.abilityToScaleAsBusinessGrows.name,
-                                      value: StoreCreationFeaturesQuestionViewModel.Feature.abilityToScaleAsBusinessGrows.rawValue)])
 
         // Then
         let indexOfEvent = try XCTUnwrap(analyticsProvider.receivedEvents.firstIndex(where: { $0 == "site_creation_profiler_data" }))
@@ -397,7 +279,5 @@ final class StoreCreationProfilerQuestionContainerViewModelTests: XCTestCase {
         XCTAssertEqual(eventProperties["user_commerce_journey"] as? String, "im_already_selling")
         XCTAssertEqual(eventProperties["ecommerce_platforms"] as? String, "big_cartel")
         XCTAssertEqual(eventProperties["country_code"] as? String, "US")
-        XCTAssertEqual(eventProperties["challenges"] as? String, "finding_customers,shipping_and_logistics")
-        XCTAssertEqual(eventProperties["features"] as? String, "product_management_and_inventory,scale_as_business_grows")
     }
 }

5e359d8b2ea59b3bcafa17d9e317e07d5248000b
Huong Do <hi.huongdo@gmail.com>
2023-12-12 11:35:59 +0700

    Integrate loading suggested themes

--- WooCommerce/Classes/ViewRelated/Themes/ThemeCarouselViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Themes/ThemeCarouselViewModel.swift
@@ -6,6 +6,8 @@ import Yosemite
 final class ThemeCarouselViewModel: ObservableObject {
 
     @Published private(set) var themes: [WordPressTheme] = []
+    @Published private(set) var fetchingThemes: Bool = false
+    @Published private(set) var failedToFetchThemes: Bool = false
 
     private let stores: StoresManager
 
@@ -13,4 +15,31 @@ final class ThemeCarouselViewModel: ObservableObject {
         self.stores = stores
     }
 
+    @MainActor
+    func fetchThemes() async {
+        fetchingThemes = true
+        failedToFetchThemes = false
+        do {
+            themes = try await loadSuggestedThemes()
+        } catch {
+            failedToFetchThemes = true
+        }
+        fetchingThemes = false
+    }
+}
+
+private extension ThemeCarouselViewModel {
+    @MainActor
+    func loadSuggestedThemes() async throws -> [WordPressTheme] {
+        try await withCheckedThrowingContinuation { continuation in
+            stores.dispatch(WordPressThemeAction.loadSuggestedThemes { result in
+                switch result {
+                case .success(let themes):
+                    continuation.resume(returning: themes)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            })
+        }
+    }
 }

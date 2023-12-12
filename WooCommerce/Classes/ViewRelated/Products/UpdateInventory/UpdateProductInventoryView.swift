9a9f289833fc3401a13f94372b1b48d446a7fa80
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2023-12-12 10:36:51 +0100

    Fix padding

--- WooCommerce/Classes/ViewRelated/Products/UpdateInventory/UpdateProductInventoryView.swift
+++ WooCommerce/Classes/ViewRelated/Products/UpdateInventory/UpdateProductInventoryView.swift
@@ -93,11 +93,11 @@ struct UpdateProductInventoryView: View {
                         Button(Localization.viewProductDetailsButtonTitle) {
                             isPresentingDetailsView = true
                         }
-                            .buttonStyle(SecondaryButtonStyle())
-                            .padding(.bottom)
-                            .sheet(isPresented: $isPresentingDetailsView) {
-                                viewModel.productDetailsView()
-                            }
+                        .buttonStyle(SecondaryButtonStyle())
+                        .padding(.bottom)
+                        .sheet(isPresented: $isPresentingDetailsView) {
+                            viewModel.productDetailsView()
+                        }
                     }
                     .padding()
                     .frame(minHeight: geometry.size.height)

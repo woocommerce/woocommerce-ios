783aba1361c82a0c22b2354b6bb078c5813fce6f
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-12-13 08:15:03 +0700

    Extract mock to its own file. Move to test target

--- WooCommerce/Classes/ViewRelated/Products/ProductsListViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/ProductsListViewModel.swift
@@ -2,27 +2,10 @@ import Foundation
 import Yosemite
 import Experiments
 
-final class MockProductListViewModel: ProductsListViewModelProtocol {
-    private(set) var scanToUpdateInventoryShouldBeVisible: Bool = false
-    private let featureFlagService: FeatureFlagService
-
-    init(featureFlagService: FeatureFlagService) {
-        self.featureFlagService = featureFlagService
-    }
-
-    func scanToUpdateInventoryButtonShouldBeVisible(completion: @escaping (Bool) -> (Void)) {
-        guard self.featureFlagService.isFeatureFlagEnabled(.scanToUpdateInventory) else {
-            scanToUpdateInventoryShouldBeVisible = false
-            return completion(false)
-        }
-        scanToUpdateInventoryShouldBeVisible = true
-        completion(true)
-    }
-}
-
 protocol ProductsListViewModelProtocol {
     func scanToUpdateInventoryButtonShouldBeVisible(completion: @escaping (Bool) -> (Void))
 }
+
 /// View model for `ProductsViewController`. Has stores logic related to Bulk Editing and Woo Subscriptions.
 ///
 final class ProductListViewModel: ProductsListViewModelProtocol {

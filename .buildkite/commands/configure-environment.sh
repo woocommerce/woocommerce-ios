0ea38e3d356856f1ff92d905694be72076e6b76c
Spencer Transier <17955542+spencertransier@users.noreply.github.com>
2023-12-14 23:00:34 -0800

    Remove unneeded gem cache restore

--- .buildkite/commands/configure-environment.sh
+++ .buildkite/commands/configure-environment.sh
@@ -4,5 +4,4 @@ echo '--- :git: Configure Git for release management'
 .buildkite/commands/configure-git-for-release-management.sh
 
 echo '--- :ruby: Setup Ruby tools'
-restore_cache "$(hash_file .ruby-version)-$(hash_file Gemfile.lock)"
 install_gems

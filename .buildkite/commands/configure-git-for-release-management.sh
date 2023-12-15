90d60831070b46d830e98f0156451db15d1dbe43
Spencer Transier <17955542+spencertransier@users.noreply.github.com>
2023-12-14 23:00:23 -0800

    Remove unneeded queue

--- .buildkite/commands/configure-git-for-release-management.sh
+++ .buildkite/commands/configure-git-for-release-management.sh
@@ -7,4 +7,4 @@ git config --global user.email "mobile+wpmobilebot@automattic.com"
 git config --global user.name "Automattic Release Bot"
 
 # Buildkite is currently using the https url to checkout. We need to override it to be able to use the deploy key.
-git remote set-url origin git@github.com:woocommerece/woocommerce-ios.git
+git remote set-url origin git@github.com:woocommerce/woocommerce-ios.git

2085481b4c7ac9447ea1c95fe7aa9d5bc740cc63
Spencer Transier <17955542+spencertransier@users.noreply.github.com>
2023-12-14 22:48:33 -0800

    Add Buildkite labels to steps

--- .buildkite/commands/configure-environment.sh
+++ .buildkite/commands/configure-environment.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eu
 
+echo '--- :git: Configure Git for release management'
 .buildkite/commands/configure-git-for-release-management.sh
 
+echo '--- :ruby: Setup Ruby tools'
 restore_cache "$(hash_file .ruby-version)-$(hash_file Gemfile.lock)"
 install_gems

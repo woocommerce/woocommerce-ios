5fba029a61f1bce797da7ee211b25c0c376113e1
Spencer Transier <17955542+spencertransier@users.noreply.github.com>
2023-12-16 00:25:45 -0800

    Use CI Toolkit `add_host_to_ssh_known_hosts` action

--- .buildkite/commands/configure-git-for-release-management.sh
+++ .buildkite/commands/configure-git-for-release-management.sh
@@ -2,7 +2,7 @@
 
 # Git command line client is not configured in Buildkite. Temporarily, we configure it in each step.
 # Later on, we should be able to configure the agent instead.
-curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >> ~/.ssh/known_hosts
+add_host_to_ssh_known_hosts github.com
 git config --global user.email "mobile+wpmobilebot@automattic.com"
 git config --global user.name "Automattic Release Bot"
 

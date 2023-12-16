aee6cd0c44b210c1ad3984e45318665cf7d8fa3b
Spencer Transier <17955542+spencertransier@users.noreply.github.com>
2023-12-16 00:25:57 -0800

    Remove unneeded `set-url` command

--- .buildkite/commands/configure-git-for-release-management.sh
+++ .buildkite/commands/configure-git-for-release-management.sh
@@ -5,6 +5,3 @@
 add_host_to_ssh_known_hosts github.com
 git config --global user.email "mobile+wpmobilebot@automattic.com"
 git config --global user.name "Automattic Release Bot"
-
-# Buildkite is currently using the https url to checkout. We need to override it to be able to use the deploy key.
-git remote set-url origin git@github.com:woocommerce/woocommerce-ios.git

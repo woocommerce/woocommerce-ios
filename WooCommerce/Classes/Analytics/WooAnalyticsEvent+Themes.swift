478c8c74ed4b644879895b6dfc65b5df5ec6e90b
Huong Do <hi.huongdo@gmail.com>
2023-12-18 11:08:22 +0700

    Add track event for pickerScreenDisplayed

--- WooCommerce/Classes/Analytics/WooAnalyticsEvent+Themes.swift
+++ WooCommerce/Classes/Analytics/WooAnalyticsEvent+Themes.swift
@@ -1,5 +1,26 @@
 import Foundation
 
 extension WooAnalyticsEvent {
-    
+
+    enum Themes {
+        private enum Key: String {
+            case source
+            case theme
+            case layout
+            case page
+            case pageURL = "page_url"
+        }
+
+        enum Source: String {
+            case profiler
+            case settings
+        }
+
+        /// Tracks when the theme picker screen is displayed
+        static func pickerScreenDisplayed(source: Source) -> WooAnalyticsEvent {
+            .init(statName: .themePickerScreenDisplayed, properties: [
+                Key.source.rawValue: source.rawValue
+            ])
+        }
+    }
 }

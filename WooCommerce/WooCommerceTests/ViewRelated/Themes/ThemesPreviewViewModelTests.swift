8e5d60b4812e25ea38ceca07078e82a29bcaf95f
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2023-12-20 09:10:53 +0530

    Unit test that theme installation works as expected in Theme preview screen.

--- WooCommerce/WooCommerceTests/ViewRelated/Themes/ThemesPreviewViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Themes/ThemesPreviewViewModelTests.swift
@@ -1,9 +1,12 @@
 import XCTest
 @testable import WooCommerce
 @testable import Yosemite
+import Combine
 
+@MainActor
 final class ThemesPreviewViewModelTests: XCTestCase {
     private var stores: MockStoresManager!
+    private var subscriptions = Set<AnyCancellable>()
 
     override func setUp() {
         super.setUp()
@@ -15,10 +18,14 @@ final class ThemesPreviewViewModelTests: XCTestCase {
         super.tearDown()
     }
 
-
     func test_initial_state_is_pagesLoading() {
         // Given
-        let viewModel = ThemesPreviewViewModel(themeDemoURL: "https://tsubakidemo.wpcomstaging.com/")
+        let viewModel = ThemesPreviewViewModel(siteID: 123,
+                                               mode: .storeCreationProfiler,
+                                               theme: .init(id: "123",
+                                                            description: "Woo Theme",
+                                                            name: "Woo",
+                                                            demoURI: "https://tsubakidemo.wpcomstaging.com/"))
 
         // Then
         XCTAssertEqual(viewModel.state, .pagesLoading)
@@ -27,7 +34,13 @@ final class ThemesPreviewViewModelTests: XCTestCase {
 
     func test_fetchPages_sets_the_right_pages_and_state() async {
         // Given
-        let viewModel = ThemesPreviewViewModel(themeDemoURL: "https://tsubakidemo.wpcomstaging.com/", stores: stores)
+        let viewModel = ThemesPreviewViewModel(siteID: 123,
+                                               mode: .storeCreationProfiler,
+                                               theme: .init(id: "123",
+                                                            description: "Woo Theme",
+                                                            name: "Woo",
+                                                            demoURI: "https://tsubakidemo.wpcomstaging.com/"),
+                                               stores: stores)
 
         let expectedPages = [
             WordPressPage(id: 1, title: "Page1", link: "https://tsubakidemo.wpcomstaging.com/page1"),
@@ -53,7 +66,12 @@ final class ThemesPreviewViewModelTests: XCTestCase {
 
     func test_setSelectedPage() {
         // Given
-        let viewModel = ThemesPreviewViewModel(themeDemoURL: "testURL")
+        let viewModel = ThemesPreviewViewModel(siteID: 123,
+                                               mode: .storeCreationProfiler,
+                                               theme: .init(id: "123",
+                                                            description: "Woo Theme",
+                                                            name: "Woo",
+                                                            demoURI: "testURL"))
         let page = WordPressPage(id: 1, title: "Page1", link: "testURL")
 
         // When
@@ -61,5 +79,169 @@ final class ThemesPreviewViewModelTests: XCTestCase {
 
         // Then
         XCTAssertEqual(viewModel.selectedPage, page)
+    }
+
+    // MARK: installingTheme
+
+    func test_installingTheme_is_initially_false() {
+        // Given
+        let viewModel = ThemesPreviewViewModel(siteID: 123,
+                                               mode: .storeCreationProfiler,
+                                               theme: .init(id: "123",
+                                                            description: "Woo Theme",
+                                                            name: "Woo",
+                                                            demoURI: "https://tsubakidemo.wpcomstaging.com/"))
+
+        // Then
+        XCTAssertFalse(viewModel.installingTheme)
+    }
+
+    func test_installingTheme_is_updated_properly__when_installing_theme_is_successful() async throws {
+        // Given
+        let themeInstaller = MockThemeInstaller()
+        let viewModel = ThemesPreviewViewModel(siteID: 123,
+                                               mode: .themeSettings,
+                                               theme: .init(id: "123",
+                                                            description: "Woo Theme",
+                                                            name: "Woo",
+                                                            demoURI: "https://tsubakidemo.wpcomstaging.com/"),
+                                               stores: stores,
+                                               themeInstaller: themeInstaller)
+        var loadingStates = [Bool]()
+        viewModel.$installingTheme
+            .dropFirst() // Initial value
+            .collect(2)  // Collect toggle
+            .first()
+            .sink { states in
+                loadingStates = states
+            }
+            .store(in: &self.subscriptions)
+
+        // When
+        try await viewModel.installTheme()
+
+        // Then
+        assertEqual(loadingStates, [true, false])
+    }
+
+    func test_installingTheme_is_updated_properly_when_installing_theme_fails() async {
+        // Given
+        let themeInstaller = MockThemeInstaller()
+        themeInstaller.installThemeError = MockError()
+        let viewModel = ThemesPreviewViewModel(siteID: 123,
+                                               mode: .themeSettings,
+                                               theme: .init(id: "123",
+                                                            description: "Woo Theme",
+                                                            name: "Woo",
+                                                            demoURI: "https://tsubakidemo.wpcomstaging.com/"),
+                                               stores: stores,
+                                               themeInstaller: themeInstaller)
+        var loadingStates = [Bool]()
+        viewModel.$installingTheme
+            .dropFirst() // Initial value
+            .collect(2)  // Collect toggle
+            .first()
+            .sink { states in
+                loadingStates = states
+            }
+            .store(in: &self.subscriptions)
+
+        // When
+        do {
+            try await viewModel.installTheme()
+            XCTFail("Installing theme should fail")
+        } catch {
+            // Then
+            assertEqual(loadingStates, [true, false])
+        }
+    }
+
+    // MARK: notice
+
+    func test_notice_is_initially_false() {
+        // Given
+        let viewModel = ThemesPreviewViewModel(siteID: 123,
+                                               mode: .storeCreationProfiler,
+                                               theme: .init(id: "123",
+                                                            description: "Woo Theme",
+                                                            name: "Woo",
+                                                            demoURI: "https://tsubakidemo.wpcomstaging.com/"),
+                                               stores: stores)
+
+        // Then
+        XCTAssertNil(viewModel.notice)
+    }
+
+    func test_notice_is_not_nil_when_installing_theme_fails() async {
+        // Given
+        let themeInstaller = MockThemeInstaller()
+        themeInstaller.installThemeError = MockError()
+        let viewModel = ThemesPreviewViewModel(siteID: 123,
+                                               mode: .themeSettings,
+                                               theme: .init(id: "123",
+                                                            description: "Woo Theme",
+                                                            name: "Woo",
+                                                            demoURI: "https://tsubakidemo.wpcomstaging.com/"),
+                                               stores: stores,
+                                               themeInstaller: themeInstaller)
+
+        // When
+        do {
+            try await viewModel.installTheme()
+            XCTFail("Installing theme should fail")
+        } catch {
+            // Then
+            XCTAssertNotNil(viewModel.notice)
+        }
+    }
+
+    // MARK: Theme installation
+
+    func test_it_installs_theme_when_mode_is_themeSettings() async throws {
+        // Given
+        let themeInstaller = MockThemeInstaller()
+        let viewModel = ThemesPreviewViewModel(siteID: 123,
+                                               mode: .themeSettings,
+                                               theme: .init(id: "123",
+                                                            description: "Woo Theme",
+                                                            name: "Woo",
+                                                            demoURI: "https://tsubakidemo.wpcomstaging.com/"),
+                                               stores: stores,
+                                               themeInstaller: themeInstaller)
+
+        // When
+        try await viewModel.installTheme()
+
+        // Then
+        XCTAssertTrue(themeInstaller.installThemeCalled)
+        XCTAssertEqual(themeInstaller.installThemeCalledForSiteID, 123)
+        XCTAssertEqual(themeInstaller.installThemeCalledWithThemeID, "123")
+    }
+
+    func test_it_does_not_install_theme_when_mode_is_storeCreationProfiler() async throws {
+        // Given
+        let themeInstaller = MockThemeInstaller()
+        let viewModel = ThemesPreviewViewModel(siteID: 123,
+                                               mode: .storeCreationProfiler,
+                                               theme: .init(id: "123",
+                                                            description: "Woo Theme",
+                                                            name: "Woo",
+                                                            demoURI: "https://tsubakidemo.wpcomstaging.com/"),
+                                               stores: stores,
+                                               themeInstaller: themeInstaller)
+
+        // When
+        try await viewModel.installTheme()
+
+        // Then
+        XCTAssertFalse(themeInstaller.installThemeCalled)
+    }
+}
+
+private extension ThemesPreviewViewModelTests {
+    final class MockError: Error {
+        var localizedDescription: String {
+            "description"
+        }
     }
 }

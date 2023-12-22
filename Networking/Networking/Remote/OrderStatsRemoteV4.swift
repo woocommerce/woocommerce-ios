e4040d544e1b49326f682410a8f0fd13177478ac
Jaclyn Chen <watertranquil@gmail.com>
2023-12-22 12:51:06 +0800

    Update comment to mention the WC version that includes the parameter support.

--- Networking/Networking/Remote/OrderStatsRemoteV4.swift
+++ Networking/Networking/Remote/OrderStatsRemoteV4.swift
@@ -36,7 +36,7 @@ public final class OrderStatsRemoteV4: Remote {
             ParameterKeys.forceRefresh: forceRefresh,
             // Product stats in `ProductsReportsRemote.loadTopProductsReport` are based on the order creation date, while the order/revenue
             // stats are based on a store option in the analytics settings with the order paid date as the default.
-            // In a later WC version, a new parameter `date_type` is available to override the date type so that we can
+            // In WC version 8.6+, a new parameter `date_type` is available to override the date type so that we can
             // show the order/revenue and product stats based on the same date column, order creation date.
             ParameterKeys.dateType: ParameterValues.dateType,
             ParameterKeys.fields: ParameterValues.fieldValues

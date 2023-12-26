b5d15c237885f753ee06a0ea64f87bd815c117e9
Hafiz Rahman <hfzrhmn@gmail.com>
2023-12-26 16:41:29 +0700

    Make celebration bottom sheet appear in product form once a Blaze campaign is created.

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewController.swift
@@ -48,6 +48,7 @@ final class ProductFormViewController<ViewModel: ProductFormViewModelProtocol>: 
     private let productUIImageLoader: ProductUIImageLoader
     private let productImageUploader: ProductImageUploaderProtocol
     private var tooltipPresenter: TooltipPresenter?
+    private var productFormBottomSheetPresenter: BottomSheetPresenter?
 
     private let currency: String
 
@@ -1142,6 +1143,20 @@ private extension ProductFormViewController {
 
     func handlePostCreation() {
         navigationController?.popViewController(animated: true)
+        showBlazeCreationCelebrationView()
+    }
+
+    func showBlazeCreationCelebrationView() {
+        productFormBottomSheetPresenter = buildBottomSheetPresenter()
+        let controller = CelebrationHostingController(
+            title: Localization.Blaze.celebrationTitle,
+            subtitle: Localization.Blaze.celebrationSubtitle,
+            closeButtonTitle: Localization.Blaze.celebrationCTA,
+            onTappingDone: { [weak self] in
+            self?.productFormBottomSheetPresenter?.dismiss()
+            self?.productFormBottomSheetPresenter = nil
+        })
+        productFormBottomSheetPresenter?.present(controller, from: self)
     }
 
     func trackVariationRemoveButtonTapped() {
@@ -1971,6 +1986,22 @@ private extension ProductFormViewController {
     }
 }
 
+
+// MARK: Bottom sheet helpers
+//
+private extension ProductFormViewController {
+    func buildBottomSheetPresenter() -> BottomSheetPresenter {
+        BottomSheetPresenter(configure: { bottomSheet in
+            var sheet = bottomSheet
+            sheet.prefersEdgeAttachedInCompactHeight = true
+            sheet.largestUndimmedDetentIdentifier = .none
+            sheet.prefersGrabberVisible = true
+            sheet.detents = [.medium()]
+        })
+    }
+}
+
+
 // MARK: Constants
 //
 private enum Localization {
@@ -2000,6 +2031,24 @@ private enum Localization {
                                                comment: "The message for the Write with AI tooltip")
         static let gotIt = NSLocalizedString("Got it",
                                              comment: "Button title that dismisses the Write with AI tooltip")
+    }
+
+    enum Blaze {
+        static let celebrationTitle = NSLocalizedString(
+            "productFormViewController.blazeCelebrationTitle",
+            value: "All set!",
+            comment: "Title of the celebration view when a Blaze campaign is successfully created."
+        )
+        static let celebrationSubtitle = NSLocalizedString(
+            "productFormViewController.blazeCelebrationSubtitle",
+            value: "The ad has been submitted for approval. We'll send you a confirmation email once it's approved and running.",
+            comment: "Subtitle of the celebration view when a Blaze campaign is successfully created."
+        )
+        static let celebrationCTA = NSLocalizedString(
+            "productFormViewController.blazeCelebrationCTA",
+            value: "Got it",
+            comment: "Button to dismiss the celebration view when a Blaze campaign is successfully created."
+        )
     }
 }
 

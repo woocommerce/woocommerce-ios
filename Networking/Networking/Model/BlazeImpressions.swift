a198c1215fbb48f20ca59b31fc9f514c44f8cd20
Hafiz Rahman <hfzrhmn@gmail.com>
2023-12-27 14:00:34 +0700

    Model file for Blaze impressions.

---
+++ Networking/Networking/Model/BlazeImpressions.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+/// Impressions forecast for a Blaze campaign
+///
+public struct BlazeImpressions: Decodable, Equatable {
+
+    /// Minimum forecasted impressions a Blaze campaign might get
+    public let totalImpressionsMin: Int64
+
+    /// Maximum forecasted impressions a Blaze campaign might get
+    public let totalImpressionsMax: Int64
+
+    public init(totalImpressionsMin: Int64, totalImpressionsMax: Int64) {
+        self.totalImpressionsMin = totalImpressionsMin
+        self.totalImpressionsMax = totalImpressionsMax
+    }
+}

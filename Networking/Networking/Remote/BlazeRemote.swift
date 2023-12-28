64329ce52f2a5a6ee4814b519d39e8caba1ea029
Hafiz Rahman <hfzrhmn@gmail.com>
2023-12-28 12:05:56 +0700

    Add fetchForecastedImpressions to Blaze Response

--- Networking/Networking/Remote/BlazeRemote.swift
+++ Networking/Networking/Remote/BlazeRemote.swift
@@ -34,6 +34,26 @@ public protocol BlazeRemoteProtocol {
     ///    - query: Keyword to search for locations. Requires a minimum of 3 characters, or else will return error.
     ///
     func fetchTargetLocations(for siteID: Int64, query: String) async throws -> [BlazeTargetLocation]
+
+
+    /// Fetches forecasted campaign impressions.
+    /// - Parameters:
+    ///    - siteID: WPCom ID for the site to create ads campaigns.
+    ///    - startDate: Start date of the campaign.
+    ///    - endDate: End date of the campaign.
+    ///    - formattedTotalBudget: Formatted string of total budget of the campaign.
+    ///    - targetings: Targetings of the campaign.
+    ///
+    func fetchForecastedImpressions(
+        for siteID: Int64,
+        from startDate: Date,
+        to endDate: Date,
+        formattedTotalBudget: String,
+        targetLocations: [BlazeTargetLocation],
+        targetLanguages: [BlazeTargetLanguage],
+        targetDevices: [BlazeTargetDevice],
+        targetTopics: [BlazeTargetTopic]
+    ) async throws -> BlazeImpressions
 }
 
 /// Blaze: Remote Endpoints
@@ -87,6 +107,40 @@ public final class BlazeRemote: Remote, BlazeRemoteProtocol {
         let mapper = BlazeTargetLocationListMapper()
         return try await enqueue(request, mapper: mapper)
     }
+
+    /// Fetches forecasted campaign impressions.
+    ///
+    public func fetchForecastedImpressions(
+        for siteID: Int64,
+        from startDate: Date,
+        to endDate: Date,
+        formattedTotalBudget: String,
+        targetLocations: [BlazeTargetLocation] = [],
+        targetLanguages: [BlazeTargetLanguage] = [],
+        targetDevices: [BlazeTargetDevice] = [],
+        targetTopics: [BlazeTargetTopic]  = []
+    ) async throws -> BlazeImpressions {
+        let path = Paths.campaignImpressions(siteID: siteID)
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+
+        let parameters: [String: Any] = [
+            Keys.startDate: dateFormatter.string(from: startDate),
+            Keys.endDate: dateFormatter.string(from: endDate),
+            Keys.totalBudget: formattedTotalBudget,
+            Keys.targetings: [
+                Keys.targetLocations: targetLocations.map { $0.id },
+                Keys.targetLanguages: targetLanguages.map { $0.id },
+                Keys.targetDevices: targetDevices.map { $0.id },
+                Keys.targetTopics: targetTopics.map { $0.id }
+            ] as [String : [Any]]
+        ]
+
+        let request = DotcomRequest(wordpressApiVersion: .wpcomMark2, method: .post, path: path, parameters: parameters)
+        let mapper = BlazeImpressionsMapper()
+        return try await enqueue(request, mapper: mapper)
+    }
 }
 
 private extension BlazeRemote {
@@ -118,12 +172,24 @@ private extension BlazeRemote {
         static func campaignSearch(siteID: Int64) -> String {
             "sites/\(siteID)/wordads/dsp/api/v1/search/campaigns/site/\(siteID)"
         }
+
+        static func campaignImpressions(siteID: Int64) -> String {
+            "sites/\(siteID)/wordads/dsp/api/v1.1/forecast"
+        }
     }
 
     enum Keys {
         static let orderBy = "order_by"
         static let order =  "order"
         static let page = "page"
+        static let startDate = "start_date"
+        static let endDate = "end_date"
+        static let totalBudget = "total_budget"
+        static let targetings = "targetings"
+        static let targetLocations = "locations"
+        static let targetLanguages = "languages"
+        static let targetDevices = "devices"
+        static let targetTopics = "page _topics"
     }
 
     enum Values {

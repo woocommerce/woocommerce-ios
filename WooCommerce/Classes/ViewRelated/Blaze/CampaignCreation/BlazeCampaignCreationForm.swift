edad6d70e7820861ba502a75093329f18c452d35
Huong Do <hi.huongdo@gmail.com>
2024-01-02 13:56:39 +0700

    Add new file for Blaze campaign creation form

---
+++ WooCommerce/Classes/ViewRelated/Blaze/CampaignCreation/BlazeCampaignCreationForm.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct BlazeCampaignCreationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlazeCampaignCreationForm()
+}

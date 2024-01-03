1e9ebbbc1dfe0841ffabef57e7310c77919b4592
Huong Do <hi.huongdo@gmail.com>
2024-01-03 10:58:35 +0700

    Add file for BlazeBudgetSettingView

---
+++ WooCommerce/Classes/ViewRelated/Blaze/BudgetSetting/BlazeBudgetSettingView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+/// View to set budget for a new Blaze campaign
+struct BlazeBudgetSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlazeBudgetSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlazeBudgetSettingView()
+    }
+}

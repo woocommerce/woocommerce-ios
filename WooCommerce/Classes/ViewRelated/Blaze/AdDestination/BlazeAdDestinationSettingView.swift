4f5a811f273752d3a34d0caba89bf676cc3c6e0b
Hafiz Rahman <hfzrhmn@gmail.com>
2024-01-04 19:36:25 +0700

    Add toolbar.

--- WooCommerce/Classes/ViewRelated/Blaze/AdDestination/BlazeAdDestinationSettingView.swift
+++ WooCommerce/Classes/ViewRelated/Blaze/AdDestination/BlazeAdDestinationSettingView.swift
@@ -2,8 +2,53 @@ import SwiftUI
 
 /// View to set ad destination for a new Blaze campaign
 struct BlazeAdDestinationSettingView: View {
+    @Environment(\.dismiss) private var dismiss
+
     var body: some View {
-        Text("Blaze ad  destination setting view")
+        NavigationView {
+            ScrollableVStack(alignment: .leading) {
+                // todo add content:
+                // -----------------
+
+                // Destination URL section
+                // - small heading
+                // - two rows (each row has a selection, only 1 can be selected at a time)
+                //   - The product URL with label text below
+                //   - The site home
+
+                // URL parameters section
+                // - small heading
+                // - foreach row of added parameters
+                // - + Add parameter button
+                // - character count label
+                // - Final URL destination preview
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle(Localization.adDestination)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button(Localization.cancel) {
+                        dismiss()
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+private extension BlazeAdDestinationSettingView {
+    enum Localization {
+        static let cancel = NSLocalizedString(
+            "blazeAdDestinationSettingView.cancel",
+            value: "Cancel",
+            comment: "Button to dismiss the Blaze Ad Destination setting screen"
+        )
+        static let adDestination = NSLocalizedString(
+            "blazeAdDestinationSettingView.adDestination",
+            value: "Ad Destination",
+            comment: "Title of the Blaze Ad Destination setting screen."
+        )
     }
 }
 

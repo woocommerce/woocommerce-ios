0fe0ad9ffb15fec22247742e99d64b1e8ecd173a
Hafiz Rahman <hfzrhmn@gmail.com>
2024-01-04 18:04:30 +0700

    Start BlazeAdDestinationSettingView file

---
+++ WooCommerce/Classes/ViewRelated/Blaze/AdDestination/BlazeAdDestinationSettingView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+/// View to set ad destination for a new Blaze campaign
+struct BlazeAdDestinationSettingView: View {
+    var body: some View {
+        Text("Blaze ad  destination setting view")
+    }
+}
+
+struct BlazeAdDestinationSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlazeAdDestinationSettingView()
+    }
+}

9d30f7fdb48d69f52b344c46fc85ea9d9a1d292a
Hafiz Rahman <hfzrhmn@gmail.com>
2024-01-05 14:45:43 +0700

    Add basic layout with static content.

--- WooCommerce/Classes/ViewRelated/Blaze/AdDestination/BlazeAdDestinationSettingView.swift
+++ WooCommerce/Classes/ViewRelated/Blaze/AdDestination/BlazeAdDestinationSettingView.swift
@@ -6,23 +6,33 @@ struct BlazeAdDestinationSettingView: View {
 
     var body: some View {
         NavigationView {
-            ScrollableVStack(alignment: .leading) {
-                // todo add content:
-                // -----------------
+            VStack(alignment: .leading, spacing: Layout.verticalSpacing) {
+                Text("Destination URL")
 
-                // Destination URL section
-                // - small heading
-                // - two rows (each row has a selection, only 1 can be selected at a time)
-                //   - The product URL with label text below
-                //   - The site home
+                destinationItem(title: "The Product URL",
+                                subtitle: "It will link to https://woo.com/",
+                                showCheckmark: true,
+                                showBottomDivider: true)
 
-                // URL parameters section
-                // - small heading
-                // - foreach row of added parameters
-                // - + Add parameter button
-                // - character count label
-                // - Final URL destination preview
+                destinationItem(title: "The site home",
+                                subtitle: "It will link to https://woo.com/")
+
+                Text("URL Parameters")
+
+                parameterItem(itemName: "specialpromo")
+
+                Button("Add parameter") {
+                    // todo
+                }
+                .buttonStyle(PlusButtonStyle())
+
+                Text("2096 characters remaining")
+
+                Text("Destination: http://woo.com/")
+
+                Spacer()
             }
+            .background(Color(.systemBackground))
             .navigationBarTitleDisplayMode(.inline)
             .navigationTitle(Localization.adDestination)
             .toolbar {
@@ -31,13 +41,59 @@ struct BlazeAdDestinationSettingView: View {
                         dismiss()
                     }
                 }
-
             }
+        }
+    }
+
+    @ViewBuilder
+    private func destinationItem(title: String,
+                                 subtitle: String,
+                                 showCheckmark: Bool = false,
+                                 showBottomDivider: Bool = false) -> some View {
+        HStack(alignment: .center, spacing: Layout.contentSpacing) {
+            if showCheckmark {
+                Image(systemName: "checkmark")
+                    .padding(.leading, Layout.contentSpacing)
+            } else {
+                Image(systemName: "checkmark")
+                    .hidden() // Small hack to make the icon space consistent while not showing the icon.
+                    .padding(.leading, Layout.contentSpacing)
+            }
+
+            VStack(alignment: .leading, spacing: Layout.contentSpacing) {
+                Text(title)
+                    .font(.body)
+                    .fontWeight(.bold)
+
+                Text(subtitle)
+                    .font(.caption)
+
+                if showBottomDivider {
+                    Divider()
+                }
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func parameterItem(itemName: String)  -> some View {
+        VStack {
+            HStack {
+                Text(itemName)
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            Divider()
         }
     }
 }
 
 private extension BlazeAdDestinationSettingView {
+    enum Layout {
+        static let verticalSpacing: CGFloat = 16
+        static let contentSpacing: CGFloat = 16
+    }
+
     enum Localization {
         static let cancel = NSLocalizedString(
             "blazeAdDestinationSettingView.cancel",

3ee26a06436057e2ec8390cd171006c4c1f40c79
Hafiz Rahman <hfzrhmn@gmail.com>
2024-01-09 16:22:47 +0700

    Toolbar area for Blaze Add Parameter.

--- WooCommerce/Classes/ViewRelated/Blaze/AdDestination/BlazeAddParameterView.swift
+++ WooCommerce/Classes/ViewRelated/Blaze/AdDestination/BlazeAddParameterView.swift
@@ -5,18 +5,30 @@ final class BlazeAddParameterViewModel: ObservableObject {
     @Published var value: String = ""
 
     let remainingCharacters: Int
+    let isFirstParameter: Bool
+
+    var totalInputLength: Int {
+        (isFirstParameter ? 0 : "&".count) + key.count + "=".count + value.count
+    }
+
+    var shouldDisableSaveButton: Bool {
+        key.isEmpty || value.isEmpty || remainingCharacters - totalInputLength <= 0
+    }
+
 
     typealias BlazeAddParameterCompletionHandler = (_ key: String, _ value: String) -> Void
     private let completionHandler: BlazeAddParameterCompletionHandler
 
     init(remainingCharacters: Int,
+         isFirstParameter: Bool = true,
          onCompletion: @escaping BlazeAddParameterCompletionHandler) {
         self.remainingCharacters = remainingCharacters
+        self.isFirstParameter = isFirstParameter
         self.completionHandler = onCompletion
     }
 
     // todo: use this in view
-    func onSave() {
+    func didTapSave() {
         completionHandler(key, value)
     }
 }
@@ -25,6 +37,8 @@ final class BlazeAddParameterViewModel: ObservableObject {
 /// View for adding a parameter to a Blaze campaign's URL.
 ///
 struct BlazeAddParameterView: View {
+    @Environment(\.dismiss) private var dismiss
+
     @ObservedObject private var viewModel: BlazeAddParameterViewModel
 
 
@@ -34,12 +48,56 @@ struct BlazeAddParameterView: View {
 
 
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                Text("Hello World")
+            }
+            .navigationTitle(Localization.title)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button(Localization.cancel) {
+                        dismiss()
+                    }
+                    .foregroundColor(Color(uiColor: .accent))
+                }
+
+                ToolbarItem(placement: .primaryAction) {
+                    Button(Localization.save) {
+                        viewModel.didTapSave()
+                    }
+                    .disabled(viewModel.shouldDisableSaveButton)
+                }
+            }
+        }
     }
 }
 
 struct BlazeAddParameterView_Previews: PreviewProvider {
     static var previews: some View {
         BlazeAddParameterView(viewModel: BlazeAddParameterViewModel(remainingCharacters: 999) { _, _ in })
+    }
+}
+
+
+private extension BlazeAddParameterView {
+    enum Localization {
+        static let cancel = NSLocalizedString(
+            "blazeAddParameterView.cancel",
+            value: "Cancel",
+            comment: "Button to dismiss the Blaze Add Parameter screen"
+        )
+
+        static let title = NSLocalizedString(
+            "blazeAddParameterView.title",
+            value: "Add Parameter",
+            comment: "Title of the Blaze Add Parameter screen"
+        )
+
+        static let save = NSLocalizedString(
+            "blazeAddParameterView.save",
+            value: "Save",
+            comment: "Button to save on the Blaze Add Parameter screen"
+        )
     }
 }

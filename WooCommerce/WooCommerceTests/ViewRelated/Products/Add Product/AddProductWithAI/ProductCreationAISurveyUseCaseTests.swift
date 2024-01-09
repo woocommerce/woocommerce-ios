06a6ad414730c7bc6bc4f610fba9d99bd9aa1430
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-01-09 10:51:50 +0530

    Unit test that survey use case works as expected.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Add Product/AddProductWithAI/ProductCreationAISurveyUseCaseTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Add Product/AddProductWithAI/ProductCreationAISurveyUseCaseTests.swift
@@ -2,48 +2,58 @@ import XCTest
 @testable import WooCommerce
 
 final class ProductCreationAISurveyUseCaseTests: XCTestCase {
-    func test_shouldShowProductCreationAISurvey_is_false_when_number_of_AI_generation_is_less_than_3() throws {
+    func test_shouldShowProductCreationAISurvey_is_false_true_if_survey_not_displayed_before() throws {
         // Given
         let uuid = UUID().uuidString
         let defaults = try XCTUnwrap(UserDefaults(suiteName: uuid))
 
         // When
         let sut = ProductCreationAISurveyUseCase(defaults: defaults)
-        sut.didCreateAIProduct()
-        sut.didCreateAIProduct()
 
         // Then
         XCTAssertFalse(sut.shouldShowProductCreationAISurvey())
     }
 
-    func test_it_asks_to_show_survey_when_number_of_AI_generation_is_greater_than_3() throws {
+    func test_it_asks_to_show_survey_if_user_dismissed_first_time() throws {
         // Given
         let uuid = UUID().uuidString
         let defaults = try XCTUnwrap(UserDefaults(suiteName: uuid))
 
         // When
         let sut = ProductCreationAISurveyUseCase(defaults: defaults)
-        sut.didCreateAIProduct()
-        sut.didCreateAIProduct()
-        sut.didCreateAIProduct()
-        sut.didCreateAIProduct()
+        XCTAssertTrue(sut.shouldShowProductCreationAISurvey())
+
+        sut.didSuggestProductCreationAISurvey()
 
         // Then
         XCTAssertTrue(sut.shouldShowProductCreationAISurvey())
     }
 
-    func test_it_asks_to_not_show_survey_when_number_of_AI_generation_is_greater_than_3_but_we_asked_confirmation_already() throws {
+    func test_it_asks_to_not_show_survey_if_user_dismissed_second_time() throws {
         // Given
         let uuid = UUID().uuidString
         let defaults = try XCTUnwrap(UserDefaults(suiteName: uuid))
 
         // When
         let sut = ProductCreationAISurveyUseCase(defaults: defaults)
-        defaults.set(true, forKey: UserDefaults.Key.didSuggestProductCreationAISurvey.rawValue)
-        sut.didCreateAIProduct()
-        sut.didCreateAIProduct()
-        sut.didCreateAIProduct()
-        sut.didCreateAIProduct()
+        XCTAssertTrue(sut.shouldShowProductCreationAISurvey())
+
+        sut.didSuggestProductCreationAISurvey()
+        sut.didSuggestProductCreationAISurvey()
+
+        // Then
+        XCTAssertFalse(sut.shouldShowProductCreationAISurvey())
+    }
+
+    func test_it_asks_to_not_show_survey_if_user_started_survey_already() throws {
+        // Given
+        let uuid = UUID().uuidString
+        let defaults = try XCTUnwrap(UserDefaults(suiteName: uuid))
+        let sut = ProductCreationAISurveyUseCase(defaults: defaults)
+        defaults.set(true, forKey: UserDefaults.Key.didStartProductCreationAISurvey.rawValue)
+
+        // When
+        sut.didStartProductCreationAISurvey()
 
         // Then
         XCTAssertFalse(sut.shouldShowProductCreationAISurvey())
@@ -63,18 +73,5 @@ final class ProductCreationAISurveyUseCaseTests: XCTestCase {
 
         // Then
         XCTAssertNotNil(analyticsProvider.receivedEvents.first(where: { $0 == "product_creation_ai_survey_confirmation_view_displayed" }))
-    }
-
-    func test_it_saves_asked_confirmation_value_to_defaults() throws {
-        // Given
-        let uuid = UUID().uuidString
-        let defaults = try XCTUnwrap(UserDefaults(suiteName: uuid))
-        let sut = ProductCreationAISurveyUseCase(defaults: defaults)
-
-        // When
-        sut.didSuggestProductCreationAISurvey()
-
-        // Then
-        XCTAssertEqual(try XCTUnwrap(defaults.bool(forKey: UserDefaults.Key.didSuggestProductCreationAISurvey.rawValue)), true)
     }
 }

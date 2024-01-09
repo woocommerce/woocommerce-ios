0d22a1e00c06b6eafa7506f9af80645e26068ace
Huong Do <hi.huongdo@gmail.com>
2024-01-09 12:52:07 +0700

    Add BlazeTargetDevicePickerViewModel

---
+++ WooCommerce/Classes/ViewRelated/Blaze/TargetDevices/BlazeTargetDevicePickerViewModel.swift
@@ -0,0 +1,62 @@
+import Foundation
+import Yosemite
+import protocol Storage.StorageManagerType
+
+/// View model for `BlazeTargetDevicePicker`
+final class BlazeTargetDevicePickerViewModel: ObservableObject {
+
+    @Published private(set) var devices: [BlazeTargetDevice] = []
+
+    /// Blaze target device ResultsController.
+    private lazy var resultsController: ResultsController<StorageBlazeTargetDevice> = {
+        let predicate = NSPredicate(format: "locale == %@", locale.identifier)
+        let sortDescriptorByID = NSSortDescriptor(keyPath: \StorageBlazeTargetDevice.id, ascending: true)
+        let resultsController = ResultsController<StorageBlazeTargetDevice>(storageManager: storageManager,
+                                                                            matching: predicate,
+                                                                            sortedBy: [sortDescriptorByID])
+        return resultsController
+    }()
+
+    private let siteID: Int64
+    private let locale: Locale
+    private let stores: StoresManager
+    private let storageManager: StorageManagerType
+    private let onSelection: (Set<BlazeTargetDevice>?) -> Void
+
+    init(siteID: Int64,
+         locale: Locale = .current,
+         stores: StoresManager = ServiceLocator.stores,
+         storageManager: StorageManagerType = ServiceLocator.storageManager,
+         onSelection: @escaping (Set<BlazeTargetDevice>?) -> Void) {
+        self.siteID = siteID
+        self.locale = locale
+        self.stores = stores
+        self.storageManager = storageManager
+        self.onSelection = onSelection
+
+        configureResultsController()
+    }
+}
+
+private extension BlazeTargetDevicePickerViewModel {
+    /// Performs initial fetch from storage and updates results.
+    func configureResultsController() {
+        resultsController.onDidChangeContent = { [weak self] in
+            self?.updateResults()
+        }
+        resultsController.onDidResetContent = { [weak self] in
+            self?.updateResults()
+        }
+
+        do {
+            try resultsController.performFetch()
+            updateResults()
+        } catch {
+            ServiceLocator.crashLogging.logError(error)
+        }
+    }
+
+    func updateResults() {
+        devices = resultsController.fetchedObjects
+    }
+}

a7f72a7ea2f24bbdafa818f69aa57f4cdf0d11af
Hafiz Rahman <hfzrhmn@gmail.com>
2024-01-10 14:41:40 +0700

    Improve type documentation.

--- WooCommerce/Classes/ViewRelated/Blaze/AdDestination/BlazeAdUrlParameter.swift
+++ WooCommerce/Classes/ViewRelated/Blaze/AdDestination/BlazeAdUrlParameter.swift
@@ -1,11 +1,13 @@
 import Foundation
 
+/// A Blaze campaign's custom URL parameter. By default a campaign can link to a product URL,
+/// or to home page URL. Optionally it can also have one or more URL parameters added to the link.
 struct BlazeAdURLParameter: Equatable, Hashable {
     let key: String
     let value: String
 }
 
-/// Converts a [BlazeAdURLParameter] to a query string (or empty string if the array is empty).
+/// Converts a `[BlazeAdURLParameter]` into a URL query parameter string (or empty string if the array is empty).
 /// Parameter string should be in a format of "key=value&key2=value2&key3=value3"
 extension Array where Element == BlazeAdURLParameter {
     func convertToQueryString() -> String {

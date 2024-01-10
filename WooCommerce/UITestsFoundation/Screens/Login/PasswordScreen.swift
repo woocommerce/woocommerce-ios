31dfd189c10d4261a1f5bd87786a69837b5b6973
Gio Lodi <gio@mokacoding.com>
2024-01-10 14:15:59 +1100

    Add an extra wait for button before attempting to hit "Not Now"
    See how this build failed
    https://buildkite.com/automattic/woocommerce-ios/builds/18711#018cf0ff-49d1-4a8e-8d44-dac6bacc01dd

--- WooCommerce/UITestsFoundation/Screens/Login/PasswordScreen.swift
+++ WooCommerce/UITestsFoundation/Screens/Login/PasswordScreen.swift
@@ -53,7 +53,10 @@ public final class PasswordScreen: ScreenObject {
         if app.buttons["Save Password"].waitForExistence(timeout: 15) {
             // There should be no need to wait for this button to exist since it's part of the same
             // alert where "Save Password" is.
-            app.buttons["Not Now"].tap()
+            let dismissButton = app.buttons["Not Now"]
+            // Additionally wait for existence of the button to account for animations, even though the test runner should wait for the app to idle before moving on.
+            XCTAssertTrue(dismissButton.waitForExistence(timeout: 2))
+            dismissButton.tap()
         }
     }
 

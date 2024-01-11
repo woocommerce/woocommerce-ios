ee1579541ec6a97be1dfdef5800f21ea6f868d01
Hafiz Rahman <hfzrhmn@gmail.com>
2024-01-11 17:15:41 +0700

    Improve readability for the adjustedRemainingCharacters logic.
    Co-authored-by: Huong Do <hi.huongdo@gmail.com>

--- WooCommerce/Classes/ViewRelated/Blaze/AdDestination/BlazeAdDestinationSettingViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Blaze/AdDestination/BlazeAdDestinationSettingViewModel.swift
@@ -36,10 +36,13 @@ final class BlazeAdDestinationSettingViewModel: ObservableObject {
         // The remaining characters to be used for validation in `BlazeAddParameterViewModel.
         // If more than 1 parameter exist and user is adding a new character, the value should be reduced further by 1.
         // This to take into account the need to add "&" separator character to the new parameter.
-        var adjustedRemainingCharacters = calculateRemainingCharacters()
-        if parameters.count > 1 && selectedParameter == nil {
-            adjustedRemainingCharacters = calculateRemainingCharacters() - 1
-        }
+        let adjustedRemainingCharacters: Int = {
+            let remainingCharacters = calculateRemainingCharacters()
+            if parameters.count > 1 && selectedParameter == nil {
+                return remainingCharacters - 1
+            }
+            return remainingCharacters
+        }()
 
         return BlazeAddParameterViewModel(
             remainingCharacters: adjustedRemainingCharacters,

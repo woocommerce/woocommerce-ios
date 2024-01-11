03134715b463dca84bf6cbb82f558b926cf76401
Huong Do <hi.huongdo@gmail.com>
2024-01-11 15:06:53 +0700

    Add view and view model for target topic picker

---
+++ WooCommerce/Classes/ViewRelated/Blaze/TargetTopics/BlazeTargetTopicPickerView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+/// View for picking target devices for a Blaze campaign
+struct BlazeTargetTopicPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlazeTargetTopicPickerView()
+}
---
+++ WooCommerce/Classes/ViewRelated/Blaze/TargetTopics/BlazeTargetTopicPickerViewModel.swift
@@ -0,0 +1,7 @@
+import Foundation
+import Yosemite
+
+/// View model for `BlazeTargetTopicPickerView`
+final class BlazeTargetTopicPickerViewModel {
+
+}

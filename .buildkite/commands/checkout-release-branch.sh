47dc9987265330169963e7c8a3e237c4d4f6eca4
Spencer Transier <spencer.transier@automattic.com>
2024-01-12 12:56:48 -0800

    Add `BUILDKITE_` prefix to environment variables

--- .buildkite/commands/checkout-release-branch.sh
+++ .buildkite/commands/checkout-release-branch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -eu
 
-# RELEASE_VERSION is passed as an environment variable from fastlane to Buildkite
-#
-if [[ -z "${RELEASE_VERSION}" ]]; then
-    echo "RELEASE_VERSION is not set."
+# BUILDKITE_RELEASE_VERSION is passed as an environment variable from fastlane to Buildkite
+# It must use the `BUILDKITE_` prefix to be passed to the agent due to how `hostmgr` works.
+if [[ -z "${BUILDKITE_RELEASE_VERSION}" ]]; then
+    echo "BUILDKITE_RELEASE_VERSION is not set."
     exit 1
 fi
 
 # Buildkite, by default, checks out a specific commit. For many release actions, we need to be
 # on a release branch instead.
-BRANCH_NAME="release/${RELEASE_VERSION}"
+BRANCH_NAME="release/${BUILDKITE_RELEASE_VERSION}"
 git fetch origin "$BRANCH_NAME"
 git checkout "$BRANCH_NAME"

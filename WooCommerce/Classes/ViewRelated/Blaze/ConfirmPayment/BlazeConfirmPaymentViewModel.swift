46c2068b3b7e3efb08f39f44405da7ff5fd3e4d8
Huong Do <hi.huongdo@gmail.com>
2024-01-17 12:42:39 +0700

    Handle fetching selected payment method in BlazeConfirmPaymentViewModel

--- WooCommerce/Classes/ViewRelated/Blaze/ConfirmPayment/BlazeConfirmPaymentViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Blaze/ConfirmPayment/BlazeConfirmPaymentViewModel.swift
@@ -8,7 +8,15 @@ final class BlazeConfirmPaymentViewModel: ObservableObject {
     private let campaignInfo: CreateBlazeCampaign
     private let stores: StoresManager
 
+    private(set) var selectedPaymentMethod: BlazePaymentMethod?
+
     let totalAmount: String
+
+    @Published private(set) var isFetchingPaymentInfo = false
+    @Published private(set) var cardIcon: UIImage?
+    @Published private(set) var cardTypeName: String?
+    @Published private(set) var cardName: String?
+    @Published private(set) var fetchError: Error?
 
     init(siteID: Int64,
          campaignInfo: CreateBlazeCampaign,
@@ -17,5 +25,37 @@ final class BlazeConfirmPaymentViewModel: ObservableObject {
         self.campaignInfo = campaignInfo
         self.stores = stores
         self.totalAmount = "$\(campaignInfo.totalBudget)"
+    }
+
+    @MainActor
+    func updatePaymentInfo() async {
+        fetchError = nil
+        isFetchingPaymentInfo = true
+        do {
+            let info = try await fetchPaymentInfo()
+            selectedPaymentMethod = info.savedPaymentMethods.first
+            if let selectedPaymentMethod {
+                let rawCardType = selectedPaymentMethod.info.type
+                let cardType = CreditCardType(rawType: rawCardType)
+                cardIcon = cardType.icon
+                cardTypeName = selectedPaymentMethod.info.type
+                cardName = selectedPaymentMethod.name
+            }
+        } catch {
+            DDLogError("⛔️ Error fetching payment info for Blaze campaign creation: \(error)")
+            fetchError = error
+        }
+        isFetchingPaymentInfo = false
+    }
+}
+
+private extension BlazeConfirmPaymentViewModel {
+    @MainActor
+    func fetchPaymentInfo() async throws -> BlazePaymentInfo {
+        try await withCheckedThrowingContinuation { continuation in
+            stores.dispatch(BlazeAction.fetchPaymentInfo(siteID: siteID, onCompletion: { result in
+                continuation.resume(with: result)
+            }))
+        }
     }
 }

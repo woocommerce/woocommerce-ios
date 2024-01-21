841a77f3f47d8439cdd62c4956f532adfc0ab2b5
Hafiz Rahman <hfzrhmn@gmail.com>
2024-01-21 21:24:33 +0700

    Refactor unit tests to use UINavigationController directly.

--- WooCommerce/WooCommerceTests/ViewRelated/Blaze/BlazeCampaignCreationCoordinatorTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Blaze/BlazeCampaignCreationCoordinatorTests.swift
@@ -13,18 +13,26 @@ final class BlazeCampaignCreationCoordinatorTests: XCTestCase {
         storageManager.viewStorage
     }
 
+    private var navigationController: UINavigationController!
+
     override func setUp() {
         super.setUp()
         storageManager = MockStorageManager()
+
+        navigationController = UINavigationController()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIViewController()
+        window.makeKeyAndVisible()
+        window.rootViewController = navigationController
      }
 
      override func tearDown() {
          super.tearDown()
+         navigationController = nil
      }
 
     func test_webview_is_presented_when_blazei3NativeCampaignCreation_is_disabled() {
         // Given
-        let navigationController = MockSourceNavigationController()
         let featureFlagService = MockFeatureFlagService(blazei3NativeCampaignCreation: false)
         let sut = BlazeCampaignCreationCoordinator(siteID: 1,
                                                    siteURL: "https://woo.com/",
@@ -38,12 +46,11 @@ final class BlazeCampaignCreationCoordinatorTests: XCTestCase {
         sut.start()
 
         // Then
-        XCTAssertTrue(navigationController.shownViewControllers.first is AuthenticatedWebViewController)
+        XCTAssertTrue(sut.navigationController.viewControllers.first is AuthenticatedWebViewController)
     }
 
     func test_given_enabled_i3_featureflag_when_product_id_supplied_then_navigate_to_creation_form() {
         // Given
-        let navigationController = MockSourceNavigationController()
         let featureFlagService = MockFeatureFlagService(blazei3NativeCampaignCreation: true)
         let sut = BlazeCampaignCreationCoordinator(siteID: 1,
                                                    siteURL: "https://woo.com/",
@@ -58,12 +65,11 @@ final class BlazeCampaignCreationCoordinatorTests: XCTestCase {
         sut.start()
 
         // Then
-        XCTAssertTrue(navigationController.shownViewControllers[0] is BlazeCampaignCreationFormHostingController)
+        XCTAssertTrue(sut.navigationController.viewControllers.first is BlazeCampaignCreationFormHostingController)
     }
 
     func test_given_enabled_i3_featureflag_when_no_product_id_supplied_and_there_is_one_eligible_product_then_navigate_to_creation_form() {
         // Given
-        let navigationController = MockSourceNavigationController()
         let featureFlagService = MockFeatureFlagService(blazei3NativeCampaignCreation: true)
         insertProduct(.fake().copy(siteID: 1,
                                    productID: 1,
@@ -82,12 +88,11 @@ final class BlazeCampaignCreationCoordinatorTests: XCTestCase {
         sut.start()
 
         // Then
-        XCTAssertTrue(navigationController.shownViewControllers[0] is BlazeCampaignCreationFormHostingController)
+        XCTAssertTrue(sut.navigationController.viewControllers.first is BlazeCampaignCreationFormHostingController)
     }
 
     func test_given_enabled_i3_featureflag_when_no_product_id_supplied_and_there_are_multiple_eligible_products_then_navigate_to_product_selector() throws {
         // Given
-        let navigationController = MockSourceNavigationController()
         let featureFlagService = MockFeatureFlagService(blazei3NativeCampaignCreation: true)
         insertProduct(.fake().copy(siteID: 1,
                                    productID: 1,
@@ -112,7 +117,7 @@ final class BlazeCampaignCreationCoordinatorTests: XCTestCase {
         // Then
         // The Product Selector is shown using a WooNavigationController
         // The Product Selector is the first view controller in the presented WooNavigationController
-        let presentedNavigationController = try XCTUnwrap(navigationController.presentedViewControllers.first as? WooNavigationController)
+        let presentedNavigationController = try XCTUnwrap(sut.navigationController.presentedViewController as? WooNavigationController)
         let viewController = try XCTUnwrap(presentedNavigationController.viewControllers.first)
         XCTAssertTrue(viewController is ProductSelectorViewController)
     }
@@ -125,22 +130,5 @@ private extension BlazeCampaignCreationCoordinatorTests {
         let product = storage.insertNewObject(ofType: StorageProduct.self)
         product.update(with: readOnlyProduct)
         storage.saveIfNeeded()
-    }
-}
-
-private final class MockSourceNavigationController: UINavigationController {
-    private(set) var shownViewControllers: [UIViewController] = []
-    private(set) var presentedViewControllers: [UIViewController] = []
-
-    override func show(_ vc: UIViewController, sender: Any?) {
-        shownViewControllers.append(vc)
-    }
-
-    override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)? = nil) {
-        presentedViewControllers.append(viewControllerToPresent)
-    }
-
-    override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
-        presentedViewControllers.removeLast(1)
     }
 }

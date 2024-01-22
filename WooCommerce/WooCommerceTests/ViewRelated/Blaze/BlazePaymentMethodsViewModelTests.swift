274de2c2c5ebc1200df8171c0d9404c2c75f20f9
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-01-22 22:26:50 +0530

    Update unit tests.

--- WooCommerce/WooCommerceTests/ViewRelated/Blaze/BlazePaymentMethodsViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Blaze/BlazePaymentMethodsViewModelTests.swift
@@ -91,48 +91,6 @@ final class BlazePaymentMethodsViewModelTests: XCTestCase {
         XCTAssertEqual(viewModel.addPaymentSuccessURL, "https://example.com/blaze-pm-success")
     }
 
-    // MARK: `isDoneButtonEnabled`
-    func test_isDoneButtonEnabled_is_false_if_no_payment_methods_available() async throws {
-        // Given
-        let viewModel = BlazePaymentMethodsViewModel(siteID: sampleSiteID,
-                                                     paymentInfo: samplePaymentInfo.copy(savedPaymentMethods: []),
-                                                     selectedPaymentMethodID: nil,
-                                                     stores: stores) { _ in }
-
-        // Then
-        XCTAssertFalse(viewModel.isDoneButtonEnabled)
-    }
-
-    func test_isDoneButtonEnabled_is_false_if_selection_not_changed() async throws {
-        // Given
-        let viewModel = BlazePaymentMethodsViewModel(siteID: sampleSiteID,
-                                                     paymentInfo: samplePaymentInfo,
-                                                     selectedPaymentMethodID: "payment-method-1",
-                                                     stores: stores) { _ in }
-
-        // When
-        viewModel.didSelectPaymentMethod(withID: "payment-method-2")
-        // Restore original selection
-        viewModel.didSelectPaymentMethod(withID: "payment-method-1")
-
-        // Then
-        XCTAssertFalse(viewModel.isDoneButtonEnabled)
-    }
-
-    func test_isDoneButtonEnabled_is_true_if_selection_not_changed() async throws {
-        // Given
-        let viewModel = BlazePaymentMethodsViewModel(siteID: sampleSiteID,
-                                                     paymentInfo: samplePaymentInfo,
-                                                     selectedPaymentMethodID: "payment-method-1",
-                                                     stores: stores) { _ in }
-
-        // When
-        viewModel.didSelectPaymentMethod(withID: "payment-method-2")
-
-        // Then
-        XCTAssertTrue(viewModel.isDoneButtonEnabled)
-    }
-
     // MARK: `didSelectPaymentMethod`
 
     func test_didSelectPaymentMethod_updates_selected_payment_method_id() async throws {
@@ -149,70 +107,8 @@ final class BlazePaymentMethodsViewModelTests: XCTestCase {
         XCTAssertEqual(viewModel.selectedPaymentMethodID, "payment-method-2")
     }
 
-    // MARK: Sync payments
-
-    func test_isFetchingPaymentInfo_is_updated_correctly_when_fetching_payment_info() async {
-        // Given
-        let viewModel = BlazePaymentMethodsViewModel(siteID: sampleSiteID,
-                                                     paymentInfo: samplePaymentInfo,
-                                                     selectedPaymentMethodID: "payment-method-1",
-                                                     stores: stores) { _ in }
-        var fetchingStates: [Bool] = []
-        subscription = viewModel.$isFetchingPaymentInfo
-            .sink { isFetching in
-                fetchingStates.append(isFetching)
-            }
-
-        // When
-        mockPaymentFetch(with: .success(.fake()))
-        await viewModel.syncPaymentInfo()
-
-        // Then
-        XCTAssertEqual(fetchingStates, [false, true, false])
-    }
-
-    func test_shouldDisplayPaymentErrorAlert_is_true_when_fetching_payment_info_fails() async {
-        // Given
-        let viewModel = BlazePaymentMethodsViewModel(siteID: sampleSiteID,
-                                                     paymentInfo: samplePaymentInfo,
-                                                     selectedPaymentMethodID: "payment-method-1",
-                                                     stores: stores) { _ in }
-        XCTAssertFalse(viewModel.shouldDisplayPaymentErrorAlert)
-
-        // When
-        mockPaymentFetch(with: .failure(NSError(domain: "Test", code: 500)))
-        await viewModel.syncPaymentInfo()
-
-        // Then
-        XCTAssertTrue(viewModel.shouldDisplayPaymentErrorAlert)
-    }
-
-    func test_syncPaymentInfo_refreshes_payment_methods_on_success() async {
-        // Given
-        let viewModel = BlazePaymentMethodsViewModel(siteID: sampleSiteID,
-                                                     paymentInfo: samplePaymentInfo.copy(savedPaymentMethods: []),
-                                                     selectedPaymentMethodID: nil,
-                                                     stores: stores) { _ in }
-        let paymentMethod = BlazePaymentMethod(id: "test-id",
-                                               rawType: "credit-card",
-                                               name: "Card ending in 7284",
-                                               info: .init(lastDigits: "7284",
-                                                           expiring: .fake(),
-                                                           type: "Mastercard",
-                                                           nickname: nil,
-                                                           cardholderName: "Jane Doe"))
-        XCTAssertTrue(viewModel.paymentMethods.isEmpty)
-
-        // When
-        mockPaymentFetch(with: .success(samplePaymentInfo))
-        await viewModel.syncPaymentInfo()
-
-        // Then
-        XCTAssertEqual(viewModel.paymentMethods, samplePaymentInfo.savedPaymentMethods)
-    }
-
     // MARK: Save selection
-    func test_saveSelection_send_selected_payment_id_via_completion_handler() async throws {
+    func test_didSelectPaymentMethod_sends_selected_payment_id_via_completion_handler() async throws {
         // Given
         var selectedPaymentID = ""
         let viewModel = BlazePaymentMethodsViewModel(siteID: sampleSiteID,
@@ -223,11 +119,51 @@ final class BlazePaymentMethodsViewModelTests: XCTestCase {
         }
         viewModel.didSelectPaymentMethod(withID: "payment-method-2")
 
-        // When
-        viewModel.saveSelection()
-
         // Then
         XCTAssertEqual(selectedPaymentID, "payment-method-2")
+    }
+
+    // MARK: Add new payment method
+
+    func test_addPaymentMethodURL_has_correct_value() async throws {
+        // Given
+        let viewModel = BlazePaymentMethodsViewModel(siteID: sampleSiteID,
+                                                     paymentInfo: samplePaymentInfo,
+                                                     selectedPaymentMethodID: "payment-method-1",
+                                                     stores: stores) { _ in }
+
+        // Then
+        let url = try XCTUnwrap(URL(string: samplePaymentInfo.addPaymentMethod.formUrl))
+        XCTAssertEqual(viewModel.addPaymentMethodURL, url)
+    }
+
+    func test_addPaymentSuccessURL_has_correct_value() async throws {
+        // Given
+        let viewModel = BlazePaymentMethodsViewModel(siteID: sampleSiteID,
+                                                     paymentInfo: samplePaymentInfo,
+                                                     selectedPaymentMethodID: "payment-method-1",
+                                                     stores: stores) { _ in }
+
+        // Then
+        let url = try XCTUnwrap(samplePaymentInfo.addPaymentMethod.successUrl)
+        XCTAssertEqual(viewModel.addPaymentSuccessURL, url)
+    }
+
+    func test_didAddNewPaymentMethod_sends_newly_added_payment_id_via_completion_handler() async throws {
+        // Given
+        var selectedPaymentID = ""
+        let viewModel = BlazePaymentMethodsViewModel(siteID: sampleSiteID,
+                                                     paymentInfo: samplePaymentInfo,
+                                                     selectedPaymentMethodID: "payment-method-1",
+                                                     stores: stores) { id in
+            selectedPaymentID = id
+        }
+
+        let successURL = try XCTUnwrap(URL(string: "\(samplePaymentInfo.addPaymentMethod.successUrl)?\(samplePaymentInfo.addPaymentMethod.idUrlParameter)=123"))
+        viewModel.didAddNewPaymentMethod(successURL: successURL)
+
+        // Then
+        XCTAssertEqual(selectedPaymentID, "123")
     }
 }
 

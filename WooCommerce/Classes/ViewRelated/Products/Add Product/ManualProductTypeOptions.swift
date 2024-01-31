87dec2e4ae6d9fe27e791ac3f58c0b845836b6f0
Hafiz Rahman <hfzrhmn@gmail.com>
2024-01-31 14:45:15 +0700

    Add SwiftUI file for displaying manual product type creation options.

---
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/ManualProductTypeOptions.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+/// View to show the manual product type creation options.
+///
+struct ManualProductTypeOptions: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ManualProductTypeOptions()
+}

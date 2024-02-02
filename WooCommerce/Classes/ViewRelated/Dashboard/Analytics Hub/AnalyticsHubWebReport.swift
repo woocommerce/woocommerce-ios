4a2734237d618d5d573053bfd2a45be64c75afb0
Rachel McRoberts <rachel@automattic.com>
2024-02-02 15:26:03 +0000

    Add method to get the URL for a web analytics report

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubWebReport.swift
@@ -0,0 +1,87 @@
+import Foundation
+
+/// Represents analytics reports the app can link to on the store's web admin
+struct AnalyticsHubWebReport {
+
+    /// Supported types of analytics reports
+    enum ReportType {
+        case revenue
+        case orders
+        case products
+    }
+
+    /// Provides the URL for a web analytics report
+    /// - Parameters:
+    ///   - report: Type of analytics report
+    ///   - period: Time range for the report
+    ///   - storeAdminURL: The store's wp-admin URL
+    ///
+    static func getUrl(for report: ReportType,
+                       timeRange: AnalyticsHubTimeRangeSelection.SelectionType?,
+                       storeAdminURL: String? = ServiceLocator.stores.sessionManager.defaultSite?.adminURL) -> URL? {
+        guard let storeAdminURL else {
+            return nil
+        }
+
+        let path = getPath(for: report)
+        let defaultReportString = storeAdminURL + "admin.php?page=wc-admin&path=\(path)"
+
+        // Build the web report URL based on the time range
+        // Note: the `compare` parameter only applies if the period is also specified
+        let period = getPeriod(for: timeRange)
+        switch (timeRange, period) {
+        case let (.custom(startDate, endDate), .some(period)):
+            let after = DateFormatter.Defaults.yearMonthDayDateFormatter.string(from: startDate)
+            let before = DateFormatter.Defaults.yearMonthDayDateFormatter.string(from: endDate)
+            return URL(string: defaultReportString + "&period=\(period)&after=\(after)&before=\(before)&compare=previous_period")
+        case let (_, .some(period)):
+            return URL(string: defaultReportString + "&period=\(period)&compare=previous_period")
+        default:
+            return URL(string: defaultReportString)
+        }
+    }
+
+    /// Gets the path parameter for the web report, based on the provided report type
+    ///
+    private static func getPath(for reportType: ReportType) -> String {
+        switch reportType {
+        case .revenue:
+            return "%2Fanalytics%2Frevenue"
+        case .orders:
+            return "%2Fanalytics%2Forders"
+        case .products:
+            return "%2Fanalytics%2Fproducts"
+        }
+    }
+
+    /// Gets the period parameter for the web report, based on the provided time range
+    ///
+    private static func getPeriod(for timeRange: AnalyticsHubTimeRangeSelection.SelectionType?) -> String? {
+        switch timeRange {
+        case .custom:
+            return "custom"
+        case .today:
+            return "today"
+        case .yesterday:
+            return "yesterday"
+        case .lastWeek:
+            return "last_week"
+        case .lastMonth:
+            return "last_month"
+        case .lastQuarter:
+            return "last_quarter"
+        case .lastYear:
+            return "last_year"
+        case .weekToDate:
+            return "week"
+        case .monthToDate:
+            return "month"
+        case .quarterToDate:
+            return "quarter"
+        case .yearToDate:
+            return "year"
+        default:
+            return nil
+        }
+    }
+}
---
+++ WooCommerce/WooCommerceTests/ViewRelated/Dashboard/Analytics Hub/AnalyticsHubWebReportTests.swift
@@ -0,0 +1,171 @@
+import XCTest
+import TestKit
+@testable import WooCommerce
+
+final class AnalyticsHubWebReportTests: XCTestCase {
+
+    let exampleAdminURL = "https://example.com/wp-admin/"
+    let exampleDefaultReport = "admin.php?page=wc-admin&path=%2Fanalytics%2Frevenue"
+
+    func test_getUrl_returns_expected_default_revenue_report_url() {
+        // Given
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .revenue, timeRange: nil, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + "admin.php?page=wc-admin&path=%2Fanalytics%2Frevenue")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_default_orders_report_url() {
+        // Given
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .orders, timeRange: nil, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + "admin.php?page=wc-admin&path=%2Fanalytics%2Forders")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_default_products_report_url() {
+        // Given
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .products, timeRange: nil, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + "admin.php?page=wc-admin&path=%2Fanalytics%2Fproducts")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_URL_with_custom_time_range() throws {
+        // Given
+        let start = try XCTUnwrap(Date.dateWithISO8601String("2024-01-01T00:00:00Z"))
+        let end = try XCTUnwrap(Date.dateWithISO8601String("2024-01-07T00:00:00Z"))
+        let timeRange: AnalyticsHubTimeRangeSelection.SelectionType = .custom(start: start, end: end)
+
+        // When
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .revenue, timeRange: timeRange, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + exampleDefaultReport + "&period=custom&after=2024-01-01&before=2024-01-07&compare=previous_period")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_URL_for_today_time_range() {
+        // Given
+        let timeRange: AnalyticsHubTimeRangeSelection.SelectionType = .today
+
+        // When
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .revenue, timeRange: timeRange, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + exampleDefaultReport + "&period=today&compare=previous_period")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_URL_for_yesterday_time_range() {
+        // Given
+        let timeRange: AnalyticsHubTimeRangeSelection.SelectionType = .yesterday
+
+        // When
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .revenue, timeRange: timeRange, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + exampleDefaultReport + "&period=yesterday&compare=previous_period")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_URL_for_last_week_time_range() {
+        // Given
+        let timeRange: AnalyticsHubTimeRangeSelection.SelectionType = .lastWeek
+
+        // When
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .revenue, timeRange: timeRange, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + exampleDefaultReport + "&period=last_week&compare=previous_period")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_URL_for_last_month_time_range() {
+        // Given
+        let timeRange: AnalyticsHubTimeRangeSelection.SelectionType = .lastMonth
+
+        // When
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .revenue, timeRange: timeRange, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + exampleDefaultReport + "&period=last_month&compare=previous_period")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_URL_for_last_quarter_time_range() {
+        // Given
+        let timeRange: AnalyticsHubTimeRangeSelection.SelectionType = .lastQuarter
+
+        // When
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .revenue, timeRange: timeRange, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + exampleDefaultReport + "&period=last_quarter&compare=previous_period")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_URL_for_last_year_time_range() {
+        // Given
+        let timeRange: AnalyticsHubTimeRangeSelection.SelectionType = .lastYear
+
+        // When
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .revenue, timeRange: timeRange, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + exampleDefaultReport + "&period=last_year&compare=previous_period")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_URL_for_week_to_date_time_range() {
+        // Given
+        let timeRange: AnalyticsHubTimeRangeSelection.SelectionType = .weekToDate
+
+        // When
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .revenue, timeRange: timeRange, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + exampleDefaultReport + "&period=week&compare=previous_period")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_URL_for_month_to_date_time_range() {
+        // Given
+        let timeRange: AnalyticsHubTimeRangeSelection.SelectionType = .monthToDate
+
+        // When
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .revenue, timeRange: timeRange, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + exampleDefaultReport + "&period=month&compare=previous_period")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_URL_for_quarter_to_date_time_range() {
+        // Given
+        let timeRange: AnalyticsHubTimeRangeSelection.SelectionType = .quarterToDate
+
+        // When
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .revenue, timeRange: timeRange, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + exampleDefaultReport + "&period=quarter&compare=previous_period")
+        assertEqual(expectedURL, reportURL)
+    }
+
+    func test_getUrl_returns_expected_URL_for_year_to_date_time_range() {
+        // Given
+        let timeRange: AnalyticsHubTimeRangeSelection.SelectionType = .yearToDate
+
+        // When
+        let reportURL = AnalyticsHubWebReport.getUrl(for: .revenue, timeRange: timeRange, storeAdminURL: exampleAdminURL)
+
+        // Then
+        let expectedURL = URL(string: exampleAdminURL + exampleDefaultReport + "&period=year&compare=previous_period")
+        assertEqual(expectedURL, reportURL)
+    }
+
+}

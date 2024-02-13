1da24ee2753c6f154ea8cc2ffed50f532fe4f64e
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-02-13 08:29:05 +0530

    Unit test that custom range is converted into rawValue as expected.

--- Yosemite/YosemiteTests/Model/Enums/StatsTimeRangeTests.swift
+++ Yosemite/YosemiteTests/Model/Enums/StatsTimeRangeTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import Yosemite
 
-class StatsTimeRangeTests: XCTestCase {
+final class StatsTimeRangeTests: XCTestCase {
 
     func testVisitStatsQuantityOnFebInLeapYear() {
         // GMT: Saturday, February 1, 2020 12:29:29 AM
@@ -10,5 +10,43 @@ class StatsTimeRangeTests: XCTestCase {
         let quantity = StatsTimeRangeV4.thisMonth.siteVisitStatsQuantity(date: date,
                                                                          siteTimezone: timezone)
         XCTAssertEqual(quantity, 29)
+    }
+
+    // MARK: Custom range
+
+    func test_initializing_custom_range_from_rawValue() {
+        // Given
+
+        // GMT: Saturday, February 1, 2020 12:29:29 AM
+        let fromDate = Date(timeIntervalSince1970: 1580516969)
+        let fromDateString = DateFormatter.Defaults.yearMonthDayDateFormatter.string(from: fromDate)
+
+        // GMT: Tuesday, February 13, 2024 2:47:04 AM
+        let toDate = Date(timeIntervalSince1970: 1707792424)
+        let toDateString = DateFormatter.Defaults.yearMonthDayDateFormatter.string(from: toDate)
+
+        // When
+        let range = StatsTimeRangeV4(rawValue: "custom_\(fromDateString)_\(toDateString)")
+
+        // Then
+        XCTAssertEqual(range, .custom(from: fromDate, to: toDate))
+    }
+
+    func test_getting_rawValue_from_custom_range() {
+        // Given
+
+        // GMT: Saturday, February 1, 2020 12:29:29 AM
+        let fromDate = Date(timeIntervalSince1970: 1580516969)
+        let fromDateString = DateFormatter.Defaults.yearMonthDayDateFormatter.string(from: fromDate)
+
+        // GMT: Tuesday, February 13, 2024 2:47:04 AM
+        let toDate = Date(timeIntervalSince1970: 1707792424)
+        let toDateString = DateFormatter.Defaults.yearMonthDayDateFormatter.string(from: toDate)
+
+        // When
+        let range: StatsTimeRangeV4 = .custom(from: fromDate, to: toDate)
+
+        // Then
+        XCTAssertEqual(range.rawValue, "custom_\(fromDateString)_\(toDateString)")
     }
 }

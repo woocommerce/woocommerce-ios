aa9556628e7e7b4444c657d674d18ccaf47c15a3
Ian Maia <ian.maia@automattic.com>
2024-02-14 20:23:01 +0100

    Try using the PR diff instead of listing files with git

--- .buildkite/commands/run-swiftlint.sh
+++ .buildkite/commands/run-swiftlint.sh
@@ -3,8 +3,22 @@
 echo "--- :swift: Running SwiftLint"
 
 # Run SwiftLint only on the modified files, similarly to what is done on Scripts/build-phases/swiftlint.sh
-MODIFIED_SWIFT_FILES=$(git diff --name-only --diff-filter=d HEAD -- '*.swift' \
-                     $(git ls-files --others --exclude-standard -- '*.swift'))
+
+PULL_REQUEST_REPO="${BUILDKITE_PULL_REQUEST_REPO%.git}"
+PULL_REQUEST_DIFF_URL="$PULL_REQUEST_REPO/pull/$BUILDKITE_PULL_REQUEST.diff"
+
+# fetch the diff file using Swift -- the SwiftLint image doesn't have curl or wget
+DIFF_FILE=$(swift - <<'EOF'
+  import Foundation
+  if let data = try? Data(contentsOf: URL(string: "$PULL_REQUEST_DIFF_URL")!), let diffFile = String(data: data, encoding: .utf8) {
+      print(diffFile)
+  } else {
+      fatalError("Failed to fetch or decode diff file")
+  }
+EOF
+)
+
+MODIFIED_SWIFT_FILES=$(echo "$DIFF_FILE" | grep '^diff --git' | sed 's/^diff --git a\///' | cut -d' ' -f1)
 
 set +e
 SWIFTLINT_OUTPUT=$(echo "$MODIFIED_SWIFT_FILES" | xargs swiftlint lint --quiet "$@" --reporter relative-path)

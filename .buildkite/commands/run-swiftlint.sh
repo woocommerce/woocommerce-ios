98d31a6ee052ad8cee566f073542285ff2d25567
Ian Maia <ian.maia@automattic.com>
2024-02-15 11:19:44 +0100

    Revert back to run SwiftLint on all files

--- .buildkite/commands/run-swiftlint.sh
+++ .buildkite/commands/run-swiftlint.sh
@@ -2,26 +2,8 @@
 
 echo "--- :swift: Running SwiftLint"
 
-# Run SwiftLint only on the modified files, similarly to what is done on Scripts/build-phases/swiftlint.sh
-
-PULL_REQUEST_REPO="${BUILDKITE_PULL_REQUEST_REPO%.git}"
-PULL_REQUEST_DIFF_URL="$PULL_REQUEST_REPO/pull/$BUILDKITE_PULL_REQUEST.diff"
-
-# fetch the diff file using Swift -- the SwiftLint image doesn't have curl or wget
-DIFF_FILE=$(swift - <<'EOF'
-  import Foundation
-  if let data = try? Data(contentsOf: URL(string: "$PULL_REQUEST_DIFF_URL")!), let diffFile = String(data: data, encoding: .utf8) {
-      print(diffFile)
-  } else {
-      fatalError("Failed to fetch or decode diff file")
-  }
-EOF
-)
-
-MODIFIED_SWIFT_FILES=$(echo "$DIFF_FILE" | grep '^diff --git' | sed 's/^diff --git a\///' | cut -d' ' -f1)
-
 set +e
-SWIFTLINT_OUTPUT=$(echo "$MODIFIED_SWIFT_FILES" | xargs swiftlint lint --quiet "$@" --reporter relative-path)
+SWIFTLINT_OUTPUT=$(swiftlint lint --quiet "$@" --reporter relative-path)
 SWIFTLINT_EXIT_STATUS=$?
 set -e
 

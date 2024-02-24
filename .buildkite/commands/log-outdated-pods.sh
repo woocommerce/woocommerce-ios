d015af05818024ee5bfcf8ff5fd93327950125ac
Spencer Transier <spencer.transier@automattic.com>
2024-02-24 00:08:18 -0800

    Use updated regex

--- .buildkite/commands/log-outdated-pods.sh
+++ .buildkite/commands/log-outdated-pods.sh
@@ -4,16 +4,16 @@ echo '--- :cocoapods: Install Pods (required to check for outdated next)'
 install_cocoapods
 
 # Run `bundle exec pod outdated` and capture the output
-outdated_pods=$(bundle exec pod outdated)
+OUTDATED_PODS=$(bundle exec pod outdated)
 
 # Filter the outdated pods from the rest of the output
-filtered_pods=$(echo "$outdated_pods" | grep -o '- (\S+) \S+ -> \S+ \(latest version \S+\)')
+FILTERED_PODS=$(echo "$OUTDATED_PODS" | grep -oE '\- ([^[:space:]]+) [^[:space:]]+ -> [^[:space:]]+ \(latest version [^[:space:]]+\)')
 
 # Check if we have any outdated pods to report
-if [ -z "$filtered_pods" ]; then
+if [ -z "$FILTERED_PODS" ]; then
   echo "No outdated pods to report."
 else
 # Create a Buildkite annotation with the filtered pods
-  echo "$filtered_pods"
-  buildkite-agent annotate --style "info" --context 'ctx-outdated-pods' <<< "$filtered_pods"
+  echo "$FILTERED_PODS"
+  buildkite-agent annotate --style "info" --context 'ctx-outdated-pods' <<< "$FILTERED_PODS"
 fi

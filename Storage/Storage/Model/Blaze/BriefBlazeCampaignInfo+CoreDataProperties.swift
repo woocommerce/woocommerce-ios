19efd06beed13f1bfab94aca7946c1a5af20f77f
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-02-28 05:27:55 +0530

    Update core data model property types.

--- Storage/Storage/Model/Blaze/BriefBlazeCampaignInfo+CoreDataProperties.swift
+++ Storage/Storage/Model/Blaze/BriefBlazeCampaignInfo+CoreDataProperties.swift
@@ -8,18 +8,18 @@ extension BriefBlazeCampaignInfo {
         return NSFetchRequest<BriefBlazeCampaignInfo>(entityName: "BriefBlazeCampaignInfo")
     }
 
-    @NSManaged public var campaignID: String?
+    @NSManaged public var campaignID: String
     @NSManaged public var clicks: Int64
     @NSManaged public var imageURL: String?
     @NSManaged public var impressions: Int64
-    @NSManaged public var name: String?
-    @NSManaged public var productID: Int64
+    @NSManaged public var name: String
+    @NSManaged public var productID: NSNumber?
     @NSManaged public var siteID: Int64
     @NSManaged public var spentBudget: Double
     @NSManaged public var targetUrl: String?
-    @NSManaged public var textSnippet: String?
+    @NSManaged public var textSnippet: String
     @NSManaged public var totalBudget: Double
-    @NSManaged public var uiStatus: String?
+    @NSManaged public var rawStatus: String
 
 }
 
---
+++ Yosemite/Yosemite/Model/Storage/BriefBlazeCampaignInfo+ReadOnlyConvertible.swift
@@ -0,0 +1,9 @@
+//
+//  BriefBlazeCampaignInfo+ReadOnlyConvertible.swift
+//  Yosemite
+//
+//  Created by Sharma Elanthiraiyan on 28/02/24.
+//  Copyright © 2024 Automattic. All rights reserved.
+//
+
+import Foundation

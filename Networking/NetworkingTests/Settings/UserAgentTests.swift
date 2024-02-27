e6d6c280d532644fe058eb8d312ed624340be23e
Tony Li <tony.li@automattic.com>
2024-02-28 12:52:17 +1300

    Compose an user agent instead of using WKWebView

--- Networking/NetworkingTests/Settings/UserAgentTests.swift
+++ Networking/NetworkingTests/Settings/UserAgentTests.swift
@@ -18,4 +18,12 @@ final class UserAgentTests: XCTestCase {
         let message = "This method for retrieveing the user agent seems to be no longer working. We need to figure out an alternative."
         XCTAssertFalse(UserAgent.webkitUserAgent.isEmpty, message)
     }
+
+    func testUserAgentFormat() throws {
+        let regex = #"^Mozilla/5\.0 \([a-zA-Z]+; CPU [\sa-zA-Z]+ [_0-9]+ like Mac OS X\) AppleWebKit/605\.1\.15 \(KHTML, like Gecko\) Mobile/15E148$"#
+        let regulardExpression = try NSRegularExpression(pattern: regex)
+        let userAgent = UserAgent.webkitUserAgent
+        XCTAssertEqual(regulardExpression.numberOfMatches(in: userAgent, range: NSMakeRange(0, userAgent.count)), 1)
+    }
+
 }

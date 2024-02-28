eea56aaf28b137a031cca18dc60f2e01d3f5db69
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-02-28 15:49:26 +0700

    Move ProductSelectorViewController to own file

--- WooCommerce/Classes/ViewRelated/Products/ProductSelector/ProductSelectorView.swift
+++ WooCommerce/Classes/ViewRelated/Products/ProductSelector/ProductSelectorView.swift
@@ -1,24 +1,6 @@
 import SwiftUI
 import Yosemite
 
-/// Hosting controller for `ProductSelectorView`.
-///
-final class ProductSelectorViewController: UIHostingController<ProductSelectorView> {
-    init(configuration: ProductSelectorView.Configuration,
-         source: ProductSelectorView.Source,
-         viewModel: ProductSelectorViewModel) {
-
-        super.init(rootView: ProductSelectorView(configuration: configuration,
-                                                 source: source,
-                                                 isPresented: .constant(true),
-                                                 viewModel: viewModel))
-    }
-
-    @available(*, unavailable)
-    required dynamic init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
 /// View showing a list of products to select.
 ///
 struct ProductSelectorView: View {
---
+++ WooCommerce/Classes/ViewRelated/Products/ProductSelector/ProductSelectorViewController.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+/// Hosting controller for `ProductSelectorView`.
+///
+final class ProductSelectorViewController: UIHostingController<ProductSelectorView> {
+    init(configuration: ProductSelectorView.Configuration,
+         source: ProductSelectorView.Source,
+         viewModel: ProductSelectorViewModel) {
+
+        super.init(rootView: ProductSelectorView(configuration: configuration,
+                                                 source: source,
+                                                 isPresented: .constant(true),
+                                                 viewModel: viewModel))
+    }
+
+    @available(*, unavailable)
+    required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

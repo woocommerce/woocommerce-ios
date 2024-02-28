dac67affe4b64e7fb83a8e17e54174ccb6e944ad
Tony Li <tony.li@automattic.com>
2024-02-28 18:56:23 +1300

    Add an unit test to report potential unexpected WKWebView user agent

--- Networking/NetworkingTests/Settings/UserAgentTests.swift
+++ Networking/NetworkingTests/Settings/UserAgentTests.swift
@@ -1,9 +1,13 @@
 import XCTest
+import WebKit
+
 @testable import Networking
 
 /// UserAgent Unit Tests
 ///
 final class UserAgentTests: XCTestCase {
+
+    let webkitUserAgentRegex = #"^Mozilla/5\.0 \([a-zA-Z]+; CPU [\sa-zA-Z]+ [_0-9]+ like Mac OS X\) AppleWebKit/605\.1\.15 \(KHTML, like Gecko\) Mobile/15E148$"#
 
     func testDefaultUserAgent() {
         let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? String()
@@ -20,9 +24,17 @@ final class UserAgentTests: XCTestCase {
     }
 
     func testUserAgentFormat() throws {
-        let regex = #"^Mozilla/5\.0 \([a-zA-Z]+; CPU [\sa-zA-Z]+ [_0-9]+ like Mac OS X\) AppleWebKit/605\.1\.15 \(KHTML, like Gecko\) Mobile/15E148$"#
-        let regulardExpression = try NSRegularExpression(pattern: regex)
+        let regulardExpression = try NSRegularExpression(pattern: webkitUserAgentRegex)
         let userAgent = UserAgent.webkitUserAgent
+        XCTAssertEqual(regulardExpression.numberOfMatches(in: userAgent, range: NSMakeRange(0, userAgent.count)), 1)
+    }
+
+    // If this test fails, it may mean `WKWebView` uses a user agent with an unexpected format (see `webkitUserAgentRegex`)
+    // and we may need to adjust `UserAgent.webkitUserAgent`'s implementation to match `WKWebView`'s user agent.
+    func testWKWebViewUserAgentFormat() throws {
+        let regulardExpression = try NSRegularExpression(pattern: webkitUserAgentRegex)
+        // Please note: WKWebView's user agent may be different on different test device types.
+        let userAgent = try XCTUnwrap(WKWebView().value(forKey: "_userAgent") as? String)
         XCTAssertEqual(regulardExpression.numberOfMatches(in: userAgent, range: NSMakeRange(0, userAgent.count)), 1)
     }
 

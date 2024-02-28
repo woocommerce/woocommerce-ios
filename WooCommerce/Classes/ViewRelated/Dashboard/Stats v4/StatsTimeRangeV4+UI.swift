c354425dc6d17f6434a360dae6019738e89ab59f
Huong Do <hi.huongdo@gmail.com>
2024-02-28 12:12:54 +0700

    Calculate max number of intervals given the custom time range

--- WooCommerce/Classes/ViewRelated/Dashboard/Stats v4/StatsTimeRangeV4+UI.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Stats v4/StatsTimeRangeV4+UI.swift
@@ -12,9 +12,23 @@ extension StatsTimeRangeV4 {
             return 31
         case .thisYear:
             return 12
-        case .custom:
-            // TODO: 11935 Calculate number of intervals
-            return 7
+        case let .custom(startDate, endDate):
+            let calendar = Calendar.current
+            let quantity: Int? = {
+                switch intervalGranularity {
+                case .hourly:
+                    calendar.dateComponents([.hour], from: startDate, to: endDate).hour
+                case .daily, .weekly:
+                    calendar.dateComponents([.day], from: startDate, to: endDate).day
+                case .monthly:
+                    calendar.dateComponents([.month], from: startDate, to: endDate).month
+                case .quarterly:
+                    calendar.dateComponents([.quarter], from: startDate, to: endDate).quarter
+                case .yearly:
+                    calendar.dateComponents([.year], from: startDate, to: endDate).year
+                }
+            }()
+            return quantity ?? 7
         }
     }
 

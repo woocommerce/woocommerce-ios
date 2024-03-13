5cb8bf74f0ffcd0960ccb6955e732c8d586029be
Paolo Musolino <info@codeido.com>
2024-03-13 17:20:36 +0100

    refactor: OrderDetailsNotices

--- WooCommerce/Classes/ViewModels/Order Details/OrderDetailsNotices.swift
+++ WooCommerce/Classes/ViewModels/Order Details/OrderDetailsNotices.swift
@@ -4,22 +4,30 @@ import Yosemite
 final class OrderDetailsNotices {
     /// Displays the `Unable to delete tracking` Notice.
     ///
-    func displayDeleteErrorNotice(order: Order, tracking: ShipmentTracking, onAction: @escaping () -> Void) {
-        let titleFormat = NSLocalizedString(
-            "Unable to delete tracking for order #%1$d",
-            comment: "Content of error presented when Delete Shipment Tracking Action Failed. "
-                + "It reads: Unable to delete tracking for order #{order number}. "
-                + "Parameters: %1$d - order number"
-        )
-        let title = String.localizedStringWithFormat(titleFormat, order.orderID)
-        let actionTitle = NSLocalizedString("Retry", comment: "Retry Action")
+    func displayDeleteTrackingErrorNotice(order: Order, tracking: ShipmentTracking, onAction: @escaping () -> Void) {
+        let title = String.localizedStringWithFormat(Localization.deleteTracking, order.orderID)
         let notice = Notice(title: title,
                             message: nil,
                             feedbackType: .error,
-                            actionTitle: actionTitle) {
+                            actionTitle: Localization.retry) {
                                 onAction()
         }
 
         ServiceLocator.noticePresenter.enqueue(notice: notice)
+    }
+}
+
+private extension OrderDetailsNotices {
+    enum Localization {
+        // Here to be added to Localizable.strings so it can be looked up by the `LocalizedStringResource` above
+        static let deleteTracking = NSLocalizedString(
+                     "OrderDetail.deleteTracking.notice.title",
+                     value: "Unable to delete tracking for order #%1$d",
+                     comment: "Content of error presented when Delete Shipment Tracking Action Failed. "
+                     + "It reads: Unable to delete tracking for order #{order number}. "
+                     + "Parameters: %1$d - order number")
+        static let retry = NSLocalizedString("OrderDetail.retry.notice.button",
+                                             value:"Retry",
+                                             comment: "Retry Action inside notice")
     }
 }

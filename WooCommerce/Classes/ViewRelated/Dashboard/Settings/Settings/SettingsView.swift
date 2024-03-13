fea98a1eb70676c666ccf611c73c9e5d4690e30e
Huong Do <hi.huongdo@gmail.com>
2024-03-13 11:39:36 +0700

    Restore workaround in SettingsView

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Settings/SettingsView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Settings/SettingsView.swift
@@ -4,8 +4,23 @@ import SwiftUI
 ///
 struct SettingsView: UIViewControllerRepresentable {
 
+
+    class Coordinator {
+        var parentObserver: NSKeyValueObservation?
+    }
+
+    /// This is a UIKit solution for fixing Navigation Title and Bar Button Items ignored in NavigationView.
+    /// This solution doesn't require making internal changes to the destination `UIViewController`
+    /// and should be called once, when wrapped.
+    /// Solution proposed here: https://stackoverflow.com/a/68567095/7241994
+    ///
+    ///
     func makeUIViewController(context: Self.Context) -> SettingsViewController {
         let viewController = SettingsViewController()
+        context.coordinator.parentObserver = viewController.observe(\.parent, changeHandler: { vc, _ in
+            vc.parent?.navigationItem.title = vc.title
+            vc.parent?.navigationItem.rightBarButtonItems = vc.navigationItem.rightBarButtonItems
+        })
         return viewController
     }
 

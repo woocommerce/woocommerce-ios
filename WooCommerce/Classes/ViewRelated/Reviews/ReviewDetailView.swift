74dfb130ab27c8922401e0e72dade560ff30baf3
Huong Do <hi.huongdo@gmail.com>
2024-03-14 14:29:00 +0700

    Add wrapper for ReviewDetailView

--- WooCommerce/Classes/ViewRelated/Reviews/ReviewDetailView.swift
+++ WooCommerce/Classes/ViewRelated/Reviews/ReviewDetailView.swift
@@ -1,9 +1,35 @@
 import SwiftUI
 import Yosemite
 
+/// SwiftUI view for `ReviewDetailsViewController`
+///
+struct ReviewDetailView: View {
+    let productReview: ProductReview
+    let product: Product?
+    let notification: Note?
+
+    var body: some View {
+        ReviewDetailWrapperView(productReview: productReview,
+                                product: product,
+                                notification: notification)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle(Localization.title)
+    }
+}
+
+private extension ReviewDetailView {
+    enum Localization {
+        static let title = NSLocalizedString(
+            "reviewDetailView.title",
+            value: "Product Review",
+            comment: "Title of the product review detail screen"
+        )
+    }
+}
+
 /// SwiftUI wrapper for `ReviewDetailsViewController`
 ///
-struct ReviewDetailView: UIViewControllerRepresentable {
+private struct ReviewDetailWrapperView: UIViewControllerRepresentable {
     let productReview: ProductReview
     let product: Product?
     let notification: Note?

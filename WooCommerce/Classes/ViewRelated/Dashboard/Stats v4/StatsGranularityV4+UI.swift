bc98939556174f7e90046a4a08a2f12e620f6ab8
Huong Do <hi.huongdo@gmail.com>
2024-03-19 09:48:37 +0700

    Update wording for granularity

--- WooCommerce/Classes/ViewRelated/Dashboard/Stats v4/StatsGranularityV4+UI.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Stats v4/StatsGranularityV4+UI.swift
@@ -7,31 +7,31 @@ extension StatsGranularityV4 {
         case .daily:
             NSLocalizedString(
                 "statsGranularityV4.dailyMetrics",
-                value: "Metrics by day",
+                value: "Daily intervals",
                 comment: "Display text for the daily granularity of store stats on the My Store screen"
             )
         case .hourly:
             NSLocalizedString(
                 "statsGranularityV4.hourlyMetrics",
-                value: "Metrics by hour",
+                value: "Hourly intervals",
                 comment: "Display text for the hourly granularity of store stats on the My Store screen"
             )
         case .weekly:
             NSLocalizedString(
                 "statsGranularityV4.weeklyMetrics",
-                value: "Metrics by week",
+                value: "Weekly intervals",
                 comment: "Display text for the weekly granularity of store stats on the My Store screen"
             )
         case .monthly:
             NSLocalizedString(
                 "statsGranularityV4.monthlyMetrics",
-                value: "Metrics by month",
+                value: "Monthly intervals",
                 comment: "Display text for the monthly granularity of store stats on the My Store screen"
             )
         case .yearly:
             NSLocalizedString(
                 "statsGranularityV4.yearlyMetrics",
-                value: "Metrics by year",
+                value: "Yearly intervals",
                 comment: "Display text for the yearly granularity of store stats on the My Store screen"
             )
         }

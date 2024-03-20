6cfdf8ee8ad3546ae196bad4f2bc8133c36081e8
Rachel McRoberts <rachel@automattic.com>
2024-03-20 13:16:55 +0000

    Add support for syncing customers with infinite scroll

--- WooCommerce/Classes/ViewRelated/Customers/CustomersListViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Customers/CustomersListViewModel.swift
@@ -3,10 +3,28 @@ import Yosemite
 import protocol Storage.StorageManagerType
 
 final class CustomersListViewModel: ObservableObject {
+
     /// Customers to display in customer list.
     @Published private(set) var customers: [WCAnalyticsCustomer] = []
 
+    // MARK: Sync
+
+    /// Current sync status; used to determine the view state.
+    @Published private(set) var syncState: SyncState = .empty
+
+    /// Tracks if the infinite scroll indicator should be displayed.
+    @Published private(set) var shouldShowBottomActivityIndicator = false
+
+    // MARK: Private properties
+
     private let siteID: Int64
+
+    /// Supports infinite scroll.
+    private let paginationTracker: PaginationTracker
+    private let pageFirstIndex: Int = PaginationTracker.Defaults.pageFirstIndex
+
+    /// Stores to sync customers.
+    private let stores: StoresManager
 
     /// Storage to fetch customer list.
     private let storageManager: StorageManagerType
@@ -19,17 +37,65 @@ final class CustomersListViewModel: ObservableObject {
     }()
 
     init(siteID: Int64,
+         stores: StoresManager = ServiceLocator.stores,
          storageManager: StorageManagerType = ServiceLocator.storageManager) {
         self.siteID = siteID
+        self.stores = stores
         self.storageManager = storageManager
+        self.paginationTracker = PaginationTracker(pageFirstIndex: pageFirstIndex)
 
         configureResultsController()
+        configurePaginationTracker()
+    }
+
+    /// Called when the next page should be loaded.
+    func onLoadNextPageAction() {
+        paginationTracker.ensureNextPageIsSynced()
+    }
+
+    /// Called when the user pulls down the list to refresh.
+    /// - Parameter completion: called when the refresh completes.
+    func onRefreshAction(completion: @escaping () -> Void) {
+        paginationTracker.resync(reason: nil) {
+            completion()
+        }
+    }
+}
+
+// MARK: - Remote Sync
+
+extension CustomersListViewModel: PaginationTrackerDelegate {
+    /// Syncs the first page of customers from remote.
+    func loadCustomers() {
+        paginationTracker.syncFirstPage()
+    }
+
+    /// Syncs the given page of customers from remote.
+    func sync(pageNumber: Int, pageSize: Int, reason: String?, onCompletion: SyncCompletion?) {
+        transitionToSyncingState()
+        let action = CustomerAction.synchronizeAllCustomers(siteID: siteID, pageNumber: pageNumber, pageSize: pageSize) { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case let .success(hasNextPage):
+                onCompletion?(.success(hasNextPage))
+            case let .failure(error):
+                DDLogError("⛔️ Error synchronizing customers: \(error)")
+                onCompletion?(.failure(error))
+            }
+            self.updateResults()
+        }
+        stores.dispatch(action)
     }
 }
 
 // MARK: - Configuration
 
 private extension CustomersListViewModel {
+    /// Configures pagination tracker for infinite scroll.
+    func configurePaginationTracker() {
+        paginationTracker.delegate = self
+    }
+
     /// Performs initial fetch from storage and updates results.
     func configureResultsController() {
         resultsController.onDidChangeContent = { [weak self] in
@@ -46,8 +112,54 @@ private extension CustomersListViewModel {
         }
     }
 
-    /// Updates row view models and sync state.
+    /// Updates customers and sync state.
     func updateResults() {
         customers = resultsController.fetchedObjects
+        transitionToResultsUpdatedState()
+    }
+}
+
+// MARK: - State Machine
+
+extension CustomersListViewModel {
+    /// Represents possible states for syncing customers list.
+    enum SyncState: Equatable {
+        case syncingFirstPage
+        case results
+        case empty
+    }
+
+    /// Update states for sync from remote.
+    func transitionToSyncingState() {
+        shouldShowBottomActivityIndicator = true
+        if customers.isEmpty {
+            syncState = .syncingFirstPage
+        }
+    }
+
+    /// Update states after sync is complete.
+    func transitionToResultsUpdatedState() {
+        shouldShowBottomActivityIndicator = false
+        syncState = customers.isNotEmpty ? .results: .empty
+    }
+
+    /// View models for placeholder rows.
+    static let placeholderRows: [WCAnalyticsCustomer] = [Int64](0..<3).map {
+        // The content does not matter because the text in placeholder rows is redacted.
+        WCAnalyticsCustomer(siteID: 0,
+                            customerID: $0,
+                            userID: 0,
+                            name: nil,
+                            email: nil,
+                            username: nil,
+                            dateRegistered: nil,
+                            dateLastActive: Date(),
+                            ordersCount: 0,
+                            totalSpend: 0,
+                            averageOrderValue: 0,
+                            country: "",
+                            region: "",
+                            city: "",
+                            postcode: "")
     }
 }

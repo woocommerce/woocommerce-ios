56534b93413a50688686bc5aaa0a32b131ddd99e
Rachel McRoberts <rachel@automattic.com>
2024-03-20 17:53:48 +0000

    Add customer sync unit tests

--- WooCommerce/Classes/ViewRelated/Customers/CustomersListViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Customers/CustomersListViewModel.swift
@@ -131,6 +131,7 @@ private extension CustomersListViewModel {
 
         do {
             try resultsController.performFetch()
+            updateResults()
         } catch {
             ServiceLocator.crashLogging.logError(error)
         }

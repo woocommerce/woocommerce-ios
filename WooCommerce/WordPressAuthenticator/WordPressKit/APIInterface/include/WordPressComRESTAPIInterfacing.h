643b387b7a1dcc2d8798739a67457e0840d97e9a
Gio Lodi <gio@mokacoding.com>
2024-03-21 12:47:09 +1100

    Define `WordPressComRESTAPIInterfacing` abstraction with `GET` method
    Use it in one call withing `AccountServiceRemoteREST`. The tests verify
    it works as expected.

---
+++ WooCommerce/WordPressAuthenticator/WordPressKit/APIInterface/include/WordPressComRESTAPIInterfacing.h
@@ -0,0 +1,10 @@
+@import Foundation;
+
+@protocol WordPressComRESTAPIInterfacing
+
+- (void)get:(NSString * _Nonnull)URLString
+ parameters:(NSDictionary<NSString *, NSObject *> * _Nullable)parameters
+    success:(void (^ _Nonnull)(id _Nonnull, NSHTTPURLResponse * _Nullable))success
+    failure:(void (^ _Nonnull)(NSError * _Nonnull, NSHTTPURLResponse * _Nullable))failure;
+
+@end

88ec60301ea8db7d61a1cd02446358fc392ba983
Gio Lodi <gio@mokacoding.com>
2024-03-21 12:56:55 +1100

    Add POST method to `WordPressComRESTAPIInterfacing`
    Used for all POST requests in `AccountServiceRemoteREST`

--- WooCommerce/WordPressAuthenticator/WordPressKit/APIInterface/include/WordPressComRESTAPIInterfacing.h
+++ WooCommerce/WordPressAuthenticator/WordPressKit/APIInterface/include/WordPressComRESTAPIInterfacing.h
@@ -7,4 +7,9 @@
     success:(void (^ _Nonnull)(id _Nonnull, NSHTTPURLResponse * _Nullable))success
     failure:(void (^ _Nonnull)(NSError * _Nonnull, NSHTTPURLResponse * _Nullable))failure;
 
+- (void)post:(NSString * _Nonnull)URLString
+  parameters:(NSDictionary<NSString *, NSObject *> * _Nullable)parameters
+     success:(void (^ _Nonnull)(id _Nonnull, NSHTTPURLResponse * _Nullable))success
+     failure:(void (^ _Nonnull)(NSError * _Nonnull, NSHTTPURLResponse * _Nullable))failure;
+
 @end

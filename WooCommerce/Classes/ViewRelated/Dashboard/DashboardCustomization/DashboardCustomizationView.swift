33a81f05f89d8db7c6ec37574e5e61c7b904da33
Huong Do <hi.huongdo@gmail.com>
2024-04-09 10:03:08 +0700

    Add navigation bar and localization for dashboard customization

--- WooCommerce/Classes/ViewRelated/Dashboard/DashboardCustomization/DashboardCustomizationView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/DashboardCustomization/DashboardCustomizationView.swift
@@ -4,13 +4,41 @@ import SwiftUI
 /// 
 struct DashboardCustomizationView: View {
     @ObservedObject private var viewModel: DashboardCustomizationViewModel
+    @Environment(\.dismiss) private var dismiss
 
     init(viewModel: DashboardCustomizationViewModel) {
         self.viewModel = viewModel
     }
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+            .toolbar(content: {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button {
+                        // TODO: save changes
+                        dismiss()
+                    } label: {
+                        Text(Localization.saveButton)
+                    }
+//                    .disabled(!viewModel.hasChanges)
+                }
+            })
+            .navigationTitle(Localization.title)
+            .navigationBarTitleDisplayMode(.inline)
+            .background(Color(uiColor: .listBackground))
+//            .closeButtonWithDiscardChangesPrompt(hasChanges: viewModel.hasChanges)
+    }
+}
+
+// MARK: - Constants
+private extension DashboardCustomizationView {
+    enum Localization {
+        static let title = NSLocalizedString("dashboardCustomization.title",
+                                             value: "Customize Dashboard",
+                                             comment: "Title for the screen to customize the dashboard screen")
+        static let saveButton = NSLocalizedString("dashboardCustomization.saveButton",
+                                                  value: "Save",
+                                                  comment: "Button to save changes on the Customize Dashboard screen")
     }
 }
 

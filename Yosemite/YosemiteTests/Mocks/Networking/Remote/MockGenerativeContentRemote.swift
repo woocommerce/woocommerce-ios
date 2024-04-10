c0f37f77d648a48eca7312c53d231acf4dd49c4d
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-04-10 17:04:30 +0530

    Send response format parameter while generating text using AI.

--- Yosemite/YosemiteTests/Mocks/Networking/Remote/MockGenerativeContentRemote.swift
+++ Yosemite/YosemiteTests/Mocks/Networking/Remote/MockGenerativeContentRemote.swift
@@ -6,6 +6,7 @@ import XCTest
 final class MockGenerativeContentRemote {
     private(set) var generateTextBase: String?
     private(set) var generateTextFeature: GenerativeContentRemoteFeature?
+    private(set) var generateTextResponseFormat: GenerativeContentRemoteResponseFormat?
 
     /// The results to return in `generateText`.
     private var generateTextResult: Result<String, Error>?
@@ -38,9 +39,11 @@ final class MockGenerativeContentRemote {
 extension MockGenerativeContentRemote: GenerativeContentRemoteProtocol {
     func generateText(siteID: Int64,
                       base: String,
-                      feature: GenerativeContentRemoteFeature) async throws -> String {
+                      feature: GenerativeContentRemoteFeature,
+                      responseFormat: GenerativeContentRemoteResponseFormat) async throws -> String {
         generateTextBase = base
         generateTextFeature = feature
+        generateTextResponseFormat = responseFormat
         guard let result = generateTextResult else {
             XCTFail("Could not find result for generating text.")
             throw NetworkError.notFound()

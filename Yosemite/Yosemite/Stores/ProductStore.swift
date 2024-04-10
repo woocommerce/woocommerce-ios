9ad7c8371cdcaad9cdfcea929cea181feaabba79
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-04-10 17:07:37 +0530

    Send text as response format for AI generation.

--- Yosemite/Yosemite/Stores/ProductStore.swift
+++ Yosemite/Yosemite/Stores/ProductStore.swift
@@ -597,7 +597,7 @@ private extension ProductStore {
 
         Task { @MainActor in
             let result = await Result {
-                let description = try await generativeContentRemote.generateText(siteID: siteID, base: prompt, feature: .productDescription)
+                let description = try await generativeContentRemote.generateText(siteID: siteID, base: prompt, feature: .productDescription, responseFormat: .text)
                 return description
             }
             completion(result)
@@ -624,7 +624,7 @@ private extension ProductStore {
 
         Task { @MainActor in
             let result = await Result {
-                let message = try await generativeContentRemote.generateText(siteID: siteID, base: prompt, feature: .productSharing)
+                let message = try await generativeContentRemote.generateText(siteID: siteID, base: prompt, feature: .productSharing, responseFormat: .text)
                     .trimmingCharacters(in: CharacterSet(["\""]))  // Trims quotation mark
                 return message
             }
@@ -685,7 +685,7 @@ private extension ProductStore {
 
         Task { @MainActor in
             let result = await Result {
-                let description = try await generativeContentRemote.generateText(siteID: siteID, base: prompt, feature: .productName)
+                let description = try await generativeContentRemote.generateText(siteID: siteID, base: prompt, feature: .productName, responseFormat: .text)
                 return description
             }
             completion(result)

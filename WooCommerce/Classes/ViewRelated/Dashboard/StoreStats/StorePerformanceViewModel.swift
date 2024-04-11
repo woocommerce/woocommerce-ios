50d805daa3f2793a0ce6fb773e9258d28c906228
Huong Do <hi.huongdo@gmail.com>
2024-04-11 13:20:30 +0700

    Add initializer for StorePerformanceViewModel

--- WooCommerce/Classes/ViewRelated/Dashboard/StoreStats/StorePerformanceViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/StoreStats/StorePerformanceViewModel.swift
@@ -5,4 +5,13 @@ import Yosemite
 ///
 final class StorePerformanceViewModel: ObservableObject {
     @Published private(set) var timeRange = StatsTimeRangeV4.today
+
+    private let siteID: Int64
+    private let stores: StoresManager
+
+    init(siteID: Int64,
+         stores: StoresManager = ServiceLocator.stores) {
+        self.siteID = siteID
+        self.stores = stores
+    }
 }

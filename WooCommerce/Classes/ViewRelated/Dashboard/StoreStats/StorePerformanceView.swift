0e797091fd7c82c39593f55f2da2b657f0efda56
Huong Do <hi.huongdo@gmail.com>
2024-04-11 11:54:16 +0700

    Add header view

--- WooCommerce/Classes/ViewRelated/Dashboard/StoreStats/StorePerformanceView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/StoreStats/StorePerformanceView.swift
@@ -10,7 +10,55 @@ struct StorePerformanceView: View {
     }
 
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            header
+        }
+        .padding(Layout.padding)
+        .overlay {
+            RoundedRectangle(cornerRadius: Layout.cornerRadius)
+                .stroke(Color(uiColor: .separator), lineWidth: Layout.strokeWidth)
+        }
+    }
+}
+
+private extension StorePerformanceView {
+    var header: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(Localization.title)
+                    .headlineStyle()
+            }
+            Spacer()
+            Menu {
+                Button(Localization.hideCard) {
+                    // TODO
+                }
+            } label: {
+                Image(systemName: "ellipsis")
+                    .foregroundStyle(Color.secondary)
+            }
+        }
+    }
+}
+
+private extension StorePerformanceView {
+    enum Layout {
+        static let padding: CGFloat = 16
+        static let cornerRadius: CGFloat = 8
+        static let strokeWidth: CGFloat = 0.5
+    }
+
+    enum Localization {
+        static let title = NSLocalizedString(
+            "storePerformanceView.title",
+            value: "Performance",
+            comment: "Title of the store performance section on the Dashboard screen"
+        )
+        static let hideCard = NSLocalizedString(
+            "storePerformanceView.hideCard",
+            value: "Hide this card",
+            comment: "Menu item to dismiss the store performance section on the Dashboard screen"
+        )
     }
 }
 

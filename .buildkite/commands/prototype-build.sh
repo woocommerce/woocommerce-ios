e7c400a458154de078773e1b6de1e563755b9c79
Olivier Halligon <olivier.halligon@automattic.com>
2024-04-15 18:23:56 +0200

    Revert "[TO REVERT] Dummy commit to test the change"
    This reverts commit c407476936a138346e8834924dbe3189cfbc6dab.

--- .buildkite/commands/prototype-build.sh
+++ .buildkite/commands/prototype-build.sh
@@ -12,8 +12,5 @@ install_swiftpm_dependencies
 echo "--- :closed_lock_with_key: Installing Secrets"
 bundle exec fastlane run configure_apply
 
-echo "--- Test unstable internal pods annotation"
-bundle exec fastlane test_check_pods_references
-
 echo "--- :hammer_and_wrench: Building"
 bundle exec fastlane build_and_upload_prototype_build

8f3004cebd1dec8cc9a5e27d3562353394c0d9c1
Huong Do <hi.huongdo@gmail.com>
2024-04-16 13:23:06 +0700

    Add tests for statsVersion and siteVisitStatMode

--- WooCommerce/WooCommerceTests/ViewRelated/Dashboard/StoreStats/StorePerformanceViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Dashboard/StoreStats/StorePerformanceViewModelTests.swift
@@ -4,6 +4,7 @@ import enum Storage.StatsVersion
 import enum Networking.DotcomError
 @testable import WooCommerce
 
+@MainActor
 final class StorePerformanceViewModelTests: XCTestCase {
 
     func test_dates_for_custom_range_are_correct_for_non_custom_time_range() throws {
@@ -100,6 +101,149 @@ final class StorePerformanceViewModelTests: XCTestCase {
 
         // Then
         XCTAssertEqual(savedTimeRange, .thisYear)
+    }
+
+    func test_statsVersion_is_updated_correctly_when_sync_stats_failed_with_noRestRoute_error() async {
+        // Given
+        let stores = MockStoresManager(sessionManager: .makeForTesting())
+        let viewModel = StorePerformanceViewModel(siteID: 123, stores: stores, usageTracksEventEmitter: .init())
+        mockSyncAllStats(with: stores, toReturn: .v3)
+        XCTAssertEqual(viewModel.statsVersion, .v4) // Initial value
+
+        // When
+        await viewModel.reloadData()
+
+        // Then
+        XCTAssertEqual(viewModel.statsVersion, .v3)
+    }
+
+    func test_siteVisitStatMode_is_default_if_syncing_stats_succeeds_for_non_custom_time_range() async {
+        // Given
+        let stores = MockStoresManager(sessionManager: .makeForTesting())
+        let viewModel = StorePerformanceViewModel(siteID: 123, stores: stores, usageTracksEventEmitter: .init())
+        mockSyncAllStats(with: stores)
+
+        // When
+        viewModel.didSelectTimeRange(.thisMonth)
+        await viewModel.reloadData()
+
+        // Then
+        XCTAssertEqual(viewModel.siteVisitStatMode, .default)
+    }
+
+    func test_siteVisitStatMode_is_default_if_syncing_stats_succeeds_with_custom_time_range_of_same_day_for_jetpack_site() async throws {
+        // Given
+        let defaultSite = Site.fake().copy(isJetpackThePluginInstalled: true,
+                                           isJetpackConnected: true)
+        let stores = MockStoresManager(sessionManager: .makeForTesting(defaultSite: defaultSite))
+        let viewModel = StorePerformanceViewModel(siteID: 123, stores: stores, usageTracksEventEmitter: .init())
+        mockSyncAllStats(with: stores)
+
+        // When
+        let endDate = Date().endOfDay(timezone: .current)
+        let startDate = Date().startOfDay(timezone: .current)
+        viewModel.didSelectTimeRange(.custom(from: startDate, to: endDate))
+        await viewModel.reloadData()
+
+        // Then
+        XCTAssertEqual(viewModel.siteVisitStatMode, .default)
+    }
+
+    func test_siteVisitStatMode_is_redactedDueToCustomRange_if_syncing_stats_succeeds_with_custom_time_range_longer_than_1_day_for_jetpack_site() async throws {
+        // Given
+        let defaultSite = Site.fake().copy(isJetpackThePluginInstalled: true,
+                                           isJetpackConnected: true)
+        let stores = MockStoresManager(sessionManager: .makeForTesting(defaultSite: defaultSite))
+        let viewModel = StorePerformanceViewModel(siteID: 123, stores: stores, usageTracksEventEmitter: .init())
+        mockSyncAllStats(with: stores)
+
+        // When
+        let endDate = Date()
+        let startDate = try XCTUnwrap(endDate.adding(days: -10))
+        viewModel.didSelectTimeRange(.custom(from: startDate, to: endDate))
+        await viewModel.reloadData()
+
+        // Then
+        XCTAssertEqual(viewModel.siteVisitStatMode, .redactedDueToCustomRange)
+    }
+
+    func test_siteVisitStatMode_is_redactedDueToJetpack_if_syncing_stats_succeeds_with_custom_time_range_for_jcp_site() async throws {
+        // Given
+        let defaultSite = Site.fake().copy(isJetpackThePluginInstalled: false,
+                                           isJetpackConnected: true)
+        let stores = MockStoresManager(sessionManager: .makeForTesting(defaultSite: defaultSite))
+        let viewModel = StorePerformanceViewModel(siteID: 123, stores: stores, usageTracksEventEmitter: .init())
+        mockSyncAllStats(with: stores)
+
+        // When
+        let endDate = Date()
+        let startDate = try XCTUnwrap(endDate.adding(days: -10))
+        viewModel.didSelectTimeRange(.custom(from: startDate, to: endDate))
+        await viewModel.reloadData()
+
+        // Then
+        XCTAssertEqual(viewModel.siteVisitStatMode, .redactedDueToJetpack)
+    }
+
+    func test_siteVisitStatMode_is_hidden_if_syncing_stats_succeeds_with_custom_time_range_for_non_jetpack_site() async throws {
+        // Given
+        let defaultSite = Site.fake().copy(isJetpackThePluginInstalled: false,
+                                           isJetpackConnected: false)
+        let stores = MockStoresManager(sessionManager: .makeForTesting(defaultSite: defaultSite))
+        let viewModel = StorePerformanceViewModel(siteID: 123, stores: stores, usageTracksEventEmitter: .init())
+        mockSyncAllStats(with: stores)
+
+        // When
+        let endDate = Date()
+        let startDate = try XCTUnwrap(endDate.adding(days: -10))
+        viewModel.didSelectTimeRange(.custom(from: startDate, to: endDate))
+        await viewModel.reloadData()
+
+        // Then
+        XCTAssertEqual(viewModel.siteVisitStatMode, .hidden)
+    }
+
+    func test_siteVisitStatMode_is_hidden_if_syncing_stats_failed_with_noPermission_error() async {
+        // Given
+        let stores = MockStoresManager(sessionManager: .makeForTesting())
+        let viewModel = StorePerformanceViewModel(siteID: 123, stores: stores, usageTracksEventEmitter: .init())
+        mockSyncAllStats(with: stores, visitorStatsError: SiteStatsStoreError.noPermission)
+
+        // When
+        await viewModel.reloadData()
+
+        // Then
+        XCTAssertEqual(viewModel.siteVisitStatMode, .hidden)
+    }
+
+    func test_siteVisitStatMode_is_hidden_if_syncing_stats_failed_with_statsModuleDisabled_error_for_non_JCP_site() async {
+        // Given
+        let defaultSite = Site.fake().copy(isJetpackThePluginInstalled: false,
+                                           isJetpackConnected: false)
+        let stores = MockStoresManager(sessionManager: .makeForTesting(defaultSite: defaultSite))
+        let viewModel = StorePerformanceViewModel(siteID: 123, stores: stores, usageTracksEventEmitter: .init())
+        mockSyncAllStats(with: stores, visitorStatsError: SiteStatsStoreError.statsModuleDisabled)
+
+        // When
+        await viewModel.reloadData()
+
+        // Then
+        XCTAssertEqual(viewModel.siteVisitStatMode, .hidden)
+    }
+
+    func test_siteVisitStatMode_is_redactedDueToJetpack_if_syncing_stats_failed_with_statsModuleDisabled_error_for_JCP_site() async {
+        // Given
+        let defaultSite = Site.fake().copy(isJetpackThePluginInstalled: false,
+                                           isJetpackConnected: true)
+        let stores = MockStoresManager(sessionManager: .makeForTesting(defaultSite: defaultSite))
+        let viewModel = StorePerformanceViewModel(siteID: 123, stores: stores, usageTracksEventEmitter: .init())
+        mockSyncAllStats(with: stores, visitorStatsError: SiteStatsStoreError.statsModuleDisabled)
+
+        // When
+        await viewModel.reloadData()
+
+        // Then
+        XCTAssertEqual(viewModel.siteVisitStatMode, .redactedDueToJetpack)
     }
 }
 

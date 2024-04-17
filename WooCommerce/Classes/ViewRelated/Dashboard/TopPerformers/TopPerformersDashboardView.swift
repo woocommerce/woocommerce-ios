39a167d5a27856e995ea707f6c9586c4a7de1bf7
Huong Do <hi.huongdo@gmail.com>
2024-04-17 11:31:37 +0700

    Add new TopPerformersDashboardView

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/TopPerformers/TopPerformersDashboardView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+/// SwiftUI view for the Top Performers dashboard card.
+///
+struct TopPerformersDashboardView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    TopPerformersDashboardView()
+}

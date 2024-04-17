79955da1bb419667ecfd9143ff44dee52a652306
Huong Do <hi.huongdo@gmail.com>
2024-04-17 13:55:31 +0700

    Update UI for TopPerformersDashboardView

--- WooCommerce/Classes/ViewRelated/Dashboard/TopPerformers/TopPerformerDashboardViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/TopPerformers/TopPerformerDashboardViewModel.swift
@@ -1,7 +1,114 @@
-import Foundation
+import WooFoundation
+import Yosemite
+import protocol Storage.StorageManagerType
 
 /// View model for `TopPerformerDashboardView`
 ///
 final class TopPerformerDashboardViewModel: ObservableObject {
-    // TODO
+
+    @Published private(set) var timeRange = StatsTimeRangeV4.today
+    @Published private(set) var syncingData = false
+
+    let siteID: Int64
+    let siteTimezone: TimeZone
+    private let stores: StoresManager
+    private let storageManager: StorageManagerType
+    private let currencyFormatter: CurrencyFormatter
+    private let currencySettings: CurrencySettings
+    private let usageTracksEventEmitter: StoreStatsUsageTracksEventEmitter
+    private let analytics: Analytics
+
+    init(siteID: Int64,
+         siteTimezone: TimeZone = .siteTimezone,
+         stores: StoresManager = ServiceLocator.stores,
+         storageManager: StorageManagerType = ServiceLocator.storageManager,
+         currencyFormatter: CurrencyFormatter = CurrencyFormatter(currencySettings: ServiceLocator.currencySettings),
+         currencySettings: CurrencySettings = ServiceLocator.currencySettings,
+         usageTracksEventEmitter: StoreStatsUsageTracksEventEmitter,
+         analytics: Analytics = ServiceLocator.analytics) {
+        self.siteID = siteID
+        self.stores = stores
+        self.siteTimezone = siteTimezone
+        self.storageManager = storageManager
+        self.currencyFormatter = currencyFormatter
+        self.currencySettings = currencySettings
+        self.analytics = analytics
+        self.usageTracksEventEmitter = usageTracksEventEmitter
+
+        observeTimeRange()
+
+        Task { @MainActor in
+            self.timeRange = await loadLastTimeRange() ?? .today
+        }
+    }
+
+    func didSelectTimeRange(_ newTimeRange: StatsTimeRangeV4) {
+        timeRange = newTimeRange
+        saveLastTimeRange(timeRange)
+        usageTracksEventEmitter.interacted()
+        analytics.track(event: .Dashboard.dashboardMainStatsDate(timeRange: timeRange))
+    }
+}
+
+// MARK: - Data for `TopPerformerDashboardView`
+//
+extension TopPerformerDashboardViewModel {
+    var startDateForCustomRange: Date {
+        if case let .custom(startDate, _) = timeRange {
+            return startDate
+        }
+        return Date(timeInterval: -Constants.thirtyDaysInSeconds, since: endDateForCustomRange) // 30 days before end date
+    }
+
+    var endDateForCustomRange: Date {
+        if case let .custom(_, endDate) = timeRange {
+            return endDate
+        }
+        return Date()
+    }
+
+    var buttonTitleForCustomRange: String? {
+        if case .custom = timeRange {
+            return nil
+        }
+        return Localization.addCustomRange
+    }
+}
+
+// MARK: - Private helpers
+//
+private extension TopPerformerDashboardViewModel {
+    func observeTimeRange() {
+        // TODO
+    }
+
+    @MainActor
+    func loadLastTimeRange() async -> StatsTimeRangeV4? {
+        await withCheckedContinuation { continuation in
+            let action = AppSettingsAction.loadLastSelectedStatsTimeRange(siteID: siteID) { timeRange in
+                continuation.resume(returning: timeRange)
+            }
+            stores.dispatch(action)
+        }
+    }
+
+    func saveLastTimeRange(_ timeRange: StatsTimeRangeV4) {
+        let action = AppSettingsAction.setLastSelectedStatsTimeRange(siteID: siteID, timeRange: timeRange)
+        stores.dispatch(action)
+    }
+}
+
+// MARK: Constants
+//
+private extension TopPerformerDashboardViewModel {
+    enum Constants {
+        static let thirtyDaysInSeconds: TimeInterval = 86400*30
+    }
+    enum Localization {
+        static let addCustomRange = NSLocalizedString(
+            "topPerformerDashboardViewModel.addCustomRange",
+            value: "Add",
+            comment: "Button in date range picker to add a Custom Range tab"
+        )
+    }
 }
--- WooCommerce/Classes/ViewRelated/Dashboard/TopPerformers/TopPerformersDashboardView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/TopPerformers/TopPerformersDashboardView.swift
@@ -1,13 +1,157 @@
 import SwiftUI
+import enum Yosemite.StatsTimeRangeV4
 
 /// SwiftUI view for the Top Performers dashboard card.
 ///
 struct TopPerformersDashboardView: View {
+    @ObservedObject private var viewModel: TopPerformerDashboardViewModel
+    @State private var showingCustomRangePicker = false
+
+    private let onViewAllAnalytics: (_ siteID: Int64,
+                                     _ timeZone: TimeZone,
+                                     _ timeRange: StatsTimeRangeV4) -> Void
+
+    init(viewModel: TopPerformerDashboardViewModel,
+         onViewAllAnalytics: @escaping (Int64, TimeZone, StatsTimeRangeV4) -> Void) {
+        self.viewModel = viewModel
+        self.onViewAllAnalytics = onViewAllAnalytics
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            header
+                .padding(.horizontal, Layout.padding)
+                .redacted(reason: viewModel.syncingData ? [.placeholder] : [])
+                .shimmering(active: viewModel.syncingData)
+
+            timeRangeBar
+                .padding(.horizontal, Layout.padding)
+                .redacted(reason: viewModel.syncingData ? [.placeholder] : [])
+                .shimmering(active: viewModel.syncingData)
+
+            Divider()
+
+            // TODO
+
+            Divider()
+
+            viewAllAnalyticsButton
+                .padding([.top, .horizontal], Layout.padding)
+                .redacted(reason: viewModel.syncingData ? [.placeholder] : [])
+                .shimmering(active: viewModel.syncingData)
+
+        }
+        .padding(.vertical, Layout.padding)
+        .background(Color(.listForeground(modal: false)))
+        .clipShape(RoundedRectangle(cornerSize: Layout.cornerSize))
+        .padding(.horizontal, Layout.padding)
+        .sheet(isPresented: $showingCustomRangePicker) {
+            RangedDatePicker(startDate: viewModel.startDateForCustomRange,
+                             endDate: viewModel.endDateForCustomRange,
+                             datesFormatter: DatesFormatter(),
+                             customApplyButtonTitle: viewModel.buttonTitleForCustomRange,
+                             datesSelected: { start, end in
+                viewModel.didSelectTimeRange(.custom(from: start, to: end))
+            })
+        }
+    }
+}
+
+private extension TopPerformersDashboardView {
+    var header: some View {
+        HStack {
+            Text(Localization.title)
+                .headlineStyle()
+            Spacer()
+            Menu {
+                Button(Localization.hideCard) {
+                    // TODO
+                }
+            } label: {
+                Image(systemName: "ellipsis")
+                    .foregroundStyle(Color.secondary)
+                    .padding(.leading, Layout.padding)
+                    .padding(.vertical, Layout.hideIconVerticalPadding)
+            }
+            .disabled(viewModel.syncingData)
+        }
+    }
+
+    var timeRangeBar: some View {
+        HStack {
+            Text(viewModel.timeRange.tabTitle)
+                .foregroundStyle(Color(.text))
+                .subheadlineStyle()
+            Spacer()
+            StatsTimeRangePicker(currentTimeRange: viewModel.timeRange) { newTimeRange in
+                if newTimeRange.isCustomTimeRange {
+                    showingCustomRangePicker = true
+                } else {
+                    viewModel.didSelectTimeRange(newTimeRange)
+                }
+            }
+            .disabled(viewModel.syncingData)
+        }
+    }
+
+    var viewAllAnalyticsButton: some View {
+        Button {
+            onViewAllAnalytics(viewModel.siteID, viewModel.siteTimezone, viewModel.timeRange)
+        } label: {
+            HStack {
+                Text(Localization.viewAll)
+                Spacer()
+                Image(systemName: "chevron.forward")
+                    .foregroundStyle(Color.secondary)
+                    .fontWeight(.semibold)
+            }
+        }
+    }
+}
+
+private extension TopPerformersDashboardView {
+    enum Layout {
+        static let padding: CGFloat = 16
+        static let cornerSize = CGSize(width: 8.0, height: 8.0)
+        static let strokeWidth: CGFloat = 0.5
+        static let chartViewHeight: CGFloat = 176
+        static let statValuePadding: CGFloat = 8
+        static let redactedViewCornerSize = CGSize(width: 2.0, height: 2.0)
+        static let redactedViewWidth: CGFloat = 32
+        static let redactedViewHeight: CGFloat = 10
+        static let redactedViewIconSize: CGFloat = 14
+        static let redactedViewIconOffset = CGSize(width: 16, height: 0)
+        static let hideIconVerticalPadding: CGFloat = 8
+    }
+
+    enum Localization {
+        static let title = NSLocalizedString(
+            "topPerformersDashboardView.title",
+            value: "Top Performers",
+            comment: "Title of the Top Performers section on the Dashboard screen"
+        )
+        static let hideCard = NSLocalizedString(
+            "topPerformersDashboardView.hideCard",
+            value: "Hide Performance",
+            comment: "Menu item to dismiss the Top Performers section on the Dashboard screen"
+        )
+        static let viewAll = NSLocalizedString(
+            "topPerformersDashboardView.viewAll",
+            value: "View all store analytics",
+            comment: "Button to navigate to Analytics Hub."
+        )
+    }
+
+    /// Specific `DatesFormatter` for the `RangedDatePicker` when presented in the analytics hub module.
+    ///
+    struct DatesFormatter: RangedDateTextFormatter {
+        func format(start: Date, end: Date) -> String {
+            start.formatAsRange(with: end, timezone: .current, calendar: Locale.current.calendar)
+        }
     }
 }
 
 #Preview {
-    TopPerformersDashboardView()
+    TopPerformersDashboardView(viewModel: .init(siteID: 123, usageTracksEventEmitter: .init()),
+                               onViewAllAnalytics: { _, _, _ in })
 }

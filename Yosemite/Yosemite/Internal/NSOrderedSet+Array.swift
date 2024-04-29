cbdbc57510758adf0f7a14ad4b17e47c56342db7
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-04-29 09:41:11 +0700

    Add toTypeCheckedArray extension
    The crash on peaMlT-vd-p2 indicates there could be unexpected data when converting from the NSOrderedSet [Storage.OrderItem] to a Swift array.
    
    The new extension attempts to type check each element that will be passed into the array and assert in debug if something went wrong with the conversion.
    
    Also updates the nil-coalescing fallback type in the read-only convertible implementation when fails to convert, so the empty array is explicitely of type `[Storage.OrderItem]` on compile time, rather than leaving it for type-inference on runtime.

--- Yosemite/Yosemite/Internal/NSOrderedSet+Array.swift
+++ Yosemite/Yosemite/Internal/NSOrderedSet+Array.swift
@@ -8,3 +8,19 @@ extension NSOrderedSet {
         return array
     }
 }
+
+/// Explicitely checks the type for each generic element that will be converted from NSOrderedSet to a Swift array.  Crash and log the error in debug builds if there's a miss-match
+extension NSOrderedSet {
+    func toTypeCheckedArray<T>() -> [T] {
+        var typeCheckedArray = [T]()
+
+        for element in self {
+            guard let typeCheckedElement = element as? T else {
+                assert(false, "Failed conversion. \(element) is not of type \(T.self)")
+                return []
+            }
+            typeCheckedArray.append(typeCheckedElement)
+        }
+        return typeCheckedArray
+    }
+}

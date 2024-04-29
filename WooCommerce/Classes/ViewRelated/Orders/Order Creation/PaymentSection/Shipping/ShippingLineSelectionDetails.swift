5d2d189c1b61ad59aa063a4e0a438d4005b0d932
Rachel McRoberts <rachel@automattic.com>
2024-04-29 15:53:09 +0100

    Add view with new styles for enhanced shipping line details

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/PaymentSection/Shipping/ShippingLineSelectionDetails.swift
@@ -0,0 +1,123 @@
+import SwiftUI
+
+/// View to add/edit a single shipping line in an order, including shipping method selection, with the option to remove it.
+///
+struct ShippingLineSelectionDetails: View {
+
+    /// View model to drive the view content
+    ///
+    @StateObject var viewModel: ShippingLineSelectionDetailsViewModel
+
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        NavigationView {
+            List {
+                // MARK: Amount
+                VStack(alignment: .leading) {
+                    Text(Localization.amountTitle)
+                        .font(.title3)
+                        .foregroundColor(Color(.textSubtle))
+                    FormattableAmountTextField(viewModel: viewModel.formattableAmountViewModel)
+                }
+                .accessibilityIdentifier("add-shipping-amount-field")
+
+                // MARK: Name
+                VStack(alignment: .leading) {
+                    Text(Localization.nameTitle)
+                        .font(.title3)
+                        .foregroundColor(Color(.textSubtle))
+                    TextField(Localization.namePlaceholder, text: $viewModel.methodTitle)
+                        .secondaryTitleStyle()
+                }
+                .accessibilityIdentifier("add-shipping-name-field")
+
+                // MARK: Delete Shipping Button
+                Button(Localization.deleteShippingButton) {
+                    viewModel.didSelectSave(nil)
+                    dismiss()
+                }
+                .foregroundColor(.init(uiColor: .error))
+                .buttonStyle(RoundedBorderedStyle(borderColor: .init(uiColor: .error)))
+                .renderedIf(viewModel.isExistingShippingLine)
+                .listRowSeparator(.hidden, edges: .bottom)
+            }
+            .listStyle(.plain)
+            .safeAreaInset(edge: .bottom, content: {
+                // MARK: Add Shipping Button
+                Button(Localization.doneButton(isEditing: viewModel.isExistingShippingLine)) {
+                    viewModel.saveData()
+                    dismiss()
+                }
+                .buttonStyle(PrimaryButtonStyle())
+                .disabled(!viewModel.enableDoneButton)
+                .padding()
+            })
+            .navigationTitle(viewModel.isExistingShippingLine ? Localization.shipping : Localization.addShipping)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button(Localization.cancel) {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+private extension ShippingLineSelectionDetails {
+    enum Localization {
+        static let addShipping = NSLocalizedString("order.shippingLineDetails.addShippingTitle",
+                                                   value: "Add Shipping",
+                                                   comment: "Title for the Shipping Line screen during order creation")
+        static let shipping = NSLocalizedString("order.shippingLineDetails.shippingTitle",
+                                                value: "Shipping",
+                                                comment: "Title for the Shipping Line Details screen during order creation")
+        static let cancel = NSLocalizedString("order.shippingLineDetails.cancel",
+                                              value: "Cancel",
+                                              comment: "Text for the cancel button in the Shipping Line Details screen")
+
+        static let amountTitle = NSLocalizedString("order.shippingLineDetails.amount",
+                                                 value: "Amount",
+                                                 comment: "Title above the amount field on the Shipping Line Details screen")
+
+        static let nameTitle = NSLocalizedString("order.shippingLineDetails.name",
+                                                 value: "Name",
+                                                 comment: "Title above the name field on the Shipping Line Details screen")
+        static let namePlaceholder = NSLocalizedString("order.shippingLineDetails.namePlaceholder",
+                                                       value: "Shipping",
+                                                       comment: "Placeholder for the name field on the Shipping Line Details screen")
+
+        static func doneButton(isEditing: Bool) -> String {
+            if isEditing {
+                return editShippingButton
+            } else {
+                return addShippingButton
+            }
+        }
+        static let editShippingButton = NSLocalizedString("order.shippingLineDetails.editShipping",
+                                                          value: "Edit Shipping",
+                                                          comment: "Button to edit a shipping line to the order during order creation")
+        static let addShippingButton = NSLocalizedString("order.shippingLineDetails.addShipping",
+                                                         value: "Add Shipping",
+                                                         comment: "Button to add a shipping line to the order during order creation")
+        static let deleteShippingButton = NSLocalizedString("order.shippingLineDetails.removeShipping",
+                                                            value: "Remove Shipping from Order",
+                                                            comment: "Button to remove a shipping line from the order during order creation")
+    }
+}
+
+#Preview("Add shipping") {
+    ShippingLineSelectionDetails(viewModel: ShippingLineSelectionDetailsViewModel(isExistingShippingLine: false,
+                                                                                  initialMethodTitle: "",
+                                                                                  shippingTotal: "",
+                                                                                  didSelectSave: { _ in }))
+}
+
+#Preview("Edit shipping") {
+    ShippingLineSelectionDetails(viewModel: ShippingLineSelectionDetailsViewModel(isExistingShippingLine: true,
+                                                                                  initialMethodTitle: "Shipping",
+                                                                                  shippingTotal: "10.00",
+                                                                                  didSelectSave: { _ in }))
+}
---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/PaymentSection/Shipping/ShippingLineSelectionDetailsViewModel.swift
@@ -0,0 +1,102 @@
+import SwiftUI
+import WooFoundation
+import struct Yosemite.ShippingLine
+import Combine
+
+class ShippingLineSelectionDetailsViewModel: ObservableObject {
+
+    /// Closure to be invoked when the shipping line is updated.
+    ///
+    var didSelectSave: ((ShippingLine?) -> Void)
+
+    /// View model for the amount text field with currency symbol.
+    ///
+    let formattableAmountViewModel: FormattableAmountTextFieldViewModel
+
+    /// Stores the method title entered by the merchant.
+    ///
+    @Published var methodTitle: String
+
+    private let initialAmount: Decimal?
+    private let initialMethodTitle: String
+
+    /// Returns true when existing shipping line is edited.
+    ///
+    let isExistingShippingLine: Bool
+
+    /// Method title entered by user or placeholder if it's empty.
+    ///
+    private var finalMethodTitle: String {
+        methodTitle.isNotEmpty ? methodTitle : Localization.namePlaceholder
+    }
+
+    /// Returns true when there are valid pending changes.
+    ///
+    @Published var enableDoneButton: Bool = false
+
+    init(isExistingShippingLine: Bool,
+         initialMethodTitle: String,
+         shippingTotal: String,
+         locale: Locale = Locale.autoupdatingCurrent,
+         storeCurrencySettings: CurrencySettings = ServiceLocator.currencySettings,
+         didSelectSave: @escaping ((ShippingLine?) -> Void)) {
+        self.isExistingShippingLine = isExistingShippingLine
+        self.initialMethodTitle = initialMethodTitle
+        self.methodTitle = initialMethodTitle
+
+        let currencyFormatter = CurrencyFormatter(currencySettings: storeCurrencySettings)
+        if isExistingShippingLine, let initialAmount = currencyFormatter.convertToDecimal(shippingTotal) {
+            self.initialAmount = initialAmount as Decimal
+        } else {
+            self.initialAmount = nil
+        }
+
+        self.formattableAmountViewModel = FormattableAmountTextFieldViewModel(size: .title2, locale: locale, storeCurrencySettings: storeCurrencySettings)
+        if isExistingShippingLine {
+            formattableAmountViewModel.presetAmount(shippingTotal)
+        }
+
+        self.didSelectSave = didSelectSave
+
+        observeFormattableAmountForUIStates(with: currencyFormatter)
+    }
+
+    func observeFormattableAmountForUIStates(with currencyFormatter: CurrencyFormatter) {
+        // Maps the formatted amount to a boolean indicating whether the amount has been updated with a valid amount.
+        let amountUpdated: AnyPublisher<Bool, Never> = formattableAmountViewModel.$amount.map { [weak self] amount in
+            guard let self, let amountDecimal = currencyFormatter.convertToDecimal(amount) as? Decimal else {
+                return false
+            }
+            return amountDecimal != self.initialAmount
+        }.eraseToAnyPublisher()
+
+        amountUpdated.combineLatest($methodTitle)
+            .map { [weak self] (amountUpdated, methodTitle) in
+                guard let self else { return false }
+                let methodTitleUpdated = methodTitle != self.initialMethodTitle
+                return amountUpdated || methodTitleUpdated
+            }
+            .assign(to: &$enableDoneButton)
+    }
+
+    func saveData() {
+        // TODO-12578: Save selected shipping method ID
+        let shippingLine = ShippingLine(shippingID: 0,
+                                        methodTitle: finalMethodTitle,
+                                        methodID: "other",
+                                        total: formattableAmountViewModel.amount,
+                                        totalTax: "",
+                                        taxes: [])
+        didSelectSave(shippingLine)
+    }
+}
+
+// MARK: Constants
+
+extension ShippingLineSelectionDetailsViewModel {
+    enum Localization {
+        static let namePlaceholder = NSLocalizedString("order.shippingLineDetails.namePlaceholder",
+                                                       value: "Shipping",
+                                                       comment: "Placeholder for the name field on the Shipping Line Details screen in order form")
+    }
+}

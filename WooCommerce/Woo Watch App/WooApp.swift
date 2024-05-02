4e47d250e3aac69897a4fcc07a2cbbc7502199f4
Ernesto Carrion <ernesto.carrion@automattic.com>
2024-05-02 15:59:36 -0500

    Adds initial WooWatchApp setup

---
+++ WooCommerce/Woo Watch App/WooApp.swift
@@ -0,0 +1,18 @@
+//
+//  WooApp.swift
+//  Woo Watch App
+//
+//  Created by Ernesto Carrion on 2/05/24.
+//  Copyright © 2024 Automattic. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Woo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

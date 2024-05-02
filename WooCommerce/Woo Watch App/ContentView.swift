72b88977aab0b1e7adbe61b44e336b1993364c3c
Ernesto Carrion <ernesto.carrion@automattic.com>
2024-05-02 15:59:36 -0500

    Adds initial WooWatchApp setup

---
+++ WooCommerce/Woo Watch App/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Woo Watch App
+//
+//  Created by Ernesto Carrion on 2/05/24.
+//  Copyright © 2024 Automattic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world Woo!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

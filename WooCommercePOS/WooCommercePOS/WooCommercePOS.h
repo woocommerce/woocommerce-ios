0747cc4c7063de1c8f9ecd7993c33699ed7f69d6
Josh Heald <joshheald@users.noreply.github.com>
2024-05-08 16:31:11 +0100

    Add WooCommercePOS framework

---
+++ WooCommercePOS/WooCommercePOS/WooCommercePOS.h
@@ -0,0 +1,18 @@
+//
+//  WooCommercePOS.h
+//  WooCommercePOS
+//
+//  Created by Josh Heald on 08/05/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WooCommercePOS.
+FOUNDATION_EXPORT double WooCommercePOSVersionNumber;
+
+//! Project version string for WooCommercePOS.
+FOUNDATION_EXPORT const unsigned char WooCommercePOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WooCommercePOS/PublicHeader.h>
+
+

4fce3b33ad9bd3d0edfb0ffef7857d1a6cd6a63e
Ernesto Carrion <ernesto.carrion@automattic.com>
2024-05-09 00:35:32 -0500

    Extract watch dependencies to it's own object together with their serialization

--- WooCommerce/Classes/System/WatchDependencies.swift
+++ WooCommerce/Classes/System/WatchDependencies.swift
@@ -1,5 +1,9 @@
 import Foundation
 
+#if canImport(Networking)
+import enum Networking.Credentials
+#endif
+
 /// WatchOS session dependencies.
 ///
 public struct WatchDependencies {
@@ -17,6 +21,45 @@ public struct WatchDependencies {
 
     let storeID: Int64?
     let credentials: Credentials?
+
+    public init(storeID: Int64?, credentials: Credentials?) {
+        self.storeID = storeID
+        self.credentials = credentials
+    }
+
+    /// Create Dependencies from a serialized dictionary.
+    public init(dictionary: [String: Any]) {
+        let storeID: Int64? = {
+            guard let storeDic = dictionary[Keys.store] as? [String: Int64] else {
+                return nil
+            }
+
+            return storeDic[Keys.id]
+        }()
+
+        let credentials: Credentials? = {
+            guard let credentialsDic = dictionary[Keys.credentials] as? [String: String],
+                  let type = credentialsDic[Keys.type],
+                  let username = credentialsDic[Keys.username],
+                  let secret = credentialsDic[Keys.secret],
+                  let siteAddress = credentialsDic[Keys.address] else {
+                return nil
+            }
+
+            switch type {
+            case "AuthenticationType.wpcom":
+                return .wpcom(username: username, authToken: secret, siteAddress: siteAddress)
+            case "AuthenticationType.wporg":
+                return .wporg(username: username, password: secret, siteAddress: siteAddress)
+            case "AuthenticationType.applicationPassword":
+                return .applicationPassword(username: username, password: secret, siteAddress: siteAddress)
+            default:
+                return nil
+            }
+        }()
+
+        self.init(storeID: storeID, credentials: credentials)
+    }
 
     /// Dictionary to be transferred between sessions.
     ///

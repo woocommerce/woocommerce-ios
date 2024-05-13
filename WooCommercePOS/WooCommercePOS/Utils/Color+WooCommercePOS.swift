31f518b4ed2110cae8d48a9e24aa834e0be2923e
Božidar Ševo <bozidarsevo@gmail.com>
2024-05-13 11:18:42 +0200

    Create Color+WooCommercePOS.swift

---
+++ WooCommercePOS/WooCommercePOS/Utils/Color+WooCommercePOS.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+extension Color {
+
+    /// Default POS background color
+    ///
+    static var posBackground: Color {
+        return Color(red: 11, green: 146, blue: 213)
+    }
+
+    /// Default POS foreground color
+    ///
+    static var posForeground: Color {
+        return Color(red: 89, green: 181, blue: 227)
+    }
+
+    /// Default POS fields color
+    ///
+    static var posFields: Color {
+        return Color(red: 142, green: 208, blue: 240)
+    }
+
+    /// Default POS text color
+    ///
+    static var posText: Color {
+        return Color.primary
+    }
+}

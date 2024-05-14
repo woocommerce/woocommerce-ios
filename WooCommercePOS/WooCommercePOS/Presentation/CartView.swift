55d6bea206220fcd279d16f6d26bf81975491ef0
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-05-14 18:29:48 +0700

    Update components background colors

--- WooCommercePOS/WooCommercePOS/Presentation/CartView.swift
+++ WooCommercePOS/WooCommercePOS/Presentation/CartView.swift
@@ -9,15 +9,19 @@ struct CartView: View {
 
     var body: some View {
         VStack {
+            Text("Cart")
+                .foregroundColor(Color.white)
             ForEach(viewModel.productsInCart, id: \.product.productID) { cartProduct in
                 ProductRowView(cartProduct: cartProduct) {
                     viewModel.removeProductFromCart(cartProduct)
                 }
+                .background(Color.tertiaryBackground)
             }
             Spacer()
             Button("Pay now") {
                 viewModel.submitCart()
             }
+            .background(Color.secondaryBackground)
         }
         .background(Color.secondaryBackground)
     }
--- WooCommercePOS/WooCommercePOS/Presentation/ProductGridView.swift
+++ WooCommercePOS/WooCommercePOS/Presentation/ProductGridView.swift
@@ -12,13 +12,13 @@ struct ProductGridView: View {
                                         count: viewModel.products.count)
 
         ScrollView {
-            LazyVGrid(columns: columns, spacing: 20) {
+            LazyVGrid(columns: columns, spacing: 8) {
                 ForEach(viewModel.products, id: \.productID) { product in
                     ProductCardView(product: product) {
                         viewModel.addProductToCart(product)
                     }
                     .foregroundColor(Color.primaryText)
-                    .background(Color.tertiaryBackground)
+                    .background(Color.secondaryBackground)
                 }
             }
         }

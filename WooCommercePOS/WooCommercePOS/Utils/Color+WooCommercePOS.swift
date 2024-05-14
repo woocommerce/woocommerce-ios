7ac1fe5bc16e8de182f3c34418cbff8b57dd6bfb
Božidar Ševo <bozidarsevo@gmail.com>
2024-05-14 10:01:55 +0200

    Update Color+WooCommercePOS.swift
    Co-authored-by: Gabriel Maldonado <gabriel.maldonado@automattic.com>

--- WooCommercePOS/WooCommercePOS/Utils/Color+WooCommercePOS.swift
+++ WooCommercePOS/WooCommercePOS/Utils/Color+WooCommercePOS.swift
@@ -4,25 +4,25 @@ extension Color {
 
     /// Default POS background color
     ///
-    static var posBackground: Color {
+    static var background: Color {
         return Color(red: 11, green: 146, blue: 213)
     }
 
     /// Default POS foreground color
     ///
-    static var posForeground: Color {
+    static var foreground: Color {
         return Color(red: 89, green: 181, blue: 227)
     }
 
     /// Default POS fields color
     ///
-    static var posFields: Color {
+    static var fields: Color {
         return Color(red: 142, green: 208, blue: 240)
     }
 
     /// Default POS text color
     ///
-    static var posText: Color {
+    static var primaryText: Color {
         return Color.primary
     }
 }

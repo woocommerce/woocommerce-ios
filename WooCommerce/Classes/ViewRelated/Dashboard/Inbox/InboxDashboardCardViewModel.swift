d8814545358eb50427a79132215517fe4be8c005
Huong Do <hi.huongdo@gmail.com>
2024-05-14 18:23:39 +0700

    Reuse `InboxViewModel` in `InboxDashboardCardViewModel`

--- WooCommerce/Classes/ViewRelated/Dashboard/Inbox/InboxDashboardCardViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Inbox/InboxDashboardCardViewModel.swift
@@ -9,26 +9,49 @@ final class InboxDashboardCardViewModel: ObservableObject {
     // Set externally to trigger callback upon hiding the Inbox card.
     var onDismiss: (() -> Void)?
 
-    @Published private(set) var syncingData = false
-    @Published private(set) var syncingError: Error?
+    @Published private(set) var syncState: InboxViewModel.SyncState = .empty
+
+    /// View models for inbox note rows.
+    @Published private(set) var noteRowViewModels: [InboxNoteRowViewModel] = []
 
     private let siteID: Int64
-    private let stores: StoresManager
-    private let storage: StorageManagerType
     private let analytics: Analytics
+
+    let contentViewModel: InboxViewModel
 
     init(siteID: Int64,
          stores: StoresManager = ServiceLocator.stores,
          storage: StorageManagerType = ServiceLocator.storageManager,
          analytics: Analytics = ServiceLocator.analytics) {
         self.siteID = siteID
-        self.stores = stores
-        self.storage = storage
         self.analytics = analytics
+        self.contentViewModel = InboxViewModel(siteID: siteID, pageSize: 3, stores: stores, storageManager: storage)
+
+        forwardStatesFromContentViewModel()
+    }
+
+    @MainActor
+    func reloadData() async {
+        await withCheckedContinuation { continuation in
+            contentViewModel.onRefreshAction {
+                continuation.resume()
+            }
+        }
     }
 
     func dismissInbox() {
         // TODO: add tracking
         onDismiss?()
+    }
+}
+
+// MARK: - Private helpers
+private extension InboxDashboardCardViewModel {
+    func forwardStatesFromContentViewModel() {
+        contentViewModel.$noteRowViewModels
+            .assign(to: &$noteRowViewModels)
+
+        contentViewModel.$syncState
+            .assign(to: &$syncState)
     }
 }

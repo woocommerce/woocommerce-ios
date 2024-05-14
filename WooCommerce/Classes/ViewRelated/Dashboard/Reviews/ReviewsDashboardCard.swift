0a9e48b2e4ca68e3e124dbf275b15583aed675de
Hafiz Rahman <hfzrhmn@gmail.com>
2024-05-14 17:19:15 +0700

    Initial UI with dummy data.

--- WooCommerce/Classes/ViewRelated/Dashboard/Reviews/ReviewsDashboardCard.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Reviews/ReviewsDashboardCard.swift
@@ -1,10 +1,24 @@
 import SwiftUI
 import struct Yosemite.DashboardCard
+import struct Yosemite.ProductReview
 
 /// SwiftUI view for the Reviews dashboard card
 ///
 struct ReviewsDashboardCard: View {
     private let viewModel: ReviewsDashboardCardViewModel
+
+    let dummyData: [ProductReview] = [
+        ProductReview(siteID: 1, reviewID: 1, productID: 1, dateCreated: Date(),
+                      statusKey: "", reviewer: "Sherlock", reviewerEmail: "", reviewerAvatarURL: "",
+                      review: "The best product in the whole world. This is meant to be really long to be more than two lines",
+                      rating: 5, verified: true),
+        ProductReview(siteID: 1, reviewID: 2, productID: 1, dateCreated: Date(),
+                      statusKey: "", reviewer: "Holmes", reviewerEmail: "", reviewerAvatarURL: "",
+                      review: "Amazing!", rating: 5, verified: true),
+        ProductReview(siteID: 1, reviewID: 3, productID: 1, dateCreated: Date(),
+                      statusKey: "", reviewer: "Agatha", reviewerEmail: "", reviewerAvatarURL: "",
+                      review: "Great!", rating: 5, verified: true)
+    ]
 
     init(viewModel: ReviewsDashboardCardViewModel) {
         self.viewModel = viewModel
@@ -20,6 +34,14 @@ struct ReviewsDashboardCard: View {
                 .padding(.horizontal, Layout.padding)
                 .redacted(reason: viewModel.syncingData ? [.placeholder] : [])
                 .shimmering(active: viewModel.syncingData)
+
+            ForEach(Array(dummyData.enumerated()), id: \.element.reviewID) { index, review in
+                ReviewRow(for: review)
+
+                if index != dummyData.count-1 {
+                    Divider()
+                }
+            }
         }
         .padding(.vertical, Layout.padding)
         .background(Color(.listForeground(modal: false)))
@@ -75,6 +97,24 @@ private extension ReviewsDashboardCard {
             } label: {
                 Image(systemName: "line.3.horizontal.decrease")
                     .foregroundStyle(Color.secondary)
+            }
+        }
+    }
+
+    func ReviewRow(for review: ProductReview) -> some View {
+        HStack {
+            Image(systemName: "bubble.fill")
+                .foregroundStyle(Color.secondary)
+
+            VStack(alignment: .leading) {
+                Text("Neil Gaiman left a review on Fallen Angel Candelabra")
+                Text(review.review)
+                    .lineLimit(2)
+                HStack {
+                    ForEach(0..<abs(review.rating), id: \.self) { _ in
+                        Image(systemName: "star.fill")
+                    }
+                }
             }
         }
     }

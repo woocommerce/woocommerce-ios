89fe4b30e2189ed123708496430132386ac7b748
Jaclyn Chen <watertranquil@gmail.com>
2024-05-15 15:45:03 +0800

    Implement simulated happy reader connection flow.

--- WooCommercePOS/WooCommercePOS/Presentation/CardReaderConnection/CardReaderConnectionViewModel.swift
+++ WooCommercePOS/WooCommercePOS/Presentation/CardReaderConnection/CardReaderConnectionViewModel.swift
@@ -6,4 +6,39 @@ final class CardReaderConnectionViewModel: ObservableObject {
     init(state: CardReaderConnectionUIState) {
         self.state = state
     }
+
+    /// TODO: replace this with card reader connection implementation.
+    /// Simulates a successful
+    func checkReaderConnection() {
+        Task { @MainActor in
+            state = .scanningForReader(cancel: { [weak self] in
+                self?.state = .notConnected(search: { [weak self] in
+                    self?.checkReaderConnection()
+                })
+            })
+
+            try await Task.sleep(nanoseconds: UInt64(2 * 1_000_000_000))
+
+            state = .readersFound(readerIDs: ["Mock reader 1"],
+                                  connect: { [weak self] readerID in
+                self?.state = .connectingToReader
+
+                Task { @MainActor in
+                    try await Task.sleep(nanoseconds: UInt64(2 * 1_000_000_000))
+                    self?.state = .connected(readerID: readerID, disconnect: {
+                        // TODO: implement reader disconnection action
+                        self?.state = .notConnected(search: {
+                            self?.checkReaderConnection()
+                        })
+                    })
+                }
+            }, continueSearch: { [weak self] in
+                self?.state = .scanningForReader(cancel: { [weak self] in
+                    self?.state = .notConnected(search: {
+                        self?.checkReaderConnection()
+                    })
+                })
+            })
+        }
+    }
 }

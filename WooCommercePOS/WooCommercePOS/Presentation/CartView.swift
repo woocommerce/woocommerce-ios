8f221c4da995c459bb61b5fb5be4ff23adf96cba
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-05-16 14:55:37 +0700

    Remove unnecessary ViewBuilder macro

--- WooCommercePOS/WooCommercePOS/Presentation/CartView.swift
+++ WooCommercePOS/WooCommercePOS/Presentation/CartView.swift
@@ -35,7 +35,6 @@ struct CartView: View {
 /// View sub-components
 ///
 private extension CartView {
-    @ViewBuilder
     var checkoutButton: some View {
         Button("Checkout") {
             viewModel.submitCart()

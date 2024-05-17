32ceaec95e2fc6a0b5638f94c13ab2364e2d414a
Huong Do <hi.huongdo@gmail.com>
2024-05-17 15:24:39 +0700

    Add tests for edge cases

--- Networking/NetworkingTests/Mapper/ProductStockListMapperTests.swift
+++ Networking/NetworkingTests/Mapper/ProductStockListMapperTests.swift
@@ -31,6 +31,48 @@ final class ProductStockListMapperTests: XCTestCase {
         XCTAssertEqual(item.stockQuantity, 0)
         XCTAssertFalse(item.manageStock)
     }
+
+    func test_product_stock_fields_are_properly_parsed_with_decimal_sku() throws {
+        // When
+        let stock = mapStockItems(from: "product-stock-decimal-sku")
+        let item = try XCTUnwrap(stock.first)
+
+        // Then
+        XCTAssertEqual(item.productID, 2051)
+        XCTAssertEqual(item.name, "貴志川線 1日乘車券")
+        XCTAssertEqual(item.sku, "123")
+        XCTAssertEqual(item.productStockStatus, .outOfStock)
+        XCTAssertEqual(item.stockQuantity, 0)
+        XCTAssertFalse(item.manageStock)
+    }
+
+    func test_product_stock_fields_are_properly_parsed_with_parent_managing_stock() throws {
+        // When
+        let stock = mapStockItems(from: "product-stock-parent-manage-stock")
+        let item = try XCTUnwrap(stock.first)
+
+        // Then
+        XCTAssertEqual(item.productID, 2051)
+        XCTAssertEqual(item.name, "貴志川線 1日乘車券")
+        XCTAssertEqual(item.sku, "")
+        XCTAssertEqual(item.productStockStatus, .outOfStock)
+        XCTAssertEqual(item.stockQuantity, 0)
+        XCTAssertTrue(item.manageStock)
+    }
+
+    func test_product_stock_fields_are_properly_parsed_with_quantity_in_string() throws {
+        // When
+        let stock = mapStockItems(from: "product-stock-string-quantity")
+        let item = try XCTUnwrap(stock.first)
+
+        // Then
+        XCTAssertEqual(item.productID, 2051)
+        XCTAssertEqual(item.name, "貴志川線 1日乘車券")
+        XCTAssertEqual(item.sku, "")
+        XCTAssertEqual(item.productStockStatus, .outOfStock)
+        XCTAssertEqual(item.stockQuantity, 12)
+        XCTAssertFalse(item.manageStock)
+    }
 }
 
 private extension ProductStockListMapperTests {

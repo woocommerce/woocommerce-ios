41ad56ef4eb621d717eda152cd75453a517b3958
Ernesto Carrion <ernesto.carrion@automattic.com>
2024-05-20 15:41:10 -0500

    Main UI for navigation list card

--- WooCommerce/Woo Watch App/Orders/OrdersListView.swift
+++ WooCommerce/Woo Watch App/Orders/OrdersListView.swift
@@ -12,11 +12,59 @@ struct OrdersListView: View {
     }
 
     var body: some View {
-        VStack {
-            Text("Order List...")
-            Button("Go back to my store view") {
-                self.watchTab = .myStore
+        NavigationSplitView() {
+            List() {
+                OrderListCard()
+                OrderListCard()
+                OrderListCard()
+                OrderListCard()
+                OrderListCard()
+                OrderListCard()
+                OrderListCard()
+                OrderListCard()
             }
+            .navigationTitle("Orders")
+            .listStyle(.plain)
+        } detail: {
+            Text("Order Detail")
         }
+    }
+}
+
+struct OrderListCard: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+
+            HStack {
+                Text("25 Feb")
+                Spacer()
+                Text("#1031")
+            }
+            .font(.footnote)
+            .foregroundStyle(.secondary)
+
+            Text("Jemima Kirk")
+                .font(.body)
+
+            Text("$149.50")
+                .font(.body)
+                .bold()
+
+            Text("Pending payment")
+                .font(.footnote)
+                .foregroundStyle(Colors.wooPurple20)
+        }
+        .listRowBackground(
+            LinearGradient(gradient: Gradient(colors: [Colors.wooBackgroundStart, Colors.wooBackgroundEnd]), startPoint: .top, endPoint: .bottom)
+                .cornerRadius(10)
+        )
+    }
+}
+
+private extension OrderListCard {
+    enum Colors {
+        static let wooPurple20 = Color(red: 190/255.0, green: 160/255.0, blue: 242/255.0)
+        static let wooBackgroundStart = Color(red: 69/255.0, green: 43/255.0, blue: 100/255.0)
+        static let wooBackgroundEnd = Color(red: 49/255.0, green: 31/255.0, blue: 71/255.0)
     }
 }
--- WooCommerce/Woo Watch App/WooApp.swift
+++ WooCommerce/Woo Watch App/WooApp.swift
@@ -5,7 +5,7 @@ struct Woo_Watch_AppApp: App {
 
     @StateObject var phoneDependencySynchronizer = PhoneDependenciesSynchronizer()
 
-    @State private var selectedTab = WooWatchTab.myStore
+    @State private var selectedTab = WooWatchTab.ordersList
 
     var body: some Scene {
         WindowGroup {

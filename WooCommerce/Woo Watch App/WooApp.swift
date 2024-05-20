d40ce0dfea16d6a6e1454eb512fb55a7ae568807
Ernesto Carrion <ernesto.carrion@automattic.com>
2024-05-20 11:29:39 -0500

    Watch: Add add tab view interface

--- WooCommerce/Woo Watch App/WooApp.swift
+++ WooCommerce/Woo Watch App/WooApp.swift
@@ -8,11 +8,35 @@ struct Woo_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
             if let dependencies = phoneDependencySynchronizer.dependencies {
-                MyStoreView(dependencies: dependencies)
-                    .environment(\.dependencies, dependencies)
+                TabView {
+                    MyStoreView(dependencies: dependencies)
+                    OrdersListView()
+                }
+                .compatibleVerticalStyle()
+                .environment(\.dependencies, dependencies)
             } else {
                 ConnectView()
             }
         }
+    }
+}
+
+/// Backwards compatible vertical `tabViewStyle` modifier.
+///
+private struct VerticalPageModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        if #available(watchOS 10.0, *) {
+            content
+                .tabViewStyle(.verticalPage)
+        } else {
+            content
+                .tabViewStyle(.carousel)
+        }
+    }
+}
+
+private extension View {
+    func compatibleVerticalStyle() -> some View {
+        self.modifier(VerticalPageModifier())
     }
 }

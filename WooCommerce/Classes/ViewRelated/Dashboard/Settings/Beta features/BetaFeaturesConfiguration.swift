ea463200bb7e822239ff1c8b695c3efdcbf8f633
Jaclyn Chen <watertranquil@gmail.com>
2024-05-22 14:45:00 +0800

    Update BetaFeaturesConfiguration view to use a view model that observes POS eligibility for available features.

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Beta features/BetaFeaturesConfiguration.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Beta features/BetaFeaturesConfiguration.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 import Yosemite
+import protocol Experiments.FeatureFlagService
+import struct Storage.GeneralAppSettingsStorage
 
 final class BetaFeaturesConfigurationViewController: UIHostingController<BetaFeaturesConfiguration> {
 
     init() {
-        super.init(rootView: BetaFeaturesConfiguration())
+        super.init(rootView: BetaFeaturesConfiguration(viewModel: .init()))
     }
 
     required dynamic init?(coder aDecoder: NSCoder) {
@@ -12,14 +14,63 @@ final class BetaFeaturesConfigurationViewController: UIHostingController<BetaFea
     }
 }
 
+final class BetaFeaturesConfigurationViewModel: ObservableObject {
+    @Published private(set) var availableFeatures: [BetaFeature] = []
+    private let appSettings: GeneralAppSettingsStorage
+    private let featureFlagService: FeatureFlagService
+    private let posEligibilityChecker: POSEligibilityChecker
+
+    init(appSettings: GeneralAppSettingsStorage = ServiceLocator.generalAppSettings,
+         featureFlagService: FeatureFlagService = ServiceLocator.featureFlagService,
+         posEligibilityChecker: POSEligibilityChecker = POSEligibilityChecker(cardPresentPaymentsOnboarding: CardPresentPaymentsOnboardingUseCase(),
+                                                                              siteSettings: ServiceLocator.selectedSiteSettings,
+                                                                              currencySettings: ServiceLocator.currencySettings,
+                                                                              featureFlagService: ServiceLocator.featureFlagService)) {
+        self.appSettings = appSettings
+        self.featureFlagService = featureFlagService
+        self.posEligibilityChecker = posEligibilityChecker
+        observePOSEligibilityForAvailableFeatures()
+    }
+
+    func isOn(feature: BetaFeature) -> Binding<Bool> {
+        appSettings.betaFeatureEnabledBinding(feature)
+    }
+}
+
+private extension BetaFeaturesConfigurationViewModel {
+    func observePOSEligibilityForAvailableFeatures() {
+        posEligibilityChecker.$isEligible
+            .map { [weak self] isEligibleForPOS in
+                guard let self else {
+                    return []
+                }
+                return BetaFeature.allCases.filter { betaFeature in
+                    switch betaFeature {
+                        case .viewAddOns:
+                            return true
+                        case .inAppPurchases:
+                            return self.featureFlagService.isFeatureFlagEnabled(.inAppPurchasesDebugMenu)
+                        case .pointOfSale:
+                            return isEligibleForPOS
+                    }
+                }
+            }
+            .assign(to: &$availableFeatures)
+    }
+}
+
 struct BetaFeaturesConfiguration: View {
-    let appSettings = ServiceLocator.generalAppSettings
+    @StateObject private var viewModel: BetaFeaturesConfigurationViewModel
+
+    init(viewModel: BetaFeaturesConfigurationViewModel) {
+        self._viewModel = .init(wrappedValue: viewModel)
+    }
 
     var body: some View {
         List {
-            ForEach(BetaFeature.availableFeatures) { feature in
+            ForEach(viewModel.availableFeatures) { feature in
                 Section(footer: Text(feature.description)) {
-                    TitleAndToggleRow(title: feature.title, isOn: appSettings.betaFeatureEnabledBinding(feature))
+                    TitleAndToggleRow(title: feature.title, isOn: viewModel.isOn(feature: feature))
                 }
             }
         }
@@ -36,7 +87,7 @@ private enum Localization {
 struct BetaFeaturesConfiguration_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            BetaFeaturesConfiguration()
+            BetaFeaturesConfiguration(viewModel: .init())
         }
     }
 }

4e33d0e988c607fa33f24252067ce805827f7a2a
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-05-22 15:51:20 +0700

    Render list of products in-cart within total list

--- WooCommerce/Classes/POS/Presentation/PointOfSaleDashboardView.swift
+++ WooCommerce/Classes/POS/Presentation/PointOfSaleDashboardView.swift
@@ -1,11 +1,27 @@
 import SwiftUI
 
 struct TotalsView: View {
+    @ObservedObject private var viewModel: PointOfSaleDashboardViewModel
+
+    init(viewModel: PointOfSaleDashboardViewModel) {
+        self.viewModel = viewModel
+    }
+
     var body: some View {
         VStack {
             Text("Totals")
                 .font(.title)
                 .foregroundColor(Color.white)
+                ForEach(viewModel.productsInCart, id: \.product.productID) { cartProduct in
+                    VStack {
+                        HStack {
+                            Text("\(cartProduct.quantity) x \(cartProduct.product.name) ")
+                            Spacer()
+                            Text("\(cartProduct.product.price)")
+                        }
+                    }
+                    .foregroundColor(.white)
+                }
             Spacer()
             HStack {
                 Button("Take payment") { debugPrint("Not implemented") }
@@ -59,7 +75,7 @@ struct PointOfSaleDashboardView: View {
                         .background(Color.secondaryBackground)
                         .frame(maxWidth: .infinity)
                     Spacer()
-                    TotalsView()
+                    TotalsView(viewModel: viewModel)
                         .background(Color.secondaryBackground)
                         .frame(maxWidth: .infinity)
                 }

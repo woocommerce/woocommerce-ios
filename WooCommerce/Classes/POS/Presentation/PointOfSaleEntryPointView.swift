4a5ded658dff4ba801e677a8a72dd7eee271ce9d
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-05-23 10:14:22 +0700

    Make pos vm StateObject

--- WooCommerce/Classes/POS/Presentation/PointOfSaleEntryPointView.swift
+++ WooCommerce/Classes/POS/Presentation/PointOfSaleEntryPointView.swift
@@ -1,15 +1,17 @@
 import SwiftUI
 
 struct PointOfSaleEntryPointView: View {
-    @ObservedObject private var viewModel: PointOfSaleDashboardViewModel
+    @StateObject private var viewModel: PointOfSaleDashboardViewModel = PointOfSaleDashboardViewModel(
+        products: POSProductFactory.makeFakeProducts(),
+        cardReaderConnectionViewModel: .init(state: .connectingToReader)
+    )
 
     private let hideAppTabBar: ((Bool) -> Void)
 
     // Necessary to expose the View's entry point to WooCommerce
     // Otherwise the current switch on `HubMenu` where this View is created
     // will error with "No exact matches in reference to static method 'buildExpression'"
-    public init(viewModel: PointOfSaleDashboardViewModel, hideAppTabBar: @escaping ((Bool) -> Void)) {
-        self.viewModel = viewModel
+    public init(hideAppTabBar: @escaping ((Bool) -> Void)) {
         self.hideAppTabBar = hideAppTabBar
     }
 
@@ -26,6 +28,6 @@ struct PointOfSaleEntryPointView: View {
 
 #if DEBUG
 #Preview {
-    PointOfSaleEntryPointView(viewModel: .defaultPreview(), hideAppTabBar: { _ in })
+    PointOfSaleEntryPointView(hideAppTabBar: { _ in })
 }
 #endif

43abd4046767f7d3cb2fd1e34009456acfddf671
Hafiz Rahman <hfzrhmn@gmail.com>
2024-05-29 16:22:41 +0700

    Add new cards notice card.

--- WooCommerce/Classes/ViewRelated/Dashboard/DashboardView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/DashboardView.swift
@@ -245,12 +245,12 @@ private extension DashboardView {
                 }
             }
 
-            if !viewModel.hasOrders {
-                shareStoreCard
+            if viewModel.showNewCardsNotice {
+                newCardsNoticeCard
             }
 
-            if viewModel.showNewCardsNotice {
-                // TODO show new cards notice
+            if !viewModel.hasOrders {
+                shareStoreCard
             }
         }
     }
@@ -289,6 +289,31 @@ private extension DashboardView {
             RoundedRectangle(cornerRadius: Layout.cornerRadius)
                 .stroke(Color(.border), lineWidth: 1)
         )
+        .padding(.horizontal, Layout.padding)
+    }
+
+    var newCardsNoticeCard: some View {
+        VStack(spacing: Layout.padding) {
+            Text(Localization.NewCardsNoticeCard.title)
+                .headlineStyle()
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, Layout.elementPadding)
+                .padding(.top, Layout.padding)
+
+            Text(Localization.NewCardsNoticeCard.subtitle)
+                .bodyStyle()
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, Layout.elementPadding)
+
+            Button(Localization.NewCardsNoticeCard.addSectionsButtonLabel) {
+                viewModel.showCustomizationScreen()
+            }
+            .buttonStyle(PrimaryButtonStyle())
+            .padding(.horizontal, Layout.elementPadding)
+            .padding(.bottom, Layout.padding)
+        }
+        .background(Color(.listForeground(modal: false)))
+        .clipShape(RoundedRectangle(cornerSize: Layout.cornerSize))
         .padding(.horizontal, Layout.padding)
     }
 
@@ -349,6 +374,7 @@ private extension DashboardView {
         static let imagePadding: CGFloat = 40
         static let textPadding: CGFloat = 8
         static let cornerRadius: CGFloat = 8
+        static let cornerSize = CGSize(width: 8.0, height: 8.0)
         static let dotBadgePadding = EdgeInsets(top: 6, leading: 0, bottom: 0, trailing: 2)
         static let dotBadgeSize: CGFloat = 6
         static let dotBadgeOffset = CGSize(width: 7, height: -7)
@@ -393,6 +419,26 @@ private extension DashboardView {
                 "dashboardView.shareStoreCard.shareButtonLabel",
                 value: "Share Your Store",
                 comment: "Label of the button to share the store"
+            )
+        }
+
+        enum NewCardsNoticeCard {
+            static let title = NSLocalizedString(
+                "dashboardView.newCardsNoticeCard.title",
+                value: "Looking for more insights?",
+                comment: "Title of the New Cards Notice card"
+            )
+
+            static let subtitle = NSLocalizedString(
+                "dashboardView.newCardsNoticeCard.subtitle",
+                value: "Add new sections to customize your store management experience",
+                comment: "Subtitle of the New Cards Notice card"
+            )
+
+            static let addSectionsButtonLabel = NSLocalizedString(
+                "dashboardView.newCardsNoticeCard.addSectionsButtonLabel",
+                value: "Add new sections",
+                comment: "Label of the button to add sections"
             )
         }
     }

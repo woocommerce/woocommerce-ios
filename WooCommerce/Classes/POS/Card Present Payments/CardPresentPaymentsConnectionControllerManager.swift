004d8303537456212acab28cdb01d949e722b348
Josh Heald <joshheald@users.noreply.github.com>
2024-05-31 16:10:52 +0100

    12865 Add ConnectionControllerManager
    This allows us to create our various connection controllers

---
+++ WooCommerce/Classes/POS/Card Present Payments/CardPresentPaymentsConnectionControllerManager.swift
@@ -0,0 +1,29 @@
+import Foundation
+import struct Yosemite.CardPresentPaymentsConfiguration
+
+class CardPresentPaymentsConnectionControllerManager {
+    let externalReaderConnectionController: CardReaderConnectionController
+    let tapToPayConnectionController: BuiltInCardReaderConnectionController
+
+    init(siteID: Int64,
+         configuration: CardPresentPaymentsConfiguration,
+         alertsPresenter: CardPresentPaymentAlertsPresenting) {
+        let analyticsTracker = CardReaderConnectionAnalyticsTracker(
+            configuration: configuration,
+            siteID: siteID,
+            connectionType: .userInitiated)
+        self.externalReaderConnectionController = CardReaderConnectionController(
+            forSiteID: siteID,
+            knownReaderProvider: CardReaderSettingsKnownReaderStorage(),
+            alertsPresenter: alertsPresenter,
+            alertsProvider: BluetoothReaderConnectionAlertsProvider(),
+            configuration: configuration,
+            analyticsTracker: analyticsTracker)
+        self.tapToPayConnectionController = BuiltInCardReaderConnectionController(
+            forSiteID: siteID,
+            alertsPresenter: alertsPresenter,
+            alertsProvider: BuiltInReaderConnectionAlertsProvider(),
+            configuration: configuration,
+            analyticsTracker: analyticsTracker)
+    }
+}

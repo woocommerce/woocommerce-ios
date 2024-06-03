79e98e365d2c68c809f2845b111097531cacb0c9
Božidar Ševo <bozidarsevo@gmail.com>
2024-06-03 16:07:34 +0200

    Update TotalsView.swift

--- WooCommerce/Classes/POS/Presentation/TotalsView.swift
+++ WooCommerce/Classes/POS/Presentation/TotalsView.swift
@@ -8,53 +8,70 @@ struct TotalsView: View {
     }
 
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             VStack(alignment: .leading, spacing: 32) {
-                HStack {
+                HStack(spacing: 40) {
                     priceFieldView(title: "Subtotal", formattedPrice: viewModel.formattedCartTotalPrice ?? "-")
                     priceFieldView(title: "Taxes", formattedPrice: viewModel.formattedOrderTotalTaxPrice ?? "-")
                 }
                 totalPriceView(formattedPrice: viewModel.formattedOrderTotalPrice ?? "-")
             }
+            .padding()
             Spacer()
-            HStack {
-                Button("Take payment") { debugPrint("Not implemented") }
-                    .padding(.all, 10)
-                    .frame(maxWidth: .infinity, idealHeight: 120)
-                    .font(.title)
-                    .foregroundColor(Color.white)
-                    .border(.white, width: 2)
-                Button("Cash") { debugPrint("Not implemented") }
-                    .padding(.all, 10)
-                    .frame(maxWidth: .infinity, idealHeight: 120)
-                    .font(.title)
-                    .foregroundColor(Color.primaryBackground)
-                    .background(Color.white)
-                Button("Card") { debugPrint("Not implemented") }
-                    .padding(.all, 10)
-                    .frame(maxWidth: .infinity, idealHeight: 120)
-                    .font(.title)
-                    .foregroundColor(Color.primaryBackground)
-                    .background(Color.white)
-            }
+            cardReaderView
+                .padding()
+            Spacer()
+            paymentsView
+                .padding()
         }
     }
 }
 
 private extension TotalsView {
+    private var paymentsView: some View {
+        HStack {
+            Button("Take payment") { debugPrint("Not implemented") }
+                .padding(.all, 10)
+                .frame(maxWidth: .infinity, idealHeight: 120)
+                .font(.title)
+                .foregroundColor(Color.white)
+                .border(.white, width: 2)
+            Button("Cash") { debugPrint("Not implemented") }
+                .padding(.all, 10)
+                .frame(maxWidth: .infinity, idealHeight: 120)
+                .font(.title)
+                .foregroundColor(Color.primaryBackground)
+                .background(Color.white)
+            Button("Card") { debugPrint("Not implemented") }
+                .padding(.all, 10)
+                .frame(maxWidth: .infinity, idealHeight: 120)
+                .font(.title)
+                .foregroundColor(Color.primaryBackground)
+                .background(Color.white)
+        }
+    }
+
+    private var cardReaderView: some View {
+        Text("Card reader status placeholder view")
+    }
+
     @ViewBuilder func priceFieldView(title: String, formattedPrice: String) -> some View {
         VStack(alignment: .leading, spacing: .zero) {
             Text(title)
             Text(formattedPrice)
+                .font(.title2)
+                .fontWeight(.medium)
         }
     }
 
     @ViewBuilder func totalPriceView(formattedPrice: String) -> some View {
         VStack(alignment: .leading, spacing: .zero) {
             Text("Total")
-                .bold()
+                .font(.title2)
+                .fontWeight(.medium)
             Text(formattedPrice)
-                .font(.title)
+                .font(.largeTitle)
+                .bold()
         }
     }
 }

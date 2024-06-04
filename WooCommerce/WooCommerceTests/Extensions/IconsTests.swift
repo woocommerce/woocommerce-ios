2a7f5f95a7ed5f49eb0b17566f1bd1ef79efa235
Rachel McRoberts <rachel@automattic.com>
2024-06-04 11:39:13 +0100

    Add tooltip icon

--- WooCommerce/WooCommerceTests/Extensions/IconsTests.swift
+++ WooCommerce/WooCommerceTests/Extensions/IconsTests.swift
@@ -952,4 +952,8 @@ final class IconsTests: XCTestCase {
     func test_magnifyingGlassNotFound_is_not_nil() {
         XCTAssertNotNil(UIImage.magnifyingGlassNotFound)
     }
+
+    func test_tooltipImage_is_not_nil() {
+        XCTAssertNotNil(UIImage.tooltipImage)
+    }
 }

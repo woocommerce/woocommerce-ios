02d04745cd05d5e79e82dbda74e3a96bcc456afd
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-06-04 10:17:28 +0700

    Update POSProduct previews

--- Yosemite/Yosemite/PointOfSale/POSProductProvider.swift
+++ Yosemite/Yosemite/PointOfSale/POSProductProvider.swift
@@ -72,15 +72,15 @@ extension POSProductProvider {
         POSProduct(itemID: UUID(),
                    productID: 1,
                    name: "Product 1",
-                   price: "1.00")
+                   price: "$1.00")
     }
 
     public static func provideProductsForPreview() -> [POSProduct] {
         return [
-            POSProduct(itemID: UUID(), productID: 1, name: "Product 1", price: "1.00"),
-            POSProduct(itemID: UUID(), productID: 2, name: "Product 2", price: "2.00"),
-            POSProduct(itemID: UUID(), productID: 3, name: "Product 3", price: "3.00"),
-            POSProduct(itemID: UUID(), productID: 4, name: "Product 4", price: "4.00")
+            POSProduct(itemID: UUID(), productID: 1, name: "Product 1", price: "$1.00"),
+            POSProduct(itemID: UUID(), productID: 2, name: "Product 2", price: "$2.00"),
+            POSProduct(itemID: UUID(), productID: 3, name: "Product 3", price: "$3.00"),
+            POSProduct(itemID: UUID(), productID: 4, name: "Product 4", price: "$4.00")
         ]
     }
 }

b5617529b37ec67c5abca65f350ea9db99dcf1ff
Jaclyn Chen <watertranquil@gmail.com>
2024-06-10 12:03:55 +0800

    Update `BasicCardPresentPaymentAlert` layout to try matching UIKit view.

--- WooCommerce/Classes/POS/Presentation/CardPresentPaymentAlert.swift
+++ WooCommerce/Classes/POS/Presentation/CardPresentPaymentAlert.swift
@@ -11,7 +11,7 @@ struct CardPresentPaymentAlert: View {
     }
 
     var body: some View {
-        BasicCardPresentPaymentAlert(alertViewModel: viewModel.alertViewModel)
+        BasicCardPresentPaymentAlert(viewModel: viewModel.alertViewModel)
             .sheet(item: $viewModel.wcSettingsWebViewModel) { webViewModel in
                 WCSettingsWebView(adminUrl: webViewModel.webViewURL, completion: webViewModel.onCompletion)
             }
@@ -19,32 +19,107 @@ struct CardPresentPaymentAlert: View {
 }
 
 struct BasicCardPresentPaymentAlert: View {
-    let alertViewModel: CardPresentPaymentAlertViewModel
+    let viewModel: CardPresentPaymentAlertViewModel
 
     var body: some View {
-        VStack {
-            Text(alertViewModel.topTitle)
-
-            if let bottomTitle = alertViewModel.bottomTitle {
-                Text(bottomTitle)
+        VStack(spacing: Layout.stackViewVerticalSpacing) {
+            VStack(alignment: .center) {
+                Text(viewModel.topTitle)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                if let topSubtitle = viewModel.topSubtitle, shouldShowTopSubtitle() {
+                    Text(topSubtitle)
+                        .font(.title)
+                }
             }
 
-            if let primaryButton = alertViewModel.primaryButtonViewModel {
-                Button(primaryButton.title, action: primaryButton.actionHandler)
+            if viewModel.showLoadingIndicator {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .gray))
+                    .scaleEffect(1.5, anchor: .center)
+            } else {
+                Image(uiImage: viewModel.image)
+                    .padding()
             }
 
-            if let secondaryButton = alertViewModel.secondaryButtonViewModel {
-                Button(secondaryButton.title, action: secondaryButton.actionHandler)
+            if let bottomTitle = viewModel.bottomTitle, shouldShowBottomLabels() {
+                VStack(alignment: .center) {
+                    Text(bottomTitle)
+                        .font(.subheadline)
+
+                    if let bottomSubtitle = viewModel.bottomSubtitle, shouldShowBottomSubtitle() {
+                        Text(bottomSubtitle)
+                            .font(.footnote)
+                    }
+                }
             }
 
-            if let auxiliaryButton = alertViewModel.auxiliaryButtonViewModel {
-                Button(auxiliaryButton.title, action: auxiliaryButton.actionHandler)
+            VStack(spacing: Layout.buttonVerticalSpacing) {
+                if let primaryButton = viewModel.primaryButtonViewModel {
+                    Button(primaryButton.title, action: primaryButton.actionHandler)
+                        .buttonStyle(PrimaryButtonStyle())
+                }
+
+                if let secondaryButton = viewModel.secondaryButtonViewModel {
+                    Button(secondaryButton.title, action: secondaryButton.actionHandler)
+                        .buttonStyle(SecondaryButtonStyle())
+                }
+
+                if let auxiliaryButton = viewModel.auxiliaryButtonViewModel {
+                    Button(auxiliaryButton.title, action: auxiliaryButton.actionHandler)
+                        .buttonStyle(LinkButtonStyle())
+                }
             }
         }
+        .padding(Layout.padding)
     }
 }
 
-#Preview {
-    let alertViewModel = CardPresentModalFoundReader(name: "Stripe M2", connect: {}, continueSearch: {}, cancel: {})
-    return CardPresentPaymentAlert(alertViewModel: alertViewModel)
+private extension BasicCardPresentPaymentAlert {
+    func shouldShowTopSubtitle() -> Bool {
+        viewModel.textMode != .reducedTopInfo
+    }
+
+    func shouldShowBottomLabels() -> Bool {
+        viewModel.textMode != .noBottomInfo
+    }
+
+    func shouldShowBottomSubtitle() -> Bool {
+        let textMode = viewModel.textMode
+        return textMode == .fullInfo ||
+            textMode == .reducedTopInfo
+    }
 }
+
+private extension BasicCardPresentPaymentAlert {
+    enum Layout {
+        static let padding: EdgeInsets = .init(top: 40, leading: 96, bottom: 56, trailing: 96)
+        static let stackViewVerticalSpacing: CGFloat = 32
+        static let buttonVerticalSpacing: CGFloat = 16
+    }
+}
+
+#if DEBUG
+
+#Preview {
+    enum AlertType: String {
+        case scanningForReaders
+        case foundReader
+    }
+
+    let alertViewModelsByType: [AlertType: CardPresentPaymentAlertViewModel] = [
+        .scanningForReaders: CardPresentModalScanningForReader(cancel: {}),
+        .foundReader: CardPresentModalFoundReader(name: "Stripe M2", connect: {}, continueSearch: {}, cancel: {})
+    ]
+
+    return Text("Presenting view")
+        .sheet(isPresented: .constant(true)) {
+            if let viewModel = alertViewModelsByType[.foundReader] {
+                CardPresentPaymentAlert(alertViewModel: viewModel)
+            } else {
+                EmptyView()
+            }
+        }
+}
+
+#endif

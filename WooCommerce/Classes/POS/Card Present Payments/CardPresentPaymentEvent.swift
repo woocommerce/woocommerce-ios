edf5e152482516eebe1fa493442b755cfcf72153
Jaclyn Chen <watertranquil@gmail.com>
2024-06-13 11:42:24 +0800

    Disable simulated Stripe card reader configuration.

--- WooCommerce/Classes/POS/Card Present Payments/CardPresentPaymentEvent.swift
+++ WooCommerce/Classes/POS/Card Present Payments/CardPresentPaymentEvent.swift
@@ -3,6 +3,6 @@ import Foundation
 enum CardPresentPaymentEvent {
     case idle
     case showAlert(_ alertDetails: CardPresentPaymentAlertDetails)
-    case showReaderList(_ readerIDs: [String], selectionHandler: ((String) -> Void))
+    case showReaderList(_ readerIDs: [String], selectionHandler: ((String?) -> Void))
     case showOnboarding(_ onboardingViewModel: CardPresentPaymentsOnboardingViewModel)
 }

020a7ee468262d705b86f9dab1c9f4a34f5f0998
Božidar Ševo <bozidarsevo@gmail.com>
2024-06-13 16:03:06 +0200

    Update tests

--- WooCommerce/WooCommerceTests/POS/ViewModels/PointOfSaleDashboardViewModelTests.swift
+++ WooCommerce/WooCommerceTests/POS/ViewModels/PointOfSaleDashboardViewModelTests.swift
@@ -1,12 +1,18 @@
 import XCTest
 @testable import WooCommerce
+@testable import Yosemite
 
 final class PointOfSaleDashboardViewModelTests: XCTestCase {
 
     func test_cart_is_empty_initially() {
         // Given
+        let siteID = ServiceLocator.stores.sessionManager.defaultStoreID ?? Int64.min
+        let credentials = Credentials(authToken: "token")
+        let orderService = PointOfSaleOrderService(siteID: siteID,
+                                                   credentials: credentials)
         let viewModel = PointOfSaleDashboardViewModel(items: POSItemProviderPreview().providePointOfSaleItems(),
-                                                      cardPresentPaymentService: CardPresentPaymentPreviewService())
+                                                      cardPresentPaymentService: CardPresentPaymentPreviewService(),
+                                                      orderService: orderService)
 
         // Then
         XCTAssertTrue(viewModel.itemsInCart.isEmpty)

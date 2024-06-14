c7614a4e767588eee5b8e81a795ff16b6bb26d32
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-06-14 13:23:34 +0530

    Mock use case for unit testing.

--- WooCommerce/WooCommerceTests/Mocks/MockFavoriteProductsUseCase.swift
+++ WooCommerce/WooCommerceTests/Mocks/MockFavoriteProductsUseCase.swift
@@ -1,9 +1,31 @@
-//
-//  MockFavoriteProductsUseCase.swift
-//  WooCommerceTests
-//
-//  Created by Sharma Elanthiraiyan on 14/06/24.
-//  Copyright © 2024 Automattic. All rights reserved.
-//
+@testable import WooCommerce
 
-import Foundation
+final class MockFavoriteProductsUseCase: FavoriteProductsUseCase {
+    var markAsFavoriteCalledForProductID: Int64?
+
+    var removeFromFavoriteCalledForProductID: Int64?
+
+    var isFavoriteCalledForProductID: Int64?
+    var isFavoriteValue = false
+
+    var favoriteProductIDsCalled = false
+    var favoriteProductIDsValue: [Int64] = []
+
+    func markAsFavorite(productID: Int64) {
+        markAsFavoriteCalledForProductID = productID
+    }
+
+    func removeFromFavorite(productID: Int64) {
+        removeFromFavoriteCalledForProductID = productID
+    }
+
+    func isFavorite(productID: Int64) async -> Bool {
+        isFavoriteCalledForProductID = productID
+        return isFavoriteValue
+    }
+
+    func favoriteProductIDs() async -> [Int64] {
+        favoriteProductIDsCalled = true
+        return favoriteProductIDsValue
+    }
+}

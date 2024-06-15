c0d7ab3c362b8d6a243bcdcc6eeec6a861a87f74
David Galbraith <soundsokay@users.noreply.github.com>
2024-06-15 18:57:29 -0400

    test: add two more test cases
    Added testCreateResultsController and testCreateCellViewModel.

--- WooCommerce/WooCommerceTests/ViewRelated/Search/Order/OrderSearchUICommandTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Search/Order/OrderSearchUICommandTests.swift
@@ -4,6 +4,7 @@ import XCTest
 import Yosemite
 
 final class OrderSearchUICommandTests: XCTestCase {
+    let siteID: Int64 = 12345
 
     override func setUpWithError() throws {
         super.setUp()
@@ -15,13 +16,42 @@ final class OrderSearchUICommandTests: XCTestCase {
 
     func testCreateStarterViewControllerReturnsNil() throws {
         // Given
-        let command = OrderSearchUICommand(siteID: 12345, onSelectSearchResult: { _, _ in })
+        let command = OrderSearchUICommand(siteID: siteID, onSelectSearchResult: { _, _ in })
 
         // When
         let starterViewController = command.createStarterViewController()
 
         // Then
         XCTAssertNil(starterViewController, "Expected createStarterViewController to return nil")
+    }
+
+    func testCreateResultsController() {
+        // Given
+
+        let command = OrderSearchUICommand(siteID: siteID, onSelectSearchResult: { _, _ in })
+
+        // When
+        let resultsController = command.createResultsController()
+
+        // Then
+        XCTAssertNotNil(resultsController, "Expected createResultsController to return a ResultsController instance")
+        XCTAssertEqual(resultsController.predicate?.predicateFormat, "siteID == \(siteID)", "Predicate format did not match")
+        XCTAssertEqual(resultsController.sortDescriptors?.first?.key, "dateCreated", "First sort descriptor key did not match")
+        XCTAssertEqual(resultsController.sortDescriptors?.first?.ascending, false, "First sort descriptor ascending did not match")
+    }
+
+    func testCreateCellViewModel() {
+        // Given
+        let order = MockOrders().makeOrder()
+
+        let command = OrderSearchUICommand(siteID: siteID, onSelectSearchResult: { _, _ in })
+
+        // When
+        let cellViewModel = command.createCellViewModel(model: order)
+
+        // Then
+        XCTAssertNotNil(cellViewModel, "Expected createCellViewModel to return an OrderListCellViewModel instance")
+        XCTAssertEqual(cellViewModel.status, OrderStatusEnum.processing, "Expected order status to match")
     }
 
 }

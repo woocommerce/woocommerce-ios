980b3cd4154f252fadf720d2f7f1f4fd333aa9cb
Josh Heald <joshheald@users.noreply.github.com>
2024-06-18 16:04:50 +0100

    12972 Move inline messages to new container view

--- WooCommerce/Classes/POS/Presentation/PointOfSaleDashboardView.swift
+++ WooCommerce/Classes/POS/Presentation/PointOfSaleDashboardView.swift
@@ -26,26 +26,8 @@ struct PointOfSaleDashboardView: View {
                     Spacer()
                     VStack {
                         totalsView
-                        // TODO: replace temporary inline message UI based on design
                         if let inlinePaymentMessage = viewModel.cardPresentPaymentInlineMessage {
-                            switch inlinePaymentMessage {
-                            case .preparingForPayment(let viewModel):
-                                PointOfSaleCardPresentPaymentPreparingForPaymentMessageView(viewModel: viewModel)
-                            case .tapSwipeOrInsertCard(let viewModel):
-                                PointOfSaleCardPresentPaymentTapSwipeInsertCardMessageView(viewModel: viewModel)
-                            case .processing:
-                                Text("processing...")
-                            case .displayReaderMessage(let viewModel):
-                                PointOfSaleCardPresentPaymentDisplayReaderMessageMessageView(viewModel: viewModel)
-                            case .paymentSuccess:
-                                Text("Payment successful!")
-                            case .paymentError(let viewModel):
-                                PointOfSaleCardPresentPaymentErrorMessageView(viewModel: viewModel)
-                            case .paymentErrorNonRetryable(let viewModel):
-                                PointOfSaleCardPresentPaymentNonRetryableErrorMessageView(viewModel: viewModel)
-                            case .cancelledOnReader:
-                                Text("Payment cancelled on reader")
-                            }
+                            PointOfSaleCardPresentPaymentInLineMessage(messageType: inlinePaymentMessage)
                         }
                     }
                     // TODO: remove this after replacing temporary inline message UI based on design

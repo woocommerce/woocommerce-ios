88f47bb76f0a67c285fb8c889ffe3b9291d41324
Božidar Ševo <bozidarsevo@gmail.com>
2024-06-18 12:19:05 +0200

    Update ProductInputTransformer.swift

--- Yosemite/Yosemite/Stores/Order/ProductInputTransformer.swift
+++ Yosemite/Yosemite/Stores/Order/ProductInputTransformer.swift
@@ -272,17 +272,3 @@ private extension ProductInputTransformer {
         return itemSubtotal / item.quantity
     }
 }
-
-//// MARK: - OrderItem Helper Methods
-////
-//extension OrderItem {
-//    /// Returns the variant if it exists
-//    ///
-//    var productOrVariationID: Int64 {
-//        if variationID == 0 {
-//            return productID
-//        }
-//
-//        return variationID
-//    }
-//}

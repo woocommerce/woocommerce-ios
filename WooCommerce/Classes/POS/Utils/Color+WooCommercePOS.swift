ea4995d882a75fbecf4dc45e78ed3cea57c3414b
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-06-18 13:17:40 +0700

    remove unused POS colors

--- WooCommerce/Classes/POS/Utils/Color+WooCommercePOS.swift
+++ WooCommerce/Classes/POS/Utils/Color+WooCommercePOS.swift
@@ -14,18 +14,6 @@ extension Color {
         return Color(red: 89.0 / 255.0, green: 181.0 / 255.0, blue: 227.0 / 255.0)
     }
 
-    /// Tertiary POS background color
-    ///
-    static var tertiaryBackground: Color {
-        return Color(red: 142.0 / 255.0, green: 208.0 / 255.0, blue: 240.0 / 255.0)
-    }
-
-    /// POS color: Light Blue
-    ///
-    static var lightBlue: Color {
-        return Color(red: 202.0 / 255.0, green: 237.0 / 255.0, blue: 255.0 / 255.0)
-    }
-
     /// Default POS text color
     ///
     static var primaryText: Color {

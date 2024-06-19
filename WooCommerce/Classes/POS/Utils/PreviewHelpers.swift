3df7ab0cb98a07ada9701924e25c705d0a170ad7
Božidar Ševo <bozidarsevo@gmail.com>
2024-06-19 09:15:08 +0200

    Update PreviewHelpers.swift
    Co-authored-by: Jaclyn Chen <watertranquil@gmail.com>

--- WooCommerce/Classes/POS/Utils/PreviewHelpers.swift
+++ WooCommerce/Classes/POS/Utils/PreviewHelpers.swift
@@ -19,10 +19,10 @@ struct POSProductPreview: POSItem {
 final class POSItemProviderPreview: POSItemProvider {
     func providePointOfSaleItems() -> [POSItem] {
         return [
-            POSProductPreview(itemID: UUID(), productID: 1, name: "Product 1", price: "$1.00", itemCategories: [], productType: ProductType.simple),
-            POSProductPreview(itemID: UUID(), productID: 2, name: "Product 2", price: "$2.00", itemCategories: [], productType: ProductType.simple),
-            POSProductPreview(itemID: UUID(), productID: 3, name: "Product 3", price: "$3.00", itemCategories: [], productType: ProductType.simple),
-            POSProductPreview(itemID: UUID(), productID: 4, name: "Product 4", price: "$4.00", itemCategories: [], productType: ProductType.simple)
+            POSProductPreview(itemID: UUID(), productID: 1, name: "Product 1", price: "$1.00", itemCategories: [], productType: .simple),
+            POSProductPreview(itemID: UUID(), productID: 2, name: "Product 2", price: "$2.00", itemCategories: [], productType: .simple),
+            POSProductPreview(itemID: UUID(), productID: 3, name: "Product 3", price: "$3.00", itemCategories: [], productType: .simple),
+            POSProductPreview(itemID: UUID(), productID: 4, name: "Product 4", price: "$4.00", itemCategories: [], productType: .simple)
         ]
     }
 

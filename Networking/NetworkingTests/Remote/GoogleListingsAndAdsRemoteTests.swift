ef6d84cdf45ed737a95f4f1c89c20e96d7aafdd5
Rachel McRoberts <rachel@automattic.com>
2024-06-26 17:28:50 +0100

    Add remote endpoint to load paid campaign stats

--- Networking/NetworkingTests/Remote/GoogleListingsAndAdsRemoteTests.swift
+++ Networking/NetworkingTests/Remote/GoogleListingsAndAdsRemoteTests.swift
@@ -57,4 +57,52 @@ final class GoogleListingsAndAdsRemoteTests: XCTestCase {
         }
     }
 
+    // MARK: - Load campaign stats
+
+    func test_loadCampaignStats_returns_parsed_stats() async throws {
+        // Given
+        let remote = GoogleListingsAndAdsRemote(network: network)
+
+        let suffix = "wc/gla/ads/reports/programs"
+        network.simulateResponse(requestUrlSuffix: suffix, filename: "google-ads-reports-programs-without-data")
+
+        // When
+        let results = try await remote.loadCampaignStats(for: sampleSiteID,
+                                                         timeZone: .current,
+                                                         earliestDateToInclude: Date(),
+                                                         latestDateToInclude: Date(),
+                                                         totals: nil,
+                                                         orderby: nil)
+
+        // Then
+        XCTAssertEqual(results.siteID, sampleSiteID)
+        XCTAssertEqual(results.totals, GoogleAdsCampaignStatsTotals(sales: 11, spend: 73.01, clicks: 154, impressions: 16938, conversions: 3))
+        XCTAssertEqual(results.campaigns.count, 2)
+    }
+
+    func test_loadCampaignStats_properly_relays_networking_errors() async {
+        // Given
+        let remote = GoogleListingsAndAdsRemote(network: network)
+
+        let expectedError = NetworkError.unacceptableStatusCode(statusCode: 403)
+        let suffix = "wc/gla/ads/reports/programs"
+        network.simulateError(requestUrlSuffix: suffix, error: expectedError)
+
+        do {
+            // When
+            _ = try await remote.loadCampaignStats(for: sampleSiteID,
+                                                   timeZone: .current,
+                                                   earliestDateToInclude: Date(),
+                                                   latestDateToInclude: Date(),
+                                                   totals: nil,
+                                                   orderby: nil)
+
+            // Then
+            XCTFail("Request should fail")
+        } catch {
+            // Then
+            XCTAssertEqual(error as? NetworkError, expectedError)
+        }
+    }
+
 }

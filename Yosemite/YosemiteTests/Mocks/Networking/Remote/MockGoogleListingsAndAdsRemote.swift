614b0c5999da29bc14fb3842176f68b7865ac0f9
Huong Do <hi.huongdo@gmail.com>
2024-06-27 17:25:13 +0700

    Update MockGoogleListingsAndAdsRemote

--- Yosemite/YosemiteTests/Mocks/Networking/Remote/MockGoogleListingsAndAdsRemote.swift
+++ Yosemite/YosemiteTests/Mocks/Networking/Remote/MockGoogleListingsAndAdsRemote.swift
@@ -4,9 +4,14 @@ import Networking
 final class MockGoogleListingsAndAdsRemote {
 
     private var checkingConnectionResult: Result<GoogleAdsConnection, Error>?
+    private var fetchingAdsCampaignsResult: Result<[GoogleAdsCampaign], Error>?
 
     func whenCheckingConnection(thenReturn result: Result<GoogleAdsConnection, Error>) {
         checkingConnectionResult = result
+    }
+
+    func whenFetchingAdsCampaigns(thenReturn result: Result<[GoogleAdsCampaign], Error>) {
+        fetchingAdsCampaignsResult = result
     }
 }
 
@@ -19,6 +24,19 @@ extension MockGoogleListingsAndAdsRemote: GoogleListingsAndAdsRemoteProtocol {
         switch result {
         case .success(let connection):
             return connection
+        case .failure(let error):
+            throw error
+        }
+    }
+
+    func fetchAdsCampaign(for siteID: Int64) async throws -> [GoogleAdsCampaign] {
+        guard let result = fetchingAdsCampaignsResult else {
+            XCTFail("Could not find result for fetching GLA campaigns.")
+            throw NetworkError.notFound()
+        }
+        switch result {
+        case .success(let campaigns):
+            return campaigns
         case .failure(let error):
             throw error
         }

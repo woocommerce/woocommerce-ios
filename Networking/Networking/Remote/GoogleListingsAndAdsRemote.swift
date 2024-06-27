39a00e59c3e83a7f47f8333095c8f075786f64e3
Huong Do <hi.huongdo@gmail.com>
2024-06-27 17:22:58 +0700

    Update GoogleListingsAndAdsRemote to support fetching campaign list

--- Networking/Networking/Remote/GoogleListingsAndAdsRemote.swift
+++ Networking/Networking/Remote/GoogleListingsAndAdsRemote.swift
@@ -6,6 +6,10 @@ public protocol GoogleListingsAndAdsRemoteProtocol {
     /// Check Google ads connection for the given site.
     ///
     func checkConnection(for siteID: Int64) async throws -> GoogleAdsConnection
+
+    /// Fetch ads campaigns for the given site
+    ///
+    func fetchAdsCampaign(for siteID: Int64) async throws -> [GoogleAdsCampaign]
 }
 
 /// Google Listings & Ads: Endpoints
@@ -22,10 +26,22 @@ public final class GoogleListingsAndAdsRemote: Remote, GoogleListingsAndAdsRemot
         let mapper = GoogleAdsConnectionMapper()
         return try await enqueue(request, mapper: mapper)
     }
+
+    public func fetchAdsCampaign(for siteID: Int64) async throws -> [GoogleAdsCampaign] {
+        let path = Paths.adsCampaigns
+        let request = JetpackRequest(wooApiVersion: .none,
+                                     method: .get,
+                                     siteID: siteID,
+                                     path: path,
+                                     availableAsRESTRequest: true)
+        let mapper = GoogleAdsCampaignListMapper()
+        return try await enqueue(request, mapper: mapper)
+    }
 }
 
 private extension GoogleListingsAndAdsRemote {
     enum Paths {
         static let connection = "wc/gla/ads/connection"
+        static let adsCampaigns = "wc/gla/ads/campaigns"
     }
 }

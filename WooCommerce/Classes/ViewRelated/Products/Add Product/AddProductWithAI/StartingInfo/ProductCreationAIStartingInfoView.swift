d766f41afbbc9f627ddbf5c867d0f3ebb0d0c012
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-07-02 19:23:08 +0530

    View for AI tone selection button.

--- WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/StartingInfo/ProductCreationAIStartingInfoView.swift
+++ WooCommerce/Classes/ViewRelated/Products/Add Product/AddProductWithAI/StartingInfo/ProductCreationAIStartingInfoView.swift
@@ -277,6 +277,51 @@ private extension ProductCreationAIStartingInfoView {
 }
 
 private extension ProductCreationAIStartingInfoView {
+    struct ToneOfVoiceView: View {
+        @ObservedObject var viewModel: AIToneVoiceViewModel
+
+        var body: some View {
+            HStack(alignment: .center) {
+                Text(Localization.title)
+                    .bodyStyle()
+
+                Spacer()
+
+                Menu {
+                    ForEach(viewModel.tones, id: \.self) { tone in
+                        Button(tone.description) {
+                            viewModel.onSelectTone(tone)
+                        }
+                    }
+                } label: {
+                    HStack(alignment: .center, spacing: Layout.hSpacing) {
+                        Text(viewModel.selectedTone.description)
+                            .foregroundStyle(Color.accentColor)
+                            .bodyStyle()
+
+                        Image(systemName: "chevron.up.chevron.down")
+                            .foregroundStyle(Color.accentColor)
+                            .bodyStyle()
+                    }
+                }
+            }
+        }
+
+        enum Layout {
+            static let hSpacing: CGFloat = 4
+        }
+
+        enum Localization {
+            static let title = NSLocalizedString(
+                "productCreationAIStartingInfoView.toneOfVoiceView.title",
+                value: "Tone of voice",
+                comment: "Title of the AI tone selection button."
+            )
+        }
+    }
+}
+
+private extension ProductCreationAIStartingInfoView {
     enum Layout {
         static let insets: EdgeInsets = .init(top: 24, leading: 16, bottom: 16, trailing: 16)
 

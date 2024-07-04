3e074eb24935b0c72cbe42e4ff29bab028cdfcc9
Huong Do <hi.huongdo@gmail.com>
2024-07-04 10:02:34 +0700

    Add new files for Google Ads dashboard card

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/GoogleAds/GoogleAdsDashboardCard.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct GoogleAdsDashboardCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoogleAdsDashboardCard()
+}
---
+++ WooCommerce/Classes/ViewRelated/Dashboard/GoogleAds/GoogleAdsDashboardCardViewModel.swift
@@ -0,0 +1,6 @@
+import Foundation
+import Yosemite
+
+/// View model for `GoogleAdsDashboardCard`.
+final class GoogleAdsDashboardCardViewModel: ObservableObject {
+}

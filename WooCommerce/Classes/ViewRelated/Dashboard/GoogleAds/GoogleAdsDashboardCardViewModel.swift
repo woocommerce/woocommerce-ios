6253444e3704e745f363368a41161f89e80d0252
Huong Do <hi.huongdo@gmail.com>
2024-07-04 10:10:22 +0700

    Add initializer for GoogleAdsDashboardCardViewModel

--- WooCommerce/Classes/ViewRelated/Dashboard/GoogleAds/GoogleAdsDashboardCardViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/GoogleAds/GoogleAdsDashboardCardViewModel.swift
@@ -1,6 +1,20 @@
 import Foundation
 import Yosemite
+import protocol WooFoundation.Analytics
 
 /// View model for `GoogleAdsDashboardCard`.
 final class GoogleAdsDashboardCardViewModel: ObservableObject {
+    private let siteID: Int64
+    private let stores: StoresManager
+    private let analytics: Analytics
+
+    @Published private(set) var syncingError: Error?
+
+    init(siteID: Int64,
+         stores: StoresManager = ServiceLocator.stores,
+         analytics: Analytics = ServiceLocator.analytics) {
+        self.siteID = siteID
+        self.stores = stores
+        self.analytics = analytics
+    }
 }

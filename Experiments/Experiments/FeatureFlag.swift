686e43b16929acfba94fced381ad42aba1cf3862
Paolo Musolino <info@codeido.com>
2024-07-08 15:56:27 +0200

    feat: added new feature flag productCreationAIv2M3

--- Experiments/Experiments/FeatureFlag.swift
+++ Experiments/Experiments/FeatureFlag.swift
@@ -203,6 +203,10 @@ public enum FeatureFlag: Int {
     /// Enables M1 updates of product creation AI version 2
     ///
     case productCreationAIv2M1
+    
+    /// Enables M3 updates of product creation AI version 2
+    ///
+    case productCreationAIv2M3
 
     /// Enables Google ads campaign creation on web view
     ///

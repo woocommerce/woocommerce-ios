977555aa6c738d5e0f82cc0e5dc268239c9ef06c
Ernesto Carrion <ernesto.carrion@automattic.com>
2024-07-09 17:30:37 -0500

    Improve timestamp handling

--- WooCommerce/Classes/Tools/BackgroundTasks/OrderListSyncBackgroundTask.swift
+++ WooCommerce/Classes/Tools/BackgroundTasks/OrderListSyncBackgroundTask.swift
@@ -8,7 +8,7 @@ struct OrderListSyncBackgroundTask {
 
     /// The last time we successfully run a sync update.
     ///
-    static private(set) var latestSyncDate: Date {
+    static var latestSyncDate: Date {
         get {
             return UserDefaults.standard[.latestBackgroundOrderSyncDate] as? Date ?? Date.distantPast
         }
@@ -41,6 +41,7 @@ struct OrderListSyncBackgroundTask {
 
                 let useCase = CurrentOrderListSyncUseCase(siteID: siteID, stores: stores)
                 try await useCase.sync()
+                Self.latestSyncDate = Date.now
 
                 DDLogInfo("📱 Successfully synchronized orders in the background")
                 backgroundTask?.setTaskCompleted(success: true)

624bf3d74ae27f3b1f77a19e1bbd8a8761b6e7bd
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-07-10 10:36:25 +0700

    Make isSyncingItems only gettable

--- WooCommerce/Classes/POS/ViewModels/ItemSelectorViewModel.swift
+++ WooCommerce/Classes/POS/ViewModels/ItemSelectorViewModel.swift
@@ -11,8 +11,10 @@ final class ItemSelectorViewModel: ObservableObject {
     }
 
     @Published private(set) var items: [POSItem] = []
-    @Published private(set) var isSyncingItems: Bool = true
     @Published private(set) var state: ItemSelectorState = .loading
+    var isSyncingItems: Bool {
+        state == .loading
+    }
 
     private let itemProvider: POSItemProvider
     private let selectedItemSubject: PassthroughSubject<POSItem, Never> = .init()
@@ -30,19 +32,16 @@ final class ItemSelectorViewModel: ObservableObject {
 
     @MainActor
     func populatePointOfSaleItems() async {
-        isSyncingItems = true
         do {
             items = try await itemProvider.providePointOfSaleItems()
         } catch {
             DDLogError("Error on load while fetching product data: \(error)")
         }
-        isSyncingItems = false
         state = .loaded
     }
 
     @MainActor
     func reload() async {
-        isSyncingItems = true
         do {
             let newItems = try await itemProvider.providePointOfSaleItems()
             // Only clears in-memory items if the `do` block continues, otherwise we keep them in memory.
@@ -51,7 +50,6 @@ final class ItemSelectorViewModel: ObservableObject {
         } catch {
             DDLogError("Error on reload while updating product data: \(error)")
         }
-        isSyncingItems = false
         state = .loaded
     }
 }

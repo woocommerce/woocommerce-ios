2a81f1da0a9a83295b40a35132ee8a9d9822c493
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-07-25 09:49:33 +0530

    Use component separated by to count words.

--- WooCommerce/Classes/Analytics/WooAnalyticsEvent+ProductCreationAI.swift
+++ WooCommerce/Classes/Analytics/WooAnalyticsEvent+ProductCreationAI.swift
@@ -36,7 +36,7 @@ extension WooAnalyticsEvent {
 
         static func generateDetailsTapped(isFirstAttempt: Bool,
                                           features: String) -> WooAnalyticsEvent {
-            let wordCount = features.split { $0 == " " || $0.isNewline }.count
+            let wordCount = features.components(separatedBy: .whitespacesAndNewlines).count
             let properties: [String: WooAnalyticsEventPropertyType?] = [Key.isFirstAttempt.rawValue: isFirstAttempt,
                                                                         Key.featureWordCount.rawValue: wordCount]
             return WooAnalyticsEvent(statName: .productCreationAIGenerateDetailsTapped,

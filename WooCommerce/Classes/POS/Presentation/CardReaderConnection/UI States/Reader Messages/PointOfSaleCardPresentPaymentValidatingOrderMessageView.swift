022a4ba646b0492891cf74725a278fe1cb384108
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2024-07-29 16:16:47 +0300

    Use dimmed style for preparing for payment and validating order message states

--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Reader Messages/PointOfSaleCardPresentPaymentValidatingOrderMessageView.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Reader Messages/PointOfSaleCardPresentPaymentValidatingOrderMessageView.swift
@@ -4,9 +4,10 @@ struct PointOfSaleCardPresentPaymentValidatingOrderMessageView: View {
     let viewModel: PointOfSaleCardPresentPaymentValidatingOrderMessageViewModel
 
     var body: some View {
-        POSCardPresentPaymentMessageView(viewModel: .init(showProgress: true,
-                                                          title: viewModel.title,
-                                                          message: viewModel.message))
+        let messageViewModel = POSCardPresentPaymentMessageViewModel(showProgress: true,
+                                                                     title: viewModel.title,
+                                                                     message: viewModel.message)
+        POSCardPresentPaymentMessageView(viewModel: messageViewModel, style: .dimmed)
     }
 }
 

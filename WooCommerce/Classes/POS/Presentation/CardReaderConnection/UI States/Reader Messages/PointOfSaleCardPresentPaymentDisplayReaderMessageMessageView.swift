f1247d84f7de49db37a3195e1b1defeb12505d2b
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2024-07-30 21:04:57 +0300

    Add padding to ReaderMessage and PaymentProcessing views
    According to designs, the views are not perfectly aligned vertically and should be a little bit closer to the top

--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Reader Messages/PointOfSaleCardPresentPaymentDisplayReaderMessageMessageView.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Reader Messages/PointOfSaleCardPresentPaymentDisplayReaderMessageMessageView.swift
@@ -24,6 +24,7 @@ struct PointOfSaleCardPresentPaymentDisplayReaderMessageMessageView: View {
                         .bold()
                 }
             }
+            .padding(.bottom)
             .multilineTextAlignment(.center)
             Spacer()
         }
--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Reader Messages/PointOfSaleCardPresentPaymentProcessingMessageView.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Reader Messages/PointOfSaleCardPresentPaymentProcessingMessageView.swift
@@ -24,6 +24,7 @@ struct PointOfSaleCardPresentPaymentProcessingMessageView: View {
                         .bold()
                 }
             }
+            .padding(.bottom)
             .multilineTextAlignment(.center)
             Spacer()
         }

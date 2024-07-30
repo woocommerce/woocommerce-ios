6b448a88a81c6b5128dd72f74f13c51f06daaf0a
Josh Heald <joshheald@users.noreply.github.com>
2024-07-30 16:29:24 +0100

    13175 Add whitespace into image name definitions

--- WooCommerce/Classes/Extensions/String+POS.swift
+++ WooCommerce/Classes/Extensions/String+POS.swift
@@ -4,27 +4,35 @@ extension String {
     static var posNewTransactionImageName: String {
         "pos-new-transaction-icon"
     }
+
     static var posPaymentSuccessfulImageName: String {
         "pos-payment-successful"
     }
+
     static var posProcessingPaymentImageName: String {
         "pos-processing-payment"
     }
+
     static var posReadyForPaymentImageName: String {
         "pos-ready-for-payment"
     }
+
     static var posCartBackImageName: String {
         "pos-cart-back"
     }
+
     static var posExitImageName: String {
         "pos-exit"
     }
+
     static var posGetSupportImageName: String {
         "pos-get-support"
     }
+
     static var posRemoveCartItemImageName: String {
         "pos-remove-cart-item"
     }
+
     static var posDismissProductsBannerImageName: String {
         "pos-dismiss-products-banner"
     }

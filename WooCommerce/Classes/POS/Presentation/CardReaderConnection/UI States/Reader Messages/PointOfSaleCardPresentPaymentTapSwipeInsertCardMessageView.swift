17fc43b14ba5a3994253dacbf3cc34b4c5a15b32
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2024-07-30 10:21:34 +0300

    Implement PointOfSaleCardPresentPaymentTapSwipeInsertCardMessageView

--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Reader Messages/PointOfSaleCardPresentPaymentTapSwipeInsertCardMessageView.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Reader Messages/PointOfSaleCardPresentPaymentTapSwipeInsertCardMessageView.swift
@@ -4,10 +4,28 @@ struct PointOfSaleCardPresentPaymentTapSwipeInsertCardMessageView: View {
     let viewModel: PointOfSaleCardPresentPaymentTapSwipeInsertCardMessageViewModel
 
     var body: some View {
-        let messageViewModel = POSCardPresentPaymentMessageViewModel(imageName: viewModel.imageName,
-                                                                     title: viewModel.title,
-                                                                     message: viewModel.message)
-        POSCardPresentPaymentMessageView(viewModel: messageViewModel, style: .standard)
+        HStack(alignment: .center) {
+            Spacer()
+            VStack(alignment: .center, spacing: PointOfSaleCardPresentPaymentLayout.headerSpacing) {
+                Image(viewModel.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: PointOfSaleCardPresentPaymentLayout.headerSize.width,
+                           height: PointOfSaleCardPresentPaymentLayout.headerSize.height)
+                VStack(alignment: .center, spacing: PointOfSaleCardPresentPaymentLayout.textSpacing) {
+                    Text(viewModel.title)
+                        .foregroundStyle(Color.posPrimaryTexti3)
+                        .font(.posBody)
+
+                    Text(viewModel.message)
+                        .font(.posTitle)
+                        .foregroundStyle(Color.posPrimaryTexti3)
+                        .bold()
+                }
+            }
+            .multilineTextAlignment(.center)
+            Spacer()
+        }
     }
 }
 

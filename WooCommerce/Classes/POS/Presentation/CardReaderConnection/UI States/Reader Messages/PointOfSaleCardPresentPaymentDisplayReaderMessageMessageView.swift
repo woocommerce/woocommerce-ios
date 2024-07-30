31414c0a2372daf5508487c74cd9c75f2a3f1058
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2024-07-30 15:50:27 +0300

    Implement basic UI for PointOfSaleCardPresentPaymentDisplayReaderMessageMessageView
    PointOfSaleCardPresentPaymentDisplayReaderMessageMessageView is shown during processing payment stage.
    
    For example, "Processing payment": "Remove card"

--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Reader Messages/PointOfSaleCardPresentPaymentDisplayReaderMessageMessageView.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Reader Messages/PointOfSaleCardPresentPaymentDisplayReaderMessageMessageView.swift
@@ -6,11 +6,22 @@ struct PointOfSaleCardPresentPaymentDisplayReaderMessageMessageView: View {
     var body: some View {
         HStack(alignment: .center) {
             Spacer()
-            VStack(alignment: .center, spacing: PointOfSaleCardPresentPaymentLayout.textSpacing) {
-                Text(viewModel.message)
-                    .font(.posTitle)
-                    .foregroundStyle(Color.posPrimaryTexti3)
-                    .bold()
+            VStack(alignment: .center, spacing: PointOfSaleCardPresentPaymentLayout.headerSpacing) {
+                Rectangle()
+                    .foregroundStyle(Color(.wooCommercePurple(.shade20)))
+                    .cornerRadius(13)
+                    .frame(width: Layout.headerSize.width, height: Layout.headerSize.height)
+
+                VStack(alignment: .center, spacing: PointOfSaleCardPresentPaymentLayout.textSpacing) {
+                    Text(viewModel.title)
+                        .foregroundStyle(.white)
+                        .font(.posBody)
+
+                    Text(viewModel.message)
+                        .font(.posTitle)
+                        .foregroundStyle(.white)
+                        .bold()
+                }
             }
             .multilineTextAlignment(.center)
             Spacer()
@@ -21,5 +32,12 @@ struct PointOfSaleCardPresentPaymentDisplayReaderMessageMessageView: View {
 #Preview {
     PointOfSaleCardPresentPaymentDisplayReaderMessageMessageView(
         viewModel: PointOfSaleCardPresentPaymentDisplayReaderMessageMessageViewModel(
-            message: "Please remove card"))
+            message: "Remove card"))
+}
+
+private extension PointOfSaleCardPresentPaymentDisplayReaderMessageMessageView {
+    enum Layout {
+        static let headerSize: CGSize = .init(width: 130, height: 114.4)
+        static let cornerRadius: CGFloat = 13
+    }
 }

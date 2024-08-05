97c09f469d3e6d6991caea2300fe2586f2e85447
Huong Do <hi.huongdo@gmail.com>
2024-08-05 14:53:09 +0700

    Make BlazeEligibilityChecker check for Blaze plugin for JCP sites

--- WooCommerce/Classes/Blaze/BlazeEligibilityChecker.swift
+++ WooCommerce/Classes/Blaze/BlazeEligibilityChecker.swift
@@ -3,8 +3,8 @@ import Yosemite
 
 /// Protocol for checking Blaze eligibility for easier unit testing.
 protocol BlazeEligibilityCheckerProtocol {
-    func isSiteEligible(_ site: Site) -> Bool
-    func isProductEligible(site: Site, product: ProductFormDataModel, isPasswordProtected: Bool) -> Bool
+    func isSiteEligible(_ site: Site) async -> Bool
+    func isProductEligible(site: Site, product: ProductFormDataModel, isPasswordProtected: Bool) async -> Bool
 }
 
 /// Checks for Blaze eligibility for a site and its products.
@@ -17,38 +17,65 @@ final class BlazeEligibilityChecker: BlazeEligibilityCheckerProtocol {
 
     /// Checks if the site is eligible for Blaze.
     /// - Returns: Whether the site is eligible for Blaze.
-    func isSiteEligible(_ site: Site) -> Bool {
-        checkSiteEligibility(site)
+    func isSiteEligible(_ site: Site) async -> Bool {
+        await checkSiteEligibility(site)
     }
 
     /// Checks if the product is eligible for Blaze.
     /// - Parameter product: The product to check for Blaze eligibility.
     /// - Parameter isPasswordProtected: Whether the product is password protected.
     /// - Returns: Whether the product is eligible for Blaze.
-    func isProductEligible(site: Site, product: ProductFormDataModel, isPasswordProtected: Bool) -> Bool {
+    func isProductEligible(site: Site, product: ProductFormDataModel, isPasswordProtected: Bool) async -> Bool {
         guard product.status == .published && isPasswordProtected == false else {
             return false
         }
-        return checkSiteEligibility(site)
+        return await checkSiteEligibility(site)
     }
 }
 
 private extension BlazeEligibilityChecker {
-    func checkSiteEligibility(_ site: Site) -> Bool {
+    func checkSiteEligibility(_ site: Site) async -> Bool {
         guard site.isAdmin && site.canBlaze else {
-            return false
-        }
-
-        /// Blaze DSP requires a Jetpack full sync to work. So, Jetpack CP sites are excluded from Blaze.
-        /// More discussion links at - https://github.com/woocommerce/woocommerce-ios/issues/13057
-        ///
-        guard site.isJetpackConnected && site.isJetpackThePluginInstalled else {
             return false
         }
 
         guard stores.isAuthenticatedWithoutWPCom == false else {
             return false
         }
-        return true
+
+        /// Blaze DSP requires a Jetpack full sync to work. So, Jetpack CP sites are excluded from Blaze unless the store has Blaze plugin.
+        /// More discussion links at - https://github.com/woocommerce/woocommerce-ios/issues/13057
+        ///
+        if site.isJetpackConnected && site.isJetpackThePluginInstalled {
+            return true
+        }
+
+        if let blazePlugin = await fetchBlazePluginFromRemote(siteID: site.siteID),
+           blazePlugin.active {
+            return true
+        }
+
+        return false
+    }
+
+    @MainActor
+    func fetchBlazePluginFromRemote(siteID: Int64) async -> SystemPlugin? {
+        await withCheckedContinuation { continuation in
+            stores.dispatch(SystemStatusAction.synchronizeSystemInformation(siteID: siteID) { result in
+                switch result {
+                case .success(let info):
+                    let plugin = info.systemPlugins.first(where: { $0.plugin == Constants.pluginSlug })
+                    continuation.resume(returning: plugin)
+                case .failure:
+                    continuation.resume(returning: nil)
+                }
+            })
+        }
+    }
+}
+
+private extension BlazeEligibilityChecker {
+    enum Constants {
+        static let pluginSlug = "blaze-ads/blaze-ads.php"
     }
 }

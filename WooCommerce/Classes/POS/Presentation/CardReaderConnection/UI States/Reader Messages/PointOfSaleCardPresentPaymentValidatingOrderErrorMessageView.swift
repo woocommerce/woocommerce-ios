dcff294604df133dbd55ed2e5937d0f43942f891
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2024-08-09 17:58:49 +0300

    Use consistent padding with other error views

--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Reader Messages/PointOfSaleCardPresentPaymentValidatingOrderErrorMessageView.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Reader Messages/PointOfSaleCardPresentPaymentValidatingOrderErrorMessageView.swift
@@ -6,8 +6,7 @@ struct PointOfSaleCardPresentPaymentValidatingOrderErrorMessageView: View {
 
     var body: some View {
         HStack(alignment: .center) {
-            Spacer()
-            VStack(alignment: .center, spacing: Constants.headerSpacing) {
+            VStack(alignment: .center, spacing: PointOfSaleCardPresentPaymentLayout.errorElementSpacing) {
                 POSErrorExclamationMark()
                 VStack(alignment: .center, spacing: Constants.textSpacing) {
                     Text(viewModel.title)
@@ -18,25 +17,22 @@ struct PointOfSaleCardPresentPaymentValidatingOrderErrorMessageView: View {
                     Text(viewModel.message)
                         .foregroundStyle(Color.posPrimaryTexti3)
                         .font(.posBody)
-                        .padding([.leading, .trailing])
+                        .lineLimit(2)
+                        .fixedSize(horizontal: false, vertical: true)
                 }
 
                 Button(viewModel.tryAgainButtonViewModel.title, action: viewModel.tryAgainButtonViewModel.actionHandler)
                     .buttonStyle(POSPrimaryButtonStyle())
-                    .padding([.leading, .trailing, .bottom], Constants.buttonPadding)
-                .padding()
             }
+            .padding(.horizontal, PointOfSaleCardPresentPaymentLayout.horizontalPadding)
             .multilineTextAlignment(.center)
-            Spacer()
         }
     }
 }
 
 private extension PointOfSaleCardPresentPaymentValidatingOrderErrorMessageView {
     enum Constants {
-        static let headerSpacing: CGFloat = 24
         static let textSpacing: CGFloat = 16
-        static let buttonPadding: CGFloat = 40
     }
 }
 

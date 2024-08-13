c5663cdb8dc2f4e21a8a690277f6dad5f470d2a2
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-08-13 09:49:38 +0700

    update font to scale with dynamic type

--- WooCommerce/Classes/POS/Utils/Font+WooCommercePOS.swift
+++ WooCommerce/Classes/POS/Utils/Font+WooCommercePOS.swift
@@ -6,7 +6,7 @@ extension Font {
     static let posDetail: Font = Font.system(size: UIFontMetrics.default.scaledValue(for: 16))
     static let posModalBody: Font = Font.system(size: UIFontMetrics.default.scaledValue(for: 24))
     static let posModalTitle: Font = Font.system(size: UIFontMetrics.default.scaledValue(for: 36), weight: .bold)
-    static let posHeaderTitle: Font = Font.system(size: 40,
+    static let posHeaderTitle: Font = Font.system(size: UIFontMetrics.default.scaledValue(for: 40),
                                                   weight: .bold,
                                                   design: .default)
 }

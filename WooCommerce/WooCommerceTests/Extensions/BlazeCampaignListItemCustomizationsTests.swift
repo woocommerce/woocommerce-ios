21d4d984d088a72d0ad5f4a47aff08f291fe964f
Huong Do <hi.huongdo@gmail.com>
2024-08-15 10:57:38 +0700

    Update unit tests for BlazeCampaignListItem extension

--- WooCommerce/WooCommerceTests/Extensions/BlazeCampaignListItemCustomizationsTests.swift
+++ WooCommerce/WooCommerceTests/Extensions/BlazeCampaignListItemCustomizationsTests.swift
@@ -4,9 +4,26 @@ import struct Yosemite.BlazeCampaignListItem
 
 final class BlazeCampaignListItemCustomizationsTests: XCTestCase {
 
-    func test_budgetToDisplay_is_total_budget_for_non_evergreen_campaign() {
+    func test_isActive_is_correct() {
         // Given
-        let campaign = BlazeCampaignListItem.fake().copy(totalBudget: 120, isEvergreen: false, durationDays: 6)
+        let campaigns = BlazeCampaignListItem.Status.allCases.map { status in
+            BlazeCampaignListItem.fake().copy(uiStatus: status.rawValue)
+        }
+
+        // Then
+        let activeStatuses: [BlazeCampaignListItem.Status] = [.active, .pending, .scheduled]
+        for campaign in campaigns {
+            if activeStatuses.contains(campaign.status) {
+                XCTAssertTrue(campaign.isActive)
+            } else {
+                XCTAssertFalse(campaign.isActive)
+            }
+        }
+    }
+
+    func test_budgetToDisplay_is_total_budget_for_inactive_non_evergreen_campaign() {
+        // Given
+        let campaign = BlazeCampaignListItem.fake().copy(uiStatus: "cancelled", totalBudget: 120, isEvergreen: false, durationDays: 6)
 
         // When
         let budgetToDisplay = campaign.budgetToDisplay
@@ -15,6 +32,19 @@ final class BlazeCampaignListItemCustomizationsTests: XCTestCase {
         // Then
         XCTAssertEqual(budgetToDisplay, "$120")
         XCTAssertEqual(budgetTitle, "Total budget")
+    }
+
+    func test_budgetToDisplay_is_remaining_budget_for_active_non_evergreen_campaign() {
+        // Given
+        let campaign = BlazeCampaignListItem.fake().copy(uiStatus: "active", totalBudget: 120, spentBudget: 50, isEvergreen: false, durationDays: 6)
+
+        // When
+        let budgetToDisplay = campaign.budgetToDisplay
+        let budgetTitle = campaign.budgetTitle
+
+        // Then
+        XCTAssertEqual(budgetToDisplay, "$70")
+        XCTAssertEqual(budgetTitle, "Remaining budget")
     }
 
     func test_budgetToDisplay_is_weekly_budget_for_evergreen_campaign() {

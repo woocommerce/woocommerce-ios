4bad5c515b7657b11ac29cc05e31b86461cd96a8
Josh Heald <joshheald@users.noreply.github.com>
2024-08-19 18:14:11 +0100

    13517 Correctly annouce simple products banner in a11y
    In VoiceOver, we don’t want to show the info label as it’s decorative.
    
    We also need to make it clear that the text is all a button (to open “learn more”, which should be styled as a link.)
    
    Finally, we need to make it clear that the x button dismisses the banner, “close” isn’t quite right.

--- WooCommerce/Classes/POS/Presentation/ItemListView.swift
+++ WooCommerce/Classes/POS/Presentation/ItemListView.swift
@@ -70,16 +70,20 @@ private extension ItemListView {
                     .frame(width: Constants.bannerInfoIconSize, height: Constants.bannerInfoIconSize)
                     .padding(Constants.iconPadding)
                     .foregroundColor(Color(uiColor: .wooCommercePurple(.shade30)))
+                    .accessibilityHidden(true)
                 Spacer()
             }
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: Constants.bannerTitleSpacing) {
                 Text(Localization.headerBannerTitle)
                     .font(Constants.bannerTitleFont)
-                    .padding(.bottom, Constants.bannerTitleBottomPadding)
-                Text(Localization.headerBannerSubtitle)
-                    .font(Constants.bannerSubtitleFont)
-                Text(Localization.headerBannerHint)
-                    .font(Constants.bannerSubtitleFont)
+                    .accessibilityAddTraits(.isHeader)
+                VStack(alignment: .leading, spacing: Constants.bannerTextSpacing) {
+                    Text(Localization.headerBannerSubtitle)
+                        .font(Constants.bannerSubtitleFont)
+                    Text(Localization.headerBannerHint)
+                        .font(Constants.bannerSubtitleFont)
+                }
+                .accessibilityElement(children: .combine)
             }
             .padding(.vertical, Constants.bannerVerticalPadding)
             Spacer()
@@ -90,6 +94,7 @@ private extension ItemListView {
                     Image(systemName: "xmark")
                         .font(.posBodyRegular)
                         .foregroundColor(Color.posTertiaryTexti3)
+                        .accessibilityLabel(Localization.dismissBannerAccessibilityLabel)
                 })
                 .padding(Constants.iconPadding)
                 Spacer()
@@ -100,6 +105,7 @@ private extension ItemListView {
         .background(Color.posBackgroundWhitei3)
         .cornerRadius(Constants.bannerCornerRadius)
         .shadow(color: Color.black.opacity(0.08), radius: 4, y: 2)
+        .accessibilityAddTraits(.isButton)
         .onTapGesture {
             viewModel.simpleProductsInfoButtonTapped()
         }
@@ -132,7 +138,8 @@ private extension ItemListView {
         static let bannerHeight: CGFloat = 164
         static let bannerCornerRadius: CGFloat = 8
         static let bannerVerticalPadding: CGFloat = 26
-        static let bannerTitleBottomPadding: CGFloat = 16
+        static let bannerTextSpacing: CGFloat = 0
+        static let bannerTitleSpacing: CGFloat = 8
         static let infoIconSize: CGFloat = 36
         static let bannerInfoIconSize: CGFloat = 44
         static let iconPadding: CGFloat = 26
@@ -145,15 +152,24 @@ private extension ItemListView {
             value: "Showing simple products only",
             comment: "Title of the product selector header banner, which explains current POS limitations"
         )
+
         static let headerBannerSubtitle = NSLocalizedString(
             "pos.itemlistview.headerBannerSubtitle",
             value: "Only simple physical products are available with POS right now.",
             comment: "Subtitle of the product selector header banner, which explains current POS limitations"
         )
+
         static let headerBannerHint = NSLocalizedString(
             "pos.itemlistview.headerBannerHint",
             value: "Other product types, such as variable and virtual, will become available in future updates. Learn more",
             comment: "Additional text within the product selector header banner, which explains current POS limitations"
+        )
+
+        static let dismissBannerAccessibilityLabel = NSLocalizedString(
+            "pos.itemListView.headerBanner.dismiss.button.accessibiltyLabel",
+            value: "Dismiss",
+            comment: "Accessibility label for button to dismiss the product selector header banner. " +
+            "The banner explains current POS limitations. Tapping the button prevents it being shown again."
         )
     }
 }

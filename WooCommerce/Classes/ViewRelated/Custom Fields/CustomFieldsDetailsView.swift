1706a3fa8a3a6a4e9abe47892321f5971b050da2
Hafiz Rahman <hfzrhmn@gmail.com>
2024-08-21 14:25:20 +0700

    Refactor the view to only use a single view and use isEditable flag for design changes

--- WooCommerce/Classes/ViewRelated/Custom Fields/CustomFieldsDetailsView.swift
+++ WooCommerce/Classes/ViewRelated/Custom Fields/CustomFieldsDetailsView.swift
@@ -12,16 +12,10 @@ struct CustomFieldsDetailsView: View {
                 ScrollView {
                     VStack(alignment: .leading) {
                         ForEach(customFields) { customField in
-
-                            if isEditable {
-                                EditableCustomFieldRow(title: customField.title,
-                                               content: customField.content,
-                                               contentURL: customField.contentURL)
-                            } else {
-                                CustomFieldRow(title: customField.title,
-                                               content: customField.content,
-                                               contentURL: customField.contentURL)
-                            }
+                            CustomFieldRow(isEditable: isEditable,
+                                           title: customField.title,
+                                           content: customField.content,
+                                           contentURL: customField.contentURL)
 
                             Divider()
                                 .padding(.leading)
@@ -51,6 +45,9 @@ struct CustomFieldsDetailsView: View {
 }
 
 private struct CustomFieldRow: View {
+    /// Determines if the row is editable
+    let isEditable: Bool
+
     /// Custom Field title
     ///
     let title: String
@@ -90,77 +87,30 @@ private struct CustomFieldRow: View {
                                 openURL(url) // Open in associated app for URL scheme
                             }
                         }
+                        .lineLimit(isEditable ? 3 : nil)
                 } else { // Display content as plain text
                     Text(content)
                         .footnoteStyle()
-                }
-            }.padding([.leading, .trailing], Constants.vStackPadding)
-
-            Spacer()
-        }
-        .padding([.top, .bottom], Constants.hStackPadding)
-        .padding(.horizontal, Constants.hStackPadding)
-        .frame(minHeight: Constants.height)
-    }
-}
-
-private struct EditableCustomFieldRow: View {
-    /// Custom Field title
-    ///
-    let title: String
-
-    /// Custom Field content
-    ///
-    let content: String
-
-    /// Optional URL to link the content
-    ///
-    let contentURL: URL?
-
-    /// URL to display in `SafariSheet` in app
-    ///
-    @State private var displayedURL: URL?
-
-    /// Action to open URL with system handler
-    ///
-    @Environment(\.openURL) private var openURL
-
-    var body: some View {
-        HStack {
-            VStack(alignment: .leading,
-                   spacing: Constants.spacing) {
-                Text(title)
-
-                if let url = contentURL { // Display content as a link if URL is provided
-                    Text(content)
-                        .font(.footnote)
-                        .foregroundColor(Color(.textLink))
-                        .safariSheet(url: $displayedURL)
-                        .onTapGesture {
-                            switch url.scheme {
-                            case "http", "https":
-                                displayedURL = url // Open in `SafariSheet` in app
-                            default:
-                                openURL(url) // Open in associated app for URL scheme
-                            }
-                        }
-                } else { // Display content as plain text
-                    Text(content)
-                        .footnoteStyle()
-                        .lineLimit(3)
+                        .lineLimit(isEditable ? 3 : nil)
                 }
             }.padding([.leading, .trailing], Constants.vStackPadding)
 
             Spacer()
 
-            // Chevron icon
-            Image(uiImage: .chevronImage)
-                .flipsForRightToLeftLayoutDirection(true)
-                .foregroundStyle(Color(.textTertiary))
+            if isEditable {
+                // Chevron icon
+                Image(uiImage: .chevronImage)
+                    .flipsForRightToLeftLayoutDirection(true)
+                    .foregroundStyle(Color(.textTertiary))
+            }
         }
-        .padding([.top, .bottom], Constants.hStackPadding)
-        .padding(.horizontal, Constants.hStackPadding)
+        .padding(Constants.hStackPadding)
         .frame(minHeight: Constants.height)
+        .onTapGesture {
+            if isEditable {
+                // todo-13493 add tap handling
+            }
+        }
     }
 }
 
@@ -174,16 +124,6 @@ extension CustomFieldsDetailsView {
 }
 
 private extension CustomFieldRow {
-    enum Constants {
-        static let spacing: CGFloat = 8
-        static let vStackPadding: CGFloat = 16
-        static let hStackPadding: CGFloat = 10
-        static let height: CGFloat = 64
-    }
-}
-
-
-private extension EditableCustomFieldRow {
     enum Constants {
         static let spacing: CGFloat = 8
         static let vStackPadding: CGFloat = 16

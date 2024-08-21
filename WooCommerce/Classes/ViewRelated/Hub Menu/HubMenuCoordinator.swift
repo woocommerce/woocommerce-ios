71df1bfa07ddd2527980408820f25f42fafac749
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-08-21 11:19:21 +0530

    Wrap hub menu controller in a navigation controller for navigation.

--- WooCommerce/Classes/ViewRelated/Hub Menu/HubMenuCoordinator.swift
+++ WooCommerce/Classes/ViewRelated/Hub Menu/HubMenuCoordinator.swift
@@ -66,7 +66,8 @@ final class HubMenuCoordinator {
                                                   navigationController: navigationController,
                                                   tapToPayBadgePromotionChecker: tapToPayBadgePromotionChecker)
         if let hubMenuController = hubMenuController {
-            navigationController.viewControllers = [hubMenuController]
+            let navigationController = UINavigationController(rootViewController: hubMenuController)
+            tabContainerController.wrappedController = navigationController
         }
 
         if notificationsSubscription == nil {

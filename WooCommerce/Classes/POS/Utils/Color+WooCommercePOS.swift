4eb7b155a99a5142ce961bf81acc1e5940b3b412
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2024-08-22 22:20:02 +0300

    Define generic posBackground colors

--- WooCommerce/Classes/POS/Utils/Color+WooCommercePOS.swift
+++ WooCommerce/Classes/POS/Utils/Color+WooCommercePOS.swift
@@ -2,9 +2,43 @@ import SwiftUI
 
 extension Color {
 
+    // MARK: - Background
+
+    /* POS Background colors are defined in a similar philosophy as system background colors:
+     *
+     * The first color is intended to be the main background, farthest back.
+     * Secondary and tertiary colors are layered on top of the main background, when needed.
+     */
+
+    static var posPrimaryBackground: Color {
+        Color(
+            UIColor(
+                light: UIColor(red: 246.0 / 255.0, green: 247.0 / 255.0, blue: 247.0 / 255.0, alpha: 1.0),
+                dark: UIColor(red: 30.0 / 255.0, green: 30.0 / 255.0, blue: 30.0 / 255.0, alpha: 1.0)
+            )
+        )
+    }
+
+    static var posSecondaryBackground: Color {
+        Color(
+            UIColor(
+                light: .white,
+                dark: .tertiarySystemBackground
+            )
+        )
+    }
+
+    static var posTertiaryBackground: Color {
+        Color(
+            UIColor(
+                light: UIColor(red: 250.0 / 255.0, green: 250.0 / 255.0, blue: 250.0 / 255.0, alpha: 1.0),
+                dark: .tertiarySystemBackground
+            )
+        )
+    }
+
     /// Default POS text color
     ///
-    static var primaryText: Color {
         return Color.primary
     }
 
@@ -39,18 +73,6 @@ extension Color {
 
     static var posIconGrayi3: Color {
         return Color.gray
-    }
-
-    static var posBackgroundGreyi3: Color {
-        Color(red: 246.0 / 255.0, green: 247.0 / 255.0, blue: 247.0 / 255.0)
-    }
-
-    static var posBackgroundWhitei3: Color {
-        Color.white
-    }
-
-    static var posBackgroundEmptyWhitei3: Color {
-        Color(red: 250.0 / 255.0, green: 250.0 / 255.0, blue: 250.0 / 255.0)
     }
 
     static var posTotalsSeparator: Color {

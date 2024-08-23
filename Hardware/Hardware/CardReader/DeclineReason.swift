dbd4dc06155dd5ee5c212c931601944b9705cd2e
Josh Heald <joshheald@users.noreply.github.com>
2024-08-23 13:26:08 +0100

    Improve punctuation of card payment errors

--- Hardware/Hardware/CardReader/DeclineReason.swift
+++ Hardware/Hardware/CardReader/DeclineReason.swift
@@ -100,56 +100,71 @@ extension DeclineReason {
     public var localizedDescription: String? {
         switch self {
         case .temporary:
-            return NSLocalizedString("Trying again may succeed, or try another means of payment",
+            return NSLocalizedString("Trying again may succeed, or try another means of payment.",
                                      comment: "Message when a card is declined due to a potentially temporary problem.")
+
         case .fraud:
-            return NSLocalizedString("Try another means of payment",
+            return NSLocalizedString("Try another means of payment.",
                                      comment: "Message when a lost or stolen card is presented for payment. Do NOT disclose fraud.")
+
         case .generic:
-            return NSLocalizedString("Payment was declined for an unspecified reason. Try another means of payment",
+            return NSLocalizedString("Payment was declined for an unspecified reason. Try another means of payment.",
                                      comment: "Message when payment is declined for a non specific reason.")
+
         case .invalidAccount:
-            return NSLocalizedString("The card or card account is invalid. Try another means of payment",
+            return NSLocalizedString("The card or card account is invalid. Try another means of payment.",
                                      comment: "Message when payment is declined for a non specific reason.")
 
         case .cardNotSupported:
-            return NSLocalizedString("The card does not support this type of purchase. Try another means of payment",
+            return NSLocalizedString("The card does not support this type of purchase. Try another means of payment.",
                                      comment: "Message when the card presented does not allow this type of purchase.")
+
         case .currencyNotSupported:
-            return NSLocalizedString("The card does not support this currency. Try another means of payment",
+            return NSLocalizedString("The card does not support this currency. Try another means of payment.",
                                      comment: "Message when the card presented does not support the order currency.")
+
         case .duplicateTransaction:
-            return NSLocalizedString("An identical transaction was submitted recently. If you wish to continue, try another means of payment",
+            return NSLocalizedString("An identical transaction was submitted recently. If you wish to continue, try another means of payment.",
                                      comment: "Message when it looks like a duplicate transaction is being attempted.")
+
         case .expiredCard:
-            return NSLocalizedString("The card has expired. Try another means of payment",
+            return NSLocalizedString("The card has expired. Try another means of payment.",
                                      comment: "Message when the presented card is past its expiration date.")
+
         case .incorrectPostalCode:
-            return NSLocalizedString("The transaction postal code and card postal code do not match. Try another means of payment",
+            return NSLocalizedString("The transaction postal code and card postal code do not match. Try another means of payment.",
                                      comment: "Message when the presented card postal code doesn't match the order postal code.")
+
         case .insufficientFunds:
-            return NSLocalizedString("Payment declined due to insufficient funds. Try another means of payment",
+            return NSLocalizedString("Payment declined due to insufficient funds. Try another means of payment.",
                                      comment: "Message when the presented card remaining credit or balance is insufficient for the purchase.")
+
         case .invalidAmount:
             return NSLocalizedString("The payment amount is not allowed for the card presented. Try another means of payment.",
                                      comment: "Message when the presented card does not allow the purchase amount.")
+
         case .pinRequired:
-            return NSLocalizedString("This card requires a PIN code and thus cannot be processed. Try another means of payment",
+            return NSLocalizedString("This card requires a PIN code and thus cannot be processed. Try another means of payment.",
                                      comment: "Message when a card requires a PIN code and we have no means of entering such a code.")
+
         case .incorrectPin:
-            return NSLocalizedString("An incorrect PIN has been entered. Try again, or use another means of payment",
+            return NSLocalizedString("An incorrect PIN has been entered. Try again, or use another means of payment.",
                                      comment: "Message when an incorrect PIN has been entered.")
+
         case .tooManyPinTries:
-            return NSLocalizedString("An incorrect PIN has been entered too many times. Try another means of payment",
+            return NSLocalizedString("An incorrect PIN has been entered too many times. Try another means of payment.",
                                      comment: "Message when an incorrect PIN has been entered too many times.")
+
         case .testCard:
-            return NSLocalizedString("System test cards are not permitted for payment. Try another means of payment",
+            return NSLocalizedString("System test cards are not permitted for payment. Try another means of payment.",
                                      comment: "Message when attempting to pay for a live transaction with a test card.")
+
         case .testModeLiveCard:
             return NSLocalizedString("A live card was used on a site in test mode. Use a test card instead.",
                                      comment: "Message when attempting to pay for a test transaction with a live card.")
+
         case .unknown:
-            return NSLocalizedString("Payment was declined for an unknown reason. Try another means of payment",
+            return NSLocalizedString("Payment was declined for an unknown reason. Try another means of payment.",
                                      comment: "Message when we don't know exactly why the payment was declined.")
         }
     }

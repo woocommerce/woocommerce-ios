cb0a3e272232d67a4ee0e5f8c1f8d341bdeecf00
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-08-26 16:31:22 +0700

    Update font PointOfSaleCardPresentPaymentConnectingFailedNonRetryableView

--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Connection Alerts/PointOfSaleCardPresentPaymentConnectingFailedNonRetryableView.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Connection Alerts/PointOfSaleCardPresentPaymentConnectingFailedNonRetryableView.swift
@@ -6,11 +6,13 @@ struct PointOfSaleCardPresentPaymentConnectingFailedNonRetryableView: View {
     var body: some View {
         VStack(spacing: PointOfSaleReaderConnectionModalLayout.verticalSpacing) {
             Text(viewModel.title)
+                .font(POSFontStyle.posBodyRegular)
                 .accessibilityAddTraits(.isHeader)
 
             Image(decorative: viewModel.imageName)
 
             Text(viewModel.errorDetails)
+                .font(POSFontStyle.posDetailLight)
 
             Button(viewModel.cancelButtonViewModel.title,
                    action: viewModel.cancelButtonViewModel.actionHandler)

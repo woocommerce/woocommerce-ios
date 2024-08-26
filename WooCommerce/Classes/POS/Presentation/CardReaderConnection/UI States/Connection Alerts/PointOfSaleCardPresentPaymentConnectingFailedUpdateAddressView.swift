04f78fc57eb24283bcd0456be18a32aab7601f81
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-08-26 16:46:42 +0700

    Update font PointOfSaleCardPresentPaymentConnectingFailedUpdateAddressView

--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Connection Alerts/PointOfSaleCardPresentPaymentConnectingFailedUpdateAddressView.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Connection Alerts/PointOfSaleCardPresentPaymentConnectingFailedUpdateAddressView.swift
@@ -5,6 +5,7 @@ struct PointOfSaleCardPresentPaymentConnectingFailedUpdateAddressView: View {
     var body: some View {
         VStack(spacing: PointOfSaleReaderConnectionModalLayout.verticalSpacing) {
             Text(viewModel.title)
+                .font(POSFontStyle.posBodyRegular)
                 .accessibilityAddTraits(.isHeader)
 
             Image(decorative: viewModel.imageName)

0d80b8973df0030efd7e4c1108af00d4e7f97722
Božidar Ševo <bozidarsevo@gmail.com>
2024-08-26 11:30:15 +0200

    Add order id extraction from note full data

--- WooCommerce/Classes/ServiceLocator/PushNotification.swift
+++ WooCommerce/Classes/ServiceLocator/PushNotification.swift
@@ -28,6 +28,9 @@ struct PushNotification {
     /// The `alert.message` value received from the Remote Notification's `userInfo`.
     ///
     let message: String?
+    /// The orderID value received from the Remote Notification's `userInfo`, extracted from note_full_data.
+    ///
+    let orderID: Int64?
 }
 
 extension PushNotification {
@@ -42,7 +45,28 @@ extension PushNotification {
               }
         let subtitle = alert.string(forKey: APNSKey.alertSubtitle)
         let message = alert.string(forKey: APNSKey.alertMessage)
-        return PushNotification(noteID: noteID, siteID: siteID, kind: noteKind, title: title, subtitle: subtitle, message: message)
+        let orderID: Int64? = orderIdFromNoteData(userInfo.string(forKey: APNSKey.noteFullData))
+        return PushNotification(noteID: noteID, siteID: siteID, kind: noteKind, title: title, subtitle: subtitle, message: message, orderID: orderID)
+    }
+
+    static func orderIdFromNoteData(_ noteFulldata: String?) -> Int64? {
+        guard let noteFulldata, var data = Data(base64Encoded: noteFulldata) else {
+            return nil
+        }
+        data.removeFirst(2)
+        guard let zlib = try? (data as NSData).decompressed(using: .zlib) else {
+            return nil
+        }
+        let zlibData = Data(referencing: zlib)
+        guard let dataDictionary = try? JSONSerialization.jsonObject(with: zlibData) as? [String: Any],
+              let notes = dataDictionary[APNSKey.notes] as? [[String: Any]],
+              let firstNote = notes.first, let meta = firstNote[APNSKey.meta] as? [String: Any],
+              let ids = meta[APNSKey.ids] as? [String: Any],
+              let orderID = ids[APNSKey.order] as? Int64
+        else {
+            return nil
+        }
+        return orderID
     }
 }
 
@@ -56,6 +80,11 @@ enum APNSKey {
     static let type = "type"
     static let siteID = "blog"
     static let postID = "post_id"
+    static let noteFullData = "note_full_data"
+    static let notes = "notes"
+    static let meta = "meta"
+    static let ids = "ids"
+    static let order = "order"
 }
 
 /// SwiftUI Identifiable conformance

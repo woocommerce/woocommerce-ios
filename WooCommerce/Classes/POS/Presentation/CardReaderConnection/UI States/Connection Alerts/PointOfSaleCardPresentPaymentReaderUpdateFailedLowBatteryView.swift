9c62e6b15376a0b9859c289393ae0f753bff5051
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-08-26 16:59:06 +0700

    Update font PointOfSaleCardPresentPaymentReaderUpdateFailedLowBatteryView

--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Connection Alerts/PointOfSaleCardPresentPaymentReaderUpdateFailedLowBatteryView.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Connection Alerts/PointOfSaleCardPresentPaymentReaderUpdateFailedLowBatteryView.swift
@@ -10,11 +10,13 @@ struct PointOfSaleCardPresentPaymentReaderUpdateFailedLowBatteryView: View {
     var body: some View {
         VStack(spacing: PointOfSaleReaderConnectionModalLayout.verticalSpacing) {
             Text(viewModel.title)
+                .font(POSFontStyle.posBodyRegular)
                 .accessibilityAddTraits(.isHeader)
 
             Image(decorative: viewModel.imageName)
 
             Text(viewModel.batteryLevelInfo)
+                .font(POSFontStyle.posDetailLight)
 
             Button(viewModel.cancelButtonViewModel.title,
                    action: viewModel.cancelButtonViewModel.actionHandler)

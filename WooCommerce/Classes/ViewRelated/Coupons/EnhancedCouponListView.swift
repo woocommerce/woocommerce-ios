151896e66d94a2872f803ef23193d83ba21854bf
Hafiz Rahman <hfzrhmn@gmail.com>
2024-08-27 12:21:07 +0700

    Restore workaround to still support showing bar button items when navigating from swift UI.

--- WooCommerce/Classes/ViewRelated/Coupons/EnhancedCouponListView.swift
+++ WooCommerce/Classes/ViewRelated/Coupons/EnhancedCouponListView.swift
@@ -3,12 +3,35 @@ import SwiftUI
 struct EnhancedCouponListView: UIViewControllerRepresentable {
     let siteID: Int64
 
+    class Coordinator {
+         var parentObserver: NSKeyValueObservation?
+         var rightBarButtonItemsObserver: NSKeyValueObservation?
+     }
+
+    /// This is a UIKit solution for fixing Bar Button Items ignored in NavigationView.
+    /// This solution doesn't require making internal changes to the destination `UIViewController`
+    /// and should be called once, when wrapped.
+    /// Solution proposed here: https://stackoverflow.com/a/68567095/7241994
+    ///
+
     func makeUIViewController(context: Self.Context) -> EnhancedCouponListViewController {
         let viewController = EnhancedCouponListViewController(siteID: siteID)
+        context.coordinator.parentObserver = viewController.observe(\.parent, changeHandler: { vc, _ in
+             vc.parent?.navigationItem.rightBarButtonItems = vc.navigationItem.rightBarButtonItems
+         })
+
+        // This fixes the issue when `rightBarButtonItem` is updated in `CouponListViewController`,
+        // the hosting controller should be updated to reflect the change.
+         context.coordinator.rightBarButtonItemsObserver = viewController.observe(\.navigationItem.rightBarButtonItems, changeHandler: { vc, _ in
+             vc.parent?.navigationItem.rightBarButtonItems = vc.navigationItem.rightBarButtonItems
+         })
+
         return viewController
     }
 
     func updateUIViewController(_ uiViewController: EnhancedCouponListViewController, context: Context) {
         // nothing to do here
     }
+
+    func makeCoordinator() -> Self.Coordinator { Coordinator() }
 }

6a51a50393a4592ec73244bf1d046deb37b95aae
Hafiz Rahman <hfzrhmn@gmail.com>
2024-08-27 12:29:26 +0700

    Restore workaround also to ReviewsView to still support showing bar button items when navigating from swift UI.

--- WooCommerce/Classes/ViewRelated/Coupons/EnhancedCouponListView.swift
+++ WooCommerce/Classes/ViewRelated/Coupons/EnhancedCouponListView.swift
@@ -16,6 +16,9 @@ struct EnhancedCouponListView: UIViewControllerRepresentable {
 
     func makeUIViewController(context: Self.Context) -> EnhancedCouponListViewController {
         let viewController = EnhancedCouponListViewController(siteID: siteID)
+
+        // This makes sure that the navigation item of the hosting controller
+        // is in sync with that of the wrapped controller.
         context.coordinator.parentObserver = viewController.observe(\.parent, changeHandler: { vc, _ in
              vc.parent?.navigationItem.rightBarButtonItems = vc.navigationItem.rightBarButtonItems
          })
--- WooCommerce/Classes/ViewRelated/Reviews/ReviewsView.swift
+++ WooCommerce/Classes/ViewRelated/Reviews/ReviewsView.swift
@@ -5,14 +5,40 @@ import SwiftUI
 struct ReviewsView: UIViewControllerRepresentable {
     let siteID: Int64
 
+    class Coordinator {
+         var parentObserver: NSKeyValueObservation?
+         var rightBarButtonItemsObserver: NSKeyValueObservation?
+     }
+
     typealias UIViewControllerType = ReviewsViewController
+
+    /// This is a UIKit solution for fixing Bar Button Items ignored in NavigationView.
+    /// This solution doesn't require making internal changes to the destination `UIViewController`
+    /// and should be called once, when wrapped.
+    /// Solution proposed here: https://stackoverflow.com/a/68567095/7241994
+    ///
 
     func makeUIViewController(context: Self.Context) -> ReviewsViewController {
         let viewController = ReviewsViewController(siteID: siteID)
+
+        // This makes sure that the navigation item of the hosting controller
+        // is in sync with that of the wrapped controller.
+        context.coordinator.parentObserver = viewController.observe(\.parent, changeHandler: { vc, _ in
+             vc.parent?.navigationItem.rightBarButtonItems = vc.navigationItem.rightBarButtonItems
+         })
+
+        // This fixes the issue when `rightBarButtonItem` is updated in `CouponListViewController`,
+        // the hosting controller should be updated to reflect the change.
+         context.coordinator.rightBarButtonItemsObserver = viewController.observe(\.navigationItem.rightBarButtonItems, changeHandler: { vc, _ in
+             vc.parent?.navigationItem.rightBarButtonItems = vc.navigationItem.rightBarButtonItems
+         })
+
         return viewController
     }
 
     func updateUIViewController(_ uiViewController: ReviewsViewController, context: Context) {
         // nothing to do here
     }
+
+    func makeCoordinator() -> Self.Coordinator { Coordinator() }
 }

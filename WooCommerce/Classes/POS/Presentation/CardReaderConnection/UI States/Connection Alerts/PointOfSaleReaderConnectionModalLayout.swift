47a21a54635f4c97f465811bb1fba8e01e63f136
Josh Heald <joshheald@users.noreply.github.com>
2024-08-28 11:32:35 +0100

    13731 Update reader alert button spacing to design

--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Connection Alerts/PointOfSaleReaderConnectionModalLayout.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/Connection Alerts/PointOfSaleReaderConnectionModalLayout.swift
@@ -2,6 +2,6 @@ import Foundation
 
 enum PointOfSaleReaderConnectionModalLayout {
     static var verticalSpacing: CGFloat = 32
-    static var buttonSpacing: CGFloat = 16
+    static var buttonSpacing: CGFloat = 24
     static var contentPadding: CGFloat = 64
 }

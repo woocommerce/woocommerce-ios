9b8f06fa0c9a0a6a3d00a5e94b1710b60df9fb25
Paolo Musolino <info@codeido.com>
2024-09-05 19:09:44 +0200

    feat: add fake and copy functionality to Networking.MetaData
    - Update `Networking.generated.swift` to add a fake method for `Networking.MetaData`.
    - Enhance `Models+Copiable.generated.swift` to implement a copy method for `Networking.MetaData`.
    - Modify `MetaData.swift` to include `GeneratedCopiable` and `GeneratedFakeable` protocols, and update the initializer to accept an optional `value`.

--- Networking/Networking/Model/MetaData.swift
+++ Networking/Networking/Model/MetaData.swift
@@ -4,14 +4,14 @@ import Codegen
 /// Represents the metadata within an Order or Product
 /// Currently only handles `String` metadata values
 ///
-public struct MetaData: Codable, Equatable, Sendable {
-    public let metadataID: Int64
+public struct MetaData: Codable, Equatable, Sendable, GeneratedCopiable, GeneratedFakeable {
+    public let metadataID: Int64 //read-only
     public let key: String
-    public let value: String
+    public let value: String?
 
     /// MetaData struct initializer.
     ///
-    public init(metadataID: Int64, key: String, value: String) {
+    public init(metadataID: Int64, key: String, value: String?) {
         self.metadataID = metadataID
         self.key = key
         self.value = value
@@ -23,7 +23,7 @@ public struct MetaData: Codable, Equatable, Sendable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let metadataID = try container.decode(Int64.self, forKey: .metadataID)
         let key = try container.decode(String.self, forKey: .key)
-        let value = container.failsafeDecodeIfPresent(String.self, forKey: .value) ?? ""
+        let value = container.failsafeDecodeIfPresent(String.self, forKey: .value)
 
         self.init(metadataID: metadataID, key: key, value: value)
     }

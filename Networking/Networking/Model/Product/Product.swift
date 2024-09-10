175567baba71b8f3c7e881ad300791844f6d2a1b
Paolo Musolino <info@codeido.com>
2024-09-10 17:33:47 +0200

    revert: Refactor Product custom fields handling and update tests - Simplify the `buildCustomFields` method in `Product.swift`. - Remove redundant tests for metadata encoding in `ProductMapperTests.swift`.

--- Networking/Networking/Model/Product/Product.swift
+++ Networking/Networking/Model/Product/Product.swift
@@ -774,14 +774,7 @@ public struct Product: Codable, GeneratedCopiable, Equatable, GeneratedFakeable 
     private func buildCustomFields() -> [[String: String]] {
         var customFieldsArray: [[String: String]] = []
         for customField in customFields {
-            var fieldDict: [String: String] = ["key": customField.key]
-            if let metadataID = customField.metadataID {
-                fieldDict["id"] = String(describing: customField.metadataID)
-            }
-            if let value = customField.value {
-                fieldDict["value"] = value
-            }
-            customFieldsArray.append(fieldDict)
+            customFieldsArray.append(["id": "\(customField.metadataID)", "key": customField.key, "value": customField.value])
         }
         return customFieldsArray
     }

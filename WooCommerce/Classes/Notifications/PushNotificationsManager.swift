ecf4d31980efe59ba0f78c736c8686c6c878ec92
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-09-10 11:48:14 +0530

    Revert "WIP"
    This reverts commit 5d8b62f31591139bcb0a7209e2237ed432290461.

--- WooCommerce/Classes/Notifications/PushNotificationsManager.swift
+++ WooCommerce/Classes/Notifications/PushNotificationsManager.swift
@@ -473,17 +473,10 @@ private extension PushNotificationsManager {
     func handleRemoteNotificationInAllAppStates(_ userInfo: [AnyHashable: Any]) {
         DDLogVerbose("📱 Push Notification Received: \n\(userInfo)\n")
 
-        if let jsonData = try? JSONSerialization.data(withJSONObject: userInfo, options: .prettyPrinted),
-           let theJSONText = String(data: jsonData,
-                                          encoding: .utf8) {
-            DDLogVerbose("📱 theJSONText: \n\(theJSONText)\n")
-        }
-
         if let typeString = userInfo.string(forKey: APNSKey.type),
            let type = Note.Kind(rawValue: typeString),
            let siteID = siteID,
            let notificationSiteID = userInfo[APNSKey.siteID] as? Int64 {
-
             // Badge: Update
             incrementNotificationCount(siteID: notificationSiteID, type: type, incrementCount: 1) { [weak self] in
                 self?.loadNotificationCountAndUpdateApplicationBadgeNumber(siteID: siteID, type: type, postNotifications: true)

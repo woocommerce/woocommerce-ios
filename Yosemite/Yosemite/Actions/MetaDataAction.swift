c6515bbf2baaf389c9b46418018192187ba5234b
Paolo Musolino <info@codeido.com>
2024-09-11 18:02:47 +0200

    feat: Add MetaDataAction and MetaDataStore classes
    - Add MetaDataAction.swift to define actions for updating order and product metadata.
    - Add MetaDataStore.swift to manage the storage and retrieval of metadata for orders and products.

---
+++ Yosemite/Yosemite/Actions/MetaDataAction.swift
@@ -0,0 +1,31 @@
+import Foundation
+import Networking
+
+/// MetaDataAction: Defines all of the Actions supported by the MetaData.
+///
+public enum MetaDataAction: Action {
+
+    /// Update order metadata in the local database.
+    ///
+    /// - Parameter siteID: Site id of the order.
+    /// - Parameter orderID: ID of the order.
+    /// - Parameter metadata: Metadata to be updated.
+    /// - Parameter onCompletion: Callback called when the action is finished, including the result of the update operation.
+    ///
+    case updateOrderMetaData(siteID: Int64,
+                             orderID: Int64,
+                             metadata: [[String: Any]],
+                             onCompletion: (Result<[MetaData], Error>) -> Void)
+    
+    /// Update product metadata in the local database.
+    ///
+    /// - Parameter siteID: Site id of the product.
+    /// - Parameter productID: ID of the product.
+    /// - Parameter metadata: Metadata to be updated.
+    /// - Parameter onCompletion: Callback called when the action is finished, including the result of the update operation.
+    ///
+    case updateProductMetaData(siteID: Int64,
+                               productID: Int64,
+                               metadata: [[String: Any]],
+                               onCompletion: (Result<[MetaData], Error>) -> Void)
+}

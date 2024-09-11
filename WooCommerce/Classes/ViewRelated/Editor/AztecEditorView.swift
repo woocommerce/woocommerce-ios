3f75b67d1096f0f55a16be444a03c510fac26f09
Hafiz Rahman <hfzrhmn@gmail.com>
2024-09-11 19:35:16 +0700

    Add comment to explain AztecEditorView usage.

--- WooCommerce/Classes/ViewRelated/Editor/AztecEditorView.swift
+++ WooCommerce/Classes/ViewRelated/Editor/AztecEditorView.swift
@@ -1,5 +1,11 @@
 import SwiftUI
 
+/// `UIViewControllerRepresentable` to use an Aztec Editor in SwiftUI.
+/// This view needs to be wrapped inside a parent View that manages its states and toolbar as needed.
+/// Params:
+/// - value: Binding to the content of the editor.
+/// - viewController: To get the content of the editor, the parent View can pass this and then call `getLatestContent()`
+///
 struct AztecEditorView: UIViewControllerRepresentable {
     @Binding var value: String
     @Binding var viewController: AztecEditorViewController?

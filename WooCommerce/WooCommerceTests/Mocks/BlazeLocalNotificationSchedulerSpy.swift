b5909f19204773c6a230a00e7ddcb05780148cf7
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-09-13 10:12:08 +0530

    Unit test that Dashboard starts observing local notification user response upon init.

--- WooCommerce/WooCommerceTests/Mocks/BlazeLocalNotificationSchedulerSpy.swift
+++ WooCommerce/WooCommerceTests/Mocks/BlazeLocalNotificationSchedulerSpy.swift
@@ -1,8 +1,13 @@
 @testable import WooCommerce
 
 final class MockBlazeLocalNotificationScheduler: BlazeLocalNotificationScheduler {
+    private(set) var observeNotificationUserResponseCalled = false
     private(set) var scheduleNoCampaignReminderCalled = false
     private(set) var scheduleAbandonedCreationReminderCalled = false
+
+    func observeNotificationUserResponse() {
+        observeNotificationUserResponseCalled = true
+    }
 
     func scheduleNoCampaignReminder() async {
         scheduleNoCampaignReminderCalled = true

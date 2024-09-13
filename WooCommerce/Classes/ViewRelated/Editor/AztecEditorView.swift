f32a3f3636e4aa55cf4c9f2d7753cc6671913a36
Hafiz Rahman <hfzrhmn@gmail.com>
2024-09-13 20:07:53 +0700

    Fix typo.

--- WooCommerce/Classes/ViewRelated/Editor/AztecEditorView.swift
+++ WooCommerce/Classes/ViewRelated/Editor/AztecEditorView.swift
@@ -57,7 +57,7 @@ struct AztecEditorView: UIViewControllerRepresentable {
 private extension AztecEditorView {
     enum Localization {
         static let cancel = NSLocalizedString(
-            "aztedEditorView.cancel",
+            "aztecEditorView.cancel",
             value: "Cancel",
             comment: "Button to dismiss the Aztec Editor View"
         )

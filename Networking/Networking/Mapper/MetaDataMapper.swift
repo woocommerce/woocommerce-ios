3fba503a2358613a8031ab9322c3c9d2c5e1d7cb
Paolo Musolino <info@codeido.com>
2024-09-16 10:22:35 +0200

    feat: add comment to specify why keys starting with `_` are filtered out

--- Networking/Networking/Mapper/MetaDataMapper.swift
+++ Networking/Networking/Mapper/MetaDataMapper.swift
@@ -10,9 +10,11 @@ struct MetaDataMapper: Mapper {
         let decoder = JSONDecoder()
         if hasDataEnvelope(in: response) {
             let envelope = try decoder.decode(DataEnvelope.self, from: response)
+            // Filter out metadata if the key is prefixed with an underscore (internal meta keys)
             return envelope.data.metadata.filter { !$0.key.hasPrefix("_") }
         } else {
             let envelope = try decoder.decode(MetaDataEnvelope.self, from: response)
+            // Filter out metadata if the key is prefixed with an underscore (internal meta keys)
             return envelope.metadata.filter { !$0.key.hasPrefix("_") }
         }
     }

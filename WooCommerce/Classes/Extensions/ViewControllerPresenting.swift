37c2564944026908bf3c2071ba9487d06d23e22c
Josh Heald <joshheald@users.noreply.github.com>
2024-09-23 11:59:30 +0100

    Dismiss Onboarding when successful
    Onboarding can be either pushed (on iPhone from Collect Payment in settings) or modally presented (in all other contexts.)
    
    When it was pushed onto a nav stack, it would be dismissed automatically when it completed… but when it was modally presented, it would remain on screen until the user swiped it away.
    
    To improve this, we can detect whether the `onboardingViewController` is in a navstack, or modally presented, and react accordingly when dismissing it.
    
    Now, the onboarding screen is correctly removed and payment continues, when it’s completed.

--- WooCommerce/Classes/Extensions/ViewControllerPresenting.swift
+++ WooCommerce/Classes/Extensions/ViewControllerPresenting.swift
@@ -8,6 +8,7 @@ protocol ViewControllerPresenting: AnyObject {
     func dismiss(animated: Bool, completion: (() -> Void)?)
     func dismiss(animated: Bool)
     func show(_ vc: UIViewController, sender: Any?)
+    var presentingViewController: UIViewController? { get }
     var presentedViewController: UIViewController? { get }
     var navigationController: UINavigationController? { get }
 }
@@ -44,6 +45,8 @@ final class NullViewControllerPresenting: ViewControllerPresenting {
     func show(_ vc: UIViewController, sender: Any?) {
         // no-op
     }
+
+    var presentingViewController: UIViewController?
 
     var presentedViewController: UIViewController?
 

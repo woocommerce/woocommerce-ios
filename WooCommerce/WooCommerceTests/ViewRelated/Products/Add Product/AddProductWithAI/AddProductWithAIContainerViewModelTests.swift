5c7bdfe4ad84c48509cd75cfa594040256c3bb33
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-09-26 12:36:22 +0530

    Remove unit test for deleted code.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Add Product/AddProductWithAI/AddProductWithAIContainerViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Add Product/AddProductWithAI/AddProductWithAIContainerViewModelTests.swift
@@ -57,31 +57,6 @@ final class AddProductWithAIContainerViewModelTests: XCTestCase {
         XCTAssertFalse(viewModel.canBeDismissed)
     }
 
-    // MARK: `didGenerateDataFromPackage`
-
-    func test_didGenerateDataFromPackage_sets_values_from_package_flow() {
-        // Given
-        let expectedName = "Fancy new smart phone"
-        let expectedDescription = "Phone, White color"
-        let expectedFeatures = expectedDescription
-
-        let viewModel = AddProductWithAIContainerViewModel(siteID: 123,
-                                                           source: .productDescriptionAIAnnouncementModal,
-                                                           onCancel: { },
-                                                           onCompletion: { _ in })
-
-        // When
-        viewModel.didGenerateDataFromPackage(.init(name: expectedName,
-                                                   description: expectedDescription,
-                                                   image: nil))
-
-        // Then
-        XCTAssertEqual(viewModel.productName, expectedName)
-        XCTAssertEqual(viewModel.productDescription, expectedDescription)
-        XCTAssertEqual(viewModel.productFeatures, expectedFeatures)
-        XCTAssertEqual(viewModel.addProductNameViewModel.productName, expectedName)
-    }
-
     // MARK: `onProductFeaturesAdded`
 
     func test_onProductFeaturesAdded_tracks_generate_details_event_with_is_first_attempt_as_true_for_first_time_generation() throws {

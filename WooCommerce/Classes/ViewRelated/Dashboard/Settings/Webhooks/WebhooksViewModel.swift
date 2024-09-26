fab68badd18b535d3c86298f5deea8575db4ffbc
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-09-26 12:12:08 +0700

    Add MockWebhooksService. Tests for createWebhook

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Webhooks/WebhooksViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Webhooks/WebhooksViewModel.swift
@@ -13,14 +13,11 @@ final class WebhooksViewModel: ObservableObject {
 
     @MainActor
     func listAllWebhooks() async throws {
-        do {
-            webhooks = try await service.listAllWebhooks()
-        } catch {
-            throw NSError(domain: error.localizedDescription, code: 0)
-        }
+        webhooks = try await service.listAllWebhooks()
     }
 
-    func createWebhook(_ webhook: AvailableWebhook, _ deliveryURLString: String) async throws {
+    @discardableResult
+    func createWebhook(_ webhook: AvailableWebhook, _ deliveryURLString: String) async throws -> Webhook {
         var topic: String
         switch webhook {
         case .orderCreated:
@@ -36,6 +33,6 @@ final class WebhooksViewModel: ObservableObject {
         guard let url = URL(string: deliveryURLString) else {
             throw NSError(domain: "Invalid URL", code: 0)
         }
-        let webhook = try await service.createWebhook(topic: topic, url: url)
+        return try await service.createWebhook(topic: topic, url: url)
     }
 }
--- WooCommerce/WooCommerceTests/ViewRelated/Webhooks/WebhooksViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Webhooks/WebhooksViewModelTests.swift
@@ -3,32 +3,141 @@ import Yosemite
 @testable import WooCommerce
 
 final class MockWebhooksService: WebhooksServiceProtocol {
-    var webhooks: [Webhook] = []
+    var hasWebhooks: Bool = true
+    var hasError: Bool = false
 
     func listAllWebhooks() async throws -> [Yosemite.Webhook] {
-        webhooks
+        if hasError {
+            throw NSError(domain: "Error", code: 0)
+        }
+        if hasWebhooks {
+            return makeWebhooksForTesting()
+        } else {
+            return []
+        }
     }
 
-    func createWebhook(topic: String, url: URL) async throws -> Yosemite.Webhook {
-        Webhook(name: "name",
-                status: "status",
-                topic: "topic",
-                deliveryURL: URL(string: "https://server.site/1234")!)
+    func createWebhook(topic: String = "order.created",
+                       url: URL = URL(string: "https://server.site/1234")!) async throws -> Yosemite.Webhook {
+        let webhook = Webhook(name: "some name",
+                              status: "active",
+                              topic: topic,
+                              deliveryURL: url)
+        if hasError {
+            throw NSError(domain: "Error", code: 0)
+        } else {
+            return webhook
+        }
+    }
+
+    private func makeWebhooksForTesting() -> [Webhook] {
+        let firstWebhook =  Webhook(name: "Webhook created on Sep 26, 2024 @ 02:16 AM",
+                                    status: "active",
+                                    topic: "order.updated",
+                                    deliveryURL: URL(string: "https://server.site/1234")!)
+        let secondWebhook = Webhook(name: "Webhook created on Sep 26, 2024 @ 02:30 AM",
+                                    status: "active",
+                                    topic: "order.created",
+                                    deliveryURL: URL(string: "https://server.site/1234")!)
+        return [firstWebhook, secondWebhook]
     }
 }
 
 final class WebhooksViewModelTests: XCTestCase {
-
     func test_listAllWebhooks_when_site_has_no_webhooks_then_returns_empty() async {
-        let sut = WebhooksViewModel(service: MockWebhooksService())
-        var expectedWebhooks: [Webhook] = []
-        
+        // Given
+        let service = MockWebhooksService()
+        service.hasWebhooks = false
+
+        let sut = WebhooksViewModel(service: service)
+        let expectedWebhooks: [Webhook] = []
+
         do {
+            // When
             try await sut.listAllWebhooks()
+            // Then
             XCTAssertEqual(sut.webhooks, expectedWebhooks)
         } catch {
             XCTFail(error.localizedDescription)
         }
+    }
 
+    func test_listAllWebhooks_when_site_has_webhooks_then_returns_expected_webhooks() async {
+        // Given
+        let service = MockWebhooksService()
+        service.hasWebhooks = true
+
+        let sut = WebhooksViewModel(service: service)
+        let expectedWebhooks: [Webhook] = [
+            Webhook(name: "Webhook created on Sep 26, 2024 @ 02:16 AM",
+                                        status: "active",
+                                        topic: "order.updated",
+                                        deliveryURL: URL(string: "https://server.site/1234")!),
+            Webhook(name: "Webhook created on Sep 26, 2024 @ 02:30 AM",
+                                        status: "active",
+                                        topic: "order.created",
+                                        deliveryURL: URL(string: "https://server.site/1234")!)
+        ]
+
+        do {
+            // When
+            try await sut.listAllWebhooks()
+            // Then
+            XCTAssertEqual(sut.webhooks, expectedWebhooks)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+
+    func test_listAllWebhooks_when_there_is_an_error_then_returns_error() async {
+        // Given
+        let service = MockWebhooksService()
+        service.hasError = true
+
+        let sut = WebhooksViewModel(service: service)
+        let expectedError = NSError(domain: "Error", code: 0)
+
+        do {
+            // When
+            try await sut.listAllWebhooks()
+            XCTFail("Expected error, but got success.")
+        } catch {
+            // Then
+            XCTAssertEqual(error as NSError, expectedError)
+        }
+    }
+
+    func test_createWebhook_ok() async throws {
+        // Given
+        let service = MockWebhooksService()
+        let sut = WebhooksViewModel(service: service)
+        let topic = AvailableWebhook.orderCreated
+        let deliveryURLString = "https://server.site/1234"
+
+        // When
+        let expectedWebhook = try await sut.createWebhook(topic, deliveryURLString)
+
+        // Then
+        XCTAssertEqual(expectedWebhook.name, "some name")
+        XCTAssertEqual(expectedWebhook.topic, "order.created")
+        XCTAssertEqual(expectedWebhook.status, "active")
+        XCTAssertEqual(expectedWebhook.deliveryURL, URL(string: "https://server.site/1234")!)
+    }
+
+    func test_createWebhook_when_there_is_an_error_then_returns_error() async {
+        // Given
+        let service = MockWebhooksService()
+        service.hasError = true
+        let expectedError = NSError(domain: "Error", code: 0)
+        let sut = WebhooksViewModel(service: service)
+
+        do {
+            // When
+            try await sut.createWebhook(.couponCreated, "some delivery url")
+            // Then
+            XCTFail("Expected failure, but got success.")
+        } catch {
+            XCTAssertEqual(error as NSError, expectedError)
+        }
     }
 }

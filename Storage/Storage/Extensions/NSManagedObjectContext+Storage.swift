7dc1a266c8f1b5a4376af95684302ce1397c4e5b
Huong Do <hi.huongdo@gmail.com>
2024-09-27 10:22:36 +0700

    Wrap context check in debug build only

--- Storage/Storage/Extensions/NSManagedObjectContext+Storage.swift
+++ Storage/Storage/Extensions/NSManagedObjectContext+Storage.swift
@@ -60,7 +60,7 @@ extension NSManagedObjectContext: StorageType {
     /// Deletes the specified Object Instance
     ///
     public func deleteObject<T: Object>(_ object: T) {
-        Self.ensureCorrectContextUsage(for: T.entityName)
+        Self.ensureCorrectContextUsageIfNeeded(for: T.entityName)
         guard let object = object as? NSManagedObject else {
             logErrorAndExit("Invalid Object Kind")
         }
@@ -71,7 +71,7 @@ extension NSManagedObjectContext: StorageType {
     /// Deletes all of the NSMO instances associated to the specified kind
     ///
     public func deleteAllObjects<T: Object>(ofType type: T.Type) {
-        Self.ensureCorrectContextUsage(for: T.entityName)
+        Self.ensureCorrectContextUsageIfNeeded(for: T.entityName)
         let request = fetchRequest(forType: type)
         request.includesPropertyValues = false
         request.includesSubentities = false
@@ -106,7 +106,7 @@ extension NSManagedObjectContext: StorageType {
     /// Inserts a new Entity. For performance reasons, this helper *DOES NOT* persists the context.
     ///
     public func insertNewObject<T: Object>(ofType type: T.Type) -> T {
-        Self.ensureCorrectContextUsage(for: T.entityName)
+        Self.ensureCorrectContextUsageIfNeeded(for: T.entityName)
         return NSEntityDescription.insertNewObject(forEntityName: T.entityName, into: self) as! T
     }
 
@@ -181,7 +181,8 @@ extension NSManagedObjectContext: StorageType {
 
     /// Helper to trigger failure in debug mode or print warning when write operations are called from the view context (main thread).
     ///
-    private static func ensureCorrectContextUsage(for entityName: String) {
+    private static func ensureCorrectContextUsageIfNeeded(for entityName: String) {
+        #if DEBUG
         let enforceWriteInBackground = ProcessInfo.processInfo.arguments.contains("-enforce-core-data-write-in-background")
         if enforceWriteInBackground {
             assert(Thread.isMainThread == false,
@@ -189,5 +190,6 @@ extension NSManagedObjectContext: StorageType {
         } else {
             DDLogWarn("⚠️ Write operations for \(entityName) should only be done on a background context ⚠️")
         }
+        #endif
     }
 }

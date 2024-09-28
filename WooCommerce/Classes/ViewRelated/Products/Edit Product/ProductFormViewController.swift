a56dcf9001dc03d5fcd5cc9ea70a2f4b69c3dd16
Hafiz Rahman <hfzrhmn@gmail.com>
2024-09-28 19:25:04 +0700

    add "show custom fields" functionality on simple products.

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewController.swift
@@ -4,6 +4,7 @@ import Photos
 import UIKit
 import WordPressUI
 import Yosemite
+import SwiftUI
 import protocol Storage.StorageManagerType
 
 /// The entry UI for adding/editing a Product.
@@ -434,8 +435,7 @@ final class ProductFormViewController<ViewModel: ProductFormViewModelProtocol>: 
                 eventLogger.logPriceSettingsTapped()
                 editPriceSettings()
             case .customFields:
-                // TODO-13493: add tap handling.
-                return
+                showCustomFields()
             case .reviews:
                 ServiceLocator.analytics.track(.productDetailViewReviewsTapped)
                 showReviews()
@@ -1455,6 +1455,34 @@ private extension ProductFormViewController {
                                       dateOnSaleEnd: dateOnSaleEnd,
                                       taxStatus: taxStatus,
                                       taxClass: taxClass)
+    }
+}
+
+// MARK: Action - Show Custom Fields
+private extension ProductFormViewController {
+    func showCustomFields() {
+        guard let product = product as? EditableProductModel else {
+            return
+        }
+
+        let customFields = product.product.customFields.map {
+            CustomFieldViewModel(metadata: $0)
+        }
+
+        let customFieldsView = UIHostingController(
+            rootView: CustomFieldsListView(
+                isEditable: true,
+                viewModel: CustomFieldsListViewModel(customFields: customFields),
+                onBackButtonTapped: { [weak self] in
+                    // Restore the hidden navigation bar
+                    self?.navigationController?.setNavigationBarHidden(false, animated: true)
+            })
+        )
+
+        // Hide the navigation bar as `CustomFieldsListView` will create its own toolbar.
+        navigationController?.setNavigationBarHidden(true, animated: true)
+        navigationController?.pushViewController(customFieldsView, animated: true)
+
     }
 }
 

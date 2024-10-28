59d5ed4f70d1f9b5f3262fef19060359363fb316
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-10-28 18:04:21 +0100

    Change the button text to Done

--- WooCommerce/Classes/ViewRelated/Custom Fields/CustomFieldEditorView.swift
+++ WooCommerce/Classes/ViewRelated/Custom Fields/CustomFieldEditorView.swift
@@ -112,7 +112,7 @@ struct CustomFieldEditorView: View {
                         saveChanges()
                         dismiss()
                     } label: {
-                        Text(Localization.saveButton)
+                        Text(Localization.doneButton)
                     }
                     .disabled(!hasUnsavedChanges)
 
@@ -174,10 +174,10 @@ private extension CustomFieldEditorView {
             comment: "Label for the Cancel button to close the editor"
         )
 
-        static let saveButton = NSLocalizedString(
-            "customFieldEditorView.save",
-            value: "Save",
-            comment: "Label for the Save button to save changes"
+        static let doneButton = NSLocalizedString(
+            "customFieldEditorView.done",
+            value: "Done",
+            comment: "Label for the Done button to save changes"
         )
 
         static let keyLabel = NSLocalizedString(

f289e2f18dc6e889396b42d03f063211a14029f8
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2024-10-28 15:16:42 +0100

    Fix indentation

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/EditableProductVariationModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/EditableProductVariationModel.swift
@@ -138,10 +138,10 @@ extension EditableProductVariationModel: ProductFormDataModel, TaxClassRequestab
         productVariation.sku
     }
 
-	var globalUniqueID: String? {
+    var globalUniqueID: String? {
         productVariation.globalUniqueID
-	}
-
+    }
+    
     var manageStock: Bool {
         productVariation.manageStock
     }

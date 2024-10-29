92e571542ef0b607f5d379e2a7e2c047eb082384
Rachel McRoberts <rachel@automattic.com>
2024-10-29 13:29:02 +0000

    Add post-purchase text and buttons with styles

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/WooShipping Post-Purchase/WooShippingPostPurchaseView.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/WooShipping Post-Purchase/WooShippingPostPurchaseView.swift
@@ -1,20 +1,128 @@
 import SwiftUI
+import Yosemite
 
 struct WooShippingPostPurchaseView: View {
+    private let labelSizes: [ShippingLabelPaperSize] = [.label, .letter, .legal]
+
+    @State var selectedLabelSize: ShippingLabelPaperSize = .label
+
     var body: some View {
-        Text(Localization.readyToPrint)
-            .headlineStyle()
+        VStack(alignment: .leading, spacing: 8) {
+            Text(Localization.readyToPrint)
+                .headlineStyle()
+            Text(Localization.printMessage)
+                .font(.subheadline)
+                .foregroundStyle(Color(.text))
+
+            VStack(alignment: .leading, spacing: 16) {
+                Menu {
+                    ForEach(labelSizes, id: \.self) { labelSize in
+                        Button {
+                            selectedLabelSize = labelSize
+                        } label: {
+                            Text(labelSize.description)
+                            if labelSize == selectedLabelSize {
+                                Image(uiImage: .checkmarkStyledImage.withTintColor(UIColor(Layout.panelHighlight)))
+                            }
+                        }
+                    }
+                } label: {
+                    HStack {
+                        Text(selectedLabelSize.description)
+                            .bodyStyle()
+                        Spacer()
+                        Image(systemName: "chevron.up.chevron.down")
+                            .bold()
+                    }
+                    .padding()
+                    .roundedBorder(cornerRadius: 8, lineColor: Color(.separator), lineWidth: 1)
+                }
+                Button {
+                    // TODO: Request label from remote and open print dialog
+                } label: {
+                    Text(Localization.printButton)
+                        .bold()
+                }
+                .buttonStyle(HighlightButtonStyle(background: Layout.panelHighlight, backgroundPressed: Layout.buttonPressed))
+                Button {
+                    // TODO: Open instructions for how to print
+                } label: {
+                    Text(Localization.info)
+                        .font(.footnote)
+                }
+                Divider()
+                Group {
+                    Button {
+                        // TODO: Open link for shipment tracking
+                    } label: {
+                        Text(Localization.trackShipment)
+                    }
+                    Button {
+                        // TODO: Open link to schedule pickup
+                    } label: {
+                        Text(Localization.schedulePickup)
+                    }
+                    Button {
+                        // TODO: Request label refund
+                    } label: {
+                        Text(Localization.requestRefund)
+                    }
+                }
+                .font(.subheadline)
+                .bold()
+            }
+            .padding()
+            .foregroundStyle(Layout.panelHighlight)
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Layout.panelBackground)
+            )
+
+            Text(Localization.note)
+                .footnoteStyle()
+        }
+        .padding(.vertical)
     }
 }
 
 private extension WooShippingPostPurchaseView {
+    enum Layout {
+        static let panelBackground: Color = Color(light: .withColorStudio(name: .green, shade: .shade0),
+                                                  dark: .withColorStudio(name: .green, shade: .shade100))
+        static let panelHighlight: Color = Color(light: .withColorStudio(name: .green, shade: .shade70),
+                                                 dark: .withColorStudio(name: .green, shade: .shade50))
+        static let buttonPressed: Color = .withColorStudio(name: .green, shade: .shade90)
+    }
+
     enum Localization {
         static let readyToPrint = NSLocalizedString("wooShipping.createLabels.postPurchase.readyToPrint",
                                                     value: "Your shipping label is ready to print",
+                                                    comment: "Heading displayed on the shipping label screen when a purchased shipping label can be printed")
+        static let printMessage = NSLocalizedString("wooShipping.createLabels.postPurchase.printMessage",
+                                                    value: "From here you can print the shipping label again or change the paper size of the label.",
                                                     comment: "Message displayed on the shipping label screen when a purchased shipping label can be printed")
+        static let printButton = NSLocalizedString("wooShipping.createLabels.postPurchase.printButton",
+                                                   value: "Print Shipping Label",
+                                                   comment: "Title for button to print a purchased shipping label on the shipping label screen")
+        static let info = NSLocalizedString("wooShipping.createLabels.postPurchase.info",
+                                            value: "Learn how to print from your mobile device",
+                                            comment: "Link for more information about how to print a purchased shipping label on the shipping label screen")
+        static let trackShipment = NSLocalizedString("wooShipping.createLabels.postPurchase.trackShipment",
+                                                     value: "Track shipment",
+                                                     comment: "Link to track a shipment for a purchase shipping label on the shipping label screen")
+        static let schedulePickup = NSLocalizedString("wooShipping.createLabels.postPurchase.schedulePickup",
+                                                      value: "Schedule pickup",
+                                                      comment: "Link to schedule a pickup for a purchased shipping label on the shipping label screen")
+        static let requestRefund = NSLocalizedString("wooShipping.createLabels.postPurchase.requestRefund",
+                                                     value: "Request refund",
+                                                     comment: "Link to request a refund for a purchased shipping label on the shipping label screen")
+        static let note = NSLocalizedString("wooShipping.createLabels.postPurchase.note",
+                                            value: "Note: Reusing a printed label is a violation of our terms of service and may result in criminal charges.",
+                                            comment: "Note about reusing a purchased shipping label on the shipping label screen")
     }
 }
 
 #Preview {
     WooShippingPostPurchaseView()
+        .padding()
 }

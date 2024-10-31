8a01a932824126c4df70205690446a611eacdce3
Rachel McRoberts <rachel@automattic.com>
2024-10-31 13:43:12 +0000

    Update button text and navigate to new shipping label screen when feature flag is enabled

--- WooCommerce/Classes/ViewModels/Order Details/OrderDetailsDataSource.swift
+++ WooCommerce/Classes/ViewModels/Order Details/OrderDetailsDataSource.swift
@@ -737,21 +737,26 @@ private extension OrderDetailsDataSource {
     }
 
     private func configureShippingLabelDetail(cell: WooBasicTableViewCell) {
-        cell.bodyLabel?.text = Footer.showShippingLabelDetails
+        let showRevampedShippingLabelCreation = featureFlags.isFeatureFlagEnabled(.revampedShippingLabelCreation)
+        cell.bodyLabel?.text = showRevampedShippingLabelCreation ? Footer.viewShippingLabel : Footer.showShippingLabelDetails
         cell.applyPlainTextStyle()
         cell.accessoryType = .disclosureIndicator
         cell.selectionStyle = .default
 
         cell.accessibilityTraits = .button
-        cell.accessibilityLabel = NSLocalizedString(
-            "View Shipment Details",
-            comment: "Accessibility label for the 'View Shipment Details' button"
-        )
+        if showRevampedShippingLabelCreation {
+            cell.accessibilityLabel = Footer.viewShippingLabel
+        } else {
+            cell.accessibilityLabel = NSLocalizedString(
+                "View Shipment Details",
+                comment: "Accessibility label for the 'View Shipment Details' button"
+            )
 
-        cell.accessibilityHint = NSLocalizedString(
-            "Show the shipment details for this shipping label.",
-            comment: "VoiceOver accessibility hint, informing the user that the button can be used to view shipping label shipment details."
-        )
+            cell.accessibilityHint = NSLocalizedString(
+                "Show the shipment details for this shipping label.",
+                comment: "VoiceOver accessibility hint, informing the user that the button can be used to view shipping label shipment details."
+            )
+        }
     }
 
     private func configureShippingLabelPrintingInfo(cell: ImageAndTitleAndTextTableViewCell) {
@@ -1760,6 +1765,9 @@ extension OrderDetailsDataSource {
                                                    comment: "Button on bottom of Customer's information to show the billing details")
         static let showShippingLabelDetails = NSLocalizedString("View Shipment Details",
                                                                 comment: "Button on bottom of shipping label package card to show shipping details")
+        static let viewShippingLabel = NSLocalizedString("orderDetailsDataSource.shippingLabels.viewLabel",
+                                                         value: "View purchased shipping label",
+                                                         comment: "Button on bottom of shipping label card to view the shipping label")
     }
 
     enum Accessibility {

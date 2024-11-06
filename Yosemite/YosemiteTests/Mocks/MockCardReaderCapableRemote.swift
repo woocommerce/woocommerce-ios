83eeb27eac3ff69d9876463c1227e3ad54995dc3
Josh Heald <joshheald@users.noreply.github.com>
2024-11-06 16:33:16 +0000

    14333 Show IPP address errors with site credentials
    Previously, we decoded specific `DotcomError` instances to show the specific errors needed when a user hasn’t set their site address or postcode in WP-Admin, which prevents IPP reader connections from working.
    
    These errors weren’t decoded when using site credentials to log in, as in that case a `NetworkError` is received, and our error handling didn’t decode that correctly.
    
    This adds correct decoding and tests for the `NetworkError` variants of these issues.

---
+++ Yosemite/YosemiteTests/Mocks/MockCardReaderCapableRemote.swift
@@ -0,0 +1,16 @@
+import Foundation
+import Yosemite
+import Networking
+import Fakes
+
+struct MockCardReaderCapableRemote: CardReaderCapableRemote {
+    func loadConnectionToken(for siteID: Int64, completion: @escaping (Result<Networking.ReaderConnectionToken, any Error>) -> Void) {
+        // no-op
+    }
+
+    var resultForDefaultReaderLocation: (Result<RemoteReaderLocation, any Error>) = .success(RemoteReaderLocation.fake())
+    func loadDefaultReaderLocation(for siteID: Int64, onCompletion: @escaping (Result<RemoteReaderLocation, any Error>) -> Void) {
+        onCompletion(resultForDefaultReaderLocation)
+    }
+    
+}

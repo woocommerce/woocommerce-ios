7014c51d52918688adf43cd3f66a2701225c4d8d
Spencer Transier <spencer@transier.us>
2024-11-11 14:39:19 -0800

    Remove usages of `BUILDKITE_` prefixed env vars

--- .buildkite/commands/checkout-release-branch.sh
+++ .buildkite/commands/checkout-release-branch.sh
@@ -2,16 +2,8 @@
 
 echo "--- :git: Checkout Release Branch"
 
-# Note: `BUILDKITE_RELEASE_VERSION` is the legacy environment variable passed to Buildkite by ReleaseV2.
-# It used the `BUILDKITE_` prefix so it was not filtered out when passed to the MacOS VMs, due to how `hostmgr` works.
-# This is considered legacy: we should eventually remove all use of custom `BUILDKITE_` variables, and instead
-# resolve the value of those sooner (i.e. in the YML pipeline) then pass it as parameter to the `.sh` calls instead.
-
-# Use the provided argument if there's one, otherwise fall back to the legacy BUILDKITE_RELEASE_VERSION
-RELEASE_VERSION=${1:-$BUILDKITE_RELEASE_VERSION}
-
 if [[ -z "${RELEASE_VERSION}" ]]; then
-    echo "RELEASE_VERSION is not set and BUILDKITE_RELEASE_VERSION is not available."
+    echo "RELEASE_VERSION is not set."
     exit 1
 fi
 

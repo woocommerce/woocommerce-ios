f3f153bdcb6097b67c79c163c958b6a167c5ba58
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2024-11-12 14:22:05 +0530

    Enable the configuration to allow login into WPCOM suspended sites.

--- WooCommerce/Classes/Extensions/WordPressAuthenticator+Woo.swift
+++ WooCommerce/Classes/Extensions/WordPressAuthenticator+Woo.swift
@@ -42,7 +42,7 @@ extension WordPressAuthenticator {
                                                                 useEnterEmailAddressAsStepValueForGetStartedVC: true,
                                                                 enableSiteAddressLoginOnlyInPrologue: true,
                                                                 enableSiteCreationGuide: true,
-                                                                enableSiteCredentialsLoginForWPCOMSuspendedSites: false)
+                                                                enableSiteCredentialsLoginForWPCOMSuspendedSites: true)
 
         let systemGray3LightModeColor = UIColor(red: 199/255.0, green: 199/255.0, blue: 204/255.0, alpha: 1)
         let systemLabelLightModeColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)

b6254787161265af825a210f308bd53ef6966fa5
Božidar Ševo <bozidarsevo@gmail.com>
2024-11-19 10:12:39 +0100

    Lint

--- Networking/Networking/Remote/WooShippingRemote.swift
+++ Networking/Networking/Remote/WooShippingRemote.swift
@@ -98,7 +98,7 @@ public final class WooShippingRemote: Remote, WooShippingRemoteProtocol {
             completion(.failure(error))
         }
     }
-    
+
     /// Loads packages.
     /// - Parameters:
     ///   - siteID: Remote ID of the site.

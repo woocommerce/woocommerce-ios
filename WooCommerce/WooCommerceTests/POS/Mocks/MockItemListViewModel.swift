fbce4d7b0f2ec6869236da0184401902e8c8a838
Josh Heald <joshheald@users.noreply.github.com>
2024-11-19 18:02:53 +0000

    Removed unused mock properties

--- WooCommerce/WooCommerceTests/POS/Mocks/MockItemListViewModel.swift
+++ WooCommerce/WooCommerceTests/POS/Mocks/MockItemListViewModel.swift
@@ -4,11 +4,6 @@ import protocol Yosemite.POSItem
 @testable import WooCommerce
 
 class MockItemListViewModel: ItemListViewModelProtocol {
-    @Published var items: [any Yosemite.POSItem] = []
-
-    @Published var state: ItemListState = .initialLoading
-    var statePublisher: Published<ItemListState>.Publisher { $state }
-
     @Published var isHeaderBannerDismissed: Bool = false
 
     func dismissBanner() {

a9c08185880a28501d8a395cbe04ba5d099b54d6
Božidar Ševo <bozidarsevo@gmail.com>
2024-11-25 14:23:44 +0100

    Update Model.swift

--- Yosemite/Yosemite/Model/Model.swift
+++ Yosemite/Yosemite/Model/Model.swift
@@ -179,6 +179,7 @@ public typealias TopEarnerStatsItem = Networking.TopEarnerStatsItem
 public typealias User = Networking.User
 public typealias WooAPIVersion = Networking.WooAPIVersion
 public typealias WooShippingCustomPackage = Networking.WooShippingCustomPackage
+public typealias WooShippingSavedPredefinedPackage = Networking.WooShippingSavedPredefinedPackage
 public typealias WooShippingPredefinedPackage = Networking.WooShippingPredefinedPackage
 public typealias WooShippingCreatePackageResponse = Networking.WooShippingCreatePackageResponse
 public typealias WooShippingPackagesResponse = Networking.WooShippingPackagesResponse

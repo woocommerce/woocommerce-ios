4d2c5cf3c219e63abd37b3985426da851c1e798b
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2024-11-27 19:57:38 +0200

    Remove number formatting logic from TapToPayEducationStepViewModel

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/CardReadersV2/Tap to Pay Education/TapToPayEducationStepViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/CardReadersV2/Tap to Pay Education/TapToPayEducationStepViewModel.swift
@@ -5,13 +5,6 @@ final class TapToPayEducationStepViewModel {
     let imageName: String
     let descriptionSteps: [String]
 
-    private lazy var numberFormatter: NumberFormatter = {
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.locale = Locale.current
-        return numberFormatter
-    }()
-
     init(title: String, imageName: String, descriptionSteps: [String]) {
         self.title = title
         self.imageName = imageName
@@ -22,9 +15,5 @@ final class TapToPayEducationStepViewModel {
         self.title = title
         self.imageName = imageName
         self.descriptionSteps = [description]
-    }
-
-    func format(index: Int) -> String {
-        return numberFormatter.string(from: index as NSNumber) ?? "\(index)"
     }
 }

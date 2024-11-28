d4fd4474e20444b6420464e0f50379f26f284372
Jaclyn Chen <watertranquil@gmail.com>
2024-11-28 09:51:31 +0800

    Remove select payment provider option row background color while still ensuring the whole row is tappable.

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsSelectPluginView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/In-Person Payments/InPersonPaymentsSelectPluginView.swift
@@ -25,8 +25,8 @@ struct InPersonPaymentsSelectPluginRow: View {
         .background(
             RoundedRectangle(cornerRadius: 8)
                 .stroke(borderColor, lineWidth: 1)
-                .background(Color(.tertiarySystemBackground))
                )
+        .contentShape(Rectangle()) // Ensures the whole HStack is tappable.
         .accessibilityElement(children: .combine)
                 .accessibilityLabel(name)
                 .accessibilityRemoveTraits([.isImage])

3156e83fd74883528e25f01ff1b5f5e6c119e6ed
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2024-12-02 16:59:27 +0100

    Update TapToPayEducationView.swift

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/CardReadersV2/Tap to Pay Education/TapToPayEducationView.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/CardReadersV2/Tap to Pay Education/TapToPayEducationView.swift
@@ -83,7 +83,7 @@ final class TapToPayEducationViewViewHostingController: UIHostingController<TapT
             }
         }
     }
-    
+
     @MainActor @preconcurrency required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }

ae1e46eec3b9aa25f833cbc34e3494fb087560e2
Huong Do <hi.huongdo@gmail.com>
2024-12-02 15:21:44 +0700

    Update Storage/Storage/Tools/StorageType+Extensions.swift
    Co-authored-by: Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>

--- Storage/Storage/Tools/StorageType+Extensions.swift
+++ Storage/Storage/Tools/StorageType+Extensions.swift
@@ -561,7 +561,7 @@ public extension StorageType {
         return allObjects(ofType: BlazeCampaignListItem.self, matching: predicate, sortedBy: nil)
     }
 
-    /// Returns  stored BlazeCampaignListItem s for a site matching the given IDs
+    /// Returns stored BlazeCampaignListItem s for a site matching the given IDs
     ///
     func loadBlazeCampaignListItems(siteID: Int64, with campaignIDs: [String]) -> [BlazeCampaignListItem] {
         let predicate = NSPredicate(format: "siteID == %lld && campaignID in %@", siteID, campaignIDs)

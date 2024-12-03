198a16e39118dbc6a3503818eb123d96bab741c1
Rachel McRoberts <rachel@automattic.com>
2024-12-03 14:15:36 +0000

    Remove iOS 16 check from ProductDescriptionGenerationView

--- WooCommerce/Classes/ViewRelated/Products/AI/ProductDescriptionGenerationView.swift
+++ WooCommerce/Classes/ViewRelated/Products/AI/ProductDescriptionGenerationView.swift
@@ -173,13 +173,8 @@ struct ProductDescriptionGenerationView: View {
 
     @ViewBuilder
     private var nameTextField: some View {
-        if #available(iOS 16.0, *) {
-            TextField(Localization.productNamePlaceholder, text: $viewModel.name, axis: .vertical)
-                .bodyStyle()
-        } else {
-            TextField(Localization.productNamePlaceholder, text: $viewModel.name)
-                .bodyStyle()
-        }
+        TextField(Localization.productNamePlaceholder, text: $viewModel.name, axis: .vertical)
+            .bodyStyle()
     }
 }
 

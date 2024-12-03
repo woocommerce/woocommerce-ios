6dd71e0ec4a0dc0d78c04ebb9ea7b091c44dd936
Rachel McRoberts <rachel@automattic.com>
2024-12-03 14:01:25 +0000

    Remove iOS 16 check from CreateOrderAppIntent

--- WooCommerce/Classes/App Intents/CreateOrderAppIntent.swift
+++ WooCommerce/Classes/App Intents/CreateOrderAppIntent.swift
@@ -2,7 +2,6 @@ import AppIntents
 import Foundation
 
 
-@available(iOS 16, *)
 struct CreateOrderAppIntent: AppIntent {
     // looks up in our Localizable.string to localize
     static var title: LocalizedStringResource = "Create order"
@@ -17,7 +16,6 @@ struct CreateOrderAppIntent: AppIntent {
     }
 }
 
-@available(iOS 16, *)
 extension CreateOrderAppIntent {
     enum Localization {
         // Here to be added to Localizable.strings so it can be looked up by the `LocalizedStringResource` above

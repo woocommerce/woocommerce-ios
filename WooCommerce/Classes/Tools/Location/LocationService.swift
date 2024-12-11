a67604a2c6094d6dd01ba5c5f66768a941b84eee
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2024-12-11 16:47:29 +0100

    Create LocationService to centralize location permission request process

---
+++ WooCommerce/Classes/Tools/Location/LocationService.swift
@@ -0,0 +1,66 @@
+import Foundation
+import CoreLocation
+
+protocol LocationServiceProtocol {
+    func requestPermission(_ completion: @escaping (LocationAuthorizationStatus) -> Void)
+    var authorizationStatus: LocationAuthorizationStatus { get }
+}
+
+enum LocationAuthorizationStatus {
+    case notDetermined
+    case denied
+    case authorized
+}
+
+final class LocationService: NSObject, LocationServiceProtocol {
+    private let locationManager = CLLocationManager()
+    private var permissionCompletion: ((LocationAuthorizationStatus) -> Void)?
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+    }
+
+    func requestPermission(_ completion: @escaping (LocationAuthorizationStatus) -> Void) {
+        permissionCompletion = completion
+
+        let status = locationManager.authorizationStatus
+
+        guard status == .notDetermined else {
+            return completion(authorizationStatus(from: status))
+        }
+
+        locationManager.requestWhenInUseAuthorization()
+    }
+
+    var authorizationStatus: LocationAuthorizationStatus {
+        let status = locationManager.authorizationStatus
+        return authorizationStatus(from: status)
+    }
+}
+
+extension LocationService: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if let completion = permissionCompletion {
+            completion(authorizationStatus(from: status))
+            permissionCompletion = nil
+        }
+    }
+}
+
+// MARK: - Mapping Status
+
+private extension LocationService {
+    func authorizationStatus(from status: CLAuthorizationStatus) -> LocationAuthorizationStatus {
+        switch status {
+        case .authorizedWhenInUse, .authorizedAlways:
+            return .authorized
+        case .restricted, .denied:
+            return .denied
+        case .notDetermined:
+            return .notDetermined
+        @unknown default:
+            return .denied
+        }
+    }
+}

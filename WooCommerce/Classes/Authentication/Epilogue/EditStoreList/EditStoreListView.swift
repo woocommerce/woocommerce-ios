06233645127dc2c5f3bbe5c78ffb75d6c3aad77a
Huong Do <hi.huongdo@gmail.com>
2024-12-17 12:47:29 +0700

    Update UI for EditStoreListView

--- WooCommerce/Classes/Authentication/Epilogue/EditStoreList/EditStoreListView.swift
+++ WooCommerce/Classes/Authentication/Epilogue/EditStoreList/EditStoreListView.swift
@@ -3,8 +3,50 @@ import SwiftUI
 /// View to edit the items to be displayed on the store picker
 ///
 struct EditStoreListView: View {
+    @ObservedObject var viewModel: EditStoreListViewModel
+
+    private let onDismiss: () -> Void
+
+    init(viewModel: EditStoreListViewModel, onDismiss: @escaping () -> Void) {
+        self.viewModel = viewModel
+        self.onDismiss = onDismiss
+    }
+
     var body: some View {
-        EmptyView()
+        NavigationStack {
+            List {
+                Section {
+                    ForEach(viewModel.availableSites, id: \.siteID) { item in
+                        SelectableItemRow(title: item.name,
+                                          subtitle: item.url,
+                                          selected: viewModel.isSelected(item),
+                                          displayMode: .full,
+                                          selectionStyle: .checkcircle)
+                        .listRowInsets(.zero)
+                        .onTapGesture {
+                            viewModel.toggleSelection(item)
+                        }
+                        .disabled(viewModel.isLastSelected(item))
+                    }
+                } footer: {
+                    Text("Stores that are not selected will be excluded from the store picker")
+                }
+                
+            }
+            .listStyle(.plain)
+            .navigationTitle("Visible Stores")
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Cancel") {
+                        onDismiss()
+                    }
+
+                    Button("Save") {
+                        viewModel.didSaveChanges()
+                    }
+                    .disabled(viewModel.hasChanges == false)
+                }
+            }
+        }
     }
 }
-
--- WooCommerce/Classes/Authentication/Epilogue/EditStoreList/EditStoreListViewModel.swift
+++ WooCommerce/Classes/Authentication/Epilogue/EditStoreList/EditStoreListViewModel.swift
@@ -9,28 +9,52 @@ final class EditStoreListViewModel: ObservableObject {
     let availableSites: [Site]
 
     /// Sites selected to be displayed on the store picker
-    @Published var selectedSites: [Site]
+    @Published var selectedSites: Set<Site>
 
     var hasChanges: Bool {
-        Set(selectedSites) != Set(originalSelectedSites)
+        selectedSites != Set(originalSelectedSites)
     }
 
     private let originalSelectedSites: [Site]
     private let analytics: Analytics
-    private let onCompletion: (_ selectedSites: [Site]) -> Void
+    private let onCompletion: (_ selectedSites: Set<Site>) -> Void
 
     init(availableSites: [Site],
          selectedSites: [Site],
          analytics: Analytics = ServiceLocator.analytics,
-         onCompletion: @escaping (_ selectedSites: [Site]) -> Void) {
+         onCompletion: @escaping (_ selectedSites: Set<Site>) -> Void) {
         self.availableSites = availableSites
         self.originalSelectedSites = selectedSites
-        self.selectedSites = selectedSites
+        self.selectedSites = Set(selectedSites)
         self.analytics = analytics
         self.onCompletion = onCompletion
     }
 
     func didSaveChanges() {
         onCompletion(selectedSites)
+    }
+}
+
+// MARK: - Helper methods for selection
+extension EditStoreListViewModel {
+
+    /// Checks if the given site is selected.
+    func isSelected(_ site: Site) -> Bool {
+        selectedSites.contains(site)
+    }
+
+    /// Checks if the given site is the last selected item.
+    /// This is used to disable that row, so it can't be deselected.
+    func isLastSelected(_ site: Site) -> Bool {
+        isSelected(site) && selectedSites.count == 1
+    }
+
+    /// Selects or deselects the given site.
+    func toggleSelection(_ site: Site) {
+        if selectedSites.contains(site) {
+            selectedSites.remove(site)
+        } else {
+            selectedSites.insert(site)
+        }
     }
 }

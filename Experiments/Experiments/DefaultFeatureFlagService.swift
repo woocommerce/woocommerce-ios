56f60e8cfb227ab914b75404ca8320d8841900a1
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-12-17 11:53:18 +0700

    enable sendReceiptsForPointOfSale flag

--- Experiments/Experiments/DefaultFeatureFlagService.swift
+++ Experiments/Experiments/DefaultFeatureFlagService.swift
@@ -94,7 +94,7 @@ public struct DefaultFeatureFlagService: FeatureFlagService {
         case .sendReceiptAfterPayment:
             return false
         case .sendReceiptsForPointOfSale:
-            return false
+            return true
         case .tapToPayEducation:
             return false
         case .variableProductsInPointOfSale:

099e6fc98e1e9201bc7b57110ab9ef54390af12a
Jaclyn Chen <watertranquil@gmail.com>
2024-12-18 09:29:53 +0800

    Fix SwiftLint errors on trailing whitespace.

--- WooCommerce/WooCommerceTests/POS/Mocks/MockPOSItemProvider.swift
+++ WooCommerce/WooCommerceTests/POS/Mocks/MockPOSItemProvider.swift
@@ -44,13 +44,13 @@ extension MockPointOfSaleItemService {
     static func makeInitialItems() -> [POSItem] {
         let fakeUUID1 = UUID(uuidString: "DC55E3B9-9D83-4C07-82A7-4C300A50E84E") ?? UUID()
         let fakeUUID2 = UUID(uuidString: "DC55E3B8-9D82-4C06-82A5-4C300A50E84A") ?? UUID()
-        
+
         let product1 = POSSimpleProduct(id: fakeUUID1,
                                         name: "Choco",
                                         formattedPrice: "$2.00",
                                         productID: 1,
                                         price: "2.00")
-        
+
         let product2 = POSSimpleProduct(id: fakeUUID2,
                                         name: "Vanilla",
                                         formattedPrice: "$3.00",
@@ -58,17 +58,17 @@ extension MockPointOfSaleItemService {
                                         price: "2.00")
         return [.simpleProduct(product1), .simpleProduct(product2)]
     }
-    
+
     static func makeSecondPageItems() -> [POSItem] {
         let fakeUUID3 = UUID(uuidString: "DC55E3B9-9D83-4C07-82A7-4C300A50E86D") ?? UUID()
         let fakeUUID4 = UUID(uuidString: "DC55E3B8-9D82-4C06-82A5-4C300A50E86F") ?? UUID()
-        
+
         let product3 = POSSimpleProduct(id: fakeUUID3,
                                         name: "Strawberry",
                                         formattedPrice: "$2.00",
                                         productID: 1,
                                         price: "2.00")
-        
+
         let product4 = POSSimpleProduct(id: fakeUUID4,
                                         name: "Pistachio",
                                         formattedPrice: "$3.00",

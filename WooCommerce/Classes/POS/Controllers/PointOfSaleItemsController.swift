f75a9bf5509275507b1bbf48d92b30fe529eb225
Jaclyn Chen <watertranquil@gmail.com>
2024-12-19 11:10:49 +0800

    Create `AsyncPaginationTracker` based on `PaginationTracker` to enable async/await interface in POS. Some PointOfSaleItemsControllerTests cases are still failing.

--- WooCommerce/Classes/POS/Controllers/PointOfSaleItemsController.swift
+++ WooCommerce/Classes/POS/Controllers/PointOfSaleItemsController.swift
@@ -15,36 +15,66 @@ class PointOfSaleItemsController: PointOfSaleItemsControllerProtocol {
     private(set) var itemListStatePublisher: any Publisher<ItemListState, Never>
     private var itemListStateSubject: PassthroughSubject<ItemListState, Never> = .init()
     private var allItems: [POSItem] = []
-    private var isInitialLoading: Bool = true
-    private let paginationTracker: PaginationTracker
+    private let paginationTracker: AsyncPaginationTracker
     private let itemProvider: PointOfSaleItemServiceProtocol
 
     init(itemProvider: PointOfSaleItemServiceProtocol) {
         self.itemProvider = itemProvider
         self.paginationTracker = .init(pageFirstIndex: Constants.initialPage)
         itemListStatePublisher = itemListStateSubject.eraseToAnyPublisher()
-
-        paginationTracker.delegate = self
     }
 
     @MainActor
     func loadInitialItems() async {
-        paginationTracker.syncFirstPage()
+        itemListStateSubject.send(.initialLoading)
+        do {
+            try await paginationTracker.syncFirstPage { [weak self] pageNumber in
+                guard let self else { return true }
+                return try await fetchItems(pageNumber: pageNumber)
+            }
+            updateItemListStateAfterLoadAttempt()
+        } catch {
+            itemListStateSubject.send(.error(PointOfSaleErrorState.errorOnLoadingProducts()))
+        }
     }
 
     @MainActor
     func loadNextItems() async {
-        paginationTracker.ensureNextPageIsSynced()
+        itemListStateSubject.send(.loading(allItems))
+        do {
+            let nextPageState = try await paginationTracker.ensureNextPageIsSynced { [weak self] pageNumber in
+                guard let self else { return true }
+                return try await fetchItems(pageNumber: pageNumber)
+            }
+            switch nextPageState {
+                case .noNextPage, .synced:
+                    updateItemListStateAfterLoadAttempt()
+                case .syncing:
+                    break
+            }
+        } catch {
+            // TODO: 14694 - Handle error from loading the next page, like showing an error UI at the end or as an overlay.
+            updateItemListStateAfterLoadAttempt()
+        }
     }
 
     @MainActor
     func reload() async {
         allItems.removeAll()
-        paginationTracker.resync()
+        itemListStateSubject.send(.loading(allItems))
+        do {
+            try await paginationTracker.resync { [weak self] pageNumber in
+                guard let self else { return true }
+                return try await fetchItems(pageNumber: pageNumber)
+            }
+            updateItemListStateAfterLoadAttempt()
+        } catch {
+            // TODO: 14694 - Handle error from pull-to-refresh, like showing an error UI at the beginning or as an overlay.
+        }
     }
 
-    /// <#Description#>
-    /// - Parameter pageNumber: <#pageNumber description#>
+    /// Fetches items given a page number and appends new unique items to the `allItems` array.
+    /// - Parameter pageNumber: Page number to fetch items from.
     /// - Returns: A boolean that indicates whether there is next page for the paginated items.
     @MainActor
     private func fetchItems(pageNumber: Int) async throws -> Bool {
@@ -66,29 +96,5 @@ class PointOfSaleItemsController: PointOfSaleItemsControllerProtocol {
 
     private enum Constants {
         static let initialPage: Int = 1
-    }
-}
-
-extension PointOfSaleItemsController: PaginationTrackerDelegate {
-    func sync(pageNumber: Int, pageSize: Int, reason: String?, onCompletion: SyncCompletion?) {
-        if isInitialLoading {
-            isInitialLoading = false
-            itemListStateSubject.send(.initialLoading)
-        } else {
-            itemListStateSubject.send(.loading(allItems))
-        }
-        Task { @MainActor in
-            do {
-                let hasNextPage = try await fetchItems(pageNumber: pageNumber)
-                updateItemListStateAfterLoadAttempt()
-                onCompletion?(.success(hasNextPage))
-            } catch PointOfSaleProductServiceError.pageOutOfRange {
-                updateItemListStateAfterLoadAttempt()
-                onCompletion?(.failure(PointOfSaleProductServiceError.pageOutOfRange))
-            } catch {
-                itemListStateSubject.send(.error(PointOfSaleErrorState.errorOnLoadingProducts()))
-                onCompletion?(.failure(error))
-            }
-        }
     }
 }

04f069149772f63ee2936d5f429f8d243ca199be
Huong Do <hi.huongdo@gmail.com>
2024-12-23 11:18:54 +0700

    Update unit tests for saving changes on the edit store list

--- WooCommerce/WooCommerceTests/Authentication/EditStoreListViewModelTests.swift
+++ WooCommerce/WooCommerceTests/Authentication/EditStoreListViewModelTests.swift
@@ -1,6 +1,6 @@
 import Foundation
 import Testing
-import struct Yosemite.Site
+import Yosemite
 @testable import WooCommerce
 
 struct EditStoreListViewModelTests {
@@ -11,8 +11,8 @@ struct EditStoreListViewModelTests {
     @Test func hasChanges_returns_correct_values() {
         // Given
         let availableSites = [site1, site2]
-        let viewModel = EditStoreListViewModel(availableSites: [site1, site2],
-                                               displayedSites: [site1, site2],
+        let viewModel = EditStoreListViewModel(availableSites: availableSites,
+                                               displayedSites: availableSites,
                                                currentlySelectedSite: nil,
                                                onCompletion: {})
 
@@ -83,13 +83,17 @@ struct EditStoreListViewModelTests {
     }
 
     @MainActor
-    @Test func didSaveChanges_saves_hidden_store_ids_to_user_defaults_and_triggers_completion() async {
+    @Test func saveChanges_saves_hidden_store_ids_to_user_defaults_and_triggers_completion_if_there_is_no_deviceID() async {
         // Given
         let userDefaults = UserDefaults(suiteName: UUID().uuidString)!
         var completionTriggered = false
+
+        let notificationManager = MockPushNotificationsManager(mockedDeviceID: nil)
+
         let viewModel = EditStoreListViewModel(availableSites: [site1, site2],
                                                displayedSites: [site1, site2],
                                                currentlySelectedSite: nil,
+                                               pushNotificationManager: notificationManager,
                                                userDefaults: userDefaults,
                                                onCompletion: { completionTriggered = true })
 
@@ -100,5 +104,79 @@ struct EditStoreListViewModelTests {
         // Then
         #expect(userDefaults.hiddenStoreIDs == [site1.siteID])
         #expect(completionTriggered == true)
+    }
+
+    @MainActor
+    @Test func saveChanges_succeeds_when_updating_notification_settings_succeeds() async {
+        // Given
+        let userDefaults = UserDefaults(suiteName: UUID().uuidString)!
+        var completionTriggered = false
+
+        let deviceID = "13435352"
+        let notificationManager = MockPushNotificationsManager(mockedDeviceID: deviceID)
+
+        let stores = MockStoresManager(sessionManager: .makeForTesting())
+        stores.whenReceivingAction(ofType: AccountAction.self) { action in
+            switch action {
+            case .updateNotificationSettings(_, let onCompletion):
+                onCompletion(.success(()))
+            default:
+                break
+            }
+        }
+
+        let viewModel = EditStoreListViewModel(availableSites: [site1, site2],
+                                               displayedSites: [site1, site2],
+                                               currentlySelectedSite: nil,
+                                               stores: stores,
+                                               pushNotificationManager: notificationManager,
+                                               userDefaults: userDefaults,
+                                               onCompletion: { completionTriggered = true })
+
+        // When
+        viewModel.toggleSelection(site1)
+        await viewModel.saveChanges()
+
+        // Then
+        #expect(userDefaults.hiddenStoreIDs == [site1.siteID])
+        #expect(completionTriggered == true)
+    }
+
+    @MainActor
+    @Test func saveChanges_fails_when_updating_notification_settings_fails() async {
+        // Given
+        let userDefaults = UserDefaults(suiteName: UUID().uuidString)!
+        var completionTriggered = false
+
+        let deviceID = "13435352"
+        let notificationManager = MockPushNotificationsManager(mockedDeviceID: deviceID)
+
+        let stores = MockStoresManager(sessionManager: .makeForTesting())
+        stores.whenReceivingAction(ofType: AccountAction.self) { action in
+            switch action {
+            case .updateNotificationSettings(_, let onCompletion):
+                let error = NSError(domain: "notification-settings-update-error", code: 501)
+                onCompletion(.failure(error))
+            default:
+                break
+            }
+        }
+
+        let viewModel = EditStoreListViewModel(availableSites: [site1, site2],
+                                               displayedSites: [site1, site2],
+                                               currentlySelectedSite: nil,
+                                               stores: stores,
+                                               pushNotificationManager: notificationManager,
+                                               userDefaults: userDefaults,
+                                               onCompletion: { completionTriggered = true })
+
+        // When
+        viewModel.toggleSelection(site1)
+        await viewModel.saveChanges()
+
+        // Then
+        #expect(userDefaults.hiddenStoreIDs == [])
+        #expect(completionTriggered == false)
+        #expect(viewModel.shouldShowErrorAlert == true)
     }
 }

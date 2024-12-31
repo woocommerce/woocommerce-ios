866abce2da5268f71d80cd76947abfe983add24e
Gio Lodi <gio@mokacoding.com>
2024-12-31 10:36:31 +0100

    Move all WordPressAuthenticator sources into this repo

---
+++ WordPressAuthenticator/WordPressAuthenticator/Model/WordPressComSiteInfo.swift
@@ -0,0 +1,74 @@
+import Foundation
+
+// MARK: - WordPress.com Site Info
+//
+public class WordPressComSiteInfo {
+
+    /// Site's Name!
+    ///
+    public let name: String
+
+    /// Tagline.
+    ///
+    public let tagline: String
+
+    /// Public URL.
+    ///
+    public let url: String
+
+    /// Indicates if Jetpack is available, or not.
+    ///
+    public let hasJetpack: Bool
+
+    /// Indicates if Jetpack is active, or not.
+    ///
+    public let isJetpackActive: Bool
+
+    /// Indicates if Jetpack is connected, or not.
+    ///
+    public let isJetpackConnected: Bool
+
+    /// URL of the Site's Blavatar.
+    ///
+    public let icon: String
+
+    /// Indicates whether the site is WordPressDotCom, or not.
+    ///
+    public let isWPCom: Bool
+
+    /// Inidcates wheter the site is WordPress, or not.
+    ///
+    public let isWP: Bool
+
+    /// Inidcates whether the site exists, or not.
+    ///
+    public let exists: Bool
+
+    public init(name: String, tagline: String, url: String, hasJetpack: Bool, isJetpackActive: Bool, isJetpackConnected: Bool, icon: String, isWPCom: Bool, isWP: Bool, exists: Bool) {
+        self.name = name
+        self.tagline = tagline
+        self.url = url
+        self.hasJetpack = hasJetpack
+        self.isJetpackActive = isJetpackActive
+        self.isJetpackConnected = isJetpackConnected
+        self.icon = icon
+        self.isWPCom = isWPCom
+        self.isWP = isWP
+        self.exists = exists
+    }
+
+    /// Initializes the current SiteInfo instance with a raw dictionary.
+    ///
+    public init(remote: [AnyHashable: Any]) {
+        name                = remote["name"] as? String                 ?? ""
+        tagline             = remote["description"] as? String          ?? ""
+        url                 = remote["urlAfterRedirects"] as? String    ?? ""
+        hasJetpack          = remote["hasJetpack"] as? Bool             ?? false
+        isJetpackActive     = remote["isJetpackActive"] as? Bool        ?? false
+        isJetpackConnected  = remote["isJetpackConnected"] as? Bool     ?? false
+        icon                = remote["icon.img"] as? String             ?? ""
+        isWPCom             = remote["isWordPressDotCom"] as? Bool      ?? false
+        isWP                = remote["isWordPress"] as? Bool            ?? false
+        exists              = remote["exists"] as? Bool                 ?? false
+    }
+}
---
+++ WordPressAuthenticator/WordPressAuthenticator/Services/WordPressComBlogService.swift
@@ -0,0 +1,81 @@
+import Foundation
+import WordPressKit
+
+// MARK: - WordPress.com BlogService
+//
+class WordPressComBlogService {
+
+    /// Returns a new anonymous instance of WordPressComRestApi.
+    ///
+    private var anonymousAPI: WordPressComRestApi {
+        let userAgent = WordPressAuthenticator.shared.configuration.userAgent
+        let baseUrl = WordPressAuthenticator.shared.configuration.wpcomAPIBaseURL
+        return WordPressComRestApi(oAuthToken: nil, userAgent: userAgent, baseURL: baseUrl)
+    }
+
+    /// Retrieves the WordPressComSiteInfo instance associated to a WordPress.com Site Address.
+    ///
+    func fetchSiteInfo(for address: String, success: @escaping (WordPressComSiteInfo) -> Void, failure: @escaping (Error) -> Void) {
+        let remote = BlogServiceRemoteREST(wordPressComRestApi: anonymousAPI, siteID: 0)
+
+        remote.fetchSiteInfo(forAddress: address, success: { response in
+            guard let response = response else {
+                failure(WordPressComBlogServiceError.unknown)
+                return
+            }
+
+            let site = WordPressComSiteInfo(remote: response)
+            success(site)
+
+        }, failure: { error in
+            let result = error ?? WordPressComBlogServiceError.unknown
+            failure(result)
+        })
+    }
+
+     func fetchUnauthenticatedSiteInfoForAddress(for address: String, success: @escaping (WordPressComSiteInfo) -> Void, failure: @escaping (Error) -> Void) {
+        let remote = BlogServiceRemoteREST(wordPressComRestApi: anonymousAPI, siteID: 0)
+        remote.fetchUnauthenticatedSiteInfo(forAddress: address, success: { response in
+            guard let response = response else {
+                failure(WordPressComBlogServiceError.unknown)
+                return
+            }
+
+            let site = WordPressComSiteInfo(remote: response)
+            guard site.url != Constants.wordPressBlogURL else {
+                failure(WordPressComBlogServiceError.invalidWordPressAddress)
+                return
+            }
+            success(site)
+        }, failure: { error in
+            let result: Error = {
+                /// Check whether the site is suspended on WordPress.com and can't be connected using Jetpack
+                ///
+                if let apiError = error as? WordPressAPIError<WordPressComRestApiEndpointError>,
+                   case let .endpointError(endpointError) = apiError,
+                   endpointError.apiErrorCode == "connection_disabled" {
+                    return WordPressComBlogServiceError.wpcomSiteSuspended
+                }
+
+                return error ?? WordPressComBlogServiceError.unknown
+            }()
+            failure(result)
+        })
+    }
+}
+
+// MARK: - Nested Types
+//
+extension WordPressComBlogService {
+    enum Constants {
+        static let wordPressBlogURL = "https://wordpress.com/blog"
+    }
+}
+
+public enum WordPressComBlogServiceError: Error {
+    case unknown
+    case invalidWordPressAddress
+    /// Whether the site is suspended on WordPress.com and can't be connected using Jetpack
+    ///
+    case wpcomSiteSuspended
+}

94ca0fbb7e0491de7e391656e38dba92829ccd8f
Gio Lodi <gio@mokacoding.com>
2025-01-02 08:44:10 +0100

    Create new, empty, WordPressKit

---
+++ WordPressKit/WordPressKit/WordPressKit.h
@@ -0,0 +1,18 @@
+//
+//  WordPressKit.h
+//  WordPressKit
+//
+//  Created by Gio Lodi on 2/1/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WordPressKit.
+FOUNDATION_EXPORT double WordPressKitVersionNumber;
+
+//! Project version string for WordPressKit.
+FOUNDATION_EXPORT const unsigned char WordPressKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WordPressKit/PublicHeader.h>
+
+

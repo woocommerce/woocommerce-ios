9b2e17d14745415181446b82d42c90b9dbf9f956
Gio Lodi <gio@mokacoding.com>
2025-01-08 11:06:11 +0100

    Remove Comment services and models

--- WooCommerce/WordPressAuthenticator/WordPressKit/Services/CommentServiceRemoteXMLRPC.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <WordPressAuthenticator/CommentServiceRemote.h>
-#import <WordPressAuthenticator/ServiceRemoteWordPressXMLRPC.h>
-
-@interface CommentServiceRemoteXMLRPC : ServiceRemoteWordPressXMLRPC<CommentServiceRemote>
-
-@end

4cfa64e2cd4952b4688eaee58f21fab0f841f232
Josh Heald <joshheald@users.noreply.github.com>
2025-01-13 11:26:43 +0000

    14694 Update variation page size to match products

--- Networking/Networking/Remote/ProductVariationsRemote.swift
+++ Networking/Networking/Remote/ProductVariationsRemote.swift
@@ -337,6 +337,6 @@ public extension ProductVariationsRemote {
 
 private extension ProductVariationsRemote {
     enum POSConstants {
-        static let variationsPerPage: Int = 100
+        static let variationsPerPage: Int = 25
     }
 }

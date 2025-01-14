434ebbc12166de11d6ebfca63c4bc20b8872bf07
iamgabrielma <gabrielmaldonado.art@gmail.com>
2025-01-14 15:33:34 +0800

    Wrap calculation methods in CollectCashViewHelper

--- WooCommerce/Classes/POS/Presentation/PointOfSaleCollectCashView.swift
+++ WooCommerce/Classes/POS/Presentation/PointOfSaleCollectCashView.swift
@@ -1,17 +1,80 @@
 import SwiftUI
 import class WooFoundation.CurrencyFormatter
 
+final class CollectCashViewHelper {
+    private let currencyFormatter: CurrencyFormatter = WooFoundation.CurrencyFormatter(currencySettings: ServiceLocator.currencySettings)
+
+    func updatechangeDueMessage(orderTotal: String,
+                                textFieldAmountInput: String) -> String? {
+        guard let orderDecimal = parseCurrency(orderTotal),
+              let inputDecimal = parseCurrency(textFieldAmountInput) else {
+            return nil
+        }
+        if inputDecimal.compare(orderDecimal) == .orderedDescending {
+            let changeDue = inputDecimal.subtracting(orderDecimal)
+            return  String.localizedStringWithFormat(Localization.changeDueMessage, formatAsCurrency(changeDue))
+        } else {
+            return nil
+        }
+    }
+
+    func validateAmountOnSubmit(orderTotal: String,
+                                textFieldAmountInput: String,
+                                onError: (String) -> Void) -> Bool {
+        guard let orderDecimal = parseCurrency(orderTotal),
+              let inputDecimal = parseCurrency(textFieldAmountInput) else {
+            onError(Localization.failedToCollectCashPayment)
+            return false
+        }
+
+        if inputDecimal.compare(orderDecimal) == .orderedAscending {
+            onError(Localization.cashPaymentAmountNotEnough)
+            return false
+        }
+        return true
+    }
+
+    func parseCurrency(_ amountString: String) -> NSDecimalNumber? {
+        currencyFormatter.convertToDecimal(amountString, locale: .current)
+    }
+
+    func formatAsCurrency(_ amount: NSDecimalNumber) -> String {
+        currencyFormatter.formatAmount(amount) ?? "$0.00"
+    }
+}
+
+private extension CollectCashViewHelper {
+    enum Localization {
+        static let changeDueMessage = NSLocalizedString(
+            "collectcashviewhelper.changedue",
+            value: "Change due: %1$@",
+            comment: "Change due when the cash amount entered exceeds the order total." +
+            "Reads as 'Change due: $1.23'"
+        )
+        static let failedToCollectCashPayment = NSLocalizedString(
+            "collectcashviewhelper.failedtocollectcashpayment.errormessage",
+            value: "Error trying to process payment. Try again.",
+            comment: "Error message when the system fails to collect a cash payment."
+        )
+        static let cashPaymentAmountNotEnough = NSLocalizedString(
+            "collectcashviewhelper.cashpaymentamountnotenough.errormessage",
+            value: "Amount must be more or equal to total.",
+            comment: "Error message when the cash amount entered is less than the order total."
+        )
+    }
+}
+
 struct PointOfSaleCollectCashView: View {
     @Environment(\.colorScheme) var colorScheme
     @EnvironmentObject private var posModel: PointOfSaleAggregateModel
     @FocusState private var isTextFieldFocused: Bool
 
+    private let viewHelper = CollectCashViewHelper()
+
     @State private var textFieldAmountInput: String = ""
     @State private var isLoading: Bool = false
     @State private var errorMessage: String?
     @State private var changeDueMessage: String?
-
-    private let currencyFormatter: CurrencyFormatter = WooFoundation.CurrencyFormatter(currencySettings: ServiceLocator.currencySettings)
 
     let orderTotal: String
 
@@ -119,41 +182,26 @@ struct PointOfSaleCollectCashView: View {
 
 private extension PointOfSaleCollectCashView {
     func parseCurrency(_ amountString: String) -> NSDecimalNumber? {
-        currencyFormatter.convertToDecimal(amountString, locale: .current)
+        viewHelper.parseCurrency(amountString)
     }
 
     private func formatAsCurrency(_ amount: NSDecimalNumber) -> String {
-        currencyFormatter.formatAmount(amount) ?? "$0.00"
+        viewHelper.formatAsCurrency(amount)
     }
 
     private func updateChangeDueMessage() {
-        guard let orderDecimal = parseCurrency(orderTotal),
-              let inputDecimal = parseCurrency(textFieldAmountInput) else {
-            changeDueMessage = nil
-            return
-        }
-
-        if inputDecimal.compare(orderDecimal) == .orderedDescending {
-            let changeDue = inputDecimal.subtracting(orderDecimal)
-            changeDueMessage = String.localizedStringWithFormat(Localization.changeDueMessage, formatAsCurrency(changeDue))
-        } else {
-            changeDueMessage = nil
-        }
+        changeDueMessage = viewHelper.updatechangeDueMessage(
+            orderTotal: orderTotal,
+            textFieldAmountInput: textFieldAmountInput)
     }
 
     private func validateAmountOnSubmit() -> Bool {
-            guard let orderDecimal = parseCurrency(orderTotal),
-                  let inputDecimal = parseCurrency(textFieldAmountInput) else {
-                errorMessage = Localization.failedToCollectCashPayment
-                return false
-            }
-
-            if inputDecimal.compare(orderDecimal) == .orderedAscending {
-                errorMessage = Localization.cashPaymentAmountNotEnough
-                return false
-            }
-            errorMessage = nil
-            return true
+        viewHelper.validateAmountOnSubmit(
+            orderTotal: orderTotal,
+            textFieldAmountInput: textFieldAmountInput,
+            onError: { error in
+                errorMessage = error
+            })
         }
 }
 
@@ -195,17 +243,6 @@ private extension PointOfSaleCollectCashView {
             "pointOfSale.cashview.failedtocollectcashpayment.errormessage",
             value: "Error trying to process payment. Try again.",
             comment: "Error message when the system fails to collect a cash payment."
-        )
-        static let cashPaymentAmountNotEnough = NSLocalizedString(
-            "pointOfSale.cashview.cashpaymentamountnotenough.errormessage",
-            value: "Amount must be more or equal to total.",
-            comment: "Error message when the cash amount entered is less than the order total."
-        )
-        static let changeDueMessage = NSLocalizedString(
-            "pointOfSale.cashview.changedue",
-            value: "Change due: %1$@",
-            comment: "Change due when the cash amount entered exceeds the order total." +
-            "Reads as 'Change due: $1.23'"
         )
     }
 }

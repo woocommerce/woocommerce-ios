0e62a531e53977f801a2f67ad7245e22d2c9d73a
iamgabrielma <gabrielmaldonado.art@gmail.com>
2025-01-15 14:25:04 +0800

    Header alignment
    We align the header for the cash view as the previous header on the navigation flow (cart)

--- WooCommerce/Classes/POS/Presentation/PointOfSaleCollectCashView.swift
+++ WooCommerce/Classes/POS/Presentation/PointOfSaleCollectCashView.swift
@@ -24,7 +24,7 @@ struct PointOfSaleCollectCashView: View {
                                                                                       allowNegativeNumber: false)
 
     var body: some View {
-        VStack(alignment: .center, spacing: 20) {
+        VStack(alignment: .center) {
             HStack {
                 Button(action: {
                     Task { @MainActor in
@@ -32,15 +32,14 @@ struct PointOfSaleCollectCashView: View {
                     }
                 }, label: {
                     HStack(alignment: .top) {
-                        Image(systemName: "chevron.left")
-                            .font(.posTitleRegular)
-                            .bold()
+                        Image(systemName: "chevron.backward")
+                            .font(.posBodyEmphasized, maximumContentSizeCategory: .accessibilityLarge)
                             .foregroundColor(.primary)
-                        VStack(alignment: .leading, spacing: Constants.navigationButtonSpacing) {
+                        VStack(alignment: .leading) {
                             Text(Localization.backNavigationTitle)
-                                .font(.posTitleRegular)
-                                .bold()
-                                .foregroundColor(.primary)
+                                .font(.posTitleEmphasized)
+                                .foregroundColor(.posPrimaryText)
+                                .accessibilityAddTraits(.isHeader)
 
                             Text(formattedOrderTotal)
                                 .font(.posBodyRegular)
@@ -51,7 +50,6 @@ struct PointOfSaleCollectCashView: View {
                 })
                 Spacer()
             }
-            .padding()
 
             FormattableAmountTextField(viewModel: textFieldViewModel, style: .pos)
                 .onChange(of: textFieldViewModel.amount) { newValue in
@@ -108,7 +106,7 @@ struct PointOfSaleCollectCashView: View {
             Spacer()
         }
         .background(backgroundColor)
-        .padding()
+        .padding([.horizontal, .bottom])
         .animation(.easeInOut, value: errorMessage)
         .animation(.easeInOut, value: changeDueMessage)
         .onChange(of: textFieldAmountInput) { _ in
@@ -143,7 +141,7 @@ private extension PointOfSaleCollectCashView {
         static let buttonSpacing: CGFloat = 12
         static let buttonPadding: CGFloat = 32
         static let buttonMinHeight: CGFloat = 32
-        static let navigationButtonSpacing: CGFloat = 4
+        static let navigationButtonSpacing: CGFloat = 8
         static let buttonFont: POSFontStyle = .posBodyEmphasized
         static let buttonCornerRadius: CGFloat = 8
     }

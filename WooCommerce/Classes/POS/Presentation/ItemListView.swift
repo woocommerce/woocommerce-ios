891598fac48ed4459ec862a595386d71d301fbbd
Josh Heald <joshheald@users.noreply.github.com>
2025-01-15 13:58:02 +0000

    14772 Fix background color for item list

--- WooCommerce/Classes/POS/Presentation/ItemListView.swift
+++ WooCommerce/Classes/POS/Presentation/ItemListView.swift
@@ -33,11 +33,11 @@ struct ItemListView: View {
             .navigationDestination(for: POSItem.self, destination: { item in
                 childListView(parentItem: item)
             })
+            .background(Color.posPrimaryBackground)
         }
         .refreshable {
             await posModel.reloadItems(base: .root)
         }
-        .background(Color.posPrimaryBackground)
         .accessibilityElement(children: .contain)
         .posModal(isPresented: $showSimpleProductsModal) {
             SimpleProductsOnlyInformation(isPresented: $showSimpleProductsModal)

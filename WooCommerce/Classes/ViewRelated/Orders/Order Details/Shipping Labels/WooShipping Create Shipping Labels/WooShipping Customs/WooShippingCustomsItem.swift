90f3f37002d6fb98c0cda253157599a92b597472
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2025-01-16 12:18:12 +0100

    Remove trailing whitespace

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/WooShipping Customs/WooShippingCustomsItem.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/WooShipping Customs/WooShippingCustomsItem.swift
@@ -141,7 +141,7 @@ struct WooShippingCustomsItem: View {
                         .roundedBorder(cornerRadius: Layout.borderCornerRadius,
                                        lineColor: viewModel.valuePerUnit.isEmpty ? warningRedColor : Color(.separator),
                                        lineWidth: Layout.borderLineWidth)
-                        
+                    
                         Text(Localization.valueRequiredWarningText)
                             .foregroundColor(warningRedColor)
                             .footnoteStyle()

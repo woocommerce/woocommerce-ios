c59b2c95afe24b450018102950afcf72b6bfab90
Jaclyn Chen <watertranquil@gmail.com>
2025-01-16 17:43:14 +0800

    Disable capitalization in POS receipt email form.

--- WooCommerce/Classes/POS/Presentation/Reusable Views/POSSendReceiptView.swift
+++ WooCommerce/Classes/POS/Presentation/Reusable Views/POSSendReceiptView.swift
@@ -35,7 +35,7 @@ struct POSSendReceiptView: View {
 
             TextField(Localization.textfieldPlaceholder, text: $textFieldInput)
                 .keyboardType(.emailAddress)
-                .textInputAutocapitalization(.none)
+                .textInputAutocapitalization(.never)
                 .autocorrectionDisabled()
                 .multilineTextAlignment(.center)
                 .font(POSFontStyle.posTitleRegular)

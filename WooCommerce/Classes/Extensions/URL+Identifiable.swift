4eeb61bd7e02a11c1ccfd02791b6715ca9c023f9
Paolo Musolino <info@codeido.com>
2025-01-20 13:46:21 +0100

    fix: comment lenght

--- WooCommerce/Classes/Extensions/URL+Identifiable.swift
+++ WooCommerce/Classes/Extensions/URL+Identifiable.swift
@@ -3,7 +3,7 @@ import Foundation
 /// Conformance to display SwiftUI sheet based on a URL.
 /// 
 /// The `@retroactive` attribute is used to apply `Identifiable` to `URL` from the Foundation module.
-/// This is necessary due to Swift 6's [SE-0364 proposal](https://github.com/swiftlang/swift-evolution/blob/main/proposals/0364-retroactive-conformance-warning.md).
+/// This is necessary due to Swift 6 [SE-0364 proposal](https://github.com/swiftlang/swift-evolution/blob/main/proposals/0364-retroactive-conformance-warning.md).
 extension URL: @retroactive Identifiable {
     public var id: String { absoluteString }
 }

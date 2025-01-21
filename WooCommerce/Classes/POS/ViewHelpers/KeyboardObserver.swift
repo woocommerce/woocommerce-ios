8f90faed2635c5bcf9b83cc1a6917991d8fac7d5
iamgabrielma <gabrielmaldonado.art@gmail.com>
2025-01-21 12:07:13 +0100

    clean up keyboard observer

--- WooCommerce/Classes/POS/ViewHelpers/KeyboardObserver.swift
+++ WooCommerce/Classes/POS/ViewHelpers/KeyboardObserver.swift
@@ -6,16 +6,30 @@ final class KeyboardObserver: ObservableObject {
     private var cancellables = Set<AnyCancellable>()
 
     init() {
-        let keyboardWillShow = NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
+        observeKeyboardNotifications()
+    }
+
+    private func observeKeyboardNotifications() {
+        let keyboardWillChange = NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
         let keyboardWillHide = NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
 
-        keyboardWillShow.merge(with: keyboardWillHide)
+        keyboardWillChange
+            .merge(with: keyboardWillHide)
             .sink { [weak self] notification in
-                guard let self = self else { return }
-                if let endFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
-                    self.keyboardHeight = endFrame.origin.y >= UIScreen.main.bounds.height ? 0 : endFrame.height
-                }
+                self?.handleKeyboardNotification(notification)
             }
             .store(in: &cancellables)
+    }
+
+    private func handleKeyboardNotification(_ notification: Notification) {
+        // UIResponder.keyboardFrameEndUserInfoKey contains where the keyboard will be after the animation completes,
+        // so we know the keyboard's end position in the screen
+        guard let userInfo = notification.userInfo,
+              let endFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+
+        let screenHeight = UIScreen.main.bounds.height
+        keyboardHeight = (endFrame.origin.y >= screenHeight) ? 0 : endFrame.height
     }
 }

f7df657d4f145da74ed745856078bb9de3e6d007
Rachel McRoberts <rachel@automattic.com>
2025-01-21 11:23:30 +0000

    Add generate fake method to WooShippingAddress

--- Networking/Networking/Model/ShippingLabel/WooShippingAddress.swift
+++ Networking/Networking/Model/ShippingLabel/WooShippingAddress.swift
@@ -3,7 +3,7 @@ import Codegen
 
 /// Represents an address for the WooCommerce Shipping extension.
 ///
-public struct WooShippingAddress: Equatable {
+public struct WooShippingAddress: Equatable, GeneratedFakeable {
     /// The name of the company at the address.
     public let company: String
 

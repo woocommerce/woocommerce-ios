114ff43869447a0f37af755ea98b27d49cef61f2
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2025-01-22 15:05:43 +0100

    Add unit tests

--- WooCommerce/WooCommerceTests/ViewRelated/Shipping Label/WooShipping Create Shipping Labels/WooShipping Customs/WooShippingCustomsFormViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Shipping Label/WooShipping Create Shipping Labels/WooShipping Customs/WooShippingCustomsFormViewModelTests.swift
@@ -26,10 +26,12 @@ class WooShippingCustomsFormViewModelTests: XCTestCase {
             passedForm = form
         })
 
-        viewModel.restrictionType = .quarantine
-        viewModel.contentType = .gift
         viewModel.internationalTransactionNumber = "NOEEI 30.37(a)"
         viewModel.returnToSenderIfNotDelivered = false
+        viewModel.contentType = .other
+        viewModel.contentExplanation = "explanation test"
+        viewModel.restrictionType = .other
+        viewModel.restrictionDetails = "restriction details"
 
         viewModel.itemsViewModels.first?.description = "Test Item"
         viewModel.itemsViewModels.first?.valuePerUnit = "10"
@@ -41,8 +43,10 @@ class WooShippingCustomsFormViewModelTests: XCTestCase {
         viewModel.onDismiss()
 
         // Then
-        XCTAssertEqual(passedForm?.restrictionType, .quarantine)
-        XCTAssertEqual(passedForm?.contentsType, .gift)
+        XCTAssertEqual(passedForm?.restrictionType, .other)
+        XCTAssertEqual(passedForm?.restrictionComments, viewModel.restrictionDetails)
+        XCTAssertEqual(passedForm?.contentsType, .other)
+        XCTAssertEqual(passedForm?.contentExplanation, viewModel.contentExplanation)
         XCTAssertEqual(passedForm?.itn, viewModel.internationalTransactionNumber)
         XCTAssertEqual(passedForm?.nonDeliveryOption, .abandon)
 
@@ -72,6 +76,31 @@ class WooShippingCustomsFormViewModelTests: XCTestCase {
 
         // Then
         XCTAssertTrue(passedForm?.items.first?.hsTariffNumber.isEmpty ?? false)
+    }
+
+    func test_onDismiss_when_calls_onCompletion_with_content_and_restriction_not_other_then_returns_empty() {
+        // Given
+        let orderItems = [MockOrderItem.sampleItem(productID: 123, quantity: 2), MockOrderItem.sampleItem()]
+
+        var passedForm: ShippingLabelCustomsForm?
+        viewModel = WooShippingCustomsFormViewModel(order: Order.fake().copy(items: orderItems), onCompletion: { form in
+            passedForm = form
+        })
+
+        viewModel.contentType = .documents
+        viewModel.contentExplanation = "content explanation"
+
+        viewModel.restrictionType = .quarantine
+        viewModel.restrictionDetails = "restriction details"
+
+        // When
+        viewModel.onDismiss()
+
+        // Then
+        XCTAssertEqual(passedForm?.contentsType, .documents)
+        XCTAssertEqual(passedForm?.restrictionType, .quarantine)
+        XCTAssertEqual(passedForm?.contentExplanation, "")
+        XCTAssertEqual(passedForm?.restrictionComments, "")
     }
 
     func test_onDismiss_when_calls_onCompletion_with_invalid_itn_then_returns_empty() {

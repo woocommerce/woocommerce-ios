8777c024c742ff7e7618f5bda93222db235a6aba
Huong Do <hi.huongdo@gmail.com>
2025-01-23 17:17:19 +0700

    Send notification to reset storage after dropping database

--- Storage/Storage/CoreData/CoreDataManager.swift
+++ Storage/Storage/CoreData/CoreDataManager.swift
@@ -321,6 +321,7 @@ private extension CoreDataManager {
         var persistentStoreRemovalError: Error?
         do {
             try persistentContainer.persistentStoreCoordinator.destroyPersistentStore(at: storeURL, type: .sqlite, options: nil)
+            NotificationCenter.default.post(name: .StorageManagerDidResetStorage, object: self)
         } catch {
             persistentStoreRemovalError = error
         }

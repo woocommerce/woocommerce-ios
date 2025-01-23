f137a526107d2819d89fe7b1d051a616c73d0723
Huong Do <hi.huongdo@gmail.com>
2025-01-23 21:03:30 +0700

    Revert "Enable viewing product page for private products"
    This reverts commit ab372902f05bd8a605b6cf851e4a2cdb86517408.

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewModel.swift
@@ -304,7 +304,7 @@ extension ProductFormViewModel {
     }
 
     func canViewProductInStore() -> Bool {
-        formType != .add
+        originalProduct.product.productStatus == .published && formType != .add
     }
 
     func canShareProduct() -> Bool {

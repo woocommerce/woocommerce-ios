412d7dd77309186028aac49a1fa847a352b36a4a
Josh Heald <joshheald@users.noreply.github.com>
2025-01-23 16:54:26 +0000

    14965 Add variations empty state strings

--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/PointOfSaleItemListEmptyView.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/PointOfSaleItemListEmptyView.swift
@@ -1,7 +1,11 @@
 import SwiftUI
 
 struct PointOfSaleItemListEmptyView: View {
-    let base: ItemListBaseItem
+    private let baseItem: ItemListBaseItem
+
+    init(base: ItemListBaseItem) {
+        self.baseItem = base
+    }
 
     var body: some View {
         VStack(alignment: .center, spacing: PointOfSaleItemListErrorLayout.headerSpacing) {
@@ -11,14 +15,14 @@ struct PointOfSaleItemListEmptyView: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: Constants.iconSize, height: Constants.iconSize)
                 .foregroundColor(.posSecondaryText)
-            Text(Localization.emptyProductsTitle)
+            Text(title)
                 .foregroundStyle(Color.posSecondaryText)
                 .font(.posTitleEmphasized)
-            Text(Localization.emptyProductsSubtitle)
+            Text(subtitle)
                 .foregroundStyle(Color.posSecondaryText)
                 .font(.posBodyRegular)
                 .padding([.leading, .trailing])
-            Text(Localization.emptyProductsHint)
+            Text(hint)
                 .foregroundStyle(Color.posSecondaryText)
                 .font(.posBodyRegular)
                 .padding([.leading, .trailing])
@@ -28,6 +32,42 @@ struct PointOfSaleItemListEmptyView: View {
 }
 
 private extension PointOfSaleItemListEmptyView {
+    var title: String {
+        switch baseItem {
+        case .root:
+            return Localization.emptyProductsTitle
+        case .parent(.variableParentProduct):
+            return Localization.emptyVariableParentProductTitle
+        default:
+            assertionFailure("No title defined for \(baseItem)")
+            return Localization.emptyProductsTitle
+        }
+    }
+
+    var subtitle: String {
+        switch baseItem {
+        case .root:
+            return Localization.emptyProductsSubtitle
+        case .parent(.variableParentProduct):
+            return Localization.emptyVariableParentProductSubtitle
+        default:
+            assertionFailure("No subtitle defined for \(baseItem)")
+            return Localization.emptyProductsTitle
+        }
+    }
+
+    var hint: String {
+        switch baseItem {
+        case .root:
+            return Localization.emptyProductsHint
+        case .parent(.variableParentProduct):
+            return Localization.emptyVariableParentProductHint
+        default:
+            assertionFailure("No hint defined for \(baseItem)")
+            return Localization.emptyProductsTitle
+        }
+    }
+
     enum Constants {
         static let iconSystemName: String = "plus.magnifyingglass"
         static let iconSize: CGFloat = 100
@@ -46,6 +86,22 @@ private extension PointOfSaleItemListEmptyView {
         static let emptyProductsHint = NSLocalizedString(
             "pos.pointOfSaleItemListEmptyView.emptyProductsHint",
             value: "To add one, exit POS and go to Products",
+            comment: "Text hinting the merchant to create a product."
+        )
+
+        static let emptyVariableParentProductTitle = NSLocalizedString(
+            "pos.pointOfSaleItemListEmptyView.emptyVariableParentProductTitle",
+            value: "No supported variations found.",
+            comment: "Text appearing on screen when there are no variations to load."
+        )
+        static let emptyVariableParentProductSubtitle = NSLocalizedString(
+            "pos.pointOfSaleItemListEmptyView.emptyVariableParentProductSubtitle",
+            value: "POS only supports enabled, non-downloadable variations.",
+            comment: "Subtitle text on screen when there are no products to load."
+        )
+        static let emptyVariableParentProductHint = NSLocalizedString(
+            "pos.pointOfSaleItemListEmptyView.emptyVariableParentProductHint",
+            value: "To add one, exit POS and edit this product in the Products tab.",
             comment: "Text hinting the merchant to create a product."
         )
     }

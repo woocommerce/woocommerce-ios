26eef5c605f9353c7479bc3b4d9860ff7032b0b0
Huong Do <hi.huongdo@gmail.com>
2025-01-23 22:20:22 +0700

    Check for Jetpack plugin before authenticate user

--- WooCommerce/Classes/ViewRelated/JetpackSetup/JetpackSetupCoordinator.swift
+++ WooCommerce/Classes/ViewRelated/JetpackSetup/JetpackSetupCoordinator.swift
@@ -222,7 +222,7 @@ private extension JetpackSetupCoordinator {
 
         /// WPCom credentials to authenticate the user in the Jetpack connection web view automatically
         let credentials: Credentials = .wpcom(username: username, authToken: authToken, siteAddress: site.url)
-        guard jetpackConnectedEmail == nil else {
+        guard jetpackConnectedEmail == nil || !site.isJetpackThePluginInstalled else {
             // authenticate user immediately
             return authenticateUserAndRefreshSite(with: credentials)
         }

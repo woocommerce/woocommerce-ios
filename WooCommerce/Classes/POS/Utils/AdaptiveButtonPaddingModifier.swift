0cddff1d6bdc2d045c957efd2cca25b7c279638e
iamgabrielma <gabrielmaldonado.art@gmail.com>
2025-01-24 07:01:43 +0100

    styling

--- WooCommerce/Classes/POS/Utils/AdaptiveButtonPaddingModifier.swift
+++ WooCommerce/Classes/POS/Utils/AdaptiveButtonPaddingModifier.swift
@@ -12,14 +12,22 @@ struct AdaptiveButtonPaddingModifier: ViewModifier {
 
     private func adjustablePadding(for size: DynamicTypeSize, defaultButtonPadding: CGFloat) -> CGFloat {
         switch size {
-        case .xxxLarge:       return defaultButtonPadding * 0.7
-        case .xxLarge:        return defaultButtonPadding * 0.8
-        case .xLarge:         return defaultButtonPadding * 0.9
-        case .accessibility1: return defaultButtonPadding * 0.6
-        case .accessibility2: return defaultButtonPadding * 0.5
-        case .accessibility3: return defaultButtonPadding * 0.4
-        case .accessibility4: return defaultButtonPadding * 0.3
-        case .accessibility5: return defaultButtonPadding * 0.2
+        case .xxxLarge:
+            return defaultButtonPadding * 0.7
+        case .xxLarge:
+            return defaultButtonPadding * 0.8
+        case .xLarge:
+            return defaultButtonPadding * 0.9
+        case .accessibility1:
+            return defaultButtonPadding * 0.6
+        case .accessibility2:
+            return defaultButtonPadding * 0.5
+        case .accessibility3:
+            return defaultButtonPadding * 0.4
+        case .accessibility4:
+            return defaultButtonPadding * 0.3
+        case .accessibility5:
+            return defaultButtonPadding * 0.2
         default:
             return defaultButtonPadding
         }

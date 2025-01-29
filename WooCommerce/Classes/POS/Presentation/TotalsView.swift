4a6a1fc7d3fd1e88c6fc3f70ef4a34400b08f834
Josh Heald <joshheald@users.noreply.github.com>
2025-01-29 12:57:07 +0000

    Allow cash payment for free orders

--- WooCommerce/Classes/POS/Presentation/TotalsView.swift
+++ WooCommerce/Classes/POS/Presentation/TotalsView.swift
@@ -35,7 +35,7 @@ struct TotalsView: View {
                         .renderedIf(cardReaderViewLayout.topPadding == nil)
 
                     VStack(alignment: .center, spacing: 0) {
-                        if isShowingCardReaderStatus {
+                        if isShowingPaymentView {
                             paymentView
                                 .font(.title)
                                 .padding([.leading, .trailing],
@@ -58,25 +58,29 @@ struct TotalsView: View {
                                 .opacity(viewHelper.shouldShowTotalsFields(for: posModel.paymentState) ? 1 : 0)
                                 .layoutPriority(2)
                         }
-                        Button(action: {
-                            Task { @MainActor in
-                                await posModel.startCashPayment()
-                            }
-                        }, label: {
-                            Text(Localization.cashPaymentButtonTitle)
-                                .font(POSFontStyle.posBodyEmphasized)
-                                .foregroundColor(.posPrimaryText)
-                                .frame(height: Constants.buttonHeight)
-                                .minimumScaleFactor(0.5)
-                        })
-                        .buttonStyle(SecondaryButtonStyle())
-                        .padding(.horizontal, Constants.buttonHorizontalPadding)
+
+                        Group {
+                            Spacer()
+                            Button(action: {
+                                Task { @MainActor in
+                                    await posModel.startCashPayment()
+                                }
+                            }, label: {
+                                Text(Localization.cashPaymentButtonTitle)
+                                    .font(POSFontStyle.posBodyEmphasized)
+                                    .foregroundColor(.posPrimaryText)
+                                    .frame(height: Constants.buttonHeight)
+                                    .minimumScaleFactor(0.5)
+                            })
+                            .buttonStyle(SecondaryButtonStyle())
+                            .padding(.horizontal, Constants.buttonHorizontalPadding)
+                        }
                         .renderedIf(shouldShowCollectCashPaymentButton)
                     }
                     .animation(.default, value: posModel.cardPresentPaymentInlineMessage)
                     Spacer()
                 }
-                .animation(.default, value: isShowingCardReaderStatus)
+                .animation(.default, value: isShowingPaymentView)
             case .error(let viewModel):
                 PointOfSaleOrderSyncErrorMessageView(viewModel: viewModel)
                     .transition(.opacity)
@@ -284,7 +288,7 @@ private extension TotalsView {
         )
     }
 
-    private var isShowingCardReaderStatus: Bool {
+    private var isShowingPaymentView: Bool {
         guard posModel.orderState.isLoaded else {
             // When the order's being created or synced, we only show the shimmering totals.
             // Before the order exists, we don’t want to show the card payment status, as it will
@@ -294,7 +298,12 @@ private extension TotalsView {
 
         switch posModel.cardReaderConnectionStatus {
         case .connected, .disconnecting, .cancellingConnection:
-            return posModel.cardPresentPaymentInlineMessage != nil
+            switch posModel.paymentState {
+            case .card:
+                return posModel.cardPresentPaymentInlineMessage != nil
+            case .cash:
+                return true
+            }
         case .disconnected:
             // Since the reader is disconnected, this will show the "Connect your reader" CTA button view.
             return true
@@ -302,7 +311,7 @@ private extension TotalsView {
     }
 
     private var cardReaderViewLayout: PaymentViewLayout {
-        guard isShowingCardReaderStatus else {
+        guard isShowingPaymentView else {
             return .primary
         }
 

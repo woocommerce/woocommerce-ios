c9db157cde65c8726edd1ca7b232812bd7ce2d59
Josh Heald <joshheald@users.noreply.github.com>
2025-01-29 13:19:17 +0000

    Pin cash button to the bottom of the view

--- WooCommerce/Classes/POS/Presentation/TotalsView.swift
+++ WooCommerce/Classes/POS/Presentation/TotalsView.swift
@@ -58,27 +58,26 @@ struct TotalsView: View {
                                 .opacity(viewHelper.shouldShowTotalsFields(for: posModel.paymentState) ? 1 : 0)
                                 .layoutPriority(2)
                         }
-
-                        Group {
-                            Spacer()
-                            Button(action: {
-                                Task { @MainActor in
-                                    await posModel.startCashPayment()
-                                }
-                            }, label: {
-                                Text(Localization.cashPaymentButtonTitle)
-                                    .font(POSFontStyle.posBodyEmphasized)
-                                    .foregroundColor(.posPrimaryText)
-                                    .frame(height: Constants.buttonHeight)
-                                    .minimumScaleFactor(0.5)
-                            })
-                            .buttonStyle(SecondaryButtonStyle())
-                            .padding(.horizontal, Constants.buttonHorizontalPadding)
-                        }
-                        .renderedIf(shouldShowCollectCashPaymentButton)
                     }
                     .animation(.default, value: posModel.cardPresentPaymentInlineMessage)
+
                     Spacer()
+
+                    Button(action: {
+                        Task { @MainActor in
+                            await posModel.startCashPayment()
+                        }
+                    }, label: {
+                        Text(Localization.cashPaymentButtonTitle)
+                            .font(POSFontStyle.posBodyEmphasized)
+                            .foregroundColor(.posPrimaryText)
+                            .frame(height: Constants.buttonHeight)
+                            .minimumScaleFactor(0.5)
+                    })
+                    .buttonStyle(SecondaryButtonStyle())
+                    .padding(.horizontal, Constants.buttonHorizontalPadding)
+                    .padding(.bottom, Constants.cashButtonBottomPadding)
+                    .renderedIf(shouldShowCollectCashPaymentButton)
                 }
                 .animation(.default, value: isShowingPaymentView)
             case .error(let viewModel):
@@ -350,6 +349,7 @@ private extension TotalsView {
         static let verticalSpacing: CGFloat = 56
         static let buttonHeight: CGFloat = 56
         static let buttonHorizontalPadding: CGFloat = 48
+        static let cashButtonBottomPadding: CGFloat = 16
 
         static let totalsLineViewPadding: EdgeInsets = .init(top: 20, leading: 24, bottom: 20, trailing: 24)
         static let subtotalsVerticalSpacing: CGFloat = 8

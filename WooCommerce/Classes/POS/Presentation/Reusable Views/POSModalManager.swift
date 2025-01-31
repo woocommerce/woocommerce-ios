7220b7bb209aa18db448d638679c620da3fabfdd
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2025-01-31 13:55:10 +0200

    Remove additional isPresented check

--- WooCommerce/Classes/POS/Presentation/Reusable Views/POSModalManager.swift
+++ WooCommerce/Classes/POS/Presentation/Reusable Views/POSModalManager.swift
@@ -1,26 +1,15 @@
 import SwiftUI
-import Combine
 
 class POSModalManager: ObservableObject {
     @Published private(set) var isPresented: Bool = false
     @Published private(set) var allowsInteractiveDismissal: Bool = true
     private var contentBuilder: (() -> AnyView)?
     private var onDismiss: (() -> Void)?
-    private var subscriptions = Set<AnyCancellable>()
 
     func present<Content: View>(onDismiss: @escaping () -> Void, content: @escaping () -> Content) {
         contentBuilder = { AnyView(content()) }
         self.onDismiss = onDismiss
         isPresented = true
-
-        $isPresented
-            .sink { [weak self] _ in
-                guard let self else { return }
-                if !isPresented {
-                    reset()
-                }
-            }
-            .store(in: &subscriptions)
     }
 
     func dismiss() {
@@ -45,6 +34,5 @@ class POSModalManager: ObservableObject {
         onDismiss = nil
         allowsInteractiveDismissal = true
         contentBuilder = nil
-        subscriptions = Set()
     }
 }

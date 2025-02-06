a405984d9af5343dc4edf0717ce1d874338e725a
Jaclyn Chen <watertranquil@gmail.com>
2025-02-06 13:48:21 +0800

    Replace `posSuccessColor` with `posSuccess` and update checkmark color to `posOnSuccess`.

--- WooCommerce/Classes/POS/Utils/Color+WooCommercePOS.swift
+++ WooCommerce/Classes/POS/Utils/Color+WooCommercePOS.swift
@@ -47,15 +47,6 @@ extension Color {
         )
     }
 
-    static var posSuccessColor: Color {
-        Color(
-            UIColor(
-                light: UIColor(red: 8.0/255.0, green: 251.0/255.0, blue: 135.0/255.0, alpha: 1.0),
-                dark: UIColor(red: 2.0/255.0, green: 140.0/255.0, blue: 89.0/255.0, alpha: 1.0)
-            )
-        )
-    }
-
     // MARK: - Buttons
 
     static var posPrimaryButtonBackground: Color = .posAccent

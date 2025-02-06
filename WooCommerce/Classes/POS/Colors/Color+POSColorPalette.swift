584d419da9c13a17eb0403c2b04f987657df9cc0
Jaclyn Chen <watertranquil@gmail.com>
2025-02-06 10:01:08 +0800

    Create a color palette for POS with constants in SwiftUI colors.

---
+++ WooCommerce/Classes/POS/Colors/Color+POSColorPalette.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+
+extension Color {
+    // MARK: - Primary Colors
+    static var posPrimary: Color { Color("primary") }
+    static var posOnPrimary: Color { Color("onPrimary") }
+    static var posPrimaryContainer: Color { Color("primaryContainer") }
+    static var posOnPrimaryContainer: Color { Color("onPrimaryContainer") }
+    
+    // MARK: - Secondary Container Colors
+    static var posSecondaryContainer: Color { Color("secondaryContainer") }
+    static var posOnSecondaryContainer: Color { Color("onSecondaryContainer") }
+    
+    // MARK: - Disabled Container Colors
+    static var posDisabledContainer: Color { Color("disabledContainer") }
+    static var posOnDisabledContainer: Color { Color("onDisabledContainer") }
+    
+    // MARK: - Surface Colors
+    static var posSurface: Color { Color("surface") }
+    static var posOnSurface: Color { Color("onSurface") }
+    static var posSurfaceDim: Color { Color("surfaceDim") }
+    static var posSurfaceBright: Color { Color("surfaceBright") }
+    static var posSurfaceContainerLowest: Color { Color("surfaceContainerLowest") }
+    static var posSurfaceContainerLow: Color { Color("surfaceContainerLow") }
+    static var posSurfaceContainerHighest: Color { Color("surfaceContainerHighest") }
+    
+    // MARK: - Surface Variant Colors
+    static var posOnSurfaceVariantLowest: Color { Color("onSurfaceVariantLowest") }
+    static var posOnSurfaceVariantHighest: Color { Color("onSurfaceVariantHighest") }
+    
+    // MARK: - Inverse Surface Colors
+    static var posInverseSurface: Color { Color("inverseSurface") }
+    static var posOnInverseSurface: Color { Color("onInverseSurface") }
+    
+    // MARK: - Outline Colors
+    static var posOutline: Color { Color("outline") }
+    static var posOutlineVariant: Color { Color("outlineVariant") }
+    
+    // MARK: - Status Colors
+    static var posError: Color { Color("error") }
+    static var posOnError: Color { Color("onError") }
+    static var posSuccess: Color { Color("success") }
+    static var posOnSuccess: Color { Color("onSuccess") }
+    static var posAlert: Color { Color("alert") }
+    static var posOnAlert: Color { Color("onAlert") }
+    
+    // MARK: - Text Colors
+    static var posTextPrimary: Color { Color("textPrimary") }
+    static var posTextSecondary: Color { Color("textSecondary") }
+    static var posTextTertiary: Color { Color("textTertiary") }
+    
+    // MARK: - Other
+    static var posShadow: Color { Color("shadow") }
+}

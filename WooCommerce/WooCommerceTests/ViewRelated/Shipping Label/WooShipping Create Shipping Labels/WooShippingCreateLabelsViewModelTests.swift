3d2970b8773207be0189bdcff5b8b603ce7e8b5b
Rachel McRoberts <rachel@automattic.com>
2025-02-06 11:32:29 +0000

    Add unit tests for unverified and missing destination address

--- WooCommerce/WooCommerceTests/ViewRelated/Shipping Label/WooShipping Create Shipping Labels/WooShippingCreateLabelsViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Shipping Label/WooShipping Create Shipping Labels/WooShippingCreateLabelsViewModelTests.swift
@@ -473,6 +473,32 @@ final class WooShippingCreateLabelsViewModelTests: XCTestCase {
         // Then
         XCTAssertTrue(viewModel.customsInformationIsCompleted)
     }
+
+    func test_destinationAddressStatus_unverified_and_noticeLabel_set_for_unverified_address() {
+        // Given
+        let address = Address.fake().copy(address1: "1 Main Street", city: "San Francisco", state: "CA", postcode: "12345", country: "US")
+        let order = Order.fake().copy(shippingAddress: address)
+
+        // When
+        let viewModel = WooShippingCreateLabelsViewModel(order: order)
+
+        // Then
+        XCTAssertEqual(viewModel.destinationAddressStatus, .unverified)
+        XCTAssertNotNil(viewModel.destinationAddressStatusNoticeLabel)
+    }
+
+    func test_destinationAddressStatus_missing_and_noticeLabel_set_for_empty_address() {
+        // Given
+        let destinationAddress = Address.fake()
+        let order = Order.fake().copy(shippingAddress: destinationAddress)
+
+        // When
+        let viewModel = WooShippingCreateLabelsViewModel(order: order)
+
+        // Then
+        XCTAssertEqual(viewModel.destinationAddressStatus, .missing)
+        XCTAssertNotNil(viewModel.destinationAddressStatusNoticeLabel)
+    }
 }
 
 private extension WooShippingCreateLabelsViewModelTests {

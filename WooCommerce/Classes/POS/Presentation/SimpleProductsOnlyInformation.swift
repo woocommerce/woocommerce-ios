d8802c810f2cd8b4573524e9285009448fc55627
Jaclyn Chen <watertranquil@gmail.com>
2025-02-10 10:08:22 +0800

    Replace one-off color in `SimpleProductsOnlyInformation` with DS color.

--- WooCommerce/Classes/POS/Presentation/SimpleProductsOnlyInformation.swift
+++ WooCommerce/Classes/POS/Presentation/SimpleProductsOnlyInformation.swift
@@ -40,7 +40,7 @@ struct SimpleProductsOnlyInformation: View {
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, Constants.hintVerticalPadding)
                 .padding(.horizontal, Constants.hintHorizontalPadding)
-                .background(Color(.quaternarySystemFill))
+                .background(Color(.posSurfaceDim))
                 .clipShape(RoundedRectangle(cornerRadius: Constants.hintBackgroundCornerRadius))
             }
             .multilineTextAlignment(.center)

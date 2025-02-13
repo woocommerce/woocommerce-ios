19909d16c211f39cbfe9169250c6fb5e9b55dbc8
Jaclyn Chen <watertranquil@gmail.com>
2025-02-13 15:12:12 +0800

    Replace most shadow cases to `posShadow` when applicable.

--- WooCommerce/Classes/POS/Presentation/ItemListView.swift
+++ WooCommerce/Classes/POS/Presentation/ItemListView.swift
@@ -116,7 +116,7 @@ private extension ItemListView {
         .fixedSize(horizontal: false, vertical: true)
         .background(Color.posSurfaceBright)
         .cornerRadius(Constants.bannerCornerRadius)
-        .shadow(color: Color.black.opacity(0.08), radius: 4, y: 2)
+        .posShadow(.medium)
         .accessibilityAddTraits(.isButton)
         .onTapGesture {
             showSimpleProductsModal = true

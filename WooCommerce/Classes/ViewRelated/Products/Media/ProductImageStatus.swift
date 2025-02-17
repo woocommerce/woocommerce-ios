f64e9ec545260578eb6596d82457e035cb36884d
Paolo Musolino <info@codeido.com>
2025-02-17 20:00:40 +0100

    feat: Implemented class for storing in UserDefaults Product Image Upload statuses + updated ProductImageStatusModel adding Codable conformance

--- WooCommerce/Classes/ViewRelated/Products/Media/ProductImageStatus.swift
+++ WooCommerce/Classes/ViewRelated/Products/Media/ProductImageStatus.swift
@@ -6,11 +6,11 @@ import Yosemite
 enum ProductImageStatus: Equatable {
     /// An image asset is being uploaded.
     ///
-    case uploading(asset: ProductImageAssetType)
+    case uploading(asset: ProductImageAssetType, siteID: Int64, productID: ProductOrVariationID)
 
     /// The Product image exists remotely.
     ///
-    case remote(image: ProductImage)
+    case remote(image: ProductImage, siteID: Int64, productID: ProductOrVariationID)
 }
 
 /// The type of product image asset.
@@ -26,8 +26,8 @@ extension Collection where Element == ProductImageStatus {
     var images: [ProductImage] {
         compactMap { status in
             switch status {
-            case .remote(let productImage):
-                return productImage
+            case .remote(let image, _, _):
+                return image
             default:
                 return nil
             }
@@ -39,7 +39,7 @@ extension Collection where Element == ProductImageStatus {
     var hasPendingUpload: Bool {
         return contains(where: {
             switch $0 {
-            case .uploading:
+            case .uploading(_, _, _):
                 return true
             default:
                 return false
@@ -67,15 +67,100 @@ extension ProductImageStatus {
     ///
     var dragItemIdentifier: String {
         switch self {
-        case .uploading(let asset):
+        case .uploading(let asset, _, _):
             switch asset {
                 case let .phAsset(asset):
                     return asset.identifier()
                 case .uiImage:
                     return UUID().uuidString
             }
-        case .remote(let image):
+        case .remote(let image, _, _):
             return "\(image.imageID)"
         }
     }
+}
+
+// Add Codable conformance for ProductImageStatus.
+// This will be used to encode and decode the status of a product image in UserDefaults
+extension ProductImageStatus: Codable {
+	enum CodingKeys: String, CodingKey { case type, asset, image, siteID, productID }
+
+	public func encode(to encoder: Encoder) throws {
+		var container = encoder.container(keyedBy: CodingKeys.self)
+		switch self {
+		case .uploading(let asset, let siteID, let productID):
+			try container.encode("uploading", forKey: .type)
+			try container.encode(asset, forKey: .asset)
+            try container.encode(siteID, forKey: .siteID)
+            try container.encode(productID, forKey: .productID)
+		case .remote(let image, let siteID, let productID):
+			try container.encode("remote", forKey: .type)
+			try container.encode(image, forKey: .image)
+            try container.encode(siteID, forKey: .siteID)
+            try container.encode(productID, forKey: .productID)
+		}
+	}
+
+	public init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		let type = try container.decode(String.self, forKey: .type)
+		switch type {
+		case "uploading":
+			let asset = try container.decode(ProductImageAssetType.self, forKey: .asset)
+            let siteID = try container.decode(Int64.self, forKey: .siteID)
+            let productID = try container.decode(ProductOrVariationID.self, forKey: .productID)
+			self = .uploading(asset: asset, siteID: siteID, productID: productID)
+		case "remote":
+			let image = try container.decode(ProductImage.self, forKey: .image)
+            let siteID = try container.decode(Int64.self, forKey: .siteID)
+            let productID = try container.decode(ProductOrVariationID.self, forKey: .productID)
+			self = .remote(image: image, siteID: siteID, productID: productID)
+		default:
+			throw DecodingError.dataCorruptedError(forKey: .type, in: container, debugDescription: "Unknown ProductImageStatus type")
+		}
+	}
+}
+
+// Add Codable conformance for ProductImageAssetType.
+extension ProductImageAssetType: Codable {
+	enum CodingKeys: String, CodingKey { case type, phAsset, uiImage, filename, altText }
+
+	public func encode(to encoder: Encoder) throws {
+		var container = encoder.container(keyedBy: CodingKeys.self)
+		switch self {
+		case .phAsset(let asset):
+			try container.encode("phAsset", forKey: .type)
+			try container.encode(asset.localIdentifier, forKey: .phAsset)
+		case .uiImage(let image, let filename, let altText):
+			try container.encode("uiImage", forKey: .type)
+			guard let imageData = image.pngData() else {
+				throw EncodingError.invalidValue(image, EncodingError.Context(codingPath: [CodingKeys.uiImage], debugDescription: "Unable to convert UIImage to PNG data"))
+			}
+			try container.encode(imageData, forKey: .uiImage)
+			try container.encode(filename, forKey: .filename)
+			try container.encode(altText, forKey: .altText)
+		}
+	}
+
+	public init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		let type = try container.decode(String.self, forKey: .type)
+		if type == "phAsset" {
+			let localId = try container.decode(String.self, forKey: .phAsset)
+			guard let asset = PHAsset.fetchAssets(withLocalIdentifiers: [localId], options: nil).firstObject else {
+				throw DecodingError.dataCorruptedError(forKey: .phAsset, in: container, debugDescription: "PHAsset not found for identifier \(localId)")
+			}
+			self = .phAsset(asset: asset)
+		} else if type == "uiImage" {
+			let imageData = try container.decode(Data.self, forKey: .uiImage)
+			guard let image = UIImage(data: imageData) else {
+				throw DecodingError.dataCorruptedError(forKey: .uiImage, in: container, debugDescription: "Unable to create UIImage from data")
+			}
+			let filename = try container.decodeIfPresent(String.self, forKey: .filename)
+			let altText = try container.decodeIfPresent(String.self, forKey: .altText)
+			self = .uiImage(image: image, filename: filename, altText: altText)
+		} else {
+			throw DecodingError.dataCorruptedError(forKey: .type, in: container, debugDescription: "Unknown ProductImageAssetType type")
+		}
+	}
 }
---
+++ WooCommerce/Classes/ViewRelated/Products/Media/ProductImagesUserDefaultsStatuses.swift
@@ -0,0 +1,84 @@
+import Foundation
+
+final class ProductImagesUserDefaultsStatuses {
+    private static let key = "savedProductUploadImageStatuses"
+
+    static func addStatus(_ status: ProductImageStatus) {
+        var statuses = getAllStatuses()
+        statuses.append(status)
+        saveAllStatuses(statuses)
+    }
+
+    static func removeStatus(_ status: ProductImageStatus) {
+        var statuses = getAllStatuses()
+        statuses.removeAll(where: { $0 == status })
+        saveAllStatuses(statuses)
+    }
+
+    static func findStatus(where predicate: (ProductImageStatus) -> Bool) -> ProductImageStatus? {
+        return getAllStatuses().first(where: predicate)
+    }
+
+    static func getAllStatuses() -> [ProductImageStatus] {
+        guard let data = UserDefaults.standard.data(forKey: key) else {
+            return []
+        }
+        do {
+            let statuses = try JSONDecoder().decode([ProductImageStatus].self, from: data)
+            return statuses
+        } catch {
+            DDLogError("Error decoding saved product image statuses: \(error)")
+            return []
+        }
+    }
+
+    static func getAllStatuses(for siteID: Int64, productID: ProductOrVariationID) -> [ProductImageStatus] {
+        return getAllStatuses().filter { status in
+            switch status {
+            case .uploading(_, let sID, let pID),
+                 .remote(_, let sID, let pID):
+                return sID == siteID && pID == productID
+            }
+        }
+    }
+
+    static func addStatus(_ status: ProductImageStatus, for siteID: Int64, productID: ProductOrVariationID) {
+        var statuses = getAllStatuses()
+        statuses.append(status)
+        saveAllStatuses(statuses)
+    }
+
+    static func removeStatus(_ status: ProductImageStatus, for siteID: Int64, productID: ProductOrVariationID) {
+        var statuses = getAllStatuses()
+        statuses.removeAll(where: { $0 == status })
+        saveAllStatuses(statuses)
+    }
+
+    private static func saveAllStatuses(_ statuses: [ProductImageStatus]) {
+        do {
+            let data = try JSONEncoder().encode(statuses)
+            UserDefaults.standard.set(data, forKey: key)
+        } catch {
+            DDLogError("Error encoding saved product image statuses: \(error)")
+        }
+    }
+}
+
+extension ProductImagesUserDefaultsStatuses {
+    static func setAllStatuses(_ statuses: [ProductImageStatus]) {
+        saveAllStatuses(statuses)
+    }
+
+    static func setAllStatuses(_ statuses: [ProductImageStatus], for siteID: Int64, productID: ProductOrVariationID) {
+        // Merge with existing, removing any old statuses for this site/product
+        var all = getAllStatuses().filter { st in
+            switch st {
+            case .uploading(_, let sID, let pID),
+                 .remote(_, let sID, let pID):
+                return !(sID == siteID && pID == productID)
+            }
+        }
+        all.append(contentsOf: statuses)
+        saveAllStatuses(all)
+    }
+}

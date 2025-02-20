b98b108a8c7367e53cbe595f830e7a8028f372b3
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2025-02-21 00:32:36 +0200

    Remove unused code in PointOfSaleLoadingView

--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/PointOfSaleLoadingView.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/PointOfSaleLoadingView.swift
@@ -36,13 +36,6 @@ private extension PointOfSaleLoadingView {
     }
 }
 
-private extension PointOfSaleLoadingView {
-    enum Layout {
-        static let textSpacing: CGFloat = POSSpacing.medium
-        static let progressViewSpacing: CGFloat = 72
-    }
-}
-
 #Preview {
     PointOfSaleLoadingView()
 }

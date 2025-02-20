146ca3c887bfab7456992658a57f7a36d27799ec
Jaclyn Chen <watertranquil@gmail.com>
2025-02-20 15:44:09 +0800

    Update message localizable string key to remove the cached value.

--- WooCommerce/Classes/POS/Presentation/Card Present Payments/Reader Messages/PointOfSaleCardPresentPaymentProcessingMessageViewModel.swift
+++ WooCommerce/Classes/POS/Presentation/Card Present Payments/Reader Messages/PointOfSaleCardPresentPaymentProcessingMessageViewModel.swift
@@ -14,7 +14,7 @@ private extension PointOfSaleCardPresentPaymentProcessingMessageViewModel {
         )
 
         static let pleaseWait = NSLocalizedString(
-            "pointOfSale.cardPresent.paymentProcessing.message",
+            "pointOfSale.cardPresent.waitForPaymentProcessing.message",
             value: "Please wait",
             comment: "Indicates to wait while payment is processing. Presented to users when payment collection starts"
         )

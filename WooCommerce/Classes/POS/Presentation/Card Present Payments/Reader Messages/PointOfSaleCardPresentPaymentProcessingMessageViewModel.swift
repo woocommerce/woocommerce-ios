cdded707c6f70006e8bec70daaca7774d6665424
Jaclyn Chen <watertranquil@gmail.com>
2025-02-20 10:39:29 +0800

    Update layout of `PointOfSaleCardPresentPaymentProcessingMessageView` to match design.

--- WooCommerce/Classes/POS/Presentation/Card Present Payments/Reader Messages/PointOfSaleCardPresentPaymentProcessingMessageViewModel.swift
+++ WooCommerce/Classes/POS/Presentation/Card Present Payments/Reader Messages/PointOfSaleCardPresentPaymentProcessingMessageViewModel.swift
@@ -15,7 +15,7 @@ private extension PointOfSaleCardPresentPaymentProcessingMessageViewModel {
 
         static let pleaseWait = NSLocalizedString(
             "pointOfSale.cardPresent.paymentProcessing.message",
-            value: "Please wait...",
+            value: "Please wait",
             comment: "Indicates to wait while payment is processing. Presented to users when payment collection starts"
         )
     }

4e34cd2f382c20d0b97a49e722ef2245e280f590
Josh Heald <joshheald@users.noreply.github.com>
2025-02-20 13:33:43 +0000

    15180 Create AggregateModel when POS appears
    Based on [Apple’s advice](https://developer.apple.com/documentation/swiftui/state#Store-observable-objects), this commit updates the `posModel` creation to happen in the task, to avoid it being created whenever init is called (confusingly, this may happen more than once for a view.)

--- WooCommerce/Classes/POS/Presentation/PointOfSaleEntryPointView.swift
+++ WooCommerce/Classes/POS/Presentation/PointOfSaleEntryPointView.swift
@@ -2,11 +2,15 @@ import SwiftUI
 
 @available(iOS 17.0, *)
 struct PointOfSaleEntryPointView: View {
-    @State private var posModel: PointOfSaleAggregateModel
+    @State private var posModel: PointOfSaleAggregateModel?
     @StateObject private var posModalManager = POSModalManager()
     @Environment(\.horizontalSizeClass) private var horizontalSizeClass
 
     private let onPointOfSaleModeActiveStateChange: ((Bool) -> Void)
+    private let itemsController: PointOfSaleItemsControllerProtocol
+    private let cardPresentPaymentService: CardPresentPaymentFacade
+    private let orderController: PointOfSaleOrderControllerProtocol
+    private let collectOrderPaymentAnalyticsTracker: POSCollectOrderPaymentAnalyticsTracking
 
     init(itemsController: PointOfSaleItemsControllerProtocol,
          onPointOfSaleModeActiveStateChange: @escaping ((Bool) -> Void),
@@ -15,26 +19,36 @@ struct PointOfSaleEntryPointView: View {
          collectOrderPaymentAnalyticsTracker: POSCollectOrderPaymentAnalyticsTracking) {
         self.onPointOfSaleModeActiveStateChange = onPointOfSaleModeActiveStateChange
 
-        let posModel = PointOfSaleAggregateModel(
-            itemsController: itemsController,
-            cardPresentPaymentService: cardPresentPaymentService,
-            orderController: orderController,
-            collectOrderPaymentAnalyticsTracker: collectOrderPaymentAnalyticsTracker)
-
-        self._posModel = State(wrappedValue: posModel)
+        self.itemsController = itemsController
+        self.cardPresentPaymentService = cardPresentPaymentService
+        self.orderController = orderController
+        self.collectOrderPaymentAnalyticsTracker = collectOrderPaymentAnalyticsTracker
     }
 
     var body: some View {
-        PointOfSaleDashboardView()
+        Group {
+            if let posModel = posModel {
+                PointOfSaleDashboardView()
+                    .environment(posModel)
+            } else {
+                PointOfSaleLoadingView()
+            }
+        }
+        .task {
+            posModel = PointOfSaleAggregateModel(
+                itemsController: itemsController,
+                cardPresentPaymentService: cardPresentPaymentService,
+                orderController: orderController,
+                collectOrderPaymentAnalyticsTracker: collectOrderPaymentAnalyticsTracker)
+        }
         .environmentObject(posModalManager)
-        .environment(posModel)
         .onAppear {
             onPointOfSaleModeActiveStateChange(true)
         }
         .onDisappear {
             onPointOfSaleModeActiveStateChange(false)
             posModalManager.onDisappear()
-            posModel.pointOfSaleClosed()
+            posModel?.pointOfSaleClosed()
         }
     }
 }

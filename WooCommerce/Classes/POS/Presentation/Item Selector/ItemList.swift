b90281deef326c83fe423e5efce051eb417b525c
Jaclyn Chen <watertranquil@gmail.com>
2025-02-21 09:21:57 +0800

    Nit: use spacing instead of padding.

--- WooCommerce/Classes/POS/Presentation/Item Selector/ItemList.swift
+++ WooCommerce/Classes/POS/Presentation/Item Selector/ItemList.swift
@@ -73,7 +73,7 @@ struct ItemList<HeaderView: View>: View {
 
 private enum Constants {
     static let itemListPadding: CGFloat = POSPadding.medium
-    static let itemSpacing: CGFloat = POSPadding.small
+    static let itemSpacing: CGFloat = POSSpacing.small
 }
 
 @available(iOS 17.0, *)

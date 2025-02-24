d7ed5124fc8a5ff75827c78f92108bfd959f2f61
Ian Maia <ian.maia@automattic.com>
2025-02-24 19:53:03 +0100

    [to revert] Test code change

--- WooCommerce/Classes/Analytics/AppStartupWaitingTimeTracker.swift
+++ WooCommerce/Classes/Analytics/AppStartupWaitingTimeTracker.swift
@@ -2,6 +2,7 @@ import Foundation
 import Yosemite
 import protocol WooFoundation.Analytics
 
+// test code change -- should trigger ui tests
 /// Tracks the waiting time for app startup, allowing to evaluate as analytics
 /// how much time in seconds it took between the init and the final `end(action:)` function call.
 ///

f5803bba4d371f3ba82c58f65f030bead54eafc8
Paolo Musolino <info@codeido.com>
2025-02-24 14:39:14 +0100

    refactor: Set up the observer first to wait for the status change to pending upload, then trigger the media upload after adding the observer.

--- WooCommerce/WooCommerceTests/ViewRelated/Products/Media/ProductImageUploaderTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Products/Media/ProductImageUploaderTests.swift
@@ -76,7 +76,6 @@ final class ProductImageUploaderTests: XCTestCase {
                                                                originalImages: []))
 
         // When
-        actionHandler.uploadMediaAssetToSiteMediaLibrary(asset: .phAsset(asset: asset))
         let statuses = waitFor { promise in
             actionHandler.addUpdateObserver(self) { statuses in
                 if statuses.hasPendingUpload {
@@ -84,6 +83,7 @@ final class ProductImageUploaderTests: XCTestCase {
                 }
             }
         }
+        actionHandler.uploadMediaAssetToSiteMediaLibrary(asset: .phAsset(asset: asset))
         XCTAssertTrue(statuses.hasPendingUpload)
         XCTAssertTrue(imageUploader.hasUnsavedChangesOnImages(key: .init(siteID: siteID,
                                                                          productOrVariationID: productID,

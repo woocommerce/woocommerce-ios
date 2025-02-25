8978d0a2c7092702bb463b25f967373728a077b0
Ian Maia <ian.maia@automattic.com>
2025-02-25 17:10:54 +0100

    Adjust Unit Tests skip message

--- .buildkite/commands/run-unit-tests.sh
+++ .buildkite/commands/run-unit-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
 
 if .buildkite/commands/should-skip-tests.sh; then
-  message="Skipping Unit Tests as only documentation and non-code files were changed"
+  message="Skipping Unit Tests as only documentation, tooling and/or non-code files were changed"
   echo "$message" | buildkite-agent annotate --style "info" --context "skip-unit-tests"
   echo "$message"
   exit 0

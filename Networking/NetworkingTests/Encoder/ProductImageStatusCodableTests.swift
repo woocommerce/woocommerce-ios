8c048aa562ea6a1e835e3b9d9bc7659712369025
Paolo Musolino <info@codeido.com>
2025-02-25 23:21:01 +0100

    feat: unit tests for `ProductImageAssetType` and moved `PHAssetMock` class to a different file

--- Networking/NetworkingTests/Encoder/ProductImageStatusCodableTests.swift
+++ Networking/NetworkingTests/Encoder/ProductImageStatusCodableTests.swift
@@ -167,17 +167,3 @@
             }
         }
     }
-
-    // PHAsset mock to allow encoding without an actual Photos library asset
-    private class PHAssetMock: PHAsset, @unchecked Sendable {
-        private let customLocalIdentifier: String
-
-        init(localIdentifier: String) {
-            self.customLocalIdentifier = localIdentifier
-            super.init()
-        }
-
-        override var localIdentifier: String {
-            return customLocalIdentifier
-        }
-    }

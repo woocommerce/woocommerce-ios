e8f0665928b97bdff5cd3c31cff89be65b6365e0
Jaclyn Chen <watertranquil@gmail.com>
2025-02-25 09:08:35 +0800

    ItemListErrorCardView: replace frame max width with modifier to fit width to content.

--- WooCommerce/Classes/POS/Presentation/Item Selector/ItemListErrorCardView.swift
+++ WooCommerce/Classes/POS/Presentation/Item Selector/ItemListErrorCardView.swift
@@ -35,7 +35,7 @@ struct ItemListErrorCardView: View {
                     .font(Constants.itemTitleFont)
             }
             .buttonStyle(POSOutlinedButtonStyle(size: .normal))
-            .frame(maxWidth: Constants.accessoryButtonMaxWidth * scale)
+            .fixedSize(horizontal: true, vertical: false)
             .padding(Constants.accessoryButtonPadding * (1 / scale))
         }
         .frame(maxWidth: .infinity, idealHeight: Constants.productCardSize * scale)

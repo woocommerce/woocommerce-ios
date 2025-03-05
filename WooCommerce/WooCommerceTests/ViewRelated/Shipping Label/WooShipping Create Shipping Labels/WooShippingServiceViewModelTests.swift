326b812bf8def2a7b8e3e1435dd8b876a1c660ad
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2025-03-05 17:54:04 +0530

    Test that error state is updated when shipment address is zero.

--- WooCommerce/WooCommerceTests/ViewRelated/Shipping Label/WooShipping Create Shipping Labels/WooShippingServiceViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Shipping Label/WooShipping Create Shipping Labels/WooShippingServiceViewModelTests.swift
@@ -248,15 +248,27 @@ final class WooShippingServiceViewModelTests: XCTestCase {
         XCTAssertEqual(viewModel.loadingState, .error(.missingDestinationAddress))
     }
 
-    func test_hasDestinationAddress_false_when_destination_address_is_empty() {
+    func test_it_sets_correct_error_state_when_total_shipment_weight_is_zero() {
         // Given
+        let stores = MockStoresManager(sessionManager: .testingInstance)
+        stores.whenReceivingAction(ofType: WooShippingAction.self) { action in
+            switch action {
+            case let .loadLabelRates(_, _, _, _, _, completion):
+                completion(.failure(NetworkError.timeout()))
+            default:
+                XCTFail("Received unexpected action: \(action)")
+            }
+        }
         let viewModel = WooShippingServiceViewModel(order: Order.fake(),
                                                     originAddress: WooShippingAddress.fake(),
-                                                    destinationAddress: WooShippingAddress.fake(),
+                                                    destinationAddress: sampleDestinationAddress(),
                                                     stores: stores)
 
+        // When
+        viewModel.loadLabelRates(for: samplePackage.copy(weight: 0))
+
         // Then
-        XCTAssertFalse(viewModel.hasDestinationAddress)
+        XCTAssertEqual(viewModel.loadingState, .error(.missingShipmentWeight))
     }
 }
 

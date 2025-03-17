3a3887d82ac2e1b8ad258158708c253162657b12
Huong Do <hi.huongdo@gmail.com>
2025-03-17 17:18:20 +0700

    Add feature flag for notification settings

--- Experiments/Experiments/DefaultFeatureFlagService.swift
+++ Experiments/Experiments/DefaultFeatureFlagService.swift
@@ -95,6 +95,8 @@ public struct DefaultFeatureFlagService: FeatureFlagService {
             return buildConfig == .localDeveloper || buildConfig == .alpha
         case .backgroundProductImageUpload:
             return buildConfig == .localDeveloper || buildConfig == .alpha
+        case .notificationSettings:
+            return buildConfig == .localDeveloper || buildConfig == .alpha
         default:
             return true
         }

4f5bb25962b0dbf78b177db20136c4efc19302c8
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2025-03-19 12:20:55 +0200

    Create PointOfSaleScanToPayView that displays a QR code on a POS checkout view

---
+++ WooCommerce/Classes/POS/Presentation/PointOfSaleScanToPayView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+@available(iOS 17.0, *)
+struct PointOfSaleScanToPayView: View {
+    @Environment(PointOfSaleAggregateModel.self) private var posModel
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
+
+    var body: some View {
+        VStack(alignment: .center, spacing: POSSpacing.medium) {
+            if let qrCodeImage = ScanToPayViewModel(paymentURL: posModel.paymentURL).generateQRCodeImage() {
+                Text(ScanToPayView.Localization.title)
+                    .font(.posHeadingBold)
+                    .dynamicTypeSize(...DynamicTypeSize.large)
+                Image(uiImage: qrCodeImage)
+                    .interpolation(.none)
+                    .resizable()
+                .buttonStyle(PrimaryButtonStyle())
+                .aspectRatio(1, contentMode: .fit)
+                .frame(minWidth: 250, minHeight: 250)
+                .layoutPriority(1)
+                .padding(dynamicTypeSize.isAccessibilitySize ? 0 : POSPadding.large)
+            }
+        }
+        .padding(.top, dynamicTypeSize.isAccessibilitySize ? 0 : POSPadding.large)
+        .onAppear {
+            posModel.startScanToPay()
+        }
+        .onDisappear() {
+            posModel.stopScanToPay()
+        }
+    }
+}

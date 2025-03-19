ec1e14a2a04f377e1450e1cfabb48da41e732e81
Huong Do <hi.huongdo@gmail.com>
2025-03-19 11:21:35 +0700

    Update tests for NotificationSettingsViewModel

--- WooCommerce/WooCommerceTests/ViewRelated/Settings/NotificationSettingsViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Settings/NotificationSettingsViewModelTests.swift
@@ -1,5 +1,6 @@
-import Testing
+import UIKit
 import UserNotifications
+import Testing
 @testable import WooCommerce
 
 struct NotificationSettingsViewModelTests {
@@ -10,25 +11,48 @@ struct NotificationSettingsViewModelTests {
         UNAuthorizationStatus.provisional,
         UNAuthorizationStatus.ephemeral
     ])
-    func notificationsEnabled_is_false_notification_permission_is_not_authorized(authorizationStatus: UNAuthorizationStatus) {
+    func notificationsEnabled_is_false_notification_permission_is_not_authorized(authorizationStatus: UNAuthorizationStatus) async throws {
         // Given
         let notificationCenter = MockUserNotificationsCenterAdapter()
         notificationCenter.authorizationStatus = authorizationStatus
 
         // When
         let viewModel = NotificationSettingsViewModel(notificationCenter: notificationCenter)
+        // bad workaround to wait for async update of `notificationsEnabled`
+        try await Task.sleep(nanoseconds: 300)
 
         // Then
         #expect(viewModel.notificationsEnabled == false)
     }
 
-    func notificationsEnabled_is_true_notification_permission_is_authorized() {
+    @Test func notificationsEnabled_is_true_notification_permission_is_authorized() async throws {
         // Given
         let notificationCenter = MockUserNotificationsCenterAdapter()
         notificationCenter.authorizationStatus = .authorized
 
         // When
         let viewModel = NotificationSettingsViewModel(notificationCenter: notificationCenter)
+        // bad workaround to wait for async update of `notificationsEnabled`
+        try await Task.sleep(nanoseconds: 300)
+
+        // Then
+        #expect(viewModel.notificationsEnabled == true)
+    }
+
+    @Test func notificationEnabled_is_updated_when_app_becomes_active() async throws {
+        // Given
+        let notificationCenter = MockUserNotificationsCenterAdapter()
+        notificationCenter.authorizationStatus = .denied
+
+        let viewModel = NotificationSettingsViewModel(notificationCenter: notificationCenter)
+        // Confidence check
+        #expect(viewModel.notificationsEnabled == false)
+
+        // When
+        notificationCenter.authorizationStatus = .authorized
+        NotificationCenter.default.post(name: UIApplication.didBecomeActiveNotification, object: nil)
+        // bad workaround to wait for async update of `notificationsEnabled`
+        try await Task.sleep(nanoseconds: 300)
 
         // Then
         #expect(viewModel.notificationsEnabled == true)

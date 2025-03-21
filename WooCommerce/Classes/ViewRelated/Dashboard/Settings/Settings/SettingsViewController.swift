cc975b0d581e7a8123c1123da385a4b0cf602f38
Huong Do <hi.huongdo@gmail.com>
2025-03-21 10:57:16 +0700

    Configure connectivity row in settings

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Settings/SettingsViewController.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Settings/SettingsViewController.swift
@@ -144,6 +144,8 @@ private extension SettingsViewController {
             configureWooCommmerceDetails(cell: cell)
         case let cell as BasicTableViewCell where row == .domain:
             configureDomain(cell: cell)
+        case let cell as BasicTableViewCell where row == .connectivity:
+            configureConnectivity(cell: cell)
         case let cell as BasicTableViewCell where row == .installJetpack:
             configureInstallJetpack(cell: cell)
         case let cell as BasicTableViewCell where row == .themes:
@@ -211,6 +213,12 @@ private extension SettingsViewController {
         cell.accessoryType = .disclosureIndicator
         cell.selectionStyle = .default
         cell.textLabel?.text = Localization.domain
+    }
+
+    func configureConnectivity(cell: BasicTableViewCell) {
+        cell.accessoryType = .disclosureIndicator
+        cell.selectionStyle = .default
+        cell.textLabel?.text = Localization.connectivity
     }
 
     func configureInstallJetpack(cell: BasicTableViewCell) {
@@ -489,6 +497,11 @@ private extension SettingsViewController {
         show(controller, sender: self)
     }
 
+    func showConnectivityTool() {
+        let controller = ConnectivityToolViewController()
+        show(controller, sender: self)
+    }
+
     func deviceSettingsWasPressed() {
         guard let targetURL = URL(string: UIApplication.openSettingsURLString) else {
             return
@@ -668,6 +681,8 @@ extension SettingsViewController: UITableViewDelegate {
             showThemeSettings()
         case .notifications:
             showNotificationSettings()
+        case .connectivity:
+            showConnectivityTool()
         default:
             break
         }
@@ -727,6 +742,7 @@ extension SettingsViewController {
         case installJetpack
         case storeName
         case themes
+        case connectivity
 
         // Help & Feedback
         case support
@@ -772,7 +788,7 @@ extension SettingsViewController {
                 return HostingTableViewCell<PluginDetailsRowContent>.self
             case .support:
                 return BasicTableViewCell.self
-            case .domain:
+            case .domain, .connectivity:
                 return BasicTableViewCell.self
             case .installJetpack:
                 return BasicTableViewCell.self
@@ -846,6 +862,12 @@ private extension SettingsViewController {
         static let domain = NSLocalizedString(
             "Domains",
             comment: "Navigates to domain settings screen."
+        )
+
+        static let connectivity = NSLocalizedString(
+            "settingsViewController.connectivity",
+            value: "Troubleshoot Connection",
+            comment: "Navigates to connectivity test screen."
         )
 
         static let installJetpack = NSLocalizedString(

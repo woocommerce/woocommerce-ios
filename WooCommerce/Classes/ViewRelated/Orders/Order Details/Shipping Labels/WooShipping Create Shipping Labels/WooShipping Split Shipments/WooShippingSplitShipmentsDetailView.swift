8c7a83dbb14c269c5165ba21cad70d47a6f09a1c
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2025-03-23 16:16:52 +0530

    Show shipment items in split shipment flow.

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/WooShipping Split Shipments/WooShippingSplitShipmentsDetailView.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/WooShipping Split Shipments/WooShippingSplitShipmentsDetailView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Yosemite
 
 struct WooShippingSplitShipmentsDetailView: View {
     @Environment(\.dismiss) private var dismiss
@@ -17,11 +18,9 @@ struct WooShippingSplitShipmentsDetailView: View {
                             .foregroundStyle(Color(.textSubtle))
                     }
 
-                    VStack {
-                        ForEach(viewModel.items) { item in
-                            WooShippingItemRow(viewModel: item)
-                                .padding()
-                                .roundedBorder(cornerRadius: Layout.borderCornerRadius, lineColor: Color(.separator), lineWidth: Layout.borderWidth)
+                    VStack(spacing: Layout.verticalSpacing) {
+                        ForEach(viewModel.shipmentCardViewModels) { item in
+                            CollapsibleShipmentCard(viewModel: item)
                         }
                     }
                 }
@@ -32,7 +31,7 @@ struct WooShippingSplitShipmentsDetailView: View {
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
                     Button(Localization.selectAll) {
-
+                        viewModel.selectAll()
                     }
                 }
                 ToolbarItem(placement: .confirmationAction) {
@@ -50,6 +49,7 @@ private extension WooShippingSplitShipmentsDetailView {
         static let contentPadding: CGFloat = 16
         static let borderCornerRadius: CGFloat = 8
         static let borderWidth: CGFloat = 0.5
+        static let verticalSpacing: CGFloat = 8
     }
     enum Localization {
         static let title = NSLocalizedString(
@@ -73,6 +73,16 @@ private extension WooShippingSplitShipmentsDetailView {
 #if DEBUG
 #Preview {
     WooShippingSplitShipmentsDetailView(viewModel: WooShippingSplitShipmentsViewModel(order: ShippingLabelSampleData.sampleOrder(),
-                                                                                       config: ShippingLabelSampleData.sampleWooShippingConfig()))
+                                                                                      config: ShippingLabelSampleData.sampleWooShippingConfig(),
+                                                                                      items: [ShippingLabelPackageItem(productOrVariationID: 1,
+                                                                                                                       name: "Shirt",
+                                                                                                                       weight: 0.5,
+                                                                                                                       quantity: 2,
+                                                                                                                       value: 9.99,
+                                                                                                                       dimensions: ProductDimensions(length: "",
+                                                                                                                                                     width: "",
+                                                                                                                                                     height: ""),
+                                                                                                                       attributes: [],
+                                                                                                                       imageURL: nil)]))
 }
 #endif

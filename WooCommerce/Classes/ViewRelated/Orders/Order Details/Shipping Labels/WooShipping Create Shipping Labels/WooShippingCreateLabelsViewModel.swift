3577da620e60c6497f264a85b896857188e57fea
Huong Do <hi.huongdo@gmail.com>
2025-03-24 12:33:40 +0700

    Observe HAZMAT change to send a notice with undo button

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/WooShippingCreateLabelsViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/WooShippingCreateLabelsViewModel.swift
@@ -22,6 +22,7 @@ final class WooShippingCreateLabelsViewModel: ObservableObject {
     private var debounceDuration: Double = 1
 
     @Published var hazmatCategory: ShippingLabelHazmatCategory?
+    @Published var hazmatNotice: Notice?
 
     @Published var labelPurchaseErrorNotice: Notice?
 
@@ -228,6 +229,7 @@ final class WooShippingCreateLabelsViewModel: ObservableObject {
         observeSelectedPackage()
         observeForLabelRates()
         observeForCustomsForm()
+        observeHAZMATChanges()
         Task {
             await loadRequiredData()
         }
@@ -569,6 +571,21 @@ private extension WooShippingCreateLabelsViewModel {
             .store(in: &subscriptions)
     }
 
+    func observeHAZMATChanges() {
+        $hazmatCategory
+            .scan((nil, nil)) { (previous: (current: ShippingLabelHazmatCategory?, previous: ShippingLabelHazmatCategory?),
+                                 newValue: ShippingLabelHazmatCategory?) in
+                return (current: newValue, previous: previous.current)
+            }
+            .map { [weak self] (newValue, oldValue) in
+                let noticeTitle = newValue != nil ? Localization.hazmatSet : Localization.hazmatRemoved
+                return Notice(title: noticeTitle, actionTitle: Localization.undo, actionHandler: {
+                    self?.hazmatCategory = oldValue
+                })
+            }
+            .assign(to: &$hazmatNotice)
+    }
+
     func observeForCustomsForm() {
         $selectedOriginAddress.combineLatest($destinationAddress)
             .map { (originAddress, destinationAddress) -> Bool in
@@ -695,6 +712,24 @@ private extension WooShippingCreateLabelsViewModel {
                                                    value: "Retry",
                                                    comment: "Button to retry label purchase when an error occurs")
         }
+
+        static let hazmatSet = NSLocalizedString(
+            "wooShipping.createLabels.hazmatSet",
+            value: "Hazardous materials category set",
+            comment: "Notice when a hazardous materials category is set on the shipping label creation screen"
+        )
+
+        static let hazmatRemoved = NSLocalizedString(
+            "wooShipping.createLabels.hazmatRemoved",
+            value: "Remove hazardous materials category",
+            comment: "Notice when a hazardous materials category is removed on the shipping label creation screen"
+        )
+
+        static let undo = NSLocalizedString(
+            "wooShipping.createLabels.undo",
+            value: "Undo",
+            comment: "Button to undo a change on the shipping label creation screen"
+        )
     }
 }
 

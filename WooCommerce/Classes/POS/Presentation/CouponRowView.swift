c2a47e7e9a11bbb2b7887eeca10eb047453acff1
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2025-03-28 14:14:32 +0200

    Add dummy UI for displaying coupon state and discount in CouponRowView

--- WooCommerce/Classes/POS/Presentation/CouponRowView.swift
+++ WooCommerce/Classes/POS/Presentation/CouponRowView.swift
@@ -2,12 +2,14 @@ import SwiftUI
 
 struct CouponRowView: View {
     private let couponItem: CartCouponItem
+    private let couponRowState: CouponRowState?
     private let onItemRemoveTapped: (() -> Void)?
 
     @ScaledMetric private var scale: CGFloat = 1.0
 
-    init(couponItem: CartCouponItem, onItemRemoveTapped: (() -> Void)? = nil) {
+    init(couponItem: CartCouponItem, couponRowState: CouponRowState? = nil, onItemRemoveTapped: (() -> Void)? = nil) {
         self.couponItem = couponItem
+        self.couponRowState = couponRowState
         self.onItemRemoveTapped = onItemRemoveTapped
     }
 
@@ -22,11 +24,29 @@ struct CouponRowView: View {
                 }
                 .frame(width: Constants.couponCardSize, height: Constants.couponCardSize)
 
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: Constants.itemTitleAndPriceSpacing * (1 / scale)) {
                 Text(couponItem.code)
                     .foregroundColor(PointOfSaleItemListCardConstants.titleColor)
                     .font(Constants.itemTitleFont)
+
+                switch couponRowState {
+                case .valid(let couponTotal):
+                    Text("-\(couponTotal.total)")
+                        .foregroundColor(PointOfSaleItemListCardConstants.detailColor)
+                        .font(Constants.itemPriceFont)
+                case .idle, .none:
+                    EmptyView()
+                case .invalid:
+                    Text("Invalid coupon")
+                        .foregroundColor(PointOfSaleItemListCardConstants.detailColor)
+                        .font(Constants.itemPriceFont)
+                case .validating:
+                    Text("Validating...")
+                        .foregroundColor(PointOfSaleItemListCardConstants.detailColor)
+                        .font(Constants.itemPriceFont)
+                }
             }
+            .animation(.default, value: couponRowState)
             .frame(maxWidth: .infinity, alignment: .leading)
 
             if let onItemRemoveTapped {
@@ -54,12 +74,14 @@ private extension CouponRowView {
         static let horizontalElementSpacing: CGFloat = POSSpacing.medium
         static let cardContentHorizontalPadding: CGFloat = POSPadding.medium
         static let itemTitleFont: POSFontStyle = .posBodySmallBold
+        static let itemTitleAndPriceSpacing: CGFloat = POSSpacing.xSmall
+        static let itemPriceFont: POSFontStyle = .posBodySmallRegular()
     }
 }
 
 #if DEBUG
 @available(iOS 17.0, *)
 #Preview(traits: .sizeThatFitsLayout) {
-    CouponRowView(couponItem: CartCouponItem(id: UUID(), code: "10-Discount")) {}
+    CouponRowView(couponItem: CartCouponItem(id: UUID(), code: "10-Discount"), couponRowState: .idle) {}
 }
 #endif

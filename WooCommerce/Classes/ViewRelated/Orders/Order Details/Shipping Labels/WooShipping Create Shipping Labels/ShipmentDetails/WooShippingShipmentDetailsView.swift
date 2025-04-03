06271467394b04a566be15c052adc7e5ee6dc8c6
Huong Do <hi.huongdo@gmail.com>
2025-04-03 15:23:44 +0700

    Add new files for shipment details

---
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/ShipmentDetails/WooShippingShipmentDetailsView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct WooShippingShipmentDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WooShippingShipmentDetailsView()
+}

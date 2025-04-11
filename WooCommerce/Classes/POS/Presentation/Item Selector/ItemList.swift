fc236b8dfd644baeca020711dee1bc4bf0a8531a
Josh Heald <joshheald@users.noreply.github.com>
2025-04-11 09:07:06 +0100

    Fix whitespace

--- WooCommerce/Classes/POS/Presentation/Item Selector/ItemList.swift
+++ WooCommerce/Classes/POS/Presentation/Item Selector/ItemList.swift
@@ -61,7 +61,6 @@ struct ItemList<HeaderView: View>: View {
                case let .variableParentProduct(parentProduct) = activeItem {
                 // This always uses the non-search itemsStack, otherwise it will have the search term and not work properly
                 // This is a temporary fix until we tidy up the stack selection, as it means non-products child lists won't work.
-                
                 NavigationLink(
                     destination: ChildItemList(parentItem: activeItem, title: parentProduct.name, itemsStack: posModel.itemsViewState.itemsStack),
                     isActive: Binding(

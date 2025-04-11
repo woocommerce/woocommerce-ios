ab9b9ff55e5dcc874e1a20894e75b6dd079a2ba8
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2025-04-11 12:35:01 +0300

    Confirm enableCoupons() loads items in the aggregate model tests

--- WooCommerce/WooCommerceTests/POS/Mocks/MockPointOfSaleCouponsController.swift
+++ WooCommerce/WooCommerceTests/POS/Mocks/MockPointOfSaleCouponsController.swift
@@ -2,14 +2,23 @@
 
 @available(iOS 17.0, *)
 final class MockPointOfSaleCouponsController: PointOfSaleCouponsControllerProtocol {
-    func enableCoupons() async { }
+    var enableCouponsResult: Bool = false
+    var loadItemsCalled = false
+    var loadItemsBase: ItemListBaseItem?
+
+    func enableCoupons() async -> Bool {
+        return enableCouponsResult
+    }
 
     var itemsViewState: ItemsViewState = .init(containerState: .content,
                                                itemsStack: .init(root: .empty, itemStates: [:]))
 
-    func loadItems(base: ItemListBaseItem) async { }
+    func loadItems(base: ItemListBaseItem) async {
+        loadItemsCalled = true
+        loadItemsBase = base
+    }
 
     func refreshItems(base: ItemListBaseItem) async { }
-
     func loadNextItems(base: ItemListBaseItem) async { }
+    func searchItems(query: String) async { }
 }

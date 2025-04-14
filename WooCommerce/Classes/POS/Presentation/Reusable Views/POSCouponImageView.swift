154507845fee8c782f5da4519573fc1b54e9e8c4
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2025-04-14 20:53:05 +0300

    Update Coupon Cards thumbnail colors

--- WooCommerce/Classes/POS/Presentation/Reusable Views/POSCouponImageView.swift
+++ WooCommerce/Classes/POS/Presentation/Reusable Views/POSCouponImageView.swift
@@ -1,19 +1,49 @@
 import SwiftUI
 
+enum POSCouponImageState {
+    case success
+    case error
+    case normal
+}
+
 struct POSCouponImageView: View {
     private let size: CGFloat
+    private let state: POSCouponImageState
 
-    init(size: CGFloat) {
+    init(size: CGFloat, state: POSCouponImageState = .normal) {
         self.size = size
+        self.state = state
+    }
+
+    private var foregroundColor: Color {
+        switch state {
+        case .success:
+            return .posSuccess
+        case .error:
+            return .posError
+        case .normal:
+            return .posSurfaceDim
+        }
+    }
+
+    private var tagColor: Color {
+        switch state {
+        case .success:
+            return .posOnSuccess
+        case .error:
+            return .posOnError
+        case .normal:
+            return .posOnSurfaceVariantLowest
+        }
     }
 
     var body: some View {
         Rectangle()
-            .foregroundColor(.posSurfaceDim)
+            .foregroundColor(foregroundColor)
             .overlay {
                 Image(systemName: "tag")
                     .font(.posButtonSymbolMedium)
-                    .foregroundColor(.posOnSurfaceVariantLowest)
+                    .foregroundColor(tagColor)
             }
             .frame(width: size)
             .frame(minHeight: size)

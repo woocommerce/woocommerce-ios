2071d0fd3b53d7928699bbe215e9cf522ac160a7
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2025-04-18 11:19:41 +0300

    Revert isNotEmpty -> isEmpty change

--- WooCommerce/Classes/POS/Presentation/CartView.swift
+++ WooCommerce/Classes/POS/Presentation/CartView.swift
@@ -26,7 +26,7 @@ struct CartView: View {
     @State private var shouldShowItemImages: Bool = false
 
     private var shouldShowCoupons: Bool {
-        ServiceLocator.featureFlagService.isFeatureFlagEnabled(.enableCouponsInPointOfSale) && !posModel.cart.coupons.isEmpty
+        ServiceLocator.featureFlagService.isFeatureFlagEnabled(.enableCouponsInPointOfSale) && posModel.cart.coupons.isNotEmpty
     }
 
     var body: some View {

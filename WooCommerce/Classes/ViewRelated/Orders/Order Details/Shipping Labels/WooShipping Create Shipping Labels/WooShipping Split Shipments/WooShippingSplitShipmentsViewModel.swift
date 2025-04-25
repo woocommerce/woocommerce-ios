3b6fa0c62a004d7035a38688a3c20c56f0c3b375
RafaelKayumov <justpoc@gmail.com>
2025-04-25 15:06:48 +0300

    Disable shipment removal option if its a last unfulfilled one

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/WooShipping Split Shipments/WooShippingSplitShipmentsViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/WooShipping Split Shipments/WooShippingSplitShipmentsViewModel.swift
@@ -298,6 +298,17 @@ final class WooShippingSplitShipmentsViewModel: ObservableObject {
         return String.localizedStringWithFormat(Localization.shipmentFormat, index + 1)
     }
 
+    /// Determines if a shipment's delete option should be disabled.
+    /// A shipment's delete option should be disabled if:
+    /// 1. The shipment is already purchased
+    /// 2. The view model is currently saving shipment info
+    /// 3. The shipment is the last unfulfilled shipment
+    func isShipmentDeleteOptionDisabled(for shipment: Shipment) -> Bool {
+        let unfulfilledShipments = shipments.filter { !$0.isPurchased }
+        let isLastUnfulfilled = unfulfilledShipments.count == 1 && unfulfilledShipments.first == shipment
+        return shipment.isPurchased || isSavingShipmentInfo || isLastUnfulfilled
+    }
+
     func shipmentsToMerge(for shipment: Shipment) -> [Shipment] {
         shipments.filter { $0 != shipment }
     }

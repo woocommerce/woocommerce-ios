28342f108b00e242dadffbb8abe5240b2d5dcd2f
Jaclyn Chen <watertranquil@gmail.com>
2025-04-28 15:12:30 +0800

    Add/update POS order service and controller unit tests now that the core logic is moved to the service.

--- Yosemite/YosemiteTests/Mocks/MockPOSOrdersRemote.swift
+++ Yosemite/YosemiteTests/Mocks/MockPOSOrdersRemote.swift
@@ -3,13 +3,23 @@ import Networking
 final class MockPOSOrdersRemote: POSOrdersRemoteProtocol {
     var updatePOSOrderCalled: Bool = false
     var spyUpdatePOSOrder: Order?
+    var spyUpdatePOSOrderFields: [OrdersRemote.UpdateOrderField]?
+    var spyUpdatePOSOrderCashPaymentChangeDueAmount: String?
+    var updatePOSOrderResult: Result<Order, Error> = .success(Order.fake())
     func updatePOSOrder(siteID: Int64,
                         order: Order,
                         cashPaymentChangeDueAmount: String?,
                         fields: [OrdersRemote.UpdateOrderField]) async throws -> Order {
         updatePOSOrderCalled = true
         spyUpdatePOSOrder = order
-        return Order.fake()
+        spyUpdatePOSOrderFields = fields
+        spyUpdatePOSOrderCashPaymentChangeDueAmount = cashPaymentChangeDueAmount
+        switch updatePOSOrderResult {
+        case .success(let updatedOrder):
+            return updatedOrder
+        case .failure(let error):
+            throw error
+        }
     }
 
     var createPOSOrderCalled: Bool = false

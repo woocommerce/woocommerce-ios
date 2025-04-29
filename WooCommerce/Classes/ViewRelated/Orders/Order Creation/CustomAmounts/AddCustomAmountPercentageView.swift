6d52699cd15ba35a42d3ab24bbf412b6ca604716
Josh Heald <joshheald@users.noreply.github.com>
2025-04-29 16:34:46 +0100

    Remove broken attempt to localise percentages
    This was close to working… but if you typed 40.001, the two zeroes after the decimal wouldn’t show up until the 1 was typed. We can handle this separately.

--- WooCommerce/Classes/ViewRelated/Orders/Order Creation/CustomAmounts/AddCustomAmountPercentageView.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Creation/CustomAmounts/AddCustomAmountPercentageView.swift
@@ -59,15 +59,6 @@ private extension AddCustomAmountPercentageView {
         @Binding var text: String
         var onChangeText: (String) -> (Void)
 
-        private let percentFormatter: NumberFormatter = {
-            let formatter = NumberFormatter()
-            formatter.numberStyle = .percent
-            formatter.maximumFractionDigits = 3
-            formatter.minimumFractionDigits = 0
-            formatter.multiplier = 1
-            return formatter
-        }()
-
         var body: some View {
             ZStack {
                 TextField("",
@@ -80,7 +71,7 @@ private extension AddCustomAmountPercentageView {
                 .keyboardType(.decimalPad)
                 .opacity(0)
 
-                Text(formatPercentage(text))
+                Text(text + "%")
                     .font(.system(size: Layout.percentageFontSize(scale: scale), weight: .bold))
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .foregroundColor(text.isEmpty ? Color(.textSubtle) : Color(.text))
@@ -94,14 +85,6 @@ private extension AddCustomAmountPercentageView {
                     }
             }
             .fixedSize(horizontal: false, vertical: true)
-        }
-
-        private func formatPercentage(_ text: String) -> String {
-            guard !text.isEmpty,
-                  let number = NumberFormatter.double(from: text) else {
-                return "0%"
-            }
-            return percentFormatter.string(from: NSNumber(value: number)) ?? "\(text)%"
         }
     }
 }

0f03ace80798cb5df95e563f687cd379e5c5633c
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2025-05-02 17:20:45 +0200

    Adapt lines and frame height when is accessibility size

--- WooCommerce/Classes/POS/Presentation/Item Selector/SimpleProductCardView.swift
+++ WooCommerce/Classes/POS/Presentation/Item Selector/SimpleProductCardView.swift
@@ -24,6 +24,7 @@ struct SimpleProductCardView: View {
 
             VStack(alignment: .leading, spacing: Constants.textSpacing) {
                 Text(product.name)
+                    .lineLimit(dynamicTypeSize.isAccessibilitySize ? nil : 2)
                     .foregroundStyle(Constants.titleColor)
                     .multilineTextAlignment(.leading)
                     .font(Constants.itemTitleFont)
@@ -37,7 +38,7 @@ struct SimpleProductCardView: View {
             .padding(.vertical, Constants.verticalTextPadding * (1 / scale))
             Spacer()
         }
-        .frame(maxWidth: .infinity)
+        .frame(maxWidth: .infinity, idealHeight: dynamicTypeSize.isAccessibilitySize ? nil : dimension)
         .background(Constants.backgroundColor)
         .posItemCardBorderStyles()
     }

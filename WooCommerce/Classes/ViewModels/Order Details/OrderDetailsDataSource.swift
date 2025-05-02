949f37c9637285d795b9b1f6437431738bb2db63
iamgabrielma <gabrielmaldonado.art@gmail.com>
2025-05-02 13:26:53 +0700

    restore unnecessary changes

--- WooCommerce/Classes/ViewModels/Order Details/OrderDetailsDataSource.swift
+++ WooCommerce/Classes/ViewModels/Order Details/OrderDetailsDataSource.swift
@@ -1211,55 +1211,84 @@ extension OrderDetailsDataSource {
         let products: Section? = {
             if items.isEmpty { return nil }
             let aggregateOrderItemCount = aggregateOrderItems.count
-            guard aggregateOrderItemCount > 0 else { return nil }
+            guard aggregateOrderItemCount > 0 else {
+                return nil
+            }
             var rows: [Row] = Array(repeating: .aggregateOrderItem, count: aggregateOrderItemCount)
 
             switch (shouldShowShippingLabelCreation, isProcessingStatus, isRefundedStatus, isEligibleForPayment) {
             case (true, false, false, false):
+                // Order completed and eligible for shipping label creation:
                 rows.append(.shippingLabelCreateButton)
                 rows.append(.shippingLabelCreationInfo(showsSeparator: false))
             case (true, true, false, false):
+                // Order processing shippable:
                 rows.append(.shippingLabelCreateButton)
                 rows.append(.markCompleteButton(style: .secondary, showsBottomSpacing: false))
                 rows.append(.shippingLabelCreationInfo(showsSeparator: false))
             case (false, true, false, false):
+                // Order processing digital:
                 rows.append(.markCompleteButton(style: .primary, showsBottomSpacing: true))
             default:
                 break
             }
 
-            guard rows.isNotEmpty else { return nil }
+            if rows.count == 0 {
+                return nil
+            }
 
-            let headerStyle: Section.HeaderStyle = shouldAllowRecreatingShippingLabels
-                ? .actionablePrimary(actionConfig: PrimarySectionHeaderView.ActionConfiguration(image: .moreImage) { [weak self] sourceView in
+            let headerStyle: Section.HeaderStyle
+            if shouldAllowRecreatingShippingLabels {
+                let headerActionConfig = PrimarySectionHeaderView.ActionConfiguration(image: .moreImage) { [weak self] sourceView in
                     self?.onProductsMoreMenuTapped?(sourceView)
-                  })
-                : .twoColumn
-
-            return Section(category: .products, title: Title.products, rows: rows, headerStyle: headerStyle)
+                }
+                headerStyle = .actionablePrimary(actionConfig: headerActionConfig)
+            } else {
+                headerStyle = .twoColumn
+            }
+            return Section(category: .products,
+                           title: Title.products,
+                           rows: rows,
+                           headerStyle: headerStyle)
         }()
 
         let customAmountsSection: Section? = {
-            guard customAmounts.isNotEmpty else { return nil }
-            return Section(category: .customAmounts, title: Title.customAmounts, rows: Array(repeating: .customAmount, count: customAmounts.count))
+            guard customAmounts.isNotEmpty else {
+                return nil
+            }
+
+            return Section(category: .customAmounts,
+                          title: Title.customAmounts,
+                          rows: Array(repeating: .customAmount, count: customAmounts.count))
         }()
 
         let customFields: Section? = {
             Section(category: .customFields, row: .customFields)
         }()
 
-        let refundedProducts: Section? = refundedProductsCount > 0
-            ? Section(category: .refundedProducts, title: Title.refundedProducts, row: .refundedProducts)
-            : nil
+        let refundedProducts: Section? = {
+            // Refunds on
+            guard refundedProductsCount > 0 else {
+                return nil
+            }
+
+            let row: Row = .refundedProducts
+
+            return Section(category: .refundedProducts, title: Title.refundedProducts, row: row)
+        }()
 
         let installWCShipSection: Section? = {
-            guard shouldAllowWCShipInstallation else { return nil }
+            guard shouldAllowWCShipInstallation else {
+                return nil
+            }
             let rows: [Row] = [.installWCShip]
             return Section(category: .installWCShip, title: nil, rows: rows)
         }()
 
         let shippingLabelSections: [Section] = {
-            guard shippingLabels.isNotEmpty else { return [] }
+            guard shippingLabels.isNotEmpty else {
+                return []
+            }
             let sections = shippingLabels.enumerated().map { index, shippingLabel -> Section in
                 let title = String.localizedStringWithFormat(Title.shippingLabelPackageFormat, index + 1)
                 let isRefunded = shippingLabel.refund != nil
@@ -1281,39 +1310,64 @@ extension OrderDetailsDataSource {
         }()
 
         let shippingLinesSection: Section? = {
-            guard shippingLines.count > 0 else { return nil }
+            guard shippingLines.count > 0 else {
+                return nil
+            }
             return Section(category: .shippingLines, title: Title.shippingLines, rows: Array(repeating: .shippingLine, count: shippingLines.count))
         }()
 
         let subscriptions: Section? = {
-            guard orderSubscriptions.isNotEmpty else { return nil }
+            // Subscriptions section is hidden if there are no subscriptions for the order.
+            guard orderSubscriptions.isNotEmpty else {
+                return nil
+            }
             let rows: [Row] = Array(repeating: .subscriptions, count: orderSubscriptions.count)
             return Section(category: .subscriptions, title: Title.subscriptions, rows: rows)
         }()
 
         let giftCards: Section? = {
-            guard shouldShowGiftCards else { return nil }
+            guard shouldShowGiftCards else {
+                return nil
+            }
             let rows: [Row] = Array(repeating: .giftCards, count: appliedGiftCards.count)
             return Section(category: .giftCards, title: Title.giftCards, rows: rows)
         }()
 
         let tracking: Section? = {
-            guard shippingLabels.nonRefunded.isEmpty else { return nil }
-            guard orderTracking.count > 0 else { return nil }
+            // Tracking section is hidden if there are non-empty non-refunded shipping labels.
+            guard shippingLabels.nonRefunded.isEmpty else {
+                return nil
+            }
+
+            guard orderTracking.count > 0 else {
+                return nil
+            }
             let rows: [Row] = Array(repeating: .tracking, count: orderTracking.count)
             return Section(category: .tracking, title: Title.tracking, rows: rows)
         }()
 
         let addTracking: Section? = {
-            guard shippingLabels.nonRefunded.isEmpty else { return nil }
-            guard trackingIsReachable else { return nil }
-            let title: String? = orderTracking.count == 0
-                ? NSLocalizedString(
+            // Add tracking section is hidden if there are non-empty non-refunded shipping labels.
+            guard shippingLabels.nonRefunded.isEmpty else {
+                return nil
+            }
+
+            // Hide the section if the shipment
+            // tracking plugin is not installed
+            guard trackingIsReachable else {
+                return nil
+            }
+
+            let title: String?
+            if orderTracking.count == 0 {
+                title = NSLocalizedString(
                     "orderDetails.addTrackingRow.title",
                     value: "Optional Tracking Information",
                     comment: "Title for the row to add tracking information."
                 )
-                : nil
+            } else {
+                title = nil
+            }
             let row = Row.trackingAdd
             return Section(category: .addTracking, title: title, rightTitle: nil, rows: [row])
         }()
@@ -1326,15 +1380,38 @@ extension OrderDetailsDataSource {
         let attribution: Section? = {
             let rows: [Row] = {
                 var rows: [Row] = [.attributionOrigin]
-                guard let orderAttributionInfo = order.attributionInfo else { return rows }
-                if let _ = orderAttributionInfo.sourceType { rows.append(.attributionSourceType) }
-                if let _ = orderAttributionInfo.campaign { rows.append(.attributionCampaign) }
-                if let _ = orderAttributionInfo.source { rows.append(.attributionSource) }
-                if let _ = orderAttributionInfo.medium { rows.append(.attributionMedium) }
-                if let _ = orderAttributionInfo.deviceType { rows.append(.attributionDeviceType) }
-                if let _ = orderAttributionInfo.sessionPageViews { rows.append(.attributionSessionPageViews) }
+
+                guard let orderAttributionInfo = order.attributionInfo else {
+                    return rows
+                }
+
+                if let _ = orderAttributionInfo.sourceType {
+                    rows.append(.attributionSourceType)
+                }
+
+                if let _ = orderAttributionInfo.campaign {
+                    rows.append(.attributionCampaign)
+                }
+
+                if let _ = orderAttributionInfo.source {
+                    rows.append(.attributionSource)
+                }
+
+                if let _ = orderAttributionInfo.medium {
+                    rows.append(.attributionMedium)
+                }
+
+                if let _ = orderAttributionInfo.deviceType {
+                    rows.append(.attributionDeviceType)
+                }
+
+                if let _ = orderAttributionInfo.sessionPageViews {
+                    rows.append(.attributionSessionPageViews)
+                }
+
                 return rows
             }()
+
             return Section(category: .attribution, title: Title.orderAttribution, rows: rows)
         }()
 

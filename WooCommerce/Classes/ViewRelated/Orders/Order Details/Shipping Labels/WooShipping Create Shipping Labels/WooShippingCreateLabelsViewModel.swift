2c9e15edf8ab40e229866ee8cd3c706a85bdf991
Huong Do <hi.huongdo@gmail.com>
2025-05-06 19:20:41 +0700

    Remove extra line

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/WooShippingCreateLabelsViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/WooShippingCreateLabelsViewModel.swift
@@ -564,7 +564,6 @@ private extension WooShippingCreateLabelsViewModel {
                 shipments[selectedIndex].isPurchased == false
             }
             .assign(to: &$shouldShowNotices)
-
     }
 
     /// Gets the destination address as a `ShippingLabelAddress`.

6893fe93499f813406a163ff57e87cfc8e976105
Huong Do <hi.huongdo@gmail.com>
2025-05-08 10:25:20 +0700

    Update WooShippingRefundView with view model

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/Refund/WooShippingRefundView.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/WooShipping Create Shipping Labels/Refund/WooShippingRefundView.swift
@@ -1,17 +1,57 @@
 import SwiftUI
 
 struct WooShippingRefundView: View {
+    let viewModel: WooShippingRefundViewModel
+
     var body: some View {
-        NavigationStack {
-            VStack {
-                Text("")
+        ScrollableVStack(alignment: .leading, spacing: 8) {
+            HStack {
+                Button(Localization.cancelButton) {
+                    // TODO
+                }
+                Spacer()
             }
+
+            Text(Localization.title)
+                .font(.title2)
+                .bold()
+                .padding(.vertical, 16)
+
+            Text(Localization.description)
+
+            Text(Localization.purchaseDate).bold() +
+            Text(" ") +
+            Text(viewModel.formattedPurchaseDate)
+
+            Text(Localization.refundAmount).bold() +
+            Text(" ") +
+            Text(viewModel.formattedRefundAmount)
+
+            Text(Localization.note)
+                .italic()
+            Spacer()
+        }
+        .multilineTextAlignment(.leading)
+        .safeAreaInset(edge: .bottom) {
+            VStack {
+                Button(String.localizedStringWithFormat(Localization.submitButton, "-" + viewModel.formattedRefundAmount)) {
+                    viewModel.submitRefundRequest()
+                }
+                .buttonStyle(PrimaryLoadingButtonStyle(isLoading: false))
+                .padding()
+            }
+            .background(Color(.systemBackground))
         }
     }
 }
 
 private extension WooShippingRefundView {
     enum Localization {
+        static let title = NSLocalizedString(
+            "wooShippingRefundView.title",
+            value: "Request a shipping label refund",
+            comment: "title on the Request shipping label refund view"
+        )
         static let description = NSLocalizedString(
             "wooShippingRefundView.description",
             value: "Request a refund for your unused shipping label. " +
@@ -21,15 +61,13 @@ private extension WooShippingRefundView {
         )
         static let purchaseDate = NSLocalizedString(
             "wooShippingRefundView.purchaseDate",
-            value: "Purchase date: %1$@",
-            comment: "Purchase date label on the Request shipping label refund view. " +
-            "The placeholder is the date of the purchase. Reads as: 'Purchase date: Feb 19, 2025'"
+            value: "Purchase date:",
+            comment: "Purchase date label on the Request shipping label refund view."
         )
         static let refundAmount = NSLocalizedString(
             "wooShippingRefundView.refundAmount",
-            value: "Amount eligible for refund: %1$@",
-            comment: "Refund amount label on the Request shipping label refund view. " +
-            "The placeholder is the amount to be refunded. Reads as: 'Amount eligible for refund: $11.33'"
+            value: "Amount eligible for refund:",
+            comment: "Refund amount label on the Request shipping label refund view."
         )
         static let note = NSLocalizedString(
             "wooShippingRefundView.note",
@@ -37,9 +75,19 @@ private extension WooShippingRefundView {
             "shipping label and will not affect the order itself.",
             comment: "Note on the Request shipping label refund view"
         )
+        static let cancelButton = NSLocalizedString(
+            "wooShippingRefundView.cancelButton",
+            value: "Cancel",
+            comment: "Button to dismiss the Request shipping label refund view"
+        )
+        static let submitButton = NSLocalizedString(
+            "wooShippingRefundView.submitButton",
+            value: "Refund Label (%1$@)",
+            comment: "Button to submit refund request the Request shipping label refund view"
+        )
     }
 }
 
 #Preview {
-    WooShippingRefundView()
+    WooShippingRefundView(viewModel: .init(refundableAmount: 11.33, purchaseDate: Date()))
 }

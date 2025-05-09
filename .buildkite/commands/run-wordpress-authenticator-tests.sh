1b6266ef65861d0d40f664a981e4c91075672b3e
Ian Maia <ian.maia@automattic.com>
2025-05-09 18:30:06 +0200

    Extract WordPressAuthenticator Unit Tests to separate script

---
+++ .buildkite/commands/run-wordpress-authenticator-tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if .buildkite/commands/should-skip-job.sh --validation; then
+  message="Skipping WordPressAuthenticator Unit Tests - no relevant files changed"
+  echo "$message" | buildkite-agent annotate --style "info" --context "skip-wp-auth-tests"
+  echo "$message"
+  exit 0
+fi
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :cocoapods: Setting up Pods"
+install_cocoapods
+
+echo "--- :swift: Setting up Swift Packages"
+install_swiftpm_dependencies
+
+echo "--- 🧪 Testing"
+bundle exec fastlane run scan \
+  scheme:WordPressAuthenticator \
+  prelaunch_simulator:true \
+  device:'iPhone 16' 

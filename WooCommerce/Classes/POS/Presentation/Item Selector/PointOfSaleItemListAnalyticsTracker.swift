8d9cfab028516b88a31cc6c7f5d8697fc9b74452
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2025-05-20 14:17:48 +0300

    Cover the rest of the cases in PointOfSaleItemListAnalyticsTrackerTests

--- WooCommerce/Classes/POS/Presentation/Item Selector/PointOfSaleItemListAnalyticsTracker.swift
+++ WooCommerce/Classes/POS/Presentation/Item Selector/PointOfSaleItemListAnalyticsTracker.swift
@@ -3,36 +3,43 @@ import WooFoundation
 import enum Yosemite.POSItemType
 
 struct PointOfSaleItemListAnalyticsTracker {
-    let source: WooAnalyticsEvent.PointOfSale.Source
-    let sourceType: WooAnalyticsEvent.PointOfSale.SourceType
+    private let source: WooAnalyticsEvent.PointOfSale.Source
+    private let sourceType: WooAnalyticsEvent.PointOfSale.SourceType
+    private let analytics: Analytics
 
     init(
         source: WooAnalyticsEvent.PointOfSale.Source,
-        sourceType: WooAnalyticsEvent.PointOfSale.SourceType
+        sourceType: WooAnalyticsEvent.PointOfSale.SourceType,
+        analytics: Analytics = ServiceLocator.analytics
     ) {
         self.source = source
         self.sourceType = sourceType
+        self.analytics = analytics
     }
 
-    init(selectedItemListType: ItemListType, searchTerm: String) {
+    init(
+        selectedItemListType: ItemListType,
+        searchTerm: String,
+        analytics: Analytics = ServiceLocator.analytics
+    ) {
         switch selectedItemListType {
         case .products(search: false):
-            self.init(source: .product, sourceType: .list)
+            self.init(source: .product, sourceType: .list, analytics: analytics)
         case .coupons(search: false):
-            self.init(source: .coupon, sourceType: .list)
+            self.init(source: .coupon, sourceType: .list, analytics: analytics)
         case .products(search: true):
-            self.init(source: .product, sourceType: searchTerm.isEmpty ? .preSearch : .search)
+            self.init(source: .product, sourceType: searchTerm.isEmpty ? .preSearch : .search, analytics: analytics)
         case .coupons(search: true):
-            self.init(source: .coupon, sourceType: searchTerm.isEmpty ? .preSearch : .search)
+            self.init(source: .coupon, sourceType: searchTerm.isEmpty ? .preSearch : .search, analytics: analytics)
         }
     }
 
     func trackItemListSelected(itemListType: ItemListType) {
-        ServiceLocator.analytics.track(event: .PointOfSale.itemsHeaderTapped(itemListType: itemListType))
+        analytics.track(event: .PointOfSale.itemsHeaderTapped(itemListType: itemListType))
     }
 
     func trackNextPageWillLoad() {
-        ServiceLocator.analytics.track(
+        analytics.track(
             event: WooAnalyticsEvent.PointOfSale.itemsNextPageLoaded(
                 source: source,
                 sourceType: sourceType
@@ -41,7 +48,7 @@ struct PointOfSaleItemListAnalyticsTracker {
     }
 
     func trackRefresh() {
-        ServiceLocator.analytics.track(
+        analytics.track(
             event: WooAnalyticsEvent.PointOfSale.itemsPullToRefresh(
                 source: source,
                 sourceType: sourceType
@@ -50,6 +57,6 @@ struct PointOfSaleItemListAnalyticsTracker {
     }
 
     func trackSearchTapped(itemListType: ItemListType) {
-        ServiceLocator.analytics.track(event: .PointOfSale.searchButtonTapped(itemListType: itemListType))
+        analytics.track(event: .PointOfSale.searchButtonTapped(itemListType: itemListType))
     }
 }

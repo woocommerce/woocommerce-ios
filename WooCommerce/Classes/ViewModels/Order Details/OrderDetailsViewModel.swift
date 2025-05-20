a83c033edb2f489dd54ab7c23d8171c3beafb5eb
Huong Do <hi.huongdo@gmail.com>
2025-05-20 12:52:07 +0300

    Use plugin path to check for eligibility of shipping label creation

--- WooCommerce/Classes/ViewModels/Order Details/OrderDetailsViewModel.swift
+++ WooCommerce/Classes/ViewModels/Order Details/OrderDetailsViewModel.swift
@@ -796,11 +796,11 @@ extension OrderDetailsViewModel {
             return false
         }
 
-        guard await !isPluginActive(SitePlugin.SupportedPlugin.LegacyWCShip) else {
+        guard await !isPluginActive(pluginPath: SitePlugin.SupportedPluginPath.LegacyWCShip) else {
             return true
         }
 
-        return await isPluginActive(SitePlugin.SupportedPlugin.WooShipping)
+        return await isPluginActive(pluginPath: SitePlugin.SupportedPluginPath.WooShipping)
     }
 
     /// Checks if the Woo Shipping extension is active, with the minimum version required for its shipping label flow.
@@ -808,7 +808,7 @@ extension OrderDetailsViewModel {
     @MainActor
     func isWooShippingSupported() async -> Bool {
         guard featureFlagService.isFeatureFlagEnabled(.revampedShippingLabelCreation),
-              let plugin = await fetchPlugin(SitePlugin.SupportedPlugin.WooShipping) else {
+              let plugin = await fetchPluginByPath(SitePlugin.SupportedPluginPath.WooShipping) else {
             return false
         }
 
@@ -885,7 +885,7 @@ extension OrderDetailsViewModel {
     ///
     private func isPluginActive(_ pluginNames: [String], completion: @escaping (Bool) -> (Void)) {
         Task { @MainActor in
-            let plugin = await fetchPlugin(pluginNames)
+            let plugin = await fetchPluginByNames(pluginNames)
             completion(plugin?.active == true)
         }
     }
@@ -894,7 +894,7 @@ extension OrderDetailsViewModel {
     /// Additionally it logs to tracks if the plugin store is accessed without it being in sync so we can handle that edge-case if it happens recurrently.
     ///
     @MainActor
-    private func fetchPlugin(_ pluginNames: [String]) async -> SystemPlugin? {
+    private func fetchPluginByNames(_ pluginNames: [String]) async -> SystemPlugin? {
         guard arePluginsSynced() else {
             DDLogError("⚠️ SystemPlugins accessed without being in sync.")
             ServiceLocator.analytics.track(event: WooAnalyticsEvent.Orders.pluginsNotSyncedYet())
@@ -903,6 +903,24 @@ extension OrderDetailsViewModel {
 
         return await withCheckedContinuation { continuation in
             stores.dispatch(SystemStatusAction.fetchSystemPluginListWithNameList(siteID: order.siteID, systemPluginNameList: pluginNames, onCompletion: { plugin in
+                continuation.resume(returning: plugin)
+            }))
+        }
+    }
+
+    /// Fetches a plugin from storage, based on the provided plugin path.
+    /// Additionally it logs to tracks if the plugin store is accessed without it being in sync so we can handle that edge-case if it happens recurrently.
+    ///
+    @MainActor
+    private func fetchPluginByPath(_ path: String) async -> SystemPlugin? {
+        guard arePluginsSynced() else {
+            DDLogError("⚠️ SystemPlugins accessed without being in sync.")
+            ServiceLocator.analytics.track(event: WooAnalyticsEvent.Orders.pluginsNotSyncedYet())
+            return nil
+        }
+
+        return await withCheckedContinuation { continuation in
+            stores.dispatch(SystemStatusAction.fetchSystemPluginWithPath(siteID: order.siteID, pluginPath: path, onCompletion: { plugin in
                 continuation.resume(returning: plugin)
             }))
         }
@@ -944,6 +962,12 @@ private extension OrderDetailsViewModel {
                 continuation.resume(returning: isActive)
             }
         }
+    }
+
+    @MainActor
+    func isPluginActive(pluginPath: String) async -> Bool {
+        let plugin = await fetchPluginByPath(pluginPath)
+        return plugin?.active == true
     }
 }
 

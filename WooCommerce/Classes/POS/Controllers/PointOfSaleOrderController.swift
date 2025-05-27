d09a69d48b61e220bbd104267747bc4728a61170
Jaclyn Chen <watertranquil@gmail.com>
2025-05-27 15:18:37 +0800

    Replace manual dev/beta version checks with `includesDevAndBetaVersions` parameter in `VersionHelpers.isVersionSupported`.

--- WooCommerce/Classes/POS/Controllers/PointOfSaleOrderController.swift
+++ WooCommerce/Classes/POS/Controllers/PointOfSaleOrderController.swift
@@ -215,14 +215,10 @@ private extension PointOfSaleOrderController {
                     return continuation.resume(returning: false)
                 }
 
-                // Checking for concrete versions to cover dev and beta versions
-                if plugin.version.contains(minimumVersion) {
-                    return continuation.resume(returning: true)
-                }
-
                 // If plugin version is higher than minimum required version, mark as eligible
                 let isSupported = VersionHelpers.isVersionSupported(version: plugin.version,
-                                                                    minimumRequired: minimumVersion)
+                                                                    minimumRequired: minimumVersion,
+                                                                    includesDevAndBetaVersions: true)
                 continuation.resume(returning: isSupported)
             }
             stores.dispatch(action)

8f96f05ca7986ccea6584b8e6591ae44d1a63077
Gio Lodi <gio@mokacoding.com>
2025-06-03 16:52:02 +1000

    Leave a comment in empty Fakes codegenerated sources
    Also avoids a SwiftLint violation ;)
    
    See https://buildkite.com/automattic/woocommerce-ios/builds/30231/steps?jid=019733fa-da57-4d51-b063-9866c0d67157

--- Fakes/Fakes/WooFoundation.generated.swift
+++ Fakes/Fakes/WooFoundation.generated.swift
@@ -1,8 +1,4 @@
 // Generated using Sourcery 2.2.6 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 
-import Yosemite
-import Networking
-import Hardware
-import WooFoundation
-
+// Currently empty because none of the given sources conforms to GeneratedFakeable

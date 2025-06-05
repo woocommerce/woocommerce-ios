4ba8dafecd301a13767c232130234d87399daa6d
Josh Heald <joshheald@users.noreply.github.com>
2025-06-05 18:31:10 +0100

    Enable barcode scanning of variations in POS

--- Yosemite/Yosemite/PointOfSale/Items/PointOfSaleBarcodeScanService.swift
+++ Yosemite/Yosemite/PointOfSale/Items/PointOfSaleBarcodeScanService.swift
@@ -11,6 +11,8 @@ public protocol PointOfSaleBarcodeScanServiceProtocol {
 
 public enum PointOfSaleBarcodeScanError: Error, Equatable {
     case unknown
+    case noParentProductForVariation
+    case variationCouldNotBeConverted
 }
 
 /// Service for handling barcode scanning in Point of Sale
@@ -41,17 +43,68 @@ public final class PointOfSaleBarcodeScanService: PointOfSaleBarcodeScanServiceP
     /// - Returns: A POSItem if found, or throws an error
     public func getItem(barcode: String) async throws -> POSItem {
         do {
-            let product = try await productsRemote.fetchPOSProductByGlobalUniqueIdentifier(for: siteID, globalUniqueID: barcode)
-
-            let items = itemMapper.mapProductsToPOSItems(products: [product])
-
-            guard let item = items.first else {
-                throw PointOfSaleBarcodeScanError.unknown
-            }
-
-            return item
+            let productOrVariation = try await productsRemote.fetchPOSProductByGlobalUniqueIdentifier(for: siteID,
+                                                                                                      globalUniqueID: barcode)
+            return try await itemForScannedBarcodeProduct(productOrVariation)
         } catch {
             throw PointOfSaleBarcodeScanError.unknown
         }
+    }
+
+    private func itemForScannedBarcodeProduct(_ productOrVariation: POSProduct) async throws -> POSItem {
+        let items: [POSItem]
+        switch productOrVariation.productType {
+        case .simple:
+            let product = productOrVariation
+            items = itemMapper.mapProductsToPOSItems(products: [product])
+        case .custom("variation"):
+            let variation = try productOrVariation.toProductVariation()
+            let variableProduct = try await parentProductForVariation(variation)
+            items = itemMapper.mapVariationsToPOSItems(variations: [variation], parentProduct: variableProduct)
+        default:
+            throw PointOfSaleBarcodeScanError.unknown
+        }
+
+        guard let item = items.first else {
+            throw PointOfSaleBarcodeScanError.unknown
+        }
+        return item
+    }
+
+    private func parentProductForVariation(_ variation: POSProductVariation) async throws -> POSVariableParentProduct {
+        guard variation.productID != 0 else {
+            throw PointOfSaleBarcodeScanError.noParentProductForVariation
+        }
+
+        let parentProduct = try await productsRemote.fetchPOSProduct(for: siteID, productID: variation.productID)
+        let mappedProducts = itemMapper.mapProductsToPOSItems(products: [parentProduct])
+
+        guard let mappedProduct = mappedProducts.first,
+              case .variableParentProduct(let variableProduct) = mappedProduct else {
+            throw PointOfSaleBarcodeScanError.variationCouldNotBeConverted
+        }
+        return variableProduct
+    }
+}
+
+
+private extension POSProduct {
+    func toProductVariation() throws -> POSProductVariation {
+        POSProductVariation(
+            siteID: siteID,
+            productID: parentID,
+            productVariationID: productID,
+            attributes: try attributes.compactMap { try $0.toProductVariationAttribute() },
+            image: images.first,
+            sku: sku,
+            globalUniqueID: globalUniqueID,
+            price: price,
+            regularPrice: regularPrice,
+            salePrice: salePrice,
+            onSale: onSale,
+            downloadable: downloadable,
+            manageStock: manageStock,
+            stockQuantity: stockQuantity,
+            stockStatusKey: stockStatusKey)
     }
 }

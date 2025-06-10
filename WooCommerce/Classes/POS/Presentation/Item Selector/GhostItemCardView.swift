2d5d4e70eac331ebb7c27d8cbf8ee4f847dc12ea
Josh Heald <joshheald@users.noreply.github.com>
2025-06-10 16:31:20 +0100

    Update ghost card to enable use in the cart

--- WooCommerce/Classes/POS/Presentation/Item Selector/GhostItemCardView.swift
+++ WooCommerce/Classes/POS/Presentation/Item Selector/GhostItemCardView.swift
@@ -3,46 +3,94 @@ import SwiftUI
 struct GhostItemCardView: View {
     @ScaledMetric private var scale: CGFloat = 1.0
     @State private var viewWidth: CGFloat = 0.0
+    private let configuration: GhostItemCardViewConfiguration
+    private let accessory: AnyView?
 
     private var dimension: CGFloat {
         min(Constants.productCardSize * scale, Constants.maximumProductCardSize)
     }
 
+    init(configuration: GhostItemCardViewConfiguration = .itemList) {
+        self.configuration = configuration
+        self.accessory = nil
+    }
+
+    init<Accessory: View>(configuration: GhostItemCardViewConfiguration = .itemList,
+                         @ViewBuilder accessory: () -> Accessory) {
+        self.configuration = configuration
+        self.accessory = AnyView(accessory())
+    }
+
     var body: some View {
         HStack(alignment: .center, spacing: Constants.cardSpacing) {
-            Rectangle()
-                .frame(width: dimension, height: dimension)
-            VStack(alignment: .leading) {
-                Rectangle()
-                    .frame(width: viewWidth * 0.5, height: Layout.placeholderHeight * scale)
-                    .cornerRadius(Layout.cornerRadius)
-                Rectangle()
-                    .frame(width: viewWidth * 0.1, height: Layout.placeholderHeight * scale)
-                    .cornerRadius(Layout.cornerRadius)
-            }
-            .padding(.horizontal, Constants.horizontalTextPadding)
+            placeholders
+                .foregroundStyle(Color.posOnSurfaceVariantLowest)
+                .shimmering()
+
             Spacer()
+
+            if let accessory {
+                accessory
+            }
         }
         .measureWidth { width in
             viewWidth = width
         }
-        .foregroundColor(.posOnSurfaceVariantLowest)
-        .shimmering()
         .frame(maxWidth: .infinity, idealHeight: dimension)
         .background(Color.posSurfaceBright)
-        .posItemCardBorderStyles()
+        .if(configuration.bordered) { $0.posItemCardBorderStyles() }
+    }
+
+    @ViewBuilder var placeholders: some View {
+        HStack(alignment: .center, spacing: Constants.cardSpacing) {
+            Rectangle()
+                .aspectRatio(1, contentMode: .fit)
+            VStack(alignment: .leading) {
+                Rectangle()
+                    .frame(width: viewWidth * 0.5, height: configuration.titleHeight * scale)
+                    .cornerRadius(Layout.cornerRadius)
+                Rectangle()
+                    .frame(width: viewWidth * 0.1, height: configuration.priceHeight * scale)
+                    .cornerRadius(Layout.cornerRadius)
+            }
+            .foregroundColor(.posOnSurfaceVariantLowest)
+            .padding(.horizontal, Constants.horizontalTextPadding)
+        }
     }
 }
 
-private extension GhostItemCardView {
-    typealias Constants = PointOfSaleItemListCardConstants
+fileprivate typealias Constants = PointOfSaleItemListCardConstants
 
-    enum Layout {
-        static let placeholderHeight: CGFloat = 32
-        static let cornerRadius: CGFloat = POSCornerRadiusStyle.medium.value
-    }
+fileprivate enum Layout {
+    static let cornerRadius: CGFloat = POSCornerRadiusStyle.medium.value
+}
+
+struct GhostItemCardViewConfiguration {
+    let titleHeight: CGFloat
+    let priceHeight: CGFloat
+    let bordered: Bool
+
+    static let itemList = GhostItemCardViewConfiguration(
+        titleHeight: 32,
+        priceHeight: 32,
+        bordered: true
+    )
+
+    static let cart = GhostItemCardViewConfiguration(
+        titleHeight: 20,
+        priceHeight: 20,
+        bordered: false
+    )
 }
 
 #Preview {
-    GhostItemCardView()
+    VStack(spacing: 20) {
+        GhostItemCardView(configuration: .itemList) {
+            CartRowRemoveButton {}
+        }
+        GhostItemCardView(configuration: .cart) {
+            CartRowRemoveButton {}
+        }
+        GhostItemCardView(configuration: .itemList)
+    }
 }

e810d5f1a2a1520b26fdb8fe44b7da2d9a3d3ec0
iamgabrielma <gabrielmaldonado.art@gmail.com>
2025-06-10 11:45:13 +0700

    add accessibility label to edit button

--- WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/PencilEditButton.swift
+++ WooCommerce/Classes/ViewRelated/ReusableViews/SwiftUI Components/PencilEditButton.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-/// Renders a button with the pencil system name image
+/// Renders an edit button with the pencil gridicon image
 ///
 struct PencilEditButton: View {
     @ScaledMetric private var size: CGFloat = Layout.editIconImageSize
@@ -13,11 +13,20 @@ struct PencilEditButton: View {
                 .frame(width: size,
                        height: size)
         }
+        .accessibilityLabel(Localization.editButtonAccessibilityLabel)
     }
 }
 
 private extension PencilEditButton {
     enum Layout {
         static let editIconImageSize: CGFloat = 24
+    }
+
+    enum Localization {
+        static let editButtonAccessibilityLabel = NSLocalizedString(
+            "pencilEditButton.accessibility.editButtonAccessibilityLabel",
+            value: "Edit",
+            comment: "Accessibility label for the button to edit"
+        )
     }
 }

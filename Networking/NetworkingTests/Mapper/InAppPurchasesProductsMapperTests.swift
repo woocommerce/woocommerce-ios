6fc5a40283bc6fc3e573d7036c70d0276d40ea59
Gio Lodi <gio@mokacoding.com>
2025-06-10 16:11:51 +1000

    Use `try XCTSkipIf(true)` instead of excluding tests from test plan

--- Networking/NetworkingTests/Mapper/InAppPurchasesProductsMapperTests.swift
+++ Networking/NetworkingTests/Mapper/InAppPurchasesProductsMapperTests.swift
@@ -3,6 +3,8 @@ import XCTest
 
 final class InAppPurchasesProductsMapperTests: XCTestCase {
     func test_iap_products_list_is_decoded_from_json_response() throws {
+        try XCTSkipIf(true)
+
         // Given
         let jsonData = try XCTUnwrap(Loader.contentsOf("iap-products"))
         let expectedProductIdentifiers = [

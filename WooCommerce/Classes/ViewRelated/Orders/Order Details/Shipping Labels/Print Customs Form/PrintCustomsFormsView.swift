d62c975c0e9222d78db83125b9a90449c11a8ae1
iamgabrielma <gabrielmaldonado.art@gmail.com>
2025-06-20 12:55:54 +0700

    remove shipping image duplication
    The same image is used for woo-shipping-label-printing-instructions and woo-shipping-label-creation, which is already in Assets

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Print Customs Form/PrintCustomsFormsView.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Shipping Labels/Print Customs Form/PrintCustomsFormsView.swift
@@ -23,7 +23,7 @@ struct PrintCustomsFormsView: View {
                     .bodyStyle()
                     .padding(.top, Constants.verticalSpacing)
                 VStack(spacing: Constants.verticalSpacing) {
-                    Image("woo-shipping-label-printing-instructions")
+                    Image(.shippingLabelCreationInfoImage)
                     Text(invoiceURLs.count == 1 ? Localization.singlePrintingMessage : Localization.printingMessage)
                         .bodyStyle()
                         .multilineTextAlignment(.center)

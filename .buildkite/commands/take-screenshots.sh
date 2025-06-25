9f6eb2e821adf82d3a865495be53c463aa8792ec
Jaclyn Chen <watertranquil@gmail.com>
2025-06-25 18:08:50 -0400

    [WIP] Try fixing `./.buildkite/commands/take-screenshots.sh: line 18: SCREENSHOT_LANGUAGE: unbound variable`.

--- .buildkite/commands/take-screenshots.sh
+++ .buildkite/commands/take-screenshots.sh
@@ -15,15 +15,19 @@ tar -xf screenshot-artifacts.tar -C fastlane/DerivedData/Build/Products/Debug-ip
 echo "--- :gear: Setup Fastlane Dependencies"
 bundle exec fastlane run configure_apply
 
-echo "--- :camera: Generate Screenshots for ${SCREENSHOT_LANGUAGE} (${SCREENSHOT_MODE} mode)"
+echo "--- :information_source: Debug Environment Variables"
+echo "SCREENSHOT_LANGUAGE: '${SCREENSHOT_LANGUAGE:-NOT_SET}'"
+echo "SCREENSHOT_MODE: '${SCREENSHOT_MODE:-NOT_SET}'"
+
+echo "--- :camera: Generate Screenshots for ${SCREENSHOT_LANGUAGE:-unknown} (${SCREENSHOT_MODE:-unknown} mode)"
 bundle exec fastlane take_screenshots \
-  languages:"${SCREENSHOT_LANGUAGE}" \
-  mode:"${SCREENSHOT_MODE}"
+  languages:"${SCREENSHOT_LANGUAGE:-en-US}" \
+  mode:"${SCREENSHOT_MODE:-light}"
 
 echo "--- :arrow_up: Upload Screenshots to S3"
 # Create unique directory for this job's screenshots
-SCREENSHOT_DIR="fastlane/screenshots-${SCREENSHOT_LANGUAGE}-${SCREENSHOT_MODE}"
+SCREENSHOT_DIR="fastlane/screenshots-${SCREENSHOT_LANGUAGE:-en-US}-${SCREENSHOT_MODE:-light}"
 if [ -d "fastlane/screenshots" ]; then
   mv fastlane/screenshots "${SCREENSHOT_DIR}"
-  aws s3 cp "${SCREENSHOT_DIR}" "s3://${S3_BUCKET}/${BUILDKITE_BUILD_ID}/screenshots-${SCREENSHOT_LANGUAGE}-${SCREENSHOT_MODE}/" --recursive --exclude "*.html"
+  aws s3 cp "${SCREENSHOT_DIR}" "s3://${S3_BUCKET}/${BUILDKITE_BUILD_ID}/screenshots-${SCREENSHOT_LANGUAGE:-en-US}-${SCREENSHOT_MODE:-light}/" --recursive --exclude "*.html"
 fi

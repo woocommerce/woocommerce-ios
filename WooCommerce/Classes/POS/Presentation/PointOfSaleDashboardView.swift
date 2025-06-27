ff7b2e999d03c259a9b46dcfae07bbcbc13053ba
Josh Heald <joshheald@users.noreply.github.com>
2025-06-27 09:51:07 +0100

    Fix padding and spacing of POS items

--- WooCommerce/Classes/POS/Presentation/PointOfSaleDashboardView.swift
+++ WooCommerce/Classes/POS/Presentation/PointOfSaleDashboardView.swift
@@ -119,7 +119,7 @@ struct PointOfSaleDashboardView: View {
     private var contentView: some View {
         @Bindable var viewStateCoordinator = viewStateCoordinator
         return GeometryReader { geometry in
-            HStack {
+            HStack(spacing: 0) {
                 if posModel.orderStage == .building {
                     ItemListView(selectedItemListType: $viewStateCoordinator.selectedItemListType,
                                  searchTerm: $viewStateCoordinator.searchTerm)

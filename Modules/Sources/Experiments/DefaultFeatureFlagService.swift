d8a2c51d63abcbd8004fce7bc7a05c54f42b5b45
iamgabrielma <gabrielmaldonado.art@gmail.com>
2025-06-30 11:41:57 +0700

    adds feature flag for `pointOfSaleOrdersi1`

--- Modules/Sources/Experiments/DefaultFeatureFlagService.swift
+++ Modules/Sources/Experiments/DefaultFeatureFlagService.swift
@@ -93,6 +93,8 @@ public struct DefaultFeatureFlagService: FeatureFlagService {
             return false
         case .pointOfSaleAsATabi1:
             return true
+        case .pointOfSaleOrdersi1:
+            return buildConfig == .localDeveloper || buildConfig == .alpha
         default:
             return true
         }

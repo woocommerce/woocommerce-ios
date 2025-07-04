20ecee24d6946cb1922471f95dbc989026a77a17
Jaclyn Chen <watertranquil@gmail.com>
2025-07-04 14:45:21 -0400

    Revert "Revert changes to use `POSTabEligibilityCheckerI2` now that it is separated to another PR for easier review."
    This reverts commit 9e611a751e38cb8c5aa14f7edefde3ae3e1f493a.

--- WooCommerce/Classes/ViewRelated/MainTabBarController.swift
+++ WooCommerce/Classes/ViewRelated/MainTabBarController.swift
@@ -149,20 +149,29 @@ final class MainTabBarController: UITabBarController {
         self.analytics = analytics
         self.stores = stores
         self.posEligibilityCheckerFactory = posEligibilityCheckerFactory ?? { siteID in
-            POSTabEligibilityChecker(siteID: siteID)
+            if featureFlagService.isFeatureFlagEnabled(.pointOfSaleAsATabi2) {
+                POSTabEligibilityCheckerI2(siteID: siteID)
+            } else {
+                POSTabEligibilityChecker(siteID: siteID)
+            }
         }
         self.posEligibilityService = posEligibilityService
         super.init(coder: coder)
     }
 
     required init?(coder: NSCoder) {
-        self.featureFlagService = ServiceLocator.featureFlagService
+        let featureFlagService = ServiceLocator.featureFlagService
+        self.featureFlagService = featureFlagService
         self.noticePresenter = ServiceLocator.noticePresenter
         self.productImageUploader = ServiceLocator.productImageUploader
         self.analytics = ServiceLocator.analytics
         self.stores = ServiceLocator.stores
         self.posEligibilityCheckerFactory = { siteID in
-            POSTabEligibilityChecker(siteID: siteID)
+            if featureFlagService.isFeatureFlagEnabled(.pointOfSaleAsATabi2) {
+                POSTabEligibilityCheckerI2(siteID: siteID)
+            } else {
+                POSTabEligibilityChecker(siteID: siteID)
+            }
         }
         self.posEligibilityService = POSEligibilityService()
         super.init(coder: coder)

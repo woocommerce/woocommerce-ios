32e3fcdd9e16931d7f481a45ca090af433b31997
Josh Heald <joshheald@users.noreply.github.com>
2025-07-08 10:37:55 +0100

    Add analytic for showing set up flow

--- WooCommerce/Classes/Analytics/WooAnalyticsStat.swift
+++ WooCommerce/Classes/Analytics/WooAnalyticsStat.swift
@@ -1305,6 +1305,7 @@ enum WooAnalyticsStat: String {
     case pointOfSaleSearchRemoteResultsFetched = "search_remote_results_fetched"
     case pointOfSaleBarcodeScanningMenuItemTapped = "barcode_scanning_menu_item_tapped"
     case pointOfSaleBarcodeScanningExplanationDialogShown = "barcode_scanning_explanation_dialog_shown"
+    case pointOfSaleBarcodeScannerSetupFlowShown = "barcode_scanner_setup_flow_shown"
 
     // MARK: Custom Fields events
     case productDetailCustomFieldsTapped = "product_detail_custom_fields_tapped"

8bfda856adba5a6ec7e6d2cd940adebda8e911c1
Josh Heald <joshheald@users.noreply.github.com>
2025-07-08 10:07:43 +0100

    Add empty set-up flow screen

---
+++ WooCommerce/Classes/POS/Presentation/PointOfSaleBarcodeScannerSetUpFlow.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct PointOfSaleBarcodeScannerSetUpFlow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PointOfSaleBarcodeScannerSetUpFlow()
+}

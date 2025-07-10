42c98a9b54d34e15789499714bbb102e5a9f8d24
iamgabrielma <gabrielmaldonado.art@gmail.com>
2025-07-10 11:32:26 +0700

    update badge color init so dynamic appearance is correctly applied

--- WooCommerce/Classes/Extensions/UILabel+SalesChannel.swift
+++ WooCommerce/Classes/Extensions/UILabel+SalesChannel.swift
@@ -23,7 +23,7 @@ private extension UILabel {
     enum Layout {
         static let borderWidth = CGFloat(0.0)
         static let cornerRadius = CGFloat(4.0)
-        static let salesChannelLabelTextColor = UIColor(color: .wooCommercePurple(.shade80))
-        static let salesChannelLabelBackgroundColor = UIColor(color: .wooCommercePurple(.shade10))
+        static let salesChannelLabelTextColor = UIColor.wooCommercePurple(.shade80)
+        static let salesChannelLabelBackgroundColor = UIColor.wooCommercePurple(.shade10)
     }
 }

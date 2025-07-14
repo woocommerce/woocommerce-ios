299e3fb3d77964975a4c207f813e93244c583d01
Huong Do <hi.huongdo@gmail.com>
2025-07-14 10:16:28 +0700

    Remove unused extension method to load single shipment

--- Modules/Sources/Storage/Tools/StorageType+Extensions.swift
+++ Modules/Sources/Storage/Tools/StorageType+Extensions.swift
@@ -607,13 +607,6 @@ public extension StorageType {
         return allObjects(ofType: WooShippingShipment.self, matching: predicate, sortedBy: nil)
     }
 
-    /// Returns a single shipment given a `siteID`, `orderID`, and `index`
-    ///
-    func loadShipment(siteID: Int64, orderID: Int64, index: String) -> WooShippingShipment? {
-        let predicate = \WooShippingShipment.siteID == siteID && \WooShippingShipment.orderID == orderID && \WooShippingShipment.index == index
-        return firstObject(ofType: WooShippingShipment.self, matching: predicate)
-    }
-
     // MARK: - BlazeCampaignListItem
 
     /// Returns a single BlazeCampaignListItem given a `siteID` and `campaignID`

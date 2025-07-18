dda7a33e15bf0d64f4aef6c36d57640adad1cc99
Jaclyn Chen <watertranquil@gmail.com>
2025-07-18 13:58:19 -0400

    Revert changes in `PointOfSaleLoadingView` as it tracks `pointOfSaleLoaded` event.

--- WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/PointOfSaleLoadingView.swift
+++ WooCommerce/Classes/POS/Presentation/CardReaderConnection/UI States/PointOfSaleLoadingView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct PointOfSaleLoadingView: View {
+    @State private var waitingTimeTracker: WaitingTimeTracker?
+
     var body: some View {
         HStack(alignment: .center) {
             Spacer()
@@ -13,7 +15,25 @@ struct PointOfSaleLoadingView: View {
             .multilineTextAlignment(.center)
             Spacer()
         }
+        .onAppear {
+            trackTimeOnAppear()
+        }
+        .onDisappear {
+            trackElapsedTimeOnDisappear()
+        }
         .background(Color.posSurface)
+    }
+}
+
+private extension PointOfSaleLoadingView {
+    func trackTimeOnAppear() {
+        waitingTimeTracker = WaitingTimeTracker(trackScenario: .pointOfSaleLoaded)
+    }
+
+    func trackElapsedTimeOnDisappear() {
+        if let waitingTimeTracker = waitingTimeTracker {
+            waitingTimeTracker.end(using: .milliseconds)
+        }
     }
 }
 

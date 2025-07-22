d9d38000f7250acbdb65d57ebe570d3cb76014ad
Jaclyn Chen <watertranquil@gmail.com>
2025-07-22 13:55:01 -0400

    PluginsService: add `loadPluginInStorage` with a plugin enum to load a plugin synchronously.

--- Modules/Sources/Yosemite/Tools/Plugins/PluginsService.swift
+++ Modules/Sources/Yosemite/Tools/Plugins/PluginsService.swift
@@ -11,6 +11,14 @@ public protocol PluginsServiceProtocol {
     ///   - isActive: Whether the plugin is active or not.
     /// - Returns: The SystemPlugin when found in storage.
     func waitForPluginInStorage(siteID: Int64, pluginPath: String, isActive: Bool) async -> SystemPlugin
+
+    /// Loads a specific plugin from storage synchronously.
+    /// - Parameters:
+    ///   - siteID: The site ID to search for the plugin.
+    ///   - plugin: The plugin to load.
+    ///   - isActive: Whether the plugin is active, inactive, or nil for any state.
+    /// - Returns: The SystemPlugin if found in storage, nil otherwise.
+    func loadPluginInStorage(siteID: Int64, plugin: Plugin, isActive: Bool?) -> SystemPlugin?
 }
 
 public class PluginsService: PluginsServiceProtocol {
@@ -48,5 +56,11 @@ public class PluginsService: PluginsServiceProtocol {
                 continuation.resume(returning: plugin)
             }
         }
+    }
+
+    public func loadPluginInStorage(siteID: Int64, plugin: Plugin, isActive: Bool?) -> SystemPlugin? {
+        storageManager.viewStorage.loadSystemPlugin(siteID: siteID,
+                                                    fileNameWithoutExtension: plugin.fileNameWithoutExtension,
+                                                    active: isActive)?.toReadOnly()
     }
 }

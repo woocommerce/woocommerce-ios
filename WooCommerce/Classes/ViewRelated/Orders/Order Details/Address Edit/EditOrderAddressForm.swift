6555ef5fd42f905a6e5780b61d95b39f5c5a074f
Jaclyn Chen <watertranquil@gmail.com>
2025-07-30 13:29:20 +0800

    Update CTA copy.

--- WooCommerce/Classes/ViewRelated/Orders/Order Details/Address Edit/EditOrderAddressForm.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Order Details/Address Edit/EditOrderAddressForm.swift
@@ -498,7 +498,11 @@ private enum Localization {
     static let hintOptional = NSLocalizedString("Optional", comment: "Text field placeholder in Edit Address Form")
     static let hintSelectOption = NSLocalizedString("Select an option", comment: "Text field placeholder in Edit Address Form")
 
-    static let pickOnMap = NSLocalizedString("Address Search", comment: "Button to open map address picker")
+    static let pickOnMap = NSLocalizedString(
+        "editOrderAddressForm.pickOnMap",
+        value: "Find on Map",
+        comment: "Button to open map address picker."
+    )
 }
 
 #if DEBUG

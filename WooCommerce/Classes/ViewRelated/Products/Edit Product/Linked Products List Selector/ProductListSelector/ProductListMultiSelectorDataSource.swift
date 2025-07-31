07f9d23c1124f002274c2cfd3c6ccdab898a2137
Huong Do <hi.huongdo@gmail.com>
2025-07-31 21:05:11 +0700

    Fix swiftlint issue

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/Linked Products List Selector/ProductListSelector/ProductListMultiSelectorDataSource.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/Linked Products List Selector/ProductListSelector/ProductListMultiSelectorDataSource.swift
@@ -5,7 +5,7 @@ import Yosemite
 /// Enables the user to select multiple products from a paginated list.
 final class ProductListMultiSelectorDataSource: PaginatedListSelectorDataSource {
     var customResultsSortOrder: ((ProductListItem, ProductListItem) -> Bool)?
-    
+
     typealias StorageModel = StorageProduct
 
     // Observable list of the latest product IDs

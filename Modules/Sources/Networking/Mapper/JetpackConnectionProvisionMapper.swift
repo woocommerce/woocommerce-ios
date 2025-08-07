f271e905bd60539c59da052f1c3937db6b72b04f
Huong Do <hi.huongdo@gmail.com>
2025-08-07 12:35:41 +0700

    Add exceptions for periphery warnings

--- Modules/Sources/Networking/Mapper/JetpackConnectionProvisionMapper.swift
+++ Modules/Sources/Networking/Mapper/JetpackConnectionProvisionMapper.swift
@@ -2,6 +2,7 @@ import Foundation
 
 /// Mapper: Jetpack connection registration
 ///
+/// periphery: ignore - used in `JetpackConnectionRemote`
 struct JetpackConnectionProvisionMapper: Mapper {
 
     /// (Attempts) to extract the updated `currentUser` field from a given JSON Encoded response.

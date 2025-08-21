2a16d51cee61f2459496e42006a11f67a64e944c
Huong Do <hi.huongdo@gmail.com>
2025-08-21 11:08:48 +0700

    Update DefaultApplicationPasswordUseCaseTests

--- Modules/Tests/NetworkingTests/ApplicationPassword/DefaultApplicationPasswordUseCaseTests.swift
+++ Modules/Tests/NetworkingTests/ApplicationPassword/DefaultApplicationPasswordUseCaseTests.swift
@@ -90,4 +90,57 @@ final class DefaultApplicationPasswordUseCaseTests: XCTestCase {
         // Then
         XCTAssertTrue(failure == .unauthorizedRequest)
     }
+
+    func test_password_is_generated_with_correct_values_upon_success_response_when_authenticated_with_wpcom() async throws {
+        // Given
+        network.simulateResponse(requestUrlSuffix: URLSuffix.generateApplicationPassword,
+                                 filename: "generate-application-password-using-wporg-creds-success")
+        let email = "test@example.com"
+        let sut = DefaultApplicationPasswordUseCase(type: .wpcom(emailAddress: email, siteID: 123), network: network)
+
+        // When
+        let password = try await sut.generateNewPassword()
+
+        // Then
+        XCTAssertEqual(password.password.secretValue, "passwordvalue")
+        XCTAssertEqual(password.wpOrgUsername, email)
+    }
+
+    func test_applicationPasswordsDisabled_error_is_thrown_if_generating_password_fails_with_501_error_when_authenticated_with_wpcom() async throws {
+        // Given
+        let error = AFError.responseValidationFailed(reason: .unacceptableStatusCode(code: 501))
+        network.simulateError(requestUrlSuffix: URLSuffix.generateApplicationPassword, error: error)
+        let email = "test@example.com"
+        let sut = DefaultApplicationPasswordUseCase(type: .wpcom(emailAddress: email, siteID: 123), network: network)
+
+        // When
+        var failure: ApplicationPasswordUseCaseError?
+        do {
+            let _ = try await sut.generateNewPassword()
+        } catch {
+            failure = error as? ApplicationPasswordUseCaseError
+        }
+
+        // Then
+        XCTAssertTrue(failure == .applicationPasswordsDisabled)
+    }
+
+    func test_unauthorizedRequest_error_is_thrown_if_generating_password_fails_with_401_error_when_authenticated_with_wpcom() async throws {
+        // Given
+        let error = AFError.responseValidationFailed(reason: .unacceptableStatusCode(code: 401))
+        network.simulateError(requestUrlSuffix: URLSuffix.generateApplicationPassword, error: error)
+        let email = "test@example.com"
+        let sut = DefaultApplicationPasswordUseCase(type: .wpcom(emailAddress: email, siteID: 123), network: network)
+
+        // When
+        var failure: ApplicationPasswordUseCaseError?
+        do {
+            let _ = try await sut.generateNewPassword()
+        } catch {
+            failure = error as? ApplicationPasswordUseCaseError
+        }
+
+        // Then
+        XCTAssertTrue(failure == .unauthorizedRequest)
+    }
 }

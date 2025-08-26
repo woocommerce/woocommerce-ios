0846b0a87c20a58de66a4e6d0422436cb6ec3e45
iamgabrielma <gabrielmaldonado.art@gmail.com>
2025-08-26 11:01:27 +0700

    render options for help section

--- WooCommerce/Classes/POS/Presentation/Settings/PointOfSaleSettingsHelpDetailView.swift
+++ WooCommerce/Classes/POS/Presentation/Settings/PointOfSaleSettingsHelpDetailView.swift
@@ -1,16 +1,85 @@
 import SwiftUI
 
 struct PointOfSaleSettingsHelpDetailView: View {
+    @State private var showProductRestrictions = false
+    @State private var showDocumentation = false
+    @State private var showSupport = false
+
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading) {
                 Text("Help Settings")
                     .font(.title2)
-                Text("Help-related configuration")
-                    .font(.caption)
-                    .foregroundStyle(.secondary)
+                List {
+                    Button {
+                        showProductRestrictions = true
+                    } label: {
+                        HStack(alignment: .firstTextBaseline) {
+                            Image(systemName: "magnifyingglass")
+                                .font(.posBodyLargeRegular())
+                            VStack(alignment: .leading, spacing: POSPadding.xSmall) {
+                                Text("Where are my products?")
+                                    .font(.posBodyLargeRegular())
+                                Text("Where are my products subtitle")
+                                    .font(.posBodyMediumRegular())
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                    .buttonStyle(.plain)
+                    Button {
+                        showDocumentation = true
+                    } label: {
+                        HStack(alignment: .firstTextBaseline) {
+                            Image(systemName: "doc.text")
+                                .font(.posBodyLargeRegular())
+                            VStack(alignment: .leading, spacing: POSPadding.xSmall) {
+                                Text("Documentation")
+                                    .font(.posBodyLargeRegular())
+                                Text("Documentation subtitle")
+                                    .font(.posBodyMediumRegular())
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                    .buttonStyle(.plain)
+
+                    Button {
+                        showSupport = true
+                    } label: {
+                        HStack(alignment: .firstTextBaseline) {
+                            Image(systemName: "questionmark")
+                                .font(.posBodyLargeRegular())
+                            VStack(alignment: .leading, spacing: POSPadding.xSmall) {
+                                Text("Get Support")
+                                    .font(.posBodyLargeRegular())
+                                Text("Support subtitle")
+                                    .font(.posBodyMediumRegular())
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                    .buttonStyle(.plain)
+                }
             }
             .padding()
+        }
+        .posModal(isPresented: $showProductRestrictions) {
+            // TODO: Remove copy on POSFloatingControlView.documentationView
+            // WOOMOB-1168
+            SimpleProductsOnlyInformation(isPresented: $showProductRestrictions)
+        }
+        .posSheet(isPresented: $showDocumentation) {
+            // TODO: Remove copy on PointOfSaleDashboardView.documentationView
+            // WOOMOB-1168
+            SafariView(url: WooConstants.URLs.pointOfSaleDocumentation.asURL())
+
+        }
+        .posSheet(isPresented: $showSupport) {
+            // TODO: Remove copy on PointOfSaleDashboardView.supportForm
+            // WOOMOB-1168
+            Text("Support form")
+                .interactiveDismissDisabled(true)
         }
     }
 }

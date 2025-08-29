f4cf1553d7f2b6df467e7b0db62ae28843c18ddd
Josh Heald <joshheald@users.noreply.github.com>
2025-08-29 18:34:26 +0100

    Add mapping for persisted models
    Fix description use when making persistedproduct

---
+++ Modules/Sources/Yosemite/Model/Storage/PersistedProduct+Conversions.swift
@@ -0,0 +1,97 @@
+import Foundation
+import Storage
+
+// MARK: - PersistedProduct Conversions
+extension PersistedProduct {
+    public init(from posProduct: POSProduct) {
+        self.init(
+            id: posProduct.productID,
+            siteID: posProduct.siteID,
+            name: posProduct.name,
+            productTypeKey: posProduct.productTypeKey,
+            fullDescription: posProduct.fullDescription,
+            shortDescription: posProduct.shortDescription,
+            sku: posProduct.sku,
+            globalUniqueID: posProduct.globalUniqueID,
+            price: posProduct.price,
+            downloadable: posProduct.downloadable,
+            parentID: posProduct.parentID,
+            manageStock: posProduct.manageStock,
+            stockQuantity: posProduct.stockQuantity,
+            stockStatusKey: posProduct.stockStatusKey
+        )
+    }
+
+    public func toPOSProduct(images: [ProductImage] = [], attributes: [ProductAttribute] = []) -> POSProduct {
+        return POSProduct(
+            siteID: siteID,
+            productID: id,
+            name: name,
+            productTypeKey: productTypeKey,
+            fullDescription: fullDescription,
+            shortDescription: shortDescription,
+            sku: sku,
+            globalUniqueID: globalUniqueID,
+            price: price,
+            downloadable: downloadable,
+            parentID: parentID,
+            images: images,
+            attributes: attributes,
+            manageStock: manageStock,
+            stockQuantity: stockQuantity,
+            stockStatusKey: stockStatusKey
+        )
+    }
+}
+
+// MARK: - PersistedProductAttribute Conversions
+extension PersistedProductAttribute {
+    public init(from productAttribute: ProductAttribute, productID: Int64) {
+        self.init(
+            productID: productID,
+            name: productAttribute.name,
+            position: Int64(productAttribute.position),
+            visible: productAttribute.visible,
+            variation: productAttribute.variation,
+            options: productAttribute.options
+        )
+    }
+
+    public func toProductAttribute(siteID: Int64) -> ProductAttribute {
+        return ProductAttribute(
+            siteID: siteID,
+            attributeID: 0,
+            name: name,
+            position: Int(position),
+            visible: visible,
+            variation: variation,
+            options: options
+        )
+    }
+}
+
+// MARK: - PersistedProductImage Conversions
+extension PersistedProductImage {
+    public init(from productImage: ProductImage, productID: Int64) {
+        self.init(
+            id: productImage.imageID,
+            productID: productID,
+            dateCreated: productImage.dateCreated,
+            dateModified: productImage.dateModified,
+            src: productImage.src,
+            name: productImage.name,
+            alt: productImage.alt
+        )
+    }
+
+    public func toProductImage() -> ProductImage {
+        return ProductImage(
+            imageID: id,
+            dateCreated: dateCreated,
+            dateModified: dateModified,
+            src: src,
+            name: name,
+            alt: alt
+        )
+    }
+}
---
+++ Modules/Sources/Yosemite/Model/Storage/PersistedProductVariation+Conversions.swift
@@ -0,0 +1,84 @@
+import Foundation
+import Storage
+
+// MARK: - PersistedProductVariation Conversions
+extension PersistedProductVariation {
+    public init(from posProductVariation: POSProductVariation) {
+        self.init(
+            id: posProductVariation.productVariationID,
+            siteID: posProductVariation.siteID,
+            productID: posProductVariation.productID,
+            sku: posProductVariation.sku,
+            globalUniqueID: posProductVariation.globalUniqueID,
+            price: posProductVariation.price,
+            downloadable: posProductVariation.downloadable,
+            fullDescription: posProductVariation.fullDescription,
+            manageStock: posProductVariation.manageStock,
+            stockQuantity: posProductVariation.stockQuantity,
+            stockStatusKey: posProductVariation.stockStatusKey
+        )
+    }
+
+    public func toPOSProductVariation(attributes: [ProductVariationAttribute] = [], image: ProductImage? = nil) -> POSProductVariation {
+        return POSProductVariation(
+            siteID: siteID,
+            productID: productID,
+            productVariationID: id,
+            attributes: attributes,
+            image: image,
+            fullDescription: fullDescription,
+            sku: sku,
+            globalUniqueID: globalUniqueID,
+            price: price,
+            downloadable: downloadable,
+            manageStock: manageStock,
+            stockQuantity: stockQuantity,
+            stockStatusKey: stockStatusKey
+        )
+    }
+}
+
+// MARK: - PersistedProductVariationAttribute Conversions
+extension PersistedProductVariationAttribute {
+    public init(from productVariationAttribute: ProductVariationAttribute, productVariationID: Int64) {
+        self.init(
+            productVariationID: productVariationID,
+            name: productVariationAttribute.name,
+            option: productVariationAttribute.option
+        )
+    }
+
+    public func toProductVariationAttribute() -> ProductVariationAttribute {
+        return ProductVariationAttribute(
+            id: id ?? 0,
+            name: name,
+            option: option
+        )
+    }
+}
+
+// MARK: - PersistedProductVariationImage Conversions
+extension PersistedProductVariationImage {
+    public init(from productImage: ProductImage, productVariationID: Int64) {
+        self.init(
+            id: productImage.imageID,
+            productVariationID: productVariationID,
+            dateCreated: productImage.dateCreated,
+            dateModified: productImage.dateModified,
+            src: productImage.src,
+            name: productImage.name,
+            alt: productImage.alt
+        )
+    }
+
+    public func toProductImage() -> ProductImage {
+        return ProductImage(
+            imageID: id,
+            dateCreated: dateCreated,
+            dateModified: dateModified,
+            src: src,
+            name: name,
+            alt: alt
+        )
+    }
+}

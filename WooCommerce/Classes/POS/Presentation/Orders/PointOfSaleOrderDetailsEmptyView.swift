15003d130895e35ec26cfea1d0b7156ce235e29d
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2025-09-11 22:30:07 +0300

    Updated empty view when there are no orders loaded

--- WooCommerce/Classes/POS/Presentation/Orders/PointOfSaleOrderDetailsEmptyView.swift
+++ WooCommerce/Classes/POS/Presentation/Orders/PointOfSaleOrderDetailsEmptyView.swift
@@ -2,24 +2,40 @@ import SwiftUI
 
 struct PointOfSaleOrderDetailsEmptyView: View {
     var body: some View {
-        // TODO: WOOMOB-1136
         VStack(spacing: 0) {
             POSPageHeaderView(
-                title: "Orders",
+                title: Localization.title,
                 backButtonConfiguration: nil
             )
 
             VStack {
                 Spacer()
-                Text("No Orders Loaded")
+
+                Text(Localization.noOrderToDisplay)
                     .font(.posBodyLargeRegular())
                     .foregroundStyle(Color.posOnSurfaceVariantHighest)
+                    .multilineTextAlignment(.center)
+
                 Spacer()
             }
         }
         .background(Color.posSurface)
         .navigationBarHidden(true)
     }
+}
+
+private enum Localization {
+    static let title = NSLocalizedString(
+        "pos.orderDetailsEmptyView.ordersTitle",
+        value: "Order",
+        comment: "Title at the header for the Order Details empty view."
+    )
+
+    static let noOrderToDisplay = NSLocalizedString(
+        "pos.orderDetailsEmptyView.noOrderToDisplay",
+        value: "No order to display",
+        comment: "Text appearing in the order details pane when there are no orders available."
+    )
 }
 
 #if DEBUG

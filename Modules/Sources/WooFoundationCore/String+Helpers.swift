925a3655837f4198b031cf7703c0f65c3478b10f
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2025-09-17 09:24:03 +0300

    Remove unused or duplicate methods from String+Helpers

--- Modules/Sources/WooFoundationCore/String+Helpers.swift
+++ Modules/Sources/WooFoundationCore/String+Helpers.swift
@@ -44,60 +44,9 @@ public extension String {
         }
     }
 
-    /// Helper method to remove the last newline character in a given string.
-    ///
-    /// - Parameters:
-    ///   - string: the string to format
-    /// - Returns: a string with the newline character removed, if the
-    ///            newline character is the last character in the string.
-    ///
-    static func stripLastNewline(in string: String) -> String {
-        var newText = string
-        let lastChar = newText.suffix(1)
-
-        let newline = String(lastChar)
-        if newline == "\n" {
-            newText = String(newText.dropLast())
-        }
-
-        return newText
-    }
-
     /// A Boolean value indicating whether a string has characters.
     var isNotEmpty: Bool {
         return !isEmpty
-    }
-
-    /// Get quotation marks from Locale
-    static var quotes: (String, String) {
-        guard
-            let bQuote = Locale.current.quotationBeginDelimiter,
-            let eQuote = Locale.current.quotationEndDelimiter
-        else { return ("\"", "\"") }
-
-        return (bQuote, eQuote)
-    }
-
-    /// Puts quotation marks at the beginning and the end of the string
-    var quoted: String {
-        let (bQuote, eQuote) = String.quotes
-        return bQuote + self + eQuote
-    }
-
-    /// Given an string made of tags separated by commas, returns an array with these tags
-    ///
-    func setOfTags() -> Set<String>? {
-        guard !self.isEmpty else {
-            return [String()]
-        }
-
-        let arrayOfTags = self.components(separatedBy: ",").map({ $0.trimmingCharacters(in: .whitespacesAndNewlines) })
-
-        guard !arrayOfTags.isEmpty else {
-            return nil
-        }
-
-        return Set(arrayOfTags)
     }
 }
 

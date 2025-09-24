46566eb91aed457977ad3d96b3c907fd25a5608a
Huong Do <hi.huongdo@gmail.com>
2025-09-24 16:42:55 +0700

    Add tests

---
+++ Modules/Tests/YosemiteTests/Mocks/MockBookingsRemote.swift
@@ -0,0 +1,19 @@
+import Foundation
+@testable import Networking
+
+/// Mock for BookingsRemoteProtocol
+///
+final class MockBookingsRemote: BookingsRemoteProtocol {
+    private var loadAllBookingsResult: Result<[Booking], Error>?
+
+    func whenLoadingAllBookings(thenReturn result: Result<[Booking], Error>) {
+        loadAllBookingsResult = result
+    }
+
+    func loadAllBookings(for siteID: Int64, pageNumber: Int, pageSize: Int) async throws -> [Booking] {
+        guard let result = loadAllBookingsResult else {
+            throw NetworkError.timeout()
+        }
+        return try result.get()
+    }
+}

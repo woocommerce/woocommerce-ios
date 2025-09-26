2ebef2fe107c5203b25c8e810af5fc12816bf71c
Huong Do <hi.huongdo@gmail.com>
2025-09-26 10:26:23 +0700

    Ignore periphery warning for view model

--- WooCommerce/Classes/Bookings/BookingList/BookingListView.swift
+++ WooCommerce/Classes/Bookings/BookingList/BookingListView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct BookingListView: View {
+    // periphery:ignore
     @ObservedObject private var viewModel: BookingListViewModel
 
     init(viewModel: BookingListViewModel) {

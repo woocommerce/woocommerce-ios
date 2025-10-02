e2a619ff042e6911eeed11d8250c53359f07f492
Jaclyn Chen <watertranquil@gmail.com>
2025-10-02 09:46:20 +0800

    Try reverting back to dot syntax color symbols for POS colors to see how it works on CI.

--- Modules/Sources/PointOfSale/Colors/Color+POSColorPalette.swift
+++ Modules/Sources/PointOfSale/Colors/Color+POSColorPalette.swift
@@ -3,50 +3,50 @@ import SwiftUI
 
 extension Color {
     // MARK: - Primary Colors
-    static var posPrimary: Color { Color("posPrimary", bundle: .module) }
-    static var posOnPrimary: Color { Color("posOnPrimary", bundle: .module) }
-    static var posPrimaryContainer: Color { Color("posPrimaryContainer", bundle: .module) }
-    static var posOnPrimaryContainer: Color { Color("posOnPrimaryContainer", bundle: .module) }
+    static var posPrimary: Color { Color(.posPrimary) }
+    static var posOnPrimary: Color { Color(.posOnPrimary) }
+    static var posPrimaryContainer: Color { Color(.posPrimaryContainer) }
+    static var posOnPrimaryContainer: Color { Color(.posOnPrimaryContainer) }
 
     // MARK: - Secondary Container Colors
-    static var posSecondary: Color { Color("posSecondary", bundle: .module) }
-    static var posOnSecondary: Color { Color("posOnSecondary", bundle: .module) }
-    static var posSecondaryContainer: Color { Color("posSecondaryContainer", bundle: .module) }
-    static var posOnSecondaryContainer: Color { Color("posOnSecondaryContainer", bundle: .module) }
+    static var posSecondary: Color { Color(.posSecondary) }
+    static var posOnSecondary: Color { Color(.posOnSecondary) }
+    static var posSecondaryContainer: Color { Color(.posSecondaryContainer) }
+    static var posOnSecondaryContainer: Color { Color(.posOnSecondaryContainer) }
 
     // MARK: - Disabled Container Colors
-    static var posDisabledContainer: Color { Color("posDisabledContainer", bundle: .module) }
-    static var posOnDisabledContainer: Color { Color("posOnDisabledContainer", bundle: .module) }
+    static var posDisabledContainer: Color { Color(.posDisabledContainer) }
+    static var posOnDisabledContainer: Color { Color(.posOnDisabledContainer) }
 
     // MARK: - Surface Colors
-    static var posSurface: Color { Color("posSurface", bundle: .module) }
-    static var posOnSurface: Color { Color("posOnSurface", bundle: .module) }
-    static var posSurfaceDim: Color { Color("posSurfaceDim", bundle: .module) }
-    static var posSurfaceBright: Color { Color("posSurfaceBright", bundle: .module) }
-    static var posSurfaceContainerLowest: Color { Color("posSurfaceContainerLowest", bundle: .module) }
-    static var posSurfaceContainerLow: Color { Color("posSurfaceContainerLow", bundle: .module) }
-    static var posSurfaceContainerHighest: Color { Color("posSurfaceContainerHighest", bundle: .module) }
+    static var posSurface: Color { Color(.posSurface) }
+    static var posOnSurface: Color { Color(.posOnSurface) }
+    static var posSurfaceDim: Color { Color(.posSurfaceDim) }
+    static var posSurfaceBright: Color { Color(.posSurfaceBright) }
+    static var posSurfaceContainerLowest: Color { Color(.posSurfaceContainerLowest) }
+    static var posSurfaceContainerLow: Color { Color(.posSurfaceContainerLow) }
+    static var posSurfaceContainerHighest: Color { Color(.posSurfaceContainerHighest) }
 
     // MARK: - Surface Variant Colors
-    static var posOnSurfaceVariantLowest: Color { Color("posOnSurfaceVariantLowest", bundle: .module) }
-    static var posOnSurfaceVariantHighest: Color { Color("posOnSurfaceVariantHighest", bundle: .module) }
+    static var posOnSurfaceVariantLowest: Color { Color(.posOnSurfaceVariantLowest) }
+    static var posOnSurfaceVariantHighest: Color { Color(.posOnSurfaceVariantHighest) }
 
     // MARK: - Inverse Surface Colors
-    static var posInverseSurface: Color { Color("posInverseSurface", bundle: .module) }
-    static var posOnInverseSurface: Color { Color("posOnInverseSurface", bundle: .module) }
+    static var posInverseSurface: Color { Color(.posInverseSurface) }
+    static var posOnInverseSurface: Color { Color(.posOnInverseSurface) }
 
     // MARK: - Outline Colors
-    static var posOutline: Color { Color("posOutline", bundle: .module) }
-    static var posOutlineVariant: Color { Color("posOutlineVariant", bundle: .module) }
+    static var posOutline: Color { Color(.posOutline) }
+    static var posOutlineVariant: Color { Color(.posOutlineVariant) }
 
     // MARK: - Status Colors
-    static var posError: Color { Color("posError", bundle: .module) }
-    static var posOnError: Color { Color("posOnError", bundle: .module) }
-    static var posSuccess: Color { Color("posSuccess", bundle: .module) }
-    static var posOnSuccess: Color { Color("posOnSuccess", bundle: .module) }
-    static var posAlert: Color { Color("posAlert", bundle: .module) }
-    static var posOnAlert: Color { Color("posOnAlert", bundle: .module) }
+    static var posError: Color { Color(.posError) }
+    static var posOnError: Color { Color(.posOnError) }
+    static var posSuccess: Color { Color(.posSuccess) }
+    static var posOnSuccess: Color { Color(.posOnSuccess) }
+    static var posAlert: Color { Color(.posAlert) }
+    static var posOnAlert: Color { Color(.posOnAlert) }
 
     // MARK: - Other
-    static var posShadow: Color { Color("posShadow", bundle: .module) }
+    static var posShadow: Color { Color(.posShadow) }
 }

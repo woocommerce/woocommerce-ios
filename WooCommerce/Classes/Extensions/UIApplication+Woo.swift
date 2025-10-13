b33da62e2e93c0eaf1010f39e14049e7108daf12
Adam Borbas <adborbas@gmail.com>
2025-10-13 13:55:57 +0200

    Just a bit of doc

--- WooCommerce/Classes/Extensions/UIApplication+Woo.swift
+++ WooCommerce/Classes/Extensions/UIApplication+Woo.swift
@@ -30,6 +30,11 @@ extension UIApplication {
             .first { $0.isKeyWindow }
     }
 
+    /// Returns the delegate of the first connected scene.
+    ///
+    /// - Note: This approach is valid while the app supports only a single scene.
+    ///       If multi‑window support is introduced in the future,
+    ///       this will need to be revisited to handle multiple scenes safely.
     static var sceneDelegate: SceneDelegate? {
         UIApplication.shared.connectedScenes
             .compactMap({ $0 as? UIWindowScene })

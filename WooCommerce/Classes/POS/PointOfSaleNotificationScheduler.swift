de97c651f85eae4a45cc6170674cdd4dad4813e6
iamgabrielma <gabrielmaldonado.art@gmail.com>
2025-10-14 10:40:09 +0700

    refactor to simplify cases

--- WooCommerce/Classes/POS/PointOfSaleNotificationScheduler.swift
+++ WooCommerce/Classes/POS/PointOfSaleNotificationScheduler.swift
@@ -2,16 +2,48 @@ import Foundation
 import UserNotifications
 import Yosemite
 import Experiments
-
+// periphery: ignore - work in progress
 final class PointOfSaleNotificationScheduler {
     enum MerchantType {
         case potentialMerchant
         case currentMerchant
+
+        var scenario: LocalNotification.Scenario {
+            switch self {
+            case .potentialMerchant:
+                return .pointOfSalePotentialMerchant
+            case .currentMerchant:
+                return .pointOfSaleCurrentMerchant
+            }
+        }
+
+        var surveyURL: String {
+            switch self {
+            case .potentialMerchant:
+                return LocalNotification.SurveyURL.pointOfSalePotentialMerchant
+            case .currentMerchant:
+                return LocalNotification.SurveyURL.pointOfSaleCurrentMerchant
+            }
+        }
+
+        var timeIntervalInSeconds: Int {
+            switch self {
+            case .potentialMerchant:
+                return 60
+            case .currentMerchant:
+                return 60 * 5
+            }
+        }
+
+        var timeInterval: TimeInterval {
+            TimeInterval(timeIntervalInSeconds)
+        }
     }
+
     private let siteSettings: [SiteSetting]
     private let featureFlagService: FeatureFlagService
     private let pushNotificationsManager: PushNotesManager
-    
+
     init(siteSettings: [SiteSetting] = ServiceLocator.selectedSiteSettings.siteSettings,
          featureFlagService: FeatureFlagService = ServiceLocator.featureFlagService,
          pushNotificationsManager: PushNotesManager = ServiceLocator.pushNotesManager) {
@@ -19,7 +51,7 @@ final class PointOfSaleNotificationScheduler {
         self.featureFlagService = featureFlagService
         self.pushNotificationsManager = pushNotificationsManager
     }
-    
+
     func scheduleLocalNotificationIfEligible(for merchantType: PointOfSaleNotificationScheduler.MerchantType) {
         // TODO: Additional check to see if .currentMerchant case has used POS before - WOOMOB-1498
         // TODO: Check as well if the notification hasn't been scheduled already WOOMOB-1461
@@ -28,7 +60,7 @@ final class PointOfSaleNotificationScheduler {
 
         scheduleLocalNotification(for: merchantType)
     }
-    
+
     private func isCountryEligible() -> Bool {
         let storeCountry = SiteAddress(siteSettings: siteSettings).countryCode
         if storeCountry == .US || storeCountry == .GB {
@@ -37,49 +69,25 @@ final class PointOfSaleNotificationScheduler {
             return false
         }
     }
-    
+
     private func scheduleLocalNotification(for merchantType: PointOfSaleNotificationScheduler.MerchantType) {
         // TODO: Set scheduled notification value in app storage - WOOMOB-1461
-        switch merchantType {
-        case .potentialMerchant:
-            Task { @MainActor in
-                let seconds = TimeInterval(Constants.potentialMerchantNotificationTimeIntervalInSeconds)
-                let payload: [AnyHashable: Any] = [
-                    LocalNotification.UserInfoKey.surveyURL: LocalNotification.SurveyURL.pointOfSalePotentialMerchant
-                ]
-                let notification = LocalNotification(
-                    title: LocalNotification.Localization.PointOfSalePotentialMerchant.title,
-                    body: LocalNotification.Localization.PointOfSalePotentialMerchant.body,
-                    scenario: .pointOfSalePotentialMerchant,
-                    actions: nil,
-                    userInfo: payload
-                )
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: seconds, repeats: false)
-                await pushNotificationsManager.requestLocalNotification(notification, trigger: trigger)
-            }
-        case .currentMerchant:
-            Task { @MainActor in
-                let seconds = TimeInterval(Constants.currentMerchantNotificationTimeIntervalInSeconds)
-                let payload: [AnyHashable: Any] = [
-                    LocalNotification.UserInfoKey.surveyURL: LocalNotification.SurveyURL.pointOfSaleCurrentMerchant
-                ]
-                let notification = LocalNotification(
-                    title: LocalNotification.Localization.PointOfSaleCurrentMerchant.title,
-                    body: LocalNotification.Localization.PointOfSaleCurrentMerchant.body,
-                    scenario: .pointOfSaleCurrentMerchant,
-                    actions: nil,
-                    userInfo: payload
-                )
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: seconds, repeats: false)
-                await pushNotificationsManager.requestLocalNotification(notification, trigger: trigger)
-            }
-        }
-    }
-}
+        Task { @MainActor in
+            let payload: [AnyHashable: Any] = [
+                LocalNotification.UserInfoKey.surveyURL: merchantType.surveyURL
+            ]
 
-private extension PointOfSaleNotificationScheduler {
-    enum Constants {
-        static let potentialMerchantNotificationTimeIntervalInSeconds: Int = 60
-        static let currentMerchantNotificationTimeIntervalInSeconds: Int = 60 * 5
+            let notification = LocalNotification(
+                scenario: merchantType.scenario,
+                userInfo: payload
+            )
+
+            let trigger = UNTimeIntervalNotificationTrigger(
+                timeInterval: merchantType.timeInterval,
+                repeats: false
+            )
+
+            await pushNotificationsManager.requestLocalNotification(notification, trigger: trigger)
+        }
     }
 }

31f5aec6cdb14cc9c8b4e510bf11f56369b3d40a
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2025-10-14 22:36:40 +0300

    Create BackgroundTaskSchedule to provide a prioritization mechanism
    BackgroundTaskSchedule uses a simple logic to provide the next task to schedule.
    - When app goes to background, sets the preferred refresh date based on the predefined periods (30 min for order sync and 60 for pos catalog sync)
    - Provides the next task based on the earliest time
    - The preferred time is only reset when app goes into background ensuring that when the app is longer in the background, eventually all tasks will get executed

---
+++ WooCommerce/Classes/Tools/BackgroundTasks/BackgroundTaskSchedule.swift
@@ -0,0 +1,56 @@
+import Foundation
+import WooFoundation
+
+extension BackgroundTaskRefreshDispatcher.BackgroundTaskType {
+    var period: TimeInterval {
+        switch self {
+        case .ordersAndDashboardSync:
+            return 30 * 60 // 30 minutes
+        case .posCatalogSync:
+            return 60 * 60 // 60 minutes
+        }
+    }
+}
+
+/// BackgroundTaskSchedule is a helper tool to etermine the next BackgroundTask based on the preferred run period
+///
+final class BackgroundTaskSchedule {
+    private var preferredTaskDate: [BackgroundTaskRefreshDispatcher.BackgroundTaskType: Date] = [:]
+    private let timeProvider: TimeProvider
+
+    init(timeProvider: TimeProvider = DefaultTimeProvider()) {
+        self.timeProvider = timeProvider
+    }
+
+    // Set preferred task dates when going into background
+    // This allows to pick the most appropriate next task
+    /// Example:
+    /// Set preferred dates Task A: in 30 min and Task B: in 45 min when app enters background
+    /// System executes Task A after 40 min. It allows us to know that preferred time for Task B is in 5 min, not 45 min
+    /// Next task is Task B with preferred time in 5 minutes
+    ///
+    func setDefaultPreferredTaskDates() {
+        for task in BackgroundTaskRefreshDispatcher.BackgroundTaskType.allCases {
+            preferredTaskDate[task] = timeProvider.now().addingTimeInterval(task.period)
+        }
+    }
+
+    func getNextTask() -> BackgroundTaskRefreshDispatcher.BackgroundTaskType {
+        return BackgroundTaskRefreshDispatcher.BackgroundTaskType.allCases.min { task1, task2 in
+            preferredRunDate(for: task1) < preferredRunDate(for: task2)
+        } ?? .ordersAndDashboardSync
+    }
+
+    func preferredRunDate(for task: BackgroundTaskRefreshDispatcher.BackgroundTaskType) -> Date {
+        if let preferred = preferredTaskDate[task] {
+            return preferred
+        }
+        let next = timeProvider.now().addingTimeInterval(task.period)
+        preferredTaskDate[task] = next
+        return next
+    }
+
+    func setNextPreferredRunDate(for task: BackgroundTaskRefreshDispatcher.BackgroundTaskType) {
+        preferredTaskDate[task] = timeProvider.now().addingTimeInterval(task.period)
+    }
+}

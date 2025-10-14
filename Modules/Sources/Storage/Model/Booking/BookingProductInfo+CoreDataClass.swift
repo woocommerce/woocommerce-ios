7adc582efbae519a70ebd7a64dd9b4b22dc56fd4
Huong Do <hi.huongdo@gmail.com>
2025-10-14 21:47:34 +0700

    Remove unused types

--- Modules/Sources/Storage/Model/Booking/BookingProductInfo+CoreDataClass.swift
+++ Modules/Sources/Storage/Model/Booking/BookingProductInfo+CoreDataClass.swift
@@ -1,8 +1,6 @@
 import Foundation
 import CoreData
 
-public typealias BookingProductInfoCoreDataClassSet = NSSet
-
 @objc(BookingProductInfo)
 public class BookingProductInfo: NSManagedObject {
 
--- Modules/Sources/Storage/Model/Booking/BookingProductInfo+CoreDataProperties.swift
+++ Modules/Sources/Storage/Model/Booking/BookingProductInfo+CoreDataProperties.swift
@@ -1,9 +1,6 @@
 import Foundation
 import CoreData
 
-
-public typealias BookingProductInfoCoreDataPropertiesSet = NSSet
-
 extension BookingProductInfo {
     @NSManaged public var name: String?
     @NSManaged public var orderInfo: BookingOrderInfo?

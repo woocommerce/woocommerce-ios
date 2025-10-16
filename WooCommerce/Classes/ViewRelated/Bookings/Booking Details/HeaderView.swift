fbe50e9f214dfe76bec8c6b052211ed41aca8a53
RafaelKayumov <justpoc@gmail.com>
2025-10-16 19:59:42 +0300

    Omit empty customer/service line in header

--- WooCommerce/Classes/ViewRelated/Bookings/Booking Details/HeaderView.swift
+++ WooCommerce/Classes/ViewRelated/Bookings/Booking Details/HeaderView.swift
@@ -6,12 +6,16 @@ extension BookingDetailsView {
 
         var body: some View {
             VStack(alignment: .leading, spacing: Layout.headerContentVerticalPadding) {
-                Text(content.bookingDate)
-                    .font(TextFont.bodyMedium)
-                    .foregroundColor(.primary)
-                Text(content.serviceAndCustomerLine)
-                    .font(.footnote.weight(.medium))
-                    .foregroundColor(.secondary)
+                if !content.bookingDate.isEmpty {
+                    Text(content.bookingDate)
+                        .font(TextFont.bodyMedium)
+                        .foregroundColor(.primary)
+                }
+                if !content.serviceAndCustomerLine.isEmpty {
+                    Text(content.serviceAndCustomerLine)
+                        .font(.footnote.weight(.medium))
+                        .foregroundColor(.secondary)
+                }
                 HStack {
                     ForEach(content.status, id: \.self) { status in
                         Text(status.labelText)

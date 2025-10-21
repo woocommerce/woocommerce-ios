97b2109df56917f9fc0c6791c9b6fbda0ea9ec09
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2025-10-21 13:11:27 +0300

    Make withObservationTracking internal

--- Modules/Sources/PointOfSale/Extensions/Observation+WithObservationTrackingOf.swift
+++ Modules/Sources/PointOfSale/Extensions/Observation+WithObservationTrackingOf.swift
@@ -3,7 +3,7 @@ import Observation
 
 /// A helper method for continuously tracking observations on a value.
 ///
-public func withObservationTracking<T: Sendable>(of value: @Sendable @escaping @autoclosure () -> T, execute: @Sendable @escaping (T) -> Void) {
+func withObservationTracking<T: Sendable>(of value: @Sendable @escaping @autoclosure () -> T, execute: @Sendable @escaping (T) -> Void) {
     Observation.withObservationTracking {
         execute(value())
     } onChange: {

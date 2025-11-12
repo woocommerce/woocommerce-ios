3ebd807a9e956f3205ab821fa86d4446539fd798
Huong Do <hi.huongdo@gmail.com>
2025-11-12 12:27:20 +0700

    Remove default content margin

--- WooCommerce/StoreWidgets/StoreInfoWidget.swift
+++ WooCommerce/StoreWidgets/StoreInfoWidget.swift
@@ -24,6 +24,7 @@ struct StoreInfoWidget: Widget {
         .configurationDisplayName(Localization.title)
         .description(Localization.description)
         .supportedFamilies(supportedFamilies)
+        .contentMarginsDisabled()
     }
 }
 

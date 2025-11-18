f68c411b725a19eb6d43ac3d44b34961cd2aa382
Huong Do <hi.huongdo@gmail.com>
2025-11-18 16:44:14 +0700

    Update tests for ProductDetailNavigator

--- WooCommerce/WooCommerceTests/Routing/ProductDetail/ProductDetailNavigatorTests.swift
+++ WooCommerce/WooCommerceTests/Routing/ProductDetail/ProductDetailNavigatorTests.swift
@@ -5,32 +5,57 @@ import Yosemite
 @MainActor
 final class ProductDetailNavigatorTests {
     private static let aNonBookingProduct = Product.fake().copy(productTypeKey: "simple")
-    private static let aBookingProduct = Product.fake().copy(productTypeKey: "booking")
+    private static let aBookingProduct = Product.fake().copy(productTypeKey: "bookable-service")
     private lazy var coordinatorFactory = MockProductDetailCoordinatorFactory()
 
     @Test(arguments: [
         (isCIABSite: true, product: aNonBookingProduct),
-        (isCIABSite: false, product: aBookingProduct),
         (isCIABSite: false, product: aNonBookingProduct),
     ])
-    private func regardlessOfCIABSiteWeShouldDirectToNative(isCIABSite: Bool, product: Product) {
+    func non_bookable_product_directs_to_native_view_regardless_of_CIAB(isCIABSite: Bool, product: Product) {
+        // Given
         let navigator = ProductDetailNavigator(
             ciabChecker: MockCIABEligibilityChecker(mockedIsCurrentSiteCIAB: isCIABSite),
             coordinatorFactory: coordinatorFactory
         )
+
+        // When
         _ = navigator.makeDestination(product: product, isReadOnly: false)
+
+        // Then
         #expect(coordinatorFactory.createdNativeCoordiantor)
         #expect(!coordinatorFactory.createdWebCoordiantor)
     }
 
     @Test
-    private func bookableProductOnCIABSiteWeShouldDirectToWeb() {
+    func bookable_product_on_CIAB_site_directs_to_web_view() {
+        // Given
         let navigator = ProductDetailNavigator(
             ciabChecker: MockCIABEligibilityChecker(mockedIsCurrentSiteCIAB: true),
             coordinatorFactory: coordinatorFactory
         )
+
+        // When
         _ = navigator.makeDestination(product: Self.aBookingProduct, isReadOnly: false)
+
+        // Then
         #expect(coordinatorFactory.createdWebCoordiantor)
         #expect(!coordinatorFactory.createdNativeCoordiantor)
+    }
+
+    @Test
+    func bookable_product_on_non_CIAB_site_directs_to_native_view() {
+        // Given
+        let navigator = ProductDetailNavigator(
+            ciabChecker: MockCIABEligibilityChecker(mockedIsCurrentSiteCIAB: false),
+            coordinatorFactory: coordinatorFactory
+        )
+
+        // When
+        _ = navigator.makeDestination(product: Self.aBookingProduct, isReadOnly: false)
+
+        // Then
+        #expect(coordinatorFactory.createdNativeCoordiantor)
+        #expect(!coordinatorFactory.createdWebCoordiantor)
     }
 }

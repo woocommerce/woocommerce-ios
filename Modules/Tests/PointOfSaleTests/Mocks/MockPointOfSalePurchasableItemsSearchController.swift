d37005d7a90447ed120bd883d5af7aa1ffa063c7
Josh Heald <joshheald@users.noreply.github.com>
2025-11-20 14:07:45 +0000

    Fix lint

--- Modules/Tests/PointOfSaleTests/Mocks/MockPointOfSalePurchasableItemsSearchController.swift
+++ Modules/Tests/PointOfSaleTests/Mocks/MockPointOfSalePurchasableItemsSearchController.swift
@@ -2,7 +2,7 @@ import Foundation
 import Combine
 @testable import PointOfSale
 import enum Yosemite.POSItem
-import Yosemite
+import enum Yosemite.SearchDebounceStrategy
 
 final class MockPointOfSalePurchasableItemsSearchController: PointOfSaleSearchingItemsControllerProtocol {
     var itemsViewState: ItemsViewState = .init(containerState: .content,
@@ -18,5 +18,5 @@ final class MockPointOfSalePurchasableItemsSearchController: PointOfSaleSearchin
 
     func loadNextItems(base: ItemListBaseItem) async { }
 
-    func clearSearchItems(baseItem: PointOfSale.ItemListBaseItem) { }
+    func clearSearchItems(baseItem: ItemListBaseItem) { }
 }

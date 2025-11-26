fed061c4962fc9fd3cb22621a039aa7890b46d74
Josh Heald <joshheald@users.noreply.github.com>
2025-11-26 10:25:04 +0000

    Use our own in-memory UserDefaults mock

--- WooCommerce/WooCommerceTests/Tools/BackgroundTaskScheduleTests.swift
+++ WooCommerce/WooCommerceTests/Tools/BackgroundTaskScheduleTests.swift
@@ -8,13 +8,9 @@ struct BackgroundTaskScheduleTests {
     private let timeProvider: MockTimeProvider
 
     init() {
-        // Use a unique volatile domain for in-memory storage that doesn't persist to disk
-        let userDefaults = UserDefaults()
-        let volatileDomain = "test.BackgroundTaskSchedule.\(UUID().uuidString)"
-        userDefaults.setVolatileDomain([:], forName: volatileDomain)
-        userDefaults.addSuite(named: volatileDomain)
-
         timeProvider = MockTimeProvider()
+        let userDefaults = InMemoryUserDefaults()
+
         sut = BackgroundTaskSchedule(timeProvider: timeProvider, userDefaults: userDefaults)
     }
 
@@ -224,5 +220,27 @@ private class MockTimeProvider: TimeProvider {
 
     func scheduleTimer(timeInterval: TimeInterval, target: Any, selector: Selector) -> Timer {
         fatalError("not implemented")
+    }
+}
+
+/// In-memory UserDefaults that doesn't persist to disk
+/// Each instance has its own isolated storage
+private class InMemoryUserDefaults: UserDefaults {
+    private var storage: [String: Any] = [:]
+
+    override func set(_ value: Any?, forKey defaultName: String) {
+        storage[defaultName] = value
+    }
+
+    override func data(forKey defaultName: String) -> Data? {
+        storage[defaultName] as? Data
+    }
+
+    override func object(forKey defaultName: String) -> Any? {
+        storage[defaultName]
+    }
+
+    override func removeObject(forKey defaultName: String) {
+        storage.removeValue(forKey: defaultName)
     }
 }

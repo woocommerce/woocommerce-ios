045ba00036ee7dfb8b96f5ef120e8b273b306008
RafaelKayumov <justpoc@gmail.com>
2025-11-27 11:54:49 +0300

    Update tests

--- WooCommerce/WooCommerceTests/ViewRelated/Bookings/BookingDateTimeFilterViewTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Bookings/BookingDateTimeFilterViewTests.swift
@@ -5,20 +5,18 @@ import Combine
 @testable import WooCommerce
 
 final class BookingDateTimeFilterViewTests: XCTestCase {
-    func test_clear_button_is_disabled_initially_when_no_dates_selected() throws {
+    func test_clear_button_is_not_rendered_initially_when_no_dates_selected() throws {
         let sut = BookingDateTimeFilterView(startDate: nil, endDate: nil, onSelection: { _, _ in })
         let view = try sut.inspect()
 
-        let button = try view.find(button: "Clear")
-        XCTAssertTrue(button.isDisabled())
+        XCTAssertThrowsError(try view.find(button: "Clear"))
     }
 
-    func test_clear_button_is_enabled_when_dates_selected() throws {
+    func test_clear_button_is_rendered_when_dates_selected() throws {
         let date = Date()
         let sut = BookingDateTimeFilterView(startDate: date, endDate: date, onSelection: { _, _ in })
         let view = try sut.inspect()
 
-        let button = try view.find(button: "Clear")
-        XCTAssertFalse(button.isDisabled())
+        _ = try view.find(button: "Clear")
     }
 }

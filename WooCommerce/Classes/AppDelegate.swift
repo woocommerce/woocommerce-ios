8e19e497cb45c001fcb079fdc890ca27b824aeb0
Huong Do <hi.huongdo@gmail.com>
2025-12-03 15:24:27 +0700

    Request notification only when authenticated

--- WooCommerce/Classes/AppDelegate.swift
+++ WooCommerce/Classes/AppDelegate.swift
@@ -63,7 +63,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         setupCocoaLumberjack()
         setupLibraryLogger()
         setupLogLevel(.verbose)
-        setupPushNotificationsManagerIfPossible(pushNotesManager, stores: stores)
+        /// We're sending notifications for logged in state only.
+        /// Revisit this check if a local notification in unauthenticated state is needed.
+        if stores.isAuthenticated {
+            setupPushNotificationsManagerIfPossible(pushNotesManager, stores: stores)
+        }
         setupAppRatingManager()
         setupWormholy()
         setupKeyboardStateProvider()

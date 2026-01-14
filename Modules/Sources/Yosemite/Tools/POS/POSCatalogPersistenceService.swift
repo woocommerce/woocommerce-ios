4132d1389b261ecf122feb18ade6762464f7e539
iamgabrielma <gabrielmaldonado.art@gmail.com>
2026-01-14 15:57:19 +0900

    update console log with removal details

--- Modules/Sources/Yosemite/Tools/POS/POSCatalogPersistenceService.swift
+++ Modules/Sources/Yosemite/Tools/POS/POSCatalogPersistenceService.swift
@@ -91,7 +91,10 @@ final class POSCatalogPersistenceService: POSCatalogPersistenceServiceProtocol {
     }
 
     func persistIncrementalCatalogData(_ catalog: POSCatalog, siteID: Int64) async throws {
-        DDLogInfo("💾 Persisting incremental catalog with \(catalog.products.count) updated products and \(catalog.variations.count) updated variations")
+        DDLogInfo("💾 Persisting incremental catalog with \(catalog.products.count) updated products, " +
+                  "\(catalog.variations.count) updated variations, " +
+                  "\(catalog.productsToRemove.count) products to remove, " +
+                  "\(catalog.variationsToRemove.count) variations to remove")
 
         try await grdbManager.databaseConnection.write { db in
             for product in catalog.products {

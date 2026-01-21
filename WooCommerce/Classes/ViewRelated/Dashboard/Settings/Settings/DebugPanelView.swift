e66073d39fae6353ef07c8b4f0c57b0ae405b97a
Adam Borbas <adborbas@gmail.com>
2026-01-21 10:14:09 +0100

    Switch to Debug Panel

---
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Settings/DebugPanelView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct DebugPanelView: View {
+    var body: some View {
+        List {
+            Button {
+                UserDefaults.standard[.hasSavedPrivacyBannerSettings] = false
+            } label: {
+                Text("Reset Privacy Choice Banner State")
+            }
+
+            Button {
+                ServiceLocator.crashLogging.crash()
+            } label: {
+                Text("Crash Immediately")
+            }
+
+            NavigationLink(destination: OverrideFeatureFlagsView()) {
+                Text("Override Feature Flags")
+            }
+        }
+        .contentMargins(20)
+        .navigationTitle("Debug Panel")
+    }
+}

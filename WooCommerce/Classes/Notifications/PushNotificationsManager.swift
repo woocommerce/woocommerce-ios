ef6e541da7266d0257dd84a17cae017c3987ba97
Huong Do <hi.huongdo@gmail.com>
2026-01-23 13:28:31 +0700

    Remove the disabling after initialization

--- WooCommerce/Classes/Notifications/PushNotificationsManager.swift
+++ WooCommerce/Classes/Notifications/PushNotificationsManager.swift
@@ -155,7 +155,6 @@ final class PushNotificationsManager: PushNotesManager {
         self.backgroundSynchronizerFactory = backgroundSynchronizerFactory
         self.analytics = analytics
         self.selfDrivenPushNotificationEnabled = shouldRegisterSelfDrivenPushNotification
-        disableWPComPushNotificationsIfNeeded(siteIDs: siteIDsRegisteredForWooPNs, deviceID: deviceID)
     }
 }
 

98c6c496754b2cbb40499267bba2d85d3a07a994
Huong Do <hi.huongdo@gmail.com>
2026-01-27 10:26:34 +0700

    Fix attributed text on email login screen

--- WooCommerce/Classes/Authentication/WPComLogin/WPComEmailLoginView.swift
+++ WooCommerce/Classes/Authentication/WPComLogin/WPComEmailLoginView.swift
@@ -112,7 +112,7 @@ struct WPComEmailLoginView: View {
                 .disabled(viewModel.emailOrUsername.isEmpty)
 
                 // Terms label
-                AttributedText(viewModel.termsAttributedString)
+                Text(viewModel.termsAttributedString)
             }
             .padding(Constants.contentPadding)
             .background(Color(uiColor: .systemBackground))

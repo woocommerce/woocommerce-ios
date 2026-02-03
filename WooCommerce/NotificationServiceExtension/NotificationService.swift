42afd7ddf8a532f6bf65aeb89b4016370602b9f9
Huong Do <hi.huongdo@gmail.com>
2026-02-03 15:34:37 +0700

    Update notification service to block WPCom PNs

--- WooCommerce/NotificationServiceExtension/NotificationService.swift
+++ WooCommerce/NotificationServiceExtension/NotificationService.swift
@@ -1,28 +1,41 @@
+import Foundation
 import UserNotifications
 
 final class NotificationService: UNNotificationServiceExtension {
 
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
-
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
+        let content = request.content
+        if shouldBlockNotifications(with: content) {
+            return
         }
+        contentHandler(content)
     }
     
     override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
+        // No-op
+    }
+}
+
+private extension NotificationService {
+    func shouldBlockNotifications(with content: UNNotificationContent) -> Bool {
+        let userInfo = content.userInfo
+        let registeredSites = UserDefaults.standard.string(forKey: Constants.registeredIDsKey)?
+            .components(separatedBy: ",")
+            .compactMap { Int64($0) }
+
+        if let siteID = userInfo[Constants.APNSKey.siteID] as? Int64,
+           let _ = userInfo[Constants.APNSKey.noteID] as? Int64,
+           registeredSites?.contains(siteID) == true {
+            return true
         }
+        return false
     }
 
+    enum Constants {
+        enum APNSKey {
+            static let siteID = "blog"
+            static let noteID = "note_id"
+        }
+        static let registeredIDsKey = "siteIDsRegisteredForWooPushNotifications"
+    }
 }

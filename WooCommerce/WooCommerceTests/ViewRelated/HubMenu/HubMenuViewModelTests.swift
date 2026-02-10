f17a124d9d33a6aa5b3b836b3c3f8499a05eb7b3
RafaelKayumov <justpoc@gmail.com>
2026-02-10 15:13:07 +0300

    Add test to hide bookings from menu

--- WooCommerce/WooCommerceTests/ViewRelated/HubMenu/HubMenuViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/HubMenu/HubMenuViewModelTests.swift
@@ -523,6 +523,40 @@ final class HubMenuViewModelTests: XCTestCase {
     }
 
     @MainActor
+    func test_menuElements_exclude_bookings_on_ipad_when_pos_not_available() {
+        // Given
+        let stores = MockStoresManager(sessionManager: .makeForTesting())
+        stores.updateDefaultStore(storeID: sampleSiteID)
+        stores.updateDefaultStore(.fake().copy(siteID: sampleSiteID))
+
+        let mockBookingsEligibilityChecker = MockBookingsEligibilityChecker(isEligible: true)
+        let posEligibilityService = MockPOSEligibilityService()
+        posEligibilityService.cachedTabVisibility[sampleSiteID] = false
+
+        // When
+        let viewModel = HubMenuViewModel(
+            siteID: sampleSiteID,
+            tapToPayBadgePromotionChecker: TapToPayBadgePromotionChecker(),
+            stores: stores,
+            posEligibilityService: posEligibilityService,
+            bookingsEligibilityCheckerFactory: { _ in mockBookingsEligibilityChecker },
+            isPad: true
+        )
+        viewModel.setupMenuElements()
+
+        waitUntil {
+            viewModel.generalElements.firstIndex(where: { item in
+                item.id == HubMenuViewModel.Bookings.id
+            }) == nil
+        }
+
+        // Then
+        XCTAssertNil(viewModel.generalElements.firstIndex(where: { item in
+            item.id == HubMenuViewModel.Bookings.id
+        }))
+    }
+
+    @MainActor
     func test_showPayments_replaces_navigationPath_with_payments() {
         // Given
         var navigationPath = NavigationPath(["testPath1", "testPath2"])

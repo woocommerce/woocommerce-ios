0818e1a9471b50bb71cc5a0f50f5d75827f89ed0
iamgabrielma <gabrielmaldonado.art@gmail.com>
2026-02-12 19:05:21 +0700

    updateAttendanceStatus call controller service

--- Modules/Sources/Yosemite/PointOfSale/BookingList/POSBookingServiceProtocol.swift
+++ Modules/Sources/Yosemite/PointOfSale/BookingList/POSBookingServiceProtocol.swift
@@ -13,4 +13,6 @@ public protocol POSBookingServiceProtocol: Sendable {
                        searchQuery: String?) async throws -> PagedItems<POSBooking>
 
     func cancelBooking(bookingID: Int64) async throws
+
+    func updateAttendanceStatus(bookingID: Int64, status: BookingAttendanceStatus) async throws
 }

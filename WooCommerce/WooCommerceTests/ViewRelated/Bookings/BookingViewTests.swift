a248c97d5e71b8e1c4c3011c7652e50537672bc1
RafaelKayumov <justpoc@gmail.com>
2026-02-12 13:47:21 +0300

    Add tests

---
+++ WooCommerce/WooCommerceTests/ViewRelated/Bookings/BookingViewTests.swift
@@ -0,0 +1,27 @@
+import XCTest
+import Yosemite
+@testable import WooCommerce
+
+final class BookingViewTests: XCTestCase {
+    func test_bookingItemHeaderStatusBadge_returnsBookingStatusWhenCancelled() {
+        let booking = Booking.fake().copy(
+            statusKey: "cancelled",
+            attendanceStatusKey: "booked"
+        )
+
+        let badge = booking.bookingItemHeaderStatusBadge
+
+        XCTAssertEqual(badge as? BookingStatus, .cancelled)
+    }
+
+    func test_bookingItemHeaderStatusBadge_returnsAttendanceStatusWhenNotCancelled() {
+        let booking = Booking.fake().copy(
+            statusKey: "paid",
+            attendanceStatusKey: "no-show"
+        )
+
+        let badge = booking.bookingItemHeaderStatusBadge
+
+        XCTAssertEqual(badge as? BookingAttendanceStatus, .noShow)
+    }
+}

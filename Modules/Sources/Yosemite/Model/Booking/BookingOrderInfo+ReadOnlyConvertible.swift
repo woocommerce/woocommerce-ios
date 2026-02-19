90eddbeb1afb6becd741a58b96f04f348bed7c89
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2026-02-19 15:35:31 +0200

    Wire new BookingOrderInfo fields through ReadOnlyConvertible
    Update update(with:) and toReadOnly() to include orderID,
    orderNumber, dateCreated, datePaid, and discountTotal. Use
    update(with:) in BookingStore upsert instead of setting
    statusKey manually, so all fields are persisted.

--- Modules/Sources/Yosemite/Model/Booking/BookingOrderInfo+ReadOnlyConvertible.swift
+++ Modules/Sources/Yosemite/Model/Booking/BookingOrderInfo+ReadOnlyConvertible.swift
@@ -6,11 +6,21 @@ import Storage
 extension Storage.BookingOrderInfo: ReadOnlyConvertible {
     public func update(with orderInfo: Yosemite.BookingOrderInfo) {
         statusKey = orderInfo.statusKey
+        orderID = orderInfo.orderID
+        orderNumber = orderInfo.orderNumber
+        dateCreated = orderInfo.dateCreated
+        datePaid = orderInfo.datePaid
+        discountTotal = orderInfo.discountTotal
         // Relationships are handled in BookingStore
     }
 
     public func toReadOnly() -> Yosemite.BookingOrderInfo {
         return .init(statusKey: statusKey ?? "",
+                     orderID: orderID,
+                     orderNumber: orderNumber,
+                     dateCreated: dateCreated,
+                     datePaid: datePaid,
+                     discountTotal: discountTotal,
                      paymentInfo: paymentInfo?.toReadOnly(),
                      customerInfo: customerInfo?.toReadOnly(),
                      productInfo: productInfo?.toReadOnly())

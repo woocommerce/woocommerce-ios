68fbe9fe2252461cddb8b4ae3839ecb4f30128ae
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2026-02-19 15:35:00 +0200

    Add order fields to Networking BookingOrderInfo
    Extract orderID, orderNumber, dateCreated, datePaid, and
    discountTotal from Order when constructing BookingOrderInfo.
    These fields are needed to build POSOrder from cached data.

--- Modules/Sources/Networking/Model/Bookings/BookingOrderInfo.swift
+++ Modules/Sources/Networking/Model/Bookings/BookingOrderInfo.swift
@@ -3,21 +3,41 @@ import Foundation
 
 public struct BookingOrderInfo: Hashable {
     public let statusKey: String
+    public let orderID: Int64
+    public let orderNumber: String?
+    public let dateCreated: Date?
+    public let datePaid: Date?
+    public let discountTotal: String?
     public let paymentInfo: BookingPaymentInfo?
     public let customerInfo: BookingCustomerInfo?
     public let productInfo: BookingProductInfo?
 
     public init(statusKey: String,
+                orderID: Int64 = 0,
+                orderNumber: String? = nil,
+                dateCreated: Date? = nil,
+                datePaid: Date? = nil,
+                discountTotal: String? = nil,
                 paymentInfo: BookingPaymentInfo?,
                 customerInfo: BookingCustomerInfo?,
                 productInfo: BookingProductInfo?) {
         self.statusKey = statusKey
+        self.orderID = orderID
+        self.orderNumber = orderNumber
+        self.dateCreated = dateCreated
+        self.datePaid = datePaid
+        self.discountTotal = discountTotal
         self.paymentInfo = paymentInfo
         self.customerInfo = customerInfo
         self.productInfo = productInfo
     }
 
     public init(booking: Booking, order: Order) {
+        self.orderID = order.orderID
+        self.orderNumber = order.number
+        self.dateCreated = order.dateCreated
+        self.datePaid = order.datePaid
+        self.discountTotal = order.discountTotal
         self.customerInfo = {
             guard let billingAddress = order.billingAddress else {
                 return nil

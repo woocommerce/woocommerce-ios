59d332b08b987cc5d18640fecdea4504adb28f20
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2026-02-25 17:51:25 +0200

    Revert unreleased Core Data migration

--- Modules/Sources/Networking/Model/Bookings/BookingOrderInfo.swift
+++ Modules/Sources/Networking/Model/Bookings/BookingOrderInfo.swift
@@ -3,41 +3,21 @@ import Foundation
 
 public struct BookingOrderInfo: Hashable {
     public let statusKey: String
-    public let orderID: Int64
-    public let orderNumber: String
-    public let dateCreated: Date
-    public let datePaid: Date?
-    public let discountTotal: String
     public let paymentInfo: BookingPaymentInfo?
     public let customerInfo: BookingCustomerInfo?
     public let productInfo: BookingProductInfo?
 
     public init(statusKey: String,
-                orderID: Int64 = 0,
-                orderNumber: String = "",
-                dateCreated: Date = Date(),
-                datePaid: Date? = nil,
-                discountTotal: String = "",
                 paymentInfo: BookingPaymentInfo?,
                 customerInfo: BookingCustomerInfo?,
                 productInfo: BookingProductInfo?) {
         self.statusKey = statusKey
-        self.orderID = orderID
-        self.orderNumber = orderNumber
-        self.dateCreated = dateCreated
-        self.datePaid = datePaid
-        self.discountTotal = discountTotal
         self.paymentInfo = paymentInfo
         self.customerInfo = customerInfo
         self.productInfo = productInfo
     }
 
     public init(booking: Booking, order: Order) {
-        self.orderID = order.orderID
-        self.orderNumber = order.number
-        self.dateCreated = order.dateCreated
-        self.datePaid = order.datePaid
-        self.discountTotal = order.discountTotal
         self.customerInfo = {
             guard let billingAddress = order.billingAddress else {
                 return nil
--- Modules/Sources/Yosemite/Model/Booking/BookingOrderInfo+ReadOnlyConvertible.swift
+++ Modules/Sources/Yosemite/Model/Booking/BookingOrderInfo+ReadOnlyConvertible.swift
@@ -6,21 +6,11 @@ import Storage
 extension Storage.BookingOrderInfo: ReadOnlyConvertible {
     public func update(with orderInfo: Yosemite.BookingOrderInfo) {
         statusKey = orderInfo.statusKey
-        orderID = orderInfo.orderID
-        orderNumber = orderInfo.orderNumber
-        dateCreated = orderInfo.dateCreated
-        datePaid = orderInfo.datePaid
-        discountTotal = orderInfo.discountTotal
         // Relationships are handled in BookingStore
     }
 
     public func toReadOnly() -> Yosemite.BookingOrderInfo {
         return .init(statusKey: statusKey ?? "",
-                     orderID: orderID,
-                     orderNumber: orderNumber ?? "",
-                     dateCreated: dateCreated ?? Date(),
-                     datePaid: datePaid,
-                     discountTotal: discountTotal ?? "",
                      paymentInfo: paymentInfo?.toReadOnly(),
                      customerInfo: customerInfo?.toReadOnly(),
                      productInfo: productInfo?.toReadOnly())

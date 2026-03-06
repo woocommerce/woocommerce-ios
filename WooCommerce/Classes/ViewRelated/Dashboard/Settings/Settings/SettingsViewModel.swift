a1591054b1b6c3a96b68bd57e9c3fbb255eee583
Huong Do <hi.huongdo@gmail.com>
2026-03-06 12:04:45 +0700

    Show enable PN in Settings for JCP site

--- WooCommerce/Classes/ViewRelated/Dashboard/Settings/Settings/SettingsViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Dashboard/Settings/Settings/SettingsViewModel.swift
@@ -384,7 +384,7 @@ private extension SettingsViewModel {
     }
 
     func shouldShowEnablePushNotificationsRow(siteID: Int64) -> Bool {
-        guard stores.isAuthenticatedWithoutWPCom else {
+        guard stores.isAuthenticatedWithoutWPCom || stores.sessionManager.defaultSite?.isJetpackCPConnected == true else {
             return false
         }
 

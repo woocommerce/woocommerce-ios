2b84cd670e81e20ed3281e089914675f1083cb3d
Huong Do <hi.huongdo@gmail.com>
2026-03-09 20:48:06 +0700

    Update URL base following recent changes

--- WooCommerce/Classes/Routing/ProductDetail/ProductAdminURLProvider.swift
+++ WooCommerce/Classes/Routing/ProductDetail/ProductAdminURLProvider.swift
@@ -6,8 +6,9 @@ enum ProductAdminURLProvider {
 
     static func editURL(for product: Product, site: Site?) -> URL? {
         guard let base = site?.adminURLWithFallback() else { return nil }
+        let adminBase = base.appending(path: "admin.php", directoryHint: .notDirectory)
 
-        var components = URLComponents(url: base, resolvingAgainstBaseURL: false)!
+        var components = URLComponents(url: adminBase, resolvingAgainstBaseURL: false)!
         components.queryItems = [
             URLQueryItem(name: "page", value: "next-admin"),
             URLQueryItem(name: "p", value: "/woocommerce/services/edit/\(product.productID)")

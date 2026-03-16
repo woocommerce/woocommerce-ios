0b21f9bd03f2a7b896fba6ac096a131f23f3a227
RafaelKayumov <justpoc@gmail.com>
2026-03-16 14:59:43 +0300

    Update booking filter to use user query param and userID predicate
    The Bookings v2 API now uses 'user' for WP user ID filtering
    (previously 'customer'). The local DB predicate also switches from
    customerID to userID to match the new field semantics.

--- Modules/Sources/Networking/Remote/BookingsRemote.swift
+++ Modules/Sources/Networking/Remote/BookingsRemote.swift
@@ -138,7 +138,7 @@ public final class BookingsRemote: Remote, BookingsRemoteProtocol {
             }
 
             if filters.customerIDs.isNotEmpty {
-                parameters[ParameterKey.customer] = filters.customerIDs.map(String.init)
+                parameters[ParameterKey.user] = filters.customerIDs.map(String.init)
             }
 
             if filters.resourceIDs.isNotEmpty {
@@ -341,7 +341,7 @@ public extension BookingsRemote {
         static let order: String           = "order"
         static let orderBy: String         = "orderby"
         static let product: String         = "product"
-        static let customer: String        = "customer"
+        static let user: String             = "user"
         static let resource: String        = "resource"
         static let attendanceStatus        = "attendance_status"
         static let paymentStatus           = "booking_status" // to be updated later when payment filtering is supported

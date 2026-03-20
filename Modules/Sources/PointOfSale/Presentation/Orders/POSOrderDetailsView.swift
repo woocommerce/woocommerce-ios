063e4500d9c062513705fd28ae7c3ad7b282247e
iamgabrielma <gabrielmaldonado.art@gmail.com>
2026-03-20 15:58:29 +0700

    clarification comment

--- Modules/Sources/PointOfSale/Presentation/Orders/POSOrderDetailsView.swift
+++ Modules/Sources/PointOfSale/Presentation/Orders/POSOrderDetailsView.swift
@@ -183,6 +183,7 @@ private extension POSOrderDetailsView {
         var quantities: [Int64: Int] = [:]
         for refund in order.refunds {
             for item in refund.items {
+                // Accumulate the total refunded quantity for each item ID in the dictionary
                 guard let itemID = item.refundedItemID else { continue }
                 quantities[itemID, default: 0] += abs(NSDecimalNumber(decimal: item.quantity).intValue)
             }

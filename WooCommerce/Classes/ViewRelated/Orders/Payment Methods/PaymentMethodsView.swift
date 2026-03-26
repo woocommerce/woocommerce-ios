898eec09a28a29f6b45a28968ffb127faf08590d
iamgabrielma <gabrielmaldonado.art@gmail.com>
2026-03-26 11:40:05 +0700

    use navigationDestination to address unwanted dismissal and double-tap troubles

--- WooCommerce/Classes/ViewRelated/Orders/Payment Methods/PaymentMethodsView.swift
+++ WooCommerce/Classes/ViewRelated/Orders/Payment Methods/PaymentMethodsView.swift
@@ -24,6 +24,8 @@ struct PaymentMethodsView: View {
 
     @State private var showingScanToPayView = false
 
+    @State private var showingLearnMore = false
+
     ///   Environment safe areas
     ///
     @Environment(\.safeAreaInsets) var safeAreaInsets: EdgeInsets
@@ -94,16 +96,14 @@ struct PaymentMethodsView: View {
                     .padding(.horizontal)
                     .background(Color(.listForeground(modal: false)))
 
-                    NavigationLink(destination: AuthenticatableWebView(url: viewModel.learnMoreViewModel.url,
-                                                                       onDismiss: {
-                                                    viewModel.syncSiteAndRefreshVisibility()
-                                                })
-                                                .onAppear {
-                                                    viewModel.learnMoreViewModel.learnMoreTapped()
-                                                }
-                    ) {
+                    Button {
+                        showingLearnMore = true
+                        viewModel.learnMoreViewModel.learnMoreTapped()
+                    } label: {
                         AttributedText(viewModel.learnMoreViewModel.learnMoreAttributedString)
-                    }.padding(.horizontal)
+                            .allowsHitTesting(false)
+                    }
+                    .padding(.horizontal)
                 }
 
                 // Pushes content to the top
@@ -138,6 +138,12 @@ struct PaymentMethodsView: View {
                 }
             }
                 .background(FullScreenCoverClearBackgroundView())
+        }
+        .navigationDestination(isPresented: $showingLearnMore) {
+            AuthenticatableWebView(url: viewModel.learnMoreViewModel.url,
+                                   onDismiss: {
+                viewModel.syncSiteAndRefreshVisibility()
+            })
         }
         .navigationDestination(isPresented: $showingCashAlert) {
             CashPaymentTenderView(viewModel: CashPaymentTenderViewModel(total: viewModel.total,

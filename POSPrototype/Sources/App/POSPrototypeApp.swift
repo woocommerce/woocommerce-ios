7e23e87345051c560957f3755a1ae48c82edcb34
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2026-04-07 09:58:29 +0300

    Point Inject library to InjectionNext bundle path

--- POSPrototype/Sources/App/POSPrototypeApp.swift
+++ POSPrototype/Sources/App/POSPrototypeApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import Inject
 
 @main
 struct POSPrototypeApp: App {
+    init() {
+        // Point Inject library to InjectionNext's bundle path instead of InjectionIII's
+        InjectConfiguration.bundlePath = "/Applications/InjectionNext.app/Contents/Resources/"
+    }
+
     private let scenarios: [any POSPrototypeScenario] = [
         SimpleStoreScenario(),
         LargeCatalogScenario(),

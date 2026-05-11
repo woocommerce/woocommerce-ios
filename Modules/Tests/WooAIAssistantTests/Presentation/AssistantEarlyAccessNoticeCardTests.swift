3de00a6b5dbb46a5618cf2bdbe178d29bef5d9cf
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2026-05-11 15:52:11 +0300

    Test early access notice card invokes feedback handler on tap

---
+++ Modules/Tests/WooAIAssistantTests/Presentation/AssistantEarlyAccessNoticeCardTests.swift
@@ -0,0 +1,21 @@
+import Foundation
+import Testing
+@testable import WooAIAssistant
+
+@Suite(.timeLimit(.minutes(1)))
+@MainActor
+struct AssistantEarlyAccessNoticeCardTests {
+
+    @Test
+    func test_AssistantEarlyAccessNoticeCard_when_feedback_tapped_then_invokes_handler() {
+        // Given
+        var fired = false
+        let card = AssistantEarlyAccessNoticeCard(onFeedbackTap: { fired = true })
+
+        // When
+        card.onFeedbackTap()
+
+        // Then
+        #expect(fired == true)
+    }
+}

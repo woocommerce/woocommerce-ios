7aca50b85abc05f24ce6ac58d8043770c27679ff
Rafael Bartolome <rafael.bartolome@automattic.com>
2026-05-12 17:41:08 +0200

    Remove unused store stats date range property

--- WooCommerce/StoreWidgets/StoreStatsWidgetDateRange+Service.swift
+++ WooCommerce/StoreWidgets/StoreStatsWidgetDateRange+Service.swift
@@ -1,13 +1,6 @@
 import Foundation
 
 extension StoreStatsWidgetDateRange {
-    /// Maps the user-selected widget range to the primitive parameters consumed by
-    /// `StoreInfoDataService.fetchStats(for:dateRange:)`.
-    ///
-    var serviceDateRange: StoreInfoDataService.DateRange {
-        serviceDateRange()
-    }
-
     /// Maps the user-selected widget range using the selected store's timezone.
     ///
     func serviceDateRange(timezone: TimeZone = .current) -> StoreInfoDataService.DateRange {

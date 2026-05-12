904d79992ca049c4a711913df8d161673068305f
Povilas Staskus <4062343+staskus@users.noreply.github.com>
2026-05-12 13:58:29 +0300

    Remove the legacy Jetpack AI client and JWT plumbing

--- Modules/Sources/WooAIAssistant/Protocols/AssistantJWTProviding.swift
+++
@@ -1,9 +0,0 @@
-public protocol AssistantJWTProviding: Sendable {
-    func currentJWT() async throws -> String
-    func invalidate() async
-}
-
-public extension AssistantJWTProviding {
-    // Default for non-caching conformers (e.g. test stubs) that have no token to drop.
-    func invalidate() async {}
-}

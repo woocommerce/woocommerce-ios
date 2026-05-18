14fade44b963d87d180a1006919078cee20ee540
Gio Lodi <gio@mokacoding.com>
2026-05-18 14:01:48 +1000

    Enable `redundant_nil_coalescing` SwiftLint rule

--- Modules/Tests/PointOfSaleTests/Mocks/MockPluginsService.swift
+++ Modules/Tests/PointOfSaleTests/Mocks/MockPluginsService.swift
@@ -9,7 +9,7 @@ final class MockPluginsService: PluginsServiceProtocol {
     }
 
     func loadPluginInStorage(siteID: Int64, plugin: Yosemite.Plugin, isActive: Bool?) -> SystemPlugin? {
-        pluginsToReturnForLoadPluginInStorageByPlugin[plugin] ?? nil
+        pluginsToReturnForLoadPluginInStorageByPlugin[plugin]
     }
 }
 

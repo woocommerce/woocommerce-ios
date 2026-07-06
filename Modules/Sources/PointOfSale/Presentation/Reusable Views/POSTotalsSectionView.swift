23abc9c4206c2da2b77d9b2bebd2671c7245ea54
Jaclyn Chen <watertranquil@gmail.com>
2026-07-06 17:30:04 -0400

    Fix POS order details link color

--- Modules/Sources/PointOfSale/Presentation/Reusable Views/POSTotalsSectionView.swift
+++ Modules/Sources/PointOfSale/Presentation/Reusable Views/POSTotalsSectionView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import WooFoundation
 import struct Yosemite.POSOrderRefund
 
 /// A reusable totals breakdown card used by both order details and booking details.
@@ -143,7 +144,7 @@ struct POSTotalsSectionView: View {
                 } label: {
                     Text(Localization.viewDetailsLabel)
                         .font(.posBodyMediumRegular())
-                        .foregroundStyle(Color.posPrimary)
+                        .foregroundStyle(Color(uiColor: .textLink))
                         .underline()
                 }
             }

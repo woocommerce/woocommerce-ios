9e6ecb409ad2eb1e5659cc0f92395b3b530d6e63
iamgabrielma <gabrielmaldonado.art@gmail.com>
2026-07-20 13:10:46 +0700

    add tests to confirm non-numeric price returns dash

--- WooCommerce/WooCommerceTests/ViewRelated/Orders/Order Creation/ProductRowViewModelTests.swift
+++ WooCommerce/WooCommerceTests/ViewRelated/Orders/Order Creation/ProductRowViewModelTests.swift
@@ -130,6 +130,19 @@ final class ProductRowViewModelTests: XCTestCase {
                       "Expected label to contain \"\(expectedPriceLabel)\" but actual label was \"\(viewModel.productDetailsLabel)\"")
     }
 
+    func test_priceAndDiscountsLabel_when_price_is_non_numeric_then_returns_dash() {
+        // Given
+        let price = "not-a-number"
+        let product = Product.fake().copy(price: price)
+        let currencyFormatter = CurrencyFormatter(currencySettings: CurrencySettings())
+
+        // When
+        let viewModel = ProductRowViewModel(product: product, currencyFormatter: currencyFormatter)
+
+        // Then
+        assertEqual("-", viewModel.priceAndDiscountsLabel)
+    }
+
     func test_view_model_creates_expected_label_for_product_with_no_price() {
         // Given
         let price = ""

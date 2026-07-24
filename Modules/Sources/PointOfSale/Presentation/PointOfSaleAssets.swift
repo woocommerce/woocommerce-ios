b1915a32012e1e2c4eed7e0bc35a85cfa4d7b266
Rubens Castro <rubens.castro@a8c.com>
2026-07-24 17:33:06 -0300

    Replaces illustrations files and sets light and dark mode for each

--- Modules/Sources/PointOfSale/Presentation/PointOfSaleAssets.swift
+++ Modules/Sources/PointOfSale/Presentation/PointOfSaleAssets.swift
@@ -13,6 +13,9 @@ enum PointOfSaleAssets: CaseIterable {
     case readerDisconnected
     case successCheck
     case gears
+    case coupons
+    case shoppingBags
+    case location
     case barcodeFieldScreenshot
     case starBsh20SetupBarcode
     case tera12002DHIDBarcode
@@ -54,6 +57,12 @@ enum PointOfSaleAssets: CaseIterable {
             "pos-success-check"
         case .gears:
             "pos-gears"
+        case .coupons:
+            "pos-coupons"
+        case .shoppingBags:
+            "pos-shopping-bags"
+        case .location:
+            "pos-location"
         case .barcodeFieldScreenshot:
             "barcode-field-screenshot"
         case .starBsh20SetupBarcode:

0a130a3564607118698b8b5e959f6e3cb1a713dd
RafaelKayumov <justpoc@gmail.com>
2026-09-04 16:43:21 +0300

    Fix modifier order in the consent blocker and normalize the release note priority

--- WooCommerce/Classes/ViewRelated/AgeGate/SignificantChangeConsentBlockingView.swift
+++ WooCommerce/Classes/ViewRelated/AgeGate/SignificantChangeConsentBlockingView.swift
@@ -24,7 +24,7 @@ final class SignificantChangeConsentBlockingHostingController: UIHostingControll
     }
 
     @available(*, unavailable)
-    required dynamic init?(coder aDecoder: NSCoder) {
+    dynamic required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }

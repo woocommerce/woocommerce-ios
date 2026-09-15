912c3576b9a191f9e0705aa8ffb7b63a1bacc9ec
iamgabrielma <gabrielmaldonado.art@gmail.com>
2026-09-15 15:51:46 +0700

    remove cancellable on deinit
    AnyCancellable is documented to call cancel() on itself when it is deallocated. Once the class is MainActor, every stored property belongs to the main actor. A deinit is always nonisolated, because Swift cannot guarantee which thread releases the last reference.

--- WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewModel.swift
+++ WooCommerce/Classes/ViewRelated/Products/Edit Product/ProductFormViewModel.swift
@@ -230,7 +230,6 @@ final class ProductFormViewModel: ProductFormViewModelProtocol {
     ///
     private lazy var remoteActionUseCase = ProductFormRemoteActionUseCase(stores: stores)
 
-    @MainActor
     init(product: EditableProductModel,
          formType: ProductFormType,
          productImageActionHandler: ProductImageActionHandler,
@@ -261,10 +260,6 @@ final class ProductFormViewModel: ProductFormViewModelProtocol {
         updateVariationsPriceState()
         configureResultsController()
         updateBlazeEligibility()
-    }
-
-    deinit {
-        cancellable?.cancel()
     }
 
     func hasUnsavedChanges() -> Bool {
@@ -913,7 +908,6 @@ private extension ProductFormViewModel {
 // MARK: Favorite
 //
 extension ProductFormViewModel {
-    @MainActor
     func isFavorite() async -> Bool {
         await favoriteProductsUseCase.isFavorite(productID: product.productID)
     }
